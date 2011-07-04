9514ee9eb87b0c64a535b70f5355ea8b34469bad
Ravi Dey <ravi@iwebnotes.com>
2011-07-04 16:49:25 +0530

    support feed revert

--- home/__init__.py
+++ home/__init__.py
@@ -96,5 +96,5 @@ def update_feed(doc):
 	"adds a new feed"
 	subject, color = feed_dict.get(doc.doctype, [None, None])
 	if subject:
-     subject = subject % doc.fields
-     make_feed(doc, subject, color)
+		subject = subject % doc.fields
+		make_feed(doc, subject, color)
