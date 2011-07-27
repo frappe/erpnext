5f3010735b60e20df00f1fb6003bfba53e531dee
Rushabh Mehta <rmehta@gmail.com>
2011-07-27 17:43:21 +0530

    updated email digest ui

--- setup/doctype/email_digest/email_digest.coffee
+++ setup/doctype/email_digest/email_digest.coffee
@@ -1,29 +1,46 @@
 content_items = ['Sales','Expenses','Bank Balance','Activity']
-freq = ['Daily', 'Weekly']
 
 # make a grid with items and columns of checkboxes
 # Parameters:
+#   parent
 # 	label (main heading)
 #	items = [] (rows)
 #	columns = [] (columns of checks)
+#	widths
+#	description
 
 class CheckGrid
-	constructor: (@parent, @label, @items, @columns) ->
-		@tab = make_table @parent, @items.length + 1, @columns.length + 1, '80%'
+	constructor: (@args) ->
+		$.extend @, args
+		@wrapper = $a @parent, 'div', 'check-grid round'
+		@render()
+		
+	render: ->
+		$a @wrapper, 'h3', 'check-grid-title', null, @label
+		
+		if @description
+			$a @wrapper, 'div', 'help-box', null, @description
+		
+		@tab = make_table @wrapper, @items.length + 1, @columns.length, '100%', @widths
 		@checks = {}
 
 		# render heads
 		for i in [0..@columns.length-1]
-			$td(@tab, 0, i+1).innerHTML = @columns[i]
-		
+			$($td(@tab, 0, i))
+				.addClass('check-grid-head gradient')
+				.html @columns[i]
+
+		@render_rows()
+	
+	render_rows: ->
 		# render rows
 		for i in [0..@items.length-1]
 			$td(@tab, i+1, 0).innerHTML = @items[i]
 			
 			# render checkboxes for this row
 			@checks[@items[i]] = {}
-			for c in [0..@columns.length-1]
-				check = $a_input $td(@tab, i+1, c+1), 'checkbox'
+			for c in [1..@columns.length-1]
+				check = $a_input $td(@tab, i+1, c), 'checkbox'
 				
 				# tag keys to checkbox
 				check.item = @items[i]
@@ -38,8 +55,7 @@ class CheckGrid
 		for item in keys @checks
 			for column in keys @checks[item]
 				check = @checks[item][column]
-				if not val[check.item]
-					val[check.item] = {}
+				val[check.item] or= {}
 				val[check.item][check.column] = if check.checked then 1 else 0
 		val
 	
@@ -47,25 +63,39 @@ class CheckGrid
 	set: (val) =>
 		for item in keys @checks
 			for column in keys @checks[item]
-				check = @checks[item][column] 
-				check.checked = val[check.item][check.row]
+				if val[item][column]
+					@checks[item][column] .checked = val[item][column] 
+		return
 
 # attach it to onload
 cx = cur_frm.cscript
 cx.onload = (doc, dt, dn) ->
 
 	# make the content grid
-	cx.content_grid = new CheckGrid cur_frm.fields_dict.Body.wrapper, 'Email Settings',
-		content_items, freq
+	cx.content_grid = new CheckGrid 
+		parent: cur_frm.fields_dict.Body.wrapper
+		label: 'Email Settings'
+		items: content_items
+		columns: ['Item','Daily','Weekly']
+		widths: ['60%', '20%', '20%']
+		description: 'Select items to be compiled for Email Digest'
 
 	# make the email grid
-	cx.email_grid = new CheckGrid cur_frm.fields_dict.Body.wrapper, 'Send To',
-		['test1@erpnext', 'test2@erpnext'], freq
+	cx.email_grid = new CheckGrid 
+		parent: cur_frm.fields_dict.Body.wrapper
+		label: 'Send To'
+		items: ['test1@erpnext', 'test2@erpnext']
+		columns: ['Email','Daily','Weekly']
+		widths: ['60%', '20%', '20%']
+		description: 'Select who gets daily and weekly mails'
+		
+	cx.content_grid.set JSON.parse doc.content_config if doc.content_config
+	cx.email_grid.set JSON.parse doc.email_config if doc.email_config
+	
+	return
 
 # update the data before sending
 cx.validate = (doc, dt, dn) ->
 	doc.content_config = JSON.stringify cx.content_grid.get()
 	doc.email_config = JSON.stringify cx.email_grid.get()
-
-	
 		
--- setup/doctype/email_digest/email_digest.js
+++ setup/doctype/email_digest/email_digest.js
@@ -1,35 +1,51 @@
 (function() {
-  var CheckGrid, content_items, cx, freq;
+  var CheckGrid, content_items, cx;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   content_items = ['Sales', 'Expenses', 'Bank Balance', 'Activity'];
-  freq = ['Daily', 'Weekly'];
   CheckGrid = (function() {
-    function CheckGrid(parent, label, items, columns) {
-      var c, check, i, _ref, _ref2, _ref3;
-      this.parent = parent;
-      this.label = label;
-      this.items = items;
-      this.columns = columns;
+    function CheckGrid(args) {
+      this.args = args;
       this.set = __bind(this.set, this);
       this.get = __bind(this.get, this);
-      this.tab = make_table(this.parent, this.items.length + 1, this.columns.length + 1, '80%');
+      $.extend(this, args);
+      this.wrapper = $a(this.parent, 'div', 'check-grid round');
+      this.render();
+    }
+    CheckGrid.prototype.render = function() {
+      var i, _ref;
+      $a(this.wrapper, 'h3', 'check-grid-title', null, this.label);
+      if (this.description) {
+        $a(this.wrapper, 'div', 'help-box', null, this.description);
+      }
+      this.tab = make_table(this.wrapper, this.items.length + 1, this.columns.length, '100%', this.widths);
       this.checks = {};
       for (i = 0, _ref = this.columns.length - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
-        $td(this.tab, 0, i + 1).innerHTML = this.columns[i];
+        $($td(this.tab, 0, i)).addClass('check-grid-head gradient').html(this.columns[i]);
       }
-      for (i = 0, _ref2 = this.items.length - 1; 0 <= _ref2 ? i <= _ref2 : i >= _ref2; 0 <= _ref2 ? i++ : i--) {
+      return this.render_rows();
+    };
+    CheckGrid.prototype.render_rows = function() {
+      var c, check, i, _ref, _results;
+      _results = [];
+      for (i = 0, _ref = this.items.length - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
         $td(this.tab, i + 1, 0).innerHTML = this.items[i];
         this.checks[this.items[i]] = {};
-        for (c = 0, _ref3 = this.columns.length - 1; 0 <= _ref3 ? c <= _ref3 : c >= _ref3; 0 <= _ref3 ? c++ : c--) {
-          check = $a_input($td(this.tab, i + 1, c + 1), 'checkbox');
-          check.item = this.items[i];
-          check.column = this.columns[c];
-          this.checks[this.items[i]][this.columns[c]] = check;
-        }
+        _results.push((function() {
+          var _ref2, _results2;
+          _results2 = [];
+          for (c = 1, _ref2 = this.columns.length - 1; 1 <= _ref2 ? c <= _ref2 : c >= _ref2; 1 <= _ref2 ? c++ : c--) {
+            check = $a_input($td(this.tab, i + 1, c), 'checkbox');
+            check.item = this.items[i];
+            check.column = this.columns[c];
+            _results2.push(this.checks[this.items[i]][this.columns[c]] = check);
+          }
+          return _results2;
+        }).call(this));
       }
-    }
+      return _results;
+    };
     CheckGrid.prototype.get = function() {
-      var check, column, item, val, _i, _j, _len, _len2, _ref, _ref2;
+      var check, column, item, val, _i, _j, _len, _len2, _name, _ref, _ref2;
       val = {};
       _ref = keys(this.checks);
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
@@ -38,40 +54,52 @@
         for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
           column = _ref2[_j];
           check = this.checks[item][column];
-          if (!val[check.item]) {
-            val[check.item] = {};
-          }
+          val[_name = check.item] || (val[_name] = {});
           val[check.item][check.column] = check.checked ? 1 : 0;
         }
       }
       return val;
     };
     CheckGrid.prototype.set = function(val) {
-      var check, column, item, _i, _len, _ref, _results;
+      var column, item, _i, _j, _len, _len2, _ref, _ref2;
       _ref = keys(this.checks);
-      _results = [];
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         item = _ref[_i];
-        _results.push((function() {
-          var _j, _len2, _ref2, _results2;
-          _ref2 = keys(this.checks[item]);
-          _results2 = [];
-          for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
-            column = _ref2[_j];
-            check = this.checks[item][column];
-            _results2.push(check.checked = val[check.item][check.row]);
+        _ref2 = keys(this.checks[item]);
+        for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+          column = _ref2[_j];
+          if (val[item][column]) {
+            this.checks[item][column].checked = val[item][column];
           }
-          return _results2;
-        }).call(this));
+        }
       }
-      return _results;
     };
     return CheckGrid;
   })();
   cx = cur_frm.cscript;
   cx.onload = function(doc, dt, dn) {
-    cx.content_grid = new CheckGrid(cur_frm.fields_dict.Body.wrapper, 'Email Settings', content_items, freq);
-    return cx.email_grid = new CheckGrid(cur_frm.fields_dict.Body.wrapper, 'Send To', ['test1@erpnext', 'test2@erpnext'], freq);
+    cx.content_grid = new CheckGrid({
+      parent: cur_frm.fields_dict.Body.wrapper,
+      label: 'Email Settings',
+      items: content_items,
+      columns: ['Item', 'Daily', 'Weekly'],
+      widths: ['60%', '20%', '20%'],
+      description: 'Select items to be compiled for Email Digest'
+    });
+    cx.email_grid = new CheckGrid({
+      parent: cur_frm.fields_dict.Body.wrapper,
+      label: 'Send To',
+      items: ['test1@erpnext', 'test2@erpnext'],
+      columns: ['Email', 'Daily', 'Weekly'],
+      widths: ['60%', '20%', '20%'],
+      description: 'Select who gets daily and weekly mails'
+    });
+    if (doc.content_config) {
+      cx.content_grid.set(JSON.parse(doc.content_config));
+    }
+    if (doc.email_config) {
+      cx.email_grid.set(JSON.parse(doc.email_config));
+    }
   };
   cx.validate = function(doc, dt, dn) {
     doc.content_config = JSON.stringify(cx.content_grid.get());
