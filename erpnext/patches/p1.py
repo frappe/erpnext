69554511da879abe9d638dae0e7b6b3b9cc2f777
Nabin Hait <nabinhait@gmail.com>
2011-10-19 16:35:39 +0530

    fixes in patch

--- erpnext/patches/p1.py
+++ erpnext/patches/p1.py
@@ -1,4 +1,4 @@
 def execute():
 	import webnotes
 	if not webnotes.conn.sql("select name from tabDocFormat where parent = 'Receivable Voucher' and format != 'POS Invoice'"):
-		webnotes.conn.sql("update tabDocType set default_print_format = 'Standard' where name =  'Receivable Voucher' and default_print_format = ''")
+		webnotes.conn.sql("update tabDocType set default_print_format = 'Standard' where name =  'Receivable Voucher'")
