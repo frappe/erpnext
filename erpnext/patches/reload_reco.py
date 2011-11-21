2690e72c14ebf9541aa7114ca2f1455defcfaca7
Nabin Hait <nabinhait@gmail.com>
2011-11-21 17:27:36 +0530

    updated reco patch

--- erpnext/patches/reload_reco.py
+++ erpnext/patches/reload_reco.py
@@ -1,5 +1,6 @@
 def execute():
 	import webnotes
 	from webnotes.modules.module_manager import reload_doc
-
+	
 	reload_doc('stock', 'doctype', 'stock_reconciliation')
+	webnotes.conn.sql("delete from `tabDocField` where (label in ('Validate Data', 'Attachment HTML', 'Attachment') or fieldname in ('next_step', 'company', 'fiscal_year', 'amendment_date')) and parent = 'Stock Reconciliation'")
