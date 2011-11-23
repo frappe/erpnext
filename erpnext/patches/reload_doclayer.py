6b8d0bfb2afd60617474b263cc6fd571f9648f20
Anand Doshi <anand@iwebnotes.com>
2011-11-23 18:39:08 +0530

    Added patch to reload doclayer, doclayer field and print format doctypes

---
+++ erpnext/patches/reload_doclayer.py
@@ -0,0 +1,8 @@
+"""
+	Reload DocLayer, DocLayerField and Print Format doctypes
+"""
+def execute():
+	from webnotes.modules.module_manager import reload_doc
+	reload_doc('core', 'doctype', 'print_format')
+	reload_doc('core', 'doctype', 'doclayer')
+	reload_doc('core', 'doctype', 'doclayerfield')
