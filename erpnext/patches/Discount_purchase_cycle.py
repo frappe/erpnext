4e54d7a434847c9af615a1fc39c220757c284379
Nijil <nijil.y@live.com>
2011-11-24 16:18:42 +0530

    pvdetail and patch

--- erpnext/patches/Discount_purchase_cycle.py
+++ erpnext/patches/Discount_purchase_cycle.py
@@ -2,6 +2,6 @@ def execute():
 	import webnotes
 	from webnotes.modules.module_manager import reload_doc
 
-	reload_doc('accounts', 'doctype', 'payable_voucher')
-	reload_doc('buying', 'doctype', 'purchase_common')
+	reload_doc('accounts', 'doctype', 'pv_detail')
+	reload_doc('buying', 'doctype', 'po_detail')
 	reload_doc('stock', 'doctype', 'purchase_receipt_detail')
