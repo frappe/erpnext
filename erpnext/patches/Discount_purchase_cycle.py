536e0cb1c4668cbf284f16d955c623e76a9583eb
Nabin Hait <nabinhait@gmail.com>
2011-12-13 19:05:13 +0530

    fixes in patch

--- erpnext/patches/Discount_purchase_cycle.py
+++ erpnext/patches/Discount_purchase_cycle.py
@@ -5,5 +5,5 @@ def execute():
 	reload_doc('accounts', 'doctype', 'pv_detail')
 	reload_doc('buying', 'doctype', 'po_detail')
 	reload_doc('stock', 'doctype', 'purchase_receipt_detail')
-	if webnotes.conn.sql("select name from 'tabDocField` where parent = 'PO Detail' and fieldname = 'discount'"):
+	if webnotes.conn.sql("select name from `tabDocField` where parent = 'PO Detail' and fieldname = 'discount'"):
 		webnotes.conn.sql("update `tabPO Detail` set discount_rate=discount")
