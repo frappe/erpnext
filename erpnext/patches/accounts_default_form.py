b40f826c5a27115a8987606d823384e7dcd42418
Nijil Y <nijil.y@live.com>
2011-12-14 15:50:09 +0530

    .

--- erpnext/patches/accounts_default_form.py
+++ erpnext/patches/accounts_default_form.py
@@ -3,5 +3,4 @@ def execute():
         from webnotes.modules.module_manager import reload_doc
         reload_doc('setup', 'doctype', 'company')
         reload_doc('setup', 'doctype', 'manage_account')
-        
-~                                                          
+ 
