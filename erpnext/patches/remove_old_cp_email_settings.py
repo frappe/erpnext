ca39b180f5c0a7fbac9b14d238f0e7e28ca3fde6
Anand Doshi <anand@iwebnotes.com>
2011-12-15 15:40:15 +0530

    patch to remove old control panel email settings

---
+++ erpnext/patches/remove_old_cp_email_settings.py
@@ -0,0 +1,15 @@
+def execute():
+	"""
+		remove control panel email settings if automail.webnotestech.com
+	"""
+	from webnotes.model.doc import Document
+	cp = Document('Control Panel')
+	if cp:
+		if cp.outgoing_mail_server == 'mail.webnotestech.com':
+			cp.outgoing_mail_server = None;
+			cp.mail_login = None;
+			cp.mail_password = None;
+			cp.mail_port = None;
+			cp.auto_email_id = 'automail@erpnext.com'
+			cp.save()
+
