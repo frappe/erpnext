d82e8df7e56d8ef18540cc39ac5e7ec5b0693c26
Nijil Y <nijil.y@live.com>
2011-12-20 17:30:41 +0530

    patch update for auto indent with email notify

--- erpnext/patches/auto_indent.py
+++ erpnext/patches/auto_indent.py
@@ -3,5 +3,6 @@ def execute():
 	from webnotes.modules.module_manager import reload_doc
 	reload_doc('manage_account', 'doctype', 'auto_indent')
 	reload_doc('item', 'doctype', 'email_notify')
+	webnotes.conn.sql("alter table `tabSandbox` drop column `minimum_inventory_level`")
 	webnotes.conn.sql("update `tabItem` set re_order_level = min_order_qty where re_order_level=''")
 		
