35aa35bea3b12f866aa07ce9ceb098228b047e68
Nijil Y <nijil.y@live.com>
2011-12-21 11:50:20 +0530

    Update erpnext/patches/auto_indent.py

--- erpnext/patches/auto_indent.py
+++ erpnext/patches/auto_indent.py
@@ -1,8 +1,8 @@
 def execute():
 	import webnotes
 	from webnotes.modules.module_manager import reload_doc
-	reload_doc('manage_account', 'doctype', 'auto_indent')
-	reload_doc('item', 'doctype', 'email_notify')
+	reload_doc('setup', 'doctype', 'manage_account')
+	reload_doc('stock', 'doctype', 'item')
 	webnotes.conn.sql("alter table `tabSandbox` drop column `minimum_inventory_level`")
-	webnotes.conn.sql("update `tabItem` set re_order_level = min_order_qty where re_order_level=''")
+	webnotes.conn.sql("update `tabItem` set re_order_level = ifnull(re_order_level,min_order_qty) ")
 		
