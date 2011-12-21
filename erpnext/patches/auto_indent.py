36f58835e66eda277ebad51da3f0bc6048d94f13
Nijil Y <nijil.y@live.com>
2011-12-21 16:35:43 +0530

    Update erpnext/patches/auto_indent.py

--- erpnext/patches/auto_indent.py
+++ erpnext/patches/auto_indent.py
@@ -3,6 +3,6 @@ def execute():
 	from webnotes.modules.module_manager import reload_doc
 	reload_doc('setup', 'doctype', 'manage_account')
 	reload_doc('stock', 'doctype', 'item')
-	webnotes.conn.sql("delete from `tabDocField` where fieldname=`minimum_inventory_level`")
+	webnotes.conn.sql("delete from `tabDocField` where fieldname=`minimum_inventory_level` and parent='item'")
 	webnotes.conn.sql("update `tabItem` set re_order_level = minimum_inventory_level wehre ifnull(re_order_level,0) = 0 ")
 		
