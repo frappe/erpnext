a0286e986e918e3ff742d5abec304aa9345dd12e
Nabin Hait <nabinhait@gmail.com>
2011-12-26 11:08:32 +0530

    patch updated

--- erpnext/patches/update_gle_against_voucher_for_jv.py
+++ erpnext/patches/update_gle_against_voucher_for_jv.py
@@ -9,7 +9,7 @@ def execute():
 	jv = webnotes.conn.sql("select distinct parent from `tabJournal Voucher Detail` where docstatus = 1 and ifnull(against_jv, '') != ''")
 
 	for d in jv:
-		jv_obj = get_obj('Journal Voucher', d.journal_voucher, with_children=1)
+		jv_obj = get_obj('Journal Voucher', d[0], with_children=1)
 
 		# cancel
 		get_obj(dt='GL Control').make_gl_entries(jv_obj.doc, jv_obj.doclist, cancel =1, adv_adj = 1)
