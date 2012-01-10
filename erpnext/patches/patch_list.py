bc45f896f73c5f45891817c1855c84f327f72777
Anand Doshi <anand@iwebnotes.com>
2012-01-10 14:11:07 +0530

    Merged master with stable

--- erpnext/patches/patch_list.py
+++ erpnext/patches/patch_list.py
@@ -1,14 +1,5 @@
 patch_list = [
 	{
-		'patch_file': 'reload_print_format',
-		'description': 'Reload doctype print format'
-	},
-	{
-		'patch_module': 'patches',
-		'patch_file': 'reload_project_task',
-		'description': 'Reload doctype task of project module'
-	},
-	{
 		'patch_module': 'patches.jan_mar_2012',
 		'patch_file': 'stable_branch_shift_09_01_12',
 		'description': 'Various Reloads for shifting branch from master to stable'
