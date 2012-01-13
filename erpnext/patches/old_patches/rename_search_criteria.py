4b80a81a2e74c78832242f3bc2b9abb212256164
Anand Doshi <anand@iwebnotes.com>
2012-01-13 11:14:29 +0530

    Rename Search Criteria Patch

---
+++ erpnext/patches/old_patches/rename_search_criteria.py
@@ -0,0 +1,9 @@
+import webnotes
+from webnotes.model.code import get_obj
+def execute():
+	for sc in webnotes.conn.sql("""select name from `tabSearch Criteria` where ifnull(name,'')
+		like 'srch%' or ifnull(name,'') like '%stdsrch'"""):
+		try:
+			get_obj('Search Criteria', sc[0]).rename()
+		except AttributeError, e:
+			pass
