4c923909728ee511b2f6c8688f2d83d77bb68ef4
Anand Doshi <anand@iwebnotes.com>
2012-01-20 14:32:01 +0530

    Fix to other charges total

--- erpnext/selling/doctype/sales_common/sales_common.js
+++ erpnext/selling/doctype/sales_common/sales_common.js
@@ -252,7 +252,7 @@ cur_frm.cscript.calc_doc_values = function(doc, cdt, cdn, tname, fname, other_fn
 	}
 
 	doc.net_total = inclusive_rate ? flt(net_total_incl) : flt(net_total);
-	doc.other_charges_total = flt(other_charges_total).toFixed(2);
+	doc.other_charges_total = flt(flt(other_charges_total).toFixed(2));
 	//doc.grand_total = flt(flt(net_total) + flt(other_charges_total));
 	doc.grand_total = flt((flt(net_total) + flt(other_charges_total)).toFixed(2));
 	doc.rounded_total = Math.round(doc.grand_total);
