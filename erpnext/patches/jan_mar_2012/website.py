50e2cfe78eb4f3f46a92da73c607e401e33a0b2f
Rushabh Mehta <rmehta@gmail.com>
2012-02-02 16:40:01 +0530

    dateutil.js/patch fix

--- erpnext/patches/jan_mar_2012/website.py
+++ erpnext/patches/jan_mar_2012/website.py
@@ -8,24 +8,37 @@ def execute():
 	cleanup_file_data()
 	update_patch_log()
 	from webnotes.modules import reload_doc
-	reload_doc('website', 'Role', 'Website Manager')
+	from webnotes.model import delete_doc
+	
+	delete_doc('Website', 'Module Def', 'Website')
 	reload_doc('website', 'Module Def', 'Website')
-	reload_doc('website', 'doctype', 'website_settings')
-	reload_doc('website', 'doctype', 'style_settings')
-	reload_doc('website', 'doctype', 'top_bar_item')
-	reload_doc('website', 'doctype', 'contact_us_settings')
+	reload_doc('website', 'Role', 'Website Manager')
+
+	webnotes.conn.sql("""delete from `tabModule Def Role` where parent='Website'"""):
+	d = Document('Module Def Role')
+	d.parent = 'Website'
+	d.role = 'Website Manager'
+	d.save()
+	
 	reload_doc('website', 'doctype', 'about_us_settings')
-	reload_doc('website', 'doctype', 'products_settings')
+	reload_doc('website', 'doctype', 'about_us_team')
 	reload_doc('website', 'doctype', 'blog')
 	reload_doc('website', 'doctype', 'blog_subscriber')
+	reload_doc('website', 'doctype', 'contact_us_settings')
 	reload_doc('website', 'doctype', 'product')
+	reload_doc('website', 'doctype', 'product_group')
+	reload_doc('website', 'doctype', 'products_settings')
+	reload_doc('website', 'doctype', 'related_page')
+	reload_doc('website', 'doctype', 'style_settings')
+	reload_doc('website', 'doctype', 'top_bar_item')
 	reload_doc('website', 'doctype', 'web_page')
+	reload_doc('website', 'doctype', 'website_settings')
 
-	reload_doc('website', 'page', 'contact')
 	reload_doc('website', 'page', 'about')
 	reload_doc('website', 'page', 'blog')
-	reload_doc('website', 'page', 'unsubscribe')
+	reload_doc('website', 'page', 'contact')
 	reload_doc('website', 'page', 'products')
+	reload_doc('website', 'page', 'unsubscribe')
 		
 def add_website_manager():
 	"""add website manager to system manager"""
