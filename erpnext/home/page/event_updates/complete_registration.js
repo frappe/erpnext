ad37575701f5ae508c50f74a65ae5d3e34d9e226
Anand Doshi <anand@iwebnotes.com>
2012-02-09 12:48:04 +0530

    First name and last name in setup account dialog

--- erpnext/home/page/event_updates/complete_registration.js
+++ erpnext/home/page/event_updates/complete_registration.js
@@ -1,14 +1,18 @@
 // complete my company registration
 // --------------------------------
-pscript.complete_registration = function(is_complete) {
+pscript.complete_registration = function(is_complete, profile) {
 	if(is_complete == 'No'){
-		var d = new Dialog(400, 200, "Please Complete Your Registration");
+		var d = new Dialog(400, 200, "Setup your Account");
 		if(user != 'Administrator'){
 			d.no_cancel(); // Hide close image
 			$('header').toggle(false);
 		}
 
 		d.make_body([
+			['HTML', 'Your Profile Details', '<h4>Your Profile Details</h4>'],
+			['Data', 'First Name'],
+			['Data', 'Last Name'],
+			['HTML', 'Company Details', '<h4>Create your first company</h4>'],
 			['Data','Company Name','Example: Your Company LLC'],
 	  		['Data','Company Abbreviation', 'Example: YC (all your acconts will have this as a suffix)'],
 	  		['Select','Fiscal Year Start Date'],
@@ -23,6 +27,16 @@ pscript.complete_registration = function(is_complete) {
 			d.widgets['Company Name'].disabled = 1;
 		}
 		
+		if(profile && profile.length>0) {
+			if(profile[0].first_name && profile[0].first_name!='None') {
+				d.widgets['First Name'].value = profile[0].first_name;
+			}
+
+			if(profile[0].last_name && profile[0].last_name!='None') {
+				d.widgets['Last Name'].value = profile[0].last_name;
+			}
+		}
+
 
 		//d.widgets['Save'].disabled = true;	  // disable Save button
 		pscript.make_dialog_field(d);
@@ -39,14 +53,20 @@ pscript.complete_registration = function(is_complete) {
 					d.widgets['Company Name'].value,
 					d.widgets['Company Abbreviation'].value,
 					d.widgets['Fiscal Year Start Date'].value,
-					d.widgets['Default Currency'].value
+					d.widgets['Default Currency'].value,
+					d.widgets['First Name'].value,
+					d.widgets['Last Name'].value
 				];
 				
 				$c_obj('Setup Control','setup_account',JSON.stringify(args),function(r, rt){
 					sys_defaults = r.message;
+					user_fullname = r.message.user_fullname;
 					d.hide();
 					$('header').toggle(true);
+					page_body.wntoolbar.set_user_name();
 				});
+			} else {
+				d.widgets['Save'].done_working();
 			}
 		}
 		d.show();
@@ -72,12 +92,12 @@ pscript.make_dialog_field = function(d)
 // ---------------
 pscript.validate_fields = function(d)
 {
-	var lst = ['Company Abbreviation', 'Fiscal Year Start Date', 'Default Currency'];
+	var lst = ['First Name', 'Company Name', 'Company Abbreviation', 'Fiscal Year Start Date', 'Default Currency'];
 	var msg = 'Please enter the following fields';
 	var flag = 1;
 	for(var i=0; i<lst.length; i++)
 	{
-		if(!d.widgets[lst[i]].value){
+		if(!d.widgets[lst[i]].value || d.widgets[lst[i]].value=='None'){
 			flag = 0;
 			msg = msg + NEWLINE + lst[i];
 		}
