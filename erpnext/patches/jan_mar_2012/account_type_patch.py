e63ea67b540a255c5681b3a3270913b2d04831ce
Nabin Hait <nabinhait@gmail.com>
2012-02-13 15:38:15 +0530

    error fixed in patch

--- erpnext/patches/jan_mar_2012/account_type_patch.py
+++ erpnext/patches/jan_mar_2012/account_type_patch.py
@@ -2,10 +2,10 @@ def execute():
 	import webnotes
 	webnotes.conn.sql("""update `tabAccount` 
 		set account_type = 'Chargeable' 
-		where name in ('CENVAT Capital Goods', 'CENVAT Service Tax', 'CENVAT Service Tax Cess 1', 'CENVAT Service Tax Cess 2')
+		where account_name in ('CENVAT Capital Goods', 'CENVAT Service Tax', 'CENVAT Service Tax Cess 1', 'CENVAT Service Tax Cess 2')
 	""")
 	webnotes.conn.sql("""update tabAccount 
 		set account_type = 'Tax' 
-		where name in ('P L A', 'P L A - Cess Portion', 'VAT', 'TDS (Advertisement)', 'TDS (Commission)',
+		where account_name in ('P L A', 'P L A - Cess Portion', 'VAT', 'TDS (Advertisement)', 'TDS (Commission)',
 			'TDS (Contractor)', 'TDS (Interest)', 'TDS (Rent)', 'TDS (Salary)')
 	""")
