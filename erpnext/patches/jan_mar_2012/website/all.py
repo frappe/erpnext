e8928b8bff5305a1779c93d2a311bbb068f26eea
Anand Doshi <anand@iwebnotes.com>
2012-02-13 17:56:59 +0530

    website settings

--- erpnext/patches/jan_mar_2012/website/all.py
+++ erpnext/patches/jan_mar_2012/website/all.py
@@ -2,9 +2,11 @@ import patches.jan_mar_2012.website.login
 import patches.jan_mar_2012.website.feed
 import patches.jan_mar_2012.website.website
 import patches.jan_mar_2012.website.cleanups
+import patches.jan_mar_2012.website.domain_list
 
 def execute():
 	patches.jan_mar_2012.website.login.execute()
 	patches.jan_mar_2012.website.feed.execute()
 	patches.jan_mar_2012.website.website.execute()
 	patches.jan_mar_2012.website.cleanups.execute()
+	patches.jan_mar_2012.website.domain_list.execute()
