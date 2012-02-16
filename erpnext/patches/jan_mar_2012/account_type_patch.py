da1f48b96ea22baaccebdb574611789e9d669f73
Nabin Hait <nabinhait@gmail.com>
2012-02-16 10:55:05 +0530

    mentioed account type for some tax/chargeable account

--- erpnext/patches/jan_mar_2012/account_type_patch.py
+++ erpnext/patches/jan_mar_2012/account_type_patch.py
@@ -2,10 +2,8 @@ def execute():
 	import webnotes
 	webnotes.conn.sql("""update `tabAccount` 
 		set account_type = 'Chargeable' 
-		where account_name in ('CENVAT Capital Goods', 'CENVAT Service Tax', 'CENVAT Service Tax Cess 1', 'CENVAT Service Tax Cess 2')
+		where account_name in ('CENVAT Capital Goods', 'CENVAT Service Tax', 'CENVAT Service Tax Cess 1', 'CENVAT Service Tax Cess 2', 
+			'P L A', 'P L A - Cess Portion', 'VAT', 'TDS (Advertisement)', 'TDS (Commission)', 'TDS (Contractor)', 'TDS (Interest)', 
+			'TDS (Rent)', 'TDS (Salary)')
 	""")
-	webnotes.conn.sql("""update tabAccount 
-		set account_type = 'Tax' 
-		where account_name in ('P L A', 'P L A - Cess Portion', 'VAT', 'TDS (Advertisement)', 'TDS (Commission)',
-			'TDS (Contractor)', 'TDS (Interest)', 'TDS (Rent)', 'TDS (Salary)')
-	""")
+
