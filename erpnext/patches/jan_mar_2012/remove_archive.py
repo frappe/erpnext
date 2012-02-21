c05ee5bf64394655b29c8db51592da084ef2dcec
Anand Doshi <anand@iwebnotes.com>
2012-02-21 14:12:13 +0530

    patch to remove archive

---
+++ erpnext/patches/jan_mar_2012/remove_archive.py
@@ -0,0 +1,23 @@
+# TODO take backup before running this patch
+def execute():
+	"""
+		* Restore archived data from arc tables
+		* Drop arc tables
+	"""
+	import webnotes
+	from webnotes.utils import archive
+	arc_tables = webnotes.conn.sql('show tables like "arc%"')
+	try:
+		for tab in arc_tables:
+			tab = tab[0]
+			dt = tab[3:]
+			res = webnotes.conn.sql("SELECT name FROM `%s`" % tab)
+			for dn in res:
+				archive.archive_doc(dt, dn[0], restore=1)
+	except Exception, e:
+		raise e
+	else:
+		webnotes.conn.commit()
+		for tab in arc_tables:
+			webnotes.conn.sql("DROP TABLE `%s`" % tab[0])
+		webnotes.conn.begin()
