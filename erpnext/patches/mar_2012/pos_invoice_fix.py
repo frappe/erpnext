6ba51ca693b8af27d533ba78841d2385e9a28227
Anand Doshi <anand@iwebnotes.com>
2012-03-20 12:24:51 +0530

    POS Invoice fix

--- erpnext/patches/mar_2012/pos_invoice_fix.py
+++ erpnext/patches/mar_2012/pos_invoice_fix.py
@@ -1,5 +1,5 @@
 def execute():
 	import webnotes
 	webnotes.conn.sql("DELETE FROM `tabDocFormat` WHERE format='POS Invoice'")
-	from webnotes.modules.modules_manager import reload_doc
+	from webnotes.modules.module_manager import reload_doc
 	reload_doc('accounts', 'Print Format', 'POS Invoice')
