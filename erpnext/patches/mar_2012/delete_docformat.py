a30fbd004ed9c0cd44f6158e7e7153379a9a64a4
Anand Doshi <anand@iwebnotes.com>
2012-03-27 19:31:38 +0530

    merge of sync handler and sync_file_db - first cut

---
+++ erpnext/patches/mar_2012/delete_docformat.py
@@ -0,0 +1,8 @@
+def execute():
+	import webnotes
+	webnotes.conn.sql("DELETE FROM `tabDocField` WHERE options='DocFormat'")
+	webnotes.conn.sql("DELETE FROM `tabDocField` WHERE parent='DocFormat'")
+	webnotes.conn.sql("DELETE FROM `tabDocType` WHERE name='DocFormat'")
+	webnotes.conn.commit()
+	webnotes.conn.sql("DROP TABLE `tabDocFormat`")
+	webnotes.conn.begin()
