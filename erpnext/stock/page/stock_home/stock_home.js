f1264a7f76848b42e1c1e66990b351d5aad836b1
Rushabh Mehta <rmehta@gmail.com>
2012-04-13 11:41:43 +0530

    listing, module pages in appframe

--- erpnext/stock/page/stock_home/stock_home.js
+++ erpnext/stock/page/stock_home/stock_home.js
@@ -15,5 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 pscript['onload_stock-home'] = function(wrapper) {
+	wrapper.appframe = new wn.ui.AppFrame($(wrapper).find('.appframe-area'));
 	erpnext.module_page.setup_page('Stock', wrapper);
 }
