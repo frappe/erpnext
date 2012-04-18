ecf59f4c7710e0d0a42c8e5368136caf06a2e636
Anand Doshi <anand@iwebnotes.com>
2012-04-18 17:18:33 +0530

    fix in breadcrubs - module home page issue

--- js/all-app.js
+++ js/all-app.js
@@ -1388,7 +1388,7 @@ return false;}
  *	lib/js/wn/views/breadcrumbs.js
  */
 wn.provide('wn.views');wn.views.breadcrumbs=function(parent,module,doctype,name){$(parent).empty();var $bspan=$(repl('<span class="breadcrumbs">\
-  <a href="#%(home_page)s">Home</a></span>',{home_page:wn.boot.home_page}));if(module){$bspan.append(repl(' / <a href="#!%(module_small)s-home">%(module)s Home</a>',{module:module,module_small:module.toLowerCase()}))}
+  <a href="#%(home_page)s">Home</a></span>',{home_page:wn.boot.home_page}));if(module){$bspan.append(repl(' / <a href="#!%(module_page)s">%(module)s Home</a>',{module:module,module_page:erpnext.modules[module]}))}
 if(doctype&&(locals.DocType[doctype]&&!locals.DocType[doctype].issingle)){$bspan.append(repl(' / <a href="#!List/%(doctype)s">%(doctype)s</a>',{doctype:doctype}))}
 if(name){$bspan.append(' / '+name.bold())}
 $bspan.appendTo(parent);}
