4b6c1f8de58dc93da141f17addcc2f517e7e2094
Anand Doshi <anand@iwebnotes.com>
2012-05-03 16:26:38 +0530

    fixes in notification control due to renaming of doctype

---
+++ erpnext/patches/may_2012/cleanup_notification_control.py
@@ -0,0 +1,28 @@
+def execute():
+	import webnotes
+	webnotes.conn.sql("""
+		delete from `tabSingles`
+		where doctype='Notification Control'
+		and field in (
+			'payable_voucher',
+			'payment_received_message',
+			'payment_sent_message',
+			'enquiry')
+	""")
+	ren_list = [
+		['expense_voucher', 'expense_claim'],
+		['receivable_voucher', 'sales_invoice'],
+		['enquiry', 'opportunity'],
+	]
+	for r in ren_list:
+		webnotes.conn.sql("""
+			update `tabSingles`
+			set field=%s
+			where field=%s
+			and doctype='Notification Control'
+		""", (r[1], r[0]))
+	
+	webnotes.conn.commit()
+	webnotes.conn.begin()
+	import webnotes.model.sync
+	webnotes.model.sync.sync('setup', 'notification_control')
