be93aa027f117c461d7b53f876873ff1feb10f22
Rushabh Mehta <rmehta@gmail.com>
2012-05-09 14:44:40 +0530

    cms fixes

--- erpnext/website/doctype/style_settings/style_settings.py
+++ erpnext/website/doctype/style_settings/style_settings.py
@@ -34,13 +34,14 @@ class DocType:
 		self.doc.custom_css = temp.render(doc = self.doc)
 		if self.doc.add_css:
 			self.doc.custom_css += '\n\n/* User CSS */\n\n' + self.doc.add_css
-
-		from webnotes.cms.make import make_web_core
-		make_web_core()
 		
 		from webnotes.session_cache import clear_cache
 		clear_cache('Guest')
 		
 		del self.doc.fields['small_font_size']
-		
+	
+	def on_update(self):
+		"""rebuild pages"""
+		from webnotes.cms.make import make_web_core
+		make_web_core()
 		
