cb1f1d81aab8404115e515d8332bcd813ddbd1bf
Rushabh Mehta <rmehta@gmail.com>
2012-05-09 14:10:49 +0530

    cms patch

--- erpnext/patches/may_2012/cms.py
+++ erpnext/patches/may_2012/cms.py
@@ -1,9 +1,7 @@
 import webnotes
 
 def execute():
-	from webnotes.model.doclist import DocList
-	import os
-		
-	for name in webnotes.conn.sql("""select name from `tabWeb Page` where docstatus=0"""):
-		print name
-		DocList('Web Page', name[0]).save()
+	from webnotes.model.code import get_obj
+	
+	# rewrite pages
+	get_obj('Website Settings').rewrite_pages()
