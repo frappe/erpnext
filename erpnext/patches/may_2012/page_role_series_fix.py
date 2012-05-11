43dd041ced5341e4885cf5ccf1ef7d5adbb530b5
Nabin Hait <nabinhait@gmail.com>
2012-05-11 11:47:18 +0530

    page role series pacth

--- erpnext/patches/may_2012/page_role_series_fix.py
+++ erpnext/patches/may_2012/page_role_series_fix.py
@@ -2,4 +2,4 @@ def execute():
 	import webnotes
 	sr = webnotes.conn.sql("select max(name) from `tabPage Role`")
 	if sr and sr[0][0].startswith('PR'):
-		sql("update tabSeries set current = %s where name = 'PR'", int(sr[0][0][2:]))	
+		webnotes.conn.sql("update tabSeries set current = %s where name = 'PR'", int(sr[0][0][2:]))	
