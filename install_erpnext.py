84cd7e5c322b4bd27e3b9dfa97b3d4bd77aa7419
Anand Doshi <anand@iwebnotes.com>
2012-05-15 11:40:12 +0530

    do not track wn-web.js and wn-web.css in repositor as they are built for each implementation, changed install file to create wn-web.js and wn-web.css

--- install_erpnext.py
+++ install_erpnext.py
@@ -107,13 +107,13 @@ os.system("lib/wnf.py -l")
 os.system("lib/wnf.py --sync_all -f")
 
 # create website files
-#from webnotes.model.code import get_obj
+from webnotes.model.code import get_obj
 # rewrite pages
-#get_obj('Website Settings').rewrite_pages()
-
-#ss = get_obj('Style Settings')
-#ss.validate()
-#ss.on_update()
+ws = get_obj('Website Settings')
+ws.rewrite_pages()
+ss = get_obj('Style Settings')
+ss.validate()
+ss.on_update()
 
 # set filemode false
 os.system("git config core.filemode false")
