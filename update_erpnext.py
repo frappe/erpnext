0a4d1e337db4561f4726495b28d34b188e797e02
Anand Doshi <anand@iwebnotes.com>
2012-05-26 15:17:38 +0530

    basic update utility -- for complementing installs. Does pull, applies latest patches and sync

---
+++ update_erpnext.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import commands
+
+cmd_list = [
+	'lib/wnf.py --pull origin master',
+	'lib/wnf.py -l',
+	'lib/wnf.py --sync_all'
+]
+
+for cmd in cmd_list:
+	stat, op = commands.getstatusoutput(cmd)
+	if stat != 0:
+		print "something went wrong"
+		print "cannot proceed with update"
+		print "status: %s" % stat
+		print "output: %s" % op
+		break
