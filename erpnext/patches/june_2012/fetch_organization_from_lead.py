7357fc18173d80b1c6172df4da89ca0696a2d7a9
Nabin Hait <nabinhait@gmail.com>
2012-06-08 18:02:38 +0530

    fetch organization from lead to quote

---
+++ erpnext/patches/june_2012/fetch_organization_from_lead.py
@@ -0,0 +1,3 @@
+def execute():
+	import webnotes
+	webnotes.conn.sql("update `tabQuotation` t1, `tabLead` t2 set t1.organization = t2.company where ifnull(t1.lead, '') = ''  and t1.quotation_to = 'Lead' and ifnull(t1.organization, '') = '' and t1.lead = t2.name")
