8eaa2dd570073f72ac52d7e2f9ebf27dd86c1ccc
Anand Doshi <anand@iwebnotes.com>
2012-06-12 14:16:37 +0530

    sync purchase invoice item and purchase order item before patch

--- erpnext/patches/june_2012/copy_uom_for_pur_inv_item.py
+++ erpnext/patches/june_2012/copy_uom_for_pur_inv_item.py
@@ -1,4 +1,7 @@
 def execute():
 	import webnotes
+	import webnotes.model.sync
+	webnotes.model.sync.sync('buying', 'purchase_order_item')
+	webnotes.model.sync.sync('accounts', 'purchase_invoice_item')
 	webnotes.conn.sql("update `tabPurchase Invoice Item` t1, `tabPurchase Order Item` t2 set t1.uom = t2.uom where ifnull(t1.po_detail, '') != '' and t1.po_detail = t2.name")
 	webnotes.conn.sql("update `tabPurchase Invoice Item` t1, `tabPurchase Receipt Item` t2 set t1.uom = t2.uom where ifnull(t1.pr_detail, '') != '' and t1.pr_detail = t2.name")
