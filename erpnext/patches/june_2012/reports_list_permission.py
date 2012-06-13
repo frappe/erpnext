94f26eae7c62d38f7242d2a2d0d5f0422b7770ca
Anand Doshi <anand@iwebnotes.com>
2012-06-13 19:04:27 +0530

    patch for list of search criteria and reports

--- erpnext/patches/june_2012/reports_list_permission.py
+++ erpnext/patches/june_2012/reports_list_permission.py
@@ -1,18 +1,14 @@
 def execute():
 	"""allow read permission to all for report list"""
 	import webnotes
-	import webnotes.model.doc
-	new_perms = [
-		{
-			'parent': 'Report',
-			'parentfield': 'permissions',
-			'parenttype': 'DocType',
-			'role': 'All',			
-			'permlevel': 0,
-			'read': 1,
-		},
-	]
-	for perms in new_perms:
-		doc = webnotes.model.doc.Document('DocPerm')
-		doc.fields.update(perms)
-		doc.save()
+	webnotes.conn.sql("""\
+		delete from `tabDocPerm`
+		where parent in ('Report', 'Search Criteria')""")
+	
+	webnotes.conn.commit()
+	
+	import webnotes.model.sync
+	webnotes.model.sync.sync('core', 'search_criteria')
+	webnotes.model.sync.sync('core', 'report')
+
+	webnotes.conn.begin()
