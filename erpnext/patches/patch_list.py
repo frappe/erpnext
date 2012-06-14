3878b2a1ac45c2956a3e1627594809fa76ad02c2
Anand Doshi <anand@iwebnotes.com>
2012-06-14 18:28:29 +0530

    add unique constraint to tabSeries name column

--- erpnext/patches/patch_list.py
+++ erpnext/patches/patch_list.py
@@ -437,4 +437,9 @@ patch_list = [
 		'patch_file': 'support_ticket_autoreply',
 		'description': 'New Send Autoreply checkbox in Email Settings'
 	},
+	{
+		'patch_module': 'patches.june_2012',
+		'patch_file': 'series_unique_patch',
+		'description': "add unique constraint to series table's name column"
+	},
 ]
