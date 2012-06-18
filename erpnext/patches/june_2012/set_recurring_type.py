34b4d26c1a8d40e854913886224359ce08394b19
Nabin Hait <nabinhait@gmail.com>
2012-06-18 11:18:27 +0530

    reload_doc replaced with sync in patch

--- erpnext/patches/june_2012/set_recurring_type.py
+++ erpnext/patches/june_2012/set_recurring_type.py
@@ -1,6 +1,6 @@
 def execute():
 	import webnotes
-	from webnotes.modules import reload_doc
-	reload_doc('accounts', 'doctype', 'sales_invoice')
+	from webnotes.model.sync import sync
+	sync('accounts', 'sales_invoice')
 	
 	webnotes.conn.sql("update `tabSales Invoice` set recurring_type = 'Monthly' where ifnull(convert_into_recurring_invoice, 0) = 1")
