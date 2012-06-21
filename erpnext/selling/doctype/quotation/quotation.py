2939a0ea65a761a88289b71e85dddda9954c1771
Anand Doshi <anand@iwebnotes.com>
2012-06-21 11:20:37 +0530

    fetch price list rates when creating quotation from opportunity

--- erpnext/selling/doctype/quotation/quotation.py
+++ erpnext/selling/doctype/quotation/quotation.py
@@ -81,12 +81,19 @@ class DocType(TransactionBase):
 			obj = get_obj('Sales Common')
 			for doc in self.doclist:
 				if doc.fields.get('item_code'):
-					arg = {'item_code':doc.fields.get('item_code'), 'income_account':doc.fields.get('income_account'), 
-						'cost_center': doc.fields.get('cost_center'), 'warehouse': doc.fields.get('warehouse')};
-					ret = obj.get_item_defaults(arg)
-					for r in ret:
-						if not doc.fields.get(r):
-							doc.fields[r] = ret[r]					
+					arg = {
+						'item_code': doc.fields.get('item_code'),
+						'income_account': doc.fields.get('income_account'),
+						'cost_center': doc.fields.get('cost_center'),
+						'warehouse': doc.fields.get('warehouse')
+					}
+					fields_dict = obj.get_item_details(arg, self)
+					if fields_dict:
+						doc.fields.update(fields_dict)
+					#ret = obj.get_item_defaults(arg)
+					#for r in ret:
+					#	if not doc.fields.get(r):
+					#		doc.fields[r] = ret[r]					
 
 
 	# Re-calculates Basic Rate & amount based on Price List Selected
