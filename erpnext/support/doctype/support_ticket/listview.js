36f5e257ed770b41eb9adfa4e033e99761829dcc
Anand Doshi <anand@iwebnotes.com>
2012-06-28 13:08:43 +0530

    minor fix in support ticket list view

--- erpnext/support/doctype/support_ticket/listview.js
+++ erpnext/support/doctype/support_ticket/listview.js
@@ -34,9 +34,12 @@ wn.doclistviews['Support Ticket'] = wn.views.ListView.extend({
 		});
 		
 		// escape double quotes
-		data.description = cstr(data.subject).replace(/"/gi, '\"')
-			+ " | " + cstr(data.description).replace(/"/gi, '\"');
-		
+		data.description = cstr(data.subject)
+			+ " | " + cstr(data.description);
+			
+		data.description = data.description.replace(/"/gi, '\"')
+							.replace(/</gi, '&lt;').replace(/>/gi, '&gt;');
+
 		// description
 		if(data.description && data.description.length > 50) {
 			data.description = '<span title="'+data.description+'">' + data.description.substr(0,50) + '...</span>';
