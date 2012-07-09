0f5e220d6cf8d83ceb7aae1f45999c168bb51564
Anand Doshi <anand@iwebnotes.com>
2012-07-09 12:48:32 +0530

    Email Digest: New Transactions --> consider feed_type is null records only

--- erpnext/setup/doctype/email_digest/email_digest.py
+++ erpnext/setup/doctype/email_digest/email_digest.py
@@ -255,7 +255,10 @@ class DocType:
 				args['sum_if_reqd'] = "IFNULL(SUM(IFNULL(%(sum_col)s, 0)), 0) AS '%(sum_col)s'," % args
 			
 			if args['type'] == 'new_transactions':
-				args['company_condition'] = ''
+				# tabFeed doesn't have company column
+				# using this arg to set condition of feed_type as null
+				# so that comments, logins and assignments are not counted
+				args['company_condition'] = "feed_type IS NULL AND"
 			else:
 				args['company_condition'] = "company = '%(company)s' AND" % args
 				
