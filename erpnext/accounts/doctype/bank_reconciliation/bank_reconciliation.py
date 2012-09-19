6a1e40bb4666602317187d21f0a1b7e1cb6cd987
Nabin Hait <nabinhait@gmail.com>
2012-09-19 12:21:59 +0530

    Date validation in bank reco

--- erpnext/accounts/doctype/bank_reconciliation/bank_reconciliation.py
+++ erpnext/accounts/doctype/bank_reconciliation/bank_reconciliation.py
@@ -63,6 +63,13 @@ class DocType:
 		vouchers = []
 		for d in getlist(self.doclist, 'entries'):
 			if d.clearance_date:
+				if getdate(d.clearance_date) < getdate(d.cheque_date):
+					msgprint("Clearance Date can not be before Cheque Date (Row #%s)" % 
+						d.idx, raise_exception=1)
+				if getdate(d.clearance_date) < getdate(d.posting_date):
+					msgprint("Clearance Date can not be before Posting Date (Row #%s)" %
+						d.idx, raise_exception=1)
+					
 				sql("update `tabJournal Voucher` set clearance_date = %s, modified = %s where name=%s", (d.clearance_date, nowdate(), d.voucher_id))
 				vouchers.append(d.voucher_id)
 
