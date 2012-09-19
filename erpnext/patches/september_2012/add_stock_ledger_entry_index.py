86096ff0b4a47ec2a567e70227fae8d2855dae0c
Anand Doshi <anand@iwebnotes.com>
2012-09-19 12:23:35 +0530

    syntax error fix

--- erpnext/patches/september_2012/add_stock_ledger_entry_index.py
+++ erpnext/patches/september_2012/add_stock_ledger_entry_index.py
@@ -3,7 +3,8 @@ import webnotes
 def execute():
 	webnotes.conn.commit()
 	try:
-		webnotes.conn.sql("""alter table `tabStock Ledger Entry` add index posting_sort_index(posting_date, posting_time, name)""")webnotes.conn.commit()
+		webnotes.conn.sql("""alter table `tabStock Ledger Entry` add index posting_sort_index(posting_date, posting_time, name)""")
+		webnotes.conn.commit()
 	except Exception, e:
 		if e.args[0]!=1061: raise e
 	webnotes.conn.begin()
