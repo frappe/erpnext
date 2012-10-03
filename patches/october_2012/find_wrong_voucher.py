3fd3b8e7fe6aed3e854f48f15a41ec9c692d0bb2
Nabin Hait <nabinhait@gmail.com>
2012-10-03 12:08:01 +0530

    Find wrong vouchers (temp)

---
+++ patches/october_2012/find_wrong_voucher.py
@@ -0,0 +1,9 @@
+def execute():
+	import webnotes
+	vouchers = webnotes.conn.sql("""
+		select parent from `tabPurchase Taxes and Charges`
+		where modified >= '2012-10-02'
+		and (category = 'Total' or category = 'Valuation')
+		and parenttype != 'Purchase Taxes and Charges Master'
+	""")
+	print vouchers
