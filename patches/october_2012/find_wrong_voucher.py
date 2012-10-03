92dd87f0fef0b78922089a5660d424886d041c90
Nabin Hait <nabinhait@gmail.com>
2012-10-03 13:33:13 +0530

    debit-credit tolerance increased to .005

--- patches/october_2012/find_wrong_voucher.py
+++ patches/october_2012/find_wrong_voucher.py
@@ -1,7 +1,7 @@
 def execute():
 	import webnotes
 	vouchers = webnotes.conn.sql("""
-		select parent from `tabPurchase Taxes and Charges`
+		select parent, parenttype, modified from `tabPurchase Taxes and Charges`
 		where modified >= '2012-10-02'
 		and (category = 'Total' or category = 'Valuation')
 		and parenttype != 'Purchase Taxes and Charges Master'
