f9037a69f826fb45e3deb0256288bba829a36219
Nabin Hait <nabinhait@gmail.com>
2012-10-04 18:37:53 +0530

    customer addresses and contacts report (query)

--- patches/october_2012/remove_old_customer_contact_address.js
+++
@@ -1,7 +0,0 @@
-def execute():
-	import webnotes
-	webnotes.conn.sql("""delete from `tabSearch Criteria` \
-		where name = 'customer_address_contact'""")
-		
-	from webnotes.modules import reload_doc
-	reload_doc('report', 'customer_addresses_and_contacts', 'selling')
