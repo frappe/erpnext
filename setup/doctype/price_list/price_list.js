14c8223985307c2676f9e48d5833f746328b3eb4
Rushabh Mehta <rmehta@gmail.com>
2012-11-15 12:44:41 +0530

    updated data import template and removed upload feature in price list

--- setup/doctype/price_list/price_list.js
+++ setup/doctype/price_list/price_list.js
@@ -15,33 +15,9 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 cur_frm.cscript.refresh = function(doc, cdt, cdn) {
+	cur_frm.set_intro("");
 	if(doc.__islocal) {
 		cur_frm.set_intro("Save this list to begin.");
 		return;
-	}
-	if (wn.boot.profile.can_create.indexOf(cdt) !== -1) {
-		if(!doc.file_list) {
-			cur_frm.set_intro('<p>1. Click on "Download Template" \
-				to download the template of all Items.</p>'
-			+'<p>2. Update prices and Currency.</p>'
-			+'<p>3. Save it as a CSV (.csv) file.</p>'
-			+'<p>4. Upload the file.</p>');
-		
-			cur_frm.add_custom_button('Download Template', function() {
-				$c_obj_csv(cur_frm.get_doclist(), 'download_template');
-			}, 'icon-download')
-		
-			cur_frm.add_custom_button('Upload Price List', function() {
-				cur_frm.attachments.add_attachment();
-			}, 'icon-upload');
-		} else {
-			cur_frm.set_intro('To update prices from the attachment, click on "Update Prices". \
-				To reset prices, delete the attachment (in the sidebar) and upload again.');
-		
-			// Update Prices
-			cur_frm.add_custom_button('Update Prices', function() {
-				cur_frm.call_server('update_prices');
-			}, 'icon-refresh');
-		}
 	}
 }
