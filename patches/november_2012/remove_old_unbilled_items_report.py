7d665818b5bc56d3d719d7971fdd7f723ddcdb0c
Rushabh Mehta <rmehta@gmail.com>
2012-11-23 17:56:30 +0530

    moved delivered_items_to_be_billed from stock to accounts

--- patches/november_2012/remove_old_unbilled_items_report.py
+++ patches/november_2012/remove_old_unbilled_items_report.py
@@ -3,4 +3,4 @@ import webnotes
 def execute():
 	webnotes.conn.sql("""delete from `tabSearch Criteria` where name='Delivery Note Itemwise Pending To Bill'""")
 	from webnotes.modules import reload_doc
-	reload_doc("stock", "report", "delivered_items_to_be_billed")
+	reload_doc("accounts", "report", "delivered_items_to_be_billed")
