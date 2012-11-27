33c8d0d3e2c4b0d215364138b970b188d52257a1
Rushabh Mehta <rmehta@gmail.com>
2012-11-27 18:45:21 +0530

    refactored communication and added to Lead / Contact

--- patches/november_2012/communication_sender_and_recipient.py
+++ patches/november_2012/communication_sender_and_recipient.py
@@ -1,6 +1,7 @@
 import webnotes
 
 def execute():
+	webnotes.reload_doc("DocType", "Communication")
 	webnotes.conn.sql("""update tabCommunication set sender=email_address 
 		where ifnull(support_ticket,'') != ''""")
 	webnotes.conn.sql("""update tabCommunication set recipients=email_address where
