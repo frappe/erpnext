711f98654d3dc1facf83345a050245b1d8e66720
Anand Doshi <anand@iwebnotes.com>
2012-12-06 19:49:00 +0530

    fix in blog list

--- website/blog.py
+++ website/blog.py
@@ -20,7 +20,7 @@ def get_blog_list(args=None):
 				comment_doctype='Blog' and comment_docname=name) as comments
 		from `tabBlog`
 		where ifnull(published,0)=1
-		order by published desc, name asc"""
+		order by creation desc, name asc"""
 	
 	from webnotes.widgets.query_builder import add_limit_to_query
 	query, args = add_limit_to_query(query, args)
