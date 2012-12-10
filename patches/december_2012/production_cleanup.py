1ea0944586aa39a0126cb8ec9d19d712191c9958
Nabin Hait <nabinhait@gmail.com>
2012-12-10 19:02:38 +0530

    rename production to manufacturing

--- patches/december_2012/production_cleanup.py
+++ patches/december_2012/production_cleanup.py
@@ -1,5 +1,20 @@
 def execute():
 	import webnotes
+	delete_doctypes()
+	rename_module()
+	
+def delete_doctypes():
 	from webnotes.model import delete_doc
 	delete_doc("DocType", "Production Control")
 	delete_doc("DocType", "BOM Control")
+	
+def rename_module():
+	webnotes.rename_doc("Module Def", "Production", "Manufacturing")
+	
+	webnotes.rename_doc("Role", "Production User", "Manufacturing User")
+	webnotes.rename_doc("Role", "Production Manager", "Manufacturing Manager")
+	
+	webnotes.rename_doc("Page", "production-home", "manufacturing-home")
+	
+	
+	
