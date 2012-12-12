686022f777139021597b666647d519a284cafdda
Rushabh Mehta <rmehta@gmail.com>
2012-12-12 15:25:51 +0530

    fixes to recent patch and removed company_control, home_control, webforms, wip_monitor

--- startup/event_handlers.py
+++ startup/event_handlers.py
@@ -25,14 +25,10 @@ def on_login_post_session(login_manager):
 		update login_from and delete parallel sessions
 	"""
 	# Clear previous sessions i.e. logout previous log-in attempts
-	exception_list = ['demo@erpnext.com', 'Administrator', 'Guest']
-	if webnotes.session['user'] not in exception_list:
-		sid_list = webnotes.conn.sql("""
-			DELETE FROM `tabSessions`
-			WHERE
-				user=%s AND
-				sid!=%s""", \
-			(webnotes.session['user'], webnotes.session['sid']), as_list=1)
+	allow_multiple_sessions = ['demo@erpnext.com', 'Administrator', 'Guest']
+	if webnotes.session['user'] not in allow_multiple_sessions:
+		from webnotes.sessions import clear_sessions
+		clear_sessions(webnotes.session.user, keep_current=True)
 
 		# check if account is expired
 		check_if_expired()
