72d2a627f5c2fd510b64f25d0a607e009e7fad55
Anand Doshi <anand@iwebnotes.com>
2012-12-13 19:35:32 +0530

    replace createLocal in Client script only

--- patches/december_2012/replace_createlocal.py
+++ patches/december_2012/replace_createlocal.py
@@ -1,4 +1,5 @@
 def execute():
 	import webnotes
 	webnotes.conn.sql("""update `tabCustom Script` 
-		set script = replace(script, 'createLocal', 'wn.model.make_new_doc_and_get_name')""")
+		set script = replace(script, 'createLocal', 'wn.model.make_new_doc_and_get_name') 
+		where script_type='Client'""")
