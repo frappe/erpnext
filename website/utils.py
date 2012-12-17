0e9e848cfca38bcbe93bce72cbee66222eff3fad
Rushabh Mehta <rmehta@gmail.com>
2012-12-17 16:00:34 +0530

    added item specification, bugfix for form grid

--- website/utils.py
+++ website/utils.py
@@ -156,13 +156,14 @@ def get_template_pages():
 
 def get_doc_fields(page_name):
 	doc_type, doc_name = get_source_doc(page_name)
-	obj = webnotes.get_obj(doc_type, doc_name)
+	obj = webnotes.get_obj(doc_type, doc_name, with_children=True)
 
 	if hasattr(obj, 'prepare_template_args'):
 		obj.prepare_template_args()
 
 	args = obj.doc.fields
 	args['template'] = page_map[doc_type].template
+	args['obj'] = obj
 	
 	return args
 
