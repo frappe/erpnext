69f855ebfb324cca1b9d1fec008a9d4d93358f14
Anand Doshi <anand@erpnext.com>
2012-12-20 17:06:44 +0530

    changed production to manufacturing in reload doc

--- patches/november_2012/production_order_patch.py
+++ patches/november_2012/production_order_patch.py
@@ -1,7 +1,7 @@
 def execute():
 	import webnotes
 	
-	webnotes.reload_doc("production", "doctype", "production_order")
+	webnotes.reload_doc("manufacturing", "doctype", "production_order")
 	webnotes.reload_doc("stock", "doctype", "stock_entry")
 	
 	webnotes.conn.sql("""update `tabProduction Order` 
