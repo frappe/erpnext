5ee3ec18f66010703d9b025fff52c4cb59dd0032
Anand Doshi <anand@erpnext.com>
2013-01-02 16:08:27 +0530

    deprecated stock search criteria patches

--- patches/november_2012/reload_stock_ledger_report.py
+++
@@ -1,4 +0,0 @@
-def execute():
-	import webnotes
-	from webnotes.modules import reload_doc
-	reload_doc("stock", "search_criteria", "stock_ledger")
--- patches/september_2012/reload_criteria_stock_ledger.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-def execute():
-	import webnotes
-	from webnotes.modules import reload_doc
-	reload_doc('stock', 'Search Criteria', 'Stock Ledger')
-
-	from webnotes.model import delete_doc
-	delete_doc("Report", "Stock Ledger")
