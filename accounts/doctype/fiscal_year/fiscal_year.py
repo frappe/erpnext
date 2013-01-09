f8c6531c3d8cbb4470da2cd2992573bb78996d0f
Nabin Hait <nabinhait@gmail.com>
2013-01-09 14:44:24 +0530

    set as default button in fiscal year

--- accounts/doctype/fiscal_year/fiscal_year.py
+++ accounts/doctype/fiscal_year/fiscal_year.py
@@ -16,7 +16,18 @@
 
 from __future__ import unicode_literals
 import webnotes
+from webnotes import msgprint, _
 
 class DocType:
 	def __init__(self, d, dl):
 		self.doc, self.doclist = d, dl
+		
+	def set_as_default(self):
+		webnotes.conn.set_value("Global Defaults", None, "current_fiscal_year", self.doc.name)
+		webnotes.get_obj("Global Defaults").on_update()
+		
+		# clear cache
+		webnotes.clear_cache()
+		
+		msgprint(self.doc.name + _(""" is now the default Fiscal Year. \
+			Please refresh your browser for the change to take effect."""))
