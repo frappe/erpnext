73f84176f0a682a7e379d0ba2a3f8dbedc34b6cd
Rushabh Mehta <rmehta@gmail.com>
2013-01-15 17:36:36 +0530

    added job application

--- hr/doctype/job_applicant/job_applicant.js
+++ hr/doctype/job_applicant/job_applicant.js
@@ -2,19 +2,12 @@
 
 cur_frm.cscript = {
 	refresh: function(doc) {
-		cur_frm.set_intro("");
-		if(doc.extract_emails) {
-			cur_frm.set_intro(wn._("Active: Will extract emails from ") + doc.email_id);
-		} else {
-			cur_frm.set_intro(wn._("Not Active"));
-		}
 		cur_frm.cscript.make_listing(doc);
 	},
 	make_listing: function(doc) {
-		var wrapper = cur_frm.fields_dict['thread_html'].wrapper;
 		cur_frm.communication_view = new wn.views.CommunicationList({
-			list: comm_list,
-			parent: wn.model.get("Communication", {"job_applicant": doc.name}),
+			list: wn.model.get("Communication", {"job_applicant": doc.name}),
+			parent: cur_frm.fields_dict['thread_html'].wrapper,
 			doc: doc,
 			recipients: doc.email_id
 		})
--- hr/doctype/job_applicant/job_applicant.py
+++ hr/doctype/job_applicant/job_applicant.py
@@ -2,7 +2,11 @@
 
 from __future__ import unicode_literals
 import webnotes
+from utilities.transaction_base import TransactionBase
 
-class DocType:
+class DocType(TransactionBase):
 	def __init__(self, d, dl):
 		self.doc, self.doclist = d, dl
+		
+	def onload(self):
+		self.add_communication_list()		
