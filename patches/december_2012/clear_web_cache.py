468bb31d88eb657cedbe88a048d679f4018d7327
Anand Doshi <anand@erpnext.com>
2013-01-22 12:48:22 +0530

    fixes in clear web cache patch

--- patches/december_2012/clear_web_cache.py
+++ patches/december_2012/clear_web_cache.py
@@ -3,7 +3,7 @@ def execute():
 	webnotes.reload_doc("website", "doctype", "web_page")
 	webnotes.reload_doc("website", "doctype", "blog")
 	webnotes.reload_doc("stock", "doctype", "item")
-	webnotes.reload_doc("stock", "doctype", "item_group")
+	webnotes.reload_doc("setup", "doctype", "item_group")
 	
 	# build wn-web.js and wn-web.css
 	from website.helpers.make_web_include_files import make
