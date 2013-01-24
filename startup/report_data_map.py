96601597e32aa7b5ab7a1ef9ec21fb6a0830689f
Nabin Hait <nabinhait@gmail.com>
2013-01-24 18:24:33 +0530

    sales analytics based on sales order/delivery note/sales invoice

--- startup/report_data_map.py
+++ startup/report_data_map.py
@@ -134,6 +134,7 @@ data_map = {
 			"warehouse": ["Warehouse", "name"]
 		},
 	},
+	
 	"Sales Order Item": {
 		"columns": ["item_code", "(ifnull(qty, 0) - ifnull(delivered_qty, 0)) as qty", 
 			"reserved_warehouse as warehouse"],
@@ -182,6 +183,42 @@ data_map = {
 		"order_by": "parent",
 		"links": {
 			"parent": ["Sales Invoice", "name"],
+			"item_code": ["Item", "name"]
+		}
+	},
+	"Sales Order": {
+		"columns": ["name", "customer", "transaction_date as posting_date", "company"],
+		"conditions": ["docstatus=1"],
+		"order_by": "transaction_date",
+		"links": {
+			"customer": ["Customer", "name"],
+			"company":["Company", "name"]
+		}
+	},
+	"Sales Order Item[Sales Analytics]": {
+		"columns": ["parent", "item_code", "qty", "amount"],
+		"conditions": ["docstatus=1", "ifnull(parent, '')!=''"],
+		"order_by": "parent",
+		"links": {
+			"parent": ["Sales Order", "name"],
+			"item_code": ["Item", "name"]
+		}
+	},
+	"Delivery Note": {
+		"columns": ["name", "customer", "posting_date", "company"],
+		"conditions": ["docstatus=1"],
+		"order_by": "posting_date",
+		"links": {
+			"customer": ["Customer", "name"],
+			"company":["Company", "name"]
+		}
+	},
+	"Delivery Note Item[Sales Analytics]": {
+		"columns": ["parent", "item_code", "qty", "amount"],
+		"conditions": ["docstatus=1", "ifnull(parent, '')!=''"],
+		"order_by": "parent",
+		"links": {
+			"parent": ["Delivery Note", "name"],
 			"item_code": ["Item", "name"]
 		}
 	},
