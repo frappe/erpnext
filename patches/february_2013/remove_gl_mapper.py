259a55676ba414fbceede8bbf9bb17c9f4e50b5e
Nabin Hait <nabinhait@gmail.com>
2013-02-01 12:38:15 +0530

    fixes in remove_gl_mapper patch

--- patches/february_2013/remove_gl_mapper.py
+++ patches/february_2013/remove_gl_mapper.py
@@ -1,5 +1,4 @@
 def execute():
 	import webnotes
-	from webnotes.model.doc import delete_doc
 	for mapper in webnotes.conn.sql("""select name from `tabGL Mapper`"""):
-		delete_doc("GL Mapper", mapper[0])
+		webnotes.delete_doc("GL Mapper", mapper[0])
