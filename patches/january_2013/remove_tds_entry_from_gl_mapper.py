e76adfcfdd5e809239fa4dcd70bb43cda30535c8
Rushabh Mehta <rmehta@gmail.com>
2013-02-02 12:50:14 +0530

    deprecate gl_mapper patch

--- patches/january_2013/remove_tds_entry_from_gl_mapper.py
+++ patches/january_2013/remove_tds_entry_from_gl_mapper.py
@@ -1,4 +1,5 @@
 def execute():
+	return
 	from webnotes.modules.import_file import import_file
 	import_file("accounts", "GL Mapper", "Journal Voucher")
 	import_file("accounts", "GL Mapper", "Purchase Invoice")
