c2fa4956f9e2d5d37079c7e3e2d4fc832bda4b7f
Nabin Hait <nabinhait@gmail.com>
2013-02-05 16:16:01 +0530

    fixed issue

--- patches/february_2013/account_negative_balance.py
+++ patches/february_2013/account_negative_balance.py
@@ -1,4 +1,4 @@
 def execute():
 	import webnotes
-	webnotes.reload_doc("accounts", "doctype", "Account")
+	webnotes.reload_doc("accounts", "doctype", "account")
 	webnotes.conn.sql("update `tabAccount` set allow_negative_balance = 1")
