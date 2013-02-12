4860754407d0284ede75f3fc9f39d437f680420d
Nabin Hait <nabinhait@gmail.com>
2013-02-12 18:49:55 +0530

    call calc_adjustment_amount function only if docstatus is zero

--- accounts/doctype/payment_to_invoice_matching_tool/payment_to_invoice_matching_tool.js
+++ accounts/doctype/payment_to_invoice_matching_tool/payment_to_invoice_matching_tool.js
@@ -33,6 +33,7 @@ cur_frm.cscript.refresh = function(doc) {
 }
 
 cur_frm.fields_dict.voucher_no.get_query = function(doc) {
+	// TO-do: check for pos, it should not come
 	if (!doc.account) msgprint("Please select Account first");
 	else {
 		return repl("select gle.voucher_no, gle.posting_date, gle.%(account_type)s \
