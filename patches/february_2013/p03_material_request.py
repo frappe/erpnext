c06ce254703991a92488bcc8d216c4afb13a7525
Rushabh Mehta <rmehta@gmail.com>
2013-02-18 14:24:05 +0530

    updated translations

--- patches/february_2013/p03_material_request.py
+++ patches/february_2013/p03_material_request.py
@@ -1,4 +1,4 @@
-import webnotes
+import webnotes, os, sys
 
 def execute():
 	webnotes.reload_doc("core", "doctype", "doctype")
@@ -6,3 +6,11 @@ def execute():
 	webnotes.rename_doc("DocType", "Purchase Request", "Material Request", force=True)
 	webnotes.reload_doc("buying", "search_criteria", "pending_po_items_to_bill")
 	webnotes.reload_doc("buying", "search_criteria", "pending_po_items_to_receive")
+	
+	os.system("rm -rf app/buying/doctype/purchase_request")
+	os.system("rm -rf app/buying/doctype/purchase_request_item")
+	
+	os.system("rm -rf app/hr/doctype/holiday_block_list")
+	os.system("rm -rf app/hr/doctype/holiday_block_list_allow")
+	os.system("rm -rf app/hr/doctype/holiday_block_list_date")
+	
