fe62769078773047b3573847e6c8258dfc6ff3e7
Anand Doshi <anand@erpnext.com>
2013-02-18 13:51:41 +0530

    reload search criteria in material request patch

--- patches/february_2013/p03_material_request.py
+++ patches/february_2013/p03_material_request.py
@@ -4,3 +4,5 @@ def execute():
 	webnotes.reload_doc("core", "doctype", "doctype")
 	webnotes.rename_doc("DocType", "Purchase Request Item", "Material Request Item", force=True)
 	webnotes.rename_doc("DocType", "Purchase Request", "Material Request", force=True)
+	webnotes.reload_doc("buying", "search_criteria", "pending_po_items_to_bill")
+	webnotes.reload_doc("buying", "search_criteria", "pending_po_items_to_receive")
