8ff938a3771084b68b75d2cec1ae9942f6322692
Rushabh Mehta <rmehta@gmail.com>
2013-03-06 11:20:56 +0530

    backup manager (wip)

--- patches/october_2012/remove_old_customer_contact_address.py
+++ patches/october_2012/remove_old_customer_contact_address.py
@@ -3,5 +3,6 @@ def execute():
 	webnotes.conn.sql("""delete from `tabSearch Criteria` \
 		where name = 'customer_address_contact'""")
 	
+	webnotes.reload_doc("core", "doctype", "docfield")
 	webnotes.reload_doc("core", "doctype", "report")
 	webnotes.reload_doc('selling', 'report', 'customer_addresses_and_contacts')
