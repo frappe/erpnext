d35293de17cf6f98217909cd9b0088ff070e4bca
Nabin Hait <nabinhait@gmail.com>
2013-04-09 11:39:16 +0530

    updated all role in some pages

--- patches/patch_list.py
+++ patches/patch_list.py
@@ -236,4 +236,5 @@ patch_list = [
 	'execute:webnotes.reload_doc("selling", "Print Format", "Quotation Modern") # 2013-04-02',
 	'execute:webnotes.reload_doc("selling", "Print Format", "Quotation Spartan") # 2013-04-02',
 	"execute:webnotes.delete_doc('Search Criteria', 'time_log_summary')",
+	"patches.april_2013.p04_update_role_in_pages",
 ]
