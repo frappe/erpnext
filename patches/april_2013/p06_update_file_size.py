7c179bd00c35024ceed699009f6584726962617b
Rushabh Mehta <rmehta@gmail.com>
2013-04-10 14:22:06 +0530

    [files] [cleanup/redesign] removed the old file_list structure and also added file listing in setup

--- patches/april_2013/p06_update_file_size.py
+++ patches/april_2013/p06_update_file_size.py
@@ -4,8 +4,9 @@ def execute():
 	files_path = webnotes.utils.get_path("public", "files")
 	for f in webnotes.conn.sql("""select name, file_name from 
 		`tabFile Data`""", as_dict=True):
-		filepath = os.path.join(files_path, f.file_name)
-		if os.path.exists(filepath):
-			webnotes.conn.set_value("File Data", f.name, "file_size", os.stat(filepath).st_size)
+		if f.file_name:
+			filepath = os.path.join(files_path, f.file_name)
+			if os.path.exists(filepath):
+				webnotes.conn.set_value("File Data", f.name, "file_size", os.stat(filepath).st_size)
 			
 		
