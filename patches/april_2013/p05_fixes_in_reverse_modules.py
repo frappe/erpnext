1cfab4e21d67478a4ae8ae67466069a94dbb23ff
Rushabh Mehta <rmehta@gmail.com>
2013-04-11 09:36:20 +0530

    [patch] [fix]

--- patches/april_2013/p05_fixes_in_reverse_modules.py
+++ patches/april_2013/p05_fixes_in_reverse_modules.py
@@ -4,7 +4,7 @@ import webnotes.utils
 def execute():
 	modules = webnotes.get_config().modules
 	
-	ml = json.loads(webnotes.conn.get_global("hidden_modules"))
+	ml = json.loads(webnotes.conn.get_global("hidden_modules") or "[]")
 	
 	if len(ml) == len(modules.keys()):
 		webnotes.conn.set_global("hidden_modules", json.dumps([]))
