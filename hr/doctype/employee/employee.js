60666a278f227a5084eb371808ffdee4abdd41b0
Anand Doshi <anand@erpnext.com>
2013-04-12 20:19:53 +0530

    [Leave Application] [fixes] Validate Leave Approver based on a list specified in Employee

--- hr/doctype/employee/employee.js
+++ hr/doctype/employee/employee.js
@@ -14,62 +14,95 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.	If not, see <http://www.gnu.org/licenses/>.
 
-cur_frm.cscript.onload = function(doc) {
-	// bc
-	var india_specific = ["esic_card_no", "gratuity_lic_id", "pan_number", "pf_number"]
-	if(wn.control_panel.country!="India") {
-		hide_field(india_specific);
-	}
-}
-
-cur_frm.cscript.refresh = function(doc) {
-	if(!doc.__islocal) {
-		hide_field("naming_series");
-		cur_frm.add_custom_button('Make Salary Structure', 
-			cur_frm.cscript['Make Salary Structure']);
-	}
-}
-
-cur_frm.cscript.date_of_birth = function(doc, dt, dn) {
-	get_server_fields('get_retirement_date','','',doc,dt,dn,1);
-}
-
-cur_frm.cscript.salutation = function(doc,dt,dn) {
-	if(doc.salutation){
-		if(doc.salutation=='Mr')
-			doc.gender='Male';
-		else if(doc.salutation=='Ms')
-			doc.gender='Female';
-		refresh_field('gender');
-	}
-}
-
-cur_frm.cscript['Make Salary Structure']=function(){
-	$c_obj(make_doclist (cur_frm.doc.doctype, cur_frm.doc.name), 'check_sal_structure',
-	 	cur_frm.doc.name, function(r, rt) {
-			if(r.message)
-				msgprint("You have already created Active salary structure.\n \
-					If you want to create new one, please ensure that no active salary structure \
-					 exist.\nTo inactive salary structure select 'Is Active' as 'No'.");
-			else
-				cur_frm.cscript.make_salary_structure(cur_frm.doc); 
+wn.provide("erpnext.hr");
+erpnext.hr.EmployeeController = wn.ui.form.Controller.extend({
+	setup: function() {
+		this.setup_leave_approver_select();
+		this.frm.fields_dict.user_id.get_query = erpnext.utils.profile_query;
+		this.frm.fields_dict.reports_to.get_query = erpnext.utils.employee_query;
+	},
+	
+	onload: function() {
+		this.frm.toggle_display(["esic_card_no", "gratuity_lic_id", "pan_number", "pf_number"],
+			wn.control_panel.country==="India");
+	},
+	
+	refresh: function() {
+		var me = this;
+		erpnext.hide_naming_series();
+		if(!this.frm.doc.__islocal) {
+			cur_frm.add_custom_button('View Active Salary Structure', function() {
+				me.view_active_salary_structure(this); });
+			
+			cur_frm.add_custom_button('Make Salary Structure', function() {
+				me.make_salary_structure(this); });
+			
 		}
-	);
-}
-
-cur_frm.cscript.make_salary_structure = function(doc, dt, dn, det){
-	var st = wn.model.make_new_doc_and_get_name('Salary Structure');
-	st = locals['Salary Structure'][st];
-	st.employee = doc.name;
-	st.employee_name = doc.employee_name;
-	st.branch=doc.branch;
-	st.designation=doc.designation;
-	st.department=doc.department;
-	st.fiscal_year = doc.fiscal_year
-	st.grade=doc.grade;
-	loaddoc('Salary Structure', st.name);
-}
-
-cur_frm.fields_dict.user_id.get_query = erpnext.utils.profile_query;
-
-cur_frm.fields_dict.reports_to.get_query = erpnext.utils.employee_query;
+	},
+	
+	setup_leave_approver_select: function() {
+		var me = this;
+		this.frm.call({
+			method:"hr.utils.get_leave_approver_list",
+			callback: function(r) {
+				me.frm.fields_dict.employee_leave_approvers.grid.get_field("leave_approver").df.options =
+					$.map(r.message, function(profile) { 
+						return {value: profile, label: wn.user_info(profile).fullname}; 
+					});
+			}
+		});
+	},
+	
+	date_of_birth: function() {
+		cur_frm.call({
+			method: "get_retirement_date",
+			args: {date_of_birth: this.frm.doc.date_of_birth}
+		});
+	},
+	
+	salutation: function() {
+		if(this.frm.doc.salutation) {
+			this.frm.set_value("gender", {
+				"Mr": "Male",
+				"Ms": "Female"
+			}[this.frm.doc.salutation]);
+		}
+	},
+	
+	make_salary_structure: function(btn) {
+		var me = this;
+		this.validate_salary_structure(btn, function(r) {
+			if(r.message) {
+				msgprint(wn._("Employee") + ' "' + me.frm.doc.name + '": ' 
+					+ wn._("An active Salary Structure already exists. \
+						If you want to create new one, please ensure that no active Salary Structure \
+					 	exists for this Employee. Go to the active Salary Structure and set \
+						\"Is Active\" = \"No\""));
+			} else if(!r.exc) {
+				wn.model.map({
+					source: wn.model.get_doclist(me.frm.doc.doctype, me.frm.doc.name),
+					target: "Salary Structure"
+				});
+			}
+		});
+	},
+	
+	validate_salary_structure: function(btn, callback) {
+		var me = this;
+		this.frm.call({
+			btn: btn,
+			method: "webnotes.client.get_value",
+			args: {
+				doctype: "Salary Structure",
+				fieldname: "name",
+				filters: {
+					employee: me.frm.doc.name,
+					is_active: "Yes",
+					docstatus: ["!=", 2]
+				},
+			},
+			callback: callback
+		});
+	},
+});
+cur_frm.cscript = new erpnext.hr.EmployeeController({frm: cur_frm});
