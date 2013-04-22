f19822b041308ab19c8b01ce1425857f04a35ccc
Nabin Hait <nabinhait@gmail.com>
2013-04-22 12:01:01 +0530

    fixes in sales/purchase invoice list view

--- accounts/doctype/purchase_invoice/purchase_invoice_list.js
+++ accounts/doctype/purchase_invoice/purchase_invoice_list.js
@@ -1,9 +1,9 @@
 // render
 wn.listview_settings['Purchase Invoice'] = {
 	add_fields: ["`tabPurchase Invoice`.grand_total", "`tabPurchase Invoice`.outstanding_amount"],
-	add_columns: [{"content":"outstanding_amount", width:"10%", type:"bar-graph", label: "Paid"}],
+	add_columns: [{"content":"paid_amount", width:"10%", type:"bar-graph", label: "Paid"}],
 	prepare_data: function(data) {
-		data.outstanding_amount = ((flt(data.grand_total) - 
-			flt(data.outstanding_amount)) / flt(data.grand_total)) * 100;
+		data.paid_amount = flt(data.grand_total) ? (((flt(data.grand_total) - 
+			flt(data.outstanding_amount)) / flt(data.grand_total)) * 100) : 0;
 	}
 };
