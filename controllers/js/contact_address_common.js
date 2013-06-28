8f9f8a43c71443a6db46b4e849f58fca3ed73fca
Anand Doshi <anand@erpnext.com>
2013-06-28 19:18:33 +0530

    [lead] [address] store lead address in Address doctype, with link field=lead

--- controllers/js/contact_address_common.js
+++ controllers/js/contact_address_common.js
@@ -5,6 +5,11 @@ cur_frm.cscript.onload = function(doc, cdt, cdn) {
 	cur_frm.fields_dict.customer.get_query = erpnext.utils.customer_query;
 	cur_frm.fields_dict.supplier.get_query = erpnext.utils.supplier_query;
 	
+	if(cur_frm.fields_dict.lead) {
+		cur_frm.fields_dict.lead.get_query = erpnext.utils.lead_query;
+		cur_frm.add_fetch('lead', 'lead_name', 'lead_name');
+	}
+	
 	if(doc.__islocal) {
 		var last_route = wn.route_history.slice(-2, -1)[0];
 		if(last_route && last_route[0]==="Form") {
@@ -13,10 +18,13 @@ cur_frm.cscript.onload = function(doc, cdt, cdn) {
 				"Maintenance Schedule"]
 				.indexOf(last_route[1])!==-1) {
 				var refdoc = wn.model.get_doc(last_route[1], last_route[2]);
-				cur_frm.set_value("customer", refdoc.customer || refdoc.name);
-				cur_frm.set_value("customer_name", refdoc.customer_name);
-				if(cur_frm.doc.doctype==="Address")
-					cur_frm.set_value("address_title", cur_frm.doc.customer)
+
+				if(refdoc.doctype == "Quotation" ? refdoc.quotation_to=="Customer" : true) {
+					cur_frm.set_value("customer", refdoc.customer || refdoc.name);
+					cur_frm.set_value("customer_name", refdoc.customer_name);
+					if(cur_frm.doc.doctype==="Address")
+						cur_frm.set_value("address_title", cur_frm.doc.customer_name);
+				}
 			}
 			if(["Supplier", "Supplier Quotation", "Purchase Order", "Purchase Invoice", "Purchase Receipt"]
 				.indexOf(last_route[1])!==-1) {
@@ -24,7 +32,18 @@ cur_frm.cscript.onload = function(doc, cdt, cdn) {
 				cur_frm.set_value("supplier", refdoc.supplier || refdoc.name);
 				cur_frm.set_value("supplier_name", refdoc.supplier_name);
 				if(cur_frm.doc.doctype==="Address")
-					cur_frm.set_value("address_title", cur_frm.doc.supplier)
+					cur_frm.set_value("address_title", cur_frm.doc.supplier_name);
+			}
+			if(["Lead", "Quotation"]
+				.indexOf(last_route[1])!==-1) {
+				var refdoc = wn.model.get_doc(last_route[1], last_route[2]);
+				
+				if(refdoc.doctype == "Quotation" ? refdoc.quotation_to=="Lead" : true) {
+					cur_frm.set_value("lead", refdoc.lead || refdoc.name);
+					cur_frm.set_value("lead_name", refdoc.customer_name || refdoc.company_name || refdoc.lead_name);
+					if(cur_frm.doc.doctype==="Address")
+						cur_frm.set_value("address_title", cur_frm.doc.lead_name);
+				}
 			}
 		}
 	}
--- utilities/doctype/address/address.py
+++ utilities/doctype/address/address.py
@@ -27,7 +27,7 @@ class DocType:
 
 	def autoname(self):
 		if not self.doc.address_title:
-			self.doc.address_title = self.doc.customer or self.doc.supplier or self.doc.sales_partner
+			self.doc.address_title = self.doc.customer or self.doc.supplier or self.doc.sales_partner or self.doc.lead
 			
 		if self.doc.address_title:
 			self.doc.name = cstr(self.doc.address_title).strip() + "-" + cstr(self.doc.address_type).strip()
@@ -35,40 +35,32 @@ class DocType:
 		else:
 			webnotes.msgprint("""Address Title is mandatory.""", raise_exception=True)
 		
-
 	def validate(self):
 		self.validate_primary_address()
 		self.validate_shipping_address()
 	
 	def validate_primary_address(self):
 		"""Validate that there can only be one primary address for particular customer, supplier"""
-		sql = webnotes.conn.sql
 		if self.doc.is_primary_address == 1:
-			if self.doc.customer: 
-				sql("update tabAddress set is_primary_address=0 where customer = '%s'" % (self.doc.customer))
-			elif self.doc.supplier:
-				sql("update tabAddress set is_primary_address=0 where supplier = '%s'" % (self.doc.supplier))
-			elif self.doc.sales_partner:
-				sql("update tabAddress set is_primary_address=0 where sales_partner = '%s'" % (self.doc.sales_partner))
-		elif not self.doc.is_shipping_address:
-			if self.doc.customer: 
-				if not sql("select name from tabAddress where is_primary_address=1 and customer = '%s'" % (self.doc.customer)):
-					self.doc.is_primary_address = 1
-			elif self.doc.supplier:
-				if not sql("select name from tabAddress where is_primary_address=1 and supplier = '%s'" % (self.doc.supplier)):
-					self.doc.is_primary_address = 1
-			elif self.doc.sales_partner:
-				if not sql("select name from tabAddress where is_primary_address=1 and sales_partner = '%s'" % (self.doc.sales_partner)):
-					self.doc.is_primary_address = 1
-
+			self._unset_other("is_primary_address")
+			
+		elif self.doc.is_shipping_address != 1:
+			for fieldname in ["customer", "supplier", "sales_partner", "lead"]:
+				if self.doc.fields.get(fieldname):
+					if not webnotes.conn.sql("""select name from `tabAddress` where is_primary_address=1
+						and `%s`=%s and name!=%s""" % (fieldname, "%s", "%s"), 
+						(self.doc.fields[fieldname], self.doc.name)):
+							self.doc.is_primary_address = 1
+					break
 				
 	def validate_shipping_address(self):
 		"""Validate that there can only be one shipping address for particular customer, supplier"""
-		sql = webnotes.conn.sql
 		if self.doc.is_shipping_address == 1:
-			if self.doc.customer: 
-				sql("update tabAddress set is_shipping_address=0 where customer = '%s'" % (self.doc.customer))
-			elif self.doc.supplier:
-				sql("update tabAddress set is_shipping_address=0 where supplier = '%s'" % (self.doc.supplier))			
-			elif self.doc.sales_partner:
-				sql("update tabAddress set is_shipping_address=0 where sales_partner = '%s'" % (self.doc.sales_partner))			
+			self._unset_other("is_shipping_address")
+			
+	def _unset_other(self, is_address_type):
+		for fieldname in ["customer", "supplier", "sales_partner", "lead"]:
+			if self.doc.fields.get(fieldname):
+				webnotes.conn.sql("""update `tabAddress` set `%s`=0 where `%s`=%s and name!=%s""" %
+					(is_address_type, fieldname, "%s", "%s"), (self.doc.fields[fieldname], self.doc.name))
+				break
