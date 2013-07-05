8f48db33d2e65177545fb01b671e3aa5150c957e
Rushabh Mehta <rmehta@gmail.com>
2013-07-05 16:54:40 +0530

    [sales invoice] added progress bar of payment

--- accounts/doctype/sales_invoice/sales_invoice_list.js
+++ accounts/doctype/sales_invoice/sales_invoice_list.js
@@ -1,10 +1,10 @@
 // render
 wn.listview_settings['Sales Invoice'] = {
 	add_fields: ["`tabSales Invoice`.grand_total", "`tabSales Invoice`.outstanding_amount"],
-	add_columns: [{"content":"paid_amount", width:"10%", type:"bar-graph",
+	add_columns: [{"content":"Percent Paid", width:"10%", type:"bar-graph",
 		label: "Payment Received"}],
 	prepare_data: function(data) {
-		data.paid_amount =  flt(data.grand_total) ? (((flt(data.grand_total) - 
+		data["Percent Paid"] =  flt(data.grand_total) ? (((flt(data.grand_total) - 
 			flt(data.outstanding_amount)) / flt(data.grand_total)) * 100) : 0;
 	}
 };
