e62a85afcf7b9f07a749ac835ed126f98a5e515c
Rushabh Mehta <rmehta@gmail.com>
2013-07-10 20:44:13 +0530

    [patch] [minor] removed patch

--- patches/patch_list.py
+++ patches/patch_list.py
@@ -21,7 +21,6 @@ patch_list = [
 	"execute:webnotes.reload_doc('core', 'doctype', 'report')",
 	"execute:webnotes.reload_doc('core', 'doctype', 'doctype') # 2013-07-08",
 	"execute:webnotes.reload_doc('core', 'doctype', 'page') # 2013-06-24",
-	"patches.mar_2012.so_rv_mapper_fix", 
 	"patches.mar_2012.clean_property_setter", 
 	"patches.april_2012.naming_series_patch", 
 	"patches.mar_2012.cleanup_control_panel", 
