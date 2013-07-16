38bef0e4c9fc671adee343484ec3ac9632bbe296
Anand Doshi <anand@erpnext.com>
2013-07-16 12:59:23 +0530

    [patch] merge duplicate leads based on email id

---
+++ patches/july_2013/p04_merge_duplicate_leads.py
@@ -0,0 +1,13 @@
+import webnotes
+from webnotes.utils import extract_email_id
+
+def execute():
+	email_lead = {}
+	for name, email in webnotes.conn.sql("""select name, email_id from `tabLead`
+		where ifnull(email_id, '')!='' order by creation asc"""):
+		email = extract_email_id(email)
+		if email:
+			if email not in email_lead:
+				email_lead[email] = name
+			else:
+				webnotes.rename("Lead", name, email_lead[email], merge=True)
