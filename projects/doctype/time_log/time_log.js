97bde682f12bad6eeda468e45d86c3a0f2e69f10
Nabin Hait <nabinhait@gmail.com>
2013-07-19 11:13:15 +0530

    [fix] [time log] syntax error

--- projects/doctype/time_log/time_log.js
+++ projects/doctype/time_log/time_log.js
@@ -1,9 +1,9 @@
 wn.provide("erpnext.projects");
 
 erpnext.projects.TimeLog = wn.ui.form.Controller.extend({
-	setup: function() {
-		this.frm.set_query("task", function() {
-			return { query: "projects.utils.query_task" }
-		});
+	onload: function() {
+		this.frm.set_query("task", erpnext.queries.task);
+	}
+});
 
 cur_frm.cscript = new erpnext.projects.TimeLog({frm: cur_frm});
