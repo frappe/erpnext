2b1d33f1783e9e53f24c710a0f44c69958a8b751
Anand Doshi <anand@erpnext.com>
2013-07-25 17:17:47 +0530

    [minor] [cleanup] [issue] webnotes/erpnext#438 - change user_type Partner to Website User

--- patches/march_2013/p09_unset_user_type_partner.py
+++
@@ -1,6 +0,0 @@
-import webnotes
-
-def execute():
-	webnotes.conn.sql("""update `tabProfile` set user_type='System User'
-		where user_type='Partner' and exists (select name from `tabUserRole`
-			where parent=`tabProfile`.name)""")
