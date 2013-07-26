86537edd10a58fcb6af521adf37c36d1e4cd9296
Anand Doshi <anand@erpnext.com>
2013-07-26 11:34:16 +0530

    [fix] [patch] [minor] custom print format net total export patch

--- patches/july_2013/p08_custom_print_format_net_total_export.py
+++ patches/july_2013/p08_custom_print_format_net_total_export.py
@@ -3,13 +3,14 @@ import webnotes
 import re
 
 def execute():
-	for name, html in webnotes.conn.sql("""select name, html from `tabPrint Format` where standard='No'"""):
-		changed = False
-		for match in re.findall("(doc.net_total.*doc.conversion_rate)", html):
-			if match.replace(" ", "") == "doc.net_total/doc.conversion_rate":
-				html = html.replace(match, "doc.net_total_export")
-				changed = True
+	for name, html in webnotes.conn.sql("""select name, html from `tabPrint Format` where standard='No'
+		and ifnull(html, '')!=''"""):
+			changed = False
+			for match in re.findall("(doc.net_total.*doc.conversion_rate)", html):
+				if match.replace(" ", "") == "doc.net_total/doc.conversion_rate":
+					html = html.replace(match, "doc.net_total_export")
+					changed = True
 		
-		if changed:
-			webnotes.conn.set_value("Print Format", name, "html", html)
+			if changed:
+				webnotes.conn.set_value("Print Format", name, "html", html)
 			
