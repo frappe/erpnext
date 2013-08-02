b865db910b4ef6d9fea40fb8d9f66b0bb494e6d7
Nabin Hait <nabinhait@gmail.com>
2013-08-02 11:24:51 +0530

    [accounts] [perpetual] accounts settings for perpetual accounting for inventory

--- accounts/doctype/accounts_settings/accounts_settings.py
+++ accounts/doctype/accounts_settings/accounts_settings.py
@@ -2,23 +2,24 @@
 
 from __future__ import unicode_literals
 import webnotes
-from webnotes.utils import cint
+from webnotes.utils import cint, cstr
+from webnotes import msgprint, _
 
 class DocType:
 	def __init__(self, d, dl):
 		self.doc, self.doclist = d, dl
 
 	def validate(self):
-		self.make_adjustment_jv_for_auto_inventory()
+		self.validate_perpetual_accounting()
 		
-	def make_adjustment_jv_for_auto_inventory(self):
-		previous_auto_inventory_accounting = cint(webnotes.conn.get_value("Accounts Settings", 
-			None, "auto_inventory_accounting"))
-		if cint(self.doc.auto_inventory_accounting) != previous_auto_inventory_accounting:
-			from accounts.utils import create_stock_in_hand_jv
-			create_stock_in_hand_jv(reverse = \
-				cint(self.doc.auto_inventory_accounting) < previous_auto_inventory_accounting)
-		
+	def validate_perpetual_accounting(self):
+		if cint(self.doc.perpetual_accounting) == 1:
+			previous_val = cint(webnotes.conn.get_value("Accounts Settings", 
+				None, "perpetual_accounting"))
+			if cint(self.doc.perpetual_accounting) != previous_val:
+				from accounts.utils import validate_stock_and_account_balance
+				validate_stock_and_account_balance()
+	
 	def on_update(self):
-		for key in ["auto_inventory_accounting"]:
+		for key in ["perpetual_accounting"]:
 			webnotes.conn.set_default(key, self.doc.fields.get(key, ''))
