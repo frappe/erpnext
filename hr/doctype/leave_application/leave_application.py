37fe72e4e96229df8c7678df1f8d13150635a5f1
Nabin Hait <nabinhait@gmail.com>
2013-08-12 15:29:50 +0530

    [fix] [minor] Do not validate leave balance if status is Rejected

--- hr/doctype/leave_application/leave_application.py
+++ hr/doctype/leave_application/leave_application.py
@@ -115,7 +115,8 @@ class DocType(DocListController):
 				self.doc.leave_balance = get_leave_balance(self.doc.employee,
 					self.doc.leave_type, self.doc.fiscal_year)["leave_balance"]
 
-				if self.doc.leave_balance - self.doc.total_leave_days < 0:
+				if self.doc.status != "Rejected" \
+						and self.doc.leave_balance - self.doc.total_leave_days < 0:
 					#check if this leave type allow the remaining balance to be in negative. If yes then warn the user and continue to save else warn the user and don't save.
 					msgprint("There is not enough leave balance for Leave Type: %s" % \
 						(self.doc.leave_type,), 
