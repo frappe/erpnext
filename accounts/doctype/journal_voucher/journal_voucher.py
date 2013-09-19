7c2617446e0b08fe54d10aef5a575c067efb5341
Anand Doshi <anand@erpnext.com>
2013-09-19 18:31:52 +0530

    [fix] [minor]

--- accounts/doctype/journal_voucher/journal_voucher.py
+++ accounts/doctype/journal_voucher/journal_voucher.py
@@ -7,7 +7,7 @@ import webnotes
 from webnotes.utils import cint, cstr, flt, fmt_money, formatdate, getdate
 from webnotes.model.doc import addchild
 from webnotes.model.bean import getlist
-from webnotes import msgprint
+from webnotes import msgprint, _
 from setup.utils import get_company_currency
 
 from controllers.accounts_controller import AccountsController
