629a032a7493bf4a4c1e11556d3aeb712b9cac6d
Bárbara Perretti <bags_per@hotmail.com>
2013-09-26 18:41:42 -0300

    Fix translate Customer.js

--- selling/doctype/customer/customer.js
+++ selling/doctype/customer/customer.js
@@ -81,7 +81,7 @@ cur_frm.cscript.make_address = function() {
 				return "select name, address_type, address_line1, address_line2, city, state, country, pincode, fax, email_id, phone, is_primary_address, is_shipping_address from tabAddress where customer='"+cur_frm.docname+"' and docstatus != 2 order by is_primary_address desc"
 			},
 			as_dict: 1,
-			no_results_message: 'No addresses created',
+			no_results_message: wn._('No addresses created'),
 			render_row: cur_frm.cscript.render_address_row,
 		});
 		// note: render_address_row is defined in contact_control.js
@@ -99,7 +99,7 @@ cur_frm.cscript.make_contact = function() {
 				return "select name, first_name, last_name, email_id, phone, mobile_no, department, designation, is_primary_contact from tabContact where customer='"+cur_frm.docname+"' and docstatus != 2 order by is_primary_contact desc"
 			},
 			as_dict: 1,
-			no_results_message: 'No contacts created',
+			no_results_message: wn._('No contacts created'),
 			render_row: cur_frm.cscript.render_contact_row,
 		});
 		// note: render_contact_row is defined in contact_control.js
