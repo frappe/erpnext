c685da006126656b82d52e237818a3dd69c97dc8
Bárbara Perretti <bags_per@hotmail.com>
2013-09-30 18:11:48 -0300

    Fix translate gross_profit.js

--- accounts/report/gross_profit/gross_profit.js
+++ accounts/report/gross_profit/gross_profit.js
@@ -5,20 +5,20 @@ wn.query_reports["Gross Profit"] = {
 	"filters": [
 		{
 			"fieldname":"company",
-			"label": "Company",
+			"label": wn._("Company"),
 			"fieldtype": "Link",
 			"options": "Company",
 			"default": wn.defaults.get_user_default("company")
 		},
 		{
 			"fieldname":"from_date",
-			"label": "From Date",
+			"label": wn._("From Date"),
 			"fieldtype": "Date",
 			"default": wn.defaults.get_user_default("year_start_date")
 		},
 		{
 			"fieldname":"to_date",
-			"label": "To Date",
+			"label": wn._("To Date"),
 			"fieldtype": "Date",
 			"default": wn.defaults.get_user_default("year_end_date")
 		},
