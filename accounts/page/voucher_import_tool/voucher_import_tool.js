d4279c7fe50d9fd211b7834f8100f42d26813efa
Bárbara Perretti <bags_per@hotmail.com>
2013-09-30 19:05:32 -0300

    Fix translate voucher_import_tool.js

--- accounts/page/voucher_import_tool/voucher_import_tool.js
+++ accounts/page/voucher_import_tool/voucher_import_tool.js
@@ -8,21 +8,27 @@ wn.pages['voucher-import-tool'].onload = function(wrapper) {
 		single_column: true
 	});
 	
-	$(wrapper).find('.layout-main').html('<p class="help">'+wn._('Import multiple accounting entries via CSV (spreadsheet) file:')+'</p>'+
-		'<h3> 1.'+wn._(' Download Template')+'</h3><br>'+
+	$(wrapper).find('.layout-main').html('<p class="help">' +
+		wn._('Import multiple accounting entries via CSV (spreadsheet) file:') +
+		'</p><h3> 1. ' + wn._('Download Template') + '</h3><br>' +
+		'<div style="padding-left: 30px;">' +
+		    '<button class="btn btn-default btn-download-two-accounts">' +
+		    wn._('Download') + '</button>' +
+			'<p class="help">' + 
+			wn._('Import multiple vouchers with one debit and one credit entry') +
+			'</p></div>'+
 		'<div style="padding-left: 30px;">'+
-		    '<button class="btn btn-default btn-download-two-accounts">'+wn._('Download')+'</button>'+
-			'<p class="help">'+wn._('Import multiple vouchers with one debit and one credit entry')+'</p>'+
-		'</div>'+
-		'<div style="padding-left: 30px;">'+
-			'<button class="btn btn-default btn-download-multiple-accounts">'+wn._('Download')+'</button>'+
-			'<p class="help">'+wn._('Import multiple vouchers with multiple accounts')+'</p>'+
+			'<button class="btn btn-default btn-download-multiple-accounts">' +
+				wn._('Download') + 
+			'</button><p class="help">' + 
+				wn._('Import multiple vouchers with multiple accounts')+
+			'</p>'+
 		'</div>'+
 		'<hr>'+
-		'<h3> 2.'+wn._(' Upload')+'</h3><br>'+
+		'<h3> 2. ' + wn._('Upload') + '</h3><br>'+
 		'<div style="padding-left: 30px;">'+
-			'<p class="help">'+wn._('Upload file in CSV format with UTF-8 encoding')+'</p>'+
-			'<div id="voucher-upload"></div>'+
+			'<p class="help">' + wn._('Upload file in CSV format with UTF-8 encoding') +
+			'</p><div id="voucher-upload"></div>'+
 		'</div><br>'+
 		'<div class="working"></div>'+
 		'<div class="well messages" style="display: none;"></div>');
