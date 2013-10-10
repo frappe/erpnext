93c836c86dbe2b0f860addfed50dda485b5ab742
Nabin Hait <nabinhait@gmail.com>
2013-10-10 19:15:58 +0530

    [fix] [minor] validate warehouse user

--- stock/utils.py
+++ stock/utils.py
@@ -209,7 +209,7 @@ def get_warehouse_list(doctype, txt, searchfield, start, page_len, filters):
 	return wlist
 
 def validate_warehouse_user(warehouse):
-	if webnotes.session.user=="Administrator":
+	if webnotes.session.user=="Administrator" or not warehouse:
 		return
 	warehouse_users = [p[0] for p in webnotes.conn.sql("""select user from `tabWarehouse User`
 		where parent=%s""", warehouse)]
