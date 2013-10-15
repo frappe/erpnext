79341409693a1ac5e73cfa78b3f2b9c1912e05e2
Bárbara Perretti <bags_per@hotmail.com>
2013-10-15 17:14:28 -0300

    Fix translate projects/report/daily_time_log_summary/daily_time_log_summary.js

--- projects/report/daily_time_log_summary/daily_time_log_summary.js
+++ projects/report/daily_time_log_summary/daily_time_log_summary.js
@@ -5,13 +5,13 @@ wn.query_reports["Daily Time Log Summary"] = {
 	"filters": [
 		{
 			"fieldname":"from_date",
-			"label": "From Date",
+			"label": wn._("From Date"),
 			"fieldtype": "Date",
 			"default": wn.datetime.get_today()
 		},
 		{
 			"fieldname":"to_date",
-			"label": "To Date",
+			"label": wn._("To Date"),
 			"fieldtype": "Date",
 			"default": wn.datetime.get_today()
 		},
