b03f870441a7e1ff91538403c2ad13a9050275f6
Anand Doshi <anand@erpnext.com>
2013-11-04 15:22:50 +0530

    Merge branch '1310' of github.com:webnotes/erpnext

---
+++ patches/october_2013/p08_cleanup_after_item_price_module_change.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd.
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import webnotes, os
+
+def execute():
+	from webnotes.utils import get_base_path
+	
+	for dt in ("item_price", "price_list"):
+		path = os.path.join(get_base_path(), "app", "setup", "doctype", dt)
+		if os.path.exists(path):
+			os.system("rm -rf {path}".format(path=path))
--- patches/patch_list.py
+++ patches/patch_list.py
@@ -236,4 +236,5 @@ patch_list = [
 	"patches.october_2013.p06_update_control_panel_and_global_defaults",
 	"patches.october_2013.p07_rename_for_territory",
 	"patches.june_2013.p07_taxes_price_list_for_territory",
+	"patches.october_2013.p08_cleanup_after_item_price_module_change",
 ]
