7e8f98368ab20121adadb56519d73b60dad58f0d
Pratik Vyas <m@pd.io>
2013-11-29 19:57:38 +0530

    [fix] allow lot of queries in update comments patch

--- patches/1311/p04_update_comments.py
+++ patches/1311/p04_update_comments.py
@@ -3,6 +3,8 @@
 
 def execute():
 	import webnotes
+	webnotes.conn.auto_commit_on_many_writes = True
 	for name in webnotes.conn.sql_list("""select name from tabComment"""):
 		webnotes.get_obj("Comment", name).update_comment_in_doc()
+	webnotes.conn.auto_commit_on_many_writes = False
 	
