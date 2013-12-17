2298d59b8b50c1ad30fea7a9a17afe6e8297f68a
Anand Doshi <anand@erpnext.com>
2013-12-17 19:04:08 +0530

    [patch] new permission system

--- patches/1311/p08_new_permissions.py
+++ patches/1311/p08_new_permissions.py
@@ -5,10 +5,9 @@ from __future__ import unicode_literals
 import webnotes
 
 def execute():
-	# add role "Restriction Manager"
-	if not webnotes.conn.exists("Role", "Restriction Manager"):
-		webnotes.bean({"doctype": "Role", "role_name": "Restriction Manager"}).insert()
-
 	# reset Page perms
 	from core.page.permission_manager.permission_manager import reset
 	reset("Page")
+	
+	# patch to move print, email into DocPerm
+	
