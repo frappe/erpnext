ceb940ec17a4cd894254fbb0df7bf78cc63513ad
Nabin Hait <nabinhait@gmail.com>
2013-12-17 17:53:05 +0530

    [reports] [minor] patch to delete old report files

--- patches/1312/p01_delete_old_stock_reports.py
+++ patches/1312/p01_delete_old_stock_reports.py
@@ -2,14 +2,16 @@
 # License: GNU General Public License v3. See license.txt
 
 def execute():
-	import webnotes, os
+	import webnotes, os, shutil
+	from webnotes.utils import get_base_path
 	
 	webnotes.delete_doc('Page', 'stock-ledger')
 	webnotes.delete_doc('Page', 'stock-ageing')
 	webnotes.delete_doc('Page', 'stock-level')
 	webnotes.delete_doc('Page', 'general-ledger')
 	
-	os.system("rm -rf app/stock/page/stock_ledger")
-	os.system("rm -rf app/stock/page/stock_ageing")
-	os.system("rm -rf app/stock/page/stock_level")
-	os.system("rm -rf app/accounts/page/general_ledger")
+	for d in [["stock", "stock_ledger"], ["stock", "stock_ageing"],
+		 	["stock", "stock_level"], ["accounts", "general_ledger"]]:
+		path = os.path.join(get_base_path(), "app", d[0], "page", d[1])
+		if os.path.exists(path):
+			shutil.rmtree(path)
