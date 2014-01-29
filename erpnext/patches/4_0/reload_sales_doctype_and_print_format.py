b665d862925d025b7e15a4a43b84e8e41f284a59
Akhilesh Darjee <akhileshdarjee@gmail.com>
2014-01-29 17:40:34 +0530

    patch to reload all sales and purchase doctypes and print formats

---
+++ erpnext/patches/4_0/reload_sales_doctype_and_print_format.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import webnotes
+
+def execute():
+	webnotes.reload_doc('accounts', 'doctype', 'sales_invoice')
+	webnotes.reload_doc('selling', 'doctype', 'sales_order')
+	webnotes.reload_doc('selling', 'doctype', 'quotation')
+	webnotes.reload_doc('stock', 'doctype', 'delivery_note')
+	webnotes.reload_doc('accounts', 'Print Format', 'POS Invoice')
+	webnotes.reload_doc('accounts', 'Print Format', 'Sales Invoice Classic')
+	webnotes.reload_doc('accounts', 'Print Format', 'Sales Invoice Modern')
+	webnotes.reload_doc('accounts', 'Print Format', 'Sales Invoice Spartan')
+	webnotes.reload_doc('selling', 'Print Format', 'Quotation Classic')
+	webnotes.reload_doc('selling', 'Print Format', 'Quotation Modern')
+	webnotes.reload_doc('selling', 'Print Format', 'Quotation Spartan')
+	webnotes.reload_doc('selling', 'Print Format', 'Sales Order Classic')
+	webnotes.reload_doc('selling', 'Print Format', 'Sales Order Modern')
+	webnotes.reload_doc('selling', 'Print Format', 'Sales Order Spartan')
+	webnotes.reload_doc('stock', 'Print Format', 'Delivery Note Classic')
+	webnotes.reload_doc('stock', 'Print Format', 'Delivery Note Modern')
+	webnotes.reload_doc('stock', 'Print Format', 'Delivery Note Spartan')
