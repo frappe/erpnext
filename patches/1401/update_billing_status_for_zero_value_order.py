2867cd03f2ada4ff9f11c6d9b6f9536188862292
Nabin Hait <nabinhait@gmail.com>
2014-02-18 14:51:03 +0530

    minor fix in patches

--- patches/1401/update_billing_status_for_zero_value_order.py
+++ patches/1401/update_billing_status_for_zero_value_order.py
@@ -11,19 +11,20 @@ def execute():
 			where par.name = child.parent and par.docstatus = 1 
 			and ifnull(par.net_total, 0) = 0 group by par.name""" % 
 			(order_type, order_type), as_dict=1):
+				if flt(d.total_qty):
+					billed_qty = flt(webnotes.conn.sql("""select sum(ifnull(qty, 0)) 
+						from `tab%s Invoice Item` where %s=%s and docstatus=1""" % 
+						(order_type, "sales_order" if order_type=="Sales" 
+							else "purchase_order", '%s'), (d.name))[0][0])
 				
-				billed_qty = flt(webnotes.conn.sql("""select sum(ifnull(qty, 0)) 
-					from `tab%s Invoice Item` where %s=%s and docstatus=1""" % 
-					(order_type, "sales_order" if order_type=="Sales" else "purchase_order", '%s'), 
-					(d.name))[0][0])
+					per_billed = ((d.total_qty if billed_qty > d.total_qty else billed_qty)\
+						/ d.total_qty)*100
+					webnotes.conn.set_value(order_type+ " Order", d.name, "per_billed", per_billed)
 				
-				per_billed = ((d.total_qty if billed_qty > d.total_qty else billed_qty)\
-					/ d.total_qty)*100
-				webnotes.conn.set_value(order_type+ " Order", d.name, "per_billed", per_billed)
-				
-				if order_type == "Sales":
-					if per_billed < 0.001: billing_status = "Not Billed"
-					elif per_billed >= 99.99: billing_status = "Fully Billed"
-					else: billing_status = "Partly Billed"
+					if order_type == "Sales":
+						if per_billed < 0.001: billing_status = "Not Billed"
+						elif per_billed >= 99.99: billing_status = "Fully Billed"
+						else: billing_status = "Partly Billed"
 	
-					webnotes.conn.set_value("Sales Order", d.name, "billing_status", billing_status)
+						webnotes.conn.set_value("Sales Order", d.name, 
+							"billing_status", billing_status)
