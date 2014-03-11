24e136e3f37bf94530955b804e5442425a39955b
Nabin Hait <nabinhait@gmail.com>
2014-03-11 17:57:38 +0530

    Patch: set root_type for old existing accounts

--- erpnext/patches/4_0/countrywise_coa.py
+++ erpnext/patches/4_0/countrywise_coa.py
@@ -5,5 +5,16 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	frappe.reload_doc("setup", 'doctype', "company")
+	frappe.reload_doc("accounts", 'doctype', "account")
+	
 	frappe.db.sql("""update tabAccount set account_type='Fixed Asset' 
 		where account_type='Fixed Asset Account'""")
+		
+	for d in (('Asset', 'Debit', 'No'), ('Liability', 'Credit', 'No'), ('Expense', 'Debit', 'Yes'), 
+			('Income', 'Credit', 'Yes')):
+		frappe.db.sql("""update `tabAccount` set root_type = %s 
+			where debit_or_credit=%s and is_pl_account=%s""", d)
+			
+	frappe.db.sql("""update `tabAccount` set balance_must_be=debit_or_credit 
+		where ifnull(allow_negative_balance, 0) = 0""")
