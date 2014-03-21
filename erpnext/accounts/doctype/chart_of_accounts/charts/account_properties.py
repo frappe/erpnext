e0f98d720271943255b45f2fc36f42fb57ea9eb1
Nabin Hait <nabinhait@gmail.com>
2014-03-21 11:07:53 +0530

    account properties file added

---
+++ erpnext/accounts/doctype/chart_of_accounts/charts/account_properties.py
@@ -0,0 +1,13 @@
+account_properties = {
+	"Deutscher Kontenplan SKR03": {
+		"Bilanzkonten": {
+			"report_type": "Balance Sheet",
+		},
+		"Gewinn u. Verlust": {
+			"report_type": "Profit and Loss",
+		},
+		"Vortrags- Kapital- und Statistische Konten": {
+			"report_type": "Balance Sheet"
+		}
+	}
+}
