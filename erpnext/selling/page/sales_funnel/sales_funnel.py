48962e1148f8207ad7127629ce50318ffa3d4c88
Nabin Hait <nabinhait@gmail.com>
2014-04-28 15:43:32 +0530

    Minor fix in sales funnel report

--- erpnext/selling/page/sales_funnel/sales_funnel.py
+++ erpnext/selling/page/sales_funnel/sales_funnel.py
@@ -9,22 +9,22 @@ def get_funnel_data(from_date, to_date):
 	active_leads = frappe.db.sql("""select count(*) from `tabLead`
 		where (date(`modified`) between %s and %s)
 		and status != "Do Not Contact" """, (from_date, to_date))[0][0]
-		
+
 	active_leads += frappe.db.sql("""select count(distinct customer) from `tabContact`
 		where (date(`modified`) between %s and %s)
 		and status != "Passive" """, (from_date, to_date))[0][0]
-	
+
 	opportunities = frappe.db.sql("""select count(*) from `tabOpportunity`
 		where docstatus = 1 and (date(`creation`) between %s and %s)
 		and status != "Lost" """, (from_date, to_date))[0][0]
-	
+
 	quotations = frappe.db.sql("""select count(*) from `tabQuotation`
 		where docstatus = 1 and (date(`creation`) between %s and %s)
 		and status != "Lost" """, (from_date, to_date))[0][0]
-	
-	sales_orders = frappe.db.sql("""select count(*) from `tabQuotation`
+
+	sales_orders = frappe.db.sql("""select count(*) from `tabSales Order`
 		where docstatus = 1 and (date(`creation`) between %s and %s)""", (from_date, to_date))[0][0]
-	
+
 	return [
 		{ "title": "Active Leads / Customers", "value": active_leads, "color": "#B03B46" },
 		{ "title": "Opportunities", "value": opportunities, "color": "#F09C00" },
