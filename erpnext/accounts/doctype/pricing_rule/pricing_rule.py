0c7e28ec9f90e12fcaca9fbcf532342875402282
Nabin Hait <nabinhait@gmail.com>
2014-05-05 11:13:36 +0530

    Pricing Rule improvements #565

--- erpnext/accounts/doctype/pricing_rule/pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/pricing_rule.py
@@ -6,18 +6,26 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import throw, _
+from frappe.utils import flt
 from frappe.model.document import Document
 
 class PricingRule(Document):
 	def validate(self):
 		self.validate_mandatory()
+		self.validate_min_max_qty()
 		self.cleanup_fields_value()
+
 
 	def validate_mandatory(self):
 		for field in ["apply_on", "applicable_for", "price_or_discount"]:
 			tocheck = frappe.scrub(self.get(field) or "")
 			if tocheck and not self.get(tocheck):
 				throw(_("{0} is required").format(self.meta.get_label(tocheck)), frappe.MandatoryError)
+
+	def validate_min_max_qty(self):
+		if self.min_qty and self.max_qty and flt(self.min_qty) > flt(self.max_qty):
+			throw(_("Min Qty can not be greater than Max Qty"))
+
 
 	def cleanup_fields_value(self):
 		for logic_field in ["apply_on", "applicable_for", "price_or_discount"]:
