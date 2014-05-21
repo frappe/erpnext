27b12ef1b6f6e70f0ad765e15f56241617c0b67a
Nabin Hait <nabinhait@gmail.com>
2014-05-21 11:48:48 +0530

    update account root type patch

--- erpnext/patches/v4_0/update_account_root_type.py
+++ erpnext/patches/v4_0/update_account_root_type.py
@@ -9,12 +9,15 @@ def execute():
 
 	account_table_columns = frappe.db.get_table_columns("Account")
 	if "debit_or_credit" in account_table_columns and "is_pl_account" in account_table_columns:
-		frappe.db.sql("""UPDATE tabAccount SET root_type = CASE
-			WHEN (debit_or_credit='Debit' and is_pl_account = 'No') THEN 'Asset'
-			WHEN (debit_or_credit='Credit' and is_pl_account = 'No') THEN 'Liability'
-			WHEN (debit_or_credit='Debit' and is_pl_account = 'Yes') THEN 'Expense'
-			WHEN (debit_or_credit='Credit' and is_pl_account = 'Yes') THEN 'Income'
-			END""")
+		frappe.db.sql("""UPDATE tabAccount
+			SET root_type = CASE
+				WHEN (debit_or_credit='Debit' and is_pl_account = 'No') THEN 'Asset'
+				WHEN (debit_or_credit='Credit' and is_pl_account = 'No') THEN 'Liability'
+				WHEN (debit_or_credit='Debit' and is_pl_account = 'Yes') THEN 'Expense'
+				WHEN (debit_or_credit='Credit' and is_pl_account = 'Yes') THEN 'Income'
+				END
+			WHERE ifnull(parent_account, '') = ''
+		""")
 
 	else:
 		frappe.db.sql("""UPDATE tabAccount
@@ -24,11 +27,13 @@ def execute():
 				WHEN name like '%%expense%%' THEN 'Expense'
 				WHEN name like '%%income%%' THEN 'Income'
 				END
-			WHERE
-				ifnull(parent_account, '') = ''
+			WHERE ifnull(parent_account, '') = ''
 		""")
 
-		for root in frappe.db.sql("""SELECT lft, rgt, root_type FROM `tabAccount`
-			WHERE ifnull(parent_account, '')=''""",	as_dict=True):
+	for root in frappe.db.sql("""SELECT name, lft, rgt, root_type FROM `tabAccount`
+		WHERE ifnull(parent_account, '')=''""",	as_dict=True):
+			if root.root_type:
 				frappe.db.sql("""UPDATE tabAccount SET root_type=%s WHERE lft>%s and rgt<%s""",
 					(root.root_type, root.lft, root.rgt))
+			else:
+				print "Root type not found for {0}".format(root.name)
