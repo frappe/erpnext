92d67438847c3bfb4756faf1127906cffa13c3a2
Anand Doshi <anand@erpnext.com>
2014-05-28 18:49:13 +0530

    Role and User Permissions

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -48,6 +48,7 @@ doc_events = {
 		"on_cancel": "erpnext.stock.doctype.material_request.material_request.update_completed_qty"
 	},
 	"User": {
+		"validate": "erpnext.hr.doctype.employee.employee.validate_employee_role",
 		"on_update": "erpnext.hr.doctype.employee.employee.update_user_permissions"
 	}
 }
