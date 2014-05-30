c25d044c050cbc17b7462103900284918321e28b
Anand Doshi <anand@erpnext.com>
2014-05-30 14:43:36 +0530

    Set Employee Name if missing

--- erpnext/hr/utils.py
+++ erpnext/hr/utils.py
@@ -22,3 +22,7 @@ def get_expense_approver_list():
 	if not roles:
 		frappe.msgprint(_("No Expense Approvers. Please assign 'Expense Approver' Role to atleast one user"))
 	return roles
+
+def set_employee_name(doc):
+	if doc.employee and not doc.employee_name:
+		doc.employee_name = frappe.db.get_value("Employee", doc.employee, "employee_name")
