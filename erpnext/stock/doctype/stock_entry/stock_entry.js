b6843f20876a951cc437dcf0ed5d501c1a2fe2c2
Rushabh Mehta <rmehta@gmail.com>
2014-06-04 13:10:41 +0530

    added multiple item select and allow non purchase items in Material Request of type transfer

--- erpnext/stock/doctype/stock_entry/stock_entry.js
+++ erpnext/stock/doctype/stock_entry/stock_entry.js
@@ -56,6 +56,7 @@ erpnext.stock.StockEntry = erpnext.stock.StockController.extend({
 	},
 
 	onload_post_render: function() {
+		cur_frm.get_field(this.fname).grid.set_multiple_add("item_code");
 		this.set_default_account();
 	},
 
