290ed09dc618df8c3b0789ce1dbb0c3329a3cdcc
Anand Doshi <anand@erpnext.com>
2014-06-05 17:34:59 +0530

    Update new_address_template.py

--- erpnext/patches/v4_0/new_address_template.py
+++ erpnext/patches/v4_0/new_address_template.py
@@ -1,6 +1,7 @@
 import frappe
 
 def execute():
+	frappe.reload_doc("utilities", "doctype", "address_template")
 	d = frappe.new_doc("Address Template")
 	d.update({"country":frappe.db.get_default("country")})
 	try:
