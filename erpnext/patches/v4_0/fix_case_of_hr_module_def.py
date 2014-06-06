fc601659f578afa2d4e24cb1d24f8d1fe6498f1a
Anand Doshi <anand@erpnext.com>
2014-06-06 17:07:50 +0530

    Fixed Address Template

--- erpnext/patches/v4_0/fix_case_of_hr_module_def.py
+++ erpnext/patches/v4_0/fix_case_of_hr_module_def.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
-# MIT License. See license.txt
 
 from __future__ import unicode_literals
 import frappe
--- erpnext/patches/v4_0/new_address_template.py
+++ erpnext/patches/v4_0/new_address_template.py
@@ -2,9 +2,7 @@ import frappe
 
 def execute():
 	frappe.reload_doc("utilities", "doctype", "address_template")
-	d = frappe.new_doc("Address Template")
-	d.update({"country":frappe.db.get_default("country")})
-	try:
+	if not frappe.db.sql("select name from `tabAddress Template`"):
+		d = frappe.new_doc("Address Template")
+		d.update({"country":frappe.db.get_default("country")})
 		d.insert()
-	except Exception:
-		pass
