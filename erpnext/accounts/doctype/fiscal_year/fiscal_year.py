b5cefa92fa42eca1ad137aedeaaff5ac6cd485b4
Anand Doshi <anand@erpnext.com>
2014-06-06 11:17:00 +0530

    Improved Fiscal Year validation messages

--- erpnext/accounts/doctype/fiscal_year/fiscal_year.py
+++ erpnext/accounts/doctype/fiscal_year/fiscal_year.py
@@ -25,15 +25,15 @@ class FiscalYear(Document):
 
 		if year_start_end_dates:
 			if getdate(self.year_start_date) != year_start_end_dates[0][0] or getdate(self.year_end_date) != year_start_end_dates[0][1]:
-				frappe.throw(_("Cannot change Year Start Date and Year End Date once the Fiscal Year is saved."))
+				frappe.throw(_("Cannot change Fiscal Year Start Date and Fiscal Year End Date once the Fiscal Year is saved."))
 
 	def on_update(self):
 		# validate year start date and year end date
 		if getdate(self.year_start_date) > getdate(self.year_end_date):
-			frappe.throw(_("Year Start Date should not be greater than Year End Date"))
+			frappe.throw(_("Fiscal Year Start Date should not be greater than Fiscal Year End Date"))
 
 		if (getdate(self.year_end_date) - getdate(self.year_start_date)).days > 366:
-			frappe.throw(_("Year Start Date and Year End Date are not within Fiscal Year."))
+			frappe.throw(_("Fiscal Year Start Date and Fiscal Year End Date cannot be more than a year apart."))
 
 		year_start_end_dates = frappe.db.sql("""select name, year_start_date, year_end_date
 			from `tabFiscal Year` where name!=%s""", (self.name))
@@ -41,4 +41,4 @@ class FiscalYear(Document):
 		for fiscal_year, ysd, yed in year_start_end_dates:
 			if (getdate(self.year_start_date) == ysd and getdate(self.year_end_date) == yed) \
 				and (not frappe.flags.in_test):
-					frappe.throw(_("Year Start Date and Year End Date are already set in Fiscal Year {0}").format(fiscal_year))
+					frappe.throw(_("Fiscal Year Start Date and Fiscal Year End Date are already set in Fiscal Year {0}").format(fiscal_year))
