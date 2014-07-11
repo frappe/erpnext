059922cef1036278ead50f391267cbc2b14e265c
webnotes <webnotes@webnotes-HP-ProBook-4431s.(none)>
2014-07-11 11:20:44 +0530

    Add Payment Reconciliation Feature/Tool

---
+++ erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+// For license information, please see license.txt
+
+frappe.provide("erpnext.accounts");
+
+erpnext.accounts.PaymentReconciliationController = frappe.ui.form.Controller.extend({
+
+	onload: function() {
+		var me = this
+		this.frm.set_query ("party_account", function() {
+			return{
+				filters:[
+					["Account", "company", "=", me.frm.doc.company],
+					["Account", "group_or_ledger", "=", "Ledger"],
+					["Account", "master_type", "in", ["Customer", "Supplier"]]
+				]
+			};
+		});
+	}
+
+});
+
+$.extend(cur_frm.cscript, new erpnext.accounts.PaymentReconciliationController({frm: cur_frm}));
+
+cur_frm.add_fetch("party_account", "master_type", "party_type")
---
+++ erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+from frappe.utils import flt
+
+from frappe import msgprint, _
+
+from frappe.model.document import Document
+
+class PaymentReconciliation(Document):
+	pass
