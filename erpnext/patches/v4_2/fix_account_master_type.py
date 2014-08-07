d91cefb23db4ef118d310f3fca957f420d11983d
Nabin Hait <nabinhait@gmail.com>
2014-08-07 19:22:02 +0530

    Update fix_account_master_type.py

--- erpnext/patches/v4_2/fix_account_master_type.py
+++ erpnext/patches/v4_2/fix_account_master_type.py
@@ -6,7 +6,7 @@ import frappe
 
 def execute():
 	for d in frappe.db.sql("""select name from `tabAccount`
-		where ifnull(master_type, '') not in ('Customer', 'Supplier', 'Employee', '')"""):
+		where ifnull(master_type, '') not in ('Customer', 'Supplier', 'Employee', '') and docstatus=0"""):
 			ac = frappe.get_doc("Account", d[0])
 			ac.master_type = None
 			ac.save()
