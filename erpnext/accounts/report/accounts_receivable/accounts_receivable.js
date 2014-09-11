76030540cb3475097e85823fbdf96803c4b98bc8
Nabin Hait <nabinhait@gmail.com>
2014-09-11 19:27:24 +0530

    Accounts Receivable/Payable report based on party

--- erpnext/accounts/report/accounts_receivable/accounts_receivable.js
+++ erpnext/accounts/report/accounts_receivable/accounts_receivable.js
@@ -11,21 +11,10 @@ frappe.query_reports["Accounts Receivable"] = {
 			"default": frappe.defaults.get_user_default("company")
 		},
 		{
-			"fieldname":"account",
-			"label": __("Account"),
+			"fieldname":"customer",
+			"label": __("Customer"),
 			"fieldtype": "Link",
-			"options": "Account",
-			"get_query": function() {
-				var company = frappe.query_report.filters_by_name.company.get_value();
-				return {
-					"query": "erpnext.controllers.queries.get_account_list", 
-					"filters": {
-						"report_type": "Balance Sheet",
-						"company": company,
-						"master_type": "Customer"
-					}
-				}
-			}
+			"options": "Customer"
 		},
 		{
 			"fieldname":"report_date",
