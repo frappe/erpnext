1d66b71464e6a89765d55137e03f5c2040278b79
Neil Trini Lasrado <neil@frappe.io>
2014-12-12 14:48:30 +0530

    patch fix

--- erpnext/patches/v5_0/set_default_company_in_bom.py
+++ erpnext/patches/v5_0/set_default_company_in_bom.py
@@ -4,12 +4,5 @@
 import frappe
 
 def execute():
-	for d in frappe.db.sql("""select name from `tabBOM` where docstatus < 2"""):
-		try:
-			bom = frappe.get_doc('BOM', d.name)
-			bom.ignore_validate_update_after_submit = True
-			bom.company = frappe.db.get_value("Global Defaults", None, "default_company")
-			bom.save()
-			frappe.db.commit()
-		except:
-			frappe.db.rollback()
+	company = frappe.db.get_value("Global Defaults", None, "default_company")
+	frappe.db.sql("""update  `tabBOM` set company = %s""",company)
