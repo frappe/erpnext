0a4da34f068791767ccf02ad266585855eb96d2b
akshay83 <mehta.akshay@gmail.com>
2015-01-12 22:41:12 +0530

    Update straight_line.py

--- erpnext/accounts/report/straight_line/straight_line.py
+++ erpnext/accounts/report/straight_line/straight_line.py
@@ -20,187 +20,62 @@ def execute(filters=None):
     day_before_start = finyrfrom - timedelta (days=1)
     TOTAL_DAYS_IN_YEAR = 365
 
-    ps = frappe.db.sql("""select led.*,ifnull((select sum(pur.gross_purchase_value) from `tabFixed Asset Account` pur where pur.fixed_asset_name=led.fixed_asset_name and pur.purchase_date>=%s and pur.purchase_date<=%s),0) as total_pur_value,
-           ifnull((select sum(sale.asset_purchase_cost) from `tabFixed Asset Sale` sale where sale.fixed_asset_account=led.fixed_asset_name and sale.docstatus=1  and sale.posting_date>=%s and sale.posting_date<=%s),0) as total_sale_value
+    ps = frappe.db.sql("""select led.*,ifnull((select sum(sale.asset_purchase_cost) from `tabFixed Asset Sale` sale where sale.fixed_asset_account=led.fixed_asset_name and sale.docstatus=1  and sale.posting_date>=%s and sale.posting_date<=%s),0) as total_sale_value
            from `tabFixed Asset Account` led where is_sold=false or (is_sold=true and (select count(*) from `tabFixed Asset Sale` sale where sale.fixed_asset_account=led.fixed_asset_name and docstatus=1 and sale.posting_date>=%s and sale.posting_date<=%s)>0)""", (finyrfrom,finyrto,finyrfrom,finyrto,finyrfrom,finyrto), as_dict=True)
-
 
     for assets in ps:
         fixed_asset_name = assets.fixed_asset_name
         fixed_asset_account = assets.fixed_asset_account
-        global deprtilllastyr
-        global depr
+        rateofdepr = abs(assets.depreciation_rate)
+
+        global deprtilllastyr # Depreciation provided till close of last fiscal Yr.
         deprtilllastyr = 0
-        depr = 0
         for accdepr in frappe.get_doc("Fixed Asset Account", fixed_asset_name).depreciation:
            if get_fiscal_year(fiscal_year = accdepr.fiscal_year) == get_fiscal_year(date = day_before_start):
-               depr = accdepr.total_accumulated_depreciation
-        global opening_balance
+               deprtilllastyr = accdepr.total_accumulated_depreciation
+
+        global opening_balance # Opening Cost of Asset for the Year
         opening_balance = 0
-        if depr > 0:
+        if deprtilllastyr > 0:
         	opening_balance = abs(assets.gross_purchase_value)
-        rateofdepr = abs(assets.depreciation_rate)
-        totalpurchase = assets.total_pur_value
+
+        global totalpurchase # Purchases in this Fiscal Yr
+        totalpurchase = 0
+        if assets.purchase_date>=finyrfrom and assets.purchase_date<=finyrto
+            totalpurchase = assets.gross_purchase_value
+
         totalsales = assets.total_sale_value
 
-        global depronopening
+        global depronopening # Depreciation to be Provided in on the Opening Cost for this Fiscal Yr.
         depronopening = float(0)
 
         if totalsales == 0:
-            depronopening = float(depronopening + (((opening_balance - deprtilllastyr) * rateofdepr / 100)))
-            print opening_balance, depronopening
+            depronopening = float(depronopening + ((opening_balance * rateofdepr / 100)))
         elif totalsales >= opening_balance:
             sales_sql = frappe.db.sql("""select * from `tabFixed Asset Sale` where docstatus=1 and fixed_asset_account=%s and posting_date>=%s and posting_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-            value = totalsales
             factor = 1
             if opening_balance > 0:
-                factor = float(deprtilllastyr / opening_balance)
+                factor = 0
 
-            salesupto = 0
-            for sales in sales_sql:
-                saleamount = float(sales.asset_purchase_cost)
-                saledate = datetime.strptime(sales.posting_date, "%Y-%m-%d").date()
-                days = getDateDiffDays(finyrfrom, saledate)
-                if salesupto > opening_balance:
-                    calcdepron = abs(abs((opening_balance - salesupto)) - saleamount)
-                    calcdepron = calcdepron - (calcdepron * factor)
-                    depronopening = depronopening + ((calcdepron * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-                else:
-                    depronopening = depronopening + (((saleamount - (saleamount * factor)) * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-                value = value - saleamount;
-                if (value <= 0):
-                    break
-
-        elif (totalsales < opening_balance):
-            sales_sql = frappe.db.sql("""select * from `tabFixed Asset Sale` where docstatus=1 and fixed_asset_account=%s and posting_date>=%s and posting_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-            value = totalsales
-            factor = 1
-            factor = deprtilllastyr / opening_balance
             for sales in sales_sql:
                 saleamount = float(sales.asset_purchase_cost)
                 saledate = datetime.strptime(sales.posting_date, "%Y-%m-%d").date()
                 days = getDateDiffDays(finyrfrom, saledate)
                 depronopening = depronopening + (((saleamount - (saleamount * factor)) * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-                value = value - saleamount;
-            if value > 0:
-                depronopening = depronopening + (((value - (value * factor)) * rateofdepr / 100))
 
-        global depronpurchases
+        global depronpurchases # Depreciation provided on Purchase in the Current FY
         depronpurchases = float(0)
+        if assets.purchase_date>=finyrfrom and assets.purchase_date<=finyrto
+        	purdate = datetime.strptime(assets.purchase_date, "%Y-%m-%d").date()
+        	days = getDateDiffDays(purdate, finyrto)
+        	depronpurchases = depronpurchases + ((assets.gross_purchase_value * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
 
-        if (totalsales <= opening_balance or totalsales == 0):
-            purchases_sql = frappe.db.sql("""select * from `tabFixed Asset Account` pur where pur.fixed_asset_name=%s and purchase_date>=%s and purchase_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-            for purchases in purchases_sql:
-                puramount = purchases.gross_purchase_value
-                purdate = datetime.strptime(purchases.purchase_date, "%Y-%m-%d").date()
-                days = getDateDiffDays(purdate, finyrto)
-                depronpurchases = depronpurchases + ((puramount * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-
-        elif totalsales < (opening_balance + totalpurchase):
-            difference = totalsales - opening_balance;
-            purchases_sql = frappe.db.sql("""select * from `tabFixed Asset Account` pur where pur.fixed_asset_name=%s and purchase_date>=%s and purchase_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-            for purchases in purchases_sql:
-                puramount = purchases.gross_purchase_value
-                purdate = purchases.purchase_date
-                sales_sql = frappe.db.sql("""select * from `tabFixed Asset Sale` where docstatus=1 and fixed_asset_account=%s and posting_date>=%s and posting_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-                selectsaledt = 0;
-                days = 0;
-                for sales in sales_sql:
-                    saleamount = float(sales.asset_purchase_cost)
-                    selectsaledt = selectsaledt + saleamount
-                    saledate = datetime.strptime(sales.posting_date, "%Y-%m-%d").date()
-                    if (selectsaledt > opening_balance and selectsaledt <= (opening_balance + totalpurchase)) :
-                        days = getDateDiffDays(purdate, saledate)
-
-                while ((puramount - difference) > 0):
-                    if (difference <= 0 and puramount > 0):
-                        days = getDateDiffDays(purdate, finyrto)
-                        depronpurchases = depronpurchases + ((((puramount - difference) * rateofdepr / 100)) * (days / TOTAL_DAYS_IN_YEAR))
-                        break
-                    elif (difference < puramount):
-                        depronpurchases = depronpurchases + (((difference * rateofdepr / 100)) * (days / TOTAL_DAYS_IN_YEAR))
-                        puramount = puramount - difference
-                        difference = 0;
-                    elif (difference >= puramount):
-                        depronpurchases = depronpurchases + (((puramount * rateofdepr / 100)) * (days / TOTAL_DAYS_IN_YEAR))
-                        difference = difference - puramount
-                    puramount = puramount - difference
-
-        elif (totalsales == (opening_balance + totalpurchase)):
-                difference = (opening_balance + totalpurchase) - totalsales
-                purchases_sql = frappe.db.sql("""select * from `tabFixed Asset Account` pur where pur.fixed_asset_name=%s and purchase_date>=%s and purchase_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-                for purchases in purchases_sql:
-                    puramount = purchases.gross_purchase_value
-                    purdate = datetime.strptime(purchases.purchase_date, "%Y-%m-%d").date()
-                    if ((puramount + difference) < totalpurchase):
-                        sales_sql = frappe.db.sql("""select * from `tabFixed Asset Sale` where docstatus = 1 and fixed_asset_account=%s and posting_date>=%s and posting_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-                        selectsaledt = 0
-                        days = 0
-                        for sales in sales_sql:
-                            saleamount = float(sales.asset_purchase_cost)
-                            selectsaledt = selectsaledt + saleamount
-                            saledate = datetime.strptime(sales.posting_date, "%Y-%m-%d").date()
-                            if (selectsaledt > opening_balance and selectsaledt <= (opening_balance + totalpurchase)):
-                                days = getDateDiffDays(purdate, saledate)
-
-                        depronpurchases = depronpurchases + ((puramount * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-
-                    elif ((puramount + difference) == totalpurchase):
-                        sales_sql = frappe.db.sql("""select * from `tabFixed Asset Sale` where docstatus = 1 and fixed_asset_account=%s and posting_date>=%s and posting_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-                        selectsaledt = 0
-                        days = 0
-                        for sales in sales_sql:
-                            saleamount = float(sales.asset_purchase_cost)
-                            selectsaledt = selectsaledt + saleamount
-                            saledate = datetime.strptime(sales.posting_date, "%Y-%m-%d").date()
-                            if (selectsaledt > opening_balance and selectsaledt <= (opening_balance + totalpurchase)):
-                                days = getDateDiffDays(purdate, saledate)
-
-                        depronpurchases = depronpurchases + ((puramount * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-
+	global deprwrittenback
         deprwrittenback = 0
+	if totalsales > 0        
+        	deprwrittenback = depronopening + deprtilllastyr
 
-        if (totalsales == opening_balance):
-            deprwrittenback = depronopening + depr
-        elif (totalsales == (opening_balance + totalpurchase)):
-            deprwrittenback = depr + depronopening + depronpurchases
-        elif (totalsales < opening_balance):
-            sales_sql = frappe.db.sql("""select * from `tabFixed Asset Sale` where docstatus = 1 and fixed_asset_account=%s and posting_date>=%s and posting_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-            value = totalsales
-            factor = deprtilllastyr / opening_balance
-            for sales in sales_sql:
-                saleamount = float(sales.asset_purchase_cost)
-                saledate = datetime.strptime(sales.posting_date, "%Y-%m-%d").date()
-                days = getDateDiffDays(finyrfrom, saledate)
-                deprwrittenback = deprwrittenback + ((saleamount * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-                deprwrittenback = deprwrittenback + (saleamount * factor)
-                value = value - saleamount
-                if (value <= 0):
-                    break
-
-        elif (totalsales < (opening_balance + totalpurchase)):
-                purchases_sql = frappe.db.sql("""select * from `tabFixed Asset Account` pur where pur.fixed_asset_name=%s and purchase_date>=%s and purchase_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-                deprwrittenback = depronopening + deprtilllastyr
-                value = totalsales - opening_balance
-                for purchases in purchases_sql:
-                    puramount = purchases.purchase_amount
-                    purdate = datetime.strptime(purchases.purchase_date, "%Y-%m-%d").date()
-                    if (value <= puramount):
-                        sales_sql = frappe.db.sql("""select * from `tabFixed Asset Sale` where docstatus = 1 and fixed_asset_account=%s and posting_date>=%s and posting_date<=%s""", (fixed_asset_name, finyrfrom, finyrto), as_dict=True)
-                        salevsopen = 0;
-                        for sales in sales_sql:
-                            saleamount = float(sales.asset_purchase_cost)
-                            saledate = datetime.strptime(sales.posting_date, "%Y-%m-%d").date()
-                            salevsopen = salevsopen + saleamount
-                            if (value <= saleamount and salevsopen > opening_balance):
-                                days = getDateDiffDays(purdate, saledate)
-                                deprwrittenback = deprwrittenback + ((value * rateofdepr / 100) * (days / TOTAL_DAYS_IN_YEAR))
-                                value = value - saleamount
-
-                    if (value <= 0) :
-                        break
-
-        if depr < opening_balance and opening_balance - depr >= depronopening:
+	if depronopening <= (assets.gross_purchase_value - deprtilllastyr):
 	        row = [fixed_asset_name,
 	               fixed_asset_account,
 	               rateofdepr,
@@ -208,12 +83,13 @@ def execute(filters=None):
 	               flt(totalpurchase,2),
 	               flt(totalsales,2),
 	               flt(((opening_balance + totalpurchase) - totalsales),2),
-	               flt(depr,2),
+	               flt(deprtilllastyr,2),
 	               flt(depronopening,2),
 	               flt(depronpurchases,2),
 	               flt(deprwrittenback,2),
-	               flt(((depr + depronopening + depronpurchases) - 	deprwrittenback),2)]
-	elif depr < opening_balance and opening_balance - depr < depronopening:
+	               flt(((deprtilllastyr + depronopening + depronpurchases) - deprwrittenback),2)]
+	elif depronopening > (assets.gross_purchase_value - deprtilllastyr) and \
+	(assets.gross_purchase_value - deprtilllastyr) < (assets.gross_purchase_value * rateofdepr / 100) :
 	        row = [fixed_asset_name,
 	               fixed_asset_account,
 	               rateofdepr,
@@ -221,12 +97,12 @@ def execute(filters=None):
 	               flt(totalpurchase,2),
 	               flt(totalsales,2),
 	               flt(((opening_balance + totalpurchase) - totalsales),2),
-	               flt(depr,2),
-	               flt(opening_balance - depr,2),
+	               flt(deprtilllastyr,2),
+	               flt((assets.gross_purchase_value - deprtilllastyr),2),
 	               flt(depronpurchases,2),
 	               flt(deprwrittenback,2),
-	               flt(((depr + (opening_balance - depr) + depronpurchases) - 	deprwrittenback),2)]
-	elif depr == opening_balance:
+	               flt(((deprtilllastyr + (assets.gross_purchase_value - deprtilllastyr)) - deprwrittenback),2)]
+	else:
 	        row = [fixed_asset_name,
 	               fixed_asset_account,
 	               rateofdepr,
@@ -234,13 +110,11 @@ def execute(filters=None):
 	               flt(totalpurchase,2),
 	               flt(totalsales,2),
 	               flt(((opening_balance + totalpurchase) - totalsales),2),
-	               flt(depr,2),
+	               flt(deprtilllastyr,2),
 	               flt(0,2),
 	               flt(depronpurchases,2),
 	               flt(deprwrittenback,2),
-	               flt(((depr + 0 + depronpurchases) - 	deprwrittenback),2)]
-
-
+	               flt((deprtilllastyr - deprwrittenback),2)]
 
         data.append(row)
 
@@ -258,3 +132,5 @@ def execute(filters=None):
                "DEPRECIATION_CLOSING_AS_ON "+str(finyrto)]
 
     return columns, data
+
+
