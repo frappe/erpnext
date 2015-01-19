c784fa219b959329e23f043af6605cb5d7fb59de
Neil Trini Lasrado <neil@frappe.io>
2015-01-19 17:04:47 +0530

    commit

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -390,7 +390,7 @@ class Item(WebsiteGenerator):
 			if not frappe.db.exists("Item", newdn):
 				frappe.throw(_("Item {0} does not exist").format(newdn))
 
-			field_list = ["stock_uom", "is_stock_item", "has_serial_no", "has_batch_no", "is_manufactured_item"]
+			field_list = ["stock_uom", "is_stock_item", "has_serial_no", "has_batch_no"]
 			new_properties = [cstr(d) for d in frappe.db.get_value("Item", newdn, field_list)]
 			if new_properties != [cstr(self.get(fld)) for fld in field_list]:
 				frappe.throw(_("To merge, following properties must be same for both items")
