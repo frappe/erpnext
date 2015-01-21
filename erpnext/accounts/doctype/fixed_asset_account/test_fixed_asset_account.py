7d28a0d0b7675da133ce7abf4f889c0fb9b8c8f9
Akshay Mehta <mehta.akshay@gmail.com>
2015-01-21 17:03:31 +0530

    Started Tests

--- erpnext/accounts/doctype/fixed_asset_account/test_fixed_asset_account.py
+++ erpnext/accounts/doctype/fixed_asset_account/test_fixed_asset_account.py
@@ -7,4 +7,10 @@ import unittest
 test_records = frappe.get_test_records('Fixed Asset Account')
 
 class TestFixedAssetAccount(unittest.TestCase):
-	pass
+	
+	def test_fixed_asset_account(self):
+		account = frappe.copy_doc(test_records[0])
+		account.insert()
+		je_name = account.post_journal_entry()
+		je = frappe.get_doc("Journal Entry", je_name)
+		self.assertTrue("Fixed Assets" in [d.account for d in je.accounts])
