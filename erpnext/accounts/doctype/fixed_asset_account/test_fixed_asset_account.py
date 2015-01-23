5fa689f26bada9516dbecdd6a52606c6e824659c
Akshay Mehta <mehta.akshay@gmail.com>
2015-01-23 20:19:52 +0530

    Added Tests

--- erpnext/accounts/doctype/fixed_asset_account/test_fixed_asset_account.py
+++ erpnext/accounts/doctype/fixed_asset_account/test_fixed_asset_account.py
@@ -3,31 +3,23 @@
 
 import frappe
 import unittest
+from frappe.exceptions import ValidationError
 
 test_records = frappe.get_test_records('Fixed Asset Account')
 
 class TestFixedAssetAccount(unittest.TestCase):
 	
-	def test_fixed_asset_account_carried_forward(self):		
-		account = frappe.new_doc("Fixed Asset Account")
-		account.fixed_asset_name = "Test Fixed Asset Name 1"
-		account.fixed_asset_account = "_Test Fixed Asset Account"
-		account.company = "_Test Company"
-		account.depreciation_rate = 5.00
-		account.purchase_date = "2012-10-01"
-		previous = account.append("depreciation")
-		previous.fiscal_year = "_Test Fiscal Year 2012"
-		previous.total_accumulated_depreciation = 623.28
+	def test_fixed_asset_account_carried_forward(self):	
+		frappe.db.sql("""delete from `tabFixed Asset Account` where 
+			fixed_asset_name='_Test Fixed Asset Name 1'""")
+		account = frappe.copy_doc(test_records[0])
 		account.insert()
 		self.assertRaises(ValidationError, account.post_journal_entry)
 
 	def test_fixed_asset_account_purchased(self):
-		account = frappe.new_doc("Fixed Asset Account")
-		account.fixed_asset_name = "Test Fixed Asset Name 2"
-		account.fixed_asset_account = "_Test Fixed Asset Account"
-		account.company = "_Test Company"
-		account.depreciation_rate = 5.00
-		account.purchase_date = "2013-10-01"
+		frappe.db.sql("""delete from `tabFixed Asset Account` where 
+			fixed_asset_name='_Test Fixed Asset Name 2'""")
+		account = frappe.copy_doc(test_records[1])
 		account.insert()
 		je_name = account.post_journal_entry()
 		je = frappe.get_doc("Journal Entry", je_name)
--- erpnext/accounts/doctype/fixed_asset_sale/test_fixed_asset_sale.py
+++ erpnext/accounts/doctype/fixed_asset_sale/test_fixed_asset_sale.py
@@ -12,54 +12,27 @@ class TestFixedAssetSale(unittest.TestCase):
 		self.setup_accumulated_depreciation_account()
 		company = frappe.get_doc("Company", "_Test Company")
 		company.default_depreciation_method = "Straight Line"	
-		company.default_depreciation_expense_account = "_Test Expense Account"
-		company.default_accumulated_depreciation_account = "_Test Accumulated Depreciation Account"
+		company.default_depreciation_expense_account = "_Test Depreciation Expense Account - _TC"
+		company.default_accumulated_depreciation_account = "_Test Accumulated Depreciation Account - _TC"
 		company.save()
 
 	def setup_depreciation_expense_account(self):
-		account = frappe.new_doc("Account")
-		account.parent_account = "_Test Expense Account"
-		account.account_type = "Expense Account"
-		account.company = "_Test Company"
-		account.account_name = "_Test Depreciation Expense Account"
-		account.group_or_ledger = "Ledger"
+		frappe.db.sql("""delete from `tabAccount` where 
+			account_name like '%Depreciation Exp%'""")		
+		account = frappe.copy_doc(test_records[1])
 		account.insert()
 
-	def setup_accumulated_depreciation_account(self)
-		account = frappe.new_doc("Account")
-		account.parent_account = "_Test Asset Account"
-		account.account_type = "Fixed Asset"
-		account.company = "_Test Company"
-		account.account_name = "_Test Accumulated Depreciation Account"
-		account.group_or_ledger = "Ledger"
+	def setup_accumulated_depreciation_account(self):
+		frappe.db.sql("""delete from `tabAccount` where 
+			account_name like '%Accumulated Depr%'""")		
+		account = frappe.copy_doc(test_records[2])
 		account.insert()
-		
-
-	def setup_fixed_asset_account(self):
-		account = frappe.get_doc("Fixed Asset Account", "_Test Fixed Asset Name 1")
-		if not account:
-			account = frappe.new_doc("Fixed Asset Account")
-			account.fixed_asset_name = "_Test Fixed Asset Name 1"
-			account.fixed_asset_account = "_Test Fixed Asset Account"
-			account.company = "_Test Company"
-			account.depreciation_rate = 5.00
-			account.purchase_date = "2012-10-01"
-			previous = account.append("depreciation")
-			previous.fiscal_year = "_Test Fiscal Year 2012"
-			previous.total_accumulated_depreciation = 623.28
-			account.insert()
 	
 
-	def sell_carried_over_fixed_asset(self):
+	def test_sell_carried_over_fixed_asset(self):
+		print "Testing Sale of a Fixed Asset"
 		self.setup_defaults_in_company()
-		self.setup_fixed_asset_account()
-
-		sale = frappe.new_doc("Fixed Asset Sale")
-		sale.company = "_Test Company"
-		sale.posting_date = "2014-01-01"
-		sale.fixed_asset_account = "Test Fixed Asset Name 1"
-		sale.sales_amount = 5000
-		sale.sold_to = "_Test Supplier 1"
+		sale = frappe.copy_doc(test_records[0])
 		sale.insert()
 		self.assertTrue(sale.asset_purchase_cost == 25000)
 		self.assertTrue(sale.accumulated_depreciation >= 924 and sale.accumulated_depreciation <= 945)
