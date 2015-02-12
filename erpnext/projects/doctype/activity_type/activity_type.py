744aec8def36fe62b1f10a25fa0a7f97a2f9c74d
Neil Trini Lasrado <neil@frappe.io>
2015-02-12 18:47:17 +0530

    activity type fixs

--- erpnext/projects/doctype/activity_type/activity_type.py
+++ erpnext/projects/doctype/activity_type/activity_type.py
@@ -7,4 +7,10 @@ import frappe
 from frappe.model.document import Document
 
 class ActivityType(Document):
-	pass
+	
+	def on_trash(self):
+		self.validate_manufacturing_type()
+		
+	def validate_manufacturing_type(self):
+		if self.activity_type == 'Manufacturing':
+			frappe.throw(_("Activity Type 'Manufacturing' cannot be deleted."))
