7182d349dfb28b95cbfa00c1a3731554c934ab11
Neil Trini Lasrado <neil@frappe.io>
2015-02-16 18:47:58 +0530

    fixes for capacity planning using production planning tool

--- erpnext/manufacturing/doctype/production_order/production_order.py
+++ erpnext/manufacturing/doctype/production_order/production_order.py
@@ -222,11 +222,14 @@ class ProductionOrder(Document):
 			self.actual_end_date = None
 
 	def validate_delivery_date(self):
-		if self.planned_start_date and self.expected_delivery_date and getdate(self.expected_delivery_date) < getdate(self.planned_start_date):
-			frappe.throw(_("Expected Delivery Date cannot be greater than Planned Start Date"))
-
-		if self.planned_end_date and self.expected_delivery_date and getdate(self.expected_delivery_date) < getdate(self.planned_end_date):
-			frappe.msgprint(_("Production might not be able to finish by the Expected Delivery Date."))
+		if self.docstatus==1:
+			if self.planned_start_date and self.expected_delivery_date \
+				and getdate(self.expected_delivery_date) < getdate(self.planned_start_date):
+					frappe.throw(_("Expected Delivery Date cannot be greater than Planned Start Date"))
+		
+			if self.planned_end_date and self.expected_delivery_date \
+				and getdate(self.expected_delivery_date) < getdate(self.planned_end_date):
+					frappe.msgprint(_("Production might not be able to finish by the Expected Delivery Date."))
 
 @frappe.whitelist()
 def get_item_details(item):
