96bc31db270854cf165034df38b6914649022dfb
Neil Trini Lasrado <neil@frappe.io>
2015-03-02 18:54:45 +0530

    job application fixed

--- erpnext/hr/doctype/job_applicant/job_applicant.py
+++ erpnext/hr/doctype/job_applicant/job_applicant.py
@@ -4,14 +4,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
 from frappe.model.document import Document
-from frappe.utils import extract_email_id
 
 class JobApplicant(Document):
-	def validate(self):
-		self.set_status()
-
-	def set_sender(self, sender):
-		"""Will be called by **Communication** when a Job Application is created from an incoming email."""
-		self.email_id = sender
+	pass
