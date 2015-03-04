0c677aa151c541f6939a51b4a7ecea3533b989be
Rushabh Mehta <rmehta@gmail.com>
2015-03-04 12:08:28 +0530

    fix #2847

--- erpnext/setup/doctype/notification_control/notification_control.py
+++ erpnext/setup/doctype/notification_control/notification_control.py
@@ -4,18 +4,8 @@
 from __future__ import unicode_literals
 import frappe
 
-from frappe import _
-
 from frappe.model.document import Document
 
 class NotificationControl(Document):
-	def get_message(self, arg):
-		fn = arg.lower().replace(' ', '_') + '_message'
-		v = frappe.db.sql("select value from tabSingles where field=%s and doctype=%s", (fn, 'Notification Control'))
-		return v and v[0][0] or ''
-
-	def set_message(self, arg = ''):
-		fn = self.select_transaction.lower().replace(' ', '_') + '_message'
-		frappe.db.set(self, fn, self.custom_message)
-		frappe.msgprint(_("Message updated"))
-
+	def validate(self):
+		frappe.clear_cache()
