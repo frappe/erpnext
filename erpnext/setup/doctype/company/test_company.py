b6061e5d3f91533f85b063eba461351bb78f1e26
Nabin Hait <nabinhait@gmail.com>
2015-03-24 17:11:58 +0530

    deleted chart of accounts and style settings doctype

--- erpnext/setup/doctype/company/test_company.py
+++ erpnext/setup/doctype/company/test_company.py
@@ -8,23 +8,7 @@ import frappe
 import unittest
 
 class TestCompany(unittest.TestCase):
-	def atest_coa(self):
-		for country, chart_name in frappe.db.sql("""select country, chart_name
-			from `tabChart of Accounts` where name = 'Deutscher Kontenplan SKR03'""", as_list=1):
-				company_doc = frappe.get_doc({
-					"doctype": "Company",
-					"company_name": "_Test Company 2",
-					"abbr": "_TC2",
-					"default_currency": "INR",
-					"country": country,
-					"chart_of_accounts": chart_name
-				})
-
-				company_doc.insert()
-				self.assertTrue(frappe.db.sql("""select count(*) from tabAccount
-					where company='_Test Company 2'""")[0][0] > 10)
-
-				frappe.delete_doc("Company", "_Test Company 2")
+	pass
 
 
 test_records = frappe.get_test_records('Company')
