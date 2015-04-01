28b0cc4d0b9972db5fb1fb55023ae397d14ba4da
Neil Trini Lasrado <neil@frappe.io>
2015-04-01 15:13:15 +0530

    fixes in offer letter

--- erpnext/hr/doctype/job_applicant/job_applicant.js
+++ erpnext/hr/doctype/job_applicant/job_applicant.js
@@ -5,3 +5,25 @@
 
 // for communication
 cur_frm.email_field = "email_id";
+
+frappe.ui.form.on("Job Applicant", {
+	refresh: function(frm) {
+		if (!frm.doc.__islocal) {
+			if (frm.doc.__onload && frm.doc.__onload.offer_letter) {
+				frm.add_custom_button(__("View Offer Letter"), function() {
+					frappe.set_route("Form", "Offer Letter", frm.doc.__onload.offer_letter);
+				});
+			} else {
+				frm.add_custom_button(__("Make Offer Letter"), function() {
+					frappe.route_options = {
+						"job_applicant": frm.doc.name,
+						"applicant_name": frm.doc.applicant_name,
+						"designation": frm.doc.job_opening,
+					};
+					new_doc("Offer Letter");
+				});
+			}
+		}
+		
+	}
+});
--- erpnext/hr/doctype/offer_letter_term/offer_letter_term.py
+++ erpnext/hr/doctype/offer_letter_term/offer_letter_term.py
@@ -5,5 +5,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class OfferLetterTerms(Document):
+class OfferLetterTerm(Document):
 	pass
