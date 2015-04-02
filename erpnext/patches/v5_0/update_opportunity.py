7c32770a695c00efcfb7facd16273c98c049db4d
Rushabh Mehta <rmehta@gmail.com>
2015-04-02 22:06:36 +0530

    move opportunity item to CRM

--- erpnext/patches/v5_0/update_opportunity.py
+++ erpnext/patches/v5_0/update_opportunity.py
@@ -5,8 +5,10 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	frappe.reload_doctype("Opportunity")
+	frappe.reload_doc('crm', 'doctype', 'opportunity')
+	frappe.reload_doc('crm', 'doctype', 'opportunity_item')
 
 	# all existing opportunities were with items
+	frappe.db.sql("update `tabDocType` set module = 'CRM' where name='Opportunity Item'")
 	frappe.db.sql("update tabOpportunity set with_items=1, title=customer_name")
 	frappe.db.sql("update `tabEmail Account` set append_to='Opportunity' where append_to='Lead'")
