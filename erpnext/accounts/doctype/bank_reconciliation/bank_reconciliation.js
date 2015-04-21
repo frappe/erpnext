80381475c499fb0273d795843a150cfc7d53cf7a
Rushabh Mehta <rmehta@gmail.com>
2015-04-21 11:08:53 +0530

    [ux] re-arranged columns in bank reconciliation, fixes #3114

--- erpnext/accounts/doctype/bank_reconciliation/bank_reconciliation.js
+++ erpnext/accounts/doctype/bank_reconciliation/bank_reconciliation.js
@@ -1,10 +1,25 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
 
-cur_frm.cscript.onload = function(doc, cdt, cdn){
-	cur_frm.set_intro('<i class="icon-question" /> ' +
-		__("Update clearance date of Journal Entries marked as 'Bank Entry'"));
+frappe.ui.form.on("Bank Reconciliation", {
+	update_clearance_date: function(frm) {
+		return frappe.call({
+			method: "update_details",
+			doc: frm.doc
+		});
+	},
+	get_relevant_entries: function(frm) {
+		return frappe.call({
+			method: "get_details",
+			doc: frm.doc,
+			callback: function(r, rt) {
+				frm.refresh()
+			}
+		});
+	}
+});
 
+cur_frm.cscript.onload = function(doc, cdt, cdn) {
 	cur_frm.add_fetch("bank_account", "company", "company");
 
 	cur_frm.set_query("bank_account", function() {
@@ -15,21 +30,8 @@ cur_frm.cscript.onload = function(doc, cdt, cdn){
 			}
 		};
 	});
+
+	cur_frm.set_value("from_date", frappe.datetime.month_start());
+	cur_frm.set_value("to_date", frappe.datetime.month_end());
 }
 
-frappe.ui.form.on("Bank Reconciliation", "update_clearance_date", function(frm) {
-	return frappe.call({
-		method: "update_details",
-		doc: frm.doc
-	})
-})
-
-frappe.ui.form.on("Bank Reconciliation", "get_relevant_entries", function(frm) {
-	return frappe.call({
-		method: "get_details",
-		doc: frm.doc,
-		callback: function(r, rt) {
-			frm.refresh()
-		}
-	})
-})
