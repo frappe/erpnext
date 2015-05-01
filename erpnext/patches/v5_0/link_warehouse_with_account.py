d51b09282a60a3b15a77e894bc5b4685689fdd42
Nabin Hait <nabinhait@gmail.com>
2015-05-01 20:31:11 +0530

    link warehouse with account

---
+++ erpnext/patches/v5_0/link_warehouse_with_account.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	frappe.db.sql("""update tabAccount set warehouse=master_name
+		where ifnull(account_type, '') = 'Warehouse' and ifnull(master_name, '') != ''""")
