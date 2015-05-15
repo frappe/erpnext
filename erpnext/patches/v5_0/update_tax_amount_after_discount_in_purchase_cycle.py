b8939724865b4b5e8848490bad600c9261beeef2
Nabin Hait <nabinhait@gmail.com>
2015-05-15 15:15:25 +0530

    update tax amount after discount amount in purchase cycle

---
+++ erpnext/patches/v5_0/update_tax_amount_after_discount_in_purchase_cycle.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+import frappe
+
+def execute():
+	frappe.db.sql("""
+		update 
+			`tabPurchase Taxes and Charges`
+		set
+			tax_amount_after_discount_amount = tax_amount,
+			base_tax_amount_after_discount_amount = base_tax_amount
+		where
+			ifnull(tax_amount_after_discount_amount, 0) = 0 
+			and ifnull(base_tax_amount_after_discount_amount, 0) = 0 
+	""")
