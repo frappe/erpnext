b0b446e89950b764793d4de5006aa990e09bb66f
Rushabh Mehta <rmehta@gmail.com>
2015-05-29 12:43:39 +0530

    [fix] cleanup tables from description and re-sync print format

--- erpnext/patches/v5_0/update_item_and_description_again.py
+++ erpnext/patches/v5_0/update_item_and_description_again.py
@@ -2,20 +2,18 @@
 # License: GNU General Public License v3. See license.txt
 
 import frappe
-from frappe.website.utils import find_first_image
 from frappe.utils import cstr
 import re
 
 def execute():
 	item_details = frappe._dict()
-	for d in frappe.db.sql("select name, description_html, description from `tabItem`", as_dict=1):
-		description = cstr(d.description_html).strip() or cstr(d.description).strip()
-		image_url, new_desc = extract_image_and_description(description)
+	for d in frappe.db.sql("select name, description from `tabItem`", as_dict=1):
+		description = cstr(d.description).strip()
+		new_desc = extract_description(description)
 
 		item_details.setdefault(d.name, frappe._dict({
 			"old_description": description,
-			"new_description": new_desc,
-			"image_url": image_url
+			"new_description": new_desc
 		}))
 
 
@@ -25,29 +23,27 @@ def execute():
 	for dt in dt_list:
 		frappe.reload_doctype(dt)
 		records = frappe.db.sql("""select name, `{0}` as item_code, description from `tab{1}`
-			where description is not null and image is null and description like '%%<img%%'"""
+			where description is not null and description like '%%<table%%'"""
 			.format("item" if dt=="BOM" else "item_code", dt), as_dict=1)
 
 		count = 1
 		for d in records:
 			if d.item_code and item_details.get(d.item_code) \
 					and cstr(d.description) == item_details.get(d.item_code).old_description:
-				image_url = item_details.get(d.item_code).image_url
 				desc = item_details.get(d.item_code).new_description
 			else:
-				image_url, desc = extract_image_and_description(cstr(d.description))
+				desc = extract_description(cstr(d.description))
 
-			if image_url:
-				frappe.db.sql("""update `tab{0}` set description = %s, image = %s
-					where name = %s """.format(dt), (desc, image_url, d.name))
+			frappe.db.sql("""update `tab{0}` set description = %s
+				where name = %s """.format(dt), (desc, d.name))
 
-				count += 1
-				if count % 500 == 0:
-					frappe.db.commit()
+			count += 1
+			if count % 500 == 0:
+				frappe.db.commit()
 
 
-def extract_image_and_description(data):
-	image_url = find_first_image(data)
-	desc =  re.sub("\<img[^>]+\>", "", data)
+def extract_description(desc):
+	for tag in ("img", "table", "tr", "td"):
+		desc =  re.sub("\</*{0}[^>]*\>".format(tag), "", desc)
 
-	return image_url, desc
+	return desc
