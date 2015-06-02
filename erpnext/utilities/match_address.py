b61a8d44ed4efdf22de8b2e058fadbc4baff7b6a
Anand Doshi <anand@erpnext.com>
2015-06-02 11:49:42 -0400

    started with get_doc_from_address

--- erpnext/utilities/match_address.py
+++ erpnext/utilities/match_address.py
@@ -75,3 +75,66 @@ def validate_unique_combinations(doc):
 		if match_country:
 			frappe.throw(_("Another {0} exists for {1}").format(doc.doctype, doc.country), frappe.DuplicateEntryError)
 
+def get_doc_from_address(doctype, address):
+	"""
+		Match address values with configuration to find the document.
+		Search Order:
+			1. If Address Matches == "Country, State, Post Code Pattern"
+				a. (Country, Post Code)
+				b. (Country, State)
+				c. Country
+			4. Is Home Country? (Country == Company's Country)
+			5. Rest of the World (Country != Company's Country)
+			6. Any Country
+	"""
+	name = (match_country_state_post_code_pattern(doctype, address)
+		or match_home_or_rest_of_the_world(doctype, address)
+		or match_any_country(doctype, address))
+
+	if name:
+		return frappe.get_doc(doctype, name)
+
+def match_country_state_post_code_pattern(doctype, address):
+	doc = None
+
+	results = frappe.get_all(doctype,
+		fields=["name", "country", "state", "post_code_pattern"],
+		filters=prepare_filters(doctype, {
+			"if_address_matches": "Country, State, Post Code Pattern",
+			"country": address.country,
+		}),
+		order_by="creation asc") # to get consistent results everytime
+
+	if results:
+		doc = (_match_country_post_code_pattern(results, address)
+			or _match_country_state(results, address)
+			or _match_only_country(results, address))
+
+	return doc
+
+def _match_country_post_code_pattern(results, address):
+	pass
+
+def _match_country_state(results, address):
+	pass
+
+def _match_only_country(results, address):
+	pass
+
+def match_home_or_rest_of_the_world(doctype, address):
+	pass
+
+def match_any_country(doctype, address):
+	results = frappe.get_all(doctype, filters=prepare_filters({
+		"if_address_matches": "Any Country"
+	}))
+	return results[0].name if results else None
+
+def prepare_filters(doctype, filters):
+	meta = frappe.get_meta(doctype)
+	if meta.get_field("enabled"):
+		filters["enabled"] = 1
+	elif meta.get_field("disabled"):
+		filters["disabled"] = 0
+
+	return filters
