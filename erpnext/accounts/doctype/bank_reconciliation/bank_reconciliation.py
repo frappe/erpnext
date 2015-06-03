8a2b1bcb97524d06989b7310d34e38887bc25bdf
Nabin Hait <nabinhait@gmail.com>
2015-06-03 17:50:35 +0530

    [fix] bank reconciliation: fetch journal entry order by posting date asc

--- erpnext/accounts/doctype/bank_reconciliation/bank_reconciliation.py
+++ erpnext/accounts/doctype/bank_reconciliation/bank_reconciliation.py
@@ -25,7 +25,8 @@ class BankReconciliation(Document):
 			where
 				t2.parent = t1.name and t2.account = %s
 				and t1.posting_date >= %s and t1.posting_date <= %s and t1.docstatus=1
-				and ifnull(t1.is_opening, 'No') = 'No' %s""" %
+				and ifnull(t1.is_opening, 'No') = 'No' %s
+				order by t1.posting_date""" %
 				('%s', '%s', '%s', condition), (self.bank_account, self.from_date, self.to_date), as_dict=1)
 
 		self.set('journal_entries', [])
