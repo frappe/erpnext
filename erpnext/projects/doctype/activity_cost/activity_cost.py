ae4cc078ea23aaf34fe7ede14e46237d293a2433
Neil Trini Lasrado <neil@frappe.io>
2015-06-16 15:30:08 +0530

    Activity Cost - Mandatory removed for Employee.

--- erpnext/projects/doctype/activity_cost/activity_cost.py
+++ erpnext/projects/doctype/activity_cost/activity_cost.py
@@ -15,12 +15,21 @@ class ActivityCost(Document):
 		self.check_unique()
 		
 	def set_title(self):
-		if not self.employee_name:
-			self.employee_name = frappe.db.get_value("Employee", self.employee, "employee_name")
-		self.title = _("{0} for {1}").format(self.employee_name, self.activity_type)
-		
+		if self.employee:
+			if not self.employee_name:
+				self.employee_name = frappe.db.get_value("Employee", self.employee, "employee_name")
+			self.title = _("{0} for {1}").format(self.employee_name, self.activity_type)
+		else:
+			self.title = self.activity_type
+
 	def check_unique(self):
-		if frappe.db.sql("""select name from `tabActivity Cost` where employee_name= %s and activity_type= %s and name != %s""",
-			(self.employee_name, self.activity_type, self.name)):
-				frappe.throw(_("Activity Cost exists for Employee {0} against Activity Type - {1}")
-					.format(self.employee, self.activity_type), DuplicationError)
+		if self.employee:
+			if frappe.db.sql("""select name from `tabActivity Cost` where employee_name= %s and activity_type= %s and name != %s""",
+				(self.employee_name, self.activity_type, self.name)):
+					frappe.throw(_("Activity Cost exists for Employee {0} against Activity Type - {1}")
+						.format(self.employee, self.activity_type), DuplicationError)
+		else:
+			if frappe.db.sql("""select name from `tabActivity Cost` where employee_name IS NULL and activity_type= %s and name != %s""",
+				(self.activity_type, self.name)):
+					frappe.throw(_("Default Activity Cost exists for Activity Type - {0}")
+						.format(self.activity_type), DuplicationError)
