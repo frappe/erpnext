b45a6bcb882b65c26e01baddb76c11a37a4acb07
Rushabh Mehta <rmehta@gmail.com>
2015-06-17 01:54:56 +0530

    [report] BOM Search

--- erpnext/config/manufacturing.py
+++ erpnext/config/manufacturing.py
@@ -103,6 +103,12 @@ def get_data():
 					"name": "Completed Production Orders",
 					"doctype": "Production Order"
 				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "BOM Search",
+					"doctype": "BOM"
+				},
 			]
 		},
 		{
