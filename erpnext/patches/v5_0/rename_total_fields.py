1401e3f679de58c8313feb144ea8571d7c86809c
Nabin Hait <nabinhait@gmail.com>
2015-07-17 14:43:19 +0530

    Update rename_total_fields.py

--- erpnext/patches/v5_0/rename_total_fields.py
+++ erpnext/patches/v5_0/rename_total_fields.py
@@ -3,7 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.model import rename_field
+from frappe.model.utils import rename_field
 from frappe.modules import scrub, get_doctype_module
 
 selling_doctypes = ("Quotation", "Sales Order", "Delivery Note", "Sales Invoice")
