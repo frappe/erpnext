228ff87ea22cc1592496b1129d32609d6a43cfe9
Nabin Hait <nabinhait@gmail.com>
2015-07-27 16:25:56 +0600

    bumped to version 5.3.0

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -27,7 +27,7 @@ blogs.
 """
 app_icon = "icon-th"
 app_color = "#e74c3c"
-app_version = "5.2.1"
+app_version = "5.3.0"
 github_link = "https://github.com/frappe/erpnext"
 
 error_report_email = "support@erpnext.com"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "5.2.1"
+version = "5.3.0"
 
 with open("requirements.txt", "r") as f:
 	install_requires = f.readlines()
