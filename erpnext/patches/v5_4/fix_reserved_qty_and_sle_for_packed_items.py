17405d4130058fc402c8c1fef83e009fe6624a54
Nabin Hait <nabinhait@gmail.com>
2015-07-30 13:47:02 +0530

    repost stock for product bundle materials

--- erpnext/patches/v5_4/fix_reserved_qty_and_sle_for_packed_items.py
+++ erpnext/patches/v5_4/fix_reserved_qty_and_sle_for_packed_items.py
@@ -10,15 +10,19 @@ def execute():
 		where docstatus = 2 and ifnull(update_stock, 0) = 1""")
 
 	if cancelled_invoices:
+		repost_for = frappe.db.sql("""select distinct item_code, warehouse from `tabStock Ledger Entry`
+			where voucher_type = 'Sales Invoice' and voucher_no in (%s)""" 
+			% (', '.join(['%s']*len(cancelled_invoices))), tuple(cancelled_invoices))
+			
 		frappe.db.sql("""delete from `tabStock Ledger Entry` 
 			where voucher_type = 'Sales Invoice' and voucher_no in (%s)""" 
 			% (', '.join(['%s']*len(cancelled_invoices))), tuple(cancelled_invoices))
 			
-	for item_code, warehouse in frappe.db.sql("select item_code, warehouse from tabBin where ifnull(reserved_qty, 0) < 0"):
-		
-		repost_actual_qty(item_code, warehouse)
-		
-		update_bin_qty(item_code, warehouse, {
-			"reserved_qty": get_reserved_qty(item_code, warehouse)
-		})
-	
+		for item_code, warehouse in repost_for:
+			repost_actual_qty(item_code, warehouse)
+			
+	for item_code, warehouse in frappe.db.sql("""select distinct item_code, warehouse 
+		from `tabPacked Item` where parenttype = 'Sales Invoice' and docstatus = 1"""):
+			update_bin_qty(item_code, warehouse, {
+				"reserved_qty": get_reserved_qty(item_code, warehouse)
+			})
