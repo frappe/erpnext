b9b49632f2ec4c006bd1ef99877b789ed6646de0
Neil Trini Lasrado <neil@frappe.io>
2015-08-06 13:35:12 +0530

    Added patch to fetch Item Template Attributes

---
+++ erpnext/patches/v5_4/item_template.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	item_attribute = {}
+	for d in  frappe.db.sql("""select DISTINCT va.attribute, i.variant_of from `tabVariant Attribute` va, `tabItem` i \
+		where va.parent = i.name""", as_dict=1):
+		item_attribute.setdefault(d.variant_of, []).append({"attribute": d.attribute})
+	
+	for item, attributes in item_attribute.items():
+		template = frappe.get_doc("Item", item)
+		template.set('valid_attributes', attributes)
+		template.save()
