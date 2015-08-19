6ce72c5b0726fc2e3ae78c6f0a22e4f03f26a2ca
Nabin Hait <nabinhait@gmail.com>
2015-08-19 14:15:28 +0530

    [fix] Update project cost for draft project only

--- erpnext/patches/v5_4/update_purchase_cost_against_project.py
+++ erpnext/patches/v5_4/update_purchase_cost_against_project.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	for p in frappe.get_all("Project"):
+	for p in frappe.get_all("Project", filters={"docstatus": 0}):
 		project = frappe.get_doc("Project", p.name)
 		project.update_purchase_costing()
 		project.save()
