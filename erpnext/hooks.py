be090fa512122e4a4deabb6c250c48746011a5c1
Anand Doshi <anand@erpnext.com>
2015-09-02 19:28:14 +0600

    bumped to version 6.0.1

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -27,7 +27,7 @@ blogs.
 """
 app_icon = "icon-th"
 app_color = "#e74c3c"
-app_version = "6.0.0"
+app_version = "6.0.1"
 github_link = "https://github.com/frappe/erpnext"
 
 error_report_email = "support@erpnext.com"
