e74d7ca33e514db7ad491736fb8193f7c53d7216
Anand Doshi <anand@erpnext.com>
2015-09-24 15:04:15 +0530

    [fix] [patch] tax rule

--- erpnext/patches/v5_8/tax_rule.py
+++ erpnext/patches/v5_8/tax_rule.py
@@ -5,20 +5,22 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	customers = frappe.db.sql("""select name, default_taxes_and_charges from tabCustomer where 
+	frappe.reload_doc("account", "doctype", "tax_rule")
+
+	customers = frappe.db.sql("""select name, default_taxes_and_charges from tabCustomer where
 		ifnull(default_taxes_and_charges, '') != '' """, as_dict=1)
-		
+
 	for d in customers:
 		tr = frappe.new_doc("Tax Rule")
 		tr.tax_type = "Sales"
 		tr.customer = d.name
 		tr.sales_tax_template = d.default_taxes_and_charges
 		tr.save()
-		
 
-	suppliers = frappe.db.sql("""select name, default_taxes_and_charges from tabSupplier where 
+
+	suppliers = frappe.db.sql("""select name, default_taxes_and_charges from tabSupplier where
 		ifnull(default_taxes_and_charges, '') != '' """, as_dict=1)
-		
+
 	for d in suppliers:
 		tr = frappe.new_doc("Tax Rule")
 		tr.tax_type = "Purchase"
