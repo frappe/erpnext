db48b7d7647e011241761369b4f0829b3d9cb045
Nabin Hait <nabinhait@gmail.com>
2015-10-02 12:05:55 +0530

    [fix] Payment Reconciliation in multi-currency

--- erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.js
+++ erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.js
@@ -74,21 +74,7 @@ erpnext.accounts.PaymentReconciliationController = frappe.ui.form.Controller.ext
 			doc: me.frm.doc,
 			method: 'get_unreconciled_entries',
 			callback: function(r, rt) {
-				var invoices = [];
-
-				$.each(me.frm.doc.invoices || [], function(i, row) {
-					if (row.invoice_number && !inList(invoices, row.invoice_number))
-						invoices.push(row.invoice_type + " | " + row.invoice_number);
-				});
-
-				frappe.meta.get_docfield("Payment Reconciliation Payment", "invoice_number",
-					me.frm.doc.name).options = invoices.join("\n");
-
-				$.each(me.frm.doc.payments || [], function(i, p) {
-					if(!inList(invoices, cstr(p.invoice_number))) p.invoice_number = null;
-				});
-
-				refresh_field("payments");
+				me.set_invoice_options();
 			}
 		});
 
@@ -98,8 +84,29 @@ erpnext.accounts.PaymentReconciliationController = frappe.ui.form.Controller.ext
 		var me = this;
 		return this.frm.call({
 			doc: me.frm.doc,
-			method: 'reconcile'
+			method: 'reconcile',
+			callback: function(r, rt) {
+				me.set_invoice_options();
+			}
 		});
+	},
+	
+	set_invoice_options: function() {
+		var invoices = [];
+
+		$.each(me.frm.doc.invoices || [], function(i, row) {
+			if (row.invoice_number && !inList(invoices, row.invoice_number))
+				invoices.push(row.invoice_type + " | " + row.invoice_number);
+		});
+
+		frappe.meta.get_docfield("Payment Reconciliation Payment", "invoice_number",
+			me.frm.doc.name).options = invoices.join("\n");
+
+		$.each(me.frm.doc.payments || [], function(i, p) {
+			if(!inList(invoices, cstr(p.invoice_number))) p.invoice_number = null;
+		});
+
+		refresh_field("payments");
 	}
 
 });
