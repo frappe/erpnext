2a1ba886f10c8ccf967dfde20a512d456563a470
Rushabh Mehta <rmehta@gmail.com>
2015-10-15 16:56:53 +0530

    [patch-fix]

--- erpnext/patches/v6_4/email_digest_update.py
+++ erpnext/patches/v6_4/email_digest_update.py
@@ -4,3 +4,6 @@ def execute():
 	frappe.reload_doctype("Email Digest")
 	frappe.db.sql("""update `tabEmail Digest` set expense_year_to_date =
 		income_year_to_date""")
+
+	if frappe.db.exists("Email Digest", "Scheduler Errors"):
+		frappe.delete_doc("Email Digest", "Scheduler Errors")
