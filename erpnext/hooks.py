99f4b43641f72eea1962851fff8f92a6093ea4ff
Nabin Hait <nabinhait@gmail.com>
2015-10-27 11:59:35 +0600

    bumped to version 6.6.5

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -29,7 +29,7 @@ blogs.
 """
 app_icon = "icon-th"
 app_color = "#e74c3c"
-app_version = "6.6.4"
+app_version = "6.6.5"
 github_link = "https://github.com/frappe/erpnext"
 
 error_report_email = "support@erpnext.com"
