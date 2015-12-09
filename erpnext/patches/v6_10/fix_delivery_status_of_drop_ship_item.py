5d39a9594dd7fe262049582834e7d8deb2ce55b6
Nabin Hait <nabinhait@gmail.com>
2015-12-09 17:06:09 +0530

    [fix] Reload SO Item in patch

--- erpnext/patches/v6_10/fix_delivery_status_of_drop_ship_item.py
+++ erpnext/patches/v6_10/fix_delivery_status_of_drop_ship_item.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	frappe.reload_doctype("Sales Order Item")
 	for so_name in frappe.db.sql("""select distinct parent from `tabSales Order Item`
 			where delivered_by_supplier=1 and docstatus=1"""):
 		so = frappe.get_doc("Sales Order", so_name[0])
