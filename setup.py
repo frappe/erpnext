c85112b093dd30b2b35acf1f72f23a39ce9d6010
Rushabh Mehta <rmehta@gmail.com>
2016-01-12 12:55:17 +0600

    bumped to version 6.17.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = "6.16.4"
+version = "6.17.0"
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
