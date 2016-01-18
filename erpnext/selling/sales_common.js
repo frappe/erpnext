18642e1e6e5604c9f5f1de440528edd2678487f6
mbauskar <mbauskar@gmail.com>
2016-01-18 16:21:25 +0530

    triggers on Margin type and Margin rate fields to calculate the revised Total Margin and Rate

--- erpnext/selling/sales_common.js
+++ erpnext/selling/sales_common.js
@@ -128,8 +128,13 @@ erpnext.selling.SellingController = erpnext.TransactionController.extend({
 		var item = frappe.get_doc(cdt, cdn);
 		frappe.model.round_floats_in(item, ["price_list_rate", "discount_percentage"]);
 
-		item.rate = flt(item.price_list_rate * (1 - item.discount_percentage / 100.0),
-			precision("rate", item));
+		// check if child doctype is Sales Order Item/Qutation Item and calculate the rate
+		if((cdt == "Quotation Item" || cdt == "Sales Order Item") && item.total_margin)
+			item.rate = flt(item.total_margin * (1 - item.discount_percentage / 100.0),
+				precision("rate", item));
+		else
+			item.rate = flt(item.price_list_rate * (1 - item.discount_percentage / 100.0),
+				precision("rate", item));
 
 		this.calculate_taxes_and_totals();
 	},
@@ -306,6 +311,19 @@ erpnext.selling.SellingController = erpnext.TransactionController.extend({
 				}
 			}
 		})
+	},
+	rate_or_amount: function(doc, cdt, cdn) {
+		// calculated the revised total margin and rate on margin rate changes
+		item = locals[cdt][cdn];
+		cur_frm.cscript.calculate_revised_margin_and_rate(item)
+		this.apply_pricing_rule(frappe.get_doc(cdt, cdn), true);
+		cur_frm.refresh_fields();
+	},
+	type: function(doc, cdt, cdn){
+		// calculate the revised total margin and rate on margin type changes
+		item = locals[cdt][cdn];
+		cur_frm.cscript.calculate_revised_margin_and_rate(item)	
+		this.apply_pricing_rule(frappe.get_doc(cdt, cdn), true);
 	}
 });
 
@@ -325,3 +343,18 @@ frappe.ui.form.on(cur_frm.doctype,"project_name", function(frm) {
 		})
 	}
 })
+
+cur_frm.cscript.calculate_revised_margin_and_rate = function(item){
+	// calculate rate
+
+	if(item.type == "Percentage")
+		item.total_margin = item.price_list_rate + item.price_list_rate * ( item.rate_or_amount / 100);
+	else
+		item.total_margin = item.price_list_rate + item.rate_or_amount;
+
+	// subtracting the discount from total margin
+	item.rate = item.total_margin - item.total_margin * ( item.discount_percentage / 100 );
+	item.amount = item.qty * item.rate
+
+	cur_frm.refresh_fields();
+}
