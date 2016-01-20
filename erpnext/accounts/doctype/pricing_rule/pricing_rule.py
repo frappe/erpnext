dcd8b7773d3bf110192ddcd52d8dc6c1414275f0
mbauskar <mbauskar@gmail.com>
2016-01-20 12:39:57 +0530

    [fix] calculated rate on py side, added depends on for sales order item

--- erpnext/accounts/doctype/pricing_rule/pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/pricing_rule.py
@@ -129,9 +129,9 @@ def get_pricing_rule_for_item(args):
 		# if ignore pricing rule then set the rate or amount field to zero
 		if item_details.doctype in ["Quotation Item", "Sales Order Item"]:
 			item_details.update({
-				"type":"Percentage",
+				"type":"",
 				"rate_or_amount": 0.00
-				})
+			})
 		return item_details
 
 	if not (args.item_group and args.brand):
