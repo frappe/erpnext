cbc18ffdbf2e5a8b3f480fb210d0ce0ddab07fde
shreyas <shreyaspatil87@gmail.com>
2016-01-27 18:07:33 +0530

    [Partial] Fixed the issue for Territory Item Group Variance report

--- erpnext/selling/sales_common.js
+++ erpnext/selling/sales_common.js
@@ -80,10 +80,6 @@ erpnext.selling.SellingController = erpnext.TransactionController.extend({
 				}
 			});
 		}
-
-		if(this.frm.fields_dict.sales_team && this.frm.fields_dict.sales_team.grid.get_field("sales_person")) {
-			this.frm.set_query("sales_person", "sales_team", erpnext.queries.not_a_group_filter);
-		}
 	},
 
 	refresh: function() {
