9a933788bee9270cba85f2d0d335fb99c00a4c8d
Anand Doshi <anand@erpnext.com>
2016-02-16 15:30:17 +0530

    [cleanup] Compact Item Print

--- erpnext/controllers/print_settings.py
+++ erpnext/controllers/print_settings.py
@@ -6,23 +6,24 @@ import frappe
 from frappe.utils import cint
 
 def print_settings_for_item_table(doc):
-	
+
 	doc.print_templates = {
 		"description": "templates/print_formats/includes/item_table_description.html",
 		"qty": "templates/print_formats/includes/item_table_qty.html"
 	}
-	compact_item_view = cint(frappe.db.get_value("Features Setup", None, "compact_item_view"))
-	
+
 	doc.hide_in_print_layout = ["item_code", "item_name", "image", "uom", "stock_uom"]
 
-	std_fields = ["item_code", "item_name", "description", "qty", "rate", "amount", "stock_uom", "uom"] 
+	doc.flags.compact_item_print = cint(frappe.db.get_value("Features Setup", None, "compact_item_print"))
+	doc.flags.compact_item_fields = doc.hide_in_print_layout + ["description", "qty", "rate", "amount"]
+	doc.flags.show_in_description = []
 
-	if compact_item_view:
-		
+	if doc.flags.compact_item_print:
 		for df in doc.meta.fields:
 			if df.fieldtype not in ("Section Break", "Column Break", "Button"):
 				if not doc.is_print_hide(df.fieldname):
-					if df.fieldname not in doc.hide_in_print_layout and df.fieldname not in std_fields:				
+					if df.fieldname not in doc.hide_in_print_layout and df.fieldname not in doc.flags.compact_item_fields:
 						doc.hide_in_print_layout.append(df.fieldname)
+						doc.flags.show_in_description.append(df.fieldname)
 
 
