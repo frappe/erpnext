c26c6c368c5c3bde93e929f9334f7c5a0ca17806
saurabh <saurabh6790@gmail.com>
2016-02-19 11:13:22 +0530

    changed method to set code of language

--- erpnext/setup/doctype/translation/translation.js
+++ erpnext/setup/doctype/translation/translation.js
@@ -6,7 +6,16 @@ frappe.ui.form.on('Translation', {
 	}
 });
 
+frappe.ui.form.on('Translation', {
+	validate: function(frm, cdt, cdn) {
+		var doc = frm.doc;
+		if(!doc.language_code){
+			frm.cscript.update_language_code(frm)
+		}
+	}
+});
+
 cur_frm.cscript.update_language_code = function(frm){
 	var doc = frm.doc;
-	frm.set_value('language_code', frappe.boot.lang_dict[doc.language])
+	frm.set_value('language_code', doc.language)
 }
