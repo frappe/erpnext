755fefbfd4024aab567fc293e84e80b02eb90d46
saurabh <saurabh6790@gmail.com>
2016-02-19 11:11:33 +0530

    import method to load all languages in language field  on customer form

--- erpnext/selling/doctype/customer/customer.js
+++ erpnext/selling/doctype/customer/customer.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
+frappe.require("assets/erpnext/js/controllers/load_languages.js")
 
 frappe.ui.form.on("Customer", {
 	refresh: function(frm) {
