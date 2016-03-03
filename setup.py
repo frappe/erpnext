369b9cf9281e2134fe048eef4375e9e92e36de3c
Rushabh Mehta <rmehta@gmail.com>
2016-03-03 13:43:21 +0600

    bumped to version 6.24.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = "6.24.2"
+version = "6.24.3"
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
