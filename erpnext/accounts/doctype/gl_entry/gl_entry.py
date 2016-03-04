8778344273ac561514e2c3cad11da2e2da32e261
Valmik Jangla <mail@valmik.in>
2016-03-04 14:33:49 +0530

    Removed from Delivery Note and Appraisal Added back in Stock Ledger Entry Added validation in GL Entry and SL Entry Stock Controller sets Fiscal Year using Posting Date

--- erpnext/accounts/doctype/gl_entry/gl_entry.py
+++ erpnext/accounts/doctype/gl_entry/gl_entry.py
@@ -9,6 +9,7 @@ from frappe.model.document import Document
 from erpnext.accounts.party import validate_party_gle_currency, validate_party_frozen_disabled
 from erpnext.accounts.utils import get_account_currency
 from erpnext.setup.doctype.company.company import get_company_currency
+from erpnext.accounts.utils import get_fiscal_year
 from erpnext.exceptions import InvalidAccountCurrency
 
 exclude_from_linked_with = True
@@ -22,6 +23,7 @@ class GLEntry(Document):
 		self.validate_cost_center()
 		self.validate_party()
 		self.validate_currency()
+		self.validate_and_set_fiscal_year()
 
 	def on_update_with_args(self, adv_adj, update_outstanding = 'Yes'):
 		self.validate_account_details(adv_adj)
@@ -106,6 +108,12 @@ class GLEntry(Document):
 
 		if self.party_type and self.party:
 			validate_party_gle_currency(self.party_type, self.party, self.company, self.account_currency)
+
+
+	def validate_and_set_fiscal_year(self):
+		if not self.fiscal_year:
+			self.fiscal_year = get_fiscal_year(self.posting_date, company=self.company)[0]
+
 
 def validate_balance_type(account, adv_adj=False):
 	if not adv_adj and account:
