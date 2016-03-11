39dda5cf49eac3a66a400d4f40f51b6572d65889
Anand Doshi <anand@erpnext.com>
2016-03-11 13:00:42 +0600

    bumped to version 6.25.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = "6.25.4"
+version = "6.25.5"
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
