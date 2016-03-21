7364f0cf1c3e2d544b09264fd0fdd5b0834e0f1d
Rushabh Mehta <rmehta@gmail.com>
2016-03-21 14:47:35 +0530

    [patch-fix] remove old patches for web-forms

--- erpnext/patches/v5_0/portal_fixes.py
+++ erpnext/patches/v5_0/portal_fixes.py
@@ -3,4 +3,4 @@ import erpnext.setup.install
 
 def execute():
 	frappe.reload_doc("website", "doctype", "web_form_field", force=True)
-	erpnext.setup.install.add_web_forms()
+	#erpnext.setup.install.add_web_forms()
--- erpnext/patches/v6_8/make_webform_standard.py
+++ erpnext/patches/v6_8/make_webform_standard.py
@@ -1,9 +1,13 @@
 import frappe
 
 def execute():
-	frappe.reload_doctype("Web Form")
-	frappe.delete_doc("Web Form", "Issues")
-	frappe.delete_doc("Web Form", "Addresses")
+	pass
 
-	from erpnext.setup.install import add_web_forms
-	add_web_forms()
+	# done via fixtures
+
+	# frappe.reload_doctype("Web Form")
+	# frappe.delete_doc("Web Form", "Issues")
+	# frappe.delete_doc("Web Form", "Addresses")
+
+	# from erpnext.setup.install import add_web_forms
+	# add_web_forms()
