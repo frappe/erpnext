2ad801cfd8b517fc0e6b93cade5ef30b382053fd
Kanchan Chauhan <angel143.dr@gmail.com>
2016-03-22 16:00:41 +0530

    Minor Updates

--- erpnext/templates/pages/projects.js
+++ erpnext/templates/pages/projects.js
@@ -1,172 +1,110 @@
 frappe.ready(function() {
-	var reload_tasks = function(taskstatus) {
-		$.ajax({
-			method: "GET",
-			url: "/",
-			dataType: "json",
-			data: {
-				cmd: "erpnext.templates.pages.projects.get_tasks_html",
-				project: '{{ doc.name }}',
-				taskstatus: taskstatus,
-			},
-			dataType: "json",
-			success: function(data) {
-				$('.project-tasks').html(data.message);
-				
-				$('.project-tasks-section .btn-group .btn-primary').removeClass('btn-primary');
-				$('.btn-'+ taskstatus +'-tasks').addClass( "btn-primary" );
-			}
-		});	
-		
-	}
+	$( window ).load(function() {
+		$(".btn-open-tasks").click();
+		$(".btn-open-issues").click();
+	});
 	
 	$('.btn-closed-tasks').click(function() {
-		reload_tasks('closed');
+		reload_items('closed','tasks');
 	});	
-	$('.btn-open-tasks').click(function() {
-		reload_tasks('open');
-	});
 	
-	var reload_issues = function(issuestatus) {
+	$('.btn-open-tasks').click(function() {
+		reload_items('open','tasks');
+	});
+
+	$('.btn-closed-issues').click(function() {
+		reload_items('closed','issues');
+	});	
+	
+	$('.btn-open-issues').click(function() {
+		reload_items('open','issues');
+	});
+
+	var start = 10;
+	$(".more-tasks").click(function() {
+		more_items('tasks', true);
+	});	
+	
+	$(".more-issues").click(function() {
+		more_items('issues', true);
+	});	
+	
+	$(".more-timelogs").click(function() {
+		more_items('timelogs', false);
+	});	
+	
+	$(".more-timelines").click(function() {
+		more_items('timelines', false);
+	});	
+	
+	$( ".project-tasks" ).on('click', '.task-x', function() {
+		var item_name = $(this).attr('id');
+		close_item('task', item_name);
+	});	
+	
+	$( ".project-issues" ).on('click', '.issue-x', function() {
+		var item_name = $(this).attr('id');
+		close_item('issue', item_name);
+	});	
+		
+	var reload_items = function(item_status, item) {
 		$.ajax({
 			method: "GET",
 			url: "/",
 			dataType: "json",
 			data: {
-				cmd: "erpnext.templates.pages.projects.get_issues_html",
+				cmd: "erpnext.templates.pages.projects.get_"+ item +"_html",
 				project: '{{ doc.name }}',
-				issuestatus: issuestatus,
+				item_status: item_status,
 			},
 			dataType: "json",
 			success: function(data) {
-				$('.project-issues').html(data.message);
+				$('.project-'+ item).html(data.message);
 				
-				$('.project-issues-section .btn-group .btn-primary').removeClass('btn-primary');
-				$('.btn-'+ issuestatus +'-issues').addClass( "btn-primary" );
+				$('.project-'+ item +'-section .btn-group .btn-primary').removeClass('btn-primary');
+				$('.btn-'+ item_status +'-'+ item).addClass( "btn-primary" );
+				$(".more-"+ item).toggle(true);
 			}
 		});	
 		
 	}
 	
-	$('.btn-closed-issues').click(function() {
-		reload_issues('closed');
-	});	
-	$('.btn-open-issues').click(function() {
-		reload_issues('open');
-	});
-
-	var taskstart = 5;
-	$(".more-tasks").click(function() {
-		var task_status = $('.project-tasks-section .btn-group .btn-primary').hasClass('btn-closed-tasks') 
-			? 'closed' : 'open';
-
+	var more_items = function(item, item_status){
+		if(item_status)
+		{
+			var item_status = $('.project-'+ item +'-section .btn-group .btn-primary').hasClass('btn-closed-'+ item) 
+				? 'closed' : 'open';
+		}
 		$.ajax({
 			method: "GET",
 			url: "/",
 			dataType: "json",
 			data: {
-				cmd: "erpnext.templates.pages.projects.get_tasks_html",
+				cmd: "erpnext.templates.pages.projects.get_"+ item +"_html",
 				project: '{{ doc.name }}',
-				start: taskstart,
-				taskstatus: task_status,
+				start: start,
+				item_status: item_status,
 			},
 			dataType: "json",
 			success: function(data) {
-				$(data.message).appendTo('.project-tasks');
+				$(data.message).appendTo('.project-'+ item);
 				if(typeof data.message == 'undefined') {	
-					$(".more-tasks").toggle(false);	
+					$(".more-"+ item).toggle(false);	
 				}
-			taskstart = taskstart+5;
+			start = start+10;
 			}
-		});	
-			
-	});
+		});				
+	}
 	
-	var issuestart = 2;
-	$(".more-issues").click(function() {
-		var issue_status = $('.project-issues-section .btn-group .btn-primary').hasClass('btn-closed-issues') 
-			? 'closed' : 'open';
-		
-		$.ajax({
-			method: "GET",
-			url: "/",
-			dataType: "json",
-			data: {
-				cmd: "erpnext.templates.pages.projects.get_issues_html",
-				project: '{{ doc.name }}',
-				start: issuestart,
-				issuestatus: issue_status,
-			},
-			dataType: "json",
-			success: function(data) {
-				$(data.message).appendTo('.project-issues');
-				if(typeof data.message == 'undefined')
-				{	
-					$(".more-issues").toggle(false);
-				}
-				issuestart = issuestart+5;
-			}
-		});	
-	});
-	
-	var timelogstart = 2;
-	$(".more-timelogs").click(function() {
-		$.ajax({
-			method: "GET",
-			url: "/",
-			dataType: "json",
-			data: {
-				cmd: "erpnext.templates.pages.projects.get_timelogs_html",
-				project: '{{ doc.name }}',
-				start: timelogstart,
-			},
-			dataType: "json",
-			success: function(data) {
-				$(data.message).appendTo('.project-timelogs');
-				if(typeof data.message == 'undefined')
-				{	
-					$(".more-timelogs").toggle(false);	
-				}
-				timelogstart = timelogstart+2;
-				
-			}
-		});	
-			
-	});
-	
-	var timelinestart = 10;
-	$(".more-timelines").click(function() {
-		$.ajax({
-			method: "GET",
-			url: "/",
-			dataType: "json",
-			data: {
-				cmd: "erpnext.templates.pages.projects.get_timeline_html",
-				project: '{{ doc.name }}',
-				start: timelinestart,
-				
-			},
-			dataType: "json",
-			success: function(data) {
-				$(data.message).appendTo('.project-timeline');
-				if(typeof data.message == 'undefined')
-				{	
-					$(".more-timelines").toggle(false);
-				}
-				timelinestart = timelinestart+10;
-			}
-		});	
-	});	
-		
-	$( ".project-tasks" ).on('click', '.task-x', function() {
+	var close_item = function(item, item_name){
 		var args = {
 			project: '{{ doc.name }}',
-			task_name: $(this).attr('id'),
-		}
+			item_name: item_name,
+		}		
 		frappe.call({
 			btn: this,
 			type: "POST",
-			method: "erpnext.templates.pages.projects.set_task_status",
+			method: "erpnext.templates.pages.projects.set_"+ item +"_status",
 			args: args,
 			callback: function(r) {
 				if(r.exc) {
@@ -178,27 +116,5 @@ frappe.ready(function() {
 			}
 		})
 		return false;
-	});	
-		
-	$( ".project-issues" ).on('click', '.issue-x', function() {
-		var args = {
-			project: '{{ doc.name }}',
-			issue_name: $(this).attr('id'),
-		}
-		frappe.call({
-			btn: this,
-			type: "POST",
-			method: "erpnext.templates.pages.projects.set_issue_status",
-			args: args,
-			callback: function(r) {
-				if(r.exc) {
-					if(r._server_messages)
-						frappe.msgprint(r._server_messages);
-				} else {
-					$(this).remove();
-				}
-			}
-		})
-		return false;
-	});		
+	}
 });
