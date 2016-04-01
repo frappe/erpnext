2966d89e2a5a86a1a225934e87887957c37e8c70
Saurabh <saurabh6790@gmail.com>
2016-04-01 17:25:24 +0530

    [fixes] create module flow list

---
+++ erpnext/public/js/module_flow.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// MIT License. See license.txt
+
+// for module flow
+
+
+frappe.module_flow =  {
+	"Selling": {
+		"Sales Order": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"],
+		"Quotation": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"]
+	},
+	"Accounts": {
+		"Sales Invoice": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"],
+		"Purchase Invoice": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
+			"Purchase Invoice", "Journal Entry"]
+	},
+	"Buying": {
+		"Purchase Order": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
+			"Purchase Invoice", "Journal Entry"],
+		"Supplier Quotation": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
+			"Purchase Invoice", "Journal Entry"]
+	},
+	"Stock": {
+		"Delivery Note": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"],
+		"Purchase Receipt": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
+			"Purchase Invoice", "Journal Entry"]
+	}
+}
--- erpnext/startup/boot.py
+++ erpnext/startup/boot.py
@@ -32,30 +32,6 @@ def boot_session(bootinfo):
 		bootinfo.docs += frappe.db.sql("""select name, default_currency, cost_center,
 			default_terms, default_letter_head, default_bank_account from `tabCompany`""",
 			as_dict=1, update={"doctype":":Company"})
-		
-		bootinfo.module_flow = {
-			"Selling": {
-				"Sales Order": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"],
-				"Quotation": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"]
-			},
-			"Accounts": {
-				"Sales Invoice": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"],
-				"Purchase Invoice": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
-					"Purchase Invoice", "Journal Entry"]
-			},
-			"Buying": {
-				"Purchase Order": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
-					"Purchase Invoice", "Journal Entry"],
-				"Supplier Quotation": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
-					"Purchase Invoice", "Journal Entry"]
-			},
-			"Stock": {
-				"Delivery Note": ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Journal Entry"],
-				"Purchase Receipt": ["Supplier Quotation", "Purchase Order", "Purchase Receipt",
-					"Purchase Invoice", "Journal Entry"]
-			}
-		}
-		
 
 def load_country_and_currency(bootinfo):
 	country = frappe.db.get_default("country")
