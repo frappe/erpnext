d824fbbdddeb40c1920d776f04971971948f94cd
Nabin Hait <nabinhait@gmail.com>
2016-05-02 15:49:16 +0600

    bumped to version 6.27.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = "6.27.14"
+version = "6.27.15"
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
