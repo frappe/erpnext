bb95d362188e5ffed5a20f7109c85dca3fd8e5bd
Saurabh <saurabh6790@gmail.com>
2016-05-12 11:48:36 +0530

    [fixes] field re-structuring

--- erpnext/accounts/doctype/cheque_print_template/cheque_print_template.js
+++ erpnext/accounts/doctype/cheque_print_template/cheque_print_template.js
@@ -9,14 +9,45 @@ frappe.ui.form.on('Cheque Print Template', {
 			frm.add_custom_button(__("Cheque Print Preview"), function() {
 					erpnext.cheque_print.view_cheque_print(frm);
 				});
+				
+			$(frm.fields_dict.cheque_print_preview.wrapper).empty()
+			
+			$("<div style='position: relative; overflow-x: scroll;'>\
+				<div style='width:"+ frm.doc.cheque_width + "cm; \
+					height: "+ frm.doc.cheque_height +"cm;\
+					background-image: url("+frm.doc.scanned_cheque+");\
+					background-repeat: no-repeat;\
+					background-size: cover;'>\
+					<span style='top: "+frm.doc.date_dist_from_top_edge+"cm;\
+						left: "+ flt(frm.doc.date_dist_from_left_edge) +"cm;\
+						position: absolute;'> "+ frappe.datetime.obj_to_user() +" </span>\
+					<span style='top: "+frm.doc.acc_no_dist_from_top_edge+"cm;\
+						left: "+ frm.doc.acc_no_dist_from_left_edge +"cm;\
+						position: absolute;'> Acc. No. </span>\
+					<span style='top:"+ frm.doc.payer_name_from_top_edge +"cm;\
+						left: "+ flt(frm.doc.payer_name_from_left_edge) +"cm;\
+						position: absolute;'> Payer Name </span>\
+					<span style='top:"+ frm.doc.amt_in_words_from_top_edge +"cm;\
+						left: "+ flt(frm.doc.amt_in_words_from_left_edge) +"cm;\
+						position: absolute;\
+						display: block;\
+						width: "+frm.doc.amt_in_word_width+"cm;\
+						line-height: "+frm.doc.amt_in_words_line_spacing+"cm;\
+						word-wrap: break-word;'> Amount in Words </span>\
+					<span style='top:"+ frm.doc.amt_in_figures_from_top_edge +"cm;\
+						left: "+ flt(frm.doc.amt_in_figures_from_left_edge) +"cm;\
+						position: absolute;'> 1000 </span>\
+					<span style='top:"+ frm.doc.signatory_from_top_edge +"cm;\
+						left: "+ flt(frm.doc.signatory_from_left_edge) +"cm;\
+						position: absolute;'> Signatory Name </span>\
+				</div>\
+			</div>").appendTo(frm.fields_dict.cheque_print_preview.wrapper)
 		}
 	}
 });
 
 
 erpnext.cheque_print.view_cheque_print = function(frm) {
-		console.log("here")
-		console.log(frm.doc)
 		var dialog = new frappe.ui.Dialog({
 			title: 'Cheque Print Preview'
 		});
@@ -28,21 +59,31 @@ erpnext.cheque_print.view_cheque_print = function(frm) {
 
 		$(body).html("<div style='width:"+ frm.doc.cheque_width + "cm; \
 						height: "+ frm.doc.cheque_height +"cm;\
+						background-image: url("+frm.doc.cheque_scan+");\
+						background-repeat: no-repeat;\
+						background-size: cover;\
 						border: 1px solid black;'>\
 							<span style='top: "+frm.doc.data_7+"cm;\
-								right: "+ flt(frm.doc.cheque_width-frm.doc.data_8) +"cm;\
+								left: "+ flt(frm.doc.data_8) +"cm;\
 								position: absolute;'> "+ frappe.datetime.obj_to_user() +" </span>\
+							<span style='top: "+frm.doc.acc_no_ps+"cm;\
+								left: "+ frm.doc.str_loc_acc_no +"cm;\
+								position: absolute;'> 1234567890 </span>\
 							<span style='top:"+ frm.doc.data_9 +"cm;\
 								left: "+ flt(frm.doc.data_10) +"cm;\
-								position: absolute;'> saurabh Palnde </span>\
+								position: absolute;'> Saurabh Palande </span>\
 							<span style='top:"+ frm.doc.data_11 +"cm;\
 								left: "+ flt(frm.doc.data_13) +"cm;\
-								position: absolute;'> Forty One Thousand Six hundred And Sixty Six Only </span>\
+								position: absolute;\
+								display: block;\
+								width: "+frm.doc.ln_width+"cm;\
+								line-height: "+frm.doc.ln_spacing+"cm;\
+								word-wrap: break-word;'>One Crore One Lakh Forty One Thousand Six hundred And Sixty Six Only </span>\
 							<span style='top:"+ frm.doc.data_15 +"cm;\
-								right: "+ flt(frm.doc.cheque_width-frm.doc.data_16) +"cm;\
-								position: absolute;'> 41666.00 </span>\
+								left: "+ flt(frm.doc.data_16) +"cm;\
+								position: absolute;'> 1,01,41666.00 </span>\
 							<span style='top:"+ frm.doc.data_17 +"cm;\
-								right: "+ flt(frm.doc.cheque_width-frm.doc.data_18) +"cm;\
+								left: "+ flt(frm.doc.data_18) +"cm;\
 								position: absolute;'> Frappe Technologies Pvt Ltd </span>\
 					</div>")
 		
--- erpnext/accounts/doctype/cheque_print_template/cheque_print_template.py
+++ erpnext/accounts/doctype/cheque_print_template/cheque_print_template.py
@@ -5,6 +5,54 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt, cstr
 
 class ChequePrintTemplate(Document):
-	pass
+	def on_update(self):
+		if not frappe.db.exists("Print Format", self.name):
+			cheque_print = frappe.new_doc("Print Format")
+			cheque_print.update({
+				"doc_type": "Journal Entry",
+				"standard": "Yes",
+				"custom_format": 1,
+				"print_format_type": "Server",
+				"name": self.name
+			})
+		else:
+			cheque_print = frappe.get_doc("Print Format", self.name)
+		
+		
+		cheque_print.html = """
+<div style="position: relative">
+	<div style="width:%scm;height:%scm;">
+		<span style="top:%s cm; left:%scm;position: absolute;">
+			{{doc.cheque_date or '' }}
+		</span>
+		<span style="top:%scm;left:%scm;position: absolute;">
+			1234567890
+		</span>
+		<span style="top:%scm;left: %scm; position: absolute;">
+			{{doc.pay_to_recd_from}}
+		</span>
+		<span style="top:%scm; left:%scm; position: absolute; display: block;
+			width: %scm; line-height:%scm; word-wrap: break-word;">
+				{{doc.total_amount_in_words}}
+		</span>
+		<span style="top:%scm;left: %scm;position: absolute;">
+			{{doc.get_formatted("total_amount")}}
+		</span>
+		<span style="top:%scm;left: %scm; position: absolute;">
+			{{doc.company}}
+		</span>
+</div>"""%(self.cheque_width, self.cheque_height,
+		self.date_dist_from_top_edge, self.date_dist_from_left_edge,
+		self.acc_no_dist_from_top_edge, self.acc_no_dist_from_left_edge,
+		self.payer_name_from_top_edge, self.payer_name_from_left_edge,
+		self.amt_in_words_from_top_edge, self.amt_in_words_from_left_edge,
+		self.amt_in_word_width, self.amt_in_words_line_spacing,
+		self.amt_in_figures_from_top_edge, self.amt_in_figures_from_left_edge,
+		self.signatory_from_top_edge, self.signatory_from_left_edge)
+			
+		cheque_print.save(ignore_permissions=True)
+		
+			
