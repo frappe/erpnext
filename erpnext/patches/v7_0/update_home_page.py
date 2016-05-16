f1bdae449656b69a5e772a31831fb68e3f0e5de8
Rushabh Mehta <rmehta@gmail.com>
2016-05-16 11:41:29 +0530

    [patch] [fix]

--- erpnext/patches/v7_0/update_home_page.py
+++ erpnext/patches/v7_0/update_home_page.py
@@ -2,13 +2,15 @@ import frappe
 import erpnext
 
 def execute():
+	frappe.db.reload_doctype('Homepage')
+
 	website_settings = frappe.get_doc('Website Settings', 'Website Settings')
 	if frappe.db.exists('Web Page', website_settings.home_page):
 		header = frappe.db.get_value('Web Page', website_settings.home_page, 'header')
 		if header and header.startswith("<div class='hero text-center'>"):
 			homepage = frappe.get_doc('Homepage', 'Homepage')
 			homepage.company = erpnext.get_default_company()
-			homepage.tag_line = header.split('<h1>')[1].split('</h1>')[0] or 'Default Website'
+			homepage.tagline = header.split('<h1>')[1].split('</h1>')[0] or 'Default Website'
 			homepage.setup_items()
 			homepage.save()
 
