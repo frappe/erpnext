d64794510215ee2382e72c4bdbf2f405ef36522e
Saurabh <saurabh6790@gmail.com>
2016-05-26 16:56:14 +0530

    [fixes]

---
+++ erpnext/stock/doctype/warehouse/warehouse_treeview.js
@@ -0,0 +1,4 @@
+frappe.treeview_settings['Warehouse'] = {
+	get_tree_nodes: "erpnext.selling.page.sales_browser.sales_browser.get_children",
+	add_tree_node: "erpnext.selling.page.sales_browser.sales_browser.add_node"
+}
