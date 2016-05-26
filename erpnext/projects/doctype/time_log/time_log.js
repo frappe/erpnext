7d05cee48e58ccccaceabe356a6b7a8c3ddd8f7d
ERPNext <erpnext@erpnext-vm.erpnext-vm>
2016-05-26 10:39:29 +0530

    solve conflict

--- erpnext/projects/doctype/time_log/time_log.js
+++ erpnext/projects/doctype/time_log/time_log.js
@@ -15,6 +15,83 @@ frappe.ui.form.on("Time Log", {
 			return {
 				filters:{
 					'project': frm.doc.project
+	}
+});
+
+frappe.ui.form.on("Time Log", "refresh", function(frm) {
+	// set default user if created
+	if (frm.doc.__islocal && !frm.doc.user) {
+		frm.set_value("user", user);
+	}
+
+	frm.toggle_reqd("activity_type", !frm.doc.for_manufacturing);
+});
+
+
+// set to time if hours is updated
+frappe.ui.form.on("Time Log", "hours",function(frm) {
+	update_time(frm);
+});
+
+frappe.ui.form.on("Time Log","date_worked",function(frm) {
+	update_time(frm);
+});
+
+var update_time = function(frm) {
+	if(cint(sys_defaults.fs_simplified_time_log)){
+		if(frm.doc.date_worked){
+			if(!frm.doc.from_time) {
+				frm.set_value("from_time",moment(frm.doc.date_worked+" "+(moment().format('hh:mm:ss'))).format('YYYY-MM-DD hh:mm:ss'));
+			}
+		} else {
+			return;
+		}
+	}else{
+		if(!frm.doc.from_time) {
+			frm.set_value("from_time", frappe.datetime.now_datetime());
+		}
+	}
+	var d = moment(frm.doc.from_time);
+	d.add(frm.doc.hours, "hours");
+	frm._setting_hours = true;
+	frm.set_value("to_time", d.format(moment.defaultDatetimeFormat));
+	frm._setting_hours = false;
+}
+
+// clear production order if making time log
+frappe.ui.form.on("Time Log", "before_save", function(frm) {
+	frm.doc.production_order && frappe.model.remove_from_locals("Production Order",
+		frm.doc.production_order);
+});
+
+// set hours if to_time is updated
+frappe.ui.form.on("Time Log", "to_time", function(frm) {
+	if(frm._setting_hours) return;
+	frm.set_value("hours", moment(cur_frm.doc.to_time).diff(moment(cur_frm.doc.from_time),
+		"seconds") / 3600);
+
+});
+
+var calculate_cost = function(frm) {
+	frm.set_value("costing_amount", frm.doc.costing_rate * frm.doc.hours);
+	if (frm.doc.billable==1){
+		frm.set_value("billing_amount", (frm.doc.billing_rate * frm.doc.hours) + frm.doc.additional_cost);
+	}
+}
+
+var get_activity_cost = function(frm) {
+	if (frm.doc.activity_type){
+		return frappe.call({
+			method: "erpnext.projects.doctype.time_log.time_log.get_activity_cost",
+			args: {
+				"employee": frm.doc.employee,
+				"activity_type": frm.doc.activity_type
+			},
+			callback: function(r) {
+				if(!r.exc && r.message) {
+					frm.set_value("costing_rate", r.message.costing_rate);
+					frm.set_value("billing_rate", r.message.billing_rate);
+					calculate_cost(frm);
 				}
 			}
 		});
