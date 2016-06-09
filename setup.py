36a0c5c5646ff837f1477dbb2bdf23c700da5443
Anand Doshi <anand@erpnext.com>
2016-06-09 16:29:12 +0530

    [fix] change in versioning: store __version__ in __init__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
+import re, ast
 
-version = "6.27.22"
+# get version from __version__ variable in erpnext/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('erpnext/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
