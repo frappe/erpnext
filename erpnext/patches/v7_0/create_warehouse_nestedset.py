52c2affc9cb89134d2cddb99264e7e761c394e0c
Saurabh <saurabh6790@gmail.com>
2016-06-28 14:41:48 +0530

    [fix] check for parent account with type stock in patch if not exists get parent account from existing warehouse accounts

--- erpnext/patches/v7_0/create_warehouse_nestedset.py
+++ erpnext/patches/v7_0/create_warehouse_nestedset.py
@@ -1,17 +1,21 @@
 import frappe
 from frappe import _
+from frappe.utils import cint
 
 def execute():
 	frappe.reload_doc("stock", "doctype", "warehouse")
-	
+
 	for company in frappe.get_all("Company", fields=["name", "abbr"]):
+		validate_parent_account_for_warehouse(company)
+		
 		if not frappe.db.get_value("Warehouse", "{0} - {1}".format(_("All Warehouses"), company.abbr)):
 			create_default_warehouse_group(company)
 		
 		for warehouse in frappe.get_all("Warehouse", filters={"company": company.name}, fields=["name", "create_account_under",
 			"parent_warehouse", "is_group"]):
 			set_parent_to_warehouses(warehouse, company)
-			set_parent_to_warehouse_acounts(warehouse, company)
+			if cint(frappe.defaults.get_global_default("auto_accounting_for_stock")):
+				set_parent_to_warehouse_acounts(warehouse, company)
 
 def set_parent_to_warehouses(warehouse, company):
 	warehouse = frappe.get_doc("Warehouse", warehouse.name)
@@ -46,3 +50,17 @@ def create_default_warehouse_group(company):
 		"company": company.name,
 		"parent_warehouse": ""
 	}).insert(ignore_permissions=True)
+	
+def validate_parent_account_for_warehouse(company):
+	if cint(frappe.defaults.get_global_default("auto_accounting_for_stock")):
+		parent_account = frappe.db.sql("""select name from tabAccount
+			where account_type='Stock' and company=%s and is_group=1
+			and (warehouse is null or warehouse = '')""", company.name)
+		if not parent_account:
+			current_parent_accounts_for_warehouse = frappe.db.sql("""select parent_account from tabAccount
+				where account_type='Warehouse' and (warehouse is not null or warehouse != '') """)
+			if current_parent_accounts_for_warehouse:
+				doc = frappe.get_doc("Account", current_parent_accounts_for_warehouse[0][0])
+				doc.account_type = "Stock"
+				doc.warehouse = ""
+				doc.save(ignore_permissions=True)
