417750486077062e3784f4c5de61a476d414991e
Rushabh Mehta <rmehta@gmail.com>
2016-07-04 11:41:12 +0530

    [fix] [patch] reload doctype missing in patch

--- erpnext/patches/v7_0/update_prevdoc_values_for_supplier_quotation_item.py
+++ erpnext/patches/v7_0/update_prevdoc_values_for_supplier_quotation_item.py
@@ -1,8 +1,8 @@
 import frappe
-from frappe.utils import cint
 
 def execute():
-	for data in frappe.db.sql(""" select prevdoc_docname, prevdoc_detail_docname, name 
+	frappe.reload_doctype('Supplier Quotation Item')
+	for data in frappe.db.sql(""" select prevdoc_docname, prevdoc_detail_docname, name
 		from `tabSupplier Quotation Item` where prevdoc_docname is not null""", as_dict=True):
 		frappe.db.set_value("Supplier Quotation Item", data.name, "material_request", data.prevdoc_docname)
 		frappe.db.set_value("Supplier Quotation Item", data.name, "material_request_item", data.prevdoc_detail_docname)
