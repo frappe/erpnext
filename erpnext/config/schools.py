20038ad2c940ae128e013d708d5284d58a1ba41e
Rushabh Mehta <rmehta@gmail.com>
2016-07-21 16:01:59 +0530

    [fixes] setup wizard and domainify for schools;

---
+++ erpnext/config/schools.py
@@ -0,0 +1,110 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Student"),
+			"items": [
+
+				{
+					"type": "doctype",
+					"name": "Student"
+				},
+				{
+					"type": "doctype",
+					"name": "Student Group"
+				},
+				{
+					"type": "doctype",
+					"name": "Student Applicant"
+				},
+				{
+					"type": "doctype",
+					"name": "Program Enrollment"
+				},
+				{
+					"type": "doctype",
+					"name": "Program Enrollment Tool"
+				},
+				{
+					"type": "doctype",
+					"name": "Student Group Creation Tool"
+				}
+			]
+		},
+		{
+			"label": _("Schedule"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Course Schedule",
+					"route": "Calendar/Course Schedule"
+				},
+				{
+					"type": "doctype",
+					"name": "Student Attendance"
+				},
+				{
+					"type": "doctype",
+					"name": "Scheduling Tool"
+				},
+				{
+					"type": "doctype",
+					"name": "Examination"
+				}
+			]
+		},
+		{
+			"label": _("Fees"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Fees"
+				},
+				{
+					"type": "doctype",
+					"name": "Fee Structure"
+				},
+				{
+					"type": "doctype",
+					"name": "Fee Category"
+				},
+				{
+					"type": "report",
+					"name": "Student Fee Collection",
+					"doctype": "Fees",
+					"is_query_report": True
+				}
+			]
+		},
+		{
+			"label": _("Setup"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Course"
+				},
+				{
+					"type": "doctype",
+					"name": "Program"
+				},
+				{
+					"type": "doctype",
+					"name": "Instructor"
+				},
+				{
+					"type": "doctype",
+					"name": "Room"
+				},
+				{
+					"type": "doctype",
+					"name": "Academic Term"
+				},
+				{
+					"type": "doctype",
+					"name": "Academic Year"
+				}
+			]
+		},
+	]
--- erpnext/setup/setup_wizard/setup_wizard.py
+++ erpnext/setup/setup_wizard/setup_wizard.py
@@ -32,6 +32,15 @@ def setup_complete(args=None):
 	create_items(args)
 	create_customers(args)
 	create_suppliers(args)
+
+	if args.domain.lower() == 'education':
+		create_academic_term()
+		create_academic_year()
+		create_program(args)
+		create_course(args)
+		create_instructor(args)
+		create_room(args)
+
 	frappe.local.message_log = []
 	setup_domain(args.get('domain'))
 
@@ -492,4 +501,48 @@ def create_users(args):
 				})
 				emp.flags.ignore_mandatory = True
 				emp.insert(ignore_permissions = True)
+
+def create_academic_term():
+	at = ["Semester 1", "Semester 2", "Semester 3"]
+	for d in at:
+		academic_term = frappe.new_doc("Academic Term")
+		academic_term.term_name = d
+		academic_term.save()
+
+def create_academic_year():
+	ac = ["2013-14", "2014-15", "2015-16", "2016-17", "2017-18"]
+	for d in ac:
+		academic_year = frappe.new_doc("Academic Year")
+		academic_year.academic_year_name = d
+		academic_year.save()
+
+def create_program(args):
+	for i in xrange(1,6):
+		if args.get("program_" + str(i)):
+			program = frappe.new_doc("Program")
+			program.program_name = args.get("program_" + str(i))
+			program.save()
+
+def create_course(args):
+	for i in xrange(1,6):
+		if args.get("course_" + str(i)):
+			course = frappe.new_doc("Course")
+			course.course_name = args.get("course_" + str(i))
+			course.save()
+
+def create_instructor(args):
+	for i in xrange(1,6):
+		if args.get("instructor_" + str(i)):
+			instructor = frappe.new_doc("Instructor")
+			instructor.instructor_name = args.get("instructor_" + str(i))
+			instructor.save()
+
+def create_room(args):
+	for i in xrange(1,6):
+		if args.get("room_" + str(i)):
+			room = frappe.new_doc("Room")
+			room.room_name = args.get("room_" + str(i))
+			room.seating_capacity = args.get("room_capacity_" + str(i))
+			room.save()
+
 
