6b3706aef893713d2d1ac61d9fad6cf3ebb05b9d
Nabin Hait <nabinhait@gmail.com>
2016-08-18 17:32:53 +0530

    Update move_employee_parent_to_child_in_salary_structure.py

--- erpnext/patches/v7_0/move_employee_parent_to_child_in_salary_structure.py
+++ erpnext/patches/v7_0/move_employee_parent_to_child_in_salary_structure.py
@@ -2,6 +2,7 @@ import frappe
 
 def execute():
 	frappe.reload_doc('hr', 'doctype', 'salary_structure')
+	frappe.reload_doc('hr', 'doctype', 'salary_structure_employee')
 	for ss in frappe.db.sql(""" select employee, name from `tabSalary Structure`""", as_dict=True):
 		ss_doc = frappe.get_doc('Salary Structure', ss.name)
 		se = ss_doc.append('employees',{})
