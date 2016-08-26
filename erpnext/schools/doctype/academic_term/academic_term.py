760871a8466e2aa8599d042f40a4eafcb4e02271
Chude Osiegbu <chude.osiegbu@manqala.com>
2016-08-26 03:56:10 +0100

    School improvements. Modifications to Academic Term, Assessment, Fees, Student Batch, Student Group. Addition of Grading System.

--- erpnext/schools/doctype/academic_term/academic_term.py
+++ erpnext/schools/doctype/academic_term/academic_term.py
@@ -4,12 +4,18 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import msgprint, _
 from frappe.model.document import Document
 
 class AcademicTerm(Document):
-    def autoname(self):
-        #Concatenate the academic year and the term name to form a unique name for combination of academic year/term
-        self.name = self.academic_year + "/" + self.term_name
-        
-    def on_update(self):
-        pass
+    def validate(self):
+        #Check if entry with same academic_year and the term_name already exists
+        validate_duplication(self)
+        self.title = self.academic_year + " ({})".format(self.term_name) if self.term_name else ""
+    
+
+def validate_duplication(self):
+    term = frappe.db.sql("""select name from `tabAcademic Term` where academic_year= %s and term_name= %s 
+    and docstatus<2 and name != %s""", (self.academic_year, self.term_name, self.name))
+    if term:
+        frappe.throw(_("An academic term with this 'Academic Year' {0} and 'Term Name' {1} already exists. Please modify these entries and try again.").format(self.academic_year,self.term_name))
