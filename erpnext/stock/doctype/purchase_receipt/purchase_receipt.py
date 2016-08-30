ba4456b42bfd1ec20063496f6aeb4834447422f8
RobertSchouten <robert_schouten@live.com>
2016-08-30 08:21:20 +0800

    dont run duplicate set status on submit

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -47,8 +47,9 @@ class PurchaseReceipt(BuyingController):
 
 	def validate(self):
 		super(PurchaseReceipt, self).validate()
-
-		self.set_status()
+		
+		if not self._action=="submit":
+			self.set_status()
 		self.po_required()
 		self.validate_with_previous_doc()
 		self.validate_inspection()
