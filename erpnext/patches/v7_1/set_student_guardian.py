52891b55d2dfd64a8662830c6610dc629216e002
Neil Trini Lasrado <neil@frappe.io>
2016-09-06 17:50:29 +0530

    Fixed Student Guardian patch - reload issue

--- erpnext/patches/v7_1/set_student_guardian.py
+++ erpnext/patches/v7_1/set_student_guardian.py
@@ -4,6 +4,7 @@ def execute():
 	if frappe.db.exists("DocType", "Guardian"):
 		frappe.reload_doc("schools", "doctype", "student")
 		frappe.reload_doc("schools", "doctype", "student_guardian")
+		frappe.reload_doc("schools", "doctype", "student_sibling")
 		guardian = frappe.get_all("Guardian", fields=["name", "student"])
 		for d in guardian:
 			if d.student:
