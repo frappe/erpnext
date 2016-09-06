f79d7188de63bd6dbe10c83bd3d119b71963db02
Nabin Hait <nabinhait@gmail.com>
2016-09-06 17:47:05 +0530

    Patch fixed

--- erpnext/patches/v7_0/migrate_mode_of_payments_v6_to_v7.py
+++ erpnext/patches/v7_0/migrate_mode_of_payments_v6_to_v7.py
@@ -5,8 +5,17 @@ def execute():
 	frappe.reload_doc('accounts', 'doctype', 'sales_invoice_timesheet')
 	frappe.reload_doc('accounts', 'doctype', 'sales_invoice_payment')
 	
-	for data in frappe.db.sql("""select name, mode_of_payment, cash_bank_account, paid_amount from 
-		`tabSales Invoice` where is_pos = 1 and docstatus < 2 and cash_bank_account is not null""", as_dict=1):
+	for data in frappe.db.sql("""select name, mode_of_payment, cash_bank_account, paid_amount, company 
+		from `tabSales Invoice` 
+		where is_pos = 1 and docstatus < 2 
+		and cash_bank_account is not null and cash_bank_account != ''""", as_dict=1):
+		
+		if not data.mode_of_payment and not frappe.db.exists("Mode of Payment", "Cash"):
+			mop = frappe.new_doc("Mode of Payment")
+			mop.mode_of_payment = "Cash"
+			mop.type = "Cash"
+			mop.save()
+		
 		si_doc = frappe.get_doc('Sales Invoice', data.name)
 		si_doc.append('payments', {
 			'mode_of_payment': data.mode_of_payment or 'Cash',
