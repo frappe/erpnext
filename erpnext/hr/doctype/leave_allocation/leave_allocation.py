f4498307c6a031ce7568c2d6b7b3e96b5e08480b
RobertSchouten <robert_schouten@live.com>
2016-09-09 11:35:51 +0800

    fix allow negatives in leave type check

--- erpnext/hr/doctype/leave_allocation/leave_allocation.py
+++ erpnext/hr/doctype/leave_allocation/leave_allocation.py
@@ -87,7 +87,10 @@ class LeaveAllocation(Document):
 			self.from_date, self.to_date)
 		
 		if flt(leaves_taken) > flt(self.total_leaves_allocated):
-			frappe.throw(_("Total allocated leaves {0} cannot be less than already approved leaves {1} for the period").format(self.total_leaves_allocated, leaves_taken), LessAllocationError)
+			if frappe.db.get_value("Leave Type", self.leave_type, "allow_negative"):
+				frappe.msgprint(_("Note: Total allocated leaves {0} shouldn't be less than already approved leaves {1} for the period").format(self.total_leaves_allocated, leaves_taken), LessAllocationError)
+			else:
+				frappe.throw(_("Total allocated leaves {0} cannot be less than already approved leaves {1} for the period").format(self.total_leaves_allocated, leaves_taken), LessAllocationError)
 
 @frappe.whitelist()
 def get_carry_forwarded_leaves(employee, leave_type, date, carry_forward=None):
