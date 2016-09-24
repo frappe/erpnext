73d97dbd7dc0eaa99c3fd2828bb800c84cb15acf
pawan <pawan@erpnext.com>
2016-09-25 03:16:21 +0530

    [fix] #6171

--- erpnext/fleet_management/doctype/vehicle/vehicle_dashboard.py
+++ erpnext/fleet_management/doctype/vehicle/vehicle_dashboard.py
@@ -2,7 +2,7 @@ from frappe import _
 
 data = {
 	'heatmap': True,
-	'heatmap_message': _('This is based on transactions against this Vehicle. See timeline below for details'),
+	'heatmap_message': _('This is based on logs against this Vehicle. See timeline below for details'),
 	'fieldname': 'license_plate',
 	'transactions': [
 		{
