75d455422a5269ecff84b59d80a1ae3df18eb535
Rushabh Mehta <rmehta@gmail.com>
2016-11-21 16:19:50 +0530

    [fix] [patch] rename_quality_inspection_field

--- erpnext/patches/v7_1/rename_quality_inspection_field.py
+++ erpnext/patches/v7_1/rename_quality_inspection_field.py
@@ -5,30 +5,30 @@ from frappe.model.utils.rename_field import *
 def execute():
 	for doctype in ("Purchase Receipt Item", "Delivery Note Item"):
 		frappe.reload_doctype(doctype)
-		
+
 		table_columns = frappe.db.get_table_columns(doctype)
 		if "qa_no" in table_columns:
 			rename_field(doctype, "qa_no", "quality_inspection")
-			
+
 	frappe.reload_doctype("Item")
 	rename_field("Item", "inspection_required", "inspection_required_before_purchase")
-	
-	frappe.reload_doctype("Quality Inspection")
+
+	frappe.reload_doc('stock', 'doctype', 'quality_inspection')
 	frappe.db.sql("""
 		update
 			`tabQuality Inspection`
-		set 
+		set
 			reference_type = 'Purchase Receipt', reference_name = purchase_receipt_no
-		where 
+		where
 			ifnull(purchase_receipt_no, '') != '' and inspection_type = 'Incoming'
 	""")
-	
+
 	frappe.db.sql("""
 		update
 			`tabQuality Inspection`
-		set 
+		set
 			reference_type = 'Delivery Note', reference_name = delivery_note_no
-		where 
+		where
 			ifnull(delivery_note_no, '') != '' and inspection_type = 'Outgoing'
 	""")
 
@@ -36,4 +36,3 @@ def execute():
 		update_reports("Quality Inspection", old_fieldname, "reference_name")
 		update_users_report_view_settings("Quality Inspection", old_fieldname, "reference_name")
 		update_property_setters("Quality Inspection", old_fieldname, "reference_name")
-	
