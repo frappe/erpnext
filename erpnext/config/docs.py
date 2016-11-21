0d5d4aefe5db69cc48f67f44f571665a7dfcc58c
Rushabh Mehta <rmehta@gmail.com>
2016-11-21 18:47:00 +0530

    [docs] added style

--- erpnext/config/docs.py
+++ erpnext/config/docs.py
@@ -19,10 +19,12 @@ docs_version = "7.x.x"
 splash_light_background = True
 
 def get_context(context):
-	context.brand_html = "ERPNext"
+	context.brand_html = ('<img class="brand-logo" src="'+context.docs_base_url
+		+'/assets/img/erpnext-docs.png"> ERPNext</img>')
 	context.app.splash_light_background = True
 	context.top_bar_items = [
 		{"label": "User Manual", "url": context.docs_base_url + "/user/manual", "right": 1},
 		{"label": "Videos", "url": context.docs_base_url + "/user/videos", "right": 1},
-		{"label": "API Documentation", "url": context.docs_base_url + "/current", "right": 1}
+		{"label": "API", "url": context.docs_base_url + "/current", "right": 1},
+		{"label": "Forum", "url": 'https://discuss.erpnext.com', "right": 1}
 	]
