e0089184e60d2d60ac78e0ca17f171d4f130978d
Nabin Hait <nabinhait@gmail.com>
2016-11-23 17:34:01 +0530

    Create Chart of Accounts based on existing company

--- erpnext/setup/doctype/company/test_company.py
+++ erpnext/setup/doctype/company/test_company.py
@@ -7,8 +7,40 @@ test_ignore = ["Account", "Cost Center"]
 import frappe
 import unittest
 
-class TestCompany(unittest.TestCase):
-	pass
-
-
 test_records = frappe.get_test_records('Company')
+
+class TestCompany(unittest.TestCase):
+	def test_coa_based_on_existing_company(self):
+		make_company()
+		
+		expected_results = {
+			"Debtors - CFEC": {
+				"account_type": "Receivable",
+				"is_group": 0,
+				"root_type": "Asset",
+				"parent_account": "Accounts Receivable - CFEC",
+			},
+			"_Test Cash - CFEC": {
+				"account_type": "Cash",
+				"is_group": 0,
+				"root_type": "Asset",
+				"parent_account": "Cash In Hand - CFEC"
+			}
+		}
+		
+		for account, acc_property in expected_results.items():
+			acc = frappe.get_doc("Account", account)
+			for prop, val in acc_property.items():
+				self.assertEqual(acc.get(prop), val)
+		
+		
+def make_company():
+	company = frappe.new_doc("Company")
+	company.company_name = "COA from Existing Company"
+	company.abbr = "CFEC"
+	company.default_currency = "INR"
+	company.create_chart_of_accounts_based_on = "Existing Company"
+	company.existing_company = "_Test Company"
+	company.save()
+
+
--- erpnext/setup/setup_wizard/setup_wizard.py
+++ erpnext/setup/setup_wizard/setup_wizard.py
@@ -88,6 +88,7 @@ def create_fiscal_year_and_company(args):
 			'abbr':args.get('company_abbr'),
 			'default_currency':args.get('currency'),
 			'country': args.get('country'),
+			'create_chart_of_accounts_based_on': 'Standard Template',
 			'chart_of_accounts': args.get(('chart_of_accounts')),
 			'domain': args.get('domain')
 		}).insert()
