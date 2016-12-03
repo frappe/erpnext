7418c86ec510bc41473aa7106419da1f339b42d7
system19 <rick@system19.net>
2016-12-04 04:06:08 +1100

    Minor
    Minor

--- erpnext/schools/doctype/student_attendance/student_attendance.js
+++ erpnext/schools/doctype/student_attendance/student_attendance.js
@@ -1,8 +1,5 @@
 // Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Student Attendance', {
-	refresh: function(frm) {
-
-	}
-});
+cur_frm.add_fetch("course_schedule", "schedule_date", "date");
+cur_frm.add_fetch("course_schedule", "student_batch", "student_batch")
---
+++ erpnext/schools/doctype/student_batch_creation_tool/student_batch_creation_tool.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class StudentBatchCreationTool(Document):
+	def make_batch(self):
+		if self.academic_year and self.program and self.student_batch_name:
+			students = frappe.get_list("Program Enrollment", fields=["student", "student_name"],
+				 filters={"academic_year":self.academic_year, "program": self.program, "student_batch_name": self.student_batch_name},
+				 order_by= "student_name")
+			if students:
+				student_batch = frappe.new_doc("Student Batch")
+				student_batch.update({
+					"academic_year": self.academic_year,
+					"program": self.program,
+					"student_batch_name": self.student_batch_name,
+					"students": students
+				})
+				student_batch.save()
+				frappe.msgprint("Student Batch created.")
+			else:
+				frappe.msgprint("No students found.")
+
