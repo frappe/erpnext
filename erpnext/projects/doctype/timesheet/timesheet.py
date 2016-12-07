86ed030b5cf8e7c9ea107b2cc89130bdafaef543
Rohit Waghchaure <rohitw1991@gmail.com>
2016-12-07 18:49:07 +0530

    minor issue

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -80,8 +80,8 @@ class Timesheet(Document):
 
 	def set_dates(self):
 		if self.docstatus < 2 and self.time_logs:
-			start_date = min([d.from_time for d in self.time_logs])
-			end_date = max([d.to_time for d in self.time_logs])
+			start_date = min([getdate(d.from_time) for d in self.time_logs])
+			end_date = max([getdate(d.to_time) for d in self.time_logs])
 
 			if start_date and end_date:
 				self.start_date = getdate(start_date)
