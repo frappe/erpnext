346a6a5d841c9372156f815985b1d45913f81001
Nabin Hait <nabinhait@gmail.com>
2016-12-13 11:27:46 +0530

    Update set_sales_person_status.py

--- erpnext/patches/v7_1/set_sales_person_status.py
+++ erpnext/patches/v7_1/set_sales_person_status.py
@@ -4,4 +4,5 @@ import frappe
 def execute():
 	frappe.reload_doc('setup','doctype','sales_person')
 	frappe.db.sql("""update `tabSales Person` set enabled=1 
-		where employee IN (select employee from tabEmployee where status != "Left")""")
+		where (employee is null or employee = '' 
+			or employee IN (select employee from tabEmployee where status != "Left"))""")
