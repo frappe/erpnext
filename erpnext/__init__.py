43594b245c8246a12c12ce9125678cc58edbbd26
Nabin Hait <nabinhait@gmail.com>
2016-12-15 19:37:53 +0530

    changed version to beta

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import frappe
 
-__version__ = '7.1.26'
+__version__ = '7.2.0-beta'
 
 def get_default_company(user=None):
 	'''Get default company for user'''
