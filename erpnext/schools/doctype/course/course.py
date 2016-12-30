577cfd559f9a8cbf140eee5e10098d61c8c2d18f
Neil Trini Lasrado <neil@frappe.io>
2016-12-30 16:58:46 +0530

    Removed LMS code in Course master

--- erpnext/schools/doctype/course/course.py
+++ erpnext/schools/doctype/course/course.py
@@ -18,20 +18,3 @@ class Course(Document):
 				total_weightage += criteria.weightage
 			if total_weightage != 100:
 				frappe.throw(_("Total Weightage of all Evaluation Criterias must be 100%"))
-
-def get_sg_list(doctype, txt, filters, limit_start, limit_page_length=20):
-	user = frappe.session.user
-	student = frappe.db.sql("select name from `tabStudent` where student_email_id= %s", user)
-	if student:
-		return frappe.db.sql('''select course, academic_term, academic_year, SG.name from `tabStudent Group`
-			as SG, `tabStudent Group Student` as SGS where SG.name = SGS.parent and SGS.student = %s
-			order by SG.name asc limit {0} , {1}'''.format(limit_start, limit_page_length), student, as_dict=True)
-
-def get_list_context(context=None):
-	return {
-		"show_sidebar": True,
-		'no_breadcrumbs': True,
-		"title": _("Courses"),
-		"get_list": get_sg_list,
-		"row_template": "templates/includes/course/course_row.html"
-	}
