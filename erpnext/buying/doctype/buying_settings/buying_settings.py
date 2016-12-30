985cb8226dcd3c31c8c9bdbac4e92bffe147e22b
Rushabh Mehta <rmehta@gmail.com>
2016-12-30 15:06:54 +0530

    [minor] dont get warehouse in opening entry #7353

--- erpnext/buying/doctype/buying_settings/buying_settings.py
+++ erpnext/buying/doctype/buying_settings/buying_settings.py
@@ -9,11 +9,10 @@ import frappe
 from frappe.model.document import Document
 
 class BuyingSettings(Document):
-		
 	def validate(self):
 		for key in ["supplier_type", "supp_master_name", "maintain_same_rate", "buying_price_list"]:
 			frappe.db.set_default(key, self.get(key, ""))
 
 		from erpnext.setup.doctype.naming_series.naming_series import set_by_naming_series
-		set_by_naming_series("Supplier", "supplier_name", 
+		set_by_naming_series("Supplier", "supplier_name",
 			self.get("supp_master_name")=="Naming Series", hide_name_field=False)
