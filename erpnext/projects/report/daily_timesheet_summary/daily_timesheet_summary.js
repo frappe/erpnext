81130e848eead13aa9706b8914a60723885fd217
robert schouten <robert_schouten@live.com>
2017-01-09 08:26:21 +0800

    proposed usage of daterange picker

--- erpnext/projects/report/daily_timesheet_summary/daily_timesheet_summary.js
+++ erpnext/projects/report/daily_timesheet_summary/daily_timesheet_summary.js
@@ -4,16 +4,12 @@
 frappe.query_reports["Daily Timesheet Summary"] = {
 	"filters": [
 		{
-			"fieldname":"from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.get_today()
-		},
-		{
-			"fieldname":"to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.get_today()
+			"fieldname":"date",
+			"label": __("Date"),
+			"fieldtype": "DateRange",
+			"start_with_value": true,
+			"default_from":frappe.datetime.get_today(),
+			"default_to":frappe.datetime.get_today(),
 		},
 	]
 }
