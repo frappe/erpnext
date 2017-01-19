00d3cd6d64c9c93ae6717b7f48f559be4e0d5b3e
Rushabh Mehta <rmehta@gmail.com>
2017-01-19 14:27:02 +0530

    [fix] contact address link patch

--- erpnext/patches/v7_2/contact_address_links.py
+++ erpnext/patches/v7_2/contact_address_links.py
@@ -1,8 +1,10 @@
 import frappe
+from frappe.core.doctype.dynamic_link.dynamic_link import deduplicate_dynamic_links
 
 def execute():
-	frappe.reload_doctype('Contact')
-	frappe.reload_doctype('Address')
+	frappe.reload_doc('core', 'doctype', 'dynamic_link')
+	frappe.reload_doc('email', 'doctype', 'contact')
+	frappe.reload_doc('geo', 'doctype', 'address')
 	map_fields = (
 		('Customer', 'customer'),
 		('Supplier', 'supplier'),
@@ -21,4 +23,5 @@ def execute():
 						dirty = True
 
 					if dirty:
-						doc.save()
+						deduplicate_dynamic_links(doc)
+						doc.update_children()
