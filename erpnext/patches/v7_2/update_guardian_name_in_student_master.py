6d0da98a78d7f70ba8f686f4fa6b369022c77741
Neil Trini Lasrado <neil@frappe.io>
2017-01-27 17:44:28 +0530

    Fixes to update student guardian name patch

--- erpnext/patches/v7_2/update_guardian_name_in_student_master.py
+++ erpnext/patches/v7_2/update_guardian_name_in_student_master.py
@@ -2,6 +2,7 @@ import frappe
 from frappe.model.utils.rename_field import rename_field
 
 def execute():
+    frappe.reload_doc("schools", "doctype", "student_guardian")
     student_guardians = frappe.get_all("Student Guardian", fields=["guardian"])
     for student_guardian in student_guardians:
         guardian_name = frappe.db.get_value("Guardian", student_guardian.guardian, "guardian_name")
