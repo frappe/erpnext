879a2d84dccd4d122488cf739acc0efebe1af32c
Neil Trini Lasrado <neil@frappe.io>
2017-01-31 17:03:37 +0530

    Added Evaluation Criteria Group

---
+++ erpnext/schools/doctype/evaluation_criteria_group/evaluation_criteria_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Evaluation Criteria Group', {
+	refresh: function(frm) {
+
+	}
+});
