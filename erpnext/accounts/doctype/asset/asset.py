e5cb3e34fe26f4733056fea01fdcc47ef127f016
nick9822 <nick9822@gmail.com>
2017-02-15 15:32:50 +0530

    nowdate changed to purchase_date
    next depreciation date should be compared with purchase_date instead of nowdate.

--- erpnext/accounts/doctype/asset/asset.py
+++ erpnext/accounts/doctype/asset/asset.py
@@ -80,7 +80,7 @@ class Asset(Document):
 			if cint(self.number_of_depreciations_booked) > cint(self.total_number_of_depreciations):
 				frappe.throw(_("Number of Depreciations Booked cannot be greater than Total Number of Depreciations"))
 						
-		if self.next_depreciation_date and getdate(self.next_depreciation_date) < getdate(nowdate()):
+		if self.next_depreciation_date and getdate(self.next_depreciation_date) < getdate(self.purchase_date):
 			frappe.msgprint(_("Next Depreciation Date is entered as past date"))
 			
 		if (flt(self.value_after_depreciation) > flt(self.expected_value_after_useful_life) 
