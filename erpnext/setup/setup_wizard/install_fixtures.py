fe79334c910b8c351af5f9d5c74613ecb3c8290c
Neil Trini Lasrado <neil@frappe.io>
2017-02-22 16:26:41 +0530

    Added 'All Assessment Group' to fixtures

--- erpnext/setup/setup_wizard/install_fixtures.py
+++ erpnext/setup/setup_wizard/install_fixtures.py
@@ -193,6 +193,11 @@ def install(country=None):
 
 		{'doctype': "Print Heading", 'print_heading': _("Credit Note")},
 		{'doctype': "Print Heading", 'print_heading': _("Debit Note")},
+
+		# Assessment Group
+		{'doctype': 'Assessment Group', 'assessment_group_name': _('All Assessment Groups'),
+			'is_group': 1, 'parent_assessment_group': ''},
+	
 	]
 
 	from erpnext.setup.setup_wizard.industry_type import get_industry_types
