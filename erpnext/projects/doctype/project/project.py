f800892ec3f92ebc58579e8ec5477358990c8fc9
superlack <gd84@hotmail.com>
2017-02-24 12:33:59 -0800

    Update project.py

--- erpnext/projects/doctype/project/project.py
+++ erpnext/projects/doctype/project/project.py
@@ -178,8 +178,8 @@ class Project(Document):
 		self.total_purchase_cost = total_purchase_cost and total_purchase_cost[0][0] or 0
 		
 	def update_sales_costing(self):
-		total_sales_cost = frappe.db.sql("""select sum(base_net_amount)
-			from `tabSales Order Item` where project = %s and docstatus=1""", self.name)
+		total_sales_cost = frappe.db.sql("""select sum(total)
+			from `tabSales Order` where project = %s and docstatus=1""", self.name)
 
 		self.total_sales_cost = total_sales_cost and total_sales_cost[0][0] or 0
 				
