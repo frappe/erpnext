0bb434af3fdfc1e042e1808129f3523264b7b1c6
Rohit Waghchaure <rohitw1991@gmail.com>
2017-03-06 18:52:34 +0530

    minor fix in patch

--- erpnext/patches/v7_2/stock_uom_in_selling.py
+++ erpnext/patches/v7_2/stock_uom_in_selling.py
@@ -9,5 +9,6 @@ def execute():
 	doctype_list = ['Sales Order Item', 'Delivery Note Item', 'Quotation Item', 'Sales Invoice Item']
 
 	for doctype in doctype_list:
+		frappe.reload_doctype(doctype)
 		frappe.db.sql("""update `tab{doctype}` 
 		 		set uom = stock_uom, conversion_factor = 1, stock_qty = qty""".format(doctype=doctype))
