d41836b3860aa6798466e49c9c668ee7ccf98650
Manas Solanki <manas@erpnext.com>
2017-03-08 11:32:00 +0530

    [Fix] Student Sibling

--- erpnext/schools/doctype/student/student.js
+++ erpnext/schools/doctype/student/student.js
@@ -1,1 +1,17 @@
-cur_frm.add_fetch("guardian", "guardian_name", "guardian_name");
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+
+frappe.ui.form.on("Student Guardian", {
+	guardian: function(frm) {
+		frm.add_fetch("guardian", "guardian_name", "guardian_name");
+	}
+});
+
+frappe.ui.form.on('Student Sibling', {
+	student: function(frm) {
+		frm.add_fetch("student", "title", "full_name");
+		frm.add_fetch("student", "gender", "gender");
+		frm.add_fetch("student", "date_of_birth", "date_of_birth");
+	}
+});
