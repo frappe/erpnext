68b2a1281b876addb7dafcfa040b5c49469ad4d4
Ricardo Johann <johannricardo@yahoo.com.br>
2017-03-16 05:44:50 -0300

    Export total labels

--- erpnext/accounts/report/trial_balance/trial_balance.js
+++ erpnext/accounts/report/trial_balance/trial_balance.js
@@ -61,7 +61,34 @@ frappe.require("assets/erpnext/js/financial_statements.js", function() {
 				"fieldtype": "Check"
 			}
 		],
-		"formatter": erpnext.financial_statements.formatter,
+		"formatter": function(row, cell, value, columnDef, dataContext, default_formatter) {
+			if (columnDef.df.fieldname=="account") {
+				value = dataContext.account_name;
+								
+				if (typeof dataContext.account_name != 'undefined' && dataContext.account_name[0] == "'"
+					&& dataContext.account_name[dataContext.account_name.length -1] == "'") 
+				{
+					dataContext.account = dataContext.account_name.substring(1, dataContext.account_name.length - 1);
+				}
+				
+				columnDef.df.link_onclick =
+					"erpnext.financial_statements.open_general_ledger(" + JSON.stringify(dataContext) + ")";
+				columnDef.df.is_tree = true;
+			}
+	
+			value = default_formatter(row, cell, value, columnDef, dataContext);
+	
+			if (!dataContext.parent_account) {
+				var $value = $(value).css("font-weight", "bold");
+				if (dataContext.warn_if_negative && dataContext[columnDef.df.fieldname] < 0) {
+					$value.addClass("text-danger");
+				}
+	
+				value = $value.wrap("<p></p>").parent().html();
+			}
+	
+			return value;
+		},
 		"tree": true,
 		"name_field": "account",
 		"parent_field": "parent_account",
