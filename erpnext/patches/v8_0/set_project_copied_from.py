825e053e6693cca9f5002a7baab13198a598a758
Nabin Hait <nabinhait@gmail.com>
2017-04-20 08:50:27 +0530

    Fixed project_copied_from patch

--- erpnext/patches/v8_0/set_project_copied_from.py
+++ erpnext/patches/v8_0/set_project_copied_from.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-    frappe.db.sql('''
-        UPDATE `tabProject`
-        SET copied_from=name
-        WHERE copied_from is NULL
-    ''')
+	frappe.reload_doctype("Project")
+	
+	frappe.db.sql('''
+		UPDATE `tabProject`
+		SET copied_from=name
+		WHERE copied_from is NULL
+	''')
