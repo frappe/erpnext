4df8c60e3e9618655db1871c199631c0ae38ffba
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2017-04-20 12:29:42 +0300

    Improving Item Price DocType (fixes #8456)

--- erpnext/stock/doctype/item_price/test_item_price.py
+++ erpnext/stock/doctype/item_price/test_item_price.py
@@ -5,10 +5,27 @@ from __future__ import unicode_literals
 import unittest
 import frappe
 
+
 class TestItem(unittest.TestCase):
-	def test_duplicate_item(self):
-		from erpnext.stock.doctype.item_price.item_price import ItemPriceDuplicateItem
-		doc = frappe.copy_doc(test_records[0])
-		self.assertRaises(ItemPriceDuplicateItem, doc.save)
+	def test_addition_of_new_fields(self):
+		# Based on https://github.com/frappe/erpnext/issues/8456
+		test_fields_existance = [
+			'disable', 'customer', 'uom', 'min_qty', 'lead_time_days',
+			'packing_unit', 'valid_from', 'valid_upto', 'note'
+		]
+		doc_fields = frappe.copy_doc(test_records[1]).__dict__.keys()
+
+		for test_field in test_fields_existance:
+			self.assertTrue(test_field in doc_fields)
+
+	def test_dates_validation_error(self):
+		doc = frappe.copy_doc(test_records[1])
+		# Enter invalid dates valid_from  >= valid_upto
+		doc.valid_from = "2017-04-20"
+		doc.valid_upto = "2017-04-17"
+		# Valid Upto Date can not be less/equal than Valid From Date
+		self.assertRaises(frappe.ValidationError, doc.save)
+
+
 
 test_records = frappe.get_test_records('Item Price')
