fd163f1382c59463d7e8545c8f14b8d13274767b
pratu16x7 <pratu16x7@gmail.com>
2017-04-22 17:27:06 +0530

    Show filters

--- erpnext/hub_node/__init__.py
+++ erpnext/hub_node/__init__.py
@@ -5,11 +5,13 @@ from __future__ import unicode_literals
 import frappe, requests
 
 @frappe.whitelist()
-def get_items(text, start, limit):
+def get_items(text, start, limit, category=None, seller=None):
 	hub = frappe.get_single("Hub Settings")
 	response = requests.get(hub.hub_url + "/api/method/hub.hub.api.get_items", params={
 		"password": hub.password,
 		"text": text,
+		"category": category,
+		"seller": seller,
 		"start": start,
 		"limit": limit
 	})
@@ -29,3 +31,23 @@ def get_categories():
 	response = requests.get(hub.hub_url + "/api/method/hub.hub.api.get_categories")
 	response.raise_for_status()
 	return response.json().get("message")
+
+@frappe.whitelist()
+def get_seller_details(user_name):
+	hub = frappe.get_single("Hub Settings")
+	response = requests.get(hub.hub_url + "/api/method/hub.hub.api.get_user_details", params={
+		"password": hub.password,
+		"user_name": user_name,
+	})
+	response.raise_for_status()
+	return response.json().get("message")
+
+@frappe.whitelist()
+def send_rfq(website, supplier, supplier_name, email_id, company):
+	response = requests.get(website + "/api/method/erpnext.hub_node.api.make_rfq", params={
+		"supplier": supplier,
+		"supplier_name": supplier_name,
+		"email_id": email_id,
+		"company": company
+	})
+	return frappe._dict({"message":"sent_rfq"})
