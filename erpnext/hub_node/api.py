d71602991e44ad772888f8449b271735d3aac235
pratu16x7 <pratu16x7@gmail.com>
2017-04-24 13:32:47 +0530

    [start] cart, api for rfq and opp

--- erpnext/hub_node/api.py
+++ erpnext/hub_node/api.py
@@ -5,11 +5,36 @@ import frappe, json
 from frappe.utils import now, nowdate
 
 @frappe.whitelist(allow_guest=True)
-def make_rfq(supplier, supplier_name, email_id, company):
+def make_rfq(supplier, item):
+
+	# supplier_name = "Supp1"
+	# supplier_type = "Distributor"
+	# supplier_email = "supp1@example.com"
+	# company = "ABC"
+
+	# item_code = "Item1"
+	# item_group = "Products"
+
+	supplier = frappe.new_doc("Supplier")
+	supplier.supplier_name = supplier_name
+	supplier.supplier_type = supplier_type
+
+	supplier.insert(ignore_permissions = True)
+	frappe.db.commit()
+
+	item = frappe.new_doc("Item")
+	item.item_code = item_code
+	item.item_group = item_group
+
+	item.insert(ignore_permissions = True)
+	frappe.db.commit()
+
+
+	# supplier, supplier_name, email_id, company
 	supplier_data = {
-		"supplier": supplier,
+		"supplier": supplier_name,
 		"supplier_name": supplier_name,
-		"email_id": email_id
+		"email_id": supplier_email
 	}
 	rfq = frappe.new_doc('Request for Quotation')
 	rfq.transaction_date = nowdate()
@@ -20,15 +45,62 @@ def make_rfq(supplier, supplier_name, email_id, company):
 	rfq.append('suppliers', supplier_data)
 
 	rfq.append("items", {
-		"item_code": "_Test Item",
-		"description": "_Test Item",
-		"uom": "_Test UOM",
+		"item_code": item_code,
+		"description": "ajhs asghaoshf wanfq wdjow",
+		"uom": "Nos",
 		"qty": 5,
-		"warehouse": "_Test Warehouse - _TC",
+		"warehouse": "Stores - A",
 		"schedule_date": nowdate()
 	})
-	print "--------------------3---------------------"
 	rfq.insert(ignore_permissions=True)
-	print "-----------------4------------------------"
+	frappe.db.commit()
 
 	return rfq
+
+
+	# WORKING
+
+	# supplier_name = "Eg5"
+	# email_id = "eg5@example.com"
+
+	# lead = frappe.new_doc("Lead")
+
+	# lead.lead_name = supplier_name
+	# lead.email_id = email_id
+
+	# lead.insert(ignore_permissions=True)
+	# frappe.db.commit()
+
+
+	# opportunity = frappe.new_doc("Opportunity")
+	# opportunity.enquiry_from = "Lead"
+	# opportunity.lead = frappe.get_all("Lead", filters={"email_id": email_id}, fields = ["name"])[0]["name"]
+
+	# opportunity.insert(ignore_permissions=True)
+	# frappe.db.commit()
+
+	# return opportunity
+
+@frappe.whitelist(allow_guest=True)
+def make_opportunity(buyer_name, email_id):
+	# buyer_name = "Eg5"
+	# email_id = "eg5@example.com"
+
+	buyer_name = "HUB-" + buyer_name
+
+	lead = frappe.new_doc("Lead")
+
+	lead.lead_name = buyer_name
+	lead.email_id = email_id
+
+	lead.insert(ignore_permissions=True)
+	frappe.db.commit()
+
+
+	opportunity = frappe.new_doc("Opportunity")
+	opportunity.enquiry_from = "Lead"
+	opportunity.lead = frappe.get_all("Lead", filters={"email_id": email_id}, fields = ["name"])[0]["name"]
+	opportunity.insert(ignore_permissions=True)
+	frappe.db.commit()
+
+	return opportunity
