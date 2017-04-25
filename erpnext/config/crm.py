053ad207ca92e6c1440ef0b4e79bba2320fd64d0
pawan <pawan@erpnext.com>
2017-04-25 19:01:27 +0530

    Add Report Links

--- erpnext/config/crm.py
+++ erpnext/config/crm.py
@@ -62,6 +62,18 @@ def get_data():
 					"name": "Inactive Customers",
 					"doctype": "Sales Order"
 				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Campaign Efficiency",
+					"doctype": "Lead"
+				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Lead Owner Efficiency",
+					"doctype": "Lead"
+				}
 			]
 		},
 		{
