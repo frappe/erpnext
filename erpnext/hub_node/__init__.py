a10f47b20fe9946abb4251798ec8e9a6d4ae175e
pratu16x7 <pratu16x7@gmail.com>
2017-05-01 10:46:56 +0530

    rfq working

--- erpnext/hub_node/__init__.py
+++ erpnext/hub_node/__init__.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe, requests
+import frappe, requests, json
 
 @frappe.whitelist()
 def get_items(text, start, limit, category=None, seller=None):
@@ -42,29 +42,44 @@ def get_seller_details(user_name):
 	response.raise_for_status()
 	return response.json().get("message")
 
+# TESTER
 @frappe.whitelist()
-def send_rfq():
+def test():
 	hub = frappe.get_single("Hub Settings")
-	response = requests.get(hub.hub_url + "/api/method/hub.hub.api.send_rfq")
+	response = requests.get(hub.hub_url + "/api/method/hub.hub.api.test")
 	response.raise_for_status()
 	return frappe._dict({"message":"sent_rfq_message"})
 
-@frappe.whitelist()
-def send_rfq_message(website, supplier, supplier_name, email_id, company):
-	response = requests.get(website + "/api/method/erpnext.hub_node.api.load_message", params={
-		"supplier": supplier,
-		"supplier_name": supplier_name,
-		"email_id": email_id,
-		"company": company
-	})
-	return frappe._dict({"message":"sent_rfq_message"})
+# @frappe.whitelist()
+# def send_rfq_message(website, supplier, supplier_name, email_id, company):
+# 	response = requests.get(website + "/api/method/erpnext.hub_node.api.load_message", params={
+# 		"supplier": supplier,
+# 		"supplier_name": supplier_name,
+# 		"email_id": email_id,
+# 		"company": company
+# 	})
+# 	return frappe._dict({"message":"sent_rfq_message"})
+
+# @frappe.whitelist()
+# def send_opportunity_message(website, supplier, supplier_name, email_id, company):
+# 	response = requests.get(website + "/api/method/erpnext.hub_node.api.load_message", params={
+# 		"supplier": supplier,
+# 		"supplier_name": supplier_name,
+# 		"email_id": email_id,
+# 		"company": company
+# 	})
+# 	return frappe._dict({"message":"sent_opportunity_message"})
 
 @frappe.whitelist()
-def send_opportunity_message(website, supplier, supplier_name, email_id, company):
-	response = requests.get(website + "/api/method/erpnext.hub_node.api.load_message", params={
-		"supplier": supplier,
-		"supplier_name": supplier_name,
-		"email_id": email_id,
-		"company": company
+def load_message(msg_type, receiver, receiver_website, item):
+	hub = frappe.get_single("Hub Settings")
+	args = json.loads(item)
+	response = requests.get(hub.hub_url + "/api/method/hub.hub.api.load_message", params={
+		"password": hub.password,
+		"msg_type": msg_type,
+		"sender": hub.hub_user_name,
+		"receiver": receiver,
+		"receiver_website": receiver_website,
+		"msg_data": item
 	})
-	return frappe._dict({"message":"sent_opportunity_message"})
+	return frappe._dict({"message":"sent_rfq_message"})
