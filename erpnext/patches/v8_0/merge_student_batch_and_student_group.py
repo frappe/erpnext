4968e912da795fc0dced08469e0b1f8ee3ad9eac
Manas Solanki <manassolanki@gmail.com>
2017-05-01 12:29:32 +0530

    Patch for renaming the student batch as student group

--- erpnext/patches/v8_0/merge_student_batch_and_student_group.py
+++ erpnext/patches/v8_0/merge_student_batch_and_student_group.py
@@ -3,9 +3,17 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.model.utils.rename_field import *
 
 def execute():
 
 	# for deleting the student batch creation tool
 	frappe.delete_doc("DocType", "Student Batch Creation Tool", force=1)
 	frappe.db.sql("drop table if exists `Student Batch Creation Tool`")
+
+	# changing the student batch to student group in the student attendance
+	frappe.reload_doctype("Student Attendance")
+
+	table_columns = frappe.db.get_table_columns("Student Attendance")
+	if "student_batch" in table_columns:
+		rename_field(doctype, "student_batch", "student_group")
