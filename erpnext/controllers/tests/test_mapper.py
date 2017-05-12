dcf8dbabcf49d8941a3623ad478c2718b615b847
pratu16x7 <pratu16x7@gmail.com>
2017-05-12 15:23:19 +0530

    [minor] fix test with make_test_records

--- erpnext/controllers/tests/test_mapper.py
+++ erpnext/controllers/tests/test_mapper.py
@@ -6,11 +6,13 @@ import random, json
 import frappe.utils
 from frappe.utils import nowdate
 from frappe.model import mapper
+from frappe.test_runner import make_test_records
 
 class TestMapper(unittest.TestCase):
 	def test_map_docs(self):
 		'''Test mapping of multiple source docs on a single target doc'''
 
+		make_test_records("Item")
 		items = frappe.get_all("Item", fields = ["name", "item_code"], filters = {'is_sales_item': 1, 'has_variants': 0})
 		customers = frappe.get_all("Customer")
 		if items and customers:
@@ -18,7 +20,7 @@ class TestMapper(unittest.TestCase):
 			customer = random.choice(customers).name
 			qtn1, item_list_1 = self.make_quotation(items, customer)
 			qtn2, item_list_2 = self.make_quotation(items, customer)
-			so, item_list_3 = self.make_sales_order(items, customer)
+			so, item_list_3 = self.make_sales_order()
 
 		# Map source docs to target with corresponding mapper method
 		method = "erpnext.selling.doctype.quotation.quotation.make_sales_order"
@@ -28,8 +30,6 @@ class TestMapper(unittest.TestCase):
 		src_items = item_list_1 + item_list_2 + item_list_3
 		self.assertEqual(set([d.item_code for d in src_items]),
 			set([d.item_code for d in updated_so.items]))
-
-		updated_so.insert(ignore_permissions=True)
 
 	def get_random_items(self, items, limit):
 		'''Get a number of random items from a list of given items'''
@@ -52,16 +52,22 @@ class TestMapper(unittest.TestCase):
 		qtn.submit()
 		return qtn, item_list
 
-	def make_sales_order(self, items, customer):
-		item_list = self.get_random_items(items, 3)
-		so = frappe.get_doc({
-			"doctype": "Sales Order",
-			"delivery_date": nowdate(),
-			"customer": customer,
-			"order_type": "Sales"
+	def make_sales_order(self):
+		item = frappe.get_doc({
+			"base_amount": 1000.0,
+			"base_rate": 100.0,
+			"description": "CPU",
+			"doctype": "Sales Order Item",
+			"item_code": "_Test Item Home Desktop 100",
+			"item_name": "CPU",
+			"parentfield": "items",
+			"qty": 10.0,
+			"rate": 100.0,
+			"warehouse": "_Test Warehouse - _TC",
+			"stock_uom": "_Test UOM",
+			"conversion_factor": 1.0,
+			"uom": "_Test UOM"
 		})
-		for item in item_list:
-			so.append("items", {"qty": "3", "item_code": item.item_code,
-				"uom": "Nos", "delivery_warehouse": "Stores - WPL"})
+		so = frappe.get_doc(frappe.get_test_records('Sales Order')[0])
 		so.insert(ignore_permissions=True)
-		return so, item_list
+		return so, [item]
