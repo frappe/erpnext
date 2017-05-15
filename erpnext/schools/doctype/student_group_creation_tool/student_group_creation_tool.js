b31a04668bc38585447f641be9cc8d6d7191dd55
Manas Solanki <manassolanki@gmail.com>
2017-05-15 11:43:26 +0530

    Changes in the student group creation tool

--- erpnext/schools/doctype/student_group_creation_tool/student_group_creation_tool.js
+++ erpnext/schools/doctype/student_group_creation_tool/student_group_creation_tool.js
@@ -6,20 +6,27 @@ frappe.ui.form.on("Student Group Creation Tool", "refresh", function(frm) {
 			doc:frm.doc
 		})
 	});
-	
+	frappe.realtime.on("student_group_creation_progress", function(data) {
+			if(data.progress) {
+				frappe.hide_msgprint(true);
+				frappe.show_progress(__("Creating student groups"), data.progress[0],data.progress[1]);
+			}
+	});
 });
 
 frappe.ui.form.on("Student Group Creation Tool", "get_courses", function(frm) {
 	frm.set_value("courses",[]);
-	frappe.call({
-		method: "get_courses",
-		doc:frm.doc,
-		callback: function(r) {
-			if(r.message) {
-				frm.set_value("courses", r.message);
+	if (frm.doc.academic_year && frm.doc.program) {
+		frappe.call({
+			method: "get_courses",
+			doc:frm.doc,
+			callback: function(r) {
+				if(r.message) {
+					frm.set_value("courses", r.message);
+				}
 			}
-		}
-	})
+		})
+	}
 });
 
 frappe.ui.form.on("Student Group Creation Tool", "onload", function(frm){
