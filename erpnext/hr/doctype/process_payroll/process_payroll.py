b074a69024cf24a5f2d0ef6aaed76dc64d0038e8
Ayush AS <ayush.as.shukla@gmail.com>
2017-05-17 13:15:49 +0530

    [minor] process_payroll_create salary fixed

--- erpnext/hr/doctype/process_payroll/process_payroll.py
+++ erpnext/hr/doctype/process_payroll/process_payroll.py
@@ -112,7 +112,7 @@ class ProcessPayroll(Document):
 
 
 	def create_log(self, ss_list):
-		if not ss_list:
+		if not ss_list or len(ss_list) < 1: 
 			log = "<p>" + _("No employee for the above selected criteria OR salary slip already created") + "</p>"
 		else:
 			log = frappe.render_template("templates/includes/salary_slip_log.html",
