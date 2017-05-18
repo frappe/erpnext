09134d40c79f2a03a884a68434a2e39bf4f2ed8a
tunde <tundebabzy@gmail.com>
2017-05-18 16:00:13 +0100

    replaces whitespace indentation with tabs

--- erpnext/tests/test_init.py
+++ erpnext/tests/test_init.py
@@ -8,7 +8,6 @@ test_records = frappe.get_test_records('Company')
 
 
 class TestInit(unittest.TestCase):
-
 	def test_encode_company_abbr(self):
 		company = frappe.new_doc("Company")
 		company.company_name = "New from Existing Company For Test"
@@ -20,9 +19,9 @@ class TestInit(unittest.TestCase):
 
 		names = [
 			"Warehouse Name", "ERPNext Foundation India", "Gold - Member - {a}".format(a=abbr),
-		    " - {a}".format(a=abbr), "ERPNext - Foundation - India",
-		    "ERPNext Foundation India - {a}".format(a=abbr),
-		    "No-Space-{a}".format(a=abbr), "- Warehouse"
+			" - {a}".format(a=abbr), "ERPNext - Foundation - India",
+			"ERPNext Foundation India - {a}".format(a=abbr),
+			"No-Space-{a}".format(a=abbr), "- Warehouse"
 		]
 
 		expected_names = [
@@ -37,5 +36,5 @@ class TestInit(unittest.TestCase):
 			enc_name = encode_company_abbr(names[i], company.name)
 			self.assertTrue(
 				enc_name == expected_names[i],
-			    "{enc} is not same as {exp}".format(enc=enc_name, exp=expected_names[i])
+				"{enc} is not same as {exp}".format(enc=enc_name, exp=expected_names[i])
 			)
