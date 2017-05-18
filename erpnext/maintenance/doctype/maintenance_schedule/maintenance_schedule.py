52e1ba77144ae5177d994f561f1727ddb579af72
Omar Jaber <omar.ja93@gmail.com>
2017-05-18 09:23:26 +0300

    update throw message in maintenance_schedule.py (#8896)
    updating Throw message

--- erpnext/maintenance/doctype/maintenance_schedule/maintenance_schedule.py
+++ erpnext/maintenance/doctype/maintenance_schedule/maintenance_schedule.py
@@ -163,7 +163,7 @@ class MaintenanceSchedule(TransactionBase):
 					`tabMaintenance Schedule Item` msi where msi.parent=ms.name and
 					msi.sales_order=%s and ms.docstatus=1""", d.sales_order)
 				if chk:
-					throw(_("Maintenance Schedule {0} exists against {0}").format(chk[0][0], d.sales_order))
+					throw(_("Maintenance Schedule {0} exists against {1}").format(chk[0][0], d.sales_order))
 
 	def validate(self):
 		self.validate_maintenance_detail()
