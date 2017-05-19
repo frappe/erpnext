113fb995fd5771824bbd9468c3e9907dcb4c8d37
Charles-Henri Decultot <chdecultot@dokos.io>
2017-05-19 12:34:10 +0200

    View Attachments in portal (#8830)
    * View Attachments in portal
    
    * HTML beautify correction
    
    * Move option to shopping cart settings
    
    * new field in shopping cart settings for public attachments display in portal

--- erpnext/templates/pages/order.py
+++ erpnext/templates/pages/order.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import frappe
 
 from frappe import _
+from erpnext.shopping_cart.doctype.shopping_cart_settings.shopping_cart_settings import show_attachments
 
 def get_context(context):
 	context.no_cache = 1
@@ -12,6 +13,9 @@ def get_context(context):
 	context.doc = frappe.get_doc(frappe.form_dict.doctype, frappe.form_dict.name)
 	if hasattr(context.doc, "set_indicator"):
 		context.doc.set_indicator()
+
+	if show_attachments():
+                context.attachments = get_attachments(frappe.form_dict.doctype, frappe.form_dict.name)
 
 	context.parents = frappe.form_dict.parents
 	context.payment_ref = frappe.db.get_value("Payment Request",
@@ -21,3 +25,7 @@ def get_context(context):
 
 	if not frappe.has_website_permission(context.doc):
 		frappe.throw(_("Not Permitted"), frappe.PermissionError)
+
+def get_attachments(dt, dn):
+        return frappe.get_all("File", fields=["name", "file_name", "file_url", "is_private"],
+                              filters = {"attached_to_name": dn, "attached_to_doctype": dt, "is_private":0})
