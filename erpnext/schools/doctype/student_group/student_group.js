d7612720769c3318a978b35936481f2f736bd1ed
Nabin Hait <nabinhait@gmail.com>
2017-05-24 17:31:58 +0530

    Revert "link the different doctype in the school module (#8844)"
    This reverts commit c320537e4ca682c6139fd5cb40fa0cf61a0cd2bb.

--- erpnext/schools/doctype/student_group/student_group.js
+++ erpnext/schools/doctype/student_group/student_group.js
@@ -13,23 +13,11 @@ frappe.ui.form.on("Student Group", {
 
 	refresh: function(frm) {
 		if (!frm.doc.__islocal) {
-			frm.add_custom_button(__("Attendance"), function() {
-				frappe.route_options = {
-					based_on: "Student Group",
-					student_group: frm.doc.name
-				}
-				frappe.set_route("List", "Student Attendance Tool");
-			});
 			frm.add_custom_button(__("Course Schedule"), function() {
-				frappe.route_options = {
-					student_group: frm.doc.name
-				}
 				frappe.set_route("List", "Course Schedule");
 			});
+
 			frm.add_custom_button(__("Assessment Plan"), function() {
-				frappe.route_options = {
-					student_group: frm.doc.name
-				}
 				frappe.set_route("List", "Assessment Plan");
 			});
 			frm.add_custom_button(__("Update Email Group"), function() {
@@ -42,9 +30,6 @@ frappe.ui.form.on("Student Group", {
 				});
 			});
 			frm.add_custom_button(__("Newsletter"), function() {
-				frappe.route_options = {
-					"Newsletter Email Group.email_group": frm.doc.name
-				}
 				frappe.set_route("List", "Newsletter");
 			});
 		}
