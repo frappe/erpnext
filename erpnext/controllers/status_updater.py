6fc350097c88314b333c57197b30a30c7a1737f5
tunde <tundebabzy@gmail.com>
2017-06-01 11:21:14 +0100

    adds stricter status update conditions

--- erpnext/controllers/status_updater.py
+++ erpnext/controllers/status_updater.py
@@ -89,11 +89,11 @@ status_map = {
 	"Material Request": [
 		["Draft", None],
 		["Stopped", "eval:self.status == 'Stopped'"],
-		["Pending", "eval:self.per_ordered == 0 and self.docstatus == 1"],
-		["Partially Ordered", "eval:self.per_ordered < 100 and self.per_ordered > 0 and self.docstatus == 1"],
-		["Ordered", "eval:self.per_ordered == 100 and self.docstatus == 1 and self.material_request_type == 'Purchase'"],
-		["Transferred", "eval:self.per_ordered == 100 and self.docstatus == 1 and self.material_request_type == 'Material Transfer'"],
-		["Issued", "eval:self.per_ordered == 100 and self.docstatus == 1 and self.material_request_type == 'Material Issue'"]
+		["Pending", "eval:self.status != 'Stopped' and self.per_ordered == 0 and self.docstatus == 1"],
+		["Partially Ordered", "eval:self.status != 'Stopped' and self.per_ordered < 100 and self.per_ordered > 0 and self.docstatus == 1"],
+		["Ordered", "eval:self.status != 'Stopped' and self.per_ordered == 100 and self.docstatus == 1 and self.material_request_type == 'Purchase'"],
+		["Transferred", "eval:self.status != 'Stopped' and self.per_ordered == 100 and self.docstatus == 1 and self.material_request_type == 'Material Transfer'"],
+		["Issued", "eval:self.status != 'Stopped' and self.per_ordered == 100 and self.docstatus == 1 and self.material_request_type == 'Material Issue'"]
 	]
 }
 
