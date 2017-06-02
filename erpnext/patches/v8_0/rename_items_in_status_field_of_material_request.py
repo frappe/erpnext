32aebec3b0a2991b8d5ab9e40f064148bb4c10c4
tunde <tundebabzy@gmail.com>
2017-06-02 04:25:13 +0100

    adds patch to rename the present material request status

--- erpnext/patches/v8_0/rename_items_in_status_field_of_material_request.py
+++ erpnext/patches/v8_0/rename_items_in_status_field_of_material_request.py
@@ -5,14 +5,21 @@ def execute():
 	frappe.db.sql(
 		"""
 		UPDATE `tabMaterial Request`
-		SET status = CASE
-						WHEN per_ordered = 0 AND docstatus = 1 THEN 'Pending'
-						WHEN per_ordered < 100 AND per_ordered > 0 AND docstatus = 1 THEN 'Partially Ordered'
-						WHEN per_ordered = 100 AND docstatus = 1 AND material_request_type = 'Purchase' THEN 'Ordered'
-						WHEN per_ordered = 100 AND docstatus = 1 AND material_request_type = 'Material Transfer' THEN 'Transferred'
-						WHEN per_ordered = 100 AND docstatus = 1 AND material_request_type = 'Material Issue' THEN 'Issued'
-					END
-		WHERE
-			status != 'Stopped' and status != 'Draft' AND status != 'Cancelled';
+			SET status = CASE
+							WHEN docstatus = 2 THEN 'Cancelled'
+							WHEN docstatus = 0 THEN 'Draft'
+							ELSE CASE
+								WHEN status = 'Stopped' THEN 'Stopped'
+								WHEN status != 'Stopped' AND per_ordered = 0 THEN 'Pending'
+								WHEN per_ordered < 100 AND per_ordered > 0 AND status != 'Stopped'
+									THEN 'Partially Ordered'
+								WHEN per_ordered = 100 AND material_request_type = 'Purchase'
+									AND status != 'Stopped' THEN 'Ordered'
+								WHEN per_ordered = 100 AND material_request_type = 'Material Transfer'
+									AND status != 'Stopped' THEN 'Transferred'
+								WHEN per_ordered = 100 AND material_request_type = 'Material Issue'
+									AND status != 'Stopped' THEN 'Issued'
+							END
+			END
 		"""
 	)
