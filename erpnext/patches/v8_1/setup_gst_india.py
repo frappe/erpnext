c20abf6b2ae6cb2518971ea03b7edbf7035b1661
Saurabh <saurabh6790@gmail.com>
2017-06-27 13:21:46 +0530

    send email to system managers about gst setup

--- erpnext/patches/v8_1/setup_gst_india.py
+++ erpnext/patches/v8_1/setup_gst_india.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.email import sendmail_to_system_managers
 
 def execute():
 	frappe.reload_doc('regional', 'doctype', 'gst_hsn_code')
@@ -11,3 +12,22 @@ def execute():
 	if frappe.db.get_single_value('System Settings', 'country')=='India':
 		from erpnext.regional.india.setup import setup
 		setup()
+		send_gst_update_email()
+
+def send_gst_update_email():
+	message = """Hello,
+
+<p>ERPNext is now GST Ready.</p>
+
+<p>To start making GST Invoices from 1st of July, you just need to create new Tax Accounts, Templates and update your Customer's and Supplier's GST Numbers.</p>
+
+<p>Please refer {gst_document_link} to know more about how to setup and implement GST in ERPNext.</p>
+
+<p>Please contact us at support@erpnext.com, if you have any questions.</p>
+
+<p>Thanks,</p>
+ERPNext Team.
+	""".format(gst_document_link="<a href='http://frappe.github.io/erpnext/user/manual/en/regional/india/'> ERPNext GST Document </a>")
+
+	sendmail_to_system_managers("[Important] ERPNext GST updates", message)
+
