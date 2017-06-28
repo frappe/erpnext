7cd9001ca34edd56ac5991fd5cc1f88f7826dbac
Jamsheer <jamsheer@earthianslive.com>
2017-06-28 14:37:13 +0530

    Unused Childs - Work Schedule and User List - Removed

--- erpnext/healthcare/doctype/physician/physician.js
+++ erpnext/healthcare/doctype/physician/physician.js
@@ -19,14 +19,6 @@ frappe.ui.form.on('Physician', {
 		if(!frm.is_new()) {
 			frappe.contacts.render_address_and_contact(frm);
 		}
-	},
-	validate: function (frm) {
-		schedules = frm.doc.schedule
-		if (schedules){
-			for(j=0; j<schedules.length; j++){
-				if (validate_schedule(schedules, schedules[j]) == false) return false;
-			}
-		};
 	}
 });
 
@@ -80,68 +72,4 @@ frappe.ui.form.on("Physician", "employee",
 				}
 		})
 	}
-});
-
-var get_average = function (start, end, limit) {
-	if(limit){
-		duration = moment.utc(moment(end,"HH:mm:ss").diff(moment(start,"HH:mm:ss"))).format("HH:mm:ss")
-
-		//I am combining the snippets I found in multiple pages.
-		//Conversion of hh:mm:ss to seconds, divided by the limit and then again convert to hh:mm:ss.
-		var hms =	duration	// your input string
-		var a = hms.split(':'); // split it at the colons
-
-		// minutes are worth 60 seconds. Hours are worth 60 minutes.
-		var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
-		var newSeconds= seconds/limit;
-
-		// multiply by 1000 because Date() requires miliseconds
-		var date = new Date(newSeconds * 1000);
-		var hh = date.getUTCHours();
-		var mm = date.getUTCMinutes();
-		var ss = date.getSeconds();
-		// If you were building a timestamp instead of a duration, you would uncomment the following line to get 12-hour (not 24) time
-		// if (hh > 12) {hh = hh % 12;}
-		// These lines ensure you have two-digits
-		if (hh < 10) {hh = "0"+hh;}
-		if (mm < 10) {mm = "0"+mm;}
-		//if (ss < 10) {ss = "0"+ss;}
-		// This formats your string to HH:MM:SS
-		var t = hh+":"+mm+":00";
-		return t
-	};
-};
-
-var validate_schedule = function (schedules, child) {
-	start = new Date(new Date().toDateString() + ' ' + child.start)
-	end = new Date(new Date().toDateString() + ' '+ child.end)
-	if(start >= end){
-		frappe.msgprint(__("From and To times are not valid in line {0}", [child.idx]));
-		return false
-	};
-	for(i=0; i<schedules.length; i++){
-		if(schedules[i].day === child.day && schedules[i].name !== child.name){
-			sch_start = new Date(new Date().toDateString() + ' ' + schedules[i].start)
-			sch_end = new Date(new Date().toDateString() + ' '+ schedules[i].end)
-			if ((start > sch_start	 && start < sch_end) || (end > sch_start	&& end < sch_end) || (start >= sch_start	&& end <= sch_end)){
-				frappe.msgprint(__("Schedule for {0}	{1} - {2} and {3} - {4} overlap", [child.day, sch_start.toTimeString(), sch_end.toTimeString(), start.toTimeString(), end.toTimeString()]))
-				return false;
-			}
-		};
-	};
-	return true;
-};
-
-frappe.ui.form.on("Work Schedule", {
-	limit: function(frm, cdt, cdn) {
-		var child = locals[cdt][cdn]
-		avg = get_average(child.start, child.end, child.limit)
-		frappe.model.set_value(cdt, cdn, 'average', avg)
-		if(child.start && child.end && child.day){
-			if (validate_schedule(frm.doc.schedule, child) !== true ){
-				frappe.msgprint("Schedules overlap")
-				return
-			}
-		}
-	},
 });
