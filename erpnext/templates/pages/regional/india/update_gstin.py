02b0ed41990e7cbd2417e07c82c7dfbf1aeefeba
Rushabh Mehta <rmehta@gmail.com>
2017-06-29 11:43:37 +0530

    [fix] update-gstin

--- erpnext/templates/pages/regional/india/update_gstin.py
+++ erpnext/templates/pages/regional/india/update_gstin.py
@@ -4,6 +4,7 @@ from frappe import _
 def get_context(context):
 	context.no_cache = 1
 	party = frappe.form_dict.party
+	context.party_name = party
 
 	try:
 		update_gstin(context)
@@ -11,14 +12,15 @@ def get_context(context):
 		context.invalid_gstin = 1
 
 	party_type = 'Customer'
-	party = frappe.db.get_value('Customer', party)
+	party_name = frappe.db.get_value('Customer', party)
 
-	if not party:
+	if not party_name:
 		party_type = 'Supplier'
-		party = frappe.db.get_value('Supplier', party)
+		party_name = frappe.db.get_value('Supplier', party)
 
-	if not party:
-		frappe.throw(_("Not Found"), frappe.DoesNotExistError)
+	if not party_name:
+		context.not_found = 1
+		return
 
 	context.party = frappe.get_doc(party_type, party)
 	context.party.onload()
