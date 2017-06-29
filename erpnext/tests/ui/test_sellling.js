217061f00bd43b1b549aa2b1463481277e6b2c6a
Rushabh Mehta <rmehta@gmail.com>
2017-06-29 18:15:35 +0530

    [minor] remove old tests

--- erpnext/tests/ui/test_sellling.js
+++ erpnext/tests/ui/test_sellling.js
@@ -1,36 +1,3 @@
-erpnext.selling_tests = {
-	new_quotation: (assert) => {
-		frappe.tests.require(['Customer', 'Item'])
-		return new Promise(resolve => {
-			frappe.set_route('List', 'Quotation')
-				.then(() => {
-					return frappe.new_doc('Quotation')
-				})
-				.then(() => {
-					return cur_frm
-						.get_field('customer')
-						.set_value(frappe.tests.get_any('Customer'));
-				})
-				.then(() => {
-					return cur_frm
-						.get_field('items').grid
-						.get_row(0)
-						.activate()
-						.get_field('item_code')
-						.set_value(frappe.tests.get_any('Item'));
-				})
-				.then(() => {
-					return cur_frm.save()
-				})
-				.then(() => {
-					assert.ok(!cur_frm.doc.is_new());
-					assert.ok(!cur_frm.doc.is_new());
-					resolve();
-				});
-		});
-	}
-};
-
 QUnit.module('sales');
 
 QUnit.test("test quotation", function(assert) {
