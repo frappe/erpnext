a6a24add09aa74f79592542e6793eca0fa999179
Rohit Waghchaure <rohitw1991@gmail.com>
2017-06-30 16:13:23 +0530

    [Fix] GSN code auto name

--- erpnext/patches/v8_1/removed_roles_from_gst_report_non_indian_account.py
+++ erpnext/patches/v8_1/removed_roles_from_gst_report_non_indian_account.py
@@ -9,10 +9,10 @@ def execute():
 	company = frappe.get_all('Company', filters = {'country': 'India'})
 
 	if not company:
-		frappe.db.sql(""" 
-			delete from 
-				`tabHas Role` 
-			where 
-				parenttype = 'Report' and parent in('GST Sales Register', 
-					'GST Purchase Register', 'GST Itemised Sales Register', 
+		frappe.db.sql("""
+			delete from
+				`tabHas Role`
+			where
+				parenttype = 'Report' and parent in('GST Sales Register',
+					'GST Purchase Register', 'GST Itemised Sales Register',
 					'GST Itemised Purchase Register')""")
