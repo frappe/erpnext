264eeb04b1eb759485a5bcc9a40cff522238aa40
tunde <tundebabzy@gmail.com>
2017-06-30 23:10:04 +0100

    adds test case for Payroll Cycle

--- erpnext/hr/doctype/payroll_cycle/test_payroll_cycle.py
+++ erpnext/hr/doctype/payroll_cycle/test_payroll_cycle.py
@@ -6,5 +6,40 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
+from frappe.utils import getdate
+
+designations = {"name": "President", "designation_name": "President",
+                "doctype": "Designation"}
+
+period = {"end_date": "2017-06-30", "start_date": "2017-06-01",
+          "doctype": "Payroll Period", "idx": "1"}
+
+cycle_dict = {"cycle_name": '_Test Payment Cycle All Employees', "company_name": '_Test Company',
+              "payroll_frequency": "Monthly", "doctype": "Payroll Cycle"}
+
+
+def get_document(dt, designatione):
+	designation = frappe.get_doc(designatione)
+	if not frappe.db.exists(dt, designation.name):
+		designation.insert()
+	return designation
+
+get_document('Designation', designations)
+
+
 class TestPayrollCycle(unittest.TestCase):
-	pass
+	def test_create_payment_cycle(self):
+		payroll_cycle = frappe.get_doc(cycle_dict)
+		payroll_cycle.append("payment_period", period)
+
+		payroll_cycle.save()
+
+		self.assertEqual(
+			getdate(payroll_cycle.payment_period[0].start_date),
+			frappe.get_doc('Payroll Cycle', payroll_cycle.name).payment_period[0].start_date
+		)
+
+		self.assertEqual(
+			getdate(payroll_cycle.payment_period[0].end_date),
+			frappe.get_doc('Payroll Cycle', payroll_cycle.name).payment_period[0].end_date
+		)
