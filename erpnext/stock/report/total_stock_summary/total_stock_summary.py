67bbb878476a4c2b3e158a100e0b976342c7ed23
Makarand Bauskar <mbauskar@gmail.com>
2017-07-03 13:31:15 +0530

    indentation fixes and removed the for loop

--- erpnext/stock/report/total_stock_summary/total_stock_summary.py
+++ erpnext/stock/report/total_stock_summary/total_stock_summary.py
@@ -5,64 +5,56 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 
-
 def execute(filters=None):
-    if not filters: filters = {}
-    validate_filters(filters)
-    columns = get_columns()
-    data = []
-    stock = get_total_stock(filters)
+	if not filters: filters = {}
+	validate_filters(filters)
+	columns = get_columns()
+	stock = get_total_stock(filters)
 
-    for row in stock:
-        if filters.get("group_by") == "Warehouse":
-            data.append(["", row.warehouse, row.item_code, row.description, row.actual_qty])
-        else:
-            data.append([row.company, "", row.item_code, row.description, row.actual_qty])
-
-    return columns, data
-
+	return columns, stock
 
 def get_columns():
-    columns = [
-        _("Company") + ":Link/Item:250",
-        _("Warehouse") + ":Link/Item:150",
-        _("Item") + ":Link/Item:150",
-        _("Description") + "::300",
-        _("Current Qty") + ":Float:100",
-    ]
+	columns = [
+		_("Company") + ":Link/Item:250",
+		_("Warehouse") + ":Link/Item:150",
+		_("Item") + ":Link/Item:150",
+		_("Description") + "::300",
+		_("Current Qty") + ":Float:100",
+	]
 
-    return columns
-
+	return columns
 
 def get_total_stock(filters):
-    conditions = ""
-    columns = ""
+	conditions = ""
+	columns = ""
 
-    if filters.get("group_by") == "Warehouse":
-        if filters.get("company"):
-            conditions += " AND warehouse.company = '%s' GROUP BY ledger.warehouse, item.item_code" % frappe.db.escape(
-                filters.get("company"), percent=False)
-        else:
-            conditions += " GROUP BY ledger.warehouse, item.item_code"
-        columns += " ledger.warehouse, item.item_code , item.description , sum(ledger.actual_qty) as actual_qty"
-    else:
-        conditions += " GROUP BY warehouse.company, item.item_code"
-        columns += " warehouse.company, item.item_code , item.description , sum(ledger.actual_qty) as actual_qty"
+	if filters.get("group_by") == "Warehouse":
+		if filters.get("company"):
+			conditions += " AND warehouse.company = '%s'" % frappe.db.escape(filters.get("company"), percent=False)
+		
+		conditions += " GROUP BY ledger.warehouse, item.item_code"
+		columns += "'' as company, ledger.warehouse"
+	else:
+		conditions += " GROUP BY warehouse.company, item.item_code"
+		columns += " warehouse.company, '' as warehouse"
 
-    return frappe.db.sql("""
-    		SELECT
-                %s
-            FROM
-    	        `tabBin` AS ledger
-    	        INNER JOIN `tabItem` AS item
-    		    ON ledger.item_code = item.item_code
-    		    INNER JOIN `tabWarehouse` warehouse
-    		    ON warehouse.name = ledger.warehouse
-    		WHERE
-    			actual_qty != 0
-    	        %s""" % (columns, conditions), as_dict=1)
-
+	return frappe.db.sql("""
+			SELECT
+				%s,
+				item.item_code,
+				item.description,
+				sum(ledger.actual_qty) as actual_qty
+			FROM
+				`tabBin` AS ledger
+			INNER JOIN `tabItem` AS item
+				ON ledger.item_code = item.item_code
+			INNER JOIN `tabWarehouse` warehouse
+				ON warehouse.name = ledger.warehouse
+			WHERE
+				actual_qty != 0 %s""" % (columns, conditions))
 
 def validate_filters(filters):
-    if (filters.get("group_by") == 'Company') and filters.get("company"):
-        frappe.throw(_("Please set Company filter Blank if Group By is 'Company'"))
+	if filters.get("group_by") == 'Company' and \
+		not filters.get("company"):
+
+		frappe.throw(_("Please set Company filter Blank if Group By is 'Company'"))
