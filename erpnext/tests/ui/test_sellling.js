5ae5ac156b8ce2ea8af6a29250c9733d25293c28
Vishal <vishaldhayagude07@gmail.com>
2017-07-05 11:54:19 +0530

    UI test for sales order

--- erpnext/tests/ui/test_sellling.js
+++ erpnext/tests/ui/test_sellling.js
@@ -27,3 +27,33 @@ QUnit.test("test quotation", function(assert) {
 		() => done()
 	]);
 });
+
+
+QUnit.test("test sales order", function(assert) {
+	assert.expect(2);
+	let done = assert.async();
+	frappe.run_serially([
+		() => frappe.tests.setup_doctype('Customer'),
+		() => frappe.tests.setup_doctype('Item'),
+		() => {
+			return frappe.tests.make('Sales Order', [
+				{customer: 'Test Customer 1'},
+				{delivery_date: frappe.datetime.add_days(frappe.defaults.get_default("year_end_date"), 1)},
+				{items: [
+					[
+						{'item_code': 'Test Product 1'},
+						{'qty': 5}
+					]
+				]}
+			]);
+		},
+		() => {
+			// get_item_details
+			assert.ok(cur_frm.doc.items[0].item_name=='Test Product 1');
+
+			// calculate_taxes_and_totals
+			assert.ok(cur_frm.doc.grand_total==500);
+		},
+		() => done()
+	]);
+});
