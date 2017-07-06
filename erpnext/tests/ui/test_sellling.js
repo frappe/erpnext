7ca72d35f63c51bcdacc64e41414a7152e155dee
Shivam Mishra <scm.mymail@gmail.com>
2017-07-06 11:58:53 +0530

    Fixed formatting errors

--- erpnext/tests/ui/test_sellling.js
+++ erpnext/tests/ui/test_sellling.js
@@ -1,14 +1,14 @@
-QUnit.module('sales');
+QUnit.module("sales");
 
 QUnit.test("test: lead", function (assert) {
 	assert.expect(1);
 	let done = assert.async();
 	let random = frappe.utils.get_random(10);
 	frappe.run_serially([
-		() => frappe.tests.setup_doctype('Lead'),
-		() => frappe.set_route('List', 'Lead'),
-		() => frappe.new_doc('Lead'),
-		() => cur_frm.set_value('lead_name', random),
+		() => frappe.tests.setup_doctype("Lead"),
+		() => frappe.set_route("List", "Lead"),
+		() => frappe.new_doc("Lead"),
+		() => cur_frm.set_value("lead_name", random),
 		() => cur_frm.save(),
 		() => {
 			assert.ok(cur_frm.doc.lead_name.includes(random));
@@ -22,56 +22,57 @@ QUnit.test("test: opportunity", function (assert) {
 	let done = assert.async();
 	frappe.run_serially([
 		() => {
-			return frappe.tests.make('Opportunity', [{
-				enquiry_from: 'Lead'
+			return frappe.tests.make("Opportunity", [{
+				enquiry_from: "Lead"
 			},
 			{
-				lead: 'LEAD-00002'
+				lead: "LEAD-00002"
 			}
 			]);
 		},
 		() => {
-			assert.ok(cur_frm.doc.lead == 'LEAD-00002');
+			assert.ok(cur_frm.doc.lead === "LEAD-00002");
 			return done();
 		}
 	]);
 });
 
-QUnit.only("test: quotation", function (assert) {
+QUnit.test("test: quotation", function (assert) {
 	assert.expect(4);
 	let done = assert.async();
 	frappe.run_serially([
-		() => frappe.tests.setup_doctype('Customer'),
-		() => frappe.tests.setup_doctype('Item'),
-		() => frappe.tests.setup_doctype('Address'),
-		() => frappe.tests.setup_doctype('Contact'),
+		() => frappe.tests.setup_doctype("Customer"),
+		() => frappe.tests.setup_doctype("Item"),
+		() => frappe.tests.setup_doctype("Address"),
+		() => frappe.tests.setup_doctype("Contact"),
 		() => {
-			return frappe.tests.make('Quotation', [{
-				customer: 'Test Customer 1'
+			return frappe.tests.make("Quotation", [{
+				customer: "Test Customer 1"
 			},
 			{
 				items: [
 					[{
-						'item_code': 'Test Product 1'
+						"item_code": "Test Product 1"
 					},
 					{
-						'qty': 5
+						"qty": 5
 					}
 					]
 				]
 			}
 			]);
 		},
-		() => cur_frm.set_value('customer_address', 'Test1-Billing'),
-		() => cur_frm.set_value('shipping_address_name', 'Test1-Shipping'),
+		() => cur_frm.set_value("customer_address", "Test1-Billing"),
+		() => cur_frm.set_value("shipping_address_name", "Test1-Warehouse"),
 		() => cur_frm.save(),
+		() => cur_frm.set_value("contact_person", 'Contact 1-Test Customer 1'),
 		() => {
 			// get_item_details
-			assert.ok(cur_frm.doc.items[0].item_name == 'Test Product 1');
+			assert.ok(cur_frm.doc.items[0].item_name == "Test Product 1");
 
-			//Check Address
-			assert.ok(cur_frm.doc.address_display == 'Billing Street 1<br>Billing City 1<br>\nIndia<br>\n')
-			assert.ok(cur_frm.doc.shipping_address == 'Shipping Street 1<br>Shipping City 1<br>\nIndia<br>\n')
+			// Check Address
+			assert.ok(cur_frm.doc.address_display == "Billing Street 1<br>Billing City 1<br>\nIndia<br>\n");
+			assert.ok(cur_frm.doc.shipping_address == "Warehouse Street 1<br>Warehouse City 1<br>\nIndia<br>\n");
 			// calculate_taxes_and_totals
 			assert.ok(cur_frm.doc.grand_total == 500);
 		},
