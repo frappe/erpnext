ba8dec6e9d14efd898cf1467ec1c461931341f4a
Shivam Mishra <scm.mymail@gmail.com>
2017-07-07 13:27:53 +0530

    Added Test for Print Format

--- erpnext/tests/ui/test_sellling.js
+++ erpnext/tests/ui/test_sellling.js
@@ -37,8 +37,8 @@ QUnit.test("test: opportunity", function (assert) {
 	]);
 });
 
-QUnit.only("test: quotation", function (assert) {
-	assert.expect(14);
+QUnit.test("test: quotation", function (assert) {
+	assert.expect(18);
 	let done = assert.async();
 	frappe.run_serially([
 		() => frappe.tests.setup_doctype("Customer"),
@@ -70,7 +70,7 @@ QUnit.only("test: quotation", function (assert) {
 			assert.ok(cur_frm.doc.items[0].item_name == "Test Product 1", "Added Test Product 1");
 
 			// calculate_taxes_and_totals
-			assert.ok(cur_frm.doc.grand_total == 500, "Total Amount is correct");
+			assert.ok(cur_frm.doc.grand_total === 500, "Total Amount is correct");
 		},
 		() => cur_frm.set_value("customer_address", "Test1-Billing"),
 		() => cur_frm.set_value("shipping_address_name", "Test1-Warehouse"),
@@ -78,27 +78,43 @@ QUnit.only("test: quotation", function (assert) {
 		() => cur_frm.set_value("currency", "USD"),
 		() => frappe.timeout(0.3),
 		() => cur_frm.set_value("selling_price_list", "Test-Selling-USD"),
-		() => frappe.timeout(0.3),
+		() => frappe.timeout(0.5),
 		() => cur_frm.doc.items[0].rate = 200,
+		() => frappe.timeout(0.3),
 		() => cur_frm.set_value("tc_name", "Test Term 1"),
 		() => cur_frm.set_value("taxes_and_charges", "TEST In State GST"),
 		() => frappe.timeout(0.3),
 		() => cur_frm.save(),
 		() => {
-			// Check Address
+			// Check Address and Contact Info
 			assert.ok(cur_frm.doc.address_display.includes("Billing Street 1"), "Address Changed");
 			assert.ok(cur_frm.doc.shipping_address.includes("Warehouse Street 1"), "Address Changed");
 			assert.ok(cur_frm.doc.contact_display == "Contact 1", "Contact info changed");
+
+			// Check Currency
 			assert.ok(cur_frm.doc_currency == "USD", "Currency Changed");
 			assert.ok(cur_frm.doc.selling_price_list == "Test-Selling-USD", "Price List Changed");
 			assert.ok(cur_frm.doc.items[0].rate == 200, "Price Changed Manually");
 			assert.ok(cur_frm.doc.total == 1000, "New Total Calculated");
+
+			// Check Terms and Condtions
 			assert.ok(cur_frm.doc.tc_name == "Test Term 1", "Terms and Conditions Checked");
+
+			// Check Taxes
 			assert.ok(cur_frm.doc.taxes[0].account_head.includes("CGST"));
 			assert.ok(cur_frm.doc.taxes[1].account_head.includes("SGST"));
 			assert.ok(cur_frm.doc.grand_total == 1180, "Tax Amount Added to Total");
 			assert.ok(cur_frm.doc.taxes_and_charges == "TEST In State GST", "Tax Template Selected");
 		},
+		() => frappe.timeout(0.3),
+		() => cur_frm.print_doc(),
+		() => frappe.timeout(1),
+		() => assert.ok($('.btn-print-print').is(':visible'), "Print Format Available"),
+		() => assert.ok(RegExp(/QTN-\d\d\d\d\d/g).test($("#header-html small").text())),
+		() => assert.ok($(".important .text-right.value").text().includes("$ 1,180.00")),
+		() => assert.ok($(".section-break+ .section-break .column-break:nth-child(1) .data-field:nth-child(1) .value").text().includes("Billing Street 1"), "Print Preview Works As Expected"),
+		() => frappe.timeout(0.3),
+		() => cur_frm.print_doc(),
 		() => done()
 	]);
 });
