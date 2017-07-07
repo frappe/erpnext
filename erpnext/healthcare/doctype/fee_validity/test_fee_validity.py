e3e817dd028c9aab115bca88a5f20735a10eab35
Jamsheer <jamsheer@earthianslive.com>
2017-07-07 14:45:51 +0530

    Test Fee Validity - Optimised

--- erpnext/healthcare/doctype/fee_validity/test_fee_validity.py
+++ erpnext/healthcare/doctype/fee_validity/test_fee_validity.py
@@ -6,10 +6,8 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 from erpnext.healthcare.doctype.patient_appointment.patient_appointment import create_invoice
-from frappe.utils import getdate
 from frappe.utils.make_random import get_random
-import datetime
-from datetime import timedelta
+from frappe.utils import nowdate, add_days
 # test_records = frappe.get_test_records('Fee Validity')
 
 class TestFeeValidity(unittest.TestCase):
@@ -33,16 +31,17 @@ class TestFeeValidity(unittest.TestCase):
 		frappe.db.set_value("Healthcare Settings", None, "max_visit", 2)
 		frappe.db.set_value("Healthcare Settings", None, "valid_days", 7)
 
-		today = datetime.date.today()
-
-		appointment = create_appointment(patient, physician, getdate("21/01/2017"))
+		appointment = create_appointment(patient, physician, nowdate())
 		invoice = frappe.db.get_value("Patient Appointment", appointment.name, "sales_invoice")
 		self.assertEqual(invoice, None)
 		create_invoice(frappe.defaults.get_global_default("company"), physician, patient, appointment.name, appointment.appointment_date)
-		appointment = create_appointment(patient, physician, getdate("25/01/2017"))
+		appointment = create_appointment(patient, physician, add_days(nowdate(), 4))
 		invoice = frappe.db.get_value("Patient Appointment", appointment.name, "sales_invoice")
 		self.assertTrue(invoice)
-		appointment = create_appointment(patient, physician, getdate("29/01/2017"))
+		appointment = create_appointment(patient, physician, add_days(nowdate(), 5))
+		invoice = frappe.db.get_value("Patient Appointment", appointment.name, "sales_invoice")
+		self.assertEqual(invoice, None)
+		appointment = create_appointment(patient, physician, add_days(nowdate(), 10))
 		invoice = frappe.db.get_value("Patient Appointment", appointment.name, "sales_invoice")
 		self.assertEqual(invoice, None)
 
