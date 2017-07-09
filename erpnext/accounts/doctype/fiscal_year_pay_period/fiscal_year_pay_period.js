0d75d9d63b60ccee63440a0c2f2d2c847a632f78
tunde <tundebabzy@gmail.com>
2017-07-09 16:33:53 +0100

    enhancement to Fiscal Year Pay Period form: - auto populate `dates` table when user enters `Pay Period Start Date`, `Pay Period End Date`, `Is Active` and `Payment Frequency`

--- erpnext/accounts/doctype/fiscal_year_pay_period/fiscal_year_pay_period.js
+++ erpnext/accounts/doctype/fiscal_year_pay_period/fiscal_year_pay_period.js
@@ -3,6 +3,69 @@
 
 frappe.ui.form.on('Fiscal Year Pay Period', {
 	refresh: function(frm) {
+		var pay_period_start_date = frm.doc.pay_period_start_date;
+		var pay_period_end_date = frm.doc.pay_period_end_date;
+		var payment_frequency = frm.doc.payment_frequency;
 
+		if(pay_period_start_date && pay_period_end_date && payment_frequency){
+			get_pay_period_dates(frm);
+		}
+	},
+
+	pay_period_start_date: function(frm) {
+		if (frm.doc.pay_period_start_date){
+			frm.refresh();
+		}
+	},
+
+	pay_period_end_date: function(frm) {
+		if (frm.doc.pay_period_end_date){
+			frm.refresh();
+		}
 	}
 });
+
+get_pay_period_dates = function(frm){
+	return frappe.call({
+				type: "GET",
+				method: "erpnext.accounts.doctype.fiscal_year_pay_period.fiscal_year_pay_period.get_pay_period_dates",
+				args: {
+					"payroll_start": frm.doc.pay_period_start_date,
+					"payroll_end": frm.doc.pay_period_end_date,
+					"payroll_frequency": frm.doc.payment_frequency
+				},
+				callback: function(r) {
+					if(r.message) {
+						auto_populate_date_table(frm, r.message);
+					}
+				}
+			})
+};
+
+auto_populate_date_table = function(frm, dates_array){
+	var proceed = 0;
+	var date_table;
+
+	if(dates_array){
+		// Check if table is default empty table
+		if (!proceed){
+			remove_first_empty_row(frm, 'dates');
+			contnue = 1;
+		}
+
+		for(i=0; i < dates_array.length; i++){
+			var child = frm.add_child("dates");
+			date_obj = dates_array[i];
+			frappe.model.set_value(child.doctype, child.name, "start_date", date_obj.start_date);
+			frappe.model.set_value(child.doctype, child.name, "end_date", date_obj.end_date);
+		}
+		frm.refresh_field("dates");
+	}
+};
+
+remove_first_empty_row = function(frm, child_table_name) {
+	date_table = frm.doc.dates || [];
+	if (date_table.length === 1 && !date_table[0].end_date && !date_table[0].start_date){
+		frm.get_field(child_table_name).grid.grid_rows[0].remove();
+	}
+}
--- erpnext/accounts/doctype/fiscal_year_pay_period/fiscal_year_pay_period.py
+++ erpnext/accounts/doctype/fiscal_year_pay_period/fiscal_year_pay_period.py
@@ -5,6 +5,33 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate, DATE_FORMAT, add_months, add_to_date
+from six.moves import range
+
 
 class FiscalYearPayPeriod(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_pay_period_dates(payroll_start, payroll_end, payroll_frequency):
+	dates = []
+	payroll_frequency = payroll_frequency.lower()
+	start = getdate(payroll_start)
+
+	key = {'monthly': 12, 'bimonthly': 6, 'fortnightly': 27, 'weekly': 52, 'daily': 366}
+	for _ in range(key.get(payroll_frequency)):
+		end = getdate(add_months(start, 1))
+		dates.append(
+			{
+				'start_date': start.strftime(DATE_FORMAT),
+				'end_date': add_to_date(end, days=-1).strftime(DATE_FORMAT),
+			}
+		)
+
+		if end >= getdate(payroll_end):
+			break
+		else:
+			start = end
+
+	return dates
