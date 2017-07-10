7ad604f1df45ee659ce7f3b557c0e8388936705c
Rushabh Mehta <rmehta@gmail.com>
2017-07-10 17:12:53 +0530

    [check] daily work summary fails?

--- erpnext/hr/doctype/daily_work_summary/test_daily_work_summary.py
+++ erpnext/hr/doctype/daily_work_summary/test_daily_work_summary.py
@@ -44,12 +44,14 @@ class TestDailyWorkSummary(unittest.TestCase):
 		email_account.db_set('enable_incoming', 1)
 		email_account.receive(test_mails=test_mails)
 
-		print frappe.get_all('Daily Work Summary')
+		frappe.db.commit()
 
 		daily_work_summary = frappe.get_doc('Daily Work Summary',
 			frappe.get_all('Daily Work Summary')[0].name)
 
 		summary = daily_work_summary.get_summary_message()
+
+		print summary
 
 		self.assertTrue('I built Daily Work Summary!' in summary)
 
