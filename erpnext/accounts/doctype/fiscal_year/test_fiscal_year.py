c8b4c2ff05cf948129f391d118edd1ca14b45ca0
tunde <tundebabzy@gmail.com>
2017-07-12 13:24:25 +0100

    adds test case to verify that pay period is not mandatory in fiscal year

--- erpnext/accounts/doctype/fiscal_year/test_fiscal_year.py
+++ erpnext/accounts/doctype/fiscal_year/test_fiscal_year.py
@@ -67,6 +67,43 @@ class TestFiscalYear(unittest.TestCase):
 		if overlapping_fy_coy:
 			reset_fiscal_year_company(overlapping_fy_coy)
 
+	def test_create_fiscal_year_no_pay_period(self):
+		if frappe.db.exists("Fiscal Year", "_Test Fiscal Year 2017"):
+			frappe.delete_doc("Fiscal Year", "_Test Fiscal Year 2017")
+
+		year_start = "2017-01-07"
+		year_end = "2018-01-06"
+		year = "_Test Fiscal Year 2017"
+		companies = [{
+					"doctype": 'Fiscal Year Company',
+					"company": '_Test Company'}]
+
+
+		fy = frappe.get_doc({
+			"doctype": "Fiscal Year",
+			"year_start_date": year_start,
+			"year": year,
+			"year_end_date": year_end,
+			"companies": companies,
+		})
+
+		overlapping_fy_coy = fix_overlaps_get_initial_coy(fy)
+
+		fy.insert()
+		self.assertEquals(fy.year_end_date, year_end)
+		self.assertEquals(fy.year_start_date, year_start)
+		self.assertEquals(fy.companies[0].company, companies[0]['company'])
+
+		saved_fy = frappe.get_doc('Fiscal Year', fy.name)
+		self.assertEquals(saved_fy.year_end_date, getdate(year_end))
+		self.assertEquals(saved_fy.year_start_date, getdate(year_start))
+		self.assertEquals(saved_fy.companies[0].company, companies[0]['company'])
+
+		# reset things
+		saved_fy.delete()
+		if overlapping_fy_coy:
+			reset_fiscal_year_company(overlapping_fy_coy)
+
 
 def fix_overlaps_get_initial_coy(fiscal_year):
 	"""
