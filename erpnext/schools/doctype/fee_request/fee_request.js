f073814635dde0b2a25db489efe70face29e80f3
Manas Solanki <manassolanki@gmail.com>
2017-07-13 13:39:09 +0530

    added fee request doctype

---
+++ erpnext/schools/doctype/fee_request/fee_request.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fee Request', {
+	refresh: function(frm) {
+		
+	},
+	fee_request_against: function(frm) {
+		if (frm.doc.fee_request_against == "Program") {
+			frm.set_value('student_groups', null)	
+		} else if (frm.doc.fee_request_against == "Student Group") {
+			frm.set_value('programs', null)				
+		}
+	},
+	fee_structure: function(frm) {
+		if (frm.doc.fee_structure) {
+			frappe.call({
+				method: "erpnext.schools.doctype.fee_request.fee_request.get_fee_structure",
+				args: {
+					"target_doc": frm.doc.name,
+					"source_name": frm.doc.fee_structure
+				},
+				callback: function(r) {
+					var doc = frappe.model.sync(r.message);
+					frappe.set_route("Form", doc[0].doctype, doc[0].name);
+				}
+			});
+		}
+	}
+});
+
+frappe.ui.form.on("Fee Component", {
+	refresh: function(frm) {
+		frm.set_read_only();
+	}
+});
