2c5b3e83f571cd3f886f093520d55db4356e85d1
Faris Ansari <netchampfaris@users.noreply.github.com>
2017-07-13 18:37:18 +0530

    New design for daily work summary (#9844)
    * New design for daily work summary
    
    * Update tests

--- erpnext/hr/doctype/daily_work_summary/daily_work_summary.py
+++ erpnext/hr/doctype/daily_work_summary/daily_work_summary.py
@@ -8,7 +8,7 @@ from frappe.model.document import Document
 from frappe import _
 from email_reply_parser import EmailReplyParser
 from erpnext.hr.doctype.employee.employee import is_holiday
-from frappe.utils import formatdate
+from frappe.utils import global_date_format
 from markdown2 import markdown
 
 class DailyWorkSummary(Document):
@@ -24,17 +24,18 @@ class DailyWorkSummary(Document):
 
 	def send_summary(self):
 		'''Send summary of all replies. Called at midnight'''
-		message = self.get_summary_message()
+		args = self.get_message_details()
 
 		frappe.sendmail(recipients = get_employee_emails(self.company, False),
-			message = message,
+			template='daily_work_summary',
+			args=args,
 			subject = _('Daily Work Summary for {0}').format(self.company),
 			reference_doctype=self.doctype, reference_name=self.name)
 
 		self.db_set('status', 'Sent')
 
-	def get_summary_message(self):
-		'''Return summary of replies as HTML'''
+	def get_message_details(self):
+		'''Return args for template'''
 		settings = frappe.get_doc('Daily Work Summary Settings')
 
 		replies = frappe.get_all('Communication', fields=['content', 'text_content', 'sender'],
@@ -45,8 +46,12 @@ class DailyWorkSummary(Document):
 		did_not_reply = self.email_sent_to.split()
 
 		for d in replies:
-			d.sender_name = frappe.db.get_value("Employee", {"user_id": d.sender},
-				"employee_name") or d.sender
+			emp = frappe.db.get_values("Employee", {"user_id": d.sender},
+				["employee_name", "image"], as_dict=True)
+
+			d.sender_name = emp[0].employee_name if emp else d.sender
+			d.image = emp[0].image if emp and emp[0].image else None
+
 			if d.sender in did_not_reply:
 				did_not_reply.remove(d.sender)
 			if d.text_content:
@@ -56,30 +61,12 @@ class DailyWorkSummary(Document):
 		did_not_reply = [(frappe.db.get_value("Employee", {"user_id": email}, "employee_name") or email)
 			for email in did_not_reply]
 
-		return frappe.render_template(self.get_summary_template(),
-			dict(replies=replies,
-				original_message=settings.message,
-				title=_('Daily Work Summary for {0}'.format(formatdate(self.creation))),
-				did_not_reply= ', '.join(did_not_reply) or '',
-				did_not_reply_title = _('No replies from')))
+		return dict(replies=replies,
+			original_message=settings.message,
+			title=_('Daily Work Summary for {0}'.format(global_date_format(self.creation))),
+			did_not_reply= ', '.join(did_not_reply) or '',
+			did_not_reply_title = _('No replies from'))
 
-	def get_summary_template(self):
-		return '''
-<h3>{{ title }}</h3>
-
-{% for reply in replies %}
-<h4>{{ reply.sender_name }}</h4>
-<p style="padding-bottom: 20px">
-	{{ reply.content }}
-</p>
-<hr>
-{% endfor %}
-
-{% if did_not_reply %}
-<p>{{ did_not_reply_title }}: {{ did_not_reply }}</p>
-{% endif %}
-
-'''
 
 def get_employee_emails(company, only_working=True):
 	'''Returns list of Employee user ids for the given company who are working today
