d66bef0d92fce276e73932d56a3de6192efb4a45
Vishal <vishaldhayagude07@gmail.com>
2017-07-13 23:18:33 +0530

    [new]Timesheet with Sales Invoice on Web Portal

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -398,16 +398,7 @@ def get_conditions(filters):
 
 
 def get_t_list(doctype, txt, filters, limit_start, limit_page_length=20, order_by="modified"):
-	# from frappe.www.list import get_list
 	user = frappe.session.user
-	# ignore_permissions = False
-	# if is_website_user():
-	# 	if not filters: filters = []
-	# 	filters.append(("Timesheet", "owner", "=", user))
-	# 	ignore_permissions = True
-
-	# return get_list(doctype, txt, filters, limit_start, limit_page_length, ignore_permissions=ignore_permissions)
-
 	return frappe. db.sql('''SELECT A.name, B.activity_type, A.status, A.total_billable_hours, B.sales_invoice, B.project  FROM \
 		`tabTimesheet` AS A inner join `tabTimesheet Detail` AS B ON B.parent = A.name\
 		order by end_date asc limit {0} , {1}'''
