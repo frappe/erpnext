1983523453a38c761448a9b90299f87e008c816d
tunde <tundebabzy@gmail.com>
2017-07-18 11:08:52 +0100

    adds new function `get_end_date`: - it tries to calculate the appropriate end date for a given frequency - returns an empty string if frequency is 'biweekly' - adds test cases

--- erpnext/hr/doctype/process_payroll/process_payroll.py
+++ erpnext/hr/doctype/process_payroll/process_payroll.py
@@ -3,7 +3,8 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import cint, flt, nowdate, add_days, getdate, fmt_money
+from dateutil.relativedelta import relativedelta
+from frappe.utils import cint, nowdate, add_days, getdate, fmt_money, add_to_date, DATE_FORMAT
 from frappe import _
 from erpnext.accounts.utils import get_fiscal_year
 
@@ -390,6 +391,32 @@ def get_start_end_dates(payroll_frequency, start_date=None, company=None):
 	return frappe._dict({
 		'start_date': start_date, 'end_date': end_date
 	})
+
+
+def get_frequency_kwargs(frequency_name):
+	frequency_dict = {
+		'monthly': {'months': 1},
+		'fortnightly': {'days': 14},
+		'weekly': {'days': 7},
+		'daily': {'days': 1}
+	}
+	return frequency_dict.get(frequency_name)
+
+
+@frappe.whitelist()
+def get_end_date(start_date, frequency):
+	start_date = getdate(start_date)
+	frequency = frequency.lower()
+	kwargs = get_frequency_kwargs(frequency) if frequency != 'biweekly' else get_frequency_kwargs('monthly')
+
+	# weekly, fortnightly and daily intervals have fixed days so no problems
+	end_date = add_to_date(start_date, **kwargs) - relativedelta(days=1)
+	if frequency != 'biweekly':
+		return end_date.strftime(DATE_FORMAT)
+
+	else:
+		return ''
+
 
 def get_month_details(year, month):
 	ysd = frappe.db.get_value("Fiscal Year", year, "year_start_date")
