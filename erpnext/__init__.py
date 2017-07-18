3e4b2743c6d0dc6fcfeee0f58ec3f490ca617a1f
mbauskar <mbauskar@gmail.com>
2017-07-18 13:37:00 +0600

    bumped to version 8.5.0

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -4,7 +4,7 @@ import inspect
 import frappe
 from erpnext.hooks import regional_overrides
 
-__version__ = '8.4.3'
+__version__ = '8.5.0'
 
 def get_default_company(user=None):
 	'''Get default company for user'''
