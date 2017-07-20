9d27cf3c624a2c069ab3817b9db5c78dded95ca3
Rushabh Mehta <rmehta@gmail.com>
2017-07-20 15:35:03 +0530

    [tests] refactored (#9984)
    * [tests] refactored
    
    * [fix] test_quotation.js
    
    * [fix] tests.text
    
    * [fix] fiscal year not needed
    
    * [test] add long test
    
    * [fix] add timeout in lead

--- erpnext/accounts/doctype/account/test_account.js
+++ erpnext/accounts/doctype/account/test_account.js
@@ -5,16 +5,16 @@ QUnit.test("test account", function(assert) {
 	let done = assert.async();
 	frappe.run_serially([
 		() => frappe.set_route('Tree', 'Account'),
-		() => frappe.tests.click_button('Expand All'),
-		() => frappe.tests.click_link('Debtors'),
-		() => frappe.tests.click_button('Edit'),
+		() => frappe.click_button('Expand All'),
+		() => frappe.click_link('Debtors'),
+		() => frappe.click_button('Edit'),
 		() => frappe.timeout(1),
 		() => {
 			assert.ok(cur_frm.doc.root_type=='Asset');
 			assert.ok(cur_frm.doc.report_type=='Balance Sheet');
 			assert.ok(cur_frm.doc.account_type=='Receivable');
 		},
-		() => frappe.tests.click_button('Ledger'),
+		() => frappe.click_button('Ledger'),
 		() => frappe.timeout(1),
 		() => {
 			// check if general ledger report shown
--- erpnext/selling/doctype/quotation/test_quotation.js
+++ erpnext/selling/doctype/quotation/test_quotation.js
@@ -1,29 +1,16 @@
 QUnit.test("test: quotation", function (assert) {
-	assert.expect(18);
+	assert.expect(10);
 	let done = assert.async();
 	frappe.run_serially([
-		() => frappe.tests.setup_doctype("Customer"),
-		() => frappe.tests.setup_doctype("Item"),
-		() => frappe.tests.setup_doctype("Address"),
-		() => frappe.tests.setup_doctype("Contact"),
-		() => frappe.tests.setup_doctype("Price List"),
-		() => frappe.tests.setup_doctype("Terms and Conditions"),
-		() => frappe.tests.setup_doctype("Sales Taxes and Charges Template"),
 		() => {
-			return frappe.tests.make("Quotation", [{
-				customer: "Test Customer 1"
-			},
-			{
-				items: [
-					[{
-						"item_code": "Test Product 1"
-					},
-					{
-						"qty": 5
-					}
-					]
-				]
-			}
+			return frappe.tests.make("Quotation", [
+				{customer: "Test Customer 1"},
+				{items: [
+					[
+						{"item_code": "Test Product 1"},
+						{"qty": 5}
+					]]
+				}
 			]);
 		},
 		() => {
@@ -43,7 +30,6 @@ QUnit.test("test: quotation", function (assert) {
 		() => cur_frm.doc.items[0].rate = 200,
 		() => frappe.timeout(0.3),
 		() => cur_frm.set_value("tc_name", "Test Term 1"),
-		() => cur_frm.set_value("taxes_and_charges", "TEST In State GST"),
 		() => frappe.timeout(0.3),
 		() => cur_frm.save(),
 		() => {
@@ -56,26 +42,12 @@ QUnit.test("test: quotation", function (assert) {
 			assert.ok(cur_frm.doc_currency == "USD", "Currency Changed");
 			assert.ok(cur_frm.doc.selling_price_list == "Test-Selling-USD", "Price List Changed");
 			assert.ok(cur_frm.doc.items[0].rate == 200, "Price Changed Manually");
-			assert.ok(cur_frm.doc.total == 1000, "New Total Calculated");
+			assert.equal(cur_frm.doc.total, 1000, "New Total Calculated");
 
 			// Check Terms and Condtions
 			assert.ok(cur_frm.doc.tc_name == "Test Term 1", "Terms and Conditions Checked");
 
-			// Check Taxes
-			assert.ok(cur_frm.doc.taxes[0].account_head.includes("CGST"));
-			assert.ok(cur_frm.doc.taxes[1].account_head.includes("SGST"));
-			assert.ok(cur_frm.doc.grand_total == 1180, "Tax Amount Added to Total");
-			assert.ok(cur_frm.doc.taxes_and_charges == "TEST In State GST", "Tax Template Selected");
 		},
-		() => frappe.timeout(0.3),
-		() => cur_frm.print_doc(),
-		() => frappe.timeout(1),
-		() => assert.ok($('.btn-print-print').is(':visible'), "Print Format Available"),
-		() => assert.ok(RegExp(/QTN-\d\d\d\d\d/g).test($("#header-html small").text())),
-		() => assert.ok($(".important .text-right.value").text().includes("$ 1,180.00")),
-		() => assert.ok($(".section-break+ .section-break .column-break:nth-child(1) .data-field:nth-child(1) .value").text().includes("Billing Street 1"), "Print Preview Works As Expected"),
-		() => frappe.timeout(0.3),
-		() => cur_frm.print_doc(),
 		() => done()
 	]);
 });
