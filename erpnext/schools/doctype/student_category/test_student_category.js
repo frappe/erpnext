a8cb6cd5b9e12ad31b2f8265619af17d0f707854
Zlash65 <zarrar65@gmail.com>
2017-07-24 16:28:11 +0530

    [UI Tests] School Student Category test

---
+++ erpnext/schools/doctype/student_category/test_student_category.js
@@ -0,0 +1,20 @@
+// Testing Setup Module in Schools
+QUnit.module('setup');
+
+// Testing setting Student Category
+QUnit.test('test student category', function(assert){
+	assert.expect(1);
+	let done = assert.async();
+	frappe.run_serially([
+		() => {
+			return frappe.tests.make('Student Category', [
+				{category: 'Reservation'}
+			]);
+		},
+		() => cur_frm.save(),
+		() => {
+			assert.ok(cur_frm.doc.name=='Reservation');
+		},
+		() => done()
+	]);
+});
