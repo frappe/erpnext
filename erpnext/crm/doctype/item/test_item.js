91b28337086026c7348b3660f9cfadd7548541d8
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-07-24 14:34:30 +0530

    [ui-tests] added workstation and operation  (#10044)
    * [ui-test] workstation and operation testing added
    
    * [ui-tests] removed unnecessary assertions and used logical names for operations and workstations

--- erpnext/crm/doctype/item/test_item.js
+++ erpnext/crm/doctype/item/test_item.js
@@ -1,6 +1,12 @@
 QUnit.test("test: item", function (assert) {
-	assert.expect(18);
+	assert.expect(6);
 	let done = assert.async();
+	let keyboard_cost  = 800;
+	let screen_cost  = 4000;
+	let CPU_cost  = 15000;
+	let scrap_cost = 100;
+	let no_of_items_to_stock = 100;
+	let is_stock_item = 1;
 	frappe.run_serially([
 		// test item creation
 		() => frappe.set_route("List", "Item"),
@@ -10,9 +16,9 @@ QUnit.test("test: item", function (assert) {
 			"Item", [
 				{item_code: "Keyboard"},
 				{item_group: "Products"},
-				{is_stock_item: 1},
-				{standard_rate: 1000},
-				{opening_stock: 100}
+				{is_stock_item: is_stock_item},
+				{standard_rate: keyboard_cost},
+				{opening_stock: no_of_items_to_stock}
 			]
 		),
 		() => {
@@ -22,11 +28,11 @@ QUnit.test("test: item", function (assert) {
 				'item_code for Keyboard set correctly');
 			assert.ok(cur_frm.doc.item_group.includes('Products'),
 				'item_group for Keyboard set correctly');
-			assert.equal(cur_frm.doc.is_stock_item, 1,
+			assert.equal(cur_frm.doc.is_stock_item, is_stock_item,
 				'is_stock_item for Keyboard set correctly');
-			assert.equal(cur_frm.doc.standard_rate, 1000,
+			assert.equal(cur_frm.doc.standard_rate, keyboard_cost,
 				'standard_rate for Keyboard set correctly');
-			assert.equal(cur_frm.doc.opening_stock, 100,
+			assert.equal(cur_frm.doc.opening_stock, no_of_items_to_stock,
 				'opening_stock for Keyboard set correctly');
 		},
 
@@ -35,50 +41,41 @@ QUnit.test("test: item", function (assert) {
 			"Item", [
 				{item_code: "Screen"},
 				{item_group: "Products"},
-				{is_stock_item: 1},
-				{standard_rate: 1000},
-				{opening_stock: 100}
+				{is_stock_item: is_stock_item},
+				{standard_rate: screen_cost},
+				{opening_stock: no_of_items_to_stock}
 			]
 		),
-		() => {
-			assert.ok(cur_frm.doc.item_name.includes('Screen'),
-				'Item Screen created correctly');
-			assert.ok(cur_frm.doc.item_code.includes('Screen'),
-				'item_code for Screen set correctly');
-			assert.ok(cur_frm.doc.item_group.includes('Products'),
-				'item_group for Screen set correctly');
-			assert.equal(cur_frm.doc.is_stock_item, 1,
-				'is_stock_item for Screen set correctly');
-			assert.equal(cur_frm.doc.standard_rate, 1000,
-				'standard_rate for Screen set correctly');
-			assert.equal(cur_frm.doc.opening_stock, 100,
-				'opening_stock for Screen set correctly');
-		},
 
 		// Create a CPU item
 		() => frappe.tests.make(
 			"Item", [
 				{item_code: "CPU"},
 				{item_group: "Products"},
-				{is_stock_item: 1},
-				{standard_rate: 1000},
-				{opening_stock: 100}
+				{is_stock_item: is_stock_item},
+				{standard_rate: CPU_cost},
+				{opening_stock: no_of_items_to_stock}
 			]
 		),
-		() => {
-			assert.ok(cur_frm.doc.item_name.includes('CPU'),
-				'Item CPU created correctly');
-			assert.ok(cur_frm.doc.item_code.includes('CPU'),
-				'item_code for CPU set correctly');
-			assert.ok(cur_frm.doc.item_group.includes('Products'),
-				'item_group for CPU set correctly');
-			assert.equal(cur_frm.doc.is_stock_item, 1,
-				'is_stock_item for CPU set correctly');
-			assert.equal(cur_frm.doc.standard_rate, 1000,
-				'standard_rate for CPU set correctly');
-			assert.equal(cur_frm.doc.opening_stock, 100,
-				'opening_stock for CPU set correctly');
-		},
+
+		// Create a laptop item
+		() => frappe.tests.make(
+			"Item", [
+				{item_code: "Laptop"},
+				{item_group: "Products"}
+			]
+		),
+
+		// Create a scrap item
+		() => frappe.tests.make(
+			"Item", [
+				{item_code: "Scrap item"},
+				{item_group: "Products"},
+				{is_stock_item: is_stock_item},
+				{standard_rate: scrap_cost},
+				{opening_stock: no_of_items_to_stock}
+			]
+		),
 
 		() => done()
 	]);
