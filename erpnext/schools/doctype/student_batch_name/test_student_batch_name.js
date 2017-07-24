03d20ca1e8f49d7a5868bebaea255d1a330d4fc5
Zlash65 <zarrar65@gmail.com>
2017-07-24 16:21:54 +0530

    [UI Tests] School Student Batch Name test

---
+++ erpnext/schools/doctype/student_batch_name/test_student_batch_name.js
@@ -0,0 +1,20 @@
+// Testing Setup Module in Schools
+QUnit.module('setup');
+
+// Testing setting Batch Name
+QUnit.test('test student batch name', function(assert){
+	assert.expect(1);
+	let done = assert.async();
+	frappe.run_serially([
+		() => {
+			return frappe.tests.make('Student Batch Name', [
+				{batch_name: 'A'}
+			]);
+		},
+		() => cur_frm.save(),
+		() => {
+			assert.ok(cur_frm.doc.batch_name=='A');
+		},
+		() => done()
+	]);
+});
