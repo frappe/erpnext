1906309ad7865a353031e3da8817629b90e40b02
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-07-25 04:49:14 -0700

    Improve email format for training event, add ui test

--- erpnext/hr/doctype/training_event/training_event.js
+++ erpnext/hr/doctype/training_event/training_event.js
@@ -17,5 +17,37 @@ frappe.ui.form.on('Training Event', {
 				frappe.set_route("List", "Training Feedback");
 			});
 		}
+	},
+	onload: function(frm) {
+		var params = getSearchParameters();
+		if (params.hasOwnProperty('employee') && params.hasOwnProperty('status')) {
+			var newTemp = frm.doc.employees.filter(function(obj) {
+				return obj.name == params.employee
+			});
+			if (newTemp) {
+				newTemp[0].status = params.status;
+				frm.refresh_field("employees");
+				frappe.msgprint(__(frm.doc.name + ": Status for " + newTemp[0].employee_name +
+					" is updated to " + newTemp[0].status));
+				frappe.route_options = {};
+				frappe.set_route("List", "Training Event");
+			}
+		}
 	}
 });
+
+function getSearchParameters() {
+	var prmstr = window.location.href.split('?')[2];
+	return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
