6d918706f38b069850c0c6574e0ec54efdf8b3ae
Utkarsh <utkyadav800@gmail.com>
2017-07-25 17:33:26 +0530

    added test for employee

---
+++ erpnext/hr/doctype/employee/test_employee.js
@@ -0,0 +1,40 @@
+QUnit.module('hr');
+
+//not added path in tests.txt yet
+
+QUnit.test("Test: Employee [HR]", function (assert) {
+	assert.expect(3);
+	let done = assert.async();
+	let today_date = frappe.datetime.nowdate();
+
+	frappe.run_serially([
+		// test employee creation
+		() => frappe.set_route("List", "Employee", "List"),
+		() => frappe.new_doc("Employee"),
+		() => frappe.timeout(1),
+		() => cur_frm.set_value("employee_name", "Employee test"),
+		() => cur_frm.set_value("salutation", "Ms"),
+		() => cur_frm.set_value("date_of_joining", frappe.datetime.add_months(today_date, -2)),	// joined 2 month from now
+		() => cur_frm.set_value("date_of_birth", frappe.datetime.add_months(today_date, -240)),	// age is 20 years
+		() => cur_frm.set_value("employment_type", "Full-time"),
+		() => cur_frm.set_value("holiday_list", "Holiday list test"),
+		() => cur_frm.set_value("branch", "Branch test"),
+		() => cur_frm.set_value("department", "Department test"),
+		() => cur_frm.set_value("designation", "Designation test"),
+		() => frappe.click_button('Add Row'),
+		() => cur_frm.fields_dict.leave_approvers.grid.grid_rows[0].doc.leave_approver="Administrator",
+		// save data
+		() => cur_frm.save(),
+		() => frappe.timeout(1),
+		// check name of employee
+		() => assert.equal("Employee test", cur_frm.doc.employee_name,
+			'name of employee correctly saved'),
+		// check auto filled gender according to salutation
+		() => assert.equal("Female", cur_frm.doc.gender,
+			'gender correctly saved as per salutation'),
+		// check auto filled retirement date [60 years from DOB]
+		() => assert.equal(frappe.datetime.add_months(today_date, 480), cur_frm.doc.date_of_retirement,	// 40 years from now
+			'retirement date correctly saved as per date of birth'),
+		() => done()
+	]);
+});
