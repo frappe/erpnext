d65b4b4238de3beb63589fd5517ff97e20bbd4ad
KanchanChauhan <angel143.dr@gmail.com>
2017-07-25 11:39:46 +0530

    Patch was faling, reload doc was missing (#10072)

--- erpnext/patches/v8_4/make_scorecard_records.py
+++ erpnext/patches/v8_4/make_scorecard_records.py
@@ -2,8 +2,10 @@
 # License: GNU General Public License v3. See license.txt
 
 from __future__ import unicode_literals
+import frappe
 
 from erpnext.buying.doctype.supplier_scorecard.supplier_scorecard import make_default_records
 def execute():
-
+	frappe.reload_doc('buying', 'doctype', 'supplier_scorecard_variable')
+	frappe.reload_doc('buying', 'doctype', 'supplier_scorecard_standing')
 	make_default_records()
