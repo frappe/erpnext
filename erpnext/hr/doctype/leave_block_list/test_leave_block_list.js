7dfb6157655facccf58fd2c3a2e5c0b10abbd2af
Utkarsh <utkyadav800@gmail.com>
2017-07-25 11:55:37 +0530

    minor change in leave block

--- erpnext/hr/doctype/leave_block_list/test_leave_block_list.js
+++ erpnext/hr/doctype/leave_block_list/test_leave_block_list.js
@@ -3,6 +3,7 @@ QUnit.module('hr');
 QUnit.test("Test: Leave block list [HR]", function (assert) {
 	assert.expect(1);
 	let done = assert.async();
+	let today_date = frappe.datetime.nowdate();
 
 	frappe.run_serially([
 		// test leave block list creation
@@ -10,7 +11,11 @@ QUnit.test("Test: Leave block list [HR]", function (assert) {
 		() => frappe.new_doc("Leave Block List"),
 		() => frappe.timeout(1),
 		() => cur_frm.set_value("leave_block_list_name", "Leave block list test"),
-		
+		() => frappe.click_button('Add Row'),
+		() => {
+			cur_frm.fields_dict.leave_block_list_dates.grid.grid_rows[0].doc.block_date = today_date;
+			cur_frm.fields_dict.leave_block_list_dates.grid.grid_rows[0].doc.reason = "Blocked leave test";
+		},
 		// save form
 		() => cur_frm.save(),
 		() => frappe.timeout(1),
