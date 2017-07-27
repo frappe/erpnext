6825f6eb3e5a110205dc43b865161d792bd26baf
Rushabh Mehta <rmehta@gmail.com>
2017-07-27 16:32:07 +0530

    [docs]

--- erpnext/config/docs.py
+++ erpnext/config/docs.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 
 source_link = "https://github.com/frappe/erpnext"
-headline = "ERPNext Documentation"
-sub_heading = "Detailed explanation for all ERPNext features and developer API"
-long_description = """ERPNext is a fully featured ERP system designed for Small and Medium Sized
+docs_base_url = "https://frappe.github.io/erpnext"
+headline = "ERP Made Simple"
+sub_heading = "ERPNext User Guides and API References"
+long_description = """
+ERPNext is a fully featured ERP system designed for Small and Medium Sized
 business. ERPNext covers a wide range of features including Accounting, CRM,
 Inventory management, Selling, Purchasing, Manufacturing, Projects, HR &
 Payroll, Website, E-Commerce and much more.
@@ -14,7 +16,15 @@ to extend ERPNext functionality.
 
 ERPNext is Open Source under the GNU General Public Licence v3 and has been
 listed as one of the Best Open Source Softwares in the world by many online
-blogs."""
+blogs.
+
+### Getting Started
+
+To install ERPNext on a server, you will need to install [Frappe Bench](https://github.com/frappe/bench).
+
+You can also start a free 30 day trial at [https://erpnext.com](https://erpnext.com)
+
+"""
 
 splash_light_background = True
 google_analytics_id = 'UA-8911157-22'
