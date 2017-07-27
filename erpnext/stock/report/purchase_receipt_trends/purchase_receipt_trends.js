cb48d7c51c7ab3e3ee28709b602587258dd821b2
mbauskar <mbauskar@gmail.com>
2017-07-27 13:27:37 +0530

    [hotfix] included the get_purchase_trends_filters instead of get_sales_trends_filters in Purchase Receipts trends

--- erpnext/stock/report/purchase_receipt_trends/purchase_receipt_trends.js
+++ erpnext/stock/report/purchase_receipt_trends/purchase_receipt_trends.js
@@ -3,7 +3,7 @@
 
 frappe.require("assets/erpnext/js/purchase_trends_filters.js", function() {
 	frappe.query_reports["Purchase Receipt Trends"] = {
-		filters: erpnext.get_sales_trends_filters()
+		filters: erpnext.get_purchase_trends_filters()
 	}
 });
 
