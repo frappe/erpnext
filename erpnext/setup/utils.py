fcd977b8f8879257522ea39bfcba92f2ea604247
Zlash65 <zarrar65@gmail.com>
2017-07-27 13:09:10 +0530

    updating utils

--- erpnext/setup/utils.py
+++ erpnext/setup/utils.py
@@ -111,7 +111,7 @@ def enable_all_roles_and_domains():
 		print _role
 
 	# add all roles to users
-	user = frappe.get_doc("User", "test@erpnext.com")
+	user = frappe.get_doc("User", "test@example.com")
 	print user
 	user.add_roles(*[role.get("name") for role in roles])
 	print user
