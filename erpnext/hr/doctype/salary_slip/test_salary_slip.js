c083d3013b4432b83e5119751c56445954d73348
Utkarsh Goswami <goswamiutkarsh0@gmail.com>
2017-07-31 15:39:42 +0530

    Updated

--- erpnext/hr/doctype/salary_slip/test_salary_slip.js
+++ erpnext/hr/doctype/salary_slip/test_salary_slip.js
@@ -13,11 +13,11 @@ QUnit.test("test salary slip", function(assert) {
 				{ employee: employee_name}
 			]);
 		},
-		() => frappe.timeout(5),
+		() => frappe.timeout(1),
 		() => {
 			// To check if all the calculations are correctly done
 			assert.ok(cur_frm.doc.gross_pay==42500,
-				'Gross amount is correctly calculated'+cur_frm.doc.gross_pay);
+				'Gross amount is correctly calculated');
 			assert.ok(cur_frm.doc.total_deduction==7000,
 				'Deduction amount is correctly calculated');
 			assert.ok(cur_frm.doc.net_pay==35500,
