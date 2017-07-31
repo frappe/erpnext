4e91f28ce529a6ba8384ec9daff96c7b117dcc2c
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-07-31 11:02:28 +0530

    [ui-testing] added test for production order (#10138)
    * temporary commit for switching branches
    
    * [ui-tests] added bill_of_materials test
    
    * added warehouses required for production order
    
    * [ui-test] added production order test
    
    * debugging travis failure

---
+++ erpnext/stock/doctype/warehouse/test_warehouse.js
@@ -0,0 +1,19 @@
+QUnit.test("test: warehouse", function (assert) {
+	assert.expect(0);
+	let done = assert.async();
+
+	frappe.run_serially([
+		// test warehouse creation
+		() => frappe.set_route("List", "Warehouse"),
+
+		// Create a Laptop Scrap Warehouse
+		() => frappe.tests.make(
+			"Warehouse", [
+				{warehouse_name: "Laptop Scrap Warehouse"},
+				{company: "Razer Blade"}
+			]
+		),
+
+		() => done()
+	]);
+});
