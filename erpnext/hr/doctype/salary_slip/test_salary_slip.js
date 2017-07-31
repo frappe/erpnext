07bbed9bd41e0bb95324d55759f65a51073960b6
Utkarsh Goswami <goswamiutkarsh0@gmail.com>
2017-07-31 14:52:58 +0530

    Test for Salary Structure and Salary Slip

---
+++ erpnext/hr/doctype/salary_slip/test_salary_slip.js
@@ -0,0 +1,27 @@
+QUnit.test("test salary slip", function(assert) {
+	assert.expect(3);
+	let done = assert.async();
+	let employee_name;
+	frappe.run_serially([
+		() => frappe.db.get_value('Employee', {'employee_name': 'Test Employee 1'}, 'name'),
+		(r) => {
+			employee_name = r.message.name;
+		},
+		() => {
+			frappe.tests.make('Salary Slip', [
+				{ employee: employee_name}
+			]);
+		},
+		() => frappe.timeout(5),
+		() => {
+			// To check if all the fields are correctly set
+			assert.ok(cur_frm.doc.gross_pay==42500,
+				'Gross amount is correctly calculated'+cur_frm.doc.gross_pay);
+			assert.ok(cur_frm.doc.total_deduction==7000,
+				'Deduction amount is correctly calculated');
+			assert.ok(cur_frm.doc.net_pay==35500,
+				'Net amount is correctly calculated');
+		},
+		() => done()
+	]);
+});
