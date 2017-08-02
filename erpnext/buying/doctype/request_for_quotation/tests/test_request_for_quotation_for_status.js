75ffe9fbaa12c088403c16e60d3b56af4cec15e5
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-08-02 05:25:45 -0700

    Fixed codacy issues

--- erpnext/buying/doctype/request_for_quotation/tests/test_request_for_quotation_for_status.js
+++ erpnext/buying/doctype/request_for_quotation/tests/test_request_for_quotation_for_status.js
@@ -14,15 +14,15 @@ QUnit.test("Test: Request for Quotation", function (assert) {
 		() => cur_frm.set_value("transaction_date", "04-04-2017"),
 		() => cur_frm.set_value("company", "_Test Company"),
 		// Add Suppliers
- 		() => {
+		() => {
 			cur_frm.fields_dict.suppliers.grid.grid_rows[0].toggle_view();
 		},
 		() => frappe.timeout(1),
  		() => {
- 			cur_frm.fields_dict.suppliers.grid.grid_rows[0].doc.supplier = "_Test Supplier";
- 			frappe.click_check('Send Email');
- 			cur_frm.cur_grid.frm.script_manager.trigger('supplier');
- 		},
+			cur_frm.fields_dict.suppliers.grid.grid_rows[0].doc.supplier = "_Test Supplier";
+			frappe.click_check('Send Email');
+			cur_frm.cur_grid.frm.script_manager.trigger('supplier');
+		},
 		() => frappe.timeout(1),
 		() => {
 			cur_frm.cur_grid.toggle_view();
@@ -35,7 +35,7 @@ QUnit.test("Test: Request for Quotation", function (assert) {
 		},
 		() => frappe.timeout(1),
  		() => {
- 			cur_frm.fields_dict.suppliers.grid.grid_rows[1].doc.supplier = "_Test Supplier 1";
+			cur_frm.fields_dict.suppliers.grid.grid_rows[1].doc.supplier = "_Test Supplier 1";
  			frappe.click_check('Send Email');
  			cur_frm.cur_grid.frm.script_manager.trigger('supplier');
  		},
@@ -44,18 +44,18 @@ QUnit.test("Test: Request for Quotation", function (assert) {
 			cur_frm.cur_grid.toggle_view();
 		},
 		() => frappe.timeout(1),
-		//Add Item
+		// Add Item
 		() => {
 			cur_frm.fields_dict.items.grid.grid_rows[0].toggle_view();
 		},
 		() => frappe.timeout(1),
- 		() => {
+		() => {
 			cur_frm.fields_dict.items.grid.grid_rows[0].doc.item_code = "_Test Item";
- 			frappe.set_control('item_code',"_Test Item");
- 			frappe.set_control('qty',5);
- 			frappe.set_control('schedule_date', "05-05-2017");
+			frappe.set_control('item_code',"_Test Item");
+			frappe.set_control('qty',5);
+			frappe.set_control('schedule_date', "05-05-2017");
 			cur_frm.cur_grid.frm.script_manager.trigger('supplier');
- 		},
+		},
 		() => frappe.timeout(2),
 		() => {
 			cur_frm.cur_grid.toggle_view();
@@ -85,7 +85,7 @@ QUnit.test("Test: Request for Quotation", function (assert) {
 		},
 		() => frappe.timeout(1),
  		() => {
- 			frappe.click_check('No Quote');
+			frappe.click_check('No Quote');
  		},
 		() => frappe.timeout(1),
 		() => {
@@ -94,7 +94,7 @@ QUnit.test("Test: Request for Quotation", function (assert) {
 		() => frappe.click_button('Update'),
 		() => frappe.timeout(1),
 
-		() => frappe.click_button('Supplier Quotation'),
+		() => frappe.click_button('Supplier Quotation'), 
 		() => frappe.timeout(1),
 		() => frappe.click_link('Make'),
 		() => frappe.timeout(1),
@@ -126,7 +126,6 @@ QUnit.test("Test: Request for Quotation", function (assert) {
 		() => {
 			assert.ok(cur_frm.fields_dict.suppliers.grid.grid_rows[1].doc.quote_status == "Received");
 			assert.ok(cur_frm.fields_dict.suppliers.grid.grid_rows[0].doc.no_quote == 1);
-			console.log(cur_frm.fields_dict.suppliers.grid.grid_rows[1].doc);
 		},
 		() => done()
 	]);
