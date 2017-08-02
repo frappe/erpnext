af5c7d1945e8aca2cfdc3d88bc55e1d7a37bfd60
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-08-02 12:53:06 +0530

    removed css selectors and used cur_frm

--- erpnext/manufacturing/doctype/production_order/test_production_order.js
+++ erpnext/manufacturing/doctype/production_order/test_production_order.js
@@ -2,26 +2,6 @@ QUnit.test("test: production order", function (assert) {
 	assert.expect(25);
 	let done = assert.async();
 	let laptop_quantity = 5;
-	let required_item_code = (i) => {
-		return $(`div:nth-child(4) > div.section-body > div > form > div > div > div > div.grid-body\
-			> div.rows > div:nth-child(${i}) > div > div:nth-child(2) > div.static-area.ellipsis > a:visible`).text();
-	};
-	let required_source_warehouse = (i) => {
-		return $(`div:nth-child(4) > div.section-body > div > form > div > div > div > div.grid-body\
-			> div.rows > div:nth-child(${i}) > div > div:nth-child(3) > div.static-area.ellipsis > a:visible`).text();
-	};
-	let required_item_quantity = (i) => {
-		return $(`div:nth-child(4) > div.section-body > div > form > div > div > div > div.grid-body\
-			> div.rows > div:nth-child(${i}) > div > div:nth-child(4) > div.static-area.ellipsis > div:visible`).text();
-	};
-	let operation = (i) => {
-		return $(`div:nth-child(6) > div.section-body > div > form > div > div > div > div.grid-body\
-			> div.rows > div:nth-child(${i}) > div > div.col.grid-static-col.col-xs-3.bold > div.static-area.ellipsis > a:visible`).text();
-	};
-	let operation_workstaion = (i) => {
-		return $(`div:nth-child(6) > div.section-body > div > form > div > div > div > div.grid-body\
-			div:nth-child(${i}) > div > div:nth-child(4) > div.static-area.ellipsis > a:visible`).text();
-	};
 	let items = ["Screen", "CPU", "Keyboard"];
 	let operation_items = ["CPU", "Keyboard", "Screen"];
 	let click_make = () => {
@@ -56,26 +36,32 @@ QUnit.test("test: production order", function (assert) {
 		() => frappe.timeout(2),
 
 		() => {
-			assert.equal(cur_frm.doc.planned_operating_cost, cur_frm.doc.total_operating_cost, "Total and Planned Cost is equal");
-			assert.equal(cur_frm.doc.planned_operating_cost, cur_frm.doc.total_operating_cost, "Total and Planned Cost is equal");
+			assert.equal(cur_frm.doc.planned_operating_cost, cur_frm.doc.total_operating_cost,
+				"Total and Planned Cost is equal");
+			assert.equal(cur_frm.doc.planned_operating_cost, cur_frm.doc.total_operating_cost,
+				"Total and Planned Cost is equal");
 
 			items.forEach(function(item, index) {
-				assert.equal(item, required_item_code(index+1), `Required item ${item} added`);
-				assert.equal("Stores - RB", required_source_warehouse(1), `Item ${item} warhouse verified`);
-				assert.equal("5", required_item_quantity(1), `Item ${item} quantity verified`);
+				assert.equal(item, cur_frm.doc.required_items[index].item_code, `Required item ${item} added`);
+				assert.equal("Stores - RB", cur_frm.doc.required_items[index].source_warehouse, `Item ${item} warhouse verified`);
+				assert.equal("5", cur_frm.doc.required_items[index].required_qty, `Item ${item} quantity verified`);
 			});
 
 			operation_items.forEach(function(operation_item, index) {
-				assert.equal(`Assemble ${operation_item}`, operation(index+1), `Operation ${operation_item} added`);
-				assert.equal(`${operation_item} assembly workstation`, operation_workstaion(index+1), `Workstation ${operation_item} linked`);
+				assert.equal(`Assemble ${operation_item}`, cur_frm.doc.operations[index].operation,
+					`Operation ${operation_item} added`);
+				assert.equal(`${operation_item} assembly workstation`, cur_frm.doc.operations[index].workstation,
+					`Workstation ${operation_item} linked`);
 			});
 		},
 
+		// Submit the production order
 		() => cur_frm.savesubmit(),
 		() => frappe.timeout(1),
 		() => frappe.click_button('Yes'),
 		() => frappe.timeout(1),
 
+		// Confirm the production order timesheet, save and submit it
 		() => frappe.click_link("TS-00"),
 		() => frappe.timeout(1),
 		() => frappe.click_button("Save"),
@@ -85,13 +71,13 @@ QUnit.test("test: production order", function (assert) {
 		() => frappe.click_button("Yes"),
 		() => frappe.timeout(2),
 
+		// Start the production order process
 		() => frappe.set_route("List", "Production Order"),
 		() => frappe.timeout(2),
 		() => frappe.set_route("List", "Production Order"),
 		() => frappe.timeout(2),
 		() => frappe.click_link("Laptop"),
 		() => frappe.timeout(1),
-
 		() => frappe.click_button("Start"),
 		() => frappe.timeout(0.5),
 		() => click_make(),
@@ -99,15 +85,18 @@ QUnit.test("test: production order", function (assert) {
 		() => frappe.timeout(0.5),
 
 		() => {
-			assert.equal(cur_frm.doc.total_outgoing_value, cur_frm.doc.total_incoming_value, "Total incoming and outgoing cost is equal");
-			assert.equal(cur_frm.doc.total_outgoing_value, "99000", "Outgoing cost is correct"); // Price of each item x5
+			assert.equal(cur_frm.doc.total_outgoing_value, cur_frm.doc.total_incoming_value,
+				"Total incoming and outgoing cost is equal");
+			assert.equal(cur_frm.doc.total_outgoing_value, "99000",
+				"Outgoing cost is correct"); // Price of each item x5
 		},
-
+		// Submit for production
 		() => frappe.click_button("Submit"),
 		() => frappe.timeout(0.5),
 		() => frappe.click_button("Yes"),
 		() => frappe.timeout(0.5),
 
+		// Finish the production order by sending for manufacturing
 		() => frappe.set_route("List", "Production Order"),
 		() => frappe.timeout(1),
 		() => frappe.click_link("Laptop"),
@@ -122,9 +111,12 @@ QUnit.test("test: production order", function (assert) {
 		() => frappe.timeout(0.5),
 		() => click_make(),
 		() => {
-			assert.equal(cur_frm.doc.total_incoming_value, "105700", "Incoming cost is correct"); // Price of each item x5
-			assert.equal(cur_frm.doc.total_outgoing_value, "99000", "Outgoing cost is correct"); // Price of each item x5
-			assert.equal(cur_frm.doc.total_incoming_value - cur_frm.doc.total_outgoing_value, cur_frm.doc.value_difference, "Value difference is correct"); // Price of each item x5
+			assert.equal(cur_frm.doc.total_incoming_value, "105700",
+				"Incoming cost is correct"); // Price of each item x5, values are in USD
+			assert.equal(cur_frm.doc.total_outgoing_value, "99000",
+				"Outgoing cost is correct"); // Price of each item x5, values are in USD
+			assert.equal(cur_frm.doc.total_incoming_value - cur_frm.doc.total_outgoing_value, cur_frm.doc.value_difference,
+				"Value difference is correct"); // Price of each item x5, values are in USD
 		},
 		() => frappe.click_button("Save"),
 		() => frappe.timeout(1),
@@ -133,6 +125,7 @@ QUnit.test("test: production order", function (assert) {
 		() => frappe.click_button("Yes"),
 		() => frappe.timeout(1),
 
+		// Manufacturing finished
 		() => frappe.set_route("List", "Production Order"),
 		() => frappe.timeout(1),
 		() => frappe.click_link("Laptop"),
