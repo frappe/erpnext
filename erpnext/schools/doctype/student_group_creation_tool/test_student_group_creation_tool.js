01fa972544c5ae77a0874170c237689524656987
Zlash65 <zarrar65@gmail.com>
2017-08-02 21:21:14 +0530

    changes done accordingly

--- erpnext/schools/doctype/student_group_creation_tool/test_student_group_creation_tool.js
+++ erpnext/schools/doctype/student_group_creation_tool/test_student_group_creation_tool.js
@@ -5,8 +5,7 @@ QUnit.test('Test: Student Group Creation Tool', function(assert){
 	assert.expect(5);
 	let done = assert.async();
 	let instructor_code;
-	let grid;
-	let grid_row;
+	let d;
 	let i;
 	frappe.run_serially([
 		// Saving Instructor code beforehand
@@ -18,14 +17,14 @@ QUnit.test('Test: Student Group Creation Tool', function(assert){
 
 		// Setting up the creation tool to generate and save Student Group
 		() => frappe.set_route('Form', 'Student Group Creation Tool'),
-		() => frappe.timeout(1),
+		() => frappe.timeout(0.5),
 		() => {
 			cur_frm.set_value("academic_year", "2016-17");
 			cur_frm.set_value("academic_term", "2016-17 (Semester 1)");
 			cur_frm.set_value("program", "Standard Test");
 			$(`.btn:contains("Get Courses"):visible`).click();
 		},
-		() => frappe.timeout(0.2),
+		() => frappe.timeout(0.5),
 		() => {
 			assert.ok(cur_frm.get_field("courses").grid.grid_rows.length == 4, 'successfully created groups using the tool');
 		},
@@ -51,54 +50,43 @@ QUnit.test('Test: Student Group Creation Tool', function(assert){
 
 		// Generating Student Group
 		() => frappe.timeout(0.5),
-		() => {$(`.btn:contains("Create Student Groups"):visible`).click();},
+		() => frappe.tests.click_button("Create Student Groups"),
 		() => frappe.timeout(0.5),
-		() => {$(`.btn:contains("Close"):visible`).click();},
-		() => frappe.timeout(0.5),
-		() => {$(`.btn:contains("Close"):visible`).click();},
+		() => frappe.tests.click_button("Close"),
 
 		// Goin to the generated group to set up student and instructor list
-		() => frappe.timeout(0.3),
+		() => frappe.timeout(1),
 		() => frappe.set_route("Form", 'Student Group/test-batch-wise-group-2'),
-		() => frappe.timeout(0.3),
-		() => {$(`.btn:contains("Get Students"):visible`).click();},
-		() => frappe.timeout(0.2),
+		() => frappe.timeout(0.5),
+		() => frappe.tests.click_button("Get Students"),
+		() => frappe.timeout(0.5),
 		() => {
-			assert.ok(cur_frm.get_field("students").grid.grid_rows.length == 1, 'Successfully fetched list of students');
+			assert.ok(cur_frm.get_field("students").grid.grid_rows.length == 2, 'Successfully fetched list of students');
 		},
 		() => frappe.timeout(0.5),
 		() => {
-			grid = cur_frm.get_field("instructors").grid;
-			grid.add_new_row();
-			grid_row = grid.get_row(0).toggle_view(true);
-			grid_row.doc.instructor = instructor_code;
+			d = cur_frm.add_child('instructors');
+			d.instructor = instructor_code;
 			cur_frm.save();
 		},
 		() => {
-			assert.ok(cur_frm.get_field("instructors").grid.grid_rows.length == 1, 'instructor detail stored successfully');
+			assert.ok(cur_frm.get_field("instructors").grid.grid_rows.length == 2, 'instructor detail stored successfully');
 		},
 
 		// Goin to the generated group to set up student and instructor list
-		() => frappe.timeout(0.3),
+		() => frappe.timeout(1),
 		() => frappe.set_route("Form", 'Student Group/test-course-wise-group-2'),
-		() => frappe.timeout(0.3),
-		() => {$(`.btn:contains("Get Students"):visible`).click();},
-		() => frappe.timeout(0.2),
+		() => frappe.timeout(0.5),
+		// () => {$(`.btn:contains("Get Students"):visible`).click();},
+		() => frappe.tests.click_button("Get Students"),
+		() => frappe.timeout(0.5),
 		() => {
 			assert.ok(cur_frm.get_field("students").grid.grid_rows.length == 1, 'Successfully fetched list of students');
 		},
-		() => {
-			$(`.btn:contains("Add Row"):visible`)[1].click();
-			(cur_frm.get_field("instructors").grid).get_row(0).toggle_view(true);
-			cur_frm.doc.instructors[0].instructor = instructor_code;
-			$(`.octicon.octicon-triangle-up`).click();
-		},
 		() => frappe.timeout(0.5),
 		() => {
-			grid = cur_frm.get_field("instructors").grid;
-			grid.add_new_row();
-			grid_row = grid.get_row(0).toggle_view(true);
-			grid_row.doc.instructor = instructor_code;
+			d = cur_frm.add_child('instructors');
+			d.instructor = instructor_code;
 			cur_frm.save();
 		},
 		() => {
