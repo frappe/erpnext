f5a2b8454bf37852edf1c35592604b3bcf2360b8
Rushabh Mehta <rmehta@gmail.com>
2017-08-04 00:19:27 +0530

    [patch] project type

--- erpnext/patches/v8_5/remove_project_type_property_setter.py
+++ erpnext/patches/v8_5/remove_project_type_property_setter.py
@@ -5,3 +5,13 @@ def execute():
 		property='options'))
 	if ps:
 		frappe.delete_doc('Property Setter', ps)
+
+	project_types = frappe.db.sql_list('select distinct project_type from tabProject')
+
+	for project_type in project_types:
+		if not frappe.db.exists("Project Type", project_type):
+			p_type = frappe.get_doc({
+				"doctype": "Project Type",
+				"project_type": project_type
+			})
+			p_type.insert()
