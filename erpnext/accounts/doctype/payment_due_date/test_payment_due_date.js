3e349c5897947856d87e9003b6bc50d8e59606a9
tunde <tundebabzy@gmail.com>
2017-08-06 20:59:12 +0100

    ui test validators

--- erpnext/accounts/doctype/payment_due_date/test_payment_due_date.js
+++ erpnext/accounts/doctype/payment_due_date/test_payment_due_date.js
@@ -1,14 +1,18 @@
 QUnit.module('accounts');
 
 QUnit.test('Create new Payment Due Date in quick view ', function(assert){
-	assert.expect(3);
+	assert.expect(4);
 	let done = assert.async();
 
 	frappe.run_serially([
 		() => frappe.set_route('List', 'Payment Due Date'),
-		() => frappe.timeout(0.5),
+		() => frappe.timeout(2),
 		() => frappe.click_button('New'),
 		// open in quick view
+		() => frappe.timeout(2),
+		() => {
+			assert.ok(cur_dialog);
+		},
 		() => cur_dialog.set_value('code', '_COD'),
 		() => cur_dialog.set_value('description', '_Cash on Delivery'),
 		() => {
@@ -18,7 +22,7 @@ QUnit.test('Create new Payment Due Date in quick view ', function(assert){
 		() => frappe.click_button('Save'),
 		// confirm successful save
 		() => frappe.click_button('Refresh'),
-		() => frappe.timeout(1),
+		() => frappe.timeout(2),
 		() => {
 			assert.ok(cur_list.data.some(obj => obj.name === '_COD'))
 		},
@@ -32,8 +36,9 @@ QUnit.test('Create new Payment Due Date in full view ', function(assert){
 
 	frappe.run_serially([
 		() => frappe.set_route('List', 'Payment Due Date'),
-		() => frappe.timeout(0.5),
+		() => frappe.timeout(2),
 		() => frappe.click_button('New'),
+		() => frappe.timeout(2),
 		// open in full view
 		() => frappe.click_link('Edit in full page'),
 		() => cur_frm.set_value('code', '_2/10 N30'),
@@ -52,12 +57,106 @@ QUnit.test('Create new Payment Due Date in full view ', function(assert){
 			assert.equal(cur_frm.doc.discount_days, 10)
 		},
 		() => frappe.click_button('Save'),
-		() => frappe.timeout(1),
+		() => frappe.timeout(2),
 		// confirm successful save
 		() => frappe.set_route('List', 'Payment Due Date'),
-		() => frappe.timeout(0.5),
+		() => frappe.timeout(2),
 		() => {
 			assert.ok(cur_list.data.some(obj => obj.name === '_2/10 N30'))
+		},
+		() => done()
+	]);
+});
+
+QUnit.test('Payment Due Date Validation', function(assert){
+	assert.expect(8);
+	let done = assert.async();
+	frappe.run_serially([
+		() => frappe.set_route('List', 'Payment Due Date'),
+		() => frappe.timeout(2.5),
+		() => frappe.click_button('New'),
+		() => frappe.timeout(2),
+		// open in full view
+		() => frappe.click_link('Edit in full page'),
+		() => cur_frm.set_value('code', '_Test Code'),
+		() => cur_frm.set_value('description', '_2% Cash Discount within 10 days; Net 30 days'),
+		// validate numerical fields cannot be negative
+		() => cur_frm.set_value('term_days', '-30'),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			// dialog is shown
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
+		() => cur_frm.set_value('term_days', 30),
+		() => frappe.click_link('Cash Discounts'),
+		() => frappe.click_check('Include Cash Discount'),
+		() => cur_frm.set_value('discount_percentage', -22),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			// dialog is shown
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
+		() => cur_frm.set_value('discount_percentage', 2),
+		() => cur_frm.set_value('discount_days', -10),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			// dialog is shown
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
+
+		// validate discount component
+		() => cur_frm.set_value('term_days', 0),
+		() => cur_frm.set_value('discount_days', 0),
+		() => cur_frm.set_value('discount_percentage', 0),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			// dialog is shown
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
+		() => cur_frm.set_value('discount_days', 1),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			// dialog is shown
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
+		() => cur_frm.set_value('discount_days', 0),
+		() => cur_frm.set_value('discount_percentage', 2),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			// dialog is shown
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
+		() => cur_frm.set_value('discount_percentage', 2),
+		() => cur_frm.set_value('discount_days', 10),
+		() => cur_frm.set_value('term_days', 0),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			// dialog is shown
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
+		() => cur_frm.set_value('term_days', 30),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+
+		// confirm successful save
+		() => frappe.set_route('List', 'Payment Due Date'),
+		() => frappe.timeout(2),
+		() => {
+			assert.ok(cur_list.data.some(obj => obj.name === '_Test Code'));
 		},
 		() => done()
 	]);
