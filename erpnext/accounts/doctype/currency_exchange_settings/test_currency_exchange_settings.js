50c2c3d64928a2d17ef77d1ea37ad4b55f80e2fa
tunde <tundebabzy@gmail.com>
2017-08-12 13:46:35 +0100

    ui test validation: stale days cannot be negative

--- erpnext/accounts/doctype/currency_exchange_settings/test_currency_exchange_settings.js
+++ erpnext/accounts/doctype/currency_exchange_settings/test_currency_exchange_settings.js
@@ -1,23 +1,35 @@
-/* eslint-disable */
-// rename this file from _test_[name] to test_[name] to activate
-// and remove above this line
+QUnit.module('accounts');
 
-QUnit.test("test: Currency Exchange Settings", function (assert) {
+QUnit.test("test: Currency Exchange Settings doesn't allow negatives", function (assert) {
 	let done = assert.async();
 
-	// number of asserts
-	assert.expect(1);
+	assert.expect(2);
 
-	frappe.run_serially('Currency Exchange Settings', [
-		// insert a new Currency Exchange Settings
-		() => frappe.tests.make([
-			// values to be set
-			{key: 'value'}
-		]),
+	frappe.run_serially([
+		() => frappe.set_route('Form', 'Currency Exchange Settings', 'Currency Exchange Settings'),
+		() => frappe.timeout(2),
+		() => unchecked_if_checked(cur_frm, 'Allow Stale Exchange Rates', frappe.click_check),
+		() => cur_frm.set_value('stale_days', 0),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
 		() => {
-			assert.equal(cur_frm.doc.key, 'value');
+			assert.ok(cur_dialog);
 		},
+		() => frappe.click_button('Close'),
+		() => cur_frm.set_value('stale_days', -1),
+		() => frappe.click_button('Save'),
+		() => frappe.timeout(2),
+		() => {
+			assert.ok(cur_dialog);
+		},
+		() => frappe.click_button('Close'),
 		() => done()
 	]);
 
 });
+
+const unchecked_if_checked = function(frm, field_name, fn){
+	if (frm.doc.allow_stale) {
+		return fn(field_name);
+	};
+}
