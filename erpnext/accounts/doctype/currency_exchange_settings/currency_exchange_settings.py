2127dadc94284146873ade02fc17d6a8e26be727
tunde <tundebabzy@gmail.com>
2017-08-12 13:44:06 +0100

    add validation: stale days cannot be negative

--- erpnext/accounts/doctype/currency_exchange_settings/currency_exchange_settings.py
+++ erpnext/accounts/doctype/currency_exchange_settings/currency_exchange_settings.py
@@ -4,7 +4,17 @@
 
 from __future__ import unicode_literals
 from frappe.model.document import Document
+from frappe.utils import cint
+from frappe import msgprint
 
 
 class CurrencyExchangeSettings(Document):
-	pass
+	def validate(self):
+		self.validate_stale_days()
+
+	def validate_stale_days(self):
+		if not self.allow_stale and cint(self.stale_days) <= 0:
+			msgprint(
+				"Stale Days should start from 1.", title='Error', indicator='red',
+				raise_exception=1)
+
