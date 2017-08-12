268e4b9bfa66d002807882f51c51d292855404e0
tunde <tundebabzy@gmail.com>
2017-08-12 13:45:59 +0100

    test validation: stale days cannot be negative

--- erpnext/accounts/doctype/currency_exchange_settings/test_currency_exchange_settings.py
+++ erpnext/accounts/doctype/currency_exchange_settings/test_currency_exchange_settings.py
@@ -8,4 +8,12 @@ import unittest
 
 
 class TestCurrencyExchangeSettings(unittest.TestCase):
-	pass
+	def test_stale_days(self):
+		cur_settings = frappe.get_doc('Currency Exchange Settings', 'Currency Exchange Settings')
+		cur_settings.allow_stale = 0
+		cur_settings.stale_days = 0
+
+		self.assertRaises(frappe.ValidationError, cur_settings.save)
+
+		cur_settings.stale_days = -1
+		self.assertRaises(frappe.ValidationError, cur_settings.save)
