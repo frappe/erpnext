019aa237d05cdbf1f57fbf5b722b07668f2737b9
Utkarsh Goswami <goswamiutkarsh0@gmail.com>
2017-08-17 19:19:05 +0530

    Codacy fix

--- erpnext/hr/doctype/job_applicant/test_job_applicant.js
+++ erpnext/hr/doctype/job_applicant/test_job_applicant.js
@@ -12,16 +12,15 @@ QUnit.test("Test: Job Opening [HR]", function (assert) {
 				{ email_id: 'goswamiutkarsh0@gmail.com'},
 				{ job_title: 'software-developer'},
 				{ cover_letter: 'Highly skilled in designing, testing, and developing software.'+
-					' This is just a test.'}				
+					' This is just a test.'}
 			]);
 		},
 		() => frappe.timeout(4),
 		() => frappe.set_route('List','Job Applicant'),
 		() => frappe.timeout(3),
- 
 		() => {
 			assert.ok(cur_list.data.length==1, 'Job Applicant created successfully');
-			assert.ok(cur_list.data[0].name=='Utkarsh Goswami - goswamiutkarsh0@gmail.com - software-developer', 
+			assert.ok(cur_list.data[0].name=='Utkarsh Goswami - goswamiutkarsh0@gmail.com - software-developer',
 				'Correct job applicant with valid job title');
 		},
 		() => done()
