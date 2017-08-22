04b664f99f0dc0372a812d50cae9b515c32f3d1a
Utkarsh Goswami <goswamiutkarsh0@gmail.com>
2017-08-22 13:22:46 +0530

    Travis fixes

--- erpnext/hr/doctype/expense_claim_type/test_expense_claim_type.js
+++ erpnext/hr/doctype/expense_claim_type/test_expense_claim_type.js
@@ -18,9 +18,9 @@ QUnit.test("Test: Expense Claim Type [HR]", function (assert) {
 			]);
 		},
 		() => frappe.timeout(3),
-		() => frappe.set_route('List','Expense Claim Type','List'),
+		() => frappe.set_route('List','Expense Claim Type'),
+		() => frappe.timeout(3),
 		// Checking if the created type is present in the list
-		() => frappe.timeout(2),
 		() => {
 			assert.equal('Test Expense Type 1', cur_list.data[0].name,
 				'Expense Claim Type created successfully');
