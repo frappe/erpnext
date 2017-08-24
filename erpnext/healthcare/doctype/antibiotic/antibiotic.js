9b39c9a01f0cad5d590637a622e3701bd7af5d60
jamsheer <kuttypjamsheer@gmail.com>
2017-08-24 17:18:57 +0530

    Codacy fixes

--- erpnext/healthcare/doctype/antibiotic/antibiotic.js
+++ erpnext/healthcare/doctype/antibiotic/antibiotic.js
@@ -2,7 +2,4 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Antibiotic', {
-	refresh: function(frm) {
-
-	}
 });
--- erpnext/healthcare/doctype/antibiotic/test_antibiotic.py
+++ erpnext/healthcare/doctype/antibiotic/test_antibiotic.py
@@ -2,8 +2,6 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
 from __future__ import unicode_literals
-
-import frappe
 import unittest
 
 class TestAntibiotic(unittest.TestCase):
