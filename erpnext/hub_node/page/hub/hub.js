3450d3873584d9f35ef30e013277ca1b9daa6d3f
Faris Ansari <netchamp.faris@gmail.com>
2017-08-28 13:03:11 +0530

    add hub.less, refactor code

--- erpnext/hub_node/page/hub/hub.js
+++ erpnext/hub_node/page/hub/hub.js
@@ -1,50 +1,39 @@
-frappe.provide("erpnext.hub");
+/* globals ERPNextHub, ERPNextHubList */
+
+frappe.provide('erpnext.hub');
 
 frappe.pages['hub'].on_page_load = function(wrapper) {
-	let page = frappe.ui.make_app_page({
+	const page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'ERPNext Hub',
-		single_column: true
+		title: 'ERPNext Hub'
 	});
 
-	frappe.hub = new erpnext.hub.Hub({page:page});
+	erpnext.hub.Hub = new ERPNextHub({ page });
+};
 
-}
-
-// frappe.pages['hub'].on_page_load = function() {
-// 	frappe.hub.go_to_home_page();
-// }
-
-// frappe.pages['hub'].on_page_show = function() {
-// 	frappe.hub.go_to_home_page();
-// }
-
-erpnext.hub.Hub = class {
-	constructor({
-		page = null
-	}) {
+window.ERPNextHub = class ERPNextHub {
+	constructor({ page }) {
 		this.page = page;
-		this.setup();
+
+		frappe.require('/assets/erpnext/css/hub.css',
+			() => this.setup()
+		);
 	}
 
 	setup() {
 		this.setup_header();
-		this.$hub_main_section = $(`<div class="hub-main-section">`).appendTo(this.page.body);
+		this.$hub_main_section =
+			$(`<div class='hub-main-section'>`).appendTo(this.page.body);
 		this.refresh();
 	}
 
 	refresh() {
 		this.$hub_main_section.empty();
 		this.page.page_form.hide();
-		// if(this.hub && this.hub.publish && !this.$hub_list) {
-		// 	this.setup_live_state();
-		// } else {
-		// 	this.setup_empty_state();
-		// }
 
-		frappe.model.with_doc("Hub Settings", "Hub Settings", () => {
-			this.hub = locals["Hub Settings"]["Hub Settings"];
-			if(!this.hub.enabled) {
+		frappe.model.with_doc('Hub Settings', 'Hub Settings', () => {
+			this.hub_settings = frappe.get_doc('Hub Settings');
+			if(!this.hub_settings.enabled) {
 				this.setup_empty_state();
 			} else {
 				this.setup_live_state();
@@ -53,23 +42,32 @@ erpnext.hub.Hub = class {
 	}
 
 	setup_header() {
-		this.page.page_title = this.page.wrapper.find(".page-title");
-		this.tag_line = $(`<div class="tag-line-container"><span class="tag-line text-muted">Product listing and discovery for
-			ERPNext users</span></div>`).appendTo(this.page.page_title);
-		this.account_details = $(`<div class="account-details">
-			<i class="fa fa-user"></i> <a class="user_name small"></a>
-			<i class="octicon octicon-globe" style="margin-left: 20px;"></i> <a class="company_name small"></a>
-		</div>`).appendTo(this.page.page_actions);
-		this.account_details.hide();
+		this.page.page_title = this.page.wrapper.find('.page-title');
+		this.tag_line = $(`
+			<div class='tag-line-container'>
+				<span class='tag-line text-muted small'>
+					Product listing and discovery for ERPNext users
+				</span>
+			</div>`)
+			.appendTo(this.page.page_title);
+
+		this.account_details = $(`
+			<div class='account-details text-muted'>
+				<!-- <i class='octicon octicon-person'></i> <a class='user-name small'></a> -->
+				<i class='octicon octicon-globe' style='margin-left: 20px;'></i> <a class='company-name small'></a>
+			</div>`)
+			.appendTo(this.page.page_actions)
+			.hide();
+
 		this.bind_title();
 	}
 
 	setup_empty_state() {
 		this.remove_account_from_header();
-		let $empty_state = $(frappe.render_template("register_in_hub", {}));
+		let $empty_state = $(frappe.render_template('register_in_hub', {}));
 		this.$hub_main_section.append($empty_state);
-		this.$hub_main_section.find(".hub-settings-btn").on('click', () => {
-			frappe.set_route("Form", "Hub Settings", {});
+		this.$hub_main_section.find('.hub-settings-btn').on('click', () => {
+			frappe.set_route('Form', 'Hub Settings', {});
 		});
 	}
 
@@ -91,11 +89,11 @@ erpnext.hub.Hub = class {
 			return {label: d, value: d}
 		});
 
-		this.category_select = this.page.add_select(__("Category"),
-			[{"label": __("Select Category..."), value: "" }].concat(categories)
+		this.category_select = this.page.add_select(__('Category'),
+			[{'label': __('Select Category...'), value: '' }].concat(categories)
 		);
-		this.country_select = this.page.add_select(__("Country"),
-			[{"label": __("Select Country..."), value: "" }].concat(countries)
+		this.country_select = this.page.add_select(__('Country'),
+			[{'label': __('Select Country...'), value: '' }].concat(countries)
 		);
 
 		this.get_hub_companies((companies) => {
@@ -103,23 +101,23 @@ erpnext.hub.Hub = class {
 				return {label: d.company, value: d.company}
 			});
 
-			this.company_select = this.page.add_select(__("Company"),
-				[{"label": __("Select Company..."), value: "" }].concat(company_names)
+			this.company_select = this.page.add_select(__('Company'),
+				[{'label': __('Select Company...'), value: '' }].concat(company_names)
 			);
 
-			this.company_select.on("change", () => {
-				this.$search.val("");
-				let val = $(this.company_select).val() || "";
+			this.company_select.on('change', () => {
+				this.$search.val('');
+				let val = $(this.company_select).val() || '';
 				this.go_to_items_only_page(
-					["hub", "Company", val, "Products"],
+					['hub', 'Company', val, 'Products'],
 					'Products by '  + val,
-					"company-product-list",
-					{text: "", company: val}
+					'company-product-list',
+					{text: '', company: val}
 				);
 			});
 		});
 
-		this.$search = this.page.add_data(__("Search"));
+		this.$search = this.page.add_data(__('Search'));
 		this.bind_filters();
 		this.setup_search();
 	}
@@ -130,7 +128,7 @@ erpnext.hub.Hub = class {
 	}
 
 	reset_filters() {
-		$(this.company_select).val("");
+		$(this.company_select).val('');
 	}
 
 	bind_title() {
@@ -140,27 +138,34 @@ erpnext.hub.Hub = class {
 	}
 
 	render_body() {
-		this.$home_page = $(frappe.render_template("hub_home_page", {}))
-			.appendTo(this.$hub_main_section);
+		this.$home_page = $(`
+			<div class = 'hub-home-page'>
+				<div class='banner'></div>
+				<div class='listing-body row'>
+					<div class='main-list-section'></div>
+				</div>
+			</div>
+		`).appendTo(this.$hub_main_section);
 
-		this.$banner = this.$hub_main_section.find(".banner");
-		this.$listing_body = this.$hub_main_section.find(".listing-body");
-		this.$main_list_section = this.$hub_main_section.find(".main-list-section");
-		this.$side_list_section = this.$hub_main_section.find(".side-list-section");
+		this.$banner = this.$hub_main_section.find('.banner');
+		this.$listing_body = this.$hub_main_section.find('.listing-body');
+		this.$main_list_section = this.$hub_main_section.find('.main-list-section');
+		this.$side_list_section = this.$hub_main_section.find('.side-list-section');
 	}
 
 	setup_lists() {
-		this.home_item_list = new erpnext.hub.HubList({
+		this.home_item_list = new ERPNextHubList({
 			parent: this.$main_list_section,
-			title: "New",
+			title: 'New',
 			page_length: 20,
-			list_css_class: "home-product-list",
-			method: "erpnext.hub_node.get_items",
-			filters: {text: ""} // filters at the time of creation
+			list_css_class: 'home-product-list',
+			method: 'erpnext.hub_node.get_items',
+			filters: {text: ''}, // filters at the time of creation
+			on_item_click: (item) => {
+				this.go_to_item_page(item);
+			}
 		});
-		this.home_item_list.item_on_click = (item) => {
-			this.go_to_item_page(item);
-		}
+
 		this.home_item_list.setup();
 
 		this.setup_company_list();
@@ -169,20 +174,20 @@ erpnext.hub.Hub = class {
 	setup_company_list() {
 		this.get_hub_companies((companies) => {
 			companies.map(company => {
-				let company_card = $(`<div class="company_card">
+				let company_card = $(`<div class='company_card'>
 				</div>`).appendTo(this.$side_list_section);
 			});
 		})
 	}
 
 	setup_search() {
-		this.$search.on("keypress", (e) => {
+		this.$search.on('keypress', (e) => {
 			if(e.which === 13) {
-				var search_term = ($(this.$search).val() || "").toLowerCase();
+				var search_term = ($(this.$search).val() || '').toLowerCase();
 				this.go_to_items_only_page(
-					["hub", "search", search_term],
-					'Search results for "'  + search_term + '"',
-					"search-product-list",
+					['hub', 'search', search_term],
+					'Search results for \''  + search_term + '\'',
+					'search-product-list',
 					{text: search_term}
 				);
 			}
@@ -192,12 +197,12 @@ erpnext.hub.Hub = class {
 	go_to_items_only_page(route, title, class_name, filters) {
 		frappe.set_route(route);
 		this.$hub_main_section.empty();
-		this.filtered_item_list = new erpnext.hub.HubList({
+		this.filtered_item_list = new ERPNextHubList({
 			parent: this.$hub_main_section,
 			title: title,
 			page_length: 20,
 			list_css_class: class_name,
-			method: "erpnext.hub_node.get_items",
+			method: 'erpnext.hub_node.get_items',
 			filters: filters
 		});
 		this.filtered_item_list.item_on_click = (item) => {
@@ -208,20 +213,20 @@ erpnext.hub.Hub = class {
 
 	go_to_item_page(item) {
 		// TODO: Check if item quote already requested
-		frappe.set_route("hub", "Item", item.item_name);
+		frappe.set_route('hub', 'Item', item.item_name);
 		this.$hub_main_section.empty();
 		let $item_page = $(frappe.render_template("hub_item_page", {item:item}))
 			.appendTo(this.$hub_main_section);
 
 		let $company_items = $item_page.find('.company-items');
 
-		let company_item_list = new erpnext.hub.HubList({
+		let company_item_list = new ERPNextHubList({
 			parent: $company_items,
-			title: "More by " + item.company,
+			title: 'More by ' + item.company,
 			page_length: 20,
-			list_css_class: "company-item-list",
-			method: "erpnext.hub_node.get_items",
-			filters: {text: "", company: item.company}
+			list_css_class: 'company-item-list',
+			method: 'erpnext.hub_node.get_items',
+			filters: {text: '', company: item.company}
 		});
 		company_item_list.item_on_click = (item) => {
 			this.go_to_item_page(item);
@@ -230,12 +235,12 @@ erpnext.hub.Hub = class {
 
 		let $rfq_btn = $item_page.find('.rfq-btn');
 		$rfq_btn.on('click', () => {
-			$rfq_btn.addClass("disabled");
+			$rfq_btn.addClass('disabled');
 			this.make_rfq(item, (success) => {
 				if(success) {
-					$rfq_btn.html('<span><i class="fa fa-check"></i> Quote Requested</span>');
+					$rfq_btn.html(`<span><i class='fa fa-check'></i> Quote Requested</span>`);
 				} else {
-					frappe.msgprint(__("Sorry, we cannot process your request at this time."));
+					frappe.msgprint(__('Sorry, we cannot process your request at this time.'));
 				}
 			});
 		});
@@ -246,20 +251,21 @@ erpnext.hub.Hub = class {
 	}
 
 	go_to_company_page(company) {
-		frappe.set_route("hub", "Company", company.name);
+		frappe.set_route('hub', 'Company', company.name);
 	}
 
 	// bind_breadcrumbs() {}
 
 	go_to_home_page() {
-		frappe.set_route("hub");
+		frappe.set_route('hub');
 		this.reset_filters();
 		this.refresh();
 	}
 
 	add_account_to_header() {
-		this.account_details.find('.user_name').empty().append(this.hub.hub_user_name);
-		this.account_details.find('.company_name').empty().append(this.hub.company);
+		const { hub_user_name, company } = this.hub_settings;
+		// this.account_details.find('.user-name').hide();
+		this.account_details.find('.company-name').text(company);
 		this.account_details.show();
 	}
 
@@ -283,7 +289,7 @@ erpnext.hub.Hub = class {
 	}
 	get_hub_companies(callback) {
 		frappe.call({
-			method: "erpnext.hub_node.get_all_companies",
+			method: 'erpnext.hub_node.get_all_companies',
 			args: {},
 			callback: function(r) {
 				let companies = r.message.companies ? r.message.companies : [];
@@ -299,10 +305,10 @@ erpnext.hub.Hub = class {
 	make_rfq(item, callback) {
 		// should be through hub?
 		frappe.call({
-			method: "erpnext.hub_node.make_rfq_and_send_opportunity",
+			method: 'erpnext.hub_node.make_rfq_and_send_opportunity',
 			args: {
 				item_code: item.item_code,
-				item_group: "Products",
+				item_group: 'Products',
 				supplier_name: item.hub_user_name,
 				supplier_email: item.hub_user_email,
 				company: item.company,
@@ -315,15 +321,15 @@ erpnext.hub.Hub = class {
 	}
 }
 
-erpnext.hub.HubList = class {
+window.ERPNextHubList = class ERPNextHubList {
 	constructor({
 		parent = null,
-		title = "Items",
+		title = 'Items',
 		page_length = 10,
-		list_css_class = "",
-		method = "",
-		filters = {text: ""},
-
+		list_css_class = '',
+		method = '',
+		filters = {text: ''},
+		on_item_click = null
 	}) {
 		this.parent = parent;
 		this.title = title;
@@ -331,26 +337,30 @@ erpnext.hub.HubList = class {
 		this.list_css_class = list_css_class;
 		this.method = method;
 		this.filters = filters;
+		this.on_item_click = on_item_click;
 		// this.setup();
 	}
 
 	setup() {
-		this.container = $(`<div class="item-list-container
-			${this.list_css_class}" data-page-length="${this.page_length}">
-			<div class="item-list-header"><h3>${this.title}</h3></div>
-			<div class="item-list"></div>
-			<div class="list-state">
-				<div class="loading">
-					<p class="text-muted text-center">${__("Loading...")}</p>
+		this.container = $(`
+			<div class='item-list-container ${this.list_css_class}' data-page-length='${this.page_length}'>
+				<div class='item-list-header'>
+					<h3>${this.title}</h3>
 				</div>
-				<div class="done hide">
-					<p class="text-muted text-center">${__("No more results.")}</p>
+				<div class='item-list'></div>
+				<div class='list-state'>
+					<div class='loading'>
+						<p class='text-muted text-center'>${__('Loading...')}</p>
+					</div>
+					<div class='done hide'>
+						<p class='text-muted text-center'>${__('No more results')}</p>
+					</div>
+					<div class='more text-right'>
+						<button class='btn btn-default btn-sm'>${__('More')}</div>
+					</div>
 				</div>
-				<div class="more text-center">
-					<button class="btn btn-default btn-sm">${__("More")}</div>
-				</div>
-			</div>
-		</div>`).appendTo(this.parent);
+			</div>`)
+			.appendTo(this.parent);
 
 		this.$item_list_title = this.container.find('.item-list-header h3');
 		this.$list = this.container.find('.item-list');
@@ -384,42 +394,56 @@ erpnext.hub.HubList = class {
 
 	next_page() {
 		this.$item_list_title.html(this.title);
-		let me = this;
-		let start = this.$list.find('.item-card').length;
+		const me = this;
+		const start = this.$list.find('.item-card').length;
 		this.$loading.show();
+
+		// build args
 		let args = {
 			start: start,
 			limit: this.page_length + 1
-		}
-		$.extend(args, this.filters);
-		console.log("next page called with args", args, this.filters, me.page_length, start);
+		};
+		Object.assign(args, this.filters);
+
+		console.log('next page called with args', args, this.filters, me.page_length, start);
+
 		frappe.call({
-			method: me.method,
+			method: this.method,
 			args: args,
 			callback: function(r) {
+<<<<<<< HEAD
 				let items = r.message.items;
 				console.log("items: ", items);
+=======
+				console.log('items: ', r.message);
+>>>>>>> 3577d6734... add hub.less, refactor code
 				me.$loading.hide();
 				if(items) {
 					if(items.length && items.length > me.page_length) {
 						items.pop();
 						me.$more.show();
-						me.$done.addClass("hide");
+						me.$done.addClass('hide');
 					} else {
-						me.$done.removeClass("hide");
+						me.$done.removeClass('hide');
 						me.$more.hide();
 					}
+<<<<<<< HEAD
 					items.forEach(function(item) {
 						let $item = me.make_item_card(item).appendTo(me.$list);
+=======
+					r.message.forEach(function(item) {
+						me.make_item_card(item).appendTo(me.$list);
+>>>>>>> 3577d6734... add hub.less, refactor code
 					});
 				} else {
-					me.$item_list_title.html("No results found");
+					me.$item_list_title.html('No results found');
 				}
 			}
 		});
 	}
 
 	make_item_card(item) {
+<<<<<<< HEAD
 		let $item =  $(`<div class="item-card">
 			<div class="image">
 				<a class="item-link"><img src="${item.image}"></a>
@@ -431,12 +455,25 @@ erpnext.hub.HubList = class {
 					item.formatted_price +  '</div>': ""}
 			</div>
 		</div>`);
+=======
+		let $item =  $(`
+			<div class='item-card'>
+				<div class='image'>
+					<a class='item-link'><img src='${item.image}'></a>
+				</div>
+				<div class='content'>
+					<div class='title'><a class='item-link'>${item.item_name}</a></div>
+					<div class='company'>${item.company}</div>
+					${item.standard_rate ? `<div class='price'>` +
+						item.standard_rate.toFixed(2) +  '</div>': ''}
+				</div>
+			</div>`);
+>>>>>>> 3577d6734... add hub.less, refactor code
 
 		$item.find('.item-link').on('click', () => {
-			this.item_on_click(item);
+			this.on_item_click &&
+			this.on_item_click(item);
 		});
 		return $item;
 	}
-
-	item_on_click(item) {}
-}
+};
