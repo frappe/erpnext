61b5222878c54abaf4eecf963f5d8aecc36b917c
pratu16x7 <pratu16x7@gmail.com>
2017-08-31 10:37:03 +0530

    [progress] remove slide data

--- erpnext/utilities/user_progress.py
+++ erpnext/utilities/user_progress.py
@@ -13,62 +13,20 @@ def get_slide_settings():
 	return [
 		frappe._dict(
 			action_name='Add Company',
-			title=_("Add Your Company") if domain != 'Education' else _("Add Your Institution"),
+			title=_("Setup Company") if domain != 'Education' else _("Setup Institution"),
 			help=_('Setup your ' + ('company' if domain != 'Education' else 'institution') + ' and brand.'),
 			# image_src="/assets/erpnext/images/illustrations/shop.jpg",
 			fields=[],
-			action_cards=[
+			done_state_title=_("You added " +  company),
+			done_state_title_route=["Form", "Company", company],
+			help_links=[
 				{
-					"action_name": "Make Company",
-					"title": _("You added " +  company),
-					"title_route": ["Form", "Company", company],
-					"done": 1
-					# "actions": [{
-					# 	"label": _("View " + company),
-					# 	"route":
-					# }]
-				},
-				# {
-				# 	"action_name": "Watch Accounts Opening Balance",
-				# 	"title": _("Accounts Opening Balance"),
-				# 	"title_link": _("Company"),
-				# 	"content": _("Set an opening balance in Accounts."),
-				# 	"video_id": "U5wPIvEn-0c"
-				# },
-				{
-					"action_name": "Review Chart of Accounts",
-					"title": _("Review Chart of Accounts"),
-					"title_route": ["Tree", "Account"],
-					# "content": _("Review the Chart of Accounts, update your opening balance and more"),
-					# "actions": [{
-					# 	"label": _("View Chart of Accounts"),
-					# 	"route": ["Tree", "Account"]
-					# }],
-					"help_links": [{
-						"label": _('Need help?'),
-						"url": ["https://erpnext.org/docs/user/manual/en/accounts"]
-					},
-					{
-						"label": _('Set an Opening Balance'),
-						"video_id": "U5wPIvEn-0c"
-					}
-
-					]
+					"label": _("Chart of Accounts"),
+					"url": ["https://erpnext.org/docs/user/manual/en/accounts/chart-of-accounts"]
 				},
 				{
-					"action_name": "Set Sales Target",
-					"title": _("Set a Sales Target"),
-					"title_route": ["Form", "Company", company],
-					# "content": _("Set a monthly sales goal that you'd like to achieve for your company " + company +
-					# 	"."),
-					# "actions": [{
-					# 	"label": _('Set a monthly target'),
-					# 	"route": ["Form", "Company", company]
-					# }],
-					"help_links": [{
-						"label": _('Need help?'),
-						"url": ["https://erpnext.org/docs/user/manual/en/setting-up/setting-company-sales-goal"]
-					}]
+					"label": _("Opening Balances"),
+					"video_id": "U5wPIvEn-0c"
 				}
 			]
 		)
@@ -89,41 +47,12 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_customers",
-			action_cards=[
+			done_state_title=_("Go to Customers"),
+			done_state_title_route=["List", "Customer"],
+			help_links=[
 				{
-					"action_name": "Add Customers",
-					"title": _("You added Customers"),
-					"done": 1,
-					"actions": [{
-						"label": _('Go to Customers'),
-						"route": ["List", "Customer"]
-					}, {
-						"label": _("Import Customers"),
-						"route": ["data-import-tool", {"doctype": "Customer"}]
-					}],
-					"help_links": [{
-						"label": _('Learn More'),
-						"link": ["https://erpnext.org/docs/user/manual/en/CRM/customer.html"]
-					}]
-				},
-				{
-					"action_name": "Create Leads",
-					"title": _("Create Leads"),
-					"content": _("Leads help you get business, add all your contacts and more as your leads"),
-					"actions": [{
-						"label": _('Make a Lead'),
-						"new_doc": 'Lead'
-					}],
-					"help_links": [{
-						"label": _('Learn More'),
-						"url": ["https://erpnext.org/docs/user/manual/en/CRM/lead"]
-					}]
-				},
-				{
-					"action_name": "Watch CRM",
-					"title": _("The CRM Module"),
-					"content": _("How to manage Leads, Opportunities and Quotations in ERPNext."),
-					"video_id": "o9XCSZHJfpA"
+					"label": _('Learn More'),
+					"url": ["https://erpnext.org/docs/user/manual/en/CRM/customer.html"]
 				}
 			]
 		),
@@ -143,35 +72,17 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_suppliers",
-			action_cards=[
+			done_state_title=_("Go to Suppliers"),
+			done_state_title_route=["List", "Supplier"],
+			help_links=[
 				{
-					"action_name": "Add Suppliers",
-					"title": _("You added Suppliers"),
-					"done": 1,
-					"actions": [{
-						"label": _('Go to Suppliers'),
-						"route": ["List", "Supplier"]
-					}, {
-						"label": _("Import Suppliers"),
-						"route": ["data-import-tool", {"doctype": "Supplier"}]
-					}],
-					"help_links": [{
-						"label": _('Learn More'),
-						"link": ["https://erpnext.org/docs/user/manual/en/buying/supplier"]
-					}]
+					"label": _('Learn More'),
+					"url": ["https://erpnext.org/docs/user/manual/en/buying/supplier"]
 				},
 				{
-					"action_name": "Watch Customers and Suppliers",
-					"title": _("Customers and Suppliers"),
-					"content": _("Manage Customers, Supplier, their address and contact and other defaults."),
+					"label": _('Customers and Suppliers'),
 					"video_id": "zsrrVDk6VBs"
 				},
-				{
-					"action_name": "Watch Request for Quotation",
-					"title": _("Request for Quotation"),
-					"content": _("RFQs and Supplier Quotations in ERPNext."),
-					"video_id": "q85GFvWfZGI"
-				}
 			]
 		),
 		frappe._dict(
@@ -194,38 +105,13 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_items",
-			action_cards=[
+			done_state_title=_("Go to Items"),
+			done_state_title_route=["List", "Item"],
+			help_links=[
 				{
-					"action_name": "Add Products",
-					"title": _("You added Products"),
-					"done": 1,
-					"actions": [{
-						"label": _("View Items"),
-						"route": ["List", "Item"]
-					}, {
-						"label": _("Import Items"),
-						"route": ["data-import-tool", {"doctype": "Item"}]
-					}]
-				},
-				{
-					"action_name": "Make Sales Order",
-					"title": _("Manage your orders"),
-					"content": _("Make Sales Orders to help you plan your work and deliver on-time."),
-					"actions": [{
-						"label": _('Make Sales Order'),
-						"new_doc": 'Sales Order'
-					}],
-					"help_links": [{
-						"label": _('Learn More'),
-						"url": ["https://erpnext.org/docs/user/manual/en/selling/sales-order"]
-					}]
-				},
-				{
-					"action_name": "Learn Sales Cycle",
-					"title": _("Sales Cycle"),
-					"content": _("Learn how to create transactions related to sales cycle like Sales Order, Delivery Note, Sales Invoice and Payment Entry."),
+					"label": _("Explore Sales Cycle"),
 					"video_id": "1eP90MWoDQM"
-				}
+				},
 			]
 		),
 
@@ -241,25 +127,13 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_program",
-			action_cards=[
+			done_state_title=_("Go to Programs"),
+			done_state_title_route=["List", "Program"],
+			help_links=[
 				{
-					"action_name": "Add Programs",
-					"title": _("You added Programs"),
-					"done": 1,
-					"actions": [{
-						"label": _('Go to Programs'),
-						"route": ["List", "Program"]
-					}, {
-						"label": _("Import Programs"),
-						"route": ["data-import-tool", {"doctype": "Program"}]
-					}]
-				},
-				{
-					"action_name": "Watch Student Application",
-					"title": _("Student Application"),
-					"content": _("Manage student applications and create a web form for online applications"),
+					"label": _("Student Application"),
 					"video_id": "l8PUACusN3E"
-				}
+				},
 			]
 
 		),
@@ -274,27 +148,12 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_course",
-			action_cards=[
+			done_state_title=_("Go to Courses"),
+			done_state_title_route=["List", "Course"],
+			help_links=[
 				{
-					"action_name": "Add Courses",
-					"title": _("You added Courses"),
-					"done": 1,
-					"actions": [{
-						"label": _('Go to Courses'),
-						"route": ["List", "Course"]
-					}, {
-						"label": _("Import Courses"),
-						"route": ["data-import-tool", {"doctype": "Course"}]
-					}]
-				},
-				{
-					"action_name": "Add Students",
-					"title": _("Add Students"),
-					"content": _("Students are at the heart of the system. Add all your students."),
-					"actions": [{
-						"label": _('Make a Student'),
-						"new_doc": 'Student'
-					}]
+					"label": _('Add Students'),
+					"route": ["List", "Student"]
 				}
 			]
 		),
@@ -309,27 +168,12 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_instructor",
-			action_cards=[
+			done_state_title=_("Go to Instructors"),
+			done_state_title_route=["List", "Instructor"],
+			help_links=[
 				{
-					"action_name": "Add Instructors",
-					"title": _("You added Instructors"),
-					"done": 1,
-					"actions": [{
-						"label": _('Go to Instructors'),
-						"route": ["List", "Instructor"]
-					}, {
-						"label": _("Import Instructors"),
-						"route": ["data-import-tool", {"doctype": "Instructor"}]
-					}]
-				},
-				{
-					"action_name": "Make a Student Batch",
-					"title": _("Make a Student Batch"),
-					"content": _("Student Batches help you track attendance, assessments and fees for students"),
-					"actions": [{
-						"label": _('Make a Student Batch'),
-						"new_doc": 'Student Batch'
-					}]
+					"label": _('Student Batches'),
+					"route": ["List", "Student Batch"]
 				}
 			]
 		),
@@ -346,20 +190,9 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_room",
-			action_cards=[
-				{
-					"action_name": "Add Rooms",
-					"title": _("You added Rooms"),
-					"done": 1,
-					"actions": [{
-						"label": _('Go to Rooms'),
-						"route": ["List", "Room"]
-					}, {
-						"label": _("Import Rooms"),
-						"route": ["data-import-tool", {"doctype": "Room"}]
-					}]
-				}
-			]
+			done_state_title=_("Go to Rooms"),
+			done_state_title_route=["List", "Room"],
+			help_links=[]
 		),
 		# School slides end
 
@@ -377,29 +210,17 @@ def get_slide_settings():
 			],
 			add_more=1, max_count=3, mandatory_entry=1,
 			submit_method="erpnext.utilities.user_progress_utils.create_users",
-			action_cards=[
+			done_state_title=_("Go to Users"),
+			done_state_title_route=["List", "User"],
+			help_links=[
 				{
-					"action_name": "Add Users",
-					"title": _("You added Users"),
-					"done": 1,
-					"actions": [{
-						"label": _('Go to Users'),
-						"route": ["List", "User"]
-					}, {
-						"label": _("Import Users"),
-						"route": ["data-import-tool", {"doctype": "User"}]
-					}],
-					"help_links": [{
-						"label": _('Learn More'),
-						"link": ["https://erpnext.org/docs/user/manual/en/CRM/customer.html"]
-					}]
+					"label": _('Learn More'),
+					"url": ["https://erpnext.org/docs/user/manual/en/setting-up/users-and-permissions"]
 				},
 				{
-					"action_name": "Watch Users and Permissions",
-					"title": _("Users and Permissions"),
-					"content": _("Add users in an ERPNext account, and manage their permissions"),
+					"label": _('Users and Permissions'),
 					"video_id": "8Slw1hsTmUI"
-				}
+				},
 			]
 		)
 	]
