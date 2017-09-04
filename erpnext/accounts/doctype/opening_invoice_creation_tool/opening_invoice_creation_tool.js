82874c67552bb367bc8282f8d7b2f7628f185fe7
mbauskar <mbauskar@gmail.com>
2017-09-04 13:13:50 +0530

    [minor] added progress bar while creating invoice

--- erpnext/accounts/doctype/opening_invoice_creation_tool/opening_invoice_creation_tool.js
+++ erpnext/accounts/doctype/opening_invoice_creation_tool/opening_invoice_creation_tool.js
@@ -2,13 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Opening Invoice Creation Tool', {
-	setup: function(frm) {
+	setup: (frm) => {
 		frm.set_query('party_type', 'invoices', function(doc, cdt, cdn) {
 			return {
 				filters: {
 					'name': ['in', 'Customer,Supplier']
 				}
 			}
+		});
+	},
+
+	refresh: (frm) => {
+		frm.disable_save();
+		frm.page.set_primary_action(__("Make Invoice"), function() {
+			return frm.call({
+				doc: frm.doc,
+				freeze: true,
+				method: "make_invoices",
+				freeze_message: __("Creating {0} Invoice", [frm.doc.invoice_type]),
+				callback: function(r) {
+				}
+			});
 		});
 	},
 
