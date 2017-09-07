5b05335e89a581ddb74176effd956ba9084fef7c
Rohit Waghchaure <rohitw1991@gmail.com>
2017-09-07 15:14:09 +0530

    added patch and set default fields after completion of setup wizard

--- erpnext/stock/doctype/item_variant_settings/item_variant_settings.py
+++ erpnext/stock/doctype/item_variant_settings/item_variant_settings.py
@@ -3,7 +3,18 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
 from frappe.model.document import Document
 
 class ItemVariantSettings(Document):
-	pass
+	def set_default_fields(self):
+		self.fields = []
+		fields = frappe.get_meta('Item').fields
+		exclude_fields = ["item_code", "item_name", "show_in_website", "show_variant_in_website", "standard_rate"]
+
+		for d in fields:
+			if not d.no_copy and d.fieldname not in exclude_fields and \
+				d.fieldtype not in ['HTML', 'Section Break', 'Column Break', 'Button']:
+				self.append('fields', {
+					'field_name': d.fieldname
+				})
