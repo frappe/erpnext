928edafd8a66fb49b14cb8d8675a6c86b9730788
Prateeksha Singh <pratu16x7@users.noreply.github.com>
2017-09-13 18:41:01 +0530

    Progress fixes (#10710)
    * [progress] sales target slide
    
    * [fix] rerun patch for sales target slide

--- erpnext/setup/setup_wizard/install_fixtures.py
+++ erpnext/setup/setup_wizard/install_fixtures.py
@@ -25,6 +25,10 @@ def install(country=None):
 		{'doctype': "Setup Progress", "actions": [
 			{"action_name": _("Add Company"), "action_doctype": "Company", "min_doc_count": 1, "is_completed": 1,
 				"domains": '[]' },
+			{"action_name": _("Set Sales Target"), "action_doctype": "Company", "min_doc_count": 99,
+				"action_document": frappe.defaults.get_defaults().get("company") or '',
+				"action_field": "monthly_sales_target", "is_completed": 0,
+				"domains": '["Manufacturing", "Services", "Retail", "Distribution"]' },
 			{"action_name": _("Add Customers"), "action_doctype": "Customer", "min_doc_count": 1, "is_completed": 0,
 				"domains": '["Manufacturing", "Services", "Retail", "Distribution"]' },
 			{"action_name": _("Add Suppliers"), "action_doctype": "Supplier", "min_doc_count": 1, "is_completed": 0,
