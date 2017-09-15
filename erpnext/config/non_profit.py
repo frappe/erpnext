68ebed679b03425df90af32dba939635f961de8a
Vishal <vishaldhayagude07@gmail.com>
2017-09-15 22:30:55 +0530

    link member in dynamic link table

---
+++ erpnext/config/non_profit.py
@@ -0,0 +1,37 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Chapter"),
+			"icon": "fa fa-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Chapter",
+					"description": _("Chapter information."),
+				},
+				{
+					"type": "doctype",
+					"name": "Chapter Message",
+					"description": _("Chapter Message."),
+				},
+			]
+		},
+		{
+			"label": _("Membership"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Member",
+					"description": _("Member information."),
+				},
+				{
+					"type": "doctype",
+					"name": "Membership",
+					"description": _("Memebership Details"),
+				},
+			]
+		}
+	]
