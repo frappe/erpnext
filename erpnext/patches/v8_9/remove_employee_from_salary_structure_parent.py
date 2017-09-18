9c334c34a6c0b8d3b6b61a29fb4631959331088d
Saurabh <saurabh6790@gmail.com>
2017-09-18 16:17:13 +0530

    [fix]remove employee field from tab salary structure

--- erpnext/patches/v8_9/remove_employee_from_salary_structure_parent.py
+++ erpnext/patches/v8_9/remove_employee_from_salary_structure_parent.py
@@ -2,4 +2,4 @@ import frappe
 
 def execute():
 	if 'employee' in frappe.db.get_table_columns("Salary Structure"):
-		frappe.db.sql("alter table `tabEmployee` drop column employee")
+		frappe.db.sql("alter table `tabSalary Structure` drop column employee")
