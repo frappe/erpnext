3a75a5e9b57249f56bac60c819b9aaee639746ac
Vishal <vishaldhayagude07@gmail.com>
2017-09-18 16:04:52 +0530

    New Party Type Member Added

--- erpnext/non_profit/doctype/member/member.py
+++ erpnext/non_profit/doctype/member/member.py
@@ -5,6 +5,9 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.contacts.address_and_contact import load_address_and_contact, delete_contact_and_address
 
 class Member(Document):
-	pass
+	def onload(self):
+		"""Load address and contacts in `__onload`"""
+		load_address_and_contact(self)
