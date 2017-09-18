7c2a675bcac3024ce787d07bd6aa8d6b00a7f6fc
Vishal <vishaldhayagude07@gmail.com>
2017-09-18 13:18:24 +0530

    Membership Doctype Added

--- erpnext/non_profit/doctype/membership_type/membership_type.js
+++ erpnext/non_profit/doctype/membership_type/membership_type.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Membership', {
-	onload:function(frm) {
-		frm.add_fetch('membership_type', 'amount', 'amount');
+frappe.ui.form.on('Membership Type', {
+	refresh: function(frm) {
+
 	}
 });
