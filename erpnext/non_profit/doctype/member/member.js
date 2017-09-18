8336bc733c65adcb030086f5bd7fa44636b9dc54
Vishal <vishaldhayagude07@gmail.com>
2017-09-18 13:18:24 +0530

    Membership Doctype Added

--- erpnext/non_profit/doctype/member/member.js
+++ erpnext/non_profit/doctype/member/member.js
@@ -4,5 +4,29 @@
 frappe.ui.form.on('Member', {
 	refresh: function(frm) {
 
+		frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Member'}
+
+		frm.toggle_display(['address_html','contact_html'], !frm.doc.__islocal);
+
+		if(!frm.doc.__islocal) {
+			frappe.contacts.render_address_and_contact(frm);
+
+			// custom buttons
+			frm.add_custom_button(__('Accounting Ledger'), function() {
+				frappe.set_route('query-report', 'General Ledger',
+					{party_type:'Member', party:frm.doc.name});
+			});
+
+			frm.add_custom_button(__('Accounts Receivable'), function() {
+				frappe.set_route('query-report', 'Accounts Receivable', {member:frm.doc.name});
+			});
+
+			// indicator
+			erpnext.utils.set_party_dashboard_indicators(frm);
+
+		} else {
+			frappe.contacts.clear_address_and_contact(frm);
+		}
+
 	}
 });
