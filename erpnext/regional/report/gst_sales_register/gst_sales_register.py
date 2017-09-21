2275cd1f053b2c91194e23204ed836711252bfde
Rohit Waghchaure <rohitw1991@gmail.com>
2017-09-21 18:44:15 +0530

    [fix] Added billing address gstin number in the sales invoice and delivery note as well in the sales gst reports

--- erpnext/regional/report/gst_sales_register/gst_sales_register.py
+++ erpnext/regional/report/gst_sales_register/gst_sales_register.py
@@ -8,6 +8,7 @@ from erpnext.accounts.report.sales_register.sales_register import _execute
 def execute(filters=None):
 	return _execute(filters, additional_table_columns=[
 		dict(fieldtype='Data', label='Customer GSTIN', width=120),
+		dict(fieldtype='Data', label='Billing Address GSTIN', width=140),
 		dict(fieldtype='Data', label='Company GSTIN', width=120),
 		dict(fieldtype='Data', label='Place of Supply', width=120),
 		dict(fieldtype='Data', label='Reverse Charge', width=120),
@@ -16,6 +17,7 @@ def execute(filters=None):
 		dict(fieldtype='Data', label='E-Commerce GSTIN', width=130)
 	], additional_query_columns=[
 		'customer_gstin',
+		'billing_address_gstin',
 		'company_gstin',
 		'place_of_supply',
 		'reverse_charge',
