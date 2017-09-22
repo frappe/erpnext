73a83a472d6f8594e37017b49d00a8df1ca970e6
tunde <tundebabzy@gmail.com>
2017-09-22 11:04:31 +0100

    validation for Payment Terms Template

--- erpnext/accounts/doctype/payment_terms_template/payment_terms_template.py
+++ erpnext/accounts/doctype/payment_terms_template/payment_terms_template.py
@@ -3,8 +3,28 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
+from frappe import _
 
 
 class PaymentTermsTemplate(Document):
-	pass
+	def validate(self):
+		self.validate_invoice_portion()
+		self.validate_credit_days()
+
+	def validate_invoice_portion(self):
+		total_portion = 0
+		for term in self.terms:
+			total_portion += term.invoice_portion
+
+		if flt(total_portion, 2) != 100.00:
+			frappe.msgprint(_('Combined invoice portion must equal 100%'), raise_exception=1, indicator='red')
+
+	def validate_credit_days(self):
+		for term in self.terms:
+			if term.credit_days < 0:
+				frappe.msgprint(_('Credit Days cannot be a negative number'), raise_exception=1, indicator='red')
+
