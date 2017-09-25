d7be6a6af1ce13b53f3f48ffbfb2b27b931c262f
Vishal <vishaldhayagude07@gmail.com>
2017-09-25 16:51:24 +0530

    [new] Calender view Added

--- erpnext/meeting_minutes/doctype/meeting/meeting.js
+++ erpnext/meeting_minutes/doctype/meeting/meeting.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Meeting', {
-	refresh: function(frm) {
+frappe.ui.form.on("Meeting", "refresh", function(frm) {
+	frm.add_custom_button(__("Send Email"), function() {
+		if (frm.doc.status==="Planned") {
+			frappe.call({
+				method: "erpnext.meeting_minutes.api.send_invitation_emails",
+				args: {
+					meeting: frm.doc.name
+				}
+			});
+		}
+	});
 
-	}
+    });
+
+frappe.ui.form.on("Meeting Attendee", {
+	attendee: function(frm, cdt, cdn) {
+		var attendee = frappe.model.get_doc(cdt, cdn);
+		if (attendee.attendee) {
+			// if attendee, get full name
+			frappe.call({
+				method: "erpnext.meeting_minutes.doctype.meeting.meeting.get_full_name",
+				args: {
+					attendee: attendee.attendee
+				},
+				callback: function(r) {
+					frappe.model.set_value(cdt, cdn, "full_name", r.message);
+				}
+			});
+
+		} else {
+			// if no attendee, clear full name
+			frappe.model.set_value(cdt, cdn, "full_name", null);
+		}
+	},
 });
--- erpnext/meeting_minutes/doctype/meeting/meeting.py
+++ erpnext/meeting_minutes/doctype/meeting/meeting.py
@@ -4,7 +4,84 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
+from frappe.model.document import Document
 from frappe.website.website_generator import WebsiteGenerator
 
 class Meeting(WebsiteGenerator):
-	pass
+	_website = frappe._dict(
+		template = "meeting.html",
+		page_title_field = "name",
+	)
+
+	def validate(self):
+		if not self.route:
+			self.route = 'meetings/' + self.scrub(self.name)
+		self.validate_attendees()
+
+	def on_update(self):
+		self.sync_todos()
+
+	def validate_attendees(self):
+		"""Set missing names and warn if duplicate"""
+		found = []
+		for attendee in self.attendees:
+			if not attendee.full_name:
+				attendee.full_name = get_full_name(attendee.attendee)
+
+			if attendee.attendee in found:
+				frappe.throw(_("Attendee {0} entered twice").format(attendee.attendee))
+
+			found.append(attendee.attendee)
+
+	def sync_todos(self):
+		"""Sync ToDos for assignments"""
+		todos_added = [todo.name for todo in
+			frappe.get_all("ToDo",
+				filters={
+					"reference_type": self.doctype,
+					"reference_name": self.name,
+					"assigned_by": ""
+				})
+			]
+
+		for minute in self.minutes:
+			if minute.assigned_to and minute.status=="Open":
+				if not minute.todo:
+					todo = frappe.get_doc({
+						"doctype": "ToDo",
+						"description": minute.description,
+						"reference_type": self.doctype,
+						"reference_name": self.name,
+						"owner": minute.assigned_to
+					})
+					todo.insert()
+
+					minute.db_set("todo", todo.name, update_modified=False)
+
+				else:
+					todos_added.remove(minute.todo)
+
+			else:
+				minute.db_set("todo", None, update_modified=False)
+
+		for todo in todos_added:
+			# remove closed or old todos
+			todo = frappe.get_doc("ToDo", todo)
+			todo.flags.from_meeting = True
+			todo.delete()
+
+	def get_context(self, context):
+		context.parents = [{"name": "meetings", "title": "Meetings"}]
+
+		# context.parents = [dict(label='All Meetings',
+		# route='/meetings', title='All Meetings')]
+
+
+@frappe.whitelist()
+def get_full_name(attendee):
+	user = frappe.get_doc("User", attendee)
+
+	# concatenates by space if it has value
+	return " ".join(filter(None, [user.first_name, user.middle_name, user.last_name]))
+
