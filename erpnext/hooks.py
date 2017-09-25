300b1d86db6ea54aa2e84cafeabe5648b2926840
pratu16x7 <pratu16x7@gmail.com>
2017-09-25 18:05:58 +0530

    [fix] remove from setup_complete

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -27,7 +27,6 @@ doctype_js = {
 # setup wizard
 setup_wizard_requires = "assets/erpnext/js/setup_wizard.js"
 setup_wizard_complete = "erpnext.setup.setup_wizard.setup_wizard.setup_complete"
-setup_wizard_success = "erpnext.setup.setup_wizard.setup_wizard.setup_success"
 setup_wizard_test = "erpnext.setup.setup_wizard.test_setup_wizard.run_setup_wizard_test"
 
 before_install = "erpnext.setup.install.check_setup_wizard_not_completed"
--- erpnext/setup/setup_wizard/setup_wizard.py
+++ erpnext/setup/setup_wizard/setup_wizard.py
@@ -66,10 +66,6 @@ def setup_complete(args=None):
 
 		pass
 
-def setup_success(args=None):
-	company = frappe.db.sql("select name from tabCompany", as_dict=True)[0]["name"]
-	install_country_fixtures(company)
-
 def create_fiscal_year_and_company(args):
 	if (args.get('fy_start_date')):
 		curr_fiscal_year = get_fy_details(args.get('fy_start_date'), args.get('fy_end_date'))
