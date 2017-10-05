c39472fcfa47d3b166a3644c2954a5f63cba1d84
Manas Solanki <manassolanki@gmail.com>
2017-10-05 18:17:35 +0530

    test, patch, docs for student admission

--- erpnext/schools/doctype/student_admission/test_student_admission.js
+++ erpnext/schools/doctype/student_admission/test_student_admission.js
@@ -11,23 +11,29 @@ QUnit.test('Test: Student Admission', function(assert) {
 				{admission_start_date: '2016-04-20'},
 				{admission_end_date: '2016-05-31'},
 				{title: '2016-17 Admissions'},
-				{program: 'Standard Test'},
-				{application_fee: 1000},
-				{naming_series_for_student_applicant: 'AP'},
+				{application_form_route: 'student-applicant'},
 				{introduction: 'Test intro'},
-				{eligibility: 'Test eligibility'}
+				{program_details: [
+					[
+						{'program': 'Standard Test'},
+						{'application_fee': 1000},
+						{'applicant_naming_series': 'AP'},
+					]
+				]}
 			]);
 		},
+		() => cur_frm.save(),
 		() => {
 			assert.ok(cur_frm.doc.academic_year == '2016-17');
 			assert.ok(cur_frm.doc.admission_start_date == '2016-04-20');
 			assert.ok(cur_frm.doc.admission_end_date == '2016-05-31');
 			assert.ok(cur_frm.doc.title == '2016-17 Admissions');
-			assert.ok(cur_frm.doc.program == 'Standard Test', 'Program correctly selected');
-			assert.ok(cur_frm.doc.application_fee == 1000);
-			assert.ok(cur_frm.doc.naming_series_for_student_applicant == 'AP');
+			assert.ok(cur_frm.doc.application_form_route == 'student-applicant');
 			assert.ok(cur_frm.doc.introduction == 'Test intro');
-			assert.ok(cur_frm.doc.eligibility == 'Test eligibility');
+			assert.ok(cur_frm.doc.program_details[0].program == 'Standard Test', 'Program correctly selected');
+			assert.ok(cur_frm.doc.program_details[0].application_fee == 1000);
+			assert.ok(cur_frm.doc.program_details[0].applicant_naming_series == 'AP');
+			assert.ok(cur_frm.doc.route == 'admissions/2016-17-Admissions', "Route successfully set");
 		},
 		() => done()
 	]);
