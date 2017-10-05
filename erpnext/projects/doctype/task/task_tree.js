83b5d5b3e258d2868e57e0ca39c6929e846a736e
Zlash65 <zarrar65@gmail.com>
2017-10-05 15:04:37 +0530

    treeview ui added

---
+++ erpnext/projects/doctype/task/task_tree.js
@@ -0,0 +1,12 @@
+frappe.provide("frappe.treeview_settings")
+
+frappe.treeview_settings['Task'] = {
+	get_tree_nodes: "erpnext.projects.doctype.task.task.get_children",
+	add_tree_node: "erpnext.projects.doctype.task.task.add_node",
+	get_tree_root: false,
+	root_label: "Tasks",
+	ignore_fields:["parent_task"],
+	onload: function(me){
+		me.make_tree()
+	}
+}
