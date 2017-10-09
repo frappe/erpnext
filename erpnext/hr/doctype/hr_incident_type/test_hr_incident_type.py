faf38a13ccbb3d0cb444a6f24bd40899b0f35799
Paulo Almeida <almeidapaulopt@gmail.com>
2017-10-09 10:25:20 +0000

    hr incident type

---
+++ erpnext/hr/doctype/hr_incident_type/test_hr_incident_type.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+test_records = frappe.get_test_records('HR Incident Type')
