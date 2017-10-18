a9feddbb79e3938d5d3b58ad04c816d41deba83e
Nabin Hait <nabinhait@gmail.com>
2017-10-18 10:52:48 +0530

    Testcase fixes (#11238)
    * Restaurant test records fixed
    
    * Fixes for test cases

--- erpnext/restaurant/doctype/restaurant/test_restaurant.py
+++ erpnext/restaurant/doctype/restaurant/test_restaurant.py
@@ -8,9 +8,9 @@ import unittest
 
 test_records = [
 	dict(doctype='Restaurant', name='Test Restaurant 1', company='_Test Company 1',
-		invoice_series_prefix='Test-Rest-1-Inv-', defaut_customer='_Test Customer 1'),
+		invoice_series_prefix='Test-Rest-1-Inv-', default_customer='_Test Customer 1'),
 	dict(doctype='Restaurant', name='Test Restaurant 2', company='_Test Company 1',
-		invoice_series_prefix='Test-Rest-2-Inv-', defaut_customer='_Test Customer 1'),
+		invoice_series_prefix='Test-Rest-2-Inv-', default_customer='_Test Customer 1'),
 ]
 
 class TestRestaurant(unittest.TestCase):
