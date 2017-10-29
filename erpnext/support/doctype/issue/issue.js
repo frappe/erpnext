77c7507431c8515aeb1a1fc513ae9ad27dbd53e2
ci2014 <christoph.isik@wpci.de>
2017-10-29 23:34:00 +0100

    Update issue.js
    Make the close / reopen button more flexible for customized user status.
    
    For example, if the user has several status, it will now show the Close button every time the ticket is not closed! It will show Reopen only, if the ticket is closed, and not if it's on hold for example.
    
    This will make it more flexible.

--- erpnext/support/doctype/issue/issue.js
+++ erpnext/support/doctype/issue/issue.js
@@ -4,7 +4,7 @@ frappe.ui.form.on("Issue", {
 	},
 
 	"refresh": function(frm) {
-		if(frm.doc.status==="Open") {
+		if(frm.doc.status!=="Closed") {
 			frm.add_custom_button(__("Close"), function() {
 				frm.set_value("status", "Closed");
 				frm.save();
