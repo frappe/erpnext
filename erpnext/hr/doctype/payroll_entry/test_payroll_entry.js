d73dd056d63b1d337081cffd61331d904c150871
Shreya <shreyashah115@gmail.com>
2017-10-30 12:41:48 +0530

    test case

--- erpnext/hr/doctype/payroll_entry/test_payroll_entry.js
+++ erpnext/hr/doctype/payroll_entry/test_payroll_entry.js
@@ -1,18 +1,24 @@
-QUnit.module('HR')
+QUnit.module('HR');
 
 QUnit.test("test: Payroll Entry", function (assert) {
 	assert.expect(5);
 	let done = assert.async();
 
 	frappe.run_serially([
-		() => {	
+		() => {
 			return frappe.tests.make('Payroll Entry', [
 				{company: 'For Testing'},
 				{posting_date: frappe.datetime.add_days(frappe.datetime.nowdate(), 0)},
 				{payroll_frequency: 'Monthly'},
-				// {start_date: },
 				{cost_center: 'Main - '+frappe.get_abbr(frappe.defaults.get_default("Company"))}
 			]);
+		},
+
+		() => frappe.timeout(1),
+		() => {
+			assert.equal(cur_frm.doc.company, 'For Testing');
+			assert.equal(cur_frm.doc.posting_date, frappe.datetime.add_days(frappe.datetime.nowdate(), 0));
+			assert.equal(cur_frm.doc.cost_center, 'Main - FT');
 		},
 
 		() => frappe.click_button('Submit'),
@@ -20,19 +26,13 @@ QUnit.test("test: Payroll Entry", function (assert) {
 		() => frappe.click_button('Yes'),
 		() => frappe.timeout(2),
 
-		() => {
-			assert.equal(cur_frm.doc.company, 'For Testing');
-			assert.equal(cur_frm.doc.posting_date, frappe.datetime.add_days(frappe.datetime.nowdate(), 0));
-			assert.equal(cur_frm.doc.cost_center, 'Main - FT');
-		},
-
 		() => frappe.click_button('View Salary Slip'),
 		() => frappe.timeout(2),
 		() => assert.equal(cur_list.data[0].docstatus, 0),
 
-		() => frappe.set_route('Form', 'Payroll Entry', 'Payroll 0041'),
+		() => frappe.set_route('Form', 'Payroll Entry', 'Payroll 0001'),
 		() => frappe.click_button('Submit Salary Slip'),
-		() => frappe.timeout(2),
+		() => frappe.timeout(3),
 
 		() => frappe.click_button('Close'),
 		() => frappe.timeout(1),
@@ -40,7 +40,7 @@ QUnit.test("test: Payroll Entry", function (assert) {
 		() => frappe.click_button('View Salary Slip'),
 		() => frappe.timeout(2),
 		() => {
-			 assert.ok(cur_list.data[0].docstatus == 1, "Salary slip submitted");
+			assert.ok(cur_list.data[0].docstatus == 1, "Salary slip submitted");
 		},
 
 		() => done()
