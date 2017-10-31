a6a302f8a6134ff33866f7c13acadc1211143e86
Vishal <vishaldhayagude07@gmail.com>
2017-10-31 13:27:49 +0530

    [new] Grant Application Web Form Added

--- erpnext/non_profit/doctype/grant_application/grant_application.py
+++ erpnext/non_profit/doctype/grant_application/grant_application.py
@@ -3,10 +3,34 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 from frappe.contacts.address_and_contact import load_address_and_contact
 
-class GrantApplication(Document):
+class GrantApplication(WebsiteGenerator):
+	_website = frappe._dict(
+		condition_field = "published",
+	)
+
+	def validate(self):
+		if not self.route:
+			self.route = 'grant-application/' + self.scrub(self.name)
+
 	def onload(self):
 		"""Load address and contacts in `__onload`"""
 		load_address_and_contact(self)
+
+
+	def get_context(self, context):
+		context.no_cache = True
+		context.parents = [dict(label='View All ',
+			route='grant-application', title='View All')]
+
+
+
+def get_list_context(context):
+	context.allow_guest = True
+	context.no_cache = True
+	context.no_breadcrumbs = True
+	context.order_by = 'creation desc'
+	context.introduction ='<div>Grant Application List</div><br><a class="btn btn-primary" href="/my-jobs?new=1">Apply for new Grant Application</a>'
