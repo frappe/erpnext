609cc2c0693e0fffa8addb725017aa89c4ae5ad9
Manas Solanki <manas@erpnext.com>
2017-11-08 14:55:54 +0530

    remove the print option from the tools (#11484)
    * remove the print option for the tools
    
    * better student search queries and minor fixes in the student group

--- erpnext/schools/doctype/assessment_result/assessment_result.js
+++ erpnext/schools/doctype/assessment_result/assessment_result.js
@@ -7,23 +7,25 @@ cur_frm.add_fetch("assessment_plan", "maximum_assessment_score", "maximum_score"
 
 frappe.ui.form.on("Assessment Result", {
 	assessment_plan: function(frm) {
-		frappe.call({
-			method: "erpnext.schools.api.get_assessment_details",
-			args: {
-				assessment_plan: frm.doc.assessment_plan
-			},
-			callback: function(r) {
-				if (r.message) {
-					frm.doc.details = [];
-					$.each(r.message, function(i, d) {
-						var row = frappe.model.add_child(frm.doc, "Assessment Result Detail", "details");
-						row.assessment_criteria = d.assessment_criteria;
-						row.maximum_score = d.maximum_score;
-					});
+		if (frm.doc.assessment_plan) {
+			frappe.call({
+				method: "erpnext.schools.api.get_assessment_details",
+				args: {
+					assessment_plan: frm.doc.assessment_plan
+				},
+				callback: function(r) {
+					if (r.message) {
+						frm.doc.details = [];
+						$.each(r.message, function(i, d) {
+							var row = frappe.model.add_child(frm.doc, "Assessment Result Detail", "details");
+							row.assessment_criteria = d.assessment_criteria;
+							row.maximum_score = d.maximum_score;
+						});
+					}
+					refresh_field("details");
 				}
-				refresh_field("details");
-			}
-		});
+			});
+		}
 	}
 });
 
