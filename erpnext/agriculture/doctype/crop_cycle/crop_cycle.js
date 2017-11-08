88d9f50d6ff19f467a25a30a2430de3f3f40c414
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-11-08 20:38:08 +0530

    Changed the Crop Cycle Doctype - Deleted the "Crop Cycly Task" doctype and its link from "Crop Cycle" - Creation of a new Project with the same name as the Crop Cycle on creation of a new Crop Cycle - Creation of all the tasks imported from Agriculture Task doctype of the Crop in the Crop Cycle

--- erpnext/agriculture/doctype/crop_cycle/crop_cycle.js
+++ erpnext/agriculture/doctype/crop_cycle/crop_cycle.js
@@ -4,35 +4,50 @@
 frappe.ui.form.on('Crop Cycle', {
 	onload: function(frm) {
 	},
-	crop: function(frm) {
-		if (frm.doc.crop != undefined) {
+	validate: (frm) => {
+		if (frm.doc.__islocal){
 			frappe.model.with_doc("Crop", frm.doc.crop, function() {
 				let crop = frappe.model.get_doc("Crop", frm.doc.crop);
-				$.each(crop.agriculture_task, function(index, row){
-					frm.call("create_tasks", {
-						subject: row.subject + frm.doc.name,
-						day: row.day,
-						holiday_management: row.holiday_management,
-						priority: row.priority
+				console.log(crop);
+				frappe.run_serially([
+					() => frm.call("create_project", {
+						period: crop.period, 
+						crop_tasks: crop.agriculture_task
 					}, (r) => {
-						let d = frm.add_child("crop_cycle_task");
-						d.task = r.message;
-						d.land_unit = frm.doc.land_unit;
-						frm.refresh_field('crop_cycle_task');
-					});
-				});
+						frm.doc.project = r.message;
+						frm.refresh_field("project");
+					}),
+					// () => $.each(crop.agriculture_task, function(index, row){
+					// 	frm.call("create_task", {
+					// 		subject: `${row.subject} ${frm.doc.crop}`,
+					// 		day: row.day,
+					// 		project: frm.doc.project,
+					// 		holiday_management: row.holiday_management,
+					// 		priority: row.priority
+					// 	});
+					// })
+				]);
 			});
 		}
 	}
+	// crop: function(frm) {
+	// 	if (frm.doc.crop != undefined) {
+	// 		frappe.model.with_doc("Crop", frm.doc.crop, function() {
+
+	// 			$.each(crop.agriculture_task, function(index, row){
+	// 				frm.call("create_tasks", {
+	// 					subject: row.subject + frm.doc.name,
+	// 					day: row.day,
+	// 					holiday_management: row.holiday_management,
+	// 					priority: row.priority
+	// 				}, (r) => {
+	// 					let d = frm.add_child("crop_cycle_task");
+	// 					d.task = r.message;
+	// 					d.land_unit = frm.doc.land_unit;
+	// 					frm.refresh_field('crop_cycle_task');
+	// 				});
+	// 			});
+	// 		});
+	// 	}
+	// }
 });
-
-// Add crop name in title
-// Add land unit as text in details
-// Tree view when selecting the land unit
-// how to handle Intercropping? both should be able to access the same land unit, 
-	// with a validation warning (only on 2nd intercrop) if land unit is already in use, 
-	// and denote the % of land being used, initially should be 100% for 1 crop
-
-
-// On choosing the harvest date, ERPNext should be able to sugges the start date and crop cycle
-// How to map the, Banana crop cycle? Should a convertion from mother plant to child plant be consifdered as a cycle?  
