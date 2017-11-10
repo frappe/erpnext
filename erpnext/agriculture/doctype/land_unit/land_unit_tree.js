8105478c25a770bcacc1412854e69712c33bac82
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-11-10 17:57:33 +0530

    Revert "land_unit_tree.js modified"

--- erpnext/agriculture/doctype/land_unit/land_unit_tree.js
+++ erpnext/agriculture/doctype/land_unit/land_unit_tree.js
@@ -1,11 +1,4 @@
 frappe.treeview_settings["Land Unit"] = {
-	fields:[
-		{fieldtype:'Check', fieldname:'is_container',
-			label:__('Is Container ?')}, 
-		{fieldtype:'Float', fieldname:'latitude',
-			label:__('Latitude')}, 
-		{fieldtype:'Float', fieldname:'longitude',
-			label:__('Longitude')}
-	],
+	fields: ["is_container", "latitude", "longitude"],
 	ignore_fields:["parent_land_unit"]
 }
