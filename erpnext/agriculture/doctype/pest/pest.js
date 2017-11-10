81c9f890d036b82e29fea0e9bfd2e42b63088b5d
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-11-10 12:09:40 +0530

    [Agriculture Module] fixed Pest doctype not saving issue

--- erpnext/agriculture/doctype/pest/pest.js
+++ erpnext/agriculture/doctype/pest/pest.js
@@ -3,9 +3,9 @@
 
 frappe.ui.form.on('Pest', {
 	validate: (frm) => {
-		frm.doc.period = frm.doc.agriculture_task.valueOf().reduce((greatest, d) => {
+		frm.doc.treatment_period = frm.doc.treatment_task.valueOf().reduce((greatest, d) => {
 			return greatest>d.end_day?greatest:d.end_day;
 		}, 1);
-		frm.refresh_field("period");
+		frm.refresh_field("treatment_period");
 	}
 });
