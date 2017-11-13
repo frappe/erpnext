f23fdbfabb77b0d4fd8c0519f7850343e23ec629
Gupteshwar <gupteshwar@indictranstech.com>
2017-11-13 15:42:10 +0530

    [FedEx Integration] # Added DocTypes/masters FedEx Settings, Shipper, Shipping Plan (Package Details). # Company master added TIN # Address added (Office) Open and Close Time. # Delivery note Shipping Plan link.

---
+++ erpnext/logistic/doctype/fedex_settings/fedex_settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from erpnext.erpnext.logistic.utils import create_shipper
+
+class FedExSettings(Document):
+	def validate_credentials(self):
+		pass
+
+	def on_upate(self):
+		create_shipper("Fedex")
