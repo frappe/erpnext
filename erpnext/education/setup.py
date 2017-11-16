92935c41cf648e2733c275a902a428f9b82c8612
Manas Solanki <manassolanki@gmail.com>
2017-11-16 15:31:45 +0530

    added the setup file for the fixtures

--- erpnext/education/setup.py
+++ erpnext/education/setup.py
@@ -2,29 +2,21 @@ from __future__ import unicode_literals
 import frappe
 
 from frappe import _
-
-def setup_healthcare():
-	if frappe.db.exists('Medical Department', 'Cardiology'):
+def setup_education():
+	if frappe.db.exists('Academic Year', '2015-16'):
 		# already setup
 		return
-	create_medical_departments()
-	create_antibiotics()
-	create_test_uom()
-	create_duration()
-	create_dosage()
-	create_healthcare_item_groups()
-	create_lab_test_items()
-	create_lab_test_template()
-	create_sensitivity()
-	except frappe.DuplicateEntryError:
+	create_academic_sessions()
 
-def create_sensitivity():
-	records = [
-		{"doctype": "Sensitivity", "sensitivity": _("Low Sensitivity")},
-		{"doctype": "Sensitivity", "sensitivity": _("High Sensitivity")},
-		{"doctype": "Sensitivity", "sensitivity": _("Moderate Sensitivity")},
-		{"doctype": "Sensitivity", "sensitivity": _("Susceptible")},
-		{"doctype": "Sensitivity", "sensitivity": _("Resistant")},
-		{"doctype": "Sensitivity", "sensitivity": _("Intermediate")}
+def create_academic_sessions():
+	data = [
+		{doctype='Academic Year', academic_year_name='2015-16'},
+		{doctype='Academic Year', academic_year_name='2016-17'},
+		{doctype='Academic Year', academic_year_name='2017-18'},
+		{doctype='Academic Year', academic_year_name='2018-19'},
+		{doctype='Academic Term', academic_year='2016-17', term_name='Semester 1'},
+		{doctype='Academic Term', academic_year='2016-17', term_name='Semester 2'},
+		{doctype='Academic Term', academic_year='2017-18', term_name='Semester 1'},
+		{doctype='Academic Term', academic_year='2017-18', term_name='Semester 2'}
 	]
-	insert_record(records)
+	insert_record(data)
