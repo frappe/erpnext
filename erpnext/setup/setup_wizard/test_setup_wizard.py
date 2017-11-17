e1b862931359b953dcc225bce0cd5cfb0f17d514
Prateeksha Singh <pratu16x7@users.noreply.github.com>
2017-11-17 20:08:45 +0530

    Fix setup wiz test (#11640)
    * increase timeout in setup wiz test
    
    * enable profile slide

--- erpnext/setup/setup_wizard/test_setup_wizard.py
+++ erpnext/setup/setup_wizard/test_setup_wizard.py
@@ -30,16 +30,17 @@ def run_setup_wizard_test():
 	time.sleep(1)
 	driver.click(".next-btn")
 
-	# # Profile slide
-	# driver.set_field("full_name", "Great Tester")
-	# driver.set_field("email", "great@example.com")
-	# driver.set_field("password", "test")
-	# driver.wait_for_ajax(True)
-	# time.sleep(1)
-	# driver.click(".next-btn")
-	# time.sleep(1)
+	# Profile slide
+	driver.set_field("full_name", "Great Tester")
+	driver.set_field("email", "great@example.com")
+	driver.set_field("password", "test")
+	driver.wait_for_ajax(True)
+	time.sleep(1)
+	driver.click(".next-btn")
+	time.sleep(1)
 
 	# domain slide
+	# time.sleep(3)
 	driver.set_select("domain", "Manufacturing")
 	time.sleep(1)
 	driver.click(".next-btn")
