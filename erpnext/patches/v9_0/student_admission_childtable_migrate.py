a52223c97473e174eb49198c0a2e4b54936547b7
Saurabh <saurabh6790@gmail.com>
2017-11-17 19:06:08 +0530

    [fix] patch fixes to setup patch tester on travis

--- erpnext/patches/v9_0/student_admission_childtable_migrate.py
+++ erpnext/patches/v9_0/student_admission_childtable_migrate.py
@@ -5,8 +5,8 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	frappe.reload_doc('schools', 'doctype', 'Student Admission Program')
-	frappe.reload_doctype('Student Admission')
+	frappe.reload_doc('schools', 'doctype', 'student_admission_program')
+	frappe.reload_doc('schools', 'doctype', 'student_admission')
 
 	if "program" not in frappe.db.get_table_columns("Student Admission"):
 		return
