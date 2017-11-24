386e13dfcc9a1a22f64e61814f75005235a6d4a1
Shreya <shreyashah115@gmail.com>
2017-11-24 16:03:54 +0530

    patch added

---
+++ erpnext/patches/v9_2/set_item_name_in_production_order.py
@@ -0,0 +1,10 @@
+import frappe
+
+def execute():
+
+	frappe.db.sql("""
+		update `tabBOM Item` bom, `tabProduction Order Item` po_item
+		set po_item.item_name = bom.item_name,
+		po_item.description = bom.description
+		where po_item.item_code = bom.item_code
+	""")
