e3a57609f0c5845bb916ab3f98f91a47cbfe9662
Vishal <vishaldhayagude07@gmail.com>
2017-11-24 13:14:23 +0530

    [fix] Non Profit Domain clean up after review

--- erpnext/non_profit/doctype/chapter_message/chapter_message.py
+++ erpnext/non_profit/doctype/chapter_message/chapter_message.py
@@ -11,30 +11,20 @@ class ChapterMessage(Document):
 	def validate(self):
 		chapter = frappe.get_doc('Chapter', self.chapter)
 		if frappe.session.user != chapter.chapter_head:
-			frappe.throw(_('You are not athourize to send message for this Chapter.'))
+			frappe.throw(_('You are not athourized to send message for this Chapter.'))
 
 	def on_update(self):
 		chapter = frappe.get_doc('Chapter', self.chapter)
 		recipients = [d.user for d in chapter.members]
-		message = self.message
-		message += "Chapter Title: " + self.chapter
-		# chapter_title = self.chapter
 
 		frappe.sendmail(recipients = recipients,
+				sender=frappe.session.user,
+				subject = self.subject,
 				message = self.message,
-				subject = self.subject)
-				# reference_doctype=self.doctype, reference_name=self.name)
+				reference_doctype=chapter.doctype,
+				reference_name=chapter.name
+				)
 
 
-def get_members_emails(chapterTitle):
+		frappe.msgprint(_("Invitation Sent"))
 
-	members_list = frappe.get_all('User', fields=['email'],
-		filters={'chapter.title': chapterTitle})
-
-	out = []
-	for e in members_list:
-		if e.email:
-				# don't add if holiday
-			out.append(e.email)
-			print out
-	return out
