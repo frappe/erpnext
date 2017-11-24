a72a3bd536b6ab334b71f09012804b03fe63e7d0
Vishal <vishaldhayagude07@gmail.com>
2017-11-24 17:24:41 +0530

    [new] Chapter Module clean up after review

--- erpnext/non_profit/doctype/chapter/chapter.py
+++ erpnext/non_profit/doctype/chapter/chapter.py
@@ -5,18 +5,20 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
-from frappe.model.document import Document
-from frappe import _
 
 class Chapter(WebsiteGenerator):
+	_website = frappe._dict(
+		condition_field = "published",
+	)
+
 	def get_context(self, context):
 		context.no_cache = True
+		context.parents = [dict(label='View All Chapters',
+			route='chapters', title='View Chapters')]
 
 	def validate(self):
-		chapter_head = self.chapter_head
-		chapter = frappe.get_all('Chapter', filters={'published': True}, fields=['chapter_head'])
 		if not self.route:		#pylint: disable=E0203
-			self.route = 'chapter/' + self.scrub(self.name)
+			self.route = 'chapters/' + self.scrub(self.name)
 
 	def enable(self):
 		chapter = frappe.get_doc('Chapter', frappe.form_dict.name)
