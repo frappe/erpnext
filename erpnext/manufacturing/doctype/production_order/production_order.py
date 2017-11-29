91f9c6fea1300f779b2891137fc78a56b83590c0
Nabin Hait <nabinhait@gmail.com>
2017-11-29 13:54:22 +0530

    Update production_order.py

--- erpnext/manufacturing/doctype/production_order/production_order.py
+++ erpnext/manufacturing/doctype/production_order/production_order.py
@@ -639,9 +639,12 @@ def stop_unstop(production_order, status):
 
 @frappe.whitelist()
 def query_sales_order(production_item):
-	out = frappe.db.sql_list("select distinct so.name from `tabSales Order` so, `tabSales Order Item` so_item\
-		where so_item.parent=so.name and so_item.item_code='{0}'\
-		union\
-		select distinct so.name from `tabSales Order` so, `tabPacked Item` pi_item\
-		where pi_item.parent=so.name and pi_item.item_code='{0}'".format(production_item))
+	out = frappe.db.sql_list("""
+		select distinct so.name from `tabSales Order` so, `tabSales Order Item` so_item
+		where so_item.parent=so.name and so_item.item_code=%s and so.docstatus=1
+	union
+		select distinct so.name from `tabSales Order` so, `tabPacked Item` pi_item
+		where pi_item.parent=so.name and pi_item.item_code=%s and so.docstatus=1
+	""", (production_item, production_item))
+	
 	return out
