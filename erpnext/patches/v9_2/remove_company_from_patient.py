9cef161af21f3d13be1fab7e9cf8f31207088716
Jamsheer <kuttypjamsheer@gmail.com>
2017-12-01 16:10:39 +0530

    Fix Patch - remove company from patient (#11819)

--- erpnext/patches/v9_2/remove_company_from_patient.py
+++ erpnext/patches/v9_2/remove_company_from_patient.py
@@ -1,5 +1,6 @@
 import frappe
 
 def execute():
-	if 'company' in frappe.db.get_table_columns("Patient"):
-		frappe.db.sql("alter table `tabPatient` drop column company")
+	if frappe.db.exists("DocType", "Patient"):
+		if 'company' in frappe.db.get_table_columns("Patient"):
+			frappe.db.sql("alter table `tabPatient` drop column company")
