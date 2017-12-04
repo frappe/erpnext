21d5a0f115447c4f88f434a9354f71ed22beac9c
Vishal <vishaldhayagude07@gmail.com>
2017-12-04 11:57:58 +0530

    [minor] changes added in member and membership doctype

--- erpnext/non_profit/doctype/member/member.py
+++ erpnext/non_profit/doctype/member/member.py
@@ -6,7 +6,20 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 from frappe.contacts.address_and_contact import load_address_and_contact
 
+STANDARD_USERS = ("Guest", "Administrator")
+
 class Member(Document):
 	def onload(self):
 		"""Load address and contacts in `__onload`"""
 		load_address_and_contact(self)
+
+
+	def validate(self):
+		if self.name not in STANDARD_USERS:
+			self.validate_email_type(self.email)
+			self.validate_email_type(self.name)
+
+
+	def validate_email_type(self, email):
+		from frappe.utils import validate_email_add
+		validate_email_add(email.strip(), True)
