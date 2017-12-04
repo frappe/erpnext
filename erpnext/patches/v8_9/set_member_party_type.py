5d28d3e95df2a23ae79a37c721b319c820c720b3
Vishal <vishaldhayagude07@gmail.com>
2017-12-04 12:43:13 +0530

    [fix] Minor changes in patch set member as party type

--- erpnext/patches/v8_9/set_member_party_type.py
+++ erpnext/patches/v8_9/set_member_party_type.py
@@ -2,6 +2,7 @@ import frappe
 
 def execute():
 	if not frappe.db.exists("Party Type", "Member"):
+		frappe.reload_doc("non_profit", "doctype", "member")
 		party = frappe.new_doc("Party Type")
 		party.party_type = "Member"
 		party.save()
