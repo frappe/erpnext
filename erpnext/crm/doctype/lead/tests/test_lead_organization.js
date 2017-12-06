74fa478d7aec3a5cbc5c05926460c33a0b56b309
Zlash65 <zarrar65@gmail.com>
2017-12-06 13:00:42 +0530

    minor fixes with link and route

--- erpnext/crm/doctype/lead/tests/test_lead_organization.js
+++ erpnext/crm/doctype/lead/tests/test_lead_organization.js
@@ -1,7 +1,7 @@
 QUnit.module("sales");
 
 QUnit.test("test: lead", function (assert) {
-	assert.expect(6);
+	assert.expect(5);
 	let done = assert.async();
 	let lead_name = frappe.utils.get_random(10);
     frappe.run_serially([
@@ -37,16 +37,16 @@ QUnit.test("test: lead", function (assert) {
 		() => frappe.set_control('first_name', 'John'),
 		() => frappe.set_control('last_name', 'Doe'),
 		() => cur_frm.save(),
-		() => frappe.timeout(3),
-		() => assert.equal(frappe.get_route()[1], 'Lead',
-			'back to lead form'),
+        () => frappe.timeout(3),
+        () => frappe.set_route('Form', 'Lead', cur_frm.doc.links[0].link_name),
+        () => frappe.timeout(1),
 		() => frappe.click_link('Address & Contact'),
 		() => assert.ok($('.address-box').text().includes('John'),
 			'contact is seen in contact box'),
 
 		// make customer
 		() => frappe.click_button('Make'),
-		() => frappe.click_link('customer'),
+		() => frappe.click_link('Customer'),
 		() => frappe.timeout(2),
 		() => assert.equal(cur_frm.doc.lead_name, frappe.lead_name,
 			'lead name correctly mapped'),
