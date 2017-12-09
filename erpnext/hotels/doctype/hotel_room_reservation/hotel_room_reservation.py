ea469a8586b2fe8190d93fe42ede2961a3309eaf
Revant Nandgaonkar <revant.one@gmail.com>
2017-12-09 10:42:30 +0530

    Hotel Room validation for overbooking

--- erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.py
+++ erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.py
@@ -9,7 +9,50 @@ from frappe import _
 from frappe.utils import date_diff, add_days, flt
 
 class HotelRoomReservation(Document):
-	pass
+	def validate(self):
+		self.total_rooms = {}
+		self.rooms_booked = {}
+		self.validate_availability()
+
+	def validate_availability(self):
+		for d in self.items:
+			if not d.item in self.rooms_booked:
+				self.rooms_booked[d.item] = 0
+			for i in xrange(date_diff(self.to_date, self.from_date)):
+				day = add_days(self.from_date, i)
+				rooms_booked = self.get_rooms_booked(d.item, day) + d.qty + self.rooms_booked.get(d.item)
+				total_rooms = self.get_total_rooms(d.item)
+				if total_rooms < rooms_booked:
+					frappe.throw(_("Hotel Rooms of type {0} are unavailable on {1}".format(d.item,
+						frappe.format(day, dict(fieldtype="Date")))))
+				self.rooms_booked[d.item] += rooms_booked
+
+	def get_rooms_booked(self, item, day):
+		return frappe.db.sql("""
+			select sum(item.qty)
+			from
+				`tabHotel Room Reservation Item` item,
+				`tabHotel Room Reservation` reservation
+			where
+				item.parent = reservation.name
+				and item.item = %s
+				and reservation.docstatus = 1
+				and reservation.name != %s
+				and %s between reservation.from_date
+					and reservation.to_date""", (item, self.name, day))[0][0]
+
+	def get_total_rooms(self, item):
+		if not item in self.total_rooms:
+			self.total_rooms[item] = frappe.db.sql("""
+				select count(*)
+				from
+					`tabHotel Room Package` package
+				inner join
+					`tabHotel Room` room on package.hotel_room_type = room.hotel_room_type
+				where
+					package.item = %s""", item)[0][0]
+
+		return self.total_rooms[item]
 
 @frappe.whitelist()
 def get_room_rate(hotel_room_reservation):
@@ -21,7 +64,6 @@ def get_room_rate(hotel_room_reservation):
 		for i in xrange(date_diff(doc.get("to_date"), doc.get("from_date"))):
 			day = add_days(doc.get("from_date"), i)
 			if not d.get("item"):
-				print("-"*40,d)
 				continue
 			day_rate = frappe.db.sql("""
 				select 
