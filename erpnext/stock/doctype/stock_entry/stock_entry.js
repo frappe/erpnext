3ca00c7143dbf1547c800c8fbc3a00d08b3059be
KanchanChauhan <angel143.dr@gmail.com>
2017-12-11 14:59:19 +0530

    Items should not be used from Sample Retention warehouse (#11905)

--- erpnext/stock/doctype/stock_entry/stock_entry.js
+++ erpnext/stock/doctype/stock_entry/stock_entry.js
@@ -4,7 +4,6 @@ frappe.provide("erpnext.stock");
 
 frappe.ui.form.on('Stock Entry', {
 	setup: function(frm) {
-
 		frm.set_query('production_order', function() {
 			return {
 				filters: [
@@ -12,6 +11,26 @@ frappe.ui.form.on('Stock Entry', {
 					['Production Order', 'qty', '>','`tabProduction Order`.produced_qty'],
 					['Production Order', 'company', '=', frm.doc.company]
 				]
+			}
+		});
+
+		frappe.db.get_value('Stock Settings', {name: 'Stock Settings'}, 'sample_retention_warehouse', (r) => {
+			if (r.sample_retention_warehouse) {
+				var filters = [
+							["Warehouse", 'company', '=', frm.doc.company],
+							["Warehouse", "is_group", "=",0],
+							['Warehouse', 'name', '!=', r.sample_retention_warehouse]
+						]
+				frm.set_query("from_warehouse", function() {
+					return {
+						filters: filters
+					};
+				});
+				frm.set_query("s_warehouse", "items", function() {
+					return {
+						filters: filters
+					};
+				});
 			}
 		});
 
@@ -39,7 +58,6 @@ frappe.ui.form.on('Stock Entry', {
 			}
 		});
 	},
-
 	refresh: function(frm) {
 		if(!frm.doc.docstatus) {
 			frm.add_custom_button(__('Make Material Request'), function() {
