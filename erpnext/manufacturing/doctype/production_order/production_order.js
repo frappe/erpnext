e04986735310d6590c56511396df49be30758340
Nabin Hait <nabinhait@gmail.com>
2017-12-12 13:22:48 +0530

    Production Order fixes (#11951)
    * Production Order from Sales Order fixes: Validate SO and set required items on save
    
    * Codacy fixes

--- erpnext/manufacturing/doctype/production_order/production_order.js
+++ erpnext/manufacturing/doctype/production_order/production_order.js
@@ -157,6 +157,7 @@ frappe.ui.form.on("Production Order", {
 					item: frm.doc.production_item,
 					project: frm.doc.project
 				},
+				freeze: true,
 				callback: function(r) {
 					if(r.message) {
 						erpnext.in_production_item_onchange = true;
@@ -184,6 +185,7 @@ frappe.ui.form.on("Production Order", {
 		return frm.call({
 			doc: frm.doc,
 			method: "get_items_and_operations_from_bom",
+			freeze: true,
 			callback: function(r) {
 				if(r.message["set_scrap_wh_mandatory"]){
 					frm.toggle_reqd("scrap_warehouse", true);
