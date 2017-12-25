ed13b3c19a3e0ce044606a44c234c40a843d80c5
Nabin Hait <nabinhait@gmail.com>
2017-12-25 15:11:56 +0530

    Fixed minor issue

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -610,7 +610,7 @@ def get_outstanding_invoices(party_type, party, account, condition=None):
 				or (voucher_type not in ('Journal Entry', 'Payment Entry')))
 		group by voucher_type, voucher_no
 		having (invoice_amount - payment_amount) > 0.005
-		order by posting_date, name, due_date""".format(
+		order by posting_date, name""".format(
 			dr_or_cr=dr_or_cr,
 			invoice = invoice,
 			payment_dr_or_cr=payment_dr_or_cr,
