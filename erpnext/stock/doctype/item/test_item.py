66bb6a249f63182e0ed9e83b81b16e990c77a0e5
Vinayak Jethe <jethevinayak@gmail.com>
2017-12-28 20:23:55 +0530

    Added tests for commit 262aab5 and tearDown in test_item.py

--- erpnext/stock/doctype/item/test_item.py
+++ erpnext/stock/doctype/item/test_item.py
@@ -44,6 +44,16 @@ class TestItem(unittest.TestCase):
 	def setUp(self):
 		frappe.flags.attribute_values = None
 
+	def tearDown(self):
+		frappe.delete_doc("Item", "_Test Item 999")
+		frappe.delete_doc("Item Price", 
+			{
+				"item_code": "_Test Item 999",
+				"price_list": (frappe.db.get_single_value('Selling Settings', 'selling_price_list')
+				or frappe.db.get_value('Price List', _('Standard Selling')))
+			}
+		)
+
 	def get_item(self, idx):
 		item_code = test_records[idx].get("item_code")
 		if not frappe.db.exists("Item", item_code):
@@ -280,6 +290,40 @@ class TestItem(unittest.TestCase):
 			item_doc = frappe.get_doc('Item', item_code)
 			item_doc.has_variants = 1
 			self.assertRaises(StockExistsForTemplate, item_doc.save)
+
+	def test_standard_selling_rate_item_price(self):
+		def fetch_item_price_list_rate():
+			return frappe.db.get_value("Item Price", 
+			{
+				"item_code": "_Test Item 999",
+				"price_list": (frappe.db.get_single_value('Selling Settings', 'selling_price_list')
+				or frappe.db.get_value('Price List', _('Standard Selling')))
+			}
+			, "price_list_rate")
+
+		test_item = frappe.new_doc("Item")
+
+		test_item.item_code = "_Test Item 999"
+		test_item.item_group = "_Test Item Group"
+		test_item.item_name = "_Test Item 999"
+		test_item.stock_uom = "_Test UOM"
+		test_item.standard_rate = 101.2
+
+		test_item.save()
+		frappe.db.commit()
+
+		test_item_price = fetch_item_price_list_rate()
+		self.assertEquals(test_item_price, 101.2)
+
+		test_item = frappe.get_doc("Item", "_Test Item 999")
+		test_item.standard_rate = 1111.11
+		test_item.save()
+		frappe.db.commit()
+
+		test_item_price = fetch_item_price_list_rate()
+
+		self.assertEquals(test_item_price, 1111.11)
+
 
 def set_item_variant_settings(fields):
 	doc = frappe.get_doc('Item Variant Settings')
