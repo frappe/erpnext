0805953bf43e7c11b305892d0e552b0d6f21b2bd
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-12-28 14:57:45 +0530

    Removed share number tracking
    - removed share number tracking from Share Ledger child table for Shareholder doctype
    - new doctype Share Type created

--- erpnext/accounts/doctype/shareholder/shareholder.py
+++ erpnext/accounts/doctype/shareholder/shareholder.py
@@ -8,18 +8,11 @@ from frappe.model.document import Document
 
 class Shareholder(Document):
 	def validate(self):
-		bought_shares = frappe.get_all("Share Transfer", filters = {"to_shareholder": self.shareholder})
-		sold_shares = frappe.get_all("Share Transfer", filters = {"from_shareholder": self.shareholder})
-		for share_transfer in bought_shares:
-			transfer = frappe.get_doc('Share Transfer', share_transfer.name)
-			# add to shares child table
-		for share_transfer in sold_shares:
-			transfer = frappe.get_doc('Share Transfer', share_transfer.name)
-			# remove from shares child table
 		self.update_amount()
 
 	def update_amount(self):
 		total_amount = 0
 		for ledger in self.share_ledger:
+			ledger.amount = ledger.rate * ledger.no_of_shares
 			total_amount += ledger.amount
 		self.total_amount = total_amount
