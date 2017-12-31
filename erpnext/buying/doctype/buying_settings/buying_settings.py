7a5da45f723fbb484b37c6af55a3c53b0e3dc14f
szufisher <szufisher@gmail.com>
2018-01-01 00:05:35 +0800

    Update buying_settings.py
    add 2 extra fields allow item qty submit, allow item price submit which controls activating/deactivating the purchase item, i.e qty and price for change after submit.

--- erpnext/buying/doctype/buying_settings/buying_settings.py
+++ erpnext/buying/doctype/buying_settings/buying_settings.py
@@ -16,3 +16,26 @@ class BuyingSettings(Document):
 		from erpnext.setup.doctype.naming_series.naming_series import set_by_naming_series
 		set_by_naming_series("Supplier", "supplier_name",
 			self.get("supp_master_name")=="Naming Series", hide_name_field=False)
+		self.process_item_submit()
+
+	def process_item_submit(self):
+		if self.allow_item_qty_submit == 'Yes' or self.allow_item_price_submit == 'Yes':
+			frappe.db.sql(""" update tabDocField set allow_on_submit=1 where (fieldtype in ('Currency','Float','Percent') or
+				fieldname in ('items','base_in_words','in_words','status')) and
+				parent = "Purchase Order" """)
+			frappe.db.sql("""update tabDocField set allow_on_submit=1 where fieldtype in ('Currency','Float','Percent') and				
+				parent = 'Purchase Order Item' """)
+			frappe.db.sql(""" update tabDocField set allow_on_submit=1 where fieldtype in ('Currency','Float','Percent') and
+				parent = 'Purchase Taxes and Charges' """)
+			if self.allow_item_price_submit == 'No':
+				frappe.db.sql("""update tabDocField set allow_on_submit=0 where fieldname = 'rate' and				
+					parent = 'Purchase Order Item'""")
+			if self.allow_item_qty_submit == 'No':
+				frappe.db.sql(""" update tabDocField set allow_on_submit=0 where fieldname = 'qty' and				
+					parent = 'Purchase Order Item' """)
+		if self.allow_item_qty_submit == 'No' and self.allow_item_price_submit == 'No':
+			frappe.db.sql(""" update tabDocField set allow_on_submit=0 where (fieldtype in ('Currency','Float','Percent') or
+				fieldname in ('items','base_in_words','in_words','status')) and	parent = 'Purchase Order' """)
+			frappe.db.sql("""update tabDocField set allow_on_submit=0 where fieldtype in ('Currency','Float','Percent') and				
+				parent = 'Purchase Order Item' """)
+
