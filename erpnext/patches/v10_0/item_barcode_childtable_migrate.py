3aaed03dfba7f044b3b5e99fec5161c1e5121c2a
joezsweet <joezsweet@tiscali.it>
2018-01-11 11:31:31 +0100

    reload item_barcode

--- erpnext/patches/v10_0/item_barcode_childtable_migrate.py
+++ erpnext/patches/v10_0/item_barcode_childtable_migrate.py
@@ -9,6 +9,7 @@ import frappe
 def execute():
     items_barcode = frappe.db.sql("SELECT name, barcode FROM tabItem WHERE barcode IS NOT NULL", as_dict=1)
     frappe.reload_doc("stock", "doctype", "item")
+    frappe.reload_doc("stock", "doctype", "item_barcode")
     for item in items_barcode:
         doc = frappe.get_doc("Item", item.get("name"))
         if item.get("barcode"):
