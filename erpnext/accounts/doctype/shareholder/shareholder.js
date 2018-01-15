22e79973ce5f162bea5c2aa35df830662342e188
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-01-15 20:39:59 +0530

    started new shareholder structure

--- erpnext/accounts/doctype/shareholder/shareholder.js
+++ erpnext/accounts/doctype/shareholder/shareholder.js
@@ -3,19 +3,33 @@
 
 frappe.ui.form.on('Shareholder', {
 	refresh: function(frm) {
-		if (frm.doc.__islocal != 1){
-			frm.add_custom_button(__("Share Balance"), function(){
-				frappe.route_options = {
-					"shareholder": frm.doc.name,
-				};
-				frappe.set_route("query-report", "Share Balance");
-			});
-			frm.add_custom_button(__("Share Ledger"), function(){
-				frappe.route_options = {
-					"shareholder": frm.doc.name,
-				};
-				frappe.set_route("query-report", "Share Ledger");
-			});
+		frappe.dynamic_link = { doc: frm.doc, fieldname: 'name', doctype: 'Shareholder' };
+
+		frm.toggle_display(['contact_html'], !frm.doc.__islocal);
+
+		if (frm.doc.__islocal) {
+			hide_field(['contact_html']);
+			frappe.contacts.clear_address_and_contact(frm);
+		}
+		else {
+			if (frm.doc.is_company){
+				hide_field(['company']);
+			} else {
+				frm.add_custom_button(__("Share Balance"), function(){
+					frappe.route_options = {
+						"shareholder": frm.doc.name,
+					};
+					frappe.set_route("query-report", "Share Balance");
+				});
+				frm.add_custom_button(__("Share Ledger"), function(){
+					frappe.route_options = {
+						"shareholder": frm.doc.name,
+					};
+					frappe.set_route("query-report", "Share Ledger");
+				});
+				unhide_field(['contact_html']);
+				frappe.contacts.render_address_and_contact(frm);
+			}
 		}
 	}
 });
--- erpnext/accounts/doctype/shareholder/shareholder.py
+++ erpnext/accounts/doctype/shareholder/shareholder.py
@@ -5,7 +5,17 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.contacts.address_and_contact import load_address_and_contact, delete_contact_and_address
 
 class Shareholder(Document):
+	def onload(self):
+		"""Load address and contacts in `__onload`"""
+		load_address_and_contact(self)
+
+	def on_trash(self):
+		delete_contact_and_address('Shareholder', self.name)
+		if self.lead_name:
+			frappe.db.sql("update `tabLead` set status='Interested' where name=%s", self.lead_name)
+	
 	def validate(self):
 		pass
