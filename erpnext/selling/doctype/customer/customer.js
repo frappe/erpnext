24cae1cad662732d62e4214926df1fc854b343a0
Saurabh <bhosalesaurabh8@gmail.com>
2018-01-20 02:47:19 +0530

    Made Primary address field searchable and is displayed in Customer Link field options

--- erpnext/selling/doctype/customer/customer.js
+++ erpnext/selling/doctype/customer/customer.js
@@ -33,7 +33,16 @@ frappe.ui.form.on("Customer", {
 				}
 			}
 		})
+		frm.set_query('customer_primary_address', function(doc) {
+			return {
+				query: "erpnext.selling.doctype.customer.customer.get_customer_primary_address",
+				filters: {
+					'customer': doc.name
+				}
+			}
+		})
 	},
+
 	refresh: function(frm) {
 		if(frappe.defaults.get_default("cust_master_name")!="Naming Series") {
 			frm.toggle_display("naming_series", false);
--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -306,3 +306,15 @@ def get_customer_primary_contact(doctype, txt, searchfield, start, page_len, fil
 			'customer': customer,
 			'txt': '%%%s%%' % txt
 		})
+
+def get_customer_primary_address(doctype, txt, searchfield, start, page_len, filters):
+	customer = frappe.db.escape(filters.get('customer'))
+	return frappe.db.sql("""
+		select `tabAddress`.name from `tabAddress`, `tabDynamic Link`
+			where `tabAddress`.name = `tabDynamic Link`.parent and `tabDynamic Link`.link_name = %(customer)s
+			and `tabDynamic Link`.link_doctype = 'Customer' and `tabAddress`.is_primary_address = 1
+			and `tabAddress`.name like %(txt)s
+		""", {
+			'customer': customer,
+			'txt': '%%%s%%' % txt
+		})
