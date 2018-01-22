f37a6fec5a94f120c7f193649fed09d45f202e15
Yogendrasinh Chavda <y.chavda.serpentcs@gmail.com>
2018-01-22 15:56:33 +0530

    [FIX] Fixed Travis CI log.

--- erpnext/shopping_cart_coupon_code/doctype/shopping_cart_coupon_code/shopping_cart_coupon_code.js
+++ erpnext/shopping_cart_coupon_code/doctype/shopping_cart_coupon_code/shopping_cart_coupon_code.js
@@ -2,7 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Shopping Cart Coupon Code', {
-	refresh: function(frm) {
-
-	}
+//	refresh: function(frm) {
+//	}
 });
--- erpnext/shopping_cart_coupon_code/doctype/shopping_cart_coupon_code/shopping_cart_coupon_code.py
+++ erpnext/shopping_cart_coupon_code/doctype/shopping_cart_coupon_code/shopping_cart_coupon_code.py
@@ -3,7 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 class ShoppingCartCouponCode(Document):
