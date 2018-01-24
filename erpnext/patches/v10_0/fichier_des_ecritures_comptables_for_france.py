29bc14d242cd67c1ac067b908da01110f1a156bc
Rushabh Mehta <rmehta@gmail.com>
2018-01-24 14:39:04 +0530

    [fix] ficher_des_ecritures patch

--- erpnext/patches/v10_0/fichier_des_ecritures_comptables_for_france.py
+++ erpnext/patches/v10_0/fichier_des_ecritures_comptables_for_france.py
@@ -6,5 +6,6 @@ import frappe
 from erpnext.setup.doctype.company.company import install_country_fixtures
 
 def execute():
+	frappe.reload_doc('regional', 'report', 'fichier_des_ecritures_comptables_[fec]')
 	for d in frappe.get_all('Company', filters = {'country': 'France'}):
 		install_country_fixtures(d.name)
