6558b11ad4a38f907802485c8ed0e7f34c4e14f3
renzodemie <rdvdeleonio@gmail.com>
2018-01-30 11:07:44 +0800

    FRM

--- erpnext/projects/doctype/project/project.js
+++ erpnext/projects/doctype/project/project.js
@@ -2,9 +2,9 @@
 // License: GNU General Public License v3. See license.txt
 
 frappe.ui.form.on("Project", {
-    setup: function(frm) {
+    setup: function (frm) {
         frm.set_indicator_formatter('title',
-            function(doc) {
+            function (doc) {
                 let indicator = 'orange';
                 if (doc.status == 'Overdue') {
                     indicator = 'red';
@@ -20,10 +20,10 @@ frappe.ui.form.on("Project", {
         );
     },
 
-    onload: function(frm) {
+    onload: function (frm) {
         var so = frappe.meta.get_docfield("Project", "sales_order");
-        so.get_route_options_for_new_doc = function(field) {
-            if(frm.is_new()) return;
+        so.get_route_options_for_new_doc = function (field) {
+            if (frm.is_new()) return;
             return {
                 "customer": frm.doc.customer,
                 "project_name": frm.doc.name
@@ -32,14 +32,14 @@ frappe.ui.form.on("Project", {
 
         frm.set_query('customer', 'erpnext.controllers.queries.customer_query');
 
-        frm.set_query("user", "users", function() {
+        frm.set_query("user", "users", function () {
             return {
-                query:"erpnext.projects.doctype.project.project.get_users_for_project"
+                query: "erpnext.projects.doctype.project.project.get_users_for_project"
             }
         });
 
         // sales order
-        frm.set_query('sales_order', function() {
+        frm.set_query('sales_order', function () {
             var filters = {
                 'project': ["in", frm.doc.__islocal ? [""] : [frm.doc.name, ""]]
             };
@@ -54,14 +54,14 @@ frappe.ui.form.on("Project", {
         });
     },
 
-    refresh: function(frm) {
-        if(frm.doc.__islocal) {
+    refresh: function (frm) {
+        if (frm.doc.__islocal) {
             frm.web_link && frm.web_link.remove();
         } else {
             frm.add_web_link("/projects?project=" + encodeURIComponent(frm.doc.name));
 
-            if(frappe.model.can_read("Task")) {
-                frm.add_custom_button(__("Gantt Chart"), function() {
+            if (frappe.model.can_read("Task")) {
+                frm.add_custom_button(__("Gantt Chart"), function () {
                     frappe.route_options = {"project": frm.doc.name};
                     frappe.set_route("List", "Task", "Gantt");
                 });
@@ -70,21 +70,25 @@ frappe.ui.form.on("Project", {
             frm.trigger('show_dashboard');
         }
     },
-    tasks_refresh: function(frm) {
+    tasks_refresh: function (frm) {
         var grid = frm.get_field('tasks').grid;
-        grid.wrapper.find('select[data-fieldname="status"]').each(function() {
-            if($(this).val()==='Open') {
+        grid.wrapper.find('select[data-fieldname="status"]').each(function () {
+            if ($(this).val() === 'Open') {
                 $(this).addClass('input-indicator-open');
             } else {
                 $(this).removeClass('input-indicator-open');
             }
         });
     },
-    show_dashboard: function(frm) {
-        if(frm.doc.__onload.activity_summary.length) {
-            var hours = $.map(frm.doc.__onload.activity_summary, function(d) { return d.total_hours });
+    show_dashboard: function (frm) {
+        if (frm.doc.__onload.activity_summary.length) {
+            var hours = $.map(frm.doc.__onload.activity_summary, function (d) {
+                return d.total_hours
+            });
             var max_count = Math.max.apply(null, hours);
-            var sum = hours.reduce(function(a, b) { return a + b; }, 0);
+            var sum = hours.reduce(function (a, b) {
+                return a + b;
+            }, 0);
             var section = frm.dashboard.add_section(
                 frappe.render_template('project_dashboard',
                     {
@@ -93,7 +97,7 @@ frappe.ui.form.on("Project", {
                         sum: sum
                     }));
 
-            section.on('click', '.time-sheet-link', function() {
+            section.on('click', '.time-sheet-link', function () {
                 var activity_type = $(this).attr('data-activity_type');
                 frappe.set_route('List', 'Timesheet',
                     {'activity_type': activity_type, 'project': frm.doc.name, 'status': ["!=", "Cancelled"]});
@@ -103,23 +107,23 @@ frappe.ui.form.on("Project", {
 });
 
 frappe.ui.form.on("Project Task", {
-    edit_task: function(frm, doctype, name) {
+    edit_task: function (frm, doctype, name) {
         var doc = frappe.get_doc(doctype, name);
-        if(doc.task_id) {
+        if (doc.task_id) {
             frappe.set_route("Form", "Task", doc.task_id);
         } else {
             frappe.msgprint(__("Save the document first."));
         }
     },
-    edit_timesheet: function(frm, cdt, cdn) {
+    edit_timesheet: function (frm, cdt, cdn) {
         var child = locals[cdt][cdn];
         frappe.route_options = {"project": frm.doc.project_name, "task": child.task_id};
         frappe.set_route("List", "Timesheet");
     },
 
-    make_timesheet: function(frm, cdt, cdn) {
+    make_timesheet: function (frm, cdt, cdn) {
         var child = locals[cdt][cdn];
-        frappe.model.with_doctype('Timesheet', function() {
+        frappe.model.with_doctype('Timesheet', function () {
             var doc = frappe.model.get_new_doc('Timesheet');
             var row = frappe.model.add_child(doc, 'time_logs');
             row.project = frm.doc.project_name;
@@ -128,13 +132,13 @@ frappe.ui.form.on("Project Task", {
         })
     },
 
-    status: function(frm, doctype, name) {
+    status: function (frm, doctype, name) {
         frm.trigger('tasks_refresh');
     },
 });
 
 
-cur_frm.cscript.validate = function (){
+cur_frm.cscript.validate = function () {
 
     var from = cur_frm.doc.from,
         to = cur_frm.doc.to,
@@ -146,21 +150,21 @@ cur_frm.cscript.validate = function (){
     frappe.call({
         method: "erpnext.projects.doctype.project.project.times_check",
         args: {
-            "from1":from,
-            "to":to,
-            "first_email":first_email,
-            "second_email":second_email,
-            "daily_time_to_send":daily_time_to_send,
-            "weekly_time_to_send":weekly_time_to_send,
+            "from1": from,
+            "to": to,
+            "first_email": first_email,
+            "second_email": second_email,
+            "daily_time_to_send": daily_time_to_send,
+            "weekly_time_to_send": weekly_time_to_send,
         },
 
-        callback: function(r){
-        	cur_frm.doc.from = r.message[0],
-            cur_frm.doc.to = r.message[1],
-            cur_frm.doc.first_email = r.message[2],
-            cur_frm.doc.second_email = r.message[3],
-            cur_frm.doc.daily_time_to_send = r.message[4],
-            cur_frm.doc.weekly_time_to_send = r.message[5];
+        callback: function (r) {
+            cur_frm.doc.from = r.message[0],
+                cur_frm.doc.to = r.message[1],
+                cur_frm.doc.first_email = r.message[2],
+                cur_frm.doc.second_email = r.message[3],
+                cur_frm.doc.daily_time_to_send = r.message[4],
+                cur_frm.doc.weekly_time_to_send = r.message[5];
         }
     });
 }
