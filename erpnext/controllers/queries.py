7918b92d951ffa058a15476a48ad78523ae86766
Nabin Hait <nabinhait@gmail.com>
2018-01-31 15:30:03 +0530

    Invalid date value comparison, fixes for mariadb 10.2.3+

--- erpnext/controllers/queries.py
+++ erpnext/controllers/queries.py
@@ -257,7 +257,7 @@ def get_delivery_notes_to_be_billed(doctype, txt, searchfield, start, page_len, 
 def get_batch_no(doctype, txt, searchfield, start, page_len, filters):
 	cond = ""
 	if filters.get("posting_date"):
-		cond = "and (ifnull(batch.expiry_date, '')='' or batch.expiry_date >= %(posting_date)s)"
+		cond = "and (batch.expiry_date is null or batch.expiry_date >= %(posting_date)s)"
 
 	batch_nos = None
 	args = {
