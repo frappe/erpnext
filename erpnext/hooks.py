6e8feeace765b40b6285575c0d1f71e6dd97579c
Charles-Henri Decultot <chdecultot@dokos.io>
2018-02-06 12:02:50 +0000

    Regional overrides for France

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -198,13 +198,19 @@ doc_events = {
 	"Website Settings": {
 		"validate": "erpnext.portal.doctype.products_settings.products_settings.home_page_is_products"
 	},
+	"Sales Invoice": {
+		'validate': 'erpnext.regional.india.utils.set_place_of_supply',
+		"on_submit": "erpnext.regional.france.utils.create_transaction_log",
+		"on_trash": "erpnext.regional.france.utils.check_deletion_permission"
+	},
 	"Payment Entry": {
-		"on_submit": "erpnext.accounts.doctype.payment_request.payment_request.make_status_as_paid"
+		"on_submit": ["erpnext.regional.france.utils.create_transaction_log", "erpnext.accounts.doctype.payment_request.payment_request.make_status_as_paid"],
+		"on_trash": "erpnext.regional.france.utils.check_deletion_permission"
 	},
 	'Address': {
 		'validate': 'erpnext.regional.india.utils.validate_gstin_for_india'
 	},
-	('Sales Invoice', 'Purchase Invoice'): {
+	'Purchase Invoice': {
 		'validate': 'erpnext.regional.india.utils.set_place_of_supply'
 	}
 }
