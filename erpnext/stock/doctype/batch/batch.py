621740efd9ae1bcefef9eb0d04508239b8403803
sivankar <sivankarjain@gmail.com>
2018-02-12 14:33:40 +0530

    Enhancement and bug fix in batch (#12753)
    * merge shelf like and manufacturing date
    
    * setting default manufacturing date as todays date
    
    * setting default manufacturing date as todays date
    
    * fix
    
    * Improvements to batch auto naming (#12496)
    
    * refactor:
    add new function - batch_uses_naming_series
    use batch_uses_naming_series in autoname method
    
    * properly update naming series on delete:
    - add new functions - get_batch_prefix, get_batch_naming_series_key, get_batch_naming_series
    - refactor get_name_from_naming_series
    - add after_delete method
    
    * add documentation and rename some functions
    
    * PEP 8 compliance
    
    * test
    
    * added support for jinja template and added validation if expiry date is set or not if item has_expiry_date is set
    
    * bug fix, renamed item_code to item as in batch doctype we refer 'item code' by 'item'
    
    * added manufacturing date wise sort
    
    * added prefix to expiry date and manufacturing date

--- erpnext/stock/doctype/batch/batch.py
+++ erpnext/stock/doctype/batch/batch.py
@@ -7,22 +7,12 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.model.naming import make_autoname, revert_series_if_last
 from frappe.utils import flt, cint
-
+from frappe.utils.jinja import render_template, validate_template
+from frappe.utils.data import add_days
+import json
 
 class UnableToSelectBatchError(frappe.ValidationError):
 	pass
-
-
-def get_name_from_naming_series():
-	"""
-	Get a name generated for a Batch from the Batch's naming series.
-	:return: The string that was generated.
-	"""
-	naming_series_prefix = _get_batch_prefix()
-	key = _make_naming_series_key(naming_series_prefix)
-	name = make_autoname(key)
-
-	return name
 
 
 def get_name_from_hash():
@@ -93,13 +83,67 @@ def get_batch_naming_series():
 	return series
 
 
+def batch_uses_naming_series():
+	"""
+	Verify if the Batch is to be named using a naming series
+	:return: bool
+	"""
+	use_naming_series = cint(frappe.db.get_single_value('Stock Settings', 'use_naming_series'))
+	return bool(use_naming_series)
+
+
+def _get_batch_prefix():
+	"""
+	Get the naming series prefix set in Stock Settings.
+
+	It does not do any sanity checks so make sure to use it after checking if the Batch
+	is set to use naming series.
+	:return: The naming series.
+	"""
+	naming_series_prefix = frappe.db.get_single_value('Stock Settings', 'naming_series_prefix')
+	if not naming_series_prefix:
+		naming_series_prefix = 'BATCH-'
+
+	return naming_series_prefix
+
+
+def _make_naming_series_key(prefix):
+	"""
+	Make naming series key for a Batch.
+
+	Naming series key is in the format [prefix].[#####]
+	:param prefix: Naming series prefix gotten from Stock Settings
+	:return: The derived key. If no prefix is given, an empty string is returned
+	"""
+	if not unicode(prefix):
+		return ''
+	else:
+		return prefix.upper() + '.#####'
+
+
+def get_batch_naming_series():
+	"""
+	Get naming series key for a Batch.
+
+	Naming series key is in the format [prefix].[#####]
+	:return: The naming series or empty string if not available
+	"""
+	series = ''
+	if batch_uses_naming_series():
+		prefix = _get_batch_prefix()
+		key = _make_naming_series_key(prefix)
+		series = key
+
+	return series
+
+
 class Batch(Document):
 	def autoname(self):
 		"""Generate random ID for batch if not specified"""
 		if not self.batch_id:
 			if frappe.db.get_value('Item', self.item, 'create_new_batch'):
 				if batch_uses_naming_series():
-					self.batch_id = get_name_from_naming_series()
+					self.batch_id = self.get_name_from_naming_series()
 				else:
 					self.batch_id = get_name_from_hash()
 			else:
@@ -119,6 +163,28 @@ class Batch(Document):
 	def item_has_batch_enabled(self):
 		if frappe.db.get_value("Item", self.item, "has_batch_no") == 0:
 			frappe.throw(_("The selected item cannot have Batch"))
+
+	def before_save(self):
+		has_expiry_date, shelf_life_in_days = frappe.db.get_value('Item', self.item, ['has_expiry_date', 'shelf_life_in_days'])
+		if not self.expiry_date and has_expiry_date and shelf_life_in_days:
+			self.expiry_date = add_days(self.manufacturing_date, shelf_life_in_days)
+
+		if has_expiry_date and not self.expiry_date:
+			frappe.throw('Expiry date is mandatory for selected item')
+			frappe.msgprint('Set items shelf life in days, to set expiry based on manufacturing_date plus self life ')
+
+	def get_name_from_naming_series(self):
+		"""
+		Get a name generated for a Batch from the Batch's naming series.
+		:return: The string that was generated.
+		"""
+		naming_series_prefix = _get_batch_prefix()
+		# validate_template(naming_series_prefix)
+		naming_series_prefix = render_template(str(naming_series_prefix), self.__dict__)
+		key = _make_naming_series_key(naming_series_prefix)
+		name = make_autoname(key)
+
+		return name
 
 
 @frappe.whitelist()
@@ -204,6 +270,7 @@ def set_batch_nos(doc, warehouse_field, throw=False):
 				batch_qty = get_batch_qty(batch_no=d.batch_no, warehouse=warehouse)
 				if flt(batch_qty, d.precision("qty")) < flt(qty, d.precision("qty")):
 					frappe.throw(_("Row #{0}: The batch {1} has only {2} qty. Please select another batch which has {3} qty available or split the row into multiple rows, to deliver/issue from multiple batches").format(d.idx, d.batch_no, batch_qty, qty))
+
 
 
 @frappe.whitelist()
