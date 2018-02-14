068bfd6fc8c6990409e8770ba475322fa8fe2128
Suraj Shetty <surajshetty3416@gmail.com>
2018-02-14 11:53:58 +0530

    removed unwanted permission check

--- erpnext/hr/doctype/daily_work_summary_setting/daily_work_summary_setting.js
+++ erpnext/hr/doctype/daily_work_summary_setting/daily_work_summary_setting.js
@@ -7,13 +7,6 @@ frappe.ui.form.on('Daily Work Summary Setting', {
 			frm.add_custom_button(__('Daily Work Summary'), function () {
 				frappe.set_route('List', 'Daily Work Summary');
 			});
-			frm.trigger('enabled');
 		}
-	},
-	enabled: function(frm) {
-		var doc = frm.doc;
-		if(!frm.is_new() && has_common(frappe.user_roles, ["Administrator", "System Manager"])) {
-			frm.set_df_property('enabled', 'read_only', 0);
-		}
-	},
+	}
 });
