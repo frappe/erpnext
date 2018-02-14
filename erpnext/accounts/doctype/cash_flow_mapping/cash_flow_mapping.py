0a39e6ef4d84bf534599eb192e886b8eba37d411
tundebabzy <tundebabzy@gmail.com>
2018-02-14 09:10:48 +0100

    adds validation to prevent selecting multiple options

--- erpnext/accounts/doctype/cash_flow_mapping/cash_flow_mapping.py
+++ erpnext/accounts/doctype/cash_flow_mapping/cash_flow_mapping.py
@@ -7,4 +7,15 @@ import frappe
 from frappe.model.document import Document
 
 class CashFlowMapping(Document):
-	pass
+	def validate(self):
+		self.validate_checked_options()
+
+	def validate_checked_options(self):
+		checked_fields = [d for d in self.meta.fields if d.fieldtype == 'Check' and self.get(d.fieldname) == 1]
+		if len(checked_fields) > 1:
+			frappe.throw(
+				frappe._('You can only select a maximum of one option from the list of check boxes.'),
+				title='Error'
+			)
+
+
