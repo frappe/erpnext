a6781bb529c0ae7b9bc82b8b190fe3f1937fbe54
Revant Nandgaonkar <revant.one@gmail.com>
2018-02-14 17:05:24 +0530

    Verify Webhook Data

--- erpnext/erpnext_integrations/connectors/woocommerce_connection.py
+++ erpnext/erpnext_integrations/connectors/woocommerce_connection.py
@@ -1,11 +1,25 @@
 # see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, base64, hashlib, hmac, json
+from frappe import _
+
+def verify_request():
+	woocommerce_settings = frappe.get_doc("Woocommerce Settings")
+	sig = base64.b64encode(
+		hmac.new(
+			woocommerce_settings.secret.encode(),
+			str(frappe.request.data),
+			hashlib.sha256
+		).digest()
+	)
+
+	return sig == frappe.get_request_header("X-Wc-Webhook-Signature")
 
 @frappe.whitelist(allow_guest=True)
 def create_coupon():
-	pass
+	if not verify_request():
+		frappe.throw(_("Unverified Webhook Data"))
 
 @frappe.whitelist(allow_guest=True)
 def update_coupon():
