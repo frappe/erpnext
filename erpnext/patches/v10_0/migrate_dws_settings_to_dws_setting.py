f59fd78bc225b0295da1a34e72788d74fa0d2d60
Suraj Shetty <surajshetty3416@gmail.com>
2018-02-15 12:16:00 +0530

    add patch for the changes

---
+++ erpnext/patches/v10_0/migrate_dws_settings_to_dws_setting.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2018, Frappe and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	# get the previously saved settings
+	previous_setting = get_previous_setting()
+	if previous_setting["companies"]:
+		for d in previous_setting["companies"]:
+			users = frappe.get_list("Employee", dict(
+				company=d.company), "user_id as user")
+			# create new setting entry for each company entry
+			new_setting = frappe.get_doc(dict(doctype="Daily Work Summary Setting",
+											  name="Daily Work Summary for " + d.company,
+											  users=users,
+											  send_emails_at=d.send_emails_at,
+											  subject=previous_setting["subject"],
+											  message=previous_setting["message"]))
+			new_setting.insert(ignore_permissions=True)
+			new_setting.save()
+	# frappe.delete_doc("Daily Work Summary Settings")
+	# frappe.delete_doc("Daily Work Summary Settings Company")
+
+def get_setting_companies():
+	return frappe.db.sql("select * from `tabDaily Work Summary Settings Company`", as_dict=True)
+
+def get_previous_setting():
+	obj = {}
+	setting_data = frappe.db.sql(
+		"select field, value from tabSingles where doctype='Daily Work Summary Settings'")
+	for field, value in setting_data:
+		obj[field] = value
+	obj["companies"] = get_setting_companies()
+	return obj
