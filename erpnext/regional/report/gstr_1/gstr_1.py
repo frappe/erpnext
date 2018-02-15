60d45b301609ac8a125d1d8fa206230f126e6239
vishdha <vishaldhayagude07@gmail.com>
2018-02-15 12:03:12 +0530

    [fix] Patch Added for b2c limit

--- erpnext/regional/report/gstr_1/gstr_1.py
+++ erpnext/regional/report/gstr_1/gstr_1.py
@@ -112,17 +112,19 @@ class Gstr1Report(object):
 			conditions += " and invoice_type != 'Export' and is_return != 1 and customer in ('{0}')".\
 				format("', '".join([frappe.db.escape(c.name) for c in customers]))
 
+		if not self.gst_limit.b2c_limit:
+			frappe.throw(_("Please set B2C Limit in GST Settings."))
+
 		if self.filters.get("type_of_business") ==  "B2C Large":
 			conditions += """ and SUBSTR(place_of_supply, 1, 2) != SUBSTR(company_gstin, 1, 2)
 				and grand_total > {0} and is_return != 1 and customer in ('{1}')""".\
-					format(self.gst_limit.b2c_limit, "', '".join([frappe.db.escape(c.name) for c in customers]))
-
+					format(self.gst_limit.b2c_limit, "', '".join([frappe.db.escape(c.name) for c in customers])	)
+					
 		elif self.filters.get("type_of_business") ==  "B2C Small":
 			conditions += """ and (
 				SUBSTR(place_of_supply, 1, 2) = SUBSTR(company_gstin, 1, 2)
 					or grand_total <= {0}) and is_return != 1 and customer in ('{1}')""".\
 						format(self.gst_limit.b2c_limit, "', '".join([frappe.db.escape(c.name) for c in customers]))
-
 
 		elif self.filters.get("type_of_business") ==  "CDNR":
 			conditions += """ and is_return = 1 """
