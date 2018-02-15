18a7930480c17991f2a98b2f3779113d3d7f288d
Shreya <shreyashah115@gmail.com>
2018-02-15 13:43:44 +0530

    Rename Offer Letter to Job Offer

--- erpnext/hr/doctype/job_offer/job_offer.js
+++ erpnext/hr/doctype/job_offer/job_offer.js
@@ -1,9 +1,9 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
 
-frappe.provide("erpnext.offer_letter");
+frappe.provide("erpnext.job_offer");
 
-frappe.ui.form.on("Offer Letter", {
+frappe.ui.form.on("Job Offer", {
 	select_terms: function (frm) {
 		erpnext.utils.get_terms(frm.doc.select_terms, frm.doc, function (r) {
 			if (!r.exc) {
@@ -16,7 +16,7 @@ frappe.ui.form.on("Offer Letter", {
 		if ((!frm.doc.__islocal) && (frm.doc.status == 'Accepted') && (frm.doc.docstatus === 1)) {
 			frm.add_custom_button(__('Make Employee'),
 				function () {
-					erpnext.offer_letter.make_employee(frm)
+					erpnext.job_offer.make_employee(frm);
 				}
 			);
 		}
@@ -24,9 +24,9 @@ frappe.ui.form.on("Offer Letter", {
 
 });
 
-erpnext.offer_letter.make_employee = function (frm) {
+erpnext.job_offer.make_employee = function (frm) {
 	frappe.model.open_mapped_doc({
-		method: "erpnext.hr.doctype.offer_letter.offer_letter.make_employee",
+		method: "erpnext.hr.doctype.job_offer.job_offer.make_employee",
 		frm: frm
 	});
 };
