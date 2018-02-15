ebd5bdc6a0535877490eba9d1a9d9c3c1b52795e
vishdha <vishaldhayagude07@gmail.com>
2018-02-15 17:50:02 +0530

    [new] WIP Leader board Analysis

--- erpnext/utilities/page/leaderboard/leaderboard.js
+++ erpnext/utilities/page/leaderboard/leaderboard.js
@@ -17,14 +17,15 @@ frappe.Leaderboard = Class.extend({
 		this.$sidebar_list = this.page.sidebar.find('ul');
 
 		// const list of doctypes
-		this.doctypes = ["Customer", "Item", "Supplier", "Sales Partner"];
+		this.doctypes = ["Customer", "Item", "Supplier", "Sales Partner","Sales Person"];
 		this.timespans = ["Week", "Month", "Quarter", "Year"];
-		this.desc_fields = ["total_amount", "total_request", "annual_billing", "commission_rate"];
+		this.desc_fields = ["total_sales_amount", "total_request","total_purchase_amount","commission_rate"];
 		this.filters = {
-			"Customer": ["total_amount", "total_item_purchased"],
-			"Item": ["total_request", "total_purchase", "avg_price"],
-			"Supplier": ["annual_billing", "total_unpaid"],
-			"Sales Partner": ["commission_rate", "target_qty", "target_amount"],
+			"Customer": ["total_item_purchased_qty", "total_sales_amount","receivable_amount_outstanding_amount"],
+			"Item": ["total_purchase_amount", "total_purchased_qty", "total_sales_amount", "total_sold_qty","available_stock_qty"],
+			"Supplier": ["total_item_sold_qty", "total_purchase_amount","payable_amount_outstanding_amount"],
+			"Sales Partner": ["commission_rate", "target_qty", "target_amount", "total_sales_amount"],
+			"Sales Person": ["target_qty", "target_amount", "total_sales_amount"],
 		};
 
 		// for saving current selected filters
@@ -141,7 +142,7 @@ frappe.Leaderboard = Class.extend({
 						],
 						labels: graph_items.map(d=>d.name)
 					},
-					colors: ['light-green'],
+					colors: ['orange'],
 					format_tooltip_x: d=>d[me.options.selected_filter_item],
 					type: 'bar',
 					height: 140
--- erpnext/utilities/page/leaderboard/leaderboard.py
+++ erpnext/utilities/page/leaderboard/leaderboard.py
@@ -16,130 +16,177 @@ def get_leaderboard(doctype, timespan, field, start=0):
 	filters = {"modified":(">=", get_date_from_string(timespan))}
 	items = []
 	if doctype == "Customer":
-		items = get_all_customers(doctype, filters, [], field)
+		items = get_all_customers(filters, [], field)
 	elif  doctype == "Item":
 		items = get_all_items(doctype, filters, [], field)
 	elif  doctype == "Supplier":
-		items = get_all_suppliers(doctype, filters, [], field)
+		items = get_all_suppliers(filters, [], field)
 	elif  doctype == "Sales Partner":
-		items = get_all_sales_partner(doctype, filters, [], field)
+		items = get_all_sales_partner(filters, [], field)
+	elif doctype == "Sales Person":
+		items = get_all_sales_person(filters, [], field)
 
 	if len(items) > 0:
 		return items
 	return []
 
-def get_all_customers(doctype, filters, items, field, start=0, limit=20):
+def get_all_customers(filters, items, field, start=0, limit=20):
 	"""return all customers"""
 
-	x = frappe.get_list(doctype, filters=filters, limit_start=start, limit_page_length=limit)
+	customer_list = frappe.db.sql("""select A.customer, count(B.name) as item_count, sum(A.base_net_total) as base_net_total, sum(A.outstanding_amount) as outstanding_amount from 
+            `tabSales Invoice` as A LEFT JOIN `tabSales Invoice Item` as B ON A.name = B.parent where A.docstatus != 2 group by A.customer """, as_dict=1)
 
-	for val in x:
-		y = dict(frappe.db.sql('''select name, grand_total from `tabSales Invoice`\
-			where customer = %s and docstatus != 2''', (val.name)))
-		invoice_list = y.keys()
-		if len(invoice_list) > 0:
-			item_count = frappe.db.sql('''select count(name) from `tabSales Invoice Item` where parent in (%s)''' % ", ".join(
-				['%s'] * len(invoice_list)), tuple(invoice_list))
+	value = 0;
+	for cust in customer_list:
+		if field == "total_sales_amount":
+			value = cust.base_net_total
+		elif field == "receivable_amount_outstanding_amount":
+			value = cust.outstanding_amount
+		elif field == "total_item_purchased_qty":
+			value = cust.item_count
 
-			value = 0
-			if(field=="total_amount"):
-				value = sum(y.values())
-			elif(field=="total_item_purchased"):
-				value = sum(destructure_tuple_of_tuples(item_count))
+		item_obj = {"name": cust.customer,	
+			"total_item_purchased_qty": cust.item_count,
+			"total_sales_amount": get_formatted_value(cust.base_net_total),
+			"receivable_amount_outstanding_amount":get_formatted_value(cust.outstanding_amount),
+			"href":"#Form/Customer/" + cust.customer,
+			"value": value}
+		items.append(item_obj)
 
-			item_obj = {"name": val.name,
-				"total_amount": get_formatted_value(sum(y.values())),
-				"total_item_purchased": sum(destructure_tuple_of_tuples(item_count)),
-				"href":"#Form/Customer/" + val.name,
-				"value": value}
-			items.append(item_obj)
 
 	items.sort(key=lambda k: k['value'], reverse=True)
 	return items
 
 def get_all_items(doctype, filters, items, field, start=0, limit=20):
 	"""return all items"""
+	items_list = frappe.get_list(doctype, filters=filters, limit_start=start, limit_page_length=limit)
 
-	x = frappe.get_list(doctype, filters=filters, limit_start=start, limit_page_length=limit)
-	for val in x:
-		data = frappe.db.sql('''select item_code from `tabMaterial Request Item` where item_code = %s''', (val.name), as_list=1)
-		requests = destructure_tuple_of_tuples(data)
-		data = frappe.db.sql('''select price_list_rate from `tabItem Price` where item_code = %s''', (val.name), as_list=1)
-		avg_price = get_avg(destructure_tuple_of_tuples(data))
-		data = frappe.db.sql('''select item_code from `tabPurchase Invoice Item` where item_code = %s''', (val.name), as_list=1)
-		purchases = destructure_tuple_of_tuples(data)
+	for item in items_list:
+		s_invoice = frappe.db.sql("""select name, qty, amount\
+			from `tabSales Invoice Item`\
+			where item_code = %s and docstatus != 2""", {item.name},as_dict=1)
+		p_invoice = frappe.db.sql("""select name, qty, amount\
+			from `tabPurchase Invoice Item`\
+			where item_code = %s and docstatus != 2""", {item.name},as_dict=1)
+		data = frappe.db.sql('''select actual_qty from `tabBin` where item_code = %s''', (item.name), as_list=1)
+		stock_qty = destructure_tuple_of_tuples(data)
 
-		value = 0
-		if(field=="total_request"):
-			value = len(requests)
-		elif(field=="total_purchase"):
-			value = len(purchases)
-		elif(field=="avg_price"):
-			value=avg_price
-		item_obj = {"name": val.name,
-			"total_request":len(requests),
-			"total_purchase": len(purchases),
-			"avg_price": get_formatted_value(avg_price),
-			"href":"#Form/Item/" + val.name,
-			"value": value}
-		items.append(item_obj)
+
+		invoice_list = [x['name'] for x in s_invoice]
+
+		if len(invoice_list) > 0:
+			item_count = frappe.db.sql('''select count(name) from `tabSales Invoice Item` where parent in (%s)''' % ", ".join(
+				['%s'] * len(invoice_list)), tuple(invoice_list))
+			value = 0
+			if(field == "total_sales_amount"):
+				value = sum([x['amount'] for x in s_invoice])
+			elif(field == "total_sold_qty"):
+				value = sum([x['qty'] for x in s_invoice])
+			elif(field == "total_purchase_amount"):
+				value = sum([x['amount'] for x in p_invoice])
+			elif(field == "total_purchased_qty"):
+				value = sum([x['qty'] for x in p_invoice])
+			elif(field == "available_stock_qty"):
+				value = stock_qty
+
+			item_obj = {"name": item.name,
+				"total_sales_amount": get_formatted_value(sum([x['amount'] for x in s_invoice])),
+				"total_sold_qty": sum([x['qty'] for x in s_invoice]),
+				"total_purchase_amount": get_formatted_value(sum([x['amount'] for x in p_invoice])),
+				"total_purchased_qty": sum([x['qty'] for x in p_invoice]),
+				"available_stock_qty": stock_qty,
+				"href":"#Form/Customer/" + item.name,
+				"value": value}
+			items.append(item_obj)
 
 	items.sort(key=lambda k: k['value'], reverse=True)
 	return items
 
-def get_all_suppliers(doctype, filters, items, field, start=0, limit=20):
+def get_all_suppliers(filters, items, field, start=0, limit=20):
 	"""return all suppliers"""
 
-	x = frappe.get_list(doctype, filters=filters, limit_start=start, limit_page_length=limit)
+	supplier_list = frappe.db.sql("""select A.supplier, count(B.name) as item_count, sum(A.base_net_total) as base_net_total, 
+		sum(A.outstanding_amount) as outstanding_amount from `tabPurchase Invoice` as A LEFT JOIN `tabPurchase Invoice Item` as B ON A.name = B.parent where A.docstatus != 2 group by A.supplier """, as_dict=1)
 
-	for val in x:
+	value = 0;
+	for supp in supplier_list:
+		if field == "total_purchase_amount":
+			value = supp.base_net_total
+		elif field == "payable_amount_outstanding_amount":
+			value = supp.outstanding_amount
+		elif field == "total_item_sold_qty":
+			value = supp.item_count
 
-		info = get_dashboard_info(doctype, val.name)
-		value = 0
-		if(field=="annual_billing"):
-			value = info["billing_this_year"]
-		elif(field=="total_unpaid"):
-			value = abs(info["total_unpaid"])
-
-		item_obj = {"name": val.name,
-			"annual_billing":  get_formatted_value(info["billing_this_year"]),
-			"total_unpaid": get_formatted_value(abs(info["total_unpaid"])),
-			"href":"#Form/Supplier/" + val.name,
+		item_obj = {"name": supp.supplier,	
+			"total_purchase_amount": supp.item_count,
+			"payable_amount_outstanding_amount": get_formatted_value(supp.base_net_total),
+			"total_item_sold_qty":get_formatted_value(supp.outstanding_amount),
+			"href":"#Form/Supplier/" + supp.supplier,
 			"value": value}
 		items.append(item_obj)
+
 
 	items.sort(key=lambda k: k['value'], reverse=True)
 	return items
 
-def get_all_sales_partner(doctype, filters, items, field, start=0, limit=20):
+
+def get_all_sales_partner(filters, items, field, start=0, limit=20):
 	"""return all sales partner"""
 
-	x = frappe.get_list(doctype, fields=["name", "commission_rate", "modified"], filters=filters, limit_start=start, limit_page_length=limit)
-	for val in x:
-		y = frappe.db.sql('''select target_qty, target_amount from `tabTarget Detail` where parent = %s''', (val.name), as_dict=1)
-		target_qty = sum([f["target_qty"] for f in y])
-		target_amount = sum([f["target_amount"] for f in y])
+	sales_partner_list = frappe.db.sql("""select A.partner_name, A.commission_rate, B.target_qty, B.target_amount, sum(C.total_commission) as total_commission 
+		from `tabSales Partner` as A inner join `tabTarget Detail` as B ON A.name = B.parent inner join `tabSales Invoice` as C ON C.sales_partner =
+		A.partner_name group by A.partner_name""", as_dict=1)
 
-		value = 0
-		if(field=="commission_rate"):
-			value = val.commission_rate
-		elif(field=="target_qty"):
-			value = target_qty
-		elif(field=="target_amount"):
-			value = target_qty
+	value = 0
+	for sales_partner in sales_partner_list:
+		print("Sales paer", sales_partner)
+		if field == "commission_rate":
+			value = sales_partner.commission_rate
+		elif field == "target_qty":
+			value = sales_partner.target_qty
+		elif field == "target_amount":
+			value = sales_partner.target_amount
+		elif field == "total_sales_amount":
+			value = sales_partner.total_commission
 
-		item_obj = {"name": val.name,
-			"commission_rate": get_formatted_value(val.commission_rate, False),
-			"target_qty": target_qty,
-			"target_amount": get_formatted_value(target_qty),
-			"href":"#Form/Sales Partner/" + val.name,
+		item_obj = {"name": sales_partner.partner_name,
+			"commission_rate": get_formatted_value(sales_partner.commission_rate),
+			"target_qty": sales_partner.target_qty,
+			"target_amount": get_formatted_value(sales_partner.target_amount),
+			"total_sales_amount": get_formatted_value(sales_partner.total_commission),
+			"href":"#Form/Sales Partner/" + sales_partner.partner_name,
 			"value": value}
 		items.append(item_obj)
 
 	items.sort(key=lambda k: k['value'], reverse=True)
 	return items
 
+def get_all_sales_person(doctype, filters, items, field, start=0, limit=20):
+	"""return all sales partner"""
 
+	sales_person_list = frappe.db.sql('''select A.name, A.is_group, B.target_qty, B.target_amount, sum(C.allocated_amount) as allocated_amount from `tabSales Person` as 
+            A inner join `tabTarget Detail` as B ON A.name = B.parent inner join `tabSales Team` as C ON C.sales_person = A.name group by A.name''', as_dict=1)
+	value = 0
+	for sales_person in sales_person_list:
+		if not sales_person.is_group:
+			if(field=="target_qty"):
+				value=sales_person.target_qty
+			elif(field=="target_amount"):
+				value=sales_person.target_amount
+			elif(field=="total_sales_amount"):
+				value=sales_person.allocated_amount
+
+			item_obj = {"name": sales_person.name,
+				"target_qty": sales_person.target_qty,
+				"target_amount": get_formatted_value(sales_person.target_amount),
+				"total_sales_amount": get_formatted_value(sales_person.allocated_amount),
+				"href":"#Form/Sales Partner/" + sales_person.name,
+				"value": value}
+			items.append(item_obj)
+
+	items.sort(key=lambda k: k['value'], reverse=True)
+	return items
+	
 def destructure_tuple_of_tuples(tup_of_tup):
 	"""return tuple(tuples) as list"""
 	return [y for x in tup_of_tup for y in x]
