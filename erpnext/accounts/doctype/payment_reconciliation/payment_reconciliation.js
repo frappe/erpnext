ea370527a09d8801d486fa497753808bb157e381
Nabin Hait <nabinhait@gmail.com>
2018-02-16 13:14:20 +0530

    Payment reco auto allocation and maintain same order of records (#12963)
    * Automatically allocate amount after selecting invoice against a payment entry
    
    * codacy fixes

--- erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.js
+++ erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.js
@@ -3,9 +3,26 @@
 
 frappe.provide("erpnext.accounts");
 
+frappe.ui.form.on("Payment Reconciliation Payment", {
+	invoice_number: function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if(row.invoice_number) {
+			var parts = row.invoice_number.split(' | ');
+			var invoice_type = parts[0];
+			var invoice_number = parts[1];
+
+			var invoice_amount = frm.doc.invoices.filter(function(d) {
+				return d.invoice_type === invoice_type && d.invoice_number === invoice_number;
+			})[0].outstanding_amount;
+
+			frappe.model.set_value(cdt, cdn, "allocated_amount", Math.min(invoice_amount, row.amount));
+		}
+	}
+});
+
 erpnext.accounts.PaymentReconciliationController = frappe.ui.form.Controller.extend({
 	onload: function() {
-		var me = this
+		var me = this;
 		this.frm.set_query("party_type", function() {
 			return{
 				query: "erpnext.setup.doctype.party_type.party_type.get_party_type"
