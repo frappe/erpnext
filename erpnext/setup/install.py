5fe4172b05157ebee8013ee683b3794f2e4cb176
tundebabzy <tundebabzy@gmail.com>
2018-02-21 14:49:57 +0100

    PEP 8

--- erpnext/setup/install.py
+++ erpnext/setup/install.py
@@ -12,6 +12,7 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 default_mail_footer = """<div style="padding: 7px; text-align: right; color: #888"><small>Sent via
 	<a style="color: #888" href="http://erpnext.org">ERPNext</a></div>"""
 
+
 def after_install():
 	frappe.get_doc({'doctype': "Role", "role_name": "Analytics"}).insert()
 	set_single_defaults()
@@ -20,6 +21,7 @@ def after_install():
 	add_all_roles_to("Administrator")
 	frappe.db.commit()
 
+
 def check_setup_wizard_not_completed():
 	if frappe.db.get_default('desktop:home_page') == 'desktop':
 		print()
@@ -27,6 +29,7 @@ def check_setup_wizard_not_completed():
 		print("You can reinstall this site (after saving your data) using: bench --site [sitename] reinstall")
 		print()
 		return False
+
 
 def set_single_defaults():
 	for dt in ('Accounts Settings', 'Print Settings', 'HR Settings', 'Buying Settings',
@@ -46,6 +49,7 @@ def set_single_defaults():
 
 	frappe.db.set_default("date_format", "dd-mm-yyyy")
 
+
 def create_compact_item_print_custom_field():
 	create_custom_field('Print Settings', {
 		'label': _('Compact Item Print'),
@@ -54,6 +58,7 @@ def create_compact_item_print_custom_field():
 		'default': 1,
 		'insert_after': 'with_letterhead'
 	})
+
 
 def create_print_zero_amount_taxes_custom_field():
 	create_custom_field('Print Settings', {
