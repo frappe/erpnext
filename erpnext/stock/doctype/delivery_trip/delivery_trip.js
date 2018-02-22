fb568f0b9fe416254c369279fa8eaf86a85bcc3f
Rohit Waghchaure <rohitw1991@gmail.com>
2018-02-22 17:07:32 +0530

    [Fix] UX for delivery trip

--- erpnext/stock/doctype/delivery_trip/delivery_trip.js
+++ erpnext/stock/doctype/delivery_trip/delivery_trip.js
@@ -36,6 +36,24 @@ frappe.ui.form.on('Delivery Trip', {
 				frm.trigger('notify_customers');
 			});
 		}
+
+		if (frm.doc.docstatus===0) {
+			frm.add_custom_button(__('Delivery Note'), () => {
+				erpnext.utils.map_current_doc({
+					method: "erpnext.stock.doctype.delivery_note.delivery_note.make_delivery_trip",
+					source_doctype: "Delivery Note",
+					target: frm,
+					date_field: "posting_date",
+					setters: {
+						company: frm.doc.company,
+					},
+					get_query_filters: {
+						docstatus: 1,
+						company: frm.doc.company,
+					}
+				})
+			}, __("Get customers from"));
+		}
 	},
 
 	calculate_arrival_time: function (frm) {
