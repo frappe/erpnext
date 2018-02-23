13563701bbad86ae1e36c4e665a056effd6f4926
Prateeksha Singh <pratu16x7@gmail.com>
2018-02-24 01:29:41 +0530

    [hub] enable hub with OAuth token from user's client ID

--- erpnext/hub_node/doctype/hub_settings/hub_settings.js
+++ erpnext/hub_node/doctype/hub_settings/hub_settings.js
@@ -14,6 +14,14 @@ frappe.ui.form.on("Hub Settings", {
 		}
 	},
 	onload: function(frm) {
+		let token = frappe.urllib.get_arg("access_token");
+		if(token) {
+			let email = frm.get_field("user");
+			console.log('token', frappe.urllib.get_arg("access_token"));
+
+			get_user_details(frm, token, email);
+		}
+
 		if(!frm.doc.country) {
 			frm.set_value("country", frappe.defaults.get_default("Country"));
 		}
@@ -48,33 +56,47 @@ frappe.ui.form.on("Hub Settings", {
 			if(frappe.session.user === "Administrator") {
 				frappe.msgprint(__("Please login as another user."))
 			} else {
-				frappe.verify_password(() => {
-					this.frm.call({
-						doc: this.frm.doc,
-						method: "register",
-						args: {},
-						freeze: true,
-						callback: function(r) {},
-						onerror: function() {
-							frappe.msgprint(__("Wrong Password"));
-							frm.set_value("enabled", 0);
-						}
-					});
-				} );
+				// frappe.verify_password(() => {
+
+				// } );
+
+				frm.trigger("call_pre_reg");
+				// frm.trigger("call_register");
+
 			}
 		});
 	},
 
-	// update_hub: (frm) => {
-	// 	this.frm.call({
-	// 		doc: this.frm.doc,
-	// 		method: "update_hub",
-	// 		args: {},
-	// 		freeze: true,
-	// 		callback: function(r) { },
-	// 		onerror: function() { }
-	// 	});
-	// },
+	call_pre_reg: (frm) => {
+		this.frm.call({
+			doc: this.frm.doc,
+			method: "pre_reg",
+			args: {},
+			freeze: true,
+			callback: function(r) {
+				console.log(r.message);
+				authorize(frm, r.message.client_id, r.message.redirect_uri);
+			},
+			onerror: function() {
+				frappe.msgprint(__("Wrong Password"));
+				frm.set_value("enabled", 0);
+			}
+		});
+	},
+
+	call_register: (frm) => {
+		this.frm.call({
+			doc: this.frm.doc,
+			method: "register",
+			args: {},
+			freeze: true,
+			callback: function(r) {},
+			onerror: function() {
+				frappe.msgprint(__("Wrong Password"));
+				frm.set_value("enabled", 0);
+			}
+		});
+	},
 
 	unregister_from_hub: (frm) => {
 		frappe.verify_password(() => {
@@ -85,3 +107,84 @@ frappe.ui.form.on("Hub Settings", {
 		});
 	},
 });
+
+// let hub_url = 'https://hubmarket.org'
+let hub_url = 'http://159.89.175.122'
+// let hub_url = 'http://erpnext.hub:8000'
+
+function authorize(frm, client_id, redirect_uri) {
+    console.log("Fetching...", redirect_uri);
+
+    // queryStringData is details of OAuth Client (Implicit Grant) on Custom App
+	var queryStringData = {
+		response_type : "token",
+		client_id : client_id,
+        redirect_uri : redirect_uri
+	}
+
+    // Get current raw route and build url
+    const route = "/desk#" + frappe.get_raw_route_str();
+    localStorage.removeItem("route");  // Clear previously set route if any
+	localStorage.setItem("route", route);
+
+	// Go authorize!
+	let api_route = "/api/method/frappe.integrations.oauth2.authorize?";
+	let url = hub_url + api_route + $.param(queryStringData);
+	window.location.replace(url, 'test');
+}
+
+function get_user_details(frm, token, email) {
+    var route = localStorage.getItem("route");
+    if (token && route) {
+        // Clean up access token from route
+		frappe.set_route(frappe.get_route().join("/"))
+
+        // query protected resource e.g. Hub Items with token
+        var call = {
+            "async": true,
+            "crossDomain": true,
+            "url": hub_url + "/api/resource/User",
+			"method": "GET",
+			"data": {
+				// "email": email,
+				"fields": '["name", "first_name", "language"]',
+				"limit_page_length": 1
+			},
+            "headers": {
+                "authorization": "Bearer " + token,
+                "content-type": "application/x-www-form-urlencoded"
+            }
+		}
+        $.ajax(call).done(function (response) {
+			// display openid profile
+			console.log('response', response);
+
+			let data = response.data[0];
+			frm.set_value("enabled", 1);
+			frm.set_value("hub_username", data.first_name);
+			frm.set_value("hub_user_status", "Starter");
+			frm.set_value("language", data.language);
+			frm.save();
+
+            // clear route from localStorage
+            localStorage.removeItem("route");
+        });
+    }
+}
+
+function check_connection(token) {
+    var ping_call = {
+        "async": true,
+        "crossDomain": true,
+        "url": "/api/method/frappe.integrations.oauth2.openid_profile",
+		"method": "GET",
+        "xhrFields": { withCredentials:true }
+    }
+    $.ajax(ping_call).done(function (response) {
+        // display openid profile
+        console.log("Success", response.message);
+    }).fail(function() {
+        console.log("Login to Hub First");
+    });
+}
+
