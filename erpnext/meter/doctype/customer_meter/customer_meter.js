e6fc0a24f12258cbbb35b0dddb9a87f3def4eb16
ketanserpentcs <ketan.patel.serpentcs@gmail.com>
2018-02-26 15:02:21 +0530

    [IMP] Untracked working files

---
+++ erpnext/meter/doctype/customer_meter/customer_meter.js
@@ -0,0 +1,17 @@
+
+frappe.ui.form.on('Customer Meter', {
+	deactivated_date: function(frm) {
+		alert('asd');
+	},
+
+onload: function(frm) {
+		alert('asd');
+	},
+refresh: (frm) => {
+alert("refre->>");		
+	},
+	item: (frm) => {
+alert("ll");
+			},
+
+});
