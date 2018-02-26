64d79d65f9d053aa4a581d9e8bf976a1594b5df0
Shreya <shreyashah115@gmail.com>
2018-02-26 16:22:34 +0530

    Patch added

--- erpnext/patches/v11_0/rename_employee_loan_to_loan.py
+++ erpnext/patches/v11_0/rename_employee_loan_to_loan.py
@@ -4,15 +4,17 @@ from frappe.model.utils.rename_field import rename_field
 def execute():
 	if frappe.db.table_exists("Employee Loan Application") and not frappe.db.table_exists("Loan Application"):
 		frappe.rename_doc("DocType", "Employee Loan Application", "Loan Application", force=True)
-        frappe.reload_doc("hr", "doctype", "loan_application")
 
-    if frappe.db.table_exists("Employee Loan") and not frappe.db.table_exists("Loan"):
+	if frappe.db.table_exists("Employee Loan") and not frappe.db.table_exists("Loan"):
 		frappe.rename_doc("DocType", "Employee Loan", "Loan", force=True)
-		frappe.reload_doc("hr", "doctype", "loan")
 
-    frappe.reload_doc("hr", "doctype", "salary_slip_loan")
-    rename_field("Loan", "employee_loan_account", "loan_account")
-    rename_field("Salary Slip Loan", "employee_loan_account", "loan_account")
+	frappe.reload_doc("hr", "doctype", "loan_application")
+	frappe.reload_doc("hr", "doctype", "loan")
+	frappe.reload_doc("hr", "doctype", "salary_slip_loan")
 
-    for doctype in ['Employee Loan Application', 'Employee Loan']:
-			frappe.delete_doc('DocType', doctype)
+	for doctype in ['Loan', 'Salary Slip Loan']:
+		if frappe.db.has_column(doctype, 'employee_loan_account'):
+			rename_field(doctype, "employee_loan_account", "loan_account")
+
+	for doctype in ['Employee Loan Application', 'Employee Loan']:
+		frappe.delete_doc('DocType', doctype)
