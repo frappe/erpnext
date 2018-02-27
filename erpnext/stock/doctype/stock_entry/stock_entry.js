1f5bc6854298dd2f65785c3bcee53f63ff5be793
vishdha <vishaldhayagude07@gmail.com>
2018-02-27 09:43:31 +0530

    [new] Create multiple warehouse address and fetch address to stock entry

--- erpnext/stock/doctype/stock_entry/stock_entry.js
+++ erpnext/stock/doctype/stock_entry/stock_entry.js
@@ -278,6 +278,16 @@ frappe.ui.form.on('Stock Entry', {
 		frm.set_value("total_additional_costs",
 			flt(total_additional_costs, precision("total_additional_costs")));
 	},
+
+	source_warehouse_address: function(frm, cdt, cdn) {
+		erpnext.utils.get_address_display(frm, 'source_warehouse_address', 'source_address_display', false);
+	},
+
+	target_warehouse_address: function(frm, cdt, cdn) {
+		erpnext.utils.get_address_display(frm, 'target_warehouse_address', 'target_address_display', false);
+	},
+	
+	contact_person: erpnext.utils.get_contact_details,
 })
 
 frappe.ui.form.on('Stock Entry Detail', {
