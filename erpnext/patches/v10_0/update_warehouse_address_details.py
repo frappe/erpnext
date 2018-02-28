153a2f4479966cf7b184b625058b032bb86f653c
vishdha <vishaldhayagude07@gmail.com>
2018-02-28 17:18:36 +0530

    [fix] Modified patch for warehouse address

--- erpnext/patches/v10_0/update_warehouse_address_details.py
+++ erpnext/patches/v10_0/update_warehouse_address_details.py
@@ -6,16 +6,15 @@ import frappe
 
 def execute():
 	warehouse = frappe.db.sql("""select name, email_id, phone_no, mobile_no, address_line_1,
-		address_line_2, city, state, pin from `tabWarehouse`""",as_dict=1)
+		address_line_2, city, state, pin from `tabWarehouse` where ifnull(address_line_1, '') != '' """,as_dict=1)
 
 	for d in warehouse:                                                                                                                    
-		if not frappe.db.sql("select name from `tabAddress` where address_title=%s", d.name):
-			address = frappe.new_doc('Address')
-			address.address_title = d.name
-			address.address_line1 = d.address_line_1
-			address.city = d.city
-			address.state = d.state
-			address.pincode = d.pin
-			address.append('links',{'link_doctype':'Warehouse','link_name':d.name})
-			address.flags.ignore_mandatory = True
-			address.save()
+		address = frappe.new_doc('Address')
+		address.address_title = d.name
+		address.address_line1 = d.address_line_1
+		address.city = d.city
+		address.state = d.state
+		address.pincode = d.pin
+		address.db_insert()
+		address.append('links',{'link_doctype':'Warehouse','link_name':d.name})
+		address.links[0].db_insert()
