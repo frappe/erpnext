f88e20537322b0d9102c310ca919f54ca7cc1047
vishdha <vishaldhayagude07@gmail.com>
2018-02-28 09:40:26 +0530

    [fix] Codacy fixed

--- erpnext/patches/v10_0/update_warehouse_address_details.py
+++ erpnext/patches/v10_0/update_warehouse_address_details.py
@@ -5,10 +5,10 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	warehouse = frappe.db.sql("""select name, email_id, phone_no, mobile_no, address_line_1, 
-		address_line_2, city, state, pin from `tabWarehouse`""",as_dict=1) 
+	warehouse = frappe.db.sql("""select name, email_id, phone_no, mobile_no, address_line_1,
+		address_line_2, city, state, pin from `tabWarehouse`""",as_dict=1)
 
-	for d  in warehouse:                                                                                                                    
+	for d in warehouse:                                                                                                                    
 		if not frappe.db.sql("select name from `tabAddress` where address_title=%s", d.name):
 			address = frappe.new_doc('Address')
 			address.address_title = d.name
