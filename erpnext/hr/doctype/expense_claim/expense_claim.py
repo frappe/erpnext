3fecbb98c513a5b77e09b310fc158d77830b14c4
tundebabzy <tundebabzy@gmail.com>
2018-03-01 06:20:45 +0100

    Make Expense Claim from Employee Advance #13053 (#13074)
    * allow to make expense claim from employee advance
    
    * make expense claim only if claimed amount < paid_amount
    
    * expense claim made from employee advance is paid

--- erpnext/hr/doctype/expense_claim/expense_claim.py
+++ erpnext/hr/doctype/expense_claim/expense_claim.py
@@ -298,3 +298,29 @@ def get_advances(employee, advance_id=None):
 			`tabEmployee Advance`
 		where {0}
 	""".format(condition), as_dict=1)
+
+
+@frappe.whitelist()
+def get_expense_claim(
+	employee_name, company, employee_advance_name, posting_date, paid_amount, claimed_amount):
+	default_payable_account = frappe.db.get_value("Company", company, "default_payable_account")
+	default_cost_center = frappe.db.get_value('Company', company, 'cost_center')
+
+	expense_claim = frappe.new_doc('Expense Claim')
+	expense_claim.company = company
+	expense_claim.employee = employee_name
+	expense_claim.payable_account = default_payable_account
+	expense_claim.cost_center = default_cost_center
+	expense_claim.is_paid = 1 if flt(paid_amount) else 0
+	expense_claim.append(
+		'advances',
+		{
+			'employee_advance': employee_advance_name,
+			'posting_date': posting_date,
+			'advance_paid': flt(paid_amount),
+			'unclaimed_amount': flt(paid_amount) - flt(claimed_amount),
+			'allocated_amount': flt(paid_amount) - flt(claimed_amount)
+		}
+	)
+
+	return expense_claim
