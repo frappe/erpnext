1f0c50eef465f8d4ca34213cd91b897e72925eb2
Zarrar <zarrar65@gmail.com>
2018-03-05 12:32:41 +0530

    remove updating territory for customers on change (#13162)

--- erpnext/patches/v10_0/update_territory_and_customer_group.py
+++ erpnext/patches/v10_0/update_territory_and_customer_group.py
@@ -3,34 +3,32 @@ from frappe.model.rename_doc import get_fetch_fields
 
 def execute():
 	ignore_doctypes = ["Lead", "Opportunity", "POS Profile", "Tax Rule", "Pricing Rule"]
-	customers = frappe.get_all('Customer', fields=["name", "territory", "customer_group"])
+	customers = frappe.get_all('Customer', fields=["name", "customer_group"])
 
-	territory_fetch = get_fetch_fields('Customer', 'Territory', ignore_doctypes)
 	customer_group_fetch = get_fetch_fields('Customer', 'Customer Group', ignore_doctypes)
 
 	batch_size = 1000
 	for i in range(0, len(customers), batch_size):
 		batch_customers = customers[i:i + batch_size]
-		for source_fieldname, linked_doctypes_info in [["customer_group", customer_group_fetch], ["territory", territory_fetch]]:
-			for d in linked_doctypes_info:
-				when_then = []
-				for customer in batch_customers:
-					when_then.append('''
-						WHEN `{master_fieldname}` = "{docname}" and {linked_to_fieldname} != "{value}"
-						THEN "{value}"
-					'''.format(
-						master_fieldname=d["master_fieldname"],
-						linked_to_fieldname=d["linked_to_fieldname"],
-						docname=frappe.db.escape(frappe.as_unicode(customer.name)),
-						value=frappe.db.escape(frappe.as_unicode(customer.get(source_fieldname)))))
-				
-				frappe.db.sql("""
-					update
-						`tab{doctype}`
-					set
-						{linked_to_fieldname} = CASE {when_then_cond}  ELSE `{linked_to_fieldname}` END
-				""".format(
-					doctype = d['doctype'],
-					when_then_cond=" ".join(when_then),
-					linked_to_fieldname=d.linked_to_fieldname
-				))
+		for d in customer_group_fetch:
+			when_then = []
+			for customer in batch_customers:
+				when_then.append('''
+					WHEN `{master_fieldname}` = "{docname}" and {linked_to_fieldname} != "{value}"
+					THEN "{value}"
+				'''.format(
+					master_fieldname=d["master_fieldname"],
+					linked_to_fieldname=d["linked_to_fieldname"],
+					docname=frappe.db.escape(frappe.as_unicode(customer.name)),
+					value=frappe.db.escape(frappe.as_unicode(customer.get("customer_group")))))
+
+			frappe.db.sql("""
+				update
+					`tab{doctype}`
+				set
+					{linked_to_fieldname} = CASE {when_then_cond}  ELSE `{linked_to_fieldname}` END
+			""".format(
+				doctype = d['doctype'],
+				when_then_cond=" ".join(when_then),
+				linked_to_fieldname=d.linked_to_fieldname
+			))
