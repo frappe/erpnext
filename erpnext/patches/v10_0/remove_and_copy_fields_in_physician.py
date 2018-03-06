f7270b0a4be1f3826d93758648ad3feefac0e311
jamsheer <kuttypjamsheer@gmail.com>
2018-03-06 18:37:47 +0530

    patch fixes

--- erpnext/patches/v10_0/remove_and_copy_fields_in_physician.py
+++ erpnext/patches/v10_0/remove_and_copy_fields_in_physician.py
@@ -2,13 +2,12 @@ import frappe
 
 def execute():
     if frappe.db.exists("DocType", "Physician"):
-        if 'physician_schedule' in frappe.db.get_table_columns("Physician"):
+        frappe.reload_doc("healthcare", "doctype", "physician")
+        frappe.reload_doc("healthcare", "doctype", "physician_service_unit_schedule")
+        if frappe.db.has_column('Physician', 'physician_schedule'):
             for doc in frappe.get_all('Physician'):
                 _doc = frappe.get_doc('Physician', doc.name)
                 if _doc.physician_schedule:
                     _doc.append('physician_schedules', {'schedule': _doc.physician_schedule})
                     _doc.save()
-                    frappe.db.sql("alter table `tabPhysician` drop column physician_schedule")
-
-        if 'time_per_appointment' in frappe.db.get_table_columns("Physician"):
-            frappe.db.sql("alter table `tabPhysician` drop coloumn time_per_appointment")
+                
