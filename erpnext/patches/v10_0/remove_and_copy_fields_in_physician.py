c35665fa602c0db2578d646cd878e05d0ca21935
Zarrar <zarrar65@gmail.com>
2018-03-12 15:21:53 +0530

    update physician patch fail (#13277)

--- erpnext/patches/v10_0/remove_and_copy_fields_in_physician.py
+++ erpnext/patches/v10_0/remove_and_copy_fields_in_physician.py
@@ -5,9 +5,9 @@ def execute():
 		frappe.reload_doc("healthcare", "doctype", "physician")
 		frappe.reload_doc("healthcare", "doctype", "physician_service_unit_schedule")
 
-	if frappe.db.has_column('Physician', 'physician_schedule'):
-		for doc in frappe.get_all('Physician'):
-			_doc = frappe.get_doc('Physician', doc.name)
-			if _doc.physician_schedule:
-				_doc.append('physician_schedules', {'schedule': _doc.physician_schedule})
-				_doc.save()
+		if frappe.db.has_column('Physician', 'physician_schedule'):
+			for doc in frappe.get_all('Physician'):
+				_doc = frappe.get_doc('Physician', doc.name)
+				if _doc.physician_schedule:
+					_doc.append('physician_schedules', {'schedule': _doc.physician_schedule})
+					_doc.save()
