49c452b0af0000ecc576c17f385a8e2ba69e89ed
patilsangram <sangram.p@indictranstech.com>
2018-03-17 20:20:52 +0530

    [Shipping Plan] filters/queries, total handling unit calculation

--- erpnext/logistics/doctype/shipping_plan/shipping_plan.js
+++ erpnext/logistics/doctype/shipping_plan/shipping_plan.js
@@ -2,13 +2,51 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Shipping Plan', {
-	refresh: function(frm) {
+	setup: function(frm) {
+		frm.set_query('delivery_note', function(){
+			return{
+				filters: {
+					'docstatus': 0
+				}
+			};
+		});
 
+		frm.set_query('company_address_name', function(){
+			return {
+				filters: {
+					"is_your_company_address": 1
+				}
+			};
+		});
+
+		frm.set_query('weight_uom', 'items', function() {
+			return {
+				filters:{
+					"name": ["in", ["LB", "Kg"]]
+				}
+			}
+		});
+	},
+	company_address_name: function(frm) {
+		erpnext.utils.get_address_display(frm, 'company_address_name', 'company_address', true);
+	},
+	shipping_address_name: function(frm) {
+		erpnext.utils.get_address_display(frm, 'shipping_address_name', 'shipping_address', true);
+	},
+	set_total_handling_units: function(frm) {
+		var total_qty = 0;
+		$.each(frm.doc.items || [], function(idx, row) {
+			total_qty += row.qty;
+		});
+		frm.set_value("total_handling_unit", total_qty);
 	}
 });
 
-cur_frm.fields_dict['delivery_note'].get_query = function(doc) {
-	return{
-		filters:{ 'docstatus': 0}
-	};
-};
+frappe.ui.form.on('Shipping Plan Item', {
+	qty: function(frm, cdt, cdn) {
+		frm.trigger("set_total_handling_units");
+	},
+	items_remove: function(frm, cdt, cdn) {
+		frm.trigger("set_total_handling_units");
+	}
+})
