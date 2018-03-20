578983c17eb9ab24bad447aede62883e41521d11
Rohit Waghchaure <rohitw1991@gmail.com>
2018-03-20 21:01:27 +0530

    [Fix] Special character issue

--- erpnext/stock/dashboard/item_dashboard.js
+++ erpnext/stock/dashboard/item_dashboard.js
@@ -18,12 +18,12 @@ erpnext.stock.ItemDashboard = Class.extend({
 
 		// move
 		this.content.on('click', '.btn-move', function() {
-			erpnext.stock.move_item($(this).attr('data-item'), $(this).attr('data-warehouse'),
+			erpnext.stock.move_item(unescape($(this).attr('data-item')), $(this).attr('data-warehouse'),
 				null, $(this).attr('data-actual_qty'), null, function() { me.refresh(); });
 		});
 
 		this.content.on('click', '.btn-add', function() {
-			erpnext.stock.move_item($(this).attr('data-item'), null, $(this).attr('data-warehouse'),
+			erpnext.stock.move_item(unescape($(this).attr('data-item')), null, $(this).attr('data-warehouse'),
 				$(this).attr('data-actual_qty'), $(this).attr('data-rate'),
 				function() { me.refresh(); });
 		});
