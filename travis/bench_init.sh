1f6cf7c2c6217217b7c14a4777ddb5eaf5658f6f
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-03-20 10:29:43 +0530

    bench_init.sh added python executable flag for environment setup

--- travis/bench_init.sh
+++ travis/bench_init.sh
@@ -4,5 +4,5 @@ cd ~/
 curl -I https://github.com/frappe/frappe/tree/$TRAVIS_BRANCH | head -n 1 | cut -d $' ' -f2 | (
 	read response;
 	[ $response == '200' ] && branch=$TRAVIS_BRANCH || branch='develop';
-	bench init frappe-bench --frappe-path https://github.com/frappe/frappe.git --frappe-branch $branch
+	bench init frappe-bench --frappe-path https://github.com/frappe/frappe.git --frappe-branch $branch --python $(which python)
 )
