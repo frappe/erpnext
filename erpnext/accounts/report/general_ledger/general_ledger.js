4215c801d9237236d1b4affe68a9cba8407e638a
Shreya <shreyashah115@gmail.com>
2018-03-22 23:29:59 +0530

    error fix if party_type = student or shareholder

--- erpnext/accounts/report/general_ledger/general_ledger.js
+++ erpnext/accounts/report/general_ledger/general_ledger.js
@@ -82,11 +82,12 @@ frappe.query_reports["General Ledger"] = {
 					frappe.query_report_filters_by_name.party_name.set_value("");
 					return;
 				}
-
-				var fieldname = frappe.scrub(party_type) + "_name";
-				frappe.db.get_value(party_type, party, fieldname, function(value) {
-					frappe.query_report_filters_by_name.party_name.set_value(value[fieldname]);
-				});
+				if (party_type != "Student" && party_type != "Shareholder") {
+					var fieldname = frappe.scrub(party_type) + "_name";
+					frappe.db.get_value(party_type, party, fieldname, function(value) {
+						frappe.query_report_filters_by_name.party_name.set_value(value[fieldname]);
+					});
+				}
 			}
 		},
 		{
