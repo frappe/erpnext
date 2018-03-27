9ae4d6bf4d4160419b6a5010bc9e929d4e8461d9
Shreya Shah <shreyashah115@gmail.com>
2018-03-27 11:29:25 +0530

    [Fix] General Ledger Report (#13374)
    * error fix if party_type = student or shareholder
    
    * clear party filter when party_type changes to avoid error throw
    
    * dict with values of party_name in utils.js
    
    * fetch party_name from erpnext.utils.get_party_name
    
    * Update general_ledger.py

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -105,6 +105,12 @@ $.extend(erpnext.utils, {
 		}
 	},
 
+	get_party_name: function(party_type) {
+		var dict = {'Customer': 'customer_name', 'Supplier': 'supplier_name', 'Employee': 'employee_name',
+			'Member': 'member_name'};
+		return dict[party_type];
+	},
+
 	copy_value_in_all_row: function(doc, dt, dn, table_fieldname, fieldname) {
 		var d = locals[dt][dn];
 		if(d[fieldname]){
