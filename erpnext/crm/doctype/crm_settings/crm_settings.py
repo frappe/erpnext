89312e3f5603de2522c924f5d5dda8bf82558c93
Saurabh <bhosalesaurabh8@gmail.com>
2018-04-05 17:43:03 +0530

    Updated CRM settings and added dashboard console HTML

--- erpnext/crm/doctype/crm_settings/crm_settings.py
+++ erpnext/crm/doctype/crm_settings/crm_settings.py
@@ -8,3 +8,103 @@ from frappe.model.document import Document
 
 class CRMSettings(Document):
 	pass
+
+def make_popup(caller_no):
+	contact_lookup = frappe.get_list("Contact", or_filters={"phone":caller_no, "mobile_no":caller_no}, ignore_permissions=True)
+
+	if len(contact_lookup) > 0:
+		contact_doc = frappe.get_doc("Contact", contact_lookup[0].get("name"))
+		if(contact_doc.get_link_for('Customer')):
+			customer_name = frappe.db.get_value("Dynamic Link", {"parent":contact_doc.get("name")}, "link_name")
+			customer_full_name = frappe.db.get_value("Customer", customer_name, "customer_name")
+			popup_data = {
+				"title": "Customer",
+				"number": caller_no,
+				"name": customer_full_name,
+				"call_timestamp": frappe.utils.datetime.datetime.strftime(frappe.utils.datetime.datetime.today(), '%d/%m/%Y %H:%M:%S')
+			}
+
+			popup_html = render_popup(popup_data)
+			return popup_html
+
+		elif(contact_doc.get_link_for('Lead')):
+			lead_full_name = frappe.get_doc("Lead",contact_doc.get_link_for('Lead')).lead_name
+			popup_data = {
+				"title": "Lead",
+				"number": caller_no,
+				"name": lead_full_name,
+				"call_timestamp": frappe.utils.datetime.datetime.strftime(frappe.utils.datetime.datetime.today(), '%d/%m/%Y %H:%M:%S')
+			}
+			popup_html = render_popup(popup_data)
+			return popup_html
+
+	else:
+		popup_data = {
+			"title": "Unknown Caller",
+			"number": caller_no,
+			"name": "Unknown",
+			"call_timestamp": frappe.utils.datetime.datetime.strftime(frappe.utils.datetime.datetime.today(), '%d/%m/%Y %H:%M:%S')
+		}
+		popup_html = render_popup(popup_data)
+		return popup_html
+
+def render_popup(popup_data):
+	html = frappe.render_template("frappe/public/js/integrations/call_popup.html", popup_data)
+	return html
+
+def display_popup(caller_no):
+	# agent_no = popup_json.get("destination")
+
+	try:
+		popup_html = make_popup(caller_no)
+		# if agent_id:
+		# 	frappe.async.publish_realtime(event="msgprint", message=popup_html, user=agent_id)
+		# else:
+		users = frappe.get_all("Has Role", filters={"parenttype":"User","role":"Support Team"}, fields=["parent"])
+		agents = [user.get("parent") for user in users]
+		for agent in agents:
+			frappe.async.publish_realtime(event="msgprint", message=popup_html, user=agent)
+
+	except Exception as e:
+		frappe.log_error(message=frappe.get_traceback(), title="Error in popup display")
+
+@frappe.whitelist()
+def get_caller_info(caller_no):
+	if caller_no and len(caller_no) > 13:
+		frappe.msgprint("Please enter a valid number")
+		return
+
+	contact_lookup = frappe.get_list("Contact", or_filters={"phone":caller_no, "mobile_no":caller_no}, ignore_permissions=True)
+
+	if len(contact_lookup) > 0:
+		contact_doc = frappe.get_doc("Contact", contact_lookup[0].get("name"))
+		if(contact_doc.get_link_for('Customer')):
+			customer_name = frappe.db.get_value("Dynamic Link", {"parent":contact_doc.get("name")}, "link_name")
+			customer_full_name = frappe.db.get_value("Customer", customer_name, "customer_name")
+			popup_data = {
+				"title": "Customer",
+				"number": caller_no,
+				"name": customer_full_name,
+				"call_timestamp": frappe.utils.datetime.datetime.strftime(frappe.utils.datetime.datetime.today(), '%d/%m/%Y %H:%M:%S')
+			}
+
+			return popup_data
+
+		elif(contact_doc.get_link_for('Lead')):
+			lead_full_name = frappe.get_doc("Lead",contact_doc.get_link_for('Lead')).lead_name
+			popup_data = {
+				"title": "Lead",
+				"number": caller_no,
+				"name": lead_full_name,
+				"call_timestamp": frappe.utils.datetime.datetime.strftime(frappe.utils.datetime.datetime.today(), '%d/%m/%Y %H:%M:%S')
+			}
+			return popup_data
+
+	else:
+		popup_data = {
+			"title": "Unknown Caller",
+			"number": caller_no,
+			"name": "Unknown",
+			"call_timestamp": frappe.utils.datetime.datetime.strftime(frappe.utils.datetime.datetime.today(), '%d/%m/%Y %H:%M:%S')
+		}
+		return popup_data
