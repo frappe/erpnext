e50b944eca77a247daedb650d5f4d7c1f2b1c4fe
Joelios <joel.mesmer@libracore.com>
2018-04-05 09:15:35 +0200

    added feature to choose style discrete or continuous for heatmap in global defaults

--- erpnext/setup/doctype/global_defaults/global_defaults.py
+++ erpnext/setup/doctype/global_defaults/global_defaults.py
@@ -18,6 +18,7 @@ keydict = {
 	'account_url':'account_url',
 	'disable_rounded_total': 'disable_rounded_total',
 	'disable_in_words': 'disable_in_words',
+	'heatmap_style': 'heatmap_style',
 }
 
 from frappe.model.document import Document
