45da55f6e292efc768e6e9b0c46f48fd0f87463f
Saurabh <saurabh6790@gmail.com>
2018-04-09 17:09:26 +0530

    [fix] setup webhook for order and validate webhook request

--- erpnext/erpnext_integrations/doctype/shopify_settings/shopify_settings.js
+++ erpnext/erpnext_integrations/doctype/shopify_settings/shopify_settings.js
@@ -5,7 +5,7 @@ frappe.provide("erpnext_shopify.shopify_settings");
 
 frappe.ui.form.on("Shopify Settings", "onload", function(frm, dt, dn){
 	frappe.call({
-		method:"erpnext_shopify.erpnext_shopify.doctype.shopify_settings.shopify_settings.get_series",
+		method:"erpnext.erpnext_integrations.doctype.shopify_settings.shopify_settings.get_series",
 		callback:function(r){
 			$.each(r.message, function(key, value){
 				set_field_options(key, value)
@@ -30,58 +30,11 @@ frappe.ui.form.on("Shopify Settings", "refresh", function(frm){
 		frm.toggle_reqd("cash_bank_account", true);
 		frm.toggle_reqd("sales_order_series", true);
 		frm.toggle_reqd("customer_group", true);
-		
+
 		frm.toggle_reqd("sales_invoice_series", frm.doc.sync_sales_invoice);
 		frm.toggle_reqd("delivery_note_series", frm.doc.sync_delivery_note);
 
-		frm.add_custom_button(__('Sync Shopify'), function() {
-			frappe.call({
-				method:"erpnext_shopify.api.sync_shopify",
-			})
-		}).addClass("btn-primary");
 	}
-
-	if(!frm.doc.access_token && !frm.doc.api_key) {
-		frm.add_custom_button(__("Connect to Shopify"),
-			function(){
-				window.open("https://apps.shopify.com/erpnext");
-			}).addClass("btn-primary")
-	}
-
-	frm.add_custom_button(__("Shopify Log"), function(){
-		frappe.set_route("List", "Shopify Log");
-	})
-	
-	frm.add_custom_button(__("Reset Last Sync Date"), function(){
-		var dialog = new frappe.ui.Dialog({
-			title: __("Reset Last Sync Date"),
-			fields: [
-				{"fieldtype": "Datetime", "label": __("Date"), "fieldname": "last_sync_date", "reqd": 1 },
-				{"fieldtype": "Button", "label": __("Set last sync date"), "fieldname": "set_last_sync_date", "cssClass": "btn-primary"},
-			]
-		});
-		var args;
-		dialog.fields_dict.set_last_sync_date.$input.click(function() {
-			args = dialog.get_values();
-			if(!args) return;
-
-			frm.set_value("last_sync_datetime", args['last_sync_date']);
-			frm.save();
-
-			dialog.hide();
-		});
-		dialog.show();
-	})
-
-
-	frappe.call({
-		method: "erpnext_shopify.api.get_log_status",
-		callback: function(r) {
-			if(r.message){
-				frm.dashboard.set_headline_alert(r.message.text, r.message.alert_class)
-			}
-		}
-	})
 
 })
 
--- erpnext/erpnext_integrations/doctype/shopify_settings/shopify_settings.py
+++ erpnext/erpnext_integrations/doctype/shopify_settings/shopify_settings.py
@@ -5,16 +5,53 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-import requests.exceptions
 from frappe.model.document import Document
-from erpnext_shopify.shopify_requests import get_request
 from erpnext_shopify.exceptions import ShopifySetupError
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from six.moves.urllib.parse import urlparse
+from frappe.utils import get_request_session
+import json
 
 class ShopifySettings(Document):
 	def validate(self):
+		self.setup_custom_fields()
+
 		if self.enable_shopify == 1:
 			self.validate_access_credentials()
-			self.validate_access()
+		
+		self.setup_webhooks()
+
+	def setup_custom_fields(self):
+		dict(fieldname='shipping_bill_date', label='Shipping Bill Date',
+			fieldtype='Date', insert_after='shipping_bill_number', print_hide=1,
+			depends_on="eval:doc.invoice_type=='Export' ")
+
+		custom_fields = {
+			"Customer": [dict(fieldname='shopify_customer_id', label='Shopify Customer Id',
+				fieldtype='Data', insert_after='series', read_only=1, print_hide=1)],
+			"Address": [dict(fieldname='shopify_address_id', label='Shopify Address Id',
+				fieldtype='Data', insert_after='fax', read_only=1, print_hide=1)],
+			"Item": [
+				dict(fieldname='shopify_variant_id', label='Shopify Variant Id',
+					fieldtype='Data', insert_after='item_code', read_only=1, print_hide=1),
+				dict(fieldname='shopify_product_id', label='Shopify Product Id',
+					fieldtype='Data', insert_after='item_code', read_only=1, print_hide=1),
+				dict(fieldname='shopify_description', label='Shopify Description',
+					fieldtype='Text Editor', insert_after='description', read_only=1, print_hide=1)
+			],
+			"Sales Order": [dict(fieldname='shopify_order_id', label='Shopify Order Id',
+				fieldtype='Data', insert_after='title', read_only=1, print_hide=1)],
+			"Delivery Note":[
+				dict(fieldname='shopify_order_id', label='Shopify Order Id',
+					fieldtype='Data', insert_after='title', read_only=1, print_hide=1),
+				dict(fieldname='shopify_fulfillment_id', label='Shopify Fulfillment Id',
+					fieldtype='Data', insert_after='title', read_only=1, print_hide=1)
+			],
+			"Sales Invoice": [dict(fieldname='shopify_order_id', label='Shopify Order Id',
+				fieldtype='Data', insert_after='title', read_only=1, print_hide=1)]
+		}
+
+		create_custom_fields(custom_fields)
 
 	def validate_access_credentials(self):
 		if self.app_type == "Private":
@@ -24,21 +61,56 @@ class ShopifySettings(Document):
 		else:
 			if not (self.access_token and self.shopify_url):
 				frappe.msgprint(_("Access token or Shopify URL missing"), raise_exception=ShopifySetupError)
+	
+	def setup_webhooks(self):
+		webhooks = ["orders/create", "orders/paid", "orders/fulfilled"]
+		url = get_shopify_url('admin/webhooks.json', self)
 
-	def validate_access(self):
-		try:
-			get_request('/admin/products.json', {"api_key": self.api_key,
-				"password": self.get_password(raise_exception=False), "shopify_url": self.shopify_url,
-				"access_token": self.access_token, "app_type": self.app_type})
+		for webhook in webhooks:
+			session = get_request_session()
+			try:
+				d = session.post(url, data=json.dumps({
+					"webhook": {
+						"topic": webhook,
+						"address": get_webhook_address(),
+						"format": "json"
+						}
+					}), headers=get_header(self))
+				d.raise_for_status()
+			except Exception:
+				pass
 
-		except requests.exceptions.HTTPError:
-			# disable shopify!
-			frappe.db.rollback()
-			self.set("enable_shopify", 0)
-			frappe.db.commit()
+def get_shopify_url(path, settings):
+	if settings.app_type == "Private":
+		return 'https://{}:{}@{}/{}'.format(settings.api_key, settings.get_password('password'), settings.shopify_url, path)
+	else:
+		return 'https://{}/{}'.format(settings.shopify_url, path)
 
-			frappe.throw(_("""Invalid Shopify app credentials or access token"""), ShopifySetupError)
+def get_header(settings):
+	header = {'Content-Type': 'application/json'}
 
+	if settings.app_type == "Private":
+		return header
+	else:
+		header["X-Shopify-Access-Token"] = settings.access_token
+		return header
+
+def get_webhook_address():
+	endpoint = "/api/method/erpnext.erpnext_integrations.connectors.shopify_connection.order"
+
+	# try:
+# 		url = frappe.request.url
+# 	except RuntimeError:
+		# for CI Test to work
+	url = "https://testshop.localtunnel.me"
+
+	server_url = '{uri.scheme}://{uri.netloc}'.format(
+		uri=urlparse(url)
+	)
+
+	delivery_url = server_url + endpoint
+
+	return delivery_url
 
 @frappe.whitelist()
 def get_series():
