c3d6cf3fec7835443d1da52cb607e4695bee9beb
Saurabh <saurabh6790@gmail.com>
2018-04-11 08:56:30 +0530

    [Patch][fix] use remove_from_installed_apps instead of remove app

--- erpnext/patches/v11_0/refactor_erpnext_shopify.py
+++ erpnext/patches/v11_0/refactor_erpnext_shopify.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
 import frappe
-from frappe.installer import remove_app, remove_from_installed_apps
+from frappe.installer import remove_from_installed_apps
 
 def execute():
 	frappe.reload_doc('erpnext_integrations', 'doctype', 'shopify_settings')
 	frappe.reload_doc('erpnext_integrations', 'doctype', 'shopify_tax_account')
 
 	if 'erpnext_shopify' in frappe.get_installed_apps():
-		remove_app('erpnext_shopify', yes=True)
+		remove_from_installed_apps('erpnext_shopify')
 
+	frappe.delete_doc("DocType", 'Shopify Log')
+	frappe.db.sql('delete from `tabDesktop Icon` where app="erpnext_shopify" ')
+	frappe.db.sql("drop table `tabShopify Log`")
+	frappe.delete_doc("Module Def", 'erpnext_shopify')
