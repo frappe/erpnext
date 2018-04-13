2334c428a82f3d5691baedd239d0557da5a7e68f
Shreya <shreyashah115@gmail.com>
2018-04-13 15:29:10 +0530

    set query in department

--- erpnext/hr/doctype/department/department.js
+++ erpnext/hr/doctype/department/department.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Department', {
-	refresh: function(frm) {
-
+	onload: function(frm) {
+		frm.set_query("leave_approver", "leave_approvers", function(doc) {
+			return {
+				query:"erpnext.hr.doctype.employee_leave_approver.employee_leave_approver.get_approvers",
+				filters:{
+					user: doc.user_id
+				}
+			}
+		});
+		frm.set_query("expense_approver", "expense_approvers", function(doc) {
+			return {
+				query:"erpnext.hr.doctype.employee_expense_approver.employee_expense_approver.get_approvers",
+				filters:{
+					user: doc.user_id
+				}
+			}
+		});
 	}
 });
--- erpnext/hr/doctype/employee_expense_approver/employee_expense_approver.py
+++ erpnext/hr/doctype/employee_expense_approver/employee_expense_approver.py
@@ -8,3 +8,10 @@ from frappe.model.document import Document
 
 class EmployeeExpenseApprover(Document):
 	pass
+
+def get_approvers(doctype, txt, searchfield, start, page_len, filters):
+	return get_approver_list(filters.get("user"))
+
+def get_approver_list(name):
+	return frappe.db.sql("""select user.name, user.first_name, user.last_name from
+		tabUser user where user.enabled and user.name != %s""", name or "")
