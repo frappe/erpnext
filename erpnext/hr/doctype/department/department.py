cdeb584be15a50940cb336631bda0c1ad56d9c08
Shreya <shreyashah115@gmail.com>
2018-04-14 11:59:29 +0530

    removed hard coding of roles

--- erpnext/hr/doctype/department/department.py
+++ erpnext/hr/doctype/department/department.py
@@ -8,20 +8,6 @@ from erpnext.utilities.transaction_base import delete_events
 from frappe.model.document import Document
 
 class Department(NestedSet):
-	def validate(self):
-		self.validate_employee_leave_approver()
-		self.validate_employee_expense_approver()
-
-	def validate_employee_leave_approver(self):
-		for l in self.get("leave_approvers")[:]:
-			if "Leave Approver" not in frappe.get_roles(l.leave_approver):
-				frappe.get_doc("User", l.leave_approver).add_roles("Leave Approver")
-
-	def validate_employee_expense_approver(self):
-		for e in self.get("expense_approvers")[:]:
-			if "Expense Approver" not in frappe.get_roles(e.expense_approver):
-				frappe.get_doc("User", e.expense_approver).add_roles("Expense Approver")
-
 	nsm_parent_field = 'parent_department'
 
 	def update_nsm_model(self):
