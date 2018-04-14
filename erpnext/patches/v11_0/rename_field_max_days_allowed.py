7d8398a83f9dc97011fea05bc053064898913800
Shreya <shreyashah115@gmail.com>
2018-04-14 11:47:14 +0530

    Employee Property History child table

--- erpnext/patches/v11_0/rename_field_max_days_allowed.py
+++ erpnext/patches/v11_0/rename_field_max_days_allowed.py
@@ -3,4 +3,5 @@ from frappe.model.utils.rename_field import rename_field
 
 def execute():
 	frappe.reload_doc("hr", "doctype", "leave_type")
-	rename_field("Leave Type", "max_days_allowed", "max_continuous_days_allowed")
+	frappe.db.sql("""ALTER table `tabLeave Type` modify max_days_allowed int(8) NOT NULL""")
+	rename_field("Leave Type", "max_days_allowed", "max_continuous_days_allowed") 
