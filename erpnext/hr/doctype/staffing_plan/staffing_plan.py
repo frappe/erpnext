310f4f8e0515829e996b344c01535f9b3195e6a2
anoop <anoop@earthianslive.com>
2018-04-16 15:20:35 +0530

    staffing plan - py get emp count for designation and js calc vacancy, estimated cost and budget

--- erpnext/hr/doctype/staffing_plan/staffing_plan.py
+++ erpnext/hr/doctype/staffing_plan/staffing_plan.py
@@ -8,3 +8,11 @@ from frappe.model.document import Document
 
 class StaffingPlan(Document):
 	pass
+#ToDo: Validate duplicate designations in Staffing Plan Detail
+
+@frappe.whitelist()
+def get_current_employee_count(designation):
+	if not designation:
+		return False
+	employee_count = frappe.db.sql("""select count(*) from `tabEmployee` where designation = %s and status='Active'""", designation)[0][0]
+	return employee_count
