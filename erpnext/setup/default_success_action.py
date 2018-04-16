53f8ba36ae10729dc3cf90ecfffa826e4ddc964a
Suraj Shetty <surajshetty3416@gmail.com>
2018-04-16 15:18:49 +0530

    rename for_doctype to ref_doctype

--- erpnext/setup/default_success_action.py
+++ erpnext/setup/default_success_action.py
@@ -1,44 +1,44 @@
 DEFAULT_SUCCESS_ACTION = [
     {
         'doctype': 'Success Action',
-        'for_doctype': 'Purchase Receipt',
+        'ref_doctype': 'Purchase Receipt',
         'message': 'Purchase Receipt has been submitted successfully',
-        'first_creation_message': 'First Purchase Receipt has been submitted successfully',
+        'first_success_message': 'First Purchase Receipt has been submitted successfully',
         'next_actions': 'New\nPrint\nEmail'
     },
     {
         'doctype': 'Success Action',
-        'for_doctype': 'Delivery Note',
+        'ref_doctype': 'Delivery Note',
         'message': 'Delivery Note has been submitted successfully',
-        'first_creation_message': 'First Delivery Note has been submitted successfully',
+        'first_success_message': 'First Delivery Note has been submitted successfully',
         'next_actions': 'New\nPrint\nEmail'
     },
     {
         'doctype': 'Success Action',
-        'for_doctype': 'Sales Order',
+        'ref_doctype': 'Sales Order',
         'message': 'Sales Order has been submitted successfully',
-        'first_creation_message': 'First Sales Order has been submitted successfully',
+        'first_success_message': 'First Sales Order has been submitted successfully',
         'next_actions': 'New\nPrint\nEmail'
     },
     {
         'doctype': 'Success Action',
-        'for_doctype': 'Quotation',
+        'ref_doctype': 'Quotation',
         'message': 'Quotation has been submitted successfully',
-        'first_creation_message': 'First Quotation has been submitted successfully',
+        'first_success_message': 'First Quotation has been submitted successfully',
         'next_actions': 'New\nPrint\nEmail'
     },
     {
         'doctype': 'Success Action',
-        'for_doctype': 'Sales Invoice',
+        'ref_doctype': 'Sales Invoice',
         'message': 'Sales Invoice has been submitted successfully',
-        'first_creation_message': 'First Sales Invoice has been submitted successfully',
+        'first_success_message': 'First Sales Invoice has been submitted successfully',
         'next_actions': 'New\nPrint\nEmail'
     },
     {
         'doctype': 'Success Action',
-        'for_doctype': 'Purchase Invoice',
+        'ref_doctype': 'Purchase Invoice',
         'message': 'Purchase Invoice has been submitted successfully',
-        'first_creation_message': 'First Purchase Invoice has been submitted successfully',
+        'first_success_message': 'First Purchase Invoice has been submitted successfully',
         'next_actions': 'New\nPrint\nEmail'
     }
 ]
