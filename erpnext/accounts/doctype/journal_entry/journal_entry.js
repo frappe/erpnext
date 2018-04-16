735710b2df981708bc217956e6d47d0f33642d55
deepak-mnt <deepak@mntechnique.com>
2018-04-16 17:38:35 +0530

    Added naming series for reverse journal entry

--- erpnext/accounts/doctype/journal_entry/journal_entry.js
+++ erpnext/accounts/doctype/journal_entry/journal_entry.js
@@ -21,6 +21,9 @@ frappe.ui.form.on("Journal Entry", {
 				};
 				frappe.set_route("query-report", "General Ledger");
 			}, "fa fa-table");
+		}
+
+		if(frm.doc.docstatus==1 && frm.doc.naming_series=="JV-") {
 			frm.add_custom_button(__('Reverse Journal Entry'), function() {
 				return erpnext.journal_entry.reverse_journal_entry(frm);
 			});
