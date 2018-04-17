56ed39fdb7d2af7f427ff0ade062464099b3533d
jamsheer <kuttypjamsheer@gmail.com>
2018-04-17 11:53:32 +0530

    Leave Period - set to_date based on from_date

--- erpnext/hr/doctype/leave_period/leave_period.js
+++ erpnext/hr/doctype/leave_period/leave_period.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Leave Period', {
-	refresh: function(frm) {
-
+	from_date: (frm)=>{
+		if (frm.doc.from_date && !frm.doc.to_date) {
+			var a_year_from_start = frappe.datetime.add_months(frm.doc.from_date, 12);
+			frm.set_value("to_date", frappe.datetime.add_days(a_year_from_start, -1));
+		}
 	}
 });
