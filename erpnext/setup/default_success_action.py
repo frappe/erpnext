ae94904b3e9626d8d90d4d2b071c84c5f636cd49
Suraj Shetty <surajshetty3416@gmail.com>
2018-04-17 11:45:20 +0530

    case change

--- erpnext/setup/default_success_action.py
+++ erpnext/setup/default_success_action.py
@@ -4,41 +4,41 @@ DEFAULT_SUCCESS_ACTION = [
         'ref_doctype': 'Purchase Receipt',
         'message': 'Purchase Receipt has been submitted successfully',
         'first_success_message': 'First Purchase Receipt has been submitted successfully',
-        'next_actions': 'New\nPrint\nEmail'
+        'next_actions': 'new\nprint\nemail'
     },
     {
         'doctype': 'Success Action',
         'ref_doctype': 'Delivery Note',
         'message': 'Delivery Note has been submitted successfully',
         'first_success_message': 'First Delivery Note has been submitted successfully',
-        'next_actions': 'New\nPrint\nEmail'
+        'next_actions': 'new\nprint\nemail'
     },
     {
         'doctype': 'Success Action',
         'ref_doctype': 'Sales Order',
         'message': 'Sales Order has been submitted successfully',
         'first_success_message': 'First Sales Order has been submitted successfully',
-        'next_actions': 'New\nPrint\nEmail'
+        'next_actions': 'new\nprint\nemail'
     },
     {
         'doctype': 'Success Action',
         'ref_doctype': 'Quotation',
         'message': 'Quotation has been submitted successfully',
         'first_success_message': 'First Quotation has been submitted successfully',
-        'next_actions': 'New\nPrint\nEmail'
+        'next_actions': 'new\nprint\nemail'
     },
     {
         'doctype': 'Success Action',
         'ref_doctype': 'Sales Invoice',
         'message': 'Sales Invoice has been submitted successfully',
         'first_success_message': 'First Sales Invoice has been submitted successfully',
-        'next_actions': 'New\nPrint\nEmail'
+        'next_actions': 'new\nprint\nemail'
     },
     {
         'doctype': 'Success Action',
         'ref_doctype': 'Purchase Invoice',
         'message': 'Purchase Invoice has been submitted successfully',
         'first_success_message': 'First Purchase Invoice has been submitted successfully',
-        'next_actions': 'New\nPrint\nEmail'
+        'next_actions': 'new\nprint\nemail'
     }
 ]
