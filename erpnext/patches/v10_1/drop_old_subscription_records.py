20721942ad3bac1776cf17e983f0b04c2fb47f78
tundebabzy <tundebabzy@gmail.com>
2018-04-17 11:31:23 +0100

    code review: use `delete from` to delete records

--- erpnext/patches/v10_1/drop_old_subscription_records.py
+++ erpnext/patches/v10_1/drop_old_subscription_records.py
@@ -3,10 +3,4 @@ import frappe
 
 
 def execute():
-	subscriptions = frappe.db.sql('SELECT name from `tabSubscription`', as_dict=True)
-
-	for subscription in subscriptions:
-		doc = frappe.get_doc('Subscription', subscription['name'])
-		if doc.docstatus == 1:
-			doc.cancel()
-		frappe.delete_doc('Subscription', subscription['name'], force=True, ignore_permissions=True)
+	frappe.db.sql('DELETE from `tabSubscription`')
