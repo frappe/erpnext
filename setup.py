ff8f1bc88e21c042b78e9b5eb9eabc4c6b0b7c39
Rushabh Mehta <rmehta@gmail.com>
2018-04-18 02:57:44 -0800

    Revert "removed bad code and made compatible with pip 10 (#13685)" (#13713)
    This reverts commit 91b0dce4d8e838c17bbb7bc7ff41913650641a45.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+try: # for pip >= 10
+	from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+	from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in erpnext/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
 
 with open('erpnext/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
@@ -23,5 +24,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
