4437329c22c7c694c0ca10991998e73347770592
deepak-mnt <deepak@mntechnique.com>
2018-04-19 21:12:51 +0530

    Added Journal Entry button and functionality

--- erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.py
+++ erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe, erpnext
+from frappe.utils import nowdate
 from frappe.model.document import Document
 from erpnext.accounts.doctype.journal_entry.journal_entry import get_average_exchange_rate,get_balance_on
 class ExchangeRateRevaluation(Document):
@@ -36,3 +37,45 @@ class ExchangeRateRevaluation(Document):
 			where is_group=0 and report_type='Balance Sheet' and root_type in ('Asset','Liability') and company=%s
 			and not account_currency=%s order by name""",(self.company,company_currency))
 		return accounts
+
+
+	def make_jv_entry(self, accounts, total_gain_loss):
+		exchange_gain_loss = frappe.get_doc("Company",self.company).unrealized_exchange_gain_loss_account
+		if exchange_gain_loss:
+			journal_entry = frappe.new_doc('Journal Entry')
+			journal_entry.voucher_type = 'Exchange Rate Revaluation'
+			journal_entry.company = self.company
+			journal_entry.posting_date = nowdate()
+			journal_entry.multi_currency = 1
+
+			account_amt_list = []
+
+			for acc in accounts:
+				account_amt_list.append({
+					"account": acc.get("account"),
+					"balance":get_balance_on(acc.get("account")),
+					"debit_in_account_currency": acc.get("balance_in_alternate_currency"),
+					"exchange_rate":acc.get("new_exchange_rate"),
+					"reference_type": "Exchange Rate Revaluation",
+					"reference_name": self.name,
+					})
+				account_amt_list.append({
+					"account": acc.get("account"),
+					"balance":get_balance_on(acc.get("account")),
+					"credit_in_account_currency": acc.get("balance_in_alternate_currency"),
+					"exchange_rate":acc.get("current_exchange_rate"),
+					"reference_type": "Exchange Rate Revaluation",
+					"reference_name": self.name,
+					})
+			account_amt_list.append({
+				"account": exchange_gain_loss,
+				"balance":get_balance_on(exchange_gain_loss),
+				"debit_in_account_currency": total_gain_loss if total_gain_loss < 0 else 0,
+				"credit_in_account_currency": total_gain_loss if total_gain_loss > 0 else 0,
+				"reference_type": "Exchange Rate Revaluation",
+				"reference_name": self.name,
+				})
+			journal_entry.set("accounts", account_amt_list)
+			return journal_entry.as_dict()
+		else:
+			frappe.msgprint("Set the Unrealized Exchange / Gain Loss Account field in Company field")	
