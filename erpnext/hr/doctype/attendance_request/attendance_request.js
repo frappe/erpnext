d568409e0655804d89b2ecd538a392483fa108cf
jamsheer <kuttypjamsheer@gmail.com>
2018-04-19 17:04:22 +0530

    Attendance Request - validate and create submit and cancel attendance

--- erpnext/hr/doctype/attendance_request/attendance_request.js
+++ erpnext/hr/doctype/attendance_request/attendance_request.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
+cur_frm.add_fetch('employee', 'company', 'company');
 
 frappe.ui.form.on('Attendance Request', {
 	refresh: function(frm) {
