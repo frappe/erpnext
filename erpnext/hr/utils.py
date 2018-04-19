efe483515fdf621fa152669de297e0febc420abf
jamsheer <kuttypjamsheer@gmail.com>
2018-04-19 17:03:23 +0530

    HR - util - method - validate_dates

--- erpnext/hr/utils.py
+++ erpnext/hr/utils.py
@@ -4,7 +4,20 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+from frappe.utils import getdate, nowdate
 
 def set_employee_name(doc):
 	if doc.employee and not doc.employee_name:
 		doc.employee_name = frappe.db.get_value("Employee", doc.employee, "employee_name")
+
+def validate_dates(doc, from_date, to_date):
+	date_of_joining, relieving_date = frappe.db.get_value("Employee", doc.employee, ["date_of_joining", "relieving_date"])
+	if getdate(from_date) > getdate(to_date):
+		frappe.throw(_("To date can not be less than from date"))
+	elif getdate(from_date) > getdate(nowdate()):
+		frappe.throw(_("Future dates not allowed"))
+	elif date_of_joining and getdate(from_date) < getdate(date_of_joining):
+		frappe.throw(_("From date can not be less than employee's joining date"))
+	elif relieving_date and getdate(to_date) > getdate(relieving_date):
+		frappe.throw(_("To date can not greater than employee's relieving date"))
+
