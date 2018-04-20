c88ce364cef3450b81eb8ed00adfad50378a474f
Zlash65 <zarrar65@gmail.com>
2018-04-20 11:06:15 +0530

    treeview implemented for supplier group

--- erpnext/patches/v11_0/rename_supplier_type_to_supplier_group.py
+++ erpnext/patches/v11_0/rename_supplier_type_to_supplier_group.py
@@ -1,6 +1,8 @@
 import frappe
 from frappe.model.rename_doc import rename_doc
 from frappe.model.utils.rename_field import rename_field
+from frappe import _
+from frappe.utils.nestedset import rebuild_tree
 
 def execute():
 	if frappe.db.table_exists("Supplier Type") and not frappe.db.table_exists("Supplier Group"):
@@ -15,3 +17,18 @@ def execute():
 		rename_field("Buying Settings", "supplier_type", "supplier_group")
 		rename_field("Pricing Rule", "supplier_type", "supplier_group")
 		rename_field("Tax Rule", "supplier_type", "supplier_group")
+
+	build_tree()
+
+def build_tree():
+	if not frappe.db.exists("Supplier Group", _('All Supplier Groups')):
+		frappe.get_doc({
+			'doctype': 'Supplier Group',
+			'supplier_group_name': _('All Supplier Groups'),
+			'is_group': 1
+		}).insert(ignore_permissions=True)
+
+	frappe.db.sql("""update `tabSupplier Group` set parent_supplier_group = '{0}'
+		where is_group = 0""".format(_('All Supplier Groups')))
+
+	rebuild_tree("Supplier Group", "parent_supplier_group")
