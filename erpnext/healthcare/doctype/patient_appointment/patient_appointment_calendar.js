68a7ee22f2d8cdf25bbbd47edecc1f38a6c9df8e
Zarrar <zarrar65@gmail.com>
2018-04-23 13:22:03 +0530

    order_by for gantt error fix (#13752)

--- erpnext/healthcare/doctype/patient_appointment/patient_appointment_calendar.js
+++ erpnext/healthcare/doctype/patient_appointment/patient_appointment_calendar.js
@@ -8,6 +8,7 @@ frappe.views.calendar["Patient Appointment"] = {
 		"allDay": "allDay",
 		"eventColor": "color"
 	},
+	order_by: "appointment_date",
 	gantt: true,
 	get_events_method: "erpnext.healthcare.doctype.patient_appointment.patient_appointment.get_events",
 	filters: [
