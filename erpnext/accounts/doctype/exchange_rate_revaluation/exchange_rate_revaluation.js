2d27fac1a9b22298e03f823ec50003c0bc4a30b7
deepak-mnt <deepak@mntechnique.com>
2018-04-26 13:45:22 +0530

    Added account data on child table

--- erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.js
+++ erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.js
@@ -3,6 +3,61 @@
 
 frappe.ui.form.on('Exchange Rate Revaluation', {
 	refresh: function(frm) {
+	},
 
+	get_entries: function(frm) {
+		frappe.call({
+			method: "get_accounts_data",
+			doc: cur_frm.doc,
+			callback: function(r){
+				frappe.model.clear_table(frm.doc, "exchange_rate_revaluation_account");
+				r.message.forEach((d) => {
+					cur_frm.add_child("exchange_rate_revaluation_account",d);
+				});
+				refresh_field("exchange_rate_revaluation_account");
+				frm.events.get_total_gain_loss(frm);
+			}
+		});
+	},
+
+	get_total_gain_loss: function(frm) {
+		frm.doc.total_gain_loss = 0;
+		frm.doc.exchange_rate_revaluation_account.forEach((d) => {
+			frm.doc.total_gain_loss += d.difference;
+		});
+		refresh_field("total_gain_loss");
+	}
+
+});
+
+frappe.ui.form.on("Exchange Rate Revaluation Account", {
+	new_exchange_rate: function(frm) {
+		frm.doc.exchange_rate_revaluation_account.forEach((d) => {
+			d.new_balance_in_base_currency = d.new_exchange_rate * d.balance_in_alternate_currency;
+			d.difference = d.new_balance_in_base_currency - d.balance_in_base_currency;
+		});
+		refresh_field("exchange_rate_revaluation_account");
+		frm.events.get_total_gain_loss(frm);
+	},
+
+	account: function(frm, cdt, cdn) {
+		var row = frappe.get_doc(cdt,cdn);
+		frappe.call({
+			method: "get_accounts_data",
+			doc: cur_frm.doc,
+			args:{ account:row.account},
+			callback: function(r){
+				row.balance_in_base_currency = r.message[0].balance_in_base_currency;
+				row.balance_in_alternate_currency = r.message[0].balance_in_alternate_currency;
+				row.current_exchange_rate = r.message[0].current_exchange_rate;
+				row.difference = r.message[0].difference;
+				refresh_field("exchange_rate_revaluation_account");
+				frm.events.get_total_gain_loss(frm);
+			}
+		});
+	},
+
+	exchange_rate_revaluation_account_remove: function(frm) {
+		frm.events.get_total_gain_loss(frm);
 	}
 });
--- erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.py
+++ erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.py
@@ -3,8 +3,46 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, erpnext
+from frappe.utils import nowdate
 from frappe.model.document import Document
+from erpnext.setup.utils import get_exchange_rate
+from erpnext.accounts.doctype.account.account import get_account_currency
+from erpnext.accounts.doctype.journal_entry.journal_entry import get_average_exchange_rate,get_balance_on
 
 class ExchangeRateRevaluation(Document):
-	pass
+	def get_accounts_data(self, account=None):
+		child_table = []
+		if self.company:
+			if account:
+				accounts = [account]
+			else:	
+				accounts = self.get_accounts()
+			for i in accounts:
+				balance = get_balance_on(i, in_account_currency=False)
+				company_currency = erpnext.get_company_currency(self.company)
+				account_currency = get_account_currency(i)
+				new_exchange_rate = get_exchange_rate(account_currency, company_currency, self.posting_date)
+				new_balance_in_base_currency = get_balance_on(i) * new_exchange_rate
+				if balance:
+					child_table.append({
+							"account":i,
+							"balance_in_base_currency": balance,
+							"balance_in_alternate_currency": get_balance_on(i),
+							"current_exchange_rate": get_average_exchange_rate(i),
+							"new_exchange_rate": new_exchange_rate,
+							"new_balance_in_base_currency": new_balance_in_base_currency,
+							"difference": (new_balance_in_base_currency - balance)
+						})
+			return child_table
+		else :
+			return frappe.msgprint("Company is not selected")
+
+
+	def get_accounts(self):
+		company_currency = erpnext.get_company_currency(self.company)
+		accounts = frappe.db.sql_list("""
+			select name from tabAccount
+			where is_group=0 and report_type='Balance Sheet' and root_type in ('Asset','Liability') and company=%s
+			and not account_currency=%s order by name""",(self.company,company_currency))
+		return accounts
