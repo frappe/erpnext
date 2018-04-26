918c77aa48418fd9876ba1501b0e1f88ba0a26c3
Jamsheer <jamsheer@earthianslive.com>
2018-04-26 17:03:10 +0530

    Leave Allocation - fix - validate_leave_allocation_days

--- erpnext/hr/doctype/leave_allocation/leave_allocation.py
+++ erpnext/hr/doctype/leave_allocation/leave_allocation.py
@@ -31,13 +31,14 @@ class LeaveAllocation(Document):
 		company = frappe.db.get_value("Employee", self.employee, "company")
 		leave_period = get_leave_period(self.from_date, self.to_date, company)
 		max_leaves_allowed = frappe.db.get_value("Leave Type", self.leave_type, "max_leaves_allowed")
-		leave_allocated = 0
-		if leave_period:
-			leave_allocated = get_leave_allocation_for_period(self.employee, self.leave_type, leave_period[0].from_date, leave_period[0].to_date)
-		leave_allocated += self.new_leaves_allocated
-		if leave_allocated > max_leaves_allowed:
-			frappe.throw(_("Total allocated leaves are more days than maximum allocation of {0} leave type for employee {1} in the period")\
-			.format(self.leave_type, self.employee))
+		if max_leaves_allowed > 0:
+			leave_allocated = 0
+			if leave_period:
+				leave_allocated = get_leave_allocation_for_period(self.employee, self.leave_type, leave_period[0].from_date, leave_period[0].to_date)
+			leave_allocated += self.new_leaves_allocated
+			if leave_allocated > max_leaves_allowed:
+				frappe.throw(_("Total allocated leaves are more days than maximum allocation of {0} leave type for employee {1} in the period")\
+				.format(self.leave_type, self.employee))
 
 	def on_update_after_submit(self):
 		self.validate_new_leaves_allocated_value()
