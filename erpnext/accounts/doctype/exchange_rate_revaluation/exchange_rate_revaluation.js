f17cca9bd77c55a91d12e4b98fbf2828df9e3ca4
Nabin Hait <nabinhait@gmail.com>
2018-05-05 18:15:20 +0530

    Refactoring and cleanup of Exchange Rate Revaluation

--- erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.js
+++ erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation.js
@@ -15,31 +15,35 @@ frappe.ui.form.on('Exchange Rate Revaluation', {
 			method: "get_accounts_data",
 			doc: cur_frm.doc,
 			callback: function(r){
-				frappe.model.clear_table(frm.doc, "exchange_rate_revaluation_account");
-				r.message.forEach((d) => {
-					cur_frm.add_child("exchange_rate_revaluation_account",d);
-				});
-				refresh_field("exchange_rate_revaluation_account");
-				frm.events.get_total_gain_loss(frm);
+				frappe.model.clear_table(frm.doc, "accounts");
+				if(r.message) {
+					r.message.forEach((d) => {
+						cur_frm.add_child("accounts",d);
+					});
+					frm.events.get_total_gain_loss(frm);
+					refresh_field("accounts");
+				}
 			}
 		});
 	},
 
 	get_total_gain_loss: function(frm) {
-		frm.doc.total_gain_loss = 0;
-		frm.doc.exchange_rate_revaluation_account.forEach((d) => {
-			frm.doc.total_gain_loss += d.difference;
+		if(!(frm.doc.accounts && frm.doc.accounts.length)) return;
+
+		let total_gain_loss = 0;
+		frm.doc.accounts.forEach((d) => {
+			d.gain_loss = flt(d.new_balance_in_base_currency, precision("new_balance_in_base_currency", d)) - flt(d.balance_in_base_currency, precision("balance_in_base_currency", d));
+			total_gain_loss += flt(d.gain_loss, precision("gain_loss", d));
 		});
-		refresh_field("total_gain_loss");
+		
+		frm.set_value("total_gain_loss", flt(total_gain_loss, precision("total_gain_loss")));
+		frm.refresh_fields();
 	},
 
 	make_jv : function(frm) {
 		frappe.call({
 			method: "make_jv_entry",
-			doc: cur_frm.doc,
-			args:{  "accounts":cur_frm.doc.exchange_rate_revaluation_account,
-					"total_gain_loss":cur_frm.doc.total_gain_loss
-				 },
+			doc: frm.doc,
 			callback: function(r){
 				if (r.message)
 					var doc = frappe.model.sync(r.message)[0];
@@ -47,37 +51,49 @@ frappe.ui.form.on('Exchange Rate Revaluation', {
 			}
 		});
 	}
-
 });
 
 frappe.ui.form.on("Exchange Rate Revaluation Account", {
-	new_exchange_rate: function(frm) {
-		frm.doc.exchange_rate_revaluation_account.forEach((d) => {
-			d.new_balance_in_base_currency = d.new_exchange_rate * d.balance_in_alternate_currency;
-			d.difference = d.new_balance_in_base_currency - d.balance_in_base_currency;
-		});
-		refresh_field("exchange_rate_revaluation_account");
+	new_exchange_rate: function(frm, cdt, cdn) {
+		var row = frappe.get_doc(cdt, cdn);
+		row.new_balance_in_base_currency = flt(row.new_exchange_rate * flt(row.balance_in_account_currency),
+			precision("new_balance_in_base_currency", row));
+		row.gain_loss = row.new_balance_in_base_currency - flt(row.balance_in_base_currency);
+		refresh_field("accounts");
 		frm.events.get_total_gain_loss(frm);
 	},
 
 	account: function(frm, cdt, cdn) {
-		var row = frappe.get_doc(cdt,cdn);
-		frappe.call({
-			method: "get_accounts_data",
-			doc: cur_frm.doc,
-			args:{ account:row.account},
-			callback: function(r){
-				row.balance_in_base_currency = r.message[0].balance_in_base_currency;
-				row.balance_in_alternate_currency = r.message[0].balance_in_alternate_currency;
-				row.current_exchange_rate = r.message[0].current_exchange_rate;
-				row.difference = r.message[0].difference;
-				refresh_field("exchange_rate_revaluation_account");
-				frm.events.get_total_gain_loss(frm);
-			}
-		});
+		get_account_details(frm, cdt, cdn);
 	},
 
-	exchange_rate_revaluation_account_remove: function(frm) {
+	party: function(frm, cdt, cdn) {
+		get_account_details(frm, cdt, cdn);
+	},
+
+	accounts_remove: function(frm) {
 		frm.events.get_total_gain_loss(frm);
 	}
 });
+
+var get_account_details = function(frm, cdt, cdn) {
+	var row = frappe.get_doc(cdt, cdn);
+	if(!frm.doc.company || !frm.doc.posting_date) {
+		frappe.throw(__("Please select Company and Posting Date to getting entries"));
+	}
+	frappe.call({
+		method: "erpnext.accounts.doctype.exchange_rate_revaluation.exchange_rate_revaluation.get_account_details",
+		args:{
+			account: row.account,
+			company: frm.doc.company,
+			posting_date: frm.doc.posting_date,
+			party_type: row.party_type,
+			party: row.party
+		},
+		callback: function(r){
+			$.extend(row, r.message);
+			refresh_field("accounts");
+			frm.events.get_total_gain_loss(frm);
+		}
+	});
+};
