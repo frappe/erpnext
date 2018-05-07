c0c507a799a9ae3eafee860492414ca13a6f9033
Manas Solanki <manassolanki@gmail.com>
2018-05-07 13:37:34 +0530

    optimize the patch

--- erpnext/patches/v11_0/move_item_defaults_to_child_table_for_multicompany.py
+++ erpnext/patches/v11_0/move_item_defaults_to_child_table_for_multicompany.py
@@ -15,50 +15,45 @@ def execute():
 	frappe.reload_doc('stock', 'doctype', 'item_default')
 	frappe.reload_doc('stock', 'doctype', 'item')
 
-	item_details = frappe.get_all("Item", fields=["name", "default_warehouse", "buying_cost_center",
-								"expense_account", "selling_cost_center", "income_account"], limit=100)
+	companies = frappe.get_all("Company")
+	if len(companies) == 1:
+		frappe.db.sql('''
+				INSERT INTO `tabItem Default`
+					(name, parent, parenttype, parentfield, idx, company, default_warehouse,
+					buying_cost_center, selling_cost_center, expense_account, income_account, default_supplier)
+				SELECT
+					SUBSTRING(SHA2(name,224), 1, 10) as name, name as parent, 'Item' as parenttype,
+					'item_defaults' as parentfield, 1 as idx, %s as company, default_warehouse,
+					buying_cost_center, selling_cost_center, expense_account, income_account, default_supplier
+				FROM `tabItem`;
+		''', companies[0].name)
+	else:
+		item_details = frappe.get_all("Item", fields=["name", "default_warehouse", "buying_cost_center",
+									"expense_account", "selling_cost_center", "income_account"], limit=100)
 
-	for item in item_details:
-		item_defaults = []
+		for item in item_details:
+			item_defaults = []
 
-		def insert_into_item_defaults(doc_field_name, doc_field_value, company):
-			for d in item_defaults:
-				if d.get("company") == company:
-					d[doc_field_name] = doc_field_value
-					return
-			item_defaults.append({
-				"company": company,
-				doc_field_name: doc_field_value
-			})
+			def insert_into_item_defaults(doc_field_name, doc_field_value, company):
+				for d in item_defaults:
+					if d.get("company") == company:
+						d[doc_field_name] = doc_field_value
+						return
+				item_defaults.append({
+					"company": company,
+					doc_field_name: doc_field_value
+				})
 
-		if item.default_warehouse:
-			default_warehouse_company = frappe.get_value("Warehouse", item.default_warehouse, "company", cache=True)
-			insert_into_item_defaults("default_warehouse", item.default_warehouse, default_warehouse_company)
+			for d in [
+						["default_warehouse", "Warehouse"], ["expense_account", "Account"], ["expense_account", "Account"],
+						["buying_cost_center", "Cost Center"], ["selling_cost_center", "Cost Center"]
+					]:
+				if item.get(d[0]):
+					company = frappe.get_value(d[1], item.get(d[0]), "company", cache=True)
+					insert_into_item_defaults(d[0], item.get(d[0]), company)
 
-		if item.buying_cost_center:
-			buying_cost_center_company = get_cost_center_company(item.buying_cost_center)
-			insert_into_item_defaults("buying_cost_center", item.buying_cost_center, buying_cost_center_company)
+			doc = frappe.get_doc("Item", item.name)
+			doc.extend("item_defaults", item_defaults)
 
-		if item.selling_cost_center:
-			selling_cost_center_company = get_cost_center_company(item.buying_cost_center)
-			insert_into_item_defaults("selling_cost_center", item.selling_cost_center, selling_cost_center_company)
-
-		if item.expense_account:
-			expense_account_company = get_account_company(item.expense_account)
-			insert_into_item_defaults("expense_account", item.expense_account, expense_account_company)
-
-		if item.income_account:
-			income_account_company = get_account_company(item.income_account)
-			insert_into_item_defaults("income_account", item.income_account, income_account_company)
-
-		doc = frappe.get_doc("Item", item.name)
-		doc.extend("item_defaults", item_defaults)
-		
-		for child_doc in doc.item_defaults:
-			child_doc.db_update()
-
-def get_account_company(account_name):
-	return frappe.get_value("Account", account_name, "company", cache=True)
-
-def get_cost_center_company(cost_center):
-	return frappe.get_value("Cost Center", cost_center, "company", cache=True)
+			for child_doc in doc.item_defaults:
+				child_doc.db_insert()
