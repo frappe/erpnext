c2714c7d5de98d814e3d4e619f8c54769665d7f6
Gaurav Naik <gvrvnk@gmail.com>
2018-05-14 18:02:24 +0530

    [Fix] Indentation for accounting_period overlap

--- erpnext/accounts/doctype/accounting_period/accounting_period.py
+++ erpnext/accounts/doctype/accounting_period/accounting_period.py
@@ -15,20 +15,20 @@ class AccountingPeriod(Document):
 		self.name = " - ".join([self.period_name, company_abbr])
 	
 	def validate_overlap(self):
-			existing_accounting_period = frappe.db.sql("""select name from `tabAccounting Period`
-				where (
-					(%(start_date)s between start_date and end_date)
-					or (%(end_date)s between start_date and end_date)
-					or (start_date between %(start_date)s and %(end_date)s)
-					or (end_date between %(start_date)s and %(end_date)s)
-				) and name!=%(name)s and company=%(company)s""",
-				{
-					"start_date": self.start_date,
-					"end_date": self.end_date,
-					"name": self.name,
-					"company": self.company
-				}, as_dict=True)
+		existing_accounting_period = frappe.db.sql("""select name from `tabAccounting Period`
+			where (
+				(%(start_date)s between start_date and end_date)
+				or (%(end_date)s between start_date and end_date)
+				or (start_date between %(start_date)s and %(end_date)s)
+				or (end_date between %(start_date)s and %(end_date)s)
+			) and name!=%(name)s and company=%(company)s""",
+			{
+				"start_date": self.start_date,
+				"end_date": self.end_date,
+				"name": self.name,
+				"company": self.company
+			}, as_dict=True)
 
-			if len(existing_accounting_period) > 0:
-				frappe.throw("Accounting Period overlaps with {0}".format(existing_accounting_period[0].get("name")))
+		if len(existing_accounting_period) > 0:
+			frappe.throw("Accounting Period overlaps with {0}".format(existing_accounting_period[0].get("name")))
 
