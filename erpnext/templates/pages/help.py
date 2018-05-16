72b213095d5300fac8a2f3a9623650440a22b958
Prateeksha Singh <pratu16x7@gmail.com>
2018-05-16 09:31:53 +0530

    [init] support portal

---
+++ erpnext/templates/pages/help.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+import frappe
+
+from frappe import _
+
+def get_context(context):
+	context.no_cache = 1
+
+def get_discuss_posts():
+	pass
+
+def get_users_tickets():
+	pass
