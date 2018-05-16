d2d10297426ab2344e9f8396b980d145e3cf03b4
Manas Solanki <manassolanki@gmail.com>
2018-05-16 14:08:55 +0530

    [rename] Photo ID -> Identification Document

--- erpnext/hr/doctype/identification_document_type/test_identification_document_type.js
+++ erpnext/hr/doctype/identification_document_type/test_identification_document_type.js
@@ -2,15 +2,15 @@
 // rename this file from _test_[name] to test_[name] to activate
 // and remove above this line
 
-QUnit.test("test: Photo ID Type", function (assert) {
+QUnit.test("test: Identification Document Type", function (assert) {
 	let done = assert.async();
 
 	// number of asserts
 	assert.expect(1);
 
 	frappe.run_serially([
-		// insert a new Photo ID Type
-		() => frappe.tests.make('Photo ID Type', [
+		// insert a new Identification Document Type
+		() => frappe.tests.make('Identification Document Type', [
 			// values to be set
 			{key: 'value'}
 		]),
