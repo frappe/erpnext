d0fa4af001ccd95429553a898c13e12a2fbafd58
Jamsheer <kuttypjamsheer@gmail.com>
2018-05-16 10:54:00 +0530

    Clinical procedure (#13540)
    * Clinical Procesdure Template and Clinical Procedure Doctype
    
    * Consultation - Create procedure and procedure prescription
    
    * Patient Appointment - Create Procedure and Procedure appointment invoice
    
    * Physician Schedule - Validate slot overlap
    
    * Clinical Procedure Item - batch_no for Clinical Procedure not for Template
    
    * Clinical Procedure - manage item batch_no
    
    * Codacy - fixes
    
    * Yet another Codacy and travic-ci fix
    
    * Codacy - fixes
    
    * Clinical Procedure - Codacy fix
    
    * Permissions updated
    
    * fixed Qty Indicators, Minor fixes
    
    * Patient appointment for procedure prescription
    
    * Doctype Patient Service Unit to Healthcare Service Unit
    
    * Add healthcare service unit tree root
    
    * Healthcare Service Unit - Tree view script
    
    * Healthcare Service Unit - allow appointments check
    
    * Clinical Procedure - remove unused variable
    
    * Clinical Procedure - Get actual quantity from ledger - fix
    
    * Clinical Procedure - apply transilation for message
    
    * Warehouse in Healthcare Service Unit
    
    * Service Unit in Clinical Procedure
    
    * Codacy fix
    
    * Missing semicolon

--- erpnext/healthcare/doctype/clinical_procedure/test_clinical_procedure.py
+++ erpnext/healthcare/doctype/clinical_procedure/test_clinical_procedure.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017, earthians and Contributors
+# Copyright (c) 2017, ESS LLP and Contributors
 # See license.txt
 from __future__ import unicode_literals
+
 import unittest
 
-class TestPatientServiceUnit(unittest.TestCase):
+class TestClinicalProcedure(unittest.TestCase):
 	pass
--- erpnext/healthcare/doctype/patient_appointment/patient_appointment.py
+++ erpnext/healthcare/doctype/patient_appointment/patient_appointment.py
@@ -24,6 +24,8 @@ class PatientAppointment(Document):
 			self.reload()
 
 	def after_insert(self):
+		if self.procedure_prescription:
+			frappe.db.set_value("Procedure Prescription", self.procedure_prescription, "appointment_booked", True)
 		# Check fee validity exists
 		appointment = self
 		validity_exist = validity_exists(appointment.physician, appointment.patient)
@@ -122,10 +124,10 @@ def get_availability_data(date, physician):
 
 				if available_slots:
 					appointments = []
-					
+
 					if schedule.service_unit:
 						slot_name  = schedule.schedule+" - "+schedule.service_unit
-						allow_overlap = frappe.get_value('Patient Service Unit', schedule.service_unit, 'overlap_appointments')
+						allow_overlap = frappe.get_value('Healthcare Service Unit', schedule.service_unit, 'overlap_appointments')
 						if allow_overlap:
 							# fetch all appointments to physician by service unit
 							appointments = frappe.get_all(
@@ -164,8 +166,14 @@ def get_availability_data(date, physician):
 @frappe.whitelist()
 def update_status(appointment_id, status):
 	frappe.db.set_value("Patient Appointment", appointment_id, "status", status)
+	appointment_booked = True
 	if status == "Cancelled":
+		appointment_booked = False
 		appointment_cancel(appointment_id)
+
+	procedure_prescription = frappe.db.get_value("Patient Appointment", appointment_id, "procedure_prescription")
+	if procedure_prescription:
+		frappe.db.set_value("Procedure Prescription", procedure_prescription, "appointment_booked", appointment_booked)
 
 
 @frappe.whitelist()
@@ -202,7 +210,10 @@ def invoice_appointment(appointment_doc):
 	sales_invoice.debit_to = get_receivable_account(appointment_doc.company)
 
 	fee_validity = get_fee_validity(appointment_doc.physician, appointment_doc.patient, appointment_doc.appointment_date)
-	create_invoice_items(appointment_doc.physician, appointment_doc.company, sales_invoice)
+	procedure_template = False
+	if appointment_doc.procedure_template:
+		procedure_template = appointment_doc.procedure_template
+	create_invoice_items(appointment_doc.physician, appointment_doc.company, sales_invoice, procedure_template)
 
 	sales_invoice.save(ignore_permissions=True)
 	frappe.db.sql("""update `tabPatient Appointment` set sales_invoice=%s where name=%s""", (sales_invoice.name, appointment_doc.name))
@@ -256,18 +267,26 @@ def create_fee_validity(physician, patient, date):
 	return fee_validity
 
 
-def create_invoice_items(physician, company, invoice):
+def create_invoice_items(physician, company, invoice, procedure_template):
 	item_line = invoice.append("items")
-	item_line.item_name = "Consulting Charges"
-	item_line.description = "Consulting Charges:  " + physician
+	if procedure_template:
+		procedure_template_obj = frappe.get_doc("Clinical Procedure Template", procedure_template)
+		item_line.item_code = procedure_template_obj.item_code
+		item_line.item_name = procedure_template_obj.template
+		item_line.description = procedure_template_obj.description
+	else:
+		item_line.item_name = "Consulting Charges"
+		item_line.description = "Consulting Charges:  " + physician
+		item_line.uom = "Nos"
+		item_line.conversion_factor = 1
+		item_line.income_account = get_income_account(physician, company)
+		op_consulting_charge = frappe.db.get_value("Physician", physician, "op_consulting_charge")
+		if op_consulting_charge:
+			item_line.rate = op_consulting_charge
+			item_line.amount = op_consulting_charge
 	item_line.qty = 1
-	item_line.uom = "Nos"
-	item_line.conversion_factor = 1
-	item_line.income_account = get_income_account(physician, company)
-	op_consulting_charge = frappe.db.get_value("Physician", physician, "op_consulting_charge")
-	if op_consulting_charge:
-		item_line.rate = op_consulting_charge
-		item_line.amount = op_consulting_charge
+
+
 	return invoice
 
 
@@ -337,3 +356,11 @@ def get_events(start, end, filters=None):
 	for item in data:
 		item.end = item.start + datetime.timedelta(minutes = item.duration)
 	return data
+
+@frappe.whitelist()
+def get_procedure_prescribed(patient):
+	return frappe.db.sql("""select pp.name, pp.procedure, pp.parent, ct.physician,
+	ct.consultation_date, pp.physician, pp.date, pp.department
+	from tabConsultation ct, `tabProcedure Prescription` pp
+	where ct.patient='{0}' and pp.parent=ct.name and pp.appointment_booked=0
+	order by ct.creation desc""".format(patient))
