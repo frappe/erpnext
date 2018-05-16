b712a2fc2e8cc0eb6454c07ec7d9ffdcb26b8bf7
Shreya Shah <shreyashah115@gmail.com>
2018-05-16 10:42:09 +0530

    Import nowdate (#14064)

--- erpnext/hr/doctype/leave_application/leave_application.py
+++ erpnext/hr/doctype/leave_application/leave_application.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.utils import cint, cstr, date_diff, flt, formatdate, getdate, get_link_to_form, \
-	comma_or, get_fullname, add_days
+	comma_or, get_fullname, add_days, nowdate
 from erpnext.hr.utils import set_employee_name, get_leave_period
 from erpnext.hr.doctype.leave_block_list.leave_block_list import get_applicable_block_dates
 from erpnext.hr.doctype.employee.employee import get_holiday_list_for_employee
