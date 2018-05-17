f2ccde0454dbcedd54cb017b8b9e204b4f19b9be
Rohit Waghchaure <rohitw1991@gmail.com>
2018-05-17 15:40:32 +0530

    [Fix] Heatmap issue in the project

--- erpnext/projects/doctype/project/project_dashboard.py
+++ erpnext/projects/doctype/project/project_dashboard.py
@@ -4,6 +4,7 @@ def get_data():
 	return {
 		'heatmap': True,
 		'heatmap_message': _('This is based on the Time Sheets created against this project'),
+		'method' : 'erpnext.projects.doctype.project.project.get_timeline_data',
 		'fieldname': 'project',
 		'transactions': [
 			{
