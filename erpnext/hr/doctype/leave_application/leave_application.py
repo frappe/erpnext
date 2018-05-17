8df739b29542b7358892d57ddc643d6d21ccc8a9
Shreya <shreyashah115@gmail.com>
2018-05-17 17:13:56 +0530

    Mark attendance only if to_date <= nowdate in Leave Application

--- erpnext/hr/doctype/leave_application/leave_application.py
+++ erpnext/hr/doctype/leave_application/leave_application.py
@@ -116,7 +116,7 @@ class LeaveApplication(Document):
 					frappe.db.sql("""update `tabAttendance` set status = %s, leave_type = %s\
 						where name = %s""",(status, self.leave_type, d.name))
 
-			elif self.from_date <= nowdate():
+			elif self.to_date <= nowdate():
 				for dt in daterange(getdate(self.from_date), getdate(self.to_date)):
 					date = dt.strftime("%Y-%m-%d")
 					if not date == self.half_day_date:
