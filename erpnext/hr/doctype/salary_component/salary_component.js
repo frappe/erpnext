b7385d970711f3e460e2ba3e67745630337af541
Jamsheer <jamsheer@earthianslive.com>
2018-05-18 15:46:55 +0530

    Salary Component - account mandatory for separate payment entry

--- erpnext/hr/doctype/salary_component/salary_component.js
+++ erpnext/hr/doctype/salary_component/salary_component.js
@@ -40,6 +40,14 @@ frappe.ui.form.on('Salary Component', {
 		if(frm.doc.variable_based_on_taxable_salary){
 			set_value_for_condition_and_formula(frm);
 		}
+	},
+	create_separate_payment_entry_against_benefit_claim: function(frm) {
+		if(frm.doc.create_separate_payment_entry_against_benefit_claim){
+			frm.set_df_property("accounts", "reqd", 1);
+		}
+		else{
+			frm.set_df_property("accounts", "reqd", 0);
+		}
 	}
 });
 
