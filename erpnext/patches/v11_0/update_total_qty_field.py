ea3f50f41ff9804286c7a4b0779831a4042dbb52
Shreya <shreyashah115@gmail.com>
2018-05-28 12:59:19 +0530

    Add path to patches.txt

--- erpnext/patches/v11_0/update_total_qty_field.py
+++ erpnext/patches/v11_0/update_total_qty_field.py
@@ -9,11 +9,20 @@ def execute():
 	frappe.reload_doc('stock', 'doctype', 'purchase_receipt')
 	frappe.reload_doc('accounts', 'doctype', 'sales_invoice')
 	frappe.reload_doc('accounts', 'doctype', 'purchase_invoice')
-
+ 
 	doctypes = ["Sales Order", "Sales Invoice", "Delivery Note",\
 		"Purchase Order", "Purchase Invoice", "Purchase Receipt", "Quotation", "Supplier Quotation"]
 
 	for doctype in doctypes:
+		total_qty =
+			frappe.db.sql('''
+				SELECT
+					parent, SUM(qty)
+				FROM
+					`tab%s Item`
+				GROUP BY parent
+			''' % (doctype), as_dict = True)
+
 		frappe.db.sql('''
 			UPDATE
 				`tab%s` dt SET dt.total_qty =
