6386b3ac1a397046c2db02b22c66510e9a7e39b3
deepak-mnt <deepak@mntechnique.com>
2018-06-01 14:45:13 +0530

    validation to prevent duplication of supplier entry

--- erpnext/buying/doctype/supplier/supplier.py
+++ erpnext/buying/doctype/supplier/supplier.py
@@ -41,6 +41,7 @@ class Supplier(TransactionBase):
 				msgprint(_("Series is mandatory"), raise_exception=1)
 
 		validate_party_accounts(self)
+		self.validation_criteria()
 
 	def on_trash(self):
 		delete_contact_and_address('Supplier', self.name)
@@ -48,3 +49,25 @@ class Supplier(TransactionBase):
 	def after_rename(self, olddn, newdn, merge=False):
 		if frappe.defaults.get_global_default('supp_master_name') == 'Supplier Name':
 			frappe.db.set(self, "supplier_name", newdn)
+
+	def validation_criteria(self):
+		validation_criteria = frappe.db.get_value("Buying Settings",None,"validation_criteria")
+		name = self.name
+		tax_id = self.tax_id
+		pan_no = self.pan_no
+		if validation_criteria == "Supplier ID + Tax ID + Pan No.":
+			supplier_with_same_values = frappe.db.get_value("Supplier",filters={"name":name,"tax_id":tax_id,"pan_no":pan_no})
+			if supplier_with_same_values:
+				frappe.throw(_('Supplier with same Supplier ID ,Tax ID and Pan No. already exists'))
+		elif validation_criteria == "Tax ID + Pan No.":
+			supplier_with_same_values = frappe.db.get_value("Supplier",filters={"tax_id":tax_id,"pan_no":pan_no})
+			if supplier_with_same_values:
+				frappe.throw(_('Supplier with same Tax ID and Pan No. already exists'))
+		elif validation_criteria == "Pan No.":
+			supplier_with_same_values = frappe.db.get_value("Supplier",filters={"pan_no":pan_no})
+			if supplier_with_same_values:
+				frappe.throw(_('Supplier with same Pan No. already exists'))
+		elif validation_criteria == "Tax ID":
+			supplier_with_same_values = frappe.db.get_value("Supplier",filters={"tax_id":tax_id})
+			if supplier_with_same_values:
+				frappe.throw(_('Supplier with same Tax ID already exists'))				
