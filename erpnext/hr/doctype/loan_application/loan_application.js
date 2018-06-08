570cfcfe785e983e610c39b81c4c6e53c734018f
Shridhar Patil <shridharpatil2792@gmail.com>
2018-06-08 19:37:17 +0530

    loan application
    get rate of interest from loan type

--- erpnext/hr/doctype/loan_application/loan_application.js
+++ erpnext/hr/doctype/loan_application/loan_application.js
@@ -34,5 +34,23 @@ frappe.ui.form.on('Loan Application', {
 				});
 			})
 		}
+	},
+	loan_type: function(frm){
+		frappe.call({
+			method: "frappe.client.get_value",
+			args: {
+				doctype: "Loan Type",
+				fieldname: "rate_of_interest",
+				filters: {
+					name:["=", frm.doc.loan_type]
+					}           			
+			},		
+			callback: function(r) {
+				console.log(r)
+				if(r.message.rate_of_interest){
+					frm.set_value('rate_of_interest', r.message.rate_of_interest)
+				}
+			}
+		});
 	}
 });
