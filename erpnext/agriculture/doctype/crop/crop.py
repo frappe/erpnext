ca8138fd1334aebe4f4f0341c87a683b45a2979a
Alchez <Alchez@users.noreply.github.com>
2018-06-08 14:55:07 +0530

    [Agriculture] Sort crop tasks chronologically and optimize disease task creation (#14367)
    * Improve max crop period check and sort agriculture tasks chronologically
    
    * Reformat crop cycle logic and add field to optimize task creation

--- erpnext/agriculture/doctype/crop/crop.py
+++ erpnext/agriculture/doctype/crop/crop.py
@@ -3,23 +3,31 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe import _
+
 
 class Crop(Document):
 	def validate(self):
-		max_period = 0
+		self.validate_crop_tasks()
+
+	def validate_crop_tasks(self):
 		for task in self.agriculture_task:
-			# validate start_day is not > end_day
 			if task.start_day > task.end_day:
 				frappe.throw(_("Start day is greater than end day in task '{0}'").format(task.task_name))
-			# to calculate the period of the Crop Cycle
-			if task.end_day > max_period: max_period = task.end_day
-		if max_period > self.period: self.period = max_period
+
+		# Verify that the crop period is correct
+		max_crop_period = max([task.end_day for task in self.agriculture_task])
+		self.period = max(self.period, max_crop_period)
+
+		# Sort the crop tasks based on start days,
+		# maintaining the order for same-day tasks
+		self.agriculture_task.sort(key=lambda task: task.start_day)
+
 
 @frappe.whitelist()
 def get_item_details(item_code):
 	item = frappe.get_doc('Item', item_code)
-	return { "uom": item.stock_uom, "rate": item.valuation_rate }
+	return {"uom": item.stock_uom, "rate": item.valuation_rate}
