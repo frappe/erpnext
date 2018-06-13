5082870884a6f22c11ffd041fc8f4cbe2e0311d2
IOWEB TECHNOLOGIES <info@ioweb.gr>
2018-06-13 20:48:31 +0300

    Fix unicode literal issue that prevents payment entries to be created

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 
-
+from __future__ import unicode_literals
 
 import frappe, erpnext
 import frappe.defaults
