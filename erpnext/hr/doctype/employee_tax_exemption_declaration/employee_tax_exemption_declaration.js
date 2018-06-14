17bf464f985c37048833a9cdb5c9fc6e8b912950
Ranjith <rkurungadam@gmail.com>
2018-06-14 13:28:40 +0530

    remove HRA fields fom declaration, proof submission

--- erpnext/hr/doctype/employee_tax_exemption_declaration/employee_tax_exemption_declaration.js
+++ erpnext/hr/doctype/employee_tax_exemption_declaration/employee_tax_exemption_declaration.js
@@ -2,11 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Tax Exemption Declaration', {
-	refresh: function(frm){
-		if(frm.doc.__islocal){
-			frm.set_df_property('hra_declaration_section', 'hidden', 1);
-		}
-	},
 	setup: function(frm) {
 		frm.set_query('employee', function() {
 			return {
@@ -38,50 +33,6 @@ frappe.ui.form.on('Employee Tax Exemption Declaration', {
 					'is_active': 1
 				}
 			}
-		});
-	},
-	employee: function(frm){
-		frm.trigger('set_null_value');
-	},
-	company: function(frm) {
-		if(frm.doc.company){
-			frappe.call({
-				method: "frappe.client.get_value",
-				args: {
-					doctype: "Company",
-					filters: {"name": frm.doc.company},
-					fieldname: "hra_component"
-				},
-				callback: function(r){
-					if(r.message.hra_component){
-						frm.set_df_property('hra_declaration_section', 'hidden', 0);
-					}
-				}
-			});
-		}
-	},
-	monthly_house_rent: function(frm) {
-		frm.trigger("calculate_hra_exemption");
-	},
-	rented_in_metro_city: function(frm) {
-		frm.trigger("calculate_hra_exemption");
-	},
-	calculate_hra_exemption: function(frm) {
-		frappe.call({
-			method: "calculate_hra_exemption",
-			doc: frm.doc,
-			callback: function(r) {
-				if (!r.exc){
-					frm.refresh_fields();
-				}
-			}
-		});
-	},
-	set_null_value(frm){
-		let fields = ['salary_structure_hra', 'monthly_house_rent','annual_hra_exemption',
-			'monthly_hra_exemption', 'total_exemption_amount', 'payroll_period'];
-		fields.forEach(function(field) {
-			frm.set_value(field, '');
 		});
 	}
 });
