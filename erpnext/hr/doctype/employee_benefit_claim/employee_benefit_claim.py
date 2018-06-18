32c7085532abae47b3b1025dc78d45fe8ce720c8
Jamsheer <jamsheer@earthianslive.com>
2018-06-18 16:25:30 +0530

    Employee Benefit Claim - Code refactor

--- erpnext/hr/doctype/employee_benefit_claim/employee_benefit_claim.py
+++ erpnext/hr/doctype/employee_benefit_claim/employee_benefit_claim.py
@@ -7,7 +7,7 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from erpnext.hr.doctype.employee_benefit_application.employee_benefit_application import get_max_benefits
-from erpnext.hr.utils import get_payroll_period
+from erpnext.hr.utils import get_payroll_period, get_previous_claimed_amount
 from erpnext.hr.doctype.salary_structure_assignment.salary_structure_assignment import get_assigned_salary_structure
 
 class EmployeeBenefitClaim(Document):
@@ -26,7 +26,7 @@ class EmployeeBenefitClaim(Document):
 
 	def validate_benefit_claim_amount(self, max_benefits, payroll_period):
 		claimed_amount = self.claimed_amount
-		claimed_amount += self.get_previous_claimed_amount(payroll_period)
+		claimed_amount += get_previous_claimed_amount(self.employee, payroll_period)
 		if max_benefits < claimed_amount:
 			frappe.throw(_("Maximum benefit of employee {0} exceeds {1} by the sum {2} of previous claimed\
 			amount").format(self.employee, max_benefits, claimed_amount-max_benefits))
@@ -37,7 +37,7 @@ class EmployeeBenefitClaim(Document):
 
 	def validate_max_benefit_for_component(self, payroll_period):
 		claimed_amount = self.claimed_amount
-		claimed_amount += self.get_previous_claimed_amount(payroll_period, component = self.earning_component)
+		claimed_amount += get_previous_claimed_amount(self.employee, payroll_period, component = self.earning_component)
 		if claimed_amount > self.max_amount_eligible:
 			frappe.throw(_("Maximum amount eligible for the component {0} exceeds {1}").format(self.earning_component, self.max_amount_eligible))
 
@@ -51,7 +51,7 @@ class EmployeeBenefitClaim(Document):
 		if not pro_rata_amount:
 			pro_rata_amount = 0
 
-		claimed_amount += self.get_previous_claimed_amount(payroll_period, non_pro_rata = True)
+		claimed_amount += get_previous_claimed_amount(self.employee, payroll_period, non_pro_rata = True)
 		if max_benefits < pro_rata_amount + claimed_amount:
 			frappe.throw(_("Maximum benefit of employee {0} exceeds {1} by the sum {2} of benefit application pro-rata component\
 			amount and previous claimed amount").format(self.employee, max_benefits, pro_rata_amount+claimed_amount-max_benefits))
@@ -90,30 +90,6 @@ class EmployeeBenefitClaim(Document):
 		if application:
 			return frappe.db.get_value("Employee Benefit Application", application, "pro_rata_dispensed_amount")
 		return False
-
-	def get_previous_claimed_amount(self, payroll_period, non_pro_rata=False, component=False):
-		total_claimed_amount = 0
-		query = """
-		select sum(claimed_amount) as 'total_amount'
-		from `tabEmployee Benefit Claim`
-		where employee=%(employee)s
-		and docstatus = 1
-		and (claim_date between %(start_date)s and %(end_date)s)
-		"""
-		if non_pro_rata:
-			query += "and pay_against_benefit_claim = 1"
-		if component:
-			query += "and earning_component = %(component)s"
-
-		sum_of_claimed_amount = frappe.db.sql(query, {
-			'employee': self.employee,
-			'start_date': payroll_period.start_date,
-			'end_date': payroll_period.end_date,
-			'component': component
-		}, as_dict=True)
-		if sum_of_claimed_amount and sum_of_claimed_amount[0].total_amount > 0:
-			total_claimed_amount = sum_of_claimed_amount[0].total_amount
-		return total_claimed_amount
 
 def get_benefit_claim_amount(employee, start_date, end_date, struct_row):
 	benefit_claim_details = frappe.db.sql("""
