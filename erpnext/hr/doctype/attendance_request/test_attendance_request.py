0e0ae15ad2a0a5b559de924f3196ba15981c9dbe
Shreya <shreyashah115@gmail.com>
2018-06-18 16:29:54 +0530

    Add test case for Attendance Request

--- erpnext/hr/doctype/attendance_request/test_attendance_request.py
+++ erpnext/hr/doctype/attendance_request/test_attendance_request.py
@@ -5,6 +5,30 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
+from frappe.utils import add_days, nowdate
+from datetime import date
 
 class TestAttendanceRequest(unittest.TestCase):
-	pass
+	def setUp(self):
+		for dt in ["Attendance Request", "Attendance"]:
+			frappe.db.sql("delete from `tab%s`" % dt)
+
+	def test_attendance_request(self):
+		today = nowdate()
+		employee = get_employee()
+		attendance_request = frappe.new_doc("Attendance Request")
+		attendance_request.employee = employee.name
+		attendance_request.from_date = date(date.today().year, 1, 1)
+		attendance_request.to_date = date(date.today().year, 1, 2)
+		attendance_request.reason = "Work From Home"
+		attendance_request.company = "_Test Company"
+		attendance_request.insert()
+		attendance_request.submit()
+		attendance = frappe.get_doc('Attendance', {
+			'employee': employee.name,
+			'attendance_date': date(date.today().year, 1, 1)
+		})
+		self.assertEqual(attendance.status, 'Present')
+
+def get_employee():
+	return frappe.get_doc("Employee", "_T-Employee-00001")
