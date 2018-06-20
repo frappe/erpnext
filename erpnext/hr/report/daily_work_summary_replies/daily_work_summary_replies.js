4ad37d36275764284061a04fcda44f483ab8e3d8
Suraj Shetty <surajshetty3416@gmail.com>
2018-06-20 13:17:32 +0530

    ReRun Travis

--- erpnext/hr/report/daily_work_summary_replies/daily_work_summary_replies.js
+++ erpnext/hr/report/daily_work_summary_replies/daily_work_summary_replies.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
-/* eslint-disable */
 
+/* eslint-disable */
 frappe.query_reports["Daily Work Summary Replies"] = {
 	"filters": [
 		{
