63b5132cd95e0b354f3ede3086a11e51f86c44d2
Jamsheer <kuttypjamsheer@gmail.com>
2018-06-21 11:38:38 +0530

    Compensatory Leave Request (#14611)
    * Compensatory Leave Request - Validate Holidays and Attendance
    
    * Compensatory Leave Request - Cancel
    
    * Half Day in Compensatory Leave Request
    
    * Leave allocation on Compensatory Leave Request - Fix
    
    * Unused import

--- erpnext/hr/doctype/compensatory_leave_request/compensatory_leave_request.js
+++ erpnext/hr/doctype/compensatory_leave_request/compensatory_leave_request.js
@@ -10,5 +10,13 @@ frappe.ui.form.on('Compensatory Leave Request', {
 				}
 			};
 		});
+	},
+	half_day: function(frm) {
+		if(frm.doc.half_day == 1){
+			frm.set_df_property('half_day_date', 'reqd', true);
+		}
+		else{
+			frm.set_df_property('half_day_date', 'reqd', false);
+		}
 	}
 });
