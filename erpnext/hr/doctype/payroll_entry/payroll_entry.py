0c680fa3c3bf1e01ea7eca8a054226f0c1aa6441
Ranjith <rkurungadam@gmail.com>
2018-06-23 17:34:14 +0530

    fix Salary Assignment - remove to_date

--- erpnext/hr/doctype/payroll_entry/payroll_entry.py
+++ erpnext/hr/doctype/payroll_entry/payroll_entry.py
@@ -40,7 +40,7 @@ class PayrollEntry(Document):
 				{"company": self.company, "salary_slip_based_on_timesheet":self.salary_slip_based_on_timesheet})
 		if sal_struct:
 			cond += "and t2.salary_structure IN %(sal_struct)s "
-			cond += "and ((%(from_date)s between t2.from_date and ifnull(t2.to_date, '2199-12-31')) or (%(to_date)s between t2.from_date and ifnull(t2.to_date, '2199-12-31')) or (t2.from_date between %(from_date)s and %(to_date)s))"
+			cond += "and %(from_date)s >= t2.from_date"
 			emp_list = frappe.db.sql("""
 				select
 					t1.name as employee, t1.employee_name, t1.department, t1.designation, t2.name
@@ -49,7 +49,7 @@ class PayrollEntry(Document):
 				where
 					t1.name = t2.employee
 					and t2.docstatus = 1
-			%s """% cond, {"sal_struct": sal_struct, "from_date": self.start_date, "to_date": self.end_date}, as_dict=True)
+			%s order by t2.from_date desc limit 1"""% cond, {"sal_struct": sal_struct, "from_date": self.start_date}, as_dict=True)
 			return emp_list
 
 	def fill_employee_details(self):
