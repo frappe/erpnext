ca8ff50b6aeffaf9ed0255361609a3b0c170ec2d
Rohan Bansal <rohan@digithinkit.com>
2018-06-27 15:23:42 +0530

    Fix patch

--- erpnext/patches/v11_0/merge_land_unit_with_location.py
+++ erpnext/patches/v11_0/merge_land_unit_with_location.py
@@ -21,21 +21,32 @@ def execute():
 	frappe.reload_doc('assets', 'doctype', 'linked_location')
 
 	# Rename the fields in related doctypes
-	if 'lanked_land_unit' in frappe.db.get_table_columns('Crop Cycle'):
+	if 'linked_land_unit' in frappe.db.get_table_columns('Crop Cycle'):
 		rename_field('Crop Cycle', 'linked_land_unit', 'linked_location')
 
 	if 'land_unit' in frappe.db.get_table_columns('Linked Location'):
 		rename_field('Linked Location', 'land_unit', 'location')
 
-	# TODO: Move Land Unit records to Location
 	if frappe.db.table_exists('Land Unit'):
-		land_units = frappe.get_all('Land Unit', fields=['*'])
+		land_units = frappe.get_all('Land Unit', fields=['*'], order_by='parent_location')
 
 		for land_unit in land_units:
 			if not frappe.db.exists('Location', land_unit.get('land_unit_name')):
-				doc = frappe.get_doc({
+				frappe.get_doc({
 					'doctype': 'Location',
-					'location_name': land_unit.get('land_unit_name')
+					'location_name': land_unit.get('land_unit_name'),
+					'parent_location': land_unit.get('parent_land_unit'),
+					'is_container': land_unit.get('is_container'),
+					'is_group': land_unit.get('is_group'),
+					'latitude': land_unit.get('latitude'),
+					'longitude': land_unit.get('longitude'),
+					'area': land_unit.get('area'),
+					'location': land_unit.get('location'),
+					'linked_soil_texture': land_unit.get('linked_soil_texture'),
+					'linked_soil_analysis': land_unit.get('linked_soil_analysis'),
+					'linked_plant_analysis': land_unit.get('linked_plant_analysis'),
+					'lft': land_unit.get('lft'),
+					'rgt': land_unit.get('rgt')
 				}).insert(ignore_permissions=True)
 
 	# frappe.db.sql("""update `tabDesktop Icon` set label='Location', module_name='Location' where label='Land Unit'""")
