aacfe1331c773b26da344b1fecc39f0b6f432c21
Aditya Hase <aditya@adityahase.com>
2018-07-10 18:40:14 +0530

    Create interface for user authorization and obtaining authorization code

--- erpnext/erpnext_integrations/doctype/quickbooks_connector/quickbooks_connector.js
+++ erpnext/erpnext_integrations/doctype/quickbooks_connector/quickbooks_connector.js
@@ -2,7 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('QuickBooks Connector', {
-	refresh: function(frm) {
-
+	fetch_data: function(frm) {
+		frappe.call({
+			method: "erpnext.erpnext_integrations.doctype.quickbooks_connector.quickbooks_connector.get_authorization_url",
+			callback: function (url) {
+				window.open(url.message);
+			}
+		});
 	}
 });
