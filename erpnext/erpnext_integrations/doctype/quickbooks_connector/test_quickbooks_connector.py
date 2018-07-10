bf4758674f1fd35a587d3fddcfb3e86b10c49dbb
Aditya Hase <aditya@adityahase.com>
2018-07-10 15:06:46 +0530

    Create QuickBooks Connector Single DocType

---
+++ erpnext/erpnext_integrations/doctype/quickbooks_connector/test_quickbooks_connector.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestQuickBooksConnector(unittest.TestCase):
+	pass
