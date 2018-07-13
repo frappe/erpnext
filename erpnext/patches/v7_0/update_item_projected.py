da17ceea73d736ad80e935963e9e5dd10bdd7ae6
Nabin Hait <nabinhait@gmail.com>
2018-07-13 12:03:49 +0530

    patch fixes

--- erpnext/patches/v7_0/update_item_projected.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-def execute():
-	frappe.reload_doctype("Item")
-	from erpnext.stock.doctype.bin.bin import update_item_projected_qty
-	for item in frappe.get_all("Item", filters={"is_stock_item": 1}):
-		update_item_projected_qty(item.name)
