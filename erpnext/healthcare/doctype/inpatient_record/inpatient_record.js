d1228d38ed4c02de15d3056cb7d9489fcb6869bf
Jamsheer <jamsheer@earthianslive.com>
2018-07-13 17:24:35 +0530

    New DocTypes - Inpatient Record and Healthcare Service Unit Type

---
+++ erpnext/healthcare/doctype/inpatient_record/inpatient_record.js
@@ -0,0 +1,179 @@
+// Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Inpatient Record', {
+	refresh: function(frm) {
+		if(!frm.doc.__islocal && frm.doc.inpatient == "Scheduled"){
+			frm.add_custom_button(__('Admit'), function() {
+				admit_patient_dialog(frm);
+			} );
+			frm.set_df_property("btn_transfer", "hidden", 1);
+		}
+		if(!frm.doc.__islocal && frm.doc.inpatient == "Admitted"){
+			frm.add_custom_button(__('Discharge'), function() {
+				discharge_patient(frm);
+			} );
+			frm.set_df_property("btn_transfer", "hidden", 0);
+		}
+		if(!frm.doc.__islocal && frm.doc.inpatient == "Discharged"){
+			frm.disable_save();
+			frm.set_df_property("btn_transfer", "hidden", 1);
+		}
+	},
+	btn_transfer: function(frm) {
+		transfer_patient_dialog(frm);
+	}
+});
+
+var discharge_patient = function(frm) {
+	frappe.call({
+		doc: frm.doc,
+		method: "discharge",
+		callback: function(data) {
+			if(!data.exc){
+				frm.reload_doc();
+			}
+		},
+		freeze: true,
+		freeze_message: "Process Discharge"
+	});
+};
+
+var admit_patient_dialog = function(frm){
+	var dialog = new frappe.ui.Dialog({
+		title: 'Admit Patient',
+		width: 100,
+		fields: [
+				{fieldtype: "Link", label: "Service Unit Type", fieldname: "service_unit_type", options: "Healthcare Service Unit Type"},
+				{fieldtype: "Link", label: "Service Unit", fieldname: "service_unit", options: "Healthcare Service Unit", reqd: 1},
+				{fieldtype: "Datetime", label: "Date and Time In", fieldname: "datetime_in", reqd: 1},
+				{fieldtype: "Date", label: "Expected Discharge", fieldname: "expected_discharge"}
+		],
+		primary_action_label: __("Admit"),
+		primary_action : function(){
+				var service_unit = dialog.get_value('service_unit');
+				var datetime_in = dialog.get_value('datetime_in');
+				var expected_discharge = null;
+				if(dialog.get_value('expected_discharge')){
+					expected_discharge = dialog.get_value('expected_discharge');
+				}
+				if(!service_unit && !datetime_in){
+					return;
+				}
+				frappe.call({
+					doc: frm.doc,
+					method: 'admit',
+					args:{
+						'bed_location': service_unit,
+						'datetime_in': datetime_in,
+						'expected_discharge': expected_discharge
+					},
+					callback: function(data) {
+						if(!data.exc){
+							frm.reload_doc();
+						}
+					},
+					freeze: true,
+					freeze_message: "Process Admission"
+				});
+				frm.refresh_fields();
+				dialog.hide();
+			}
+	})
+
+	dialog.fields_dict["service_unit_type"].get_query = function(txt){
+		return {
+			filters: {
+				"inpatient_occupancy": 1,
+				"allow_appointments": 0
+			}
+		}
+	};
+	dialog.fields_dict["service_unit"].get_query = function(txt){
+		return {
+		filters: {
+			"is_group": 0,
+			"service_unit_type": dialog.get_value("service_unit_type"),
+			"occupied" : 0
+			}
+		}
+	};
+
+	dialog.show();
+};
+
+var transfer_patient_dialog = function(frm){
+	var dialog = new frappe.ui.Dialog({
+		title: 'Transfer Patient',
+		width: 100,
+		fields: [
+				{fieldtype: "Link", label: "From", fieldname: "leave_from", options: "Healthcare Service Unit", reqd: 1},
+				{fieldtype: "Link", label: "Service Unit Type", fieldname: "service_unit_type", options: "Healthcare Service Unit Type"},
+				{fieldtype: "Link", label: "To", fieldname: "service_unit", options: "Healthcare Service Unit", reqd: 1},
+				{fieldtype: "Datetime", label: "Date and Time", fieldname: "datetime_in", reqd: 1}
+		],
+		primary_action_label: __("Transfer"),
+		primary_action : function(){
+				var service_unit = null;
+				var datetime_in = dialog.get_value('datetime_in');
+				var leave_from = null;
+				if(dialog.get_value('leave_from')){
+					leave_from = dialog.get_value('leave_from');
+				}
+				if(dialog.get_value('service_unit')){
+					service_unit = dialog.get_value('service_unit');
+				}
+				if(!datetime_in){
+					return;
+				}
+				frappe.call({
+					doc: frm.doc,
+					method: 'transfer',
+					args:{
+						'bed_location': service_unit,
+						'datetime_in': datetime_in,
+						'leave_from': leave_from
+					},
+					callback: function(data) {
+						if(!data.exc){
+							frm.reload_doc();
+						}
+					},
+					freeze: true,
+					freeze_message: "Process Transfer"
+				});
+				frm.refresh_fields();
+				dialog.hide();
+			}
+	})
+
+	dialog.fields_dict["service_unit_type"].get_query = function(txt){
+		return {
+			filters: {
+				"inpatient_occupancy": 1,
+				"allow_appointments": 0
+			}
+		}
+	};
+	dialog.fields_dict["service_unit"].get_query = function(txt){
+		return {
+		filters: {
+			"is_group": 0,
+			"service_unit_type": dialog.get_value("service_unit_type"),
+			"occupied" : 0
+			}
+		}
+	};
+
+	dialog.show();
+
+	var not_left_bed_location = null;
+	for(let bed_location in frm.doc.bed_locations){
+		if(frm.doc.bed_locations[bed_location].left != 1){
+			not_left_bed_location = frm.doc.bed_locations[bed_location].bed_location
+		}
+	}
+	dialog.set_values({
+		'leave_from': not_left_bed_location
+	});
+};
---
+++ erpnext/healthcare/doctype/inpatient_record/inpatient_record.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import today, now_datetime
+from frappe.model.document import Document
+
+class InpatientRecord(Document):
+	def after_insert(self):
+		frappe.db.set_value("Patient", self.patient, "inpatient", "Scheduled")
+		frappe.db.set_value("Patient", self.patient, "inpatient_record", self.name)
+
+	def validate(self):
+		self.validate_already_scheduled_or_admitted()
+
+	def validate_already_scheduled_or_admitted(self):
+		query = """
+			select name, inpatient
+			from `tabInpatient Record`
+			where (inpatient = 'Admitted' or inpatient = 'Scheduled')
+			and name != %(name)s and patient = %(patient)s
+			"""
+
+		ip_record = frappe.db.sql(query,{
+				"name": self.name,
+				"patient": self.patient
+			}, as_dict = 1)
+
+		if ip_record:
+			msg = _(("Already {0} Patient {1} with Inpatient Record ").format(ip_record[0].inpatient, self.patient) \
+				+ """ <b><a href="#Form/Inpatient Record/{0}">{0}</a></b>""".format(ip_record[0].name))
+			frappe.throw(msg)
+
+	def admit(self, bed_location, datetime_in, expected_discharge=None):
+		admit_patient(self, bed_location, datetime_in, expected_discharge)
+
+	def discharge(self):
+		discharge_patient(self)
+
+	def transfer(self, bed_location, datetime_in, leave_from):
+		if leave_from:
+			patient_leave_bed_location(self, datetime_in, leave_from)
+		if bed_location:
+			transfer_patient(self, bed_location, datetime_in)
+
+@frappe.whitelist()
+def schedule_inpatient(patient, scheduled_to=None):
+	patient_obj = frappe.get_doc('Patient', patient)
+	inpatient_record = frappe.new_doc('Inpatient Record')
+	inpatient_record.patient = patient
+	inpatient_record.patient_name = patient_obj.patient_name
+	inpatient_record.gender = patient_obj.sex
+	inpatient_record.blood_group = patient_obj.blood_group
+	inpatient_record.dob = patient_obj.dob
+	inpatient_record.mobile = patient_obj.mobile
+	inpatient_record.email = patient_obj.email
+	inpatient_record.phone = patient_obj.phone
+	inpatient_record.inpatient = "Scheduled"
+	inpatient_record.scheduled_date = today()
+	inpatient_record.save(ignore_permissions = True)
+
+@frappe.whitelist()
+def discharge_from_encounter(patient):
+	inpatient_record_id = frappe.db.get_value('Patient', patient, 'inpatient_record')
+	if inpatient_record_id:
+		discharge_patient(frappe.get_doc('Inpatient Record', inpatient_record_id))
+
+def discharge_patient(inpatient_record):
+	frappe.db.set_value("Patient", inpatient_record.patient, "inpatient", None)
+	frappe.db.set_value("Patient", inpatient_record.patient, "inpatient_record", None)
+	inpatient_record.discharge_date = today()
+	inpatient_record.inpatient = "Discharged"
+
+	if inpatient_record.bed_locations:
+		for bed_location in inpatient_record.bed_locations:
+			if bed_location.left != 1:
+				bed_location.left = True
+				bed_location.left_datetime = now_datetime()
+				frappe.db.set_value("Healthcare Service Unit", bed_location.bed_location, "occupied", False)
+	inpatient_record.save(ignore_permissions = True)
+
+def admit_patient(inpatient_record, bed_location, datetime_in, expected_discharge=None):
+	inpatient_record.admitted_datetime = datetime_in
+	inpatient_record.inpatient = "Admitted"
+	inpatient_record.expected_discharge = expected_discharge
+
+	inpatient_record.set('bed_locations', [])
+	transfer_patient(inpatient_record, bed_location, datetime_in)
+
+	frappe.db.set_value("Patient", inpatient_record.patient, "inpatient", "Admitted")
+	frappe.db.set_value("Patient", inpatient_record.patient, "inpatient_record", inpatient_record.name)
+
+def transfer_patient(inpatient_record, bed_location, datetime_in):
+	item_line = inpatient_record.append('bed_locations', {})
+	item_line.bed_location = bed_location
+	item_line.datetime_in = datetime_in
+
+	inpatient_record.save(ignore_permissions = True)
+
+	frappe.db.set_value("Healthcare Service Unit", bed_location, "occupied", True)
+
+def patient_leave_bed_location(inpatient_record, leave_datetime, leave_from):
+	if inpatient_record.bed_locations:
+		for bed_location in inpatient_record.bed_locations:
+			if bed_location.left != 1 and bed_location.bed_location == leave_from:
+				bed_location.left = True
+				bed_location.left_datetime = leave_datetime
+				frappe.db.set_value("Healthcare Service Unit", bed_location.bed_location, "occupied", False)
+	inpatient_record.save(ignore_permissions = True)
