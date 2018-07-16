ba713bb09903a5c2d542c6b4e816c26fe8270bb6
Ranjith <rkurungadam@gmail.com>
2018-07-16 20:07:25 +0530

    test TDS calculation

--- erpnext/hr/doctype/payroll_entry/test_payroll_entry.py
+++ erpnext/hr/doctype/payroll_entry/test_payroll_entry.py
@@ -18,8 +18,8 @@ class TestPayrollEntry(unittest.TestCase):
 		for dt in ["Salary Slip", "Salary Component", "Salary Component Account", "Payroll Entry", "Loan"]:
 			frappe.db.sql("delete from `tab%s`" % dt)
 
-		make_earning_salary_component(["Basic Salary", "Special Allowance", "HRA", "Leave Encashment"])
-		make_deduction_salary_component(["Professional Tax", "TDS"])
+		make_earning_salary_component(setup=True)
+		make_deduction_salary_component(setup=True)
 
 	def test_payroll_entry(self): # pylint: disable=no-self-use
 		company = erpnext.get_default_company()
