e5ecabf513df80d69c9d62f938a26d6775ca4ea7
Nabin Hait <nabinhait@gmail.com>
2018-07-17 16:08:39 +0530

    Replaced spaces with tabs

--- erpnext/patches/v11_0/set_department_for_doctypes.py
+++ erpnext/patches/v11_0/set_department_for_doctypes.py
@@ -3,13 +3,12 @@ import frappe
 # Set department value based on employee value
 
 def execute():
+	doctypes_to_update = ['Appraisal', 'Leave Allocation', 'Expense Claim', 'Instructor', 'Salary Slip',
+		'Attendance', 'Training Feedback', 'Training Result Employee',
+		'Leave Application', 'Employee Advance', 'Activity Cost', 'Training Event Employee',
+		'Timesheet', 'Sales Person', 'Payroll Employee Detail']
 
-    doctypes_to_update = ['Appraisal', 'Leave Allocation', 'Expense Claim', 'Instructor', 'Salary Slip',
-        'Attendance', 'Training Feedback', 'Training Result Employee',
-        'Leave Application', 'Employee Advance', 'Activity Cost', 'Training Event Employee',
-        'Timesheet', 'Sales Person', 'Payroll Employee Detail']
-
-    for doctype in doctypes_to_update:
+	for doctype in doctypes_to_update:
 		if frappe.db.table_exists(doctype):
 			frappe.db.sql("""
 				update `tab%s` dt
