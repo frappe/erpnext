a4cad0356fbef3a30055963e7b79b75500f2e2ec
Shreya <shreyashah115@gmail.com>
2018-07-18 11:49:13 +0530

    Fixed as per review

--- erpnext/support/doctype/issue/issue.py
+++ erpnext/support/doctype/issue/issue.py
@@ -95,6 +95,7 @@ def get_issue_list(doctype, txt, filters, limit_start, limit_page_length=20, ord
 	from frappe.www.list import get_list
 	user = frappe.session.user
 	contact = frappe.db.get_value('Contact', {'user': user}, 'name')
+	customer = None
 	if contact:
 		contact_doc = frappe.get_doc('Contact', contact)
 		customer = contact_doc.get_link_for('Customer')
