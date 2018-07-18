9baa3bef8bf66f2633faafb5a3f6aac2f8dbdc3c
Shreya Shah <shreyashah115@gmail.com>
2018-07-18 12:25:38 +0530

    [Portal] Display issues raised by all users of a customer (#14947)
    * Display issues raised by all users of a customer
    
    * Fixed as per review

--- erpnext/support/doctype/issue/issue.py
+++ erpnext/support/doctype/issue/issue.py
@@ -94,10 +94,16 @@ def get_list_context(context=None):
 def get_issue_list(doctype, txt, filters, limit_start, limit_page_length=20, order_by=None):
 	from frappe.www.list import get_list
 	user = frappe.session.user
+	contact = frappe.db.get_value('Contact', {'user': user}, 'name')
+	customer = None
+	if contact:
+		contact_doc = frappe.get_doc('Contact', contact)
+		customer = contact_doc.get_link_for('Customer')
+
 	ignore_permissions = False
 	if is_website_user():
 		if not filters: filters = []
-		filters.append(("Issue", "raised_by", "=", user))
+		filters.append(("Issue", "customer", "=", customer)) if customer else filters.append(("Issue", "raised_by", "=", user))
 		ignore_permissions = True
 
 	return get_list(doctype, txt, filters, limit_start, limit_page_length, ignore_permissions=ignore_permissions)
