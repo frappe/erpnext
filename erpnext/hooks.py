18a7339335ee57622c34bff592dea1813ca6e0a5
Saurabh <saurabh6790@gmail.com>
2018-07-19 13:13:15 +0530

    Update hooks.py

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -12,7 +12,7 @@ app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
 
 develop_version = '11.x.x-develop'
-staging_version = '11.x.x'
+staging_version = '11.0.0-beta'
 
 error_report_email = "support@erpnext.com"
 
