425fb4c5f4ad87de3d8dec20c7d0fceae44015f3
Jamsheer <kuttypjamsheer@gmail.com>
2018-07-23 13:05:35 +0530

    [Feature][Enhance] Healthcare Inpatient (#14940)
    * New DocTypes - Inpatient Record and Healthcare Service Unit Type
    
    * Patient Appointment - Button - Get Prescribed Procedure - hide after save
    
    * Patient Encounter - Schedule Inpatient button
    
    * Inpatient - Healthcare Fields and references
    
    Conflicts:
    	erpnext/healthcare/doctype/lab_test/lab_test.json
    	erpnext/healthcare/doctype/patient_appointment/patient_appointment.json
    	erpnext/healthcare/doctype/patient_encounter/patient_encounter.json
    	erpnext/healthcare/doctype/vital_signs/vital_signs.json
    
    * Healthcare Service Unit Type - Item Creation - UOM Fix
    
    * Healthcare Service Unit - healthcare service unit type fix
    
    * Inpatient Record - Admission Scheduled and Discharge Scheduled status
    
    * Patient - rename field inpatient to inpatient_status
    
    * Inpatient Record - rename field inpatient to status
    
    * Refactor - Inpateint Occupancy
    
    * Remove unused imports
    
    * Healthcare service unit - patch - fix
    
    * Healthcare IP - Codacy Fix
    
    * Inpatient Record - Encounter and Practitioner References
    
    * Patch - add healthcare service unit tree - enhance
    
    * Healthcare Setup - Remove - Lab Test and Item creation
    
    * Discharge note in Inpatient Record
    
    * Codacy fix
    
    * Inpatient Record - Update transfer service unit filter
    
    * Inpatient Record - Test

--- erpnext/healthcare/doctype/patient_encounter/patient_encounter.js
+++ erpnext/healthcare/doctype/patient_encounter/patient_encounter.js
@@ -19,7 +19,28 @@ frappe.ui.form.on('Patient Encounter', {
 	refresh: function(frm) {
 		refresh_field('drug_prescription');
 		refresh_field('test_prescription');
-
+		if (!frm.doc.__islocal){
+			frappe.call({
+				method: 'frappe.client.get_value',
+				args: {
+					doctype: 'Patient',
+					fieldname: 'inpatient_status',
+					filters: {name: frm.doc.patient}
+				},
+				callback: function(data) {
+					if(data.message && data.message.inpatient_status == "Admission Scheduled" || data.message.inpatient_status == "Admitted"){
+						frm.add_custom_button(__('Schedule Discharge'), function() {
+							schedule_discharge(frm);
+						});
+					}
+					else if(data.message.inpatient_status != "Discharge Scheduled"){
+						frm.add_custom_button(__('Schedule Admission'), function() {
+							schedule_inpatient(frm);
+						});
+					}
+				}
+			});
+		}
 		frm.add_custom_button(__('Medical Record'), function() {
 			if (frm.doc.patient) {
 				frappe.route_options = {"patient": frm.doc.patient};
@@ -89,6 +110,34 @@ frappe.ui.form.on('Patient Encounter', {
 		frm.set_df_property("encounter_time", "read_only", frm.doc.__islocal ? 0:1);
 	}
 });
+
+var schedule_inpatient = function(frm) {
+	frappe.call({
+		method: "erpnext.healthcare.doctype.inpatient_record.inpatient_record.schedule_inpatient",
+		args: {patient: frm.doc.patient, encounter_id: frm.doc.name, practitioner: frm.doc.practitioner},
+		callback: function(data) {
+			if(!data.exc){
+				frm.reload_doc();
+			}
+		},
+		freeze: true,
+		freeze_message: "Process Inpatient Scheduling"
+	});
+};
+
+var schedule_discharge = function(frm) {
+	frappe.call({
+		method: "erpnext.healthcare.doctype.inpatient_record.inpatient_record.schedule_discharge",
+		args: {patient: frm.doc.patient, encounter_id: frm.doc.name, practitioner: frm.doc.practitioner},
+		callback: function(data) {
+			if(!data.exc){
+				frm.reload_doc();
+			}
+		},
+		freeze: true,
+		freeze_message: "Process Discharge"
+	});
+};
 
 var btn_invoice_encounter = function(frm){
 	var doc = frm.doc;
