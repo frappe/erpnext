8a4e11c7261aa1394d552b91577891dcf4bbef25
Rohit Waghchaure <rohitw1991@gmail.com>
2018-07-28 17:40:08 +0530

    Account no + account name in trial balance and financial statements reports

--- erpnext/accounts/report/trial_balance/trial_balance.py
+++ erpnext/accounts/report/trial_balance/trial_balance.py
@@ -51,7 +51,7 @@ def validate_filters(filters):
 		filters.to_date = filters.year_end_date
 
 def get_data(filters):
-	accounts = frappe.db.sql("""select name, parent_account, account_name, root_type, report_type, lft, rgt
+	accounts = frappe.db.sql("""select name, account_number, parent_account, account_name, root_type, report_type, lft, rgt
 		from `tabAccount` where company=%s order by lft""", filters.company, as_dict=True)
 	company_currency = erpnext.get_company_currency(filters.company)
 
@@ -179,13 +179,14 @@ def prepare_data(accounts, filters, total_row, parent_children_map, company_curr
 	for d in accounts:
 		has_value = False
 		row = {
-			"account_name": d.account_name,
 			"account": d.name,
 			"parent_account": d.parent_account,
 			"indent": d.indent,
 			"from_date": filters.from_date,
 			"to_date": filters.to_date,
-			"currency": company_currency
+			"currency": company_currency,
+			"account_name": ('{} - {}'.format(d.account_number, d.account_name)
+				if d.account_number else d.account_name)
 		}
 
 		prepare_opening_and_closing(d)
