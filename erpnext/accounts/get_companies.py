57288bdda2155b9cd4b58954a3cfb6d5e45bc9c4
Yaqin Shurrab <yshurrab@gmail.com>
2018-07-30 14:26:18 +0300

    Update get_companies.py

--- erpnext/accounts/get_companies.py
+++ erpnext/accounts/get_companies.py
@@ -1,1 +1,23 @@
 
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import datetime
+
+import frappe
+
+
+@frappe.whitelist(allow_guest=True)
+def get_companies():
+    try:
+               
+        companies = [temp.name for temp in frappe.get_all("Company",
+                                       filters=dict(
+                                       ),
+                                       ignore_permissions=True,
+                                       ignore_ifnull=True)]
+        
+    except Exception as e:
+        return dict(status=False, message=str(e))
+    return dict(status=True, message="Success", companies=companies)
+
