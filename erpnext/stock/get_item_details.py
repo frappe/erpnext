0f0309b10f6f82317a9b21b67cda7ff791c6f323
Nabin Hait <nabinhait@gmail.com>
2018-08-01 17:44:34 +0530

    Additional fields in Item Price (#14540)
    * [IMPROVEMENT] Item Price New Fields and Features
    
    * [IMPROVEMENT] Item Price
    
     - Item Price insert against Qty, Supplier/Customer, Validup/Validfrom
     - Stock Setting to accept Default Price List for Purchase or Sales Item
     - Move Item Section in Item Price
    
    * Fetch default Item Price based on Customer as null
    
    * test fixes
    
    * Code cleanup and fixed the issue of incorrect item price fetched against the customer
    
    * Removed min qty from packing_increment calculation
    
    * Removed auto_update_price_list_rate from the stock settings
    
    * Revert sales_order.json changes
    
    * Removed sales, purchase price list from stock settings because this fields are already available in selling, buying settings
    
    * Removed unnecessory file
    
    * Fixed item price validation code

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -261,13 +261,14 @@ def get_basic_details(args, item):
 		"is_fixed_asset": item.is_fixed_asset,
 		"weight_per_unit":item.weight_per_unit,
 		"weight_uom":item.weight_uom,
-		"last_purchase_rate": item.last_purchase_rate if args.get("doctype") in ["Purchase Order"] else 0
+		"last_purchase_rate": item.last_purchase_rate if args.get("doctype") in ["Purchase Order"] else 0,
+		"transaction_date": args.get("transaction_date")
 	})
 
 	if item.enable_deferred_revenue:
 		service_end_date = add_months(args.transaction_date, item.no_of_months)
 		out.update({
-			"enable_deferred_revenue": item.enable_deferred_revenue, 
+			"enable_deferred_revenue": item.enable_deferred_revenue,
 			"deferred_revenue_account": get_default_deferred_revenue_account(args, item),
 			"service_start_date": args.transaction_date,
 			"service_end_date": service_end_date
@@ -338,11 +339,7 @@ def get_price_list_rate(args, item_doc, out):
 		if meta.get_field("currency") and args.price_list:
 			validate_conversion_rate(args, meta)
 
-		price_list_rate = get_price_list_rate_for(args.price_list, item_doc.name)
-
-		# variant
-		if not price_list_rate and item_doc.variant_of:
-			price_list_rate = get_price_list_rate_for(args.price_list, item_doc.variant_of)
+		price_list_rate = get_price_list_rate_for(args, item_doc.name) or 0
 
 		# insert in database
 		if not price_list_rate:
@@ -350,10 +347,12 @@ def get_price_list_rate(args, item_doc, out):
 				insert_item_price(args)
 			return {}
 
+		# variant
+		if not price_list_rate and item_doc.variant_of:
+			price_list_rate = get_price_list_rate_for(args, item_doc.variant_of)
+
 		out.price_list_rate = flt(price_list_rate) * flt(args.plc_conversion_rate) \
 			/ flt(args.conversion_rate)
-		if not args.price_list_uom_dependant:
-			out.price_list_rate = flt(out.price_list_rate * (flt(args.conversion_factor) or 1.0))
 
 		if not out.price_list_rate and args.transaction_type=="buying":
 			from erpnext.stock.doctype.item.item import get_last_purchase_details
@@ -365,8 +364,8 @@ def insert_item_price(args):
 	if frappe.db.get_value("Price List", args.price_list, "currency") == args.currency \
 		and cint(frappe.db.get_single_value("Stock Settings", "auto_insert_price_list_rate_if_missing")):
 		if frappe.has_permission("Item Price", "write"):
-			price_list_rate = args.rate / args.conversion_factor \
-				if args.get("conversion_factor") else args.rate
+			price_list_rate = (args.rate / args.get('conversion_factor')
+				if args.get("conversion_factor") else args.rate)
 
 			item_price = frappe.get_doc({
 				"doctype": "Item Price",
@@ -376,7 +375,8 @@ def insert_item_price(args):
 				"price_list_rate": price_list_rate
 			})
 
-			name = frappe.db.get_value('Item Price', {'item_code': args.item_code, 'price_list': args.price_list, 'currency': args.currency}, 'name')
+			name = frappe.db.get_value('Item Price',
+				{'item_code': args.item_code, 'price_list': args.price_list, 'currency': args.currency}, 'name')
 
 			if name:
 				item_price = frappe.get_doc('Item Price', name)
@@ -389,9 +389,103 @@ def insert_item_price(args):
 				frappe.msgprint(_("Item Price added for {0} in Price List {1}").format(args.item_code,
 					args.price_list))
 
-def get_price_list_rate_for(price_list, item_code):
-	return frappe.db.get_value("Item Price",
-			{"price_list": price_list, "item_code": item_code}, "price_list_rate")
+def get_item_price(args, item_code):
+	"""
+		Get name, price_list_rate from Item Price based on conditions
+			Check if the Derised qty is within the increment of the packing list.
+		:param args: dict (or frappe._dict) with mandatory fields price_list, uom
+			optional fields min_qty, transaction_date, customer, supplier
+		:param item_code: str, Item Doctype field item_code
+	"""
+
+	args['item_code'] = item_code
+	conditions = "where (customer is null or customer = '') and (supplier is null or supplier = '')"
+	if args.get("customer"):
+		conditions = "where customer=%(customer)s"
+
+	if args.get("supplier"):
+		conditions = "where supplier=%(supplier)s"
+
+	conditions += """ and item_code=%(item_code)s
+		and price_list=%(price_list)s
+		and ifnull(uom, '') in ('', %(uom)s)"""
+
+	if args.get('min_qty'):
+		conditions += " and ifnull(min_qty, 0) <= %(min_qty)s"
+
+	if args.get('transaction_date'):
+		conditions += """ and %(transaction_date)s between
+			ifnull(valid_from, '2000-01-01') and ifnull(valid_upto, '2500-12-31')"""
+
+	return frappe.db.sql(""" select name, price_list_rate, uom
+		from `tabItem Price` {conditions}
+		order by uom desc, min_qty desc """.format(conditions=conditions), args)
+
+def get_price_list_rate_for(args, item_code):
+	"""
+		Return Price Rate based on min_qty of each Item Price Rate.\
+		For example, desired qty is 10 and Item Price Rates exists
+		for min_qty 9 and min_qty 20. It returns Item Price Rate for qty 9 as
+		the best fit in the range of avaliable min_qtyies
+
+        :param customer: link to Customer DocType
+        :param supplier: link to Supplier DocType
+		:param price_list: str (Standard Buying or Standard Selling)
+		:param item_code: str, Item Doctype field item_code
+		:param qty: Derised Qty
+		:param transaction_date: Date of the price
+	"""
+	item_price_args = {
+			"item_code": item_code,
+			"price_list": args.get('price_list'),
+			"customer": args.get('customer'),
+			"supplier": args.get('supplier'),
+			"uom": args.get('uom'),
+			"min_qty": args.get('qty'),
+			"transaction_date": args.get('transaction_date'),
+	}
+
+	item_price_data = 0
+	price_list_rate = get_item_price(item_price_args, item_code)
+	if price_list_rate:
+		desired_qty = args.get("qty")
+		if check_packing_list(price_list_rate[0][0], desired_qty, item_code):
+			item_price_data = price_list_rate
+	else:
+		for field in ["customer", "supplier", "min_qty"]:
+			del item_price_args[field]
+
+		general_price_list_rate = get_item_price(item_price_args, item_code)
+		if not general_price_list_rate and args.get("uom") != args.get("stock_uom"):
+			item_price_args["args"] = args.get("stock_uom")
+			general_price_list_rate = get_item_price(item_price_args, item_code)
+
+		if general_price_list_rate:
+			item_price_data = general_price_list_rate
+
+	if item_price_data:
+		if item_price_data[0][2] == args.get("uom"):
+			return item_price_data[0][1]
+		elif not args.get('price_list_uom_dependant'):
+			return flt(item_price_data[0][1] * flt(args.get("conversion_factor", 1)))
+		else:
+			return item_price_data[0][1]
+
+def check_packing_list(price_list_rate_name, desired_qty, item_code):
+	"""
+		Check if the Derised qty is within the increment of the packing list.
+		:param price_list_rate_name: Name of Item Price
+        :param desired_qty: Derised Qt
+		:param item_code: str, Item Doctype field item_code
+		:param qty: Derised Qt
+	"""
+
+	item_price = frappe.get_doc("Item Price", price_list_rate_name)
+	if desired_qty:
+		packing_increment = desired_qty % item_price.packing_unit
+
+		if packing_increment == 0:
+			return True
 
 def validate_price_list(args):
 	if args.get("price_list"):
