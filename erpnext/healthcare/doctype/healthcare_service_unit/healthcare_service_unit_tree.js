4371c7e492ccd4a8c7f1824ba483392971ee98c6
Jamsheer <jamsheer@earthianslive.com>
2018-08-01 18:40:05 +0530

    Healthcare Service Unit - Tree View Updated

--- erpnext/healthcare/doctype/healthcare_service_unit/healthcare_service_unit_tree.js
+++ erpnext/healthcare/doctype/healthcare_service_unit/healthcare_service_unit_tree.js
@@ -1,3 +1,35 @@
 frappe.treeview_settings["Healthcare Service Unit"] = {
-	ignore_fields:["parent_healthcare_service_unit"]
+	breadcrumbs: "Healthcare Service Unit",
+	title: __("Healthcare Service Unit"),
+	get_tree_root: false,
+	filters: [{
+		fieldname: "company",
+		fieldtype: "Select",
+		options: erpnext.utils.get_tree_options("company"),
+		label: __("Company"),
+		default: erpnext.utils.get_tree_default("company")
+	}],
+	get_tree_nodes: 'erpnext.healthcare.utils.get_children',
+	ignore_fields:["parent_healthcare_service_unit"],
+	onrender: function(node) {
+		if (node.data.occupied_out_of_vacant!==undefined){
+			$('<span class="balance-area pull-right text-muted small">'
+				+ " " + node.data.occupied_out_of_vacant
+				+ '</span>').insertBefore(node.$ul);
+		}
+		if (node.data && node.data.inpatient_occupancy!==undefined) {
+			if (node.data.inpatient_occupancy == 1){
+				if (node.data.occupancy_status == "Occupied"){
+					$('<span class="balance-area pull-right small">'
+						+ " " + node.data.occupancy_status
+						+ '</span>').insertBefore(node.$ul);
+				}
+				if (node.data.occupancy_status == "Vacant"){
+					$('<span class="balance-area pull-right text-muted small">'
+						+ " " + node.data.occupancy_status
+						+ '</span>').insertBefore(node.$ul);
+				}
+			}
+		}
+	},
 };
