091161a95ab665fa5fdc88359f04d3619a91f5f8
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-14 11:42:40 +0530

    [hub] Favourites UI - remove from listing - add to favourites

--- erpnext/public/js/hub/pages/favourites.js
+++ erpnext/public/js/hub/pages/favourites.js
@@ -2,6 +2,22 @@ import SubPage from './subpage';
 import { get_item_card_container_html } from '../components/items_container';
 
 erpnext.hub.Favourites = class Favourites extends SubPage {
+	make_wrapper() {
+		super.make_wrapper();
+		this.bind_events();
+	}
+
+	bind_events() {
+		this.$wrapper.on('click', '.hub-card', (e) => {
+			const $target = $(e.target);
+			if($target.hasClass('octicon-x')) {
+				e.stopPropagation();
+				const hub_item_code = $target.attr('data-hub-item-code');
+				this.on_item_remove(hub_item_code);
+			}
+		});
+	}
+
 	refresh() {
 		this.get_favourites()
 			.then(items => {
@@ -18,6 +34,40 @@ erpnext.hub.Favourites = class Favourites extends SubPage {
 	render(items) {
 		this.$wrapper.find('.hub-card-container').empty();
 		const html = get_item_card_container_html(items, __('Favourites'));
-		this.$wrapper.append(html)
+		this.$wrapper.append(html);
+
+		this.$wrapper.find('.hub-card').addClass('closable');
 	}
+
+	on_item_remove(hub_item_code, $hub_card = '') {
+		const $message = $(__(`<span>${hub_item_code} removed.
+			<a href="#" data-action="undo-remove"><b>Undo</b></a></span>`));
+
+		frappe.show_alert($message);
+
+		$hub_card = this.$wrapper.find(`.hub-card[data-hub-item-code="${hub_item_code}"]`);
+
+		$hub_card.hide();
+
+		const grace_period = 5000;
+
+		setTimeout(() => {
+			this.remove_item(hub_item_code, $hub_card);
+		}, grace_period);
+	}
+
+	remove_item(hub_item_code, $hub_card) {
+		hub.call('remove_item_from_seller_favourites', {
+			hub_item_code,
+			hub_seller: hub.settings.company_email
+		})
+		.then(() => {
+			$hub_card.remove();
+		})
+		.catch(e => {
+			console.log(e);
+		});
+	}
+
+	undo_remove(hub_item_code) { }
 }
