44ab19bf9d8c625cd7e93489f7ef801243b744f4
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-21 17:43:30 +0530

    [hub] profile email uneditable, style additions, base64 sync enable

--- erpnext/hub_node/data_migration_mapping/item_to_hub_item/__init__.py
+++ erpnext/hub_node/data_migration_mapping/item_to_hub_item/__init__.py
@@ -2,18 +2,18 @@ import frappe, io, base64, urllib, os
 
 def pre_process(doc):
 
-	# file_path = doc.image
-	# file_name = os.path.basename(file_path)
+	file_path = doc.image
+	file_name = os.path.basename(file_path)
 
-	# if file_path.startswith('http'):
-	# 	url = file_path
-	# 	file_path = os.path.join('/tmp', file_name)
-	# 	urllib.urlretrieve(url, file_path)
+	if file_path.startswith('http'):
+		url = file_path
+		file_path = os.path.join('/tmp', file_name)
+		urllib.urlretrieve(url, file_path)
 
-	# with io.open(file_path, 'rb') as f:
-	# 	doc.image = base64.b64encode(f.read())
+	with io.open(file_path, 'rb') as f:
+		doc.image = base64.b64encode(f.read())
 
-	# doc.image_file_name = file_name
+	doc.image_file_name = file_name
 
 	cached_details = frappe.get_doc('Hub Tracked Item', doc.item_code)
 
