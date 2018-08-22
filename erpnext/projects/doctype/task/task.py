8ae78bd2d8401379912de66cde2b2a221e5a939a
Valmik <mail@valmik.in>
2018-08-22 14:46:33 +0530

    check for existing task before appending to child table (#15183)

--- erpnext/projects/doctype/task/task.py
+++ erpnext/projects/doctype/task/task.py
@@ -141,12 +141,13 @@ class Task(NestedSet):
 	def populate_depends_on(self):
 		if self.parent_task:
 			parent = frappe.get_doc('Task', self.parent_task)
-			parent.append("depends_on", {
-				"doctype": "Task Depends On",
-				"task": self.name,
-				"subject": self.subject
-			})
-			parent.save()
+			if not self.name in [row.task for row in parent.depends_on]:
+				parent.append("depends_on", {
+					"doctype": "Task Depends On",
+					"task": self.name,
+					"subject": self.subject
+				})
+				parent.save()
 
 	def on_trash(self):
 		if check_if_child_exists(self.name):
