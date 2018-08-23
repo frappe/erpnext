4b0d4ce67c57b29b542f1476817e87001028582c
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-23 13:52:41 +0530

    [hub] add SearchInput

--- erpnext/public/js/hub/components/PublishPage.vue
+++ erpnext/public/js/hub/components/PublishPage.vue
@@ -3,6 +3,12 @@
 		class="marketplace-page"
 		:data-page-name="page_name"
 	>
+		<search-input
+			placeholder="Search Items ..."
+			:on_search="get_valid_items"
+			v-model="search_value"
+		>
+		</search-input>
 		<item-cards-container
 			:items="valid_items"
 			:is_local="1"
@@ -12,6 +18,7 @@
 </template>
 
 <script>
+import SearchInput from './SearchInput.vue';
 import ItemCardsContainer from './ItemCardsContainer.vue';
 
 export default {
@@ -24,31 +31,22 @@ export default {
 		};
 	},
 	components: {
+		SearchInput,
 		ItemCardsContainer
 	},
-	// watch: {
-	// 	// whenever search term changes, this function will run
-	// 	question: function (newQuestion, oldQuestion) {
-	// 		this.answer = 'Waiting for you to stop typing...'
-	// 		this.debouncedGetAnswer()
-	// 	}
-	// },
 	created() {
 		this.get_valid_items();
 	},
 	methods: {
 		get_valid_items() {
-			var vm = this;
-
 			frappe.call(
 				'erpnext.hub_node.api.get_valid_items',
 				{
 					search_value: this.search_value
 				}
 			)
-			.then(function (r) {
-				vm.valid_items = r.message;
-				frappe.dom.handle_broken_images(this.$wrapper);
+			.then((r) => {
+				this.valid_items = r.message;
 			})
 		}
 	}
---
+++ erpnext/public/js/hub/components/SearchInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hub-search-container">
+	<input
+		type="text"
+		class="form-control"
+		:placeholder="placeholder"
+		:value="value"
+		@input="on_input">
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+		placeholder: String,
+		value: String,
+		on_search: Function
+	},
+	methods: {
+		on_input(event) {
+			this.$emit('input', event.target.value);
+			this.on_search();
+
+			// TODO: Debouncing doesn't fire search
+			// frappe.utils.debounce(this.on_search, 500);
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
