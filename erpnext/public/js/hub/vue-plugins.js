f089dad7c726aba70098e8f9e3908c0167cb51e2
Faris Ansari <netchamp.faris@gmail.com>
2018-08-26 22:20:16 +0530

    feat: Sidebar in vue

--- erpnext/public/js/hub/vue-plugins.js
+++ erpnext/public/js/hub/vue-plugins.js
@@ -5,7 +5,9 @@ Vue.prototype.frappe = window.frappe;
 Vue.directive('route', {
 	bind(el, binding) {
 		const route = binding.value;
+		if (!route) return;
 		el.classList.add('cursor-pointer');
+		el.dataset.route = route;
 		el.addEventListener('click', () => frappe.set_route(route));
 	},
 	unbind(el) {
