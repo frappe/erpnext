a6d410a45b9da1a1e05b6c626503499dec1269f1
Zarrar <zarrar65@gmail.com>
2018-08-27 12:08:53 +0530

    [Enhance] Multiselect for Cost Center and Project (#15208)
    * add filter for Cost Center, make CC & Project multiselect field
    
    * update queries according to project/cost_center multiselect
    
    * add multiselect filter for cost_center and project
    
    * update query according to multiselect

--- erpnext/accounts/report/profit_and_loss_statement/profit_and_loss_statement.js
+++ erpnext/accounts/report/profit_and_loss_statement/profit_and_loss_statement.js
@@ -10,14 +10,63 @@ frappe.require("assets/erpnext/js/financial_statements.js", function() {
 		{
 			"fieldname":"cost_center",
 			"label": __("Cost Center"),
-			"fieldtype": "Link",
-			"options": "Cost Center"
+			"fieldtype": "MultiSelect",
+			get_data: function() {
+				var cost_centers = frappe.query_report.get_filter_value("cost_center") || "";
+
+				const values = cost_centers.split(/\s*,\s*/).filter(d => d);
+				const txt = cost_centers.match(/[^,\s*]*$/)[0] || '';
+				let data = [];
+
+				frappe.call({
+					type: "GET",
+					method:'frappe.desk.search.search_link',
+					async: false,
+					no_spinner: true,
+					args: {
+						doctype: "Cost Center",
+						txt: txt,
+						filters: {
+							"company": frappe.query_report.get_filter_value("company"),
+							"name": ["not in", values]
+						}
+					},
+					callback: function(r) {
+						data = r.results;
+					}
+				});
+				return data;
+			}
 		},
 		{
 			"fieldname":"project",
 			"label": __("Project"),
-			"fieldtype": "Link",
-			"options": "Project"
+			"fieldtype": "MultiSelect",
+			get_data: function() {
+				var projects = frappe.query_report.get_filter_value("project") || "";
+
+				const values = projects.split(/\s*,\s*/).filter(d => d);
+				const txt = projects.match(/[^,\s*]*$/)[0] || '';
+				let data = [];
+
+				frappe.call({
+					type: "GET",
+					method:'frappe.desk.search.search_link',
+					async: false,
+					no_spinner: true,
+					args: {
+						doctype: "Project",
+						txt: txt,
+						filters: {
+							"name": ["not in", values]
+						}
+					},
+					callback: function(r) {
+						data = r.results;
+					}
+				});
+				return data;
+			}
 		},
 		{
 			"fieldname": "accumulated_values",
