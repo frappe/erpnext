6df06cac975035e346910f5e51198d8e23677e2b
Faris Ansari <netchamp.faris@gmail.com>
2018-08-28 17:19:35 +0530

    fix: Show empty state if there are no items

--- erpnext/public/js/hub/pages/Selling.vue
+++ erpnext/public/js/hub/pages/Selling.vue
@@ -3,7 +3,7 @@
 		<section-header>
 			<h4>{{ __('Selling') }}</h4>
 		</section-header>
-		<div class="row" v-if="items">
+		<div class="row" v-if="items && items.length">
 			<div class="col-md-7"
 				style="margin-bottom: 30px;"
 				v-for="item of items"
