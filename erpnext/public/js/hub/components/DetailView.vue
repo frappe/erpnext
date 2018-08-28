1e02f0007532ab5a24119f6ba3d44a10ee15ef8a
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-28 17:24:31 +0530

    [hub] record views, update favourite api

--- erpnext/public/js/hub/components/DetailView.vue
+++ erpnext/public/js/hub/components/DetailView.vue
@@ -28,7 +28,7 @@
 			<div class="row margin-bottom">
 				<div class="col-md-3">
 					<div class="hub-item-image">
-						<img :src="image">
+						<img v-img-src="image">
 					</div>
 				</div>
 				<div class="col-md-8">
