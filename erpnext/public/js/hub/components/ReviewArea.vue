7e92a8a51f142463d68fd1d7ebf7bc0b6a03da3b
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-30 02:42:13 +0530

    [hub] add review area

--- erpnext/public/js/hub/components/ReviewArea.vue
+++ erpnext/public/js/hub/components/ReviewArea.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div ref="review-area"></div>
+		<div ref="review-area" class="timeline-head"></div>
 	</div>
 </template>
 <script>
@@ -11,25 +11,19 @@ export default {
 	},
 	methods: {
 		make_input() {
-			this.comment_area = new frappe.ui.ReviewArea({
+			this.review_area = new frappe.ui.ReviewArea({
 				parent: this.$refs['review-area'],
 				mentions: [],
 				on_submit: this.on_submit_review.bind(this)
-			});
-
-			this.message_input = new frappe.ui.CommentArea({
-				parent: this.$refs['review-area'],
-				on_submit: (message) => {
-					this.message_input.reset();
-					this.$emit('change', message);
-				},
-				no_wrapper: true
 			});
 		},
 
 		on_submit_review(values) {
 			values.user = frappe.session.user;
 			values.username = frappe.session.user_fullname;
+
+			this.review_area.reset();
+			this.$emit('change', message);
 
 			hub.call('add_item_review', {
 				hub_item_code: this.hub_item_code,
