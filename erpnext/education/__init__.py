0b26513c52907bb850be1bf560b62c0e83d5672d
Rushabh Mehta <rmehta@gmail.com>
2018-08-30 12:17:40 +0530

    fix(test_student_group): raise correct exception

--- erpnext/education/__init__.py
+++ erpnext/education/__init__.py
@@ -7,4 +7,5 @@ def validate_student_belongs_to_group(student, student_group):
 	groups = frappe.db.get_all('Student Group Student', ['parent'], dict(student = student, active=1))
 	print(groups)
 	if not student_group in [d.parent for d in groups]:
-		frappe.throw(_('Student {0} does not belong to group {1}').format(frappe.bold(student), frappe.bold(student_group)))
+		frappe.throw(_('Student {0} does not belong to group {1}').format(frappe.bold(student), frappe.bold(student_group)),
+			StudentNotInGroupError)
