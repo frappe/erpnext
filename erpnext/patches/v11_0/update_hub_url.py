adef70f8df948442cff1f7e9d7bb844b97ee03ac
Prateeksha Singh <pratu16x7@gmail.com>
2018-09-03 16:12:50 +0530

    [hub] update_hub_url patch to use marketplace_url

--- erpnext/patches/v11_0/update_hub_url.py
+++ erpnext/patches/v11_0/update_hub_url.py
@@ -2,4 +2,4 @@ import frappe
 
 def execute():
 	frappe.reload_doc('hub_node', 'doctype', 'Marketplace Settings')
-	frappe.db.set_value('Marketplace Settings', 'Marketplace Settings', 'hub_url', 'https://hubmarket.org')
+	frappe.db.set_value('Marketplace Settings', 'Marketplace Settings', 'marketplace_url', 'https://hubmarket.org')
