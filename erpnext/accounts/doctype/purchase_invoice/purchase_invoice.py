2b4c81e39c292084b837696e0ae256256f7b76dd
Zlash65 <zarrar65@gmail.com>
2018-09-05 10:46:30 +0530

    scheduler event to book expense every month

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -3,7 +3,7 @@
 
 
 import frappe, erpnext
-from frappe.utils import cint, cstr, formatdate, flt, getdate, nowdate
+from frappe.utils import cint, cstr, formatdate, flt, getdate, nowdate, date_diff
 from frappe import _, throw
 import frappe.defaults
 
@@ -450,7 +450,7 @@ class PurchaseInvoice(BuyingController):
 				elif not item.is_fixed_asset:
 					gl_entries.append(
 						self.get_gl_dict({
-							"account": item.expense_account,
+							"account": item.expense_account if not item.enable_deferred_expense else item.deferred_expense_account,
 							"against": self.supplier,
 							"debit": flt(item.base_net_amount, item.precision("base_net_amount")),
 							"debit_in_account_currency": (flt(item.base_net_amount,
@@ -687,6 +687,90 @@ class PurchaseInvoice(BuyingController):
 				}
 			))
 
+	def book_expense_for_deferred_expense(self, start_date=None, end_date=None):
+		# book the expense on the last day, but it will be trigger on the 1st of month at 12:00 AM
+		# start_date: 1st of the last month or the start date
+		# end_date: end_date or today-1
+
+		gl_entries = []
+		for item in self.get('items'):
+			last_gl_entry = False
+
+			booking_end_date = getdate(add_days(today(), -1)) if not end_date else end_date
+			if booking_end_date < item.service_start_date or (item.service_stop_date and booking_end_date.month > item.service_stop_date.month):
+				continue
+			elif booking_end_date>=item.service_end_date:
+				last_gl_entry = True
+				booking_end_date = item.service_end_date
+			elif item.service_stop_date and item.service_stop_date<=booking_end_date:
+				last_gl_entry = True
+				booking_end_date = item.service_stop_date
+
+			booking_start_date = getdate(add_months(today(), -1)) if not start_date else start_date
+			booking_start_date = booking_start_date if booking_start_date>item.service_start_date else item.service_start_date
+
+			if item.service_start_date < booking_start_date:
+				prev_gl_entry = frappe.db.sql('''
+					select name, posting_date from `tabGL Entry` where company=%s and account=%s and
+					voucher_type=%s and voucher_no=%s and voucher_detail_no=%s
+					order by posting_date desc limit 1
+				''', (self.company, item.deferred_revenue_account, "Sales Invoice", self.name, item.name), as_dict=True)[0]
+
+				if not prev_gl_entry:
+					booking_start_date = item.service_start_date
+				else:
+					booking_start_date = getdate(add_days(prev_gl_entry.posting_date, 1))
+
+			total_days = date_diff(item.service_end_date, item.service_start_date)
+			total_booking_days = date_diff(booking_end_date, booking_start_date) + 1
+			print(total_days, total_booking_days, "----------")
+			account_currency = get_account_currency(item.expense_account)
+			if not last_gl_entry:
+				base_amount = flt(item.base_net_amount*total_booking_days/flt(total_days), item.precision("base_net_amount"))
+				if account_currency==self.company_currency:
+					amount = base_amount
+				else:
+					amount = flt(item.net_amount*total_booking_days/flt(total_days), item.precision("net_amount"))
+			else:
+				gl_entries_details = frappe.db.sql('''
+					select sum(debit) as total_debit, sum(debit_in_account_currency) as total_debit_in_account_currency, voucher_detail_no
+					from `tabGL Entry` where company=%s and account=%s and voucher_type=%s and voucher_no=%s and voucher_detail_no=%s
+					group by voucher_detail_no
+				''', (self.company, item.deferred_revenue_account, "Sales Invoice", self.name, item.name), as_dict=True)[0]
+				base_amount = flt(item.base_net_amount - gl_entries_details.total_debit, item.precision("base_net_amount"))
+				if account_currency==self.company_currency:
+					amount = base_amount
+				else:
+					amount = flt(item.net_amount - gl_entries_details.total_debit_in_account_currency, item.precision("net_amount"))
+
+			# GL Entry for crediting the amount in the income
+			gl_entries.append(
+				self.get_gl_dict({
+					"account": item.expense_account,
+					"against": self.supplier,
+					"credit": base_amount,
+					"credit_in_account_currency": amount,
+					"cost_center": item.cost_center,
+					'posting_date': booking_end_date
+				}, account_currency)
+			)
+			# GL Entry to debit the amount from the deferred account
+			gl_entries.append(
+				self.get_gl_dict({
+					"account": item.deferred_expense_account,
+					"against": self.supplier,
+					"debit": base_amount,
+					"debit_in_account_currency": amount,
+					"cost_center": item.cost_center,
+					"voucher_detail_no": item.name,
+					'posting_date': booking_end_date
+				}, account_currency)
+			)
+
+		if gl_entries:
+			from erpnext.accounts.general_ledger import make_gl_entries
+			make_gl_entries(gl_entries, cancel=(self.docstatus == 2), merge_entries=True)
+
 	def on_cancel(self):
 		super(PurchaseInvoice, self).on_cancel()
 
@@ -857,3 +941,15 @@ def block_invoice(name, hold_comment):
 def make_inter_company_sales_invoice(source_name, target_doc=None):
 	from erpnext.accounts.doctype.sales_invoice.sales_invoice import make_inter_company_invoice
 	return make_inter_company_invoice("Purchase Invoice", source_name, target_doc)
+
+def booked_deferred_expense(start_date=None, end_date=None):
+	# check for the purchase invoice for which GL entries has to be done
+	invoices = frappe.db.sql_list('''
+		select parent from `tabPurchase Invoice Item` where service_start_date<=%s and service_end_date>=%s
+		and enable_deferred_expense = 1 and docstatus = 1
+	''', (end_date or today(), start_date or add_months(today(), -1)))
+
+	# ToDo also find the list on the basic of the GL entry, and make another list
+	for invoice in invoices:
+		doc = frappe.get_doc("Purchase Invoice", invoice)
+		doc.book_expense_for_deferred_expense(start_date, end_date)
