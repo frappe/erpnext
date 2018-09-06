6f77abe0dd184a2696317e8ab7ff1fd21d9d98a9
Neil Trini Lasrado <nlasrado@gmail.com>
2018-09-06 19:17:47 +0530

    Added ability to create a Return Sales Invoice/ Credit Note against a delivery note of type return (#15266)
    * Remove make subscription button on delivery note if it is of type returns
    
    * Added 'issue_credit_note' checkbox on delivery note to pass a Sales Inv of type return if checked while creating a return delivery note
    
    * Added logic to auto-create Return Sales Invoice/ Credit Note if issue_credit_note checkbox is checked while return delivey note is submited
    
    * Update delivery_note.py

--- erpnext/stock/doctype/delivery_note/delivery_note.js
+++ erpnext/stock/doctype/delivery_note/delivery_note.js
@@ -167,7 +167,7 @@ erpnext.stock.DeliveryNoteController = erpnext.selling.SellingController.extend(
 		}
 		erpnext.stock.delivery_note.set_print_hide(doc, dt, dn);
 
-		if(doc.docstatus==1 && !doc.auto_repeat) {
+		if(doc.docstatus==1 && !doc.is_return && !doc.auto_repeat) {
 			cur_frm.add_custom_button(__('Subscription'), function() {
 				erpnext.utils.make_subscription(doc.doctype, doc.name)
 			}, __("Make"))
