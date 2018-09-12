0669a0e18afeef2c11b27189d3a311b3bd74f041
Nabin Hait <nabinhait@gmail.com>
2018-09-12 10:50:10 +0530

    fix(bank account): import fixed

--- erpnext/accounts/doctype/bank_account/bank_account.py
+++ erpnext/accounts/doctype/bank_account/bank_account.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 from frappe.contacts.address_and_contact import load_address_and_contact, delete_contact_and_address
 
