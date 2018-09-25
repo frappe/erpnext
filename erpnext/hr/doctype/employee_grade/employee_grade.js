05f5416bab4c435e4549c9dc4ce71f3900ad10ea
Mohamed_Alsum <engsum@gmail.com>
2018-09-25 15:26:17 +0200

    show only submitted salary_structure and leave_policy in Employee Grade app (#15434)
    * update staff plan when add or delete job-opening
    
    * Update job_opening.py
    
    * filter default_salary_structure and default_leave_policy
    
    * merge

--- erpnext/hr/doctype/employee_grade/employee_grade.js
+++ erpnext/hr/doctype/employee_grade/employee_grade.js
@@ -2,7 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Grade', {
-	refresh: function(frm) {
+    refresh: function (frm) {
 
-	}
+    },
+    setup: function (frm) {
+        frm.set_query("default_salary_structure", function () {
+            return {
+                "filters": {
+                    "docstatus": 1,
+                    "is_active": "Yes"
+                }
+            };
+        });
+
+        frm.set_query("default_leave_policy", function () {
+            return {
+                "filters": {
+                    "docstatus": 1
+                }
+            };
+        });
+
+
+    }
+
 });
