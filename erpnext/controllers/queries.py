c5051f39ae83c96d255cf2f6fabea64c4e7df756
Saif Ur Rehman <saifi0102@gmail.com>
2018-09-29 14:34:23 +0500

    Brand in item query

--- erpnext/controllers/queries.py
+++ erpnext/controllers/queries.py
@@ -160,7 +160,7 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
 	return frappe.db.sql("""select tabItem.name,
 		if(length(tabItem.item_name) > 40,
 			concat(substr(tabItem.item_name, 1, 40), "..."), item_name) as item_name,
-		tabItem.item_group,
+		tabItem.brand, tabItem.item_group,
 		if(length(tabItem.description) > 40, \
 			concat(substr(tabItem.description, 1, 40), "..."), description) as decription
 		from tabItem
