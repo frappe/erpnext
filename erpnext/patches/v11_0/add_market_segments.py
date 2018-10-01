0c9a7084e091693731ca80f4bfeedabf832e424e
Charles-Henri Decultot <chdecultot@dokos.io>
2018-10-01 11:30:03 +0000

    Specific function for fixtures

--- erpnext/patches/v11_0/add_market_segments.py
+++ erpnext/patches/v11_0/add_market_segments.py
@@ -1,17 +1,11 @@
 
 import frappe
 from frappe import _
-from erpnext.setup.setup_wizard.operations.install_fixtures import make_fixture_records
+from erpnext.setup.setup_wizard.operations.install_fixtures import add_market_segments
 
 def execute():
 	frappe.reload_doc('crm', 'doctype', 'market_segment')
 
 	frappe.local.lang = frappe.db.get_default("lang") or 'en'
 
-	records = [
-		{"doctype": "Market Segment", "market_segment": _("Lower Income")},
-		{"doctype": "Market Segment", "market_segment": _("Middle Income")},
-		{"doctype": "Market Segment", "market_segment": _("Upper Income")}
-	]
-
-	make_fixture_records(records)
+	add_market_segments()
