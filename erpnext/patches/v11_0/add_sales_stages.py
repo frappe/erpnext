77f275852f974f10dd82a170db176e4d6d78df7e
Charles-Henri Decultot <chdecultot@dokos.io>
2018-10-01 11:35:12 +0000

    Divide sales stages fixtures in separate functions

--- erpnext/patches/v11_0/add_sales_stages.py
+++ erpnext/patches/v11_0/add_sales_stages.py
@@ -1,21 +1,10 @@
 import frappe
 from frappe import _
-from erpnext.setup.setup_wizard.operations.install_fixtures import make_fixture_records
+from erpnext.setup.setup_wizard.operations.install_fixtures import add_sale_stages
 
 def execute():
 	frappe.reload_doc('crm', 'doctype', 'sales_stage')
 
 	frappe.local.lang = frappe.db.get_default("lang") or 'en'
 
-	records = [
-		{"doctype": "Sales Stage", "stage_name": _("Prospecting")},
-		{"doctype": "Sales Stage", "stage_name": _("Qualification")},
-		{"doctype": "Sales Stage", "stage_name": _("Needs Analysis")},
-		{"doctype": "Sales Stage", "stage_name": _("Value Proposition")},
-		{"doctype": "Sales Stage", "stage_name": _("Identifying Decision Makers")},
-		{"doctype": "Sales Stage", "stage_name": _("Perception Analysis")},
-		{"doctype": "Sales Stage", "stage_name": _("Proposal/Price Quote")},
-		{"doctype": "Sales Stage", "stage_name": _("Negotiation/Review")}
-	]
-
-	make_fixture_records(records)
+	add_sale_stages()
