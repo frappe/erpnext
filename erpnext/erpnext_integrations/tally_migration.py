03b6f42e203a48280b7c353d7812809e2716686f
Aditya Hase <aditya@adityahase.com>
2018-10-01 20:57:50 +0530

    Refactor Voucher Migration

--- erpnext/erpnext_integrations/tally_migration.py
+++ erpnext/erpnext_integrations/tally_migration.py
@@ -16,30 +16,40 @@ def create_chart_of_accounts(company, accounts):
 
 	frappe.local.flags.ignore_on_update = True
 	create_root_accounts(company)
-	create_accounts(company, accounts)
+	saved_accounts = list(create_accounts(company, accounts))
 	rebuild_tree("Account", "parent_account")
 	frappe.db.commit()
 
 	frappe.clear_messages()
-
+	return saved_accounts
 
 @frappe.whitelist()
 def create_parties(company, parties):
 	parties = json.loads(parties)
 	make_custom_fields(["Customer", "Supplier"], "company")
-	_create_parties(company, parties)
+	saved_parties = list(_create_parties(company, parties))
 	frappe.db.commit()
 	frappe.clear_messages()
+	return saved_parties
 
+
+@frappe.whitelist()
+def create_items(company, items):
+	items = json.loads(items)
+	make_custom_fields(["Item"], "company")
+	saved_items = list(_create_items(items))
+	frappe.db.commit()
+	frappe.clear_messages()
+	return saved_items
 
 @frappe.whitelist()
 def create_vouchers(company, vouchers):
 	vouchers = json.loads(vouchers)
-	make_custom_fields(["Journal Entry"], "tally_id")
-	_create_vouchers(company, vouchers)
+	make_custom_fields(["Journal Entry", "Purchase Invoice", "Sales Invoice"], "tally_id")
+	saved_vouchers = list(_create_vouchers(vouchers))
 	frappe.db.commit()
 	frappe.clear_messages()
-
+	return saved_vouchers
 
 def create_root_accounts(company):
 	root_accounts = [
@@ -69,14 +79,16 @@ def create_accounts(company, accounts):
 	for account in accounts:
 		try:
 			account_name = encode_company_abbr(account["account_name"], company)
-			if not frappe.db.exists({"doctype": "Account", "name": account_name, "company": company}):
-				frappe.get_doc({
+			if not frappe.db.exists("Account", account_name):
+				account_type = {"Sundry Debtors": "Receivable", "Sundry Creditors": "Payable"}.get(account["account_name"])
+				yield frappe.get_doc({
 					"doctype": "Account",
 					"account_name": account["account_name"],
 					"parent_account": encode_company_abbr(account["parent_account"], company),
+					"account_type": account_type,
 					"is_group": account["is_group"],
 					"company": company,
-				}).insert()
+				}).insert().as_dict()
 		except:
 			traceback.print_exc()
 			print(account)
@@ -99,14 +111,15 @@ def _create_parties(company, parties):
 		try:
 			if party["party_type"] == "Customer":
 				if not frappe.db.exists({"doctype": "Customer", "customer_name": party["party_name"], "company": company}):
-					frappe.get_doc({
+					yield frappe.get_doc({
 						"doctype": "Customer",
 						"customer_name": party["party_name"],
 						"customer_group": "All Customer Groups",
 						"customer_territory": "All Territories",
 						"customer_type": "Individual",
+						"accounts": [{"company": company, "account": encode_company_abbr("Sundry Debtors", company)}],
 						"company": company,
-					}).insert()
+					}).insert().as_dict()
 			elif party["party_type"] == "Supplier":
 				if not frappe.db.exists({"doctype": "Supplier", "supplier_name": party["party_name"], "company": company}):
 					frappe.get_doc({
@@ -114,6 +127,7 @@ def _create_parties(company, parties):
 						"supplier_name": party["party_name"],
 						"supplier_group": "All Supplier Groups",
 						"supplier_type": "Individual",
+						"accounts": [{"company": company, "account": encode_company_abbr("Sundry Creditors", company)}],
 						"company": company,
 					}).insert()
 		except:
@@ -121,204 +135,75 @@ def _create_parties(company, parties):
 			print(party)
 
 
-def _create_vouchers(company, vouchers):
-	for voucher in vouchers:
+def _create_items(items):
+	for item in items:
 		try:
-			voucher_type_mapping = {
-				"Journal": create_journal_voucher,
-				"Sales": create_sales_voucher,
-				"Purchase": create_purchase_voucher,
-				"Payment": create_payment_voucher,
-				"Credit Note": create_credit_note_voucher,
-				"Receipt": create_receipt_voucher,
-				"Contra": create_contra_voucher,
-			}
-			function = voucher_type_mapping[voucher["voucher_type"]]
-			function(company, voucher)
+			if not frappe.db.exists("Item", item["item_code"]):
+				yield frappe.get_doc(item).insert().as_dict()
+		except:
+			traceback.print_exc()
+			print(item)
+
+
+def _create_vouchers(entries):
+	for entry in entries:
+		try:
+			saved_entry = {
+				"Journal Entry": create_journal_entry,
+				"Sales Invoice": create_invoice,
+				"Purchase Invoice": create_invoice,
+			}[entry["doctype"]](entry)
+			if saved_entry:
+				yield saved_entry
 		except:
 			import traceback
 			traceback.print_exc()
 
 
-def create_journal_voucher(company, voucher):
+def create_journal_entry(voucher):
 	try:
-		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["guid"], "company": company}):
+		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["tally_id"], "company": voucher["company"]}):
 			for account in voucher["accounts"]:
 				if account["is_party"]:
-					if frappe.db.exists({"doctype": "Customer", "customer_name": account["account"], "company": company}):
+					if frappe.db.exists({"doctype": "Customer", "customer_name": account["account"], "company": voucher["company"]}):
 						account["party_type"] = "Customer"
 						account["party"] = account["account"]
 						account["account"] = "Sundry Debtors"
-					elif frappe.db.exists({"doctype": "Supplier", "supplier_name": account["account"], "company": company}):
+					elif frappe.db.exists({"doctype": "Supplier", "supplier_name": account["account"], "company": voucher["company"]}):
 						account["party_type"] = "Supplier"
 						account["party"] = account["account"]
 						account["account"] = "Sundry Creditors"
-				account["account"] = encode_company_abbr(account["account"], company)
+				account["account"] = encode_company_abbr(account["account"], voucher["company"])
 
-			frappe.get_doc({
-				"doctype": "Journal Entry",
-				"naming_series": "JV-",
-				"tally_id": voucher["guid"],
-				"posting_date": voucher["posting_date"],
-				"company": company,
-				"accounts": voucher["accounts"],
-			}).insert().submit()
+			entry = frappe.get_doc(voucher).insert()
+			entry.submit()
+			return entry.as_dict()
+		else:
+			print("Already Exists : {}".format(voucher["tally_id"]))
 	except:
 		traceback.print_exc()
 		print(voucher)
 
 
-def create_sales_voucher(company, voucher):
+def create_invoice(voucher):
 	try:
-		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["guid"], "company": company}):
-			for account in voucher["accounts"]:
-				if account["is_party"]:
-					if frappe.db.exists({"doctype": "Customer", "customer_name": account["account"], "company": company}):
-						account["party_type"] = "Customer"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Debtors"
-				account["account"] = encode_company_abbr(account["account"], company)
+		if not frappe.db.exists({"doctype": voucher["doctype"], "tally_id": voucher["tally_id"], "company": voucher["company"]}):
+			if voucher["doctype"] == "Sales Invoice":
+				account_field = "income_account"
+			elif voucher["doctype"] == "Purchase Invoice":
+				account_field = "expense_account"
 
-			frappe.get_doc({
-				"doctype": "Journal Entry",
-				"naming_series": "JV-",
-				"tally_id": voucher["guid"],
-				"posting_date": voucher["posting_date"],
-				"company": company,
-				"accounts": voucher["accounts"],
-			}).insert().submit()
-	except:
-		traceback.print_exc()
-		print(voucher)
+			for item in voucher["items"]:
+				item[account_field] = encode_company_abbr(item[account_field], voucher["company"])
 
+			for tax in voucher["taxes"]:
+				tax["account_head"] = encode_company_abbr(tax["account_head"], voucher["company"])
 
-def create_purchase_voucher(company, voucher):
-	try:
-		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["guid"], "company": company}):
-			for account in voucher["accounts"]:
-				if account["is_party"]:
-					if frappe.db.exists({"doctype": "Supplier", "supplier_name": account["account"], "company": company}):
-						account["party_type"] = "Supplier"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Creditors"
-				account["account"] = encode_company_abbr(account["account"], company)
-
-			frappe.get_doc({
-				"doctype": "Journal Entry",
-				"naming_series": "JV-",
-				"tally_id": voucher["guid"],
-				"posting_date": voucher["posting_date"],
-				"company": company,
-				"accounts": voucher["accounts"],
-			}).insert().submit()
-	except:
-		traceback.print_exc()
-		print(voucher)
-
-
-def create_payment_voucher(company, voucher):
-	try:
-		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["guid"], "company": company}):
-			for account in voucher["accounts"]:
-				if account["is_party"]:
-					if frappe.db.exists({"doctype": "Customer", "customer_name": account["account"], "company": company}):
-						account["party_type"] = "Customer"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Debtors"
-					elif frappe.db.exists({"doctype": "Supplier", "supplier_name": account["account"], "company": company}):
-						account["party_type"] = "Supplier"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Creditors"
-				account["account"] = encode_company_abbr(account["account"], company)
-
-			frappe.get_doc({
-				"doctype": "Journal Entry",
-				"naming_series": "JV-",
-				"tally_id": voucher["guid"],
-				"posting_date": voucher["posting_date"],
-				"company": company,
-				"accounts": voucher["accounts"],
-			}).insert().submit()
-	except:
-		traceback.print_exc()
-		print(voucher)
-
-
-def create_credit_note_voucher(company, voucher):
-	try:
-		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["guid"], "company": company}):
-			for account in voucher["accounts"]:
-				if account["is_party"]:
-					if frappe.db.exists({"doctype": "Customer", "customer_name": account["account"], "company": company}):
-						account["party_type"] = "Customer"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Debtors"
-				account["account"] = encode_company_abbr(account["account"], company)
-
-			frappe.get_doc({
-				"doctype": "Journal Entry",
-				"naming_series": "JV-",
-				"tally_id": voucher["guid"],
-				"posting_date": voucher["posting_date"],
-				"company": company,
-				"accounts": voucher["accounts"],
-			}).insert().submit()
-	except:
-		traceback.print_exc()
-		print(voucher)
-
-
-def create_receipt_voucher(company, voucher):
-	try:
-		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["guid"], "company": company}):
-			for account in voucher["accounts"]:
-				if account["is_party"]:
-					if frappe.db.exists({"doctype": "Customer", "customer_name": account["account"], "company": company}):
-						account["party_type"] = "Customer"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Debtors"
-					elif frappe.db.exists({"doctype": "Supplier", "supplier_name": account["account"], "company": company}):
-						account["party_type"] = "Supplier"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Creditors"
-				account["account"] = encode_company_abbr(account["account"], company)
-
-			frappe.get_doc({
-				"doctype": "Journal Entry",
-				"naming_series": "JV-",
-				"tally_id": voucher["guid"],
-				"posting_date": voucher["posting_date"],
-				"company": company,
-				"accounts": voucher["accounts"],
-			}).insert().submit()
-	except:
-		traceback.print_exc()
-		print(voucher)
-
-
-def create_contra_voucher(company, voucher):
-	try:
-		if not frappe.db.exists({"doctype": "Journal Entry", "tally_id": voucher["guid"], "company": company}):
-			for account in voucher["accounts"]:
-				if account["is_party"]:
-					if frappe.db.exists({"doctype": "Customer", "customer_name": account["account"], "company": company}):
-						account["party_type"] = "Customer"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Debtors"
-					elif frappe.db.exists({"doctype": "Supplier", "supplier_name": account["account"], "company": company}):
-						account["party_type"] = "Supplier"
-						account["party"] = account["account"]
-						account["account"] = "Sundry Creditors"
-				account["account"] = encode_company_abbr(account["account"], company)
-
-			frappe.get_doc({
-				"doctype": "Journal Entry",
-				"naming_series": "JV-",
-				"tally_id": voucher["guid"],
-				"posting_date": voucher["posting_date"],
-				"company": company,
-				"accounts": voucher["accounts"],
-			}).insert().submit()
+			entry = frappe.get_doc(voucher).insert()
+			entry.submit()
+			return entry.as_dict()
+		else:
+			print("Already Exists : {}".format(voucher["tally_id"]))
 	except:
 		traceback.print_exc()
 		print(voucher)
@@ -345,8 +230,39 @@ def null():
 @frappe.whitelist()
 def clean_all():
 	frappe.db.sql("""DELETE FROM `tabAccount`""")
+
 	frappe.db.sql("""DELETE FROM `tabCustomer`""")
 	frappe.db.sql("""DELETE FROM `tabSupplier`""")
+
+	frappe.db.sql("""DELETE FROM `tabItem`""")
+	frappe.db.sql("""DELETE FROM `tabItem Default`""")
+
+	frappe.db.sql("""DELETE FROM `tabSales Invoice`""")
+	frappe.db.sql("""DELETE FROM `tabSales Invoice Item`""")
+	frappe.db.sql("""DELETE FROM `tabSales Taxes and Charges`""")
+
+	frappe.db.sql("""DELETE FROM `tabPurchase Invoice`""")
+	frappe.db.sql("""DELETE FROM `tabPurchase Invoice Item`""")
+	frappe.db.sql("""DELETE FROM `tabPurchase Taxes and Charges`""")
+
 	frappe.db.sql("""DELETE FROM `tabJournal Entry`""")
+	frappe.db.sql("""DELETE FROM `tabJournal Entry Account`""")
+
+	frappe.db.sql("""DELETE FROM `tabGL Entry`""")
+	frappe.db.commit()
+
+@frappe.whitelist()
+def clean_entries():
+	frappe.db.sql("""DELETE FROM `tabSales Invoice`""")
+	frappe.db.sql("""DELETE FROM `tabSales Invoice Item`""")
+	frappe.db.sql("""DELETE FROM `tabSales Taxes and Charges`""")
+
+	frappe.db.sql("""DELETE FROM `tabPurchase Invoice`""")
+	frappe.db.sql("""DELETE FROM `tabPurchase Invoice Item`""")
+	frappe.db.sql("""DELETE FROM `tabPurchase Taxes and Charges`""")
+
+	frappe.db.sql("""DELETE FROM `tabJournal Entry`""")
+	frappe.db.sql("""DELETE FROM `tabJournal Entry Account`""")
+
 	frappe.db.sql("""DELETE FROM `tabGL Entry`""")
 	frappe.db.commit()
