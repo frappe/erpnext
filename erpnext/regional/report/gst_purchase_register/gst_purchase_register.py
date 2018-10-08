5813ba7801e8a488ba6f65ef873ef995044cb0b2
Zarrar <zarrar65@gmail.com>
2018-10-08 14:34:34 +0530

    columns without fieldname's data not shown (#15618)

--- erpnext/regional/report/gst_purchase_register/gst_purchase_register.py
+++ erpnext/regional/report/gst_purchase_register/gst_purchase_register.py
@@ -7,12 +7,12 @@ from erpnext.accounts.report.purchase_register.purchase_register import _execute
 
 def execute(filters=None):
 	return _execute(filters, additional_table_columns=[
-		dict(fieldtype='Data', label='Supplier GSTIN', width=120),
-		dict(fieldtype='Data', label='Company GSTIN', width=120),
-		dict(fieldtype='Data', label='Reverse Charge', width=120),
-		dict(fieldtype='Data', label='Invoice Type', width=120),
-		dict(fieldtype='Data', label='Export Type', width=120),
-		dict(fieldtype='Data', label='E-Commerce GSTIN', width=130)
+		dict(fieldtype='Data', label='Supplier GSTIN', fieldname="supplier_gstin", width=120),
+		dict(fieldtype='Data', label='Company GSTIN', fieldname="company_gstin", width=120),
+		dict(fieldtype='Data', label='Reverse Charge', fieldname="reverse_charge", width=120),
+		dict(fieldtype='Data', label='Invoice Type', fieldname="invoice_type", width=120),
+		dict(fieldtype='Data', label='Export Type', fieldname="export_type", width=120),
+		dict(fieldtype='Data', label='E-Commerce GSTIN', fieldname="ecommerce_gstin", width=130)
 	], additional_query_columns=[
 		'supplier_gstin',
 		'company_gstin',
