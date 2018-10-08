faecbbf4c137879fe58cfe69a345e17a761691d4
scmmishra <scm.mymail@gmail.com>
2018-10-08 17:18:11 +0530

    templates: add templates for LMS content
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>

--- erpnext/www/lms/course.py
+++ erpnext/www/lms/course.py
@@ -1,2 +1,8 @@
-# current_module = frappe.get_doc("Course", frappe.form_dict["lesson"])
-# current_lesson = frappe.get_doc("Lesson", frappe.form_dict["lesson"])
+from __future__ import unicode_literals
+import frappe
+
+
+def get_context(context):
+    context.current_course = frappe.get_doc("Course", frappe.form_dict["course"])
+    context.current_content = frappe.get_doc("Content", frappe.form_dict["content"])
+    next_content = get_next_content()
