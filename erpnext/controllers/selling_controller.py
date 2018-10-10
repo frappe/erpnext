34acb759606a76ec1b0e0978b7f88e977d99d7f6
Shreya <shreyashah115@gmail.com>
2018-10-10 16:28:30 +0530

    fix(shipping-address-display): Fetch shipping address display as per the shipping address

--- erpnext/controllers/selling_controller.py
+++ erpnext/controllers/selling_controller.py
@@ -63,7 +63,7 @@ class SellingController(StockController):
 				ignore_permissions=self.flags.ignore_permissions,
 				doctype=self.doctype, company=self.company,
 				fetch_payment_terms_template=fetch_payment_terms_template,
-				party_address=self.customer_address, shipping_address=self.shipping_address)
+				party_address=self.customer_address, shipping_address=self.shipping_address_name)
 			if not self.meta.get_field("sales_team"):
 				party_details.pop("sales_team")
 			self.update_if_missing(party_details)
