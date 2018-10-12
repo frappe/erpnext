0945256832145b72172bbfb04880e49e294a4c08
scmmishra <scm.mymail@gmail.com>
2018-10-12 15:12:04 +0530

    Modified Student Course Activity Doctype
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>

--- erpnext/education/doctype/student_course_activity/student_course_activity.py
+++ erpnext/education/doctype/student_course_activity/student_course_activity.py
@@ -12,10 +12,11 @@ class StudentCourseActivity(Document):
 		# self.check_if_course_present()
 
 	def check_if_enrolled(self):
-		programs_list = frappe.get_list("Program Enrollment", filters={'student': self.student_name}, fields=['program'])
-		programs_enrolled_by_student = [item.program for item in programs_list]
-		if self.program_name not in programs_enrolled_by_student:
-			frappe.throw("Student <b>{0}</b> is not enrolled in <b>program {1}</b> ".format(self.student_name, self.program_name))
+		pass
+		# programs_list = frappe.get_list("Program Enrollment", filters={'student': self.student_id}, fields=['program'])
+		# programs_enrolled_by_student = [item.program for item in programs_list]
+		# if self.program_name not in programs_enrolled_by_student:
+		# 	frappe.throw("Student <b>{0}</b> is not enrolled in <b>program {1}</b> ".format(self.student_id, self.program_name))
 
 	# def check_if_course_present(self):
 	# 	""" Get set of unique courses from lms_activity child table and make a list of it as assign it to course_list"""
