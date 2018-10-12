45d888e99ef4d39c78ae9f98ee4c92416bae3539
scmmishra <scm.mymail@gmail.com>
2018-10-12 15:22:16 +0530

    Templates: modified routing logic, added data attributes in template
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>

--- erpnext/www/lms/course.py
+++ erpnext/www/lms/course.py
@@ -3,6 +3,17 @@ import frappe
 
 
 def get_context(context):
-    context.current_course = frappe.get_doc("Course", frappe.form_dict["course"])
-    context.current_content = frappe.get_doc("Content", frappe.form_dict["content"])
-    next_content = get_next_content()
+    if frappe.form_dict['course']:
+        context.current_course = frappe.get_doc("Course", frappe.form_dict["course"])
+        context.current_content = frappe.get_doc("Content", frappe.form_dict["content"])
+        context.current_program = frappe.form_dict["program"]
+        context.next_content = get_next_content(context)
+
+
+def get_next_content(context):
+    if context.current_course:
+        course_data = [content_item.content for content_item in context.current_course.course_content]
+        try: 
+            return course_data[course_data.index(context.current_content.name) + 1]
+        except IndexError:
+            return None
