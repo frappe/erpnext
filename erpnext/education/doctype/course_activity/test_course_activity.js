e7308125b42cf29ffca814e0a9d0a42f87092304
scmmishra <scm.mymail@gmail.com>
2018-10-15 16:00:04 +0530

    Replaced Student Course Activity with Course Activity

--- erpnext/education/doctype/course_activity/test_course_activity.js
+++ erpnext/education/doctype/course_activity/test_course_activity.js
@@ -2,15 +2,15 @@
 // rename this file from _test_[name] to test_[name] to activate
 // and remove above this line
 
-QUnit.test("test: Student Course Activity", function (assert) {
+QUnit.test("test: Course Activity", function (assert) {
 	let done = assert.async();
 
 	// number of asserts
 	assert.expect(1);
 
 	frappe.run_serially([
-		// insert a new Student Course Activity
-		() => frappe.tests.make('Student Course Activity', [
+		// insert a new Course Activity
+		() => frappe.tests.make('Course Activity', [
 			// values to be set
 			{key: 'value'}
 		]),
