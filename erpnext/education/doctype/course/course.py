9a9e8a3473ca917e297d32a646f67b2efaaa8ba3
Chinmay Pai <chinmaydpai@gmail.com>
2018-10-17 23:57:21 +0530

    lms: refactor entire course model
    each course item now has its own doctype.
    
    Co-authored-by: Shivam Mishra <scm.mymail@gmail.com>
    Signed-off-by: Chinmay Pai <chinmaydpai@gmail.com>

--- erpnext/education/doctype/course/course.py
+++ erpnext/education/doctype/course/course.py
@@ -10,7 +10,7 @@ from frappe import _
 class Course(Document):
 	def validate(self):
 		self.validate_assessment_criteria()
-	
+
 	def validate_assessment_criteria(self):
 		if self.assessment_criteria:
 			total_weightage = 0
@@ -18,3 +18,19 @@ class Course(Document):
 				total_weightage += criteria.weightage or 0
 			if total_weightage != 100:
 				frappe.throw(_("Total Weightage of all Assessment Criteria must be 100%"))
+
+	def get_content_data(self, data):
+		try:
+			course_content_list = self.get_all_children()
+			content_data = [frappe.get_value(course_content.content_type, course_content.content, data) for course_content in course_content_list]
+		except Exception as e:
+			print(e)
+			return None
+		return content_data
+
+	def get_content_title(self):
+		'''
+		returns all the course content for the given course object.
+		'''
+		content_title = self.get_content_data("title")
+		return content_title
---
+++ erpnext/education/doctype/quiz/quiz.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class Quiz(Document):
+
+
+	def get_quiz(self):
+		pass
+
+
+	def evaluate_quiz(self):
+		pass
+
+
+	def get_question(self):
+		pass
+
