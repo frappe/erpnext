373be1e65dd8cb4d5a75eb9c0053312733af1e21
scmmishra <scm.mymail@gmail.com>
2018-10-18 15:44:08 +0530

    LMS: Fixed views for article and video

--- erpnext/www/lms/program.py
+++ erpnext/www/lms/program.py
@@ -4,5 +4,8 @@ import frappe
 
 
 def get_context(context):
-    context.program = frappe.get_doc("Program", frappe.form_dict["program"])
-    context.course_list = utils.get_courses_in_program(frappe.form_dict["program"])
+    program = frappe.get_doc("Program", frappe.form_dict["program"])
+    course_list = program.get_course_list()
+
+    context.program = program
+    context.course_list = course_list
