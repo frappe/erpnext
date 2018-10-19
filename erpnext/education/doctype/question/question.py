7e3f9082ed6a8fc76de3cdec5ebaade0cfa2ca38
scmmishra <scm.mymail@gmail.com>
2018-10-19 14:06:14 +0530

    Quiz eval utils

--- erpnext/education/doctype/question/question.py
+++ erpnext/education/doctype/question/question.py
@@ -7,4 +7,14 @@ import frappe
 from frappe.model.document import Document
 
 class Question(Document):
-	pass
+
+	def get_answer(self):
+		options = self.get_all_children()
+		answers = [item.option for item in options if item.is_correct == True]
+		if len(answers) == 0:
+			frappe.throw("No correct answer is set for {0}".format(self.name))
+			return None
+		elif len(answers) == 1:
+			return answers[0]
+		else:
+			return answers
