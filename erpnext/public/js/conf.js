c6b4125f49cc5962db0db24019a43b11064f04cf
Saif Ur Rehman <saifi0102@gmail.com>
2018-10-21 03:22:34 +0500

    Company name on navbar to avoid confusion between instances of ERPNext

--- erpnext/public/js/conf.js
+++ erpnext/public/js/conf.js
@@ -11,8 +11,7 @@ $(document).bind('toolbar_setup', function() {
 		href="https://discuss.erpnext.com">Feedback</a></p>'
 
 
-	$('.navbar-home').html('<img class="erpnext-icon" src="'+
-			frappe.urllib.get_base_url()+'/assets/erpnext/images/erp-icon.svg" />');
+	$('.navbar-home').html(frappe.defaults.get_default('company'));
 
 	$('[data-link="docs"]').attr("href", "https://frappe.github.io/erpnext/")
 	$('[data-link="issues"]').attr("href", "https://github.com/frappe/erpnext/issues")
