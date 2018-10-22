3574ac3f5ebfc5d532e5d4d7aefc7278e5461a0f
Chinmay Pai <chinmaydpai@gmail.com>
2018-10-22 15:29:05 +0530

    quiz_activity: add quiz activity
    Signed-off-by: Chinmay Pai <chinmaydpai@gmail.com>

--- erpnext/education/doctype/quiz/quiz.py
+++ erpnext/education/doctype/quiz/quiz.py
@@ -16,8 +16,8 @@ class Quiz(Document):
 	def evaluate(self, response_dict):
 		self.get_questions()
 		answers = {q.name:q.get_answer() for q in self.get_questions()}
-		correct_answers = [answers[question] == response_dict[question] for question in response_dict.keys()]
-		return (sum(correct_answers) * 100 ) / len(answers)
+		correct_answers = {question: (answers[question] == response_dict[question]) for question in response_dict.keys()}
+		return correct_answers, (sum(correct_answers.values()) * 100 ) / len(answers)
 
 
 	def get_questions(self):
