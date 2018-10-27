8e07fcf5530209429716c92e4360c52b0192082b
Saif Ur Rehman <saifi0102@gmail.com>
2018-10-27 18:22:30 +0500

    Patch and minor fix: -Added a patch to migrate Journal Entry cheque_no, cheque_date and clearance_date to Journal Entry Account -Removed set_reqd for cheque_no and cheque_date Journal Entry with type Bank Entry

---
+++ erpnext/patches/v11_0/migrate_reference_and_clearance_to_journal_entry_accounts_table.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2018, Frappe and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	frappe.reload_doc("accounts", "doctype", "journal_entry")
+	frappe.reload_doc("accounts", "doctype", "journal_entry_account")
+
+	# Migrate clearance_date for only Bank and Cash accounts
+	frappe.db.sql("""
+		update `tabJournal Entry Account` jvd
+		inner join `tabJournal Entry Account` jv on jv.name = jvd.parent
+		inner join `tabAccount` acc on acc.name = jvd.account
+		set jvd.clearance_date = jv.clearance_date
+		where acc.account_type = 'Bank' or acc.account_type = 'Cash'
+	""")
+
+	# Migrate reference_no and reference_date for all account types
+	frappe.db.sql("""
+		update `tabJournal Entry Account` jvd
+		inner join `tabJournal Entry Account` jv on jv.name = jvd.parent
+		set jvd.cheque_no = jv.cheque_no, jvd.cheque_date = jv.cheque_date
+	""")
