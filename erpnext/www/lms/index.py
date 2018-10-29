75ad128912176b3ee60c1139ee5db01737328fd6
scmmishra <scm.mymail@gmail.com>
2018-10-29 12:30:19 +0530

    Added LMS Settings

--- erpnext/www/lms/index.py
+++ erpnext/www/lms/index.py
@@ -4,3 +4,4 @@ import erpnext.education.utils as utils
 
 def get_context(context):
     context.featured = utils.get_featured_programs()
+    context.settings = frappe.get_doc("Education Settings")
