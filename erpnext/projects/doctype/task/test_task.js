2a093ca2933144b9a5f82e7782943a956308fef0
Brown-Harry Boma <brownharryb@gmail.com>
2018-10-30 17:57:22 +0100

    Add Test

---
+++ erpnext/projects/doctype/task/test_task.js
@@ -0,0 +1,33 @@
+
+QUnit.test("test: Task", function (assert) {
+	let done = assert.async();
+	assert.expect(2);
+
+	frappe.run_serially([
+		() => frappe.tests.make('Task Description',[
+			{title:"Test Desc One"},
+			{task_description:"Without Jinja"}
+		]),
+		() => frappe.tests.make('Task Description',[
+			{title:"Test Desc Two"},
+			{task_description:"With Jinja subject = {{subject}}"}
+		]),
+		() => frappe.timeout(2),
+		() => frappe.tests.make('Task', [
+			{subject: '_Test Task1'},
+			{task_description: 'Test Desc One'}
+		]),
+		() => {
+			assert.equal(cur_frm.doc.description, 'Without Jinja');
+		},
+		() => frappe.tests.make('Task', [
+			{subject: '_Test Task2'},
+			{task_description: 'Test Desc Two'}
+		]),
+		() => {
+			assert.equal(cur_frm.doc.description, 'With Jinja subject = _Test Task2')
+		},
+		() => done()
+	]);
+
+});
