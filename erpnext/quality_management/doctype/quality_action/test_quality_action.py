babc252df85ae32f007d8fcd866f60339615416d
hrwX <himanshuwarekar@yahoo.com>
2018-10-31 12:00:47 +0530

    changes

--- erpnext/quality_management/doctype/quality_action/test_quality_action.py
+++ erpnext/quality_management/doctype/quality_action/test_quality_action.py
@@ -9,12 +9,29 @@ import unittest
 test_dependencies = ["Quality Procedure","Measurement Unit" ,"Quality Goal", "Quality Review"]
 
 class TestQualityAction(unittest.TestCase):
-
 	def test_quality_action(self):
-		test_review, test_action = get_action()
-		self.assertEquals(test_review.name, test_action.review)
+		test_create_action = create_action()
+		test_get_action = get_action()
+		self.assertEquals(test_create_action.name, test_get_action.name)
+
+def create_action():
+	review = frappe.get_list("Quality Review")
+	action = frappe.get_doc({
+		'doctype': 'Quality Action',
+		'action': 'Corrective',
+		'type': 'Quality Review',
+		'review': ''+ review[0].name +'',
+		'date': ''+ frappe.utils.nowdate() +'',
+		'procedure': '_Test Quality Procedure'
+	})
+	action_exist = frappe.get_list("Quality Action", filters={"name": ""+ review[0].name +""})
+	if len(action_exist) == 0:
+		action.insert()
+		return action
+	else:
+		return action_exist[0]
 
 def get_action():
-	review = frappe.get_list("Quality Review", fields=["name"])
-	action = frappe.get_list("Quality Action", filters={"review": ""+ review[0].name +""}, fields=["name" ,"review"])
-	return review[0], action[0]
+	review = frappe.get_list("Quality Review")
+	action = frappe.get_list("Quality Action", filters={"name": ""+ review[0].name +""})
+	return action[0]
