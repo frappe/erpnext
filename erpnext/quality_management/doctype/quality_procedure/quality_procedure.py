a464f66b5d642a15606ff3f2a6884e6d6fba952f
hrwX <himanshuwarekar@yahoo.com>
2018-10-31 23:15:08 +0530

    code refactor

--- erpnext/quality_management/doctype/quality_procedure/quality_procedure.py
+++ erpnext/quality_management/doctype/quality_procedure/quality_procedure.py
@@ -23,11 +23,10 @@ class QualityProcedure(NestedSet):
 				doc.save()
 
 	def on_trash(self):
-		if(self.parent_quality_procedure):
+		if self.parent_quality_procedure:
 			doc = frappe.get_doc("Quality Procedure", self.parent_quality_procedure)
-			is_group_flag = "Unset"
 			for data in doc.procedure_step:
-				if self.name == data.procedure_name:
+				if data.procedure_name == self.name:
 					doc.procedure_step.remove(data)
 					doc.save()
 
