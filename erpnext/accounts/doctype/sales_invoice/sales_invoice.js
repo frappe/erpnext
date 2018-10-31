4f8aa799d8ad0b21789d5b2ee65afa8c5914971c
RicardoJohann <johannricardo@yahoo.com.br>
2018-10-31 20:41:56 -0300

    Add Serial Number button in Delivery Note and Sales Invoice's Packing List

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.js
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.js
@@ -313,6 +313,10 @@ erpnext.accounts.SalesInvoiceController = erpnext.selling.SellingController.exte
 		erpnext.setup_serial_no();
 	},
 
+	packed_items_on_form_rendered: function(doc, grid_row) {
+		erpnext.setup_serial_no();
+	},
+
 	make_sales_return: function() {
 		frappe.model.open_mapped_doc({
 			method: "erpnext.accounts.doctype.sales_invoice.sales_invoice.make_sales_return",
