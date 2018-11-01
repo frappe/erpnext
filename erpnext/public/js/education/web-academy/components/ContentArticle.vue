15ebcbcf36b65189cc0cc2e0b7c14275fc8563f9
scmmishra <scm.mymail@gmail.com>
2018-11-01 19:51:33 +0530

    UI for Quiz

--- erpnext/public/js/education/web-academy/components/ContentArticle.vue
+++ erpnext/public/js/education/web-academy/components/ContentArticle.vue
@@ -4,9 +4,9 @@
         <div class='container'>
             <div class="row">
                 <div class="col-md-8">
-                    <h2>{{ content_data.title }}</h2>
+                    <h2>{{ contentData.title }}</h2>
                     <span class="text-muted">
-                        Published on {{ content_data.publish_date }}, by {{ content_data.author }}
+                        Published on {{ contentData.publish_date }}, by {{ contentData.author }}
                     </span>
                 </div>
                 <div class="col-md-4 text-right">
@@ -18,7 +18,7 @@
     </section>
     <section class="article-content-section">
         <div class='container'>
-            <div class="content" v-html="content_data.content"></div>
+            <div class="content" v-html="contentData.content"></div>
             <div class="text-right">
             </div>
             <div class="mt-3 text-right">
@@ -35,7 +35,7 @@ export default {
 	name: 'ContentArticle',
 	data() {
     	return {
-    		content_data: ''
+    		contentData: ''
     	}
     },
     mounted() {
@@ -46,7 +46,7 @@ export default {
     			content_type: this.type
     		}
     	}).then(r => {
-    			this.content_data = r.message
+    			this.contentData = r.message
     	});
     }
 };
---
+++ erpnext/public/js/education/web-academy/components/Quiz/QuizSingleChoice.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="question mt-4">
+    <h5>{{ question.question }}</h5>
+    <div class="options ml-2">
+        <div v-for="option in question.options" class="form-check pb-1">
+            <input class="form-check-input" type="radio" :name="question.name" :id="option.option" :value="option.option">
+            <label class="form-check-label" :for="option.option">
+                {{ option.option }}
+            </label>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+	props: ['question'],
+	name: 'QuizSingleChoice',
+};
+</script>
+
+<style lang="css" scoped>
+</style>
