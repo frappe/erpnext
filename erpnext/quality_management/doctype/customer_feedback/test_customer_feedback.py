facba9e5f6b439eebaad026ea3efd527e97b85ed
hrwX <himanshuwarekar@yahoo.com>
2018-11-01 15:38:03 +0530

    Unit Test crashes Fix

--- erpnext/quality_management/doctype/customer_feedback/test_customer_feedback.py
+++ erpnext/quality_management/doctype/customer_feedback/test_customer_feedback.py
@@ -8,6 +8,7 @@ import unittest
 
 class TestCustomerFeedback(unittest.TestCase):
 	def test_customer_feedback(self):
+		create_feedback_template()
 		test_create_feedback = create_feedback()
 		test_get_feedback = get_feedback()
 		self.assertEquals(test_create_feedback.name, test_get_feedback.name)
@@ -15,6 +16,7 @@ class TestCustomerFeedback(unittest.TestCase):
 def create_feedback():
 	feedback = frappe.get_doc({
 		"doctype": "Customer Feedback",
+		"template": "FDBK-TMPL-_Test Customer Feedback Template",
 		"date": ""+ frappe.utils.nowdate() +""
 	})
 	feedback_exist = frappe.get_list("Customer Feedback", filters={"date": ""+ feedback.date +""})
@@ -27,3 +29,19 @@ def create_feedback():
 def get_feedback():
 	feedback = frappe.get_list("Customer Feedback")
 	return feedback[0]
+
+def create_feedback_template():
+	print("Templte")
+	template = frappe.get_doc({
+		"doctype": "Customer Feedback Template",
+		"template": "_Test Customer Feedback Template",
+		"scope": "Company",
+		"feedback_parameter": [
+			{
+				"parameter": "_Test Customer Feedback Template Parameter",
+			}
+		]
+	})
+	template_exist = frappe.get_list("Customer Feedback Template", filters={"template": ""+ template.template +""}, fields=["name"])
+	if len(template_exist) == 0:
+		template.insert()
