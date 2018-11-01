bcafbea4810662a70300b5740e1bbfb7455b2538
scmmishra <scm.mymail@gmail.com>
2018-11-01 17:19:53 +0530

    UI for Course page

--- erpnext/public/js/education/web-academy/pages/AcademyCoursePage.vue
+++ erpnext/public/js/education/web-academy/pages/AcademyCoursePage.vue
@@ -1,10 +1,85 @@
 <template>
-<div>
-	Course Page
-</div>
+	<div>
+		<component v-bind:is="currentComponent" :content="content" :type="type">
+			<ContentNavigation :nextContent="nextContent" :nextContentType="nextContentType"/>
+		</component>
+	</div>
 </template>
 <script>
+import ContentArticle from "../components/ContentArticle.vue"
+import ContentQuiz from "../components/ContentQuiz.vue"
+import ContentVideo from "../components/ContentVideo.vue"
+import ContentNavigation from "../components/ContentNavigation.vue"
+
 export default {
-    name: "AcademyCoursePage"
+	props:['code', 'course', 'type', 'content'],
+	name: "AcademyCoursePage",
+	data() {
+		return{
+			nextContent: '',
+			nextContentType: '',
+		}
+	},
+	computed: {
+	  currentComponent: function() {
+	  	if(this.type === "Article") {
+	  		return 'ContentArticle'
+	  	}
+	  	else if(this.type === "Quiz") {
+	  		return 'ContentQuiz'
+	  	}
+	  	else if(this.type === "Video") {
+	  		return 'ContentVideo'
+	  	}
+	  },
+	},
+	mounted() {
+	  	frappe.call({
+	  		method: "erpnext.www.academy.get_next_content",
+	  		args:{
+	  			content: this.content,
+	  			content_type: this.type,
+	  			course: this.course
+	  		}
+	  	}).then(r => {
+	  		this.nextContent = r.message.content,
+	  		this.nextContentType = r.message.content_type
+	  	});
+	},
+	components: {
+		ContentArticle,
+		ContentVideo,
+		ContentQuiz,
+		ContentNavigation
+	}
 };
 </script>
+
+<style>
+.footer-message {
+	display: none;
+}
+
+.video-top-section {
+	padding-top: 3rem !important;
+	padding-bottom: 1rem !important;
+}
+
+.video-description-section {
+	padding-top: 0em !important;
+}
+
+.article-top-section {
+	padding-top: 0.5em !important;
+	padding-bottom: 0rem !important;
+}
+
+.article-content-section {
+	padding-top: 0em !important;
+}
+
+.quiz-section {
+	padding-top: 3rem !important;
+	padding-bottom: 0rem !important;
+}
+</style>
