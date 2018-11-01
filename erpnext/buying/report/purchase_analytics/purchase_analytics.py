522777527266e6aa6a1edf88fbd0be38aaea6d5b
deepeshgarg007 <deepeshgarg6@gmail.com>
2018-11-01 15:46:09 +0530

    Indentation Issue Fix

--- erpnext/buying/report/purchase_analytics/purchase_analytics.py
+++ erpnext/buying/report/purchase_analytics/purchase_analytics.py
@@ -4,7 +4,8 @@
 from __future__ import unicode_literals
 import frappe
 from erpnext.selling.report.sales_analytics.sales_analytics import (get_columns, get_period,
-	get_period_date_ranges, get_chart_data, get_item_by_group, get_data_list, get_item_data,get_groups,get_by_item_group)
+	get_period_date_ranges, get_chart_data, get_type_data, get_by_group, get_data_list, get_item_entry, 
+	get_item_by_group, get_items, get_groups)
 
 
 def execute(filters=None):
@@ -18,16 +19,16 @@ def execute(filters=None):
 
 def get_supplier_by_group(filters):
 
-	return frappe.db.sql("""select c.name, c.supplier_name, c.supplier_group, g.lft, g.rgt
+	return frappe.db.sql("""select c.name, c.supplier_name as type_name, c.supplier_group as grp, g.lft, g.rgt
 							from `tabSupplier` c ,`tab{tree_type}` g
 							where c.supplier_group = g.name"""
 			.format(tree_type=filters["tree_type"]), as_dict=1)
 
 def get_supplier():
-	return frappe.get_list("Supplier", fields=["name","supplier_name"])
+	return frappe.get_list("Supplier", fields=["name","supplier_name as type_name"])
 
-def get_supplier_data(filters):
-	data=[]
+def get_supplier_entry(filters):
+	
 	date_field = filters["doc_type"] == 'Purchase Order' and 'transaction_date' or 'posting_date'
 
 	if filters["value_quantity"] == 'Value':
@@ -44,100 +45,34 @@ def get_supplier_data(filters):
 		}
 	)
 
-	data_list = get_data_list(entry,filters)
-
-	if filters["tree_type"] == 'Supplier Group':
-		return data_list
-
-	ranges = get_period_date_ranges(filters["range"],year_start_date=filters["from_date"], year_end_date=filters["to_date"])
-
-	for d in get_supplier():
-		supplier = {}
-		total = 0
-		supplier["name"] = d.name
-		supplier["code"] = d.supplier_name
-		for dummy, end_date in ranges:
-			period = get_period(end_date, filters["range"])
-
-			if data_list.get(d.name) and data_list.get(d.name).get(period) :
-				supplier[period] = data_list.get(d.name).get(period)
-			else:
-				supplier[period] = 0.0
-			total += supplier[period]
-		supplier["total"] = total
-		data.append(supplier)
-
-	return data
-
-def get_by_group(filters):
-	data = []
-
-	groups = get_groups(filters)
-
-	data_list = get_supplier_data(filters)
-
-	ranges = get_period_date_ranges(filters["range"],year_start_date=filters["from_date"], year_end_date=filters["to_date"])
-
-	supp = get_supplier_by_group(filters)
-
-	previous_lft = 0
-	previous_rgt = 10000000000
-	indent = -1 
-
-	for g in groups:
-		out = []
-
-		if g.lft > previous_lft and g.rgt < previous_rgt:
-			indent += 1
-		else:
-			indent -= 1
-
-		group = {}
-		g_total = 0
-		group["name"] = g.name
-		group["indent"] = indent
-		group["code"] = g.name
-		for d in supp:
-			if d.lft >= g.lft and d.rgt <= g.rgt :
-				supplier = {}
-				total = 0
-				supplier["name"] = d.name
-				supplier["code"] = d.supplier_name
-				supplier["indent"] = indent + 1
-				for dummy, end_date in ranges:
-					period = get_period(end_date, filters["range"])
-					if data_list.get(d.name) and data_list.get(d.name).get(period) :
-						supplier[period] = data_list.get(d.name).get(period)
-					else:
-						supplier[period] = 0.0
-					total += supplier[period]
-					if group.get(period):
-						group[period] += supplier[period]
-					else:
-						group[period] = supplier[period]
-				supplier["total"] = total
-				g_total += total
-				if d.supplier_group== g.name:
-					out.append(supplier)
-		group["total"] = g_total
-		data.append(group)
-		data += out
-		previous_lft = g.lft
-		previous_rgt = g.rgt
-
-	return data
-
+	return entry
 
 def gen_data(filters):
 
 	if filters["tree_type"] == 'Supplier':
-		return get_supplier_data(filters)
+
+		entry = get_supplier_entry(filters)
+		suppliers = get_supplier()
+
+		return get_type_data(filters,entry,suppliers)
 		
 	elif filters["tree_type"] == 'Item':
-		return get_item_data(filters)
-	
+
+		entry = get_item_entry(filters)
+		items = get_items(filters)
+
+		return get_type_data(filters,entry,items)
+
 	elif filters["tree_type"] == 'Supplier Group' :
-		return get_by_group(filters)
+
+		supplier = get_supplier_by_group(filters)
+		entry = get_supplier_entry(filters)
+
+		return get_by_group(filters, supplier, entry)
 
 	elif filters["tree_type"] == 'Item Group' :
-		return get_by_item_group(filters)
+
+		items = get_item_by_group()
+		entry = get_item_entry(filters)
+
+		return get_by_group(filters,items,entry)
