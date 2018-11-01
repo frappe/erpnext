736bde919b110de2d81ce8727ec7acbf7188a491
scmmishra <scm.mymail@gmail.com>
2018-11-01 17:20:04 +0530

    UI for Content

---
+++ erpnext/public/js/education/web-academy/components/ContentNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="nav-buttons">
+		<button class='btn btn-outline-secondary' @click="$router.go(-1)">Previous</button>
+		<button class='btn btn-primary' @click="$router.push({ name: 'content', params: { course: $route.params.course, type:nextContentType, content:nextContent }})">Next</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['nextContent', 'nextContentType'],
+	name: 'ContentNavigation',
+};
+</script>
+
+<style lang="css" scoped>
+	.nav-buttons {
+		position: absolute;
+  		bottom: 0;
+  		right: 0;
+	}
+</style>
