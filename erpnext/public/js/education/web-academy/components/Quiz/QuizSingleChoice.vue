cfc35f44240f8c648bcbba23cd2455f772ac4177
scmmishra <scm.mymail@gmail.com>
2018-11-02 12:51:51 +0530

    Quiz: Save responses as an object

--- erpnext/public/js/education/web-academy/components/Quiz/QuizSingleChoice.vue
+++ erpnext/public/js/education/web-academy/components/Quiz/QuizSingleChoice.vue
@@ -2,12 +2,13 @@
 	<div class="question mt-4">
     <h5>{{ question.question }}</h5>
     <div class="options ml-2">
-        <div v-for="option in question.options" class="form-check pb-1">
-            <input class="form-check-input" type="radio" :name="question.name" :id="option.option" :value="option.option">
-            <label class="form-check-label" :for="option.option">
+        <div v-for="option in question.options" :key="option.name" class="form-check pb-1">
+            <input class="form-check-input" type="radio" :name="question.name" :id="option.name" :value="option.name" @change="emitResponse(question.name, option.name)" v-model="picked">
+            <label class="form-check-label" :for="option.name">
                 {{ option.option }}
             </label>
         </div>
+        <span>Picked: {{ picked }}</span>
     </div>
 </div>
 </template>
@@ -16,6 +17,16 @@
 export default {
 	props: ['question'],
 	name: 'QuizSingleChoice',
+	data() {
+		return {
+			picked: ''
+		}
+	},
+	methods: {
+		emitResponse(q, o) {
+			this.$emit('updateResponse', {'question':q , 'option': o})
+		}
+	}
 };
 </script>
 
