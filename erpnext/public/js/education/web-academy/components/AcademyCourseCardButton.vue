5669ea12ce1cd11bb8030c381926e7e68fe52b87
scmmishra <scm.mymail@gmail.com>
2018-11-02 20:51:24 +0530

    Re-factored CourseCard UI

---
+++ erpnext/public/js/education/web-academy/components/AcademyCourseCardButton.vue
@@ -0,0 +1,22 @@
+<template>
+	<button :class="className" class='btn btn-primary btn-sm btn-block' @click="$router.push($route.path + '/' + course + '/' + nextContentType + '/' + nextContent)">{{ buttonName }}</button>
+</template>
+<script>
+export default {
+    props: ['course', 'nextContent', 'nextContentType'],
+    name: "AcademyCourseCardButton",
+    data() {
+        return {
+            buttonName: 'Start',
+            className: 'btn-primary'
+        }
+    },
+    mounted() {
+    	if(this.$root.$data.checkCourseCompletion(this.course)){
+    		console.log('Completed', this.course)
+    		this.buttonName = 'Completed'
+    		this.className = 'btn-success'
+    	}
+    }
+};
+</script>
