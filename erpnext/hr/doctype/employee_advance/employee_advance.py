598d34445f0661b7f8c02467bca126d7f0ed58a0
Rohit Waghchaure <rohitw1991@gmail.com>
2018-11-02 16:02:33 +0530

    [Fix] Column ‘claimed_amount’ cannot be null

--- erpnext/hr/doctype/employee_advance/employee_advance.py
+++ erpnext/hr/doctype/employee_advance/employee_advance.py
@@ -60,7 +60,8 @@ class EmployeeAdvance(Document):
 			where employee_advance = %s and docstatus=1 and allocated_amount > 0
 		""", self.name)[0][0]
 
-		frappe.db.set_value("Employee Advance", self.name, "claimed_amount", claimed_amount)
+		if claimed_amount:
+			frappe.db.set_value("Employee Advance", self.name, "claimed_amount", claimed_amount)
 
 @frappe.whitelist()
 def make_bank_entry(dt, dn):
