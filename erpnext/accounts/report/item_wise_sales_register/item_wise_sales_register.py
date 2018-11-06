c704630d5fd465c832a5f99914ff465bed9a5987
Shreya Shah <shreyashah115@gmail.com>
2018-11-06 17:24:45 +0530

    fix: Return default if company not found (#15915)

--- erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.py
+++ erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.py
@@ -16,7 +16,7 @@ def _execute(filters=None, additional_table_columns=None, additional_query_colum
 	if not filters: filters = {}
 	columns = get_columns(additional_table_columns)
 
-	company_currency = erpnext.get_company_currency(filters.company)
+	company_currency = erpnext.get_company_currency(filters.get('company'))
 
 	item_list = get_items(filters, additional_query_columns)
 	if item_list:
