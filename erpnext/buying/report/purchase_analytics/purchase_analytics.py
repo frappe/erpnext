bf6907e6fa486d7e0ae9ac267c78e1aaf15fd8d8
deepeshgarg007 <deepeshgarg6@gmail.com>
2018-11-08 11:39:35 +0530

    Codacy issue fix

--- erpnext/buying/report/purchase_analytics/purchase_analytics.py
+++ erpnext/buying/report/purchase_analytics/purchase_analytics.py
@@ -2,9 +2,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
 from erpnext.selling.report.sales_analytics.sales_analytics import Analytics
-
 
 def execute(filters=None):
 	return Analytics(filters).run()
