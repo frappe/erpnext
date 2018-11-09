892b0c23fc64e454876dc4465ec1c2829b91389a
scmmishra <scm.mymail@gmail.com>
2018-11-09 18:24:26 +0530

    Added class functions

--- erpnext/education/doctype/course_enrollment/course_enrollment.py
+++ erpnext/education/doctype/course_enrollment/course_enrollment.py
@@ -8,7 +8,24 @@ from frappe.model.document import Document
 
 class CourseEnrollment(Document):
 	
+	def get_linked_activity(self):
+		course_activity_list = frappe.get_all("Course Activity", filters={'enrollment':self.name})
+		course_activity = [frappe.get_doc("Course Activity", activity.name) for activity in course_activity_list] 
+		quiz_activity_list = frappe.get_all("Quiz Activity", filters={'enrollment':self.name})
+		quiz_activity = [frappe.get_doc("Quiz Activity", activity.name) for activity in quiz_activity_list] 
+		return course_activity, quiz_activity
+
 	def get_all_activity(self):
-		course_activity_list = frappe.get_all("Course Activity", filters={'enrollment':self.name}, fields=['content', 'content_type', 'modified'], order_by='modified')
-		quiz_activity_list = frappe.get_all("Quiz Activity", filters={'enrollment':self.name}, fields=['quiz', 'status', 'modified'], order_by='modified')
-		return course_activity_list, quiz_activity_list
+		courses, quizes = self.get_linked_activity()
+		joined = courses + quizes
+		activity = sorted(joined, key = lambda i: i.modified) # Sorting based on modified timestamp
+		return activity
+		
+	def check_course_completion(self):
+		pass
+
+	def get_last_activity(self):
+		if len(self.get_all_activity()) == 0:
+			return None
+		else:
+			return self.get_all_activity()[-1]
