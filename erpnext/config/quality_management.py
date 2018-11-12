f5dcc3a157ed4bc8e968251053cf7e44b137493a
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-11-12 10:21:02 +0530

    indentation

--- erpnext/config/quality_management.py
+++ erpnext/config/quality_management.py
@@ -2,21 +2,21 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-    return [
-        {
-            "label": _("Goal and Procedure"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "Quality Goal",
-                    "description":_("Quality Goal."),
-                },
-                {
-                    "type": "doctype",
-                    "name": "Quality Procedure",
-                    "description":_("Quality Procedure."),
-                },
-                {
+	return [
+		{
+			"label": _("Goal and Procedure"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Quality Goal",
+					"description":_("Quality Goal."),
+				},
+				{
+					"type": "doctype",
+					"name": "Quality Procedure",
+					"description":_("Quality Procedure."),
+				},
+				{
 					"type": "doctype",
 					"name": "Quality Procedure",
 					"icon": "fa fa-sitemap",
@@ -24,46 +24,46 @@ def get_data():
 					"route": "Tree/Quality Procedure",
 					"description": _("Tree of Quality Procedures."),
 				},
-            ]
-        },
-        {
-            "label": _("Review and Action"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "Quality Review",
-                    "description":_("Quality Review"),
-                },
-                {
-                    "type": "doctype",
-                    "name": "Quality Action",
-                    "description":_("Quality Action"),
-                }
-            ]
-        },
-        {
-            "label": _("Meeting"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "Quality Meeting",
-                    "description":_("Quality Meeting"),
-                }
-            ]
-        },
-        {
-            "label": _("Feedback"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "Customer Feedback",
-                    "description":_("Customer Feedback"),
-                },
-                {
-                    "type": "doctype",
-                    "name": "Customer Feedback Template",
-                    "description":_("Customer Feedback Template"),
-                }
-            ]
-        },
-    ]
+			]
+		},
+		{
+			"label": _("Review and Action"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Quality Review",
+					"description":_("Quality Review"),
+				},
+				{
+					"type": "doctype",
+					"name": "Quality Action",
+					"description":_("Quality Action"),
+				}
+			]
+		},
+		{
+			"label": _("Meeting"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Quality Meeting",
+					"description":_("Quality Meeting"),
+				}
+			]
+		},
+		{
+			"label": _("Feedback"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Customer Feedback",
+					"description":_("Customer Feedback"),
+				},
+				{
+					"type": "doctype",
+					"name": "Customer Feedback Template",
+					"description":_("Customer Feedback Template"),
+				}
+			]
+		},
+	]
