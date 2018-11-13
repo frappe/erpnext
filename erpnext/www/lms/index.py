8e6514e42c82540167346d64e81ba5223ee7b1a7
scmmishra <scm.mymail@gmail.com>
2018-11-13 16:32:38 +0530

    Deleted Old LMS Content

--- erpnext/www/lms/index.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-import frappe
-import erpnext.education.utils as utils
-
-def get_context(context):
-    context.featured = utils.get_featured_programs()
-    context.settings = frappe.get_doc("Education Settings")
