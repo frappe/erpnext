00e28728f1392b90e8b0993cbc771482fd7b357d
sivankar <sivankar.jain@elastic.run>
2018-11-15 17:22:04 +0530

    added test cases

--- erpnext/stock/doctype/stock_ledger_entry/test_stock_ledger_entry.py
+++ erpnext/stock/doctype/stock_ledger_entry/test_stock_ledger_entry.py
@@ -9,4 +9,38 @@ import unittest
 # test_records = frappe.get_test_records('Stock Ledger Entry')
 
 class TestStockLedgerEntry(unittest.TestCase):
-	pass
+
+	def test_selling_rate_baseson_sales_invoice(self):
+		test_record = frappe.get_test_records('Sales Invoice')[1]
+		doc = frappe.copy_doc(test_record)
+		doc.update_stock = 1
+		doc.save()
+		doc.submit()
+		for item in doc.items:
+			self.assertTrue(frappe.db.get_value('Stock Ledger Entry', {'voucher_detail_no': item.name}, 'selling_rate') == item.base_rate)
+
+		doc.cancel()
+		doc.delete()
+
+	def test_selling_rate_baseson_delivery_note(self):
+		from erpnext.stock.doctype.delivery_note.test_delivery_note import create_delivery_note
+
+		doc = create_delivery_note()
+
+		for item in doc.items:
+			self.assertTrue(frappe.db.get_value('Stock Ledger Entry', {'voucher_detail_no': item.name}, 'selling_rate') == item.base_rate)
+
+		doc.cancel()
+		doc.delete()
+
+
+	def test_selling_rate_baseson_purchase_receipt(self):
+		from erpnext.stock.doctype.purchase_receipt.test_purchase_receipt import make_purchase_receipt
+
+		doc = make_purchase_receipt()
+
+		for item in doc.items:
+			self.assertTrue(frappe.db.get_value('Stock Ledger Entry', {'voucher_detail_no': item.name}, 'selling_rate') == 0)
+
+		doc.cancel()
+		doc.delete()
