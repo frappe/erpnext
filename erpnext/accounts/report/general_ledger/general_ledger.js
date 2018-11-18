cb98046bc53994982c803cf15e7ea847530759d2
Saif Ur Rehman <saifi0102@gmail.com>
2018-11-19 01:05:49 +0500

    Fixed merge problem

--- erpnext/accounts/report/general_ledger/general_ledger.js
+++ erpnext/accounts/report/general_ledger/general_ledger.js
@@ -37,8 +37,9 @@ frappe.query_reports["General Ledger"] = {
 			"fieldname":"group_by",
 			"label": __("Group by"),
 			"fieldtype": "Select",
-			"options": ["", "Group by Voucher", "Group by Account", "Group by Party"],
-			"default": "Group by Voucher"
+			"options": ["", __("Group by Voucher"), __("Group by Voucher (Consolidated)"),
+				__("Group by Account"), __("Group by Party")],
+			"default": __("Group by Voucher (Consolidated)")
 		},
 		{
 			"fieldname": "presentation_currency",
