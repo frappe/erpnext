5d2e52c4effbeaa4769132ae8c48f31c33746942
Ranjith <rkurungadam@gmail.com>
2018-11-19 20:03:25 +0530

    fix: healthcare patch

--- erpnext/patches/v11_0/add_healthcare_service_unit_tree_root.py
+++ erpnext/patches/v11_0/add_healthcare_service_unit_tree_root.py
@@ -3,12 +3,18 @@ from frappe import _
 
 def execute():
 	""" assign lft and rgt appropriately """
+	if "Healthcare" not in frappe.get_active_domains():
+		return
+
 	frappe.reload_doc("healthcare", "doctype", "healthcare_service_unit")
 	frappe.reload_doc("healthcare", "doctype", "healthcare_service_unit_type")
+	company = frappe.get_value("Company", {"domain": "Healthcare"}, "name")
 
-	if not frappe.db.exists("Healthcare Service Unit", _('All Healthcare Service Units')):
+	if company:
 		frappe.get_doc({
 			'doctype': 'Healthcare Service Unit',
 			'healthcare_service_unit_name': _('All Healthcare Service Units'),
-			'is_group': 1
+			'is_group': 1,
+			'company': company
 		}).insert(ignore_permissions=True)
+
