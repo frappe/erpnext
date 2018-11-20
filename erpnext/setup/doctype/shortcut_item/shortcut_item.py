3120434a72f0be6e5c982ac9c9b7a44e6c2cdc94
Suraj Shetty <surajshetty3416@gmail.com>
2018-11-20 13:32:31 +0530

    Fix codacy

--- erpnext/setup/doctype/shortcut_item/shortcut_item.py
+++ erpnext/setup/doctype/shortcut_item/shortcut_item.py
@@ -3,7 +3,6 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
 from frappe.model.document import Document
 
 class ShortcutItem(Document):
--- erpnext/setup/doctype/shortcut_settings/shortcut_settings.js
+++ erpnext/setup/doctype/shortcut_settings/shortcut_settings.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Shortcut Settings', {
-	refresh: function(frm) {
+	refresh: function() {
 
 	}
 });
