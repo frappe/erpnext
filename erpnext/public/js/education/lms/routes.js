e07cd31ba97f101edb5902356951ee00277cf31a
scmmishra <scm.mymail@gmail.com>
2018-11-22 12:22:20 +0530

    Renamed CoursePage to ContentPage

--- erpnext/public/js/education/lms/routes.js
+++ erpnext/public/js/education/lms/routes.js
@@ -1,12 +1,12 @@
 import Home from "./pages/Home.vue";
 import ProgramPage from "./pages/ProgramPage.vue";
-import CoursePage from "./pages/CoursePage.vue";
+import ContentPage from "./pages/ContentPage.vue";
 import ListPage from "./pages/ListPage.vue";
 
 const routes = [
 	{name: 'home', path: '', component: Home},
 	{name: 'program', path: '/Program/:program_name', component: ProgramPage, props: true},
-	{name: 'content', path: '/Program/:program_name/:course/:type/:content', component: CoursePage, props: true},
+	{name: 'content', path: '/Program/:program_name/:course/:type/:content', component: ContentPage, props: true},
 	{name: 'list', path: '/List/:master', component: ListPage, props: true},
 	{
 		name: 'signup',
