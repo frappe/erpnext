0fbf135f9d69583f912329f7b4bb93f488685f93
hrwX <himanshuwarekar@yahoo.com>
2018-11-22 16:21:12 +0530

    bug fix

--- erpnext/support/doctype/support_contract/support_contract.py
+++ erpnext/support/doctype/support_contract/support_contract.py
@@ -19,9 +19,9 @@ def check_email():
 	for email_account in frappe.get_all("Email Account", filters=[{"enable_incoming": 1}]):
 		for comm in frappe.get_all("Communication", "name", filters=[{"email_account":email_account.name}]):
 			comm = frappe.get_doc("Communication", comm.name)
-			customer = frappe.get_all("Customer", filters=[{"email_id": comm.sender}], limit=1)
-			support_contract = frappe.get_all("Support Contract", filters=[{"customer": customer.name}], limit=1)
-			issue_criticality = frappe.get_all("Issue Criticality", support_contract.issue_criticality)
+			customer = frappe.get_list("Customer", filters=[{"email_id": comm.sender}], limit=1)
+			support_contract = frappe.get_list("Support Contract", filters=[{"customer": customer[0].name}], limit=1)
+			issue_criticality = frappe.get_doc("Issue Criticality", support_contract[0].issue_criticality)
 			for keyword in issue_criticality.keyword:
 				print(keyword)
 			print("-------------------------")
