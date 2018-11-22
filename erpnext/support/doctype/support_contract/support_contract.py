93605a819a844f0878bac0656ff99073a40fa7e1
hrwX <himanshuwarekar@yahoo.com>
2018-11-22 13:43:08 +0530

    debug

--- erpnext/support/doctype/support_contract/support_contract.py
+++ erpnext/support/doctype/support_contract/support_contract.py
@@ -5,14 +5,19 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-
+from datetime import datetime, timedelta
 class SupportContract(Document):
 	pass
 
 def check_email():
 	for email_account in frappe.get_all("Email Account", filters={"enable_incoming": 1}):
 		print(email_account.name, email_account)
-		for comm in frappe.get_all("Communication", "name", filters=[{"email_account":email_account.name}]):
+		for comm in frappe.get_all("Communication", "name", filters=[
+			{"email_account":email_account.name},
+			{"email_account":email_account.name},
+			{"creation": ("<", datetime.now() - timedelta(seconds = (email_account.unreplied_for_mins or 30) * 60))},
+			{"creation": (">", datetime.now() - timedelta(seconds = (email_account.unreplied_for_mins or 30) * 60 * 3))}
+		]):
 			comm = frappe.get_doc("Communication", comm.name)
 			print("-------------------------")
 			print("Subject : " + comm.subject)
