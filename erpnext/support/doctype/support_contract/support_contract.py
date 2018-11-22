0a402219d6b25d780600d92e6ccab44b40d2bc5f
hrwX <himanshuwarekar@yahoo.com>
2018-11-22 16:41:09 +0530

    fix

--- erpnext/support/doctype/support_contract/support_contract.py
+++ erpnext/support/doctype/support_contract/support_contract.py
@@ -20,10 +20,8 @@ def check_email():
 		for comm in frappe.get_all("Communication", "name", filters=[{"email_account":email_account.name}]):
 			comm = frappe.get_doc("Communication", comm.name)
 			customer = frappe.get_list("Customer", filters=[{"email_id": comm.sender}], limit=1)
-			print(customer.name)
 			print(customer[0].name)
 			support_contract = frappe.get_list("Support Contract", filters=[{"customer": customer[0].name}], limit=1)
-			print(support_contract.issue_criticality)
 			print(support_contract[0].issue_criticality)
 			issue_criticality = frappe.get_doc("Issue Criticality", support_contract[0].issue_criticality)
 			for keyword in issue_criticality.keyword:
