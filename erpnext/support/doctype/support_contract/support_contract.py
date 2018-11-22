f45abf8a1e87d4d6242bf267c784d58339ef87c8
hrwX <himanshuwarekar@yahoo.com>
2018-11-22 15:03:51 +0530

    validate start and end date

--- erpnext/support/doctype/support_contract/support_contract.py
+++ erpnext/support/doctype/support_contract/support_contract.py
@@ -5,14 +5,16 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
 from datetime import datetime, timedelta
 class SupportContract(Document):
-	pass
+	
+	def validate(self):
+		if self.start_date >= self.end_date:
+			frappe.throw(_("Start Date of contract can't be less greater than End Date"))
 
 def check_email():
 	for email_account in frappe.get_all("Email Account", filters={"enable_incoming": 1}):
-		print(email_account.name, email_account)
-		print(datetime.now(), datetime.now() - timedelta(seconds = (30 * 60)))
 		for comm in frappe.get_all("Communication", "name", filters=[
 			{"email_account":email_account.name},
 			{"creation": (">", datetime.now() - timedelta(seconds = (30 * 60)))}
