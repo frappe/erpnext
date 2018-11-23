48e0e19b0308c67a1279eef407ba646da3cf137c
Nabin Hait <nabinhait@Nabins-MacBook-Pro.local>
2018-11-23 16:15:32 +0530

    fix(test): Fixed salary slip and sales analytics test cases

--- erpnext/selling/report/sales_analytics/test_analytics.py
+++ erpnext/selling/report/sales_analytics/test_analytics.py
@@ -8,10 +8,17 @@ from erpnext.selling.report.sales_analytics.sales_analytics import execute
 from erpnext.selling.doctype.sales_order.test_sales_order import make_sales_order
 
 class TestAnalytics(unittest.TestCase):
+	def test_sales_analytics(self):
+		frappe.db.sql("delete from `tabSales Order` where company='_Test Company 2'")
 
-	def test_by_entity(self):
-		create_sales_order()
+		create_sales_orders()
 
+		self.compare_result_for_customer()
+		self.compare_result_for_customer_group()
+		self.compare_result_for_customer_based_on_quantity()
+
+
+	def compare_result_for_customer(self):
 		filters = {
 			'doc_type': 'Sales Order',
 			'range': 'Monthly',
@@ -43,23 +50,6 @@ class TestAnalytics(unittest.TestCase):
 				"total":2000.0
 			},
 			{
-				"entity": "_Test Customer 3",
-				"entity_name": "_Test Customer 3",
-				"apr": 0.0,
-				"may": 0.0,
-				"jun": 2000.0,
-				"jul": 1000.0,
-				"aug": 0.0,
-				"sep": 0.0,
-				"oct": 0.0,
-				"nov": 0.0,
-				"dec": 0.0,
-				"jan": 0.0,
-				"feb": 0.0,
-				"mar": 0.0,
-				"total": 3000.0
-			},
-			{
 				"entity": "_Test Customer 2",
 				"entity_name": "_Test Customer 2",
 				"apr": 0.0,
@@ -75,12 +65,29 @@ class TestAnalytics(unittest.TestCase):
 				"feb": 0.0,
 				"mar": 0.0,
 				"total":2500.0
+			},
+			{
+				"entity": "_Test Customer 3",
+				"entity_name": "_Test Customer 3",
+				"apr": 0.0,
+				"may": 0.0,
+				"jun": 2000.0,
+				"jul": 1000.0,
+				"aug": 0.0,
+				"sep": 0.0,
+				"oct": 0.0,
+				"nov": 0.0,
+				"dec": 0.0,
+				"jan": 0.0,
+				"feb": 0.0,
+				"mar": 0.0,
+				"total": 3000.0
 			}
 		]
-		self.assertEqual(expected_data, report[1])
+		result = sorted(report[1], key=lambda k: k['entity'])
+		self.assertEqual(expected_data, result)
 
-	def test_by_group(self):
-	
+	def compare_result_for_customer_group(self):
 		filters = {
 			'doc_type': 'Sales Order',
 			'range': 'Monthly',
@@ -93,80 +100,26 @@ class TestAnalytics(unittest.TestCase):
 
 		report = execute(filters)
 
-		expected_data = [
-			{
-				"entity": "All Customer Groups",
-				"indent": 0,
-				"apr": 0.0,
-				"may": 0.0,
-				"jun": 2000.0,
-				"jul": 1000.0,
-				"aug": 0.0,
-				"sep": 1500.0,
-				"oct": 1000.0,
-				"nov": 0.0,
-				"dec": 0.0,
-				"jan": 0.0,
-				"feb": 2000.0,
-				"mar": 0.0,
-				"total":7500.0
-			},
-			{
-				"entity": "Individual",
-				"indent": 1,
-				"apr": 0.0,
-				"may": 0.0,
-				"jun": 0.0,
-				"jul": 0.0,
-				"aug": 0.0,
-				"sep": 0.0,
-				"oct": 0.0,
-				"nov": 0.0,
-				"dec": 0.0,
-				"jan": 0.0,
-				"feb": 0.0,
-				"mar": 0.0,
-				"total": 0.0
-			},
-			{
-				"entity": "_Test Customer Group",
-				"indent": 1,
-				"apr": 0.0,
-				"may": 0.0,
-				"jun": 0.0,
-				"jul": 0.0,
-				"aug": 0.0,
-				"sep": 0.0,
-				"oct": 0.0,
-				"nov": 0.0,
-				"dec": 0.0,
-				"jan": 0.0,
-				"feb": 0.0,
-				"mar": 0.0,
-				"total":0.0
-			},
-			{
-				"entity": "_Test Customer Group 1",
-				"indent": 1,
-				"apr": 0.0,
-				"may": 0.0,
-				"jun": 0.0,
-				"jul": 0.0,
-				"aug": 0.0,
-				"sep": 0.0,
-				"oct": 0.0,
-				"nov": 0.0,
-				"dec": 0.0,
-				"jan": 0.0,
-				"feb": 0.0,
-				"mar": 0.0,
-				"total":0.0
-			}
-		]
-		self.assertEqual(expected_data, report[1])
-	
-	def test_by_quantity(self):
+		expected_first_row = {
+			"entity": "All Customer Groups",
+			"indent": 0,
+			"apr": 0.0,
+			"may": 0.0,
+			"jun": 2000.0,
+			"jul": 1000.0,
+			"aug": 0.0,
+			"sep": 1500.0,
+			"oct": 1000.0,
+			"nov": 0.0,
+			"dec": 0.0,
+			"jan": 0.0,
+			"feb": 2000.0,
+			"mar": 0.0,
+			"total":7500.0
+		}
+		self.assertEqual(expected_first_row, report[1][0])
 
+	def compare_result_for_customer_based_on_quantity(self):
 		filters = {
 			'doc_type': 'Sales Order',
 			'range': 'Monthly',
@@ -198,23 +151,6 @@ class TestAnalytics(unittest.TestCase):
 				"total":20.0
 			},
 			{
-				"entity": "_Test Customer 3",
-				"entity_name": "_Test Customer 3",
-				"apr": 0.0,
-				"may": 0.0,
-				"jun": 20.0,
-				"jul": 10.0,
-				"aug": 0.0,
-				"sep": 0.0,
-				"oct": 0.0,
-				"nov": 0.0,
-				"dec": 0.0,
-				"jan": 0.0,
-				"feb": 0.0,
-				"mar": 0.0,
-				"total": 30.0
-			},
-			{
 				"entity": "_Test Customer 2",
 				"entity_name": "_Test Customer 2",
 				"apr": 0.0,
@@ -230,11 +166,29 @@ class TestAnalytics(unittest.TestCase):
 				"feb": 0.0,
 				"mar": 0.0,
 				"total":25.0
+			},
+			{
+				"entity": "_Test Customer 3",
+				"entity_name": "_Test Customer 3",
+				"apr": 0.0,
+				"may": 0.0,
+				"jun": 20.0,
+				"jul": 10.0,
+				"aug": 0.0,
+				"sep": 0.0,
+				"oct": 0.0,
+				"nov": 0.0,
+				"dec": 0.0,
+				"jan": 0.0,
+				"feb": 0.0,
+				"mar": 0.0,
+				"total": 30.0
 			}
 		]
-		self.assertEqual(expected_data, report[1])
+		result = sorted(report[1], key=lambda k: k['entity'])
+		self.assertEqual(expected_data, result)
 
-def create_sales_order():
+def create_sales_orders():
 	frappe.set_user("Administrator")
 
 	make_sales_order(company="_Test Company 2", qty=10,
@@ -260,16 +214,15 @@ def create_sales_order():
 		transaction_date='2017-09-23',
 		warehouse='Finished Goods - _TC2',
 		currency = 'EUR')
-		
+
 	make_sales_order(company="_Test Company 2",
 		qty=20, customer = "_Test Customer 3",
 		transaction_date='2017-06-15',
 		warehouse='Finished Goods - _TC2',
 		currency = 'EUR')
-		
+
 	make_sales_order(company="_Test Company 2",
 		qty=10, customer = "_Test Customer 3",
 		transaction_date='2017-07-10',
 		warehouse='Finished Goods - _TC2',
 		currency = 'EUR')
-		
