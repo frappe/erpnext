0e8c36473a57344d265fd700d874684c91245d74
deepeshgarg007 <deepeshgarg6@gmail.com>
2018-11-23 11:22:24 +0530

    Added colspan to indicators

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -104,15 +104,15 @@ $.extend(erpnext.utils, {
 	set_party_dashboard_indicators: function(frm) {
 		if(frm.doc.__onload && frm.doc.__onload.dashboard_info) {
 			var company_wise_info = frm.doc.__onload.dashboard_info;
-			frm.dashboard.add_indicator(__('Company'));
-			frm.dashboard.add_indicator(__('Annual Billing'));
-			frm.dashboard.add_indicator(__('Total Unpaid'));
+			frm.dashboard.add_indicator(__('Company'), '', 4);
+			frm.dashboard.add_indicator(__('Annual Billing'), '', 4);
+			frm.dashboard.add_indicator(__('Total Unpaid'), '', 4);
 			company_wise_info.forEach(function(info){
-				frm.dashboard.add_indicator(__('{0}',[info.company]));
+				frm.dashboard.add_indicator(__('{0}',[info.company]), '', 4);
 				frm.dashboard.add_indicator(__('{0}',
-					[format_currency(info.billing_this_year, info.currency)]));
+					[format_currency(info.billing_this_year, info.currency)]), '', 4);
 				frm.dashboard.add_indicator(__('{0}',
-					[format_currency(info.total_unpaid, info.currency)]));
+					[format_currency(info.total_unpaid, info.currency)]), '', 4);
 			});
 		}
 	},
