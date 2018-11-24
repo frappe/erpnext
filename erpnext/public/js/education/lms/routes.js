8c60c3b4950ae33813614715fdd4c92d8e34844e
scmmishra <scm.mymail@gmail.com>
2018-11-24 20:31:35 +0530

    Minor improvements

--- erpnext/public/js/education/lms/routes.js
+++ erpnext/public/js/education/lms/routes.js
@@ -6,8 +6,21 @@ import ListPage from "./pages/ListPage.vue";
 const routes = [
 	{name: 'home', path: '', component: Home},
 	{name: 'program', path: '/Program/:program_name', component: ProgramPage, props: true},
-	{name: 'content', path: '/Program/:program_name/:course/:type/:content', component: ContentPage, props: true},
-	{name: 'list', path: '/List/:master', component: ListPage, props: true},
+	{
+		name: 'content',
+		path: '/Program/:program_name/:course/:type/:content',
+		component: ContentPage,
+		props: true,
+		beforeEnter: (to, from, next) => {
+			if(!lms.store.isLogin) next({name: 'home'})
+		}
+	},
+	{
+		name: 'list',
+		path: '/List/:master',
+		component: ListPage,
+		props: true
+	},
 	{
 		name: 'signup',
 		path: '/Signup',
