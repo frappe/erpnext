71ecf3467341e0734d2ba60f110d07d3541f3b10
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-11-26 10:52:46 +0530

    packing slip type casting

--- erpnext/stock/doctype/packing_slip/packing_slip.py
+++ erpnext/stock/doctype/packing_slip/packing_slip.py
@@ -47,9 +47,9 @@ class PackingSlip(Document):
 		"""
 		if not cint(self.from_case_no):
 			frappe.msgprint(_("Please specify a valid 'From Case No.'"), raise_exception=1)
-		elif not self.to_case_no:
+		elif not cint(self.to_case_no):
 			self.to_case_no = self.from_case_no
-		elif self.from_case_no > self.to_case_no:
+		elif cint(self.from_case_no) > cint(self.to_case_no):
 			frappe.msgprint(_("'To Case No.' cannot be less than 'From Case No.'"),
 				raise_exception=1)
 
