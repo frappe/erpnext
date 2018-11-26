d8d5ddfdc2116a807f28736dac65dc6a2706cd9f
hrwX <himanshuwarekar@yahoo.com>
2018-11-26 14:29:02 +0530

    debug

--- erpnext/support/doctype/issue/issue.py
+++ erpnext/support/doctype/issue/issue.py
@@ -88,7 +88,6 @@ class Issue(Document):
 		now = datetime.datetime.now()
 		day = now.day
 		day_name = now.strftime("%A")
-		month=now.strftime("%B")
 		support_contract = frappe.get_list("Support Contract", filters=[{"customer": self.customer}, {"contract_status": "Active"}], fields=["name", "contract_template", "service_level", "issue_criticality", "employee_group"], limit=1)
 		self.support_contract = support_contract[0].name
 		self.service_level = support_contract[0].service_level
