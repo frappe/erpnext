f9385a504acfe3c84c3320c31d563c9eb8f74a64
hrwX <himanshuwarekar@yahoo.com>
2018-11-28 17:54:47 +0530

    dashboard changes

---
+++ erpnext/support/doctype/issue/issue_dashboard.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'service_level',
+		'transactions': [
+			{
+				#'label': _('Transactions'),
+				#'items': ['Material Request', 'Stock Entry']
+			}
+		]
+	}
