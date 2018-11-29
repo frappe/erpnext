2d5c190b451824bb307a6ffec84d30a35109c04e
Valmik Jangla <mail@valmik.in>
2018-11-29 11:29:48 -0800

    fix(delivery): update package total on validate

--- erpnext/stock/doctype/delivery_trip/delivery_trip.py
+++ erpnext/stock/doctype/delivery_trip/delivery_trip.py
@@ -26,6 +26,7 @@ class DeliveryTrip(Document):
 	def validate(self):
 		self.validate_stop_addresses()
 		self.update_status()
+		self.update_package_total()
 
 	def on_submit(self):
 		self.update_status()
@@ -37,6 +38,9 @@ class DeliveryTrip(Document):
 	def on_cancel(self):
 		self.update_status()
 		self.update_delivery_notes(delete=True)
+
+	def update_package_total(self):
+		self.package_total = sum([stop.grand_total for stop in self.delivery_stops if stop.grand_total])
 
 	def validate_stop_addresses(self):
 		for stop in self.delivery_stops:
