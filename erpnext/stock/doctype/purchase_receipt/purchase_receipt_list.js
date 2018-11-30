600228b98aa6f093d2cc96c5e855e3d1f0e00e08
deepeshgarg007 <deepeshgarg6@gmail.com>
2018-11-30 14:09:57 +0530

    List view bug fix

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt_list.js
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt_list.js
@@ -1,5 +1,5 @@
 frappe.listview_settings['Purchase Receipt'] = {
-	add_fields: ["is_return", "grand_total", "status", "per_billed"],
+	add_fields: ["is_return", "grand_total", "status", "per_billed", "currency"],
 	get_indicator: function (doc) {
 		if (cint(doc.is_return) == 1) {
 			return [__("Return"), "darkgrey", "is_return,=,Yes"];
