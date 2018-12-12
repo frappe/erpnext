6db7dd9b5ef7f297cb7e323d1ccad0f709e4ad92
Saif Ur Rehman <saifi0102@gmail.com>
2018-12-13 02:46:46 +0500

    fix: Include Contents Qty in validation of Targets for Sales Person and Territory

--- erpnext/setup/doctype/sales_person/sales_person.py
+++ erpnext/setup/doctype/sales_person/sales_person.py
@@ -13,8 +13,9 @@ class SalesPerson(NestedSet):
 
 	def validate(self):
 		for d in self.get('targets') or []:
-			if not flt(d.target_qty) and not flt(d.target_amount):
-				frappe.throw(_("Either target qty or target amount is mandatory."))
+			if not flt(d.target_qty) and not flt(d.target_alt_uom_qty) and not flt(d.target_amount):
+				frappe.throw(_("Row {0}: Either Target Stock Qty or Target Contents Qty or Target Amount is mandatory.")
+					.format(d.idx))
 		self.validate_employee_id()
 
 	def onload(self):
