6663ca1d7fed7c4dcb5df7e7613c98892014732a
scmmishra <scm.mymail@gmail.com>
2018-12-12 11:29:08 +0530

    Adding Test Utils

--- erpnext/education/doctype/program/test_program.py
+++ erpnext/education/doctype/program/test_program.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2015, Frappe Technologies and Contributors
 # See license.txt
 from __future__ import unicode_literals
+from erpnext.education.doctype.course.test_course import make_course
 
 import frappe
 import unittest
@@ -10,3 +11,27 @@ import unittest
 
 class TestProgram(unittest.TestCase):
 	pass
+
+
+def make_program(name):
+	program = frappe.get_doc({
+		"doctype": "Program",
+		"program_name": name,
+		"program_code": name,
+		"is_published": True,
+		"is_featured": True,
+	}).insert()
+	return program.name
+
+def make_program_and_linked_courses(program_name, course_name_list):
+	try:
+		program = frappe.get_doc("Program", program_name)
+	except frappe.DoesNotExistError:
+		make_program(program_name)
+		program = frappe.get_doc("Program", program_name)
+	course_list = [make_course(course_name) for course_name in course_name_list]
+	for course in course_list:
+		program.append("courses", {"course": course})
+	program.save()
+	return program.name
+
--- erpnext/education/doctype/student/test_student.py
+++ erpnext/education/doctype/student/test_student.py
@@ -2,11 +2,12 @@
 # Copyright (c) 2015, Frappe Technologies and Contributors
 # See license.txt
 from __future__ import unicode_literals
+from frappe.test_runner import make_test_records
+from erpnext.education.doctype.program.test_program import make_program_and_linked_courses
 
 import frappe
 import unittest
 
-# test_records = frappe.get_test_records('Student')
-
+test_records = frappe.get_test_records('Student')
 class TestStudent(unittest.TestCase):
 	pass
