43f44b55f52b71462214598bf5cfeeab2e773381
Anurag Mishra <mishranaman123@gmail.com>
2018-12-12 15:32:36 +0530

    master calendar intial commit

--- erpnext/education/api.py
+++ erpnext/education/api.py
@@ -187,7 +187,7 @@ def get_course_schedule_events(start, end, filters=None):
 	from frappe.desk.calendar import get_event_conditions
 	conditions = get_event_conditions("Course Schedule", filters)
 
-	data = frappe.db.sql("""select name, course, color,
+	data = frappe.db.sql("""select name, course, color, title,
 			timestamp(schedule_date, from_time) as from_datetime,
 			timestamp(schedule_date, to_time) as to_datetime,
 			room, student_group, 0 as 'allDay'
--- erpnext/quality_management/doctype/measurement_unit/measurement_unit.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, Frappe and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Measurement Unit', {
-	refresh: function(frm) {
-
-	}
-});
