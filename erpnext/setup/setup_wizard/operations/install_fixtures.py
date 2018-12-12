1580124d2490a702ddb187fa041de3a94216050b
Prateeksha Singh <pratu16x7@gmail.com>
2018-12-12 10:50:54 +0530

    [fixtures] re-add market_segments and sales_stages utils

--- erpnext/setup/setup_wizard/operations/install_fixtures.py
+++ erpnext/setup/setup_wizard/operations/install_fixtures.py
@@ -327,6 +327,31 @@ def add_uom_data():
 			"value": d.get("value")
 		}).insert(ignore_permissions=True)
 
+def add_market_segments():
+	records = [
+		# Market Segments
+		{"doctype": "Market Segment", "market_segment": _("Lower Income")},
+		{"doctype": "Market Segment", "market_segment": _("Middle Income")},
+		{"doctype": "Market Segment", "market_segment": _("Upper Income")}
+	]
+
+	make_records(records)
+
+def add_sale_stages():
+	# Sale Stages
+	records = [
+		{"doctype": "Sales Stage", "stage_name": _("Prospecting")},
+		{"doctype": "Sales Stage", "stage_name": _("Qualification")},
+		{"doctype": "Sales Stage", "stage_name": _("Needs Analysis")},
+		{"doctype": "Sales Stage", "stage_name": _("Value Proposition")},
+		{"doctype": "Sales Stage", "stage_name": _("Identifying Decision Makers")},
+		{"doctype": "Sales Stage", "stage_name": _("Perception Analysis")},
+		{"doctype": "Sales Stage", "stage_name": _("Proposal/Price Quote")},
+		{"doctype": "Sales Stage", "stage_name": _("Negotiation/Review")}
+	]
+
+	make_records(records)
+
 def install_company(args):
 	records = [
 		# Fiscal Year
