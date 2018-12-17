b333900e866db5564c42ed33370e961b3384d87c
hrwX <himanshuwarekar@yahoo.com>
2018-12-17 22:35:47 +0530

    skip date validation when support contract is default

--- erpnext/support/doctype/support_contract/support_contract.py
+++ erpnext/support/doctype/support_contract/support_contract.py
@@ -12,26 +12,6 @@ from datetime import datetime, timedelta
 class SupportContract(Document):
 	
 	def validate(self):
-		if self.end_date < frappe.utils.nowdate():
-			self.contract_status = "Expired"
-		if self.start_date >= self.end_date:
-			frappe.throw(_("Start Date of contract can't be less greater than End Date"))
-
-def check_email():
-	for email_account in frappe.get_all("Email Account", filters=[{"enable_incoming": 1}]):
-		for comm in frappe.get_all("Communication", "name", filters=[{"email_account":email_account.name}]):
-			comm = frappe.get_doc("Communication", comm.name)
-			support_contract = frappe.get_list("Support Contract", filters=[{"email_id": comm.sender}, {"contract_status": "Active"}], fields=["contract_template", "service_level", "issue_criticality", "employee_group"], limit=1)
-			if support_contract:
-				service_level = frappe.get_doc("Service Level", support_contract[0].service_level)
-				#print(service_level.support_and_resolution)
-				for level in service_level.support_and_resolution:
-					print(level.day)
-					if level.day == "Workday":
-						print("--Workday--")
-					else:
-						print("--Holiday--")
-				#issue_criticality = frappe.get_doc("Issue Criticality", support_contract[0].issue_criticality)
-				#for keyword in issue_criticality.keyword:
-				#	if re.search(r''+ keyword.keyword +'', comm.subject) or re.search(r''+ keyword.keyword +'', comm.content):
-				#		issue = make_issue_from_communication(comm.name)
+		if not self.default_contract:
+			if self.start_date >= self.end_date:
+				frappe.throw(_("Support Start Date of contract can't be greater than or equal to End Date"))
