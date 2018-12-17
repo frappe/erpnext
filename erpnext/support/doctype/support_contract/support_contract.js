80dc9e6556f472cf46e89765944a47135cb10b17
hrwX <himanshuwarekar@yahoo.com>
2018-12-17 22:24:18 +0530

    fix js bug

--- erpnext/support/doctype/support_contract/support_contract.js
+++ erpnext/support/doctype/support_contract/support_contract.js
@@ -17,8 +17,8 @@ frappe.ui.form.on('Support Contract', {
 				for (var i = 0; i < data.message.support_and_resolution.length; i++ ){
 					frm.add_child("support_and_resolution");
 					frm.fields_dict.support_and_resolution.get_value()[i].workday = data.message.support_and_resolution[i].workday;
-					frm.fields_dict.support_and_resolution.get_value()[i].from = data.message.support_and_resolution[i].from;
-					frm.fields_dict.support_and_resolution.get_value()[i].to = data.message.support_and_resolution[i].to;
+					frm.fields_dict.support_and_resolution.get_value()[i].start_time = data.message.support_and_resolution[i].start_time;
+					frm.fields_dict.support_and_resolution.get_value()[i].end_time = data.message.support_and_resolution[i].end_time;
 					frm.fields_dict.support_and_resolution.get_value()[i].response_time = data.message.support_and_resolution[i].response_time;
 					frm.fields_dict.support_and_resolution.get_value()[i].response_time_period = data.message.support_and_resolution[i].response_time_period;
 					frm.fields_dict.support_and_resolution.get_value()[i].resolution_time = data.message.support_and_resolution[i].resolution_time;
@@ -27,18 +27,5 @@ frappe.ui.form.on('Support Contract', {
 				frm.refresh();
 			}
 		});
-	},
-	default_contract: function(frm) {
-		console.log(frm.doc.default_contract);
-		console.log("frm.doc.default_contract");
-		if (frm.doc.default_contract == null){
-			console.log("null")
-		}
-		else if (frm.doc.default_contract == 0){
-			console.log("0")
-		}
-		else if (frm.doc.default_contract == 1){
-			frm.doc.customer
-		}
 	}
 });
