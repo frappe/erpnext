0209ef0fc5b7ee0ca6493be1fa9e11179dc8f6f6
Himanshu <himanshuwarekar@yahoo.com>
2018-12-25 17:42:31 +0530

    Quality Management System (#15893)
    * Added QMS to ERPNext
    
    * changes
    
    * fixed
    
    * code refracted
    
    * refracted
    
    * some bugs fixed
    
    * changes
    
    * changes
    
    * test case
    
    * bug fix unit test
    
    * bug fix
    
    * refracted code
    
    * test warning fix
    
    * test case bug fix
    
    * removed whitespace
    
    * indentation bug fix
    
    * unit test bug fix
    
    * fixed Tree
    
    * bug fix
    
    * bug fix
    
    * code refactor
    
    * fixed procedure tree
    
    * removed unnecessary fields
    
    * hide fields
    
    * removed depricated unit test method
    
    * sql injection bug fix
    
    * code refactor and added mandatory fields
    
    * Removed Add Child from TreeView
    
    * Unit Test crashes Fix
    
    * code refactor
    
    * Fixed Status of Action
    
    * removed add button from Chart of Procedure
    
    * Fixed Unit Test for Quality Action
    
    * Chamged Measurement Unit to UOM
    
    * Changed Measurement Unit to UOM
    
    * Quality Action Unit Test Fix
    
    * Renamed Chart of Procedures to Tree of Procedures
    
    * Changes Objective input type to Text
    
    * Code Refactor
    
    * Quality Action Status option changed to "Close" for consistency
    
    * fixed action status indicator
    
    * Removed vscode folder
    
    * unit tests optimization
    
    * unit test fixes
    
    * code refactor
    
    * gitignore
    
    * bug fixes and code refactor
    
    * code refactor
    
    * unit test changes
    
    * changed quality action unit test
    
    * code refactor
    
    * code refactor
    
    * code refactor
    
    * indentation
    
    * code refactor
    
    * corrected typo
    
    * Removed jQuery hide()
    
    * code refracted
    
    * remove grid elements
    
    * Removed unnecessary conditions
    
    * onload fixed in feedback
    
    * code refracted
    
    * Removed add row button from Review and Customer Feedback
    
    * made autofill feild read only
    
    * automated scheduling of review

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -254,7 +254,8 @@ scheduler_events = {
 		"erpnext.assets.doctype.asset.asset.update_maintenance_status",
 		"erpnext.assets.doctype.asset.asset.make_post_gl_entry",
 		"erpnext.crm.doctype.contract.contract.update_status_for_contracts",
-		"erpnext.projects.doctype.project.project.update_project_sales_billing"
+		"erpnext.projects.doctype.project.project.update_project_sales_billing",
+		"erpnext.quality_management.doctype.quality_review.quality_review.review"
 	],
 	"daily_long": [
 		"erpnext.manufacturing.doctype.bom_update_tool.bom_update_tool.update_latest_price_in_all_boms"
