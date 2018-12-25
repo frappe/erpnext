61b55cb7c5b4188b69669f38c13ba2eb06300ce8
Himanshu <himanshuwarekar@yahoo.com>
2018-12-25 17:42:31 +0530

    Quality Management System (#15893)
    * Added QMS to ERPNext
    
    * changes
    
    * fixed
    
    * code refracted
    
    * refracted
    
    * some bugs fixed
    
    * changes
    
    * changes
    
    * test case
    
    * bug fix unit test
    
    * bug fix
    
    * refracted code
    
    * test warning fix
    
    * test case bug fix
    
    * removed whitespace
    
    * indentation bug fix
    
    * unit test bug fix
    
    * fixed Tree
    
    * bug fix
    
    * bug fix
    
    * code refactor
    
    * fixed procedure tree
    
    * removed unnecessary fields
    
    * hide fields
    
    * removed depricated unit test method
    
    * sql injection bug fix
    
    * code refactor and added mandatory fields
    
    * Removed Add Child from TreeView
    
    * Unit Test crashes Fix
    
    * code refactor
    
    * Fixed Status of Action
    
    * removed add button from Chart of Procedure
    
    * Fixed Unit Test for Quality Action
    
    * Chamged Measurement Unit to UOM
    
    * Changed Measurement Unit to UOM
    
    * Quality Action Unit Test Fix
    
    * Renamed Chart of Procedures to Tree of Procedures
    
    * Changes Objective input type to Text
    
    * Code Refactor
    
    * Quality Action Status option changed to "Close" for consistency
    
    * fixed action status indicator
    
    * Removed vscode folder
    
    * unit tests optimization
    
    * unit test fixes
    
    * code refactor
    
    * gitignore
    
    * bug fixes and code refactor
    
    * code refactor
    
    * unit test changes
    
    * changed quality action unit test
    
    * code refactor
    
    * code refactor
    
    * code refactor
    
    * indentation
    
    * code refactor
    
    * corrected typo
    
    * Removed jQuery hide()
    
    * code refracted
    
    * remove grid elements
    
    * Removed unnecessary conditions
    
    * onload fixed in feedback
    
    * code refracted
    
    * Removed add row button from Review and Customer Feedback
    
    * made autofill feild read only
    
    * automated scheduling of review

---
+++ erpnext/quality_management/doctype/quality_procedure/quality_procedure_tree.js
@@ -0,0 +1,39 @@
+frappe.provide("frappe.treeview_settings");
+
+frappe.treeview_settings["Quality Procedure"] = {
+	ignore_fields:["parent_quality_procedure"],
+	get_tree_nodes: 'erpnext.quality_management.doctype.quality_procedure.quality_procedure.get_children',
+	add_tree_node: 'erpnext.quality_management.doctype.quality_procedure.quality_procedure.add_node',
+	filters: [
+		{
+			fieldname: "Quality Procedure",
+			fieldtype:"Link",
+			options: "Quality Procedure",
+			label: __("Quality Procedure"),
+			get_query: function() {
+				return {
+					filters: [["Quality Procedure", 'is_group', '=', 1]]
+				};
+			}
+		},
+	],
+	breadcrumb: "Setup",
+	root_label: "All Quality Procedures",
+	get_tree_root: false,
+	menu_items: [
+		{
+			label: __("New Quality Procedure"),
+			action: function() {
+				frappe.new_doc("Quality Procedure", true);
+			},
+			condition: 'frappe.boot.user.can_create.indexOf("Quality Procedure") !== -1'
+		}
+	],
+	onload: function(treeview) {
+		treeview.make_tree();
+	},
+	onrender: function() {
+		$("button:contains('Add Child')").remove();
+		$("button:contains('New')").remove();
+	}
+};
