253cca5c812dd8256142749af5b7012ca123b1fc
Suraj Shetty <surajshetty3416@gmail.com>
2018-12-26 11:32:06 +0530

    Revert "Modify bench_init.sh"
    Point back to original repo
    
    This reverts commit 87024535bd712d149de9e61ae37018ba746f9c59.

--- travis/bench_init.sh
+++ travis/bench_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ~/
-curl -I https://github.com/surajshetty3416/frappe/tree/$TRAVIS_BRANCH | head -n 1 | cut -d $' ' -f2 | (
+curl -I https://github.com/frappe/frappe/tree/$TRAVIS_BRANCH | head -n 1 | cut -d $' ' -f2 | (
 	read response;
 	[ $response == '200' ] && branch=$TRAVIS_BRANCH || branch='develop';
-	bench init frappe-bench --frappe-path https://github.com/surajshetty3416/frappe.git --frappe-branch $branch --python $(which python)
+	bench init frappe-bench --frappe-path https://github.com/frappe/frappe.git --frappe-branch $branch --python $(which python)
 )
