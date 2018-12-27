aca374e47895fe5853843c4975ca5d3d289a940e
Saif Ur Rehman <saifi0102@gmail.com>
2018-12-28 04:53:25 +0500

    feat: add_taxes_from_item_tax_template

--- erpnext/accounts/doctype/accounts_settings/accounts_settings.py
+++ erpnext/accounts/doctype/accounts_settings/accounts_settings.py
@@ -15,6 +15,7 @@ class AccountsSettings(Document):
 		frappe.clear_cache()
 
 	def validate(self):
+		frappe.db.set_default("add_taxes_from_item_tax_template", self.get("add_taxes_from_item_tax_template", ""))
 		self.validate_stale_days()
 		self.enable_payment_schedule_in_print()
 		self.enable_fields_for_cost_center_settings()
