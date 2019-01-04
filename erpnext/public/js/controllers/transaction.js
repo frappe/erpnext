158e7dcd8bcb3e80dfb60541dbbf54800a372b2c
Nabin Hait <nabinhait@gmail.com>
2019-01-04 11:04:37 +0530

    fix(bom-uom_issue)Fetch rate in BOM from Price List based on UOM

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -441,6 +441,7 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 							weight_per_unit: item.weight_per_unit,
 							weight_uom: item.weight_uom,
 							uom : item.uom,
+							stock_uom: item.stock_uom,
 							pos_profile: me.frm.doc.doctype == 'Sales Invoice' ? me.frm.doc.pos_profile : '',
 							cost_center: item.cost_center
 						}
