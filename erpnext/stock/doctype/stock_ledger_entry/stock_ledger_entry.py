7bc692d48d642a5df88f1296f60fa293e12fd3af
Aditya Hase <aditya@adityahase.com>
2019-01-07 19:01:47 +0530

    feat(naming): Deferred naming for Stock Ledger Entry and GL Entry
    For fast lockless insertions use hash as autonaming method for
    temporarily naming Stock Ledger Entry and GL Entry
    
    In an hourly run scheduled job rename these entries using autoname options

--- erpnext/stock/doctype/stock_ledger_entry/stock_ledger_entry.py
+++ erpnext/stock/doctype/stock_ledger_entry/stock_ledger_entry.py
@@ -16,6 +16,13 @@ class StockFreezeError(frappe.ValidationError): pass
 exclude_from_linked_with = True
 
 class StockLedgerEntry(Document):
+	def autoname(self):
+		"""
+		Temporarily name doc for fast insertion
+		name will be changed using autoname options (in a scheduled job)
+		"""
+		self.name = frappe.generate_hash(txt="", length=10)
+
 	def validate(self):
 		self.flags.ignore_submit_comment = True
 		from erpnext.stock.utils import validate_warehouse_company
@@ -132,4 +139,4 @@ def on_doctype_update():
 
 	frappe.db.add_index("Stock Ledger Entry", ["voucher_no", "voucher_type"])
 	frappe.db.add_index("Stock Ledger Entry", ["batch_no", "item_code", "warehouse"])
-	
+
