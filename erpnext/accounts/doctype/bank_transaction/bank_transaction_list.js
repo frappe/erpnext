4c57fae72609acf8399036283c77f0d6a2d9735e
Charles-Henri Decultot <chdecultot@dokos.io>
2019-01-07 15:15:23 +0000

    Codacy correction

--- erpnext/accounts/doctype/bank_transaction/bank_transaction_list.js
+++ erpnext/accounts/doctype/bank_transaction/bank_transaction_list.js
@@ -6,7 +6,7 @@ frappe.listview_settings['Bank Transaction'] = {
 	get_indicator: function(doc) {
 		if(flt(doc.unallocated_amount)>0) {
 			return [__("Unreconciled"), "orange", "unallocated_amount,>,0"];
-		} else if(flt(doc.unallocated_amount)==0) {
+		} else if(flt(doc.unallocated_amount)===0) {
 			return [__("Reconciled"), "green", "unallocated_amount,=,0"];
 		}
 	}
