2b06cdf2c3d6a4893d328d9ff964be677fd34cf8
Saif Ur Rehman <saifi0102@gmail.com>
2019-01-14 16:34:54 +0500

    patch: Update billing status

---
+++ erpnext/patches/v11_0/update_billing_status.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2018, Frappe and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	si_names = frappe.get_all("Sales Invoice")
+	for name in si_names:
+		name = name.name
+		doc = frappe.get_doc("Sales Invoice", name)
+		doc.update_status_updater_args()
+		doc.update_prevdoc_status()
+		doc.update_billing_status_in_dn()
+		if not doc.is_return:
+			doc.update_billing_status_for_zero_amount_refdoc("Sales Order")
+
+	pi_names = frappe.get_all("Purchase Invoice")
+	for name in pi_names:
+		name = name.name
+		doc = frappe.get_doc("Purchase Invoice", name)
+		doc.update_status_updater_args()
+		doc.update_prevdoc_status()
+		doc.update_billing_status_in_pr()
+		if not doc.is_return:
+			doc.update_billing_status_for_zero_amount_refdoc("Purchase Order")
