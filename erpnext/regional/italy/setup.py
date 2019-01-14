0a39f6e9394facb8893ef382d7a75c1cfaf675c3
Gaurav <gvrvnk@gmail.com>
2019-01-15 02:24:50 +0530

    update(regional): italian localisation setup
    Added fields for first_name and last_name in Customer, visible for individuals

--- erpnext/regional/italy/setup.py
+++ erpnext/regional/italy/setup.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 
 import frappe
+from frappe import _
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 from erpnext.regional.italy import fiscal_regimes, tax_exemption_reasons, mode_of_payment_codes
 
@@ -45,7 +46,13 @@ def make_custom_fields(update=True):
             dict(fieldname='pec', label='Recipient PEC',
                 fieldtype='Data', insert_after='fiscal_code', print_hide=1),
             dict(fieldname='is_public_administration', label='Is Public Administration',
-			    fieldtype='Select', insert_after='company_type', print_hide=1, description="Set this if the customer is a Public Administration company.")
+			    fieldtype='Check', insert_after='is_internal_customer', print_hide=1, 
+                description=_("Set this if the customer is a Public Administration company."), 
+                depends_on='eval:doc.customer_type=="Company"'),
+            dict(fieldname='first_name', label='First Name', fieldtype='Data', 
+                insert_after='salutation', print_hide=1, depends_on='eval:doc.customer_type!="Company"'),
+            dict(fieldname='last_name', label='Last Name', fieldtype='Data', 
+                insert_after='first_name', print_hide=1, depends_on='eval:doc.customer_type!="Company"')
         ],
         'Mode of Payment': [
             dict(fieldname='e_invoicing_code', label='Code',
