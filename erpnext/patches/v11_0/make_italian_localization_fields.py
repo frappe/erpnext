4fe78b79f793a970cf793e73856bc9e9b174ff88
Gaurav <gvrvnk@gmail.com>
2019-01-15 02:21:47 +0530

    fix(codacy) removed reference to frappe

--- erpnext/patches/v11_0/make_italian_localization_fields.py
+++ erpnext/patches/v11_0/make_italian_localization_fields.py
@@ -2,7 +2,6 @@
 # License: GNU General Public License v3. See license.txt
 
 from __future__ import unicode_literals
-import frappe
 from erpnext.regional.italy.setup import make_custom_fields
 
 def execute():
