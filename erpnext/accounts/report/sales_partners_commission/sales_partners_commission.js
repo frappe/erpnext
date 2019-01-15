fd236fc7db98ad5e9bb11aa80c2ccf6e72b57a44
Rohan Bansal <rohan@digithinkit.com>
2019-01-15 17:55:04 +0530

    fix(sales_commission): Add date range filter and more info on Sales Partner

---
+++ erpnext/accounts/report/sales_partners_commission/sales_partners_commission.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Partners Commission"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+	]
+}
