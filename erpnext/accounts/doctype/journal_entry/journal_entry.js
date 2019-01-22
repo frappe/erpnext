270c4c2a87716b82cd8894fdcd536217b560e893
Zlash65 <zarrar65@gmail.com>
2019-01-22 12:47:25 +0530

    fix: push party filter only if applicable

--- erpnext/accounts/doctype/journal_entry/journal_entry.js
+++ erpnext/accounts/doctype/journal_entry/journal_entry.js
@@ -175,9 +175,15 @@ erpnext.accounts.JournalEntry = frappe.ui.form.Controller.extend({
 				out.filters.push([jvd.reference_type, "per_billed", "<", 100]);
 			}
 
-			if(jvd.party_type && jvd.party) {
-				out.filters.push([jvd.reference_type,
-					(jvd.reference_type.indexOf("Sales")===0 ? "customer" : "supplier"), "=", jvd.party]);
+			var party_field = "";
+			if(jvd.reference_type.indexOf("Sales")===0) {
+				var party_field = "customer";
+			} else if (jvd.reference_type.indexOf("Purchase")===0) {
+				var party_field = "supplier";
+			}
+
+			if (party_field) {
+				out.filters.push([jvd.reference_type, party_field, "=", jvd.party]);
 			}
 
 			return out;
