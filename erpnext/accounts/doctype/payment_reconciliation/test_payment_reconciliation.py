215f97cbbfb7bd8f6028eaf91a6841b7190bc37a
Saif Ur Rehman <saifi0102@gmail.com>
2019-01-22 15:33:55 +0500

    fix(Payment Reconciliation): Attempt to fix test
    (cherry picked from commit 632fc8ea0ba40f5ff3d27cd2b95ed3e4ad4cdcac)

--- erpnext/accounts/doctype/payment_reconciliation/test_payment_reconciliation.py
+++ erpnext/accounts/doctype/payment_reconciliation/test_payment_reconciliation.py
@@ -32,47 +32,16 @@ class TestPaymentReconciliation(unittest.TestCase):
 
 		# Test records
 		jv_simple = make_payment_jv(self.customer, 100)
-		jv_simple.insert()
-		jv_simple.submit()
-
-		jv_multiple_rows = make_payment_jv(self.customer, 100)
-		jv_multiple_rows.get("accounts")[0].debit_in_account_currency = 300
-		jv_multiple_rows.append("accounts", jv_multiple_rows.get("accounts")[1].as_dict())
-		jv_multiple_rows.append("accounts", jv_multiple_rows.get("accounts")[1].as_dict())
-		jv_multiple_rows.insert()
-		jv_multiple_rows.submit()
+		jv_multiple_rows = make_payment_jv(self.customer, 300, unallocated_rows=3)
 
 		jv_half_returned = make_payment_jv(self.customer, 200)
-		jv_half_returned.insert()
-		jv_half_returned.submit()
-
-		_jv_return = make_payment_jv(self.customer, -100)
-		_jv_return.get("accounts")[1].update({
-			"reference_type": "Journal Entry", "reference_name": jv_half_returned.name
-		})
-		_jv_return.insert()
-		_jv_return.submit()
+		_jv_return = make_payment_jv(self.customer, 0, jv_half_returned.doctype, jv_half_returned.name, -100)
 
 		_jv_receivable = make_payment_jv(self.customer, -100)
-		_jv_receivable.insert()
-		_jv_receivable.submit()
-
-		jv_half_allocated = make_payment_jv(self.customer, 100)
-		jv_half_allocated.get("accounts")[0].debit_in_account_currency = 200
-		jv_half_allocated.append("accounts", jv_half_allocated.get("accounts")[1].as_dict().update({
-			"reference_type": "Journal Entry", "reference_name": _jv_receivable.name
-		}))
-		jv_half_allocated.insert()
-		jv_half_allocated.submit()
+		jv_half_allocated = make_payment_jv(self.customer, 100, _jv_receivable.doctype, _jv_receivable.name, 100)
 
 		so = make_sales_order(self.customer)
-		jv_against_so = make_payment_jv(self.customer, 100)
-		jv_against_so.get("accounts")[0].debit_in_account_currency = 200
-		jv_against_so.append("accounts", jv_against_so.get("accounts")[1].as_dict().update({
-			"reference_type": "Sales Order", "reference_name": so.name
-		}))
-		jv_against_so.insert()
-		jv_against_so.submit()
+		jv_against_so = make_payment_jv(self.customer, 100, so.doctype, so.name, 100)
 
 		# Test get_advance_journal_entries
 		advances = get_advance_journal_entries(self.customer.doctype, self.customer.name, "_Test Receivable - _TC",
@@ -101,8 +70,6 @@ class TestPaymentReconciliation(unittest.TestCase):
 
 		# Test reconcile_against_document
 		jv_receivable = make_payment_jv(self.customer, -5000)
-		jv_receivable.insert()
-		jv_receivable.submit()
 
 		# Full allocation
 		lst = [frappe._dict({
@@ -221,7 +188,7 @@ class TestPaymentReconciliation(unittest.TestCase):
 			advance_vouchers.setdefault(d.reference_name, [])
 			advance_vouchers[d.reference_name].append(d)
 
-		self.assertFalse(advance_vouchers.get("jv_simple.name"))
+		self.assertFalse(advance_vouchers.get(jv_simple.name))
 
 		self.assertEqual(len(advance_vouchers[jv_multiple_rows.name]), 1)
 		self.assertEqual(advance_vouchers[jv_multiple_rows.name][0].amount, 70)
@@ -234,46 +201,12 @@ class TestPaymentReconciliation(unittest.TestCase):
 			"Sales Order", against_all_orders=True))
 
 		# Test records
-		so = make_sales_order(self.customer)
-		pe_against_so = get_payment_entry(so.doctype, so.name, party_amount=200, bank_amount=200)
-		pe_against_so.paid_from = "_Test Receivable - _TC"
-		pe_against_so.references[0].allocated_amount = 100
-		pe_against_so.unallocated_amount = 100
-		pe_against_so.reference_no = "1"
-		pe_against_so.reference_date = nowdate()
-		pe_against_so.allocate_payment_amount = 0
-		pe_against_so.insert()
-		pe_against_so.submit()
-
-		pe_unallocated = frappe.copy_doc(get_payment_entry(so.doctype, so.name, party_amount=100, bank_amount=100))
-		pe_unallocated.paid_from = "_Test Receivable - _TC"
-		pe_unallocated.set("references", [])
-		pe_unallocated.reference_no = "1"
-		pe_unallocated.reference_date = nowdate()
-		pe_unallocated.insert()
-		pe_unallocated.submit()
-
 		_jv_receivable = make_payment_jv(self.customer, -100)
-		_jv_receivable.insert()
-		_jv_receivable.submit()
 
-		pe_half_allocated = frappe.copy_doc(pe_unallocated)
-		pe_half_allocated.paid_from = "_Test Receivable - _TC"
-		pe_half_allocated.allocate_payment_amount = 0
-		pe_half_allocated.received_amount = 200
-		pe_half_allocated.paid_amount = 200
-		pe_half_allocated.set("references", [])
-		pe_half_allocated.append("references", {
-			'reference_doctype': _jv_receivable.doctype,
-			'reference_name': _jv_receivable.name,
-			'total_amount': 100.0,
-			'outstanding_amount': 100.0,
-			'allocated_amount': 100.0
-		})
-		pe_half_allocated.unallocated_amount = 100
-		pe_half_allocated.difference_amount = 0
-		pe_half_allocated.save()
-		pe_half_allocated.submit()
+		so = make_sales_order(self.customer)
+		pe_against_so = make_payment_entry(self.customer, 100, so.doctype, so.name, 100)
+		pe_unallocated = make_payment_entry(self.customer, 100)
+		pe_half_allocated = make_payment_entry(self.customer, 100, _jv_receivable.doctype, _jv_receivable.name, 100)
 
 		# Test get_advance_payment_entries
 		advances = get_advance_payment_entries(self.customer.doctype, self.customer.name, "_Test Receivable - _TC",
@@ -284,7 +217,9 @@ class TestPaymentReconciliation(unittest.TestCase):
 			advance_vouchers[d.reference_name].append(d)
 
 		import pprint
-		print(pprint.pprint(advance_vouchers, indent=2))
+		print("-------------------------------")
+		pprint.pprint(advance_vouchers)
+		print("-------------------------------")
 
 		self.assertEqual(len(advance_vouchers[pe_against_so.name]), 2)
 		self.assertEqual(advance_vouchers[pe_against_so.name][0].reference_row, pe_against_so.references[0].name)
@@ -299,8 +234,6 @@ class TestPaymentReconciliation(unittest.TestCase):
 
 		# Test reconcile_against_document
 		jv_receivable = make_payment_jv(self.customer, -5000)
-		jv_receivable.insert()
-		jv_receivable.submit()
 
 		# Full allocation
 		lst = [frappe._dict({
@@ -374,21 +307,96 @@ class TestPaymentReconciliation(unittest.TestCase):
 			advance_vouchers.setdefault(d.reference_name, [])
 			advance_vouchers[d.reference_name].append(d)
 
-		self.assertFalse(advance_vouchers.get("pe_unallocated.name"))
+		self.assertFalse(advance_vouchers.get(pe_unallocated.name))
 		self.assertEqual(len(advance_vouchers[pe_against_so.name]), 1)
 		self.assertEqual(advance_vouchers[pe_against_so.name][0].amount, 100)
 
 
-def make_payment_jv(party, amount):
+def make_payment_entry(party, unallocated_amount, against_dt=None, against_dn=None, against_amount=None):
+	doc = frappe.new_doc("Payment Entry")
+	doc.company = "_Test Company"
+	doc.party_type = party.doctype
+	doc.party = party.name
+	doc.party_account = "_Test Payable - _TC" if doc.party_type == "Supplier" else "_Test Receivable - _TC"
+	doc.payment_type = "Pay" if doc.party_type == "Supplier" else "Receive"
+	doc.paid_from = "_Test Bank - _TC" if doc.party_type == "Supplier" else doc.party_account
+	doc.paid_to = doc.party_account if doc.party_type == "Supplier" else "_Test Bank - _TC"
+	doc.exchange_rate = 1
+	doc.allocate_payment_amount = 0
+	doc.reference_no = "1"
+	doc.reference_date = nowdate()
+
+	if against_dt and against_dn and against_amount:
+		doc.paid_amount = doc.received_amount = unallocated_amount + against_amount
+		doc.unallocated_amount = unallocated_amount + against_amount
+		doc.append("references", {
+			'reference_doctype': against_dt,
+			'reference_name': against_dn,
+			'allocated_amount': against_amount
+		})
+	else:
+		doc.paid_amount = doc.received_amount = unallocated_amount
+		doc.unallocated_amount = unallocated_amount
+
+	doc.set_missing_values()
+	doc.insert()
+	doc.submit()
+
+	import pprint
+	print("-------------------------------")
+	pprint.pprint(doc.as_dict())
+	print("-------------------------------")
+
+	return doc
+
+
+def make_payment_jv(party, unallocated_amount, against_dt=None, against_dn=None, against_amount=None,
+		unallocated_rows=None):
 	from erpnext.accounts.doctype.journal_entry.test_journal_entry import make_journal_entry
 
 	party_type = party.doctype
 	party_account = "_Test Payable - _TC" if party_type == "Supplier" else "_Test Receivable - _TC"
-	amount = -1 * amount if party_type == "Supplier" else amount
 
-	jv = make_journal_entry("_Test Bank - _TC", party_account, amount, save=False)
-	jv.get("accounts")[1].update({"party_type": party_type, "party": party.name})
+	total_amount = unallocated_amount
+	if against_dt and against_dn and against_amount:
+		total_amount += against_amount
+	if party_type == "Supplier":
+		total_amount = -1 * total_amount
+
+	jv = make_journal_entry("_Test Bank - _TC", party_account, total_amount, save=False)
+	jv.accounts[1].update({"party_type": party_type, "party": party.name})
+	if jv.accounts[1].debit_in_account_currency:
+		jv.accounts[1].debit_in_account_currency = abs(unallocated_amount)
+	else:
+		jv.accounts[1].credit_in_account_currency = abs(unallocated_amount)
+
+	if unallocated_rows:
+		if jv.accounts[1].debit_in_account_currency:
+			jv.accounts[1].debit_in_account_currency = unallocated_amount / unallocated_rows
+		else:
+			jv.accounts[1].credit_in_account_currency = unallocated_amount / unallocated_rows
+
+		for i in range(unallocated_rows-1):
+			jv.append("accounts", jv.accounts[1].as_dict())
+
+	if against_dt and against_dn and against_amount:
+		if unallocated_amount:
+			allocated_row = jv.append("accounts", jv.accounts[1].as_dict())
+		else:
+			allocated_row = jv.accounts[1]
+
+		allocated_row.update({"reference_type": against_dt, "reference_name": against_dn})
+		if allocated_row.debit_in_account_currency:
+			allocated_row.debit_in_account_currency = against_amount if against_amount > 0 else 0
+			allocated_row.credit_in_account_currency = abs(against_amount) if against_amount < 0 else 0
+		else:
+			allocated_row.credit_in_account_currency = against_amount if against_amount > 0 else 0
+			allocated_row.debit_in_account_currency = abs(against_amount) if against_amount < 0 else 0
+
+	jv.insert()
+	jv.submit()
 	return jv
+
 
 def make_sales_order(party):
 	so = frappe.get_doc(frappe.get_test_records('Sales Order')[0])
