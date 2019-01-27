c2ffcc5e71a0112c0044a023200263a783fe4beb
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-01-27 16:09:07 +0530

    feat(POS): Customer wise price list in offline mode and enhancement in POS closing voucher

--- erpnext/selling/doctype/pos_closing_voucher/pos_closing_voucher.js
+++ erpnext/selling/doctype/pos_closing_voucher/pos_closing_voucher.js
@@ -20,6 +20,16 @@ frappe.ui.form.on('POS Closing Voucher', {
 			};
 		});
 	},
+
+	total_amount: function(frm) {
+		get_difference_amount(frm);
+	},
+	custody_amount: function(frm){
+		get_difference_amount(frm);
+	},
+	expense_amount: function(frm){
+		get_difference_amount(frm);
+	},
 	refresh: function(frm) {
 		get_closing_voucher_details(frm);
 	},
@@ -47,6 +57,10 @@ frappe.ui.form.on('POS Closing Voucher Details', {
 	}
 });
 
+var get_difference_amount = function(frm){
+	frm.doc.difference = frm.doc.total_amount - frm.doc.custody_amount - frm.doc.expense_amount;
+	refresh_field("difference");
+};
 
 var get_closing_voucher_details = function(frm) {
 	if (frm.doc.period_end_date && frm.doc.period_start_date && frm.doc.company && frm.doc.pos_profile && frm.doc.user) {
