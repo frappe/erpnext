cc98983fcffce3ea3d91f3bfd312a4f250903608
Zlash65 <zarrar65@gmail.com>
2019-02-04 23:27:17 +0530

    download csv template to create custom chart

--- erpnext/accounts/doctype/chart_of_accounts_importer/chart_of_accounts_importer.py
+++ erpnext/accounts/doctype/chart_of_accounts_importer/chart_of_accounts_importer.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, csv, os
+from frappe import _
+from frappe.utils import cstr
 from frappe.model.document import Document
+from frappe.utils.csvutils import UnicodeWriter
+from frappe.utils.file_manager import get_file_path
+from erpnext.accounts.doctype.account.chart_of_accounts.chart_of_accounts import create_charts, build_tree_from_json
 
 class ChartofAccountsImporter(Document):
 	pass
@@ -13,3 +18,18 @@ class ChartofAccountsImporter(Document):
 def validate_company(company):
 	if frappe.db.get_all('GL Entry', {"company": company}, "name", limit=1):
 		return False	
+
+@frappe.whitelist()
+def download_template():
+	data = frappe._dict(frappe.local.form_dict)
+	fields = ["Account Name", "Parent Account", "Is Group", "Account Type", "Root Type"]
+	writer = UnicodeWriter()
+
+	writer.writerow([_('Chart of Accounts Template')])
+	writer.writerow([_("Column Labels : ")] + fields)
+	writer.writerow([_("Start entering data from here : ")])
+
+	# download csv file
+	frappe.response['result'] = cstr(writer.getvalue())
+	frappe.response['type'] = 'csv'
+	frappe.response['doctype'] = data.get('doctype')
