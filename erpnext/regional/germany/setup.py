ff878c0c1d10594446479a6f65687aeadd91e0e0
Raffael Meyer <raffael@alyf.de>
2019-02-07 03:28:27 +0100

    fix formatting and names

--- erpnext/regional/germany/setup.py
+++ erpnext/regional/germany/setup.py
@@ -1,21 +1,31 @@
-import frappe, os
+import os
+import frappe
+
 
 def setup(company=None, patch=True):
-    if not patch:
-        update_address_template()
+	if not patch:
+		update_address_template()
 
 
 def update_address_template():
-    with open(os.path.join(os.path.dirname(__file__), 'address_template.html'), 'r') as f:
-        html = f.read()
+	"""
+	Read address template from file. Update existing Address Template or create a
+	new one.
+	"""
+	dir_name = os.path.dirname(__file__)
+	template_path = os.path.join(dir_name, 'address_template.html')
 
-    address_template = frappe.db.get_value('Address Template', 'Germany')
-    if address_template:
-        frappe.db.set_value('Address Template', 'Germany', 'template', html)
-    else:
-        # make new html template for Germany
-        frappe.get_doc(dict(
-            doctype='Address Template',
-            country='Germany',
-            template=html
-        )).insert()
+	with open(template_path, 'r') as template_file:
+		template_html = template_file.read()
+
+	address_template = frappe.db.get_value('Address Template', 'Germany')
+
+	if address_template:
+		frappe.db.set_value('Address Template', 'Germany', 'template', template_html)
+	else:
+		# make new html template for Germany
+		frappe.get_doc(dict(
+			doctype='Address Template',
+			country='Germany',
+			template=template_html
+		)).insert()
