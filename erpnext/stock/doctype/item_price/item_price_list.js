f582616f4b96e79453f06d80d2bd156b2d2db040
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-02-08 08:13:19 +0530

    fix: Currency symbol fix in item price list

---
+++ erpnext/stock/doctype/item_price/item_price_list.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// License: GNU General Public License v3. See license.txt
+
+// render
+frappe.listview_settings['Item Price'] = {
+	add_fields: ["currency"]
+};
