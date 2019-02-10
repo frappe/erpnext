8f03fe99daa9b04296ce42844e235bb5fb2af744
Faris Ansari <netchamp.faris@gmail.com>
2019-02-10 20:35:17 +0530

    feat: Introduce Hompage Section
    - Can render cards which are provided in the child table
    - Can choose how many cards to render in one row
    - Can also render custom HTML for full flexibility
    - Section Order decides the order in which they appear on the homepage

--- erpnext/templates/pages/home.py
+++ erpnext/templates/pages/home.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import cint
 
 no_cache = 1
 no_sitemap = 1
@@ -31,5 +32,8 @@ def get_context(context):
 	context.phone = frappe.db.get_single_value('Contact Us Settings', 'phone')
 	context.explore_link = '/products'
 
-	# homepage_sections = frappe.get_all('Homepage Section', order_by='section_order asc')
-	# context.custom_sections = [frappe.get_doc('Homepage Section', name) for name in homepage_sections]
+	homepage_sections = frappe.get_all('Homepage Section', order_by='section_order asc')
+	context.homepage_sections = [frappe.get_doc('Homepage Section', name) for name in homepage_sections]
+
+	for section in context.homepage_sections:
+		section.column_value = cint(12 / cint(section.no_of_columns or 3))
