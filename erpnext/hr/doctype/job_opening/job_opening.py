4dc8fe775f602a5cb3b1bfac5f057a4b861204a5
Faris Ansari <netchamp.faris@gmail.com>
2019-02-11 11:44:36 +0530

    fix: Prettify template for job opening

--- erpnext/hr/doctype/job_opening/job_opening.py
+++ erpnext/hr/doctype/job_opening/job_opening.py
@@ -53,3 +53,26 @@ class JobOpening(WebsiteGenerator):
 def get_list_context(context):
 	context.title = _("Jobs")
 	context.introduction = _('Current Job Openings')
+	context.get_list = get_job_openings
+
+def get_job_openings(doctype, txt=None, filters=None, limit_start=0, limit_page_length=20, order_by=None):
+	fields = ['name', 'status', 'job_title', 'description']
+
+	filters = filters or {}
+	filters.update({
+		'status': 'Open'
+	})
+
+	if txt:
+		filters.update({
+			'job_title': ['like', '%{0}%'.format(txt)],
+			'description': ['like', '%{0}%'.format(txt)]
+		})
+
+	return frappe.get_all(doctype,
+		filters,
+		fields,
+		start=limit_start,
+		page_length=limit_page_length,
+		order_by=order_by
+	)
