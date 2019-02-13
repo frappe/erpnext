e5c744c6fb712b95e3981d418642de6dab50f464
Faris Ansari <netchamp.faris@gmail.com>
2019-02-13 13:02:39 +0530

    fix: Validate invalid fields in Item Variant Settings
    - Some fields like barcodes cannot be copied in Item Variants
    - Wrote a patch that deletes the fields if they exist
    - Also, validate in controller

--- erpnext/stock/doctype/item_variant_settings/item_variant_settings.py
+++ erpnext/stock/doctype/item_variant_settings/item_variant_settings.py
@@ -5,14 +5,17 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class ItemVariantSettings(Document):
+	invalid_fields_for_copy_fields_in_variants = ['barcodes']
+
 	def set_default_fields(self):
 		self.fields = []
 		fields = frappe.get_meta('Item').fields
 		exclude_fields = ["naming_series", "item_code", "item_name", "show_in_website",
 			"show_variant_in_website", "standard_rate", "opening_stock", "image", "description",
-			"variant_of", "valuation_rate", "description",
+			"variant_of", "valuation_rate", "description", "barcodes",
 			"website_image", "thumbnail", "website_specifiations", "web_long_description"]
 
 		for d in fields:
@@ -21,3 +24,13 @@ class ItemVariantSettings(Document):
 				self.append('fields', {
 					'field_name': d.fieldname
 				})
+
+	def remove_invalid_fields_for_copy_fields_in_variants(self):
+		fields = [row for row in self.fields if row.field_name not in self.invalid_fields_for_copy_fields_in_variants]
+		self.fields = fields
+		self.save()
+
+	def validate(self):
+		for d in self.fields:
+			if d.field_name in self.invalid_fields_for_copy_fields_in_variants:
+				frappe.throw(_('Cannot set the field <b>{0}</b> for copying in variants').format(d.field_name))
