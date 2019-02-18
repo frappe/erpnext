c6aaa82a463fa08b9f8327a70cb5badb7b4bf436
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-02-18 21:42:00 +0530

    add service level agreement in help desk

--- erpnext/config/help_desk.py
+++ erpnext/config/help_desk.py
@@ -2,61 +2,86 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-    return [
-        {
-            "label": _("Issues"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "Issue",
-                    "description": _("Support queries from customers."),
+	return [
+		{
+			"label": _("Issues"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Issue",
+					"description": _("Support queries from customers."),
 					"onboard": 1,
-                },
-                {
-                    "type": "doctype",
-                    "name": "Communication",
-                    "description": _("Communication log."),
+				},
+				{
+					"type": "doctype",
+					"name": "Communication",
+					"description": _("Communication log."),
 					"onboard": 1,
-                },
-            ]
-        },
-        {
-            "label": _("Warranty"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "Warranty Claim",
-                    "description": _("Warranty Claim against Serial No."),
-                },
-                {
-                    "type": "doctype",
-                    "name": "Serial No",
-                    "description": _("Single unit of an Item."),
-                },
-            ]
-        },
-        {
-            "label": _("Reports"),
-            "icon": "fa fa-list",
-            "items": [
-                {
-                    "type": "page",
-                    "name": "support-analytics",
-                    "label": _("Support Analytics"),
-                    "icon": "fa fa-bar-chart"
-                },
-                {
-                    "type": "report",
-                    "name": "Minutes to First Response for Issues",
-                    "doctype": "Issue",
-                    "is_query_report": True
-                },
-                {
-                    "type": "report",
-                    "name": "Support Hours",
-                    "doctype": "Issue",
-                    "is_query_report": True
-                },
-            ]
-        },
-    ]
+				},
+			]
+		},
+		{
+			"label": _("Warranty"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Warranty Claim",
+					"description": _("Warranty Claim against Serial No."),
+				},
+				{
+					"type": "doctype",
+					"name": "Serial No",
+					"description": _("Single unit of an Item."),
+				},
+			]
+		},
+				{
+			"label": _("Service Level Agreement"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Employee Group",
+					"description": _("Support Team."),
+				},
+				{
+					"type": "doctype",
+					"name": "Service Level",
+					"description": _("Service Level."),
+				},
+				{
+					"type": "doctype",
+					"name": "Support Contract",
+					"description": _("Support Contract."),
+				},
+				{
+					"type": "doctype",
+					"name": "Service Level Agreement",
+					"description": _("Service Level Agreement."),
+				}
+			]
+		},
+		{
+			"label": _("Reports"),
+			"icon": "fa fa-list",
+			"items": [
+				{
+					"type": "page",
+					"name": "support-analytics",
+					"label": _("Support Analytics"),
+					"icon": "fa fa-bar-chart"
+				},
+				{
+					"type": "report",
+					"name": "Minutes to First Response for Issues",
+					"doctype": "Issue",
+					"is_query_report": True
+				},
+				{
+					"type": "report",
+					"name": "Support Hours",
+					"doctype": "Issue",
+					"is_query_report": True
+				},
+			]
+		},
+	]
