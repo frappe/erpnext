204625a43018319c3fe0e1e79eda1a01be61e09c
navdeepghai1 <navdeepghai1@gmail.com>
2019-02-18 14:15:11 +0400

    fix: POS batches issue on decrement function of qty

--- erpnext/selling/sales_common.js
+++ erpnext/selling/sales_common.js
@@ -373,7 +373,7 @@ erpnext.selling.SellingController = erpnext.TransactionController.extend({
 	*/
 	set_batch_number: function(cdt, cdn) {
 		const doc = frappe.get_doc(cdt, cdn);
-		if (doc && doc.has_batch_no) {
+		if (doc && doc.has_batch_no && !doc.batch_no) {
 			this._set_batch_number(doc);
 		}
 	},
