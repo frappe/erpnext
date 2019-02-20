81cb25e9be80f2d9c492eb17cf67d26deb24bf3c
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-02-20 11:03:49 +0530

    remove unknown doctype from help_desk

--- erpnext/config/help_desk.py
+++ erpnext/config/help_desk.py
@@ -50,11 +50,6 @@ def get_data():
 				},
 				{
 					"type": "doctype",
-					"name": "Support Contract",
-					"description": _("Support Contract."),
-				},
-				{
-					"type": "doctype",
 					"name": "Service Level Agreement",
 					"description": _("Service Level Agreement."),
 				}
