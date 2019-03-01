cf1b98c150691d38e70f8ce3c6dd1bae10d8420b
Gaurav <gvrvnk@gmail.com>
2019-03-01 13:58:39 +0530

    fix(regional,italy): codacy fixes in regional sales_invoice js

--- erpnext/regional/italy/sales_invoice.js
+++ erpnext/regional/italy/sales_invoice.js
@@ -3,15 +3,15 @@ erpnext.setup_e_invoice_button = (doctype) => {
 		refresh: (frm) => {
 			if(frm.doc.docstatus == 1) {
 				frm.add_custom_button('Generate E-Invoice', () => {
-                    var w = window.open(
-                        frappe.urllib.get_full_url(
-                            "/api/method/erpnext.regional.italy.utils.generate_single_invoice?"
-                            + "docname=" + frm.doc.name
-                        )
-                    )
-                    if (!w) {
-                        frappe.msgprint(__("Please enable pop-ups")); return;
-                    }
+					var w = window.open(
+						frappe.urllib.get_full_url(
+							"/api/method/erpnext.regional.italy.utils.generate_single_invoice?"
+							+ "docname=" + frm.doc.name
+						)
+					)
+					if (!w) {
+						frappe.msgprint(__("Please enable pop-ups")); return;
+					}
 				});
 			}
 		}
