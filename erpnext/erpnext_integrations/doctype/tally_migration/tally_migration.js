0d20fd1a40e5f7daae8bb5e2a1c49a65be7b7e85
Aditya Hase <aditya@adityahase.com>
2019-03-01 15:34:47 +0530

    feat(tally): Preprocess master data to generate COA

--- erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.js
+++ erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.js
@@ -3,6 +3,35 @@
 
 frappe.ui.form.on('Tally Migration', {
 	refresh: function(frm) {
-
-	}
+		if (frm.doc.master_data && frm.doc.day_book) {
+			frm.disable_save();
+			if(frm.doc.status != "In Progress") {
+				frm.page.set_primary_action("Preprocess", () => frm.trigger("preprocess"));
+			}
+		} else {
+			frm.set_value("status", "Attach File");
+		}
+		if (frm.doc.tally_company && frm.doc.erpnext_company) {
+			frm.set_df_property("company_section", "hidden", 0);
+			frm.page.set_primary_action("Start Import", () => frm.trigger("start_import"));
+		}
+	},
+	preprocess: function(frm) {
+		frm.call({
+			doc: frm.doc,
+			method: "preprocess",
+			freeze: true
+		}).then((r) => {
+			frm.set_value("status", "Preprocessing In Progress");
+		});
+	},
+	start_import: function(frm) {
+		frm.call({
+			doc: frm.doc,
+			method: "start_import",
+			freeze: true
+		}).then((r) => {
+			frm.set_value("status", "Import In Progress");
+		});
+	},
 });
