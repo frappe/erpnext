1de50468f2ecc508048ecf2d212c914d4cebeba6
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-03-02 09:17:27 +0530

    fix: Update patch to set GST Category

--- erpnext/patches/v12_0/set_gst_category.py
+++ erpnext/patches/v12_0/set_gst_category.py
@@ -1,29 +1,13 @@
 import frappe
 from erpnext.regional.india.setup import make_custom_fields
-from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
 
-	custom_fields = {
-		'Sales Invoice': [
-			{
-				'fieldname': 'gst_category',
-				'label': 'GST Category',
-				'fieldtype': 'Data',
-				'insert_after': 'invoice_copy',
-			}
-		],
-		'Purchase Invoice': [
-			{
-				'fieldname': 'gst_category',
-				'label': 'GST Category',
-				'fieldtype': 'Data',
-				'insert_after': 'invoice_copy',
-			}
-		]
-	}
+	company = frappe.get_all('Company', filters = {'country': 'India'})
+	if not company:
+		return
 
-	create_custom_fields(custom_fields, ignore_validate = frappe.flags.in_patch, update=True)
+	make_custom_fields()
 
 	for doctype in ['Sales Invoice', 'Purchase Invoice']:
 		has_column = frappe.db.has_column(doctype,'invoice_type')
@@ -31,11 +15,13 @@ def execute():
 		if has_column:
 			update_map = {
 				'Regular': 'Registered Regular',
-				'Export': 'Overseas'
+				'Export': 'Overseas',
+				'SEZ': 'SEZ',
+				'Deemed Export': 'Deemed Export',
 			}
 
 			for old, new in update_map.items():
-				frappe.db.set_value(doctype, { 'invoice_type': old }, 'gst_category', new)
+				frappe.db.sql("UPDATE `tab{doctype}` SET gst_category = %s where invoice_type = %s".format(doctype=doctype), (old, new))
 
 	frappe.delete_doc('Custom Field', 'Sales Invoice-invoice_type')
 	frappe.delete_doc('Custom Field', 'Purchase Invoice-invoice_type')
@@ -53,8 +39,5 @@ def execute():
 		for old, new in itc_update_map.items():
 			frappe.db.sql("UPDATE `tabPurchase Invoice` SET eligibility_for_itc = replace(eligibility_for_itc, %s, %s)", (old, new))
 
-	company = frappe.get_all('Company', filters = {'country': 'India'})
-	if not company:
-		return
 
-	make_custom_fields()
+
