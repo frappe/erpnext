29766047a3f0df0bc15fa135bba18b8e1db4080d
Saif Ur Rehman <saifi0102@gmail.com>
2019-03-02 09:44:09 +0500

    saif: patch for Stock Entry alt_uom_qty

---
+++ erpnext/patches/saif/set_stock_entry_alt_uom_qty.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2017, Frappe and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	frappe.reload_doc('stock', 'doctype', 'stock_entry_detail')
+
+	frappe.db.sql("""
+		update tabItem
+		set alt_uom_size = 1
+		where ifnull(alt_uom, '') = ''
+	""")
+
+	frappe.db.sql("""
+		update `tabStock Entry Detail` d
+		inner join tabItem i on i.name = d.item_code
+		set
+			d.alt_uom = i.alt_uom,
+			d.alt_uom_size = i.alt_uom_size,
+			d.alt_uom_qty = d.transfer_qty * i.alt_uom_size
+	""")
