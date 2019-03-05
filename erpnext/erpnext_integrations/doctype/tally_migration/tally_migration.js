0ece8ad62d506d71821225314cb7000b5d5263c2
Aditya Hase <aditya@adityahase.com>
2019-03-05 19:16:00 +0530

    refactor: Major changes

--- erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.js
+++ erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.js
@@ -2,36 +2,49 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Tally Migration', {
-	refresh: function(frm) {
-		if (frm.doc.master_data && frm.doc.day_book) {
-			frm.disable_save();
-			if(frm.doc.status != "In Progress") {
-				frm.page.set_primary_action("Preprocess", () => frm.trigger("preprocess"));
+	onload: function(frm) {
+		frappe.realtime.on("tally_migration_progress_update", function (data) {
+			frm.dashboard.show_progress(data.title, (data.count / data.total) * 100, data.message);
+			if (data.count == data.total) {
+				window.setTimeout(title => frm.dashboard.hide_progress(title), 1500, data.title);
 			}
-		} else {
-			frm.set_value("status", "Attach File");
-		}
-		if (frm.doc.tally_company && frm.doc.erpnext_company) {
-			frm.set_df_property("company_section", "hidden", 0);
-			frm.page.set_primary_action("Start Import", () => frm.trigger("start_import"));
-		}
-	},
-	preprocess: function(frm) {
-		frm.call({
-			doc: frm.doc,
-			method: "preprocess",
-			freeze: true
-		}).then((r) => {
-			frm.set_value("status", "Preprocessing In Progress");
 		});
 	},
-	start_import: function(frm) {
-		frm.call({
-			doc: frm.doc,
-			method: "start_import",
-			freeze: true
-		}).then((r) => {
-			frm.set_value("status", "Import In Progress");
-		});
+	refresh: function(frm) {
+		if (frm.doc.master_data && !frm.doc.is_master_data_imported) {
+			if (frm.doc.is_master_data_processed) {
+				if (frm.doc.status != "Importing Master Data") {
+					frm.events.add_button(frm, __("Import Master Data"), "import_master_data");
+				}
+			} else {
+				if (frm.doc.status != "Processing Master Data") {
+					frm.events.add_button(frm, __("Process Master Data"), "process_master_data");
+				}
+			}
+		}
+		if (frm.doc.day_book_data && !frm.doc.is_day_book_data_imported) {
+			if (frm.doc.is_day_book_data_processed) {
+				if (frm.doc.status != "Importing Day Book Data") {
+					frm.events.add_button(frm, __("Import Day Book Data"), "import_day_book_data");
+				}
+			} else {
+				if (frm.doc.status != "Processing Day Book Data") {
+					frm.events.add_button(frm, __("Process Day Book Data"), "process_day_book_data");
+				}
+			}
+		}
 	},
+	add_button: function(frm, label, method) {
+		frm.add_custom_button(
+			label,
+			() => frm.call({
+				doc: frm.doc,
+				method: method,
+				freeze: true,
+				callback: () => {
+					frm.remove_custom_button(label);
+				}
+			})
+		);
+	}
 });
