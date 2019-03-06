c054bbf92eca97a588d6915e216f066e9876c1bc
farqalit nasir <nasirfarqalit@gmail.com>
2019-03-06 16:18:58 +0500

    For Attendance Gantt Change the field map
    For Attendance Gantt Change the field map

--- erpnext/hr/doctype/attendance/attendance_calendar.js
+++ erpnext/hr/doctype/attendance/attendance_calendar.js
@@ -16,3 +16,20 @@ frappe.views.calendar["Attendance"] = {
 	},
 	get_events_method: "erpnext.hr.doctype.attendance.attendance.get_events"
 };
+
+frappe.views.calendar["AttendanceGantt"] = {
+    field_map: {
+        "start": "attendance_date",
+        "end": "attendance_date",
+        "id": "name",
+        "docstatus": 1
+    },
+    options: {
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month'
+        }
+    },
+    get_events_method: "erpnext.hr.doctype.attendance.attendance.get_events"
+};
