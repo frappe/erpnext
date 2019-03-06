193e33884b7baf33ef0266c0a8a18be69142c0cd
scmmishra <scm.mymail@gmail.com>
2019-03-06 15:19:02 +0530

    feat: Minor changes to topic tests

--- erpnext/education/doctype/topic/test_topic.py
+++ erpnext/education/doctype/topic/test_topic.py
@@ -12,18 +12,20 @@ class TestTopic(unittest.TestCase):
 
 	def test_get_contents(self):
 		topic = frappe.get_doc("Topic", "_Test Topic 1")
-		self.assertEqual(topic.name, "_Test Topic 1")
 		contents = topic.get_contents()
 		self.assertEqual(contents[0].doctype, "Article")
 		self.assertEqual(contents[0].name, "_Test Article 1")
 		frappe.db.rollback()
 
 def make_topic(name):
-	topic = frappe.get_doc({
-		"doctype": "Topic",
-		"topic_name": name,
-		"topic_code": name,
-	}).insert()
+	try:
+		topic = frappe.get_doc("Topic", name)
+	except frappe.DoesNotExistError:
+		topic = frappe.get_doc({
+			"doctype": "Topic",
+			"topic_name": name,
+			"topic_code": name,
+		}).insert()
 	return topic.name
 
 def make_topic_and_linked_content(topic_name, content_dict_list):
