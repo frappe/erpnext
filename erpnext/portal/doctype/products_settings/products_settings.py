46254c0e22f1612c2dbf111fefe708045fefe78f
Faris Ansari <netchamp.faris@gmail.com>
2019-03-08 12:38:49 +0530

    fix: Filter fields
    - Validate filter fields
    - Load valid fields in select

--- erpnext/portal/doctype/products_settings/products_settings.py
+++ erpnext/portal/doctype/products_settings/products_settings.py
@@ -15,9 +15,10 @@ class ProductsSettings(Document):
 			website_settings.home_page = 'products'
 			website_settings.save()
 
-		self.validate_website_filters()
+		self.validate_field_filters()
+		self.validate_attribute_filters()
 
-	def validate_website_filters(self):
+	def validate_field_filters(self):
 		if not (self.enable_field_filters and self.filter_fields): return
 
 		item_meta = frappe.get_meta('Item')
@@ -26,6 +27,13 @@ class ProductsSettings(Document):
 		for f in self.filter_fields:
 			if f.fieldname not in valid_fields:
 				frappe.throw(_('Filter Fields Row #{0}: Fieldname <b>{1}</b> must be of type "Link" or "Table MultiSelect"').format(f.idx, f.fieldname))
+
+	def validate_attribute_filters(self):
+		if not (self.enable_attribute_filters and self.filter_attributes): return
+
+		# if attribute filters are enabled, hide_variants should be disabled
+		self.hide_variants = 0
+
 
 def home_page_is_products(doc, method):
 	'''Called on saving Website Settings'''
