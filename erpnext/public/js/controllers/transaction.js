eac87b4764451816158779da2bad3d549218b1ce
Rohan Bansal <rohan@digithinkit.com>
2019-03-11 14:48:26 +0530

    fix(transaction): Avoid shipping rule charge override by tax template

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -1286,7 +1286,9 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 				},
 				callback: function(r) {
 					if(!r.exc) {
-						me.frm.set_value("taxes", r.message);
+						for (let tax of r.message) {
+							me.frm.add_child("taxes", tax);
+						}
 						me.calculate_taxes_and_totals();
 					}
 				}
