80dee900418639fca82dc78a90cba3d5218f1263
Anurag Mishra <mishranaman123@gmail.com>
2019-03-12 11:00:02 +0530

    Added mandatory feilds to the report

--- erpnext/projects/report/project_billing_summary/project_billing_summary.js
+++ erpnext/projects/report/project_billing_summary/project_billing_summary.js
@@ -9,18 +9,21 @@ frappe.query_reports["Project Billing Summary"] = {
 			label: __("Project"),
 			fieldtype: "Link",
 			options: "Project",
+			reqd: 1
 		},
 		{
 			fieldname:"from_date",
 			label: __("From Date"),
 			fieldtype: "Date",
-			default: frappe.datetime.get_today()
+			default: frappe.datetime.get_today(),
+			reqd: 1
 		},
 		{
 			fieldname:"to_date",
 			label: __("To Date"),
 			fieldtype: "Date",
-			default: frappe.datetime.add_days(frappe.datetime.get_today(), 30)
+			default: frappe.datetime.add_days(frappe.datetime.get_today(), 30),
+			reqd: 1
 		},
 	]
 }
