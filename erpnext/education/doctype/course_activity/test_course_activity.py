e3b7e1381d0fc3b3722bcd60f23981a834c869fe
scmmishra <scm.mymail@gmail.com>
2019-03-14 15:34:13 +0530

    feat: Moved make_course_activity to test_course_activity.py

--- erpnext/education/doctype/course_activity/test_course_activity.py
+++ erpnext/education/doctype/course_activity/test_course_activity.py
@@ -9,3 +9,16 @@ import unittest
 class TestCourseActivity(unittest.TestCase):
 	pass
 
+def make_course_activity(enrollment, content_type, content):
+	activity = frappe.get_all("Course Activity", filters={'enrollment': enrollment, 'content_type': content_type, 'content': content})
+	try:
+		activity = frappe.get_doc("Course Activity", activity[0]['name'])
+	except (IndexError, frappe.DoesNotExistError):
+		activity = frappe.get_doc({
+			"doctype": "Course Activity",
+			"enrollment": enrollment,
+			"content_type": content_type,
+			"content": content,
+			"activity_date": frappe.utils.datetime.datetime.now()
+		}).insert()
+	return activity
