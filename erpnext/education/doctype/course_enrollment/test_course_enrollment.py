fe94cdbac4ccf2547df3f7517da1a69ee5e2f2ec
scmmishra <scm.mymail@gmail.com>
2019-03-14 15:32:22 +0530

    feat: Moved setup_program to test_program.py

--- erpnext/education/doctype/course_enrollment/test_course_enrollment.py
+++ erpnext/education/doctype/course_enrollment/test_course_enrollment.py
@@ -6,43 +6,11 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
-from erpnext.education.doctype.topic.test_topic import make_topic_and_linked_content
-from erpnext.education.doctype.course.test_course import make_course_and_linked_topic
-from erpnext.education.doctype.program.test_program import make_program_and_linked_courses
 
 from erpnext.education.doctype.student.test_student import create_student
 from erpnext.education.doctype.student.test_student import get_student
 
-test_data = frappe._dict({
-	"program_name": "_Test Program",
-	"course": [{
-		"course_name": "_Test Course 1",
-		"topic": [
-		{
-			"topic_name": "_Test Topic 1-1",
-			"content": [{
-				"type": "Article",
-				"name": "_Test Article 1-1"
-			},{
-				"type": "Article",
-				"name": "_Test Article 1-2"
-			}
-			]
-		},
-		{
-			"topic_name": "_Test Topic 1-2",
-			"content": [{
-				"type": "Article",
-				"name": "_Test Article 1-3"
-			},{
-				"type": "Article",
-				"name": "_Test Article 1-4"
-			}
-			]
-		}
-		]
-	}]
-})
+from erpnext.education.doctype.program.test_program import setup_program
 
 class TestCourseEnrollment(unittest.TestCase):
 	def setUp(self):
@@ -60,6 +28,7 @@ class TestCourseEnrollment(unittest.TestCase):
 		progress = course_enrollment.get_progress(student)
 		finished = {'content': '_Test Article 1-1', 'content_type': 'Article', 'is_complete': True}
 		self.assertTrue(finished in progress)
+		frappe.db.rollback()
 
 
 def make_course_activity(enrollment, content_type, content):
@@ -75,16 +44,3 @@ def make_course_activity(enrollment, content_type, content):
 			"activity_date": frappe.utils.datetime.datetime.now()
 		}).insert()
 	return activity
-
-def setup_program():
-	topic_list = [course['topic'] for course in test_data['course']]
-	for topic in topic_list[0]:
-		make_topic_and_linked_content(topic['topic_name'], topic['content'])
-
-	all_courses_list = [{'course': course['course_name'], 'topic': [topic['topic_name'] for topic in course['topic']]} for course in test_data['course']] # returns [{'course': 'Applied Math', 'topic': ['Trignometry', 'Geometry']}]
-	for course in all_courses_list:
-		make_course_and_linked_topic(course['course'], course['topic'])
-
-	course_list = [course['course_name'] for course in test_data['course']]
-	program = make_program_and_linked_courses(test_data.program_name, course_list)
-	return program
