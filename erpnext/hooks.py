e14e8aba2c631033b0e4128f398737875f3ccb86
Faris Ansari <netchamp.faris@gmail.com>
2019-03-20 18:31:46 +0530

    fix: Move Newsletter script from frappe to erpnext

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -23,7 +23,8 @@ web_include_css = "assets/css/erpnext-web.css"
 doctype_js = {
 	"Communication": "public/js/communication.js",
 	"Event": "public/js/event.js",
-	"Website Theme": "public/js/website_theme.js"
+	"Website Theme": "public/js/website_theme.js",
+	"Newsletter": "public/js/newsletter.js"
 }
 
 welcome_email = "erpnext.setup.utils.welcome_email"
