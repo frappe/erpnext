4dc08723f3078487f58fd87299503589d122a7ac
Saif Ur Rehman <saifi0102@gmail.com>
2019-03-28 16:03:55 +0500

    feat(Letter of Credit): Update name dialog box

--- erpnext/accounts/doctype/letter_of_credit/letter_of_credit.js
+++ erpnext/accounts/doctype/letter_of_credit/letter_of_credit.js
@@ -3,6 +3,75 @@
 
 frappe.ui.form.on('Letter of Credit', {
 	refresh: function(frm) {
+		frm.set_df_property("naming_prefix", "read_only", !frm.is_new());
+		frm.set_df_property("letter_of_credit_number", "read_only", !frm.is_new());
+		frm.set_df_property("reference_text", "read_only", !frm.is_new());
 
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Update Name'), function () {
+				frm.trigger("update_name");
+			});
+		}
+	},
+
+	update_name: function(frm) {
+		var d = new frappe.ui.Dialog({
+			title: __('Update Name'),
+			fields: [
+				{
+					"label": "Naming Prefix",
+					"fieldname": "naming_prefix",
+					"fieldtype": "Data",
+					"default": frm.doc.naming_prefix
+				},
+				{
+					"label": "Letter of Credit Number",
+					"fieldname": "letter_of_credit_number",
+					"fieldtype": "Data",
+					"reqd": 1,
+					"default": frm.doc.letter_of_credit_number
+				},
+				{
+					"label": "Reference Text",
+					"fieldname": "reference_text",
+					"fieldtype": "Data",
+					"default": frm.doc.reference_text
+				},
+			],
+			primary_action: function() {
+				var data = d.get_values();
+				if(data.naming_prefix === frm.doc.naming_prefix
+					&& data.letter_of_credit_number === frm.doc.letter_of_credit_number
+					&& data.reference_text === frm.doc.reference_text)
+				{
+					d.hide();
+					return;
+				}
+
+				frappe.call({
+					method: "erpnext.accounts.doctype.letter_of_credit.letter_of_credit.update_name",
+					args: {
+						name: frm.doc.name,
+						naming_prefix: data.naming_prefix,
+						letter_of_credit_number: data.letter_of_credit_number,
+						reference_text: data.reference_text
+					},
+					callback: function(r) {
+						if(!r.exc) {
+							if(r.message) {
+								frappe.set_route("Form", "Letter of Credit", r.message);
+							} else {
+								frm.set_value("naming_prefix", data.naming_prefix);
+								frm.set_value("letter_of_credit_number", data.letter_of_credit_number);
+								frm.set_value("reference_text", data.reference_text);
+							}
+							d.hide();
+						}
+					}
+				});
+			},
+			primary_action_label: __('Update')
+		});
+		d.show();
 	}
 });
--- erpnext/accounts/doctype/letter_of_credit/letter_of_credit.py
+++ erpnext/accounts/doctype/letter_of_credit/letter_of_credit.py
@@ -8,14 +8,8 @@ from frappe.model.document import Document
 from frappe import _
 
 class LetterofCredit(Document):
-	def validate(self):
-		self.set_title()
-
-	def set_title(self):
-		if self.reference_text:
-			self.title = "{0}{1} - {2}".format(self.naming_prefix, self.letter_of_credit_number, self.reference_text)
-		else:
-			self.title = "{0}{1}".format(self.naming_prefix, self.letter_of_credit_number)
+	def autoname(self):
+		self.name = get_letter_of_credit_autoname(self.naming_prefix, self.letter_of_credit_number, self.reference_text)
 
 	def get_default_letter_of_credit_account(self):
 		default_letter_of_credit_account = frappe.get_cached_value('Company',
@@ -26,3 +20,25 @@ class LetterofCredit(Document):
 				.format(self.company))
 
 		return default_letter_of_credit_account
+
+def get_letter_of_credit_autoname(naming_prefix, letter_of_credit_number, reference_text=None):
+	if reference_text:
+		return "{0}{1} ({2})".format(naming_prefix, letter_of_credit_number, reference_text)
+	else:
+		return "{0}{1}".format(naming_prefix, letter_of_credit_number)
+
+@frappe.whitelist()
+def update_name(name, naming_prefix, letter_of_credit_number, reference_text):
+	if not frappe.db.exists("Letter of Credit", name):
+		return
+
+	frappe.db.set_value("Letter of Credit", name, {
+		"naming_prefix": naming_prefix,
+		"letter_of_credit_number": letter_of_credit_number,
+		"reference_text": reference_text
+	}, None)
+
+	new_name = get_letter_of_credit_autoname(naming_prefix, letter_of_credit_number, reference_text)
+	if name != new_name:
+		frappe.rename_doc("Letter of Credit", name, new_name, ignore_permissions=1)
+		return new_name
