64001757c4b0425f4c498a223525439b69b6d8a1
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-04-01 22:18:10 +0530

    fix: Quotation Query fix in lead.py

--- erpnext/crm/doctype/lead/lead.py
+++ erpnext/crm/doctype/lead/lead.py
@@ -94,7 +94,7 @@ class Lead(SellingController):
 
 	def has_quotation(self):
 		return frappe.db.get_value("Quotation", {
-			"lead": self.name,
+			"customer_lead": self.name,
 			"docstatus": 1,
 			"status": ["!=", "Lost"]
 
