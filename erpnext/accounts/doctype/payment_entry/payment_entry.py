6443d7dd109e1e2d38f43eaa4d50855226168003
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-04-04 19:54:23 +0530

    refactor: declare bill_on at function start

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -749,7 +749,7 @@ def get_outstanding_on_journal_entry(name):
 
 @frappe.whitelist()
 def get_reference_details(reference_doctype, reference_name, party_account_currency):
-	total_amount = outstanding_amount = exchange_rate = None
+	total_amount = outstanding_amount = exchange_rate = bill_no = None
 	ref_doc = frappe.get_doc(reference_doctype, reference_name)
 	company_currency = ref_doc.get("company_currency") or erpnext.get_company_currency(ref_doc.company)
 
