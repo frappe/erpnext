1deb1399a6b7bd8332858625c05a07599a953050
Saif Ur Rehman <saifi0102@gmail.com>
2019-04-05 02:17:33 +0500

    fix(Desktop Icon): Update

--- erpnext/config/desktop.py
+++ erpnext/config/desktop.py
@@ -29,20 +29,8 @@ def get_data():
 			"color": colors["Buying"], "icon": "octicon octicon-briefcase"},
 		{"module_name": "Letter of Credit", "_doctype": "Letter of Credit", "type": "list",
 			"color": colors["Buying"], "icon": "fa fa-university"},
-		{"module_name": "Employee", "_doctype": "Employee", "type": "list",
-			"color": colors["HR"], "icon": "octicon octicon-organization"},
 		{"module_name": "Account", "_doctype": "Account", "type": "link", "link": "Tree/Account", "label": _("Chart of Accounts"),
 			"color": colors["Accounts"], "icon": "fa fa-sitemap"},
-
-		{"module_name": "Territory", "_doctype": "Territory", "type": "link", "link": "Tree/Territory",
-			"color": colors["Selling"], "icon": "fa fa-flag"},
-		{"module_name": "Sales Person", "_doctype": "Sales Person", "type": "link", "link": "Tree/Sales Person",
-			"color": colors["Selling"], "icon": "fa fa-male"},
-
-		{"module_name": "Issue", "_doctype": "Issue", "type": "list",
-			"color": colors["Support"], "icon": "octicon octicon-issue-opened"},
-		{"module_name": "ToDo", "_doctype": "ToDo", "type": "list",
-			"color": colors["Tools"], "icon": "fa fa-check", "reverse": 1},
 
 		{"module_name": "Project", "_doctype": "Project", "type": "list",
 			"color": colors["Projects"], "icon": "octicon octicon-rocket"},
@@ -57,8 +45,6 @@ def get_data():
 			"color": colors["Stock"], "icon": "fa fa-truck"},
 		{"module_name": "Purchase Receipt", "_doctype": "Purchase Receipt", "type": "list",
 			"color": colors["Stock"], "icon": "fa fa-truck"},
-		{"module_name": "Landed Cost Voucher", "_doctype": "Landed Cost Voucher", "type": "list",
-			"color": colors["Stock"], "icon": "fa fa-plane"},
 		{"module_name": "Sales Invoice", "_doctype": "Sales Invoice", "type": "list",
 			"color": colors["Accounts"], "icon": "fa fa-file-text"},
 		{"module_name": "Purchase Invoice", "_doctype": "Purchase Invoice", "type": "list",
@@ -92,30 +78,25 @@ def get_data():
 		{"module_name": "Customer Credit Balance", "_report": "Customer Credit Balance", "type": "query-report", "link": "query-report/Customer Credit Balance",
 			"color": colors["Selling"], "icon": "fa fa-credit-card"},
 
-		{"module_name": "Sales Analytics", "_report": "Sales Analytics", "type": "query-report", "link": "query-report/Sales Analytics",
-			"color": colors["Selling"], "icon": "fa fa-line-chart"},
-		{"module_name": "Purchase Analytics", "_report": "Purchase Analytics", "type": "query-report", "link": "query-report/Purchase Analytics",
-			"color": colors["Buying"], "icon": "fa fa-line-chart"},
-
 		{"module_name": "Stock Ledger", "_report": "Stock Ledger", "type": "query-report", "link": "query-report/Stock Ledger",
 			"color": colors["Stock"], "icon": "fa fa-exchange"},
 		{"module_name": "Stock Balance", "_report": "Stock Balance", "type": "query-report", "link": "query-report/Stock Balance",
 			"color": colors["Stock"], "icon": "octicon octicon-package"},
 
-		{"module_name": "Gross Profit", "_report": "Gross Profit", "type": "query-report", "link": "query-report/Gross Profit",
-			"color": colors["Accounts"], "icon": "fa fa-usd"},
-		{"module_name": "Profit and Loss Statement", "_report": "Profit and Loss Statement", "type": "query-report", "link": "query-report/Profit and Loss Statement",
-			"color": colors["Accounts"], "icon": "fa fa-bar-chart"},
-		{"module_name": "Balance Sheet", "_report": "Balance Sheet", "type": "query-report", "link": "query-report/Balance Sheet",
-			"color": colors["Accounts"], "icon": "fa fa-bar-chart"},
+		{"module_name": "Sales Analytics", "_report": "Sales Analytics", "type": "query-report", "link": "query-report/Sales Analytics",
+			"color": colors["Selling"], "icon": "fa fa-line-chart"},
+		{"module_name": "Sales Details", "_report": "Sales Details", "type": "query-report", "link": "query-report/Sales Details",
+			"color": colors["Selling"], "icon": "fa fa-list"},
+
+		{"module_name": "Purchase Analytics", "_report": "Purchase Analytics", "type": "query-report", "link": "query-report/Purchase Analytics",
+			"color": colors["Buying"], "icon": "fa fa-line-chart"},
+		{"module_name": "Purchase Details", "_report": "Purchase Details", "type": "query-report", "link": "query-report/Purchase Details",
+			"color": colors["Buying"], "icon": "fa fa-list"},
+
 		{"module_name": "Trial Balance", "_report": "Trial Balance", "type": "query-report", "link": "query-report/Trial Balance",
 			"color": colors["Accounts"], "icon": "fa fa-balance-scale"},
 		{"module_name": "Trial Balance for Party", "_report": "Trial Balance for Party", "type": "query-report", "link": "query-report/Trial Balance for Party",
 			"color": colors["Accounts"], "icon": "fa fa-balance-scale"},
-		{"module_name": "Trial Balance (Simple)", "_report": "Trial Balance (Simple)", "type": "query-report", "link": "query-report/Trial Balance (Simple)",
-			"color": colors["Accounts"], "icon": "fa fa-exchange"},
-		{"module_name": "Sales Payment Summary", "_report": "Sales Payment Summary", "type": "query-report", "link": "query-report/Sales Payment Summary",
-			"color": colors["Accounts"], "icon": "fa fa-exchange"},
 
 		# old
 		{
