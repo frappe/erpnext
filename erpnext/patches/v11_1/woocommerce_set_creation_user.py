307e6a8825c460439f6641e4b94f46bcfcc5a080
karthikeyan5 <skarthikeyan1410@gmail.com>
2019-04-09 15:20:24 +0530

    fix(woocommerce integration): defaults in settings
    (cherry picked from commit f788117b3ea9be83c54e169a1535ded0722f229c)

---
+++ erpnext/patches/v11_1/woocommerce_set_creation_user.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	woocommerce_settings = frappe.get_doc("Woocommerce Settings")
+	if woocommerce_settings.enable_sync:
+		woocommerce_settings.creation_user = woocommerce_settings.modified_by
+		woocommerce_settings.save()
