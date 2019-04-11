73a24ee8f9a17ce50d3317f50475c19f4be025fc
Rohan Bansal <rohan@digithinkit.com>
2019-04-11 15:03:53 +0530

    fix(patch): Reload Batch doctype before update query

--- erpnext/patches/v12_0/set_total_batch_quantity.py
+++ erpnext/patches/v12_0/set_total_batch_quantity.py
@@ -2,6 +2,8 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("stock", "doctype", "batch")
+
 	for batch in frappe.get_all("Batch", fields=["name", "batch_id"]):
 		batch_qty = frappe.db.get_value("Stock Ledger Entry", {"docstatus": 1, "batch_no": batch.batch_id}, "sum(actual_qty)")
 		frappe.db.set_value("Batch", batch.name, "batch_qty", batch_qty, update_modified=False)
