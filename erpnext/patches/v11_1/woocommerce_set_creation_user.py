63f8cda069a1421019175cfa17301e3c226767a5
karthikeyan5 <skarthikeyan1410@gmail.com>
2019-04-11 16:32:37 +0530

    fix(woocommerce integration): possible travis fix
    possible fix for travis patch error "Error: No module named woocommerce_settings.woocommerce_settings)"
    
    (cherry picked from commit a0b7ff60b8c3303a405c98eac11e71cd556ba166)

--- erpnext/patches/v11_1/woocommerce_set_creation_user.py
+++ erpnext/patches/v11_1/woocommerce_set_creation_user.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	woocommerce_settings = frappe.get_doc("Woocommerce Settings")
+	woocommerce_settings = frappe.get_single("Woocommerce Settings")
 	if woocommerce_settings.enable_sync:
 		woocommerce_settings.creation_user = woocommerce_settings.modified_by
 		woocommerce_settings.save()
