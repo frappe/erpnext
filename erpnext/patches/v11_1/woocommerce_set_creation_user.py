eb0b229eed676866937f9fa4f87ab6946de0c4f6
karthikeyan5 <skarthikeyan1410@gmail.com>
2019-04-17 19:25:10 +0530

    fix(woocommerce): fix patch failure on some sites
    > ORM cannot be used as an error occurs only on Travis.
    https://travis-ci.com/frappe/erpnext/jobs/193285704

--- erpnext/patches/v11_1/woocommerce_set_creation_user.py
+++ erpnext/patches/v11_1/woocommerce_set_creation_user.py
@@ -2,9 +2,18 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	woocommerce_setting_enable_sync = frappe.db.sql("SELECT t.value FROM tabSingles t WHERE doctype = 'Woocommerce Settings' AND field = 'enable_sync'",  as_dict=True)
+	woocommerce_setting_enable_sync = frappe.db.sql("""SELECT t.value
+		FROM tabSingles t
+		WHERE doctype = 'Woocommerce Settings'
+		AND field = 'enable_sync'""", as_dict=True)
 	if len(woocommerce_setting_enable_sync) and woocommerce_setting_enable_sync[0].value == '1':
-		frappe.db.sql("""UPDATE tabSingles
-					SET value = (SELECT t.value FROM tabSingles t WHERE doctype = 'Woocommerce Settings' AND field = 'modified_by')
-					WHERE doctype = 'Woocommerce Settings'
-					AND field = 'creation_user';""")
+		modified_by = frappe.db.sql("""SELECT t.value
+			FROM tabSingles t
+			WHERE doctype = 'Woocommerce Settings'
+			AND field = 'modified_by'""")
+		if len(modified_by):
+			modified_by = modified_by[0][0]
+			frappe.db.sql("""UPDATE tabSingles
+				SET value = %s
+				WHERE doctype = 'Woocommerce Settings'
+				AND field = 'creation_user';""", (modified_by))
