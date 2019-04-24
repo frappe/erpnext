3ffa40e634793cceb8fbf937ccee753d0d651f5e
Valmik Jangla <mail@valmik.in>
2019-04-24 16:11:35 +0530

    fix(task): temporarily disable scheduler job for task

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -288,7 +288,6 @@ scheduler_events = {
 		"erpnext.controllers.accounts_controller.update_invoice_status",
 		"erpnext.accounts.doctype.fiscal_year.fiscal_year.auto_create_fiscal_year",
 		"erpnext.hr.doctype.employee.employee.send_birthday_reminders",
-		"erpnext.projects.doctype.task.task.set_tasks_as_overdue",
 		"erpnext.assets.doctype.asset.depreciation.post_depreciation_entries",
 		"erpnext.hr.doctype.daily_work_summary_group.daily_work_summary_group.send_summary",
 		"erpnext.stock.doctype.serial_no.serial_no.update_maintenance_status",
