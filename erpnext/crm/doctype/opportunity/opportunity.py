765c292c62e34d4791fc67053a9463cf202d5987
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-04-25 19:19:01 +0530

    fix: Remove method from validation

--- erpnext/crm/doctype/opportunity/opportunity.py
+++ erpnext/crm/doctype/opportunity/opportunity.py
@@ -32,7 +32,6 @@ class Opportunity(TransactionBase):
 
 		self.validate_item_details()
 		self.validate_uom_is_integer("uom", "qty")
-		self.validate_lead_cust()
 		self.validate_cust_name()
 
 		if not self.title:
