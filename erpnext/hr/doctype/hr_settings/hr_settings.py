e01da0089223bde5475b2d6987bae06660fea85c
Shivam Mishra <scm.mymail@gmail.com>
2019-04-25 17:25:01 +0530

    feat: make password policy mandatory if encrypt option is selected

--- erpnext/hr/doctype/hr_settings/hr_settings.py
+++ erpnext/hr/doctype/hr_settings/hr_settings.py
@@ -10,6 +10,15 @@ from frappe.model.document import Document
 
 class HRSettings(Document):
 	def validate(self):
+		self.set_naming_series()
+		self.validate_password_policy()
+
+	def set_naming_series(self):
 		from erpnext.setup.doctype.naming_series.naming_series import set_by_naming_series
 		set_by_naming_series("Employee", "employee_number",
 			self.get("emp_created_by")=="Naming Series", hide_name_field=True)
+
+	def validate_password_policy(self):
+		if self.encrypt_salary_slips_in_emails:
+			if not self.password_policy:
+				frappe.throw(_("Password policy for Salary Slips is not set"))
