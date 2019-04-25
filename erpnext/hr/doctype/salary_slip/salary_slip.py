cf6a1ae124d8444e627f7ed6b06e5db52211c5b6
Shivam Mishra <scm.mymail@gmail.com>
2019-04-25 18:25:41 +0530

    feat: added password logic to email_salary_slip

--- erpnext/hr/doctype/salary_slip/salary_slip.py
+++ erpnext/hr/doctype/salary_slip/salary_slip.py
@@ -520,13 +520,17 @@ class SalarySlip(TransactionBase):
 
 	def email_salary_slip(self):
 		receiver = frappe.db.get_value("Employee", self.employee, "prefered_email")
+		hr_settings = frappe.get_single("HR Settings")
+		password = None
+		if hr_settings.encrypt_salary_slips_in_emails:
+			password = generate_password_for_pdf(hr_settings.password_policy, self.employee)
 
 		if receiver:
 			email_args = {
 				"recipients": [receiver],
 				"message": _("Please see attachment"),
 				"subject": 'Salary Slip - from {0} to {1}'.format(self.start_date, self.end_date),
-				"attachments": [frappe.attach_print(self.doctype, self.name, file_name=self.name)],
+				"attachments": [frappe.attach_print(self.doctype, self.name, file_name=self.name, password=password)],
 				"reference_doctype": self.doctype,
 				"reference_name": self.name
 				}
@@ -812,3 +816,7 @@ def unlink_ref_doc_from_salary_slip(ref_no):
 		for ss in linked_ss:
 			ss_doc = frappe.get_doc("Salary Slip", ss)
 			frappe.db.set_value("Salary Slip", ss_doc.name, "journal_entry", "")
+
+def generate_password_for_pdf(policy_template, employee):
+	employee = frappe.get_doc("Employee", employee)
+	return policy_template.format(**employee.as_dict())
