96661ba847fd457e972e0d5eb298793cfb324ee5
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-04-28 17:11:02 +0530

    feat: add filters for multiple company operation

--- erpnext/buying/report/procurement_tracker/procurement_tracker.js
+++ erpnext/buying/report/procurement_tracker/procurement_tracker.js
@@ -4,6 +4,27 @@
 
 frappe.query_reports["Procurement Tracker"] = {
 	"filters": [
-
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1
+		},
+		{
+			fieldname: "cost_center",
+			label: __("Cost Center"),
+			fieldtype: "Link",
+			options: "Cost Center",
+			reqd: 1
+		},
+		{
+			fieldname: "project",
+			label: __("Project"),
+			fieldtype: "Link",
+			options: "Project",
+			reqd: 1
+		}
 	]
 }
