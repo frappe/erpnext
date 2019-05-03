f59cf4c18150924136f2823aa1ba18fcae6350c0
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-03 12:55:48 +0530

    patch: delete table if exists

--- erpnext/patches/v11_1/delete_scheduling_tool.py
+++ erpnext/patches/v11_1/delete_scheduling_tool.py
@@ -7,4 +7,6 @@ import frappe
 def execute():
 	if frappe.db.exists("DocType", "Scheduling Tool"):
 		frappe.db.sql("""DELETE FROM `tabDocType` where name = 'Scheduling Tool'""")
+
+	if frappe.db.table_exists("Scheduling Tool"):
 		frappe.db.sql("""DROP TABLE IF EXISTS `tabScheduling Tool`""")
