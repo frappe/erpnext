cf47a7a61da8b625c734f2d88c42af39f7401098
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-03 13:00:59 +0530

    fix: remve drop table

--- erpnext/patches/v11_1/delete_scheduling_tool.py
+++ erpnext/patches/v11_1/delete_scheduling_tool.py
@@ -7,6 +7,3 @@ import frappe
 def execute():
 	if frappe.db.exists("DocType", "Scheduling Tool"):
 		frappe.db.sql("""DELETE FROM `tabDocType` where name = 'Scheduling Tool'""")
-
-	if frappe.db.table_exists("Scheduling Tool"):
-		frappe.db.sql("""DROP TABLE IF EXISTS `tabScheduling Tool`""")
