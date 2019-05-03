4836279dcc82f5e5f582e54a803132fb6b7dfbb9
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-03 17:31:55 +0530

    use orm

--- erpnext/patches/v11_1/delete_scheduling_tool.py
+++ erpnext/patches/v11_1/delete_scheduling_tool.py
@@ -6,4 +6,4 @@ import frappe
 
 def execute():
 	if frappe.db.exists("DocType", "Scheduling Tool"):
-		frappe.db.sql("""DELETE FROM `tabDocType` where name = 'Scheduling Tool'""")
+		frappe.delete_doc("DocType", "Scheduling Tool", ignore_permissions=True)
