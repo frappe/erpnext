22c31fb54e7b4ffbd2d62a5300f115579a20ddd1
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-03 12:47:16 +0530

    fix: scheduling tool

---
+++ erpnext/patches/v12_0/delete_scheduling_tool.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	if frappe.db.exists("DocType", "Scheduling Tool"):
+		frappe.db.sql("""DELETE FROM `tabDocType` where name = 'Scheduling Tool'""")
+		frappe.db.sql("""DROP TABLE IF EXISTS `tabScheduling Tool`""")
