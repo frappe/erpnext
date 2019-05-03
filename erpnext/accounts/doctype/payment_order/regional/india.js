7515d39a2e61842ad9d797dc28057830a306bf5b
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-05-03 21:19:02 +0530

    feat: add generate txt file button for india region

---
+++ erpnext/accounts/doctype/payment_order/regional/india.js
@@ -0,0 +1,22 @@
+frappe.ui.form.on('Payment Order', {
+	refresh: function(frm) {
+        if (frm.doc.docstatus==1) {
+            frm.add_custom_button(__('Generate Text File'),
+				function() {
+					frm.trigger("generate_text_and_download_file");
+            });
+        }
+    },
+    generate_text_and_download_file: (frm) => {
+        return frappe.call({
+            method: "erpnext.regional.india.bank_remittance_txt.generate_report_and_get_url",
+            args: {
+                name: this.frm.name,
+            },
+            freeze: true,
+            callback: function(r) {
+                frm.refresh();
+            }
+        });
+    }
+});
