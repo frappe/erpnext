8132262b907f11bd38ae38d8b2f870e02d7bb525
Faris Ansari <netchamp.faris@gmail.com>
2019-05-03 13:57:20 +0530

    fix: Variant naming if optional attributes

--- erpnext/controllers/item_variant.py
+++ erpnext/controllers/item_variant.py
@@ -309,7 +309,7 @@ def make_variant_item_code(template_item_code, template_item_name, variant):
 			}, as_dict=True)
 
 		if not item_attribute:
-			return
+			continue
 			# frappe.throw(_('Invalid attribute {0} {1}').format(frappe.bold(attr.attribute),
 			# 	frappe.bold(attr.attribute_value)), title=_('Invalid Attribute'),
 			# 	exc=InvalidItemAttributeValueError)
