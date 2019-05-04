898ab61dd4f9f1f60a7dc10275471ed7799a79cf
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-04 23:16:02 +0530

    fix: Child table for priority

--- erpnext/support/doctype/issue/issue.py
+++ erpnext/support/doctype/issue/issue.py
@@ -126,25 +126,29 @@ class Issue(Document):
 		return replicated_issue.name
 
 	def before_insert(self):
-		self.set_response_and_resolution_time()
+		self.set_response_and_resolution_time(priority=self.priority)
 
-	def set_response_and_resolution_time(self):
-		service_level_agreement = get_active_service_level_agreement_for(self.customer)
+	def set_response_and_resolution_time(self, priority=None):
+		service_level_agreement = get_active_service_level_agreement_for(self.customer, priority)
 		if service_level_agreement:
 			self.service_level_agreement = service_level_agreement.name
-			self.priority = service_level_agreement.priority
-
-		if not self.service_level_agreement: return
+		else:
+			return
 
 		service_level = frappe.get_doc("Service Level", service_level_agreement.service_level)
+		priority = service_level.get_service_level_priority(priority)
+		priority.update({
+			"support_and_resolution": service_level.support_and_resolution,
+			"holiday_list": service_level.holiday_list
+		})
 
 		if not self.creation:
 			self.creation = now_datetime()
 
 		start_date_time = get_datetime(self.creation)
 
-		self.response_by = get_expected_time_for('response', service_level, start_date_time)
-		self.resolution_by = get_expected_time_for('resolution', service_level, start_date_time)
+		self.response_by = get_expected_time_for(parameter='response', service_level=priority, start_date_time=start_date_time)
+		self.resolution_by = get_expected_time_for(parameter='resolution', service_level=priority, start_date_time=start_date_time)
 
 def get_expected_time_for(parameter, service_level, start_date_time):
 	current_date_time = start_date_time
@@ -154,11 +158,11 @@ def get_expected_time_for(parameter, service_level, start_date_time):
 
 	# lets assume response time is in days by default
 	if parameter == 'response':
-		allotted_days = service_level.response_time
-		time_period = service_level.response_time_period
+		allotted_days = service_level.get("response_time")
+		time_period = service_level.get("response_time_period")
 	elif parameter == 'resolution':
-		allotted_days = service_level.resolution_time
-		time_period = service_level.resolution_time_period
+		allotted_days = service_level.get("resolution_time")
+		time_period = service_level.get("resolution_time_period")
 	else:
 		frappe.throw(_("{0} parameter is invalid".format(parameter)))
 
@@ -172,13 +176,13 @@ def get_expected_time_for(parameter, service_level, start_date_time):
 	expected_time_is_set = 1 if allotted_days == 0 and time_period in ['Day', 'Week'] else 0
 
 	support_days = {}
-	for service in service_level.support_and_resolution:
+	for service in service_level.get("support_and_resolution"):
 		support_days[service.workday] = frappe._dict({
 			'start_time': service.start_time,
 			'end_time': service.end_time,
 		})
 
-	holidays = get_holidays(service_level.holiday_list)
+	holidays = get_holidays(service_level.get("holiday_list"))
 	weekdays = get_weekdays()
 
 	while not expected_time_is_set:
@@ -248,13 +252,11 @@ def set_multiple_status(names, status):
 	for name in names:
 		set_status(name, status)
 
-
 @frappe.whitelist()
 def set_status(name, status):
 	st = frappe.get_doc("Issue", name)
 	st.status = status
 	st.save()
-
 
 def auto_close_tickets():
 	"""Auto-close replied support tickets after 7 days"""
@@ -295,6 +297,7 @@ def make_task(source_name, target_doc=None):
 			"doctype": "Task"
 		}
 	}, target_doc)
+
 @frappe.whitelist()
 def make_issue_from_communication(communication, ignore_communication_links=False):
 	""" raise a issue from email """
--- erpnext/support/doctype/service_level_agreement/service_level_agreement.py
+++ erpnext/support/doctype/service_level_agreement/service_level_agreement.py
@@ -9,21 +9,20 @@ from frappe import _
 
 class ServiceLevelAgreement(Document):
 
-	def before_insert(self):
-		if self.default_service_level_agreement:
-			doc = frappe.get_list("Service Level Agreement", filters=[{"default_service_level_agreement": "1"}])
-			if doc:
-				frappe.throw(_("A Default Service Level Agreement already exists."))
-
 	def validate(self):
-		if not frm.doc.customer and not frm.doc.default_service_level_agreement:
+		if not (self.customer or self.default_service_level_agreement):
 			frappe.throw(_("Select a Customer or set as Default Service Level Agreement."))
 
-		if not self.default_service_level_agreement:
+		if self.default_service_level_agreement:
+			if frappe.db.exists("Service Level Agreement", {"default_service_level_agreement": "1"}):
+				frappe.throw(_("A Default Service Level Agreement already exists."))
+		else:
 			if not (self.start_date and self.end_date):
 				frappe.throw(_("Enter Start and End Date for the Agreement."))
 			if self.start_date >= self.end_date:
 				frappe.throw(_("Start Date of Agreement can't be greater than or equal to End Date."))
+			if self.end_date < frappe.utils.getdate():
+				frappe.throw(_("End Date of Agreement can't be less than today."))
 
 def check_agreement_status():
 	service_level_agreements = frappe.get_list("Service Level Agreement", filters=[
@@ -37,12 +36,26 @@ def check_agreement_status():
 			service_level_agreement.agreement_status = "Expired"
 		service_level_agreement.save()
 
-def get_active_service_level_agreement_for(customer):
-	agreement = frappe.get_list("Service Level Agreement",
-		filters=[{"agreement_status": "Active"}],
-		or_filters=[{'customer': customer},{"default_service_level_agreement": "1"}],
-		fields=["name", "service_level", "holiday_list", "priority"],
-		order_by='customer DESC',
-		limit=1)
+@frappe.whitelist()
+def get_active_service_level_agreement_for(customer, priority):
+
+	agreement = frappe.db.sql("""
+			select `tabService Level Agreement`.name, `tabService Level Agreement`.service_level,
+			`tabService Level Agreement`.holiday_list
+			from `tabService Level Agreement`
+			inner join `tabService Level Priority`
+			on `tabService Level Agreement`.name=`tabService Level Priority`.parent where
+			(
+				`tabService Level Agreement`.customer='{0}' and
+				`tabService Level Agreement`.agreement_status='Active' and
+				`tabService Level Priority`.priority='{1}'
+			) or
+			(
+				`tabService Level Agreement`.default_service_level_agreement='1'
+			)
+			 limit 1
+		""".format(customer, priority), as_dict=True, debug=True)
+
+	print(agreement)
 
 	return agreement[0] if agreement else None
