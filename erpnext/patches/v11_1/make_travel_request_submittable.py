f05e347e8aa77f3a314093aef4f7e76d9aa58786
Kenneth Sequeira <33246109+kennethsequeira@users.noreply.github.com>
2019-05-04 23:44:32 +0530

    Update make_travel_request_submittable.py

--- erpnext/patches/v11_1/make_travel_request_submittable.py
+++ erpnext/patches/v11_1/make_travel_request_submittable.py
@@ -6,10 +6,10 @@ import frappe
 
 def execute():
     a = frappe.get_list("Travel Request", 
-                        filters={"created": ["<", frappe.utils.add_days(frappe.utils.datetime.datetime.now(), -30)]}) #Get Travel Requests which are older than 30 days 
+                        filters={"creation": ["<", frappe.utils.add_days(frappe.utils.datetime.datetime.now(), -30)]}) #Get Travel Requests which are older than 30 days 
     for travel_request in a:
         try:
             req = frappe.get_doc('Travel Request', travel_request)
             req.submit()
         except Exception:
-            print(travel_request)
+            pass
