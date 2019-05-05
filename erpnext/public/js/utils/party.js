259c565a422bf8f1fcda5b59a2e07cceb9501dae
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-05-05 11:17:57 +0530

    fix: Codacy fixes

--- erpnext/public/js/utils/party.js
+++ erpnext/public/js/utils/party.js
@@ -104,7 +104,7 @@ erpnext.utils.set_taxes_from_address = function(frm, triggered_from_field, billi
 
 	if(frappe.meta.get_docfield(frm.doc.doctype, "taxes")) {
 		if(!erpnext.utils.validate_mandatory(frm, "Lead/Customer/Supplier",
-			frm.doc.customer || frm.doc.supplier || frm.doc.lead || frm.doc.party_name , address_field)) {
+			frm.doc.customer || frm.doc.supplier || frm.doc.lead || frm.doc.party_name)) {
 			return;
 		}
 
