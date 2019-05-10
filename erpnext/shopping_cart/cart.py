81f2da883cdb8f14656118da572eeae592d02bae
Roland <rasos@alton.at>
2019-05-10 14:11:05 +0200

    Do not require address field in webshop
    Fixes https://github.com/frappe/erpnext/issues/17559

--- erpnext/shopping_cart/cart.py
+++ erpnext/shopping_cart/cart.py
@@ -54,8 +54,9 @@ def place_order():
 	cart_settings = frappe.db.get_value("Shopping Cart Settings", None,
 		["company", "allow_items_not_in_stock"], as_dict=1)
 	quotation.company = cart_settings.company
-	if not quotation.get("customer_address"):
-		throw(_("{0} is required").format(_(quotation.meta.get_label("customer_address"))))
+## Do not require an address mandatory for webshop prodcuts and services 
+##	if not quotation.get("customer_address"):
+##		throw(_("{0} is required").format(_(quotation.meta.get_label("customer_address"))))
 
 	quotation.flags.ignore_permissions = True
 	quotation.submit()
