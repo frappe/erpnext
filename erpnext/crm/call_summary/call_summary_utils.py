ef7330124e2f4c678d5b60b9242d2741faca309c
Suraj Shetty <surajshetty3416@gmail.com>
2019-05-16 13:39:50 +0530

    feat: Open modal in realtime for incoming call

--- erpnext/crm/call_summary/call_summary_utils.py
+++ erpnext/crm/call_summary/call_summary_utils.py
@@ -2,8 +2,10 @@ import frappe
 
 @frappe.whitelist()
 def get_contact_doc(phone_number):
-	contacts = frappe.get_all('Contact', filters={
-		'phone': phone_number
+	phone_number = phone_number[-10:]
+	contacts = frappe.get_all('Contact', or_filters={
+		'phone': ['like', '%{}%'.format(phone_number)],
+		'mobile_no': ['like', '%{}%'.format(phone_number)]
 	}, fields=['*'])
 
 	if contacts:
