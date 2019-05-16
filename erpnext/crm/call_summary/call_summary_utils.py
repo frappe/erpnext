d26d69bc23bac79a5786565c4669c4ce13a5e319
Suraj Shetty <surajshetty3416@gmail.com>
2019-05-16 23:55:35 +0530

    feat: Show contact on incoming call

--- erpnext/crm/call_summary/call_summary_utils.py
+++ erpnext/crm/call_summary/call_summary_utils.py
@@ -4,9 +4,15 @@ import frappe
 def get_contact_doc(phone_number):
 	phone_number = phone_number[-10:]
 	contacts = frappe.get_all('Contact', or_filters={
-		'phone': ['like', '%{}%'.format(phone_number)],
-		'mobile_no': ['like', '%{}%'.format(phone_number)]
+		'phone': ['like', '%{}'.format(phone_number)],
+		'mobile_no': ['like', '%{}'.format(phone_number)]
 	}, fields=['*'])
 
 	if contacts:
 		return contacts[0]
+
+@frappe.whitelist()
+def get_last_communication(phone_number, customer=None):
+	# find last communication through phone_number
+	# find last issues, opportunity, lead
+	pass
