2052ef4f37dfb44faf2b6b974f583b06698405ba
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-05-18 23:47:42 +0530

    fix: Ability to disable dimensions

--- erpnext/accounts/doctype/accounting_dimension/accounting_dimension.js
+++ erpnext/accounts/doctype/accounting_dimension/accounting_dimension.js
@@ -6,5 +6,11 @@ frappe.ui.form.on('Accounting Dimension', {
 	document_type: function(frm){
 		frm.set_value('label', frm.doc.document_type);
 		frm.set_value('fieldname', frappe.scrub(frm.doc.document_type))
-	}
+
+		frappe.db.get_value('Accounting Dimension', {'document_type': frm.doc.document_type}, 'document_type', (r) => {
+			if (r.document_type) {
+				frm.set_df_property('document_type', 'description', "Document type is already set as dimension");
+			}
+		});
+	},
 });
