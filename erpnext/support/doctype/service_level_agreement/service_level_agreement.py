82beb66f0ef543a9670c77be4ae5c5760e442f05
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-19 11:06:00 +0530

    feat: change sla on the fly

--- erpnext/support/doctype/service_level_agreement/service_level_agreement.py
+++ erpnext/support/doctype/service_level_agreement/service_level_agreement.py
@@ -10,20 +10,17 @@ from frappe import _
 class ServiceLevelAgreement(Document):
 
 	def validate(self):
-		if not (self.customer or self.default_service_level_agreement):
-			frappe.throw(_("Select a Customer or set as Default Service Level Agreement."))
-
 		if self.default_service_level_agreement:
 			if frappe.db.exists("Service Level Agreement", {"default_service_level_agreement": "1", "name": ["!=", self.name]}):
 				frappe.throw(_("A Default Service Level Agreement already exists."))
 		else:
-			if not (self.start_date and self.end_date):
+			if not (self.start_date and self.end_date and self.ignore_start_and_end_date):
 				frappe.throw(_("Enter Start and End Date for the Agreement."))
 
-			if self.start_date >= self.end_date:
+			if self.start_date >= self.end_date and not self.ignore_start_and_end_date:
 				frappe.throw(_("Start Date of Agreement can't be greater than or equal to End Date."))
 
-			if self.end_date < frappe.utils.getdate():
+			if self.end_date < frappe.utils.nowdate() and not self.ignore_start_and_end_date:
 				frappe.throw(_("End Date of Agreement can't be less than today."))
 
 def check_agreement_status():
@@ -38,14 +35,16 @@ def check_agreement_status():
 				"agreement_status", "Expired")
 
 @frappe.whitelist()
-def get_active_service_level_agreement_for(priority, customer=None):
+def get_active_service_level_agreement_for(priority, customer=None, service_level_agreement=None):
 
-	if customer and frappe.db.exists("Service Level Agreement", {"customer": customer}):
+	if customer and frappe.db.exists("Service Level Agreement", {"customer": customer}) and not service_level_agreement:
 		or_filter = {"customer": customer}
+	elif service_level_agreement:
+		or_filter = {"name": service_level_agreement}
 	else:
 		or_filter = {"default_service_level_agreement": 1}
 
 	agreement = frappe.get_list("Service Level Agreement", filters={"agreement_status": "Active"},
-		or_filters=or_filter, fields=["name", "service_level"])
+		or_filters=or_filter, fields=["name", "service_level", "customer"])
 
 	return agreement[0] if agreement else None
