d1f62d1c99165c160b377f932a02c421c655ac81
Saif Ur Rehman <saifi0102@gmail.com>
2019-05-20 21:07:20 +0500

    fix(General Ledger Report): Filter by Sales Person set in Customer master

--- erpnext/accounts/report/general_ledger/general_ledger.py
+++ erpnext/accounts/report/general_ledger/general_ledger.py
@@ -198,9 +198,7 @@ def get_conditions(filters):
 		lft, rgt = frappe.db.get_value("Sales Person", filters.get("sales_person"), ["lft", "rgt"])
 		conditions.append("""exists(select name from `tabSales Team` steam where
 			steam.sales_person in (select name from `tabSales Person` where lft >= {0} and rgt <= {1})
-			and ((steam.parent = voucher_no and steam.parenttype = voucher_type)
-				or (steam.parent = against_voucher and steam.parenttype = against_voucher_type)
-				or (steam.parent = party and steam.parenttype = 'Customer')))""".format(lft, rgt))
+			and steam.parent = party and steam.parenttype = party_type)""".format(lft, rgt))
 
 	company_finance_book = erpnext.get_default_finance_book(filters.get("company"))
 	if not filters.get("finance_book") or (filters.get("finance_book") == company_finance_book):
