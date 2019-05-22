00f682278bd688cf2a8b64ff7c56e25217fae917
Suraj Shetty <surajshetty3416@gmail.com>
2019-05-22 15:48:57 +0530

    fix: Make changes to fix call flow and popup trigger

--- erpnext/crm/doctype/utils.py
+++ erpnext/crm/doctype/utils.py
@@ -1,20 +1,25 @@
 import frappe
 
+@frappe.whitelist()
 def get_document_with_phone_number(number):
 	# finds contacts and leads
+	if not number: return
 	number = number[-10:]
 	number_filter = {
 		'phone': ['like', '%{}'.format(number)],
 		'mobile_no': ['like', '%{}'.format(number)]
 	}
-	contacts = frappe.get_all('Contact', or_filters=number_filter,
-		fields=['name'], limit=1)
+	contacts = frappe.get_all('Contact', or_filters=number_filter, limit=1)
 
 	if contacts:
 		return frappe.get_doc('Contact', contacts[0].name)
 
-	leads = frappe.get_all('Leads', or_filters=number_filter,
-		fields=['name'], limit=1)
+	leads = frappe.get_all('Lead', or_filters=number_filter, limit=1)
 
 	if leads:
 		return frappe.get_doc('Lead', leads[0].name)
+
+
+def get_customer_last_interaction(contact_doc):
+	#
+	pass
