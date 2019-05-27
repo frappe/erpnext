0261472ecd305ac405dd7604913cf2b359e7bbbd
Raffael Meyer <raffael@alyf.de>
2019-05-27 16:01:05 +0200

    add default filters

--- erpnext/regional/report/datev/datev.js
+++ erpnext/regional/report/datev/datev.js
@@ -5,18 +5,20 @@ frappe.query_reports["DATEV"] = {
 			"label": __("Company"),
 			"fieldtype": "Link",
 			"options": "Company",
-			"default": frappe.defaults.get_user_default("Company"),
+			"default": frappe.defaults.get_user_default("Company") || frappe.defaults.get_global_default("Company"),
 			"reqd": 1
 		},
 		{
 			"fieldname": "from_date",
 			"label": __("From Date"),
+			"default": frappe.datetime.month_start(),
 			"fieldtype": "Date",
 			"reqd": 1
 		},
 		{
 			"fieldname": "to_date",
 			"label": __("To Date"),
+			"default": frappe.datetime.now_date(),
 			"fieldtype": "Date",
 			"reqd": 1
 		}
