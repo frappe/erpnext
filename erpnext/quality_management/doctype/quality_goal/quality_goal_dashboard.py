346791e76705586efe2cca76a95dd1bce57cb777
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-29 00:05:58 +0530

    refactor: QMS refactor initial bringup

--- erpnext/quality_management/doctype/quality_goal/quality_goal_dashboard.py
+++ erpnext/quality_management/doctype/quality_goal/quality_goal_dashboard.py
@@ -1,16 +1,12 @@
 from frappe import _
 
 def get_data():
-    return {
-        'fieldname': 'goal',
-        'transactions': [
-            {
-                'label': _('Review'),
-                'items': ['Quality Review']
-            },
-            {
-               'label': _('Action'),
-                'items': ['Quality Action']
-            }
-        ]
-    }
+	return {
+		'fieldname': 'goal',
+		'transactions': [
+			{
+				'label': _('Review'),
+				'items': ['Quality Review']
+			}
+		]
+	}
--- erpnext/quality_management/doctype/quality_review/quality_review.js
+++ erpnext/quality_management/doctype/quality_review/quality_review.js
@@ -4,69 +4,25 @@
 frappe.ui.form.on('Quality Review', {
 	onload: function(frm){
 		frm.set_value("date", frappe.datetime.get_today());
-		$(".grid-add-row").hide();
-		if(frm.doc.measurable == "Yes"){
-			show_target_achieved_unit(frm);
-		}
-		else{
-			hide_target_achieved_unit(frm);
-		}
-		frm.refresh();
 	},
 	goal: function(frm) {
-		frm.fields_dict.values.grid.remove_all();
-		if (frm.doc.goal){
-			frappe.call({
-				"method": "frappe.client.get",
-				args: {
-					doctype: "Quality Goal",
-					name: frm.doc.goal
-				},
-				callback: function (data) {
-					for (var i = 0; i < data.message.objective.length; i++ ){
-						frm.add_child("values");
-						frm.fields_dict.values.get_value()[i].objective = data.message.objective[i].objective;
-						if(frm.doc.measurable == "Yes"){
-							if(i < 1){
-								show_target_achieved_unit(frm);
-							}
-							frm.fields_dict.values.get_value()[i].target = data.message.objective[i].target;
-							frm.fields_dict.values.get_value()[i].achieved = 0;
-							frm.fields_dict.values.get_value()[i].unit = data.message.objective[i].unit;
-						}
-						if(frm.doc.measurable == "No"){
-							if(i < 1){
-								hide_target_achieved_unit(frm);
-							}
-							frm.fields_dict.values.get_value()[i].yes_no = "No";
-						}
-					}
-					frm.refresh();
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Quality Goal",
+				name: frm.doc.goal
+			},
+			callback: function(data){
+				frm.fields_dict.reviews.grid.remove_all();
+				let objectives = data.message.objectives;
+				for (var i in objectives) {
+					frm.add_child("reviews");
+					frm.fields_dict.reviews.get_value()[i].objective = objectives[i].objective;
+					frm.fields_dict.reviews.get_value()[i].target = objectives[i].target;
+					frm.fields_dict.reviews.get_value()[i].uom = objectives[i].uom;
 				}
-			});
-		}
-		else{
-			frm.doc.procedure = '';
-			frm.doc.scope = '';
-			frm.doc.action = '';
-			frm.doc.measurable = '';
-			frm.refresh();
-		}
+				frm.refresh();
+			}
+		});
 	},
 });
-
-function show_target_achieved_unit(frm){
-	//	shows target, achieved and unit columns as the goal can be measured in numeric values
-	frm.fields_dict.values.grid.docfields[1].hidden = 0;
-	frm.fields_dict.values.grid.docfields[2].hidden = 0;
-	frm.fields_dict.values.grid.docfields[3].hidden = 0;
-	frm.fields_dict.values.grid.docfields[4].hidden = 1;
-}
-
-function hide_target_achieved_unit(frm){
-	//	hides target and unit columns as the goal cannot be measured in numeric values
-	frm.fields_dict.values.grid.docfields[1].hidden = 1;
-	frm.fields_dict.values.grid.docfields[2].hidden = 1;
-	frm.fields_dict.values.grid.docfields[3].hidden = 1;
-	frm.fields_dict.values.grid.docfields[4].hidden = 0;
-}
