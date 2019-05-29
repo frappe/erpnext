9d55559eeaad3da934273c9d9680126a0637d80d
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-05-29 10:36:58 +0530

    fix: Patch for renaming enquiry_from to opportunity_from

---
+++ erpnext/patches/v11_1/set_missing_opportunity_from.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+
+	frappe.reload_doctype("Opportunity")
+
+	if frappe.db.exists("Opportunity", "enquiry_from"):
+		frappe.db.sql(""" UPDATE `tabOpportunity` set opportunity_from = enquiry_from where opportunity_from IS NULL""")
+
+	if frappe.db.exists("Opportunity", "lead") and frappe.db.exists("Opportunity", "enquiry_from"):
+		frappe.db.sql(""" UPDATE `tabOpportunity` set party_name = lead
+			where enquiry_from = 'Lead' and party_name IS NULL and lead IS NOT NULL""")
+
+	if frappe.db.exists("Opportunity", "customer") and frappe.db.exists("Opportunity", "enquiry_from"):
+		frappe.db.sql(""" UPDATE `tabOpportunity` set party_name = customer
+			 enquiry_from = 'Customer' and party_name IS NULL and customer IS NOT NULL""")
