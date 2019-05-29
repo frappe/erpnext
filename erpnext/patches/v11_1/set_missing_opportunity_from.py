e589d8490045826792e74cb60831436c49d6a14a
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-05-29 10:58:52 +0530

    fix: Use has_column instead of exists

--- erpnext/patches/v11_1/set_missing_opportunity_from.py
+++ erpnext/patches/v11_1/set_missing_opportunity_from.py
@@ -4,15 +4,14 @@ import frappe
 def execute():
 
 	frappe.reload_doctype("Opportunity")
-
-	if frappe.db.exists("Opportunity", "enquiry_from"):
+	if frappe.db.has_column("Opportunity", "enquiry_from"):
 		frappe.db.sql(""" UPDATE `tabOpportunity` set opportunity_from = enquiry_from
 			where opportunity_from IS NULL and enquiry_from IS NOT NULL""")
 
-	if frappe.db.exists("Opportunity", "lead") and frappe.db.exists("Opportunity", "enquiry_from"):
+	if frappe.db.has_column("Opportunity", "lead") and frappe.db.has_column("Opportunity", "enquiry_from"):
 		frappe.db.sql(""" UPDATE `tabOpportunity` set party_name = lead
 			where enquiry_from = 'Lead' and party_name IS NULL and lead IS NOT NULL""")
 
-	if frappe.db.exists("Opportunity", "customer") and frappe.db.exists("Opportunity", "enquiry_from"):
+	if frappe.db.has_column("Opportunity", "customer") and frappe.db.has_column("Opportunity", "enquiry_from"):
 		frappe.db.sql(""" UPDATE `tabOpportunity` set party_name = customer
 			 where enquiry_from = 'Customer' and party_name IS NULL and customer IS NOT NULL""")
