b7874157f7e84ba70aae325b926208d2157632ee
Shivam Mishra <scm.mymail@gmail.com>
2019-05-29 15:15:59 +0530

    refactor: /lms redirects to home if lms is disbaled

--- erpnext/www/lms/index.py
+++ erpnext/www/lms/index.py
@@ -6,6 +6,9 @@ no_cache = 1
 
 def get_context(context):
 	context.education_settings = frappe.get_single("Education Settings")
+	if not context.education_settings.enable_lms:
+		frappe.local.flags.redirect_location = '/'
+		raise frappe.Redirect
 	context.featured_programs = get_featured_programs()
 
 
