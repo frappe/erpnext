8fc4a0331c7b08f302f8be0bbaf6ad6fc3dd61dc
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-05-30 11:53:12 +0530

    fix: Dashboards for doctypes in accounting modules

---
+++ erpnext/accounts/doctype/purchase_taxes_and_charges_template/purchase_taxes_and_charges_template_dashboard.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+
+from frappe import _
+
+
+def get_data():
+	return {
+		'fieldname': 'taxes_and_charges',
+		'non_standard_fieldnames': {
+			'Tax Rule': 'purchase_tax_template',
+		},
+		'transactions': [
+			{
+				'label': _('Transactions'),
+				'items': ['Purchase Invoice', 'Purchase Order', 'Purchase Receipt']
+			},
+			{
+				'label': _('Supplier Quotations'),
+				'items': ['Supplier Quotation']
+			},
+			{
+				'label': _('Tax Rules'),
+				'items': ['Tax Rule']
+			}
+		]
+	}
