8d198dc59022bc5c2bef30433c2590793659615d
Prasann Shah <prasann@shipmnts.com>
2019-06-03 17:15:06 +0530

    [Add] GSTIN filter in HSN summary report

--- erpnext/regional/report/hsn_wise_summary_of_outward_supplies/hsn_wise_summary_of_outward_supplies.js
+++ erpnext/regional/report/hsn_wise_summary_of_outward_supplies/hsn_wise_summary_of_outward_supplies.js
@@ -2,6 +2,28 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
+function fetch_gstins(report) {
+	var company_gstins = report.get_filter('company_gstin');
+	var company = report.get_filter_value('company');
+	if (company) {
+		frappe.call({
+			method:'erpnext.regional.india.utils.get_gstins_for_company',
+			async: false,
+			args: {
+				company: company
+			},
+			callback: function(r) {
+				r.message.unshift("");
+				company_gstins.df.options = r.message;
+				company_gstins.refresh();
+			}
+		});
+	} else {
+		company_gstins.df.options = [""];
+		company_gstins.refresh();
+	}
+}
+
 frappe.query_reports["HSN-wise-summary of outward supplies"] = {
 	"filters": [
 		{
@@ -10,7 +32,8 @@ frappe.query_reports["HSN-wise-summary of outward supplies"] = {
 			"fieldtype": "Link",
 			"options": "Company",
 			"reqd": 1,
-			"default": frappe.defaults.get_user_default("Company")
+			"default": frappe.defaults.get_user_default("Company"),
+			"on_change": fetch_gstins
 		},
 		{
 			"fieldname":"gst_hsn_code",
@@ -18,6 +41,17 @@ frappe.query_reports["HSN-wise-summary of outward supplies"] = {
 			"fieldtype": "Link",
 			"options": "GST HSN Code",
 			"width": "80"
+		},
+		{
+			"fieldname":"company_gstin",
+			"label": __("Company GSTIN"),
+			"fieldtype": "Select",
+			"placeholder":"Company GSTIN",
+			"options": [""],
+			"width": "80"
 		}
-	]
-}
+	],
+	onload: (report) => {
+		fetch_gstins(report);
+	}
+};
