76cc19ca6d48bf376a841cd629b70962ac2f4300
Nabin Hait <nabinhait@gmail.com>
2019-06-05 14:22:41 +0530

    fix: Handled ifnull

--- erpnext/patches/v11_1/set_missing_opportunity_from.py
+++ erpnext/patches/v11_1/set_missing_opportunity_from.py
@@ -6,12 +6,12 @@ def execute():
 	frappe.reload_doctype("Opportunity")
 	if frappe.db.has_column("Opportunity", "enquiry_from"):
 		frappe.db.sql(""" UPDATE `tabOpportunity` set opportunity_from = enquiry_from
-			where opportunity_from IS NULL and enquiry_from IS NOT NULL""")
+			where ifnull(opportunity_from, '') = '' and ifnull(enquiry_from, '') != ''""")
 
 	if frappe.db.has_column("Opportunity", "lead") and frappe.db.has_column("Opportunity", "enquiry_from"):
 		frappe.db.sql(""" UPDATE `tabOpportunity` set party_name = lead
-			where enquiry_from = 'Lead' and party_name IS NULL and lead IS NOT NULL""")
+			where enquiry_from = 'Lead' and ifnull(party_name, '') = '' and ifnull(lead, '') != ''""")
 
 	if frappe.db.has_column("Opportunity", "customer") and frappe.db.has_column("Opportunity", "enquiry_from"):
 		frappe.db.sql(""" UPDATE `tabOpportunity` set party_name = customer
-			 where enquiry_from = 'Customer' and party_name IS NULL and customer IS NOT NULL""")
+			 where enquiry_from = 'Customer' and ifnull(party_name, '') = '' and ifnull(customer, '') != ''""")
