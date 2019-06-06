87c6b131b10faf9daf66981078db36344968af57
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-06-06 11:49:15 +0530

    patch: add priority in service level and agreement

---
+++ erpnext/patches/v12_0/set_priority_for_support.py
@@ -0,0 +1,47 @@
+import frappe
+
+def execute():
+	frappe.reload_doc("support", "doctype", "issue_priority")
+
+	priorities = frappe.get_meta("Issue").get_field("priority").options.split("\n")
+
+	for priority in priorities:
+		frappe.get_doc({
+			"doctype": "Issue Priority",
+			"name":priority
+		}).insert(ignore_permissions=True)
+
+	frappe.reload_doc("support", "doctype", "issue")
+	frappe.reload_doc("support", "doctype", "service_level")
+	frappe.reload_doc("support", "doctype", "service_level_agreement")
+
+	for issue in frappe.get_list("Issue", fields=["name", "priority"]):
+		frappe.db.set_value("Issue", issue.name, "priority", issue.priority)
+
+	for service_level in frappe.get_list("Service Level", fields=["name", "priority", "response_time", "response_time_period",
+		"resolution_time", "resolution_time_period"]):
+
+		doc = frappe.get_doc("Service Level", service_level.name)
+		doc.update("priorities": [{
+			"priority": service_level.priority,
+			"default_priority": 1,
+			"response_time": service_level.response_time,
+			"response_time_period": service_level.response_time_period,
+			"resolution_time": service_level.resolution_time,
+			"resolution_time_period": service_level.resolution_time_period
+		}])
+		doc.save(ignore_permissions=True)
+
+	for service_level_agreement in frappe.get_list("Service Level Agreement", fields=["name", "priority", "response_time", "response_time_period",
+		"resolution_time", "resolution_time_period"]):
+
+		doc = frappe.get_doc("Service Level", service_level_agreement.name)
+		doc.update("priorities": [{
+			"priority": service_level_agreement.priority,
+			"default_priority": 1,
+			"response_time": service_level_agreement.response_time,
+			"response_time_period": service_level_agreement.response_time_period,
+			"resolution_time": service_level_agreement.resolution_time,
+			"resolution_time_period": service_level_agreement.resolution_time_period
+		}])
+		doc.save(ignore_permissions=True)
