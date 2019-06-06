6132c8527b54caec3a23245274b081efdb88cf6c
Shivam Mishra <scm.mymail@gmail.com>
2019-06-06 14:19:36 +0530

    refactor: all programs are shown on home by deafult

--- erpnext/www/lms/all_programs.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-import erpnext.education.utils as utils
-import frappe
-
-no_cache = 1
-
-def get_context(context):
-	context.education_settings = frappe.get_single("Education Settings")
-	context.all_programs = utils.get_portal_programs()
