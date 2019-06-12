f7b864e8a4e49301e07d42cb7b6943f934110b84
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-06-12 08:18:54 +0530

    fix: GST validation

--- erpnext/regional/india/utils.py
+++ erpnext/regional/india/utils.py
@@ -16,8 +16,9 @@ def validate_gstin_for_india(doc, method):
 
 	gst_category = []
 
-	link_doctype = doc.links[0].get("link_doctype")
-	link_name = doc.links[0].get("link_name")
+	if len(doc.links):
+		link_doctype = doc.links[0].get("link_doctype")
+		link_name = doc.links[0].get("link_name")
 
 	if link_doctype in ["Customer", "Supplier"]:
 		gst_category = frappe.db.get_value(link_doctype, {'name': link_name}, ['gst_category'])
