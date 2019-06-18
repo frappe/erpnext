f636a8dd6db4b3c6ab42810641e6625f62afe0e8
Rohit Waghchaure <rohitw1991@gmail.com>
2019-06-18 18:34:14 +0530

    fix: finance book filters includes the gl data which don't have finance book

--- erpnext/accounts/report/financial_statements.py
+++ erpnext/accounts/report/financial_statements.py
@@ -355,6 +355,10 @@ def set_gl_entries_by_account(
 		"to_date": to_date,
 	}
 
+	if filters.get("include_default_book_entries"):
+		gl_filters["company_fb"] = frappe.db.get_value("Company",
+			company, 'default_finance_book')
+
 	for key, value in filters.items():
 		if value:
 			gl_filters.update({
@@ -399,7 +403,10 @@ def get_additional_conditions(from_date, ignore_closing_entries, filters):
 			additional_conditions.append("cost_center in %(cost_center)s")
 
 		if filters.get("finance_book"):
-			additional_conditions.append("ifnull(finance_book, '') in (%(finance_book)s, '')")
+			if filters.get("include_default_book_entries"):
+				additional_conditions.append("finance_book in (%(finance_book)s, %(company_fb)s)")
+			else:
+				additional_conditions.append("finance_book in (%(finance_book)s)")
 
 	if accounting_dimensions:
 		for dimension in accounting_dimensions:
