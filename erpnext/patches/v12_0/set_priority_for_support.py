e48ca44bf5e7be9d1d19cced6e73c1c7f872f0be
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-06-18 20:02:00 +0530

    patch: fix typo

--- erpnext/patches/v12_0/set_priority_for_support.py
+++ erpnext/patches/v12_0/set_priority_for_support.py
@@ -28,7 +28,7 @@ def set_priorities_service_level():
 	service_level_priorities = frappe.get_list("Service Level", fields=["name", "priority", "response_time", "response_time_period", "resolution_time", "resolution_time_period"])
 	frappe.reload_doc("support", "doctype", "service_level")
 
-	for service_level in service_level_priority:
+	for service_level in service_level_priorities:
 		doc = frappe.get_doc("Service Level", service_level.name)
 		doc.append("priorities", {
 			"priority": service_level.priority,
@@ -41,10 +41,10 @@ def set_priorities_service_level():
 		doc.save(ignore_permissions=True)
 
 def set_priorities_service_level_agreement():
-	service_level_agreement_priority = frappe.get_list("Service Level Agreement", fields=["name", "priority", "response_time", "response_time_period", "resolution_time", "resolution_time_period"])
+	service_level_agreement_priorities = frappe.get_list("Service Level Agreement", fields=["name", "priority", "response_time", "response_time_period", "resolution_time", "resolution_time_period"])
 	frappe.reload_doc("support", "doctype", "service_level_agreement")
 
-	for service_level_agreement in service_level_agreement_priority:
+	for service_level_agreement in service_level_agreement_priorities:
 		doc = frappe.get_doc("Service Level Agreement", service_level_agreement.name)
 
 		if doc.customer:
