3bb305a02cfdcbacb9f87e0e6f5a4dcf0dc1857c
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-07-01 18:41:08 +0530

    test: job offer creation and update

--- erpnext/hr/doctype/job_offer/test_job_offer.py
+++ erpnext/hr/doctype/job_offer/test_job_offer.py
@@ -20,8 +20,8 @@ class TestJobOffer(unittest.TestCase):
 		}])
 		frappe.db.set_value("HR Settings", None, "check_vacancies", 1)
 		job_applicant = create_job_applicant(email_id="test_job_offer@example.com")
-		job_offer = create_job_offer(job_applicant=job_applicant.name)
-		self.assertRaises(frappe.ValidationError, job_offer.submit())
+		job_offer = create_job_offer(job_applicant=job_applicant.name, designation="Researcher")
+		self.assertRaises(frappe.ValidationError, job_offer.submit)
 
 		# test creation of job offer when vacancies are not present
 		frappe.db.set_value("HR Settings", None, "check_vacancies", 0)
@@ -32,7 +32,6 @@ class TestJobOffer(unittest.TestCase):
 		create_staffing_plan()
 		job_applicant = create_job_applicant(email_id="test_job_applicants@example.com")
 		job_offer = create_job_offer(job_applicant=job_applicant.name)
-		job_offer.insert()
 		job_offer.submit()
 		job_applicant.reload()
 		self.assertEquals(job_applicant.status, "Accepted")
@@ -55,10 +54,9 @@ def create_job_offer(**args):
 		"doctype": "Job Offer",
 		"job_applicant": args.job_applicant or job_applicant.name,
 		"offer_date": args.offer_date or nowdate(),
-		"designation": args.designation or "Designer",
+		"designation": args.designation or "Researcher",
 		"status": args.status or "Accepted"
 	})
-	job_offer.insert()
 	return job_offer
 
 def create_staffing_plan(**args):
@@ -73,11 +71,11 @@ def create_staffing_plan(**args):
 		"from_date": args.from_date or nowdate(),
 		"to_date": args.to_date or add_days(nowdate(), 10),
 		"staffing_details": args.staffing_details or [{
-			"designation": "Designer",
+			"designation": "Researcher",
 			"vacancies": 1,
 			"estimated_cost_per_position": 50000
 		}]
-	})	
+	})
 	staffing_plan.insert()
 	staffing_plan.submit()
 	return staffing_plan
