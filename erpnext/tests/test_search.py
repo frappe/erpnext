12d9bd3ce8fba46f50c5e1a8c8a50440281393fd
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-07-02 21:01:57 +0530

    fix: fix for test test_contact_search_in_foreign_language

--- erpnext/tests/test_search.py
+++ erpnext/tests/test_search.py
@@ -4,12 +4,12 @@ import frappe
 from frappe.contacts.address_and_contact import filter_dynamic_link_doctypes
 
 class TestSearch(unittest.TestCase):
-	#Search for the word "clie", part of the word "client" (customer) in french.
+	#Search for the word "cond", part of the word "conduire" (Lead) in french.
 	def test_contact_search_in_foreign_language(self):
 		frappe.local.lang = 'fr'
-		output = filter_dynamic_link_doctypes("DocType", "clie", "name", 0, 20, {'fieldtype': 'HTML', 'fieldname': 'contact_html'})
+		output = filter_dynamic_link_doctypes("DocType", "cond", "name", 0, 20, {'fieldtype': 'HTML', 'fieldname': 'contact_html'})
 
-		result = [['found' for x in y if x=="Customer"] for y in output]
+		result = [['found' for x in y if x=="Lead"] for y in output]
 		self.assertTrue(['found'] in result)
 
 	def tearDown(self):
