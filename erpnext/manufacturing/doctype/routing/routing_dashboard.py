830f2b642bcaa7fd82f60b598d1a26a096523a4b
karthikeyan5 <skarthikeyan1410@gmail.com>
2019-07-05 09:48:38 +0530

    chore(manufacturing): added dashboard for doctypes

---
+++ erpnext/manufacturing/doctype/routing/routing_dashboard.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'routing',
+		'transactions': [
+			{
+				'items': ['BOM']
+			}
+		]
+	}
