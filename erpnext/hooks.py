36963a8e0408af1171ad0ee170ac0224ebeebc5b
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-07-09 15:14:13 +0530

    feat: Email Campaign

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -266,7 +266,8 @@ scheduler_events = {
 		"erpnext.projects.doctype.project.project.send_project_status_email_to_users",
 		"erpnext.quality_management.doctype.quality_review.quality_review.review",
 		"erpnext.support.doctype.service_level_agreement.service_level_agreement.check_agreement_status",
-		"erpnext.crm.doctype.email_campaign.email_campaign.send_email_to_leads"
+		"erpnext.crm.doctype.email_campaign.email_campaign.send_email_to_leads",
+		"erpnext.crm.doctype.email_campaign.email_campaign.set_email_campaign_status"
 	],
 	"daily_long": [
 		"erpnext.manufacturing.doctype.bom_update_tool.bom_update_tool.update_latest_price_in_all_boms"
