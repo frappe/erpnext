f7adc3ed26af49b3d3d84b3e2baefc614c4a4502
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-07-12 16:09:32 +0530

    codacy fixes

--- erpnext/crm/doctype/email_campaign/email_campaign.js
+++ erpnext/crm/doctype/email_campaign/email_campaign.js
@@ -2,8 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Email Campaign', {
-	// refresh: function(frm) {
-	// }
   email_campaign_for: function(frm) {
     frm.set_value('recipient', '');
   }
