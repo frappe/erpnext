27bdba49d4ff93fac0ddbda72f7b639db4cdffeb
Suraj Shetty <surajshetty3416@gmail.com>
2019-07-28 22:04:07 +0530

    test: Add test case statements

---
+++ erpnext/stock/doctype/pick_ticket/test_pick_ticket.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestPickTicket(unittest.TestCase):
+	def test_pick_list_picks_warehouse_for_each_item():
+		pass
+
+	def test_pick_list_skips_out_of_warranty_item():
+		pass
+
+	def test_pick_list_skips_items_in_expired_batch():
+		pass
+
+	def test_pick_list_shows_serial_no_for_serialized_item():
+		pass
+
+	def test_pick_list_for_multiple_reference_doctypes():
+		pass
