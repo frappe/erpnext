c792de964e62a411cd4dfdd40756e7614fe21339
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-07-31 14:39:52 +0530

    fix: minor changes

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -1190,7 +1190,7 @@ def update_child_qty_rate(parent_doctype, trans_items, parent_doctype_name, chil
 						 .format(child_item.idx, child_item.item_code))
 		else:
 			child_item.rate = flt(d.get("rate"))
-			child_item.discount_percentage = flt((1 - child_item.rate / child_item.price_list_rate) * 100.0, \
+			child_item.discount_percentage = flt((1 - flt(child_item.rate) / flt(child_item.price_list_rate)) * 100.0, \
 				child_item.precision("discount_percentage"))
 
 		child_item.flags.ignore_validate_update_after_submit = True
