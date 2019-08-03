b7611699764e799dece22eadcbca71a0a665ea62
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-08-03 13:40:37 +0530

    fix: Error handling in taxes and totals

--- erpnext/controllers/taxes_and_totals.py
+++ erpnext/controllers/taxes_and_totals.py
@@ -77,7 +77,7 @@ class calculate_taxes_and_totals(object):
 
 				item.net_rate = item.rate
 
-				if not item.qty and self.doc.is_return:
+				if not item.qty and self.doc.get("is_return"):
 					item.amount = flt(-1 * item.rate, item.precision("amount"))
 				else:
 					item.amount = flt(item.rate * item.qty,	item.precision("amount"))
