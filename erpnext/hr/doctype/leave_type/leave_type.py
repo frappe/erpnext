c79f39312405961b1619441995a5d4ee494b4973
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-08-14 16:09:16 +0530

    refactor: replace raw sql with orm

--- erpnext/hr/doctype/leave_type/leave_type.py
+++ erpnext/hr/doctype/leave_type/leave_type.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import calendar
 import frappe
 from datetime import datetime
+from frappe.utils import today
 from frappe import _
 
 from frappe.model.document import Document
@@ -12,6 +13,12 @@ from frappe.model.document import Document
 class LeaveType(Document):
 	def validate(self):
 		if self.is_lwp:
-			leave_allocation = frappe.db.sql_list("""select name from `tabLeave Allocation` where leave_type=%s""", (self.name))
+			leave_allocation = frappe.get_all("Leave Allocation", filters={
+				'leave_type': self.name,
+				'from_date': ("<=", today()),
+				'to_date': (">=", today())
+			}, ['name'])
+			leave_allocation = [l['name'] for l in leave_allocation]
+			frappe.db("""select name from `tabLeave Allocation` where leave_type=%s""", (self.name))
 			if leave_allocation:
 				frappe.throw(_('Leave application is linked with leave allocations {0}. Leave application cannot be set as leave without pay').format(", ".join(leave_allocation))) #nosec
