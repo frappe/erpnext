d0a95e525f15b3c9d2323f354a07e5d1e4abd2d6
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-08-15 17:06:32 +0530

    fix: Default dimensions in child doctypes

--- erpnext/public/js/utils/dimension_tree_filter.js
+++ erpnext/public/js/utils/dimension_tree_filter.js
@@ -1,12 +1,13 @@
 frappe.provide('frappe.ui.form');
 
 erpnext.doctypes_with_dimensions = ["GL Entry", "Sales Invoice", "Purchase Invoice", "Payment Entry", "Asset",
-	"Expense Claim", "Stock Entry", "Budget", "Payroll Entry", "Delivery Note", "Sales Invoice Item", "Purchase Invoice Item",
-	"Purchase Order Item", "Journal Entry Account", "Material Request Item", "Delivery Note Item", "Purchase Receipt Item",
-	"Stock Entry Detail", "Payment Entry Deduction", "Sales Taxes and Charges", "Purchase Taxes and Charges", "Shipping Rule",
-	"Landed Cost Item", "Asset Value Adjustment", "Loyalty Program", "Fee Schedule", "Fee Structure", "Stock Reconciliation",
-	"Travel Request", "Fees", "POS Profile", "Opening Invoice Creation Tool", "Opening Invoice Creation Tool Item", "Subscription",
-	"Subscription Plan"];
+	"Expense Claim", "Stock Entry", "Budget", "Payroll Entry", "Delivery Note", "Shipping Rule", "Loyalty Program",
+	"Fee Schedule", "Fee Structure", "Stock Reconciliation", "Travel Request", "Fees", "POS Profile", "Opening Invoice Creation Tool",
+	"Subscription", "Purchase Order", "Journal Entry", "Material Request", "Purchase Receipt", "Landed Cost Item", "Asset"]
+
+erpnext.child_docs = ["Sales Invoice Item", "Purchase Invoice Item", "Purchase Order Item", "Journal Entry Account",
+	"Material Request Item", "Delivery Note Item", "Purchase Receipt Item", "Stock Entry Detail", "Payment Entry Deduction",
+	"Landed Cost Item", "Asset Value Adjustment", "Opening Invoice Creation Tool Item", "Subscription Plan"]
 
 frappe.call({
 	method: "erpnext.accounts.doctype.accounting_dimension.accounting_dimension.get_dimension_filters",
@@ -26,8 +27,19 @@ erpnext.doctypes_with_dimensions.forEach((doctype) => {
 							"is_group": 0
 						});
 					}
-					if (frm.is_new() && frappe.meta.has_field(doctype, 'company') && frm.doc.company) {
-						frm.set_value(dimension['fieldname'], erpnext.default_dimensions[frm.doc.company][dimension['document_type']]);
+
+					if (frappe.meta.has_field(doctype, dimension['fieldname'])) {
+						if (frm.is_new() && frappe.meta.has_field(doctype, 'company') && frm.doc.company) {
+							frm.set_value(dimension['fieldname'], erpnext.default_dimensions[frm.doc.company][dimension['document_type']]);
+						}
+					}
+
+					if (frm.doc.items && frm.doc.items.length) {
+						frm.doc.items[0][dimension['fieldname']] = erpnext.default_dimensions[frm.doc.company][dimension['document_type']];
+					}
+
+					if (frm.doc.accounts && frm.doc.accounts.length) {
+						frm.doc.accounts[0][dimension['fieldname']] = erpnext.default_dimensions[frm.doc.company][dimension['document_type']];
 					}
 				});
 			});
@@ -36,11 +48,17 @@ erpnext.doctypes_with_dimensions.forEach((doctype) => {
 		company: function(frm) {
 			if(frm.doc.company) {
 				erpnext.dimension_filters.forEach((dimension) => {
-					frm.set_value(dimension['fieldname'], erpnext.default_dimensions[frm.doc.company][dimension['document_type']]);
+					if (frappe.meta.has_field(doctype, dimension['fieldname'])) {
+						frm.set_value(dimension['fieldname'], erpnext.default_dimensions[frm.doc.company][dimension['document_type']]);
+					}
 				});
 			}
 		},
+	});
+});
 
+erpnext.child_docs.forEach((doctype) => {
+	frappe.ui.form.on(doctype, {
 		items_add: function(frm, cdt, cdn) {
 			erpnext.dimension_filters.forEach((dimension) => {
 				var row = frappe.get_doc(cdt, cdn);
