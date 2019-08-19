a94df6f3a406c8564fed6a60d58daac33c56b723
Suraj Shetty <surajshetty3416@gmail.com>
2019-08-19 10:48:21 +0530

    fix: Get Items button

--- erpnext/stock/doctype/pick_list/pick_list.js
+++ erpnext/stock/doctype/pick_list/pick_list.js
@@ -36,7 +36,7 @@ frappe.ui.form.on('Pick List', {
 		frm.remove_custom_button(__("Get items"));
 		let source_doctype = frm.doc.items_based_on;
 		let method = 'erpnext.selling.doctype.sales_order.sales_order.make_pick_list';
-		if (source_doctype === 'Sales Order') {
+		if (source_doctype === 'Work Order') {
 			method = 'erpnext.manufacturing.doctype.work_order.work_order.make_pick_list';
 		}
 		frm.add_custom_button(__("Get items"), () => {
