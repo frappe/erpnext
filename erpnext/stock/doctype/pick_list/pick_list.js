1a090432d979b100757c943f7aa4d773cdfb31f6
Suraj Shetty <surajshetty3416@gmail.com>
2019-08-20 12:04:07 +0530

    fix: Quantity while creating pick list from work order
    - Fix get_mapped_doc config to filter items
    - Fix erroneous condition in get_mapped_doc used for creation of pick_list
    in sales order
    
    Co-authored-by: Nabin Hait <nabinhait@gmail.com>

--- erpnext/stock/doctype/pick_list/pick_list.js
+++ erpnext/stock/doctype/pick_list/pick_list.js
@@ -28,17 +28,35 @@ frappe.ui.form.on('Pick List', {
 	},
 	make_delivery_note(frm) {
 		frappe.model.open_mapped_doc({
-			method: "erpnext.stock.doctype.pick_list.pick_list.make_delivery_note",
+			method: 'erpnext.stock.doctype.pick_list.pick_list.make_delivery_note',
 			frm: frm
 		});
 	},
 	add_get_items_button(frm) {
 		frm.remove_custom_button(__("Get items"));
 		let source_doctype = frm.doc.items_based_on;
+		let date_field = 'transaction_date';
+		let get_query_method = null;
+		let get_query_filters = {
+			docstatus: 1,
+			per_delivered: ['<', 100],
+			status: ['!=', '']
+		};
 		let method = 'erpnext.selling.doctype.sales_order.sales_order.make_pick_list';
 		if (source_doctype === 'Work Order') {
 			method = 'erpnext.manufacturing.doctype.work_order.work_order.make_pick_list';
+			date_field = 'planned_start_date';
+			get_query_method = 'erpnext.stock.doctype.pick_list.pick_list.get_pending_work_orders';
+			get_query_filters = null;
 		}
+
+		let get_query = () => {
+			return {
+				'query': get_query_method,
+				'filters': get_query_filters
+			};
+		};
+
 		frm.add_custom_button(__("Get items"), () => {
 			erpnext.utils.map_current_doc({
 				method: method,
@@ -47,10 +65,8 @@ frappe.ui.form.on('Pick List', {
 				setters: {
 					company: frm.doc.company,
 				},
-				date_field: 'creation',
-				get_query_filters: {
-					docstatus: 1,
-				}
+				date_field: date_field,
+				get_query: get_query
 			});
 		});
 	}
