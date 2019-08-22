282d6ddeff214c41517b223f51f4735ae6b2897c
Saif Ur Rehman <saifi0102@gmail.com>
2019-08-22 20:20:19 +0500

    fix(AR/AP): Filter Sales Person assigned in invoices only

--- erpnext/accounts/report/accounts_receivable/accounts_receivable.py
+++ erpnext/accounts/report/accounts_receivable/accounts_receivable.py
@@ -772,8 +772,7 @@ class ReceivablePayableReport(object):
 				conditions.append("""exists(select name from `tabSales Team` steam where
 					steam.sales_person in (select name from `tabSales Person` where lft >= {0} and rgt <= {1})
 					and ((steam.parent = voucher_no and steam.parenttype = voucher_type)
-						or (steam.parent = against_voucher and steam.parenttype = against_voucher_type)
-						or (steam.parent = party and steam.parenttype = 'Customer')))""".format(lft, rgt))
+						or (steam.parent = against_voucher and steam.parenttype = against_voucher_type)))""".format(lft, rgt))
 
 		elif party_type_field=="supplier":
 			account_type = "Payable"
