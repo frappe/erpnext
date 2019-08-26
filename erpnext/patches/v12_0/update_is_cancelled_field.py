1acba163f84603f0ee1c298df8118ddbbf45b338
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-08-26 14:30:02 +0530

    fix: Patch

--- erpnext/patches/v12_0/update_is_cancelled_field.py
+++ erpnext/patches/v12_0/update_is_cancelled_field.py
@@ -5,6 +5,8 @@ from erpnext.regional.india.setup import make_custom_fields
 def execute():
 
 	try:
+		frappe.reload_doc("stock", "doctype", "stock_ledger_entry")
+		frappe.reload_doc("stock", "doctype", "serial_no")
 		frappe.db.sql("UPDATE `tabStock Ledger Entry` SET is_cancelled = IF(is_cancelled='No', 0, 1)")
 		frappe.db.sql("UPDATE `tabSerial No` SET is_cancelled = IF(is_cancelled='No', 0, 1)")
 	except:
