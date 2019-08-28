529960d2ea962f36d99cdf452a91a83d0ef54cfc
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-08-28 13:04:02 +0530

    fix: Move patch upward in patches.txt

--- erpnext/patches/v12_0/update_is_cancelled_field.py
+++ erpnext/patches/v12_0/update_is_cancelled_field.py
@@ -5,10 +5,6 @@ from erpnext.regional.india.setup import make_custom_fields
 def execute():
 
 	try:
-
-		frappe.db.sql("UPDATE `tabStock Ledger Entry` SET is_cancelled = '0' where is_cancelled IS NULL")
-		frappe.db.sql("UPDATE `tabSerial No` SET is_cancelled = '0' where is_cancelled IS NULL")
-
 		frappe.reload_doc("stock", "doctype", "stock_ledger_entry")
 		frappe.reload_doc("stock", "doctype", "serial_no")
 
