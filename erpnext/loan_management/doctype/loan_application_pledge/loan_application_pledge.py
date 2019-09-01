ff66bd658ab8c4b1162bcabd3f78ae404c96eb4c
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-09-01 20:25:02 +0530

    fix: Add loan application pledge doctype

--- erpnext/loan_management/doctype/loan_application_pledge/loan_application_pledge.py
+++ erpnext/loan_management/doctype/loan_application_pledge/loan_application_pledge.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class LoanSecuritiesPledge(Document):
+class LoanApplicationPledge(Document):
 	pass
