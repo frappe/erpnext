d0ccf4e216cf92e0cbbc58d5872905d86dd5709a
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-09-03 12:58:12 +0530

    A

--- erpnext/crm/doctype/appointment/appointment.py
+++ erpnext/crm/doctype/appointment/appointment.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Appointment(Document):
-	pass
+	def validate(self):
+		number_of_appointments_in_same_slot = frappe.db.count('Appointment',filters={'scheduled_time':self.scheduled_time})
+		settings = frappe.get_doc('Appointment Booking Settings')
+		if(number_of_appointments_in_same_slot>=settings.number_of_agents):
+			frappe.throw('Time slot is not available')
+
