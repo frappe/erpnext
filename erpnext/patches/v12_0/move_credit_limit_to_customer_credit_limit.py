4ce38059aca818da540a6dd3a02940678f169e83
Nabin Hait <nabinhait@gmail.com>
2019-09-06 14:33:10 +0530

    feat: Company wise credit limit

--- erpnext/patches/v12_0/move_credit_limit_to_customer_credit_limit.py
+++ erpnext/patches/v12_0/move_credit_limit_to_customer_credit_limit.py
@@ -8,6 +8,7 @@ def execute():
 	''' Move credit limit and bypass credit limit to the child table of customer credit limit '''
 	frappe.reload_doc("Selling", "doctype", "Customer Credit Limit")
 	frappe.reload_doc("Selling", "doctype", "Customer")
+	frappe.reload_doc("Setup", "doctype", "Customer Group")
 
 	if frappe.db.a_row_exists("Customer Credit Limit"):
 		return
@@ -17,24 +18,29 @@ def execute():
 def move_credit_limit_to_child_table():
 	''' maps data from old field to the new field in the child table '''
 
-	fields=""
-	if frappe.db.has_column('Customer', 'bypass_credit_limit_check_at_sales_order'):
-		fields = ", bypass_credit_limit_check_at_sales_order"
-
-	credit_limit_record = frappe.db.sql('''
-		SELECT name, credit_limit {0}
-		FROM `tabCustomer` where credit_limit > 0
-	'''.format(fields), as_dict=1) #nosec
-
 	companies = frappe.get_all("Company", 'name')
+	for doctype in ("Customer", "Customer Group"):
+		fields = ""
+		if doctype == "Customer" \
+				and frappe.db.has_column('Customer', 'bypass_credit_limit_check_at_sales_order'):
+			fields = ", bypass_credit_limit_check_at_sales_order"
 
-	for record in credit_limit_record:
-		customer = frappe.get_doc("Customer", record.name)
-		for company in companies:
-			customer.append("credit_limit_reference", {
-				'credit_limit': record.credit_limit,
-				'bypass_credit_limit_check': record.bypass_credit_limit_check_at_sales_order,
-				'company': company.name
-			})
-		for row in customer.credit_limit_reference:
-			row.db_insert()
+		credit_limit_records = frappe.db.sql('''
+			SELECT name, credit_limit {0}
+			FROM `tab{1}` where credit_limit > 0
+		'''.format(fields, doctype), as_dict=1) #nosec
+
+		for record in credit_limit_records:
+			doc = frappe.get_doc(doctype, record.name)
+			for company in companies:
+				row = frappe._dict({
+					'credit_limit': record.credit_limit,
+					'company': company.name
+				})
+				if doctype == "Customer":
+					row.bypass_credit_limit_check = record.bypass_credit_limit_check_at_sales_order
+
+				doc.append("credit_limits", row)
+
+			for row in doc.credit_limits:
+				row.db_insert()
