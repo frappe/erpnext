a2f9cbe2423006804f0fcfcb8deb644f67fd1da0
Saif Ur Rehman <saifi0102@gmail.com>
2019-09-07 19:58:38 +0500

    feat: Patch for Item Naming fields

---
+++ erpnext/patches/v11_1/set_item_naming_by.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+import frappe, os
+from frappe import _
+
+def execute():
+	stock_settings = frappe.get_single("Stock Settings")
+	stock_settings.item_naming_by = "Item Name"
+	stock_settings.save()
+
+	frappe.make_property_setter({'doctype': 'Item', 'fieldname': 'item_code', 'property': 'hidden',
+		'value': 0})
+
+	frappe.make_property_setter({'doctype': 'Item', 'fieldname': 'item_name', 'property': 'hidden',
+		'value': 0})
+	frappe.make_property_setter({'doctype': 'Item', 'fieldname': 'item_name', 'property': 'read_only',
+		'value': 0})
+
+	frappe.make_property_setter({'doctype': 'Item', 'fieldname': 'naming_series', 'property': 'hidden',
+		'value': 0})
+
+	for name in frappe.get_all("Item"):
+		name = name.name
+		item = frappe.get_doc("Item", name)
+		if item.clean_name(item.name) == item.clean_name(item.item_name):
+			item.db_set('item_naming_by', 'Item Name')
+		else:
+			item.db_set('item_naming_by', 'Item Code')
