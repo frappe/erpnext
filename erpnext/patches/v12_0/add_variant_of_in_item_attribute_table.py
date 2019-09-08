e352fb754bb2064782522b557981f46426338b62
Faris Ansari <netchampfaris@users.noreply.github.com>
2019-09-08 21:25:33 +0530

    fix: Reload doctype in variants patch (#18962)

--- erpnext/patches/v12_0/add_variant_of_in_item_attribute_table.py
+++ erpnext/patches/v12_0/add_variant_of_in_item_attribute_table.py
@@ -1,6 +1,7 @@
 import frappe
 
 def execute():
+	frappe.reload_doc('stock', 'doctype', 'item_variant_attribute')
 	frappe.db.sql('''
 		UPDATE `tabItem Variant Attribute` t1
 		INNER JOIN `tabItem` t2 ON t2.name = t1.parent
