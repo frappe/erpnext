caf099934cb59296c51d5f21c0918219fe745fd4
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-09 11:21:37 +1200

    Update journal_entry.py

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -661,11 +661,10 @@ def get_default_bank_cash_account(company, account_type=None, mode_of_payment=No
 				currency = frappe.get_cached_value('Company',  company,  "default_currency")
 			frappe.log_error(currency)
 			#account = frappe.get_cached_value('Company',  company,  "default_bank_account")
-			frappe.log_error("Account: " + account)
 			if not account:
 				account_list = frappe.get_all("Account", filters = {"company": company,
 					"account_type": "Bank", "is_group": 0, "currency": currency})
-				frappe.log_error("List: " + account_list)
+				frappe.log_error("List: " + str(account_list))
 				if len(account_list) == 1:
 					account = account_list[0].name
 
