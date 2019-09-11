e213983f178a22800b82f8d9a2cbb3b753866393
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-09-11 22:55:29 +0530

    fix: Add dashboard to loan master

---
+++ erpnext/loan_management/doctype/loan_type/loan_type_dashboard.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'loan_type',
+		'transactions': [
+			{
+				'items': ['Loan Repayment']
+			},
+			{
+				'items': ['Loan']
+			},
+			{
+				'items': ['Loan Application']
+			}
+		]
+	}
