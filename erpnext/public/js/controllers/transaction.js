24f06ecf9a324f7bba1ca118a44d385e11f49f42
Faris Ansari <netchamp.faris@gmail.com>
2019-09-13 01:49:14 +0530

    fix: Use input-group for scan button

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -283,19 +283,22 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 			scan_barcode_field.set_new_description("");
 
 			if (frappe.is_mobile()) {
-				if (scan_barcode_field.$input_wrapper.find('.scan-barcode-button').length) return;
+				if (scan_barcode_field.$input_wrapper.find('.input-group').length) return;
 
-				$(`<p class="scan-barcode-button">
-					<button class="btn btn-xs btn-default">${__('Scan Barcode using Camera')}</button>
-				</p>`)
+				let $input_group = $('<div class="input-group">');
+				scan_barcode_field.$input_wrapper.find('.control-input').append($input_group);
+				$input_group.append(scan_barcode_field.$input);
+				$(`<span class="input-group-btn" style="vertical-align: top">
+						<button class="btn btn-default border" type="button">
+							<i class="fa fa-camera"></i>
+						</button>
+					</span>`)
 					.on('click', '.btn', () => {
-						frappe.require('/assets/js/barcode_scanner.min.js', () => {
-							frappe.barcode.scan_barcode().then(barcode => {
-								scan_barcode_field.set_value(barcode);
-							});
+						frappe.barcode.scan_barcode().then(barcode => {
+							scan_barcode_field.set_value(barcode);
 						});
 					})
-					.appendTo(scan_barcode_field.$input_wrapper);
+					.appendTo($input_group);
 			}
 		}
 	},
