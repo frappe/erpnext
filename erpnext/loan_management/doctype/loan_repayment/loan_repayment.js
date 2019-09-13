b2ee9da402a90aaacc01834e1a11a134c88d7594
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-09-13 09:39:39 +0530

    fix: Loan Reapayment for term loans

--- erpnext/loan_management/doctype/loan_repayment/loan_repayment.js
+++ erpnext/loan_management/doctype/loan_repayment/loan_repayment.js
@@ -5,4 +5,57 @@ frappe.ui.form.on('Loan Repayment', {
 	// refresh: function(frm) {
 
 	// }
+	onload: function(frm) {
+		frm.set_query('against_loan', function() {
+			return {
+				'filters': {
+					'docstatus': 1
+				}
+			};
+		});
+
+		if (frm.doc.against_loan && frm.doc.posting_date) {
+			frm.trigger('calculate_repayment_amounts');
+		}
+	},
+
+	posting_date : function(frm) {
+		frm.trigger('calculate_repayment_amounts');
+	},
+
+	against_loan: function(frm) {
+		if (frm.doc.posting_date) {
+			frm.trigger('calculate_repayment_amounts');
+		}
+	},
+
+	payment_type: function(frm) {
+		if (frm.doc.posting_date) {
+			frm.trigger('calculate_repayment_amounts');
+		}
+	},
+
+	calculate_repayment_amounts: function(frm) {
+		frappe.call({
+			method: 'erpnext.loan_management.doctype.loan_repayment.loan_repayment.calculate_amounts',
+			args: {
+				'against_loan': frm.doc.against_loan,
+				'posting_date': frm.doc.posting_date,
+				'loan_type': frm.doc.loan_type,
+				'payment_type': frm.doc.payment_type
+			},
+			callback: function(r) {
+				console.log(r);
+				let amounts = r.message;
+
+				frm.set_value('pending_principal_amount', amounts['principal_amount']);
+				if (frm.doc.is_term_loan) {
+					frm.set_value('payable_principal_amount', amounts['principal_amount']);
+				}
+				frm.set_value('interest_payable', amounts['interest_amount']);
+				frm.set_value('penalty_amount', amounts['penalty_amount']);
+				frm.set_value('payable_amount', amounts['payable_amount']);
+			}
+		})
+	}
 });
--- erpnext/loan_management/doctype/loan_repayment/loan_repayment.py
+++ erpnext/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -3,8 +3,235 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe, erpnext
+import json
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import date_diff, add_days, getdate, add_months, get_first_day
+from erpnext.controllers.accounts_controller import AccountsController
+from erpnext.accounts.general_ledger import make_gl_entries
 
-class LoanRepayment(Document):
-	pass
+class LoanRepayment(AccountsController):
+
+	def validate(self):
+		amounts = calculate_amounts(self.against_loan, self.posting_date, self.loan_type, self.payment_type)
+		self.set_missing_values(amounts)
+
+	def on_submit(self):
+		self.mark_as_paid()
+		self.make_gl_entries()
+
+	def on_cancel(self):
+		self.mark_as_unpaid()
+		self.make_gl_entries(cancel=1)
+
+	def set_missing_values(self, amounts):
+		if not self.posting_date:
+			self.posting_date = nowdate()
+
+		if not self.cost_center:
+			self.cost_center = erpnext.get_default_cost_center(self.company)
+
+		if not self.interest_payable:
+			self.interest_payable = amounts['interest_amount']
+
+		if not self.penalty_amount:
+			self.penalty_amount = amounts['penalty_amount']
+
+		if not self.pending_principal_amount:
+			self.pending_principal_amount = amounts['pending_principal_amount']
+
+		if not self.payable_principal_amount and self.is_term_loan:
+			self.payable_principal_amount = amounts['payable_principal_amount']
+
+		if amounts.get("paid_accrual_entries"):
+			self.paid_accrual_entries = frappe.as_json(amounts.get("paid_accrual_entries"))
+
+
+	def mark_as_paid(self):
+		paid_payments = []
+		paid_amount = self.amount_paid
+
+		if not paid_amount:
+			frappe.throw(_("Amount paid cannot be zero"))
+
+		loan = frappe.get_doc("Loan", self.against_loan)
+
+		principal_amount_paid = self.amount_paid - self.penalty_amount - self.interest_payable
+		paid_accrual_entries = json.loads(self.paid_accrual_entries)
+
+		if principal_amount_paid < 0:
+			msg = _("Paid amount cannot be less than {0}").format(self.penalty_amount + self.interest_payable)
+			frappe.throw(msg)
+
+		if self.payment_type == "Loan Closure" and principal_amount_paid < self.pending_principal_amount:
+			msg = _("Amount of {0} is required for Loan closure").format(self.pending_principal_amount)
+
+		frappe.db.sql("""UPDATE `tabLoan Interest Accrual`
+			SET is_paid = 1 where name in (%s)"""
+			% ", ".join(['%s']*len(paid_accrual_entries)), tuple(paid_accrual_entries))
+
+		frappe.db.set_value("Loan", self.against_loan, "total_amount_paid", loan.total_amount_paid + principal_amount_paid)
+		frappe.db.set_value("Loan", self.against_loan, "total_principal_paid", loan.total_principal_paid + principal_amount_paid)
+
+		if loan.total_amount_paid + principal_amount_paid == loan.total_payment:
+			frappe.db.set_value("Loan", self.against_loan, "status", "Repaid/Closed")
+
+	def mark_as_unpaid(self):
+
+		loan = frappe.get_doc("Loan", self.against_loan)
+		paid_accrual_entries = json.loads(self.paid_accrual_entries)
+
+		principal_amount_paid = self.amount_paid - self.penalty_amount - self.interest_payable
+
+		frappe.db.sql("""UPDATE `tabLoan Interest Accrual`
+			SET is_paid = 1 where name in (%s)"""
+			% ", ".join(['%s']*len(paid_accrual_entries)), tuple(paid_accrual_entries))
+
+		frappe.db.set_value("Loan", self.against_loan, "total_amount_paid", loan.total_amount_paid - principal_amount_paid)
+		frappe.db.set_value("Loan", self.against_loan, "total_principal_paid", loan.total_principal_paid - principal_amount_paid)
+
+		if loan.status == "Repaid/Closed":
+			frappe.db.set_value("Loan", self.against_loan, "status", "Disbursed")
+
+	def make_gl_entries(self, cancel=0, adv_adj=0):
+		gle_map = []
+		loan_details = frappe.get_doc("Loan", self.against_loan)
+
+		gle_map.append(
+			self.get_gl_dict({
+				"account": loan_details.loan_account,
+				"against": loan_details.payment_account,
+				"debit": self.penalty_amount,
+				"debit_in_account_currency": self.penalty_amount,
+				"against_voucher_type": "Loan",
+				"against_voucher": self.against_loan,
+				"remarks": "Against Loan:" + self.against_loan,
+				"cost_center": self.cost_center
+			})
+		)
+
+		gle_map.append(
+			self.get_gl_dict({
+				"account": loan_details.penalty_income_account,
+				"against": loan_details.payment_account,
+				"credit": self.penalty_amount,
+				"credit_in_account_currency": self.penalty_amount,
+				"against_voucher_type": "Loan",
+				"against_voucher": self.against_loan,
+				"remarks": "Against Loan:" + self.against_loan,
+				"cost_center": self.cost_center
+			})
+		)
+
+		gle_map.append(
+			self.get_gl_dict({
+				"account": loan_details.payment_account,
+				"against": loan_details.loan_account + ", " + loan_details.interest_income_account
+						+ ", " + loan_details.penalty_income_account,
+				"debit": self.amount_paid,
+				"debit_in_account_currency": self.amount_paid ,
+				"against_voucher_type": "Loan",
+				"against_voucher": self.against_loan,
+				"remarks": "Against Loan:" + self.against_loan,
+				"cost_center": self.cost_center
+			})
+		)
+
+		gle_map.append(
+			self.get_gl_dict({
+				"account": loan_details.loan_account,
+				"party_type": loan_details.applicant_type,
+				"party": loan_details.applicant,
+				"against": loan_details.payment_account,
+				"credit": self.amount_paid,
+				"credit_in_account_currency": self.amount_paid,
+				"against_voucher_type": "Loan",
+				"against_voucher": self.against_loan,
+				"remarks": "Against Loan:" + self.against_loan,
+				"cost_center": self.cost_center
+			})
+		)
+
+		if gle_map:
+			make_gl_entries(gle_map, cancel=cancel, adv_adj=adv_adj)
+
+def create_repayment_entry(loan, applicant, company, posting_date, loan_type, payment_type, amount_paid):
+
+	lr = frappe.get_doc({
+		"doctype": "Loan Repayment",
+		"against_loan": loan,
+		"payment_type": payment_type,
+		"company": company,
+		"posting_date": posting_date,
+		"applicant": applicant,
+		"amount_paid": amount_paid,
+		"loan_type": loan_type
+	}).insert()
+
+	return lr
+
+def get_accured_interest_entries(against_loan):
+	accured_interest_entries = frappe.get_all("Loan Interest Accrual",
+		fields=["name", "interest_amount", "posting_date", "payable_principal_amount"],
+		filters = {
+			"loan": against_loan,
+			"is_paid": 0
+		})
+
+	return accured_interest_entries
+
+
+def get_amounts(amounts, against_loan, loan_type, posting_date):
+
+	against_loan_doc = frappe.get_doc("Loan", against_loan)
+	loan_type_details = frappe.get_doc("Loan Type", loan_type)
+	accured_interest_entries = get_accured_interest_entries(against_loan_doc.name)
+
+	paid_accrual_entries = []
+
+	total_pending_interest = 0
+	penalty_amount = 0
+	payable_principal_amount = 0
+
+	for entry in accured_interest_entries:
+
+		due_date = add_days(get_first_day(entry.posting_date), 2)
+		no_of_late_days = date_diff(posting_date,
+					add_days(due_date, loan_type_details.grace_period_in_days)) + 1
+
+		if no_of_late_days > 0 and (not against_loan_doc.repay_from_salary):
+			penalty_amount += (entry.interest_amount * (loan_type_details.penalty_interest_rate / 100) * no_of_late_days)/365
+
+		total_pending_interest += entry.interest_amount
+		payable_principal_amount += entry.payable_principal_amount
+
+		paid_accrual_entries.append(entry.name)
+
+	pending_principal_amount = against_loan_doc.total_payment - against_loan_doc.total_principal_paid - against_loan_doc.total_interest_payable
+
+	amounts["pending_principal_amount"] = pending_principal_amount
+	amounts["payable_principal_amount"] = payable_principal_amount
+	amounts["paid_accrual_entries"] = paid_accrual_entries
+	amounts["interest_amount"] = total_pending_interest
+	amounts["penalty_amount"] = penalty_amount
+
+	return amounts
+
+@frappe.whitelist()
+def calculate_amounts(against_loan, posting_date, loan_type, payment_type):
+
+	amounts = {
+		'penalty_amount': 0.0,
+		'interest_amount': 0.0,
+		'pending_principal_amount': 0.0,
+		'payable_principal_amount': 0.0,
+		'payable_amount': 0.0
+	}
+
+	amounts = get_amounts(amounts, against_loan, loan_type, posting_date)
+
+	return amounts
+
+
+
