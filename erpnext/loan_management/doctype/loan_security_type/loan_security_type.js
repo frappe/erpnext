477bb37263144eebf34dcd097266945a20951e36
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-09-15 09:53:13 +0530

    fix: Add custom button to update loan security price and trigger shortfall

--- erpnext/loan_management/doctype/loan_security_type/loan_security_type.js
+++ erpnext/loan_management/doctype/loan_security_type/loan_security_type.js
@@ -2,7 +2,48 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Loan Security Type', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		if(!frm.is_new()) {
+			frm.add_custom_button(__('Update Loan Security Price'), function() {
+				frm.trigger('update_price');
+			})
+		}
+	},
 
-	// }
+	update_price: function(frm) {
+		var d = new frappe.ui.Dialog({
+			title: __('Update Loan Security Price'),
+			fields: [
+				{
+					"label" : "From Time",
+					"fieldname": "from_time",
+					"fieldtype": "Datetime",
+					"reqd": 1,
+					"default": frappe.datetime.now_datetime()
+				},
+				{
+					"label" : "To Time",
+					"fieldname": "to_time",
+					"fieldtype": "Datetime",
+					"reqd": 1,
+					"default": frappe.datetime.add_days(frappe.datetime.now_datetime(), 1)
+				}
+
+			],
+			primary_action: function() {
+				var data = d.get_values();
+				frappe.call({
+					method: "erpnext.loan_management.doctype.loan_security_price.loan_security_price.update_loan_security_price",
+					args: {
+						'from_timestamp': data.from_time,
+						'to_timestamp': data.to_timestamp,
+						'loan_type': frm.doc.name
+					},
+				});
+				d.hide();
+			},
+			primary_action_label: __('Update')
+		});
+		d.show();
+	}
 });
