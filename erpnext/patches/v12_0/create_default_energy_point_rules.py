92790ec091b2995fb63b6e7612bd37491867460a
Suraj Shetty <surajshetty3416@gmail.com>
2019-09-17 13:55:44 +0530

    fix: Reload Energy Point Rule to fix patch

--- erpnext/patches/v12_0/create_default_energy_point_rules.py
+++ erpnext/patches/v12_0/create_default_energy_point_rules.py
@@ -1,4 +1,5 @@
 from erpnext.setup.install import create_default_energy_point_rules
 
 def execute():
+	frappe.reload_doc('social', 'doctype', 'energy_point_rule')
 	create_default_energy_point_rules()
