9fc482fff24ad9829cd2d48e5400618d2a0a3c7c
Deepesh Garg <42651287+deepeshgarg007@users.noreply.github.com>
2019-09-17 12:50:28 +0530

    fix: Mandatory accounting dimensions while creating asset depreciation entry (#19073)
    * fix: Mandatory accounting dimensions while creating asset depreciation entry
    
    * fix: Consider account types while assigning accounting dimensions

--- erpnext/accounts/doctype/accounting_dimension/accounting_dimension.py
+++ erpnext/accounts/doctype/accounting_dimension/accounting_dimension.py
@@ -174,7 +174,7 @@ def get_accounting_dimensions(as_list=True):
 		return accounting_dimensions
 
 def get_checks_for_pl_and_bs_accounts():
-	dimensions = frappe.db.sql("""SELECT p.label, p.disabled, p.fieldname, c.company, c.mandatory_for_pl, c.mandatory_for_bs
+	dimensions = frappe.db.sql("""SELECT p.label, p.disabled, p.fieldname, c.default_dimension, c.company, c.mandatory_for_pl, c.mandatory_for_bs
 		FROM `tabAccounting Dimension`p ,`tabAccounting Dimension Detail` c
 		WHERE p.name = c.parent""", as_dict=1)
 
