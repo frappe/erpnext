737a504a357bd34916201f8deac2e56cf69d6e21
Rohit Waghchaure <rohitw1991@gmail.com>
2019-09-19 17:01:26 +0530

    fix: get_bin_details_and_serial_nos() takes at least 3 arguments (4 given)

--- erpnext/selling/sales_common.js
+++ erpnext/selling/sales_common.js
@@ -209,7 +209,7 @@ erpnext.selling.SellingController = erpnext.TransactionController.extend({
 					args: {
 						item_code: item.item_code,
 						warehouse: item.warehouse,
-						has_batch_no: has_batch_no,
+						has_batch_no: has_batch_no || 0,
 						stock_qty: item.stock_qty,
 						serial_no: item.serial_no || "",
 					},
