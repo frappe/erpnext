6b0fea16b64806bdfcc1e5f391ce8fd0a5d82fab
Pranav Nachanekar <pranav@Pranavs-MacBook-Pro.local>
2019-09-23 11:26:18 +0530

    Added buttons to linked docs

--- erpnext/crm/doctype/appointment/appointment.js
+++ erpnext/crm/doctype/appointment/appointment.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Appointment', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if(frm.doc.lead){
+			frm.add_custom_button(__(frm.doc.lead),()=>{
+				frappe.set_route("Form","Lead",frm.doc.lead)
+			})
+		}
+		if(frm.doc.calendar_event){
+			frm.add_custom_button(__(frm.doc.calendar_event),()=>{
+				frappe.set_route("Form","Event",frm.doc.calendar_event)
+			})
+		}
+	}
 });
