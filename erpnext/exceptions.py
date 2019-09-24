58d79f75faf3a12f9f3bbf3d9cba4c5439cd0d81
Deepesh Garg <42651287+deepeshgarg007@users.noreply.github.com>
2019-09-24 11:44:52 +0530

    fix: Travis (develop) (#19140)
    * fix: Travis
    
    * fix: Syntax Error

--- erpnext/exceptions.py
+++ erpnext/exceptions.py
@@ -5,4 +5,3 @@ import frappe
 class PartyFrozen(frappe.ValidationError): pass
 class InvalidAccountCurrency(frappe.ValidationError): pass
 class InvalidCurrency(frappe.ValidationError): pass
-class PartyDisabled(frappe.ValidationError):pass
