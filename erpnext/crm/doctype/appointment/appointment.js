9c4550d756a945137773ca89e74d6afd68674ec3
Pranav Nachanekar <pranav@Pranavs-MacBook-Pro.local>
2019-09-25 16:01:48 +0530

    fix codacy

--- erpnext/crm/doctype/appointment/appointment.js
+++ erpnext/crm/doctype/appointment/appointment.js
@@ -5,13 +5,13 @@ frappe.ui.form.on('Appointment', {
 	refresh: function(frm) {
 		if(frm.doc.lead){
 			frm.add_custom_button(__(frm.doc.lead),()=>{
-				frappe.set_route("Form","Lead",frm.doc.lead)
-			})
+				frappe.set_route("Form","Lead",frm.doc.lead);
+			});
 		}
 		if(frm.doc.calendar_event){
 			frm.add_custom_button(__(frm.doc.calendar_event),()=>{
-				frappe.set_route("Form","Event",frm.doc.calendar_event)
-			})
+				frappe.set_route("Form","Event",frm.doc.calendar_event);
+			});
 		}
 	}
 });
