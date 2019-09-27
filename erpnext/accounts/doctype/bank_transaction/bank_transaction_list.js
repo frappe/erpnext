2f94b8c52df442690c1237168182c1faa9bd46a8
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-27 15:09:25 +1200

    Update bank_transaction_list.js

--- erpnext/accounts/doctype/bank_transaction/bank_transaction_list.js
+++ erpnext/accounts/doctype/bank_transaction/bank_transaction_list.js
@@ -5,7 +5,7 @@ frappe.listview_settings['Bank Transaction'] = {
 	add_fields: ["unallocated_amount", "allocated_amount"],
 	get_indicator: function(doc) {
 		if(flt(doc.unallocated_amount)>0 && flt(doc.allocated_amount)> 0 ) {
-			return [__("Partially Reconciled"), "orange", filters = [["Bank Transaction", "unallocated_amount", ">", "0"],["Bank Transaction", "allocated_amount", ">", "0"]]];
+			return [__("Partially Reconciled"), "orange", [["Bank Transaction", "unallocated_amount", ">", "0"],["Bank Transaction", "allocated_amount", ">", "0"]]];
 		} else if (flt(doc.unallocated_amount)>0) {
 			return [__("Unreconciled"), "red", "unallocated_amount,>,0"];
 		} else if(flt(doc.unallocated_amount)<=0) {
