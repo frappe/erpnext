a9ced267b195610a75e5cd9ea8f87379dc8610b3
Saif Ur Rehman <saifi0102@gmail.com>
2019-09-28 19:22:08 +0500

    fix: auto_fetch_serial_number based on batch_no

--- erpnext/stock/doctype/serial_no/serial_no.py
+++ erpnext/stock/doctype/serial_no/serial_no.py
@@ -471,11 +471,14 @@ def get_delivery_note_serial_no(item_code, qty, delivery_note):
 	return serial_nos
 
 @frappe.whitelist()
-def auto_fetch_serial_number(qty, item_code, warehouse):
-	serial_numbers = frappe.get_list("Serial No", filters={
+def auto_fetch_serial_number(qty, item_code, warehouse, batch_no=None):
+	filters = {
 		"item_code": item_code,
 		"warehouse": warehouse,
 		"delivery_document_no": "",
 		"sales_invoice": ""
-	}, limit=qty, order_by="creation")
+	}
+	if batch_no:
+		filters['batch_no'] = batch_no
+	serial_numbers = frappe.get_list("Serial No", filters=filters, limit=qty, order_by="creation")
 	return [item['name'] for item in serial_numbers]
