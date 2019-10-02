441d3d05a9b1e6a04fd97d4e4115be6a234284e7
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-10-02 12:20:46 +0530

    fix: check for duplicate project name after submitting prompt

--- erpnext/selling/doctype/sales_order/sales_order_list.js
+++ erpnext/selling/doctype/sales_order/sales_order_list.js
@@ -1,6 +1,6 @@
 frappe.listview_settings['Sales Order'] = {
 	add_fields: ["base_grand_total", "customer_name", "currency", "delivery_date",
-		"per_delivered", "per_billed", "status", "order_type", "name"],
+		"per_delivered", "per_billed", "status", "order_type", "name", "skip_delivery_note"],
 	get_indicator: function (doc) {
 		if (doc.status === "Closed") {
 			// Closed
@@ -10,7 +10,7 @@ frappe.listview_settings['Sales Order'] = {
 			return [__("On Hold"), "orange", "status,=,On Hold"];
 		} else if (doc.status === "Completed") {
 			return [__("Completed"), "green", "status,=,Completed"];
-		} else if (!doc.skip_delivery_note_creation && flt(doc.per_delivered, 6) < 100) {
+		} else if (!doc.skip_delivery_note && flt(doc.per_delivered, 6) < 100) {
 			if (frappe.datetime.get_diff(doc.delivery_date) < 0) {
 			// not delivered & overdue
 				return [__("Overdue"), "red",
@@ -33,7 +33,7 @@ frappe.listview_settings['Sales Order'] = {
 			// to bill
 			return [__("To Bill"), "orange",
 				"per_delivered,=,100|per_billed,<,100|status,!=,Closed"];
-		} else if (doc.skip_delivery_note_creation && flt(doc.per_billed, 6) < 100){
+		} else if (doc.skip_delivery_note && flt(doc.per_billed, 6) < 100){
 			return [__("To Bill"), "orange", "per_billed,<,100|status,!=,Closed"];
 		}
 	},
