6f71b2d8e389af8aa44a51035ee1946973b545d1
NahuelOperto <Nahuel713.-@hotmail.com>
2019-10-04 11:39:58 -0300

    remove replace for test enviroment

--- erpnext/erpnext_integrations/doctype/whatsapp_settings/whatsapp_settings.py
+++ erpnext/erpnext_integrations/doctype/whatsapp_settings/whatsapp_settings.py
@@ -28,7 +28,7 @@ def get_pdf_for_whatsapp(doctype, name, key):
 def get_url_for_whatsapp(doctype, name):
     doc = frappe.get_doc(doctype, name)
     return "{url}/api/method/erpnext.erpnext_integrations.doctype.whatsapp_settings.whatsapp_settings.get_pdf_for_whatsapp?doctype={doctype}&name={name}&key={key}".format(
-        url=frappe.utils.get_url().replace("https", "http"),
+        url=frappe.utils.get_url(),
         doctype=doctype,
         name=name,
         key=doc.get_signature()
