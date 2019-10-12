f6eba03c31f4d59107e2c5f97e5957f6b2911ab7
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-10-12 19:33:11 +0530

    feat(Sales Order): Allow to skip delivery note creation (#19222)
    * feat(sales-order): add option to skip delivery note creation for order type maintenance
    
    * fix(sales-order): hide delivery date on setting skip delivery note creation
    
    * fix: allow skipping delivery note for all order types
    
    * fix: bypass delivery note creation on setting skip delivery note creation
    
    * fix: show skip delivery note in list view
    
    * fix: check for duplicate project name after submitting prompt
    
    * fix:  toggle delivery note reqd for skip delivery note
    
    * fix: update status based on skip delivery note

--- erpnext/manufacturing/doctype/work_order/work_order.py
+++ erpnext/manufacturing/doctype/work_order/work_order.py
@@ -64,7 +64,8 @@ class WorkOrder(Document):
 				from `tabSales Order` so
 				inner join `tabSales Order Item` so_item on so_item.parent = so.name
 				left join `tabProduct Bundle Item` pk_item on so_item.item_code = pk_item.parent
-				where so.name=%s and so.docstatus = 1 and (
+				where so.name=%s and so.docstatus = 1
+					and so.skip_delivery_note  = 0 and (
 					so_item.item_code=%s or
 					pk_item.item_code=%s )
 			""", (self.sales_order, self.production_item, self.production_item), as_dict=1)
@@ -78,6 +79,7 @@ class WorkOrder(Document):
 					where so.name=%s
 						and so.name=so_item.parent
 						and so.name=packed_item.parent
+						and so.skip_delivery_note = 0
 						and so_item.item_code = packed_item.parent_item
 						and so.docstatus = 1 and packed_item.item_code=%s
 				""", (self.sales_order, self.production_item), as_dict=1)
