42b807202ec400530a7f3ceccfb4de320099b565
jiloysss <jangeles@bai.ph>
2019-10-14 14:21:05 +0800

    Expense Claim Detail Cost Center Changed Script in Expense Claim in Creating GL Entry Filterd Cost Center in Expense Claim Detail where it will only fetch child accounts Hid Cost Center in Expense Claim Added Deferred Expense Account checkbox in Expense Claim

--- erpnext/hr/doctype/expense_claim_type/expense_claim_type.js
+++ erpnext/hr/doctype/expense_claim_type/expense_claim_type.js
@@ -4,14 +4,15 @@
 frappe.ui.form.on("Expense Claim Type", {
 	refresh: function(frm){
 		frm.fields_dict["accounts"].grid.get_field("default_account").get_query = function(frm, cdt, cdn){
-			var d = locals[cdt][cdn];
-			return{
-				filters: {
-					"is_group": 0,
-					"root_type": "Expense",
-					'company': d.company
-				}
+		var d = locals[cdt][cdn];
+
+		return{
+			filters: {
+				"is_group": 0,
+				"root_type": cur_frm.doc.deferred_expense_account ? "Asset" : "Expense",
+				'company': d.company
 			}
 		}
+	}
 	}
 })
