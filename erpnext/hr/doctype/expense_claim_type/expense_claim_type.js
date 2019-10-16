3bd0b0dba49560f53c03278c12ca203b71e5d2b4
jiloysss <jangeles@bai.ph>
2019-10-16 16:32:24 +0800

    refactor: Expense Claim (js and py), Expense Claim Type (js), Added Deferred Expense Accounts checkbox field, Added cost center field in expense claim detail

--- erpnext/hr/doctype/expense_claim_type/expense_claim_type.js
+++ erpnext/hr/doctype/expense_claim_type/expense_claim_type.js
@@ -5,13 +5,14 @@ frappe.ui.form.on("Expense Claim Type", {
 	refresh: function(frm){
 		frm.fields_dict["accounts"].grid.get_field("default_account").get_query = function(frm, cdt, cdn){
 			var d = locals[cdt][cdn];
-			return{
+
+			return {
 				filters: {
 					"is_group": 0,
-					"root_type": "Expense",
+					"root_type": cur_frm.doc.deferred_expense_account ? "Asset" : "Expense",
 					'company': d.company
 				}
 			}
 		}
 	}
-})
+});
