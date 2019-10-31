28bd040d84da4a48bc9cfe0ea726d3cca543e821
Andy Zhu <andy007yan@gmail.com>
2019-11-01 09:40:16 +1300

    msg error fix

--- erpnext/accounts/page/bank_reconciliation/bank_reconciliation.py
+++ erpnext/accounts/page/bank_reconciliation/bank_reconciliation.py
@@ -28,10 +28,10 @@ def reconcile(bank_transaction, payment_doctype, payment_name):
 		frappe.throw(_("The selected payment entry should be linked with a creditor bank transaction"))
 
 	if (payment_doctype == 'Payment Entry' and payment_entry.unallocated_amount > transaction.unallocated_amount):
-		frappe.throw(_("This Payment_entry's unallocated_amount is greater than this bank transactions's unallocated_amount"))
+		frappe.throw(_("This Payment Entry's unallocated_amount is greater than this bank transactions's unallocated_amount"))
 
 	if (payment_doctype == 'Journal Entry' and payment_entry.total_amount > transaction.unallocated_amount):
-		frappe.throw(_("This Payment_entry's unallocated_amount is greater than this bank transactions's unallocated_amount"))
+		frappe.throw(_("This Journal Entry's total_amount is greater than this bank transactions's unallocated_amount"))
 
 	add_payment_to_transaction(transaction, payment_entry, gl_entry)
 
