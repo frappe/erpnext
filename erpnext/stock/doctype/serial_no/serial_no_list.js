c82aed071857ca2f20074704a24b0cd1801ecfa6
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-11-01 12:36:33 +0530

    fix: Add missing semicolon

--- erpnext/stock/doctype/serial_no/serial_no_list.js
+++ erpnext/stock/doctype/serial_no/serial_no_list.js
@@ -6,9 +6,9 @@ frappe.listview_settings['Serial No'] = {
 		} else if (doc.delivery_document_type) {
 			return [__("Delivered"), "green", "delivery_document_type,is,set|is_cancelled,=,No"];
 		} else if (doc.warranty_expiry_date && frappe.datetime.get_diff(doc.warranty_expiry_date, frappe.datetime.nowdate()) <= 0) {
-			return [__("Expired"), "red", "warranty_expiry_date,not in,|warranty_expiry_date,<=,Today|delivery_document_type,is,not set|is_cancelled,=,No"]
+			return [__("Expired"), "red", "warranty_expiry_date,not in,|warranty_expiry_date,<=,Today|delivery_document_type,is,not set|is_cancelled,=,No"];
 		} else {
 			return [__("Active"), "green", "delivery_document_type,is,not set|is_cancelled,=,No"];
-		};
+		}
 	}
 };
