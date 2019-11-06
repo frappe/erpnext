dbcacc0922bd5d832c0647ab3f914b13af6b3a91
Marica <maricadsouza221197@gmail.com>
2019-11-06 18:12:29 +0530

    fix: Added 'status' field in Payment Entry (#19507)

---
+++ erpnext/patches/v12_0/set_payment_entry_status.py
@@ -0,0 +1,8 @@
+import frappe
+
+def execute():
+	frappe.db.sql("""update `tabPayment Entry` set status = CASE
+		WHEN docstatus = 1 THEN 'Submitted'
+		WHEN docstatus = 2 THEN 'Cancelled'
+		ELSE 'Draft'
+		END;""")
