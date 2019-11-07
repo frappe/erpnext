efe5a509dfafa4476e61b3f49b603c8d6f580d9c
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-11-07 14:15:33 +0530

    fix: remove unused woocommerce_check custom field

--- erpnext/erpnext_integrations/doctype/woocommerce_settings/woocommerce_settings.py
+++ erpnext/erpnext_integrations/doctype/woocommerce_settings/woocommerce_settings.py
@@ -23,26 +23,23 @@ class WoocommerceSettings(Document):
 			for doctype in ["Customer", "Sales Order", "Item", "Address"]:
 				df = dict(fieldname='woocommerce_id', label='Woocommerce ID', fieldtype='Data', read_only=1, print_hide=1)
 				create_custom_field(doctype, df)
-				df = dict(fieldname='woocommerce_check', label='Woocommerce Check', fieldtype='Check', read_only=1, print_hide=1)
-				create_custom_field(doctype, df)
 
 			for doctype in ["Customer", "Address"]:
 				df = dict(fieldname='woocommerce_email', label='Woocommerce Email', fieldtype='Data', read_only=1, print_hide=1)
 				create_custom_field(doctype, df)
 			
-			if not frappe.get_value("Item Group",{"name": _("WooCommerce Products")}):
+			if not frappe.get_value("Item Group", {"name": _("WooCommerce Products")}):
 				item_group = frappe.new_doc("Item Group")
 				item_group.item_group_name = _("WooCommerce Products")
 				item_group.parent_item_group = get_root_of("Item Group")
-				item_group.save()
+				item_group.insert()
 
 		elif not self.enable_sync:
-			names = ["Customer-woocommerce_id", "Sales Order-woocommerce_id", "Item-woocommerce_id", "Address-woocommerce_id",
-			"Customer-woocommerce_check", "Sales Order-woocommerce_check", "Item-woocommerce_check", "Address-woocommerce_check", "Customer-woocommerce_email", "Address-woocommerce_email"]
+			names = ["Customer-woocommerce_id", "Sales Order-woocommerce_id", "Item-woocommerce_id", "Address-woocommerce_id", "Customer-woocommerce_email", "Address-woocommerce_email"]
 
 			# delete
 			for name in names:
-				frappe.delete_doc("Custom Field",name)
+				frappe.delete_doc("Custom Field", name)
 			
 			frappe.delete_doc("Item Group", _("WooCommerce Products"))
 
