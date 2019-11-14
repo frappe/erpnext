5ab47142f708226303295002dd054a8837563a40
Rohan Bansal <rohan@digithinkit.com>
2019-11-14 16:59:40 +0530

    fix: handle purchase returns

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -1763,7 +1763,12 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 erpnext.show_serial_batch_selector = function (frm, d, callback, on_close, show_dialog) {
 	let warehouse, receiving_stock, existing_stock;
 	if (frm.doc.is_return) {
-		receiving_stock = true;
+		if (["Purchase Receipt", "Purchase Invoice"].includes(frm.doc.doctype)) {
+			existing_stock = true;
+			warehouse = d.warehouse;
+		} else if (["Delivery Note", "Sales Invoice"].includes(frm.doc.doctype)) {
+			receiving_stock = true;
+		}
 	} else {
 		if (frm.doc.doctype == "Stock Entry") {
 			if (frm.doc.purpose == "Material Receipt") {
