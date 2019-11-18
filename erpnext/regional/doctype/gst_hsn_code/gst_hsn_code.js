088a736932d3b5a7259a2c8cc45555dc5b8fa07d
Anurag Mishra <mishranaman123@gmail.com>
2019-11-18 11:47:05 +0530

    feat: enqueued task of updating all items

--- erpnext/regional/doctype/gst_hsn_code/gst_hsn_code.js
+++ erpnext/regional/doctype/gst_hsn_code/gst_hsn_code.js
@@ -6,7 +6,7 @@ frappe.ui.form.on('GST HSN Code', {
 		if(! frm.doc.__islocal && frm.doc.taxes.length){
 			frm.add_custom_button(__('Update Taxes for Items'), function(){
 				frappe.confirm(
-					'Are you sure? It will overwrite taxes for all items.',
+					'Are you sure? It will overwrite taxes for all items with HSN Code <b>'+frm.doc.name+'</b>.',
 					function(){
 						frappe.call({
 							args:{
--- erpnext/regional/doctype/gst_hsn_code/gst_hsn_code.py
+++ erpnext/regional/doctype/gst_hsn_code/gst_hsn_code.py
@@ -16,7 +16,10 @@ def update_taxes_in_item_master(taxes, hsn_code):
 	})
 
 	taxes = frappe.parse_json(taxes)
+	frappe.enqueue(update_item_document, items=items, taxes=taxes)
+	return 1
 
+def update_item_document(items, taxes):
 	for item in items:
 		item_to_be_updated=frappe.get_doc("Item", item.name)
 		item_to_be_updated.taxes = []
@@ -24,4 +27,3 @@ def update_taxes_in_item_master(taxes, hsn_code):
 			tax = frappe._dict(tax)
 			item_to_be_updated.append("taxes", {'item_tax_template': tax.item_tax_template, 'tax_category': tax.tax_category})
 			item_to_be_updated.save()
-	return 1
