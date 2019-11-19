a667ebaeb67228bcefa1c6d1f484d9bb7ba8cb27
Mitchy25 <Mitch@Fxmed.co.nz>
2019-11-19 08:33:08 +0800

    WooCommerce

--- erpnext/erpnext_integrations/connectors/woocommerce_connection.py
+++ erpnext/erpnext_integrations/connectors/woocommerce_connection.py
@@ -46,40 +46,44 @@ def _order(*args, **kwargs):
 		return "success"
 
 	if event == "created":
+		#Get user ID and check GST
 		raw_billing_data = fd.get("billing")
-		customer_woo_com_email = raw_billing_data.get("email")
+		metaDataList = fd.get("meta_data")
+		gstInclusive = "True"
+		for meta in metaDataList:
+			if meta.get("key") == "store_code":
+				customerID = meta.get("value")
+			elif meta.get("key") == "order_type" and meta.get("value") == "quick_order":
+				gstInclusive = "False"
 
-		if frappe.get_value("Customer",{"woocommerce_email": customer_woo_com_email}):
-			# Edit
-			link_customer_and_address(raw_billing_data,1)
+		if (customerID):
+			frappe.log_error(customerID)
+			link_customer_and_address(raw_billing_data,1, customerID)
 		else:
-			# Create
+			frappe.log_error("Cant find Customer")
 			link_customer_and_address(raw_billing_data,0)
-
 
 		items_list = fd.get("line_items")
 		for item in items_list:
-
-			item_woo_com_id = item.get("product_id")
-
-			if frappe.get_value("Item",{"woocommerce_id": item_woo_com_id}):
+			itemID = item.get("sku")
+			frappe.log_error(itemID)
+			if frappe.get_value("Item",{"name": itemID}):
 				#Edit
 				link_item(item,1)
 			else:
 				link_item(item,0)
 
-
-		customer_name = raw_billing_data.get("first_name") + " " + raw_billing_data.get("last_name")
-
-		new_sales_order = frappe.new_doc("Sales Order")
-		new_sales_order.customer = customer_name
+		#customer_name = raw_billing_data.get("first_name") + " " + raw_billing_data.get("last_name")
+		customer = frappe.get_doc("Customer",{"name": customerID})
+		newSI = frappe.new_doc("Sales Invoice")
+		newSI.customer = customer.name
 
 		created_date = fd.get("date_created").split("T")
-		new_sales_order.transaction_date = created_date[0]
+		newSI.transaction_date = created_date[0]
 
-		new_sales_order.po_no = fd.get("id")
-		new_sales_order.woocommerce_id = fd.get("id")
-		new_sales_order.naming_series = woocommerce_settings.sales_order_series or "SO-WOO-"
+		newSI.po_no = fd.get("id")
+		newSI.woocommerce_id = fd.get("id")
+		newSI.naming_series = "ACC-SINV-.YYYY.-"
 
 		placed_order_date = created_date[0]
 		raw_date = datetime.datetime.strptime(placed_order_date, "%Y-%m-%d")
@@ -87,88 +91,98 @@ def _order(*args, **kwargs):
 		order_delivery_date_str = raw_delivery_date.strftime('%Y-%m-%d')
 		order_delivery_date = str(order_delivery_date_str)
 
-		new_sales_order.delivery_date = order_delivery_date
+		newSI.delivery_date = order_delivery_date
 		default_set_company = frappe.get_doc("Global Defaults")
-		company = raw_billing_data.get("company") or default_set_company.default_company
+		company = default_set_company.default_company
 		found_company = frappe.get_doc("Company",{"name":company})
 		company_abbr = found_company.abbr
 
-		new_sales_order.company = company
+		newSI.company = company
+		
+		if customer:
+			if customer.territory == "WA":
+				warehouse = "Perth" + " - " + company_abbr
+			else:
+				warehouse = "Brisbane" + " - " + company_abbr
+		else:
+			warehouse = "Brisbane" + " - " + company_abbr
 
 		for item in items_list:
-			woocomm_item_id = item.get("product_id")
-			found_item = frappe.get_doc("Item",{"woocommerce_id": woocomm_item_id})
+			itemID = item.get("sku")
+			found_item = frappe.get_doc("Item",{"name": itemID})
+			
+			if gstInclusive == "True":
+				rate = item.get("price")
+				ordered_items_tax = item.get("total_tax")
+			else:
+				itemTax = item.get("taxes")
+				rate = float(item.get("price")) + float(itemTax[0].get("total"))
+				ordered_items_tax = rate * 0.1
 
-			ordered_items_tax = item.get("total_tax")
-
-			new_sales_order.append("items",{
+			newSI.append("items",{
 				"item_code": found_item.item_code,
 				"item_name": found_item.item_name,
 				"description": found_item.item_name,
 				"delivery_date":order_delivery_date,
-				"uom": woocommerce_settings.uom or _("Nos"),
+				"uom": "Unit",
 				"qty": item.get("quantity"),
-				"rate": item.get("price"),
-				"warehouse": woocommerce_settings.warehouse or "Stores" + " - " + company_abbr
+				"rate": rate,
+				"warehouse": warehouse
 				})
 
-			add_tax_details(new_sales_order,ordered_items_tax,"Ordered Item tax",0)
+			add_tax_details(newSI,ordered_items_tax,"Ordered Item tax",0)
 
 		# shipping_details = fd.get("shipping_lines") # used for detailed order
 		shipping_total = fd.get("shipping_total")
-		shipping_tax = fd.get("shipping_tax")
+		shipping_tax = int(float(shipping_total)) * 0.1
 
-		add_tax_details(new_sales_order,shipping_tax,"Shipping Tax",1)
-		add_tax_details(new_sales_order,shipping_total,"Shipping Total",1)
+		add_tax_details(newSI,shipping_tax,"Shipping Tax",1)
+		add_tax_details(newSI,shipping_total,"Shipping Total",1)
 
-		new_sales_order.submit()
+		newSI.submit()
 
 		frappe.db.commit()
 
-def link_customer_and_address(raw_billing_data,customer_status):
+def link_customer_and_address(raw_billing_data,customer_status, customerID):
 
+	#Check Customer Status
 	if customer_status == 0:
 		# create
-		customer = frappe.new_doc("Customer")
-		address = frappe.new_doc("Address")
+		frappe.log_error(raw_billing_data,"Customer doesn't exist")
 
-	if customer_status == 1:
+	elif customer_status == 1:
 		# Edit
+		customer = frappe.get_doc("Customer",{"name": customerID})
+
+		#Check Address
 		customer_woo_com_email = raw_billing_data.get("email")
-		customer = frappe.get_doc("Customer",{"woocommerce_email": customer_woo_com_email})
-		old_name = customer.customer_name
+		
+		addressSQL = frappe.db.sql("SELECT * FROM `tabAddress` WHERE woocommerce_email = '" + customer_woo_com_email + "'", as_dict=True)
 
-	full_name = str(raw_billing_data.get("first_name"))+ " "+str(raw_billing_data.get("last_name"))
-	customer.customer_name = full_name
-	customer.woocommerce_email = str(raw_billing_data.get("email"))
-	customer.save()
-	frappe.db.commit()
+		if (addressSQL):
+			frappe.log_error(addressSQL, "Found Address")
+		else:
+			frappe.log_error(addressSQL, "New Address")
+			address = frappe.new_doc("Address")
 
-	if customer_status == 1:
-		frappe.rename_doc("Customer", old_name, full_name)
-		address = frappe.get_doc("Address",{"woocommerce_email":customer_woo_com_email})
-		customer = frappe.get_doc("Customer",{"woocommerce_email": customer_woo_com_email})
+		address.address_line1 = raw_billing_data.get("address_1", "Not Provided")
+		address.address_line2 = raw_billing_data.get("address_2", "Not Provided")
+		address.city = raw_billing_data.get("city", "Not Provided")
+		address.woocommerce_email = str(raw_billing_data.get("email"))
+		address.address_type = "Shipping"
+		address.country = frappe.get_value("Country", filters={"code":raw_billing_data.get("country", "IN").lower()})
+		address.state =  raw_billing_data.get("state")
+		address.pincode =  str(raw_billing_data.get("postcode"))
+		address.phone = str(raw_billing_data.get("phone"))
+		address.email_id = str(raw_billing_data.get("email"))
 
-	address.address_line1 = raw_billing_data.get("address_1", "Not Provided")
-	address.address_line2 = raw_billing_data.get("address_2", "Not Provided")
-	address.city = raw_billing_data.get("city", "Not Provided")
-	address.woocommerce_email = str(raw_billing_data.get("email"))
-	address.address_type = "Shipping"
-	address.country = frappe.get_value("Country", filters={"code":raw_billing_data.get("country", "IN").lower()})
-	address.state =  raw_billing_data.get("state")
-	address.pincode =  str(raw_billing_data.get("postcode"))
-	address.phone = str(raw_billing_data.get("phone"))
-	address.email_id = str(raw_billing_data.get("email"))
+		address.append("links", {
+			"link_doctype": "Customer",
+			"link_name": customer.name
+		})
 
-	address.append("links", {
-		"link_doctype": "Customer",
-		"link_name": customer.customer_name
-	})
-
-	address.save()
-	frappe.db.commit()
-
-	if customer_status == 1:
+		address.save()
+		frappe.db.commit()
 
 		address = frappe.get_doc("Address",{"woocommerce_email":customer_woo_com_email})
 		old_address_title = address.name
@@ -178,29 +192,29 @@ def link_customer_and_address(raw_billing_data,customer_status):
 
 		frappe.rename_doc("Address",old_address_title,new_address_title)
 
-	frappe.db.commit()
+		frappe.db.commit()
 
 def link_item(item_data,item_status):
-	woocommerce_settings = frappe.get_doc("Woocommerce Settings")
+	#woocommerce_settings = frappe.get_doc("Woocommerce Settings")
 
 	if item_status == 0:
-		#Create Item
-		item = frappe.new_doc("Item")
+		#Create Item - Log Error
+		frappe.log_error(item_data,"Item doesn't exist")
 
-	if item_status == 1:
-		#Edit Item
-		item_woo_com_id = item_data.get("product_id")
-		item = frappe.get_doc("Item",{"woocommerce_id": item_woo_com_id})
+	#else if item_status == 1:
+	#	#Edit Item
+	#	itemID = item_data.get("sku")
+	#	item = frappe.get_doc("Item",{"name": itemID})
 
-	item.item_name = str(item_data.get("name"))
-	item.item_code = "woocommerce - " + str(item_data.get("product_id"))
-	item.woocommerce_id = str(item_data.get("product_id"))
-	item.item_group = _("WooCommerce Products")
-	item.stock_uom = woocommerce_settings.uom or _("Nos")
-	item.save()
-	frappe.db.commit()
+	#item.item_code = str(item_data.get("sku"))
+	#item.item_name = str(item_data.get("name"))
+	#item.brand = str(item_data.get("brand"))
+	#item.item_group = str(item_data.get("item_group"))
+	#item.stock_uom = str(item_data.get("stock_uom"))
+	#item.save()
+	#frappe.db.commit()
 
-def add_tax_details(sales_order,price,desc,status):
+def add_tax_details(newSI,price,desc,status):
 
 	woocommerce_settings = frappe.get_doc("Woocommerce Settings")
 
@@ -212,9 +226,10 @@ def add_tax_details(sales_order,price,desc,status):
 		# Shipping taxes
 		account_head_type = woocommerce_settings.f_n_f_account
 
-	sales_order.append("taxes",{
-							"charge_type":"Actual",
-							"account_head": account_head_type,
-							"tax_amount": price,
-							"description": desc
-							})
+	newSI.append("taxes",{
+			"charge_type":"Actual",
+			"account_head": account_head_type,
+			"tax_amount": price,
+			"description": desc
+			})
+	
