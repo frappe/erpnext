0964723557d0ea22da698b858c3ea03d8899b846
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-11-21 16:24:07 +0530

    fix: Update pactch

--- erpnext/patches/v12_0/update_price_or_product_discount.py
+++ erpnext/patches/v12_0/update_price_or_product_discount.py
@@ -3,4 +3,4 @@ import frappe
 
 def execute():
 	frappe.db.sql(""" UPDATE `tabPricing Rule` SET price_or_product_discount = 'Price'
-		WHERE price_or_product_discount IS NULL """)
+		WHERE ifnull(price_or_product_discount,'') = '' """)
