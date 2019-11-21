fb8c27e9cc0af47d48227ad68b040d2bb19eebec
Raffael Meyer <raffael@alyf.de>
2019-11-21 12:37:13 +0100

    refactor: create test data

--- erpnext/regional/report/datev/test_datev.py
+++ erpnext/regional/report/datev/test_datev.py
@@ -7,6 +7,10 @@ from unittest import TestCase
 import frappe
 from frappe.utils import getdate, today
 from frappe.test_runner import make_test_objects
+from erpnext.stock.doctype.item.test_item import create_item
+from erpnext.accounts.doctype.sales_invoice.test_sales_invoice import create_sales_invoice
+from erpnext.accounts.doctype.account.chart_of_accounts.chart_of_accounts import create_charts
+
 from erpnext.regional.report.datev.datev import validate
 from erpnext.regional.report.datev.datev import get_transactions
 from erpnext.regional.report.datev.datev import get_customers
@@ -20,56 +24,123 @@ from .datev_constants import Transactions
 from .datev_constants import DebtorsCreditors
 from .datev_constants import AccountNames
 from .datev_constants import QUERY_REPORT_COLUMNS
-from erpnext.accounts.doctype.sales_invoice.test_sales_invoice import create_sales_invoice
-from erpnext.accounts.doctype.account.chart_of_accounts.chart_of_accounts import create_charts
+
+def make_company(company_name, abbr):
+	if not frappe.db.exists("Company", company_name):
+		company = frappe.get_doc({
+			"doctype": "Company",
+			"company_name": company_name,
+			"abbr": abbr,
+			"default_currency": "EUR",
+			"create_chart_of_accounts_based_on": "Standard Template",
+			"chart_of_accounts": "SKR04 mit Kontonummern"
+		}).save()
+	else:
+		company = frappe.get_doc("Company", company_name)
+
+	return company
+
+def make_cost_center(cc_name, company):
+	if not frappe.db.exists("Cost Center", cc_name):
+		cc = frappe.get_doc({
+			"doctype": "Cost Center",
+			"company": company.name,
+			"cost_center_name": cc_name,
+			"is_group": 0,
+			"parent_cost_center": "{} - {}".format(company.name, company.abbr)
+		}).save()
+	else:
+		cc = frappe.get_doc("Cost Center", cc_name)
+
+	return cc
+
+def make_customer_with_account(customer_name, company):
+	acc_name = frappe.db.get_value("Account", {
+			"account_name": customer_name,
+			"company": company.name
+		}, "name")
+
+	if not acc_name:
+		acc = frappe.get_doc({
+			"parent_account": "1 - Forderungen aus Lieferungen und Leistungen - _TG",
+			"account_name": customer_name,
+			"account_type": "Receivable",
+			"account_number": "10001"
+		}).save()
+		acc_name = acc.name
+	
+	if not frappe.db.exists("Customer", customer_name):
+		customer = frappe.get_doc({
+			"doctype": "Customer",		
+			"customer_name": customer_name,
+			"customer_type": "Company",
+			"customer_group": "_Test Customer Group",
+			"territory": "_Test Territory",
+			"accounts": [{
+				"company": company.name,
+				"account": acc_name
+			}]
+		}).save()
+	else:
+		customer = frappe.get_doc("Customer", customer_name)
+
+	return customer
+
+def make_datev_settings(company):
+	if not frappe.db.exists("DATEV Settings", company.name):
+		frappe.get_doc({
+			"doctype": "DATEV Settings",
+			"client": company.name,
+			"client_number": "12345",
+			"consultant_number": "67890"
+		}).save()
 
 class TestDatev(TestCase):
 	def setUp(self):
-		self.company = "_Test GmbH"
+		self.company = make_company("_Test GmbH", "_TG")
+		self.cost_center = make_cost_center("_Test Cost Center", self.company)
+		self.customer = make_customer_with_account("_Test Kunde GmbH", self.company)
 		self.filters = {
-			"company": self.company,
+			"company": self.company.name,
 			"from_date": today(),
 			"to_date": today()
 		}
-		test_records_path = os.path.join(os.path.dirname(__file__), "test_records.json")
-		test_coa_path = os.path.join(os.path.dirname(__file__), "test_coa.json")
 
-		with open(test_records_path, "r") as test_records_file:
-			make_test_objects("Account", json.load(test_records_file))
+		make_datev_settings(self.company)
+		item = create_item("_Test Item", {"is_stock_item": 0})
+		income_account = frappe.db.get_value("Account", {
+				"account_number":"4200", 
+				"company": self.company.name
+			}, "name")
 
-		with open(test_coa_path, "r") as test_coa_file:
-			test_coa = json.load(test_coa_file)
-			create_charts(self.company, None, None, test_coa)
-		
-		customer = frappe.get_doc("Customer", "_Test Kunde GmbH")
-		customer.append("accounts", {
-			"company": self.company, 
-			"account": "10001 - _Test Kunde GmbH - _TG"
-		})
-		customer.save()
+		tax_account = frappe.db.get_value("Account", {
+				"account_number":"3806", 
+				"company": self.company.name
+			}, "name")
 
 		si = create_sales_invoice(
-			company=self.company,
+			company=self.company.name,
 			customer="_Test Kunde GmbH",
 			currency="EUR",
-			debit_to="10001 - _Test Kunde GmbH - _TG",
+			debit_to=self.customer.accounts[0].account,
 			income_account="4200 - Erlöse - _TG",
 			do_not_save=1
 		)
 
 		si.append("items", {
-			"item_code": "_Test Item",
+			"item_code": item.name,
 			"qty": 1,
 			"rate": 100,
-			"income_account": "4200 - Erlöse - _TG",
-			"cost_center": "_Test Cost Center - _TG"
+			"income_account": income_account,
+			"cost_center": self.cost_center.name
 		})
 
 		si.append("taxes", {
 			"charge_type": "On Net Total",
-			"account_head": "3806 - Umsatzsteuer 19% - _TG",
+			"account_head": tax_account,
 			"rate": 19
 		})
+
 		si.save()
 		si.submit()
 
@@ -82,11 +153,7 @@ class TestDatev(TestCase):
 			get_data -- Function that returns a list of dicts.
 			allowed_keys -- List of allowed keys
 			"""
-			data = get_data({
-				'company': '_Test GmbH',
-				'from_date': getdate(),
-				'to_date': getdate(),
-			}, as_dict=1)
+			data = get_data(self.filters, as_dict=1)
 			actual_set = set(data[0].keys())
 			allowed_set = set(allowed_keys)
 			return actual_set.issubset(allowed_set)
