4b6de585b97bf85c5a66c7fd4c9e2941d5c2535b
jiloysss <jangeles@bai.ph>
2019-11-22 12:15:53 +0800

    refactor: Expense Claim Cost Center

--- erpnext/hr/doctype/expense_claim/expense_claim.js
+++ erpnext/hr/doctype/expense_claim/expense_claim.js
@@ -38,6 +38,7 @@ cur_frm.add_fetch('employee','employee_name','employee_name');
 cur_frm.add_fetch('expense_type','description','description');
 
 cur_frm.cscript.onload = function(doc) {
+	set_schedule_date(doc)
 	if (doc.__islocal) {
 		cur_frm.set_value("posting_date", frappe.datetime.get_today());
 		cur_frm.cscript.clear_sanctioned(doc);
@@ -325,7 +326,12 @@ frappe.ui.form.on("Expense Claim", {
 				}
 			});
 		}
-	}
+	},
+	items_add: function(doc, cdt, cdn) {
+		var row = frappe.get_doc(cdt, cdn);
+
+		this.frm.script_manager.copy_from_first_row("expenses", row, ["cost_center"]);
+	},
 });
 
 frappe.ui.form.on("Expense Claim Detail", {
@@ -420,3 +426,16 @@ cur_frm.fields_dict['task'].get_query = function(doc) {
 		}
 	};
 };
+
+cur_frm.fields_dict["expenses"].grid.get_field("cost_center").get_query = function() {
+    return {
+        filters: {
+            "is_group": 0
+        }
+    }
+}
+function set_schedule_date(frm) {
+	if(frm.doc.schedule_date){
+		erpnext.utils.copy_value_in_all_rows(frm.doc, frm.doc.doctype, frm.doc.name, "items", "schedule_date");
+	}
+}
