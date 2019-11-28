e5d6c193a34d81de231fa934fc8af656c43bf2a0
marination <maricadsouza221197@gmail.com>
2019-11-28 19:16:59 +0530

    fix: Permission issue in Stock Entry

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -569,7 +569,7 @@ def get_stock_and_account_balance(account=None, posting_date=None, company=None)
 
 	warehouse_account = get_warehouse_account_map(company)
 
-	account_balance = get_balance_on(account, posting_date, in_account_currency=False)
+	account_balance = get_balance_on(account, posting_date, in_account_currency=False, ignore_account_permission=True)
 
 	related_warehouses = [wh for wh, wh_details in warehouse_account.items()
 		if wh_details.account == account and not wh_details.is_group]
