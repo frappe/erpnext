2fe0c98772435ad5bd00cafe5c7cb960c491f6e9
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-11-28 08:09:47 +0530

    fix: Path for quotation expiry method in hooks

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -302,7 +302,7 @@ scheduler_events = {
 		"erpnext.support.doctype.service_level_agreement.service_level_agreement.check_agreement_status",
 		"erpnext.crm.doctype.email_campaign.email_campaign.send_email_to_leads_or_contacts",
 		"erpnext.crm.doctype.email_campaign.email_campaign.set_email_campaign_status",
-		"erpnext.selling.doctype.quotation.set_expired_status"
+		"erpnext.selling.doctype.quotation.quotation.set_expired_status"
 	],
 	"daily_long": [
 		"erpnext.setup.doctype.email_digest.email_digest.send",
