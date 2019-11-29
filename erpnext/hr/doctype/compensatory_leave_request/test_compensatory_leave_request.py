0694c36feaf3c97841b2d4888c3e83c1f190ad4c
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-11-29 19:51:09 +0530

    test: updation of leave allocation on submit

--- erpnext/hr/doctype/compensatory_leave_request/test_compensatory_leave_request.py
+++ erpnext/hr/doctype/compensatory_leave_request/test_compensatory_leave_request.py
@@ -15,7 +15,7 @@ class TestCompensatoryLeaveRequest(unittest.TestCase):
 		frappe.db.sql(''' delete from `tabCompensatory Leave Request`''')
 		frappe.db.sql(''' delete from `tabLeave Ledger Entry`''')
 		frappe.db.sql(''' delete from `tabLeave Allocation`''')
-		frappe.db.sql(''' delete from `tabAttendance` where attendance_date=%s ''', (today()))
+		frappe.db.sql(''' delete from `tabAttendance` where attendance_date in {0} '''.format((today(), add_days(today(), -1)))) #nosec
 		create_leave_period(add_months(today(), -3), add_months(today(), 3), "_Test Company")
 		create_holiday_list()
 
@@ -33,6 +33,28 @@ class TestCompensatoryLeaveRequest(unittest.TestCase):
 		compensatory_leave_request.submit()
 
 		self.assertEqual(get_leave_balance_on(employee.name, compensatory_leave_request.leave_type, add_days(today(), 1)), before + 1)
+
+	def test_leave_allocation_update_on_submit(self):
+		employee = get_employee()
+		mark_attendance(employee, date=add_days(today(), -1))
+		compensatory_leave_request = get_compensatory_leave_request(employee.name, leave_date=add_days(today(), -1))
+		compensatory_leave_request.submit()
+
+		# leave allocation creation on submit
+		leaves_allocated = frappe.db.get_value('Leave Allocation', {
+				'name': compensatory_leave_request.leave_allocation
+			}, ['total_leaves_allocated'])
+		self.assertEqual(leaves_allocated, 1)
+
+		mark_attendance(employee)
+		compensatory_leave_request = get_compensatory_leave_request(employee.name)
+		compensatory_leave_request.submit()
+
+		# leave allocation updates on submission of second compensatory leave request
+		leaves_allocated = frappe.db.get_value('Leave Allocation', {
+				'name': compensatory_leave_request.leave_allocation
+			}, ['total_leaves_allocated'])
+		self.assertEqual(leaves_allocated, 2)
 
 	def test_creation_of_leave_ledger_entry_on_submit(self):
 		''' check creation of leave ledger entry on submission of leave request '''
@@ -99,7 +121,12 @@ def create_holiday_list():
 			{
 				"description": "Test Holiday",
 				"holiday_date": today()
-		}],
+			},
+			{
+				"description": "Test Holiday 1",
+				"holiday_date": add_days(today(), -1)
+			}
+		],
 		"holiday_list_name": "_Test Compensatory Leave"
 	})
 	holiday_list.save()
