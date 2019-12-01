c131bd1b257ddd13fef98ef0527da63ae86e1255
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-12-02 01:09:59 +0530

    feat(marketplace): edit item dialog

---
+++ erpnext/public/js/hub/components/edit_details_dialog.js
@@ -0,0 +1,45 @@
+function EditDetailsDialog(primary_action, defaults) {
+	let dialog = new frappe.ui.Dialog({
+		title: __('Update Details'),
+		fields: [
+			{
+				"label": "Item Name",
+				"fieldname": "item_name",
+				"fieldtype": "Data",
+				"default": defaults.item_name,
+				"reqd": 1
+			},
+			{
+				"label": "Hub Category",
+				"fieldname": "hub_category",
+				"fieldtype": "Autocomplete",
+				"default": defaults.hub_category,
+				"options": [],
+				"reqd": 1
+			},
+			{
+				"label": "Description",
+				"fieldname": "description",
+				"fieldtype": "Text",
+				"default": defaults.description,
+				"options": [],
+				"reqd": 1
+			}
+		],
+		primary_action_label: primary_action.label || __('Update Details'),
+		primary_action: primary_action.fn,
+	});
+
+	hub.call('get_categories')
+		.then(categories => {
+			categories = categories.map(d => d.name);
+			dialog.fields_dict.hub_category.df.options = categories;
+			dialog.fields_dict.hub_category.set_options();
+		});
+
+	return dialog;
+}
+
+export {
+	EditDetailsDialog
+};
