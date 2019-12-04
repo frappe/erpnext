caec2bfe8ff99bcbdd30496b7187c1d5d52a0af2
Nabin Hait <nabinhait@gmail.com>
2019-12-04 11:46:24 +0530

    Update opportunity.py

--- erpnext/crm/doctype/opportunity/opportunity.py
+++ erpnext/crm/doctype/opportunity/opportunity.py
@@ -130,10 +130,10 @@ class Opportunity(TransactionBase):
 
 	def has_lost_quotation(self):
 		lost_quotation = frappe.db.sql("""
-			select q.name
-			from `tabQuotation` q
-			where q.docstatus=1
-				and q.opportunity =%s and q.status = 'Lost'
+			select name
+			from `tabQuotation`
+			where docstatus=1
+				and opportunity =%s and status = 'Lost'
 			""", self.name)
 		if lost_quotation:
 			if self.has_active_quotation():
