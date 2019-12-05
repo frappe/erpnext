66b11a755589e4f941122b2f0a7c5fdb81f75656
Parth Kharwar <parth@digithinkit.com>
2019-12-05 12:59:45 +0530

    fix: patch changes to improve speed

--- erpnext/patches/v12_0/set_employee_preferred_emails.py
+++ erpnext/patches/v12_0/set_employee_preferred_emails.py
@@ -2,8 +2,15 @@ import frappe
 
 
 def execute():
-	employees = frappe.get_all("Employee")
+	employees = frappe.get_all("Employee",
+		filters={"prefered_email": ""},
+		fields=["name", "prefered_contact_email", "company_email", "personal_email", "user_id"])
+
 	for employee in employees:
-		employee_doc = frappe.get_doc("Employee", employee)
-		employee_doc.set_preferred_email()
-		employee_doc.save()
+		preferred_email_field = frappe.scrub(employee.prefered_contact_email)
+
+		if not preferred_email_field:
+			continue
+
+		preferred_email = employee.get(preferred_email_field)
+		frappe.db.set_value("Employee", employee.name, "prefered_email", preferred_email, update_modified=False)
