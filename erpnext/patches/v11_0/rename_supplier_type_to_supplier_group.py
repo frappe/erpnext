1e4ea466d3a58c2433006cdccf919a7acd0763fa
Gavin D'souza <gavin18d@gmail.com>
2019-12-11 09:36:17 +0530

    style: update/standardize rename_doc usage

--- erpnext/patches/v11_0/rename_supplier_type_to_supplier_group.py
+++ erpnext/patches/v11_0/rename_supplier_type_to_supplier_group.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 import frappe
-from frappe.model.rename_doc import rename_doc
 from frappe.model.utils.rename_field import rename_field
 from frappe import _
 from frappe.utils.nestedset import rebuild_tree
@@ -9,7 +8,7 @@ def execute():
 	if frappe.db.table_exists("Supplier Group"):
 		frappe.reload_doc('setup', 'doctype', 'supplier_group')
 	elif frappe.db.table_exists("Supplier Type"):
-		rename_doc("DocType", "Supplier Type", "Supplier Group", force=True)
+		frappe.rename_doc("DocType", "Supplier Type", "Supplier Group", force=True)
 		frappe.reload_doc('setup', 'doctype', 'supplier_group')
 		frappe.reload_doc("accounts", "doctype", "pricing_rule")
 		frappe.reload_doc("accounts", "doctype", "tax_rule")
