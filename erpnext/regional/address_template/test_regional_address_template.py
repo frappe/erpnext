00c77ca91e76978a12cbe5c667da44baa62223c0
Raffael Meyer <raffael@alyf.de>
2019-12-11 21:49:50 +0100

    fix: ensure country exists

--- erpnext/regional/address_template/test_regional_address_template.py
+++ erpnext/regional/address_template/test_regional_address_template.py
@@ -5,11 +5,29 @@ import frappe
 from erpnext.regional.address_template.setup import get_address_templates
 from erpnext.regional.address_template.setup import update_address_template
 
+def ensure_country(country):
+	if frappe.db.exists("Country", country):
+		return frappe.get_doc("Country", country)
+	else:
+		c = frappe.get_doc({
+			"doctype": "Country",
+			"country_name": country
+		})
+		c.insert()
+		return c
+
 class TestRegionalAddressTemplate(TestCase):
 	def test_get_address_templates(self):
 		templates = get_address_templates()
 		self.assertIsInstance(templates, list)
 		self.assertIsInstance(templates[0], dict)
+
+	def test_create_address_template(self):
+		"""Create a new Address Template."""
+		country = ensure_country("Germany")
+		update_address_template(country.name, "TEST")
+		doc = frappe.get_doc("Address Template", country.name)
+		self.assertEqual(doc.template, "TEST")
 
 	def test_update_address_template(self):
 		template = "TEST {{ address_line1 }}"
