c7ce597b7f391db72b834b8644755a7ac9eb1196
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-12-27 14:48:14 +0530

    fix: return when Hub Tracked Item is empty

--- erpnext/patches/v12_0/set_published_in_hub_tracked_item.py
+++ erpnext/patches/v12_0/set_published_in_hub_tracked_item.py
@@ -3,7 +3,7 @@ import frappe
 
 def execute():
 	frappe.reload_doc("Hub Node", "doctype", "Hub Tracked Item")
-	if frappe.db.a_row_exists("Hub Tracked Item"):
+	if not frappe.db.a_row_exists("Hub Tracked Item"):
 		return
 
 	frappe.db.sql('''
