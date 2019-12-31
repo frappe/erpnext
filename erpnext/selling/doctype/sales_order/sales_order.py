2d293c585e7091439ab1c34ceb7e06c73bbda10d
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-12-31 19:15:37 +0530

    fix(Subscription): pass start_date while computing next_schedu… (#20159)

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -416,7 +416,7 @@ class SalesOrder(SellingController):
 
 		def _get_delivery_date(ref_doc_delivery_date, red_doc_transaction_date, transaction_date):
 			delivery_date = get_next_schedule_date(ref_doc_delivery_date,
-				auto_repeat_doc.frequency, cint(auto_repeat_doc.repeat_on_day))
+				auto_repeat_doc.frequency, auto_repeat_doc.start_date, cint(auto_repeat_doc.repeat_on_day))
 
 			if delivery_date <= transaction_date:
 				delivery_date_diff = frappe.utils.date_diff(ref_doc_delivery_date, red_doc_transaction_date)
