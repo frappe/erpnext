d76d05126e2e339ff7f13b5974c0297bf9a2801c
Nikkita Bersan <nbersan@crane-cam.epfl.pix4d>
2020-01-07 09:05:55 +0000

    test

--- erpnext/stock/doctype/serial_no/serial_no_list.js
+++ erpnext/stock/doctype/serial_no/serial_no_list.js
@@ -1,9 +1,9 @@
 frappe.listview_settings['Serial No'] = {
 	add_fields: ["status", "item_code", "warehouse", "warranty_expiry_date", "delivery_document_type"],
 	get_indicator: (doc) => {
-		if (doc.status) {
+		/*if (doc.status) {
 			return [__("Used"), "red", "status,=,Used"];
-		} else if (doc.status) {
+		} else*/ if (doc.status) {
 			return [__("Free"), "green", "status,=,Free"];
 		} else if (doc.status) {
 			return [__("Broken"), "red", "status,=,Broken"];
