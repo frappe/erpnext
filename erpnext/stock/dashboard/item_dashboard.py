4f36a98588b445ef39ded3bed4edd36553abc6b7
Nikkita Bersan <nbersan@crane-cam.epfl.pix4d>
2020-01-07 11:42:41 +0000

    test

--- erpnext/stock/dashboard/item_dashboard.py
+++ erpnext/stock/dashboard/item_dashboard.py
@@ -4,14 +4,14 @@ import frappe
 from frappe.model.db_query import DatabaseQuery
 
 @frappe.whitelist()
-def get_data(item_code=None, status=None, item_group=None,
+def get_data(item_code=None, warehouse=None, item_group=None,
 	start=0, sort_by='actual_qty', sort_order='desc'):
 	'''Return data to render the item dashboard'''
 	filters = []
 	if item_code:
 		filters.append(['item_code', '=', item_code])
-	if status:
-		filters.append(['status', '=', status])
+	if warehouse:
+		filters.append(['warehouse', '=', warehouse])
 	if item_group:
 		lft, rgt = frappe.db.get_value("Item Group", item_group, ["lft", "rgt"])
 		items = frappe.db.sql_list("""
@@ -23,12 +23,12 @@ def get_data(item_code=None, status=None, item_group=None,
 	try:
 		# check if user has any restrictions based on user permissions on warehouse
 		if DatabaseQuery('Warehouse', user=frappe.session.user).build_match_conditions():
-			filters.append(['Warehouse', 'in', [w.name for w in frappe.get_list('Warehouse')]])
+			filters.append(['warehouse', 'in', [w.name for w in frappe.get_list('Warehouse')]])
 	except frappe.PermissionError:
 		# user does not have access on warehouse
 		return []
 
-	items = frappe.db.get_all('Bin', fields=['item_code', 'status', 'projected_qty',
+	items = frappe.db.get_all('Bin', fields=['item_code', 'warehouse', 'projected_qty',
 			'reserved_qty', 'reserved_qty_for_production', 'reserved_qty_for_sub_contract', 'actual_qty', 'valuation_rate'],
 		or_filters={
 			'projected_qty': ['!=', 0],
