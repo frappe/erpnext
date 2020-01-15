8a5f31da34809741d713c3b71ae34a6fa3d995e5
Marica <maricadsouza221197@gmail.com>
2020-01-15 19:24:39 +0530

    fix: Applied query on PO Supplier popup field in Material Request. (#20233)

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -371,6 +371,18 @@ def get_material_requests_based_on_supplier(supplier):
 		material_requests = []
 	return material_requests, supplier_items
 
+def get_default_supplier_query(doctype, txt, searchfield, start, page_len, filters):
+	doc = frappe.get_doc("Material Request", filters.get("doc"))
+	item_list = []
+	for d in doc.items:
+		item_list.append(d.item_code)
+
+	return frappe.db.sql("""select default_supplier
+		from `tabItem Default`
+		where parent in ({0}) and
+		default_supplier IS NOT NULL
+		""".format(', '.join(['%s']*len(item_list))),tuple(item_list))
+
 @frappe.whitelist()
 def make_supplier_quotation(source_name, target_doc=None):
 	def postprocess(source, target_doc):
