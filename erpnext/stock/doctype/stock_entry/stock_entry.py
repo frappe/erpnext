40380eb0729deb0ba3519409ad1eaa4511aabfc9
Yugandhara <yugandhara.b@indictranstech.com>
2020-01-18 11:24:31 +0530

    Fix: Spelling correction in throw message on stock entry(#20337)

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -614,7 +614,6 @@ class StockEntry(StockController):
 		if self.work_order and self.purpose == "Manufacture":
 			allowed_qty = wo_qty + (allowance_percentage/100 * wo_qty)
 			if self.fg_completed_qty > allowed_qty:
-				# Spelling of greater in throw message is corrected
 				frappe.throw(_("For quantity {0} should not be greater than work order quantity {1}")
 					.format(flt(self.fg_completed_qty), wo_qty))
 
