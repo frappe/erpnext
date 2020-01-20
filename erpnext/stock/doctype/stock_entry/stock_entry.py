4f3791930fee049aac61014e0f605cea7329b1ba
marination <maricadsouza221197@gmail.com>
2020-01-20 13:41:21 +0530

    fix: 'get_additonal_costs' is not defined

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -1401,8 +1401,7 @@ def get_work_order_details(work_order, company):
 		"use_multi_level_bom": work_order.use_multi_level_bom,
 		"wip_warehouse": work_order.wip_warehouse,
 		"fg_warehouse": work_order.fg_warehouse,
-		"fg_completed_qty": pending_qty_to_produce,
-		"additional_costs": get_additional_costs(work_order, fg_qty=pending_qty_to_produce, company=company)
+		"fg_completed_qty": pending_qty_to_produce
 	}
 
 def get_operating_cost_per_unit(work_order=None, bom_no=None):
