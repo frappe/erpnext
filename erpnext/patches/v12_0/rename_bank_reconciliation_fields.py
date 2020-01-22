d85f3ccf018d978a3d3c6299fcb466ef5e9a854a
racitup <richard@casesolved.co.uk>
2020-01-22 04:32:15 +0000

    fix: Bank Reconciliation Bank Account and Bank Account No field names

---
+++ erpnext/patches/v12_0/rename_bank_reconciliation_fields.py
@@ -0,0 +1,16 @@
+import frappe
+
+def _rename_single_field(**kwargs):
+	count = frappe.db.sql("SELECT COUNT(*) FROM tabSingles WHERE doctype='{doctype}' AND field='{new_name}';".format(**kwargs))[0][0]
+	if count == 0:
+		frappe.db.sql("UPDATE tabSingles SET field='{new_name}' WHERE doctype='{doctype}' AND field='{old_name}';".format(**kwargs))
+
+def execute():
+	BR  = "Bank Reconciliation"
+	AC  = "account"
+	BA  = "bank_account"
+	BAN = "bank_account_no"
+
+	_rename_single_field(doctype = BR, old_name = BA , new_name = AC)
+	_rename_single_field(doctype = BR, old_name = BAN, new_name = BA)
+	frappe.reload_doc("Accounts", "doctype", BR)
