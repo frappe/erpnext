4fca6cae349925bf248cb161e4fa8bc3186ad648
Saqib <thefalconx33@gmail.com>
2020-02-03 15:53:27 +0530

    feat: add tax category in pos profile (#20413)
    * feat: add tax category in pos profile
    
    * fix: review fixes

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -420,6 +420,9 @@ class SalesInvoice(SellingController):
 
 		if pos:
 			self.allow_print_before_pay = pos.allow_print_before_pay
+			
+			if not for_validate:
+				self.tax_category = pos.get("tax_category")
 
 			if not for_validate and not self.customer:
 				self.customer = pos.customer
