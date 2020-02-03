49951ba59e97852ee5842f9ed9cc41ee7d1a635c
0Pranav <prnvnachnekar@gmail.com>
2020-02-03 13:25:39 +0530

    fix: customer filter default values for customer group and territory

--- erpnext/selling/doctype/customer/customer_list.js
+++ erpnext/selling/doctype/customer/customer_list.js
@@ -1,3 +1,10 @@
 frappe.listview_settings['Customer'] = {
 	add_fields: ["customer_name", "territory", "customer_group", "customer_type", "image"],
+
+	onload: function(listview) {
+		frappe.route_options = {
+			"customer_group": "",
+			"territory": ""
+		};
+	}
 };
