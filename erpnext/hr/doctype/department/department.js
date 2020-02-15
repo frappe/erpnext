7c474130ea98789227e02537c65cc5b397fcd47d
anil pise <anil.p@indictrans.in>
2020-02-15 15:58:57 +0530

    fix: Removed Missing semicolon and Mixed spaces and tabs and Trailing spaces not allowed.

--- erpnext/hr/doctype/department/department.js
+++ erpnext/hr/doctype/department/department.js
@@ -4,8 +4,8 @@
 frappe.ui.form.on('Department', {
 	onload: function(frm) {
 		frm.set_query("parent_department", function(){
-	        return { "filters": [["Department", "is_group", "=", 1]]}
-	    });
+			return {"filters": [["Department", "is_group", "=", 1]]};
+		});
 	},
 	refresh: function(frm) {
 		// read-only for root department
@@ -13,7 +13,6 @@ frappe.ui.form.on('Department', {
 			frm.set_read_only();
 			frm.set_intro(__("This is a root department and cannot be edited."));
 		}
-		
 	},
 	validate: function(frm) {
 		if(frm.doc.name=="All Departments") {
