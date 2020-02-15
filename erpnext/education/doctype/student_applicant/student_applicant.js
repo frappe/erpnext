00d46dfaeda1721735ca334ba7d64a233b4f2535
Indictrans <jitendra.r@indictranstech.com>
2020-02-15 16:40:35 +0530

    fix: restricted duplicate Guardian on Student Applicant doctype (#20615)

--- erpnext/education/doctype/student_applicant/student_applicant.js
+++ erpnext/education/doctype/student_applicant/student_applicant.js
@@ -53,3 +53,16 @@ frappe.ui.form.on('Student Sibling', {
 		frm.add_fetch("student", "date_of_birth", "date_of_birth");
 	}
 });
+
+frappe.ui.form.on('Student Guardian', {
+	guardians_add: function(frm){
+		frm.fields_dict['guardians'].grid.get_field('guardian').get_query = function(doc){
+			let guardian_list = [];
+			if(!doc.__islocal) guardian_list.push(doc.guardian);
+			$.each(doc.guardians, function(idx, val){
+				if (val.guardian) guardian_list.push(val.guardian);
+			});
+			return { filters: [['Guardian', 'name', 'not in', guardian_list]] };
+		};
+	}
+});
