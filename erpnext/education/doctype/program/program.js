cfe6c70fb6d5854850e41fb703bd4fe7d9e1a361
Indictrans <jitendra.r@indictranstech.com>
2020-02-15 12:14:39 +0530

    fix: restricted duplicate courses on program doctype (#20593)

--- erpnext/education/doctype/program/program.js
+++ erpnext/education/doctype/program/program.js
@@ -4,5 +4,18 @@
 cur_frm.add_fetch('fee_structure', 'total_amount', 'amount');
 
 frappe.ui.form.on("Program", "refresh", function(frm) {
-	
+
+});
+
+frappe.ui.form.on('Program Course', {
+	courses_add: function(frm){
+		frm.fields_dict['courses'].grid.get_field('course').get_query = function(doc){
+			var course_list = [];
+			if(!doc.__islocal) course_list.push(doc.name);
+			$.each(doc.courses, function(idx, val){
+				if (val.course) course_list.push(val.course);
+			});
+			return { filters: [['Course', 'name', 'not in', course_list]] };
+		};
+	}
 });
