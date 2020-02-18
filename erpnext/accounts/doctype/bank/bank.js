3cb9f4ab73b3eaa65a50658f7ead1b9733654fdd
P-Froggy <60393001+P-Froggy@users.noreply.github.com>
2020-02-18 02:14:03 +0100

    Add address and contact fields to bank
    Address and contact fields are enabled for DocType Bank

--- erpnext/accounts/doctype/bank/bank.js
+++ erpnext/accounts/doctype/bank/bank.js
@@ -7,7 +7,18 @@ frappe.ui.form.on('Bank', {
 	},
 	refresh: function(frm) {
 		add_fields_to_mapping_table(frm);
-	}
+
+		frappe.dynamic_link = { doc: frm.doc, fieldname: 'name', doctype: 'Bank' }
+
+		frm.toggle_display(['address_html','contact_html'], !frm.doc.__islocal);
+
+		if (frm.doc.__islocal) {
+			frappe.contacts.clear_address_and_contact(frm);
+		}
+		else {
+			frappe.contacts.render_address_and_contact(frm);
+		}
+	},
 });
 
 
