7f0d0bb51b3ab781a20dd32462b142adcb4c5a30
DeyviTabora <taborapaz@gmail.com>
2020-03-05 09:05:02 -0600

    fix bug in sales funnel

--- erpnext/selling/page/sales_funnel/sales_funnel.js
+++ erpnext/selling/page/sales_funnel/sales_funnel.js
@@ -1,12 +1,12 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
-
 frappe.pages['sales-funnel'].on_page_load = function(wrapper) {
 	frappe.ui.make_app_page({
 		parent: wrapper,
 		title: __('Sales Funnel'),
 		single_column: true
 	});
+
 	wrapper.sales_funnel = new erpnext.SalesFunnel(wrapper);
 
 	frappe.breadcrumbs.add("Selling");
@@ -56,7 +56,7 @@ erpnext.SalesFunnel = class SalesFunnel {
 			.toggle(false)
 			.appendTo(this.elements.layout);
 
-		this.elements.funnel_wrapper = $('<div class="funnel-wrapper text-center"></div>')
+		this.elements.funnel_wrapper = $('<div class="funnel-wrapper text-center" style="margin-top:50px !important"></div>')
 			.appendTo(this.elements.layout);
 
 		this.company = frappe.defaults.get_user_default('company');
@@ -74,6 +74,7 @@ erpnext.SalesFunnel = class SalesFunnel {
 			} else {
 				me.elements[k].val(v);
 			}
+
 			me.elements[k].on("change", function() {
 				if (['from_date', 'to_date'].includes(k)) {
 					me.options[k] = frappe.datetime.user_to_str($(this).val()) != 'Invalid date' ? frappe.datetime.user_to_str($(this).val()) : frappe.datetime.get_today();
@@ -97,44 +98,81 @@ erpnext.SalesFunnel = class SalesFunnel {
 
 	get_data(btn) {
 		var me = this;
-		const method_map = {
-			"sales_funnel": "erpnext.selling.page.sales_funnel.sales_funnel.get_funnel_data",
-			"opp_by_lead_source": "erpnext.selling.page.sales_funnel.sales_funnel.get_opp_by_lead_source",
-			"sales_pipeline": "erpnext.selling.page.sales_funnel.sales_funnel.get_pipeline_data"
-		};
-		frappe.call({
-			method: method_map[this.options.chart],
-			args: {
-				from_date: this.options.from_date,
-				to_date: this.options.to_date,
-				company: this.company,
-				currency: this.currency
-			},
-			btn: btn,
-			callback: function(r) {
-				if(!r.exc) {
+		if (me.options.chart == 'sales_funnel') {
+			frappe.call({
+				method: "erpnext.selling.page.sales_funnel.sales_funnel.get_funnel_data",
+				args: {
+					from_date: this.options.from_date,
+					to_date: this.options.to_date,
+					company: this.company,
+					currency: this.currency
+				},
+				btn: btn,
+				callback: function (r) {
 					me.options.data = r.message;
-					if (me.options.data=='empty') {
+					if (me.options.data == 'empty') {
 						const $parent = me.elements.funnel_wrapper;
 						$parent.html(__('No data for this period'));
 					} else {
-						me.render();
+						me.render_funnel();
 					}
 				}
-			}
-		});
-	}
-
-	render() {
-		let me = this;
-		if (me.options.chart == 'sales_funnel'){
-			me.render_funnel();
-		} else if (me.options.chart == 'opp_by_lead_source'){
-			me.render_chart("Sales Opportunities by Source");
-		} else if (me.options.chart == 'sales_pipeline'){
-			me.render_chart("Sales Pipeline by Stage");
+			});
+		} else if (me.options.chart == 'opp_by_lead_source') {
+			frappe.call({
+				method: "erpnext.selling.page.sales_funnel.sales_funnel.get_opp_by_lead_source",
+				args: {
+					from_date: this.options.from_date,
+					to_date: this.options.to_date,
+					company: this.company,
+					currency: this.currency
+				},
+				btn: btn,
+				callback: function (r) {
+					debugger
+					me.options.data = r.message;
+					if (r.message.datasets == 0) {
+						const $parent = me.elements.funnel_wrapper;
+						$parent.html(__('No data for this period'));
+					} else {
+						me.render_opp_by_lead_source();
+					}
+				}
+			});
+		} else if (me.options.chart == 'sales_pipeline') {
+			frappe.call({
+				method: "erpnext.selling.page.sales_funnel.sales_funnel.get_pipeline_data",
+				args: {
+					from_date: this.options.from_date,
+					to_date: this.options.to_date,
+					company: this.company,
+					currency: this.currency
+				},
+				btn: btn,
+				callback: function (r) {
+					debugger
+					me.options.data = r.message;
+					if (r.message.datasets == 0 || r.message == "empty") {
+						const $parent = me.elements.funnel_wrapper;
+						$parent.html(__('No data for this period'));
+					} else {
+						me.render_pipeline();
+					}
+				}
+			});
 		}
 	}
+
+	// render() {
+	// 	let me = this;
+	// 	if (me.options.chart == 'sales_funnel'){
+	// 		me.render_funnel();
+	// 	} else if (me.options.chart == 'opp_by_lead_source'){
+	// 		me.render_opp_by_lead_source();
+	// 	} else if (me.options.chart == 'sales_pipeline'){
+	// 		me.render_pipeline();
+	// 	}
+	// }
 
 	render_funnel() {
 		var me = this;
@@ -158,6 +196,7 @@ erpnext.SalesFunnel = class SalesFunnel {
 			me.draw_triangle(x_start, x_mid, x_end, y, me.options.height);
 
 			y_old = y;
+
 			// new y
 			y = y + d.height;
 
@@ -165,7 +204,9 @@ erpnext.SalesFunnel = class SalesFunnel {
 			var half_side = (me.options.height - y) / Math.sqrt(3);
 			x_start = x_mid - half_side;
 			x_end = x_mid + half_side;
+
 			var y_mid = y_old + (y - y_old) / 2.0;
+
 			me.draw_legend(x_mid, y_mid, me.options.width, me.options.height, d.value + " - " + d.title);
 			me.draw_legend(x_mid, y_mid + 20, me.options.width, me.options.height, d.currency + " " + d.amount);
 		});
@@ -179,20 +220,21 @@ erpnext.SalesFunnel = class SalesFunnel {
 		// calculate width and height options
 		this.options.width = $(this.elements.funnel_wrapper).width() * 2.0 / 3.0;
 		this.options.height = (Math.sqrt(3) * this.options.width) / 2.0;
+
 		// calculate total weightage
 		// as height decreases, area decreases by the square of the reduction
 		// hence, compensating by squaring the index value
 		this.options.total_weightage = this.options.data.reduce(
 			function(prev, curr, i) { return prev + Math.pow(i+1, 2) * curr.value; }, 0.0);
-		// calculate height for each data
+
 		var arr = this.options.data;
 		var count = arr.length;
-
+		// calculate height for each data
 		$.each(this.options.data, function(i, d) {
 			var size = me.options.height / count;
 			d.height = size;
 			// d.height = me.options.height * d.value * Math.pow(i+1, 2) / me.options.total_weightage;
-			});
+		});
 
 		this.elements.canvas = $('<canvas></canvas>')
 			.appendTo(this.elements.funnel_wrapper.empty())
@@ -240,12 +282,7 @@ erpnext.SalesFunnel = class SalesFunnel {
 		context.fillText(__(title), width + 20, y_mid);
 	}
 
-	// click(this){
-	// 	// this.draw_legend.click()
-	// 	debugger
-	// }
-
-	render_chart(title) {
+	render_opp_by_lead_source() {
 		let me = this;
 		let currency = frappe.defaults.get_default("currency");
 
@@ -253,7 +290,7 @@ erpnext.SalesFunnel = class SalesFunnel {
 
 		const parent = me.elements.funnel_wrapper[0];
 		this.chart = new frappe.Chart(parent, {
-			title: title,
+			title: 'Sales Opportunities by Source',
 			height: 400,
 			data: chart_data,
 			type: 'bar',
@@ -263,6 +300,27 @@ erpnext.SalesFunnel = class SalesFunnel {
 			tooltipOptions: {
 				formatTooltipY: d => format_currency(d, currency),
 			}
+		});
+	}
+
+	render_pipeline() {
+		let me = this;
+		debugger
+		let currency = frappe.defaults.get_default("currency");
+
+		const chart_data = me.options.data ? me.options.data : null;
+		debugger
+
+		const parent = me.elements.funnel_wrapper[0];
+		const chart = new frappe.Chart(parent, {
+			title: 'Sales Pipeline by Stage',
+			height: 400,
+			data: chart_data,
+			type: 'pie',
+			// tooltipOptions: {
+			// 	formatTooltipY: d => format_currency(d, currency),
+			// },
+			colors: ['light-green', 'green']
 		});
 	}
 };
