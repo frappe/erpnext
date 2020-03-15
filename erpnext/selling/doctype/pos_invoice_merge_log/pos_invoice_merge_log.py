2e3c33a565834dc9b1aaf44a4ff741bb4a36798c
Saqib Ansari <nextchamp.saqib@gmail.com>
2020-03-15 11:51:42 +0530

    chore: use map_doc instead of get_mapped_doc for better perf

--- erpnext/selling/doctype/pos_invoice_merge_log/pos_invoice_merge_log.py
+++ erpnext/selling/doctype/pos_invoice_merge_log/pos_invoice_merge_log.py
@@ -7,7 +7,8 @@ import frappe
 from frappe import _
 from frappe.utils import cint, flt, add_months, today, date_diff, getdate, add_days, cstr, nowdate
 from frappe.model.document import Document
-from frappe.model.mapper import get_mapped_doc
+from frappe.model.mapper import map_doc
+from frappe.model import default_fields
 
 from six import iteritems
 
@@ -30,54 +31,26 @@ class POSInvoiceMergeLog(Document):
 				frappe.throw(_("Row #{}: POS Invoice {} has been {}").format(d.idx, d.pos_invoice, status))
 
 	def on_submit(self):
-		pos_invoices_name = [d.pos_invoice for d in self.pos_invoices]
-		pos_invoices_data = frappe.db.get_all('POS Invoice', 
-			filters={ "name": ["in", pos_invoices_name]}, fields=[
-				"name", "is_return", "redeem_loyalty_points", "loyalty_redemption_account", 
-				"loyalty_redemption_cost_center", "loyalty_points", "loyalty_amount"
-			])
+		pos_invoice_docs = [frappe.get_doc("POS Invoice", d.pos_invoice) for d in self.pos_invoices]
+
+		returns = [d for d in pos_invoice_docs if d.get('is_return') == 1]
+		sales = [d for d in pos_invoice_docs if d.get('is_return') == 0]
+
+		sales_invoice = self.process_merging_into_sales_invoice(sales)
 		
-
-		sales_invoice = self.process_merging_into_sales_invoice(pos_invoices_data)
-
-		has_pos_credit_note = any([d.is_return == 1 for d in pos_invoices_data])
-		if has_pos_credit_note:
-			credit_note = self.process_merging_into_credit_note(pos_invoices_data)
+		if len(returns):
+			credit_note = self.process_merging_into_credit_note(returns)
 		else:
 			credit_note = ""
 
-		self.save() # save consolidated_sales_invoice & consolidated_credit_note
+		self.save() # save consolidated_sales_invoice & consolidated_credit_note ref in merge log
 
 		self.update_pos_invoices(sales_invoice, credit_note)
 
 	def process_merging_into_sales_invoice(self, data):
 		sales_invoice = self.get_new_sales_invoice()
-
-		for doc in data:
-			if not doc.is_return:
-				loyalty_points_redeemed = 0
-				loyalty_amount = 0
-				if doc.redeem_loyalty_points:
-					sales_invoice.redeem_loyalty_points = True
-					sales_invoice.loyalty_redemption_account = doc.loyalty_redemption_account
-					sales_invoice.loyalty_redemption_cost_center = doc.loyalty_redemption_cost_center
-					loyalty_points_redeemed += doc.loyalty_points
-					loyalty_amount += doc.loyalty_amount
-
-				sales_invoice = get_mapped_doc("POS Invoice", doc.name, {
-					"POS Invoice": {
-						"doctype": "Sales Invoice",
-						"validation": {
-							"docstatus": ["=", 1]
-						}
-					},
-					"POS Invoice Item": {
-						"doctype": "Sales Invoice Item",
-					}
-				}, sales_invoice)
-		if sales_invoice.redeem_loyalty_points:
-			sales_invoice.loyalty_points = loyalty_points_redeemed
-			sales_invoice.loyalty_amount = loyalty_amount
+		
+		sales_invoice = self.merge_pos_invoice_into(sales_invoice, data)
 
 		sales_invoice.save()
 		sales_invoice.submit()
@@ -89,31 +62,7 @@ class POSInvoiceMergeLog(Document):
 		credit_note = self.get_new_sales_invoice()
 		credit_note.is_return = 1
 
-		for doc in data:
-			if doc.is_return:
-				loyalty_points_redeemed = 0
-				loyalty_amount = 0
-				if doc.redeem_loyalty_points:
-					credit_note.redeem_loyalty_points = True
-					credit_note.loyalty_redemption_account = doc.loyalty_redemption_account
-					credit_note.loyalty_redemption_cost_center = doc.loyalty_redemption_cost_center
-					loyalty_points_redeemed += doc.loyalty_points
-					loyalty_amount += doc.loyalty_amount
-
-				credit_note = get_mapped_doc("POS Invoice", doc.name, {
-					"POS Invoice": {
-						"doctype": "Sales Invoice",
-						"validation": {
-							"docstatus": ["=", 1]
-						}
-					},
-					"POS Invoice Item": {
-						"doctype": "Sales Invoice Item",
-					}
-				}, credit_note)
-		if credit_note.redeem_loyalty_points:
-			credit_note.loyalty_points = loyalty_points_redeemed
-			credit_note.loyalty_amount = loyalty_amount
+		credit_note = self.merge_pos_invoice_into(credit_note, data)
 
 		credit_note.return_against = self.consolidated_invoice
 		credit_note.save()
@@ -121,6 +70,54 @@ class POSInvoiceMergeLog(Document):
 		self.consolidated_credit_note = credit_note.name
 
 		return credit_note.name
+	
+	def merge_pos_invoice_into(self, invoice, data):
+		sales_invoice_meta = frappe.get_meta("Sales Invoice")
+		items, payments, taxes = [], [], []
+		loyalty_amount_sum, loyalty_points_sum = 0, 0
+		for doc in data:
+			map_doc(doc, invoice, table_map={ "doctype": invoice.doctype })
+			
+			if doc.redeem_loyalty_points:
+				invoice.loyalty_redemption_account = doc.loyalty_redemption_account
+				invoice.loyalty_redemption_cost_center = doc.loyalty_redemption_cost_center
+				loyalty_points_sum += doc.loyalty_points
+				loyalty_amount_sum += doc.loyalty_amount
+			
+			for item in doc.get('items'):
+				items.append(item)
+			
+			for tax in doc.get('taxes'):
+				found = False
+				for t in taxes:
+					if t.account_head == tax.account_head and t.cost_center == tax.cost_center and t.rate == tax.rate:
+						t.tax_amount = flt(t.tax_amount) + flt(tax.tax_amount)
+						t.base_tax_amount = flt(t.base_tax_amount) + flt(tax.base_tax_amount)
+						found = True
+				if not found:
+					tax.charge_type = 'Actual'
+					taxes.append(tax)
+
+			for payment in doc.get('payments'):
+				found = False
+				for pay in payments:
+					if pay.account == payment.account and pay.mode_of_payment == payment.mode_of_payment:
+						pay.amount = flt(pay.amount) + flt(payment.amount)
+						pay.base_amount = flt(pay.base_amount) + flt(payment.base_amount)
+						found = True
+				if not found:
+					payments.append(payment)
+
+		if loyalty_points_sum:
+			invoice.redeem_loyalty_points = 1
+			invoice.loyalty_points = loyalty_points_sum
+			invoice.loyalty_amount = loyalty_amount_sum
+
+		invoice.set('items', items)
+		invoice.set('payments', payments)
+		invoice.set('taxes', taxes)
+
+		return invoice
 	
 	def get_new_sales_invoice(self):
 		sales_invoice = frappe.new_doc('Sales Invoice')
@@ -149,18 +146,22 @@ def get_all_invoices():
 	}
 	pos_invoices = frappe.db.get_all('POS Invoice', filters=filters,
 		fields=["'name' as pos_invoice", 'posting_date', 'grand_total', 'customer'])
-	
+
+def get_invoices_customer_map(pos_invoices):
 	# pos_invoice_customer_map = { 'Customer 1': [{}, {}, {}], 'Custoemr 2' : [{}] }
 	pos_invoice_customer_map = {}
-	for invoice in self.pos_invoices:
+	for invoice in pos_invoices:
 		customer = invoice.get('customer')
 		pos_invoice_customer_map.setdefault(customer, [])
 		pos_invoice_customer_map[customer].append(invoice)
 	
 	return pos_invoice_customer_map
 
-def auto_merge_pos_invoices():
-	pos_invoice_map = get_all_invoices()
+def merge_pos_invoices(pos_invoices=[]):
+	if not pos_invoices:
+		pos_invoices = get_all_invoices()
+	
+	pos_invoice_map = get_invoices_customer_map(pos_invoices)
 	create_merge_logs(pos_invoice_map)
 
 def create_merge_logs(pos_invoice_customer_map):
@@ -169,11 +170,7 @@ def create_merge_logs(pos_invoice_customer_map):
 		merge_log.posting_date = getdate(nowdate())
 		merge_log.customer = customer
 
-		refs = []
-		for d in invoices:
-			refs.append(d)
-
-		merge_log.set('pos_invoices', refs)
+		merge_log.set('pos_invoices', invoices)
 		merge_log.save()
 		merge_log.submit()
 
