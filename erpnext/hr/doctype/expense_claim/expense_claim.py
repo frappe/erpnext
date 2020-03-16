582f0ec4147c1c27e2004de80936750b3a11f114
Anurag Mishra <mishranaman123@gmail.com>
2020-03-16 18:24:28 +0530

    fix: Wrong status

--- erpnext/hr/doctype/expense_claim/expense_claim.py
+++ erpnext/hr/doctype/expense_claim/expense_claim.py
@@ -45,7 +45,7 @@ class ExpenseClaim(AccountsController):
 		paid_amount = flt(self.total_amount_reimbursed) + flt(self.total_advance_amount)
 		precision = self.precision("grand_total")
 		if (self.is_paid or (flt(self.total_sanctioned_amount) > 0
-			and flt(self.grand_total, precision) ==  flt(paid_amount, precision))) \
+			and flt(self.total_sanctioned_amount, precision) ==  flt(paid_amount, precision))) \
 			and self.docstatus == 1 and self.approval_status == 'Approved':
 				self.status = "Paid"
 		elif flt(self.total_sanctioned_amount) > 0 and self.docstatus == 1 and self.approval_status == 'Approved':
