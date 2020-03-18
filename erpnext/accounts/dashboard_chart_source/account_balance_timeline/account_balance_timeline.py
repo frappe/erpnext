95895ff28fa3a6783a38cd4187debf5b766648d0
prssanna <prssud@gmail.com>
2020-03-18 16:25:35 +0530

    fix: set filters as argument filters if passed

--- erpnext/accounts/dashboard_chart_source/account_balance_timeline/account_balance_timeline.py
+++ erpnext/accounts/dashboard_chart_source/account_balance_timeline/account_balance_timeline.py
@@ -26,7 +26,7 @@ def get(chart_name = None, chart = None, no_cache = None, filters = None, from_d
 		to_date = chart.to_date
 
 	timegrain = chart.time_interval
-	filters = frappe.parse_json(chart.filters_json)
+	filters = frappe.parse_json(filters) or frappe.parse_json(chart.filters_json)
 
 	account = filters.get("account")
 	company = filters.get("company")
