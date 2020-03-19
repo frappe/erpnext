ea16025a914b541f2587e9629bc6204b146b0322
GersonCaballero <gersoncaballero306@gmail.com>
2020-03-19 03:44:02 -0600

    Solution for issue sun total in account statement payment

--- erpnext/accounts/doctype/account_statement_payment/account_statement_payment.py
+++ erpnext/accounts/doctype/account_statement_payment/account_statement_payment.py
@@ -9,23 +9,4 @@ from frappe.model.document import Document
 from datetime import datetime, timedelta, date
 
 class AccountStatementPayment(Document):
-		
-	def on_update(self):
-		self.sum_total()
-		doc = frappe.get_doc("Patient statement", self.patient_statement)
-		doc.cumulative_total = self.total
-		doc.save()
-	
-	def sum_total(self):
-		total_price = 0
-		price = 0
-		account_statement = frappe.get_all("Account Statement Payment Item", ["name", "price", "quantity", "net_pay"], filters = {"parent": self.name})
-
-		for item in account_statement:
-			frappe.msgprint("Account Statement: item {}".format(item))
-			price = item.net_pay			
-			total_price += price	
-		
-		self.total = total_price			
-
-		frappe.msgprint("Account Statement: total price {} sel.total {}".format(total_price, self.total))
+	pass
