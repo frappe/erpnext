e5b7186d6bc66dc6eb4ffa7c95c5076201d6ef1b
sahil28297 <37302950+sahil28297@users.noreply.github.com>
2020-03-20 14:23:27 +0530

    fix: develop version as 12.x.x instead of 12.2.0

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -5,7 +5,7 @@ import frappe
 from erpnext.hooks import regional_overrides
 from frappe.utils import getdate
 
-__version__ = '12.2.0'
+__version__ = '12.x.x'
 
 def get_default_company(user=None):
 	'''Get default company for user'''
