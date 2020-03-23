551e3e508e921682d337765975c6cdd11aaa142f
Rohan <rohan@bloomstack.com>
2020-03-23 12:15:25 +0530

    fix: only set reason for return as editable

--- erpnext/stock/doctype/delivery_note/delivery_note.js
+++ erpnext/stock/doctype/delivery_note/delivery_note.js
@@ -167,7 +167,7 @@ erpnext.stock.DeliveryNoteController = erpnext.selling.SellingController.extend(
 
 			if (doc.docstatus==1) {
 				this.frm.add_custom_button(__('Sales Return'), function() {
-					me.show_rejection_prompt()}, __('Create'));
+					me.make_sales_return()}, __('Create'));
 			}
 
 			if (doc.docstatus==1) {
@@ -223,6 +223,12 @@ erpnext.stock.DeliveryNoteController = erpnext.selling.SellingController.extend(
 				erpnext.utils.make_subscription(doc.doctype, doc.name)
 			}, __('Create'))
 		}
+
+		if (doc.docstatus == 0 && doc.is_return) {
+			cur_frm.add_custom_button(__('Reason(s) for Return'), function () {
+				me.show_return_reasons_prompt(cur_frm.doc);
+			}, __('Update'))
+		}
 	},
 
 	make_sales_invoice: function() {
@@ -239,20 +245,20 @@ erpnext.stock.DeliveryNoteController = erpnext.selling.SellingController.extend(
 		});
 	},
 
-	show_rejection_prompt: function () {
-		const me = this;
+	show_return_reasons_prompt: function (doc) {
 		this.data = [];
 
-		for (let row of this.frm.doc.items) {
+		for (let row of doc.items) {
 			this.data.push({
-				"name": row.name,
+				"docname": row.name,
 				"item_code": row.item_code,
-				"item_name": row.item_name
+				"item_name": row.item_name,
+				"reason_for_return": row.reason_for_return
 			})
 		}
 
 		const dialog = new frappe.ui.Dialog({
-			title: __("Select Reasons for Rejection"),
+			title: __("Set Reason(s) for Return"),
 			fields: [
 				{
 					label: __("Items"),
@@ -261,10 +267,13 @@ erpnext.stock.DeliveryNoteController = erpnext.selling.SellingController.extend(
 					cannot_add_rows: true,
 					data: this.data,
 					in_place_edit: true,
+					get_data: () => {
+						return this.data;
+					},
 					fields: [
 						{
 							fieldtype: 'Data',
-							fieldname: "name",
+							fieldname: "docname",
 							hidden: 1
 						},
 						{
@@ -272,15 +281,15 @@ erpnext.stock.DeliveryNoteController = erpnext.selling.SellingController.extend(
 							fieldtype: 'Link',
 							fieldname: "item_code",
 							options: "Item",
-							in_list_view: 1,
-							disabled: 0
+							read_only: 1,
+							in_list_view: 1
 						},
 						{
 							label: __("Item Name"),
 							fieldtype: 'Data',
 							fieldname: "item_name",
-							in_list_view: 1,
-							disabled: 0
+							read_only: 1,
+							in_list_view: 1
 						},
 						{
 							label: __("Reason for Return"),
@@ -288,31 +297,33 @@ erpnext.stock.DeliveryNoteController = erpnext.selling.SellingController.extend(
 							fieldname: 'reason_for_return',
 							in_list_view: 1
 						}
-					],
-					get_data: () => {
-						return this.data;
-					}
+					]
 				},
 			],
 			primary_action: function () {
 				const values = dialog.get_values().items;
-
 				values.forEach(d => {
-					frappe.model.set_value("Delivery Note Item", d.name, "reason_for_return", d.reason_for_return);
+					frappe.model.set_value("Delivery Note Item", d.docname, "reason_for_return", d.reason_for_return);
 				});
 
 				dialog.hide();
-				me.make_sales_return()
+				frappe.show_alert({
+					indicator: 'green',
+					message: __("The reason(s) for return have been successfully set")
+				});
 			},
-			primary_action_label: __('Save')
+			primary_action_label: __('Update')
 		});
 		dialog.show();
 	},
 
 	make_sales_return: function() {
+		const me = this;
 		frappe.model.open_mapped_doc({
 			method: "erpnext.stock.doctype.delivery_note.delivery_note.make_sales_return",
 			frm: this.frm
+		}).then((note) => {
+			me.show_return_reasons_prompt(note.message);
 		})
 	},
 
