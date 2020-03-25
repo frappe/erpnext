bdcfab3a0fcd86c03250a6c5328191ca5cb2a226
Deepesh Garg <deepeshgarg6@gmail.com>
2020-03-25 21:32:10 +0530

    fix: Update patch and bank form cleanup

--- erpnext/patches/v12_0/move_bank_account_swift_number_to_bank.py
+++ erpnext/patches/v12_0/move_bank_account_swift_number_to_bank.py
@@ -4,17 +4,11 @@ import frappe
 def execute():
 	frappe.reload_doc('accounts', 'doctype', 'bank', force=1)
 
-	banks = frappe.get_all('Bank', 'name')
-	for bank in banks:
-		bank_accounts = frappe.get_all('Bank Account', filters={'bank': bank.name}, fields=['swift_number', 'branch_code'])
-		bank_account = ''
-		if len(bank_accounts):
-			bank_account = bank_accounts[0]
-			if bank_account and bank_account.swift_number:
-				bank.swift_number = bank_account.swift_number
-			if bank_account and bank_account.branch_code:
-				bank.branch_code = bank_account.branch_code
-			bank.save()
+	frappe.db.sql("""
+		UPDATE `tabBank` b, `tabBank Account` ba
+		SET b.swift_number = ba.swift_number, b.branch_code = ba.branch_code
+		WHERE b.name = ba.bank
+	""")
 
 	frappe.reload_doc('accounts', 'doctype', 'bank_account')
 	frappe.reload_doc('accounts', 'doctype', 'payment_request')
