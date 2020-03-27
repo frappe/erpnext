ChillarAnand/health

fix: disabled patient shown as enabled

erpnext/healthcare/doctype/clinical_procedure/clinical_procedure.js
@@ -29,7 +29,7 @@ frappe.ui.form.on('Clinical Procedure', {
 	refresh: function(frm) {
 		frm.set_query("patient", function () {
 			return {
-				filters: {"disabled": 0}
+				filters: {"status": "Disabled"}
 			};
 		});
 		frm.set_query("appointment", function () {

erpnext/healthcare/doctype/patient_appointment/patient_appointment.js
@@ -11,7 +11,7 @@ frappe.ui.form.on('Patient Appointment', {
 	refresh: function(frm) {
 		frm.set_query("patient", function () {
 			return {
-				filters: {"disabled": 0}
+				filters: {"status": "Disabled"}
 			};
 		});
 		frm.set_query("practitioner", function() {

erpnext/healthcare/doctype/patient_encounter/patient_encounter.js
@@ -62,7 +62,7 @@ frappe.ui.form.on('Patient Encounter', {
 
 		frm.set_query("patient", function () {
 			return {
-				filters: {"disabled": 0}
+				filters: {"status": "Disabled"}
 			};
 		});
 		frm.set_query("drug_code", "drug_prescription", function() {

frappe/erpnext

erpnext/healthcare/doctype/clinical_procedure/clinical_procedure.js
@@ -29,7 +29,7 @@ frappe.ui.form.on('Clinical Procedure', {
 	refresh: function(frm) {
 		frm.set_query("patient", function () {
 			return {
-				filters: {"disabled": 0}
+				filters: {"status": "Active"}
 			};
 		});
 		frm.set_query("appointment", function () {

erpnext/healthcare/doctype/patient_appointment/patient_appointment.js
@@ -11,7 +11,7 @@ frappe.ui.form.on('Patient Appointment', {
 	refresh: function(frm) {
 		frm.set_query("patient", function () {
 			return {
-				filters: {"disabled": 0}
+				filters: {"status": "Active"}
 			};
 		});
 		frm.set_query("practitioner", function() {

erpnext/healthcare/doctype/patient_encounter/patient_encounter.js
@@ -62,7 +62,7 @@ frappe.ui.form.on('Patient Encounter', {
 
 		frm.set_query("patient", function () {
 			return {
-				filters: {"disabled": 0}
+				filters: {"status": "Active"}
 			};
 		});
 		frm.set_query("drug_code", "drug_prescription", function() {