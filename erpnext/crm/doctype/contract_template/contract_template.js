941fa34c0a7a0aaf030b0e0524516e00e6d5285a
Rohan <rohan@bloomstack.com>
2020-03-27 20:08:16 +0530

    feat: add terms builder to Contract

--- erpnext/crm/doctype/contract_template/contract_template.js
+++ erpnext/crm/doctype/contract_template/contract_template.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Contract Template', {
-	refresh: function(frm) {
+frappe.ui.form.on('Contract Template Section', {
+	section: function(frm, cdt, cdn) {
+		let row = frm.selected_doc;
 
+		if (row.section) {
+			frappe.db.get_value("Contract Section", {"title": row.section}, "description", (r) => {
+				if (r.description) {
+					frappe.model.set_value(cdt, cdn, "description", r.description);
+				}
+			})
+		}
 	}
 });
