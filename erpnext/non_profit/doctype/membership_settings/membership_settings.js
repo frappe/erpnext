1f20f6348d93b12f2400beef7acbab40f4e1f2a5
Shivam Mishra <scm.mymail@gmail.com>
2020-03-30 18:13:29 +0530

    feat: added membership settings doctype

---
+++ erpnext/non_profit/doctype/membership_settings/membership_settings.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Membership Settings', {
+	refresh: function(frm) {
+		// if (frm.doc.enable_razorpay) {
+		// 	frm.add_custom_button(__("Fetch Plans from RazorPay"), () => {
+		// 		frm.trigger("fetch_razorpay_plans")
+		// 	});
+		// }
+	}
+});
