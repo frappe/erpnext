1d5bf1947acdfc0855db13455fda3f8a86524e81
Anupam K <anupamvns0099@gmail.com>
2020-04-02 00:36:43 +0530

    PR review changes

--- erpnext/crm/doctype/social_media_post/social_media_post.js
+++ erpnext/crm/doctype/social_media_post/social_media_post.js
@@ -1,12 +1,2 @@
 // Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
-
-frappe.ui.form.on('Social Media Post', {
-	refresh: async function(frm) {
-		let twitter_settings = await frappe.get_doc("Twitter Settings");
-		let linkedin_settings = await frappe.get_doc("LinkedIn Setttings");
-
-		frm.toggle_display("twitter", twitter_settings.oauth_token);
-		frm.toggle_display("linkedin", linkedin_settings.person_urn);
-	}
-});
