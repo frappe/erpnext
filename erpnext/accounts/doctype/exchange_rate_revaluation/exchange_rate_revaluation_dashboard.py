1e5f91b3a44fab7f549a0c5922cb57d519ea4452
Deepesh Garg <deepeshgarg6@gmail.com>
2020-04-02 17:26:58 +0530

    fix(minor): Minor anti pattern fixes in Exchange rate revaluation doctype

---
+++ erpnext/accounts/doctype/exchange_rate_revaluation/exchange_rate_revaluation_dashboard.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from frappe import _
+
+
+def get_data():
+	return {
+		'fieldname': 'reference_name',
+		'transactions': [
+			{
+				'items': ['Journal Entry']
+			}
+		]
+	}
