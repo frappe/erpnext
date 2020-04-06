26ccca703d308447b9f77362938d20164386fce3
Deepesh Garg <deepeshgarg6@gmail.com>
2020-04-06 21:17:36 +0530

    fix: Rename bank account type doctypes

--- erpnext/accounts/doctype/bank_account_subtype/bank_account_subtype.js
+++ erpnext/accounts/doctype/bank_account_subtype/bank_account_subtype.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Account Type', {
+frappe.ui.form.on('Bank Account Subtype', {
 	refresh: function() {
 
 	}
