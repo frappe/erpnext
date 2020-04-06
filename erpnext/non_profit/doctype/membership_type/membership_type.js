23f7145109a63dd47899f0a8188a094323e6b5cb
Shivam Mishra <scm.mymail@gmail.com>
2020-04-06 14:44:33 +0530

    fix: use false explicitly

--- erpnext/non_profit/doctype/membership_type/membership_type.js
+++ erpnext/non_profit/doctype/membership_type/membership_type.js
@@ -4,7 +4,7 @@
 frappe.ui.form.on('Membership Type', {
 	refresh: function(frm) {
 		frappe.db.get_single_value("Membership Settings", "enable_razorpay").then(val => {
-			if (val) frm.set_df_property('razorpay_plan_id', 'hidden', 'false');
+			if (val) frm.set_df_property('razorpay_plan_id', 'hidden', false);
 		})
 	}
 });
