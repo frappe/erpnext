6e5b8f31896402c71f2f8dd1297a7ca9ec8b6152
Anupam K <anupamvns0099@gmail.com>
2020-04-18 00:45:18 +0530

    Linkedin, Twitter Integration

---
+++ erpnext/crm/doctype/social_media_post/social_media_post.js
@@ -0,0 +1,54 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+frappe.ui.form.on('Social Media Post', {
+    validate: function(frm){
+        if(frm.doc.text.length > 280){
+            frappe.throw("Length Must be less than 280.")
+        }
+    },
+	refresh: function(frm){
+        if(frm.doc.docstatus === 1){
+            if(frm.doc.post_status != "Posted"){
+                add_post_btn(frm); 
+            }
+            else if(frm.doc.post_status == "Posted"){
+                frm.set_df_property('sheduled_time', 'read_only', 1);
+            }
+            let html = '<div class="row">';
+            if(frm.doc.twitter){
+                let color = frm.doc.twitter_post_id ? "green" : "red";
+                let status = frm.doc.twitter_post_id ? "Posted" : "Not Posted";
+                html += '<div class="col-xs-6">' +
+                            '<span class="indicator whitespace-nowrap '+ color +'"><span class="hidden-xs">Twitter : '+ status +'</span></span> ' +
+                        '</div>' ;
+            }
+            if(frm.doc.linkedin){
+                let color = frm.doc.linkedin_post_id ? "green" : "red";
+                let status = frm.doc.linkedin_post_id ? "Posted" : "Not Posted";
+                html += '<div class="col-xs-6">' +
+                            '<span class="indicator whitespace-nowrap '+ color +'"><span class="hidden-xs">LinkedIn : '+ status +'</span></span> ' +
+                        '</div>' ;
+            }
+            html += '</div>';
+            frm.dashboard.set_headline_alert(html);
+        }
+    }
+});
+var add_post_btn = function(frm){
+    frm.add_custom_button(('Post Now'), function(){
+        post(frm);
+    });
+}
+var post = function(frm){
+    frappe.call({
+        method: "erpnext.crm.doctype.social_media_post.social_media_post.publish",
+        args: {
+            doctype: frm.doc.doctype,
+            name: frm.doc.name
+        },
+        callback: function(r) {
+            frm.reload_doc();
+        }
+    })
+    
+}
---
+++ erpnext/crm/doctype/social_media_post/social_media_post.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from frappe import _
+
+class SocialMediaPost(Document):
+	def submit(self):
+		if self.sheduled_time:
+			self.post_status = "Scheduled"
+		super(SocialMediaPost, self).submit()
+
+	def post(self):
+		try:
+			if self.twitter and not self.twitter_post_id:
+				twitter = frappe.get_doc("Twitter Settings")
+				twitter_post = twitter.post(self.text, self.image)
+				self.db_set("twitter_post_id", twitter_post.id)
+			if self.linkedin and not self.linkedin_post_id:
+				linkedin = frappe.get_doc("LinkedIn Settings")
+				linkedin_post = linkedin.post(self.text, self.image)
+				self.db_set("linkedin_post_id", linkedin_post.headers['X-RestLi-Id'].split(":")[-1])
+			self.db_set("post_status", "Posted")
+
+		except Exception as e:
+			self.db_set("post_status", "Error")
+			title = _("Error while POSTING {0}").format(self.name)
+			traceback = frappe.get_traceback()
+			frappe.log_error(message=traceback , title=title)
+
+def process_scheduled_social_media_posts():
+	import datetime
+	posts = frappe.get_list("Social Media Post", filters={"status": "Scheduled"}, fields= ["name", "sheduled_time"])
+	start = frappe.utils.now_datetime()
+	end = start + datetime.timedelta(minutes=59)
+	for post in posts:
+		post_time = frappe.utils.get_datetime(post.scheduled_time)
+		if post_time > start and post_time <= end:
+			post = frappe.get_doc('Social Media Post',post['name'])
+			post.post()
+
+@frappe.whitelist()
+def publish(doctype, name):
+	sm_post = frappe.get_doc(doctype, name)
+	sm_post.post()
+	frappe.db.commit()
