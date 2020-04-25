823856ffd6eace0a10d338628e543cb99a92f80f
Nabin Hait <nabinhait@gmail.com>
2020-04-25 12:41:50 +0530

    fix: test fixes

--- erpnext/hr/doctype/income_tax_slab/income_tax_slab.js
+++ erpnext/hr/doctype/income_tax_slab/income_tax_slab.js
@@ -2,13 +2,5 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Income Tax Slab', {
-	setup: function(frm, cdt, cdn) {
-		frm.set_query("salary_component", "other_taxes_and_charges", function() {
-			return {
-				filters: {
-					type: "Deduction"
-				}
-			}
-		});
-	}
+
 });
