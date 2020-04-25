84187fb8e978b2dcbb700ab6774fa4cf70928340
Abhishek Balam <abhishekbalam96@gmail.com>
2020-04-26 05:00:36 +0530

    Code cleanup and improvements requested in PR:21404

--- erpnext/accounts/doctype/journal_entry_template/journal_entry_template.py
+++ erpnext/accounts/doctype/journal_entry_template/journal_entry_template.py
@@ -5,9 +5,10 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from erpnext.accounts.utils import get_balance_on
 
 class JournalEntryTemplate(Document):
-	def autoname(self):
-		self.name = self.voucher_type + ' - ' + frappe.get_value('Company', self.company, 'abbr')
+	pass
 
+@frappe.whitelist()
+def get_naming_series():
+	return frappe.get_meta("Journal Entry").get_field("naming_series").options
