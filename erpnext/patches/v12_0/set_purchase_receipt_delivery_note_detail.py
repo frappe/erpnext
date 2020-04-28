2ef4768d9fd817b70e34e041888d0a0324c47bf8
marination <maricadsouza221197@gmail.com>
2020-04-28 18:18:30 +0530

    fix: Patch fix and simplification, json timestamp updation.

--- erpnext/patches/v12_0/set_purchase_receipt_delivery_note_detail.py
+++ erpnext/patches/v12_0/set_purchase_receipt_delivery_note_detail.py
@@ -3,51 +3,61 @@ import frappe
 from collections import defaultdict
 
 def execute():
+	def map_rows(doc_row, return_doc_row, detail_field, doctype):
+		"""Map rows after identifying similar ones."""
+
+		frappe.db.sql(""" UPDATE `tab{doctype} Item` set {detail_field} = '{doc_row_name}'
+				where name = '{return_doc_row_name}'""" \
+			.format(doctype=doctype,
+					detail_field=detail_field,
+					doc_row_name=doc_row.get('name'),
+					return_doc_row_name=return_doc_row.get('name'))) #nosec
+
+	def row_is_mappable(doc_row, return_doc_row, detail_field):
+		"""Checks if two rows are similar enough to be mapped."""
+
+		if doc_row.item_code == return_doc_row.item_code and not return_doc_row.get(detail_field):
+			if doc_row.get('batch_no') and return_doc_row.get('batch_no') and doc_row.batch_no == return_doc_row.batch_no:
+				return True
+
+			elif doc_row.get('serial_no') and return_doc_row.get('serial_no'):
+				doc_sn = doc_row.serial_no.split('\n')
+				return_doc_sn = return_doc_row.serial_no.split('\n')
+
+				if set(doc_sn) & set(return_doc_sn):
+					# if two rows have serial nos in common, map them
+					return True
+
+			elif doc_row.rate == return_doc_row.rate:
+				return True
+		else:
+			return False
+
+	def make_return_document_map(doctype, return_document_map):
+		"""Returns a map of documents and it's return documents.
+		Format => { 'document' : ['return_document_1','return_document_2'] }"""
+
+		return_against_documents = frappe.db.sql("""
+			SELECT
+				return_against as document, name as return_document
+			FROM `tab{doctype}`
+			WHERE
+				is_return = 1 and docstatus = 1""".format(doctype=doctype),as_dict=1) #nosec
+
+		for entry in return_against_documents:
+			return_document_map[entry.document].append(entry.return_document)
+
+		return return_document_map
+
 	def set_document_detail_in_return_document(doctype):
-		frappe.reload_doctype("{0} Item".format(doctype))
-		return_document_map = defaultdict(list)
+		"""Map each row of the original document in the return document."""
 		mapped = []
+		return_document_map = defaultdict(list)
 		detail_field = "purchase_receipt_item" if doctype=="Purchase Receipt" else "dn_detail"
 
-		def make_return_document_map(doctype, return_document_map):
-			'''Returns a map of documents and it's return documents.
-			Format => { 'document' : ['return_document_1','return_document_2'] }'''
+		child_doc = frappe.scrub("{0} Item".format(doctype))
+		frappe.reload_doc("stock", "doctype", child_doc)
 
-			return_against_documents = frappe._dict(frappe.db.sql("""
-				SELECT
-					return_against as document, name as return_document
-				FROM `tab{0}`
-				WHERE
-					is_return = 1 and docstatus = 1""".format(doctype),as_dict=1))
-
-			for entry in return_against_documents:
-				return_document_map[entry.document].append(entry.return_document)
-
-			return return_document_map
-
-		def row_is_mappable(doc_row, return_doc_row, detail_field):
-			''' Checks if two rows are similar enough to be mapped '''
-			if doc_row.item_code == return_doc_row.item_code and not return_doc_row.get(detail_field):
-				if doc_row.get('batch_no') and return_doc_row.get('batch_no') and doc_row.batch_no == return_doc_row.batch_no:
-					return True
-
-				elif doc_row.get('serial_no') and return_doc_row.get('serial_no'):
-					doc_sn, return_doc_sn = doc_row.serial_no.split('\n'), return_doc_row.serial_no.split('\n')
-					if set(doc_sn) & set(return_doc_sn):
-						# if two rows have serial nos in common, map them
-						return True
-
-				elif doc_row.rate == return_doc_row.rate:
-					return True
-			else:
-				return False
-
-		def map_rows(doc_row, return_doc_row, detail_field, doctype):
-			frappe.db.sql(""" UPDATE `tab{0} Item` set {1} = '{2}' where name = '{3}'""" \
-				.format(doctype, detail_field, doc_row.get('name'), return_doc_row.get('name')))
-
-
-		#map each row of return document to the original document
 		return_document_map = make_return_document_map(doctype, return_document_map)
 
 		#iterate through original documents and its return documents
