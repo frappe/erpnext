2620ac31f9ef8f7636a2a7a907d4524b949ebe99
Nabin Hait <nabinhait@gmail.com>
2020-04-30 11:03:57 +0530

    fix: Desk links for Income Tax Slab and Employee Other Income (#21511)

--- erpnext/config/hr.py
+++ erpnext/config/hr.py
@@ -172,6 +172,10 @@ def get_data():
 				},
 				{
 					"type": "doctype",
+					"name": "Income Tax Slab",
+				},
+				{
+					"type": "doctype",
 					"name": "Salary Component",
 				},
 				{
@@ -208,6 +212,10 @@ def get_data():
 					"type": "doctype",
 					"name": "Employee Tax Exemption Proof Submission",
 					"dependencies": ["Employee"]
+				},
+				{
+					"type": "doctype",
+					"name": "Employee Other Income",
 				},
 				{
 					"type": "doctype",
