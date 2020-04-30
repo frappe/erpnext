fab3c8cf13d648ec25c5c5ce13b05a88f737b203
Saqib <nextchamp.saqib@gmail.com>
2020-04-30 11:04:44 +0530

    fix: list index out of range error (#21467)
    * fix: list index out of range error
    
    * fix: condititon

--- erpnext/accounts/report/gross_and_net_profit_report/gross_and_net_profit_report.py
+++ erpnext/accounts/report/gross_and_net_profit_report/gross_and_net_profit_report.py
@@ -35,6 +35,12 @@ def execute(filters=None):
 		})
 		return columns, data
 
+	# to avoid error eg: gross_income[0] : list index out of range
+	if not gross_income:
+		gross_income = [{}]
+	if not gross_expense:
+		gross_expense = [{}]
+
 	data.append({
 		"account_name": "'" + _("Included in Gross Profit") + "'",
 		"account": "'" + _("Included in Gross Profit") + "'"
