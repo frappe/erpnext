d8878c3be8ebd28828af3e3f5a040dad69b87679
Deepesh Garg <deepeshgarg6@gmail.com>
2020-05-02 18:05:33 +0530

    fix: Reverse GL Entry on cancel fix

--- erpnext/accounts/doctype/period_closing_voucher/period_closing_voucher.py
+++ erpnext/accounts/doctype/period_closing_voucher/period_closing_voucher.py
@@ -19,7 +19,7 @@ class PeriodClosingVoucher(AccountsController):
 	def on_cancel(self):
 		self.ignore_linked_doctypes = ('GL Entry', 'Stock Ledger Entry')
 		from erpnext.accounts.general_ledger import make_reverse_gl_entries
-		make_reverse_gl_entries(voucher_type="Period Closing Voucher", voucher_no=self.name, cancel=True)
+		make_reverse_gl_entries(voucher_type="Period Closing Voucher", voucher_no=self.name)
 
 	def validate_account_head(self):
 		closing_account_type = frappe.db.get_value("Account", self.closing_account_head, "root_type")
