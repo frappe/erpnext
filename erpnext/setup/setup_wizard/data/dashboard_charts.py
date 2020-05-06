631260b632f01b5b0b16e7fd61a7fd1b40bd562f
marination <maricadsouza221197@gmail.com>
2020-05-06 18:12:26 +0530

    chore: Added fixtures and Onboarding.

--- erpnext/setup/setup_wizard/data/dashboard_charts.py
+++ erpnext/setup/setup_wizard/data/dashboard_charts.py
@@ -32,6 +32,17 @@ def get_default_dashboards():
 					{ "chart": "Expenses" },
 					{ "chart": "Patient Appointments" }
 				]
+			},
+			{
+				"doctype": "Dashboard",
+				"dashboard_name": "Buying",
+				"charts": [
+					{ "chart": "Purchase Analytics" },
+					{ "chart": "Material Request Purchase Analysis" },
+					{ "chart": "Purchase Order Analysis" },
+					{ "chart": "Requested Items to Order" },
+				]
+
 			}
 		],
 		"Charts": [
@@ -123,6 +134,88 @@ def get_default_dashboards():
 				"document_type": "Patient Appointment",
 				"type": "Line",
 				"width": "Half"
+			},
+			{
+				"doctype": "Dashboard Chart",
+				"owner": "Administrator",
+				"report_name": "Purchase Analytics",
+				"filters_json": json.dumps({
+					"tree_type": "Item",
+					"doc_type": "Purchase Receipt",
+					"value_quantity": "Value",
+					"from_date": "2020-03-01",
+					"to_date": "2020-07-31",
+					"company": company.name,
+					"range": "Weekly"
+					}),
+				"type": "Line",
+				'timeseries': 0,
+				"chart_type": "Report",
+				"chart_name": "Purchase Analytics",
+				"custom_options": json.dumps({
+					"x_field": "entity",
+					"chart_type": "Line",
+					"y_axis_fields": [{"idx": 1, "__islocal": "true", "y_field": "total"}],
+					"y_fields": ["total"],
+					"lineOptions": {"regionFill": 1}
+				})
+			},
+			{
+				"doctype": "Dashboard Chart",
+				"owner": "Administrator",
+				"document_type": "Material Request",
+				"filters_json": '[["Material Request","status","not in",["Draft","Cancelled","Stopped",null],false],["Material Request","material_request_type","=","Purchase",false],["Material Request","company","=", "{company}", false]]'.format(company=company.name),
+				"is_custom": 0,
+				"type": "Donut",
+				"timeseries": 0,
+				"chart_type": "Group By",
+				"group_by_based_on": "status",
+				"chart_name": "Material Request Purchase Analysis",
+				"group_by_type": "Count",
+				"custom_options": json.dumps({"height": 300})
+
+			},
+			{
+				"doctype": "Dashboard Chart",
+				"owner": "Administrator",
+				"report_name": "Purchase Order Analysis",
+				"filters_json": json.dumps({
+					"company": company.name,
+					"from_date": "2020-04-04",
+					"to_date": "2020-07-04",
+					"chart_based_on": "Quantity"
+				}),
+				"is_custom": 1,
+				"type": "Donut",
+				"timeseries": 0,
+				"chart_type": "Report",
+				"chart_name": "Purchase Order Analysis",
+				"custom_options": json.dumps({
+					"type": "donut",
+					"height": 300,
+					"axisOptions": {"shortenYAxisNumbers": 1}
+				})
+			},
+			{
+				"doctype": "Dashboard Chart",
+				"owner": "Administrator",
+  				"report_name": "Requested Items to Order",
+				"filters_json": json.dumps({
+					"company": company.name,
+					"from_date": "2020-04-01",
+					"to_date": "2020-07-01",
+					"group_by_mr": 0
+				}),
+  				"is_custom": 1,
+				"type": "Bar",
+				"timeseries": 0,
+				"chart_type": "Report",
+				"chart_name": "Requested Items to Order",
+				"custom_options": json.dumps({
+					"type": "bar",
+					"barOptions": {"stacked": 1},
+					"axisOptions": {"shortenYAxisNumbers": 1}
+				})
 			}
 		]
 	}
