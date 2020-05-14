a0c6c0c2f4d5563068d079611909590421f09269
marination <maricadsouza221197@gmail.com>
2020-05-15 04:41:30 +0530

    fix: Avoid get_doc in patch

--- erpnext/patches/v12_0/set_serial_no_status.py
+++ erpnext/patches/v12_0/set_serial_no_status.py
@@ -1,11 +1,19 @@
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import getdate, nowdate
 
 def execute():
 	frappe.reload_doc('stock', 'doctype', 'serial_no')
 
 	for serial_no in frappe.db.sql("""select name from `tabSerial No`""", as_dict = 1):
-		doc = frappe.get_doc("Serial No", serial_no.get("name"))
-		if not doc.status:
-			doc.set_status()
-			frappe.db.set_value("Serial No", serial_no.get("name"), "status", doc.status)
+		sn_detail = frappe.db.get_value("Serial No", serial_no.get("name"), ["status", "delivery_document_type", "warranty_expiry_date"], as_dict=1)
+
+		if not sn_detail.get("status"):
+			if sn_detail.get("delivery_document_type"):
+				status = "Delivered"
+			elif sn_detail.get("warranty_expiry_date") and getdate(sn_detail.get("warranty_expiry_date")) <= getdate(nowdate()):
+				status = "Expired"
+			else:
+				status = "Active"
+
+		frappe.db.set_value("Serial No", serial_no.get("name"), "status", status)
