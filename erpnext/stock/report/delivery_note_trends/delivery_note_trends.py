09821bb10a8c856165e72b25afe55a5f606b4503
marination <maricadsouza221197@gmail.com>
2020-05-15 01:33:43 +0530

    chore: Added Dynamic Charts to DN and PR Trends Reports

--- erpnext/stock/report/delivery_note_trends/delivery_note_trends.py
+++ erpnext/stock/report/delivery_note_trends/delivery_note_trends.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from erpnext.controllers.trends	import get_columns,get_data
 
 def execute(filters=None):
@@ -10,5 +11,31 @@ def execute(filters=None):
 	data = []
 	conditions = get_columns(filters, "Delivery Note")
 	data = get_data(filters, conditions)
-	
-	return conditions["columns"], data 
+
+	chart_data = get_chart_data(data)
+
+	return conditions["columns"], data, None, chart_data
+
+def get_chart_data(data):
+	labels, datapoints = [], []
+
+	if len(data) > 10:
+		data = sorted(data, key = lambda i: i[-1],reverse=True)
+		data = data[:10]
+
+	for row in data:
+		labels.append(row[0])
+		datapoints.append(row[-1])
+
+	return {
+		"data": {
+			"labels" : labels,
+			"datasets" : [
+				{
+				"name": _("Total Revenue"),
+				"values": datapoints
+				}
+			]
+		},
+		"type" : "bar"
+	}
--- erpnext/stock/report/purchase_receipt_trends/purchase_receipt_trends.py
+++ erpnext/stock/report/purchase_receipt_trends/purchase_receipt_trends.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from erpnext.controllers.trends	import get_columns,get_data
 
 def execute(filters=None):
@@ -11,4 +12,31 @@ def execute(filters=None):
 	conditions = get_columns(filters, "Purchase Receipt")
 	data = get_data(filters, conditions)
 
-	return conditions["columns"], data  
+	chart_data = get_chart_data(data)
+
+	return conditions["columns"], data, None, chart_data
+
+def get_chart_data(data):
+	labels, datapoints = [], []
+
+	if len(data) > 10:
+		data = sorted(data, key = lambda i: i[-1],reverse=True)
+		data = data[:10]
+
+	for row in data:
+		labels.append(row[0])
+		datapoints.append(row[-1])
+
+	return {
+		"data": {
+			"labels" : labels,
+			"datasets" : [
+				{
+				"name": _("Total Expenditure"),
+				"values": datapoints
+				}
+			]
+		},
+		"type" : "bar",
+		"colors":["#5e64ff"]
+	}
