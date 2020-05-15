8c56981fa6dd7f21a0a21e255867c9ab9ceaf226
Afshan <afshan13k@gmail.com>
2020-05-15 15:24:57 +0530

    working demo

--- erpnext/www/issues/index.py
+++ erpnext/www/issues/index.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 import frappe
 
+from erpnext.support.doctype.issue.issue import get_issue_list
+from frappe.utils.user import is_website_user
+
 def get_context(context):
-	pass
+	user = frappe.session.user
+	context.doc_list=[]
+	for issue in frappe.get_list("Issue", fields="*", filters={"owner": user}, ignore_permissions = True):
+		doc = frappe.get_doc("Issue", issue.name)
+		context.doc_list.append(doc)
---
+++ erpnext/www/issues/issue.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+import frappe
+
+
+from frappe.website.utils import get_comment_list
+
+def get_context(context):
+    context.com_list = []
+    issue_name = frappe.form_dict['issue']
+    context.item = frappe.get_doc("Issue", issue_name)
+    context.reference_doctype = "Issue"
+    context.reference_name = issue_name
+    context.comment_list = get_comment_list(context.reference_doctype,
+					context.reference_name, reverse=False, get_all_records=True)
