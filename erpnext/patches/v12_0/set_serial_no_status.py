27f071df6b13c0ecf60ea2484d5fa5fc27a277a9
marination <maricadsouza221197@gmail.com>
2020-05-18 10:58:33 +0530

    fix: fetch all values and check status in one query

--- erpnext/patches/v12_0/set_serial_no_status.py
+++ erpnext/patches/v12_0/set_serial_no_status.py
@@ -5,15 +5,13 @@ from frappe.utils import getdate, nowdate
 def execute():
 	frappe.reload_doc('stock', 'doctype', 'serial_no')
 
-	for serial_no in frappe.db.sql("""select name from `tabSerial No`""", as_dict = 1):
-		sn_detail = frappe.db.get_value("Serial No", serial_no.get("name"), ["status", "delivery_document_type", "warranty_expiry_date"], as_dict=1)
-
-		if not sn_detail.get("status"):
-			if sn_detail.get("delivery_document_type"):
-				status = "Delivered"
-			elif sn_detail.get("warranty_expiry_date") and getdate(sn_detail.get("warranty_expiry_date")) <= getdate(nowdate()):
-				status = "Expired"
-			else:
-				status = "Active"
+	for serial_no in frappe.db.sql("""select name, delivery_document_type, warranty_expiry_date from `tabSerial No`
+		where (status is NULL OR status='')""", as_dict = 1):
+		if serial_no.get("delivery_document_type"):
+			status = "Delivered"
+		elif serial_no.get("warranty_expiry_date") and getdate(serial_no.get("warranty_expiry_date")) <= getdate(nowdate()):
+			status = "Expired"
+		else:
+			status = "Active"
 
 		frappe.db.set_value("Serial No", serial_no.get("name"), "status", status)
