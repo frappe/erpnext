5a2baafd8f270f577e18cfe271f977373417f61b
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-05-19 19:45:03 +0530

    fix: patch for setting missing company in Healthcare DocTypes

---
+++ erpnext/patches/v13_0/set_company_field_in_healthcare_doctypes.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	company = frappe.db.get_single_value('Global Defaults', 'default_company')
+	doctypes = ['Clinical Procedure', 'Inpatient Record', 'Lab Test', 'Patient Appointment', 'Patient Encounter', 'Vital Signs']
+	for entry in doctypes:
+		frappe.db.sql("update `tab{dt}` set company = '{company}' where ifnull(company, '') = ''".format(dt=entry, company=company))
