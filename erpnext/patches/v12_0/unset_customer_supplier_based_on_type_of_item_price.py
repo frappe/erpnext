6399851a57d477c26669d3d9e5faeef27374bf66
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-05-21 19:18:06 +0530

    fix: set customer and supplier details using sql
    instead of slowing down the query with get_doc and save()
    we can just use sql to update the required values for
    customer and supplier
    
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- erpnext/patches/v12_0/unset_customer_supplier_based_on_type_of_item_price.py
+++ erpnext/patches/v12_0/unset_customer_supplier_based_on_type_of_item_price.py
@@ -1,15 +1,29 @@
 from __future__ import unicode_literals
 import frappe
 
+
 def execute():
-    invalid_selling_item_price = frappe.db.sql(
-        """SELECT name FROM `tabItem Price` WHERE selling = 1 and buying = 0 and (supplier IS NOT NULL or supplier = '')"""
-    )
-    invalid_buying_item_price = frappe.db.sql(
-        """SELECT name FROM `tabItem Price` WHERE selling = 0 and buying = 1 and (customer IS NOT NULL or customer = '')"""
-    )
-    docs_to_modify = invalid_buying_item_price + invalid_selling_item_price
-    for d in docs_to_modify:
-        # saving the doc will auto reset invalid customer/supplier field
-        doc = frappe.get_doc("Item Price", d[0])
-        doc.save()
+    """
+    set proper customer and supplier details for item price
+    based on selling and buying values
+    """
+
+    # update for selling
+    frappe.db.sql(
+        """UPDATE `tabItem Price`
+        SET `reference` = `customer`, `supplier` = NULL
+        WHERE `selling` = 1
+        AND `buying` = 0
+        AND (`supplier` IS NOT NULL OR `supplier` = '')
+        AND `price_list` = `tabPrice List`.`name`
+        AND `tabPrice List`.`enabled` = 1""")
+
+    # update for buying
+    frappe.db.sql(
+        """UPDATE `tabItem Price``
+        SET `reference` = `supplier`, `customer` = NULL
+        WHERE `selling` = 0
+        AND `buying` = 1
+        AND (`supplier` IS NOT NULL OR `supplier` = '')
+        AND `price_list` = `tabPrice List`.`name`
+        AND `tabPrice List`.`enabled` = 1""")
