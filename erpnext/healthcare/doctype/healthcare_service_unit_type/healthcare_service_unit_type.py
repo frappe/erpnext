d9d1f442df3414796ffeb7b6e1d86acb66fdd079
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-05-21 09:02:46 +0530

    fix: service unit validation and translation

--- erpnext/healthcare/doctype/healthcare_service_unit_type/healthcare_service_unit_type.py
+++ erpnext/healthcare/doctype/healthcare_service_unit_type/healthcare_service_unit_type.py
@@ -12,12 +12,14 @@ class HealthcareServiceUnitType(Document):
 	def validate(self):
 		if self.allow_appointments and self.inpatient_occupancy:
 			frappe.msgprint(
-				_('Healthcare Service Unit Type cannot be both <b>Allow Appointments</b> and <b>Inpatient Occupancy</b>'),
+				_('Healthcare Service Unit Type cannot have both {0} and {1}').format(
+					frappe.bold('Allow Appointments'), frappe.bold('Inpatient Occupancy')),
 				raise_exception=1, title=_('Validation Error'), indicator='red'
 			)
 		elif not self.allow_appointments and not self.inpatient_occupancy:
 			frappe.msgprint(
-				_('Healthcare Service Unit Type cannot be both <b>Allow Appointments</b> and <b>Inpatient Occupancy</b>'),
+				_('Healthcare Service Unit Type must allow atleast one among {0} and {1}').format(
+					frappe.bold('Allow Appointments'), frappe.bold('Inpatient Occupancy')),
 				raise_exception=1, title=_('Validation Error'), indicator='red'
 			)
 
