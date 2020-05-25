6ef64a3ef40dccb91fbf2df29f5a18e1ffc87489
prssanna <prssud@gmail.com>
2020-05-25 19:57:58 +0530

    fix: rename fieldtypes to valid_for_fieldtypes

--- erpnext/startup/filters.py
+++ erpnext/startup/filters.py
@@ -6,7 +6,7 @@ def get_filters_config():
 		"fiscal year": {
 			"label": "Fiscal Year",
 			"get_field": "erpnext.accounts.utils.get_fiscal_year_filter_field",
-			"fieldtypes": ["Date", "Datetime", "DateRange"],
+			"valid_for_fieldtypes": ["Date", "Datetime", "DateRange"],
 			"depends_on": "company",
 		}
     }
