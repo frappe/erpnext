9c31d2193d449c97bc2ea81343a09fcf07612711
Afshan <33727827+AfshanKhan@users.noreply.github.com>
2020-05-26 18:20:47 +0530

    fix: UX date range field separated in from and to date fields. (#21765)
    * fix for issue #21419
    
    * changing group by filter to default Group by Voucher (Consolidated)
    
    * divided date range to seperate from and to date
    
    * divided date range to seperate from and to date for purcase and sales register

--- erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.py
+++ erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.py
@@ -14,7 +14,6 @@ def execute(filters=None):
 
 def _execute(filters=None, additional_table_columns=None, additional_query_columns=None):
 	if not filters: filters = {}
-	filters.update({"from_date": filters.get("date_range") and filters.get("date_range")[0], "to_date": filters.get("date_range") and filters.get("date_range")[1]})
 	columns = get_columns(additional_table_columns, filters)
 
 	company_currency = frappe.get_cached_value('Company',  filters.get("company"),  "default_currency")
