a7f2c4427cbe7abc4ac7eb178586ee6061e896b1
Suraj Shetty <surajshetty3416@gmail.com>
2020-06-10 01:28:40 +0530

    feat: Add Callback/Call Again button for Call Log

--- erpnext/communication/doctype/call_log/call_log.js
+++ erpnext/communication/doctype/call_log/call_log.js
@@ -4,6 +4,11 @@
 frappe.ui.form.on('Call Log', {
 	refresh: function(frm) {
 		frm.events.setup_recording_audio_control(frm);
+		const incoming_call = frm.doc.type == 'Incoming';
+		frm.add_custom_button(incoming_call ? __('Callback'): __('Call Again'), () => {
+			const number = incoming_call ? frm.doc.from : frm.doc.to;
+			frappe.phone_call.handler(number, frm);
+		});
 	},
 	setup_recording_audio_control(frm) {
 		const recording_wrapper = frm.get_field('recording_html').$wrapper;
