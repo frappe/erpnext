538d9c8cc1380e5708bde789f3f2dac39f1a3565
Shivam Mishra <scm.mymail@gmail.com>
2020-06-18 13:50:54 +0530

    refactor: handle exceptions when updating addresses

--- erpnext/erpnext_integrations/connectors/woocommerce_connection.py
+++ erpnext/erpnext_integrations/connectors/woocommerce_connection.py
@@ -73,10 +73,25 @@ def link_customer_and_address(raw_billing_data, raw_shipping_data, customer_name
 
 	if customer_exists:
 		frappe.rename_doc("Customer", old_name, customer_name)
-		billing_address = frappe.get_doc("Address", {"woocommerce_email": customer_woo_com_email, "address_type": "Billing"})
-		shipping_address = frappe.get_doc("Address", {"woocommerce_email": customer_woo_com_email, "address_type": "Shipping"})
-		rename_address(billing_address, customer)
-		rename_address(shipping_address, customer)
+		try:
+			billing_address = frappe.get_doc("Address", {"woocommerce_email": customer_woo_com_email, "address_type": "Billing"})
+			rename_address(billing_address, customer)
+		except (
+			frappe.DoesNotExistError,
+			frappe.DuplicateEntryError,
+			frappe.ValidationError,
+		):
+			pass
+		
+		try:
+			shipping_address = frappe.get_doc("Address", {"woocommerce_email": customer_woo_com_email, "address_type": "Shipping"})
+			rename_address(shipping_address, customer)
+		except (
+			frappe.DoesNotExistError,
+			frappe.DuplicateEntryError,
+			frappe.ValidationError,
+		):
+			pass
 	else:
 		create_address(raw_billing_data, customer, "Billing")
 		create_address(raw_shipping_data, customer, "Shipping")
