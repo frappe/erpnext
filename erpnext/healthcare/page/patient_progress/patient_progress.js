4c2fce003f536ee46ac9fd367483a3bc74bcba93
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-06-22 14:16:41 +0530

    feat: Patient Progress Charts

--- erpnext/healthcare/page/patient_progress/patient_progress.js
+++ erpnext/healthcare/page/patient_progress/patient_progress.js
@@ -51,6 +51,11 @@ class PatientProgress {
 		}
 
 		this.sidebar.find('[data-fieldname="patient"]').append('<div class="patient-info"></div>');
+		this.empty = $(
+			`<div class="chart-loading-state text-muted">${__(
+				"No Data..."
+			)}</div>`
+		);
 	}
 
 	make_patient_profile() {
@@ -58,6 +63,12 @@ class PatientProgress {
 		this.page.set_title(__('Patient Progress'));
 		this.main_section.empty().append(frappe.render_template('patient_progress'));
 		this.render_patient_details();
+		this.render_percentage_chart('therapy_type', 'Therapy Type Distribution');
+		this.create_percentage_chart_filters();
+		this.show_therapy_progress();
+		this.show_assessment_results();
+		this.show_therapy_assessment_correlation();
+		this.show_assessment_parameter_progress();
 	}
 
 	get_patient_info() {
@@ -100,4 +111,271 @@ class PatientProgress {
 		});
 	}
 
+	render_percentage_chart(field, title) {
+		frappe.xcall(
+			'erpnext.healthcare.page.patient_progress.patient_progress.get_therapy_sessions_distribution_data', {
+				patient: this.patient_id,
+				field: field
+			}
+		).then(chart => {
+			if (chart.labels.length) {
+				this.percentage_chart = new frappe.Chart('.therapy-session-percentage-chart', {
+					title: title,
+					type: 'percentage',
+					data: {
+						labels: chart.labels,
+						datasets: chart.datasets
+					},
+					truncateLegends: 1,
+					barOptions: {
+						height: 11,
+						depth: 1
+					},
+					height: 160,
+					maxSlices: 8,
+					colors: ['#5e64ff', '#743ee2', '#ff5858', '#ffa00a', '#feef72', '#28a745', '#98d85b', '#a9a7ac'],
+				});
+			} else {
+				this.wrapper.find('.percentage-chart-container').hide();
+			}
+		});
+	}
+
+	create_percentage_chart_filters() {
+		let filters = [
+			{
+				label: 'Type',
+				options: ['Therapy Type', 'Exercise Type'],
+				fieldnames: ['therapy_type', 'exercise_type'],
+				action: (selected_item, fieldname) => {
+					let title = selected_item + ' Distribution';
+					this.render_percentage_chart(fieldname, title);
+				}
+			},
+		];
+		frappe.dashboard_utils.render_chart_filters(filters, 'chart-filter', '.percentage-chart-container');
+	}
+
+	show_therapy_progress() {
+		let me = this;
+		let therapy_type = frappe.ui.form.make_control({
+			parent: $('.therapy-type-search'),
+			df: {
+				fieldtype: 'Link',
+				options: 'Therapy Type',
+				fieldname: 'therapy_type',
+				placeholder: __('Select Therapy Type'),
+				change: () => {
+					if (me.therapy_type != therapy_type.get_value() && therapy_type.get_value()) {
+						me.therapy_type = therapy_type.get_value();
+						me.render_therapy_progress_chart(me.therapy_type);
+					}
+				}
+			},
+			only_select: true
+		});
+		therapy_type.refresh();
+	}
+
+	render_therapy_progress_chart(therapy_type) {
+		frappe.xcall(
+			'erpnext.healthcare.page.patient_progress.patient_progress.get_therapy_progress_data', {
+				patient: this.patient_id,
+				therapy_type: therapy_type
+			}
+		).then(chart => {
+			let data = {
+				labels: chart.labels,
+				datasets: chart.datasets
+			}
+			if (!this.line_chart) {
+				this.wrapper.find('.therapy-progress-line-chart').show();
+				this.therapy_line_chart = new frappe.Chart('.therapy-progress-line-chart', {
+					title: __('Therapy Progress'),
+					type: 'axis-mixed',
+					height: 250,
+					data: data,
+					lineOptions: {
+						regionFill: 1,
+					},
+					axisOptions: {
+						xIsSeries: 1
+					},
+				});
+			} else {
+				if (data.labels.length) {
+					this.wrapper.find('.therapy-progress-line-chart').show();
+					this.therapy_line_chart.update(data);
+				} else {
+					this.wrapper.find('.therapy-progress-line-chart').hide();
+				}
+			}
+		});
+	}
+
+	show_assessment_results() {
+		let me = this;
+		let assessment_template = frappe.ui.form.make_control({
+			parent: $('.assessment-template-search'),
+			df: {
+				fieldtype: 'Link',
+				options: 'Patient Assessment Template',
+				fieldname: 'assessment_template',
+				placeholder: __('Select Assessment Template'),
+				change: () => {
+					if (me.assessment_template != assessment_template.get_value() && assessment_template.get_value()) {
+						me.assessment_template = assessment_template.get_value();
+						me.render_assessment_result_chart(me.assessment_template);
+					}
+				}
+			},
+			only_select: true
+		});
+		assessment_template.refresh();
+	}
+
+	render_assessment_result_chart(assessment_template) {
+		frappe.xcall(
+			'erpnext.healthcare.page.patient_progress.patient_progress.get_patient_assessment_data', {
+				patient: this.patient_id,
+				assessment_template: assessment_template
+			}
+		).then(chart => {
+			let data = {
+				labels: chart.labels,
+				datasets: chart.datasets
+			}
+			if (!this.assessment_line_chart) {
+				this.wrapper.find('.assessment-results-line-chart').show();
+				this.assessment_line_chart = new frappe.Chart('.assessment-results-line-chart', {
+					title: __('Assessment Results'),
+					type: 'axis-mixed',
+					height: 250,
+					data: data,
+					lineOptions: {
+						regionFill: 1,
+					},
+					axisOptions: {
+						xIsSeries: 1
+					},
+				});
+			} else {
+				if (data.labels.length) {
+					this.wrapper.find('.assessment-results-line-chart').show();
+					this.assessment_line_chart.update(data);
+					this.empty.hide();
+				} else {
+					this.wrapper.find('.assessment-results-line-chart').hide();
+				}
+			}
+		});
+	}
+
+	show_therapy_assessment_correlation() {
+		let me = this;
+		let assessment = frappe.ui.form.make_control({
+			parent: $('.assessment-correlation-template-search'),
+			df: {
+				fieldtype: 'Link',
+				options: 'Patient Assessment Template',
+				fieldname: 'assessment',
+				placeholder: __('Select Assessment Template'),
+				change: () => {
+					if (me.assessment != assessment.get_value() && assessment.get_value()) {
+						me.assessment = assessment.get_value();
+						me.render_therapy_assessment_correlation_chart(me.assessment);
+					}
+				}
+			},
+			only_select: true
+		});
+		assessment.refresh();
+	}
+
+	render_therapy_assessment_correlation_chart(assessment) {
+		frappe.xcall(
+			'erpnext.healthcare.page.patient_progress.patient_progress.get_therapy_assessment_correlation_data', {
+				patient: this.patient_id,
+				assessment_template: assessment
+			}
+		).then(chart => {
+			let data = {
+				labels: chart.labels,
+				datasets: chart.datasets
+			}
+			if (!this.correlation_chart) {
+				this.wrapper.find('.therapy-assessment-correlation-line-chart').show();
+				this.correlation_chart = new frappe.Chart('.therapy-assessment-correlation-chart', {
+					title: __('Correlation'),
+					type: 'axis-mixed',
+					height: 300,
+					data: data,
+					axisOptions: {
+						xIsSeries: 1
+					}
+				});
+			} else {
+				if (data.labels.length) {
+					this.wrapper.find('.therapy-assessment-correlation-chart').show();
+					this.correlation_chart.update(data);
+				} else {
+					this.wrapper.find('.therapy-assessment-correlation-chart').hide();
+				}
+			}
+		});
+	}
+
+	show_assessment_parameter_progress() {
+		let me = this;
+		let parameter = frappe.ui.form.make_control({
+			parent: $('.assessment-parameter-search'),
+			df: {
+				fieldtype: 'Link',
+				options: 'Patient Assessment Parameter',
+				fieldname: 'assessment',
+				placeholder: __('Select Assessment Parameter'),
+				change: () => {
+					if (me.parameter != parameter.get_value() && parameter.get_value()) {
+						me.parameter = parameter.get_value();
+						me.render_assessment_parameter_progress_chart(me.parameter);
+					}
+				}
+			},
+			only_select: true
+		});
+		parameter.refresh();
+	}
+
+	render_assessment_parameter_progress_chart(parameter) {
+		frappe.xcall(
+			'erpnext.healthcare.page.patient_progress.patient_progress.get_assessment_parameter_data', {
+				patient: this.patient_id,
+				parameter: parameter
+			}
+		).then(chart => {
+			let data = {
+				labels: chart.labels,
+				datasets: chart.datasets
+			}
+			if (!this.parameter_chart) {
+				this.wrapper.find('.assessment-parameter-progress-chart').show();
+				this.parameter_chart = new frappe.Chart('.assessment-parameter-progress-chart', {
+					title: __('Assessment Parameter Progress'),
+					type: 'line',
+					height: 250,
+					data: data,
+					axisOptions: {
+						xIsSeries: 1
+					}
+				});
+			} else {
+				if (data.labels.length) {
+					this.wrapper.find('.assessment-parameter-progress-chart').show();
+					this.parameter_chart.update(data);
+				} else {
+					this.wrapper.find('.assessment-parameter-progress-chart').hide();
+				}
+			}
+		});
+	}
 }
--- erpnext/healthcare/page/patient_progress/patient_progress.py
+++ erpnext/healthcare/page/patient_progress/patient_progress.py
@@ -1,5 +1,7 @@
 import frappe
 from datetime import datetime
+from frappe import _
+from frappe.utils import getdate
 
 @frappe.whitelist()
 def get_therapy_sessions_count(patient):
@@ -19,3 +21,136 @@ def get_therapy_sessions_count(patient):
 	'total_therapy_sessions': total,
 	'therapy_sessions_this_month': this_month
 	}
+
+
+@frappe.whitelist()
+def get_therapy_sessions_distribution_data(patient, field):
+	if field == 'therapy_type':
+		result = frappe.db.get_all('Therapy Session',
+			filters = {'patient': patient, 'docstatus': 1},
+			group_by = field,
+			order_by = field,
+			fields = [field, 'count(*)'],
+			as_list = True)
+
+	elif field == 'exercise_type':
+		data = frappe.db.get_all('Therapy Session',  filters={
+			'docstatus': 1,
+			'patient': patient
+		}, as_list=True)
+		therapy_sessions = [entry[0] for entry in data]
+
+		result = frappe.db.get_all('Exercise',
+			filters = {
+				'parenttype': 'Therapy Session',
+				'parent': ['in', therapy_sessions],
+				'docstatus': 1
+			},
+			group_by = field,
+			order_by = field,
+			fields = [field, 'count(*)'],
+			as_list = True)
+
+	return {
+		'labels': [r[0] for r in result if r[0] != None],
+		'datasets': [{
+			'values': [r[1] for r in result]
+		}]
+	}
+
+
+@frappe.whitelist()
+def get_therapy_progress_data(patient, therapy_type):
+	result = frappe.db.get_all('Therapy Session', filters={
+		'docstatus': 1,
+		'patient': patient,
+		'therapy_type': therapy_type
+	},
+	order_by='start_date asc',
+	fields=['start_date', 'total_counts_targeted', 'total_counts_completed'],
+	as_list=True)
+
+	return {
+		'labels': [r[0] for r in result if r[0] != None],
+		'datasets': [
+			{ 'name': _('Targetted'), 'values': [r[1] for r in result if r[0] != None] },
+			{ 'name': _('Completed'), 'values': [r[2] for r in result if r[0] != None] }
+		]
+	}
+
+@frappe.whitelist()
+def get_patient_assessment_data(patient, assessment_template):
+	result = frappe.db.get_all('Patient Assessment', filters={
+		'docstatus': 1,
+		'patient': patient,
+		'assessment_template': assessment_template
+	},
+	order_by='assessment_datetime asc',
+	fields=['assessment_datetime', 'total_score', 'total_score_obtained'],
+	as_list=True)
+
+	return {
+		'labels': [getdate(r[0]) for r in result if r[0] != None],
+		'datasets': [
+			{ 'name': _('Maximum Score'), 'values': [r[1] for r in result if r[0] != None] },
+			{ 'name': _('Score Obtained'), 'values': [r[2] for r in result if r[0] != None] }
+		]
+	}
+
+@frappe.whitelist()
+def get_therapy_assessment_correlation_data(patient, assessment_template):
+	result = frappe.db.sql('''
+		SELECT
+			therapy.therapy_type, count(*), avg(assessment.total_score_obtained), total_score
+		FROM
+			`tabPatient Assessment` assessment INNER JOIN `tabTherapy Session` therapy
+		ON
+			assessment.therapy_session = therapy.name
+		WHERE
+			assessment.assessment_template = %(assessment)s
+		GROUP BY
+			therapy.therapy_type
+	''', {'assessment': assessment_template}, as_list=True)
+
+	return {
+		'labels': [r[0] for r in result if r[0] != None],
+		'datasets': [
+			{ 'name': _('Therapy Sessions'), 'chartType': 'bar', 'values': [r[1] for r in result if r[0] != None] },
+			{ 'name': _('Average Score Obtained'), 'chartType': 'line', 'values': [round(r[2], 2) for r in result if r[0] != None] },
+			{ 'name': _('Maximum Score'), 'chartType': 'line', 'values': [r[3] for r in result if r[0] != None] }
+		]
+	}
+
+@frappe.whitelist()
+def get_assessment_parameter_data(patient, parameter):
+	results = frappe.db.sql('''
+		SELECT
+			assessment.assessment_datetime,
+			sheet.score,
+			template.scale_max
+		FROM
+			`tabPatient Assessment Sheet` sheet
+		INNER JOIN `tabPatient Assessment` assessment
+			ON sheet.parent = assessment.name
+		INNER JOIN `tabPatient Assessment Template` template
+			ON template.name = assessment.assessment_template
+		WHERE
+			sheet.parameter = %(parameter)s and
+			assessment.patient = %(patient)s
+		ORDER BY
+			assessment.assessment_datetime asc
+	''', {'parameter': parameter, 'patient': patient}, as_list=True)
+
+	score_percentages = []
+	for r in results:
+		if r[2] != 0 and r[0] != None:
+			score = round((int(r[1]) / int(r[2])) * 100, 2)
+			score_percentages.append(score)
+
+	return {
+		'labels': [getdate(r[0]) for r in results if r[0] != None],
+		'datasets': [
+			{ 'values': score_percentages }
+		]
+	}
+
