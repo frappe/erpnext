a4a59195597f3b1c31b12487f509a30378c7e9a4
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-06-29 14:21:08 +0530

    feat: add medical coding fields to templates

--- erpnext/healthcare/doctype/lab_test_template/lab_test_template.js
+++ erpnext/healthcare/doctype/lab_test_template/lab_test_template.js
@@ -8,7 +8,7 @@ frappe.ui.form.on("Lab Test Template",{
 		if (!frm.doc.lab_test_description)
 			frm.set_value("lab_test_description", frm.doc.lab_test_name);
 	},
-	refresh : function(frm) {
+	refresh: function(frm) {
 		// Restrict Special, Grouped type templates in Child TestGroups
 		frm.set_query("lab_test_template", "lab_test_groups", function() {
 			return {
@@ -16,6 +16,15 @@ frappe.ui.form.on("Lab Test Template",{
 					lab_test_template_type: ['in',['Single','Compound']]
 				}
 			};
+		});
+	},
+	medical_code: function(frm) {
+		frm.set_query("medical_code", function() {
+			return {
+				filters: {
+					medical_code_standard: frm.doc.medical_code_standard
+				}
+			}
 		});
 	}
 });
