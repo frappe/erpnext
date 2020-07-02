57613702466975e476039613c98ec03ef3df511c
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-07-02 12:35:41 +0530

    chore: add query functions to whitelist
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- erpnext/accounts/doctype/payment_order/payment_order.py
+++ erpnext/accounts/doctype/payment_order/payment_order.py
@@ -26,6 +26,7 @@ class PaymentOrder(Document):
 		for d in self.references:
 			frappe.db.set_value(self.payment_order_type, d.get(frappe.scrub(self.payment_order_type)), ref_field, status)
 
+@frappe.whitelist()
 def get_mop_query(doctype, txt, searchfield, start, page_len, filters):
 	return frappe.db.sql(""" select mode_of_payment from `tabPayment Order Reference`
 		where parent = %(parent)s and mode_of_payment like %(txt)s
@@ -36,6 +37,7 @@ def get_mop_query(doctype, txt, searchfield, start, page_len, filters):
 			'txt': "%%%s%%" % txt
 		})
 
+@frappe.whitelist()
 def get_supplier_query(doctype, txt, searchfield, start, page_len, filters):
 	return frappe.db.sql(""" select supplier from `tabPayment Order Reference`
 		where parent = %(parent)s and supplier like %(txt)s and
