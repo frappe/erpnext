88e58f506b0a6d1e0cf8e1f18a7383bc7f4b6626
Anupam K <anupamvns0099@gmail.com>
2020-07-08 14:38:25 +0530

    refactoring shopping cart settings

--- erpnext/shopping_cart/doctype/shopping_cart_settings/shopping_cart_settings.js
+++ erpnext/shopping_cart/doctype/shopping_cart_settings/shopping_cart_settings.js
@@ -1,31 +1,16 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
 
-$.extend(cur_frm.cscript, {
-	onload: function() {
-		if(cur_frm.doc.__onload && cur_frm.doc.__onload.quotation_series) {
-			cur_frm.fields_dict.quotation_series.df.options = cur_frm.doc.__onload.quotation_series;
-			cur_frm.refresh_field("quotation_series");
+frappe.ui.form.on("Shopping Cart Settings", {
+	onload: function(frm) {
+		if(frm.doc.__onload && frm.doc.__onload.quotation_series) {
+			frm.fields_dict.quotation_series.df.options = frm.doc.__onload.quotation_series;
+			frm.refresh_field("quotation_series");
 		}
 	},
-	refresh: function(){
-		toggle_mandatory(cur_frm)
-	},
-	enable_checkout: function(){
-		toggle_mandatory(cur_frm)
-	},
-	enabled: function() {
-		if (cur_frm.doc.enabled === 1) {
-			cur_frm.doc.show_configure_button = 1;
-			cur_frm.refresh_field('show_configure_button');
+	enabled: function(frm) {
+		if (frm.doc.enabled === 1) {
+			frm.set_value('enable_variants', 1);
 		}
 	}
 });
-
-
-function toggle_mandatory (cur_frm){
-	cur_frm.toggle_reqd("payment_gateway_account", false);
-	if(cur_frm.doc.enabled && cur_frm.doc.enable_checkout) {
-		cur_frm.toggle_reqd("payment_gateway_account", true);
-	}
-}
