88a2e8f8130ee9adfe5782b134d7cdc0e1a50050
Afshan <afshan13k@gmail.com>
2020-07-10 15:14:01 +0530

    created new report named "requested_items_to_order_and_receive" removed report "requested_items_to_order" also wrote a patch for deleting old report from table "tabReport"

--- erpnext/buying/report/requested_items_to_order_and_receive/requested_items_to_order_and_receive.js
+++ erpnext/buying/report/requested_items_to_order_and_receive/requested_items_to_order_and_receive.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Requested Items to Order"] = {
+frappe.query_reports["Requested Items to Order and Receive"] = {
 	"filters": [
 		{
 			"fieldname": "company",
