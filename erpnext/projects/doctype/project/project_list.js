1bb930544bf4546f771f5e96f3fdcaf85af2ccc0
Himanshu Warekar <himanshuwarekar@yahoo.com>
2020-07-13 01:38:34 +0530

    fix: hybrid listview

--- erpnext/projects/doctype/project/project_list.js
+++ erpnext/projects/doctype/project/project_list.js
@@ -1,6 +1,11 @@
 frappe.listview_settings['Project'] = {
 	add_fields: ["status", "priority", "is_active", "percent_complete", "expected_end_date", "project_name"],
 	filters:[["status","=", "Open"]],
+	is_hybrid: true,
+	hybrid: {
+		child_doctype: "Task",
+		child_link_fieldname: "project"
+	},
 	get_indicator: function(doc) {
 		if(doc.status=="Open" && doc.percent_complete) {
 			return [__("{0}%", [cint(doc.percent_complete)]), "orange", "percent_complete,>,0|status,=,Open"];
