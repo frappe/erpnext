3b14eba344a88068fcde0a417981cb10256040b3
Anurag Mishra <mishranaman123@gmail.com>
2020-07-13 18:29:21 +0530

    tests: fleet management

--- erpnext/hr/doctype/vehicle_log/test_vehicle_log.py
+++ erpnext/hr/doctype/vehicle_log/test_vehicle_log.py
@@ -5,18 +5,24 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
-from frappe.utils import nowdate,flt, cstr,random_string
+from frappe.utils import nowdate,flt,cstr,random_string
 
 class TestVehicleLog(unittest.TestCase):
 	def test_make_vehicle_log_and_syncing_of_odometer_value(self):
 		employee_id = frappe.db.sql("""select name from `tabEmployee` where status='Active' order by modified desc limit 1""")
 		employee_id = employee_id[0][0] if employee_id else None
 
-		license_plate = get_vehicle(employee_id)
+		vehicle = get_vehicle()
+		vehicle.submit()
+
+		#checking opening log for vehicle
+		check_opening_log = frappe.db.exists("Vehicle Log",{"is_opening": 1, "license_plate": vehicle.name})
+
+		self.assertIsNotNone(check_opening_log)
 
 		vehicle_log = frappe.get_doc({
 			"doctype": "Vehicle Log",
-			"license_plate": cstr(license_plate),
+			"license_plate": vehicle.name,
 			"employee":employee_id,
 			"date":frappe.utils.nowdate(),
 			"odometer":5010,
@@ -27,29 +33,17 @@ class TestVehicleLog(unittest.TestCase):
 		vehicle_log.submit()
 
 		#checking value of vehicle odometer value on submit.
-		vehicle = frappe.get_doc("Vehicle", license_plate)
-		self.assertEqual(vehicle.last_odometer, vehicle_log.odometer)
-
-		#checking value vehicle odometer on vehicle log cancellation.
-		last_odometer = vehicle_log.last_odometer
-		current_odometer = vehicle_log.odometer
-		distance_travelled = current_odometer - last_odometer
-
-		vehicle_log.cancel()
 		vehicle.reload()
+		self.assertEqual(vehicle.last_odometer_value, vehicle_log.odometer)
 
-		self.assertEqual(vehicle.last_odometer, current_odometer - distance_travelled)
-
-
-def get_vehicle(employee_id):
+def get_vehicle():
 	license_plate=random_string(10).upper()
 	vehicle = frappe.get_doc({
 			"doctype": "Vehicle",
 			"license_plate": cstr(license_plate),
 			"make": "Maruti",
 			"model": "PCM",
-			"employee": employee_id,
-			"last_odometer":5000,
+			"initial_odometer_value":5000,
 			"acquisition_date":frappe.utils.nowdate(),
 			"location": "Mumbai",
 			"chassis_no": "1234ABCD",
@@ -57,7 +51,7 @@ def get_vehicle(employee_id):
 			"vehicle_value":frappe.utils.flt(500000)
 		})
 	try:
-		vehicle.insert()
+		vehicle.save()
 	except frappe.DuplicateEntryError:
 		pass
-	return license_plate
+	return vehicle
