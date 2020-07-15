974e79087c3d0242d2d60f4f8b9d69542f27814c
Dan Navarro <danjeremynavarro@gmail.com>
2020-07-15 15:47:38 -0600

    Escaped percent characters in item code name

--- erpnext/stock/report/stock_projected_qty/stock_projected_qty.py
+++ erpnext/stock/report/stock_projected_qty/stock_projected_qty.py
@@ -109,7 +109,7 @@ def get_item_map(item_code, include_uom):
 
 	condition = ""
 	if item_code:
-		condition = 'and item_code = {0}'.format(frappe.db.escape(item_code, percent=False))
+		condition = 'and item_code = {0}'.format(frappe.db.escape(item_code))
 
 	cf_field = cf_join = ""
 	if include_uom:
@@ -130,7 +130,7 @@ def get_item_map(item_code, include_uom):
 
 	condition = ""
 	if item_code:
-		condition = 'where parent={0}'.format(frappe.db.escape(item_code, percent=False))
+		condition = 'where parent={0}'.format(frappe.db.escape(item_code))
 
 	reorder_levels = frappe._dict()
 	for ir in frappe.db.sql("""select * from `tabItem Reorder` {condition}""".format(condition=condition), as_dict=1):
