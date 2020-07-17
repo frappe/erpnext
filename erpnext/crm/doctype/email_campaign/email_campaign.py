43e418e69677824ff5fd7295639d7042d7de90b8
Abhishek Balam <abhishekbalam96@gmail.com>
2020-07-18 02:37:20 +0530

    feat: adding Email Group option in Email Campaign

--- erpnext/crm/doctype/email_campaign/email_campaign.py
+++ erpnext/crm/doctype/email_campaign/email_campaign.py
@@ -70,7 +70,12 @@ def send_email_to_leads_or_contacts():
 				send_mail(entry, email_campaign)
 
 def send_mail(entry, email_campaign):
-	recipient = frappe.db.get_value(email_campaign.email_campaign_for, email_campaign.get("recipient"), 'email_id')
+	recipient_list = []
+	if email_campaign.email_campaign_for == "Email Group":
+		for member in frappe.db.get_list("Email Group Member", filters={"email_group": email_campaign.get("recipient")}, fields=['email']):
+			recipient_list.append(member['email'])
+	else:
+		recipient_list.append(frappe.db.get_value(email_campaign.email_campaign_for, email_campaign.get("recipient"), 'email_id'))
 
 	email_template = frappe.get_doc("Email Template", entry.get("email_template"))
 	sender = frappe.db.get_value("User", email_campaign.get("sender"), 'email')
@@ -82,7 +87,7 @@ def send_mail(entry, email_campaign):
 		subject = frappe.render_template(email_template.get("subject"), context),
 		content = frappe.render_template(email_template.get("response"), context),
 		sender = sender,
-		recipients = recipient,
+		recipients = recipient_list,
 		communication_medium = "Email",
 		sent_or_received = "Sent",
 		send_email = True,
