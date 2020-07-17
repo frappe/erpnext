a49d6ffafc29e2dbd4a1d30061c13166254cf4ba
Deepesh Garg <deepeshgarg6@gmail.com>
2020-07-17 14:45:45 +0530

    fix: Move branch code back to bank account

--- erpnext/patches/v12_0/move_bank_account_swift_number_to_bank.py
+++ erpnext/patches/v12_0/move_bank_account_swift_number_to_bank.py
@@ -7,8 +7,7 @@ def execute():
 	if frappe.db.table_exists('Bank') and frappe.db.table_exists('Bank Account') and frappe.db.has_column('Bank Account', 'swift_number'):
 		frappe.db.sql("""
 			UPDATE `tabBank` b, `tabBank Account` ba
-			SET b.swift_number = ba.swift_number, b.branch_code = ba.branch_code
-			WHERE b.name = ba.bank
+			SET b.swift_number = ba.swift_number WHERE b.name = ba.bank
 		""")
 
 	frappe.reload_doc('accounts', 'doctype', 'bank_account')
