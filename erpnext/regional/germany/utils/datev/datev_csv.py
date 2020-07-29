8eb4b36879368e7006ec29ed6396bab7ff9df553
barredterra <14891507+barredterra@users.noreply.github.com>
2020-07-29 18:11:04 +0200

    fix: codacy

--- erpnext/regional/germany/utils/datev/datev_csv.py
+++ erpnext/regional/germany/utils/datev/datev_csv.py
@@ -9,15 +9,12 @@ import pandas as pd
 from frappe import _
 from csv import QUOTE_NONNUMERIC
 from six import BytesIO
-from six import string_types
 from .datev_constants import DataCategory
 from .datev_constants import Transactions
-from .datev_constants import AccountNames
 
 
 def get_datev_csv(data, filters, csv_class):
-	"""
-	Fill in missing columns and return a CSV in DATEV Format.
+	"""Fill in missing columns and return a CSV in DATEV Format.
 
 	For automatic processing, DATEV requires the first line of the CSV file to
 	hold meta data such as the length of account numbers oder the category of
@@ -158,8 +155,7 @@ def get_header(filters, csv_class):
 
 
 def download_csv_files_as_zip(csv_data_list):
-	"""
-	Put CSV files in a zip archive and send that to the client.
+	"""Put CSV files in a zip archive and send that to the client.
 
 	Params:
 	csv_data_list -- list of dicts [{'file_name': 'EXTF_Buchunsstapel.zip', 'csv_data': get_datev_csv()}]
