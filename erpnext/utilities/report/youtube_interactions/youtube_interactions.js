ccf4ab9f852f7af8cf669abd1bd664d9e15dfc58
marination <maricadsouza221197@gmail.com>
2020-08-02 16:26:36 +0530

    chore: Added Interactions Report and behaviour fixes
    - Youtube Interactions Report with Chart and Summary
    - Statistics change in doc on refresh and get updated in db as well

--- erpnext/utilities/report/youtube_interactions/youtube_interactions.js
+++ erpnext/utilities/report/youtube_interactions/youtube_interactions.js
@@ -4,6 +4,17 @@
 
 frappe.query_reports["YouTube Interactions"] = {
 	"filters": [
-
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.now_date(), -12),
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.now_date(),
+		}
 	]
 };
