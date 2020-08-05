8bf15e0ff94a6dee0350c13adeee7c5f23da0a9f
Suraj Shetty <surajshetty3416@gmail.com>
2020-08-05 12:34:09 +0530

    refactor: Repalce validate_and_sanitize_search_inputs() with validate_and_sanitize_search_inputs
    Co-authored-by: Prssanna Desai <prssud@gmail.com>
    Co-authored-by: Shivam Mishra <scmmishra@users.noreply.github.com>

--- erpnext/accounts/doctype/pricing_rule/pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/pricing_rule.py
@@ -433,7 +433,7 @@ def make_pricing_rule(doctype, docname):
 	return doc
 
 @frappe.whitelist()
-@frappe.validate_and_sanitize_search_inputs()
+@frappe.validate_and_sanitize_search_inputs
 def get_item_uoms(doctype, txt, searchfield, start, page_len, filters):
 	items = [filters.get('value')]
 	if filters.get('apply_on') != 'Item Code':
