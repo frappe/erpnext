4a051f6dafe52909a9a5d2940c0d75f299e3969e
Afshan <afshan13k@gmail.com>
2020-08-11 17:40:04 +0530

    fix: handled null condition for actual qty and added report link in stock desk page

--- erpnext/stock/report/stock_qty_vs_serial_no_count/stock_qty_vs_serial_no_count.py
+++ erpnext/stock/report/stock_qty_vs_serial_no_count/stock_qty_vs_serial_no_count.py
@@ -59,6 +59,10 @@ def get_data(warehouse):
 
 		actual_qty = frappe.db.get_value('Bin', fieldname=['actual_qty'], filters={"warehouse": warehouse, "item_code": item.item_code})
 
+		# frappe.db.get_value returns null if no record exist.
+		if not actual_qty:
+			actual_qty = 0
+
 		difference = total_serial_no - actual_qty
 
 		row = {
