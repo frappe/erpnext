6b9f7e35f431bdc80736f1f3dbf46858b89c5cb4
Abhishek Balam <abhishekbalam96@gmail.com>
2020-08-11 15:22:54 +0530

    fix: change opportunity to 'Converted' when items not selected in opportunity itself for making quotation and sales order

--- erpnext/crm/doctype/opportunity/opportunity.py
+++ erpnext/crm/doctype/opportunity/opportunity.py
@@ -119,11 +119,19 @@ class Opportunity(TransactionBase):
 				and q.status not in ('Lost', 'Closed')""", self.name)
 
 	def has_ordered_quotation(self):
-		return frappe.db.sql("""
-			select q.name
-			from `tabQuotation` q, `tabQuotation Item` qi
-			where q.name = qi.parent and q.docstatus=1 and qi.prevdoc_docname =%s
-			and q.status = 'Ordered'""", self.name)
+		if not self.with_items:
+			return frappe.get_all('Quotation',
+				{
+					'opportunity': self.name,
+					'status': 'Ordered',
+					'docstatus': 1
+				}, 'name')
+		else:
+			return frappe.db.sql("""
+				select q.name
+				from `tabQuotation` q, `tabQuotation Item` qi
+				where q.name = qi.parent and q.docstatus=1 and qi.prevdoc_docname =%s
+				and q.status = 'Ordered'""", self.name)
 
 	def has_lost_quotation(self):
 		lost_quotation = frappe.db.sql("""
