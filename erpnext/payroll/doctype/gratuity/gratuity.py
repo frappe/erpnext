f943b23b1f595389e6d94cd4f4df966eced62ac6
Anurag Mishra <mishranaman123@gmail.com>
2020-08-17 14:51:25 +0530

    fix: Some enhancements and better validation

--- erpnext/payroll/doctype/gratuity/gratuity.py
+++ erpnext/payroll/doctype/gratuity/gratuity.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _, bold
 from frappe.model.document import Document
-from frappe.utils import flt, get_datetime
+from frappe.utils import flt, get_datetime, get_link_to_form
 from math import floor
 
 from frappe.utils import get_datetime
@@ -43,8 +43,7 @@ class Gratuity(Document):
 		""", (self.name, self.employee), as_dict=1)[0].paid_amount
 
 		if flt(paid_amount) > self.amount:
-			frappe.throw(_("Row {0}# Paid Amount cannot be greater than Total amount"),
-				EmployeeAdvanceOverPayment)
+			frappe.throw(_("Row {0}# Paid Amount cannot be greater than Total amount"))
 
 
 		self.db_set("paid_amount", paid_amount)
@@ -65,7 +64,7 @@ def calculate_work_experience(employee, gratuity_rule):
 
 	date_of_joining, relieving_date = frappe.db.get_value('Employee', employee, ['date_of_joining', 'relieving_date'])
 	if not relieving_date:
-		frappe.throw(_("Please set Relieving Date for employee: {0}").format(bold(employee)))
+		frappe.throw(_("Please set Relieving Date for employee: {0}").format(bold(get_link_to_form("Employee", employee))))
 
 	method = frappe.db.get_value("Gratuity Rule", gratuity_rule, "work_experience_calculation_function")
 
@@ -108,11 +107,13 @@ def get_non_working_days(employee, relieving_date, status):
 		filters["leave_type"] =  ("IN", lwp_leave_types)
 
 
-	record = frappe.get_all("Attendance", filters=filters, fields = ["COUNT(name) as total_lwp"], debug = 1)
+	record = frappe.get_all("Attendance", filters=filters, fields = ["COUNT(name) as total_lwp"])
 	return record[0].total_lwp if len(record) else 0
 
 def calculate_gratuity_amount(employee, gratuity_rule, experience):
 	applicable_earnings_component = frappe.get_all("Gratuity Applicable Component", filters= {'parent': gratuity_rule}, fields=["salary_component"])
+	if len(applicable_earnings_component) == 0:
+		frappe.throw(_("No Applicable Earnings Component found for Gratuity Rule: {0}").format(bold(get_link_to_form("Gratuity Rule",gratuity_rule))))
 	applicable_earnings_component = [component.salary_component for component in applicable_earnings_component]
 
 	slabs = get_gratuity_rule_slabs(gratuity_rule)
@@ -137,16 +138,16 @@ def calculate_gratuity_amount(employee, gratuity_rule, experience):
 				slab_found = True
 				break
 
-			if experience > slab.to_year and experience > slab.from_year:
+			if experience > slab.to_year and experience > slab.from_year and slab.to_year !=0:
 				gratuity_amount += (slab.to_year - slab.from_year) * total_applicable_components_amount * slab.fraction_of_applicable_earnings
 				year_left -= (slab.to_year - slab.from_year)
 				slab_found = True
-			elif slab.from_year <= experience < slab.to_year:
+			elif slab.from_year <= experience and (experience < slab.to_year or slab.to_year == 0):
 				gratuity_amount += year_left * total_applicable_components_amount * slab.fraction_of_applicable_earnings
 				slab_found = True
 
-		if not slab_found:
-			frappe.throw(_("No Suitable Slab found for Calculation of gratuity amount in Gratuity Rule: {0}").format(bold(gratuity_rule)))
+	if not slab_found:
+		frappe.throw(_("No Suitable Slab found for Calculation of gratuity amount in Gratuity Rule: {0}").format(bold(gratuity_rule)))
 
 
 	return gratuity_amount
@@ -174,7 +175,7 @@ def get_total_applicable_component_amount(employee, applicable_earnings_componen
 	return total_applicable_components_amount
 
 def get_gratuity_rule_slabs(gratuity_rule):
-	return frappe.get_all("Gratuity Rule Slab", filters= {'parent': gratuity_rule}, fields = ["*"])
+	return frappe.get_all("Gratuity Rule Slab", filters= {'parent': gratuity_rule}, fields = ["*"], order_by="idx")
 
 def get_salary_structure(employee):
 	return frappe.get_list("Salary Structure Assignment", filters = {"employee": employee, 'docstatus': 1}, fields=["from_date", "salary_structure"], order_by = "from_date desc")[0].salary_structure
