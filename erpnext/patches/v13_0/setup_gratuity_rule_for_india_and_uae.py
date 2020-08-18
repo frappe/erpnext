25c356894d58b46692f4de0a34affa8008b8d707
Anurag Mishra <mishranaman123@gmail.com>
2020-08-18 14:13:54 +0530

    test: gratuity

--- erpnext/patches/v13_0/setup_gratuity_rule_for_india_and_uae.py
+++ erpnext/patches/v13_0/setup_gratuity_rule_for_india_and_uae.py
@@ -3,13 +3,16 @@
 
 from __future__ import unicode_literals
 
-import erpnext
+import erpnext, frappe
 
 def execute():
+    frappe.reload_doc('payroll', 'doctype', 'gratuity_rule')
+    frappe.reload_doc('payroll', 'doctype', 'gratuity_rule_slab')
+    frappe.reload_doc('payroll', 'doctype', 'gratuity_applicable_component')
     region = erpnext.get_region()
     if region == "India":
-        from erpnext.regional.india.setup import create_standard_documents
-        create_standard_documents()
+        from erpnext.regional.india.setup import create_gratuity_rule
+        create_gratuity_rule()
     elif region == "United Arab Emirates":
-        from erpnext.regional.united_arab_emirates.setup import create_standard_documents
-        create_standard_documents()
+        from erpnext.regional.united_arab_emirates.setup import create_gratuity_rule
+        create_gratuity_rule()
