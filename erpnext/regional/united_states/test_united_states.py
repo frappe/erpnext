d659774190c60cb43816cf31a163e692360d5915
Afshan <afshan13k@gmail.com>
2020-08-28 13:40:48 +0530

    fix:tests

--- erpnext/regional/united_states/test_united_states.py
+++ erpnext/regional/united_states/test_united_states.py
@@ -44,8 +44,8 @@ def make_payment_entry_to_irs_1099_supplier():
     pe.payment_type = "Pay"
     pe.company = "_Test Company 1"
     pe.posting_date = "2016-01-10"
-    pe.paid_from = "_Test Bank USD - _TC"
-    pe.paid_to = "_Test Payable USD - _TC"
+    pe.paid_from = "_Test Bank USD - _TC1"
+    pe.paid_to = "_Test Payable USD - _TC1"
     pe.paid_amount = 100
     pe.received_amount = 100
     pe.reference_no = "For IRS 1099 testing"
