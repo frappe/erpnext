9ce38de439ebd2a240539542a8de40f8ae2d2417
marination <maricadsouza221197@gmail.com>
2020-09-07 13:27:17 +0530

    chore: Key Validation and restructure
    - Added API Key validation in Video Settings
    - Moved Statistics section above description in Video
    - Moved id retrieving from URL to py side
    - Removed js side calls, added section toggling in js
    - Restructured Code

--- erpnext/utilities/doctype/video/video.js
+++ erpnext/utilities/doctype/video/video.js
@@ -3,29 +3,15 @@
 
 frappe.ui.form.on('Video', {
 	refresh: function (frm) {
-		if (frm.doc.provider === "YouTube") {
-			frappe.db.get_single_value("Video Settings", "enable_youtube_tracking").then(value => {
-				if (value) {
-					frm.events.get_video_stats(frm);
-				} else {
-					frm.set_df_property('youtube_tracking_section', 'hidden', true);
-				}
-			});
-		}
-
+		frm.events.toggle_youtube_statistics_section(frm);
 		frm.add_custom_button("Watch Video", () => frappe.help.show_video(frm.doc.url, frm.doc.title));
 	},
 
-	get_video_stats: (frm) => {
-		const expression = '(?:youtube.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu.be/)([^"&?\\s]{11})';
-		var youtube_id = frm.doc.url.match(expression)[1];
-
-		frappe.call({
-			method: "erpnext.utilities.doctype.video.video.get_video_stats",
-			args: {
-				docname: frm.doc.name,
-				youtube_id: youtube_id
-			}
-		});
+	toggle_youtube_statistics_section: (frm) => {
+		if (frm.doc.provider === "YouTube") {
+			frappe.db.get_single_value("Video Settings", "enable_youtube_tracking").then( val => {
+				frm.toggle_display("youtube_tracking_section", val);
+			});
+		}
 	}
 });
