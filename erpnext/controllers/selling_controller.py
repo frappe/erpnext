b62d840d98536947717b9f27846f74279f4de1fc
barry86m <65860880+barry86m@users.noreply.github.com>
2020-09-07 01:33:37 +0100

    Update selling_controller.py

--- erpnext/controllers/selling_controller.py
+++ erpnext/controllers/selling_controller.py
@@ -80,7 +80,7 @@ class SellingController(StockController):
 
 			party_details = _get_party_details(customer,
 				ignore_permissions=self.flags.ignore_permissions,
-				posting_date=self.posting_date if self.posting_date else None,
+				posting_date=self.posting_date if hasattr(self, 'posting_date') else None,
 				doctype=self.doctype, company=self.company,
 				fetch_payment_terms_template=fetch_payment_terms_template,
 				party_address=self.customer_address, shipping_address=self.shipping_address_name)
