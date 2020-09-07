fa98e81f0b4041e9496cb7f90c852203e7d39355
barry86m <65860880+barry86m@users.noreply.github.com>
2020-09-07 01:36:15 +0100

    fix: sql error on saving sales invoice without a sales order

--- erpnext/controllers/selling_controller.py
+++ erpnext/controllers/selling_controller.py
@@ -81,6 +81,7 @@ class SellingController(StockController):
 			party_details = _get_party_details(customer,
 				ignore_permissions=self.flags.ignore_permissions,
 				doctype=self.doctype, company=self.company,
+				posting_date=self.posting_date if hasattr(self, 'posting_date') else None,
 				fetch_payment_terms_template=fetch_payment_terms_template,
 				party_address=self.customer_address, shipping_address=self.shipping_address_name)
 			if not self.meta.get_field("sales_team"):
