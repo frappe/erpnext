842b600deff8aa55743028075a6ff628c2f28281
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-09-16 16:08:57 +0530

    feat: process medication orders and update stock

--- erpnext/healthcare/doctype/inpatient_medication_tool/inpatient_medication_tool.js
+++ erpnext/healthcare/doctype/inpatient_medication_tool/inpatient_medication_tool.js
@@ -5,6 +5,12 @@ frappe.ui.form.on('Inpatient Medication Tool', {
 	refresh: function(frm) {
 		frm.disable_save();
 
+		frappe.db.get_value('Healthcare Settings', 'Healthcare Settings', ['inpatient_medication_warehouse'], (r) => {
+			if (r && r.inpatient_medication_warehouse) {
+				frm.set_value('warehouse', r.inpatient_medication_warehouse);
+			}
+		})
+
 		const assets = [
 			"/assets/frappe/css/frappe-datatable.css",
 			"/assets/frappe/js/lib/clusterize.min.js",
@@ -26,8 +32,9 @@ frappe.ui.form.on('Inpatient Medication Tool', {
 				},
 				callback: function(r) {
 					if (r.message) {
-						frm.events.render_datatable(frm, r.message);
-						// frm.doc.show_submit = true;
+						let orders_datatable = frm.events.render_datatable(frm, r.message);
+						frm.doc.show_submit = true;
+						frm.events.update_orders(frm, orders_datatable);
 					}
 				}
 			});
@@ -54,17 +61,88 @@ frappe.ui.form.on('Inpatient Medication Tool', {
 			columns: columns,
 			checkboxColumn: true,
 			addCheckboxColumn: true
-		})
+		});
+
+		return orders_datatable
 	},
 
 	prepare_columns() {
 		return [
 			{id: 'patient', name: __('Patient Patient Name)'), content: __('Patient - Patient Name'), field: "patient", width: 200},
 			{id: 'service_unit', name: __('Service Unit'), content: __('Service Unit'), field: 'service_unit', width: 200},
-			{id: 'drug', name: __('Drug(Drug Name)'), field: 'drug', content: __('Drug - Drug Name'), width: 200},
+			{id: 'drug', name: __('Drug Code'), field: 'drug', content: __('Drug Code'), width: 170},
+			{id: 'drug_name', name: __('Drug Name'), field: 'drug_name', content: __('Drug Name'), width: 200},
 			{id: 'dosage', name: __('Dosage'), content: __('Dosage'), field: 'dosage', width: 60},
 			{id: 'dosage_form', name: __('Dosage'), content: __('Dosage Form'), field: 'dosage_form', width: 100},
 			{id: 'time', name: __('Time'), field: 'time', content: __('Time'), width: 100}
 		];
 	},
+
+	update_orders: function(frm, orders_datatable) {
+		if (frm.doc.show_submit) {
+			frm.page.set_primary_action(__("Submit"), function() {
+				const orders = frm.events.get_completed_orders(frm, orders_datatable);
+				frappe.call({
+					doc: frm.doc,
+					method: 'process_medication_orders',
+					args: {
+						"orders": orders
+					},
+					freeze: true,
+					callback: function(r) {
+						if (!r.exc) {
+							if (r.message == 'insufficient stock') {
+								let msg = __('Stock quantity to process the Inpatient Medication Orders is not available in the Warehouse {0}. Do you want to record a Stock Entry?',
+									[frm.doc.warehouse.bold()]);
+								frappe.confirm(
+									msg,
+									function() {
+										frappe.call({
+											doc: frm.doc,
+											method: 'make_material_receipt',
+											args: {
+												"orders": orders
+											},
+											freeze: true,
+											callback: function(r) {
+												if (r.message) {
+													frappe.msgprint({
+														title: __(`Success`),
+														message: __(`Stock Entry {0} submitted successfully. Click Submit to process the medication orders`,
+															[r.message.bold()]),
+														indicator: 'green'
+													})
+												} else {
+													frappe.msgprint({
+														title: __(`Failure`),
+														message: __(`Could not submit Stock Entries.`),
+														indicator: 'red'
+													})
+												}
+											}
+										});
+									}
+								);
+							} else if (r.message == 'success') {
+								frappe.msgprint({
+									title: __(`Success`),
+									message: __(`Medication Orders Processed successfully`),
+									indicator: 'green'
+								})
+							}
+						}
+					}
+				});
+			});
+		}
+		else {
+			frm.page.clear_primary_action();
+		}
+	},
+
+	get_completed_orders: function(frm, orders_datatable) {
+		const indexes = orders_datatable.rowmanager.getCheckedRows();
+		const orders = indexes.map(i => orders_datatable.datamanager.data[i]).filter(i => i != undefined);
+		return orders
+	}
 });
--- erpnext/healthcare/doctype/inpatient_medication_tool/inpatient_medication_tool.py
+++ erpnext/healthcare/doctype/inpatient_medication_tool/inpatient_medication_tool.py
@@ -5,16 +5,93 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate, nowtime, flt
+from erpnext.stock.stock_ledger import get_previous_sle
+from erpnext.healthcare.doctype.healthcare_settings.healthcare_settings import get_account
 
 class InpatientMedicationTool(Document):
-	pass
+	def process_medication_orders(self, orders):
+		orders = self.set_available_qty(orders)
+		in_stock = check_stock_qty(orders)
+		if not in_stock:
+			return 'insufficient stock'
+
+		self.make_stock_entry(orders)
+		self.update_medication_orders(orders)
+		return 'success'
+
+	def set_available_qty(self, orders):
+		for d in orders:
+			d['available_qty'] = get_stock_qty(d.get('drug'), self.warehouse)
+
+		return orders
+
+	def make_material_receipt(self, orders):
+		stock_entry = frappe.new_doc('Stock Entry')
+		stock_entry.stock_entry_type = 'Material Receipt'
+		stock_entry.to_warehouse = self.warehouse
+		stock_entry.company = self.company
+		cost_center = frappe.get_cached_value('Company',  self.company,  'cost_center')
+		expense_account = get_account(None, 'expense_account', 'Healthcare Settings', self.company)
+
+		for item in orders:
+			if flt(item.get('available_qty')) < flt(item.get('dosage')):
+				se_child = stock_entry.append('items')
+				se_child.item_code = item.get('drug')
+				se_child.item_name = item.get('drug_name')
+				se_child.uom = frappe.db.get_value('Item', item.get('drug'), 'stock_uom')
+				se_child.stock_uom = se_child.uom
+				se_child.qty = flt(flt(item.get('dosage')) - flt(item.get('available_qty')))
+				se_child.t_warehouse = self.warehouse
+				# in stock uom
+				se_child.conversion_factor = 1
+				se_child.cost_center = cost_center
+				se_child.expense_account = expense_account
+
+		stock_entry.submit()
+		return stock_entry.name
+
+	def make_stock_entry(self, orders):
+		stock_entry = frappe.new_doc('Stock Entry')
+		stock_entry.stock_entry_type = 'Material Issue'
+		stock_entry.from_warehouse = self.warehouse
+		stock_entry.company = self.company
+		cost_center = frappe.get_cached_value('Company',  self.company,  'cost_center')
+		expense_account = get_account(None, 'expense_account', 'Healthcare Settings', self.company)
+
+		for item in orders:
+			se_child = stock_entry.append('items')
+			se_child.item_code = item.get('drug')
+			se_child.item_name = item.get('drug_name')
+			se_child.uom = frappe.db.get_value('Item', item.get('drug'), 'stock_uom')
+			se_child.stock_uom = se_child.uom
+			se_child.qty = flt(item.get('dosage'))
+			# in stock uom
+			se_child.conversion_factor = 1
+			se_child.cost_center = cost_center
+			se_child.expense_account = expense_account
+
+		stock_entry.submit()
+		return stock_entry.name
+
+	def update_medication_orders(self, orders):
+		completed_orders = []
+		for entry in orders:
+			completed_orders.append(entry.get('name'))
+
+		frappe.db.sql("""
+			UPDATE `tabInpatient Medication Order Entry`
+			SET is_completed = 1
+			WHERE name IN %(orders)s
+		""", {'orders': completed_orders})
+
 
 @frappe.whitelist()
 def get_medication_orders(date):
 	data = frappe.db.sql("""
 		SELECT
 			ip.inpatient_record, ip.patient, ip.patient_name,
-			entry.drug, entry.drug_name, entry.dosage, entry.dosage_form, entry.time
+			entry.name, entry.drug, entry.drug_name, entry.dosage, entry.dosage_form, entry.time
 		FROM
 			`tabInpatient Medication Order` ip
 		INNER JOIN
@@ -22,7 +99,8 @@ def get_medication_orders(date):
 		ON
 			ip.name = entry.parent
 		WHERE
-			entry.date = %(date)s
+			entry.date = %(date)s and
+			entry.is_completed != 1
 		ORDER BY
 			entry.time
 	""", {'date': date}, as_dict=1)
@@ -34,12 +112,28 @@ def get_medication_orders(date):
 		if entry['patient'] != entry['patient_name']:
 			entry['patient'] = entry['patient'] + ' - ' + entry['patient_name']
 
-		if entry['drug'] != entry['drug_name']:
-			entry['drug'] = entry['drug'] + ' - ' + entry['drug_name']
-
 	return data
 
 def get_current_healthcare_service_unit(inpatient_record):
 	ip_record = frappe.get_doc('Inpatient Record', inpatient_record)
 	return ip_record.inpatient_occupancies[-1].service_unit
 
+def check_stock_qty(orders):
+	allow_negative_stock = frappe.db.get_single_value('Stock Settings', 'allow_negative_stock')
+
+	in_stock = True
+	for d in orders:
+		# validate qty
+		if not allow_negative_stock and flt(d.get('available_qty')) < flt(d.get('dosage')):
+			in_stock = False
+			break
+
+	return in_stock
+
+def get_stock_qty(item_code, warehouse):
+	return get_previous_sle({
+		'item_code': item_code,
+		'warehouse': warehouse,
+		'posting_date': nowdate(),
+		'posting_time': nowtime()
+	}).get('qty_after_transaction') or 0
