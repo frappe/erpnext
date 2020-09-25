a26c6b4c2d884202cd16901cfaf43a9b062c6d1f
Mangesh-Khairnar <mkhairnar10@gmail.com>
2020-09-25 23:56:39 +0530

    fix: fetch account balance info

--- erpnext/erpnext_integrations/doctype/mpesa_settings/mpesa_settings.js
+++ erpnext/erpnext_integrations/doctype/mpesa_settings/mpesa_settings.js
@@ -2,6 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Mpesa Settings', {
-	// refresh: function(frm) {
-	// }
+	onload_post_render: function(frm) {
+		frm.events.setup_account_balance_html(frm);
+	},
+
+	get_account_balance: function(frm) {
+		if (!frm.initiator_name && !frm.security_credentials) return;
+		frappe.call({
+			method: "get_account_balance_info",
+			doc: frm.doc
+		});
+	},
+
+	setup_account_balance_html: function(frm) {
+		console.log(frm.doc.account_balance)
+		$("div").remove(".form-dashboard-section.custom");
+		frm.dashboard.add_section(
+			frappe.render_template('account_balance', {
+				data: JSON.parse(frm.doc.account_balance)
+			})
+		);
+		frm.dashboard.show();
+	}
+
 });
