39dabe7381b62a3b8df379772f98e7043ba6fd94
Saqib Ansari <nextchamp.saqib@gmail.com>
2020-09-25 21:27:50 +0530

    feat: read public key file

--- erpnext/erpnext_integrations/doctype/e_invoice_settings/e_invoice_settings.py
+++ erpnext/erpnext_integrations/doctype/e_invoice_settings/e_invoice_settings.py
@@ -3,8 +3,19 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe.utils import cstr
 from frappe.model.document import Document
 
 class EInvoiceSettings(Document):
-	pass
+	def validate(self):
+		pass
+	
+	def before_save(self):
+		if not self.public_key or self.has_value_changed('public_key_file'):
+			self.public_key = self.read_key_file()
+
+	def read_key_file(self):
+		key_file = frappe.get_doc('File', dict(attached_to_name=self.doctype, attached_to_field='public_key_file'))
+		with open(key_file.get_full_path(), 'rb') as f:
+			return cstr(f.read())
