b52a9283bb3fd5aeb7c3f05b96ccba0a28b051eb
Saqib Ansari <nextchamp.saqib@gmail.com>
2020-09-25 21:52:00 +0530

    feat: rsa encryption with public key

--- erpnext/erpnext_integrations/doctype/e_invoice_settings/e_invoice_settings.js
+++ erpnext/erpnext_integrations/doctype/e_invoice_settings/e_invoice_settings.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('E Invoice Settings', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.trigger("show_fetch_token_btn");
+	},
 
-	// }
+	show_fetch_token_btn(frm) {
+		frm.add_custom_button(__("Fetch Token"),
+			() => {
+				frm.call({
+					doc: frm.doc,
+					method: 'make_authentication_request',
+					freeze: true
+				});
+			}
+		);
+	}
 });
--- erpnext/erpnext_integrations/doctype/e_invoice_settings/e_invoice_settings.py
+++ erpnext/erpnext_integrations/doctype/e_invoice_settings/e_invoice_settings.py
@@ -3,9 +3,15 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import os
+import json
+import base64
 import frappe
 from frappe.utils import cstr
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
 from frappe.model.document import Document
+from frappe.integrations.utils import make_post_request
 
 class EInvoiceSettings(Document):
 	def validate(self):
@@ -19,3 +25,32 @@ class EInvoiceSettings(Document):
 		key_file = frappe.get_doc('File', dict(attached_to_name=self.doctype, attached_to_field='public_key_file'))
 		with open(key_file.get_full_path(), 'rb') as f:
 			return cstr(f.read())
+	
+	def rsa_encrypt(self, msg, key):
+		if not (isinstance(msg, bytes) or isinstance(msg, bytearray)):
+			msg = str.encode(msg)
+
+		rsa_pub_key = RSA.import_key(key)
+		cipher = PKCS1_v1_5.new(rsa_pub_key)
+		enc_msg = cipher.encrypt(msg)
+		b64_enc_msg = base64.b64encode(enc_msg)
+		return b64_enc_msg.decode()
+
+	def make_authentication_request(self):
+		endpoint = 'https://einv-apisandbox.nic.in/eivital/v1.03/auth'
+		headers = { 'content-type': 'application/json' }
+		headers.update(dict(client_id=self.client_id, client_secret=self.client_secret))
+		payload = dict(UserName=self.username, ForceRefreshAccessToken=bool(self.auto_refresh_token))
+
+		appkey = bytearray(os.urandom(32))
+		enc_appkey = self.rsa_encrypt(appkey, self.public_key)
+
+		password = self.get_password(fieldname='password')
+		enc_password = self.rsa_encrypt(password, self.public_key)
+
+		payload.update(dict(Password=enc_password, AppKey=enc_appkey))
+
+		res = make_post_request(endpoint, headers=headers, data=json.dumps({ 'data': payload }))
+
+		print(res)
+
