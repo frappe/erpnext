79aca28453c2eb5a7d8c917459269c2efcb0a96d
hasnain2808 <hasnain2808@gmail.com>
2020-09-28 12:27:41 +0530

    refactor(UAE VAT 21): rename UAE VAT - UAE VAT 21

--- erpnext/regional/report/uae_vat_21/uae_vat_21.js
+++ erpnext/regional/report/uae_vat_21/uae_vat_21.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["UAE VAT"] = {
+frappe.query_reports["UAE VAT 21"] = {
 	"filters": [
 		{
 			"fieldname": "company",
