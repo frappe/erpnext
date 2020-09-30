81e6a0b9348be2fa15baa3142a7bc255069b9af7
Shivam Mishra <scmmishra@users.noreply.github.com>
2020-09-30 12:01:35 +0530

    fix: translate error log

--- erpnext/non_profit/doctype/membership/membership.py
+++ erpnext/non_profit/doctype/membership/membership.py
@@ -225,7 +225,7 @@ def trigger_razorpay_subscription(*args, **kwargs):
 		member.save(ignore_permissions=True)
 	except Exception as e:
 		message = "{0}\n\n{1}\n\n{2}: {3}".format(e, frappe.get_traceback(), __("Payment ID"), payment.id)
-		log = frappe.log_error(message, "Error creating membership entry for {0}".format(member.name))
+		log = frappe.log_error(message, _("Error creating membership entry for {0}").format(member.name))
 		notify_failure(log)
 		return { 'status': 'Failed', 'reason': e}
 
