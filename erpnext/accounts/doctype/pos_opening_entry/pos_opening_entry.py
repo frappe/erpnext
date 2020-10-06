a802161e787319d1f635a52b0e91947417b090d4
Saqib Ansari <nextchamp.saqib@gmail.com>
2020-10-06 13:04:26 +0530

    fix: show all invalid mode of payments

--- erpnext/accounts/doctype/pos_opening_entry/pos_opening_entry.py
+++ erpnext/accounts/doctype/pos_opening_entry/pos_opening_entry.py
@@ -23,12 +23,17 @@ class POSOpeningEntry(StatusUpdater):
 			frappe.throw(_("User {} has been disabled. Please select valid user/cashier".format(self.user)))
 	
 	def validate_payment_method_account(self):
+		invalid_mode_of_payments = []
 		for d in self.balance_details:
 			account = frappe.db.get_value("Mode of Payment Account", 
 				{"parent": d.mode_of_payment, "company": self.company}, "default_account")
 			if not account:
-				frappe.throw(_("Please set default Cash or Bank account in Mode of Payment {0}")
-					.format(get_link_to_form("Mode of Payment", mode_of_payment)), title=_("Missing Account"))
+				invalid_mode_of_payments.append(get_link_to_form("Mode of Payment", mode_of_payment))
+		
+		if invalid_mode_of_payments:
+			frappe.throw(_("Please set default Cash or Bank account in Mode of Payment{} {}")
+				.format("s" if len(invalid_mode_of_payments) > 1 else "", ", ".join(invalid_mode_of_payments)),
+				title=_("Missing Account"))
 
 	def on_submit(self):
 		self.set_status(update=True)
