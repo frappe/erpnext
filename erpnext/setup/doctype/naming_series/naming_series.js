b6d0124a98096199e500bd28f50708ac68b07cb8
beingeek <furqan.790@outlook.com>
2020-10-06 20:06:24 +0500

    fix: naming series enhancements

--- erpnext/setup/doctype/naming_series/naming_series.js
+++ erpnext/setup/doctype/naming_series/naming_series.js
@@ -12,6 +12,7 @@ frappe.ui.form.on("Naming Series", {
 		frappe.call({
 			method: "get_transactions",
 			doc: frm.doc,
+			freeze: 1,
 			callback: function(r) {
 				frm.set_df_property("select_doc_for_series", "options", r.message.transactions);
 				frm.set_df_property("prefix", "options", r.message.prefixes);
@@ -24,6 +25,7 @@ frappe.ui.form.on("Naming Series", {
 		frappe.call({
 			method: "get_options",
 			doc: frm.doc,
+			freeze: 1,
 			callback: function(r) {
 				frm.set_value("set_options", r.message);
 				if(r.message && r.message.split('\n')[0]=='')
@@ -34,9 +36,20 @@ frappe.ui.form.on("Naming Series", {
 	},
 
 	prefix: function(frm) {
+		frm.doc.new_prefix = "";
+		frm.refresh_field('new_prefix');
+		frm.events.get_current(frm);
+	},
+
+	new_prefix: function(frm) {
+		frm.events.get_current(frm);
+	},
+
+	get_current: function(frm) {
 		frappe.call({
 			method: "get_current",
 			doc: frm.doc,
+			freeze: 1,
 			callback: function(r) {
 				frm.refresh_field("current_value");
 			}
@@ -47,8 +60,24 @@ frappe.ui.form.on("Naming Series", {
 		frappe.call({
 			method: "update_series",
 			doc: frm.doc,
+			freeze: 1,
 			callback: function(r) {
 				frm.events.get_doc_and_prefix(frm);
+			}
+		});
+	},
+
+	update_series_start: function(frm) {
+		frappe.call({
+			method: "update_series_start",
+			doc: frm.doc,
+			freeze: 1,
+			callback: function(r) {
+				if (!r || !r.message) {
+					frm.events.get_current(frm);
+				} else {
+					frm.events.get_doc_and_prefix(frm);
+				}
 			}
 		});
 	}
