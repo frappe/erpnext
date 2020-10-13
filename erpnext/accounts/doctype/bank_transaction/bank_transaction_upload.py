027ae75537ab512c635f25550c17e459ce91319e
Glen Whitney <glen@studioinfinity.org>
2020-10-13 05:36:25 +0000

    feat(Bank Reconciliation): more options in configure statement import
      Prior to this commit, it was impossible for a single column in a
      (imported, spreadsheet) bank statement to populate different fields
      in a bank transaction depending on its value. Thus, for example, if
      your statements just list a signed "Amount" rather than having
      separate "Credit" and "Debit" columns, it was impossible to import
      them.
    
      This commit adds an optional "If Value Matches" field to the "Bank
      Transaction Mapping" record. This field, if defined, should be a
      regular expression, and the value will only be assigned to the
      associated Bank Transaction field if the value matches the given
      regular expression. Moreover, the value used to specify the Bank
      Transaction field will be the first "group" (parenthesized
      sub-expression in the regular expression) in the match. So this
      way, you can detect a debit by a minus sign included in the value
      of an "Amount" column, and then filter out that minus sign for
      setting the debit field of the Bank Transaction. Therefore, it now
      can make sense to have more than one Bank Transaction Mapping
      record with the same "Column in Bank File" value, as long as they
      have different regular expressions in the "If Value Matches" field.
    
      Similarly, this facility could be used to extract a transaction_id
      and a text description from a single "Description" column with a
      structured format. (In our case, the "Description" column in our
      exported bank statements often has the format "Deposit ID Number
      999999.")
    
      In addition, prior to this commit, if the bank statement included
      no currency column, the currency of the generated Bank
      Transactions would end up taking on a rather arbitrary value,
      presumably based on the order of entries in the internal Currency
      list. This commit causes the Bank Transaction currency to default
      to the Default Currency of the Company which holds the Bank
      Account, which seems to generally produce more useful results.

--- erpnext/accounts/doctype/bank_transaction/bank_transaction_upload.py
+++ erpnext/accounts/doctype/bank_transaction/bank_transaction_upload.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 import json
+import re
 from frappe.utils import getdate
 from frappe.utils.dateutils import parse_date
 from six import iteritems
@@ -36,14 +37,24 @@ def upload_bank_statement():
 def create_bank_entries(columns, data, bank_account):
 	header_map = get_header_mapping(columns, bank_account)
 
+	default_fields = {}
+	if 'currency' not in header_map:
+		default_fields['currency'] = get_bank_default_currency(bank_account)
+
 	success = 0
 	errors = 0
 	for d in json.loads(data):
 		if all(item is None for item in d) is True:
 			continue
-		fields = {}
-		for key, value in iteritems(header_map):
-			fields.update({key: d[int(value)-1]})
+		fields = default_fields.copy()
+		for bank_field, source in iteritems(header_map):
+			value = d[source[0]]
+			if len(source) > 1:
+				found = re.match(source[1], value)
+				if not found:
+					continue
+				value = found.group(1)
+			fields[bank_field] = value
 
 		try:
 			bank_transaction = frappe.get_doc({
@@ -64,10 +75,12 @@ def create_bank_entries(columns, data, bank_account):
 def get_header_mapping(columns, bank_account):
 	mapping = get_bank_mapping(bank_account)
 
+	header_locs = {col["content"] : col["colIndex"] for col in json.loads(columns)}
 	header_map = {}
-	for column in json.loads(columns):
-		if column["content"] in mapping:
-			header_map.update({mapping[column["content"]]: column["colIndex"]})
+	for bank_field, source in iteritems(mapping):
+		if source[0] in header_locs:
+			source[0] = header_locs[source[0]] - 1
+			header_map[bank_field] = source
 
 	return header_map
 
@@ -75,6 +88,16 @@ def get_bank_mapping(bank_account):
 	bank_name = frappe.db.get_value("Bank Account", bank_account, "bank")
 	bank = frappe.get_doc("Bank", bank_name)
 
-	mapping = {row.file_field:row.bank_transaction_field for row in bank.bank_transaction_mapping}
+	mapping = {}
+	for row in bank.bank_transaction_mapping:
+		if row.if_value_matches:
+			mapping[row.bank_transaction_field] = [row.file_field, row.if_value_matches]
+		else:
+			mapping[row.bank_transaction_field] = [row.file_field]
 
 	return mapping
+
+def get_bank_default_currency(bank_account):
+	company_name = frappe.db.get_value("Bank Account", bank_account, "company")
+	company = frappe.get_doc("Company", company_name)
+	return company.default_currency
