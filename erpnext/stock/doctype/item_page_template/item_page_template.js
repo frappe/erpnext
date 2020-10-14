a62a21b9a845b118f9a04843316b29f74a224767
Deepesh Garg <deepeshgarg6@gmail.com>
2020-10-14 14:59:35 +0530

    fix: Delete Item Page Template

--- erpnext/stock/doctype/item_page_template/item_page_template.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Web Page Block", {
-	edit_values(frm, cdt, cdn) {
-		erpnext.utils.add_web_template(frm, cdt, cdn);
-	}
-});
