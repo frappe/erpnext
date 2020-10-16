1a24e9c298d3c7460b5566a0f43a00d0317430a4
Aditya Duggal <adityaduggal@gmail.com>
2020-10-16 11:51:31 +0530

    To avoid None type error if the Pricing Rule is not applicable to any applicable_for
    There are many cases when you want a company wide pricing rule without the applicable_for field and hence for such rules it was giving a none type error.

--- erpnext/accounts/doctype/pricing_rule/pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/pricing_rule.py
@@ -49,9 +49,10 @@ class PricingRule(Document):
 			if self.apply_on == apply_on and len(self.get(field) or []) < 1:
 				throw(_("{0} is not added in the table").format(apply_on), frappe.MandatoryError)
 
-		tocheck = frappe.scrub(self.get("applicable_for", ""))
-		if tocheck and not self.get(tocheck):
-			throw(_("{0} is required").format(self.meta.get_label(tocheck)), frappe.MandatoryError)
+		if self.get("applicable_for", "") != None:
+			tocheck = frappe.scrub(self.get("applicable_for", ""))
+			if tocheck and not self.get(tocheck):
+				throw(_("{0} is required").format(self.meta.get_label(tocheck)), frappe.MandatoryError)
 
 		if self.apply_rule_on_other:
 			o_field = 'other_' + frappe.scrub(self.apply_rule_on_other)
