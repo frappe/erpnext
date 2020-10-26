fbb9f84709f0ca0aa44d45e1652e8fa2a2011d0f
Saqib Ansari <nextchamp.saqib@gmail.com>
2020-10-26 21:12:35 +0530

    fix: update all eway-bill fields

--- erpnext/patches/v12_0/add_transporter_address_field.py
+++ erpnext/patches/v12_0/add_transporter_address_field.py
@@ -8,7 +8,23 @@ def execute():
 		return
 
 	fields = [
-		# rearrange transporter name
+		{
+			'fieldname': 'transporter_info',
+ 			'label': 'Transporter Info',
+ 			'fieldtype': 'Section Break',
+ 			'insert_after': 'terms',
+ 			'collapsible': 1,
+ 			'collapsible_depends_on': 'transporter',
+ 			'print_hide': 1
+		},
+		{
+			'fieldname': 'transporter',
+			'label': 'Transporter',
+			'fieldtype': 'Link',
+			'insert_after': 'transporter_info',
+			'options': 'Supplier',
+			'print_hide': 1
+		},
 		{
 			'fieldname': 'transporter_name',
 			'label': 'Transporter Name',
@@ -29,6 +45,42 @@ def execute():
 			'translatable': 0
 		},
 		{
+			'fieldname': 'driver',
+			'label': 'Driver',
+			'fieldtype': 'Link',
+			'insert_after': 'gst_transporter_id',
+			'options': 'Driver',
+			'print_hide': 1
+		},
+		{
+			'fieldname': 'lr_no',
+			'label': 'Transport Receipt No',
+			'fieldtype': 'Data',
+			'insert_after': 'driver',
+			'print_hide': 1,
+			'translatable': 0
+		},
+		{
+			'fieldname': 'vehicle_no',
+			'label': 'Vehicle No',
+			'fieldtype': 'Data',
+			'insert_after': 'lr_no',
+			'print_hide': 1,
+			'translatable': 0
+		},
+		{
+			'fieldname': 'distance',
+			'label': 'Distance (in km)',
+			'fieldtype': 'Float',
+			'insert_after': 'vehicle_no',
+			'print_hide': 1
+		},
+		{
+			'fieldname': 'transporter_col_break',
+			'fieldtype': 'Column Break',
+			'insert_after': 'distance'
+		},
+		{
 			'fieldname': 'transporter_address',
 			'label': 'Transporter Address Name',
 			'fieldtype': 'Link',
@@ -38,7 +90,7 @@ def execute():
 		},
 		{
 			'fieldname': 'transporter_address_display',
-			'label': 'Transporter Address',
+			'label': 'Transporter Address Preview',
 			'fieldtype': 'Small Text',
 			'insert_after': 'transporter_address',
 			'read_only': 1,
@@ -55,6 +107,43 @@ def execute():
 			'print_hide': 1,
 			'translatable': 0
 		},
+		{
+			'fieldname': 'driver_name',
+			'label': 'Driver Name',
+			'fieldtype': 'Data',
+			'insert_after': 'mode_of_transport',
+			'fetch_from': 'driver.full_name',
+			'print_hide': 1,
+			'translatable': 0
+		},
+		{
+			'fieldname': 'lr_date',
+			'label': 'Transport Receipt Date',
+			'fieldtype': 'Date',
+			'insert_after': 'driver_name',
+			'default': 'Today',
+			'print_hide': 1
+		},
+		{
+			'fieldname': 'gst_vehicle_type',
+			'label': 'GST Vehicle Type',
+			'fieldtype': 'Select',
+			'options': 'Regular\nOver Dimensional Cargo (ODC)',
+			'depends_on': 'eval:(doc.mode_of_transport === "Road")',
+			'default': 'Regular',
+			'insert_after': 'lr_date',
+			'print_hide': 1,
+			'translatable': 0
+		},
+		{
+			'fieldname': 'ewaybill',
+			'label': 'e-Way Bill No.',
+			'fieldtype': 'Data',
+			'depends_on': 'eval:(doc.docstatus === 1)',
+			'allow_on_submit': 1,
+			'insert_after': 'tax_id',
+			'translatable': 0
+		}
 	]
 
 	create_custom_fields({ 'Sales Invoice': fields }, update=True)
