0d63cc00315f3e24ecd4d0c7981f62fa261bb25f
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-10-26 20:18:33 +0530

    fix: sider

--- erpnext/education/report/student_batch_wise_attendance/student_batch_wise_attendance.py
+++ erpnext/education/report/student_batch_wise_attendance/student_batch_wise_attendance.py
@@ -3,7 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import getdate, formatdate
+from frappe.utils import formatdate
 from frappe import msgprint, _
 from erpnext.education.doctype.student_attendance.student_attendance import get_holiday_list
 from erpnext.hr.doctype.holiday_list.holiday_list import is_holiday
