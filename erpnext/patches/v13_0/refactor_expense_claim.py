6d8b69ce4603e9add4c4de87a0175c2b0d3ca5fb
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-10-29 14:04:10 +0530

    fix: patch for replacing expense claim type with items

---
+++ erpnext/patches/v13_0/refactor_expense_claim.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2020, Frappe and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+
+import frappe
+from frappe.model.utils.rename_field import rename_field
+
+def execute():
+	if frappe.db.has_table('Expense Claim Type') and frappe.db.has_table('Expense Claim Account'):
+		# make items from Expense Claim Type
+		expense_types = frappe.get_all('Expense Claim Type',
+			fields = ['name', 'deferred_expense_account', 'expense_type', 'description']
+		)
+
+		for entry in expense_types:
+			doc = frappe.get_doc('Expense Claim Type', entry.name)
+
+			item = frappe.new_doc('Item')
+			item.item_code = doc.expense_type
+			item.item_group = 'Services'
+			item.is_stock_item = 0
+			item.decription = doc.description
+			item.include_item_in_manufacturing = 0
+			item.stock_uom = 'Nos'
+			item.enable_deferred_expense = doc.deferred_expense_account
+
+			for acc in doc.accounts:
+				item.append('item_defaults', {
+					'company': acc.company,
+					'expense_account': acc.default_account
+				})
+
+			item.save()
+
+		rename_field('Expense Claim Detail', 'expense_type', 'expense_item')
+
+		frappe.reload_doc('hr', 'doctype', 'expense_claim_detail')
+		frappe.reload_doc('hr', 'doctype', 'expense_claim')
+
+		frappe.db.sql("""DELETE from `tabExpense Claim Account`""")
+		frappe.db.sql("""DELETE from `tabExpense Claim Type`""")
+		frappe.delete_doc_if_exists('DocType', 'Expense Claim Account')
+		frappe.delete_doc_if_exists('DocType', 'Expense Claim Type')
