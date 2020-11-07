9c88c37102c6b28c3a2477b185df4c48379cc1c8
Deepesh Garg <deepeshgarg6@gmail.com>
2020-11-07 17:08:30 +0530

    fix: Loan write off precision issue

--- erpnext/loan_management/doctype/loan_write_off/loan_write_off.js
+++ erpnext/loan_management/doctype/loan_write_off/loan_write_off.js
@@ -4,6 +4,12 @@
 {% include 'erpnext/loan_management/loan_common.js' %};
 
 frappe.ui.form.on('Loan Write Off', {
+	loan: function(frm) {
+		frm.trigger('show_pending_principal_amount');
+	},
+	onload: function(frm) {
+		frm.trigger('show_pending_principal_amount');
+	},
 	refresh: function(frm) {
 		frm.set_query('write_off_account', function(){
 			return {
@@ -14,5 +20,17 @@ frappe.ui.form.on('Loan Write Off', {
 				}
 			}
 		});
+	},
+	show_pending_principal_amount: function(frm) {
+		if (frm.doc.loan && frm.doc.docstatus === 0) {
+			frappe.db.get_value('Loan', frm.doc.loan, ['total_payment', 'total_interest_payable',
+				'total_principal_paid', 'written_off_amount'], function(values) {
+				frm.set_df_property('write_off_amount', 'description',
+					"Pending principal amount is " + cstr(flt(values.total_payment - values.total_interest_payable
+						- values.total_principal_paid - values.written_off_amount, 2)));
+				frm.refresh_field('write_off_amount');
+			});
+
+		}
 	}
 });
