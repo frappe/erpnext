20e1781ea8a1668ca45f6a85a9dac0fd4478b9d7
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-11-10 13:54:18 +0530

    fix(patch): rename enable to enabled in SLA

--- erpnext/patches/v13_0/add_doctype_to_sla.py
+++ erpnext/patches/v13_0/add_doctype_to_sla.py
@@ -4,9 +4,12 @@
 from __future__ import unicode_literals
 
 import frappe
+from frappe.model.utils.rename_field import rename_field
 
 def execute():
 	frappe.reload_doc('support', 'doctype', 'service_level_agreement')
+	if frappe.db.has_column('Service Level Agreement', 'enable'):
+		rename_field('Service Level Agreement', 'enable', 'enabled')
 
 	for sla in frappe.get_all('Service Level Agreement'):
 		frappe.db.set_value('Service Level Agreement', sla.name, 'document_type', 'Issue')
