7b16c55c9a387ac5d688a0ee98efd7e93cf12c66
Deepesh Garg <42651287+deepeshgarg007@users.noreply.github.com>
2020-11-13 16:53:35 +0530

    feat: Sync old shopify orders (#23841)
    * feat: Sync old shopify orders
    
    * fix: Old orders syncing by date
    
    * fix: Remove unnecessary code
    
    * fix: Remove unintentional changes
    
    Co-authored-by: Saurabh <saurabh@erpnext.com>

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -308,6 +308,7 @@ scheduler_events = {
 		"erpnext.projects.doctype.project.project.collect_project_status",
 		"erpnext.hr.doctype.shift_type.shift_type.process_auto_attendance_for_all_shifts",
 		"erpnext.support.doctype.issue.issue.set_service_level_agreement_variance",
+		"erpnext.erpnext_integrations.connectors.shopify_connection.sync_old_orders",
 	],
 	"daily": [
 		"erpnext.stock.reorder_item.reorder_item",
