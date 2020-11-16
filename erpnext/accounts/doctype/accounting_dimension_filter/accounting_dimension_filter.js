e895ddc9521f9355f72c27ceb518a4998f16b573
Deepesh Garg <deepeshgarg6@gmail.com>
2020-11-16 20:32:16 +0530

    fix: Add disable and mandatory check for accounting dimension filters

--- erpnext/accounts/doctype/accounting_dimension_filter/accounting_dimension_filter.js
+++ erpnext/accounts/doctype/accounting_dimension_filter/accounting_dimension_filter.js
@@ -3,15 +3,47 @@
 
 frappe.ui.form.on('Accounting Dimension Filter', {
 	onload: function(frm) {
+		frm.set_query('applicable_on_account', 'accounts', function() {
+			return {
+				filters : {
+					'company': frm.doc.company
+				}
+			}
+		});
+
 		frappe.db.get_list('Accounting Dimension',
-			{fields: ['name']}).then((res) => {
+			{fields: ['document_type']}).then((res) => {
 			let options = ['Cost Center', 'Project'];
 
 			res.forEach((dimension) => {
-				options.push(dimension.name);
+				options.push(dimension.document_type);
 			});
 
 			frm.set_df_property('accounting_dimension', 'options', options);
+		});
+
+		frm.trigger('setup_filters');
+	},
+
+	setup_filters: function(frm) {
+		let filters = {};
+
+		frappe.model.with_doctype(frm.doc.accounting_dimension, function() {
+			if (frm.doc.accounting_dimension) {
+				if (frappe.model.is_tree(frm.doc.accounting_dimension)) {
+					filters['is_group'] = 0;
+				}
+
+				if (frappe.meta.has_field(frm.doc.accounting_dimension, 'company')) {
+					filters['company'] = frm.doc.company;
+				}
+
+				frm.set_query('dimension_value', 'dimensions', function() {
+					return {
+						filters: filters
+					}
+				});
+			}
 		});
 	},
 
@@ -20,6 +52,7 @@ frappe.ui.form.on('Accounting Dimension Filter', {
 		let row = frm.add_child("dimensions");
 		row.accounting_dimension = frm.doc.accounting_dimension;
 		frm.refresh_field("dimensions");
+		frm.trigger('setup_filters');
 	},
 });
 
