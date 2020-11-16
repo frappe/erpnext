a5bd3dd3b218ce39797ac37ad54e13a6f5d7be89
Deepesh Garg <deepeshgarg6@gmail.com>
2020-11-16 09:55:35 +0530

    fix: Remove project filter

--- erpnext/accounts/doctype/budget/budget.js
+++ erpnext/accounts/doctype/budget/budget.js
@@ -3,14 +3,6 @@
 
 frappe.ui.form.on('Budget', {
 	onload: function(frm) {
-		frm.set_query("project", function() {
-			return {
-				filters: {
-					company: frm.doc.company
-				}
-			}
-		})
-
 		frm.set_query("account", "accounts", function() {
 			return {
 				filters: {
