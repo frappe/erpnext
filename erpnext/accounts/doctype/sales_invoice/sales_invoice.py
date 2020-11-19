c630be6781f571ebda3f29cdd58759f75f73bcf4
Smit Vora <mailsmitvora@gmail.com>
2020-11-19 19:13:06 +0530

    fix: remove unused imports

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -4,7 +4,7 @@
 from __future__ import unicode_literals
 import frappe, erpnext
 import frappe.defaults
-from frappe.utils import cint, flt, add_months, today, date_diff, getdate, add_days, cstr, nowdate, formatdate
+from frappe.utils import cint, flt, getdate, add_days, cstr, nowdate, formatdate
 from frappe import _, msgprint, throw
 from erpnext.accounts.party import get_party_account, get_due_date
 from erpnext.controllers.stock_controller import update_gl_entries_after
