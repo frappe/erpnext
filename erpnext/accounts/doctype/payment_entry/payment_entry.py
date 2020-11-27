b7440abafee980465ab5106e6a9c6bc86e7782f5
Afshan <afshan13k@gmail.com>
2020-11-27 13:23:46 +0530

    fix: travis

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -909,15 +909,55 @@ def get_reference_details(reference_doctype, reference_name, party_account_curre
 	ref_doc = frappe.get_doc(reference_doctype, reference_name)
 	company_currency = ref_doc.get("company_currency") or erpnext.get_company_currency(ref_doc.company)
 
-	if reference_doctype in ['Fees', 'Dunning', 'Journal Entry']:
-		total_amount, outstanding_amount, exchange_rate = get_amounts_based_on_reference_doctype(
-			reference_doctype, ref_doc, party_account_currency, company_currency, reference_name)
-
-	elif (reference_doctype != "Journal Entry"):
-		total_amount, outstanding_amount, exchange_rate, bill_no = get_amounts_based_on_ref_doc(
-			reference_doctype, ref_doc, party_account_currency, company_currency)
-
-	if not exchange_rate:
+	if reference_doctype == "Fees":
+		total_amount = ref_doc.get("grand_total")
+		exchange_rate = 1
+		outstanding_amount = ref_doc.get("outstanding_amount")
+	elif reference_doctype == "Dunning":
+		total_amount = ref_doc.get("dunning_amount")
+		exchange_rate = 1
+		outstanding_amount = ref_doc.get("dunning_amount")
+	elif reference_doctype == "Journal Entry" and ref_doc.docstatus == 1:
+		total_amount = ref_doc.get("total_amount")
+		if ref_doc.multi_currency:
+			exchange_rate = get_exchange_rate(party_account_currency, company_currency, ref_doc.posting_date)
+		else:
+			exchange_rate = 1
+			outstanding_amount = get_outstanding_on_journal_entry(reference_name)
+	elif reference_doctype != "Journal Entry":
+		if ref_doc.doctype == "Expense Claim":
+				total_amount = flt(ref_doc.total_sanctioned_amount) + flt(ref_doc.total_taxes_and_charges)
+		elif ref_doc.doctype == "Employee Advance":
+			total_amount = ref_doc.advance_amount
+			exchange_rate = ref_doc.get("exchange_rate")
+			if party_account_currency != ref_doc.currency:
+				total_amount = flt(total_amount) * flt(exchange_rate)
+		if not total_amount:
+			if party_account_currency == company_currency:
+				total_amount = ref_doc.base_grand_total
+				exchange_rate = 1
+			else:
+				total_amount = ref_doc.grand_total
+		if not exchange_rate:
+			# Get the exchange rate from the original ref doc
+			# or get it based on the posting date of the ref doc
+			exchange_rate = ref_doc.get("conversion_rate") or \
+				get_exchange_rate(party_account_currency, company_currency, ref_doc.posting_date)
+		if reference_doctype in ("Sales Invoice", "Purchase Invoice"):
+			outstanding_amount = ref_doc.get("outstanding_amount")
+			bill_no = ref_doc.get("bill_no")
+		elif reference_doctype == "Expense Claim":
+			outstanding_amount = flt(ref_doc.get("total_sanctioned_amount")) + flt(ref_doc.get("total_taxes_and_charges"))\
+				- flt(ref_doc.get("total_amount_reimbursed")) - flt(ref_doc.get("total_advance_amount"))
+		elif reference_doctype == "Employee Advance":
+			outstanding_amount = (flt(ref_doc.advance_amount) - flt(ref_doc.paid_amount))
+			if party_account_currency != ref_doc.currency:
+				outstanding_amount = flt(outstanding_amount) * flt(exchange_rate)
+				if party_account_currency == company_currency:
+					exchange_rate = 1
+		else:
+			outstanding_amount = flt(total_amount) - flt(ref_doc.advance_paid)
+	else:
 		# Get the exchange rate based on the posting date of the ref doc
 		exchange_rate = get_exchange_rate(party_account_currency,
 			company_currency, ref_doc.posting_date)
