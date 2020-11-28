0127fbad6407f06b22e68c223ca5ae94f6a00eeb
Afshan <afshan13k@gmail.com>
2020-11-28 13:35:03 +0530

    fix: re-run travis

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -940,7 +940,7 @@ def get_reference_details(reference_doctype, reference_name, party_account_curre
 				total_amount = ref_doc.grand_total
 		if not exchange_rate:
 			# Get the exchange rate from the original ref doc
-			# or get it based on the posting date of the ref doc
+			# or get it based on the posting date of the ref doc.
 			exchange_rate = ref_doc.get("conversion_rate") or \
 				get_exchange_rate(party_account_currency, company_currency, ref_doc.posting_date)
 		if reference_doctype in ("Sales Invoice", "Purchase Invoice"):
@@ -958,7 +958,7 @@ def get_reference_details(reference_doctype, reference_name, party_account_curre
 		else:
 			outstanding_amount = flt(total_amount) - flt(ref_doc.advance_paid)
 	else:
-		# Get the exchange rate based on the posting date of the ref doc
+		# Get the exchange rate based on the posting date of the ref doc.
 		exchange_rate = get_exchange_rate(party_account_currency,
 			company_currency, ref_doc.posting_date)
 
