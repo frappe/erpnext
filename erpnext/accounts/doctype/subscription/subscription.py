d8b4044ee175bd63b1f340d0eed8e8615130ee5c
Deepesh Garg <deepeshgarg6@gmail.com>
2020-11-29 22:48:05 +0530

    fix: Add check to generate new invoices

--- erpnext/accounts/doctype/subscription/subscription.py
+++ erpnext/accounts/doctype/subscription/subscription.py
@@ -412,7 +412,7 @@ class Subscription(Document):
 				self.update_subscription_period(add_days(self.current_invoice_end, 1))
 
 			# Generate invoices periodically even if current invoice are unpaid
-			if not self.is_current_invoice_generated() and (self.is_postpaid_to_invoice()
+			if self.generate_new_invoices_past_due_date and not self.is_current_invoice_generated() and (self.is_postpaid_to_invoice()
 				or self.is_prepaid_to_invoice()):
 				prorate = frappe.db.get_single_value('Subscription Settings', 'prorate')
 				self.generate_invoice(prorate)
