e4a965235ad9c98b6fe93ccd581992b039157c6a
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-11-29 22:38:14 +0530

    feat: Doctype filter for Patient History

--- erpnext/healthcare/page/patient_history/patient_history.py
+++ erpnext/healthcare/page/patient_history/patient_history.py
@@ -4,36 +4,53 @@
 
 from __future__ import unicode_literals
 import frappe
+import json
 from frappe.utils import cint
 from erpnext.healthcare.utils import render_docs_as_html
 
 @frappe.whitelist()
-def get_feed(name, start=0, page_length=20):
+def get_feed(name, document_types=None, start=0, page_length=20):
 	"""get feed"""
-	result = frappe.db.sql("""select name, owner, creation,
-		reference_doctype, reference_name, subject
-		from `tabPatient Medical Record`
-		where patient=%(patient)s
-		order by creation desc
-		limit %(start)s, %(page_length)s""",
-		{
-			"patient": name,
-			"start": cint(start),
-			"page_length": cint(page_length)
-		}, as_dict=True)
+	filters = {'patient': name}
+	if document_types:
+		document_types = json.loads(document_types)
+		filters['reference_doctype'] = ['IN', document_types]
+
+	result = frappe.db.get_all('Patient Medical Record',
+		fields=['name', 'owner', 'creation',
+			'reference_doctype', 'reference_name', 'subject'],
+		filters=filters,
+		order_by='creation DESC',
+		limit=cint(page_length),
+		start=cint(start)
+	)
+
 	return result
 
 @frappe.whitelist()
 def get_feed_for_dt(doctype, docname):
 	"""get feed"""
-	result = frappe.db.sql("""select name, owner, modified, creation,
-			reference_doctype, reference_name, subject
-		from `tabPatient Medical Record`
-		where reference_name=%(docname)s and reference_doctype=%(doctype)s
-		order by creation desc""",
-		{
-			"docname": docname,
-			"doctype": doctype
-		}, as_dict=True)
+	result = frappe.db.get_all('Patient Medical Record',
+		fields=['name', 'owner', 'creation',
+			'reference_doctype', 'reference_name', 'subject'],
+		filters={
+			'reference_doctype': doctype,
+			'reference_name': docname
+		},
+		order_by='creation DESC'
+	)
 
 	return result
+
+@frappe.whitelist()
+def get_patient_history_doctypes():
+	document_types = []
+	settings = frappe.get_single("Patient History Settings")
+
+	for entry in settings.standard_doctypes:
+		document_types.append(entry.document_type)
+
+	for entry in settings.custom_doctypes:
+		document_types.append(entry.document_type)
+
+	return document_types
