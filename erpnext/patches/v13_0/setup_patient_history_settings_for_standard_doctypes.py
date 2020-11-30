f6756838ba63075e16ca5c4b3601d2eebe143d33
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-11-30 17:51:56 +0530

    fix: patch

--- erpnext/patches/v13_0/setup_patient_history_settings_for_standard_doctypes.py
+++ erpnext/patches/v13_0/setup_patient_history_settings_for_standard_doctypes.py
@@ -3,7 +3,11 @@ import frappe
 from erpnext.healthcare.setup import setup_patient_history_settings
 
 def execute():
-	if 'Healthcare' not in frappe.get_active_domains():
+	if "Healthcare" not in frappe.get_active_domains():
 		return
+
+	frappe.reload_doc("healthcare", "doctype", "Patient History Settings")
+	frappe.reload_doc("healthcare", "doctype", "Patient History Standard Document Type")
+	frappe.reload_doc("healthcare", "doctype", "Patient History Custom Document Type")
 
 	setup_patient_history_settings()
