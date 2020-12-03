ad398221c81fe39be0ccc0688a1b366f22ca0532
Saif Ur Rehman <saifi0102@gmail.com>
2020-12-04 00:58:23 +0500

    chore(Stock Ledger Report): Group by Item-Warehouse by default

--- erpnext/stock/report/stock_ledger/stock_ledger.js
+++ erpnext/stock/report/stock_ledger/stock_ledger.js
@@ -107,7 +107,7 @@ frappe.query_reports["Stock Ledger"] = {
 			"label": __("Group By"),
 			"fieldtype": "Select",
 			"options": "Ungrouped\nGroup by Item-Warehouse\nGroup by Item\nGroup by Warehouse\nGroup by Item Group\nGroup by Brand\nGroup by Party\nGroup by Voucher",
-			"default": "Ungrouped"
+			"default": "Group by Item-Warehouse"
 		}
 	]
 }
