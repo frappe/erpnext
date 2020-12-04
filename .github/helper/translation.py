86b67027dd7e0d1f50e2d9497bca9a9b5ba37a8a
barredterra <14891507+barredterra@users.noreply.github.com>
2020-12-04 17:44:44 +0100

    fix: improve translation pattern

--- .github/helper/translation.py
+++ .github/helper/translation.py
@@ -2,7 +2,7 @@ import re
 import sys
 
 errors_encounter = 0
-pattern = re.compile(r"_\(([\"']{,3})(?P<message>((?!\1).)*)\1(\s*,\s*context\s*=\s*([\"'])(?P<py_context>((?!\5).)*)\5)*(\s*,\s*(.)*?\s*(,\s*([\"'])(?P<js_context>((?!\11).)*)\11)*)*\)")
+pattern = re.compile(r"_{1,2}\(([\"'`]{1,3})(?P<message>((?!\1)[\s\S])*)\1(\s*,\s*context\s*=\s*([\"']{1,3})(?P<py_context>((?!\5)[\s\S])*)\5){0,1}(\s*,\s*([\s\S])*\s*(,\s*([\"'`])(?P<js_context>((?!\11).)*)\11)*){0,1}\)")
 words_pattern = re.compile(r"_{1,2}\([\"'`]{1,3}.*?[a-zA-Z]")
 start_pattern = re.compile(r"_{1,2}\([f\"'`]{1,3}")
 f_string_pattern = re.compile(r"_\(f[\"']")
