e15ef1e19f124436d8705d633a02388a1107056f
pateljannat <jannatpatel@MacBook-Air.local>
2020-12-10 20:55:25 +0530

    fix: corrected tests

--- erpnext/projects/doctype/project_template/test_project_template.py
+++ erpnext/projects/doctype/project_template/test_project_template.py
@@ -17,9 +17,7 @@ def get_project_template(project_template_name="Test Project Template", project_
 			name = project_template_name,
 			tasks = project_tasks or [
 				create_task(subject="_Test Template Task 1", is_template=1, begin=0, duration=3),
-				create_task(subject="_Test Template Task 2", is_template=1, begin=0, duration=2),
-				create_task(subject="_Test Template Task 3", is_template=1, begin=2, duration=4),
-				create_task(subject="_Test Template Task 4", is_template=1, begin=3, duration=2),
+				create_task(subject="_Test Template Task 2", is_template=1, begin=0, duration=2)
 			]
 		)).insert()
 
@@ -27,13 +25,18 @@ def get_project_template(project_template_name="Test Project Template", project_
 
 def make_project_template(project_template_name, project_tasks=[]):
 	if not frappe.db.exists('Project Template', project_template_name):
-		frappe.get_doc(dict(
-			doctype = 'Project Template',
-			name = project_template_name,
-			tasks = project_tasks or [
+		project_tasks = project_tasks or [
 				create_task(subject="_Test Template Task 1", is_template=1, begin=0, duration=3),
 				create_task(subject="_Test Template Task 2", is_template=1, begin=0, duration=2),
 			]
-		)).insert()
+		doc = frappe.get_doc(dict(
+			doctype = 'Project Template',
+			name = project_template_name
+		))
+		for task in project_tasks:
+			doc.append("tasks",{
+				"task": task.name
+			})
+		doc.insert()
 
 	return frappe.get_doc('Project Template', project_template_name)
