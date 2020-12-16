bf7413d88c7b67010cec68a56726aa0cba032f30
Anurag Mishra <mishranaman123@gmail.com>
2020-12-16 15:12:22 +0530

    feat: roles for quality management

---
+++ erpnext/quality_management/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+install_docs = [
+	{'doctype':'Role', 'role_name':'Quality Manager', 'name':'Quality Manager'},
+	{'doctype':'Role', 'role_name':'Quality User', 'name':'Quality User'},
+]
