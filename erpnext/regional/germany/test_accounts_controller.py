df8ea194064d5d7abcdf1a696324af55d679baa3
barredterra <14891507+barredterra@users.noreply.github.com>
2020-12-22 17:34:31 +0100

    fix: whitespace

--- erpnext/regional/germany/test_accounts_controller.py
+++ erpnext/regional/germany/test_accounts_controller.py
@@ -7,6 +7,6 @@ class TestAccountsController(unittest.TestCase):
 
 	def setUp(self):
 		self.sales_invoice = frappe.get_last_doc('Sales Invoice')
-	
+
 	def test_validate_regional(self):
 		validate_regional(self.sales_invoice)
