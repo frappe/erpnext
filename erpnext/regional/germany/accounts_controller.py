2acd8cbc02aca5904e35ece8dfb4b1608e23891e
barredterra <14891507+barredterra@users.noreply.github.com>
2020-12-22 17:34:22 +0100

    fix: sider

--- erpnext/regional/germany/accounts_controller.py
+++ erpnext/regional/germany/accounts_controller.py
@@ -48,6 +48,6 @@ def validate_regional(doc):
 
 def missing(field_label, regulation):
 	"""Notify the user that a required field is missing."""
-	translated_msg = _('Remember to set {field_label}. It is required by {regulation}.', context='Specific for Germany. Example: Remember to set Company Tax ID. It is required by § 14 Abs. 4 Nr. 2 UStG.')
+	translated_msg = _('Remember to set {field_label}. It is required by {regulation}.', context='Specific for Germany. Example: Remember to set Company Tax ID. It is required by § 14 Abs. 4 Nr. 2 UStG.') # noqa: E501
 	formatted_msg = translated_msg.format(field_label=frappe.bold(_(field_label)), regulation=regulation)
 	msgprint(formatted_msg)
