2b411b8edab4c599ce93053e212c165dae2f42bd
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-01-04 14:28:31 +0530

    fix: condition

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -74,8 +74,8 @@ def get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=Tru
 
 	update_party_blanket_order(args, out)
 
-	if doc.get('doctype') not in ('Sales Invoice', 'Purchase Invoice') and doc.get('is_return') == 0:
-		# get price list rate only if the invoice is not a credit note and debit note
+	if not doc or (doc.get('doctype') not in ('Sales Invoice', 'Purchase Invoice') and doc.get('is_return') == 0):
+		# get price list rate only if the invoice is not a credit or debit note
 		get_price_list_rate(args, item, out)
 
 	if args.customer and cint(args.is_pos):
