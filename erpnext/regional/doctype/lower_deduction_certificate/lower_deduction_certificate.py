3c55463b4b86e9715e194d8c8268ee6190a5d30c
pateljannat <pateljannat2308@gmail.com>
2021-01-07 12:41:44 +0530

    fix: validation for ldc against supplier and section code

--- erpnext/regional/doctype/lower_deduction_certificate/lower_deduction_certificate.py
+++ erpnext/regional/doctype/lower_deduction_certificate/lower_deduction_certificate.py
@@ -5,12 +5,16 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-from frappe.utils import getdate
+from frappe.utils import getdate, get_link_to_form
 from frappe.model.document import Document
 from erpnext.accounts.utils import get_fiscal_year
 
 class LowerDeductionCertificate(Document):
 	def validate(self):
+		self.validate_dates()
+		self.validate_supplier_against_section_code()
+		
+	def validate_dates(self):
 		if getdate(self.valid_upto) < getdate(self.valid_from):
 			frappe.throw(_("Valid Upto date cannot be before Valid From date"))
 
@@ -23,4 +27,11 @@ class LowerDeductionCertificate(Document):
 		if not (fiscal_year.year_start_date <= getdate(self.valid_upto) \
 			<= fiscal_year.year_end_date):
 			frappe.throw(_("Valid Upto date not in Fiscal Year {0}").format(frappe.bold(self.fiscal_year)))
+
+	def validate_supplier_against_section_code(self):
+		duplicate_certificate = frappe.db.get_value('Lower Deduction Certificate', {'supplier': self.supplier, 'section_code': self.section_code}, ['name'])
+		if duplicate_certificate:
+			certificate_link = get_link_to_form('Lower Deduction Certificate', duplicate_certificate)
+			frappe.throw(_("There is already a Lower Deduction Certificate {0} for Supplier {1} against Section Code {2}")
+				.format(certificate_link, frappe.bold(self.supplier), frappe.bold(self.section_code)))
 
