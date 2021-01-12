37aa81526a613232d4c63439064003dc626cc371
Rushabh Mehta <rmehta@gmail.com>
2021-01-12 10:29:00 +0530

    fix(minor): clear merge conflicts

--- erpnext/selling/page/point_of_sale/pos_item_selector.js
+++ erpnext/selling/page/point_of_sale/pos_item_selector.js
@@ -260,11 +260,7 @@ erpnext.PointOfSale.ItemSelector = class {
 	add_filtered_item_to_cart() {
 		this.$items_container.find(".item-wrapper").click();
 	}
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> develop
 	resize_selector(minimize) {
 		minimize ?
 		this.$component.find('.filter-section').css('grid-template-columns', 'repeat(1, minmax(0, 1fr))') :
