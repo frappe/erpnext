10c069588fe3eb76167c7d8fa693ca0521a01633
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-01-14 00:40:58 +0530

    feat(Payroll): compute Year to Date for Salary Slip components

--- erpnext/payroll/doctype/salary_slip/salary_slip.js
+++ erpnext/payroll/doctype/salary_slip/salary_slip.js
@@ -138,11 +138,11 @@ frappe.ui.form.on("Salary Slip", {
 	},
 
 	change_grid_labels: function(frm) {
-		frm.set_currency_labels(["amount", "default_amount", "additional_amount", "tax_on_flexible_benefit",
-			"tax_on_additional_salary"], frm.doc.currency, "earnings");
+		let fields = ["amount", "year_to_date", "default_amount", "additional_amount", "tax_on_flexible_benefit",
+		"tax_on_additional_salary"];
 
-		frm.set_currency_labels(["amount", "default_amount", "additional_amount", "tax_on_flexible_benefit",
-			"tax_on_additional_salary"], frm.doc.currency, "deductions");
+		frm.set_currency_labels(fields, frm.doc.currency, "earnings");
+		frm.set_currency_labels(fields, frm.doc.currency, "deductions");
 	},
 
 	refresh: function(frm) {
