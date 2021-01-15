0868bf5d4504712208075f0e8fce028dbc1eeeaf
Deepesh Garg <deepeshgarg6@gmail.com>
2021-01-15 10:05:37 +0530

    Revert "fix: Set batch values"
    This reverts commit 0bbb55d6ccab42d86acbe4d6958c27ee61784486.

--- erpnext/public/js/utils/serial_no_batch_selector.js
+++ erpnext/public/js/utils/serial_no_batch_selector.js
@@ -269,10 +269,9 @@ erpnext.SerialNoBatchSelector = Class.extend({
 							'qty', this.values.warehouse);
 					} else if (!this.batch_exists(batch_no)) {
 						row = this.frm.add_child("items", { ...this.item });
-						frappe.model.set_value(row.doctype, row.name, 'batch_no', batch_no);
+						row.batch_no = batch_no;
 					} else {
 						row = this.frm.doc.items.find(i => i.batch_no === batch_no);
-						frappe.model.set_value(row.doctype, row.name, 'batch_no', batch_no);
 					}
 					const values = {
 						'qty': serial_no.length,
