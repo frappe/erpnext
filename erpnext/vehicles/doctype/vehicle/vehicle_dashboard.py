69e26197886999c361a46506f7d38c52c8e7c2bf
Saif Ur Rehman <saifi0102@gmail.com>
2021-01-25 12:31:49 +0500

    feat: Project in Vehicle Dashboard

--- erpnext/vehicles/doctype/vehicle/vehicle_dashboard.py
+++ erpnext/vehicles/doctype/vehicle/vehicle_dashboard.py
@@ -1,8 +1,16 @@
 from __future__ import unicode_literals
+import frappe
 
 def get_data():
+	project_link = []
+	if 'Vehicles' in frappe.get_active_domains():
+		project_link.append('Project')
+
 	return {
 		'fieldname': 'vehicle',
+		'non_standard_fieldnames': {
+			'Project': 'applies_to_vehicle',
+		},
 		'transactions': [
 			{
 				'label': ['Sales'],
@@ -14,11 +22,11 @@ def get_data():
 			},
 			{
 				'label': ['Movement'],
-				'items': ['Stock Entry']
+				'items': ['Stock Entry', 'Vehicle Log']
 			},
 			{
 				'label': ['Reference'],
-				'items': ['Vehicle Booking Order', 'Vehicle Log']
+				'items': ['Vehicle Booking Order'] + project_link
 			},
 		]
 	}
