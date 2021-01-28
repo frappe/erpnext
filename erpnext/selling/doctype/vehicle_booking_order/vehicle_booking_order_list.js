1f77480e7dfc3a5b1a2724b507d031a5437a0a22
Saif Ur Rehman <saifi0102@gmail.com>
2021-01-28 19:22:57 +0500

    fix(Vehicle Booking): Better color coding

--- erpnext/selling/doctype/vehicle_booking_order/vehicle_booking_order_list.js
+++ erpnext/selling/doctype/vehicle_booking_order/vehicle_booking_order_list.js
@@ -3,9 +3,11 @@ frappe.listview_settings['Vehicle Booking Order'] = {
 	get_indicator: function(doc) {
 		if(doc.status === "Completed") {
 			return [__("Completed"), "green", "status,=,Completed"];
-		} else if(["To Receive Payment", "To Deposit Payment", "To Receive Vehicle",
-				"To Receive Invoice", "To Deliver Vehicle", "To Deliver Invoice",
-				"To Assign Allocation", "To Assign Vehicle"].includes(doc.status)) {
+		} else if(["To Assign Allocation", "To Assign Vehicle"].includes(doc.status)) {
+			return [__(doc.status), "purple", `status,=,${doc.status}`];
+		} else if (["To Receive Payment", "To Receive Vehicle", "To Receive Invoice"].includes(doc.status)) {
+			return [__(doc.status), "yellow", `status,=,${doc.status}`];
+		} else if(["To Deposit Payment", "To Deliver Vehicle", "To Deliver Invoice"].includes(doc.status)) {
 			return [__(doc.status), "orange", `status,=,${doc.status}`];
 		} else if(["Overdue Payment", "Overdue Delivery"].includes(doc.status)) {
 			return [__(doc.status), "red", `status,=,${doc.status}`];
