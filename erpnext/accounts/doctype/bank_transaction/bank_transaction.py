4f797f9476f42d1a3f981fd74cf95972b92133af
Richard Case <richard@casesolved.co.uk>
2021-01-28 19:25:49 +0000

    feat: Create Journal Entry from Bank Transaction, requires frappe bugfix

--- erpnext/accounts/doctype/bank_transaction/bank_transaction.py
+++ erpnext/accounts/doctype/bank_transaction/bank_transaction.py
@@ -8,6 +8,7 @@ from erpnext.controllers.status_updater import StatusUpdater
 from frappe.utils import flt
 from six.moves import reduce
 from frappe import _
+from frappe.model.mapper import get_mapped_doc
 
 class BankTransaction(StatusUpdater):
 	def after_insert(self):
@@ -112,3 +113,58 @@ def unclear_reference_payment(doctype, docname):
 			frappe.db.set_value(doc.payment_document, doc.payment_entry, "clearance_date", None)
 
 		return doc.payment_entry
+
+@frappe.whitelist()
+def make_journal_entry(source_name, target_doc=None,
+	opposite_account=None, party={'type': None, 'name': None}, reference={'type': None, 'name': None}):
+
+	doc = get_mapped_doc('Bank Transaction', source_name, {
+		'Bank Transaction': {
+			'doctype': 'Journal Entry',
+			# From Bank Transaction -> Journal Entry
+			'field_map': {
+				"company": "company",
+				"date": "cheque_date",
+				"transaction_id": "cheque_no",
+				"description": "user_remark"
+			},
+			'field_no_map': ['accounts'],
+			'validation': {
+				'docstatus': ['=', 1]
+			}
+		}
+	}, target_doc)
+	doc.voucher_type = 'Bank Entry'
+
+	bank_trans = frappe.get_cached_doc('Bank Transaction', source_name)
+	bank_ac = frappe.get_value('Bank Account', bank_trans.bank_account, 'account')
+
+	credit_row = frappe.new_doc('Journal Entry Account', doc, 'accounts')
+	doc.append('accounts', credit_row)
+	debit_row = frappe.new_doc('Journal Entry Account', doc, 'accounts')
+	doc.append('accounts', debit_row)
+
+	if bank_trans.credit > 0:
+		# They paid us
+		amount = bank_trans.credit
+		opp_row = credit_row
+		bank_row = debit_row
+	else:
+		# We paid them
+		amount = bank_trans.debit
+		opp_row = debit_row
+		bank_row = credit_row
+
+	bank_row.account = bank_ac
+	bank_row.bank_account = bank_trans.bank_account
+
+	opp_row.account = opposite_account
+	opp_row.party_type = party['type']
+	opp_row.party = party['name']
+	opp_row.reference_type = reference['type']
+	opp_row.reference_name = reference['name']
+
+	credit_row.credit_in_account_currency = amount
+	debit_row.debit_in_account_currency = amount
+
+	return doc
