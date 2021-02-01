c85f10efe5a2222a4344eeb377b429ad05b8e1df
Suraj Shetty <surajshetty3416@gmail.com>
2021-02-01 22:40:40 +0530

    fix(patch): Use db_set to ignore validation

--- erpnext/patches/v13_0/set_app_name.py
+++ erpnext/patches/v13_0/set_app_name.py
@@ -2,7 +2,6 @@ import frappe
 from frappe import _
 
 def execute():
-    frappe.reload_doctype("System Settings")
-    settings = frappe.get_doc("System Settings")
-    settings.app_name = _("ERPNext")
-    settings.save()
+	frappe.reload_doctype("System Settings")
+	settings = frappe.get_doc("System Settings")
+	settings.db_set("app_name", "ERPNext", commit=True)
