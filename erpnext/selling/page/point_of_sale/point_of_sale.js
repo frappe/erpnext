a3585e40f7aec22ee3951378a555e5c8775aaf66
prssanna <prssud@gmail.com>
2021-02-01 19:50:27 +0530

    style: fix formatting

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -10,7 +10,7 @@ frappe.pages['point-of-sale'].on_page_load = function(wrapper) {
 	frappe.require('assets/js/point-of-sale.min.js', function() {
 		wrapper.pos = new erpnext.PointOfSale.Controller(wrapper);
 		window.cur_pos = wrapper.pos;
-	})
+	});
 };
 
 frappe.pages['point-of-sale'].refresh = function(wrapper) {
