c8700e3ce024d41ea01eb9a18b13d1225db4e7b9
Anupam <anupamvns0099@gmail.com>
2021-02-08 23:02:03 +0530

    fix: Lead Source's module

--- erpnext/crm/doctype/lead_source/lead_source.py
+++ erpnext/crm/doctype/lead_source/lead_source.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 class LeadSource(Document):
