7a3dbe1b72263228e004267df18754e309d4b680
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-02-08 18:26:58 +0530

    feat: update discounted amount in payment terms

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -333,19 +333,23 @@ class PaymentEntry(AccountsController):
 		invoice_payment_amount_map = {}
 		invoice_paid_amount_map = {}
 
-		for reference in self.get('references'):
-			if reference.payment_term and reference.reference_name:
-				key = (reference.payment_term, reference.reference_name)
+		for ref in self.get('references'):
+			if ref.payment_term and ref.reference_name:
+				key = (ref.payment_term, ref.reference_name)
 				invoice_payment_amount_map.setdefault(key, 0.0)
-				invoice_payment_amount_map[key] += reference.allocated_amount
+				invoice_payment_amount_map[key] += ref.allocated_amount
 
 				if not invoice_paid_amount_map.get(key):
-					payment_schedule = frappe.get_all('Payment Schedule', filters={'parent': reference.reference_name},
-						fields=['paid_amount', 'payment_amount', 'payment_term'])
+					payment_schedule = frappe.get_all(
+						'Payment Schedule',
+						filters={'parent': ref.reference_name},
+						fields=['paid_amount', 'payment_amount', 'payment_term', 'discount_percentage']
+					)
 					for term in payment_schedule:
-						invoice_key = (term.payment_term, reference.reference_name)
+						invoice_key = (term.payment_term, ref.reference_name)
 						invoice_paid_amount_map.setdefault(invoice_key, {})
 						invoice_paid_amount_map[invoice_key]['outstanding'] = term.payment_amount - term.paid_amount
+						invoice_paid_amount_map[invoice_key]['discounted_amt'] = term.payment_amount * (term.discount_percentage / 100)
 
 		for key, amount in iteritems(invoice_payment_amount_map):
 			if cancel:
@@ -353,13 +357,16 @@ class PaymentEntry(AccountsController):
 					WHERE parent = %s and payment_term = %s""", (amount, key[1], key[0]))
 			else:
 				outstanding = flt(invoice_paid_amount_map.get(key, {}).get('outstanding'))
+				discounted_amt = flt(invoice_paid_amount_map.get(key, {}).get('discounted_amt'))
 
 				if amount > outstanding:
 					frappe.throw(_('Cannot allocate more than {0} against payment term {1}').format(outstanding, key[0]))
 
 				if amount and outstanding:
-					frappe.db.sql(""" UPDATE `tabPayment Schedule` SET paid_amount = `paid_amount` + %s
-							WHERE parent = %s and payment_term = %s""", (amount, key[1], key[0]))
+					frappe.db.sql("""
+						UPDATE `tabPayment Schedule`
+						SET paid_amount = `paid_amount` + %s, discounted_amount = `discounted_amount` + %s
+						WHERE parent = %s and payment_term = %s""", (amount, discounted_amt, key[1], key[0]))
 
 	def set_status(self):
 		if self.docstatus == 2:
