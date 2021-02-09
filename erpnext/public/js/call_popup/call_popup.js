1016ff48fcd984c7e2e6be0d2d0d377ef021c4c0
Suraj Shetty <surajshetty3416@gmail.com>
2021-02-09 12:17:29 +0530

    fix: Create new call popup if call popup does not exists

--- erpnext/public/js/call_popup/call_popup.js
+++ erpnext/public/js/call_popup/call_popup.js
@@ -200,10 +200,10 @@ $(document).on('app_ready', function () {
 	frappe.realtime.on('show_call_popup', call_log => {
 		let call_popup = erpnext.call_popup;
 		if (call_popup && call_log.name === call_popup.call_log.name) {
-			erpnext.call_popup = new CallPopup(call_log);
-		} else {
 			call_popup.update_call_log(call_log);
 			call_popup.dialog.show();
+		} else {
+			erpnext.call_popup = new CallPopup(call_log);
 		}
 	});
 });
