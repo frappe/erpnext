7bb8458e8227388826550b5123ddd359bb3b40ea
Afshan <afshan13k@gmail.com>
2021-02-24 16:29:46 +0530

    fix: slider

--- erpnext/projects/doctype/project/project.js
+++ erpnext/projects/doctype/project/project.js
@@ -78,20 +78,20 @@ frappe.ui.form.on("Project", {
 
 
 			if (frappe.model.can_read("Task")) {
-					frm.add_custom_button(__("Gantt Chart"), function () {
-						frappe.route_options = {
-							"project": frm.doc.name
-						};
-						frappe.set_route("List", "Task", "Gantt");
-					});
+				frm.add_custom_button(__("Gantt Chart"), function () {
+					frappe.route_options = {
+						"project": frm.doc.name
+					};
+					frappe.set_route("List", "Task", "Gantt");
+				});
 
-					frm.add_custom_button(__("Kanban Board"), () => {
-						frappe.call('erpnext.projects.doctype.project.project.create_kanban_board_if_not_exists', {
-							project: frm.doc.project_name
-						}).then(() => {
-							frappe.set_route('List', 'Task', 'Kanban', frm.doc.project_name);
-						});
+				frm.add_custom_button(__("Kanban Board"), () => {
+					frappe.call('erpnext.projects.doctype.project.project.create_kanban_board_if_not_exists', {
+						project: frm.doc.project_name
+					}).then(() => {
+						frappe.set_route('List', 'Task', 'Kanban', frm.doc.project_name);
 					});
+				});
 			}
 		}
 
