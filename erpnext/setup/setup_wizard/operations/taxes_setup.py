ebd1d08e55b8146e652096d2c591754f8d676504
barredterra <14891507+barredterra@users.noreply.github.com>
2021-03-08 19:53:50 +0100

    refactor: taxes setup
    Better structure of input data.

--- erpnext/setup/setup_wizard/operations/taxes_setup.py
+++ erpnext/setup/setup_wizard/operations/taxes_setup.py
@@ -17,40 +17,62 @@ def setup_taxes_and_charges(company_name: str, country: str):
 
 	country_wise_tax = tax_data.get(country)
 
-	if country_wise_tax:
-		if 'chart_of_accounts' in country_wise_tax:
-			from_detailed_data(company_name, country_wise_tax.get('chart_of_accounts'))
-		else:
-			from_simple_data(company_name, country_wise_tax)
+	if not country_wise_tax:
+		return
+
+	if 'chart_of_accounts' not in country_wise_tax:
+		country_wise_tax = simple_to_detailed(country_wise_tax)
+
+	from_detailed_data(company_name, country_wise_tax.get('chart_of_accounts'))
 
 
-def from_detailed_data(company_name, data):
+def simple_to_detailed(templates):
 	"""
-	Create Taxes and Charges Templates from detailed data like this:
+	Convert a simple taxes object into a more detailed data structure.
+
+	Example input:
 
 	{
-		"chart_of_accounts": {
-			coa_name: {
-				"sales_tax_templates": [
-					{
-						'title': '',
-						'is_default': 1,
-						'accounts': [
-							{
-								'account_name': '',
-								'account_number': '',
-								'root_type': '',
-							}
-						]
-					}
-				],
-				"purchase_tax_templates": [ ... ],
-				"item_tax_templates": [ ... ],
-				"*": [ ... ]
-			}
+		"France VAT 20%": {
+			"account_name": "VAT 20%",
+			"tax_rate": 20,
+			"default": 1
+		},
+		"France VAT 10%": {
+			"account_name": "VAT 10%",
+			"tax_rate": 10
 		}
 	}
 	"""
+	return {
+		'chart_of_accounts': {
+			'*': {
+				'item_tax_templates': [{
+					'title': title,
+					'taxes': [{
+						'tax_type': {
+							'account_name': data.get('account_name'),
+							'tax_rate': data.get('tax_rate')
+						}
+					}]
+				} for title, data in templates.items()],
+				'*': [{
+					'title': title,
+					'is_default': data.get('default', 0),
+					'taxes': [{
+						'account_head': {
+							'account_name': data.get('account_name'),
+							'tax_rate': data.get('tax_rate')
+						}
+					}]
+				} for title, data in templates.items()]
+			}
+		}
+	}
+
+
+def from_detailed_data(company_name, data):
+	"""Create Taxes and Charges Templates from detailed data."""
 	coa_name = frappe.db.get_value('Company', company_name, 'chart_of_accounts')
 	tax_templates = data.get(coa_name) or data.get('*')
 	sales_tax_templates = tax_templates.get('sales_tax_templates') or tax_templates.get('*')
@@ -59,85 +81,44 @@ def from_detailed_data(company_name, data):
 
 	if sales_tax_templates:
 		for template in sales_tax_templates:
-			make_tax_template(company_name, 'Sales Taxes and Charges Template', template)
+			make_taxes_and_charges_template(company_name, 'Sales Taxes and Charges Template', template)
 
 	if purchase_tax_templates:
 		for template in purchase_tax_templates:
-			make_tax_template(company_name, 'Purchase Taxes and Charges Template', template)
+			make_taxes_and_charges_template(company_name, 'Purchase Taxes and Charges Template', template)
 
 	if item_tax_templates:
 		for template in item_tax_templates:
 			make_item_tax_template(company_name, template)
 
 
-def from_simple_data(company_name, data):
-	"""
-	Create Taxes and Charges Templates from simple data like this:
+def make_taxes_and_charges_template(company_name, doctype, template):
+	template['company'] = company_name
+	template['doctype'] = doctype
 
-	"Austria Tax": {
-		"account_name": "VAT",
-		"tax_rate": 20.00
-	}
-	"""
-	for template_name, tax_data in data.items():
-		template = {
-			'title': template_name,
-			'is_default': tax_data.get('default'),
-			'accounts': [
-				{
-					'account_name': tax_data.get('account_name'),
-					'tax_rate': tax_data.get('tax_rate')
-				}
-			]
-		}
-		make_tax_template(company_name, 'Sales Taxes and Charges Template', template)
-		make_tax_template(company_name, 'Purchase Taxes and Charges Template', template)
-		make_item_tax_template(company_name, template)
-
-
-def make_tax_template(company_name, doctype, template):
 	if frappe.db.exists(doctype, {'title': template.get('title'), 'company': company_name}):
 		return
 
-	accounts = get_or_create_accounts(company_name, template.get('accounts'))
-
-	# Get all fields of the Taxes and Charges Template
-	tax_template = {'doctype': doctype}
-	tax_template_fields = frappe.get_meta(doctype).fields
-	tax_template_fieldnames = [field.fieldname for field in tax_template_fields]
-
-	# Get all fields of the taxes child table
-	table_doctype = [field.options for field in tax_template_fields if field.fieldname=='taxes'][0]
-	table_fields = frappe.get_meta(table_doctype).fields
-	table_field_names = [field.fieldname for field in table_fields]
-
-	# Check if field exists as a key in the import data and, if yes, set the
-	# value accordingly
-	for field in tax_template_fieldnames:
-		if field in template:
-			tax_template[field] = template.get(field)
-
-	# However, company always fixed and taxes table must be empty to start with
-	tax_template['company'] = company_name
-	tax_template['taxes'] = []
-
-	for account in accounts:
-		row = {
+	for tax_row in template.get('taxes'):
+		account_data = tax_row.get('account_head')
+		tax_row_defaults = {
 			'category': 'Total',
-			'charge_type': 'On Net Total',
-			'account_head': account.get('name'),
-			'description': '{0} @ {1}'.format(account.get('account_name'), account.get('tax_rate')),
-			'rate': account.get('tax_rate')
+			'charge_type': 'On Net Total'
 		}
-		# Check if field exists as a key in the import data and, if yes, set the
-		# value accordingly
-		for field in table_field_names:
-			if field in account:
-				row[field] = account.get(field)
 
-		tax_template['taxes'].append(row)
+		# if account_head is a dict, search or create the account and get it's name
+		if isinstance(account_data, dict):
+			tax_row_defaults['description'] = '{0} @ {1}'.format(account_data.get('account_name'), account_data.get('tax_rate'))
+			tax_row_defaults['rate'] = account_data.get('tax_rate')
+			account = get_or_create_account(company_name, account_data)
+			tax_row['account_head'] = account.name
 
-	return frappe.get_doc(tax_template).insert(ignore_permissions=True)
+		# use the default value if nothing other is specified
+		for fieldname, default_value in tax_row_defaults.items():
+			if fieldname not in tax_row:
+				tax_row[fieldname] = default_value
+
+	return frappe.get_doc(template).insert(ignore_permissions=True)
 
 
 def make_item_tax_template(company_name, template):
@@ -147,111 +128,105 @@ def make_item_tax_template(company_name, template):
 	differently from Sales and Purchase Tax Templates.
 	"""
 	doctype = 'Item Tax Template'
+	template['company'] = company_name
+	template['doctype'] = doctype
+
 	if frappe.db.exists(doctype, {'title': template.get('title'), 'company': company_name}):
 		return
 
-	accounts = get_or_create_accounts(company_name, template.get('accounts'))
+	for tax_row in template.get('taxes'):
+		account_data = tax_row.get('tax_type')
 
-	item_tax_template = {
-		'doctype': doctype,
-		'title': template.get('title'),
-		'company': company_name,
-		'taxes': [{
-			'tax_type': account.get('name'),
-			'tax_rate': account.get('tax_rate')
-		} for account in accounts]
-	}
+		# if tax_type is a dict, search or create the account and get it's name
+		if isinstance(account_data, dict):
+			account = get_or_create_account(company_name, account_data)
+			tax_row['tax_type'] = account.name
+			if 'tax_rate' not in tax_row:
+				tax_row['tax_rate'] = account_data.get('tax_rate')
 
-	return frappe.get_doc(item_tax_template).insert(ignore_permissions=True)
+	return frappe.get_doc(template).insert(ignore_permissions=True)
 
 
-def get_or_create_accounts(company: str, account_data: list):
-	for account in account_data:
-		if 'creation' in account:
-			# Hack to check if account already contains a real Account doc
-			# or just the attibutes from country_wise_tax.json
-			continue
-
-		# tax_rate should survive the following lines because it might not be
-		# specified in an existing account or different rates might get booked
-		# onto the same account.
-		tax_rate = account.get('tax_rate')
-		doc = get_or_create_account(company, account)
-		account.update(doc.as_dict())
-		account['tax_rate'] = tax_rate
-
-	return account_data
-
-
-def get_or_create_account(company, account_data):
+def get_or_create_account(company_name, account):
 	"""
 	Check if account already exists. If not, create it.
 	Return a tax account or None.
 	"""
-	root_type = account_data.get('root_type', 'Liability')
-	account_name = account_data.get('account_name')
-	account_number = account_data.get('account_number')
+	default_root_type = 'Liability'
+	root_type = account.get('root_type', default_root_type)
 
 	existing_accounts = frappe.get_list('Account',
 		filters={
-			'company': company,
+			'company': company_name,
 			'root_type': root_type
 		},
 		or_filters={
-			'account_name': account_name,
-			'account_number': account_number
+			'account_name': account.get('account_name'),
+			'account_number': account.get('account_number')
 		}
 	)
 
 	if existing_accounts:
 		return frappe.get_doc('Account', existing_accounts[0].name)
 
-	tax_group = get_or_create_tax_account_group(company, root_type)
-	full_account_data = {
-		'doctype': 'Account',
-		'account_name': account_name,
-		'account_number': account_number,
-		'tax_rate': account_data.get('tax_rate'),
-		'company': company,
-		'parent_account': tax_group,
-		'is_group': 0,
-		'report_type': 'Balance Sheet',
-		'root_type': root_type,
-		'account_type': 'Tax'
-	}
-	return frappe.get_doc(full_account_data).insert(ignore_permissions=True, ignore_mandatory=True)
+	tax_group = get_or_create_tax_group(company_name, root_type)
+
+	account['doctype'] = 'Account'
+	account['company'] = company_name
+	account['parent_account'] = tax_group
+	account['report_type'] = 'Balance Sheet'
+	account['account_type'] = 'Tax'
+	account['root_type'] = root_type
+	account['is_group'] = 0
+
+	return frappe.get_doc(account).insert(ignore_permissions=True, ignore_mandatory=True)
 
 
-def get_or_create_tax_account_group(company, root_type):
-	tax_group = frappe.db.get_value('Account', {
+def get_or_create_tax_group(company_name, root_type):
+	# Look for a group account of type 'Tax'
+	tax_group_name = frappe.db.get_value('Account', {
 		'is_group': 1,
 		'root_type': root_type,
 		'account_type': 'Tax',
-		'company': company
+		'company': company_name
 	})
 
-	if tax_group:
-		return tax_group
+	if tax_group_name:
+		return tax_group_name
 
-	root = frappe.get_list('Account', {
+	# Look for a group account named 'Duties and Taxes' or 'Tax Assets'
+	account_name = _('Duties and Taxes') if root_type == 'Liability' else _('Tax Assets')
+	tax_group_name = frappe.db.get_value('Account', {
 		'is_group': 1,
 		'root_type': root_type,
-		'company': company,
+		'account_name': account_name,
+		'company': company_name
+	})
+
+	if tax_group_name:
+		return tax_group_name
+
+	# Create a new group account named 'Duties and Taxes' or 'Tax Assets' just
+	# below the root account
+	root_account = frappe.get_list('Account', {
+		'is_group': 1,
+		'root_type': root_type,
+		'company': company_name,
 		'report_type': 'Balance Sheet',
 		'parent_account': ('is', 'not set')
-	}, limit=1)[0].name
+	}, limit=1)[0]
 
-	doc = frappe.get_doc({
+	tax_group_account = frappe.get_doc({
 		'doctype': 'Account',
-		'company': company,
+		'company': company_name,
 		'is_group': 1,
 		'report_type': 'Balance Sheet',
 		'root_type': root_type,
 		'account_type': 'Tax',
-		'account_name': _('Duties and Taxes') if root_type == 'Liability' else _('Tax Assets'),
-		'parent_account': root
+		'account_name': account_name,
+		'parent_account': root_account.name
 	}).insert(ignore_permissions=True)
 
-	tax_group = doc.name
+	tax_group_name = tax_group_account.name
 
-	return tax_group
+	return tax_group_name
