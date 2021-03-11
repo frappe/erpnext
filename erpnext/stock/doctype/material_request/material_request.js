539a11b7dd2f96f985bc36ff5b794b9605183862
Furqan Asghar <furqan.790@outlook.com>
2021-03-11 17:49:28 +0500

    fix:  Material Request load Contents while "Get Items From"

--- erpnext/stock/doctype/material_request/material_request.js
+++ erpnext/stock/doctype/material_request/material_request.js
@@ -208,7 +208,7 @@ frappe.ui.form.on('Material Request', {
 						return {filters: { docstatus:1 }};
 					}},
 				{"fieldname":"warehouse", "fieldtype":"Link", "label":__("Warehouse"),
-					options:"Warehouse", reqd: 1},
+					options:"Warehouse"},
 				{"fieldname":"qty", "fieldtype":"Float", "label":__("Quantity"),
 					reqd: 1, "default": 1},
 				{"fieldname":"fetch_exploded", "fieldtype":"Check",
@@ -221,29 +221,16 @@ frappe.ui.form.on('Material Request', {
 			if(!values) return;
 			values["company"] = frm.doc.company;
 			if(!frm.doc.company) frappe.throw(__("Company field is required"));
-			frappe.call({
-				method: "erpnext.manufacturing.doctype.bom.bom.get_bom_items",
+			frm.call({
+				method: "get_bom_items",
+				doc: frm.doc,
 				args: values,
 				callback: function(r) {
-					if (!r.message) {
-						frappe.throw(__("BOM does not contain any stock item"));
-					} else {
-						erpnext.utils.remove_empty_first_row(frm, "items");
-						$.each(r.message, function(i, item) {
-							var d = frappe.model.add_child(cur_frm.doc, "Material Request Item", "items");
-							d.item_code = item.item_code;
-							d.item_name = item.item_name;
-							d.description = item.description;
-							d.warehouse = values.warehouse;
-							d.uom = item.stock_uom;
-							d.stock_uom = item.stock_uom;
-							d.conversion_factor = 1;
-							d.qty = item.qty;
-							d.project = item.project;
-						});
+					if (!r.exc) {
+						d.hide();
+						set_schedule_date(frm);
+						refresh_field("items");
 					}
-					d.hide();
-					refresh_field("items");
 				}
 			});
 		});
