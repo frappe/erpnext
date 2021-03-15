e2d1ce3a532af70703c1b91b2bb6409cddbaa18c
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-03-15 13:31:19 +0530

    fix: indentations

--- erpnext/patches/v12_0/add_einvoice_summary_report_permissions.py
+++ erpnext/patches/v12_0/add_einvoice_summary_report_permissions.py
@@ -7,13 +7,13 @@ def execute():
 	if not company:
 		return
 
-    if not frappe.db.get_value('Custom Role', dict(report='E-Invoice Summary')):
-        frappe.get_doc(dict(
-            doctype='Custom Role',
-            report='E-Invoice Summary',
-            roles= [
-                dict(role='Accounts User'),
-                dict(role='Accounts Manager')
-            ]
-        )).insert()
+	if not frappe.db.get_value('Custom Role', dict(report='E-Invoice Summary')):
+		frappe.get_doc(dict(
+			doctype='Custom Role',
+			report='E-Invoice Summary',
+			roles= [
+				dict(role='Accounts User'),
+				dict(role='Accounts Manager')
+			]
+		)).insert()
 	
