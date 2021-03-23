739f9b0dbafef7e00b6f68509fb74b78976a073f
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-03-23 17:26:21 +0530

    fix: quote style

--- erpnext/patches/v13_0/rename_discharge_date_in_ip_record.py
+++ erpnext/patches/v13_0/rename_discharge_date_in_ip_record.py
@@ -4,5 +4,5 @@ from frappe.model.utils.rename_field import rename_field
 
 def execute():
 	frappe.reload_doc("Healthcare", "doctype", "Inpatient Record")
-	if frappe.db.has_column('Inpatient Record', 'discharge_date'):
+	if frappe.db.has_column("Inpatient Record", "discharge_date"):
 		rename_field("Inpatient Record", "discharge_date", "discharge_datetime")
