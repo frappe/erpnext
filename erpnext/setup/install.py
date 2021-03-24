920777784451ccf176992eb1c14d2fd8c70e1c1d
prssanna <prssud@gmail.com>
2021-03-24 16:45:32 +0530

    fix: re-execute add_standard_navbar_items patch
    - check that same items aren't appended again

--- erpnext/setup/install.py
+++ erpnext/setup/install.py
@@ -142,13 +142,15 @@ def add_standard_navbar_items():
 		}
 	]
 
-	current_nabvar_items = navbar_settings.help_dropdown
+	current_navbar_items = navbar_settings.help_dropdown
 	navbar_settings.set('help_dropdown', [])
 
 	for item in erpnext_navbar_items:
-		navbar_settings.append('help_dropdown', item)
+		current_labels = [item.get('item_label') for item in current_navbar_items]
+		if not item.get('item_label') in current_labels:
+			navbar_settings.append('help_dropdown', item)
 
-	for item in current_nabvar_items:
+	for item in current_navbar_items:
 		navbar_settings.append('help_dropdown', {
 			'item_label': item.item_label,
 			'item_type': item.item_type,
