c6a125e32f5639f8af2c4ea92be68ef7283f659e
Saif Ur Rehman <saifi0102@gmail.com>
2021-03-25 21:59:30 +0500

    fix(Vehicle Allocation Period): immutable dates after entries

--- erpnext/vehicles/doctype/vehicle_allocation_period/vehicle_allocation_period.py
+++ erpnext/vehicles/doctype/vehicle_allocation_period/vehicle_allocation_period.py
@@ -5,11 +5,12 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-from frappe.utils import getdate, get_first_day, get_last_day, global_date_format
+from frappe.utils import getdate, get_first_day, get_last_day, global_date_format, flt
 from frappe.model.document import Document
 
-
 class VehicleAllocationPeriod(Document):
+	_cant_change_fields = ['from_date', 'to_date']
+
 	def before_insert(self):
 		self.set_values_based_on_periodicity()
 
@@ -17,6 +18,7 @@ class VehicleAllocationPeriod(Document):
 		self.set_values_based_on_periodicity()
 		self.validate_to_date()
 		self.check_date_overlap()
+		self.cant_change()
 
 	def set_values_based_on_periodicity(self):
 		periodicity = frappe.get_cached_value("Vehicles Settings", None, "vehicle_allocation_periodicity")
@@ -45,3 +47,20 @@ class VehicleAllocationPeriod(Document):
 
 		if period_list:
 			frappe.throw(_("Date range overlaps with Allocation Period {0}").format(", ".join(period_list)))
+
+	def cant_change(self):
+		if not self.get("__islocal") and self.has_transactions():
+			before_save_values = frappe.db.get_value("Vehicle Allocation Period", self.name, self._cant_change_fields, as_dict=True)
+			if before_save_values:
+				for field in self._cant_change_fields:
+					if self.get(field) != before_save_values.get(field):
+						frappe.throw(_("As there are existing transactions against Vehicle Allocation Period {0}, you can not change the value of {1}")
+							.format(self.name, frappe.bold(self.meta.get_label(field))))
+
+	def has_transactions(self):
+		if frappe.db.sql("select name from `tabVehicle Allocation` where docstatus = 1 and (allocation_period = %s or delivery_period = %s) limit 1", (self.name, self.name)):
+			return True
+		elif frappe.db.sql("select name from `tabVehicle Booking Order` where docstatus = 1 and (allocation_period = %s or delivery_period = %s) limit 1", (self.name, self.name)):
+			return True
+		else:
+			return False
