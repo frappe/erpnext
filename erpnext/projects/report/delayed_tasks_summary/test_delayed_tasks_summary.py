a2e5a49536d35455a9f5352abf03802d2429cad8
pateljannat <pateljannat2308@gmail.com>
2021-03-30 13:48:00 +0530

    fix: renamed test

--- erpnext/projects/report/delayed_tasks_summary/test_delayed_tasks_summary.py
+++ erpnext/projects/report/delayed_tasks_summary/test_delayed_tasks_summary.py
@@ -4,7 +4,7 @@ import frappe
 import datetime
 from frappe.utils import nowdate, add_days, add_months
 from erpnext.projects.doctype.task.test_task import create_task
-from erpnext.projects.report.delayed_deliverables_summary.delayed_deliverables_summary import execute
+from erpnext.projects.report.delayed_tasks_summary.delayed_tasks_summary import execute
 
 class TestInpatientMedicationOrders(unittest.TestCase):
 	@classmethod
@@ -16,7 +16,7 @@ class TestInpatientMedicationOrders(unittest.TestCase):
 		task1.completed_on = add_days(nowdate(), -1)
 		task1.save()
 
-	def test_delayed_deliverables_summary(self):
+	def test_delayed_tasks_summary(self):
 		filters = frappe._dict({
 			"from_date": add_months(nowdate(), -1),
 			"to_date": nowdate(),
@@ -26,33 +26,28 @@ class TestInpatientMedicationOrders(unittest.TestCase):
 		expected_data = [
 			{
 				"subject": "_Test Task 99",
-				"exp_start_date": datetime.date(2021, 3, 16),
-				"exp_end_date": datetime.date(2021, 3, 25),
 				"status": "Open",
 				"priority": "Low",
 				"delay": 1
 			},
 			{
 				"subject": "_Test Task 98",
-				"exp_start_date": datetime.date(2021, 3, 16),
-				"exp_end_date": datetime.date(2021, 3, 26),
 				"status": "Completed",
 				"priority": "Low",
-				"completed_on": datetime.date(2021, 3, 25),
 				"delay": -1
 			}
 		]
 		report = execute(filters)
 		data = list(filter(lambda x: x.subject == "_Test Task 99", report[1]))[0]
 		
-		for key in ["subjet", "exp_start_date", "exp_end_date", "status", "priority", "delay"]:
+		for key in ["subjet", "status", "priority", "delay"]:
 			self.assertEqual(expected_data[0].get(key), data.get(key))
 
 		filters.status = "Completed"
 		report = execute(filters)
 		data = list(filter(lambda x: x.subject == "_Test Task 98", report[1]))[0]
 
-		for key in ["subjet", "exp_start_date", "exp_end_date", "status", "priority", "completed_on", "delay"]:
+		for key in ["subjet", "status", "priority", "delay"]:
 			self.assertEqual(expected_data[1].get(key), data.get(key))
 
 	def tearDown(self):
