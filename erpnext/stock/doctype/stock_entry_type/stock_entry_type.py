dcf9148695146ea8e3fbc3747dfe1f3d00c3d17d
Furqan Asghar <furqan.790@outlook.com>
2021-04-06 14:53:02 +0500

    fix : Expense Account in Stock Entry Type

--- erpnext/stock/doctype/stock_entry_type/stock_entry_type.py
+++ erpnext/stock/doctype/stock_entry_type/stock_entry_type.py
@@ -4,19 +4,34 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.utils import cint
 from frappe.model.document import Document
+from six import string_types
+import json
 
 class StockEntryType(Document):
 	pass
 
 
 @frappe.whitelist()
-def get_stock_entry_type_details(stock_entry_type):
-	doc = frappe.get_cached_doc("Stock Entry Type", stock_entry_type)
+def get_stock_entry_type_details(args):
+	from erpnext.stock.doctype.stock_entry.stock_entry import get_item_expense_accounts
+
+	if isinstance(args, string_types):
+		args = json.loads(args)
+
+	args = frappe._dict(args)
+	if not args.stock_entry_type:
+		frappe.throw(_("Stock Entry Type is mandatory"))
+
+	doc = frappe.get_cached_doc("Stock Entry Type", args.stock_entry_type)
 	out = frappe._dict({
-		'source_warehouse_type': doc.source_warehouse_type,
-		'target_warehouse_type': doc.target_warehouse_type,
+		"parent": {
+			'source_warehouse_type': doc.source_warehouse_type,
+			'target_warehouse_type': doc.target_warehouse_type,
+		},
+		"items": {}
 	})
 
 	if doc.is_opening:
@@ -26,5 +41,8 @@ def get_stock_entry_type_details(stock_entry_type):
 
 	if doc.customer_provided:
 		out.customer_provided = cint(doc.customer_provided == "Yes")
+
+	if args.get('items'):
+		out['items'] = get_item_expense_accounts(args)
 
 	return out
