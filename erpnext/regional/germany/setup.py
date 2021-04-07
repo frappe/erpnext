6b2e4f2b5d7555e14091f0908588ac5ebc360e64
barredterra <14891507+barredterra@users.noreply.github.com>
2021-04-07 20:03:59 +0200

    feat: add custom field debtor_creditor_number to Party Account

--- erpnext/regional/germany/setup.py
+++ erpnext/regional/germany/setup.py
@@ -1,6 +1,18 @@
 import os
 import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 def setup(company=None, patch=True):
-	pass
+	make_custom_fields()
+
+
+def make_custom_fields():
+	custom_fields = {
+		'Party Account': [
+			dict(fieldname='debtor_creditor_number', label='Debtor/Creditor Number',
+			fieldtype='Data', insert_after='account')
+		]
+	}
+
+	create_custom_fields(custom_fields)
