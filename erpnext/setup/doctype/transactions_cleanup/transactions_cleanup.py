1e124beb5f14e598db5b9301726130bf83fc507a
GangaManoj <ganga.manoj98@gmail.com>
2021-04-07 19:11:54 +0530

    feat(Transactions Cleanup): Reset naming series after deletion of docs

--- erpnext/setup/doctype/transactions_cleanup/transactions_cleanup.py
+++ erpnext/setup/doctype/transactions_cleanup/transactions_cleanup.py
@@ -15,10 +15,23 @@ class TransactionsCleanup(Document):
 			print(frappe.get_all(doctype.doctype_name)) # prints the docs to be deleted 
 			# doctype_to_be_deleted = doctype.doctype_name
 			# print(doctype_to_be_deleted)
-		print(self.as_dict())
+			print("*" * 50)
+			doctype_to_be_deleted = frappe.get_doc('DocType', doctype.doctype_name)
+			print(doctype_to_be_deleted.autoname)
+			prefix, hashes = doctype_to_be_deleted.autoname.rsplit(".", 1)
+			print(prefix)
+		# print(self.as_dict())
 		
 	def on_submit(self):
 		for doctype in self.doctypes:
 			frappe.db.delete(doctype.doctype_name, {
 				'company' : self.company
 			})
+
+			doctype_to_be_cleared = frappe.get_doc('DocType', doctype.doctype_name)
+			if '#' in doctype_to_be_cleared.autoname:
+				self.reset_naming_series(doctype_to_be_cleared)
+
+	def reset_naming_series(self, doctype):
+		prefix, hashes = doctype.autoname.rsplit(".", 1)
+		frappe.db.sql("UPDATE `tabSeries` SET `current` = 0 WHERE `name`=%s", prefix)
