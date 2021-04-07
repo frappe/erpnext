979d691d509e5f61795ab4c42e9267e372a5bdef
GangaManoj <ganga.manoj98@gmail.com>
2021-04-07 08:10:42 +0530

    feat: transactions cleanup

---
+++ erpnext/setup/doctype/transactions_cleanup/transactions_cleanup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class TransactionsCleanup(Document):
+	def before_save(self):
+		print("*" * 50)
+		print(self.company) # prints name of the company
+		for doctype in self.doctypes:
+			print(doctype.doctype_name) # prints name of the doctype to be deleted
+			print(frappe.get_all(doctype.doctype_name)) # prints the docs to be deleted 
+			# doctype_to_be_deleted = doctype.doctype_name
+			# print(doctype_to_be_deleted)
+		print(self.as_dict())
+		
+	def on_submit(self):
+		for doctype in self.doctypes:
+			for doc in frappe.get_all(doctype.doctype_name):
+				print("*" * 50)
+				print(doc)
+				frappe.db.delete(doctype.doctype_name, {
+					'name' : doc.name,
+					'company' : self.company
+				})
