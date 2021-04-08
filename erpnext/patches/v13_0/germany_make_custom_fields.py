17af5bc24120538ddeb9f434c78a76f8351a946a
barredterra <14891507+barredterra@users.noreply.github.com>
2021-04-08 16:57:20 +0200

    docs: doctring for patch

--- erpnext/patches/v13_0/germany_make_custom_fields.py
+++ erpnext/patches/v13_0/germany_make_custom_fields.py
@@ -6,7 +6,13 @@ from __future__ import unicode_literals
 import frappe
 from erpnext.regional.germany.setup import make_custom_fields
 
+
 def execute():
+	"""Execute the make_custom_fields method for german companies.
+
+	It is usually run once at setup of a new company. Since it's new, run it
+	once for existing companies as well.
+	"""
 	company_list = frappe.get_all('Company', filters = {'country': 'Germany'})
 	if not company_list:
 		return
