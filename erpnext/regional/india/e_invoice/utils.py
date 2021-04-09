5acb8ce1e8a2d1f61a92dc89936a7b99ced0a0aa
Deepesh Garg <deepeshgarg6@gmail.com>
2021-04-09 10:57:29 +0530

    fix: Absolute taxable values

--- erpnext/regional/india/e_invoice/utils.py
+++ erpnext/regional/india/e_invoice/utils.py
@@ -177,8 +177,8 @@ def get_item_list(invoice):
 		else:
 			item.discount_amount = 0
 
-		item.unit_rate = abs((item.taxable_value - item.discount_amount)/ item.qty)
-		item.gross_amount = abs(item.taxable_value + item.discount_amount)
+		item.unit_rate = abs(abs(item.taxable_value) - item.discount_amount)/ item.qty)
+		item.gross_amount = abs(item.taxable_value) + item.discount_amount
 		item.taxable_value = abs(item.taxable_value)
 
 		item.batch_expiry_date = frappe.db.get_value('Batch', d.batch_no, 'expiry_date') if d.batch_no else None
