a9158636b97ba6f707039acaf18dd2b92847de9c
GangaManoj <ganga.manoj98@gmail.com>
2021-04-10 00:13:38 +0530

    feat(Transactions Cleanup): Created and prepopulated 'Additional Doctypes' table

--- erpnext/setup/doctype/transactions_cleanup/transactions_cleanup.py
+++ erpnext/setup/doctype/transactions_cleanup/transactions_cleanup.py
@@ -12,8 +12,8 @@ class TransactionsCleanup(Document):
 		print("*" * 50)
 		print(self.name)
 		print(self.company) # prints name of the company
-		for doctype in self.doctypes:
-			print(doctype.as_dict())
+		# for doctype in self.doctypes:
+		# 	print(doctype.name)
 
 		# 	print(doctype.doctype_name) # prints name of the doctype to be deleted
 		# 	print(frappe.get_all(doctype.doctype_name)) # prints the docs to be deleted 
@@ -28,52 +28,42 @@ class TransactionsCleanup(Document):
 		# 	print(self.docstatus)
 		# print(self.as_dict())
 
-		# doctypes = frappe.get_all('Doctype',
-		# 	filters={
-		# 		'issingle' : 0,
-		# 		'istable' : 0
-		# 	})
-		
-		# linked_doctypes = []
-		# for doctype in doctypes:
-		# 	doctype_obj = frappe.get_doc('DocType', doctype.name)
-		# 	doctype_dict = doctype_obj.as_dict()
-		# 	doctype_fields = doctype_dict['fields']
-		# 	for doctype_field in doctype_fields:
-		# 		if doctype_field['fieldname'] == "company":
-		# 			linked_doctypes.append({
-		# 				"doctype" : "Transactions Cleanup Doctype",
-		# 				"doctype_name" : doctype.doctype_name,
-		# 				"parent" : self.name,
-		# 				"parenttype" : "Transactions Cleanup"
-		# 				})
-		# 			# linked_doctype = frappe.get_doc({
-		# 			# 	"doctype" : "Transactions Cleanup Doctype",
-		# 			# 	"doctype_name" : doctype.doctype_name,
-		# 			# 	"parent" : self.name,
-		# 			# 	"parenttype" : "Transactions Cleanup"
-		# 			# })
-		# 			# linked_doctype.insert()
-		# 			break
-		# new_doc = frappe.get_doc({
-		# 	"doctype" : "Transactions Cleanup",
-		# 	"company" : self.company,
-		# 	"doctypes" : linked_doctypes
- 		# })
-		# new_doc.insert()
+		# prepopulating the 'Additional DocTypes' table if it's not empty
+		if not self.doctypes:
+			doctypes = frappe.get_all('Doctype',
+				filters={
+					'issingle' : 0,
+					'istable' : 0
+				})
+			
+			for doctype in doctypes:
+				doctype_obj = frappe.get_doc('DocType', doctype.name)
+				doctype_dict = doctype_obj.as_dict()
+				doctype_fields = doctype_dict['fields']
+				for doctype_field in doctype_fields:
+					if doctype_field['fieldname'] == "company":
+						self.append('doctypes',{
+							"doctype_name" : doctype.name,
+						})
+						break
 		
 	def on_submit(self):
-		for doctype in self.doctypes:
+		for doctype in self.doctypes or self.customisable_doctypes:
 			frappe.db.delete(doctype.doctype_name, {
 				'company' : self.company
 			})
 
 			doctype_to_be_cleared = frappe.get_doc('DocType', doctype.doctype_name)
-			if '#' in doctype_to_be_cleared.autoname:
-				self.update_naming_series(doctype_to_be_cleared)
+			if doctype_to_be_cleared.autoname:
+				if '#' in doctype_to_be_cleared.autoname:
+					self.update_naming_series(doctype_to_be_cleared)
 
 	def update_naming_series(self, doctype):
-		prefix, hashes = doctype.autoname.rsplit(".", 1)
+		print(doctype.autoname)
+		if '.' in doctype.autoname:
+			prefix, hashes = doctype.autoname.rsplit(".", 1)
+		else:
+			prefix, hashes = doctype.autoname.rsplit("{", 1)
 		last = frappe.db.sql("""select max(name) from `tab{0}`
 						where name like %s""".format(doctype.name), prefix + "%")
 		if last and last[0][0]:
