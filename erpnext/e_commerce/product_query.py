09ee0d1b2cf666a79426d14f3f3aa0befe0c26db
marination <maricadsouza221197@gmail.com>
2021-04-13 00:39:26 +0530

    feat: Slashed Prices and Discount display
    - Registered mrp and price after discounts
    - slashed price with discount in listing, item page and wishlist
    - removed redundant imports
    - renamed method to `get_web_item_qty_in_stock` to get Website Item stock
    - adjusted styles for resizing
    - made add to cart button full width on cards
    
    (cherry picked from commit 60261852b291404a032c38e9a2fbb686cbe7ea01)
    
    # Conflicts:
    #	erpnext/utilities/product.py

--- erpnext/e_commerce/product_query.py
+++ erpnext/e_commerce/product_query.py
@@ -67,9 +67,13 @@ class ProductQuery:
 		# add price and availability info in results
 		for item in result:
 			product_info = get_product_info_for_website(item.item_code, skip_quotation_creation=True).get('product_info')
-			if product_info:
-				item.formatted_price = (product_info.get('price') or {}).get('formatted_price')
-				item.price = (product_info.get('price') or {}).get('price_list_rate')
+			if product_info and product_info['price']:
+				item.formatted_mrp = product_info['price'].get('formatted_mrp')
+				item.formatted_price = product_info['price'].get('formatted_price')
+				if item.formatted_mrp:
+					item.discount = product_info['price'].get('formatted_discount_percent') or \
+						product_info['price'].get('formatted_discount_rate')
+				item.price = product_info['price'].get('price_list_rate')
 
 			if self.settings.show_stock_availability:
 				if item.get("website_warehouse"):
