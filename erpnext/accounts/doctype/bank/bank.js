c6dfa497e8b1c0d1d82e54c01fc3aff7c99c070c
Rushabh Mehta <rmehta@gmail.com>
2021-04-13 12:05:10 +0530

    fix(minor): fix dynamically changing grid properties

--- erpnext/accounts/doctype/bank/bank.js
+++ erpnext/accounts/doctype/bank/bank.js
@@ -42,7 +42,9 @@ let add_fields_to_mapping_table = function (frm) {
 		});
 	});
 
-	frm.fields_dict.bank_transaction_mapping.grid.update_docfield_property('frm.fields_dict.bank_transaction_mapping.grid', 'options', options);
+	frm.fields_dict.bank_transaction_mapping.grid.update_docfield_property(
+		'bank_transaction_field', 'options', options
+	);
 };
 
 erpnext.integrations.refreshPlaidLink = class refreshPlaidLink {
