3e2355b11523c57b34737c1276eeca1521b876e4
Marica <maricadsouza221197@gmail.com>
2021-04-14 18:53:15 +0530

    fix: Sider

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -292,10 +292,9 @@ $.extend(erpnext.utils, {
 		}
 	},
 	overrides_parent_value_in_all_rows: function(doc, dt, dn, table_fieldname, fieldname, parent_fieldname) {
-		let d = locals[dt][dn];
-		if(doc[parent_fieldname]){
+		if (doc[parent_fieldname]) {
 			let cl = doc[table_fieldname] || [];
-			for(let i = 0; i < cl.length; i++) {
+			for (let i = 0; i < cl.length; i++) {
 				cl[i][fieldname] = doc[parent_fieldname];
 			}
 			frappe.refresh_field(table_fieldname);
