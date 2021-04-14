a624da520afc47192eb5834e97f03e3d9feed351
GangaManoj <ganga.manoj98@gmail.com>
2021-04-14 23:33:06 +0530

    fix(Transaction Deletion Log): Pre-populate DocTypes table

--- erpnext/setup/doctype/transaction_deletion_log/transaction_deletion_log.js
+++ erpnext/setup/doctype/transaction_deletion_log/transaction_deletion_log.js
@@ -3,6 +3,17 @@
 
 frappe.ui.form.on('Transaction Deletion Log', {
 	refresh: function(frm) {
-		
+		let doctypes_to_be_ignored = ["Account", "Cost Center", "Warehouse", "Budget",
+			"Party Account", "Employee", "Sales Taxes and Charges Template",
+			"Purchase Taxes and Charges Template", "POS Profile", "BOM",
+			"Company", "Bank Account", "Item Tax Template", "Mode Of Payment",
+			"Item Default", "Customer", "Supplier", "GST Account"]
+
+		var i;
+		for (i = 0; i < doctypes_to_be_ignored.length; i++) { 
+			frm.add_child('customisable_doctypes', {
+					doctype_name : doctypes_to_be_ignored[i]
+				});
+		}
 	}
 });
