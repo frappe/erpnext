a91448b26a7f15ab1984927d82f59273e6e6be5c
GangaManoj <ganga.manoj98@gmail.com>
2021-04-16 03:54:54 +0530

    feat(Transaction Deletion Log): Add tests

--- erpnext/setup/doctype/transaction_deletion_log/test_transaction_deletion_log.py
+++ erpnext/setup/doctype/transaction_deletion_log/test_transaction_deletion_log.py
@@ -3,8 +3,45 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
 
 class TestTransactionDeletionLog(unittest.TestCase):
-	pass
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def test_no_of_docs(self):
+		for i in range(5):
+			create_todo('Pied Piper')
+		tdr = create_transaction_deletion_request('Pied Piper')
+		for doctype in tdr.doctypes:
+			if doctype.doctype_name == 'ToDo':
+				self.assertEqual(doctype.no_of_docs, 5)
+
+	def test_doctypes_contain_company_field(self):
+		tdr = create_transaction_deletion_request('Pied Piper')
+		for doctype in tdr.doctypes:
+			flag = False
+			doctype_fields = frappe.get_meta(doctype.doctype_name).as_dict()['fields']
+			for doctype_field in doctype_fields:
+				if doctype_field['fieldtype'] == 'Link' and doctype_field['options'] == 'Company':
+					flag = True
+					break
+			self.assertTrue(flag)
+
+def create_transaction_deletion_request(company):
+	tdr = frappe.get_doc({
+		'doctype': 'Transaction Deletion Log',
+		'company': company
+	})
+	tdr.insert()
+	return tdr
+
+
+def create_todo(company):
+	todo = frappe.get_doc({
+		'doctype': 'ToDo',
+		'company': company,
+		'description': 'Delete'
+	})
+	todo.insert()
--- erpnext/setup/doctype/transaction_deletion_log/transaction_deletion_log.js
+++ erpnext/setup/doctype/transaction_deletion_log/transaction_deletion_log.js
@@ -19,6 +19,7 @@ frappe.ui.form.on('Transaction Deletion Log', {
 		}
 
 		frm.get_field('doctypes_to_be_ignored').grid.cannot_add_rows = true;
+		frm.fields_dict["doctypes_to_be_ignored"].grid.set_column_disp("no_of_docs", false)
 		frm.refresh_field('doctypes_to_be_ignored');
 
 		// var col= document.querySelector('[data-fieldname="no_of_docs"]');
