13fd28e2f5b8c3190f8a63541a45fd97d1548b99
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-04-16 11:45:24 +0530

    feat: Medications, Allergy and Drug Interactions
    Co-authored-by: Info Aakvatech <info@aakvatech.com>
    Co-authored-by: Anoop Kurungadam <anoop@earthianslive.com>
    Co-authored-by: muhammad <muhammad@earthianslive.com>

---
+++ erpnext/healthcare/doctype/mediciation_override_reason_code/mediciation_override_reason_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Mediciation Override Reason Code', {
+	// refresh: function(frm) {
+
+	// }
+});
