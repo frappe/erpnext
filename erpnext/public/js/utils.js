34f1d3a88ec88f6484babad93bae5ae5e949aa92
Alan <2.alan.tom@gmail.com>
2021-04-21 21:04:28 +0530

    fix: change subcontracted item display (#25426)

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -694,7 +694,7 @@ erpnext.utils.map_current_doc = function(opts) {
 }
 
 frappe.form.link_formatters['Item'] = function(value, doc) {
-	if (doc && value && doc.item_name && doc.item_name !== value) {
+	if (doc && value && doc.item_name && doc.item_name !== value && doc.item_code === value) {
 		return value + ': ' + doc.item_name;
 	} else if (!value && doc.doctype && doc.item_name) {
 		// format blank value in child table
