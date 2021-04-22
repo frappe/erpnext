bb7868c91fdd18fa68a323f4b9a5748534da020a
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-04-22 12:48:41 +0530

    fix: test

--- erpnext/accounts/doctype/pos_invoice/pos_invoice.py
+++ erpnext/accounts/doctype/pos_invoice/pos_invoice.py
@@ -145,8 +145,8 @@ class POSInvoice(SalesInvoice):
 					frappe.throw(_('Row #{}: Item Code: {} is not available under warehouse {}.')
 								.format(d.idx, item_code, warehouse), title=_("Item Unavailable"))
 				elif flt(available_stock) < flt(d.qty):
-					frappe.throw(_('Row #{}: Stock quantity {} not enough for Item Code: {} under warehouse {}. Available quantity {}.')
-								.format(d.idx, d.qty, item_code, warehouse, available_stock), title=_("Item Unavailable"))
+					frappe.throw(_('Row #{}: Stock quantity not enough for Item Code: {} under warehouse {}. Available quantity {}.')
+								.format(d.idx, item_code, warehouse, available_stock), title=_("Item Unavailable"))
 
 	def validate_serialised_or_batched_item(self):
 		error_msg = []
