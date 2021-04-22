6cc49084223bb9474cf65017e10b1742f14d316f
pateljannat <pateljannat2308@gmail.com>
2021-04-22 11:19:44 +0530

    fix: escape for format and msg for working hours if not set

--- erpnext/regional/india/utils.py
+++ erpnext/regional/india/utils.py
@@ -161,7 +161,7 @@ def validate_document_name(doc, method=None):
 	# Date was chosen as start of next FY to avoid irritating current users.
 	if country != "India" or getdate(doc.posting_date) < getdate("2021-04-01"):
 		return
-	print(doc.name)
+
 	if len(doc.name) > 16:
 		frappe.throw(_("Maximum length of document number should be 16 characters as per GST rules. Please change the naming series."))
 
