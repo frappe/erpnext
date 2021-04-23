4d43068e29f9812d117832d16e8420e798976816
Saif Ur Rehman <saifi0102@gmail.com>
2021-04-24 00:58:51 +0500

    fix: patch reload doctype

--- erpnext/patches/v12_0/fix_party_in_vehicle_delivery_sle.py
+++ erpnext/patches/v12_0/fix_party_in_vehicle_delivery_sle.py
@@ -3,6 +3,8 @@ import frappe, os
 from frappe import _
 
 def execute():
+	frappe.reload_doc('vehicles', 'doctype', 'vehicle_delivery')
+
 	frappe.db.sql("""
 		update `tabStock Ledger Entry` sle
 		inner join `tabVehicle Delivery` t on sle.voucher_type='Vehicle Delivery' and sle.voucher_no=t.name
