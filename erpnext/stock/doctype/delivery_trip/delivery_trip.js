667ac77fc628687d785cab1e689fadaccac29099
GangaManoj <ganga.manoj98@gmail.com>
2021-04-30 04:27:20 +0530

    feat(Delivery Trip): Make Create Expense Claim button show up after save

--- erpnext/stock/doctype/delivery_trip/delivery_trip.js
+++ erpnext/stock/doctype/delivery_trip/delivery_trip.js
@@ -41,7 +41,7 @@ frappe.ui.form.on('Delivery Trip', {
 	},
 
 	refresh: function (frm) {
-		if (frm.doc.docstatus == 1) {
+		if (frm.doc.docstatus == 1 || frm.doc.status == 'Draft') {
 			frm.add_custom_button(__('Expense Claim'), function(){
 				frappe.model.open_mapped_doc({
 					method: 'erpnext.stock.doctype.delivery_trip.delivery_trip.make_expense_claim',
