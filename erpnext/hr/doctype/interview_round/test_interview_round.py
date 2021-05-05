6b9a5df9c001c10876956bb7cdf730cb9ecd104c
Anurag Mishra <mishranaman123@gmail.com>
2021-05-05 15:25:44 +0530

    test: Interview Round

--- erpnext/hr/doctype/interview_round/test_interview_round.py
+++ erpnext/hr/doctype/interview_round/test_interview_round.py
@@ -3,8 +3,54 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
+from frappe.core.doctype.user_permission.test_user_permission import create_user
 import unittest
 
 class TestInterviewRound(unittest.TestCase):
-	pass
+
+	def tearDown(self):
+		frappe.db.sql("DELETE FROM`tabInterview Round` Where 1")
+
+	def test_role_validation(self):
+		intreviewer = create_user("test_interviewer@example.com")
+		interview_round = create_interview_round("Technical Round", ["Python", "JS"], intreviewer.name, False)
+		self.assertRaises(frappe.ValidationError, interview_round.save)
+
+
+def create_interview_round(name, skill_set, interviewer, save=True):
+	create_skill_set(skill_set)
+	interview_round = frappe.new_doc("Interview Round")
+	interview_round.round_name = name
+	interview_round.interview_type = create_interview_type()
+	interview_round.expected_average_rating = 4
+	for skill in skill_set:
+		interview_round.append("expected_skill_set", {"skill": skill})
+
+	interview_round.append("interviewer", {
+		"user": interviewer
+	})
+
+	if save:
+		interview_round.save()
+
+	return interview_round
+
+def create_skill_set(skill_set):
+	for skill in skill_set:
+		if not frappe.db.exists("Skill", skill):
+			doc = frappe.new_doc("Skill")
+			doc.skill_name = skill
+			doc.save()
+
+def create_interview_type(name = "test_interview_type"):
+	if frappe.db.exists("Interview Type", name):
+		return frappe.get_doc("Interview Type", name).name
+	else:
+		doc = frappe.new_doc("Interview Type")
+		doc.name = name
+		doc.description = "_Test_Description"
+		doc.save()
+
+		return doc.name
+
