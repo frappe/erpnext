4309239d6cb7139e26a84e5733674ac6766f47d8
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-05-05 16:04:22 +0530

    feat: Show Recent Searches

--- erpnext/www/all-products/search.js
+++ erpnext/www/all-products/search.js
@@ -1,9 +1,48 @@
 let loading = false;
 
+const MAX_RECENT_SEARCHES = 4;
+
 const searchBox = document.getElementById("search-box");
+const searchButton = document.getElementById("search-button");
 const results = document.getElementById("results");
 const categoryList = document.getElementById("category-suggestions");
 const showBrandLine = document.getElementById("show-brand-line");
+const recentSearchArea = document.getElementById("recent-search-chips");
+
+function getRecentSearches() {
+    return JSON.parse(localStorage.getItem("recent_searches") || "[]");
+}
+
+function attachEventListenersToChips() {
+    const chips = document.getElementsByClassName("recent-chip");
+
+    for (let chip of chips) {
+        chip.addEventListener("click", () => {
+            searchBox.value = chip.innerText;
+
+            // Start search with `recent query`
+            const event = new Event("input");
+            searchBox.dispatchEvent(event);
+            searchBox.focus();
+        });
+    }
+}
+
+function populateRecentSearches() {
+    let recents = getRecentSearches();
+
+    if (!recents.length) {
+        return;
+    }
+
+    html = "Recent Searches: ";
+    for (let query of recents) {
+        html += `<button class="btn btn-secondary btn-sm recent-chip mr-1">${query}</button>`;
+    }
+
+    recentSearchArea.innerHTML = html;
+    attachEventListenersToChips();
+}
 
 function populateResults(data) {
     html = ""
@@ -64,3 +103,29 @@ searchBox.addEventListener("input", (e) => {
     }
 });
 
+searchButton.addEventListener("click", (e) => {
+    let query = searchBox.value;
+    if (!query) {
+        return;
+    }
+
+    let recents = getRecentSearches();
+
+    if (recents.length >= MAX_RECENT_SEARCHES) {
+        // Remove the `First` query
+        recents.splice(0, 1);
+    }
+
+    if (recents.indexOf(query) >= 0) {
+        return;
+    }
+
+    recents.push(query);
+
+    localStorage.setItem("recent_searches", JSON.stringify(recents));
+
+    // Refresh recent searches
+    populateRecentSearches();
+});
+
+populateRecentSearches();
