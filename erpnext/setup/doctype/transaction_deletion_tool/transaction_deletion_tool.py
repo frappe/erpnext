b7947aeaa56a52325d93bc8cd7a3ec8f18ff1296
GangaManoj <ganga.manoj98@gmail.com>
2021-05-08 20:50:25 +0530

    fix: Make doctypes_to_be_ignored_list more accurate

--- erpnext/setup/doctype/transaction_deletion_tool/transaction_deletion_tool.py
+++ erpnext/setup/doctype/transaction_deletion_tool/transaction_deletion_tool.py
@@ -38,7 +38,10 @@ class TransactionDeletionTool(Document):
 
 		singles = frappe.get_all('DocType', filters = {'issingle': 1}, pluck = 'name')
 		tables = frappe.get_all('DocType', filters = {'istable': 1}, pluck = 'name')
-		doctypes_to_be_ignored_list = singles + get_doctypes_to_be_ignored()
+		doctypes_to_be_ignored_list = singles
+		for doctype in self.doctypes_to_be_ignored:
+			doctypes_to_be_ignored_list.append(doctype.doctype_name)
+
 		docfields = frappe.get_all('DocField', 
 			filters = {
 				'fieldtype': 'Link', 
