923e55948fb1903ecfad7de33fdded9f4da51eb6
Deepesh Garg <deepeshgarg6@gmail.com>
2021-05-12 11:15:32 +0530

    fix: Required by date validation in Purchase Order

--- erpnext/controllers/buying_controller.py
+++ erpnext/controllers/buying_controller.py
@@ -838,7 +838,7 @@ class BuyingController(StockController):
 		if not self.get("items"):
 			return
 
-		earliest_schedule_date = min([d.schedule_date for d in self.get("items")])
+		earliest_schedule_date = min([d.schedule_date for d in self.get("items") if d.schedule_date])
 		if earliest_schedule_date:
 			self.schedule_date = earliest_schedule_date
 
