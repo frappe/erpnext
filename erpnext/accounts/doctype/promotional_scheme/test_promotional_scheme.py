74bcb987f30dad26d66dd94caa6a3ca66eb00311
noahjacob <noahjacobkurian@gmail.com>
2021-05-18 14:52:50 +0530

    style: fixed formatting

--- erpnext/accounts/doctype/promotional_scheme/test_promotional_scheme.py
+++ erpnext/accounts/doctype/promotional_scheme/test_promotional_scheme.py
@@ -12,10 +12,10 @@ class TestPromotionalScheme(unittest.TestCase):
 		price_rules = frappe.get_all('Pricing Rule', fields = ["promotional_scheme_id", "name", "creation"],
 			filters = {'promotional_scheme': ps.name})
 		self.assertTrue(len(price_rules),1) 
-		price_doc_details = frappe.db.get_value('Pricing Rule',price_rules[0].name,['customer','min_qty','discount_percentage'],as_dict = 1)
-		self.assertTrue(price_doc_details.customer,'_Test Customer')
-		self.assertTrue(price_doc_details.min_qty,4)
-		self.assertTrue(price_doc_details.discount_percentage,20)
+		price_doc_details = frappe.db.get_value('Pricing Rule', price_rules[0].name, ['customer', 'min_qty', 'discount_percentage'], as_dict = 1)
+		self.assertTrue(price_doc_details.customer, '_Test Customer')
+		self.assertTrue(price_doc_details.min_qty, 4)
+		self.assertTrue(price_doc_details.discount_percentage, 20)
 
 		ps.price_discount_slabs[0].min_qty = 6
 		ps.append('customer', {  
@@ -23,21 +23,21 @@ class TestPromotionalScheme(unittest.TestCase):
 		ps.save()
 		price_rules = frappe.get_all('Pricing Rule', fields = ["promotional_scheme_id", "name"],
 			filters = {'promotional_scheme': ps.name})
-		self.assertTrue(len(price_rules),2) 
+		self.assertTrue(len(price_rules), 2) 
 
-		price_doc_details = frappe.db.get_value('Pricing Rule',price_rules[1].name,['customer','min_qty','discount_percentage'],as_dict = 1)
-		self.assertTrue(price_doc_details.customer,'_Test Customer 2')
-		self.assertTrue(price_doc_details.min_qty,6)
-		self.assertTrue(price_doc_details.discount_percentage,20)
+		price_doc_details = frappe.db.get_value('Pricing Rule', price_rules[1].name, ['customer', 'min_qty', 'discount_percentage'], as_dict = 1)
+		self.assertTrue(price_doc_details.customer, '_Test Customer 2')
+		self.assertTrue(price_doc_details.min_qty, 6)
+		self.assertTrue(price_doc_details.discount_percentage, 20)
 		
-		price_doc_details = frappe.db.get_value('Pricing Rule',price_rules[0].name,['customer','min_qty','discount_percentage'],as_dict = 1)
-		self.assertTrue(price_doc_details.customer,'_Test Customer')
-		self.assertTrue(price_doc_details.min_qty,6)
+		price_doc_details = frappe.db.get_value('Pricing Rule', price_rules[0].name, ['customer', 'min_qty', 'discount_percentage'], as_dict = 1)
+		self.assertTrue(price_doc_details.customer, '_Test Customer')
+		self.assertTrue(price_doc_details.min_qty, 6)
 
-		frappe.delete_doc('Promotional Scheme',ps.name)
+		frappe.delete_doc('Promotional Scheme', ps.name)
 		price_rules = frappe.get_all('Pricing Rule', fields = ["promotional_scheme_id", "name"],
 			filters = {'promotional_scheme': ps.name})
-		self.assertEqual(price_rules,[])
+		self.assertEqual(price_rules, [])
 		
 def make_promotional_scheme():
 	ps = frappe.new_doc('Promotional Scheme')
