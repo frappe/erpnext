dca4d6bbb0cb2698bf40f10dbba953beee957d99
Anurag Mishra <mishranaman123@gmail.com>
2021-05-18 15:21:12 +0530

    feat: Hr settings restructure

--- erpnext/hr/doctype/hr_settings/hr_settings.js
+++ erpnext/hr/doctype/hr_settings/hr_settings.js
@@ -2,7 +2,4 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('HR Settings', {
-	restrict_backdated_leave_application: function(frm) {
-		frm.toggle_reqd("role_allowed_to_create_backdated_leave_application", frm.doc.restrict_backdated_leave_application);
-	}
 });
