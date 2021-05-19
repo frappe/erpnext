bf7f0530e6cc14cb7722c767259c0c814fb1a8b8
noahjacob <noahjacobkurian@gmail.com>
2021-05-19 13:53:22 +0530

    fix: added error handling if entry already exists

--- erpnext/buying/doctype/supplier_item_group/supplier_item_group.py
+++ erpnext/buying/doctype/supplier_item_group/supplier_item_group.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class SupplierItemGroup(Document):
-	pass
+	def validate(self):
+		exists = frappe.db.exists({
+			'doctype': 'Supplier Item Group',
+			'supplier': self.supplier,
+			'item_group': self.item_group
+		})
+		if exists:
+			frappe.throw(_("Item Group has already been linked to this supplier."))
