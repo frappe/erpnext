072e1f95ee59b4992cda922ba39e32d4717dbbff
Anupam <anupamvns0099@gmail.com>
2021-05-20 14:15:58 +0530

    fix: sider issues

--- erpnext/patches/v13_0/update_timesheet_changes.py
+++ erpnext/patches/v13_0/update_timesheet_changes.py
@@ -11,13 +11,13 @@ def execute():
 	frappe.reload_doc("projects", "doctype", "timesheet_detail")
 
 	frappe.db.sql("""UPDATE `tabTimesheet Detail`
-            SET base_billing_rate = billing_rate,
+			SET base_billing_rate = billing_rate,
 			base_billing_amount = billing_amount,
 			base_costing_rate = costing_rate,
 			base_costing_amount = costing_amount""")
 	
 	frappe.db.sql("""UPDATE `tabTimesheet`
-            SET currency = '{0}',
+			SET currency = '{0}',
 			exchange_rate = 1.0,
 			base_total_billable_amount = total_billable_amount,
 			base_total_billed_amount = total_billed_amount,
