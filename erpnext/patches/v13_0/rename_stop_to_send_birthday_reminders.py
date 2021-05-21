74f3506db9f7bdf102e1fc56d6de92f2a0725a1c
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-05-21 15:39:08 +0530

    fix: Failing patch test

--- erpnext/patches/v13_0/rename_stop_to_send_birthday_reminders.py
+++ erpnext/patches/v13_0/rename_stop_to_send_birthday_reminders.py
@@ -2,18 +2,22 @@ import frappe
 from frappe.model.utils.rename_field import rename_field
 
 def execute():
-    frappe.reload_doc('hr', 'doctype', 'hr_settings')
+	frappe.reload_doc('hr', 'doctype', 'hr_settings')
 
-    if frappe.db.has_column('HR Settings', 'stop_birthday_reminders'):
-        # Rename the field
-        rename_field('HR Settings', 'stop_birthday_reminders', 'send_birthday_reminders')
-        
-        # Reverse the value
-        old_value = frappe.db.get_single_value('HR Settings', 'send_birthday_reminders')
+	try:
+		# Rename the field
+		rename_field('HR Settings', 'stop_birthday_reminders', 'send_birthday_reminders')
+		
+		# Reverse the value
+		old_value = frappe.db.get_single_value('HR Settings', 'send_birthday_reminders')
 
-        frappe.db.set_value(
-            'HR Settings', 
-            'HR Settings', 
-            'send_birthday_reminders', 
-            1 if old_value == 0 else 0
-        )
+		frappe.db.set_value(
+			'HR Settings', 
+			'HR Settings', 
+			'send_birthday_reminders', 
+			1 if old_value == 0 else 0
+		)
+		
+	except Exception as e:
+		if e.args[0] != 1054:
+			raise
