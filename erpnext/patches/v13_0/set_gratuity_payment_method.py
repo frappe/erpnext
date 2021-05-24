3108675dffcae5b29998b0ad71ef9120bc5791ba
Anurag Mishra <32095923+Anurag810@users.noreply.github.com>
2021-05-24 16:38:20 +0530

    Update erpnext/patches/v13_0/set_gratuity_payment_method.py
    Co-authored-by: Rucha Mahabal <ruchamahabal2@gmail.com>

--- erpnext/patches/v13_0/set_gratuity_payment_method.py
+++ erpnext/patches/v13_0/set_gratuity_payment_method.py
@@ -5,4 +5,4 @@ def execute():
     frappe.reload_doctype("Gratuity")
 
     frappe.db.sql("update `tabGratuity` set `payment_method` = 'Additional Salary' where `pay_via_salary_slip` = 1")
-    frappe.db.sql("update `tabGratuity` set `payment_method` = 'Payment Entry' where `pay_via_salary_slip` = 1")
+    frappe.db.sql("update `tabGratuity` set `payment_method` = 'Payment Entry' where `pay_via_salary_slip` = 0")
