bad7f008f1eb9dbc04a791d3e938a3770ea20289
Saif Ur Rehman <saifi0102@gmail.com>
2021-06-03 13:30:31 +0500

    fix: Rename Bility Patch check if column exists

--- erpnext/patches/v12_0/rename_vehicle_bilty_field.py
+++ erpnext/patches/v12_0/rename_vehicle_bilty_field.py
@@ -5,5 +5,7 @@ def execute():
 	frappe.reload_doc('vehicles', 'doctype', 'vehicle_receipt')
 	frappe.reload_doc('vehicles', 'doctype', 'vehicle_booking_order')
 
-	rename_field('Vehicle Receipt', 'supplier_delivery_note', 'lr_no')
-	rename_field('Vehicle Booking Order', 'supplier_delivery_note', 'lr_no')
+	if frappe.db.has_column('Vehicle Receipt', 'supplier_delivery_note'):
+		rename_field('Vehicle Receipt', 'supplier_delivery_note', 'lr_no')
+	if frappe.db.has_column('Vehicle Booking Order', 'supplier_delivery_note'):
+		rename_field('Vehicle Booking Order', 'supplier_delivery_note', 'lr_no')
