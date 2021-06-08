a4aecd712ba547a46085b89b9f3cdef0fb44fdb6
asoral <asoral@dexciss.com>
2021-06-08 19:12:48 +0530

    remaining 2 doctype added

--- erpnext/stock/doctype/delivery_planning/delivery_planning.js
+++ erpnext/stock/doctype/delivery_planning/delivery_planning.js
@@ -4,19 +4,44 @@
 frappe.ui.form.on('Delivery Planning', {
 
 	 onload: function(frm){
-//	 To fetch pin code from Address and set in Postal code from and to
-	 	console.log("--------------000000000---------------")
-         frappe.call({
-			method: "get_options",
-			doc: frm.doc,
-			callback: function(r) {
-			console.log("this is option",r.message)
-				frm.set_df_property("pincode_from", "options", r.message);
-				frm.refresh_field('pincode_from');
-				frm.set_df_property("pincode_to", "options", r.message);
-				frm.refresh_field('pincode_to');
-			}
-		});
+
+
+//		frappe.ui.form.on("Transporter", {
+//			setup: function(frm) {
+//				frm.set_query("transporter", function() {
+//					return {
+//						filters: [
+//							["is_transporter" = 1]
+//						]
+//					}
+//				});
+//			}
+//		});
+
+////	 To fetch pin code from Address and set in Postal code from and to
+//	 	console.log("--------------000000000---------------")
+//         frappe.call({
+//			method: "get_options",
+//			doc: frm.doc,
+//			callback: function(r) {
+//			console.log("this is option",r.message)
+//				frm.set_df_property("pincode_from", "options", r.message);
+//				frm.refresh_field('pincode_from');
+//				frm.set_df_property("pincode_to", "options", r.message);
+//				frm.refresh_field('pincode_to');
+//			}
+//		});
+//   		frappe.call({
+//   			method: "get_transport",
+//   			doc: frm.doc,
+//   			callback: function(r){
+//   			if(r.message)
+//   			console.log("000000000011111111111",r)
+//   				frm.set_df_property("transporter","link",r.message);
+//   			}
+//
+//   		});
+
     },
 
 //    on_submit: funtion(frm){
@@ -48,96 +73,98 @@ frappe.ui.form.on('Delivery Planning', {
 //   		 });
 //	 	}
 
-	frm.cscript.get_sales_orders = function()
-		{
-			frm.call({
-				doc:frm.doc,
-				method: 'get_sales_order',
-                callback:function(r){
-                	if(r.message){
-                		console.log("transport",r);
-                		$.each(r.message,function(index,row){
-                			if(row.qty>0){
-	                			var d = frm.add_child("item_wise_dp");
-	                			d.transporter = row.transporter
- 	                            d.customer = row.customer
-	                            d.item = row.item_code
-	                            d.item_name = row.item_name
-	                            d.src_warehouse = row.warehouse
-	                            d.a_qty = row.qty
-	                            d.o_qty = row.stock_qty
-	                            d.sales_order = row.name
-	                            d.weight_od = d.o_qty * row.weight_per_unit
-	                            d.a_stock = row.projected_qty - row.qty
-	                            d.d_date = row.delivery_date
-	                            d.c_stock = row.projected_qty
-	                            d.p_qty = d.o_qty - d.qty_tbd
-	                            d.weight_pu = row.weight_per_unit
-	                            frm.set_df_property('item_wise_dp','hidden',0)
-	                            frm.refresh_field("item_wise_dp")
-	                        }
-                        })
-                	}
-                }
-			});
-		}
+//	frm.cscript.get_sales_orders = function()
+//		{
+//			frm.call({
+//				doc:frm.doc,
+//				method: 'get_sales_order',
+//                callback:function(r){
+//                	if(r.message){
+//                		console.log("transport",r);
+//                		$.each(r.message,function(index,row){
+//                			if(row.qty>0){
+//	                			var d = frm.add_child("item_wise_dp");
+//	                			d.transporter = row.transporter
+// 	                            d.customer = row.customer
+//	                            d.item = row.item_code
+//	                            d.item_name = row.item_name
+//	                            d.src_warehouse = row.warehouse
+//	                            d.a_qty = row.qty
+//	                            d.o_qty = row.stock_qty
+//	                            d.sales_order = row.name
+//	                            d.weight_od = d.o_qty * row.weight_per_unit
+//	                            d.a_stock = row.projected_qty - row.qty
+//	                            d.d_date = row.delivery_date
+//	                            d.c_stock = row.projected_qty
+//	                            d.p_qty = d.o_qty - d.qty_tbd
+//	                            d.weight_pu = row.weight_per_unit
+//	                            frm.set_df_property('item_wise_dp','hidden',0)
+//	                            frm.refresh_field("item_wise_dp")
+//	                        }
+//                        })
+//                	}
+//                }
+//			});
+//		}
+//
+//	frm.cscript.get_daily_dp = function()
+//		{
+//			frm.call({
+//				doc:frm.doc,
+//				method: 'get_daily_d',
+//                callback:function(r){
+//                	if(r.message){
+//                		console.log("order",r);
+//                		$.each(r.message,function(index,row){
+//                			if(row.total_net_weight>0){
+//	                			var d = frm.add_child("transporter_wise_dp");
+//	                			console.log("order",row.transporter);
+//	                			d.transporter = row.transporter
+////	                            d.s_warehouse = row.warehouse
+//	                            d.qty_td = row.total_qty
+//	                            d.weight_od = row.total_net_weight
+////	                            d.weight_od = d.qty_td * row.weight_per_unit
+//	                            d.d_date = row.delivery_date
+////	                            var weight_pu = row.weight_per_unit
+//	                            frm.set_df_property('transporter_wise_dp','hidden',0)
+//	                            frm.refresh_field("transporter_wise_dp")
+//	                        }
+//                        })
+//                	}
+//                }
+//			});
+//		}
+//
+//	frm.cscript.po_to_create = function()
+//		{
+//			frm.call({
+//				doc:frm.doc,
+//				method: 'p_order_create',
+//                callback:function(r){
+//                	if(r.message){
+//                		console.log("item",r);
+//                		$.each(r.message,function(index,row){
+//                			if(row.qty>0){
+//	                			var d = frm.add_child("orderw_pplan");
+//	                			d.supplier = row.transporter
+//	                            d.item_code = row.item_code
+//	                            d.item_name = row.item_name
+//	                            d.src_warehouse = row.warehouse
+//	                            d.qty_to = row.qty
+//	                            d.o_qty = row.stock_qty
+//	                            d.sales_order = row.name
+//
+//	                            frm.set_df_property('orderw_pplan','hidden',0)
+//	                            frm.refresh_field("orderw_pplan")
+//	                        }
+//                        })
+//                	}
+//                }
+//			});
+//
+//		}
 
-	frm.cscript.get_daily_dp = function()
-		{
-			frm.call({
-				doc:frm.doc,
-				method: 'get_daily_d',
-                callback:function(r){
-                	if(r.message){
-                		console.log("order",r);
-                		$.each(r.message,function(index,row){
-                			if(row.total_net_weight>0){
-	                			var d = frm.add_child("transporter_wise_dp");
-	                			console.log("order",row.transporter);
-	                			d.transporter = row.transporter
-//	                            d.s_warehouse = row.warehouse
-	                            d.qty_td = row.total_qty
-	                            d.weight_od = row.total_net_weight
-//	                            d.weight_od = d.qty_td * row.weight_per_unit
-	                            d.d_date = row.delivery_date
-//	                            var weight_pu = row.weight_per_unit
-	                            frm.set_df_property('transporter_wise_dp','hidden',0)
-	                            frm.refresh_field("transporter_wise_dp")
-	                        }
-                        })
-                	}
-                }
-			});
-		}
 
-	frm.cscript.po_to_create = function()
-		{
-			frm.call({
-				doc:frm.doc,
-				method: 'p_order_create',
-                callback:function(r){
-                	if(r.message){
-                		console.log("item",r);
-                		$.each(r.message,function(index,row){
-                			if(row.qty>0){
-	                			var d = frm.add_child("orderw_pplan");
-	                			d.supplier = row.transporter
-	                            d.item_code = row.item_code
-	                            d.item_name = row.item_name
-	                            d.src_warehouse = row.warehouse
-	                            d.qty_to = row.qty
-	                            d.o_qty = row.stock_qty
-	                            d.sales_order = row.name
-
-	                            frm.set_df_property('orderw_pplan','hidden',0)
-	                            frm.refresh_field("orderw_pplan")
-	                        }
-                        })
-                	}
-                }
-			});
-
-		}
 
 	},
 
@@ -149,27 +176,56 @@ frappe.ui.form.on('Delivery Planning', {
 			});
 		},
 
+		show_transporter_planning_item: function(frm) {
+
+			frappe.route_options = {"related_delivery_planning": frm.doc.docname };
+			frappe.set_route("Form", "Transporter Wise Planning Item", {
+   				"related_delivey_planning": frm.doc.name
+			});
+		},
+
+		show_purchase_order_planning_item: function(frm) {
+
+			frappe.route_options = {"related_delivery_planning": frm.doc.docname };
+			frappe.set_route("Form", "Purchase Orders Planning Item", {
+   				"related_delivey_planning": frm.doc.name
+			});
+		},
+
+
+
 
 });
 
-frappe.ui.form.on("Item wise Delivery Planning", {
-	qty_tbd: function(frm,cdt,cdn) {
-		var row = locals[cdt][cdn];
-		if (row.qty_tbd)
-			row.p_qty = row.o_qty - row.qty_tbd;
-			row.weight_od = row.weight_pu * row.qty_tbd;
-			refresh_field("weight_od",cdn,"item_wise_dp")
-			refresh_field("p_qty",cdn,"item_wise_dp");
+//frappe.ui.form.on("Item wise Delivery Planning", {
+//	qty_tbd: function(frm,cdt,cdn) {
+//		var row = locals[cdt][cdn];
+//		if (row.qty_tbd)
+//			row.p_qty = row.o_qty - row.qty_tbd;
+//			row.weight_od = row.weight_pu * row.qty_tbd;
+//			refresh_field("weight_od",cdn,"item_wise_dp")
+//			refresh_field("p_qty",cdn,"item_wise_dp");
+//
+//	},
+//});
 
+frappe.ui.form.on("Show Delivery Planning Item", {
+	show_delivery_planning_item: function(frm) {
+		frappe.set_route("Form", "Delivery Planning Item", {
+   		"related_delivey_planning": frm.doc.docname
+	});
 	},
 });
 
-//frappe.ui.form.on("Show Delivery Planning Item", {
-//	show_delivery_planning_item: function(frm) {
-//		frappe.set_route("Form", "Delivery Planning Item", {
-//   		"related_delivey_planning": frm.doc.docname
-//	});
-//	},
-//});
+frappe.ui.form.on("Delivery Planning", "onload", function(frm) {
+    cur_frm.set_query("transporter", function() {
+        return {
+           "filters": {
+                "is_transporter": 1,
+            }
+        }
+    });
+});
+
 
 
--- erpnext/stock/doctype/delivery_planning/delivery_planning.py
+++ erpnext/stock/doctype/delivery_planning/delivery_planning.py
@@ -8,148 +8,158 @@ class DeliveryPlanning(Document):
 
 	def on_submit(self):
 		self.on_delivery_planning_submit()
+		self.on_transporter_planning()
+		self.on_purchase_planning()
+
 
 	# @frappe.whitelist()
-	# def get_pin(self):
-	# 	if self.pincode_from:
-	# 		pin = frappe.get_list('Address', fields = ['pincode'])
-	# 		print("111111111110000000555550000000011111111111",pin)
-	# 		return pin
+	# def get_transport(self):
+	# 	if self.transporter:
+	# 			query = frappe.db.sql(""" Select name
+	# 							from `tabSupplier`
+	# 							where is_transporter == 1
+	# 							"""
+	# 			, as_dict=1)
+	# 			print ("----------0000000--------",query)
+	# 			option_str =''
+	# 			for r in query:
+	# 				option_str += r.get('name')+"\n"
+	# 			return option_str
 
-	@frappe.whitelist()
-	def get_sales_order(self):
-		return self.get_so()
+	# @frappe.whitelist()
+	# def get_sales_order(self):
+	# 	return self.get_so()
+	#
+	# @frappe.whitelist()
+	# def get_daily_d(self):
+	# 	return self.get_dp()
+	#
+	# @frappe.whitelist()
+	# def p_order_create(self):
+	# 	return self.p_order()
 
-	@frappe.whitelist()
-	def get_daily_d(self):
-		return self.get_dp()
+# # Query for 1st child table Item wise Delivery Planning on button click Item wise Delivery Plan
+#
+# 	def get_so(self):
+# 		conditions = ""
+# 		if self.company:
+# 			conditions +="AND so.company = %s" % frappe.db.escape(self.company)
+#
+# 		if self.transporter:
+# 			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
+#
+# 		if self.delivery_date_from:
+# 			conditions += "AND soi.delivery_date >= '%s'" % self.delivery_date_from
+#
+# 		if self.delivery_date_to:
+# 			conditions += "AND soi.delivery_date <= '%s'" % self.delivery_date_to
+#
+# 		if self.pincode_from:
+# 			conditions += "AND so.address_display  LIKE '%s'" % self.pincode_from
+#
+# 		if self.pincode_to:
+# 			conditions += "AND so.address_display  LIKE '%s'" % self.pincode_to
+#
+# 		query = frappe.db.sql(""" select
+# 						so.customer,
+# 						soi.item_code,
+# 						soi.item_name,
+# 						soi.warehouse,
+# 						soi.qty,
+# 						soi.stock_qty,
+# 						so.name,
+# 						soi.name as soi_item,
+# 						soi.weight_per_unit,
+# 						soi.delivery_date,
+# 						soi.projected_qty,
+# 						so.transporter
+#
+# 						from `tabSales Order Item` soi
+# 						join `tabSales Order` so ON soi.parent = so.name
+# 						left outer join `tabAddress` as add on add.name = so.shipping_address_name
+#
+# 						where so.docstatus = 1
+# 						{conditions} """.format(conditions=conditions), as_dict=1)
+# 		print(conditions)
+# 		return query
+#
+# # Query for 3rd child table Transporter wise Delivery Planning on button click Get Daily Delivery Plan
+#
+# 	def get_dp(self):
+# 		conditions = ""
+# 		if self.company:
+# 			conditions += "AND so.company = %s" % frappe.db.escape(self.company)
+#
+# 		if self.transporter:
+# 			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
+#
+# 		if self.delivery_date_from:
+# 			conditions += "AND so.delivery_date >= '%s'" % self.delivery_date_from
+#
+# 		if self.delivery_date_to:
+# 			conditions += "AND so.delivery_date <= '%s'" % self.delivery_date_to
+#
+# 		query = frappe.db.sql(""" select
+# 					so.transporter,
+# 					so.delivery_date,
+# 					SUM(so.total_net_weight) AS total_net_weight ,
+# 					SUM(so.total_qty) AS total_qty
+#
+# 					# soi.warehouse,
+# 					# soi.weight_per_unit
+# 					from `tabSales Order` so
+# 					# from `tabSales Order Item` soi
+# 					# join `tabSales Order` so ON soi.parent = so.name
+#
+# 					where so.docstatus = 1
+# 					{conditions}
+# 					group by so.transporter, so.delivery_date
+# 					order by so.delivery_date
+# 					""".format(conditions=conditions), as_dict=1)
+#
+# 						# from `tabSupplier` s
+# 						# join `tabSales Order` so ON s.name = so.transporter
+# 		return query
+# # Query for 3rd child table Order wise Purchase Planning on button click Get Purchase Order To Be Created
+# 	def p_order(self):
+# 		conditions = ""
+# 		if self.company:
+# 			conditions += "AND so.company = %s" % frappe.db.escape(self.company)
+#
+# 		if self.transporter:
+# 			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
+#
+# 		if self.delivery_date_from:
+# 			conditions += "AND so.delivery_date >= '%s'" % self.delivery_date_from
+#
+# 		if self.delivery_date_to:
+# 			conditions += "AND so.delivery_date <= '%s'" % self.delivery_date_to
+#
+# 		query = frappe.db.sql(""" select
+# 					soi.item_code,
+# 					soi.item_name,
+# 					soi.warehouse,
+# 					soi.qty,
+# 					so.transporter,
+# 					so.name
+#
+# 					from `tabSales Order Item` soi
+# 					join `tabSales Order` so ON soi.parent = so.name
+#
+# 					where so.docstatus = 1
+# 					{conditions} """.format(conditions=conditions), as_dict=1)
+# 		return query
 
-	@frappe.whitelist()
-	def p_order_create(self):
-		return self.p_order()
-
-# Query for 3rd child table Item wise Delivery Planning on button click Item wise Delivery Plan
-
-	def get_so(self):
-		conditions = ""
-		if self.company:
-			conditions +="AND so.company = %s" % frappe.db.escape(self.company)
-
-		if self.transporter:
-			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
-
-		if self.delivery_date_from:
-			conditions += "AND soi.delivery_date >= '%s'" % self.delivery_date_from
-
-		if self.delivery_date_to:
-			conditions += "AND soi.delivery_date <= '%s'" % self.delivery_date_to
-
-		if self.pincode_from:
-			conditions += "AND so.address_display  LIKE '%s'" % self.pincode_from
-
-		if self.pincode_to:
-			conditions += "AND so.address_display  LIKE '%s'" % self.pincode_to
-
-		query = frappe.db.sql(""" select
-						so.customer,
-						soi.item_code,
-						soi.item_name,
-						soi.warehouse,
-						soi.qty,
-						soi.stock_qty,
-						so.name,
-						soi.name as soi_item,
-						soi.weight_per_unit,
-						soi.delivery_date,
-						soi.projected_qty,
-						so.transporter
-
-						from `tabSales Order Item` soi
-						join `tabSales Order` so ON soi.parent = so.name
-						left outer join `tabAddress` as add on add.name = so.shipping_address_name
-
-						where so.docstatus = 1
-						{conditions} """.format(conditions=conditions), as_dict=1)
-		print(conditions)
-		return query
-
-# Query for 3rd child table Transporter wise Delivery Planning on button click Get Daily Delivery Plan
-
-	def get_dp(self):
-		conditions = ""
-		if self.company:
-			conditions += "AND so.company = %s" % frappe.db.escape(self.company)
-
-		if self.transporter:
-			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
-
-		if self.delivery_date_from:
-			conditions += "AND so.delivery_date >= '%s'" % self.delivery_date_from
-
-		if self.delivery_date_to:
-			conditions += "AND so.delivery_date <= '%s'" % self.delivery_date_to
-
-		query = frappe.db.sql(""" select
-					so.transporter,
-					so.delivery_date,
-					SUM(so.total_net_weight) AS total_net_weight ,
-					SUM(so.total_qty) AS total_qty
-
-					# soi.warehouse,
-					# soi.weight_per_unit
-					from `tabSales Order` so
-					# from `tabSales Order Item` soi
-					# join `tabSales Order` so ON soi.parent = so.name
-
-					where so.docstatus = 1
-					{conditions}
-					group by so.transporter, so.delivery_date
-					order by so.delivery_date
-					""".format(conditions=conditions), as_dict=1)
-
-						# from `tabSupplier` s
-						# join `tabSales Order` so ON s.name = so.transporter
-		return query
-# Query for 3rd child table Order wise Purchase Planning on button click Get Purchase Order To Be Created
-	def p_order(self):
-		conditions = ""
-		if self.company:
-			conditions += "AND so.company = %s" % frappe.db.escape(self.company)
-
-		if self.transporter:
-			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
-
-		if self.delivery_date_from:
-			conditions += "AND so.delivery_date >= '%s'" % self.delivery_date_from
-
-		if self.delivery_date_to:
-			conditions += "AND so.delivery_date <= '%s'" % self.delivery_date_to
-
-		query = frappe.db.sql(""" select
-					soi.item_code,
-					soi.item_name,
-					soi.warehouse,
-					soi.qty,
-					so.transporter,
-					so.name
-
-					from `tabSales Order Item` soi
-					join `tabSales Order` so ON soi.parent = so.name
-
-					where so.docstatus = 1
-					{conditions} """.format(conditions=conditions), as_dict=1)
-		return query
-
-	@frappe.whitelist()
-	def get_options(self):
-		query = frappe.db.sql(""" Select pincode
-					from `tabAddress`"""
-		, as_dict=1)
-		print ("------------------",query)
-		option_str =''
-		for r in query:
-			option_str += r.get('pincode')+"\n"
-		return option_str
+	# @frappe.whitelist()
+	# def get_options(self):
+	# 	query = frappe.db.sql(""" Select pincode
+	# 				from `tabAddress`"""
+	# 	, as_dict=1)
+	# 	print ("------------------",query)
+	# 	option_str =''
+	# 	for r in query:
+	# 		option_str += r.get('pincode')+"\n"
+	# 	return option_str
 	# Custom button for pick list creation
 	# @frappe.whitelist()
 	# def make_pick_list(self):
@@ -195,11 +205,11 @@ class DeliveryPlanning(Document):
 			if self.delivery_date_to:
 				conditions += "AND soi.delivery_date <= '%s'" % self.delivery_date_to
 
-			if self.pincode_from:
-				conditions += "And add.pincode >= '%s" % self.pincode_from
-
-			if self.pincode_to:
-				conditions += "And add.pincode <= '%s" % self.pincode_to
+			# if self.pincode_from:
+			# 	conditions += "And add.pincode >= '%s" % self.pincode_from
+			#
+			# if self.pincode_to:
+			# 	conditions += "And add.pincode <= '%s" % self.pincode_to
 
 			query = frappe.db.sql(""" select
 									so.customer,
@@ -217,7 +227,6 @@ class DeliveryPlanning(Document):
 
 									from `tabSales Order Item` soi
 									join `tabSales Order` so ON soi.parent = so.name
-									left outer join `tabAddress` as add on add.name = so.shipping_address_name
 
 									where so.docstatus = 1
 									{conditions} """.format(conditions=conditions), as_dict=1)
@@ -245,4 +254,85 @@ class DeliveryPlanning(Document):
 				dp_item.weight_per_unit = i.weight_per_unit
 				dp_item.save(ignore_permissions = True)
 
+	def on_transporter_planning(self):
+		conditions = ""
+		if self.company:
+			conditions += "AND so.company = %s" % frappe.db.escape(self.company)
 
+		if self.transporter:
+			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
+
+		if self.delivery_date_from:
+			conditions += "AND so.delivery_date >= '%s'" % self.delivery_date_from
+
+		if self.delivery_date_to:
+			conditions += "AND so.delivery_date <= '%s'" % self.delivery_date_to
+
+			query = frappe.db.sql(""" select
+								so.transporter,
+								so.delivery_date,
+								SUM(so.total_net_weight) AS total_net_weight ,
+								SUM(so.total_qty) AS total_qty
+
+								from `tabSales Order` so
+								# from `tabSales Order Item` soi
+								# join `tabSales Order` so ON soi.parent = so.name
+
+								where so.docstatus = 1
+								{conditions}
+								group by so.transporter, so.delivery_date
+
+								""".format(conditions=conditions), as_dict=1)
+
+							# from `tabSupplier` s
+							# join `tabSales Order` so ON s.name = so.transporter
+
+			for i in query:
+				dp_item = frappe.new_doc("Transporter Wise Planning Item")
+				dp_item.transporter = i.transporter
+				dp_item.delivery_date = i.delivery_date
+				dp_item.weight_to_deliver = i.total_net_weight
+				dp_item.quantity_to_deliver = i.total_qty
+				dp_item.source_warehouse = ""
+				dp_item.related_delivery_planning = self.name
+				dp_item.save(ignore_permissions=True)
+
+
+	def on_purchase_planning(self):
+		conditions = ""
+		if self.company:
+			conditions += "AND so.company = %s" % frappe.db.escape(self.company)
+
+		if self.transporter:
+			conditions += "AND so.transporter = %s" % frappe.db.escape(self.transporter)
+
+		if self.delivery_date_from:
+			conditions += "AND soi.delivery_date >= '%s'" % self.delivery_date_from
+
+		if self.delivery_date_to:
+			conditions += "AND soi.delivery_date <= '%s'" % self.delivery_date_to
+
+		query = frappe.db.sql(""" select
+					soi.item_code,
+					soi.item_name,
+					soi.warehouse,
+					soi.qty,
+					so.transporter,
+					so.name
+
+					from `tabSales Order Item` soi
+					join `tabSales Order` so ON soi.parent = so.name
+
+					where so.docstatus = 1
+					{conditions} """.format(conditions=conditions), as_dict=1)
+
+		for i in query:
+			dp_item = frappe.new_doc("Purchase Orders Planning Item")
+			dp_item.sales_order = i.sales_order
+			dp_item.item_code = i.item_code
+			dp_item.item_name = i.item_name
+			dp_item.supplier = i.transporter
+			dp_item.related_delivery_planning = self.name
+			dp_item.save(ignore_permissions=True)
+
+# left outer join `tabAddress` as add on add.name = so.shipping_address_name
