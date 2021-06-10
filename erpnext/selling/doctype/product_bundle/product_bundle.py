ffceb971b1a3a5fb47d8143a0ad8851b599b0b00
Afshan <afshan13k@gmail.com>
2021-06-10 20:26:34 +0530

    fix: added more reference documents to verify the records.

--- erpnext/selling/doctype/product_bundle/product_bundle.py
+++ erpnext/selling/doctype/product_bundle/product_bundle.py
@@ -21,12 +21,25 @@ class ProductBundle(Document):
 		validate_uom_is_integer(self, "uom", "qty")
 
 	def on_trash(self):
-		invoices = frappe.db.get_all("Sales Invoice Item", {"item_code": self.new_item_code, "docstatus": ["!=", 2]}, ["parent"])
+		linked_doctypes = ["Delivery Note", "Sales Invoice", "POS Invoice", "Purchase Receipt", "Purchase Invoice",
+			"Stock Entry", "Stock Reconciliation", "Sales Order", "Purchase Order", "Material Request"]
+
 		invoice_links = []
-		for invoice in invoices:
-			invoice_links.append(get_link_to_form('Sales Invoice', invoice['parent']))
+		for doctype in linked_doctypes:
+			item_doctype = doctype + " Item"
+
+			if doctype == "Stock Entry":
+				item_doctype = doctype + " Detail"
+
+			invoices = frappe.db.get_all(item_doctype, {"item_code": self.new_item_code, "docstatus": ["!=", 2]}, ["parent"])
+
+			for invoice in invoices:
+				invoice_links.append(get_link_to_form(doctype, invoice['parent']))
+
 		if len(invoice_links):
-			frappe.throw("This Product Bundle is linked with Sales Invoice: {0}. You will have to cancel these invoices in order to cancel this Product Bundle".format(", ".join(invoice_links)))
+			frappe.throw(
+				"This Product Bundle is linked with {0}. You will have to cancel these documents in order to delete this Product Bundle"
+					.format(", ".join(invoice_links)), title=_("Not Allowed"))
 
 	def validate_main_item(self):
 		"""Validates, main Item is not a stock item"""
