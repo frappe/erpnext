4cd0f6ce23f703e2f98baab83ae0b38ac9e26943
Deepesh Garg <deepeshgarg6@gmail.com>
2021-06-14 20:01:04 +0530

    fix: Revert unintended changes

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -1220,9 +1220,9 @@ def validate_inclusive_tax(tax, doc):
 			# referred row should also be inclusive
 			_on_previous_row_error(tax.row_id)
 		elif tax.charge_type == "On Previous Row Total" and \
-				not all([cint(t.included_in_print_rate for t in doc.get("taxes")[:cint(tax.row_id) - 1])]):
+				not all([cint(t.included_in_print_rate) for t in doc.get("taxes")[:cint(tax.row_id) - 1]]):
 			# all rows about the referred tax should be inclusive
-			_on_previous_row_error("1 - %d" % (cint(tax.row_id),))
+			_on_previous_row_error("1 - %d" % (tax.row_id,))
 		elif tax.get("category") == "Valuation":
 			frappe.throw(_("Valuation type charges can not be marked as Inclusive"))
 
