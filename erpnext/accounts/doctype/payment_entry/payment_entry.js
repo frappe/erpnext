f57195191538c0e1df0ba5d1221f14017c22d1a8
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-06-16 18:12:31 +0530

    fix: cannot cancel payment entry if linked with invoices

--- erpnext/accounts/doctype/payment_entry/payment_entry.js
+++ erpnext/accounts/doctype/payment_entry/payment_entry.js
@@ -5,6 +5,8 @@ frappe.provide("erpnext.accounts.dimensions");
 
 frappe.ui.form.on('Payment Entry', {
 	onload: function(frm) {
+		frm.ignore_doctypes_on_cancel_all = ['Sales Invoice', 'Purchase Invoice'];
+
 		if(frm.doc.__islocal) {
 			if (!frm.doc.paid_from) frm.set_value("paid_from_account_currency", null);
 			if (!frm.doc.paid_to) frm.set_value("paid_to_account_currency", null);
