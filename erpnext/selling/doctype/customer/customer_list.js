f606e0b0b9d20c903a88c108d340a629a9f19f25
Mahesh <mahesh@sodelsolutions.com>
2021-06-16 10:29:49 +0530

    hide customer name

--- erpnext/selling/doctype/customer/customer_list.js
+++ erpnext/selling/doctype/customer/customer_list.js
@@ -1,4 +1,5 @@
 frappe.listview_settings['Customer'] = {
+	hide_name_column: true,
 	add_fields: ["customer_name", "territory", "customer_group", "customer_type", "image"],
 	onload: function(listview) {
                  $('.btn-primary').hide()
