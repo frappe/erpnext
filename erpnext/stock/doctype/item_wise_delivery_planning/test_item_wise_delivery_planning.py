42d4a284f2f1559f793ed377fd758c6ef5e2bc61
asoral <asoral@dexciss.com>
2021-06-17 18:06:38 +0530

    added dashboard, bug fix, removed unused doctype and childdt

--- erpnext/stock/doctype/item_wise_delivery_planning/test_item_wise_delivery_planning.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestItemwiseDeliveryPlanning(unittest.TestCase):
-	pass
