4c46eef1bccbf44b8e7f8f69a695dca466789f83
Afshan <afshan@erpnext.com>
2021-06-17 17:15:28 +0530

    fix: multiple invoices calculation and description formatting

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -599,7 +599,7 @@ def make_purchase_invoice(source_name, target_doc=None):
 	def update_item(source_doc, target_doc, source_parent):
 		target_doc.qty, returned_qty = get_pending_qty(source_doc)
 		if frappe.db.get_single_value("Buying Settings", "consider_rejected_quantity_for_purchase_invoice"):
-			target_doc.qty = source_doc.received_qty
+			target_doc.qty = source_doc.received_qty - invoiced_qty_map.get(source_doc.name, 0)
 			target_doc.rejected_qty = 0
 		target_doc.stock_qty = flt(target_doc.qty) * flt(target_doc.conversion_factor, target_doc.precision("conversion_factor"))
 		returned_qty_map[source_doc.name] = returned_qty
