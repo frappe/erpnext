d6969f31c484dd7592c8c8e281407e9f77fd3473
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-06-23 11:08:57 +0530

    fix: validation message

--- erpnext/hr/doctype/employee_grievance/employee_grievance.py
+++ erpnext/hr/doctype/employee_grievance/employee_grievance.py
@@ -8,9 +8,8 @@ from frappe.model.document import Document
 class EmployeeGrievance(Document):
 	def on_submit(self):
 		if self.status not in ["Invalid", "Resolved"]:
-			frappe.throw(_("Only Employee Grievance with status {0} and {1} can be submitted").format(
+			frappe.throw(_("Only Employee Grievance with status {0} or {1} can be submitted").format(
 				bold("Invalid"),
 				bold("Resolved"))
 			)
-
 
