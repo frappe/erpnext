2fc3a4cfdf31731f548b6cb8a04e8e35b9159fac
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-06-23 13:10:06 +0530

    fix: patch

--- erpnext/patches/v13_0/set_gratuity_payment_method.py
+++ erpnext/patches/v13_0/set_gratuity_payment_method.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-    frappe.reload_doctype("Gratuity")
+    frappe.reload_doc("payroll", "doctype", "gratuity")
 
     frappe.db.sql("update `tabGratuity` set `payment_method` = 'Additional Salary' where `pay_via_salary_slip` = 1")
     frappe.db.sql("update `tabGratuity` set `payment_method` = 'Payment Entry' where `pay_via_salary_slip` = 0")
