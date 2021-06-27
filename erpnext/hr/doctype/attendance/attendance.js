e0a45416ddc791685477d932117caeed3059a9ba
Saif Ur Rehman <saifi0102@gmail.com>
2021-06-28 01:22:51 +0500

    fix(Attendance): Use df default date rather than client script

--- erpnext/hr/doctype/attendance/attendance.js
+++ erpnext/hr/doctype/attendance/attendance.js
@@ -4,10 +4,6 @@
 cur_frm.add_fetch('employee', 'company', 'company');
 cur_frm.add_fetch('employee', 'employee_name', 'employee_name');
 
-cur_frm.cscript.onload = function(doc, cdt, cdn) {
-	if(doc.__islocal) cur_frm.set_value("attendance_date", frappe.datetime.get_today());
-}
-
 cur_frm.fields_dict.employee.get_query = function(doc,cdt,cdn) {
 	return{
 		query: "erpnext.controllers.queries.employee_query"
