6e5533946c20e86afc6477ff98989085efdd8f7e
shariquerik <sharique.rik@gmail.com>
2021-06-28 13:46:32 +0530

    fix: Webform Permission for custom doctype

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -60,6 +60,7 @@ treeviews = ['Account', 'Cost Center', 'Warehouse', 'Item Group', 'Customer Grou
 # website
 update_website_context = ["erpnext.shopping_cart.utils.update_website_context", "erpnext.education.doctype.education_settings.education_settings.update_website_context"]
 my_account_context = "erpnext.shopping_cart.utils.update_my_account_context"
+get_custom_website_context = "erpnext.controllers.website_list_for_contact.get_custom_website_context"
 
 calendars = ["Task", "Work Order", "Leave Application", "Sales Order", "Holiday List", "Course Schedule"]
 
