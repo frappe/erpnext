2952a48372e6d06d8f6fb18e2109c406bbcff9d3
Shahzad Naser <shahzadnaser1122@gmail.com>
2021-07-08 08:08:04 +0000

    update date filter

--- erpnext/accounts/report/general_ledger/general_ledger.js
+++ erpnext/accounts/report/general_ledger/general_ledger.js
@@ -21,7 +21,7 @@ frappe.query_reports["General Ledger"] = {
 			"fieldname":"from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
-			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"default": frappe.datetime.get_today(),
 			"reqd": 1,
 			"width": "60px"
 		},
