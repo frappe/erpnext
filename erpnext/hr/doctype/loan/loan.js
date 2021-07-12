23db6a8e3a1f4688f3fcb833f52bfd9a6aba3532
Jannat Patel <31363128+pateljannat@users.noreply.github.com>
2021-07-12 13:17:27 +0530

    fix: added company filter while fetching loans (#26296)
    Co-authored-by: Rucha Mahabal <ruchamahabal2@gmail.com>

--- erpnext/hr/doctype/loan/loan.js
+++ erpnext/hr/doctype/loan/loan.js
@@ -15,6 +15,15 @@ frappe.ui.form.on('Loan', {
 			};
 		});
 
+		frm.set_query("loan_type", function () {
+			return {
+				"filters": {
+					"docstatus": 1,
+					"company": frm.doc.company
+				}
+			};
+		});
+
 		frm.set_query("interest_income_account", function () {
 			return {
 				"filters": {
