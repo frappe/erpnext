2725e61087fb03374c5eb71cbe2c096a19a5e4a9
GangaManoj <ganga.manoj98@gmail.com>
2021-07-17 17:45:35 +0530

    fix: Only display Additional Discount Account if Enable Discount Accounting is checked

--- erpnext/accounts/doctype/accounts_settings/accounts_settings.py
+++ erpnext/accounts/doctype/accounts_settings/accounts_settings.py
@@ -41,5 +41,7 @@ class AccountsSettings(Document):
 		for doctype in ["Sales Invoice Item", "Purchase Invoice Item"]:
 			make_property_setter(doctype, "discount_account", "hidden", not(enable_discount_accounting), "Check", validate_fields_for_doctype=False)
 
+		for doctype in ["Sales Invoice", "Purchase Invoice"]:
+			make_property_setter(doctype, "additional_discount_account", "hidden", not(enable_discount_accounting), "Check", validate_fields_for_doctype=False)
+
 		make_property_setter("Item", "default_discount_account", "hidden", not(enable_discount_accounting), "Check", validate_fields_for_doctype=False)
-		make_property_setter("Sales Invoice", "additional_discount_account", "hidden", not(enable_discount_accounting), "Check", validate_fields_for_doctype=False)
