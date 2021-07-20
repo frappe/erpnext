eb327da4ef6604f3b80ebc1f98bfa73b5aa812c5
ChillarAnand <anand21nanda@gmail.com>
2021-07-20 15:03:48 +0530

    Cleanup lint issues

--- erpnext/healthcare/doctype/treatment_plan_template/treatment_plan_template.py
+++ erpnext/healthcare/doctype/treatment_plan_template/treatment_plan_template.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class TreatmentPlanTemplate(Document):
@@ -11,9 +11,9 @@ class TreatmentPlanTemplate(Document):
 
 	def validate_age(self):
 		if self.patient_age_from and self.patient_age_from < 0:
-			frappe.throw('Patient Age From cannot be less than 0')
+			frappe.throw(_('Patient Age From cannot be less than 0'))
 		if self.patient_age_to and self.patient_age_to < 0:
-			frappe.throw('Patient Age To cannot be less than 0')
+			frappe.throw(_('Patient Age To cannot be less than 0'))
 		if self.patient_age_to and self.patient_age_from and \
 			self.patient_age_to < self.patient_age_from:
-			frappe.throw('Patient Age To cannot be less than Patient Age From')
+			frappe.throw(_('Patient Age To cannot be less than Patient Age From'))
