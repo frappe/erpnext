b0c6efe798666337e770d78e2e4324d4eff1e262
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-07-22 13:01:05 +0530

    changes

--- erpnext/selling/doctype/customer_pricing_rule/customer_pricing_rule.js
+++ erpnext/selling/doctype/customer_pricing_rule/customer_pricing_rule.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2021, Sangita and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer Pricing Rule', {
--- erpnext/selling/doctype/customer_pricing_rule/customer_pricing_rule.py
+++ erpnext/selling/doctype/customer_pricing_rule/customer_pricing_rule.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Sangita and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
