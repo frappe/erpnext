18ec02c5c559a436e31560681147eb031957a037
ujjwalkumar93 <pathakujjwal93@gmail.com>
2021-07-23 15:43:09 +0530

    check country name for date conversion

--- erpnext/nepali_date.py
+++ erpnext/nepali_date.py
@@ -5,6 +5,8 @@ from datetime import timedelta
 
 @frappe.whitelist()
 def get_converted_date(date):
+    country = frappe.db.get_single_value("System Settings",'country')
     date = date.split("-")
     date_to_convert = datetime.date(int(date[0]), int(date[1]),int(date[2]))
-    return nepali_datetime.date.from_datetime_date(date_to_convert).strftime("%D-%n-%K")
+    if country == 'Nepal':
+        return nepali_datetime.date.from_datetime_date(date_to_convert).strftime("%D-%n-%K")
