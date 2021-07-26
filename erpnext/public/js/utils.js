9125115327859171f112e12581f343f9bd23f1c2
Saif Ur Rehman <saifi0102@gmail.com>
2021-07-26 13:10:09 +0500

    fix: Allow alphanumeric NTN and STRN

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -294,7 +294,8 @@ $.extend(erpnext.utils, {
 	},
 
 	get_formatted_ntn: function (value) {
-		value = value.replace(/[^0-9]+/g, "");
+		value = cstr(value).toUpperCase();
+		value = value.replace(/[^a-zA-Z0-9]+/g, "");
 
 		//0000000-0
 		if (value.length >= 7) {
@@ -314,6 +315,7 @@ $.extend(erpnext.utils, {
 	},
 
 	get_formatted_cnic: function (value) {
+		value = cstr(value).toUpperCase();
 		value = value.replace(/[^0-9]+/g, "");
 
 		// 00000-0000000-0
@@ -337,7 +339,8 @@ $.extend(erpnext.utils, {
 	},
 
 	get_formatted_strn: function (value) {
-		value = value.replace(/[^0-9]+/g, "");
+		value = cstr(value).toUpperCase();
+		value = value.replace(/[^a-zA-Z0-9]+/g, "");
 
 		// 00-00-0000-000-00
 		if (value.length >= 11) {
