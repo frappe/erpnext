0a1c62dd8f7b7cd123033c4fcca8489c103de4a1
Anupam <anupamvns0099@gmail.com>
2021-07-28 18:51:19 +0530

    fix: setup wizard

--- erpnext/setup/setup_wizard/operations/company_setup.py
+++ erpnext/setup/setup_wizard/operations/company_setup.py
@@ -45,8 +45,15 @@ def enable_shopping_cart(args):
 def create_email_digest():
 	from frappe.utils.user import get_system_managers
 	system_managers = get_system_managers(only_name=True)
+
 	if not system_managers:
 		return
+
+	recipients = []
+	for d in system_managers:
+		recipients.append({
+			'recipient': d
+		})
 
 	companies = frappe.db.sql_list("select name FROM `tabCompany`")
 	for company in companies:
@@ -56,7 +63,7 @@ def create_email_digest():
 				"name": "Default Weekly Digest - " + company,
 				"company": company,
 				"frequency": "Weekly",
-				"recipient_list": "\n".join(system_managers)
+				"recipients": recipients
 			})
 
 			for df in edigest.meta.get("fields", {"fieldtype": "Check"}):
@@ -72,7 +79,7 @@ def create_email_digest():
 			"name": "Scheduler Errors",
 			"company": companies[0],
 			"frequency": "Daily",
-			"recipient_list": "\n".join(system_managers),
+			"recipients": recipients,
 			"scheduler_errors": 1,
 			"enabled": 1
 		})
