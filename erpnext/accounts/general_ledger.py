90b18599ceba76dca8a175a2acd983e5715f801f
asad4230 <asadkhan4230@gmail.com>
2021-07-28 15:05:19 +0500

    Update general_ledger.py
    After discussion, put the journal ledger check on top in validate_account_for_perpetual_inventory

--- erpnext/accounts/general_ledger.py
+++ erpnext/accounts/general_ledger.py
@@ -147,7 +147,7 @@ def make_entry(args, adv_adj, update_outstanding, from_repost=False):
 	gle.submit()
 
 def validate_account_for_perpetual_inventory(gl_map):
-	if cint(erpnext.is_perpetual_inventory_enabled(gl_map[0].company)):
+	if cint(erpnext.is_perpetual_inventory_enabled(gl_map[0].company)) and gl_map[0].voucher_type=="Journal Entry":
 		account_list = [gl_entries.account for gl_entries in gl_map]
 
 		aii_accounts = [d.name for d in frappe.get_all("Account",
@@ -159,15 +159,12 @@ def validate_account_for_perpetual_inventory(gl_map):
 
 			account_bal, stock_bal, warehouse_list = get_stock_and_account_balance(account,
 				gl_map[0].posting_date, gl_map[0].company)
-
-			if gl_map[0].voucher_type=="Journal Entry":
-				# In case of Journal Entry, there are no corresponding SL entries,
-				# hence deducting currency amount
-				account_bal -= flt(gl_map[0].debit) - flt(gl_map[0].credit)
-				if account_bal == stock_bal:
-					frappe.throw(_("Account: {0} can only be updated via Stock Transactions")
-						.format(account), StockAccountInvalidTransaction)
-
+			# In case of Journal Entry, there are no corresponding SL entries,
+			# hence deducting currency amount
+			account_bal -= flt(gl_map[0].debit) - flt(gl_map[0].credit)
+			if account_bal == stock_bal:
+				frappe.throw(_("Account: {0} can only be updated via Stock Transactions")
+					.format(account), StockAccountInvalidTransaction)
 			# This has been comment for a temporary, will add this code again on release of immutable ledger
 			# elif account_bal != stock_bal:
 			# 	precision = get_field_precision(frappe.get_meta("GL Entry").get_field("debit"),
