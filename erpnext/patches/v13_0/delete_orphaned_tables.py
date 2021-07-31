b10ab1cb54b57d0b50d19d1669932544344231dd
GangaManoj <ganga.manoj98@gmail.com>
2021-07-31 21:47:33 +0530

    fix: Only check child tables of DocTypes that are listed in the Summary table

--- erpnext/patches/v13_0/delete_orphaned_tables.py
+++ erpnext/patches/v13_0/delete_orphaned_tables.py
@@ -8,7 +8,7 @@ from frappe.utils import getdate
 
 def execute():
     if has_deleted_company_transactions():
-        child_doctypes = frappe.get_all('DocType', filters = {'istable': 1}, pluck = 'name')
+        child_doctypes = get_child_doctypes_whose_parent_doctypes_were_affected()
 
         for doctype in child_doctypes:
             docs = frappe.get_all(doctype, fields=['name', 'parent', 'parenttype'])
@@ -22,6 +22,36 @@ def execute():
 
 def has_deleted_company_transactions():
     return frappe.get_all('Transaction Deletion Record')
+
+def get_child_doctypes_whose_parent_doctypes_were_affected():
+    parent_doctypes = get_affected_doctypes()
+    child_doctypes = frappe.get_all(
+        'DocField', 
+        filters={
+            'fieldtype': 'Table', 
+            'parent':['in', parent_doctypes]
+        }, pluck='options')
+
+    return child_doctypes
+
+def get_affected_doctypes():
+    affected_doctypes = []
+
+    tdr_docs = frappe.get_all('Transaction Deletion Record', pluck="name")
+    
+    for tdr in tdr_docs:
+        tdr_doc = frappe.get_doc("Transaction Deletion Record", tdr)
+
+        for doctype in tdr_doc.doctypes:
+            if (doctype.doctype_name not in affected_doctypes) and is_not_child_table(doctype.doctype_name):
+                affected_doctypes.append(doctype.doctype_name)
+
+    return affected_doctypes
+
+def is_not_child_table(doctype):
+    if frappe.get_value('DocType', doctype, 'istable'):
+        return False
+    return True
 
 def check_for_new_doc_with_same_name_as_deleted_parent(doc, doctype):
     """
