efb037b566b68d26c269623bcd16069d72e7f2c9
Ankush <ankush@iwebnotes.com>
2021-08-02 21:57:52 +0530

    refactor!: drop shopify integration from ERPNext (#26700)
    BREAKING CHANGE: remove Shopify integration.
    
    The integration is moved to a separate app with additional enhancements.
    
    The app is still FOSS and licensed under the same license as ERPNext. Any
    data migration required is taken care of after installing the app and
    enabling it.
    
    New app: https://github.com/frappe/ecommerce_integrations

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -341,7 +341,6 @@ scheduler_events = {
 		"erpnext.projects.doctype.project.project.hourly_reminder",
 		"erpnext.projects.doctype.project.project.collect_project_status",
 		"erpnext.hr.doctype.shift_type.shift_type.process_auto_attendance_for_all_shifts",
-		"erpnext.erpnext_integrations.connectors.shopify_connection.sync_old_orders",
 		"erpnext.support.doctype.service_level_agreement.service_level_agreement.set_service_level_agreement_variance"
 	],
 	"hourly_long": [
