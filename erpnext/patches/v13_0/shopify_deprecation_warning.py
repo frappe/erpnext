adeab4b86e70c8144a7e20195446e540816094f9
Ankush Menat <ankush@iwebnotes.com>
2021-08-02 19:18:48 +0530

    fix: change shopify patch to ignore status

--- erpnext/patches/v13_0/shopify_deprecation_warning.py
+++ erpnext/patches/v13_0/shopify_deprecation_warning.py
@@ -1,12 +1,7 @@
 import click
-import frappe
 
 
 def execute():
-
-	frappe.reload_doc("erpnext_integrations", "doctype", "shopify_settings")
-	if not frappe.db.get_single_value("Shopify Settings", "enable_shopify"):
-		return
 
 	click.secho(
 		"Shopify Integration is moved to a separate app and will be removed from ERPNext in version-14.\n"
