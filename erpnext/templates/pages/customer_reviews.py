480dd8b0569dc42829d82efbe8feeb9abdfc064a
marination <maricadsouza221197@gmail.com>
2021-08-09 21:00:31 +0530

    test: Recommendations, Reviews and Wishlist
    - Tests for verified and unverified item reviewers
    - Test for recommended items and their prices
    - Test for adding removing items from Wishlist
    - Bug: Wishlist deletes all entries of an item code irrespective of user
    - Get Item reviews only if enabled
    - Removed price fields from Wishlist Item and made fields read only
    - Removed unused price stored as data on Wishlist buttons
    - Customer Reviews page checks if reviews are enabled else shows No Reviews
    - Moved price stock fetching in Wishlist in separate function
    - Made fields read only in Item Review

--- erpnext/templates/pages/customer_reviews.py
+++ erpnext/templates/pages/customer_reviews.py
@@ -3,6 +3,7 @@
 import frappe
 from erpnext.e_commerce.doctype.item_review.item_review import get_item_reviews
 from erpnext.e_commerce.doctype.website_item.website_item import check_if_user_is_customer
+from erpnext.e_commerce.doctype.e_commerce_settings.e_commerce_settings import get_shopping_cart_settings
 
 def get_context(context):
 	context.no_cache = 1
@@ -13,4 +14,6 @@ def get_context(context):
 		context.item_code = frappe.form_dict.get("item_code")
 		context.web_item = frappe.db.get_value("Website Item", {"item_code": context.item_code}, "name")
 		context.user_is_customer = check_if_user_is_customer()
-		get_item_reviews(context.web_item, 0, 10, context)
+		context.enable_reviews = get_shopping_cart_settings().enable_reviews
+		if context.enable_reviews:
+			get_item_reviews(context.web_item, 0, 10, context)
