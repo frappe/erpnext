c70703b1722d29df682671f99b0b2afbc4c719d7
GersonCaballero <gersoncaballero306@gmail.com>
2021-08-10 14:46:34 +0000

    NewCHanges

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -1813,6 +1813,19 @@ class Payment {
 				}
 			},
 			{
+				fieldtype: 'Currency',
+				label: __("Discount"),
+				options: me.frm.doc.currency,
+				fieldname: "discount_amount",
+				default: me.frm.doc.discount_amount,
+				onchange: () => {
+					me.update_cur_frm_value('discount_amount', () => {
+						frappe.flags.discount_amount = false;
+						me.update_discount_amount()
+					});
+				}
+			},
+			{
 				fieldtype: 'Column Break',
 			},
 			{
@@ -1825,6 +1838,18 @@ class Payment {
 					me.update_cur_frm_value('change_amount', () => {
 						frappe.flags.write_off_amount = false;
 						me.update_write_off_amount();
+					});
+				}
+			},
+			{
+				fieldtype: 'Link',
+				label: __("Discount Reason"),
+				options: "Reason For Discount",
+				fieldname: "discount_reason",
+				default: me.frm.doc.discount_reason,
+				onchange: () => {
+					me.update_cur_frm_value('discount_reason', () => {
+						frappe.flags.discount_reason = false;
 					});
 				}
 			},
@@ -1861,6 +1886,8 @@ class Payment {
 		frappe.flags.loyalty_points = true;
 		frappe.flags.redeem_loyalty_points = true;
 		frappe.flags.payment_method = true;
+		frappe.flags.discount_amount = true;
+		frappe.flags.discount_reason = true;
 	}
 
 	update_cur_frm_value(fieldname, callback) {
@@ -1886,6 +1913,10 @@ class Payment {
 						});
 				}
 			});
+	}
+	update_discount_amount() {
+		this.dialog.set_value("discount_amount", this.frm.doc.discount_amount);
+		this.update_change_amount();
 	}
 
 	update_change_amount() {
