fef2ea30287602ecbbb9ff300474e778885d2789
Deepesh Garg <deepeshgarg6@gmail.com>
2021-08-12 18:18:03 +0530

    fix: Store records to delete in a separate list

--- erpnext/regional/india/utils.py
+++ erpnext/regional/india/utils.py
@@ -877,10 +877,14 @@ def delete_gst_settings_for_company(doc, method):
 		return
 
 	gst_settings = frappe.get_doc("GST Settings")
+	records_to_delete = []
 
 	for d in reversed(gst_settings.get('gst_accounts')):
 		if d.company == doc.name:
-			gst_settings.remove(d)
+			records_to_delete.append(d)
+
+	for d in records_to_delete:
+		gst_settings.remove(d)
 
 	gst_settings.save()
 
