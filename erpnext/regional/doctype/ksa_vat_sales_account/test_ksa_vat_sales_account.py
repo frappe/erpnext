9cc451fc7dbffd487316a3407d2eef1eee105d5d
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2021-08-26 07:35:59 +0500

    feat(Regional): KSA VAT Report

---
+++ erpnext/regional/doctype/ksa_vat_sales_account/test_ksa_vat_sales_account.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Havenir Solutions and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestKSAVATSalesAccount(unittest.TestCase):
+	pass
--- erpnext/regional/saudi_arabia/setup.py
+++ erpnext/regional/saudi_arabia/setup.py
@@ -3,9 +3,26 @@
 
 from __future__ import unicode_literals
 
+import frappe
+from frappe.permissions import add_permission, update_permission_property
 from erpnext.regional.united_arab_emirates.setup import make_custom_fields, add_print_formats
+from erpnext.regional.saudi_arabia.wizard.operations.setup_ksa_vat_setting import create_ksa_vat_setting
 
 
 def setup(company=None, patch=True):
 	make_custom_fields()
 	add_print_formats()
+	add_permissions()
+	create_ksa_vat_setting(company)
+	
+
+def add_permissions():
+	"""Add Permissions for KSA VAT Setting."""
+	add_permission('KSA VAT Setting', 'All', 0)
+	for role in ('Accounts Manager', 'Accounts User', 'System Manager'):
+		add_permission('KSA VAT Setting', role, 0)
+		update_permission_property('KSA VAT Setting', role, 0, 'write', 1)
+		update_permission_property('KSA VAT Setting', role, 0, 'create', 1)
+
+	"""Enable KSA VAT Report"""
+	frappe.db.set_value('Report', 'KSA VAT', 'disabled', 0)
