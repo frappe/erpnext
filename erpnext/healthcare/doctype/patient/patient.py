d091633dae393fd7535cf69a4496ed36339ba22c
Chillar Anand <anand21nanda@gmail.com>
2021-08-26 22:15:30 +0530

    feat(healthcare): Added Treatment Plan Template feature (#26557)
    * feat: Added doctypes for treatment plan template
    
    * feat: Added child doctype
    
    * feat: Added validations for patient age
    
    * chore: Clean up treatment plan template
    
    * fix: Limit plan items to templates
    
    * Added multiselectdialogbox for treatment plan template
    
    * Add template name as autoname
    
    * Clean up code
    
    * Cleanup lint issues
    
    * Clean up code
    
    * Added tests for tpt filters
    
    * Added test records
    
    * Fix order of fields
    
    * Added tests for care plan template
    
    * Added age property for patient
    
    * Clean up code
    
    * Clean up list view
    
    * Clean up code
    
    * Fix lint issues
    
    * Clean up test records
    
    * Code cleanup
    
    no-docs
    
    * chore: Code cleanup
    
    * chore: Use ORM instead of raw sql
    
    * fix: Make treatment plan item fields mandatory
    
    * fix: Added healthcare roles in permissions rules
    
    * fix: Added filters for symptoms, diagnosis
    
    * fix: Show applicable treatment plans button only if patient is present
    
    * fix: Fix key error issues
    
    * fix: Fix issues with filters in plan templates
    
    * chore: Fix age filters
    
    * refactor: appending treatment plan items
    
    * fix: treatment plan test
    
    Co-authored-by: Jannat Patel <31363128+pateljannat@users.noreply.github.com>
    Co-authored-by: Rucha Mahabal <ruchamahabal2@gmail.com>
    (cherry picked from commit f1b77360ee3b5cc418f074aefa83a1871964da99)
    
    # Conflicts:
    #	erpnext/healthcare/doctype/patient/patient.py

--- erpnext/healthcare/doctype/patient/patient.py
+++ erpnext/healthcare/doctype/patient/patient.py
@@ -122,13 +122,28 @@ class Patient(Document):
 
 		return name
 
+	@property
+	def age(self):
+		if not self.dob:
+			return
+		dob = getdate(self.dob)
+		age = dateutil.relativedelta.relativedelta(getdate(), dob)
+		return age
+
 	def get_age(self):
+<<<<<<< HEAD
 		age_str = ''
 		if self.dob:
 			dob = getdate(self.dob)
 			age = dateutil.relativedelta.relativedelta(getdate(), dob)
 			age_str = f'{str(age.years)} {_("Years(s)")} {str(age.months)} {_("Month(s)")} {str(age.days)} {_("Day(s)")}'
 
+=======
+		age = self.age
+		if not age:
+			return
+		age_str = str(age.years) + ' ' + _("Years(s)") + ' ' + str(age.months) + ' ' + _("Month(s)") + ' ' + str(age.days) + ' ' + _("Day(s)")
+>>>>>>> f1b77360ee (feat(healthcare): Added Treatment Plan Template feature (#26557))
 		return age_str
 
 	@frappe.whitelist()
--- erpnext/healthcare/doctype/patient_encounter/test_patient_encounter.py
+++ erpnext/healthcare/doctype/patient_encounter/test_patient_encounter.py
@@ -4,5 +4,82 @@
 from __future__ import unicode_literals
 import unittest
 
+import frappe
+from erpnext.healthcare.doctype.patient_encounter.patient_encounter import PatientEncounter
+
+
 class TestPatientEncounter(unittest.TestCase):
-	pass
+	def setUp(self):
+		try:
+			gender_m = frappe.get_doc({
+				'doctype': 'Gender',
+				'gender': 'MALE'
+			}).insert()
+			gender_f = frappe.get_doc({
+				'doctype': 'Gender',
+				'gender': 'FEMALE'
+			}).insert()
+		except frappe.exceptions.DuplicateEntryError:
+			gender_m = frappe.get_doc({
+				'doctype': 'Gender',
+				'gender': 'MALE'
+			})
+			gender_f = frappe.get_doc({
+				'doctype': 'Gender',
+				'gender': 'FEMALE'
+			})
+
+		self.patient_male = frappe.get_doc({
+			'doctype': 'Patient',
+			'first_name': 'John',
+			'sex': gender_m.gender,
+		}).insert()
+		self.patient_female = frappe.get_doc({
+			'doctype': 'Patient',
+			'first_name': 'Curie',
+			'sex': gender_f.gender,
+		}).insert()
+		self.practitioner = frappe.get_doc({
+			'doctype': 'Healthcare Practitioner',
+			'first_name': 'Doc',
+			'sex': 'MALE',
+		}).insert()
+		try:
+			self.care_plan_male = frappe.get_doc({
+				'doctype': 'Treatment Plan Template',
+				'template_name': 'test plan - m',
+				'gender': gender_m.gender,
+			}).insert()
+			self.care_plan_female = frappe.get_doc({
+				'doctype': 'Treatment Plan Template',
+				'template_name': 'test plan - f',
+				'gender': gender_f.gender,
+			}).insert()
+		except frappe.exceptions.DuplicateEntryError:
+			self.care_plan_male = frappe.get_doc({
+				'doctype': 'Treatment Plan Template',
+				'template_name': 'test plan - m',
+				'gender': gender_m.gender,
+			})
+			self.care_plan_female = frappe.get_doc({
+				'doctype': 'Treatment Plan Template',
+				'template_name': 'test plan - f',
+				'gender': gender_f.gender,
+			})
+
+	def test_treatment_plan_template_filter(self):
+		encounter = frappe.get_doc({
+			'doctype': 'Patient Encounter',
+			'patient': self.patient_male.name,
+			'practitioner': self.practitioner.name,
+		}).insert()
+		plans = PatientEncounter.get_applicable_treatment_plans(encounter.as_dict())
+		self.assertEqual(plans[0]['name'], self.care_plan_male.template_name)
+
+		encounter = frappe.get_doc({
+			'doctype': 'Patient Encounter',
+			'patient': self.patient_female.name,
+			'practitioner': self.practitioner.name,
+		}).insert()
+		plans = PatientEncounter.get_applicable_treatment_plans(encounter.as_dict())
+		self.assertEqual(plans[0]['name'], self.care_plan_female.template_name)
