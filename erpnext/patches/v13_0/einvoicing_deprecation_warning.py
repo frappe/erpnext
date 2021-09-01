c0ecb0dd6a011c7a3e86d3ed3346b3c333055bcd
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-09-01 15:59:08 +0530

    fix: deprecation warning message

--- erpnext/patches/v13_0/einvoicing_deprecation_warning.py
+++ erpnext/patches/v13_0/einvoicing_deprecation_warning.py
@@ -3,7 +3,7 @@ import click
 
 def execute():
 	click.secho(
-		"Indian E-Invoicing integration is moved to a separate app and will be removed from ERPNext in version-14.\n"
+		"Indian E-Invoicing integration is moved to a separate app and is removed from ERPNext.\n"
 		"Please install the app to continue using the integration: https://github.com/frappe/erpnext_gst_compliance",
 		fg="yellow",
 	)
