bf1ddac8e52e41c622b4472a48714bfab1d4eec5
GangaManoj <ganga.manoj98@gmail.com>
2021-09-01 19:28:02 +0530

    fix: Sider issues

--- erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 
 import frappe, erpnext
 
-import unittest, copy
+import unittest
+import copy
 from frappe.utils import nowdate, flt, getdate, add_days
 from frappe.model.dynamic_links import get_dynamic_link_map
 from erpnext.stock.doctype.stock_entry.test_stock_entry import make_stock_entry, get_qty_after_transaction
