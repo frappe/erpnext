0968737647ab8675021259a1203b3060d1ca6699
Deepesh Garg <deepeshgarg6@gmail.com>
2021-09-01 10:11:18 +0530

    fix: Linting and patch fixes

--- erpnext/patches/v13_0/update_dates_in_tax_withholding_category.py
+++ erpnext/patches/v13_0/update_dates_in_tax_withholding_category.py
@@ -6,17 +6,19 @@ from erpnext.accounts.utils import get_fiscal_year
 
 def execute():
 	frappe.reload_doc('accounts', 'doctype', 'Tax Withholding Rate')
-	tds_category_rates = frappe.get_all('Tax Withholding Rate', fields=['name', 'fiscal_year'])
 
-	fiscal_year_map = {}
-	for rate in tds_category_rates:
-		if not fiscal_year_map.get(rate.fiscal_year):
-			fiscal_year_map[rate.fiscal_year] = get_fiscal_year(fiscal_year=rate.fiscal_year)
+	if frappe.db.has_column('Tax Withholding Rate', 'fiscal_year'):
+		tds_category_rates = frappe.get_all('Tax Withholding Rate', fields=['name', 'fiscal_year'])
 
-		from_date = fiscal_year_map.get(rate.fiscal_year)[1]
-		to_date = fiscal_year_map.get(rate.fiscal_year)[2]
+		fiscal_year_map = {}
+		for rate in tds_category_rates:
+			if not fiscal_year_map.get(rate.fiscal_year):
+				fiscal_year_map[rate.fiscal_year] = get_fiscal_year(fiscal_year=rate.fiscal_year)
 
-		frappe.db.set_value('Tax Withholding Rate', rate.name, {
-			'from_date': from_date,
-			'to_date': to_date
-		})
+			from_date = fiscal_year_map.get(rate.fiscal_year)[1]
+			to_date = fiscal_year_map.get(rate.fiscal_year)[2]
+
+			frappe.db.set_value('Tax Withholding Rate', rate.name, {
+				'from_date': from_date,
+				'to_date': to_date
+			})
