35e0c0668b20107b608db65aabfb228a54109c08
Deepesh Garg <deepeshgarg6@gmail.com>
2021-09-03 12:40:13 +0530

    fix: Patch error and tests

--- erpnext/regional/india/utils.py
+++ erpnext/regional/india/utils.py
@@ -772,7 +772,7 @@ def update_place_of_supply(doc, method):
 	if country != 'India':
 		return
 
-	address = frappe.db.get_value("Address", doc.customer_address, ["gst_state", "gst_state_number"], as_dict=1)
+	address = frappe.db.get_value("Address", doc.get('customer_address'), ["gst_state", "gst_state_number"], as_dict=1)
 	if address and address.gst_state and address.gst_state_number:
 		doc.place_of_supply = cstr(address.gst_state_number) + "-" + cstr(address.gst_state)
 
