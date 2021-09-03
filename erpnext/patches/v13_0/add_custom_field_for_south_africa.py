1b2b74802632d85736bfa995538becdf0de5724d
Ankush Menat <ankush@iwebnotes.com>
2021-09-03 12:02:32 +0530

    fix: south africa vat patch failure (#27323)
    reload doc is necessary on new doctypes
    
    (cherry picked from commit d1fe060e4afb96324492beca77c69698cb32a085)
    
    # Conflicts:
    #	erpnext/patches/v13_0/add_custom_field_for_south_africa.py

--- erpnext/patches/v13_0/add_custom_field_for_south_africa.py
+++ erpnext/patches/v13_0/add_custom_field_for_south_africa.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2020, Frappe and Contributors
 # License: GNU General Public License v3. See license.txt
 
+<<<<<<< HEAD
 from __future__ import unicode_literals
+=======
+>>>>>>> d1fe060e4a (fix: south africa vat patch failure (#27323))
 import frappe
 from erpnext.regional.south_africa.setup import make_custom_fields, add_permissions
 
@@ -9,6 +12,9 @@ def execute():
 	company = frappe.get_all('Company', filters = {'country': 'South Africa'})
 	if not company:
 		return
+
+	frappe.reload_doc('regional', 'doctype', 'south_africa_vat_settings')
+	frappe.reload_doc('accounts', 'doctype', 'south_africa_vat_account')
 
 	make_custom_fields()
 	add_permissions()
