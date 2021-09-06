87380d021d589a9717a26ee74db19e3902948c36
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2021-09-06 23:36:55 +0500

    feat(regional): QR Code generation for Saudi Arabia Sales Invoices

--- erpnext/regional/saudi_arabia/setup.py
+++ erpnext/regional/saudi_arabia/setup.py
@@ -2,18 +2,18 @@
 # License: GNU General Public License v3. See license.txt
 
 from __future__ import unicode_literals
-
 import frappe
 from frappe.permissions import add_permission, update_permission_property
-from erpnext.regional.united_arab_emirates.setup import make_custom_fields, add_print_formats
+from erpnext.regional.united_arab_emirates.setup import make_custom_fields as uae_custom_fields, add_print_formats
 from erpnext.regional.saudi_arabia.wizard.operations.setup_ksa_vat_setting import create_ksa_vat_setting
-
+from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 
 def setup(company=None, patch=True):
-	make_custom_fields()
+	uae_custom_fields()
 	add_print_formats()
 	add_permissions()
 	create_ksa_vat_setting(company)
+	make_custom_fields()
 	
 
 def add_permissions():
@@ -26,3 +26,12 @@ def add_permissions():
 
 	"""Enable KSA VAT Report"""
 	frappe.db.set_value('Report', 'KSA VAT', 'disabled', 0)
+
+def make_custom_fields():
+	qr_code_field = dict(
+		fieldname='qr_code', 
+		label='QR Code', 
+		fieldtype='Attach Image', 
+		read_only=1, no_copy=1, hidden=1)
+	
+	create_custom_field('Sales Invoice', qr_code_field)
