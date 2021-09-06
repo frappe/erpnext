d6248b2f5be78fcd5e1dc70e8868c1ded035b3c5
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-09-06 21:39:00 +0530

    fix: test case

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -346,76 +346,94 @@ class AccountsController(TransactionBase):
 				self.conversion_rate = get_exchange_rate(self.currency,
 														 self.company_currency, transaction_date, args)
 
+	def get_args_for_get_item_details(self):
+		args = {}
+		for fieldname in self.meta.get_valid_columns():
+			args[fieldname] = self.get(fieldname)
+
+		if self.doctype in ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice"]:
+			document_type = "{} Item".format(self.doctype)
+			args.update({"document_type": document_type})
+
+		# party_name field used for customer in quotation
+		if self.doctype == "Quotation" and self.quotation_to == "Customer" and args.get("party_name"):
+			args.update({"customer": args.get("party_name")})
+
+		args["ignore_pricing_rule"] = self.get('ignore_pricing_rule', 0)
+		if self.get("is_subcontracted"):
+			args["is_subcontracted"] = self.is_subcontracted
+
+		return args
+
 	def set_missing_item_details(self, for_validate=False):
 		"""set missing item values"""
-		from erpnext.stock.doctype.serial_no.serial_no import get_serial_nos
 
 		if hasattr(self, "items"):
-			parent_dict = {}
-			for fieldname in self.meta.get_valid_columns():
-				parent_dict[fieldname] = self.get(fieldname)
+			get_item_details_args = self.get_args_for_get_item_details()
 
-			if self.doctype in ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice"]:
-				document_type = "{} Item".format(self.doctype)
-				parent_dict.update({"document_type": document_type})
+			def get_args_for_item(item):
+				args = get_item_details_args.copy()
+				args.update(item.as_dict())
+				args["doctype"] = self.doctype
+				args["name"] = self.name
+				args["child_docname"] = item.name
 
-			# party_name field used for customer in quotation
-			if self.doctype == "Quotation" and self.quotation_to == "Customer" and parent_dict.get("party_name"):
-				parent_dict.update({"customer": parent_dict.get("party_name")})
+				if not args.get("transaction_date"):
+					args["transaction_date"] = args.get("posting_date")
+
+				return get_item_details(args, self, for_validate=True, overwrite_warehouse=False)
 
 			self.pricing_rules = []
-			self.free_items = []
+			self.free_items = [] # temporary variable
+
 			for item in self.get("items"):
 				if item.get("item_code"):
-					args = parent_dict.copy()
-					args.update(item.as_dict())
+					item_details = get_args_for_item(item)
+					self.set_item_details(item, item_details)
 
-					args["doctype"] = self.doctype
-					args["name"] = self.name
-					args["child_docname"] = item.name
-					args["ignore_pricing_rule"] = self.ignore_pricing_rule if hasattr(self, 'ignore_pricing_rule') else 0
+					if self.doctype in ["Purchase Invoice", "Sales Invoice"] \
+						and item.meta.get_field('is_fixed_asset'):
+						item.set('is_fixed_asset', item_details.get('is_fixed_asset', 0))
 
-					if not args.get("transaction_date"):
-						args["transaction_date"] = args.get("posting_date")
-
-					if self.get("is_subcontracted"):
-						args["is_subcontracted"] = self.is_subcontracted
-
-					ret = get_item_details(args, self, for_validate=True, overwrite_warehouse=False)
-
-					for fieldname, value in ret.items():
-						if item.meta.get_field(fieldname) and value is not None:
-							if (item.get(fieldname) is None or fieldname in force_item_fields):
-								item.set(fieldname, value)
-
-							elif fieldname in ['cost_center', 'conversion_factor'] and not item.get(fieldname):
-								item.set(fieldname, value)
-
-							elif fieldname == "serial_no":
-								# Ensure that serial numbers are matched against Stock UOM
-								item_conversion_factor = item.get("conversion_factor") or 1.0
-								item_qty = abs(item.get("qty")) * item_conversion_factor
-
-								if item_qty != len(get_serial_nos(item.get('serial_no'))):
-									item.set(fieldname, value)
-
-					if self.doctype in ["Purchase Invoice", "Sales Invoice"] and item.meta.get_field('is_fixed_asset'):
-						item.set('is_fixed_asset', ret.get('is_fixed_asset', 0))
-
-					# Double check for cost center
-					# Items add via promotional scheme may not have cost center set
-					if hasattr(item, 'cost_center') and not item.get('cost_center'):
-						item.set('cost_center', self.get('cost_center') or erpnext.get_default_cost_center(self.company))
-
-					if ret.get("pricing_rules"):
-						self.apply_pricing_rule_on_items(item, ret)
-						self.set_pricing_rule_details(item, ret)
+					if item_details.get("pricing_rules"):
+						self.apply_pricing_rule_on_items(item, item_details)
+						self.set_pricing_rule_details(item, item_details)
 
 			if self.doctype == "Purchase Invoice":
 				self.set_expense_account(for_validate)
 
 			if self.free_items:
+				# add all free items together
+				# and remove existing invalid free items
 				apply_pricing_rule_for_free_items(self, self.free_items)
+				for item in self.get('items'):
+					if item.get('is_free_item'):
+						item_details = get_args_for_item(item)
+						self.set_item_details(item, item_details)
+
+	def set_item_details(self, item, item_details):
+		from erpnext.stock.doctype.serial_no.serial_no import get_serial_nos
+
+		for fieldname, value in item_details.items():
+			if item.meta.get_field(fieldname) and value is not None:
+				if (item.get(fieldname) is None or fieldname in force_item_fields):
+					item.set(fieldname, value)
+
+				elif fieldname in ['cost_center', 'conversion_factor'] and not item.get(fieldname):
+					item.set(fieldname, value)
+
+				elif fieldname == "serial_no":
+					# Ensure that serial numbers are matched against Stock UOM
+					item_conversion_factor = item.get("conversion_factor") or 1.0
+					item_qty = abs(item.get("qty")) * item_conversion_factor
+
+					if item_qty != len(get_serial_nos(item.get('serial_no'))):
+						item.set(fieldname, value)
+
+		# Double check for cost center
+		# Items add via promotional scheme may not have cost center set
+		if hasattr(item, 'cost_center') and not item.get('cost_center'):
+			item.set('cost_center', self.get('cost_center') or erpnext.get_default_cost_center(self.company))
 
 	def apply_pricing_rule_on_items(self, item, pricing_rule_args):
 		if not pricing_rule_args.get("validate_applied_rule", 0):
