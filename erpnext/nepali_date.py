1bf3f231f3f797072653b1952ffb5ee963031d2f
ujjwalkumar93 <pathakujjwal93@gmail.com>
2021-09-09 16:01:54 +0530

    code added to handle nepali date aswell

--- erpnext/nepali_date.py
+++ erpnext/nepali_date.py
@@ -2,9 +2,21 @@ import frappe
 import datetime
 import nepali_datetime
 from datetime import timedelta
+import nepali_datetime
 
 @frappe.whitelist()
 def get_converted_date(date):
-    date = date.split("-")
-    date_to_convert = datetime.date(int(date[0]), int(date[1]),int(date[2]))
-    return nepali_datetime.date.from_datetime_date(date_to_convert).strftime("%D-%n-%K")
+    if len(date) <= 10:
+        date = date[0:10]
+        date = date.split("-")
+        date_to_convert = datetime.date(int(date[0]), int(date[1]),int(date[2]))
+        return nepali_datetime.date.from_datetime_date(date_to_convert).strftime("%D-%n-%K")
+    else: 
+        seperated_time_date = date.split(" ")
+        date = seperated_time_date[0].split("-")
+        time = seperated_time_date[1].split(":")
+        gorgian_time = datetime.datetime(int(date[0]), int(date[1]), int(date[2]),int(time[0]),int(time[1]))
+        nepali_time = gorgian_time+timedelta(hours=.25)
+        date = date[0:10]
+        date_to_convert = datetime.date(int(date[0]), int(date[1]),int(date[2]))
+        return nepali_datetime.date.from_datetime_date(date_to_convert).strftime("%D-%n-%K")+ " "+ str(nepali_time)
