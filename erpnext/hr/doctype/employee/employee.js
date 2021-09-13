71d009292bf10f2008c68ae3e8e784dbccb28fab
Prajkta <prajktavibhute26@gmail.com>
2021-09-13 12:48:38 +0530

    custom fields added for nepali date

--- erpnext/hr/doctype/employee/employee.js
+++ erpnext/hr/doctype/employee/employee.js
@@ -3,31 +3,170 @@
 
 frappe.provide("erpnext.hr");
 erpnext.hr.EmployeeController = frappe.ui.form.Controller.extend({
-	setup: function() {
-		this.frm.fields_dict.user_id.get_query = function(doc, cdt, cdn) {
+	setup: function () {
+		this.frm.fields_dict.user_id.get_query = function (doc, cdt, cdn) {
 			return {
 				query: "frappe.core.doctype.user.user.user_query",
-				filters: {ignore_user_type: 1}
+				filters: { ignore_user_type: 1 }
 			}
 		}
-		this.frm.fields_dict.reports_to.get_query = function(doc, cdt, cdn) {
-			return { query: "erpnext.controllers.queries.employee_query"} }
+		this.frm.fields_dict.reports_to.get_query = function (doc, cdt, cdn) {
+			return { query: "erpnext.controllers.queries.employee_query" }
+		}
 	},
 
-	refresh: function() {
+	refresh: function () {
 		var me = this;
 		erpnext.toggle_naming_series();
 	},
 
-	date_of_birth: function() {
+	date_of_birth: function () {
 		return cur_frm.call({
 			method: "get_retirement_date",
-			args: {date_of_birth: this.frm.doc.date_of_birth}
+			args: { date_of_birth: this.frm.doc.date_of_birth }
 		});
+
+	},
+	date_of_birth: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.date_of_birth
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("date_of_birth_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+	date_of_joining: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.date_of_joining
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("date_of_joining_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+	scheduled_confirmation_date: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.scheduled_confirmation_date
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("offer_date_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+	date_of_retirement: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.date_of_retirement
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("date_of_retirement_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+	date_of_issue: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.date_of_issue
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("date_of_issue_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+	date_of_issue: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.date_of_issue
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("date_of_issue_nepal", resp.message)
+				}
+			}
+		})
+
+
 	},
 
-	salutation: function() {
-		if(this.frm.doc.salutation) {
+	valid_upto: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.valid_upto
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("valid_upto_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+	resignation_letter_date: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.resignation_letter_date
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("resignation_letter_date_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+	relieving_date: function (frm) {
+		frappe.call({
+			method: "erpnext.nepali_date.get_converted_date",
+			args: {
+				date: frm.relieving_date
+			},
+			callback: function (resp) {
+				if (resp.message) {
+					cur_frm.set_value("relieving_date_nepal", resp.message)
+				}
+			}
+		})
+
+
+	},
+
+
+	salutation: function () {
+		if (this.frm.doc.salutation) {
 			this.frm.set_value("gender", {
 				"Mr": "Male",
 				"Ms": "Female"
@@ -36,9 +175,9 @@ erpnext.hr.EmployeeController = frappe.ui.form.Controller.extend({
 	},
 
 });
-frappe.ui.form.on('Employee',{
-	setup: function(frm) {
-		frm.set_query("leave_policy", function() {
+frappe.ui.form.on('Employee', {
+	setup: function (frm) {
+		frm.set_query("leave_policy", function () {
 			return {
 				"filters": {
 					"docstatus": 1
@@ -46,8 +185,8 @@ frappe.ui.form.on('Employee',{
 			};
 		});
 	},
-	onload:function(frm) {
-		frm.set_query("department", function() {
+	onload: function (frm) {
+		frm.set_query("department", function () {
 			return {
 				"filters": {
 					"company": frm.doc.company,
@@ -55,24 +194,24 @@ frappe.ui.form.on('Employee',{
 			};
 		});
 	},
-	prefered_contact_email:function(frm){
+	prefered_contact_email: function (frm) {
 		frm.events.update_contact(frm)
 	},
-	personal_email:function(frm){
+	personal_email: function (frm) {
 		frm.events.update_contact(frm)
 	},
-	company_email:function(frm){
+	company_email: function (frm) {
 		frm.events.update_contact(frm)
 	},
-	user_id:function(frm){
+	user_id: function (frm) {
 		frm.events.update_contact(frm)
 	},
-	update_contact:function(frm){
+	update_contact: function (frm) {
 		var prefered_email_fieldname = frappe.model.scrub(frm.doc.prefered_contact_email) || 'user_id';
 		frm.set_value("prefered_email",
 			frm.fields_dict[prefered_email_fieldname].value)
 	},
-	status: function(frm) {
+	status: function (frm) {
 		return frm.call({
 			method: "deactivate_sales_person",
 			args: {
@@ -81,19 +220,17 @@ frappe.ui.form.on('Employee',{
 			}
 		});
 	},
-	create_user: function(frm) {
-		if (!frm.doc.prefered_email)
-		{
+	create_user: function (frm) {
+		if (!frm.doc.prefered_email) {
 			frappe.throw(__("Please enter Preferred Contact Email"))
 		}
 		frappe.call({
 			method: "erpnext.hr.doctype.employee.employee.create_user",
 			args: { employee: frm.doc.name, email: frm.doc.prefered_email },
-			callback: function(r)
-			{
+			callback: function (r) {
 				frm.set_value("user_id", r.message)
 			}
 		});
 	}
 });
-cur_frm.cscript = new erpnext.hr.EmployeeController({frm: cur_frm});
+cur_frm.cscript = new erpnext.hr.EmployeeController({ frm: cur_frm });
