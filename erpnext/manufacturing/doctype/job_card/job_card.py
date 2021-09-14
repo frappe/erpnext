469bb8d977f135532a4d4afe4352ece58b2de4b4
marination <maricadsouza221197@gmail.com>
2021-09-14 16:27:39 +0530

    test: Work Order Material Transfer against Job Card
    - Tests for multiple items transfer and excess transfer against JC
    - Remove unused __future__ imports
    - Changed Copyright year
    - Sider: (js) Added space before if

--- erpnext/manufacturing/doctype/job_card/job_card.py
+++ erpnext/manufacturing/doctype/job_card/job_card.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
-
-from __future__ import unicode_literals
-
 import datetime
 import json
 
