3de3e4fa5a9aba86cbb42e6969d077b81f8db9ad
Ankush Menat <ankush@iwebnotes.com>
2021-09-17 17:20:10 +0530

    chore: whitepsace

--- erpnext/patches/v13_0/shopify_deprecation_warning.py
+++ erpnext/patches/v13_0/shopify_deprecation_warning.py
@@ -3,7 +3,7 @@ import frappe
 
 
 def execute():
-	
+
 	frappe.reload_doc("erpnext_integrations", "doctype", "shopify_settings")
 	if not frappe.db.get_single_value("Shopify Settings", "enable_shopify"):
 		return
