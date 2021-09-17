ff7278a1e2b71b86c9891904c08d1a38d4744718
18alantom <2.alan.tom@gmail.com>
2021-09-17 16:03:36 +0530

    fix: add check for wo completion

--- erpnext/patches/v13_0/update_production_plan_status.py
+++ erpnext/patches/v13_0/update_production_plan_status.py
@@ -6,7 +6,25 @@ import frappe
 
 def execute():
 	frappe.db.sql("""
-		UPDATE `tabProduction Plan`
-		SET status="Completed"
-		WHERE total_produced_qty >= total_planned_qty
+		UPDATE `tabProduction Plan` ppl
+		SET status = "Completed"
+		WHERE ppl.name IN (
+			SELECT ss.name FROM (
+				SELECT
+					(
+						count(wo.status = "Completed") =
+						count(pp.name)
+					) =
+					(
+						pp.status != "Completed"
+						AND pp.total_produced_qty >= pp.total_planned_qty
+					) AS should_set,
+					pp.name AS name
+				FROM
+					`tabWork Order` wo INNER JOIN`tabProduction Plan` pp
+					ON wo.production_plan = pp.name
+				GROUP BY pp.name
+				HAVING should_set = 1
+			) ss
+		)
 	""")
