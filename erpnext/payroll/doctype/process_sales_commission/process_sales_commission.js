9da6d21e75828a957c807247b5baa47dab51dc69
Afshan <afshan13k@gmail.com>
2021-09-21 11:09:07 +0530

    fix: linting issues

--- erpnext/payroll/doctype/process_sales_commission/process_sales_commission.js
+++ erpnext/payroll/doctype/process_sales_commission/process_sales_commission.js
@@ -2,16 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Process Sales Commission', {
-	setup: function(frm){
+	setup: function(frm) {
 		frm.set_query("department", function() {
 			if (!frm.doc.company) {
-				frappe.throw(__("Please select company first"))
+				frappe.throw(__("Please select company first"));
 			}
 			return {
 				filters: {
 					company: frm.doc.company
 				}
-			}
+			};
 		});
 	},
 });
