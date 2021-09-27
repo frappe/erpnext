7a8ff85308aad1178cbad6abbd9ff4a7dfef3779
Saqib <nextchamp.saqib@gmail.com>
2021-09-27 12:20:16 +0530

    chore: log modified invoices
    (cherry picked from commit 30f59b09fdc1c44999d996407f4c2a283270a4ab)

--- erpnext/patches/v13_0/modify_invalid_gain_loss_gl_entries.py
+++ erpnext/patches/v13_0/modify_invalid_gain_loss_gl_entries.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import json
 import frappe
 
 
@@ -32,6 +33,9 @@ def execute():
 		group by
 			parent
 	""", as_dict=1)
+
+if purchase_invoices + sales_invoices:
+	frappe.log_error(json.dumps(purchase_invoices + sales_invoices, indent=2), title="Patch Log")
 
 	for invoice in purchase_invoices + sales_invoices:
 		doc = frappe.get_doc(invoice.type, invoice.name)
