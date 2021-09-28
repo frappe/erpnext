ce88868325a325531ad20361dc0abefa400dc5f4
Ankush Menat <ankush@iwebnotes.com>
2021-09-28 21:00:00 +0530

    debug

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -8,7 +8,7 @@ sudo apt-get install redis-server libcups2-dev
 
 pip install frappe-bench
 
-git clone https://github.com/frappe/frappe --branch "${GITHUB_BASE_REF:-${GITHUB_REF##*/}}" --depth 1
+git clone https://github.com/ankush/frappe --branch db_wat --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
 
 mkdir ~/frappe-bench/sites/test_site
