b211ab3d41e07b0f9da2d217e12a90f552231519
asoral <soral.apoorv@gmail.com>
2021-09-30 10:44:08 +0530

    line no- 102 added source_warehouse
    line no- 102 added source_warehouse instead of rm_store_warehouse

--- erpnext/manufacturing/doctype/additional_item/additional_item.py
+++ erpnext/manufacturing/doctype/additional_item/additional_item.py
@@ -99,7 +99,7 @@ class AdditionalItem(Document):
 
 @frappe.whitelist()
 def get_item_data(item,wo):
-	wo_source_warehouse = frappe.db.get_value("Work Order", {'name':wo}, ['rm_store_warehouse'])
+	wo_source_warehouse = frappe.db.get_value("Work Order", {'name':wo}, ['source_warehouse'])
 	item_stock = frappe.db.get_value("Bin", {"warehouse": wo_source_warehouse,"item_code":item},['actual_qty'])
 	q = """select item_name,weight_uom,weight_per_unit from `tabItem` where item_code='{0}'""".format(item)
 
