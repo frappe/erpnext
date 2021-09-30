af57e1e299651b2b15a53086c174c0ad01208889
marination <maricadsouza221197@gmail.com>
2021-09-30 14:53:47 +0530

    fix: reload doc in patch
    (cherry picked from commit 6b38778dcb0bba87388c17cea6c440ce6f148846)

--- erpnext/patches/v13_0/set_status_in_maintenance_schedule_table.py
+++ erpnext/patches/v13_0/set_status_in_maintenance_schedule_table.py
@@ -2,6 +2,7 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("maintenance", "doctype", "Maintenance Schedule Detail")
 	frappe.db.sql("""
 		UPDATE `tabMaintenance Schedule Detail`
 		SET completion_status = 'Pending'
