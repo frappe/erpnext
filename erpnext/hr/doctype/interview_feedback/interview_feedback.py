52800f8d2dcaf977c9dccb45c5ead24af98d5a60
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-09-30 22:10:16 +0530

    fix: linter issues

--- erpnext/hr/doctype/interview_feedback/interview_feedback.py
+++ erpnext/hr/doctype/interview_feedback/interview_feedback.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import flt, getdate, get_link_to_form
+from frappe.utils import flt, get_link_to_form, getdate
 
 
 class InterviewFeedback(Document):
