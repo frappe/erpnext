92acc0f227e3691622cf04db7feea3c8c3d0a8bb
zaqoutabed <zaqoutabed@gmail.com>
2021-10-05 15:47:18 +0300

    check leave type and make sure there us no duplicatd

--- erpnext/hr/doctype/leave_policy/leave_policy.py
+++ erpnext/hr/doctype/leave_policy/leave_policy.py
@@ -11,8 +11,18 @@ from frappe.model.document import Document
 
 class LeavePolicy(Document):
 	def validate(self):
+		self.validate_leave_type()
+		
 		if self.leave_policy_details:
 			for lp_detail in self.leave_policy_details:
+				if lp_detail.leave_type in leaves:
+					frappe.throw(_("Leave Policy {} is selected in row {}".format(lp_detail.leave_type)))
 				max_leaves_allowed = frappe.db.get_value("Leave Type", lp_detail.leave_type, "max_leaves_allowed")
 				if max_leaves_allowed > 0 and lp_detail.annual_allocation > max_leaves_allowed:
 					frappe.throw(_("Maximum leave allowed in the leave type {0} is {1}").format(lp_detail.leave_type, max_leaves_allowed))
+	
+	def validate_leave_type(self):
+		leaves = {row.leave_type for row in self.leave_policy_details}
+
+		if len(leaves) != len(self.leave_policy_details):
+			frappe.throw(_("Cannot set multiple Leave Type for a leave policy."))
