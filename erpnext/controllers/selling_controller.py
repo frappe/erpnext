2cfafede4408c943d003a04985cadb869f811c21
Ankush Menat <ankush@iwebnotes.com>
2021-10-05 14:27:09 +0530

    fix(ux): use toast instead of popup

--- erpnext/controllers/selling_controller.py
+++ erpnext/controllers/selling_controller.py
@@ -560,9 +560,9 @@ class SellingController(StockController):
 					.format(d.idx, warehouse, warehouse))
 
 		if not self.get("is_internal_customer") and any(d.get("target_warehouse") for d in items):
-			msg = _("Target Warehouse set for some items but the customer is not an internal customer.")
+			msg = _("Target Warehouse is set for some items but the customer is not an internal customer.")
 			msg += " " + _("This {} will be treated as material transfer.").format(_(self.doctype))
-			frappe.msgprint(msg, title="Internal Transfer")
+			frappe.msgprint(msg, title="Internal Transfer", alert=True)
 
 
 	def validate_items(self):
