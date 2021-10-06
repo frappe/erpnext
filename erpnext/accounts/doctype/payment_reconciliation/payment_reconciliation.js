fe40e37a7b2218c173ef631b57b3303807ea8d2b
Anuja Pawar <anuja.pawar20@gmail.com>
2021-10-06 13:10:33 +0530

    fix: removed Add row button from the tables (redundant)

--- erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.js
+++ erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.js
@@ -56,6 +56,10 @@ erpnext.accounts.PaymentReconciliationController = class PaymentReconciliationCo
 		this.frm.set_df_property('payments', 'cannot_delete_rows', true);
 		this.frm.set_df_property('allocation', 'cannot_delete_rows', true);
 
+		this.frm.set_df_property('invoices', 'cannot_add_rows', true);
+		this.frm.set_df_property('payments', 'cannot_add_rows', true);
+		this.frm.set_df_property('allocation', 'cannot_add_rows', true);
+
 
 		if (this.frm.doc.receivable_payable_account) {
 			this.frm.add_custom_button(__('Get Unreconciled Entries'), () =>
