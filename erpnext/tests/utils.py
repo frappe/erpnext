2674fbde8667f47037e4e7f0000eb2e94037a3b5
Ankush Menat <ankush@iwebnotes.com>
2021-10-08 13:24:20 +0530

    test: check for missing index after patch test

--- erpnext/tests/utils.py
+++ erpnext/tests/utils.py
@@ -2,11 +2,13 @@
 # License: GNU General Public License v3. See license.txt
 
 import copy
+import sys
 from contextlib import contextmanager
 from typing import Any, Dict, NewType, Optional
 
 import frappe
 from frappe.core.doctype.report.report import get_report_module_dotted_path
+from frappe.modules import load_doctype_module
 
 ReportFilters = Dict[str, Any]
 ReportName = NewType("ReportName", str)
@@ -119,3 +121,42 @@ def execute_script_report(
 			report_execute_fn(filter_with_optional_param)
 
 	return report_data
+
+
+def find_missing_indexes():
+
+	doctypes_to_check = [
+		"Item",
+		"Stock Ledger Entry",
+		"GL Entry",
+		"Material Request Item",
+		"Packed Item",
+		"Bin",
+		"Account",
+		"Purchase Invoice",
+		"Sales Invoice",
+		"Work Order Item",
+	]
+	missing_indexes = []
+
+	for doctype in doctypes_to_check:
+		index_before = frappe.db.sql(f"show index from `tab{doctype}`", as_dict=1)
+
+		doctype_module = load_doctype_module(doctype)
+		getattr(doctype_module, "on_doctype_update")()
+		index_after = frappe.db.sql(f"show index from `tab{doctype}`", as_dict=1)
+
+		missing_indexes.extend(
+			[f"{doctype}.{index}" for index in _compare_indexes(index_before, index_after)]
+		)
+
+	if missing_indexes:
+		print("Expected following indexes to exist")
+		print("\n".join(missing_indexes))
+		sys.exit(1)
+
+
+def _compare_indexes(before, after):
+	key_names_before = {i["Key_name"] for i in before}
+	key_names_after = {i["Key_name"] for i in after}
+	return key_names_after - key_names_before
