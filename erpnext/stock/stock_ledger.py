a4a94670fb00c379028d90b5d44e3303280cc371
Ankush Menat <ankushmenat@gmail.com>
2021-10-12 20:39:10 +0530

    fix: undo changes to allow negative stock flag
    (cherry picked from commit 7bafa11d5758c40496898cd0151efde4ff358946)

--- erpnext/stock/stock_ledger.py
+++ erpnext/stock/stock_ledger.py
@@ -231,7 +231,8 @@ class update_entries_after(object):
 		self.verbose = verbose
 		self.allow_zero_rate = allow_zero_rate
 		self.via_landed_cost_voucher = via_landed_cost_voucher
-		self.allow_negative_stock = True
+		self.allow_negative_stock = allow_negative_stock \
+			or cint(frappe.db.get_single_value("Stock Settings", "allow_negative_stock"))
 
 		self.args = frappe._dict(args)
 		self.item_code = args.get("item_code")
