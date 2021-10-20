b906cc20ae9c0fdf538772456c30fcfbc101e73f
marination <maricadsouza221197@gmail.com>
2021-10-20 14:08:14 +0530

    fix: Move thumbnail updation to different patch
    - Thumbnail updation handled via different patch
    - create_website_items will only have one purpose
    - added progress bar to `create_website_items`
    - code cleanup
    
    (cherry picked from commit 348a961b5374a692cd82950f626f62df8fdc147f)

--- erpnext/patches/v13_0/create_website_items.py
+++ erpnext/patches/v13_0/create_website_items.py
@@ -43,51 +43,30 @@ def execute():
 		fields=item_fields,
 		or_filters=or_filters
 	)
+	total_count = len(items)
 
-	count = 0
-	for item in items:
-		web_item_exists = frappe.db.exists("Website Item", {"item_code": item.item_code})
-		thumbnail_column_exists = "thumbnail" in item_table_fields
+	for count, item in enumerate(items, start=1):
+		if frappe.db.exists("Website Item", {"item_code": item.item_code}):
+			continue
 
-		if web_item_exists and thumbnail_column_exists:
-			# if website item already exists check for empty thumbnail
-			# if empty, fetch thumbnail from Item master
-			web_item_doc = frappe.db.get_values(
-				"Website Item",
-				filters={
-					"item_code": item.item_code
-				},
-				fieldname=["website_image", "thumbnail", "name"],
-				as_dict=True
-			)[0]
+		# make new website item from item (publish item)
+		website_item = make_website_item(item, save=False)
+		website_item.ranking = item.get("weightage")
 
-			if web_item_doc.get("website_image") and not web_item_doc.get("thumbnail"):
-				thumbnail = frappe.db.get_value("Item", item.item_code, "thumbnail")
-				frappe.db.set_value("Website Item", web_item_doc.name, "thumbnail", thumbnail)
-		else:
-			# else make new website item from item (publish item)
-			website_item = make_website_item(item, save=False)
-			website_item.ranking = item.get("weightage")
+		for field in web_fields_to_map:
+			website_item.update({field: item.get(field)})
 
-			for field in web_fields_to_map:
-				website_item.update({field: item.get(field)})
+		website_item.save()
 
-			website_item.save()
+		# move Website Item Group & Website Specification table to Website Item
+		for doctype in ("Website Item Group", "Item Website Specification"):
+			frappe.db.set_value(
+				doctype,
+				{"parenttype": "Item", "parent": item.item_code}, # filters
+				{"parenttype": "Website Item", "parent": website_item.name} # value dict
+			)
 
-			# move Website Item Group & Website Specification table to Website Item
-			for doctype in ("Website Item Group", "Item Website Specification"):
-				web_item, item_code = website_item.name, item.item_code
-				frappe.db.sql(f"""
-					Update
-						`tab{doctype}`
-					set
-						parenttype = 'Website Item',
-						parent = '{web_item}'
-					where
-						parenttype = 'Item'
-						and parent = '{item_code}'
-					""")
-
-		count += 1
 		if count % 20 == 0: # commit after every 20 items
 			frappe.db.commit()
+
+		frappe.utils.update_progress_bar('Creating Website Items', count, total_count)
