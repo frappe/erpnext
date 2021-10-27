7a80271e6674faabe1e6e8b439fc928bd7c23ee4
Afshan <afshan13k@gmail.com>
2021-10-27 16:33:19 +0530

    fix: remove commission rate from list view

--- erpnext/payroll/doctype/sales_commission/sales_commission_list.js
+++ erpnext/payroll/doctype/sales_commission/sales_commission_list.js
@@ -1,4 +1,5 @@
 frappe.listview_settings['Sales Commission'] = {
+    add_fields: ["total_commission_amount", "status"],
     get_indicator: function (doc) {
 		if (doc.status == "Paid") {
 			return [__(doc.status), "green", "status,=," + doc.status];
