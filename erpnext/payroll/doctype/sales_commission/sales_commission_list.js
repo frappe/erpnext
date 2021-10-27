e43cfdf460e7cde01ca2bd0e1ac21e36c06cc018
Afshan <afshan13k@gmail.com>
2021-10-27 17:24:30 +0530

    fix: linting issues

--- erpnext/payroll/doctype/sales_commission/sales_commission_list.js
+++ erpnext/payroll/doctype/sales_commission/sales_commission_list.js
@@ -1,10 +1,10 @@
 frappe.listview_settings['Sales Commission'] = {
-    add_fields: ["total_commission_amount", "status"],
-    get_indicator: function (doc) {
+	add_fields: ["total_commission_amount", "status"],
+	get_indicator: function (doc) {
 		if (doc.status == "Paid") {
 			return [__(doc.status), "green", "status,=," + doc.status];
 		}  else {
 			return [__(doc.status), "red", "status,=," + doc.status];
 		}
 	}
-}
+};
