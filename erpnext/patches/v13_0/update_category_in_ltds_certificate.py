09a5616e2dce7d0d9962326781cd7c0a4bcaebbc
Deepesh Garg <deepeshgarg6@gmail.com>
2021-10-28 19:01:24 +0530

    fix: Rewrite patch using query builder

--- erpnext/patches/v13_0/update_category_in_ltds_certificate.py
+++ erpnext/patches/v13_0/update_category_in_ltds_certificate.py
@@ -2,9 +2,17 @@ import frappe
 
 
 def execute():
+	company = frappe.get_all('Company', filters = {'country': 'India'})
+	if not company:
+		return
 
-	frappe.db.sql("""
-		UPDATE `tabLower Deduction Certificate` l, `tabSupplier` s
-		SET l.tax_withholding_category = s.tax_withholding_category
-		WHERE l.supplier = s.name
-	""")
+	ldc = frappe.qb.DocType("Lower Deduction Certificate").as_("ldc")
+	supplier = frappe.qb.DocType("Supplier")
+
+	frappe.qb.update(ldc).inner_join(supplier).on(
+		ldc.supplier == supplier.name
+	).set(
+		ldc.tax_withholding_category, supplier.tax_withholding_category
+	).where(
+		ldc.tax_withholding_category.isnull()
+	).run()
