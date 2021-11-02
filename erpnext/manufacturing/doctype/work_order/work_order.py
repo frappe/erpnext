7c7493c91d7afb3d626cdc39680e52721e5e3ae2
Anupam <anupamvns0099@gmail.com>
2021-11-02 12:46:00 +0530

    fix: linter issues
    (cherry picked from commit 59e4fd980c1c411f82df1f588bc2c80e17d8e317)

--- erpnext/manufacturing/doctype/work_order/work_order.py
+++ erpnext/manufacturing/doctype/work_order/work_order.py
@@ -1011,6 +1011,7 @@ def close_work_order(work_order, status):
 
 	work_order = frappe.get_doc("Work Order", work_order)
 	if work_order.get("operations"):
+
 		job_cards = frappe.get_list("Job Card", 
 			filters={
 				"work_order": work_order.name,
