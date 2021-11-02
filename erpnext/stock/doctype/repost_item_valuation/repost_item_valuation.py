149407351d6145e1b06f2ad30f836d59af18beb8
Ankush Menat <ankush@frappe.io>
2021-11-02 11:08:36 +0530

    perf: index for item-sh on repost item valuation
    Item-WH based reposting requires querying existing similar repost.
    Assuming there is only 1 max extra entry with same params just indexing
    item-WH is sufficient to speed up the query.
    
    (cherry picked from commit a5a8c9104fbb5279e6b25c529b6b4c0fc7ccef97)

--- erpnext/stock/doctype/repost_item_valuation/repost_item_valuation.py
+++ erpnext/stock/doctype/repost_item_valuation/repost_item_valuation.py
@@ -94,6 +94,11 @@ def on_doctype_update():
 	frappe.db.add_index("Repost Item Valuation", ["warehouse", "item_code"], "item_warehouse")
 
 
+
+def on_doctype_update():
+	frappe.db.add_index("Repost Item Valuation", ["warehouse", "item_code"], "item_warehouse")
+
+
 def repost(doc):
 	try:
 		if not frappe.db.exists("Repost Item Valuation", doc.name):
