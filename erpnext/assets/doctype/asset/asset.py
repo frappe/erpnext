6098e92ba94a4dc30e7b9dd7df2f68b4d84b7207
Ankush Menat <ankush@frappe.io>
2021-11-04 18:43:44 +0530

    chore: remove utf-8 compat code

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
 
 import json
 import math
--- erpnext/regional/india/__init__.py
+++ erpnext/regional/india/__init__.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals
 
 from six import iteritems
 
