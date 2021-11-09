e65a7208104b22f627b69cf954fbc7a9ca3bf839
Subin Tom <subintom2@gmail.com>
2021-11-09 14:46:45 +0530

    fix: Shipping Rule picking up old net_rate
    (cherry picked from commit c78b8b7897152ea5daba9bfa0005a294a40c5670)
    
    # Conflicts:
    #	erpnext/public/js/controllers/taxes_and_totals.js
    #	erpnext/public/js/controllers/transaction.js

--- erpnext/public/js/controllers/taxes_and_totals.js
+++ erpnext/public/js/controllers/taxes_and_totals.js
@@ -266,7 +266,14 @@ erpnext.taxes_and_totals = erpnext.payments.extend({
 			});
 
 		frappe.model.round_floats_in(this.frm.doc, ["total", "base_total", "net_total", "base_net_total"]);
+<<<<<<< HEAD
 	},
+=======
+		if(frappe.meta.get_docfield(this.frm.doc.doctype,"shipping_rule",this.frm.doc.name)) {
+			this.shipping_rule()
+		}
+	}
+>>>>>>> c78b8b7897 (fix: Shipping Rule picking up old net_rate)
 
 	add_taxes_from_item_tax_template: function(item_tax_map) {
 		let me = this;
