bf212da25a8181c5d965adafcaf74df08e71701d
Subin Tom <subintom2@gmail.com>
2021-11-09 14:46:45 +0530

    fix: Shipping Rule picking up old net_rate
    (cherry picked from commit c78b8b7897152ea5daba9bfa0005a294a40c5670)
    
    # Conflicts:
    #	erpnext/public/js/controllers/taxes_and_totals.js
    #	erpnext/public/js/controllers/transaction.js

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -1065,17 +1065,16 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 			return this.frm.call({
 				doc: this.frm.doc,
 				method: "apply_shipping_rule",
-				callback: function(r) {
-					if(!r.exc) {
-						me.calculate_taxes_and_totals();
-					}
-				}
 			}).fail(() => this.frm.set_value('shipping_rule', ''));
 		}
+<<<<<<< HEAD
 		else {
 			me.calculate_taxes_and_totals();
 		}
 	},
+=======
+	}
+>>>>>>> c78b8b7897 (fix: Shipping Rule picking up old net_rate)
 
 	set_margin_amount_based_on_currency: function(exchange_rate) {
 		if (in_list(["Quotation", "Sales Order", "Delivery Note", "Sales Invoice", "Purchase Invoice", "Purchase Order", "Purchase Receipt"]), this.frm.doc.doctype) {
