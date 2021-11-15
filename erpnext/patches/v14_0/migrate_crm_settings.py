2e13456cb20287c3d53c24f997e48c89904e6ead
Anupam <anupamvns0099@gmail.com>
2021-11-15 15:53:37 +0530

    added patch

--- erpnext/patches/v14_0/migrate_crm_settings.py
+++ erpnext/patches/v14_0/migrate_crm_settings.py
@@ -2,7 +2,26 @@ import frappe
 
 
 def execute():
-    pass
-    # close_opportunity_after_days = 
-    # campaign_naming_by = 
-    # frappe.reload_doc('crm', 'doctype', 'crm settings')
+	frappe.db.sql("""UPDATE `tabSingles` as t1 
+		INNER JOIN `tabSingles` as t2 ON 
+			t1.doctype = 'Selling Settings' AND 
+			t1.field = 'campaign_naming_by' AND 
+			t2.doctype = 'CRM Settings' AND 
+			t2.field = 'campaign_naming_by'
+		SET t2.value = t1.value""")
+
+	frappe.db.sql("""UPDATE `tabSingles` as t1 
+		INNER JOIN `tabSingles` as t2 ON 
+			t1.doctype = 'Selling Settings' AND 
+			t1.field = 'close_opportunity_after_days' AND 
+			t2.doctype = 'CRM Settings' AND 
+			t2.field = 'close_opportunity_after_days'
+		SET t2.value = t1.value""")
+
+	frappe.db.sql("""UPDATE `tabSingles` as t1 
+		INNER JOIN `tabSingles` as t2 ON 
+			t1.doctype = 'Selling Settings' AND 
+			t1.field = 'default_valid_till' AND 
+			t2.doctype = 'CRM Settings' AND 
+			t2.field = 'default_valid_till'
+		SET t2.value = t1.value""")
