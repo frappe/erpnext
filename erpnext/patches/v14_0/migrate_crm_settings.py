66efd92c7cec3e499da94a53e666b3b1d74c85e9
Anupam <anupamvns0099@gmail.com>
2021-11-15 16:16:14 +0530

    fix: linter issues

--- erpnext/patches/v14_0/migrate_crm_settings.py
+++ erpnext/patches/v14_0/migrate_crm_settings.py
@@ -2,26 +2,26 @@ import frappe
 
 
 def execute():
-	frappe.db.sql("""UPDATE `tabSingles` as t1 
-		INNER JOIN `tabSingles` as t2 ON 
-			t1.doctype = 'Selling Settings' AND 
+	frappe.db.sql("""UPDATE `tabSingles` as t1
+		INNER JOIN `tabSingles` as t2 ON
+			t1.doctype = 'Selling Settings' AND
 			t1.field = 'campaign_naming_by' AND 
-			t2.doctype = 'CRM Settings' AND 
+			t2.doctype = 'CRM Settings' AND
 			t2.field = 'campaign_naming_by'
 		SET t2.value = t1.value""")
 
-	frappe.db.sql("""UPDATE `tabSingles` as t1 
-		INNER JOIN `tabSingles` as t2 ON 
-			t1.doctype = 'Selling Settings' AND 
-			t1.field = 'close_opportunity_after_days' AND 
-			t2.doctype = 'CRM Settings' AND 
+	frappe.db.sql("""UPDATE `tabSingles` as t1
+		INNER JOIN `tabSingles` as t2 ON
+			t1.doctype = 'Selling Settings' AND
+			t1.field = 'close_opportunity_after_days' AND
+			t2.doctype = 'CRM Settings' AND
 			t2.field = 'close_opportunity_after_days'
 		SET t2.value = t1.value""")
 
-	frappe.db.sql("""UPDATE `tabSingles` as t1 
-		INNER JOIN `tabSingles` as t2 ON 
-			t1.doctype = 'Selling Settings' AND 
+	frappe.db.sql("""UPDATE `tabSingles` as t1
+		INNER JOIN `tabSingles` as t2 ON
+			t1.doctype = 'Selling Settings' AND
 			t1.field = 'default_valid_till' AND 
-			t2.doctype = 'CRM Settings' AND 
+			t2.doctype = 'CRM Settings' AND
 			t2.field = 'default_valid_till'
 		SET t2.value = t1.value""")
