7356767333a4b40070bdb6d1f57612abd0b9298b
Ankush Menat <ankush@frappe.io>
2021-11-24 19:07:39 +0530

    fix: use get_all instead of get_list for child tables

--- erpnext/regional/report/vat_audit_report/vat_audit_report.py
+++ erpnext/regional/report/vat_audit_report/vat_audit_report.py
@@ -41,7 +41,7 @@ class VATAuditReport(object):
 		return self.columns, self.data
 
 	def get_sa_vat_accounts(self):
-		self.sa_vat_accounts = frappe.get_list("South Africa VAT Account",
+		self.sa_vat_accounts = frappe.get_all("South Africa VAT Account",
 			filters = {"parent": self.filters.company}, pluck="account")
 		if not self.sa_vat_accounts and not frappe.flags.in_test and not frappe.flags.in_migrate:
 			link_to_settings = get_link_to_form("South Africa VAT Settings", "", label="South Africa VAT Settings")
