2ffd0a50bbc1f0fd079b2ac00f4925908eeddf03
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2021-11-29 12:11:30 +0100

    refactor: apply suggestions from code review
    Co-authored-by: Himanshu <himanshuwarekar@yahoo.com>

--- erpnext/accounts/doctype/dunning_type/dunning_type.py
+++ erpnext/accounts/doctype/dunning_type/dunning_type.py
@@ -9,4 +9,4 @@ from frappe.model.document import Document
 class DunningType(Document):
 	def autoname(self):
 		company_abbr = frappe.get_value("Company", self.company, "abbr")
-		self.name = self.dunning_type + " - " + company_abbr
+		self.name = f"{self.dunning_type} - {company_abbr}"
