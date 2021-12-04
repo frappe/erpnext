67a001d87602cb589b5edb606c03be2d2130d594
Deepesh Garg <deepeshgarg6@gmail.com>
2021-12-04 19:19:03 +0530

    fix: Better Error logging fordeferred revenue/expense booking

--- erpnext/accounts/deferred_revenue.py
+++ erpnext/accounts/deferred_revenue.py
@@ -375,11 +375,12 @@ def make_gl_entries(doc, credit_account, debit_account, against,
 		except Exception as e:
 			if frappe.flags.in_test:
 				raise e
+				traceback = frappe.get_traceback()
+				frappe.log_error(title=_('Error while processing deferred accounting for Invoice {0}').format(doc.name), message=traceback)
 			else:
 				frappe.db.rollback()
 				traceback = frappe.get_traceback()
-				frappe.log_error(message=traceback)
-
+				frappe.log_error(title=_('Error while processing deferred accounting for Invoice {0}').format(doc.name), message=traceback)
 				frappe.flags.deferred_accounting_error = True
 
 def send_mail(deferred_process):
@@ -449,7 +450,7 @@ def book_revenue_via_journal_entry(doc, credit_account, debit_account, against,
 	except Exception:
 		frappe.db.rollback()
 		traceback = frappe.get_traceback()
-		frappe.log_error(message=traceback)
+		frappe.log_error(title=_('Error while processing deferred accounting for Invoice {0}').format(doc.name), message=traceback)
 
 		frappe.flags.deferred_accounting_error = True
 
