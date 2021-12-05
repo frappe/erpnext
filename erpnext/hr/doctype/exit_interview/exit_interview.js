7412accf6d4d6b932b24e40828a886b12111c1b4
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-12-05 17:02:19 +0530

    feat: sending Exit Questionnaire

--- erpnext/hr/doctype/exit_interview/exit_interview.js
+++ erpnext/hr/doctype/exit_interview/exit_interview.js
@@ -3,16 +3,48 @@
 
 frappe.ui.form.on('Exit Interview', {
 	refresh: function(frm) {
-
+		if (!frm.doc.__islocal && !frm.doc.questionnaire_email_sent) {
+			frm.add_custom_button(__('Send Exit Questionnaire'), function () {
+				frm.trigger('send_exit_questionnaire');
+			});
+		}
 	},
 
 	employee: function(frm) {
-		frappe.db.get_value('Employee', frm.doc.employee, 'relieving_date').then(({ relieving_date }) => {
-			if (!relieving_date) {
+		frappe.db.get_value('Employee', frm.doc.employee, 'relieving_date', (message) => {
+			if (!message.relieving_date) {
 				frappe.throw({
 					message: __('Please set the relieving date for employee {0}',
 						['<a href="/app/employee/' + frm.doc.employee +'">' + frm.doc.employee + '</a>']),
 					title: __('Relieving Date Missing')
+				});
+			}
+		});
+	},
+
+	send_exit_questionnaire: function(frm) {
+		frappe.db.get_value('HR Settings', 'HR Settings',
+			['exit_questionnaire_web_form', 'exit_questionnaire_notification_template'], (r) => {
+			if (!r.exit_questionnaire_web_form || !r.exit_questionnaire_notification_template) {
+				frappe.throw({
+					message: __('Please set {0} and {1} in {2}.',
+						['Exit Questionnaire Web Form'.bold(),
+						'Notification Template'.bold(),
+						'<a href="/app/hr-settings" target="_blank">HR Settings</a>']
+					),
+					title: __('Settings Missing')
+				});
+			} else {
+				frappe.call({
+					method: 'erpnext.hr.doctype.exit_interview.exit_interview.send_exit_questionnaire',
+					args: {
+						'exit_interview': frm.doc.name
+					},
+					callback: function(r) {
+						if (!r.exc) {
+							frm.refresh_field('questionnaire_email_sent');
+						}
+					}
 				});
 			}
 		});
--- erpnext/hr/doctype/exit_interview/exit_interview.py
+++ erpnext/hr/doctype/exit_interview/exit_interview.py
@@ -6,12 +6,48 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils import get_link_to_form
 
+from erpnext.hr.doctype.employee.employee import get_employee_email
+
+
 class ExitInterview(Document):
 	def validate(self):
 		self.validate_relieving_date()
+		self.set_employee_email()
 
 	def validate_relieving_date(self):
 		if not frappe.db.get_value('Employee', self.employee, 'relieving_date'):
 			frappe.throw(_('Please set the relieving date for employee {0}').format(
 				get_link_to_form('Employee', self.employee)),
 				title=_('Relieving Date Missing'))
+
+	def set_employee_email(self):
+		employee = frappe.get_doc('Employee', self.employee)
+		self.email = get_employee_email(employee)
+
+
+@frappe.whitelist()
+def send_exit_questionnaire(exit_interview):
+	exit_interview = frappe.get_doc('Exit Interview', exit_interview)
+	context = exit_interview.as_dict()
+
+	employee = frappe.get_doc('Employee', exit_interview.employee)
+	context.update(employee.as_dict())
+
+	email = get_employee_email(employee)
+	template_name = frappe.db.get_single_value('HR Settings', 'exit_questionnaire_notification_template')
+	template = frappe.get_doc('Email Template', template_name)
+
+	if email:
+		frappe.sendmail(
+			recipients=email,
+			subject=template.subject,
+			message=frappe.render_template(template.response, context),
+			reference_doctype=exit_interview.doctype,
+			reference_name=exit_interview.name
+		)
+		frappe.msgprint(_('Exit Questionnaire sent to {0}').format(email),
+			title='Success', indicator='green')
+		exit_interview.db_set('questionnaire_email_sent', True)
+		exit_interview.notify_update()
+	else:
+		frappe.msgprint(_('Email IDs for employee not found.'))
