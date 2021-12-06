bf94d311d79a8712afcc6d8826725e9073a29117
Noah Jacob <noahjacobkurian@gmail.com>
2021-12-06 19:03:11 +0530

    test: added tests for BOQ

--- erpnext/projects/doctype/task/test_task.py
+++ erpnext/projects/doctype/task/test_task.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import unittest
 
 import frappe
-from frappe.utils import add_days, getdate, nowdate
+from frappe.utils import add_days, flt, getdate, nowdate
 
 from erpnext.projects.doctype.task.task import CircularReferenceError
 
@@ -101,6 +101,55 @@ class TestTask(unittest.TestCase):
 		set_tasks_as_overdue()
 
 		self.assertEqual(frappe.db.get_value("Task", task.name, "status"), "Overdue")
+
+	def test_task_with_boq(self):
+		task = create_task("_Test Task 2")
+		task.from_bom = frappe.db.get_value("BOM", {"item": "_Test FG Item",
+			"is_default": 1, "docstatus": 1})
+		task.project_warehouse = 'Stores - _TC'
+		task.company = '_Test Company'
+		task.project = ''
+		task.get_items()
+		task.save()
+
+		row = task.items[0]
+		self.assertEqual(row.item_code, '_Test Item')
+		self.assertEqual(row.qty, 1)
+		self.assertEqual(row.estimated_cost, flt(row.qty) * flt(row.basic_rate))
+
+		row = task.items[1]
+		self.assertEqual(row.item_code, '_Test Item Home Desktop 100')
+		self.assertEqual(row.qty, 2)
+		self.assertEqual(row.estimated_cost, flt(row.qty) * flt(row.basic_rate))
+
+		# Back updating transfers and issues
+		from erpnext.projects.doctype.task.task import make_stock_entry_mi, make_stock_entry_mt
+		mi = make_stock_entry_mi(task.name)
+		mi.items[1].qty = 1
+		mi.submit()
+
+		# Both items have the same default warehouse
+		for item in mi.items:
+			self.assertEqual(item.s_warehouse, task.items[0].warehouse)
+			self.assertEqual(item.t_warehouse, None)
+
+		task.reload()
+
+		for item in task.items:
+			self.assertEqual(item.issued, 1)
+
+		# Test for fetching only remaining required items in MT
+		mt = make_stock_entry_mt(task.name)
+		self.assertEqual(len(mt.items), 1)
+		self.assertEqual(mt.items[0].qty, 1)
+		self.assertEqual(mt.items[0].s_warehouse, task.items[0].warehouse)
+		self.assertEqual(mt.items[0].t_warehouse, task.project_warehouse)
+		mt.submit()
+
+		task.reload()
+		self.assertEqual(task.items[1].transferred, 1)
+
+		frappe.db.rollback()
 
 def create_task(subject, start=None, end=None, depends_on=None, project=None, parent_task=None, is_group=0, is_template=0, begin=0, duration=0, save=True):
 	if not frappe.db.exists("Task", subject):
