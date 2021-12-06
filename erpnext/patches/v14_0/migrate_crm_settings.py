5ae77db450b060b83680c41745c42fd54c5029d3
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-12-06 13:22:30 +0530

    fix: simplify CRM Settings migration patch

--- erpnext/patches/v14_0/migrate_crm_settings.py
+++ erpnext/patches/v14_0/migrate_crm_settings.py
@@ -2,26 +2,15 @@ import frappe
 
 
 def execute():
-	frappe.db.sql("""UPDATE `tabSingles` as t1
-		INNER JOIN `tabSingles` as t2 ON
-			t1.doctype = 'Selling Settings' AND
-			t1.field = 'campaign_naming_by' AND
-			t2.doctype = 'CRM Settings' AND
-			t2.field = 'campaign_naming_by'
-		SET t2.value = t1.value""")
+	settings = frappe.db.get_value('Selling Settings', 'Selling Settings', [
+		'campaign_naming_by',
+		'close_opportunity_after_days',
+		'default_valid_till'
+	], as_dict=True)
 
-	frappe.db.sql("""UPDATE `tabSingles` as t1
-		INNER JOIN `tabSingles` as t2 ON
-			t1.doctype = 'Selling Settings' AND
-			t1.field = 'close_opportunity_after_days' AND
-			t2.doctype = 'CRM Settings' AND
-			t2.field = 'close_opportunity_after_days'
-		SET t2.value = t1.value""")
-
-	frappe.db.sql("""UPDATE `tabSingles` as t1
-		INNER JOIN `tabSingles` as t2 ON
-			t1.doctype = 'Selling Settings' AND
-			t1.field = 'default_valid_till' AND
-			t2.doctype = 'CRM Settings' AND
-			t2.field = 'default_valid_till'
-		SET t2.value = t1.value""")
+	frappe.reload_doc('crm', 'doctype', 'crm_settings')
+	frappe.db.set_value('CRM Settings', 'CRM Settings', {
+		'campaign_naming_by': settings.campaign_naming_by,
+		'close_opportunity_after_days': settings.close_opportunity_after_days,
+		'default_valid_till': settings.default_valid_till
+	})
