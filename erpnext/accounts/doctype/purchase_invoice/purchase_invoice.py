6485ac4e596dced1a0c5bfc2fc7f9fb920d3b6d8
Sagar Sharma <sagarsharma.s312@gmail.com>
2021-12-09 17:04:00 +0530

    fix: misleading "Set Default X" fields after saving (#28798)
    * fix: misleading "Set Default X" fields after saving
    
    * refactor: remove unncessary code and minor formatting
    
    * fix: extend to more doctypes and correct fieldnames
    
    Co-authored-by: Ankush Menat <ankush@frappe.io>

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -114,6 +114,9 @@ class PurchaseInvoice(BuyingController):
 		self.set_status()
 		self.validate_purchase_receipt_if_update_stock()
 		validate_inter_company_party(self.doctype, self.supplier, self.company, self.inter_company_invoice_reference)
+		self.reset_default_field_value("set_warehouse", "items", "warehouse")
+		self.reset_default_field_value("rejected_warehouse", "items", "rejected_warehouse")
+		self.reset_default_field_value("set_from_warehouse", "items", "from_warehouse")
 
 	def validate_release_date(self):
 		if self.release_date and getdate(nowdate()) >= getdate(self.release_date):
