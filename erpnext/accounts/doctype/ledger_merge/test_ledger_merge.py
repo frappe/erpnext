32e5db7cb8e23e653896cb1f8efe0c21a58367de
Dany Robert <danyrt@wahni.com>
2021-12-17 16:38:16 +0000

    chore: styling according to linter

--- erpnext/accounts/doctype/ledger_merge/test_ledger_merge.py
+++ erpnext/accounts/doctype/ledger_merge/test_ledger_merge.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2021, Wahni Green Technologies Pvt. Ltd. and Contributors
 # See license.txt
 
-import frappe
 import unittest
+
+import frappe
+
 from erpnext.accounts.doctype.ledger_merge.ledger_merge import start_merge
 
 
