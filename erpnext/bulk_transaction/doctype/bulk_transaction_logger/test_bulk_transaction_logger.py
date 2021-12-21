46363f581eb7723a19ea1d2b0c8b8598dc0077d8
Mohammed Yusuf Shaikh <iyusufshaikh78@gmail.com>
2021-12-21 11:21:02 +0530

    fix: sider issues and test cases

--- erpnext/bulk_transaction/doctype/bulk_transaction_logger/test_bulk_transaction_logger.py
+++ erpnext/bulk_transaction/doctype/bulk_transaction_logger/test_bulk_transaction_logger.py
@@ -17,7 +17,7 @@ class TestBulkTransactionLogger(unittest.TestCase):
 		create_item()
 		so_name = create_so()
 		transaction_processing([{"name": so_name}], "Sales Order", "Sales Invoice")
-		data = frappe.db.get_list("Bulk Transaction Logger", filters = {"posting_date": date.today(), "cusomter": "Bulk Customer"}, fields=["*"])
+		data = frappe.db.get_list("Sales Invoice", filters = {"posting_date": date.today(), "customer": "Bulk Customer"}, fields=["*"])
 		if not data:
 			self.fail("No Sales Invoice Created !")
 
@@ -39,7 +39,7 @@ class TestBulkTransactionLogger(unittest.TestCase):
 
 def create_company():
 	if not frappe.db.exists('Company', 'Test Bulk'):
-		company = frappe.get_doc({
+		frappe.get_doc({
 			'doctype': 'Company',
 			'company_name': 'Test Bulk',
 			'country': 'India',
@@ -48,14 +48,14 @@ def create_company():
 
 def create_customer():
 	if not frappe.db.exists('Customer', 'Bulk Customer'):
-		customer = frappe.get_doc({
-            'doctype': 'Customer',
-            'customer_name': 'Bulk Customer'
-        }).insert()
+		frappe.get_doc({
+			'doctype': 'Customer',
+			'customer_name': 'Bulk Customer'
+		}).insert()
 
 def create_item():
 	if not frappe.db.exists("Item", "MK"):
-		item = frappe.get_doc({
+		frappe.get_doc({
 			"doctype": "Item",
 			"item_code": "MK",
 			"item_name": "Milk",
@@ -69,11 +69,11 @@ def create_so():
 	so.company = "Test Bulk"
 	so.transaction_date = date.today()
 	so.append("items", {
-        "item_code": "MK",
-        "delivery_date": date.today(),
-        "qty": 10,
-        "rate": 80,
-    })
+		"item_code": "MK",
+		"delivery_date": date.today(),
+		"qty": 10,
+		"rate": 80,
+	})
 
 	so.flags.ignore_validate = True
 	so.insert(ignore_mandatory=True)
