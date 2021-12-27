b818cf9cf4a43d8c4f6f0e726bbb447332eecee0
Farzan <farzan.wadood@gourmetpakistan.com>
2021-12-27 12:27:14 +0500

    Update item_wise_sales_register.py

--- erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.py
+++ erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.py
@@ -335,7 +335,7 @@ def get_columns(additional_table_columns, filters):
 def get_conditions(filters):
 	conditions = ""
 
-	for opts in (("company", " and company=%(company)s"),
+	for opts in (("company", " and `tabSales Invoice`.company=%(company)s"),
 		("customer", " and `tabSales Invoice`.customer = %(customer)s"),
 		("item_code", " and `tabSales Invoice Item`.item_code = %(item_code)s"),
 		("from_date", " and `tabSales Invoice`.posting_date>=%(from_date)s"),
