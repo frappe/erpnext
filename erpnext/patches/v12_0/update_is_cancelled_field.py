ba7461e0d425de5e72547a5e095db0a559a84f4b
Saurabh <saurabh6790@gmail.com>
2021-12-29 12:39:53 +0530

    chore: patch fixes
    (cherry picked from commit 8b5b146f6d2720587a16f78a8d47840be8dca2b7)
    
    # Conflicts:
    #	erpnext/patches/v13_0/make_homepage_products_website_items.py

--- erpnext/patches/v12_0/update_is_cancelled_field.py
+++ erpnext/patches/v12_0/update_is_cancelled_field.py
@@ -2,14 +2,22 @@ import frappe
 
 
 def execute():
-	try:
-		frappe.db.sql("UPDATE `tabStock Ledger Entry` SET is_cancelled = 0 where is_cancelled in ('', NULL, 'No')")
-		frappe.db.sql("UPDATE `tabSerial No` SET is_cancelled = 0 where is_cancelled in ('', NULL, 'No')")
+	#handle type casting for is_cancelled field
 
-		frappe.db.sql("UPDATE `tabStock Ledger Entry` SET is_cancelled = 1 where is_cancelled = 'Yes'")
-		frappe.db.sql("UPDATE `tabSerial No` SET is_cancelled = 1 where is_cancelled = 'Yes'")
+	for doc_mapper in (('stock','Stock Ledger Entry'), 
+			('stock','Serial No'), 
+			('accounts','GL Entry')):
+		try:
+			module = doc_mapper[0]
+			doctype = doc_mapper[1]
 
-		frappe.reload_doc("stock", "doctype", "stock_ledger_entry")
-		frappe.reload_doc("stock", "doctype", "serial_no")
-	except Exception:
-		pass
+			frappe.db.sql('''UPDATE `tab{doctype}` SET is_cancelled = 
+				CASE
+					WHEN is_cancelled = 'No' THEN 0
+					WHEN is_cancelled = 'Yes' THEN 1
+					ELSE 0
+				END'''.format(doctype=doctype))
+
+			frappe.reload_doc(module, "doctype", frappe.scrub(doctype))
+		except Exception:
+			pass
