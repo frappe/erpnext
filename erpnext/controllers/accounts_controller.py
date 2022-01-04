7f09bf098d7999ef65bc4c7fd986af0832d91f04
cpdeethree <conor@millstone.studio>
2022-01-04 17:11:20 -0600

    fix: is_pos requires int comparison in postgres

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -1737,7 +1737,7 @@ def update_invoice_status():
 				| invoice.status.like("Partly Paid%")
 			)
 			& (
-				((invoice.is_pos & invoice.due_date < today) | is_overdue)
+				((invoice.is_pos != 0 & invoice.due_date < today) | is_overdue)
 				if doctype == "Sales Invoice"
 				else is_overdue
 			)
