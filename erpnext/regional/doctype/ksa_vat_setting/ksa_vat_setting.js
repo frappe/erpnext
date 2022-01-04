01dc002a9bd2d7d5a3356ef17263353a53d8f7d0
Dany Robert <danyrt@wahni.com>
2022-01-04 12:37:24 +0530

    feat: add company and type filters for account

--- erpnext/regional/doctype/ksa_vat_setting/ksa_vat_setting.js
+++ erpnext/regional/doctype/ksa_vat_setting/ksa_vat_setting.js
@@ -1,8 +1,24 @@
-// Copyright (c) 2021, Havenir Solutions and contributors
+// Copyright (c) 2022, Havenir Solutions, Wahni Green Technologies and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('KSA VAT Setting', {
-	onload: function () {
+	setup: function(frm) {
 		frappe.breadcrumbs.add('Accounts', 'KSA VAT Setting');
+		frm.set_query("account", "ksa_vat_sales_accounts", function() {
+			return {
+				"filters": {
+					"company": frm.doc.company,
+					"account_type": "Tax"
+				}
+			};
+		});
+		frm.set_query("account", "ksa_vat_purchase_accounts", function() {
+			return {
+				"filters": {
+					"company": frm.doc.company,
+					"account_type": "Tax"
+				}
+			};
+		});
 	}
 });
