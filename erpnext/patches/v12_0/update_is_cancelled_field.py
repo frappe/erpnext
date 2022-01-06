b904ee32f633b74e91cd9fa0a504e35f0e4061ae
Saurabh <saurabh6790@gmail.com>
2022-01-06 15:45:52 +0530

    fix: remove desktop icons while deleting sales reports
    (cherry picked from commit 5f72026cb932d01fc827c382747e996a94b441fd)

--- erpnext/patches/v12_0/update_is_cancelled_field.py
+++ erpnext/patches/v12_0/update_is_cancelled_field.py
@@ -5,15 +5,14 @@ import frappe
 def execute():
 	#handle type casting for is_cancelled field
 
-	for doc_mapper in (('stock','Stock Ledger Entry'), 
-			('stock','Serial No'), 
-			('accounts','GL Entry')):
+	for doc_mapper in (('stock','Stock Ledger Entry'),
+		('stock','Serial No'),
+		('accounts','GL Entry')):
 		try:
 			module = doc_mapper[0]
 			doctype = doc_mapper[1]
 
-			frappe.db.sql('''UPDATE `tab{doctype}` SET is_cancelled = 
-				CASE
+			frappe.db.sql('''UPDATE `tab{doctype}` SET is_cancelled = CASE
 					WHEN is_cancelled = 'No' THEN 0
 					WHEN is_cancelled = 'Yes' THEN 1
 					ELSE 0
