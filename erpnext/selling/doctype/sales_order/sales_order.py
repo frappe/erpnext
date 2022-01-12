e72d84b3577407e3ac7723bbcb19d655a50f677c
Ajay Jogdand <ajayjogdand374@gmail.com>
2022-01-12 17:57:38 +0530

    commision

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -43,6 +43,7 @@ class SalesOrder(SellingController):
 
 	def validate(self):
 		super(SalesOrder, self).validate()
+		self.get_commision()
 		self.validate_delivery_date()
 		self.validate_proj_cust()
 		self.validate_po()
