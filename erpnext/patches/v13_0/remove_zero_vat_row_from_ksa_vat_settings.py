e5c88970f6cf331d376f9d68e7ad871f7556ef62
Dany Robert <danyrt@wahni.com>
2022-01-16 14:50:32 +0530

    patch: remove zero and exempt rows

---
+++ erpnext/patches/v13_0/remove_zero_vat_row_from_ksa_vat_settings.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2022, Frappe Technologies and Contributors
+# License: GNU General Public License v3. See license.txt
+
+import frappe
+
+
+def execute():
+	company = frappe.get_all('Company', filters={'country': 'Saudi Arabia'})
+	if not company:
+		return
+
+	selling = frappe.db.get_all('KSA VAT Sales Account', or_filters=[
+		['title', '=', 'Zero rated domestic sales'],
+		['title', '=', 'Exempted sales'],
+	], pluck='name')
+
+	for row in selling:
+		frappe.delete_doc('KSA VAT Sales Account', row)
+
+	buying = frappe.db.get_all('KSA VAT Purchase Account', or_filters=[
+		['title', '=', 'Zero rated purchases'],
+		['title', '=', 'Exempted purchases'],
+	], pluck='name')
+
+	for row in buying:
+		frappe.delete_doc('KSA VAT Purchase Account', row)
+		
