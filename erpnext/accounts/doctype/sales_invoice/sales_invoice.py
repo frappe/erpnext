9bf3c38905179d271894082ee814a1b4ecc26452
Noah Jacob <noahjacobkurian@gmail.com>
2022-01-21 11:50:13 +0530

    fix: updated packed_items getting fetched on Sales Return / Credit Note (#28607)
    * fix: updated packed_items getting fetched on sales return
    
    * fix: updated test cases for product bundle returns
    
    * fix: moved calculate_mapped_packed_items_return to stock.utils
    
    * fix: updated packed_items getting fetched on Return/Credit Note
    
    * refactor: change var name to avoid `self`
    
    * fix: check item code before assigning parent item
    
    Co-authored-by: Ankush Menat <ankush@frappe.io>
    (cherry picked from commit 20216fa9f1e98e612c36a89e497de3e92238db7b)

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -45,6 +45,7 @@ from erpnext.setup.doctype.company.company import update_company_current_month_s
 from erpnext.stock.doctype.batch.batch import set_batch_nos
 from erpnext.stock.doctype.delivery_note.delivery_note import update_billed_amount_based_on_so
 from erpnext.stock.doctype.serial_no.serial_no import get_delivery_note_serial_no, get_serial_nos
+from erpnext.stock.utils import calculate_mapped_packed_items_return
 
 form_grid_templates = {
 	"items": "templates/form_grid/item_grid.html"
@@ -745,8 +746,11 @@ class SalesInvoice(SellingController):
 
 	def update_packing_list(self):
 		if cint(self.update_stock) == 1:
-			from erpnext.stock.doctype.packed_item.packed_item import make_packing_list
-			make_packing_list(self)
+			if cint(self.is_return) and self.return_against:
+				calculate_mapped_packed_items_return(self)
+			else:
+				from erpnext.stock.doctype.packed_item.packed_item import make_packing_list
+				make_packing_list(self)
 		else:
 			self.set('packed_items', [])
 
