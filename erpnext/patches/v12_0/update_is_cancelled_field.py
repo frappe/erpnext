3c499e5999b4d71fc3043dcf32f566f888a56765
Ankush Menat <ankush@frappe.io>
2022-01-21 19:42:18 +0530

    fix: check type before patching

--- erpnext/patches/v12_0/update_is_cancelled_field.py
+++ erpnext/patches/v12_0/update_is_cancelled_field.py
@@ -10,7 +10,9 @@ def execute():
 	)
 
 	for module, doctype in module_doctypes:
-		if not frappe.db.has_column(doctype, "is_cancelled"):
+		if (not frappe.db.has_column(doctype, "is_cancelled")
+			or frappe.db.get_column_type(doctype, "is_cancelled").lower() == "int(1)"
+		):
 			continue
 
 		frappe.db.sql("""
