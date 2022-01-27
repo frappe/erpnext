3ce2e9ec9185ee5f3ef2094028b1075409c85549
GersonCaballero <gersoncaballero306@gmail.com>
2022-01-27 22:16:49 +0000

    ResolveIssues

--- erpnext/accounts/report/payment_entry_cxp/payment_entry_cxp.py
+++ erpnext/accounts/report/payment_entry_cxp/payment_entry_cxp.py
@@ -9,7 +9,7 @@ import datetime
 
 def execute(filters=None):
 	if not filters: filters = {}
-	columns = [_("Date") + "::240", _("Serie") + "::240", _("Customer") + "::240", _("Party RTN") + "::240", _("Company") + "::240", _("Reason Payment") + "::240",  _("Paid Amount") + ":Currency:120", _("Total Allocated Amount") + ":Currency:120",_("Unallocated Amount") + ":Currency:120",_("Difference Amount (Company Currency)") + ":Currency:120", _("Cheque/Reference No") + "::240", _("Cheque/Reference Date") + "::240", _("Created By") + "::240"]
+	columns = [_("Date") + "::240", _("Serie") + "::240", _("Status") + "::240", _("Type") + "::240", _("Name") + "::240", _("Customer") + "::240", _("Party RTN") + "::240", _("Company") + "::240", _("Reason Payment") + "::240",  _("Paid Amount") + ":Currency:120", _("Total Allocated Amount") + ":Currency:120",_("Unallocated Amount") + ":Currency:120",_("Difference Amount (Company Currency)") + ":Currency:120", _("Cheque/Reference No") + "::240", _("Cheque/Reference Date") + "::240", _("Created By") + "::240"]
 	data = return_data(filters)
 	return columns, data
 
@@ -22,8 +22,10 @@ def return_data(filters):
 	registers = frappe.get_all("Payment Entry", ["*"], filters = conditions,  order_by = "name asc")
 
 	for register in registers:
-		row = [register.posting_date, register.name, register.party_name, register.party_rtn, register.company, register.reason_payment, register.paid_amount, register.total_allocated_amount, register.unallocated_amount, register.difference_amount, register.reference_no, register.reference_date, register.user]
-		data.append(row)
+		references = frappe.get_all("Payment Entry Reference", ["*"], filters = {"parent": register.name})
+		for refenrence in references:
+			row = [register.posting_date, register.name, register.status, refenrence.reference_doctype, refenrence.reference_name, register.party_name, register.party_rtn, register.company, register.reason_payment, register.paid_amount, register.total_allocated_amount, register.unallocated_amount, register.difference_amount, register.reference_no, register.reference_date, register.user]
+			data.append(row)
 
 	return data
 
