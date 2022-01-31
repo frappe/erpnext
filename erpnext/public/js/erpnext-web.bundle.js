b2b827f50ff649c1a78717c63ca752fd943412d0
marination <maricadsouza221197@gmail.com>
2022-02-01 00:03:08 +0530

    chore: Adjust to ESbuild and missing imports
    - update erpnext web bundle and tweak to fit new build system
    - change page id in scss due to change in v14 router
    - add missing imports

--- erpnext/public/js/erpnext-web.bundle.js
+++ erpnext/public/js/erpnext-web.bundle.js
@@ -1,2 +1,7 @@
 import "./website_utils";
 import "./shopping_cart";
+import "./wishlist";
+import "../../e_commerce/product_ui/list";
+import "../../e_commerce/product_ui/views";
+import "../../e_commerce/product_ui/grid";
+import "../../e_commerce/product_ui/search";
