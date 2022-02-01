dbb3ea3a6ea05281c002b206463c3335ab165d67
Deepesh Garg <deepeshgarg6@gmail.com>
2022-02-01 17:25:26 +0530

    chore: fix tests
    (cherry picked from commit 640b2d57deb7f31bbff118e3bb8812329d3ae20b)

--- erpnext/patches/v13_0/enable_provisional_accounting.py
+++ erpnext/patches/v13_0/enable_provisional_accounting.py
@@ -2,6 +2,9 @@ import frappe
 
 
 def execute():
+	if not frappe.get_meta("Company").has_field("enable_perpetual_inventory_for_non_stock_items"):
+		return
+
 	frappe.reload_doc("setup", "doctype", "company")
 
 	company = frappe.qb.DocType("Company")
