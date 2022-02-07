b249478fcc0e22f58377596401cd974e948646c3
Deepesh Garg <42651287+deepeshgarg007@users.noreply.github.com>
2022-02-07 12:26:01 +0530

    fix: Copyright info
    (cherry picked from commit 4284017e9de3b033156ca6947665bc99f0daefc3)

--- erpnext/selling/report/payment_terms_status_for_sales_order/payment_terms_status_for_sales_order.js
+++ erpnext/selling/report/payment_terms_status_for_sales_order/payment_terms_status_for_sales_order.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
