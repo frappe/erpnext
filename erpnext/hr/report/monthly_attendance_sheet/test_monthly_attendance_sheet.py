342a45f88ae749d5307815d7418d41ba0dcf6799
ChillarAnand <anand21nanda@gmail.com>
2022-02-10 16:07:12 +0530

    test: Added test for monthly attendance report

---
+++ erpnext/hr/report/monthly_attendance_sheet/test_monthly_attendance_sheet.py
@@ -0,0 +1,44 @@
+import frappe
+from dateutil.relativedelta import relativedelta
+from frappe.utils import now_datetime
+
+from erpnext.hr.doctype.attendance.attendance import mark_attendance
+from erpnext.hr.doctype.employee.test_employee import make_employee
+from erpnext.hr.report.monthly_attendance_sheet.monthly_attendance_sheet import execute
+from erpnext.tests.utils import ERPNextTestCase
+
+
+class TestMonthlyAttendanceSheet(ERPNextTestCase):
+	def setUp(self):
+		self.employee = make_employee("test_employee@example.com")
+		frappe.db.delete('Attendance', {'employee': self.employee})
+
+	def test_monthly_attendance_sheet_report(self):
+		now = now_datetime()
+		previous_month = now.month - 1
+		month_first = now.replace(day=1).replace(month=previous_month).date()
+
+		company = frappe.db.get_value('Employee', self.employee, 'company')
+		entries = 7
+
+		for day in range(0, entries*3, 3):
+			mark_attendance(self.employee, month_first + relativedelta(days=day), 'Present')
+			mark_attendance(self.employee, month_first + relativedelta(days=day+1), 'Absent')
+			mark_attendance(self.employee, month_first + relativedelta(days=day+2), 'On Leave')
+
+		filters = frappe._dict({
+			'month': previous_month,
+			'year': now.year,
+			'company': company,
+		})
+		report = execute(filters=filters)
+		employees = report[1][0]
+		datasets = report[3]['data']['datasets']
+		absent = datasets[0]['values']
+		present = datasets[1]['values']
+		leaves = datasets[2]['values']
+
+		self.assertIn(self.employee, employees)
+		self.assertEqual(sum(present), entries)
+		self.assertEqual(sum(absent), entries)
+		self.assertEqual(sum(leaves), entries)
