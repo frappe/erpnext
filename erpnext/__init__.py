9a1afeb13be85d29c5d8bcfa4ac40b0621329c4a
Rohit Waghchaure <rohitw1991@gmail.com>
2022-02-15 22:49:43 +0550

    bumped to version 12.29.0

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -5,7 +5,7 @@ import frappe
 from erpnext.hooks import regional_overrides
 from frappe.utils import getdate
 
-__version__ = '12.28.0'
+__version__ = '12.29.0'
 
 def get_default_company(user=None):
 	'''Get default company for user'''
