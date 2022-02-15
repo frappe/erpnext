8120b615c0f303d4390ce76dabeaca9a72282d6b
Ankush Menat <ankush@frappe.io>
2022-02-15 11:36:51 +0530

    style: spaces to tabs
    [skip ci]

--- erpnext/erpnext_integrations/doctype/amazon_mws_settings/amazon_mws_settings.js
+++ erpnext/erpnext_integrations/doctype/amazon_mws_settings/amazon_mws_settings.js
@@ -3,8 +3,8 @@
 
 
 frappe.ui.form.on('Amazon MWS Settings', {
-    refresh: function (frm) {
-        let app_link = "<a href='https://github.com/frappe/ecommerce_integrations' target='_blank'>Ecommerce Integrations</a>"
-        frm.dashboard.add_comment(__("Amazon MWS Integration will be removed from ERPNext in Version 14. Please install {0} app to continue using it.", [app_link]), "yellow", true);
-    }
+	refresh: function (frm) {
+		let app_link = "<a href='https://github.com/frappe/ecommerce_integrations' target='_blank'>Ecommerce Integrations</a>"
+		frm.dashboard.add_comment(__("Amazon MWS Integration will be removed from ERPNext in Version 14. Please install {0} app to continue using it.", [app_link]), "yellow", true);
+	}
 });
