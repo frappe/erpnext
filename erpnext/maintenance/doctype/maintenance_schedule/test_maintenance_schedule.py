b027e9b27b8dc4813723c79e834e888cdf6a8d90
Noah Jacob <noahjacobkurian@gmail.com>
2022-02-18 13:08:50 +0530

    test: serials updated in schedules after save

--- erpnext/maintenance/doctype/maintenance_schedule/test_maintenance_schedule.py
+++ erpnext/maintenance/doctype/maintenance_schedule/test_maintenance_schedule.py
@@ -117,6 +117,21 @@ class TestMaintenanceSchedule(unittest.TestCase):
 
 		frappe.db.rollback()
 
+	def test_schedule_with_serials(self):
+		# Checks whether serials are automatically updated when changing in items table.
+		item_code = "_Test Serial Item"
+		make_serial_item_with_serial(item_code)
+		ms = make_maintenance_schedule(item_code=item_code, serial_no="TEST001, TEST002")
+		ms.save()
+		ms.items[0].serial_no = "TEST001"
+		# Before Save
+		self.assertEqual(ms.schedules[0].serial_no, "TEST001, TEST002")
+		# After Save
+		ms.save()
+		self.assertEqual(ms.schedules[0].serial_no, "TEST001")
+
+		frappe.db.rollback()
+
 def make_serial_item_with_serial(item_code):
 	serial_item_doc = create_item(item_code, is_stock_item=1)
 	if not serial_item_doc.has_serial_no or not serial_item_doc.serial_no_series:
