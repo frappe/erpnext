312db429e4605d6d0ce47d1034662fdf0ec053b7
Ankush Menat <ankush@frappe.io>
2022-02-19 16:26:17 +0530

    refactor: use qb for patching flag

--- erpnext/patches/v14_0/update_batch_valuation_flag.py
+++ erpnext/patches/v14_0/update_batch_valuation_flag.py
@@ -6,7 +6,6 @@ def execute():
 	- Don't use batchwise valuation for existing batches.
 	- Only batches created after this patch shoule use it.
 	"""
-	frappe.db.sql("""
-		UPDATE `tabBatch`
-		SET use_batchwise_valuation=0
-	""")
+
+	batch = frappe.qb.DocType("Batch")
+	frappe.qb.update(batch).set(batch.use_batchwise_valuation, 0).run()
