60c5d2b4714d65a18db742413e3f59818070277d
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-22 15:26:13 +0530

    feat: add option for creating subcontracting receipt thru create button in SCO

--- erpnext/buying/doctype/subcontracting_order/subcontracting_order.js
+++ erpnext/buying/doctype/subcontracting_order/subcontracting_order.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Subcontracting Order', {
-	// refresh: function(frm) {
+	refresh: function (frm) {
+		if (frm.doc.docstatus == 1) {
+			cur_frm.add_custom_button(__('Subcontracting Receipt'), make_subcontracting_receipt, __('Create'));
 
-	// }
+			cur_frm.page.set_inner_btn_group_as_primary(__('Create'));
+		}
+	},
 });
+
+let make_subcontracting_receipt = () => {
+	frappe.model.open_mapped_doc({
+		method: "erpnext.buying.doctype.subcontracting_order.subcontracting_order.make_subcontracting_receipt",
+		frm: cur_frm,
+		freeze_message: __("Creating Subcontracting Receipt ...")
+	})
+}
