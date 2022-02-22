84223a0833ad47d3c8cd3ea57753f40eb6daa635
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-22 16:45:44 +0530

    feat: Add filter for "subcontracting_order" in SCR

--- erpnext/buying/doctype/subcontracting_receipt/subcontracting_receipt.js
+++ erpnext/buying/doctype/subcontracting_receipt/subcontracting_receipt.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Subcontracting Receipt', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		frm.set_query('subcontracting_order', () => {
+			return {
+				filters: {
+					docstatus: 1,
+				}
+			};
+		});
+	}
 });
