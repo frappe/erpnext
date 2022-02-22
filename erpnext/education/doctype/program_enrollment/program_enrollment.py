a5e5c3d10bea25360170f9930304768f5d06b71f
veena56 <veena.h@promantia.com>
2022-02-22 18:50:29 +0530

    [29759] Here is the fix for the issue . An SQL , which returns a tuple was assigned to a date variable.

--- erpnext/education/doctype/program_enrollment/program_enrollment.py
+++ erpnext/education/doctype/program_enrollment/program_enrollment.py
@@ -61,15 +61,9 @@ class ProgramEnrollment(Document):
 			frappe.throw(_("Student is already enrolled."))
 
 	def update_student_joining_date(self):
-		table = frappe.qb.DocType('Program Enrollment')
-		date = (
-			frappe.qb.from_(table)
-				.select(Min(table.enrollment_date).as_('enrollment_date'))
-				.where(table.student == self.student)
-		).run(as_dict=True)
-
-		if date:
-			frappe.db.set_value("Student", self.student, "joining_date", date[0].enrollment_date)
+		date = frappe.db.sql("select min(enrollment_date) from `tabProgram Enrollment` where student= %s", self.student)
+		((date,),) = frappe.db.sql("select min(enrollment_date) from `tabProgram Enrollment` where student= %s", self.student)
+ 		frappe.db.set_value("Student", self.student, "joining_date", date)
 
 	def make_fee_records(self):
 		from erpnext.education.api import get_fee_components
