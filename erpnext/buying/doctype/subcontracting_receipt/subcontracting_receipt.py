54a6c7d04021f935e959fb10e94b2c09e9895e94
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-22 16:43:43 +0530

    feat: Add basic validation for SCO in SCR

--- erpnext/buying/doctype/subcontracting_receipt/subcontracting_receipt.py
+++ erpnext/buying/doctype/subcontracting_receipt/subcontracting_receipt.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class SubcontractingReceipt(Document):
-	pass
+	def validate(self):
+		self.validate_subcontracting_order()
+
+	def validate_subcontracting_order(self):
+		sco = frappe.get_doc("Subcontracting Order", self.get("subcontracting_order"))
+
+		if sco.docstatus != 1:
+			frappe.throw(_(f"Please submit Subcontracting Order {sco.name} before proceeding."))
+
