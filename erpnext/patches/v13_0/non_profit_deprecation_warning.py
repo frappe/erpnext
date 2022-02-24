761e528ba39d5acdbd0bb7dd8cec48705d80cfc3
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-02-24 17:57:39 +0530

    fix: deprecation warning

--- erpnext/patches/v13_0/non_profit_deprecation_warning.py
+++ erpnext/patches/v13_0/non_profit_deprecation_warning.py
@@ -5,6 +5,6 @@ def execute():
 
 	click.secho(
 		"Non Profit Domain is moved to a separate app and will be removed from ERPNext in version-14.\n"
-		"When upgrading to ERPNext version-14, please install the app to continue using the Agriculture domain: https://github.com/frappe/non_profit",
+		"When upgrading to ERPNext version-14, please install the app to continue using the Non Profit domain: https://github.com/frappe/non_profit",
 		fg="yellow",
 	)
