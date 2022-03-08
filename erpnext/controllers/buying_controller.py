61729307f76977d26dc595d5d1b3b2970a97e439
Deepesh Garg <deepeshgarg6@gmail.com>
2022-03-08 21:35:10 +0530

    fix: Remove docstatus check during budget validate

--- erpnext/controllers/buying_controller.py
+++ erpnext/controllers/buying_controller.py
@@ -532,17 +532,16 @@ class BuyingController(StockController, Subcontracting):
 			self.update_fixed_asset(field, delete_asset=True)
 
 	def validate_budget(self):
-		if self.docstatus == 1:
-			for data in self.get('items'):
-				args = data.as_dict()
-				args.update({
-					'doctype': self.doctype,
-					'company': self.company,
-					'posting_date': (self.schedule_date
-						if self.doctype == 'Material Request' else self.transaction_date)
-				})
+		for data in self.get('items'):
+			args = data.as_dict()
+			args.update({
+				'doctype': self.doctype,
+				'company': self.company,
+				'posting_date': (self.schedule_date
+					if self.doctype == 'Material Request' else self.transaction_date)
+			})
 
-				validate_expense_against_budget(args)
+			validate_expense_against_budget(args)
 
 	def process_fixed_asset(self):
 		if self.doctype == 'Purchase Invoice' and not self.update_stock:
