6ff0d138cc8c566df40f1e9ab5331b1aa5f69129
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-03-10 12:41:45 +0530

    fix: flaky attendance tests

--- erpnext/hr/doctype/leave_application/test_leave_application.py
+++ erpnext/hr/doctype/leave_application/test_leave_application.py
@@ -782,9 +782,10 @@ def allocate_leaves(employee, leave_period, leave_type, new_leaves_allocated, el
 	allocate_leave.submit()
 
 
-def get_first_sunday(holiday_list):
-	month_start_date = get_first_day(nowdate())
-	month_end_date = get_last_day(nowdate())
+def get_first_sunday(holiday_list, for_date=None):
+	date = for_date or getdate()
+	month_start_date = get_first_day(date)
+	month_end_date = get_last_day(date)
 	first_sunday = frappe.db.sql("""
 		select holiday_date from `tabHoliday`
 		where parent = %s
