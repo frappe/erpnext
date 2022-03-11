59bfee5c6ed6a40da990b3b66ce6c8356261b288
Ankush Menat <ankush@frappe.io>
2022-03-11 19:01:26 +0530

    test: standalone SI creates and attaches serial nos
    (cherry picked from commit 1c37d2711abebe7db43c89c49869ce63e533e2a7)

--- erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
@@ -2540,6 +2540,14 @@ class TestSalesInvoice(unittest.TestCase):
 
 		frappe.db.set_value('Accounts Settings', None, 'acc_frozen_upto', None)
 
+	def test_standalone_serial_no_return(self):
+		si = create_sales_invoice(item_code="_Test Serialized Item With Series", update_stock=True, is_return=True, qty=-1)
+		si.submit()
+		self.assertTrue(si.items[0].serial_no)
+
+		return si
+
+
 def get_sales_invoice_for_e_invoice():
 	si = make_sales_invoice_for_ewaybill()
 	si.naming_series = 'INV-2020-.#####'
