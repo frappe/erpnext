e6ba47f349cf67e4f648707fec446b4a7c9d5197
Noah Jacob <noahjacobkurian@gmail.com>
2022-03-15 15:12:58 +0530

    fix(test): updated test with new serial sle format

--- erpnext/stock/report/stock_ledger/test_stock_ledger_report.py
+++ erpnext/stock/report/stock_ledger/test_stock_ledger_report.py
@@ -13,7 +13,7 @@ from erpnext.stock.doctype.serial_no.serial_no import get_serial_nos
 from erpnext.stock.report.stock_ledger.stock_ledger import execute
 
 
-class TestStockLedgerReeport(FrappeTestCase):
+class TestStockLedgerReport(FrappeTestCase):
 	def setUp(self) -> None:
 		make_serial_item_with_serial("_Test Stock Report Serial Item")
 		self.filters = frappe._dict(
@@ -27,15 +27,19 @@ class TestStockLedgerReeport(FrappeTestCase):
 	def test_serial_balance(self):
 		item_code = "_Test Stock Report Serial Item"
 		# Checks serials which were added through stock in entry.
+		# SLEs have only one serial linked to it.
 		columns, data = execute(self.filters)
-		self.assertEqual(data[0].in_qty, 2)
-		serials_added = get_serial_nos(data[0].serial_no)
-		self.assertEqual(len(serials_added), 2)
+		serials_added = []
+		for row in data:
+			self.assertEqual(row.in_qty, 1)
+			serials_added.append(row.serial_no)
+			self.assertEquals("\n".join(serials_added), row.balance_serial_no)
+		self.assertEqual(len(data), 2)
 		# Stock out entry for one of the serials.
-		dn = create_delivery_note(item=item_code, serial_no=serials_added[1])
+		dn = create_delivery_note(item=item_code, serial_no=serials_added[-1])
 		self.filters.voucher_no = dn.name
 		columns, data = execute(self.filters)
 		self.assertEqual(data[0].out_qty, -1)
-		self.assertEqual(data[0].serial_no, serials_added[1])
-		self.assertEqual(data[0].balance_serial_no, serials_added[0])
+		self.assertEqual(data[0].serial_no, serials_added.pop())
+		self.assertEqual(data[0].balance_serial_no, serials_added.pop())
 
