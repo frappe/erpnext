fbeb61749f7c336f2add3d87d42e5514e2722d7a
yadavyk <32797974+yadavyk@users.noreply.github.com>
2022-03-16 17:18:07 +0530

    test: Create supplier
    An automated Cypress test to create and validate attributes of a supplier

---
+++ cypress/integration/test_supplier.js
@@ -0,0 +1,22 @@
+
+context('Supplier', () => {
+	before(() => {
+		cy.login();
+	});
+  
+	it('Insert a Supplier', () => {
+		cy.visit(`app/supplier/`);
+		cy.get('.primary-action').click();
+		cy.get('.custom-actions > .btn').click();
+		cy.get_field('supplier_group', 'Link').type("All Supplier Groups");
+		cy.get_field('supplier_name', 'Link').type("Medlink International Suppliers");
+		cy.get('#page-Supplier > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
+	});
+
+	it('Check Supplier attributes', () => {
+		cy.get_field('supplier_name', 'Link').should('have.value', 'Medlink International Suppliers');
+		cy.get_field('supplier_group', 'Link').should('have.value', 'All Supplier Groups');	
+		cy.get('#page-Supplier > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
+		cy.remove_doc('Supplier', 'Medlink International Suppliers');
+	});
+});
