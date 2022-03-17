59af5562413e1616c2c0a24541c2aabf1e80538f
marination <maricadsouza221197@gmail.com>
2022-03-17 12:32:37 +0530

    chore: Polish error handling and code sepration
    - Added Typing
    - Moved all job business logic to bom update log
    - Added `run_bom_job` that handles errors and runs either of two methods
    - UX: Replace button disabled until both inputs are filled
    - Show log creation message on UI for correctness
    - APIs return log document as result
    - Converted raw sql to QB
    
    (cherry picked from commit cff91558d4f380cc7566d009ea85ccba36976f69)
    
    # Conflicts:
    #	erpnext/manufacturing/doctype/bom_update_tool/bom_update_tool.py

--- erpnext/manufacturing/doctype/bom_update_tool/bom_update_tool.py
+++ erpnext/manufacturing/doctype/bom_update_tool/bom_update_tool.py
@@ -1,20 +1,23 @@
-# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 import json
+from typing import Dict, List, Optional, TYPE_CHECKING, Union
 
-import click
+if TYPE_CHECKING:
+	from erpnext.manufacturing.doctype.bom_update_log.bom_update_log import BOMUpdateLog
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils import cstr, flt
 from six import string_types
 
-from erpnext.manufacturing.doctype.bom_update_log.bom_update_log import update_cost
+from erpnext.manufacturing.doctype.bom.bom import get_boms_in_bottom_up_order
 
 
 class BOMUpdateTool(Document):
+<<<<<<< HEAD
 	def replace_bom(self):
 		unit_cost = get_new_bom_unit_cost(self.new_bom)
 		self.update_new_bom(unit_cost)
@@ -87,9 +90,13 @@ def get_new_bom_unit_cost(bom):
 	)
 
 	return flt(new_bom_unitcost[0][0]) if new_bom_unitcost else 0
+=======
+	pass
+>>>>>>> cff91558d4 (chore: Polish error handling and code sepration)
 
 
 @frappe.whitelist()
+<<<<<<< HEAD
 def enqueue_replace_bom(args):
 	if isinstance(args, string_types):
 		args = json.loads(args)
@@ -104,9 +111,19 @@ def enqueue_replace_bom(args):
 	create_bom_update_log(boms=args)
 >>>>>>> 4283a13e5a (feat: BOM Update Log)
 	frappe.msgprint(_("Queued for replacing the BOM. It may take a few minutes."))
+=======
+def enqueue_replace_bom(boms: Optional[Union[Dict, str]] = None, args: Optional[Union[Dict, str]] = None) -> "BOMUpdateLog":
+	"""Returns a BOM Update Log (that queues a job) for BOM Replacement."""
+	boms = boms or args
+	if isinstance(boms, str):
+		boms = json.loads(boms)
+>>>>>>> cff91558d4 (chore: Polish error handling and code sepration)
 
+	update_log = create_bom_update_log(boms=boms)
+	return update_log
 
 @frappe.whitelist()
+<<<<<<< HEAD
 def enqueue_update_cost():
 <<<<<<< HEAD
 	frappe.enqueue(
@@ -120,13 +137,20 @@ def enqueue_update_cost():
 	create_bom_update_log(update_type="Update Cost")
 	frappe.msgprint(_("Queued for updating latest price in all Bill of Materials. It may take a few minutes."))
 >>>>>>> 4283a13e5a (feat: BOM Update Log)
+=======
+def enqueue_update_cost() -> "BOMUpdateLog":
+	"""Returns a BOM Update Log (that queues a job) for BOM Cost Updation."""
+	update_log = create_bom_update_log(update_type="Update Cost")
+	return update_log
+>>>>>>> cff91558d4 (chore: Polish error handling and code sepration)
 
 
-def auto_update_latest_price_in_all_boms():
-	"Called via hooks.py."
+def auto_update_latest_price_in_all_boms() -> None:
+	"""Called via hooks.py."""
 	if frappe.db.get_single_value("Manufacturing Settings", "update_bom_costs_automatically"):
 		update_cost()
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 def replace_bom(args):
@@ -172,3 +196,22 @@ def create_bom_update_log(boms=None, update_type="Replace BOM"):
 	})
 	log_doc.submit()
 >>>>>>> 4283a13e5a (feat: BOM Update Log)
+=======
+def update_cost() -> None:
+	"""Updates Cost for all BOMs from bottom to top."""
+	bom_list = get_boms_in_bottom_up_order()
+	for bom in bom_list:
+		frappe.get_doc("BOM", bom).update_cost(update_parent=False, from_child_bom=True)
+
+def create_bom_update_log(boms: Optional[Dict] = None, update_type: str = "Replace BOM") -> "BOMUpdateLog":
+	"""Creates a BOM Update Log that handles the background job."""
+	boms = boms or {}
+	current_bom = boms.get("current_bom")
+	new_bom = boms.get("new_bom")
+	return frappe.get_doc({
+		"doctype": "BOM Update Log",
+		"current_bom": current_bom,
+		"new_bom": new_bom,
+		"update_type": update_type,
+	}).submit()
+>>>>>>> cff91558d4 (chore: Polish error handling and code sepration)
