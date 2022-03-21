d774f95ecb74d1e2fb606d8ec2d88069eb88a2af
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-21 14:39:06 +0530

    Update test_supplier.js

--- cypress/integration/test_supplier.js
+++ cypress/integration/test_supplier.js
@@ -11,9 +11,6 @@ context('Supplier', () => {
 		cy.get_field('supplier_group', 'Link').type("All Supplier Groups");
 		cy.get_field('supplier_name', 'Link').type("Medlink International Suppliers");
 		cy.get('#page-Supplier > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
-	});
-
-	it('Check Supplier attributes', () => {
 		cy.get_field('supplier_name', 'Link').should('have.value', 'Medlink International Suppliers');
 		cy.get_field('supplier_group', 'Link').should('have.value', 'All Supplier Groups');	
 		cy.get('#page-Supplier > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
