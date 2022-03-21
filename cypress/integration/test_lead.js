1484acd29c4651a8506ddae572dfbf9f9182c92e
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-21 14:32:45 +0530

    Update test_lead.js

--- cypress/integration/test_lead.js
+++ cypress/integration/test_lead.js
@@ -19,7 +19,7 @@ context('Lead', () => {
 		cy.get_field('company_name', 'Link').should('have.value', 'Eleanor School of Music');
 		cy.get_field('first_name', 'Link').should('have.value', 'Oliver');
 		cy.get_field('last_name', 'Link').should('have.value', 'Eleanor');
-		cy.get_field('email_id', 'Link').should('have.value','oliverel@gmail.com');
+		cy.get_field('email_id', 'Link').should('have.value', 'oliverel@gmail.com');
 		cy.get('#page-Lead > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
 		cy.remove_doc('Lead', 'CRM-LEAD-2022-00003');
 	});
