e638afd249d2fe3f14cb6a312ffbfa7c25f282b0
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-21 14:28:31 +0530

    Update customer_creation.js

--- cypress/integration/customer_creation.js
+++ cypress/integration/customer_creation.js
@@ -3,21 +3,18 @@ context('Customer', () => {
 	before(() => {
 		cy.login();
 	});
-	it('Insert a Customer ', () => {
+	it('Insert a Customer and check attributes', () => {
 		cy.visit(`app/customer/`);
 		cy.get('.primary-action').click();
 		cy.get('.custom-actions > .btn').click();
 		cy.get_field('customer_group', 'Link').type("All Customer Groups");
 		cy.get_field('customer_name', 'Link').type("Lara Schindler");
 		cy.get_field('territory', 'Link').type("All Territories");
-		cy.get('#page-Customer > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
-	});
-
-	it('Check Customer attributes ', () => {
+		cy.get('.modal-footer > .standard-actions > .btn-primary').contains("Save").trigger('click', {force: true});
 		cy.get_field('customer_group', 'Link').should('have.value', 'All Customer Groups');
 		cy.get_field('customer_name', 'Link').should('have.value', 'Lara Schindler');
 		cy.get_field('territory', 'Link').should('have.value', 'All Territories');
 		cy.get('#page-Customer > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
 		cy.remove_doc('Customer', 'Lara Schindler');		
-    });
+		});
 });
