7c5417c2b137e0ba9ffc528ded762d8f41b17bef
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-21 12:37:30 +0530

    Update item_creation.js

--- cypress/integration/item_creation.js
+++ cypress/integration/item_creation.js
@@ -1,10 +1,10 @@
 context('Item', () => {
-    before(() => {
-        cy.login();
-    });
-
-    it('Create an item', () => {
-        cy.visit(`app/item/`);
+	before(() => {
+		cy.login();
+	});
+	
+	it('Create an item', () => {
+		cy.visit(`app/item/`);
         cy.get('.primary-action').click();
         cy.get('.custom-actions > .btn').click();
         cy.get_field('item_code', 'Data').type("ITM-0018");
@@ -12,16 +12,9 @@ context('Item', () => {
         cy.wait(500);
         cy.get(':nth-child(2) > form > [title="is_stock_item"] > .checkbox > label > .input-area > .input-with-feedback').click();
         cy.get('.modal-footer > .standard-actions > .btn-primary').contains("Save").trigger('click', {force: true});
-
-    })
-
-    it('Check Item attributes', () => {
-        cy.get_field('item_code', 'Data').should('have.value','ITM-0018');
+		cy.get_field('item_code', 'Data').should('have.value','ITM-0018');
         cy.get_field('item_group', 'Link').should('have.value', 'All Item Groups');
         cy.get_field('stock_uom', 'Link').should('have.value','Nos');
         cy.get('#page-Item > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
         cy.remove_doc('Item', 'ITM-0018');
-    });
-   
-});
-
+	});
