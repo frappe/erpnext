a61947457110b74f129aed5f38945c42007c1d9c
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-21 17:18:43 +0530

    Update test_customer.js

--- cypress/integration/test_customer.js
+++ cypress/integration/test_customer.js
@@ -3,11 +3,19 @@ context('Customer', () => {
 	before(() => {
 		cy.login();
 	});
-	it('Check Customer Group', () => {
+	
+	it('Insert a Customer and check attributes', () => {
 		cy.visit(`app/customer/`);
 		cy.get('.primary-action').click();
-		cy.wait(500);
 		cy.get('.custom-actions > .btn').click();
+		cy.get_field('customer_group', 'Link').type("All Customer Groups");
+		cy.get_field('customer_name', 'Link').type("Lara Schindler");
+		cy.get_field('territory', 'Link').type("All Territories");
+		cy.get('.modal-footer > .standard-actions > .btn-primary').contains("Save").trigger('click', {force: true});
 		cy.get_field('customer_group', 'Link').should('have.value', 'All Customer Groups');
+		cy.get_field('customer_name', 'Link').should('have.value', 'Lara Schindler');
+		cy.get_field('territory', 'Link').should('have.value', 'All Territories');
+		cy.get('#page-Customer > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
+		cy.remove_doc('Customer', 'Lara Schindler');		
 	});
 });
