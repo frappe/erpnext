14d1dc3e9a77a561ba6523bfaec216293abc1fda
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-21 13:41:15 +0530

    Update test_lead.js

--- cypress/integration/test_lead.js
+++ cypress/integration/test_lead.js
@@ -4,27 +4,23 @@ context('Lead', () => {
 		cy.login();
 	});
 	
-	it('Insert a Lead ', () => {
-	cy.visit(`app/lead/`);
-	cy.get('.primary-action').click();
+	it('Insert and check attributes of a Lead ', () => {
+		cy.visit(`app/lead/`);
+		cy.get('.primary-action').click();
         cy.get('[title="status"] > .form-group > .control-input-wrapper > .control-input > .input-with-feedback').type("Lead");
         cy.get_field('salutation', 'Link').type("Mr");
-	      cy.get_field('company_name', 'Link').type("Eleanor School of Music");
+	    cy.get_field('company_name', 'Link').type("Eleanor School of Music");
         cy.get_field('first_name', 'Link').type("Oliver");
         cy.get_field('last_name', 'Link').type("Eleanor");
         cy.get_field('gender', 'Link').type("Male");
         cy.get_field('email_id', 'Link').type("oliverel@gmail.com");
         cy.get('#page-Lead > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
-        //cy.get('.modal-footer > .standard-actions > .btn-primary').contains("Save").trigger('click', {force: true});
-    });
-
-	it('Check Lead attributes ', () => {
         cy.get_field('salutation', 'Link').should('have.value', 'Mr');
- 	      cy.get_field('company_name', 'Link').should('have.value', 'Eleanor School of Music');
+ 	    cy.get_field('company_name', 'Link').should('have.value', 'Eleanor School of Music');
         cy.get_field('first_name', 'Link').should('have.value', 'Oliver');
         cy.get_field('last_name', 'Link').should('have.value', 'Eleanor');
         cy.get_field('email_id', 'Link').should('have.value','oliverel@gmail.com');
         cy.get('#page-Lead > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
         cy.remove_doc('Lead', 'CRM-LEAD-2022-00003');
-    });
+	});
 });
