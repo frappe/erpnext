1471b6ab83cde1f87525447be8ace85bd4a3ed16
sherinkr <sherinkrply@gmail.com>
2022-03-23 15:46:13 +0530

    fix: Added validation for single_threshold in Tax With Holding Category

--- erpnext/accounts/doctype/tax_withholding_category/tax_withholding_category.py
+++ erpnext/accounts/doctype/tax_withholding_category/tax_withholding_category.py
@@ -25,7 +25,7 @@ class TaxWithholdingCategory(Document):
 
 	def validate_thresholds(self):
 		for d in self.get('rates'):
-			if d.cumulative_threshold and d.cumulative_threshold < d.single_threshold:
+			if d.cumulative_threshold and d.single_threshold and d.cumulative_threshold < d.single_threshold:
 				frappe.throw(_("Row #{0}: Cumulative threshold cannot be less than Single Transaction threshold").format(d.idx))
 
 def get_party_details(inv):
