0273fd06631102617688749f19f5657dd2e803d7
Deepesh Garg <deepeshgarg6@gmail.com>
2022-03-24 12:56:22 +0530

    fix: Client side changes for POS Write off amount
    (cherry picked from commit 2e33e748ea248acface128f759d3982635024399)

--- erpnext/public/js/controllers/taxes_and_totals.js
+++ erpnext/public/js/controllers/taxes_and_totals.js
@@ -689,13 +689,13 @@ erpnext.taxes_and_totals = class TaxesAndTotals extends erpnext.payments {
 		}));
 		this.frm.doc.total_advance = flt(total_allocated_amount, precision("total_advance"));
 
-		if (this.frm.doc.write_off_outstanding_amount_automatically) {
-			this.frm.doc.write_off_amount = 0;
-		}
-
 		this.calculate_outstanding_amount(update_paid_amount);
-		this.calculate_write_off_amount();
+<<<<<<< HEAD
 	}
+=======
+		this.calculate_write_off_amount();
+	},
+>>>>>>> 2e33e748ea (fix: Client side changes for POS Write off amount)
 
 	is_internal_invoice() {
 		if (['Sales Invoice', 'Purchase Invoice'].includes(this.frm.doc.doctype)) {
@@ -839,14 +839,19 @@ erpnext.taxes_and_totals = class TaxesAndTotals extends erpnext.payments {
 		}
 	}
 
-	calculate_write_off_amount() {
-		if(this.frm.doc.write_off_outstanding_amount_automatically) {
-			this.frm.doc.write_off_amount = flt(this.frm.doc.outstanding_amount, precision("write_off_amount"));
+<<<<<<< HEAD
+	calculate_write_off_amount(){
+		if(this.frm.doc.paid_amount > this.frm.doc.grand_total){
+=======
+	calculate_write_off_amount: function() {
+		if (this.frm.doc.write_off_outstanding_amount_automatically) {
+>>>>>>> 2e33e748ea (fix: Client side changes for POS Write off amount)
+			this.frm.doc.write_off_amount = flt(this.frm.doc.grand_total - this.frm.doc.paid_amount
+				+ this.frm.doc.change_amount, precision("write_off_amount"));
 			this.frm.doc.base_write_off_amount = flt(this.frm.doc.write_off_amount * this.frm.doc.conversion_rate,
 				precision("base_write_off_amount"));
-
-			this.calculate_outstanding_amount(false);
 		}
 
+		this.calculate_outstanding_amount(false);
 	}
 };
