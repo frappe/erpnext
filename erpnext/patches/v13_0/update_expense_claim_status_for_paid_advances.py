4024c43b33b7b014c14f8c9bec4628ef00efa50a
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-03-24 22:36:20 +0530

    fix: Expense Claim conditions for Paid status
    - status doesn't change to Paid if the entire required amount is already covered via linked advances
    
    - status doesn't change to Paid if the claim is partially paid via advances
    
    - patch to update such uncancelled claims to Paid

---
+++ erpnext/patches/v13_0/update_expense_claim_status_for_paid_advances.py
@@ -0,0 +1,21 @@
+import frappe
+
+
+def execute():
+	"""
+	Update Expense Claim status to Paid if:
+		- the entire required amount is already covered via linked advances
+		- the claim is partially paid via advances and the rest is reimbursed
+	"""
+
+	ExpenseClaim = frappe.qb.DocType('Expense Claim')
+
+	(frappe.qb
+		.update(ExpenseClaim)
+		.set(ExpenseClaim.status, 'Paid')
+		.where(
+			((ExpenseClaim.grand_total == 0) | (ExpenseClaim.grand_total == ExpenseClaim.total_amount_reimbursed))
+			& (ExpenseClaim.approval_status == 'Approved')
+			& (ExpenseClaim.docstatus != 2)
+		)
+	).run()
