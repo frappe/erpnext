cd4e47656f9be51ca5776ee40dbaf6a374228c2e
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-03-28 12:04:18 +0530

    feat: get_indicator() in SCO List(js)

---
+++ erpnext/subcontracting/doctype/subcontracting_order/subcontracting_order_list.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings['Subcontracting Order'] = {
+    get_indicator: function (doc) {
+        const status_colors = {
+            "Draft": "grey",
+            "Open": "orange",
+            "Partially Received": "yellow",
+            "Completed": "green",
+            "Material Transferred": "blue",
+            "Partial Material Transferred": "yellow"
+        };
+        return [__(doc.status), status_colors[doc.status], "status,=," + doc.status];
+    }
+};
