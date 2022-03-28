09ef8d7ef76e0c038cc4e0afca5d067b13f41017
Ajay Jogdand <ajayjogdand374@gmail.com>
2022-03-28 17:19:23 +0530

    PriceList Generator

--- erpnext/stock/doctype/pricelist_generator/pricelist_generator.py
+++ erpnext/stock/doctype/pricelist_generator/pricelist_generator.py
@@ -1,67 +1,63 @@
 # Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
+import json
 import frappe
 from frappe.model.document import Document
-from frappe.utils.data import flt
+from frappe.utils.data import flt, getdate
 
 
 class PriceListGenerator(Document):
 
-
 	def validate(self):
+		if getdate(self.start_date)>getdate(self.end_date):
+			frappe.throw("End date Cannot be Less than Start date")
 		for res in self.price_details:
 			if res.brand:
 				lst=frappe.get_doc("Brand",res.brand)
 				for i in lst.brand_defaults:
 					if i.selling_cost_center:
-						if self.company==i.company and self.operating_unit==i.selling_cost_center:
+						if self.company==i.company and self.cost_center==i.selling_cost_center:
 							res.brand_list_price=i.item_price
 						if not i.item_price:
 							frappe.throw("Please set Item Price in brand")
-			doc=frappe.get_doc("Item",res.product)
-			if not doc.specific_gravity:
-				frappe.throw("Please set Specific Gravity On Item")
-			if doc.specific_gravity:
-				res.volume=flt(res.weight)/flt(doc.specific_gravity)
-    
-
-
+			
 	def before_save(self):
-		self.get_cost_weight()
 		self.get_cost_volume()
-
-	def get_cost_weight(self):
-		for res in self.price_details:
-			if res.product:
-				res.list_priceweight = (flt(res.brand_list_price) + flt(res.addpkg_cost)) * flt(res.weight)
 
 
 	def get_cost_volume(self):
 		for res in self.price_details:
-			if res.product:
+			if res.item:
 				res.list_priceunit = (flt(res.brand_list_price) + flt(res.addpkg_cost)) * flt(res.weight)
-				res.list_pricevolume = (flt(res.brand_list_price) + flt(res.addpkg_cost))*flt(res.volume)
-
+			
 	@frappe.whitelist()
-	def get_items_brand(self):
-		doc=frappe.db.sql("""select b.name from `tabBrand` b join `tabItem Default` bd on b.name=bd.parent where bd.selling_cost_center='{0}'""".format(self.operating_unit),as_dict=1)
+	def get_items_brand(self,filters=None):
+		doc=frappe.db.sql("""select b.name,bd.item_price from `tabBrand` b join `tabItem Default` bd on b.name=bd.parent where bd.selling_cost_center='{0}'""".format(self.cost_center),as_dict=1)
 		for i in doc:
-			lst=frappe.db.get_all("Item",{"brand":i.name},["name"])
+			filters.update({
+				"brand":i.name
+			})
+			print(filters)
+			lst=frappe.db.get_all("Item",filters=filters)
+			print(lst)
 			for j in lst:
 				doc=frappe.get_doc("Item",j.name)
-				if doc.item_group==self.item_group:
-					pack_g=frappe.db.value("Product Pack",{"item":doc.name},["name"])
-					if pack_g:
-						pack=frappe.get_doc("Product Pack",{"item":doc.name})	
-						self.append("price_details",{
-							"product":doc.name,
-							"classifications":doc.description,
-							"costunit":doc.valuation_rate,
-							"weight":doc.weight_per_unit,
-							"addpkg_cost":pack.diff_price
-
-						})
+				pack_g=frappe.db.get_value("Product Pack",{"item":doc.name},["name"])
+				if pack_g:
+					pack=frappe.get_doc("Product Pack",{"item":doc.name})	
+				self.append("price_details",{
+					"item":doc.name,
+     				"item_name":doc.item_name,
+					"item_group":doc.item_group,
+					"uom":doc.stock_uom,
+					"costunit":doc.valuation_rate,
+					"weight":doc.weight_per_unit,
+					"brand":doc.brand,
+					"min_qty":1,
+					"addpkg_cost":pack.diff_price,
+     				"brand_list_price":i.item_price
+				})
 		return True
 
 
@@ -76,10 +72,9 @@ class PriceListGenerator(Document):
 		doc.save()
 		for res in self.price_details: 
 			doc_it=frappe.new_doc("Item Price")
-			doc_it.item_code=res.product
+			doc_it.item_code=res.item
 			doc_it.uom=res.uom
 			doc_it.brand=res.brand
 			doc_it.price_list=doc.name
 			doc_it.price_list_rate=res.list_priceunit
-			doc_it.save()
-
+			doc_it.save(ignore_permissions=True)
