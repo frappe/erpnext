6a7d7267eeb78489df4f6c92188dd351e22cd938
Ajay Jogdand <ajayjogdand374@gmail.com>
2022-03-29 15:47:12 +0530

    pricelistGenerator

--- erpnext/stock/doctype/pricelist_generator/pricelist_generator.py
+++ erpnext/stock/doctype/pricelist_generator/pricelist_generator.py
@@ -38,28 +38,28 @@ class PriceListGenerator(Document):
 			
 	@frappe.whitelist()
 	def get_items_brand(self,filters=None):
-		# doc=frappe.db.sql("""select b.name,bd.item_price from `tabBrand` b join `tabItem Default` bd on b.name=bd.parent where bd.selling_cost_center='{0}'""".format(self.cost_center),as_dict=1)
-		# for i in doc:
-		# 	filters.update({
-		# 		"brand":i.name
-		# 	})
-		# 	print(filters)
-		lst=frappe.db.get_all("Item",filters=filters)
-		for j in lst:
-			doc=frappe.get_doc("Item",j.name)
-			pack_g=frappe.db.get_value("Product Pack",{"item":doc.name},["diff_price"])
-			self.append("price_details",{
-				"item":doc.name,
-				"item_name":doc.item_name,
-				"item_group":doc.item_group,
-				"uom":doc.stock_uom,
-				"costunit":doc.valuation_rate,
-				"weight":doc.weight_per_unit,
-				"brand":doc.brand,
-				"min_qty":1,
-				"addpkg_cost":pack_g,
-				"brand_list_price":i.item_price
+		doc=frappe.db.sql("""select b.name,bd.item_price from `tabBrand` b join `tabItem Default` bd on b.name=bd.parent where bd.selling_cost_center='{0}'""".format(self.cost_center),as_dict=1)
+		for i in doc:
+			filters.update({
+				"brand":i.name
 			})
+			print(filters)
+			lst=frappe.db.get_all("Item",filters=filters)
+			for j in lst:
+				doc=frappe.get_doc("Item",j.name)
+				pack_g=frappe.db.get_value("Product Pack",{"item":doc.name},["diff_price"])
+				self.append("price_details",{
+					"item":doc.name,
+     				"item_name":doc.item_name,
+					"item_group":doc.item_group,
+					"uom":doc.stock_uom,
+					"costunit":doc.valuation_rate,
+					"weight":doc.weight_per_unit,
+					"brand":doc.brand,
+					"min_qty":1,
+					"addpkg_cost":pack_g,
+     				"brand_list_price":i.item_price
+				})
 		return True
 
 
