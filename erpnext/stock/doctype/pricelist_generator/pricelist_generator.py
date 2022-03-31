6c5d529e1fa320214d64d894fb9acc519cbc4ef2
Ajay Jogdand <ajayjogdand374@gmail.com>
2022-03-31 18:58:04 +0530

    pricelist

--- erpnext/stock/doctype/pricelist_generator/pricelist_generator.py
+++ erpnext/stock/doctype/pricelist_generator/pricelist_generator.py
@@ -34,14 +34,14 @@ class PriceListGenerator(Document):
 			
 	@frappe.whitelist()
 	def get_items_brand(self,filters=None):
-		doc=frappe.db.sql("""select b.name,bd.item_price,bd.selling_cost_center from `tabBrand` b join `tabItem Default` bd on b.name=bd.parent where bd.selling_cost_center='{0}'""".format(self.cost_center),as_dict=1)
-		print(doc)
-		for i in doc:
-
-			lst=frappe.db.get_all("Item",filters=filters)
-			for j in lst:
-				doc=frappe.get_doc("Item",j.name)
-				if i.selling_cost_center==self.cost_center and doc.brand==i.name:
+	
+		lst=frappe.db.get_all("Item",filters=filters)
+		for j in lst:
+			doc=frappe.get_doc("Item",j.name)
+			doc1=frappe.db.sql("""select b.name,bd.item_price,bd.selling_cost_center,bd.item_price from `tabBrand` b join `tabItem Default` bd on b.name=bd.parent where bd.selling_cost_center='{0}'""".format(self.cost_center),as_dict=1)
+			print(doc1)
+			for i in doc1:
+				if i.selling_cost_center==self.cost_center and doc.brand==i.name and item_price > 0  :
 					pack_g=frappe.db.get_value("Product Pack",{"item":doc.name},["diff_price"])
 					self.append("price_details",{
 						"item":doc.name,
@@ -50,6 +50,7 @@ class PriceListGenerator(Document):
 						"uom":doc.stock_uom,
 						"costunit":doc.valuation_rate,
 						"weight":doc.weight_per_unit,
+						"weight_uom":doc.weight_uom,
 						"brand":doc.brand,
 						"min_qty":1,
 						"addpkg_cost":pack_g,
@@ -61,8 +62,8 @@ class PriceListGenerator(Document):
 	def on_submit(self):
 		doc=frappe.new_doc("Price List")
 		doc.price_list_name=self.pricelist_name
-		doc.selling=1
-		doc.buying=1
+		doc.selling=self.selling
+		doc.buying=self.buying
 		doc.append("countries", {
 			"Country":"India"
 		})
@@ -70,7 +71,6 @@ class PriceListGenerator(Document):
 		for res in self.price_details: 
 			doc_it=frappe.new_doc("Item Price")
 			doc_it.item_code=res.item
-			doc_it.uom=res.uom
 			doc_it.brand=res.brand
 			doc_it.price_list=doc.name
 			doc_it.price_list_rate=res.list_priceunit
