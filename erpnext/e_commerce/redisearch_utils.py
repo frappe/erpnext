42ec9db5f422c7392d5e9b7e3af3c548996172aa
marination <maricadsouza221197@gmail.com>
2022-04-04 12:33:25 +0530

    fix: Payload incorrect data (pass item_group.name)

--- erpnext/e_commerce/redisearch_utils.py
+++ erpnext/e_commerce/redisearch_utils.py
@@ -205,7 +205,7 @@ def create_item_groups_autocomplete_dict(autocompleter):
 		return
 
 	for item_group in published_item_groups:
-		payload = json.dumps({"name": item_group, "route": item_group.route})
+		payload = json.dumps({"name": item_group.name, "route": item_group.route})
 		autocompleter.add_suggestions(
 			Suggestion(
 				string=item_group.name,
