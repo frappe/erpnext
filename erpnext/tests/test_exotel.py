4a61840e39d673d8e2fdf33588c0424b21398b83
Suraj Shetty <surajshetty3416@gmail.com>
2022-04-04 11:15:29 +0530

    test: Use FrappeAPITestCase to track coverage

--- erpnext/tests/test_exotel.py
+++ erpnext/tests/test_exotel.py
@@ -1,19 +1,20 @@
-import os
-import time
-import unittest
-
 import frappe
-import requests
 from frappe.contacts.doctype.contact.test_contact import create_contact
+from frappe.tests.test_api import FrappeAPITestCase
 
 from erpnext.hr.doctype.employee.test_employee import make_employee
 
 
-class TestExotel(unittest.TestCase):
-	def setUp(self):
-		make_employee("test_employee_exotel@company.com", cell_number="9999999999")
+class TestExotel(FrappeAPITestCase):
+	@classmethod
+	def setUpClass(cls):
+		cls.CURRENT_DB_CONNECTION = frappe.db
+		cls.test_employee_name = make_employee(
+			user="test_employee_exotel@company.com", cell_number="9999999999"
+		)
 		phones = [{"phone": "+91 9999999991", "is_primary_phone": 0, "is_primary_mobile_no": 1}]
-		create_contact("Test Contact", "Mr", phones=phones)
+		create_contact(name="Test Contact", salutation="Mr", phones=phones)
+		frappe.db.commit()
 
 	def test_for_successful_call(self):
 		from .exotel_test_data import call_end_data, call_initiation_data
@@ -21,54 +22,48 @@ class TestExotel(unittest.TestCase):
 		api_method = "handle_incoming_call"
 		end_call_api_method = "handle_end_call"
 
-		emulate_api_call(call_initiation_data, api_method)
-		emulate_api_call(call_end_data, end_call_api_method)
-
+		self.emulate_api_call_from_exotel(api_method, call_initiation_data)
+		self.emulate_api_call_from_exotel(end_call_api_method, call_end_data)
 		call_log = frappe.get_doc("Call Log", call_initiation_data.CallSid)
 
 		self.assertEqual(call_log.get("from"), call_initiation_data.CallFrom)
 		self.assertEqual(call_log.get("to"), call_initiation_data.DialWhomNumber)
-		self.assertEqual(call_log.get("call_received_by"), "EMP-00001")
+		self.assertEqual(call_log.get("call_received_by"), self.test_employee_name)
 		self.assertEqual(call_log.get("status"), "Completed")
 
 	def test_for_disconnected_call(self):
 		from .exotel_test_data import call_disconnected_data
 
 		api_method = "handle_missed_call"
-		emulate_api_call(call_disconnected_data, api_method)
+		self.emulate_api_call_from_exotel(api_method, call_disconnected_data)
 		call_log = frappe.get_doc("Call Log", call_disconnected_data.CallSid)
 
 		self.assertEqual(call_log.get("from"), call_disconnected_data.CallFrom)
 		self.assertEqual(call_log.get("to"), call_disconnected_data.DialWhomNumber)
-		self.assertEqual(call_log.get("call_received_by"), "EMP-00001")
+		self.assertEqual(call_log.get("call_received_by"), self.test_employee_name)
 		self.assertEqual(call_log.get("status"), "Canceled")
 
 	def test_for_call_not_answered(self):
 		from .exotel_test_data import call_not_answered_data
 
 		api_method = "handle_missed_call"
-		emulate_api_call(call_not_answered_data, api_method)
-
+		self.emulate_api_call_from_exotel(api_method, call_not_answered_data)
 		call_log = frappe.get_doc("Call Log", call_not_answered_data.CallSid)
-
 		self.assertEqual(call_log.get("from"), call_not_answered_data.CallFrom)
 		self.assertEqual(call_log.get("to"), call_not_answered_data.DialWhomNumber)
-		self.assertEqual(call_log.get("call_received_by"), "EMP-00001")
+		self.assertEqual(call_log.get("call_received_by"), self.test_employee_name)
 		self.assertEqual(call_log.get("status"), "No Answer")
 
-	def tearDown(self):
-		frappe.db.rollback()
+	def emulate_api_call_from_exotel(self, api_method, data):
+		self.post(
+			f"/api/method/erpnext.erpnext_integrations.exotel_integration.{api_method}",
+			data=frappe.as_json(data),
+			content_type="application/json",
+			as_tuple=True,
+		)
+		# restart db connection to get latest data
+		frappe.connect()
 
-
-def emulate_api_call(data, api_method):
-	# Build URL
-	url = get_exotel_handler_endpoint(api_method)
-	res = requests.post(url=url, data=frappe.as_json(data))
-	res.raise_for_status()
-	time.sleep(1)
-
-
-def get_exotel_handler_endpoint(method):
-	site = "localhost" if os.environ.get("CI") else frappe.local.site
-	port = frappe.get_site_config().webserver_port or "8000"
-	return f"http://{site}:{port}/api/method/erpnext.erpnext_integrations.exotel_integration.{method}"
+	@classmethod
+	def tearDownClass(cls):
+		frappe.db = cls.CURRENT_DB_CONNECTION
