ca48b9de54e684f65ba9e6b6c736b9a7f81b9101
Suraj Shetty <surajshetty3416@gmail.com>
2022-04-04 09:31:15 +0530

    test: Fix erroneous code

--- erpnext/tests/test_exotel.py
+++ erpnext/tests/test_exotel.py
@@ -16,15 +16,14 @@ class TestExotel(unittest.TestCase):
 		create_contact("Test Contact", "Mr", phones=phones)
 
 	def test_for_successful_call(self):
-		from .exotel_test_data import call_initiation_data, call_end_data
+		from .exotel_test_data import call_end_data, call_initiation_data
+
 		api_method = "handle_incoming_call"
 		end_call_api_method = "handle_end_call"
 
-
 		emulate_api_call(call_initiation_data, api_method)
 		emulate_api_call(call_end_data, end_call_api_method)
-		self.assertEqual(call_log.get("from"), "09999999991")
-		self.assertEqual(call_log.get("to"), "09999999999")
+
 		call_log = frappe.get_doc("Call Log", call_initiation_data.CallSid)
 
 		self.assertEqual(call_log.get("from"), call_initiation_data.CallFrom)
@@ -34,6 +33,7 @@ class TestExotel(unittest.TestCase):
 
 	def test_for_disconnected_call(self):
 		from .exotel_test_data import call_disconnected_data
+
 		api_method = "handle_missed_call"
 		emulate_api_call(call_disconnected_data, api_method)
 		call_log = frappe.get_doc("Call Log", call_disconnected_data.CallSid)
@@ -45,6 +45,7 @@ class TestExotel(unittest.TestCase):
 
 	def test_for_call_not_answered(self):
 		from .exotel_test_data import call_not_answered_data
+
 		api_method = "handle_missed_call"
 		emulate_api_call(call_not_answered_data, api_method)
 
@@ -65,6 +66,7 @@ def emulate_api_call(data, api_method):
 	res = requests.post(url=url, data=frappe.as_json(data))
 	res.raise_for_status()
 	time.sleep(1)
+
 
 def get_exotel_handler_endpoint(method):
 	site = "localhost" if os.environ.get("CI") else frappe.local.site
