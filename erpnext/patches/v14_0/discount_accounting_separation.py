ac16f3b71fc403d2ad1f37f7ca54dc6236aa909c
rahib-hassan <rahib.hassan10@gmail.com>
2022-04-08 18:20:41 +0530

    fix: lock timeout exceeded

--- erpnext/patches/v14_0/discount_accounting_separation.py
+++ erpnext/patches/v14_0/discount_accounting_separation.py
@@ -4,11 +4,5 @@ def execute():
     doc = frappe.get_doc("Accounts Settings")
     discount_account = doc.enable_discount_accounting
     if discount_account:
-        buying_settings = frappe.get_doc("Buying Settings", "Buying Settings")
-        selling_settings = frappe.get_doc("Selling Settings", "Selling Settings")
-
-        buying_settings.enable_discount_accounting = 1
-        selling_settings.enable_discount_accounting = 1
-
-        buying_settings.save()
-        selling_settings.save()
+        for doctype in ["Buying Settings", "Selling Settings"]:
+            doc = frappe.db.set_value(doctype, doctype, 'enable_discount_accounting', 1, update_modified=False)
