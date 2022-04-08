35e875c11131465fe41be6a215c74cf81d47e67e
rahib-hassan <rahib.hassan10@gmail.com>
2022-04-08 17:27:53 +0530

    fix(patch): enable discount account in buying and selling if exist in accounts settings

---
+++ erpnext/patches/v14_0/discount_accounting_separation.py
@@ -0,0 +1,14 @@
+import frappe
+
+def execute():
+    doc = frappe.get_doc("Accounts Settings")
+    discount_account = doc.enable_discount_accounting
+    if discount_account:
+        buying_settings = frappe.get_doc("Buying Settings")
+        selling_settings = frappe.get_doc("Selling Settings")
+
+        buying_settings.enable_discount_accounting = 1
+        selling_settings.enable_discount_accounting = 1
+
+        buying_settings.save()
+        selling_settings.save()
