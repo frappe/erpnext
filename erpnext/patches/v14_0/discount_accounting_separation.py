ac7f1cbd87efe933f785a3476a478bb773bb7601
rahib-hassan <rahib.hassan10@gmail.com>
2022-04-08 17:46:49 +0530

    fix: minor fix

--- erpnext/patches/v14_0/discount_accounting_separation.py
+++ erpnext/patches/v14_0/discount_accounting_separation.py
@@ -4,8 +4,8 @@ def execute():
     doc = frappe.get_doc("Accounts Settings")
     discount_account = doc.enable_discount_accounting
     if discount_account:
-        buying_settings = frappe.get_doc("Buying Settings")
-        selling_settings = frappe.get_doc("Selling Settings")
+        buying_settings = frappe.get_doc("Buying Settings", "Buying Settings")
+        selling_settings = frappe.get_doc("Selling Settings", "Selling Settings")
 
         buying_settings.enable_discount_accounting = 1
         selling_settings.enable_discount_accounting = 1
