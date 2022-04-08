dc2944a0410811ce800107fbb3a531b5eb440498
rahib-hassan <rahib.hassan10@gmail.com>
2022-04-09 03:55:25 +0530

    fix[minor]: removed doc assignment

--- erpnext/patches/v14_0/discount_accounting_separation.py
+++ erpnext/patches/v14_0/discount_accounting_separation.py
@@ -5,4 +5,4 @@ def execute():
     discount_account = doc.enable_discount_accounting
     if discount_account:
         for doctype in ["Buying Settings", "Selling Settings"]:
-            doc = frappe.db.set_value(doctype, doctype, 'enable_discount_accounting', 1, update_modified=False)
+            frappe.db.set_value(doctype, doctype, 'enable_discount_accounting', 1, update_modified=False)
