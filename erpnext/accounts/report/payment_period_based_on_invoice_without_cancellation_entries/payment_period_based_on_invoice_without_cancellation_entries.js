8b6e3ffaa801a46e8221e33b498f053def2a5550
Shubhamk-10 <shubhamkuthe5@gmail.com>
2022-04-12 18:43:14 +0530

    new reports added

---
+++ erpnext/accounts/report/payment_period_based_on_invoice_without_cancellation_entries/payment_period_based_on_invoice_without_cancellation_entries.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Payment Period Based On Invoice Without Cancellation Entries"] = {
+	"filters": [
+
+	]
+};
