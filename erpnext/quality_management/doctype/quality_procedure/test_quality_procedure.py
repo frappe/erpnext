79fbc2c504f94d898e118a9af0f37725e79c4323
Ankush Menat <ankush@frappe.io>
2022-04-13 20:42:25 +0530

    test: flaky PR close test (#30709)

--- erpnext/quality_management/doctype/quality_procedure/test_quality_procedure.py
+++ erpnext/quality_management/doctype/quality_procedure/test_quality_procedure.py
@@ -19,6 +19,7 @@ class TestQualityProcedure(unittest.TestCase):
 				)
 			).insert()
 
+
 			frappe.local.form_dict = frappe._dict(
 				doctype="Quality Procedure",
 				quality_procedure_name="Test Child 1",
