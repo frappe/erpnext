06eea2f797e7122772f062967c819ea7b4e88ae4
Saif Ur Rehman <saifi0102@gmail.com>
2022-04-18 18:22:20 +0500

    fix(Project): list view filter queries

--- erpnext/projects/doctype/project/project_list.js
+++ erpnext/projects/doctype/project/project_list.js
@@ -29,4 +29,39 @@ frappe.listview_settings['Project'] = {
 			];
 		}
 	},
+
+	onload: function(listview) {
+		if (listview.page.fields_dict.customer) {
+			listview.page.fields_dict.customer.get_query = () => {
+				return erpnext.queries.customer();
+			}
+		}
+		if (listview.page.fields_dict.bill_to) {
+			listview.page.fields_dict.bill_to.get_query = () => {
+				return erpnext.queries.customer();
+			}
+		}
+
+		if (listview.page.fields_dict.applies_to_variant_of) {
+			listview.page.fields_dict.applies_to_variant_of.get_query = () => {
+				return erpnext.queries.item({"is_vehicle": 1, "has_variants": 1, "include_disabled": 1});
+			}
+		}
+
+		if (listview.page.fields_dict.applies_to_item) {
+			listview.page.fields_dict.applies_to_item.get_query = () => {
+				var filters = {"include_disabled": 1};
+
+				var variant_of;
+				if (listview.page.fields_dict.applies_to_variant_of) {
+					variant_of = listview.page.fields_dict.applies_to_variant_of.get_value('applies_to_variant_of')
+				}
+				if (variant_of) {
+					filters['variant_of'] = variant_of;
+				}
+
+				return erpnext.queries.item(filters);
+			}
+		}
+	}
 };
