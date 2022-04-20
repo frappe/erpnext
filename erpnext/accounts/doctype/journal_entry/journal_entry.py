f42e66563f3041ef0ffde61f088daad487931d23
ruthra kumar <ruthra@erpnext.com>
2022-04-20 13:50:18 +0530

    refactor: ignore linked ple for JE

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -94,7 +94,7 @@ class JournalEntry(AccountsController):
 		from erpnext.payroll.doctype.salary_slip.salary_slip import unlink_ref_doc_from_salary_slip
 		unlink_ref_doc_from_payment_entries(self)
 		unlink_ref_doc_from_salary_slip(self.name)
-		self.ignore_linked_doctypes = ('GL Entry', 'Stock Ledger Entry')
+		self.ignore_linked_doctypes = ('GL Entry', 'Stock Ledger Entry', 'Payment Ledger Entry')
 		self.make_gl_entries(1)
 		self.update_advance_paid()
 		self.update_expense_claim()
