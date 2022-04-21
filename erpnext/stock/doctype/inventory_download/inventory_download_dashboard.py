bcf6fd0d6260496c646e4be4e7f4ea00713b34a0
GersonCaballero <gersoncaballero306@gmail.com>
2022-04-21 20:01:21 +0000

    ResolveIssues

---
+++ erpnext/stock/doctype/inventory_download/inventory_download_dashboard.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from frappe import _
+
+
+def get_data():
+	return {
+		'fieldname': 'inventory_download',
+		'transactions': [
+			{
+				'label': _('Journal Entry'),
+				'items': ['Journal Entry']
+			}
+		]
+	}
