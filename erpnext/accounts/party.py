88e257ff6d89ba03e3b273be33fec62714751b69
Ankush Menat <ankush@frappe.io>
2022-04-24 20:17:17 +0530

    fix: column name in party query

--- erpnext/accounts/party.py
+++ erpnext/accounts/party.py
@@ -831,9 +831,9 @@ def get_party_shipping_address(doctype, name):
 		"where "
 		"dl.link_doctype=%s "
 		"and dl.link_name=%s "
-		'and dl.parenttype="Address" '
+		"and dl.parenttype='Address' "
 		"and ifnull(ta.disabled, 0) = 0 and"
-		'(ta.address_type="Shipping" or ta.is_shipping_address=1) '
+		"(ta.address_type='Shipping' or ta.is_shipping_address=1) "
 		"order by ta.is_shipping_address desc, ta.address_type desc limit 1",
 		(doctype, name),
 	)
