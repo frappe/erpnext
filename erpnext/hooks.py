95f79cce85e35fd2d37eaed59a3b7a161a4795d5
Deepesh Garg <deepeshgarg6@gmail.com>
2022-04-26 14:01:13 +0530

    fix: Consistent accounting dimensions across Sales and Purchase docs

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -521,6 +521,9 @@ accounting_dimension_doctypes = [
 	"Subscription Plan",
 	"POS Invoice",
 	"POS Invoice Item",
+	"Purchase Order",
+	"Purchase Receipt",
+	"Sales Order",
 ]
 
 regional_overrides = {
