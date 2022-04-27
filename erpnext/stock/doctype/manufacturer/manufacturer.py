b883c2107f21ab5d4eed46f4d17b4f2690fe1ecd
Govind S Menokee <govindsmenokee@gmail.com>
2022-04-28 01:11:03 +0530

    fix: update default_item_manufacturer on rename
    update default_item_manufacturer on rename of Manufacturer Doctype

--- erpnext/stock/doctype/manufacturer/manufacturer.py
+++ erpnext/stock/doctype/manufacturer/manufacturer.py
@@ -10,3 +10,19 @@ class Manufacturer(Document):
 	def onload(self):
 		"""Load address and contacts in `__onload`"""
 		load_address_and_contact(self)
+	
+	def after_rename(self, old_name, new_name, merge):
+		frappe.db.sql(
+			"""
+			update
+				tabItem
+			set
+				default_item_manufacturer = %s
+			where
+				default_item_manufacturer = %s
+			""",
+			(
+				new_name,
+				old_name
+			)
+		)
