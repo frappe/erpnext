35c7da36dd34aaeb8bbb930c7bd6c04ec4d83fc7
GangaManoj <ganga.manoj98@gmail.com>
2022-04-28 01:47:34 +0530

    feat: Create Asset Serial No

--- erpnext/assets/doctype/asset_serial_no_/asset_serial_no_.js
+++ erpnext/assets/doctype/asset_serial_no_/asset_serial_no_.js
@@ -1,8 +1,124 @@
-// Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Asset Serial No_', {
-	// refresh: function(frm) {
+	onload: function(frm) {
+		frm.set_query('asset', function() {
+			return {
+				'filters': {
+					'is_serialized_asset': 1
+				}
+			};
+		});
+	},
 
-	// }
+	refresh: function(frm) {
+		frm.trigger('toggle_depreciation_fields');
+	},
+
+	asset: (frm) => {
+		frm.trigger('toggle_depreciation_fields');
+	},
+
+	toggle_depreciation_fields: (frm) => {
+		if (frm.doc.asset) {
+			frappe.db.get_value('Asset', frm.doc.asset, ['calculate_depreciation', 'is_existing_asset'], (r) => {
+				if (r && r.calculate_depreciation) {
+					frm.set_df_property('available_for_use_date', 'hidden', 0);
+					frm.set_df_property('depreciation_posting_start_date', 'hidden', 0);
+					frm.set_df_property('salvage_value', 'hidden', 0);
+
+					frm.toggle_reqd('available_for_use_date', 1);
+					frm.toggle_reqd('depreciation_posting_start_date', 1);
+					frm.toggle_reqd('salvage_value', 1);
+
+					if (r.is_existing_asset) {
+						frm.set_df_property('opening_accumulated_depreciation', 'hidden', 0);
+					} else {
+						frm.set_df_property('opening_accumulated_depreciation', 'hidden', 1);
+					}
+
+					frappe.db.get_single_value('Accounts Settings', 'enable_finance_books')
+					.then((value) => {
+						if (value) {
+							frm.set_df_property('finance_books', 'hidden', 0);
+							frm.set_df_property('finance_books', 'reqd', 1);
+
+							frm.set_df_property('depreciation_template', 'hidden', 1);
+							frm.set_df_property('depreciation_template', 'reqd', 0);
+						} else {
+							frm.set_df_property('finance_books', 'hidden', 1);
+							frm.set_df_property('finance_books', 'reqd', 0);
+
+							frm.set_df_property('depreciation_template', 'hidden', 0);
+							frm.set_df_property('depreciation_template', 'reqd', 1);
+						}
+					});
+				}
+				else {
+					frm.set_df_property('available_for_use_date', 'hidden', 1);
+					frm.set_df_property('depreciation_posting_start_date', 'hidden', 1);
+					frm.set_df_property('opening_accumulated_depreciation', 'hidden', 1);
+					frm.set_df_property('depreciation_template', 'hidden', 1);
+					frm.set_df_property('salvage_value', 'hidden', 1);
+					frm.set_df_property('finance_books', 'hidden', 1);
+
+					frm.toggle_reqd('available_for_use_date', 0);
+					frm.toggle_reqd('depreciation_posting_start_date', 0);
+					frm.toggle_reqd('depreciation_template', 0);
+					frm.toggle_reqd('salvage_value', 0);
+					frm.toggle_reqd('finance_books', 0);
+				}
+			});
+		} else {
+			frm.set_df_property('available_for_use_date', 'hidden', 1);
+			frm.set_df_property('depreciation_posting_start_date', 'hidden', 1);
+			frm.set_df_property('opening_accumulated_depreciation', 'hidden', 1);
+			frm.set_df_property('depreciation_template', 'hidden', 1);
+			frm.set_df_property('salvage_value', 'hidden', 1);
+			frm.set_df_property('finance_books', 'hidden', 1);
+
+			frm.toggle_reqd('available_for_use_date', 0);
+			frm.toggle_reqd('depreciation_posting_start_date', 0);
+			frm.toggle_reqd('depreciation_template', 0);
+			frm.toggle_reqd('salvage_value', 0);
+			frm.toggle_reqd('finance_books', 0);
+		}
+	},
+
+	depreciation_template: function(frm) {
+		if (frm.doc.depreciation_template) {
+			frappe.db.get_value('Depreciation Template', frm.doc.depreciation_template, ['asset_life', 'asset_life_unit'], (r) => {
+				if (r) {
+					if (r.asset_life_unit == 'Years') {
+						frm.set_value('asset_life_in_months', (r.asset_life * 12));
+					}
+				}
+			})
+		}
+	},
+
+	depreciation_posting_start_date: function(frm) {
+		if (frm.doc.available_for_use_date && frm.doc.depreciation_posting_start_date == frm.doc.available_for_use_date) {
+			frappe.msgprint(__('Depreciation Posting Date should not be equal to Available for Use Date.'));
+
+			frm.set_value('depreciation_posting_start_date', '');
+			frm.refresh_field('depreciation_posting_start_date');
+		}
+	},
+});
+
+frappe.ui.form.on('Asset Finance Book', {
+	depreciation_template: function(frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+
+		frappe.db.get_value('Depreciation Template', row.depreciation_template, ['asset_life', 'asset_life_unit'], (r) => {
+			if (r) {
+				if (r.asset_life_unit == 'Years') {
+					row.asset_life_in_months = r.asset_life * 12;
+					frm.refresh_field('finance_books');
+				}
+			}
+		})
+	},
 });
--- erpnext/assets/doctype/asset_serial_no_/asset_serial_no_.py
+++ erpnext/assets/doctype/asset_serial_no_/asset_serial_no_.py
@@ -1,8 +1,94 @@
-# Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe import _
 
-class AssetSerialNo_(Document):
-	pass
+from erpnext.controllers.base_asset import BaseAsset, get_finance_books
+
+
+class AssetSerialNo(BaseAsset):
+	def validate(self):
+		self.validate_asset()
+		super().validate()
+
+	def before_submit(self):
+		self.validate_location()
+		super().before_submit()
+
+	def validate_asset(self):
+		is_serialized_asset = frappe.db.get_value("Asset", self.asset, "is_serialized_asset")
+
+		if not is_serialized_asset:
+			frappe.throw(_("{0} is not a Serialized Asset")
+				.format(frappe.bold(self.asset)), title=_("Invalid Asset"))
+
+	def validate_location(self):
+		if not self.location:
+			frappe.throw(_("Please enter Location"), title=_("Missing Field"))
+
+@frappe.whitelist()
+def create_asset_serial_no_docs(asset, num_of_assets=None):
+	asset, finance_books, asset_value = get_asset_values(asset)
+	start, total_num_of_assets = get_iteration_limits(asset, num_of_assets)
+
+	created_serial_nos = []
+	for i in range(start, total_num_of_assets):
+		serial_no = frappe.get_doc({
+			"doctype": "Asset Serial No_",
+			"asset": asset.name,
+			"serial_no": get_serial_no(asset.name, i),
+			"asset_value": asset_value,
+			"finance_books": finance_books
+		})
+		serial_no.save(ignore_permissions=True)
+
+		created_serial_nos.append(serial_no.name)
+
+	update_asset(asset, total_num_of_assets)
+	display_message_on_successful_creation(created_serial_nos)
+
+def get_asset_values(asset):
+	if isinstance(asset, str):
+		asset = frappe.get_doc("Asset", asset)
+
+	finance_books = []
+	if asset.calculate_depreciation:
+		finance_books = get_finance_books(asset.asset_category)
+
+	asset_value = asset.get_initial_asset_value()
+
+	return asset, finance_books, asset_value
+
+def get_iteration_limits(asset, num_of_assets):
+	if not num_of_assets:
+		start = 0
+		total_num_of_assets = asset.num_of_assets
+	else:
+		start = asset.num_of_assets
+		total_num_of_assets = int(num_of_assets) + start
+
+	return start, total_num_of_assets
+
+def update_asset(asset, total_num_of_assets):
+	if asset.num_of_assets != total_num_of_assets:
+		asset.flags.ignore_validate_update_after_submit = True
+		asset.num_of_assets = total_num_of_assets
+		asset.save()
+
+def display_message_on_successful_creation(created_serial_nos):
+	num_of_serial_nos_created = len(created_serial_nos)
+
+	if num_of_serial_nos_created > 5:
+		message = _("{0} Asset Serial Nos created successfully.").format(frappe.bold(num_of_serial_nos_created))
+	else:
+		serial_no_links = list(map(lambda d: frappe.utils.get_link_to_form("Asset Serial No", d), created_serial_nos))
+		serial_no_links = frappe.bold(",".join(serial_no_links))
+
+		is_plural = "s" if num_of_serial_nos_created != 1 else ""
+		message = _("Asset Serial No{0} {1} created successfully.").format(is_plural, serial_no_links)
+
+	frappe.msgprint(message, title="Sucess", indicator="green")
+
+def get_serial_no(asset_name, num_of_assets_created):
+	return asset_name + "-" + str(num_of_assets_created + 1)
