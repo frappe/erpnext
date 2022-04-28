348157a4490149be69c9feeb984de134f3deed6e
Saif Ur Rehman <saifi0102@gmail.com>
2022-04-28 16:59:40 +0500

    fix: Patch fix

--- erpnext/patches/v12_0/change_vehicle_checklist_parentfield.py
+++ erpnext/patches/v12_0/change_vehicle_checklist_parentfield.py
@@ -2,8 +2,9 @@ import frappe
 
 
 def execute():
-	frappe.db.sql("""
-		update `tabVehicle Checklist Item`
-		set parentfield = 'vehicle_checklist'
-		where parenttype = 'Vehicles Settings' and parentfield = 'vehicle_checklist_items'
-	""")
+	if frappe.db.table_exists('tabVehicle Checklist Item'):
+		frappe.db.sql("""
+			update `tabVehicle Checklist Item`
+			set parentfield = 'vehicle_checklist'
+			where parenttype = 'Vehicles Settings' and parentfield = 'vehicle_checklist_items'
+		""")
