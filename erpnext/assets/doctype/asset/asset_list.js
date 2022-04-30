f87c25add3f5897d2b06a35be4ec28b96419e59f
GangaManoj <ganga.manoj98@gmail.com>
2022-04-30 17:35:19 +0530

    fix: Function path

--- erpnext/assets/doctype/asset/asset_list.js
+++ erpnext/assets/doctype/asset/asset_list.js
@@ -36,7 +36,7 @@ frappe.listview_settings['Asset'] = {
 		me.page.add_action_item('Make Asset Movement', function() {
 			const assets = me.get_checked_items();
 			frappe.call({
-				method: "erpnext.assets.doctype.asset.asset.make_asset_movement",
+				method: "erpnext.controllers.base_asset.make_asset_movement",
 				freeze: true,
 				args:{
 					"assets": assets
