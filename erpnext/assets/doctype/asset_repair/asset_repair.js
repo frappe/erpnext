60239b9a2188ecc07dc24ced4f6e1f309094a0e9
GangaManoj <ganga.manoj98@gmail.com>
2022-04-30 18:25:56 +0530

    feat: Toggle display for serial no and num of assets

--- erpnext/assets/doctype/asset_repair/asset_repair.js
+++ erpnext/assets/doctype/asset_repair/asset_repair.js
@@ -28,42 +28,103 @@ frappe.ui.form.on('Asset Repair', {
 				}
 			};
 		};
+
+		frm.fields_dict.serial_no.get_query = function(doc) {
+			return {
+				filters: {
+					'asset': doc.asset
+				}
+			};
+		};
 	},
 
 	refresh: function(frm) {
+		frm.trigger('toggle_serial_no_and_num_of_assets');
+
 		if (frm.doc.docstatus) {
-			frm.add_custom_button(__("View General Ledger"), function() {
+			frm.add_custom_button('View General Ledger', function() {
 				frappe.route_options = {
-					"voucher_no": frm.doc.name
+					'voucher_no': frm.doc.name
 				};
-				frappe.set_route("query-report", "General Ledger");
+				frappe.set_route('query-report', 'General Ledger');
 			});
 		}
 	},
 
 	repair_status: (frm) => {
-		if (frm.doc.completion_date && frm.doc.repair_status == "Completed") {
+		if (frm.doc.completion_date && frm.doc.repair_status == 'Completed') {
 			frappe.call ({
-				method: "erpnext.assets.doctype.asset_repair.asset_repair.get_downtime",
+				method: 'erpnext.assets.doctype.asset_repair.asset_repair.get_downtime',
 				args: {
-					"failure_date":frm.doc.failure_date,
-					"completion_date":frm.doc.completion_date
+					'failure_date':frm.doc.failure_date,
+					'completion_date':frm.doc.completion_date
 				},
 				callback: function(r) {
 					if(r.message) {
-						frm.set_value("downtime", r.message + " Hrs");
+						frm.set_value('downtime', r.message + ' Hrs');
 					}
 				}
 			});
 		}
 
-		if (frm.doc.repair_status == "Completed") {
+		if (frm.doc.repair_status == 'Completed') {
 			frm.set_value('completion_date', frappe.datetime.now_datetime());
 		}
 	},
 
 	stock_items_on_form_rendered() {
 		erpnext.setup_serial_or_batch_no();
+	},
+
+	asset: (frm) => {
+		frm.trigger('toggle_serial_no_and_num_of_assets');
+	},
+
+	toggle_serial_no_and_num_of_assets: (frm) => {
+		if (frm.doc.asset) {
+			frappe.db.get_value('Asset', frm.doc.asset, ['is_serialized_asset', 'num_of_assets'], (r) => {
+				if (r && r.is_serialized_asset) {
+					frm.set_df_property('serial_no', 'hidden', 0);
+					frm.set_df_property('serial_no', 'reqd', 1);
+
+					frm.set_value('num_of_assets', 0);
+					frm.set_df_property('num_of_assets', 'hidden', 1);
+					frm.set_df_property('num_of_assets', 'reqd', 0);
+				} else {
+					frm.set_df_property('serial_no', 'hidden', 1);
+					frm.set_df_property('serial_no', 'reqd', 0);
+					frm.set_value('serial_no', '');
+
+					if (r.num_of_assets > 1) {
+						if (!frm.doc.num_of_assets) {
+							frm.set_value('num_of_assets', r.num_of_assets);
+						}
+
+						frm.set_df_property('num_of_assets', 'hidden', 0);
+						frm.set_df_property('num_of_assets', 'reqd', 1);
+					} else {
+						frm.set_df_property('num_of_assets', 'reqd', 0);
+					}
+				}
+			});
+		} else {
+			frm.set_df_property('serial_no', 'hidden', 1);
+			frm.set_df_property('num_of_assets', 'hidden', 1);
+		}
+	},
+
+	num_of_assets: (frm) => {
+		frappe.db.get_value('Asset', frm.doc.asset, ['is_serialized_asset', 'num_of_assets'], (r) => {
+			if (r && !r.is_serialized_asset) {
+				if (frm.doc.num_of_assets < r.num_of_assets) {
+					frappe.msgprint({
+						title: __('Warning'),
+						message: __('Asset {0} will be split on submitting this repair as the Number of Assets entered \
+							is less than {1}.', [frm.doc.asset, r.num_of_assets])
+					});
+				}
+			}
+		})
 	}
 });
 
@@ -74,7 +135,7 @@ frappe.ui.form.on('Asset Repair Consumed Item', {
 		let item_args = {
 			'item_code': item.item_code,
 			'warehouse': frm.doc.warehouse,
-			'qty': item.consumed_quantity,
+			'qty': item.qty,
 			'serial_no': item.serial_no,
 			'company': frm.doc.company
 		};
@@ -85,13 +146,13 @@ frappe.ui.form.on('Asset Repair Consumed Item', {
 				args: item_args
 			},
 			callback: function(r) {
-				frappe.model.set_value(cdt, cdn, 'valuation_rate', r.message);
+				frappe.model.set_value(cdt, cdn, 'rate', r.message);
 			}
 		});
 	},
 
-	consumed_quantity: function(frm, cdt, cdn) {
+	qty: function(frm, cdt, cdn) {
 		var row = locals[cdt][cdn];
-		frappe.model.set_value(cdt, cdn, 'total_value', row.consumed_quantity * row.valuation_rate);
+		frappe.model.set_value(cdt, cdn, 'amount', row.qty * row.rate);
 	},
 });
