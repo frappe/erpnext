54a4e46637eb2c19acea563cf19d322eb816b6cd
GangaManoj <ganga.manoj98@gmail.com>
2022-04-30 19:35:26 +0530

    test: Add tests for Asset Movement

--- erpnext/assets/doctype/asset_movement/test_asset_movement.py
+++ erpnext/assets/doctype/asset_movement/test_asset_movement.py
@@ -4,158 +4,303 @@
 import unittest
 
 import frappe
-from frappe.utils import now
+from frappe.utils import nowdate
 
-from erpnext.assets.doctype.asset.test_asset import create_asset_data
+from assets.asset.doctype.asset_.test_asset_ import (
+	create_asset,
+	create_company,
+	create_location,
+	create_asset_data,
+)
 from erpnext.hr.doctype.employee.test_employee import make_employee
-from erpnext.stock.doctype.purchase_receipt.test_purchase_receipt import make_purchase_receipt
 
-
-class TestAssetMovement(unittest.TestCase):
-	def setUp(self):
-		frappe.db.set_value(
-			"Company", "_Test Company", "capital_work_in_progress_account", "CWIP Account - _TC"
-		)
+class TestAssetMovement_(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		create_company()
 		create_asset_data()
-		make_location()
+		create_location("Test Location2")
+		make_employee("assetmovement@abc.com", company = "_Test Company")
 
-	def test_movement(self):
-		pr = make_purchase_receipt(
-			item_code="Macbook Pro", qty=1, rate=100000.0, location="Test Location"
+	@classmethod
+	def tearDownClass(cls):
+		frappe.db.rollback()
+
+	def test_movement_is_created_on_asset_submission(self):
+		asset = create_asset(submit=1)
+		asset_movement = frappe.get_last_doc("Asset Movement_")
+
+		self.assertEqual(asset_movement.purpose, "Receipt")
+		self.assertEqual(asset_movement.assets[0].asset, asset.name)
+
+	def test_transfer_draft_asset(self):
+		asset = create_asset(submit = 0)
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name ,
+				"source_location": "Test Location",
+				"target_location": "Test Location2"
+			}],
+			do_not_save = 1
 		)
 
-		asset_name = frappe.db.get_value("Asset", {"purchase_receipt": pr.name}, "name")
-		asset = frappe.get_doc("Asset", asset_name)
-		asset.calculate_depreciation = 1
-		asset.available_for_use_date = "2020-06-06"
-		asset.purchase_date = "2020-06-06"
-		asset.append(
-			"finance_books",
-			{
-				"expected_value_after_useful_life": 10000,
-				"next_depreciation_date": "2020-12-31",
-				"depreciation_method": "Straight Line",
-				"total_number_of_depreciations": 3,
-				"frequency_of_depreciation": 10,
-			},
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_employee_or_location_are_mandatory(self):
+		asset = create_asset(submit = 1)
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name
+			}],
+			do_not_save = 1
 		)
 
-		if asset.docstatus == 0:
-			asset.submit()
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
 
-		# check asset movement is created
-		if not frappe.db.exists("Location", "Test Location 2"):
-			frappe.get_doc({"doctype": "Location", "location_name": "Test Location 2"}).insert()
-
-		movement1 = create_asset_movement(
-			purpose="Transfer",
-			company=asset.company,
-			assets=[
-				{"asset": asset.name, "source_location": "Test Location", "target_location": "Test Location 2"}
-			],
-			reference_doctype="Purchase Receipt",
-			reference_name=pr.name,
-		)
-		self.assertEqual(frappe.db.get_value("Asset", asset.name, "location"), "Test Location 2")
-
-		create_asset_movement(
-			purpose="Transfer",
-			company=asset.company,
-			assets=[
-				{"asset": asset.name, "source_location": "Test Location 2", "target_location": "Test Location"}
-			],
-			reference_doctype="Purchase Receipt",
-			reference_name=pr.name,
-		)
-		self.assertEqual(frappe.db.get_value("Asset", asset.name, "location"), "Test Location")
-
-		movement1.cancel()
-		self.assertEqual(frappe.db.get_value("Asset", asset.name, "location"), "Test Location")
-
-		employee = make_employee("testassetmovemp@example.com", company="_Test Company")
-		create_asset_movement(
-			purpose="Issue",
-			company=asset.company,
-			assets=[{"asset": asset.name, "source_location": "Test Location", "to_employee": employee}],
-			reference_doctype="Purchase Receipt",
-			reference_name=pr.name,
+	def test_serial_no_mandatory_for_serialized_asset(self):
+		asset = create_asset(is_serialized_asset = 1, submit = 1)
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": "Test Location2"
+			}],
+			do_not_save = 1
 		)
 
-		# after issuing asset should belong to an employee not at a location
-		self.assertEqual(frappe.db.get_value("Asset", asset.name, "location"), None)
-		self.assertEqual(frappe.db.get_value("Asset", asset.name, "custodian"), employee)
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
 
-	def test_last_movement_cancellation(self):
-		pr = make_purchase_receipt(
-			item_code="Macbook Pro", qty=1, rate=100000.0, location="Test Location"
+	def test_source_location_is_fetched_from_asset(self):
+		asset = create_asset()
+		asset.location = "Test Location"
+		asset.submit()
+
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": None,
+				"target_location": "Test Location2"
+			}]
 		)
 
-		asset_name = frappe.db.get_value("Asset", {"purchase_receipt": pr.name}, "name")
-		asset = frappe.get_doc("Asset", asset_name)
-		asset.calculate_depreciation = 1
-		asset.available_for_use_date = "2020-06-06"
-		asset.purchase_date = "2020-06-06"
-		asset.append(
-			"finance_books",
-			{
-				"expected_value_after_useful_life": 10000,
-				"next_depreciation_date": "2020-12-31",
-				"depreciation_method": "Straight Line",
-				"total_number_of_depreciations": 3,
-				"frequency_of_depreciation": 10,
-			},
+		self.assertEqual(asset_movement.assets[0].source_location, asset.location)
+
+	def test_source_location_is_the_same_as_asset_location(self):
+		create_location("Test Location3")
+
+		asset = create_asset()
+		asset.location = "Test Location"
+		asset.submit()
+
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location2",
+				"target_location": "Test Location3"
+			}],
+			do_not_save = 1
 		)
-		if asset.docstatus == 0:
-			asset.submit()
 
-		if not frappe.db.exists("Location", "Test Location 2"):
-			frappe.get_doc({"doctype": "Location", "location_name": "Test Location 2"}).insert()
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
 
-		movement = frappe.get_doc({"doctype": "Asset Movement", "reference_name": pr.name})
-		self.assertRaises(frappe.ValidationError, movement.cancel)
+	def test_issue_asset_to_a_location(self):
+		asset = create_asset(submit = 1)
 
-		movement1 = create_asset_movement(
-			purpose="Transfer",
-			company=asset.company,
-			assets=[
-				{"asset": asset.name, "source_location": "Test Location", "target_location": "Test Location 2"}
-			],
-			reference_doctype="Purchase Receipt",
-			reference_name=pr.name,
+		asset_movement = create_asset_movement(
+			purpose = "Issue",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": "Test Location2"
+			}],
+			do_not_save = 1
 		)
-		self.assertEqual(frappe.db.get_value("Asset", asset.name, "location"), "Test Location 2")
 
-		movement1.cancel()
-		self.assertEqual(frappe.db.get_value("Asset", asset.name, "location"), "Test Location")
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
 
+	def test_to_employee_mandatory_for_asset_issue(self):
+		asset = create_asset(submit = 1)
+
+		asset_movement = create_asset_movement(
+			purpose = "Issue",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"to_employee": None
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_target_location_mandatory_for_asset_transfer(self):
+		asset = create_asset(submit = 1)
+
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": None
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_target_location_same_as_source_location_asset_transfer(self):
+		asset = create_asset(submit = 1)
+
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": "Test Location"
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_transfer_asset_to_employee(self):
+		asset = create_asset(submit = 1)
+
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"to_employee": "EMP-00001"
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_from_employee_is_mandatory_during_receipt(self):
+		asset = create_asset(submit = 1)
+
+		asset_movement = create_asset_movement(
+			purpose = "Receipt",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": "Test Location2",
+				"from_employee": None
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_target_location_is_mandatory_during_receipt(self):
+		asset = create_asset(submit = 1)
+
+		asset_movement = create_asset_movement(
+			purpose = "Receipt",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": None,
+				"from_employee": "EMP-00001"
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_receipt_has_both_target_location_and_to_employee(self):
+		asset = create_asset(submit = 1)
+		to_employee = make_employee("assetmovement2@abc.com", company = "_Test Company")
+
+		asset_movement = create_asset_movement(
+			purpose = "Receipt",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": "Test Location2",
+				"from_employee": "EMP-00001",
+				"to_employee": to_employee
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_from_employee_is_asset_custodian(self):
+		asset = create_asset(custodian = "EMP-00001", submit = 1)
+		from_employee = make_employee("assetmovement2@abc.com", company = "_Test Company")
+
+		asset_movement = create_asset_movement(
+			purpose = "Transfer",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"source_location": "Test Location",
+				"target_location": "Test Location2",
+				"from_employee": from_employee
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
+
+	def test_to_employee_belongs_to_the_same_company(self):
+		asset = create_asset(custodian = "EMP-00001", submit = 1)
+		to_employee = make_employee("assetmovement2@abc.com", company = "_Test Company 2")
+
+		asset_movement = create_asset_movement(
+			purpose = "Issue",
+			company = asset.company,
+			assets = [{
+				"asset": asset.name,
+				"from_employee": "EMP-00001",
+				"to_employee": to_employee
+			}],
+			do_not_save = 1
+		)
+
+		self.assertRaises(frappe.ValidationError, asset_movement.save)
 
 def create_asset_movement(**args):
 	args = frappe._dict(args)
 
 	if not args.transaction_date:
-		args.transaction_date = now()
+		args.transaction_date = nowdate()
 
 	movement = frappe.new_doc("Asset Movement")
-	movement.update(
-		{
-			"assets": args.assets,
-			"transaction_date": args.transaction_date,
-			"company": args.company,
-			"purpose": args.purpose or "Receipt",
-			"reference_doctype": args.reference_doctype,
-			"reference_name": args.reference_name,
-		}
-	)
+	movement.update({
+		"assets": args.assets,
+		"transaction_date": args.transaction_date,
+		"company": args.company or "_Test Company",
+		"purpose": args.purpose or "Receipt",
+		"reference_doctype": args.reference_doctype,
+		"reference_name": args.reference_name
+	})
 
-	movement.insert()
-	movement.submit()
+	if not args.do_not_save:
+		movement.insert()
+
+		if args.submit:
+			movement.submit()
 
 	return movement
-
-
-def make_location():
-	for location in ["Pune", "Mumbai", "Nagpur"]:
-		if not frappe.db.exists("Location", location):
-			frappe.get_doc({"doctype": "Location", "location_name": location}).insert(
-				ignore_permissions=True
-			)
