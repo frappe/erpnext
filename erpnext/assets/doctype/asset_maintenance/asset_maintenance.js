2c42fb97f18dc4fd75d9574d932d0e68dd35a7b4
GangaManoj <ganga.manoj98@gmail.com>
2022-05-03 00:25:29 +0530

    refactor: Asset Maintenance

--- erpnext/assets/doctype/asset_maintenance/asset_maintenance.js
+++ erpnext/assets/doctype/asset_maintenance/asset_maintenance.js
@@ -3,60 +3,133 @@
 
 frappe.ui.form.on('Asset Maintenance', {
 	setup: (frm) => {
-		frm.set_query("assign_to", "asset_maintenance_tasks", function(doc) {
+		frm.set_query('assign_to', 'asset_maintenance_tasks', function(doc) {
 			return {
-				query: "erpnext.assets.doctype.asset_maintenance.asset_maintenance.get_team_members",
+				query: 'erpnext.assets.doctype.asset_maintenance.asset_maintenance.get_team_members',
 				filters: {
 					maintenance_team: doc.maintenance_team
 				}
 			};
 		});
 
+		frm.fields_dict.asset.get_query = function(doc) {
+			return {
+				filters: {
+					'maintenance_required': 1
+				}
+			};
+		};
+
+		frm.fields_dict.serial_no.get_query = function(doc) {
+			return {
+				filters: {
+					'asset': doc.asset
+				}
+			};
+		};
+
 		frm.set_indicator_formatter('maintenance_status',
 			function(doc) {
 				let indicator = 'blue';
+
 				if (doc.maintenance_status == 'Overdue') {
 					indicator = 'orange';
 				}
-				if (doc.maintenance_status == 'Cancelled') {
+				else if (doc.maintenance_status == 'Cancelled') {
 					indicator = 'red';
 				}
+
 				return indicator;
 			}
 		);
 	},
 
 	refresh: (frm) => {
-		if(!frm.is_new()) {
+		frm.trigger('toggle_serial_no_and_num_of_assets');
+
+		if (!frm.is_new()) {
 			frm.trigger('make_dashboard');
 		}
 	},
+
+	asset: (frm) => {
+		frm.trigger('toggle_serial_no_and_num_of_assets');
+	},
+
+	toggle_serial_no_and_num_of_assets: (frm) => {
+		if (frm.doc.asset) {
+			frappe.db.get_value('Asset', frm.doc.asset, ['is_serialized_asset', 'num_of_assets'], (r) => {
+				if (r && r.is_serialized_asset) {
+					frm.set_df_property('serial_no', 'hidden', 0);
+					frm.set_df_property('serial_no', 'reqd', 1);
+
+					frm.set_value('num_of_assets', 0);
+					frm.set_df_property('num_of_assets', 'hidden', 1);
+					frm.set_df_property('num_of_assets', 'reqd', 0);
+				} else {
+					frm.set_df_property('serial_no', 'hidden', 1);
+					frm.set_df_property('serial_no', 'reqd', 0);
+					frm.set_value('serial_no', '');
+
+					if (r.num_of_assets > 1) {
+						if (!frm.doc.num_of_assets) {
+							frm.set_value('num_of_assets', r.num_of_assets);
+						}
+
+						frm.set_df_property('num_of_assets', 'hidden', 0);
+						frm.set_df_property('num_of_assets', 'reqd', 1);
+					} else {
+						frm.set_df_property('num_of_assets', 'reqd', 0);
+					}
+				}
+			});
+		} else {
+			frm.set_df_property('serial_no', 'hidden', 1);
+			frm.set_df_property('num_of_assets', 'hidden', 1);
+		}
+	},
+
 	make_dashboard: (frm) => {
-		if(!frm.is_new()) {
+		if (!frm.is_new()) {
 			frappe.call({
 				method: 'erpnext.assets.doctype.asset_maintenance.asset_maintenance.get_maintenance_log',
-				args: {asset_name: frm.doc.asset_name},
+				args: {
+					asset_name: frm.doc.asset
+				},
 				callback: (r) => {
-					if(!r.message) {
-						return;
+					if(r.message) {
+						const section = frm.dashboard.add_section('', __("Maintenance Log"));
+						var rows = $('<div></div>').appendTo(section);
+
+						(r.message || []).forEach(function(d) {
+							$(`<div class='row' style='margin-bottom: 10px;'>
+								<div class='col-sm-3 small'>
+									<a onclick="frappe.set_route('List', 'Asset Maintenance Log_',
+										{'asset_name': '${d.asset_name}','maintenance_status': '${d.maintenance_status}' });">
+										${d.maintenance_status} <span class="badge">${d.count}</span>
+									</a>
+								</div>
+							</div>`).appendTo(rows);
+						});
+						frm.dashboard.show();
 					}
-					const section = frm.dashboard.add_section('', __("Maintenance Log"));
-					var rows = $('<div></div>').appendTo(section);
-					// show
-					(r.message || []).forEach(function(d) {
-						$(`<div class='row' style='margin-bottom: 10px;'>
-							<div class='col-sm-3 small'>
-								<a onclick="frappe.set_route('List', 'Asset Maintenance Log',
-									{'asset_name': '${d.asset_name}','maintenance_status': '${d.maintenance_status}' });">
-									${__(d.maintenance_status)} <span class="badge">${d.count}</span>
-								</a>
-							</div>
-						</div>`).appendTo(rows);
-					});
-					frm.dashboard.show();
 				}
 			});
 		}
+	},
+
+	num_of_assets: (frm) => {
+		frappe.db.get_value('Asset', frm.doc.asset, ['is_serialized_asset', 'num_of_assets'], (r) => {
+			if (r && !r.is_serialized_asset) {
+				if (frm.doc.num_of_assets < r.num_of_assets) {
+					frappe.msgprint({
+						title: __('Warning'),
+						message: __('Asset {0} will be split on saving this document as the Number of Assets entered \
+							is less than {1}.', [frm.doc.asset, r.num_of_assets])
+					});
+				}
+			}
+		})
 	}
 });
 
@@ -77,6 +150,7 @@ frappe.ui.form.on('Asset Maintenance Task', {
 
 var get_next_due_date = function (frm, cdt, cdn) {
 	var d = locals[cdt][cdn];
+
 	if (d.start_date && d.periodicity) {
 		return frappe.call({
 			method: 'erpnext.assets.doctype.asset_maintenance.asset_maintenance.calculate_next_due_date',
@@ -89,10 +163,10 @@ var get_next_due_date = function (frm, cdt, cdn) {
 			},
 			callback: function(r) {
 				if (r.message) {
-					frappe.model.set_value(cdt, cdn, "next_due_date", r.message);
+					frappe.model.set_value(cdt, cdn, 'next_due_date', r.message);
 				}
 				else {
-					frappe.model.set_value(cdt, cdn, "next_due_date", "");
+					frappe.model.set_value(cdt, cdn, 'next_due_date', '');
 				}
 			}
 		});
--- erpnext/assets/doctype/asset_maintenance/asset_maintenance.py
+++ erpnext/assets/doctype/asset_maintenance/asset_maintenance.py
@@ -1,152 +1,224 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 import frappe
-from frappe import _, throw
+from frappe import _
 from frappe.desk.form import assign_to
 from frappe.model.document import Document
-from frappe.utils import add_days, add_months, add_years, getdate, nowdate
+from frappe.utils import getdate, add_days, add_months, add_years
 
+from erpnext.assets.doctype.asset.asset import split_asset
+from erpnext.assets.doctype.asset_repair.asset_repair import validate_serial_no, validate_num_of_assets
 
 class AssetMaintenance(Document):
 	def validate(self):
-		for task in self.get("asset_maintenance_tasks"):
-			if task.end_date and (getdate(task.start_date) >= getdate(task.end_date)):
-				throw(_("Start date should be less than end date for task {0}").format(task.maintenance_task))
-			if getdate(task.next_due_date) < getdate(nowdate()):
-				task.maintenance_status = "Overdue"
-			if not task.assign_to and self.docstatus == 0:
-				throw(_("Row #{}: Please asign task to a member.").format(task.idx))
+		self.validate_tasks()
+		self.validate_asset()
+		self.set_next_due_date()
+		self.set_name()
 
 	def on_update(self):
-		for task in self.get("asset_maintenance_tasks"):
-			assign_tasks(self.name, task.assign_to, task.maintenance_task, task.next_due_date)
+		self.assign_tasks()
 		self.sync_maintenance_tasks()
+
+	def validate_tasks(self):
+		for task in self.get("asset_maintenance_tasks"):
+			self.validate_start_date(task)
+			self.check_if_task_is_overdue(task)
+			self.validate_assignee(task)
+
+	def validate_start_date(self, task):
+		if task.end_date and (getdate(task.start_date) >= getdate(task.end_date)):
+			frappe.throw(_("Row #{0}: Start Date should be before End Date for task {1}")
+				.format(task.idx, task.maintenance_task))
+
+	def check_if_task_is_overdue(self, task):
+		if getdate(task.next_due_date) < getdate():
+			task.maintenance_status = "Overdue"
+
+	def validate_assignee(self, task):
+		if not task.assign_to and self.docstatus == 0:
+			frappe.throw(_("Row #{0}: Please asign task {1} to a team member.")
+				.format(task.idx, task.maintenance_task))
+
+	def validate_asset(self):
+		is_serialized_asset, num_of_assets, maintenance_required = frappe.get_value(
+			"Asset",
+			self.asset,
+			["is_serialized_asset", "num_of_assets", "maintenance_required"]
+		)
+
+		if not maintenance_required:
+			frappe.throw(_("Maintenance records can only be created for Assets with \
+				Maintenance Required enabled."), title=_("Invalid Asset"))
+
+		if is_serialized_asset:
+			validate_serial_no(self)
+		else:
+			validate_num_of_assets(self, num_of_assets)
+			self.split_asset_if_required(num_of_assets)
+
+	def split_asset_if_required(self, num_of_assets_in_asset_doc):
+		if self.num_of_assets < num_of_assets_in_asset_doc:
+			num_of_assets_to_be_separated = num_of_assets_in_asset_doc - self.num_of_assets
+
+			split_asset(self.asset, num_of_assets_to_be_separated)
+
+	def set_next_due_date(self):
+		for task in self.asset_maintenance_tasks:
+			task.next_due_date = calculate_next_due_date(task.periodicity, task.start_date)
+
+	def set_name(self):
+		if self.serial_no and self.name != self.serial_no:
+			self.name = self.serial_no
+
+	def assign_tasks(self):
+		for task in self.get("asset_maintenance_tasks"):
+			self.assign_task(task)
+
+	def assign_task(self, task):
+		team_member = frappe.db.get_value("User", task.assign_to, "email")
+
+		args = {
+			"doctype" : "Asset Maintenance",
+			"assign_to" : [team_member],
+			"name" : self.name,
+			"description" : task.maintenance_task,
+			"date" : task.next_due_date
+		}
+
+		if not self.have_todos_already_been_created(args):
+			assign_to.add(args)
+
+	def have_todos_already_been_created(self, args):
+		todos = frappe.get_all(
+			"ToDo",
+			filters = {
+				"reference_type": args["doctype"],
+				"reference_name": args["name"],
+				"status": "Open",
+				"allocated_to": args["assign_to"][0]
+			}
+		)
+
+		if todos:
+			return True
+		else:
+			return False
 
 	def sync_maintenance_tasks(self):
 		tasks_names = []
 		for task in self.get("asset_maintenance_tasks"):
 			tasks_names.append(task.name)
-			update_maintenance_log(
-				asset_maintenance=self.name, item_code=self.item_code, item_name=self.item_name, task=task
-			)
-		asset_maintenance_logs = frappe.get_all(
+			self.update_maintenance_log(task)
+
+		self.cancel_maintenance_logs_for_removed_tasks(tasks_names)
+
+	def update_maintenance_log(self, task):
+		asset_maintenance_log = self.get_maintenance_log(task)
+
+		if not asset_maintenance_log:
+			self.create_new_maintenance_log(task)
+		else:
+			maintenance_log = frappe.get_doc("Asset Maintenance Log", asset_maintenance_log)
+			maintenance_log.assign_to_name = task.assign_to_name
+			maintenance_log.has_certificate = task.certificate_required
+			maintenance_log.description = task.description
+			maintenance_log.periodicity = str(task.periodicity)
+			maintenance_log.maintenance_type = task.maintenance_type
+			maintenance_log.due_date = task.next_due_date
+			maintenance_log.save()
+
+	def get_maintenance_log(self, task):
+		return frappe.get_value(
 			"Asset Maintenance Log",
-			fields=["name"],
-			filters={"asset_maintenance": self.name, "task": ("not in", tasks_names)},
-		)
-		if asset_maintenance_logs:
-			for asset_maintenance_log in asset_maintenance_logs:
-				maintenance_log = frappe.get_doc("Asset Maintenance Log", asset_maintenance_log.name)
-				maintenance_log.db_set("maintenance_status", "Cancelled")
-
-
-@frappe.whitelist()
-def assign_tasks(asset_maintenance_name, assign_to_member, maintenance_task, next_due_date):
-	team_member = frappe.db.get_value("User", assign_to_member, "email")
-	args = {
-		"doctype": "Asset Maintenance",
-		"assign_to": [team_member],
-		"name": asset_maintenance_name,
-		"description": maintenance_task,
-		"date": next_due_date,
-	}
-	if not frappe.db.sql(
-		"""select owner from `tabToDo`
-		where reference_type=%(doctype)s and reference_name=%(name)s and status="Open"
-		and owner=%(assign_to)s""",
-		args,
-	):
-		assign_to.add(args)
-
-
-@frappe.whitelist()
-def calculate_next_due_date(
-	periodicity, start_date=None, end_date=None, last_completion_date=None, next_due_date=None
-):
-	if not start_date and not last_completion_date:
-		start_date = frappe.utils.now()
-
-	if last_completion_date and (
-		(start_date and last_completion_date > start_date) or not start_date
-	):
-		start_date = last_completion_date
-	if periodicity == "Daily":
-		next_due_date = add_days(start_date, 1)
-	if periodicity == "Weekly":
-		next_due_date = add_days(start_date, 7)
-	if periodicity == "Monthly":
-		next_due_date = add_months(start_date, 1)
-	if periodicity == "Yearly":
-		next_due_date = add_years(start_date, 1)
-	if periodicity == "2 Yearly":
-		next_due_date = add_years(start_date, 2)
-	if periodicity == "Quarterly":
-		next_due_date = add_months(start_date, 3)
-	if end_date and (
-		(start_date and start_date >= end_date)
-		or (last_completion_date and last_completion_date >= end_date)
-		or next_due_date
-	):
-		next_due_date = ""
-	return next_due_date
-
-
-def update_maintenance_log(asset_maintenance, item_code, item_name, task):
-	asset_maintenance_log = frappe.get_value(
-		"Asset Maintenance Log",
-		{
-			"asset_maintenance": asset_maintenance,
-			"task": task.name,
-			"maintenance_status": ("in", ["Planned", "Overdue"]),
-		},
-	)
-
-	if not asset_maintenance_log:
-		asset_maintenance_log = frappe.get_doc(
 			{
-				"doctype": "Asset Maintenance Log",
-				"asset_maintenance": asset_maintenance,
-				"asset_name": asset_maintenance,
-				"item_code": item_code,
-				"item_name": item_name,
+				"asset_maintenance": self.name,
 				"task": task.name,
-				"has_certificate": task.certificate_required,
-				"description": task.description,
-				"assign_to_name": task.assign_to_name,
-				"periodicity": str(task.periodicity),
-				"maintenance_type": task.maintenance_type,
-				"due_date": task.next_due_date,
+				"maintenance_status": ("in",["Planned","Overdue"])
 			}
 		)
-		asset_maintenance_log.insert()
-	else:
-		maintenance_log = frappe.get_doc("Asset Maintenance Log", asset_maintenance_log)
-		maintenance_log.assign_to_name = task.assign_to_name
-		maintenance_log.has_certificate = task.certificate_required
-		maintenance_log.description = task.description
-		maintenance_log.periodicity = str(task.periodicity)
-		maintenance_log.maintenance_type = task.maintenance_type
-		maintenance_log.due_date = task.next_due_date
-		maintenance_log.save()
 
+	def create_new_maintenance_log(self, task):
+		asset_maintenance_log = frappe.get_doc({
+			"doctype": "Asset Maintenance Log",
+			"asset_maintenance": self.name,
+			"asset_name": self.asset,
+			"task": task.name,
+			"has_certificate": task.certificate_required,
+			"description": task.description,
+			"assign_to_name": task.assign_to_name,
+			"periodicity": str(task.periodicity),
+			"maintenance_type": task.maintenance_type,
+			"due_date": task.next_due_date
+		})
+		asset_maintenance_log.insert()
+
+	def cancel_maintenance_logs_for_removed_tasks(self, tasks_names):
+		asset_maintenance_logs = self.get_maintenance_logs_for_removed_tasks(tasks_names)
+
+		if asset_maintenance_logs:
+			for asset_maintenance_log in asset_maintenance_logs:
+				frappe.db.set_value("Asset Maintenance Log", asset_maintenance_log.name, "maintenance_status", "Cancelled")
+
+	def get_maintenance_logs_for_removed_tasks(self, tasks_names):
+		return frappe.get_all(
+			"Asset Maintenance Log",
+			fields = ["name"],
+			filters = {
+				"asset_maintenance": self.name,
+				"task": ("not in", tasks_names)
+			}
+		)
+
+@frappe.whitelist()
+def calculate_next_due_date(periodicity, start_date = None, end_date = None, last_completion_date = None, next_due_date = None):
+	start_date = get_start_date(start_date, last_completion_date)
+
+	if periodicity == "Daily":
+		next_due_date = add_days(start_date, 1)
+	elif periodicity == "Weekly":
+		next_due_date = add_days(start_date, 7)
+	elif periodicity == "Monthly":
+		next_due_date = add_months(start_date, 1)
+	elif periodicity == "Yearly":
+		next_due_date = add_years(start_date, 1)
+	elif periodicity == "2 Yearly":
+		next_due_date = add_years(start_date, 2)
+	elif periodicity == "Quarterly":
+		next_due_date = add_months(start_date, 3)
+
+	if end_date and ((start_date and start_date >= end_date) or (last_completion_date and last_completion_date >= end_date) or next_due_date):
+		next_due_date = ""
+
+	return next_due_date
+
+def get_start_date(start_date, last_completion_date):
+	if not start_date and not last_completion_date:
+		return frappe.utils.now()
+	elif last_completion_date and ((start_date and last_completion_date > start_date) or not start_date):
+		return last_completion_date
+
+	return start_date
 
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
 def get_team_members(doctype, txt, searchfield, start, page_len, filters):
 	return frappe.db.get_values(
-		"Maintenance Team Member", {"parent": filters.get("maintenance_team")}, "team_member"
+		"Maintenance Team Member", {
+			"parent": filters.get("maintenance_team")
+		},
+		"team_member"
 	)
-
 
 @frappe.whitelist()
 def get_maintenance_log(asset_name):
 	return frappe.db.sql(
 		"""
-        select maintenance_status, count(asset_name) as count, asset_name
-        from `tabAsset Maintenance Log`
-        where asset_name=%s group by maintenance_status""",
+			select maintenance_status, count(asset_name) as count, asset_name
+			from `tabAsset Maintenance Log`
+			where asset_name=%s group by maintenance_status
+		""",
 		(asset_name),
-		as_dict=1,
+		as_dict = 1
 	)
