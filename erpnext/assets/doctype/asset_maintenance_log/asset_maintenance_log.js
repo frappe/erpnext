96e3e2db2e38f5224396c306d45adea91a1c6e42
GangaManoj <ganga.manoj98@gmail.com>
2022-05-03 00:43:14 +0530

    refactor: Asset Maintenance Log

--- erpnext/assets/doctype/asset_maintenance_log/asset_maintenance_log.js
+++ erpnext/assets/doctype/asset_maintenance_log/asset_maintenance_log.js
@@ -2,14 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Asset Maintenance Log', {
-	asset_maintenance: (frm) => {
-		frm.set_query('task', function(doc) {
-			return {
-				query: "erpnext.assets.doctype.asset_maintenance_log.asset_maintenance_log.get_maintenance_tasks",
-				filters: {
-					'asset_maintenance': doc.asset_maintenance
-				}
-			};
-		});
-	}
+	// refresh: function(frm) {
+
+	// }
 });
