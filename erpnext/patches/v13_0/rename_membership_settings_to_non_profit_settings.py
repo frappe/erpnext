2a15588a43733bfe014f7bf5dc806cd3db236317
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-05-09 09:48:44 +0530

    fix: patch for renaming membership settings

--- erpnext/patches/v13_0/rename_membership_settings_to_non_profit_settings.py
+++ erpnext/patches/v13_0/rename_membership_settings_to_non_profit_settings.py
@@ -3,11 +3,11 @@ from frappe.model.utils.rename_field import rename_field
 
 
 def execute():
-	if frappe.db.table_exists("Membership Settings"):
+	if frappe.db.exists("DocType", "Membership Settings"):
 		frappe.rename_doc("DocType", "Membership Settings", "Non Profit Settings")
 		frappe.reload_doctype("Non Profit Settings", force=True)
 
-	if frappe.db.table_exists("Non Profit Settings"):
+	if frappe.db.exists("DocType", "Non Profit Settings"):
 		rename_fields_map = {
 			"enable_invoicing": "allow_invoicing",
 			"create_for_web_forms": "automate_membership_invoicing",
@@ -20,3 +20,5 @@ def execute():
 
 		for old_name, new_name in rename_fields_map.items():
 			rename_field("Non Profit Settings", old_name, new_name)
+
+		frappe.delete_doc_if_exists("DocType", "Membership Settings")
