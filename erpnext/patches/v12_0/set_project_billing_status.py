171ea0add62d3e924041b7aa8e724a4b71e19b41
Saif Ur Rehman <saifi0102@gmail.com>
2022-05-09 18:18:31 +0500

    fix: patch fix

--- erpnext/patches/v12_0/set_project_billing_status.py
+++ erpnext/patches/v12_0/set_project_billing_status.py
@@ -4,6 +4,8 @@ import frappe
 def execute():
 	frappe.reload_doc("projects", "doctype", "project_status")
 	frappe.reload_doc("projects", "doctype", "project")
+	frappe.reload_doc("selling", "doctype", "sales_order_item")
+	frappe.reload_doc("stock", "doctype", "delivery_note_item")
 
 	projects = frappe.get_all("Project")
 	for d in projects:
