387e85f26495f2c41adf39a677f1ce24cdea598d
GangaManoj <ganga.manoj98@gmail.com>
2022-05-10 13:43:41 +0530

    feat: Create Depreciation Entry on submitting Asset Revaluation

--- erpnext/assets/doctype/asset_revaluation/asset_revaluation.py
+++ erpnext/assets/doctype/asset_revaluation/asset_revaluation.py
@@ -10,6 +10,11 @@ from erpnext.assets.doctype.asset_repair.asset_repair import (
 	validate_num_of_assets,
 	validate_serial_no,
 )
+from erpnext.assets.doctype.depreciation_schedule.depreciation_posting import (
+	add_accounting_dimensions,
+	get_depreciation_accounts,
+	get_depreciation_details,
+)
 
 
 class AssetRevaluation(Document):
@@ -17,6 +22,12 @@ class AssetRevaluation(Document):
 		self.validate_asset_values()
 		self.set_current_asset_value()
 		self.set_difference_amount()
+
+	def on_submit(self):
+		if self.current_asset_value > self.new_asset_value:
+			self.make_depreciation_entry()
+		else:
+			self.prevent_increase_in_value()
 
 	def validate_asset_values(self):
 		purchase_date, is_serialized_asset, num_of_assets = frappe.db.get_value(
@@ -47,6 +58,36 @@ class AssetRevaluation(Document):
 
 	def set_difference_amount(self):
 		self.difference_amount = abs(flt(self.current_asset_value - self.new_asset_value))
+
+	def make_depreciation_entry(self):
+		asset = frappe.get_doc("Asset", self.asset)
+
+		credit_account, debit_account = get_depreciation_accounts(asset.asset_category, asset.company)
+		depreciation_cost_center, _ = get_depreciation_details(asset)
+
+		depr_entry = frappe.new_doc("Depreciation Entry")
+		depr_entry.posting_date = self.date
+		depr_entry.company = self.company
+		depr_entry.asset = self.asset
+		depr_entry.num_of_assest = self.num_of_assets
+		depr_entry.serial_no = self.serial_no
+		depr_entry.finance_book = self.finance_book
+		depr_entry.credit_account = credit_account
+		depr_entry.debit_account = debit_account
+		depr_entry.depreciation_amount = self.difference_amount
+		depr_entry.cost_center = depreciation_cost_center or self.cost_center
+		depr_entry.reference_doctype = self.doctype
+		depr_entry.reference_docname = self.name
+
+		add_accounting_dimensions(depr_entry, asset)
+
+		depr_entry.flags.ignore_permissions = True
+		depr_entry.submit()
+
+		self.db_set("depreciation_entry", depr_entry.name)
+
+	def prevent_increase_in_value(self):
+		frappe.throw(_("New Asset Value cannot be higher than Current Asset Value."))
 
 
 @frappe.whitelist()
