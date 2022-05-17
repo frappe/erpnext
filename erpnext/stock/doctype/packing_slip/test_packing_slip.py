c58f7af65bf79b96897eed67502e2d26790ac369
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2022-05-16 20:41:38 -0400

    chore: cleanup import

--- erpnext/stock/doctype/packing_slip/test_packing_slip.py
+++ erpnext/stock/doctype/packing_slip/test_packing_slip.py
@@ -1,11 +1,8 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
 
-import unittest
-
-# test_records = frappe.get_test_records('Packing Slip')
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestPackingSlip(unittest.TestCase):
+class TestPackingSlip(FrappeTestCase):
 	pass
