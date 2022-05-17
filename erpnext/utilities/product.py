6c7277e5c7a866f82306d18e25319d0e7eb9c042
sherinkr <sherinkrply@gmail.com>
2022-05-17 08:41:12 +0530

    fix: filters - added date filter on get_price()

--- erpnext/utilities/product.py
+++ erpnext/utilities/product.py
@@ -91,7 +91,7 @@ def get_price(item_code, price_list, customer_group, company, qty=1):
 				["price_list", "=", price_list],
 				["item_code", "=", item_code],
 				["valid_from", "<=", frappe.utils.nowdate()],
-				["valid_upto", ">=", frappe.utils.nowdate()]
+				["valid_upto", ">=", frappe.utils.nowdate()],
 			]
 		)
 
@@ -103,7 +103,7 @@ def get_price(item_code, price_list, customer_group, company, qty=1):
 					["price_list", "=", price_list],
 					["item_code", "=", template_item_code],
 					["valid_from", "<=", frappe.utils.nowdate()],
-					["valid_upto", ">=", frappe.utils.nowdate()]
+					["valid_upto", ">=", frappe.utils.nowdate()],
 				]
 			)
 
