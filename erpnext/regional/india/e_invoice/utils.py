9fb7b49b435887d7e48505830389ede0d740ccff
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-05-18 11:53:00 +0530

    fix(india): error while parsing e-invoice (#31053)

--- erpnext/regional/india/e_invoice/utils.py
+++ erpnext/regional/india/e_invoice/utils.py
@@ -649,6 +649,8 @@ def make_einvoice(invoice):
 	try:
 		einvoice = safe_json_load(einvoice)
 		einvoice = santize_einvoice_fields(einvoice)
+	except json.JSONDecodeError:
+		raise
 	except Exception:
 		show_link_to_error_log(invoice, einvoice)
 
@@ -765,7 +767,9 @@ def safe_json_load(json_string):
 		frappe.throw(
 			_(
 				"Error in input data. Please check for any special characters near following input: <br> {}"
-			).format(snippet)
+			).format(snippet),
+			title=_("Invalid JSON"),
+			exc=e,
 		)
 
 
