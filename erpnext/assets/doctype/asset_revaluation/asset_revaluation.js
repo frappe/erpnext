1b7242bca96eb08e602f6d8232a7ab24a065b1f7
GangaManoj <ganga.manoj98@gmail.com>
2022-05-26 14:10:17 +0530

    fix: Filter asset and serial_no lists

--- erpnext/assets/doctype/asset_revaluation/asset_revaluation.js
+++ erpnext/assets/doctype/asset_revaluation/asset_revaluation.js
@@ -1,8 +1,36 @@
 // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Asset Revaluation', {
-	// refresh: function(frm) {
+frappe.provide("erpnext.accounts.dimensions");
 
-	// }
+frappe.ui.form.on("Asset Revaluation", {
+	setup: function (frm) {
+		frm.add_fetch("company", "cost_center", "cost_center");
+
+		frm.set_query("cost_center", function () {
+			return {
+				filters: {
+					company: frm.doc.company,
+					is_group: 0
+				}
+			}
+		});
+
+		frm.set_query("asset", function () {
+			return {
+				filters: {
+					calculate_depreciation: 1,
+					docstatus: 1
+				}
+			};
+		});
+
+		frm.set_query("serial_no", function () {
+			return {
+				filters: {
+					asset: frm.doc.asset,
+				}
+			};
+		});
+	},
 });
