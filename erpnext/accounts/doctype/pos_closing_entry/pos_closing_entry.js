82addd4274145335dec8118179495a8da533ef9c
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-05-27 12:12:27 +0530

    fix(pos): freeze screen while processing pos invoices

--- erpnext/accounts/doctype/pos_closing_entry/pos_closing_entry.js
+++ erpnext/accounts/doctype/pos_closing_entry/pos_closing_entry.js
@@ -101,32 +101,34 @@ frappe.ui.form.on('POS Closing Entry', {
 	},
 
 	before_save: async function(frm) {
-		await frappe.run_serially([
-			() => frappe.dom.freeze(__('Processing Sales! Please Wait...')),
-			async () => {
-				frm.set_value("grand_total", 0);
-				frm.set_value("net_total", 0);
-				frm.set_value("total_quantity", 0);
-				frm.set_value("taxes", []);
-		
-				for (let row of frm.doc.payment_reconciliation) {
-					row.expected_amount = row.opening_amount;
-				}
-		
-				for (let row of frm.doc.pos_transactions) {
-					await frappe.db.get_doc("POS Invoice", row.pos_invoice).then(doc => {
-						frm.doc.grand_total += flt(doc.grand_total);
-						frm.doc.net_total += flt(doc.net_total);
-						frm.doc.total_quantity += flt(doc.total_qty);
-						refresh_payments(doc, frm);
-						refresh_taxes(doc, frm);
-						refresh_fields(frm);
-						set_html_data(frm);
-					});
-				}
-			},
-			() => frappe.dom.unfreeze()
-		]);
+		frappe.dom.freeze(__('Processing Sales! Please Wait...'));
+
+		frm.set_value("grand_total", 0);
+		frm.set_value("net_total", 0);
+		frm.set_value("total_quantity", 0);
+		frm.set_value("taxes", []);
+
+		for (let row of frm.doc.payment_reconciliation) {
+			row.expected_amount = row.opening_amount;
+		}
+
+		const pos_inv_promises = frm.doc.pos_transactions.map(
+			row => frappe.db.get_doc("POS Invoice", row.pos_invoice)
+		);
+
+		const pos_invoices = await Promise.all(pos_inv_promises);
+
+		for (let doc of pos_invoices) {
+			frm.doc.grand_total += flt(doc.grand_total);
+			frm.doc.net_total += flt(doc.net_total);
+			frm.doc.total_quantity += flt(doc.total_qty);
+			refresh_payments(doc, frm);
+			refresh_taxes(doc, frm);
+			refresh_fields(frm);
+			set_html_data(frm);
+		}
+
+		frappe.dom.unfreeze();
 	}
 });
 
