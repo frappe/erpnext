531f86c2797aee65c17de6acb1c6b7f1a4204335
GangaManoj <ganga.manoj98@gmail.com>
2022-05-28 13:37:23 +0530

    fix: Only run patch if asset_quantity column exists

--- erpnext/patches/v13_0/update_asset_quantity_field.py
+++ erpnext/patches/v13_0/update_asset_quantity_field.py
@@ -2,7 +2,7 @@ import frappe
 
 
 def execute():
-	if frappe.db.count("Asset"):
+	if frappe.db.count("Asset") and frappe.db.has_column("Asset", "asset_quantity"):
 		frappe.reload_doc("assets", "doctype", "Asset")
 		asset = frappe.qb.DocType("Asset")
 		frappe.qb.update(asset).set(asset.asset_quantity, 1).run()
