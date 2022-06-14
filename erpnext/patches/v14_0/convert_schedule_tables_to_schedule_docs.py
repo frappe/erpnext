3e34febcba5eb87dafd6f240a398af3300f32d0a
GangaManoj <ganga.manoj98@gmail.com>
2022-06-14 16:29:20 +0530

    fix: Replace journal entries with depr entries

--- erpnext/patches/v14_0/convert_schedule_tables_to_schedule_docs.py
+++ erpnext/patches/v14_0/convert_schedule_tables_to_schedule_docs.py
@@ -10,7 +10,7 @@ def execute():
 		return
 
 	schedule_rows = frappe.db.sql(
-		"""select parent, finance_book, schedule_date, depreciation_amount, accumulated_depreciation_amount, depreciation_entry, finance_book_id, creation
+		"""select parent, finance_book, schedule_date, depreciation_amount, accumulated_depreciation_amount, journal_entry, finance_book_id, creation
 		from `tabDepreciation Schedule`
 		order by parent, finance_book_id
 		""",
@@ -34,12 +34,45 @@ def execute():
 			depr_schedule.finance_book = row.finance_book
 			depr_schedule.creation_date = row.creation.date
 
+		depreciation_entry = None
+		if row.journal_entry:
+			journal_entry = frappe.get_doc("Journal Entry", row.journal_entry)
+			depreciation_entry = create_depr_entry(journal_entry)
+
+			frappe.db.delete("Journal Entry", {"name": journal_entry.name})
+
 		depr_schedule.append(
 			"depreciation_schedule",
 			{
 				"schedule_date": row.schedule_date,
 				"depreciation_amount": row.depreciation_amount,
 				"accumulated_depreciation_amount": row.accumulated_depreciation_amount,
-				"depreciation_entry": row.depreciation_entry,
+				"depreciation_entry": depreciation_entry,
 			},
 		)
+
+
+def create_depr_entry(journal_entry):
+	depr_entry = frappe.new_doc("Depreciation Entry")
+	depr_entry.posting_date = journal_entry.posting_date
+	depr_entry.company = journal_entry.company
+	depr_entry.asset = journal_entry.accounts[0].reference_name
+	depr_entry.finance_book = journal_entry.finance_book
+	(
+		depr_entry.credit_account,
+		depr_entry.debit_account,
+		depr_entry.depreciation_amount,
+	) = get_depr_details(journal_entry.accounts)
+	depr_entry.cost_center = journal_entry.accounts[0].cost_center
+	depr_entry.reference_doctype = journal_entry.accounts[0].reference_type
+	depr_entry.reference_docname = journal_entry.accounts[0].reference_name
+	depr_entry.submit()
+
+	return depr_entry.name
+
+
+def get_depr_details(accounts):
+	if accounts[0].credit:
+		return accounts[0].account, accounts[1].account, accounts[0].credit
+	else:
+		return accounts[1].account, accounts[0].account, accounts[0].debit
