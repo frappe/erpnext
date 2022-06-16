6f2086d770647a449eca36114d7b8fd2e97ea25d
Ankush Menat <ankush@frappe.io>
2022-06-16 22:15:06 +0530

    test: verify that all patches exist in patches.txt (#31371)
    * chore: delete orphaned patches
    
    * test: orphan/missing entries in patches.txt
    
    
    [skip ci]

--- erpnext/patches/v13_0/print_uom_after_quantity_patch.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2019, Frappe and Contributors
-# License: GNU General Public License v3. See license.txt
-
-
-from erpnext.setup.install import create_print_uom_after_qty_custom_field
-
-
-def execute():
-	create_print_uom_after_qty_custom_field()
