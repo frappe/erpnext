c15ecf4d1f180307280123de0235a379b7b448cd
gn306029 <gn306029@users.noreply.github.com>
2022-06-29 17:15:21 +0800

    fix: Modify opts parameter misspell (#31476)
    Modify opts parameter misspell
    
    closes #31474
    
    (cherry picked from commit 2a619fd7890d542f9c6fa28594c0dae97cfb683a)

--- erpnext/buying/doctype/purchase_order/purchase_order.js
+++ erpnext/buying/doctype/purchase_order/purchase_order.js
@@ -425,7 +425,7 @@ erpnext.buying.PurchaseOrderController = erpnext.buying.BuyingController.extend(
 						company: me.frm.doc.company
 					},
 					allow_child_item_selection: true,
-					child_fielname: "items",
+					child_fieldname: "items",
 					child_columns: ["item_code", "qty"]
 				})
 			}, __("Get Items From"));
