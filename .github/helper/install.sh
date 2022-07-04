b07dd1083aecc6682dc377b2c90dd0fb9230c622
Gavin D'souza <gavin18d@gmail.com>
2022-07-04 11:11:14 +0530

    ci: Check for conflicts & valid python in early separate step

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-# Check for merge conflicts before proceeding
-python -m compileall -f "${GITHUB_WORKSPACE}"
-if grep -lr --exclude-dir=node_modules "^<<<<<<< " "${GITHUB_WORKSPACE}"
-    then echo "Found merge conflicts"
-    exit 1
-fi
-
 cd ~ || exit
 
 sudo apt update && sudo apt install redis-server libcups2-dev
