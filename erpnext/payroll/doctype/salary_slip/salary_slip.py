29725f6ccb5295caed103c895eec5e73f29ee7c9
bahaaabed <bahaa.9999.sh@gmail.com>
2022-07-05 11:54:35 +0300

    fix: Reload loan Table in Salary Slip when change Employee

--- erpnext/payroll/doctype/salary_slip/salary_slip.py
+++ erpnext/payroll/doctype/salary_slip/salary_slip.py
@@ -1350,23 +1350,23 @@ class SalarySlip(TransactionBase):
 		self.total_interest_amount = 0
 		self.total_principal_amount = 0
 
-		if not self.get("loans"):
-			for loan in self.get_loan_details():
+		if self.get("loans"):
+			[self.remove(d) for d in self.get("loans")]
+		for loan in self.get_loan_details():
+			amounts = calculate_amounts(loan.name, self.posting_date, "Regular Payment")
 
-				amounts = calculate_amounts(loan.name, self.posting_date, "Regular Payment")
-
-				if amounts["interest_amount"] or amounts["payable_principal_amount"]:
-					self.append(
-						"loans",
-						{
-							"loan": loan.name,
-							"total_payment": amounts["interest_amount"] + amounts["payable_principal_amount"],
-							"interest_amount": amounts["interest_amount"],
-							"principal_amount": amounts["payable_principal_amount"],
-							"loan_account": loan.loan_account,
-							"interest_income_account": loan.interest_income_account,
-						},
-					)
+			if amounts["interest_amount"] or amounts["payable_principal_amount"]:
+				self.append(
+					"loans",
+					{
+						"loan": loan.name,
+						"total_payment": amounts["interest_amount"] + amounts["payable_principal_amount"],
+						"interest_amount": amounts["interest_amount"],
+						"principal_amount": amounts["payable_principal_amount"],
+						"loan_account": loan.loan_account,
+						"interest_income_account": loan.interest_income_account,
+					},
+				)
 
 		for payment in self.get("loans"):
 			amounts = calculate_amounts(payment.loan, self.posting_date, "Regular Payment")
