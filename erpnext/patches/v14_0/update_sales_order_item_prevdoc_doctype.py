070271e4179a71462ff108841e42b3e1def199e3
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2022-07-07 13:49:56 -0400

    fix: linter

--- erpnext/patches/v14_0/update_sales_order_item_prevdoc_doctype.py
+++ erpnext/patches/v14_0/update_sales_order_item_prevdoc_doctype.py
@@ -2,6 +2,6 @@ import frappe
 
 
 def execute():
-	frappe.db.set_value("Sales Order Item", {
-		"prevdoc_docname": ["!=", ""]
-	}, "prevdoc_doctype", "Quotation")
+	frappe.db.set_value(
+		"Sales Order Item", {"prevdoc_docname": ["!=", ""]}, "prevdoc_doctype", "Quotation"
+	)
