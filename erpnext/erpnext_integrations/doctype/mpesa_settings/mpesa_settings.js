2615cbd37ced4afbc96799f583b50da3b65b244f
Douglas <nkubitudouglas@gmail.com>
2022-07-11 12:10:23 +0300

    Update mpesa_settings.js

--- erpnext/erpnext_integrations/doctype/mpesa_settings/mpesa_settings.js
+++ erpnext/erpnext_integrations/doctype/mpesa_settings/mpesa_settings.js
@@ -9,7 +9,7 @@ frappe.ui.form.on('Mpesa Settings', {
 	validate: function(frm) {
 		if (frm.doc.paybill_number==1 && frm.doc.buy_goods_number==1) {
 			frappe.msgprint('Kindly Select either Paybill or Buygoods Only');
-			validated = false;
+			frappe.validated = false;
 		}
 	},
 
