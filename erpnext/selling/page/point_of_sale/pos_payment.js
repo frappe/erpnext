2c1e4b4bf4ef2e3057a2d7e4ac093e1b5250b380
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-07-22 11:23:59 +0530

    fix: sider

--- erpnext/selling/page/point_of_sale/pos_payment.js
+++ erpnext/selling/page/point_of_sale/pos_payment.js
@@ -161,7 +161,8 @@ erpnext.PointOfSale.Payment = class {
 
 		frappe.ui.form.on('POS Invoice', 'contact_mobile', (frm) => {
 			const contact = frm.doc.contact_mobile;
-			const request_button = $(this.request_for_payment_field?.$input[0]);
+			if (!this.request_for_payment_field) return;
+			const request_button = $(this.request_for_payment_field.$input[0]);
 			if (contact) {
 				request_button.removeClass('btn-default').addClass('btn-primary');
 			} else {
