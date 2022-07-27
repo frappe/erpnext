32c1bb61de5e70cb8ca3d6bfc12eb11b17b96690
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-07-27 12:44:26 +0530

    fix: manually generated salary slips overwritten by structure amount (#31711)

--- erpnext/payroll/doctype/salary_slip/salary_slip.py
+++ erpnext/payroll/doctype/salary_slip/salary_slip.py
@@ -632,7 +632,11 @@ class SalarySlip(TransactionBase):
 				continue
 
 			amount = self.eval_condition_and_formula(struct_row, data)
-			if amount is not None and struct_row.statistical_component == 0:
+			if (
+				amount
+				or (struct_row.amount_based_on_formula and amount is not None)
+				and struct_row.statistical_component == 0
+			):
 				self.update_component_row(struct_row, amount, component_type, data=data)
 
 	def get_data_for_eval(self):
