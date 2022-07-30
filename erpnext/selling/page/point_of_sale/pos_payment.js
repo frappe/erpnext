4d1c22299050bb4a81331b37f78e109f1bb7be95
bvisible <info@bvisible.ch>
2022-07-30 23:53:03 +0200

    Update pos_payment.js

--- erpnext/selling/page/point_of_sale/pos_payment.js
+++ erpnext/selling/page/point_of_sale/pos_payment.js
@@ -203,7 +203,7 @@ erpnext.PointOfSale.Payment = class {
 			const paid_amount = doc.paid_amount;
 			const items = doc.items;
 
-			if (paid_amount == 0 || !items.length) {
+			if (!items.length) {
 				const message = items.length ? __("You cannot submit the order without payment.") : __("You cannot submit empty order.");
 				frappe.show_alert({ message, indicator: "orange" });
 				frappe.utils.play_sound("error");
@@ -365,7 +365,7 @@ erpnext.PointOfSale.Payment = class {
 				const amount = p.amount > 0 ? format_currency(p.amount, currency) : '';
 
 				return (`
-					<div class="payment-mode-wrapper">
+					<div class="payment-mode-wrapper ${mode}">
 						<div class="mode-of-payment" data-mode="${mode}" data-payment-type="${payment_type}">
 							${p.mode_of_payment}
 							<div class="${mode}-amount pay-amount">${amount}</div>
