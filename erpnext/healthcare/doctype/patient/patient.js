0ce33780720d87ef5a500a2aa48c207592d46d42
Abdul Jamil <abduljamil@satistrum.com>
2022-08-02 20:26:52 +0500

    Update patient.js
    CNIC format setup

--- erpnext/healthcare/doctype/patient/patient.js
+++ erpnext/healthcare/doctype/patient/patient.js
@@ -54,6 +54,14 @@ frappe.ui.form.on('Patient', {
 		}
 	},
 
+	cnic: function (frm) {
+		var regex = /[^[0-9+]{5}-[0-9+]{7}-[0-9]{1}$]/g;
+		if(regex.test(frm.doc.cnic)  != true){
+			frappe.msgprint(__("CNIC: Please follow CNIC format."));
+    		frappe.validated = false;
+		}
+	},
+
 	onload: function (frm) {
 		if (frm.doc.dob) {
 			$(frm.fields_dict['age_html'].wrapper).html(`${__('AGE')} : ${get_age(frm.doc.dob)}`);
