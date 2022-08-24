a18241b34901d528bf2944b8bbb32f06391181b7
Saif Ur Rehman <saifi0102@gmail.com>
2022-08-24 18:03:10 +0500

    fix(Leave Encashment): patch fix

--- erpnext/patches/v12_0/fix_leave_enchashment_duplicate_ledger.py
+++ erpnext/patches/v12_0/fix_leave_enchashment_duplicate_ledger.py
@@ -1,23 +1,26 @@
 import frappe
+from erpnext.hr.doctype.leave_ledger_entry.leave_ledger_entry import process_expired_allocation
 
 
 # Create function for duplicate entries
 def execute():
-	duplicate_entries  = get_leave_encashment_with_duplicate_ledger_entries()
-	if duplicate_entries:
-		for duplicate_encashment in duplicate_entries:
-			doc = frappe.get_doc('Leave Encashment',duplicate_encashment.get("transaction_name"))
+	leave_encashments = get_leave_encashment_with_duplicate_ledger_entries()
+	if leave_encashments:
+		for name in leave_encashments:
+			doc = frappe.get_doc('Leave Encashment', name)
 			doc.create_leave_ledger_entry(submit=False)
 			doc.create_leave_ledger_entry(submit=True)
 
+		process_expired_allocation()
+
 
 def get_leave_encashment_with_duplicate_ledger_entries():
-	leave_encashment_duplicate_entries = frappe.db.sql("""
+	leave_encashment_duplicate_entries = frappe.db.sql_list("""
 		SELECT transaction_name
 		FROM `tabLeave Ledger Entry`
 		WHERE transaction_type='Leave Encashment'
 		GROUP BY transaction_name
 		HAVING Count(name) > 1
-	""", as_dict =1)
+	""")
 
 	return leave_encashment_duplicate_entries
