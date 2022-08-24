f73f170ee6dd7a61dc8aeabc983da0f3c55eb1da
Abdul Basit <abdulbasitalishaik1@gmail.com>
2022-08-24 15:36:38 +0500

    fix

--- erpnext/patches/v12_0/fix_leave_enchashment_duplicate_ledger.py
+++ erpnext/patches/v12_0/fix_leave_enchashment_duplicate_ledger.py
@@ -3,7 +3,7 @@ import frappe
 
 # Create function for duplicate entries
 def execute():
-	duplicate_entries  = getting_duplicate_leave_encashment_entries()
+	duplicate_entries  = get_leave_encashment_with_duplicate_ledger_entries()
 	if duplicate_entries:
 		for duplicate_encashment in duplicate_entries:
 			doc = frappe.get_doc('Leave Encashment',duplicate_encashment.get("transaction_name"))
@@ -11,13 +11,13 @@ def execute():
 			doc.create_leave_ledger_entry(submit=True)
 
 
-def getting_duplicate_leave_encashment_entries():
+def get_leave_encashment_with_duplicate_ledger_entries():
 	leave_encashment_duplicate_entries = frappe.db.sql("""
 		SELECT transaction_name
 		FROM `tabLeave Ledger Entry`
 		WHERE transaction_type='Leave Encashment'
 		GROUP BY transaction_name
 		HAVING Count(name) > 1
-	""",as_dict =1)
+	""", as_dict =1)
 
 	return leave_encashment_duplicate_entries
