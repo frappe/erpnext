4cb685a326f05af6652b441eeaec2daa9fb496cd
Florian HENRY <florian.henry@open-concept.pro>
2022-08-25 20:45:35 +0200

    fix: upgrade process to version-14 when currency opportunity wass not set
    (cherry picked from commit 9d02fbadb43d729141422321996084daf31543d1)

--- erpnext/patches/v14_0/update_opportunity_currency_fields.py
+++ erpnext/patches/v14_0/update_opportunity_currency_fields.py
@@ -4,6 +4,8 @@ from frappe.utils import flt
 import erpnext
 from erpnext.setup.utils import get_exchange_rate
 
+import click
+
 
 def execute():
 	frappe.reload_doctype("Opportunity")
@@ -16,6 +18,19 @@ def execute():
 	for opportunity in opportunities:
 		company_currency = erpnext.get_company_currency(opportunity.company)
 
+		if opportunity.currency is None or opportunity.currency == '':
+			opportunity.currency = company_currency
+			frappe.db.set_value(
+				"Opportunity",
+				opportunity.name,
+				{"currency": opportunity.currency},
+				update_modified=False,
+			)
+			click.secho(
+				f" Opportunity `{opportunity.name}` has no currency set. We for it to company currency : `{opportunity.currency}`\"\n",
+				fg="yellow",
+			)
+
 		# base total and total will be 0 only since item table did not have amount field earlier
 		if opportunity.currency != company_currency:
 			conversion_rate = get_exchange_rate(opportunity.currency, company_currency)
@@ -23,6 +38,10 @@ def execute():
 		else:
 			conversion_rate = 1
 			base_opportunity_amount = flt(opportunity.opportunity_amount)
+
+		if conversion_rate is None:
+			print(opportunity.name,conversion_rate,opportunity.currency)
+
 
 		frappe.db.set_value(
 			"Opportunity",
