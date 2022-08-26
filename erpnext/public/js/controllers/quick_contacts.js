158617f733f6e1e44279ef240f719398a3c0c1e3
Danish <danish21x@gmail.com>
2022-08-26 15:51:26 +0500

    feat(Project & Deployment): Added Secondary contact info.

--- erpnext/public/js/controllers/quick_contacts.js
+++ erpnext/public/js/controllers/quick_contacts.js
@@ -4,16 +4,15 @@ erpnext.contacts.QuickContacts = frappe.ui.form.Controller.extend({
 	onload: function () {
 		this.setup_contact_no_fields();
 	},
-
-	contact_person: function() {
+	contact_display_info :function( contact,contact_display, type) {
 		var me = this;
 
-		if (me.frm.doc.contact_person) {
+		if (contact) {
 			me.set_dynamic_link();
 			return frappe.call({
 				method: "frappe.contacts.doctype.contact.contact.get_contact_details",
 				args: {
-					contact: me.frm.doc.contact_person,
+					contact: contact,
 					get_contact_no_list: 1,
 					link_doctype: frappe.dynamic_link.doctype,
 					link_name: me.frm.doc[frappe.dynamic_link.fieldname]
@@ -21,9 +20,10 @@ erpnext.contacts.QuickContacts = frappe.ui.form.Controller.extend({
 				callback: function (r) {
 					if (r.message) {
 						$.each(r.message || {}, function (k, v) {
-							if (me.frm.get_field(k)) {
-								me.frm.doc[k] = v;
-								me.frm.refresh_field(k);
+							var key_item = `${type}${k}`
+							if (me.frm.get_field(key_item)) {
+								me.frm.doc[key_item] = v;
+								me.frm.refresh_field(key_item);
 							}
 						});
 						me.setup_contact_no_fields(r.message.contact_nos);
@@ -31,41 +31,58 @@ erpnext.contacts.QuickContacts = frappe.ui.form.Controller.extend({
 				}
 			});
 		} else {
-			me.frm.set_value("contact_display", "");
+			me.frm.set_value(contact_display, "");
 		}
 	},
 
-	contact_mobile: function () {
-		if (this.add_new_contact_number('contact_mobile', 'is_primary_mobile_no')) {
+	contact_person: function() {
+		this.contact_display_info( me.frm.doc.contact_person,"contact_display", "");
+	},
+	secondary_contact_person: function() {
+		this.contact_display_info( me.frm.doc.secondary_contact_person,"secondary_contact_display", "secondary_");
+	},
+
+	contact_mobile_setter: function(type, has_secondary_contact) {
+		if (this.add_new_contact_number(`${type}contact_mobile`, 'is_primary_mobile_no')) {
 			return;
 		}
 
 		var tasks = [];
 
-		var mobile_no = this.frm.doc.contact_mobile;
+		var mobile_no = this.frm.doc[`${type}contact_mobile`];
 		if (mobile_no) {
 			var contacts = frappe.contacts.get_contacts_from_number(this.frm, mobile_no);
-			if (contacts && contacts.length && !contacts.includes(this.frm.doc.contact_person)) {
+			if (contacts && contacts.length && !contacts.includes(this.frm.doc[`${type}contact_person`])) {
 				tasks = [
-					() => this.frm.doc.contact_person = contacts[0],
-					() => this.frm.trigger('contact_person'),
+					() => this.frm.doc[`${type}contact_person`] = contacts[0],
+					() => this.frm.trigger(`${type}contact_person`),
 					() => {
-						this.frm.doc.contact_mobile = mobile_no;
-						this.frm.refresh_field('contact_mobile');
+						this.frm.doc[`${type}contact_mobile`] = mobile_no;
+						this.frm.refresh_field(`${type}contact_mobile`);
 					},
 				];
 			}
 		}
-
-		tasks.push(() => {
-			if (this.frm.doc.contact_mobile_2 == this.frm.doc.contact_mobile) {
-				this.frm.doc.contact_mobile_2 = '';
-				this.frm.refresh_field('contact_mobile_2');
-			}
-		});
+		if(has_secondary_contact){
+			tasks.push(() => {
+				if (this.frm.doc.contact_mobile_2 == this.frm.doc.contact_mobile) {
+					this.frm.doc.contact_mobile_2 = '';
+					this.frm.refresh_field('contact_mobile_2');
+				}
+			});
+		}
 
 		return frappe.run_serially(tasks);
 	},
+
+	contact_mobile: function () {
+		this.contact_mobile_setter("", true);
+	},
+
+	secondary_contact_mobile: function () {
+		this.contact_mobile_setter("secondary_", false);
+	},
+
 
 	contact_mobile_2: function () {
 		this.add_new_contact_number('contact_mobile_2', 'is_primary_mobile_no');
@@ -105,6 +122,8 @@ erpnext.contacts.QuickContacts = frappe.ui.form.Controller.extend({
 		frappe.contacts.set_contact_no_select_options(this.frm, 'contact_mobile', 'is_primary_mobile_no', true);
 		frappe.contacts.set_contact_no_select_options(this.frm, 'contact_mobile_2', 'is_primary_mobile_no', true);
 		frappe.contacts.set_contact_no_select_options(this.frm, 'contact_phone', 'is_primary_phone', true);
+
+		frappe.contacts.set_contact_no_select_options(this.frm, 'secondary_contact_mobile', 'is_primary_mobile_no', true);
 	},
 
 	get_all_contact_nos: function () {
@@ -116,3 +135,4 @@ erpnext.contacts.QuickContacts = frappe.ui.form.Controller.extend({
 		]);
 	},
 });
+
