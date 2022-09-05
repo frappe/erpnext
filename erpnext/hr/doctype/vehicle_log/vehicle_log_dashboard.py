8c098c0a26c70a6b68837c05a4fd5d884af0e57a
Abubakar Sohail <abubakarsohail66@gmail.com>
2022-09-05 15:01:38 +0500

    vehicle log dashboard  link to material request against a particular vehicle log
    vehicle log dashboard  link to material request against a particular vehicle log

---
+++ erpnext/hr/doctype/vehicle_log/vehicle_log_dashboard.py
@@ -0,0 +1,61 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'vehicle_log',
+		'non_standard_fieldnames': {
+            'Material Request':'vehicle_log'
+		},
+		'transactions': [
+
+			{
+				'label': _('Reference'),
+				'items': ['Material Request']
+			}
+		]
+	}
+ 
+ 
+ 
+
+# def get_data():
+# 	return {
+# 		'fieldname': 'sales_order',
+# 		'non_standard_fieldnames': {
+# 			'Delivery Note': 'against_sales_order',
+# 			'Journal Entry': 'reference_name',
+# 			'Payment Entry': 'reference_name',
+# 			'Payment Request': 'reference_name',
+# 			'Auto Repeat': 'reference_document',
+# 		},
+# 		'internal_links': {
+# 			'Quotation': ['items', 'prevdoc_docname']
+# 		},
+# 		'transactions': [
+# 			{
+# 				'label': _('Fulfillment'),
+# 				'items': ['Sales Invoice', 'Pick List', 'Delivery Note']
+# 			},
+# 			{
+# 				'label': _('Purchasing'),
+# 				'items': ['Material Request', 'Purchase Order']
+# 			},
+# 			{
+# 				'label': _('Projects'),
+# 				'items': ['Project']
+# 			},
+# 			{
+# 				'label': _('Manufacturing'),
+# 				'items': ['Work Order']
+# 			},
+# 			{
+# 				'label': _('Reference'),
+# 				'items': ['Quotation', 'Auto Repeat']
+# 			},
+# 			{
+# 				'label': _('Payment'),
+# 				'items': ['Payment Entry', 'Payment Request', 'Journal Entry']
+# 			},
+# 		]
+# 	}
