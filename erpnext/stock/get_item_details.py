49fd6c67a0cd1ee25e84e096aa31bab09aeeaa28
Abd ul samad <47312724+abdulsamad2929@users.noreply.github.com>
2022-09-20 16:24:42 +0500

    Spell Change

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -584,7 +584,7 @@ def get_price_list_rate(args, item_doc, out):
 		oblige_rate = 0
 		if args.parenttype == 'Purchase Order' or args.doctype == 'Purchase Order':
 			oblige_rate = flt(frappe.db.get_value('Item Daily Rate Table', {
-                            'category':'Buying Rate','campany':args.company, 'item_code': item_doc.name, 'supplier_code': args.supplier, 'docstatus': '1', 'date': ["<=", frappe.utils.now()]}, 'new_rate'))
+                            'category':'Buying Rate','company':args.company, 'item_code': item_doc.name, 'supplier_code': args.supplier, 'docstatus': '1', 'date': ["<=", frappe.utils.now()]}, 'new_rate'))
 
 		if  oblige_rate== None or oblige_rate == 0:
 			price_list_rate = get_price_list_rate_for(args, item_doc.name) or 0
