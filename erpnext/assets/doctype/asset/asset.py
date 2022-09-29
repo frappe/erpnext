36131a798843ade1f0cfa9572510cb3753335ad2
anandbaburajan <anandbaburajan@gmail.com>
2022-09-29 15:37:04 +0530

    chore: refactor by just using a filter
    (cherry picked from commit 6b21deedae06e9ba8cb5b4e96bf3fa4b62c894e9)

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -643,15 +643,13 @@ class Asset(AccountsController):
 		self.db_set("status", status)
 
 	def get_status(self):
-		"""Returns status based on whether it is draft, submitted, sold, scrapped or depreciated"""
+		"""Returns status based on whether it is draft, submitted, scrapped or depreciated"""
 		if self.docstatus == 0:
 			status = "Draft"
 		elif self.docstatus == 1:
 			status = "Submitted"
 
-			if self.is_sold():
-				status = "Sold"
-			elif self.journal_entry_for_scrap:
+			if self.journal_entry_for_scrap:
 				status = "Scrapped"
 			elif self.finance_books:
 				idx = self.get_default_finance_book_idx() or 0
@@ -666,21 +664,6 @@ class Asset(AccountsController):
 		elif self.docstatus == 2:
 			status = "Cancelled"
 		return status
-
-	def is_sold(self):
-		item = frappe.qb.DocType("Sales Invoice Item").as_("item")
-		si = frappe.qb.DocType("Sales Invoice").as_("si")
-
-		asset_sales_invoice = (
-			frappe.qb.from_(item)
-			.select(item.parent)
-			.inner_join(si)
-			.on(item.parent == si.name)
-			.where(item.asset == self.name)
-			.where(si.docstatus == 1)
-		).run()
-
-		return True if asset_sales_invoice else False
 
 	def get_default_finance_book_idx(self):
 		if not self.get("default_finance_book") and self.company:
