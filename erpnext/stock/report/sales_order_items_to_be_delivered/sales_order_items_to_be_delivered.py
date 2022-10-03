72478befefc42dd0af8f4d229f5de00a28ac678d
Haider Ali Khan <haiderk0987@gmail.com>
2022-10-03 12:56:28 +0500

    fix: added fields in purchase order item received report

--- erpnext/stock/report/sales_order_items_to_be_delivered/sales_order_items_to_be_delivered.py
+++ erpnext/stock/report/sales_order_items_to_be_delivered/sales_order_items_to_be_delivered.py
@@ -31,34 +31,23 @@ class OrderItemFulfilmentTracker:
 
 		orders = frappe.db.sql("""
 			SELECT
-				o.company,
-				o.name,
-				o.status,
-				o.transaction_date,
-				o.{party_type} as party,
-				o.project,
-				o.{schedule_date} as schedule_date,
-				o.currency,
-				i.item_code,
-				i.{qty_field} as qty,
-				i.{completed_qty} as completed_qty,
-				i.warehouse,
-				i.item_name,
-				i.brand,
-				i.rate,
-				i.amount
+				o.company, o.name, o.status, o.transaction_date, o.{party_type} as party, o.project,
+				o.{schedule_date} as schedule_date, o.currency, i.item_code, i.{qty_field} as qty,
+				i.{completed_qty} as completed_qty, i.warehouse, i.item_name, i.brand, i.rate, i.amount
 			FROM `tab{doctype}` o
 			INNER JOIN `tab{doctype} Item` i ON i.parent = o.name
 			INNER JOIN `tabItem` im on im.name = i.item_code
 			WHERE
-				o.docstatus = 1 AND o.status != 'Closed' and im.is_stock_item = 1
+				o.docstatus = 1 AND o.status != 'Closed' AND im.is_stock_item = 1
+				{conditions}
 			ORDER BY o.transaction_date
 		""".format(
 				party_type=self.fields.party,
 				schedule_date=self.fields.schedule_date,
 				qty_field=self.fields.qty,
 				completed_qty=self.fields.completed_qty,
-				doctype=self.filters.doctype
+				doctype=self.filters.doctype,
+				conditions = self.conditions
 			), self.filters, as_dict=1)
 
 		stock_qty_date = []
@@ -70,7 +59,7 @@ class OrderItemFulfilmentTracker:
 				from `tabBin`
 				where (item_code, warehouse) in %s
 				group by item_code, warehouse
-			""", [item_warehouse_list], as_dict=1)
+			""", [item_warehouse_list], as_dict=1,debug=1)
 
 		stock_qty_map = {}
 		for d in stock_qty_data:
@@ -79,7 +68,7 @@ class OrderItemFulfilmentTracker:
 
 		for d in orders:
 			d["remaining_qty"] = d["qty"] - d["completed_qty"]
-			d["delay_days"] = (getdate() - d["schedule_date"]).days
+			d["delay_days"] = max((getdate() - d["schedule_date"]).days, 0)
 			d["actual_qty"] = flt(stock_qty_map.get((d.item_code, d.warehouse)))
 
 		self.data = orders
@@ -95,19 +84,13 @@ class OrderItemFulfilmentTracker:
 		if self.filters.customer:
 			conditions.append("o.customer = %(customer)s")
 
-		if self.filters.item_source:
-			conditions.append("im.item_source = %(item_source)s")
-
-		if self.filters.brand:
-			conditions.append("im.brand = %(brand)s")
-
 		if self.filters.customer_group:
 			lft, rgt = frappe.db.get_value("Customer Group", self.filters.customer_group, ["lft", "rgt"])
 			conditions.append("""
 				o.customer_group IN (
 					SELECT name
 					FROM `tabCustomer Group`
-					WHERE lft>={} AND rgt<={} AND docstatus<2)
+					WHERE lft>={0} AND rgt<={1} AND docstatus<2)
 				""".format(lft, rgt))
 
 		if self.filters.item_code:
@@ -124,16 +107,26 @@ class OrderItemFulfilmentTracker:
 					FROM `tabItem Group`
 					WHERE lft>=%s AND rgt<=%s AND docstatus<2)""" % (lft, rgt))
 
+		if self.filters.brand:
+			conditions.append("im.brand = %(brand)s")
+
+		if self.filters.item_source:
+			conditions.append("im.item_source = %(item_source)s")
+
 		if self.filters.project:
 			if isinstance(self.filters.project, string_types):
 				self.filters.project = [d.strip() for d in cstr(self.filters.project).split(',')]
 			conditions.append("o.project in %(project)s")
 
+		if self.filters.doctype == "Sales Order":
+			conditions.append("i.delivered_qty < qty")
+		elif self.filters.doctype == "Purchase Order":
+			conditions.append("i.received_qty < qty")
+
 		self.conditions = "AND {}".format(" AND ".join(conditions)) if conditions else ""
 
 	def get_fields(self):
 		fields = frappe._dict({})
-		
 		if self.filters.doctype == "Sales Order":
 			fields.completed_qty = "delivered_qty"
 			fields.schedule_date = "delivery_date"
