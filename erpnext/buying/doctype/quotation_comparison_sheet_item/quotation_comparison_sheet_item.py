a9412b71d5d93319e88ce6c084a5e324c5a19c80
Anthony C. Emmanuel <mymi14s@hotmail.com>
2022-10-04 09:24:48 +0100

    Update quotation_comparison_sheet_item.py

--- erpnext/buying/doctype/quotation_comparison_sheet_item/quotation_comparison_sheet_item.py
+++ erpnext/buying/doctype/quotation_comparison_sheet_item/quotation_comparison_sheet_item.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020, omar jaber and contributors
 # For license information, please see license.txt
-
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
-
 
 class QuotationComparisonSheetItem(Document):
 	pass
