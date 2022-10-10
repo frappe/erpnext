fff9e7671844d8b6df9ff9a25dfdbfb5b5b58ba1
codezart <abdur.rahman.00798@gmail.com>
2022-10-10 11:24:42 -0400

    fix: number of months subscription plan
    (cherry picked from commit 2d30b36ccaef9a07ca38f503c8187dc5c022ccb7)

--- erpnext/accounts/doctype/subscription_plan/subscription_plan.py
+++ erpnext/accounts/doctype/subscription_plan/subscription_plan.py
@@ -8,6 +8,7 @@ from frappe.model.document import Document
 from frappe.utils import date_diff, flt, get_first_day, get_last_day, getdate
 
 from erpnext.utilities.product import get_price
+from dateutil import relativedelta
 
 
 class SubscriptionPlan(Document):
@@ -49,7 +50,7 @@ def get_plan_rate(
 		start_date = getdate(start_date)
 		end_date = getdate(end_date)
 
-		no_of_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1
+		no_of_months = relativedelta.relativedelta(end_date, start_date).months + 1
 		cost = plan.cost * no_of_months
 
 		# Adjust cost if start or end date is not month start or end
