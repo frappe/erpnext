168f3e1652df6c8f610a98c04d30168cbc126ed9
abdulsamad2929 <asramzani@gmail.com>
2022-10-13 11:36:00 +0500

    change JS Item PO rate setting

--- erpnext/buying/doctype/purchase_order/purchase_order.js
+++ erpnext/buying/doctype/purchase_order/purchase_order.js
@@ -58,7 +58,21 @@ frappe.ui.form.on("Purchase Order", {
 		if(frm.doc.purchase_order_type != ""){
 			frm.set_df_property('purchase_order_type','read_only',true);
 		}
-
+		
+		if(frm.doc.purchase_order_type == "Local" || frm.doc.purchase_order_type == "Import"){
+			
+			var rate_field = frappe.meta.get_docfield("Purchase Order Item","rate", frm.doc.name);
+			rate_field.read_only = 1;
+			var price_list_field = frappe.meta.get_docfield("Purchase Order Item","price_list_rate", frm.doc.name);
+			price_list_field.read_only = 1;
+			refresh_field("items");
+		}else{
+			var rate_field = frappe.meta.get_docfield("Purchase Order Item","rate", frm.doc.name);
+			rate_field.read_only = 0;
+			var price_list_field = frappe.meta.get_docfield("Purchase Order Item","price_list_rate", frm.doc.name);
+			price_list_field.read_only = 0;
+			refresh_field("items");
+		}
 		erpnext.queries.setup_queries(frm, "Warehouse", function() {
 			return erpnext.queries.warehouse(frm.doc);
 		});
