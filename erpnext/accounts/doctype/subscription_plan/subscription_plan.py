cab3f39f9500c89b28bb91f31e30a78e3be03c9e
Deepesh Garg <deepeshgarg6@gmail.com>
2022-10-20 12:09:42 +0530

    chore: Linting issues
    (cherry picked from commit 1ca472cc8a795fed132979a9ba3a2538ab13ca0a)

--- erpnext/accounts/doctype/subscription_plan/subscription_plan.py
+++ erpnext/accounts/doctype/subscription_plan/subscription_plan.py
@@ -3,12 +3,12 @@
 
 
 import frappe
+from dateutil import relativedelta
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils import date_diff, flt, get_first_day, get_last_day, getdate
 
 from erpnext.utilities.product import get_price
-from dateutil import relativedelta
 
 
 class SubscriptionPlan(Document):
