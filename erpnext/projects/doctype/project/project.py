f49c034ef279b3174e76b8f1e90fe9eb3cb60100
Haider Ali Khan <haiderk0987@gmail.com>
2022-10-21 13:21:48 +0500

    fix: vehicle panel quantity validation for negative value

--- erpnext/projects/doctype/project/project.py
+++ erpnext/projects/doctype/project/project.py
@@ -96,6 +96,7 @@ class Project(StatusUpdater):
 		self.validate_applies_to()
 		self.validate_readings()
 		self.validate_depreciation()
+		self.validate_panel_qty()
 
 		self.set_percent_complete()
 		self.set_vehicle_status()
@@ -119,6 +120,11 @@ class Project(StatusUpdater):
 		has_panel_project_template = [d for d in self.project_templates if d.is_panel_job]
 		if not has_panel_project_template:
 			self.vehicle_panels = []
+
+	def validate_panel_qty(self):
+		for d in self.vehicle_panels:
+			if flt(d.panel_qty) < 0:
+				frappe.throw(_("Row {0}: Vehicle Panel Qty cannot be negative").format(d.idx))
 
 	def on_update(self):
 		self.update_appointment()
