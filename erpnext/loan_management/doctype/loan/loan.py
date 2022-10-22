1b3a0be0f1067d4572eb440fca94a5bdcccf46b5
Deepesh Garg <deepeshgarg6@gmail.com>
2022-10-22 23:46:01 +0530

    chore: Add repayment date on option
    (cherry picked from commit ef0cb17fafce696fff11685a5cf74e936a3f0508)

--- erpnext/loan_management/doctype/loan/loan.py
+++ erpnext/loan_management/doctype/loan/loan.py
@@ -12,7 +12,6 @@ from frappe.utils import (
 	add_months,
 	date_diff,
 	flt,
-	get_first_day,
 	get_last_day,
 	getdate,
 	now_datetime,
@@ -117,36 +116,51 @@ class Loan(AccountsController):
 		if not self.repayment_start_date:
 			frappe.throw(_("Repayment Start Date is mandatory for term loans"))
 
-		schedule_type = frappe.db.get_value("Loan Type", self.loan_type, "repayment_schedule_type")
+		schedule_type_details = frappe.db.get_value(
+			"Loan Type", self.loan_type, ["repayment_schedule_type", "repayment_date_on"], as_dict=1
+		)
+
 		self.repayment_schedule = []
 		payment_date = self.repayment_start_date
 		balance_amount = self.loan_amount
 
 		while balance_amount > 0:
 			interest_amount, principal_amount, balance_amount, total_payment = self.get_amounts(
-				payment_date, balance_amount, schedule_type
+				payment_date,
+				balance_amount,
+				schedule_type_details.repayment_schedule_type,
+				schedule_type_details.repayment_date_on,
 			)
 
-			if schedule_type == "Pro-rated calendar months":
-				next_payment_date = add_days(get_last_day(payment_date), 1)
+			if schedule_type_details.repayment_schedule_type == "Pro-rated calendar months":
+				next_payment_date = get_last_day(payment_date)
+				if schedule_type_details.repayment_date_on == "Start of the next month":
+					next_payment_date = add_days(next_payment_date, 1)
+
 				payment_date = next_payment_date
 
 			self.add_repayment_schedule_row(
 				payment_date, principal_amount, interest_amount, total_payment, balance_amount
 			)
 
-			if schedule_type == "Monthly as per repayment start date":
+			if (
+				schedule_type_details.repayment_schedule_type == "Monthly as per repayment start date"
+				or schedule_type_details.repayment_date_on == "End of the current month"
+			):
 				next_payment_date = add_single_month(payment_date)
 				payment_date = next_payment_date
 
-	def get_amounts(self, payment_date, balance_amount, schedule_type):
-		first_day_of_month = get_first_day(payment_date)
-
+	def get_amounts(self, payment_date, balance_amount, schedule_type, repayment_date_on):
 		if schedule_type == "Monthly as per repayment start date":
 			days = 1
 			months = 12
 		else:
-			if first_day_of_month == payment_date:
+			expected_payment_date = get_last_day(payment_date)
+			if repayment_date_on == "Start of the next month":
+				expected_payment_date = add_days(expected_payment_date, 1)
+
+			if expected_payment_date == payment_date:
+				# using 30 days for calculating interest for all full months
 				days = 30
 				months = 365
 			else:
