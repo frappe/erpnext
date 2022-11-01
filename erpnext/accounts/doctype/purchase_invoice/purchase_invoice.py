88889579529db4f4222125cab1300b463f535d93
Deepesh Garg <deepeshgarg6@gmail.com>
2022-11-01 19:54:41 +0530

    fix: Issues while cancel/amending Purchase Invoice with TDS enabled
    (cherry picked from commit f7c9258770a79aff0b951dc18c0b7c39f794b03f)
    
    # Conflicts:
    #	erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -67,6 +67,9 @@ class PurchaseInvoice(BuyingController):
 		supplier_tds = frappe.db.get_value("Supplier", self.supplier, "tax_withholding_category")
 		self.set_onload("supplier_tds", supplier_tds)
 
+		if self.is_new():
+			self.set("tax_withheld_vouchers", [])
+
 	def before_save(self):
 		if not self.on_hold:
 			self.release_date = ""
@@ -1373,7 +1376,12 @@ class PurchaseInvoice(BuyingController):
 			"GL Entry",
 			"Stock Ledger Entry",
 			"Repost Item Valuation",
+<<<<<<< HEAD
 			"Purchase Invoice",
+=======
+			"Payment Ledger Entry",
+			"Tax Withheld Vouchers",
+>>>>>>> f7c9258770 (fix: Issues while cancel/amending Purchase Invoice with TDS enabled)
 		)
 
 		self.update_advance_tax_references(cancel=1)
