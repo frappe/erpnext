523566fde9f73d80bfc643e86ff18589f80fa0c2
Saif Ur Rehman <saifi0102@gmail.com>
2022-11-17 13:07:01 +0500

    fix(Item Dashboard): Fix link

--- erpnext/stock/doctype/item/item.js
+++ erpnext/stock/doctype/item/item.js
@@ -353,7 +353,7 @@ $.extend(erpnext.item, {
 		if (frm.doc.is_stock_item) {
 			frappe.require('item-dashboard.bundle.js', function() {
 				var section = frm.dashboard.add_section('<h5 style="margin-top: 0px;">\
-					<a href="#stock-balance">' + __("Stock Levels") + '</a></h5>');
+					<a href="/app/stock-balance">' + __("Stock Levels") + '</a></h5>');
 				erpnext.item.item_dashboard = new erpnext.stock.ItemDashboard({
 					parent: section,
 					item_code: frm.doc.name
