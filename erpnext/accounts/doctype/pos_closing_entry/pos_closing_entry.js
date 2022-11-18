9e8a8356e9ede1e19e68bde1c7f8021a6842feb9
gavin <gavin18d@gmail.com>
2022-11-18 17:17:54 +0530

    fix(realtime): Restrict updates to only last modified or current user (#33034)
    (cherry picked from commit dd2493a541f0f8f8a231c4ba32970898b1bab224)

--- erpnext/accounts/doctype/pos_closing_entry/pos_closing_entry.js
+++ erpnext/accounts/doctype/pos_closing_entry/pos_closing_entry.js
@@ -25,7 +25,7 @@ frappe.ui.form.on('POS Closing Entry', {
 
 		frappe.realtime.on('closing_process_complete', async function(data) {
 			await frm.reload_doc();
-			if (frm.doc.status == 'Failed' && frm.doc.error_message && data.user == frappe.session.user) {
+			if (frm.doc.status == 'Failed' && frm.doc.error_message) {
 				frappe.msgprint({
 					title: __('POS Closing Failed'),
 					message: frm.doc.error_message,
