c03ec80d1add7d2b854cf2252cc43c1a51e67c81
Sabu Siyad <hello@ssiyad.com>
2022-11-24 15:40:05 +0530

    fix(pos): warehouse should be in company
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>
    (cherry picked from commit 95a620a30d67e6b2ac5137d87efa97f3f149b95f)

--- erpnext/accounts/doctype/pos_invoice/pos_invoice.js
+++ erpnext/accounts/doctype/pos_invoice/pos_invoice.js
@@ -12,6 +12,8 @@ erpnext.selling.POSInvoiceController = class POSInvoiceController extends erpnex
 
 	company() {
 		erpnext.accounts.dimensions.update_dimension(this.frm, this.frm.doctype);
+		this.frm.set_value("set_warehouse", "");
+		this.frm.set_value("taxes_and_charges", "");
 	}
 
 	onload(doc) {
