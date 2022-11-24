acf8604b858b82754c3c6848c69ed843f518fa0a
Furqan Asghar <furqan.790@outlook.com>
2022-11-24 14:55:02 +0500

    fix: get_rate_as_cost as controller function along with check "valuation read permission"

--- erpnext/controllers/selling_controller.py
+++ erpnext/controllers/selling_controller.py
@@ -6,7 +6,7 @@ import frappe
 from frappe.utils import cint, flt, cstr, comma_or
 from frappe import _, throw
 from erpnext.stock.get_item_details import get_bin_details
-from erpnext.stock.utils import get_incoming_rate
+from erpnext.stock.utils import get_incoming_rate, has_valuation_read_permission
 from erpnext.stock.get_item_details import get_conversion_factor, get_target_warehouse_validation
 from erpnext.stock.doctype.batch.batch import get_batch_qty, auto_select_and_split_batches
 from erpnext.setup.doctype.sales_person.sales_person import get_sales_person_commission_details
@@ -551,45 +551,49 @@ class SellingController(StockController):
 
 	@frappe.whitelist()
 	def set_rate_as_cost(self):
-		for item in self.items:
-			if item.item_code:
-				cost_rate = get_rate_as_cost(item.item_code, item.warehouse, item.batch_no, item.serial_no,
-					self.posting_date, self.posting_time, item.qty, self.doctype, self.name,
-					self.company, item.delivery_note, item.delivery_note_item)
-				item.rate = cost_rate
-				item.discount_percentage = 0
-				item.margin_rate_or_amount = 0
+		if has_valuation_read_permission():
+			for item in self.items:
+				if item.item_code:
+					cost_rate = self.get_rate_as_cost(item)
+					item.rate = cost_rate
+					item.discount_percentage = 0
+					item.margin_rate_or_amount = 0
 
-		self.calculate_taxes_and_totals()
+			self.calculate_taxes_and_totals()
+		else:
+			frappe.throw('User not allowed to read valuation rate')
 
 
-@frappe.whitelist()
-def get_rate_as_cost(item_code, warehouse, batch_no, serial_no, posting_date, posting_time, qty, voucher_type,
-		voucher_no, company, delivery_note=None, delivery_note_item=None):
-	qty = -1 * qty
-	if delivery_note and delivery_note_item:
-		sum_of_stock_value_difference = frappe.db.sql("""
-			SELECT SUM(stock_value_difference)
-			FROM `tabStock Ledger Entry` 
-			WHERE voucher_type = 'Delivery Note' AND voucher_no = %s AND voucher_detail_no = %s
-		""", (delivery_note, delivery_note_item))
 
-		cost_rate = flt(sum_of_stock_value_difference[0][0]) / qty
+	def get_rate_as_cost(self, item):
+		qty = -1 * flt(item.qty)
+		if item.delivery_note and item.delivery_note_item:
+			sum_of_stock_value_difference = frappe.db.sql("""
+				SELECT SUM(stock_value_difference)
+				FROM `tabStock Ledger Entry` 
+				WHERE voucher_type = 'Delivery Note' AND voucher_no = %s AND voucher_detail_no = %s
+			""", (item.delivery_note, item.delivery_note_item))
 
-	else:
-		from erpnext.stock.utils import get_incoming_rate
-		args = frappe._dict({
-			"item_code": item_code,
-			"warehouse": warehouse,
-			"batch_no": batch_no,
-			"serial_no": serial_no,
-			"posting_date": posting_date,
-			"posting_time": posting_time,
-			"qty": qty,
-			"voucher_type": voucher_type,
-			"voucher_no": voucher_no,
-			"company": company
-		})
-		cost_rate = get_incoming_rate(args, raise_error_if_no_rate=False)
+			sum_of_stock_value_difference = sum_of_stock_value_difference[0][0] or 0
+			if qty:
+				cost_rate = flt(sum_of_stock_value_difference) / qty
+			else:
+				cost_rate = sum_of_stock_value_difference
 
-	return cost_rate
+		else:
+			from erpnext.stock.utils import get_incoming_rate
+			args = frappe._dict({
+				"item_code": item.item_code,
+				"warehouse": item.warehouse,
+				"batch_no": item.batch_no,
+				"serial_no": item.serial_no,
+				"posting_date": self.posting_date,
+				"posting_time": self.posting_time,
+				"qty": item.qty,
+				"voucher_type": self.doctype,
+				"voucher_no": self.name,
+				"company": self.company
+			})
+			cost_rate = get_incoming_rate(args, raise_error_if_no_rate=False)
+
+		return cost_rate
