c9f4f6042584632cac884b1e2c687a8633435a24
s-aga-r <sagarsharma.s312@gmail.com>
2022-11-27 16:57:38 +0530

    fix: `production_item` filter in `Job Card Summary Report`
    (cherry picked from commit ef7fd670fcbe3b5c0ea776bfd54f73e34d57509f)

--- erpnext/manufacturing/report/job_card_summary/job_card_summary.py
+++ erpnext/manufacturing/report/job_card_summary/job_card_summary.py
@@ -36,7 +36,7 @@ def get_data(filters):
 		"total_time_in_mins",
 	]
 
-	for field in ["work_order"]:
+	for field in ["work_order", "production_item"]:
 		if filters.get(field):
 			query_filters[field] = ("in", filters.get(field))
 
