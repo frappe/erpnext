362f7e933f4e6b6c16983b039ae455f1613052f8
Haider Ali Khan <haiderk0987@gmail.com>
2022-11-30 13:10:18 +0500

    chore: made PR changes

--- erpnext/vehicles/report/vehicle_maintenance_schedule/vehicle_maintenance_schedule.js
+++ erpnext/vehicles/report/vehicle_maintenance_schedule/vehicle_maintenance_schedule.js
@@ -2,20 +2,20 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Vehicle Maintenance Reminder"] = {
+frappe.query_reports["Vehicle Maintenance Schedule"] = {
 	"filters": [
 		{
 			"fieldname":"from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
-			"default": frappe.datetime.month_start(),
+			"default": frappe.datetime.add_days(frappe.datetime._date(), -3),
 			"reqd": 1
 		},
 		{
 			"fieldname":"to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
-			"default": frappe.datetime.month_end(),
+			"default": frappe.datetime.add_days(frappe.datetime._date(), 3),
 			"reqd": 1
 		}
 	]
--- erpnext/vehicles/report/vehicle_maintenance_schedule/vehicle_maintenance_schedule.py
+++ erpnext/vehicles/report/vehicle_maintenance_schedule/vehicle_maintenance_schedule.py
@@ -4,15 +4,15 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-from frappe.utils import getdate, today, flt, add_months, today
+from frappe.utils import getdate, today
 from dateutil.relativedelta import relativedelta
-from datetime import datetime
+
 
 def execute(filters=None):
-	return VehicleMaintenanceReminder(filters).run()
+	return VehicleMaintenanceSchedule(filters).run()
 
 
-class VehicleMaintenanceReminder:
+class VehicleMaintenanceSchedule:
 	def __init__(self, filters=None):
 		self.filters = frappe._dict(filters or {})
 		self.filters.from_date = getdate(filters.from_date or today())
@@ -40,50 +40,53 @@ class VehicleMaintenanceReminder:
 		vehicle_data = frappe.db.sql("""
 			SELECT
 				name as vehicle, item_code, item_name, customer, customer_name, delivery_date,
-				chassis_no, engine_no, license_plate, unregistered, variant_of, variant_of_nam e
+				chassis_no, engine_no, license_plate, unregistered, variant_of, variant_of_name
 			FROM `tabVehicle`
 			WHERE {0} and ifnull(delivery_document_no, '') != ''
 		""".format(vehicle_or_conditions), self.filters, as_dict=1)
 
 		self.data = vehicle_data
 
-
 	def process_project_template_data(self):
+		self.applies_to_item_cache = frappe._dict()
 		vehicle_or_conditions = []
-		applies_to_item_cache = frappe._dict()
 
 		for d in self.project_template_data:
 			template_condition = []
 			if d.applies_to_item:
-				if d.applies_to_item not in applies_to_item_cache:
-					item_codes = frappe.db.get_all("Item", filters={'variant_of': d.applies_to_item}, fields="item_code")
-					item_codes = [d.applies_to_item] + [i.item_code for i in item_codes]
-					applies_to_item_cache[d.applies_to_item] = item_codes
-				else:
-					item_codes = applies_to_item_cache[d.applies_to_item]
+				item_codes = self.get_item_codes(d.applies_to_item)
 
 				template_condition.append("item_code in ({0})"
 					.format(", ".join([frappe.db.escape(item_code) for item_code in item_codes])))
 
 			delivery_from_date = self.filters.from_date - relativedelta(months=d.due_after)
 			delivery_to_date = self.filters.to_date - relativedelta(months=d.due_after)
+
 			template_condition.append("delivery_date between {0} and {1}".format(
 				frappe.db.escape(delivery_from_date),
 				frappe.db.escape(delivery_to_date)
 			))
-			d.from_date, d.to_date = delivery_from_date, delivery_to_date
 
 			vehicle_or_conditions.append(" and ".join(template_condition))
+			d.from_date, d.to_date = delivery_from_date, delivery_to_date
 
 		vehicle_or_conditions = " or ".join(vehicle_or_conditions)
 		return vehicle_or_conditions
 
+	def get_item_codes(self, applies_to_item):
+		if applies_to_item not in self.applies_to_item_cache:
+			item_codes = frappe.db.get_all("Item", filters={'variant_of': applies_to_item}, fields="item_code")
+			item_codes = [applies_to_item] + [i.item_code for i in item_codes]
+			self.applies_to_item_cache[applies_to_item] = item_codes
+		else:
+			item_codes = self.applies_to_item_cache[applies_to_item]
+		
+		return item_codes
+
 	def process_data(self):
 		for d in self.data:
 			rd = relativedelta(getdate(), d.delivery_date)
-			duration_list = [str(x) + y for x, y in zip([rd.years, rd.months, rd.days], ['Y', 'M', 'D'])
-				if x or y=='D']
-			d.age = " ".join(duration_list)
+			d.age = self.convert_delta_to_string(rd)
 
 			for p in self.project_template_data:
 				if p.applies_to_item in [d.variant_of, d.item_code]\
@@ -92,8 +95,15 @@ class VehicleMaintenanceReminder:
 					break
 			
 			d.due_date = d.delivery_date + relativedelta(months=p.due_after)
+
 			if not d.license_plate and d.unregistered:
 				d.license_plate = 'Unreg'
+
+	def convert_delta_to_string(self, rd):
+		template = ['Y', 'M', 'D']
+		data = [rd.years, rd.months, rd.days]
+		duration = " ".join([str(x) + y for x, y in zip(data, template) if x or y=='D'])
+		return duration
 
 	def get_columns(self):
 		columns = [
