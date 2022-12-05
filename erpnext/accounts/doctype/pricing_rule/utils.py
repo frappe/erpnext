033b2a4825a060c60997b04802213723e28dd414
Deepesh Garg <deepeshgarg6@gmail.com>
2022-12-05 15:43:03 +0530

    chore: Add POS Invoices
    (cherry picked from commit 5f821b93a5832ee3f37d37dc9192ac656be3224e)

--- erpnext/accounts/doctype/pricing_rule/utils.py
+++ erpnext/accounts/doctype/pricing_rule/utils.py
@@ -244,7 +244,7 @@ def get_other_conditions(conditions, values, args):
 			and ifnull(`tabPricing Rule`.valid_upto, '2500-12-31')"""
 		values["transaction_date"] = args.get("transaction_date")
 
-	if args.get("doctype") in ["Sales Order", "Delivery Note", "Sales Invoice"]:
+	if args.get("doctype") in ["Sales Order", "Delivery Note", "Sales Invoice", "POS Invoice"]:
 		conditions += """ and ifnull(`tabPricing Rule`.selling, 0) = 1"""
 	else:
 		conditions += """ and ifnull(`tabPricing Rule`.buying, 0) = 1"""
