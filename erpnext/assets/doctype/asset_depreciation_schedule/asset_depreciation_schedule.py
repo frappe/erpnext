a358f777746c8acf83b40e6a53d1c3144ab7ae0f
anandbaburajan <anandbaburajan@gmail.com>
2022-12-15 14:50:53 +0530

    chore: move patch to post_model_sync and cancel asset depr schedule properly

--- erpnext/assets/doctype/asset_depreciation_schedule/asset_depreciation_schedule.py
+++ erpnext/assets/doctype/asset_depreciation_schedule/asset_depreciation_schedule.py
@@ -170,6 +170,7 @@ def cancel_asset_depr_schedules(asset_doc):
 		frappe.db.set_value(
 			"Asset Depreciation Schedule", asset_depr_schedule_doc.name, "status", "Cancelled"
 		)
+		asset_depr_schedule_doc.reload()
 
 		asset_depr_schedule_doc.cancel()
 
