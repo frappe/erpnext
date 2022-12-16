6129496b5031ae2e8d258a491ed7e61c66e08527
Saif Ur Rehman <saifi0102@gmail.com>
2022-12-16 17:59:45 +0500

    fix: Validate qty not 0 for PREC and DN

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -125,8 +125,7 @@ class AccountsController(TransactionBase):
 						_('{0} is blocked so this transaction cannot proceed'.format(supplier_name)), raise_exception=1)
 
 	def validate(self):
-		if not self.get('is_return'):
-			self.validate_qty_is_not_zero()
+		self.validate_qty_is_not_zero()
 
 		if self.get("_action") and self._action != "update_after_submit":
 			self.set_missing_values(for_validate=True)
@@ -596,9 +595,12 @@ class AccountsController(TransactionBase):
 		return gl_dict
 
 	def validate_qty_is_not_zero(self):
+		if self.get('is_return') and self.doctype in ("Sales Invoice", "Purchase Invoice") and not self.update_stock:
+			return
+
 		for item in self.items:
 			if not item.qty and not item.get('rejected_qty'):
-				frappe.throw("Item Quantity can not be zero")
+				frappe.throw(_("Row #{0}: Item Quantity can not be zero").format(item.idx))
 
 	def validate_account_currency(self, account, account_currency=None):
 		valid_currency = [self.company_currency]
