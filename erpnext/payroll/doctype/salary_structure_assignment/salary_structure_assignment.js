c3b9059c1b8a5a829c0391dd1a97db33cf9adc3d
Saurabh <saurabh6790@gmail.com>
2022-12-21 17:33:37 +0530

    feat: provision to setup opening balances for earnings and deductions while creating SSA

--- erpnext/payroll/doctype/salary_structure_assignment/salary_structure_assignment.js
+++ erpnext/payroll/doctype/salary_structure_assignment/salary_structure_assignment.js
@@ -42,6 +42,13 @@ frappe.ui.form.on('Salary Structure Assignment', {
 		});
 	},
 
+	refresh: function(frm) {
+		if(frm.doc.__onload){
+			frm.unhide_earnings_and_taxation_section = frm.doc.__onload.earning_and_deduction_entries_does_not_exists;
+			frm.trigger("set_earnings_and_taxation_section_visibility");
+		}
+	},
+
 	employee: function(frm) {
 		if(frm.doc.employee){
 			frappe.call({
@@ -59,6 +66,8 @@ frappe.ui.form.on('Salary Structure Assignment', {
 					}
 				}
 			});
+
+			frm.trigger("valiadte_joining_date_and_salary_slips");
 		}
 		else{
 			frm.set_value("company", null);
@@ -71,5 +80,33 @@ frappe.ui.form.on('Salary Structure Assignment', {
 				frm.set_value("payroll_payable_account", r.default_payroll_payable_account);
 			});
 		}
-	}
+	},
+
+	valiadte_joining_date_and_salary_slips: function(frm) {
+		frappe.call({
+			method: "earning_and_deduction_entries_does_not_exists",
+			doc: frm.doc,
+			callback: function(data) {
+				let earning_and_deduction_entries_does_not_exists = data.message;
+				frm.unhide_earnings_and_taxation_section = earning_and_deduction_entries_does_not_exists;
+				frm.trigger("set_earnings_and_taxation_section_visibility");
+			}
+		});
+	},
+
+	set_earnings_and_taxation_section_visibility: function(frm) {
+		if(frm.unhide_earnings_and_taxation_section){
+			frm.set_df_property('earnings_and_taxation_section', 'hidden', 0);
+		}
+		else{
+			frm.set_df_property('earnings_and_taxation_section', 'hidden', 1);
+		}
+	},
+
+	from_date: function(frm) {
+		if (frm.doc.from_date) {
+			frm.trigger("valiadte_joining_date_and_salary_slips" );
+		}
+	},
+
 });
