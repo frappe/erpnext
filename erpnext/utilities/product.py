556bb9d36fb8147329ae511fbff0cc48a638a153
Sabu Siyad <hello@ssiyad.com>
2023-01-04 13:06:25 +0530

    refactor: move `get_item_codes_by_attributes` to `utilities/product`
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- erpnext/utilities/product.py
+++ erpnext/utilities/product.py
@@ -201,3 +201,64 @@ def get_non_stock_item_status(item_code, item_warehouse_field):
         )
     else:
         return 1
+
+
+def get_item_codes_by_attributes(attribute_filters, template_item_code=None):
+    items = []
+
+    for attribute, values in attribute_filters.items():
+        attribute_values = values
+
+        if not isinstance(attribute_values, list):
+            attribute_values = [attribute_values]
+
+        if not attribute_values:
+            continue
+
+        wheres = []
+        query_values = []
+        for attribute_value in attribute_values:
+            wheres.append("( attribute = %s and attribute_value = %s )")
+            query_values += [attribute, attribute_value]
+
+        attribute_query = " or ".join(wheres)
+
+        if template_item_code:
+            variant_of_query = "AND t2.variant_of = %s"
+            query_values.append(template_item_code)
+        else:
+            variant_of_query = ""
+
+        query = """
+			SELECT
+				t1.parent
+			FROM
+				`tabItem Variant Attribute` t1
+			WHERE
+				1 = 1
+				AND (
+					{attribute_query}
+				)
+				AND EXISTS (
+					SELECT
+						1
+					FROM
+						`tabItem` t2
+					WHERE
+						t2.name = t1.parent
+						{variant_of_query}
+				)
+			GROUP BY
+				t1.parent
+			ORDER BY
+				NULL
+		""".format(
+            attribute_query=attribute_query, variant_of_query=variant_of_query
+        )
+
+        item_codes = set([r[0] for r in frappe.db.sql(query, query_values)])
+        items.append(item_codes)
+
+    res = list(set.intersection(*items))
+
+    return res
