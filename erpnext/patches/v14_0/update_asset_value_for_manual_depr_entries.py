da478dc0eb952edf4ab66562f158bc53ad1eca67
anandbaburajan <anandbaburajan@gmail.com>
2023-01-27 01:00:19 +0530

    chore: get depreciation_expense_account properly

--- erpnext/patches/v14_0/update_asset_value_for_manual_depr_entries.py
+++ erpnext/patches/v14_0/update_asset_value_for_manual_depr_entries.py
@@ -4,19 +4,21 @@ import frappe
 def execute():
 	asset = frappe.qb.DocType("Asset")
 	gle = frappe.qb.DocType("GL Entry")
+	aca = frappe.qb.DocType("Asset Category Account")
+	company = frappe.qb.DocType("Company")
 
-	depreciation_expense_account = frappe.db.get_value(
-		"Asset Category Account",
-		filters={"parent": asset.asset_category, "company_name": asset.company},
-		fieldname="depreciation_expense_account",
-	)
-
-	frappe.qb.update(asset).inner_join(gle).on(gle.against_voucher == asset.name).set(
+	frappe.qb.update(asset).join(gle).on(gle.against_voucher == asset.name).join(aca).on(
+		(aca.parent == asset.asset_category) & (aca.company_name == asset.company)
+	).join(company).on(company.name == asset.company).set(
 		asset.value_after_depreciation, asset.value_after_depreciation - gle.debit
-	).where(asset.docstatus == 1).where(asset.calculate_depreciation == 0).where(
+	).where(
+		asset.docstatus == 1
+	).where(
+		asset.calculate_depreciation == 0
+	).where(
 		gle.against_voucher_type == "Asset"
 	).where(
-		gle.account == depreciation_expense_account
+		gle.account == (aca.depreciation_expense_account or company.depreciation_expense_account)
 	).where(
 		gle.debit != 0
 	).where(
