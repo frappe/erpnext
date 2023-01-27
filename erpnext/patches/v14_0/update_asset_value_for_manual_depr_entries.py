f075b971355785e658260b6f6a42c8a291682541
anandbaburajan <anandbaburajan@gmail.com>
2023-01-27 15:19:18 +0530

    chore: fix patch again

--- erpnext/patches/v14_0/update_asset_value_for_manual_depr_entries.py
+++ erpnext/patches/v14_0/update_asset_value_for_manual_depr_entries.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe.query_builder.functions import IfNull
+from frappe.query_builder.functions import IfNull, Sum
 
 
 def execute():
@@ -8,18 +8,25 @@ def execute():
 	aca = frappe.qb.DocType("Asset Category Account")
 	company = frappe.qb.DocType("Company")
 
-	frappe.qb.update(asset).join(gle).on(gle.against_voucher == asset.name).join(aca).on(
-		(aca.parent == asset.asset_category) & (aca.company_name == asset.company)
-	).join(company).on(company.name == asset.company).set(
-		asset.value_after_depreciation, asset.value_after_depreciation - gle.debit
-	).where(
-		asset.docstatus == 1
-	).where(
-		asset.calculate_depreciation == 0
-	).where(
-		gle.account == IfNull(aca.depreciation_expense_account, company.depreciation_expense_account)
-	).where(
-		gle.debit != 0
-	).where(
-		gle.is_cancelled == 0
-	).run()
+	depr_values = (
+		frappe.qb.from_(gle)
+		.join(asset)
+		.on(gle.against_voucher == asset.name)
+		.join(aca)
+		.on((aca.parent == asset.asset_category) & (aca.company_name == asset.company))
+		.join(company)
+		.on(company.name == asset.company)
+		.select(Sum(gle.debit).as_("value"), asset.name.as_("asset_name"))
+		.where(
+			gle.account == IfNull(aca.depreciation_expense_account, company.depreciation_expense_account)
+		)
+		.where(gle.debit != 0)
+		.where(gle.is_cancelled == 0)
+		.where(asset.docstatus == 1)
+		.where(asset.calculate_depreciation == 0)
+		.groupby(asset.name)
+	)
+
+	frappe.qb.update(asset).join(depr_values).on(depr_values.asset_name == asset.name).set(
+		asset.value_after_depreciation, asset.value_after_depreciation - depr_values.value
+	).where(asset.docstatus == 1).where(asset.calculate_depreciation == 0).run()
