14def17f8c9a456a79e1498a21159716fa405b90
anandbaburajan <anandbaburajan@gmail.com>
2023-01-27 13:14:02 +0530

    chore: fix patch

--- erpnext/patches/v14_0/update_asset_value_for_manual_depr_entries.py
+++ erpnext/patches/v14_0/update_asset_value_for_manual_depr_entries.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.query_builder.functions import IfNull, Sum
 
 
 def execute():
@@ -10,15 +11,13 @@ def execute():
 	frappe.qb.update(asset).join(gle).on(gle.against_voucher == asset.name).join(aca).on(
 		(aca.parent == asset.asset_category) & (aca.company_name == asset.company)
 	).join(company).on(company.name == asset.company).set(
-		asset.value_after_depreciation, asset.value_after_depreciation - gle.debit
+		asset.value_after_depreciation, asset.value_after_depreciation - Sum(gle.debit)
 	).where(
 		asset.docstatus == 1
 	).where(
 		asset.calculate_depreciation == 0
 	).where(
-		gle.against_voucher_type == "Asset"
-	).where(
-		gle.account == (aca.depreciation_expense_account or company.depreciation_expense_account)
+		gle.account == IfNull(aca.depreciation_expense_account, company.depreciation_expense_account)
 	).where(
 		gle.debit != 0
 	).where(
