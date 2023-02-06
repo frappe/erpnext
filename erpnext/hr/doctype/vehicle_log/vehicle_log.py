4a615509a1b2a6c2128aecf08f92bf6c98daa482
Abubakar Sohail <abubakarsohail66@gmail.com>
2023-02-06 19:45:31 +0500

    assets log management
    assets log management

--- erpnext/hr/doctype/vehicle_log/vehicle_log.py
+++ erpnext/hr/doctype/vehicle_log/vehicle_log.py
@@ -31,11 +31,11 @@ class VehicleLog(Document):
 
 @frappe.whitelist()
 def make_expense_claim(docname):
-	expense_claim = frappe.db.exists("Expense Claim", {"vehicle_log": docname})
+	expense_claim = frappe.db.exists("Expense Claim", {"vehicle_log": docname.name})
 	if expense_claim:
 		frappe.throw(_("Expense Claim {0} already exists for the Vehicle Log").format(expense_claim))
 
-	vehicle_log = frappe.get_doc("Vehicle Log", docname)
+	vehicle_log = frappe.get_doc("Vehicle Log", docname.name)
 	service_expense = sum([flt(d.expense_amount) for d in vehicle_log.service_detail])
 
 	claim_amount = service_expense + (flt(vehicle_log.price) * flt(vehicle_log.fuel_qty) or 1)
@@ -53,31 +53,40 @@ def make_expense_claim(docname):
 	})
 	return exp_claim.as_dict()
 def make_material_request(data):  
-    data =json.loads(data)
     mr = frappe.new_doc("Material Request")
     mr.material_request_type = "Material Issue"
-    mr.cost_association=data['cost_association']
-    mr.company = data['company']
+    mr.cost_association=data.cost_association
+    mr.company = data.company
     mr.title="Issue Request for Vehicle Log"
     # mr.customer = data['customer'] or '_Test Customer'
-    mr.vehicle_log=data['name']
-    mr.sub_branch=data['sub_branch']
-    mr.transaction_date=data['date']
-    mr.schedule_date=data['date']
+    mr.vehicle_log=data.name
+    mr.sub_branch=data.sub_branch
+    mr.transaction_date=data.date
+    mr.schedule_date=data.date
     mr.naming_series="MAT-MR-.YYYY.-"
     mr.request_from="RMS"
-    for item in data['service_detail']:
-        warehouse=get_warehouse(item['item_code'],data['company'])
+    for item in data.service_detail:
+        warehouse=get_warehouse(item.item_code,data.company)
         i={}
-        i['item_code']= item['item_code']
-        i["qty"]= item['qty']
-        i["uom"]= item['uom'] 
+        i['item_code']= item.item_code
+        i["qty"]= item.qty
+        i["uom"]= item.uom 
         i["conversion_factor"]= 1
-        i["schedule_date"]= data['date'] 
-        i["cost_center"]= data['cost_center']
+        i["schedule_date"]= data.date 
+        i["cost_center"]= data.cost_center
         i["warehouse"]=warehouse[1]
         mr.append("items", i)
     # mr.items=items
     mr.insert(ignore_permissions=True)
     mr.submit()
     return mr
+def get_warehouse(item,company):
+    warehouse=frappe.db.get_list('Item Default',
+    filters={
+        'company':company,
+        'parent':item
+    },
+    fields=['company', 'default_warehouse'],
+    as_list=True
+    )
+    return warehouse[0]
