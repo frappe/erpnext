7017e97e142bfea51bd9fe1e4404837596522bbd
Deepesh Garg <deepeshgarg6@gmail.com>
2023-02-13 10:27:55 +0530

    feat: Setting to allow Sales Order creation against expired quotation (#33952)
    * feat: Setting to allow Sales Order creation against expired quotation
    
    * chore: linting issues
    
    (cherry picked from commit 148703bfc2fa9248c22d69ef92d14697a91a9301)

--- erpnext/selling/doctype/quotation/quotation.py
+++ erpnext/selling/doctype/quotation/quotation.py
@@ -195,6 +195,17 @@ def get_list_context(context=None):
 
 @frappe.whitelist()
 def make_sales_order(source_name: str, target_doc=None):
+	if not frappe.db.get_singles_value(
+		"Selling Settings", "allow_sales_order_creation_for_expired_quotation"
+	):
+		quotation = frappe.db.get_value(
+			"Quotation", source_name, ["transaction_date", "valid_till"], as_dict=1
+		)
+		if quotation.valid_till and (
+			quotation.valid_till < quotation.transaction_date or quotation.valid_till < getdate(nowdate())
+		):
+			frappe.throw(_("Validity period of this quotation has ended."))
+
 	return _make_sales_order(source_name, target_doc)
 
 
