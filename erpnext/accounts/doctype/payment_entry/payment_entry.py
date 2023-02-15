68d1f0b20f883303a8d6b39b6214364b54be9082
IRFAN BAJWA <98512717+irfanbajwase@users.noreply.github.com>
2023-02-15 18:11:42 +0500

    IS paid check on payment entry

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -1150,9 +1150,9 @@ def get_party_and_account_balance(company, date, paid_from=None, paid_to=None, p
 
 @frappe.whitelist()
 def Update_payment_entry_status(payment_entry_no):
-	update_status = frappe.db.sql("""UPDATE `tabPayment Entry` SET `status`='Paid' WHERE NAME={payment_entry_no}'""".format(payment_entry_no=payment_entry_no), as_dict=True)
-	frappe.db.commit();
-	return True;
+	update_status = frappe.db.sql("""UPDATE `tabPayment Entry` SET `status`='Paid' WHERE NAME='{payment_entry_no}'""".format(payment_entry_no=payment_entry_no), as_dict=True)
+	frappe.db.commit()
+	return True
 
 @frappe.whitelist()
 def make_payment_order(source_name, target_doc=None):
