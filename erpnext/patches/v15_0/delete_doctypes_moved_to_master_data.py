e9d12d20ec0fcb03f3de28a2cdd1267e41885fb6
s-aga-r <sagarsharma.s312@gmail.com>
2023-02-21 23:57:17 +0530

    refactor!: move `Designation` DocType to Master App

--- erpnext/patches/v15_0/delete_doctypes_moved_to_master_data.py
+++ erpnext/patches/v15_0/delete_doctypes_moved_to_master_data.py
@@ -2,6 +2,14 @@ import frappe
 
 
 def execute():
-	doctypes = ["Supplier", "Activity Type", "Bank Transaction Mapping", "Bank", "Branch", "Brand"]
+	doctypes = [
+		"Supplier",
+		"Activity Type",
+		"Bank Transaction Mapping",
+		"Bank",
+		"Branch",
+		"Brand",
+		"Designation",
+	]
 	for doctype in doctypes:
 		frappe.delete_doc("DocType", doctype, ignore_missing=True)
