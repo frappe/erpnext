5d5a2fac4ac4c76fb6d55ca8c773aefa9e6e7c07
s-aga-r <sagarsharma.s312@gmail.com>
2023-03-01 18:42:15 +0530

    chore: `Linters`

--- erpnext/public/js/master/customer_group.js
+++ erpnext/public/js/master/customer_group.js
@@ -1,14 +1,17 @@
-// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
-// License: GNU General Public License v3. See license.txt
+// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
 
-
-cur_frm.fields_dict['accounts'].grid.get_field('account').get_query = function(doc, cdt, cdn) {
-	var d  = locals[cdt][cdn];
-	return {
-		filters: {
-			'account_type': 'Receivable',
-			'company': d.company,
-			"is_group": 0
+frappe.ui.form.on('Customer Group', {
+	setup(frm) {
+		frm.fields_dict['accounts'].grid.get_field('account').get_query = function(doc, cdt, cdn) {
+			var d  = locals[cdt][cdn];
+			return {
+				filters: {
+					'account_type': 'Receivable',
+					'company': d.company,
+					'is_group': 0
+				}
+			}
 		}
-	}
-}
+	},
+});
