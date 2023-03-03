2c5f7c2ce6b13e0cf66ab11e173d6dd5c6b8a0a3
marination <maricadsouza221197@gmail.com>
2023-03-03 14:13:27 +0530

    fix: Recalculate difference amount after setting deductions
    (cherry picked from commit 75ec0a0a85a010415765518f5a9e36bb13d08b22)

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -1925,6 +1925,7 @@ def get_payment_entry(dt, dn, party_amount=None, bank_account=None, bank_amount=
 
 		discount_amount = set_early_payment_discount_loss(pe, doc, valid_discounts, discount_amount)
 		if discount_amount > 0:
+			# Set pending discount amount in deductions
 			pe.set_gain_or_loss(
 				account_details={
 					"account": frappe.get_cached_value("Company", pe.company, "default_discount_account"),
@@ -1933,7 +1934,8 @@ def get_payment_entry(dt, dn, party_amount=None, bank_account=None, bank_amount=
 					"amount": discount_amount * (-1 if payment_type == "Pay" else 1),
 				}
 			)
-			pe.set_difference_amount()
+
+		pe.set_difference_amount()
 
 	return pe
 
