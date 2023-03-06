c01bed98621caf7ef6f087661365640c0bead379
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-03-06 15:35:49 +0530

    fix(test): `get_leave_allocation_records`

--- erpnext/hr/doctype/leave_application/test_leave_application.py
+++ erpnext/hr/doctype/leave_application/test_leave_application.py
@@ -1043,6 +1043,7 @@ class TestLeaveApplication(unittest.TestCase):
 			"unused_leaves": 15.0,
 			"new_leaves_allocated": 15.0,
 			"leave_type": leave_type.name,
+			"employee": employee.name,
 		}
 		self.assertEqual(details.get(leave_type.name), expected_data)
 
