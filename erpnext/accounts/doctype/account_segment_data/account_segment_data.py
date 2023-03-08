b058c64e7fe09a2f2a30e5cd8b48186f78065040
abdulsamad2929 <asramzani@gmail.com>
2023-03-08 19:49:15 +0500

    Account Segment Report

--- erpnext/accounts/doctype/account_segment_data/account_segment_data.py
+++ erpnext/accounts/doctype/account_segment_data/account_segment_data.py
@@ -13,12 +13,22 @@ class AccountSegmentData(Document):
 
 @frappe.whitelist()
 def calculate_segment_profit():
+	bgroup = ['CSD (Carbonated Soft Drinks)','Confectionery','Water','Juice','19 Ltr','Other']
 	date_yesterday = add_days(today(), -100)
 	units = get_config_by_name("segment_wise_profit_csd", {})
 	for single_unit in units:
-		# sale_bgroup =	get_unit_sales_bgroup(single_unit,date_yesterday)
-		for segment in units[single_unit].get('segment'):
-			data =get_segment_gl_data(segment,units[single_unit].get('segment').get(segment),date_yesterday,single_unit)
+		sale_bgroup =	get_unit_sales_bgroup(single_unit,date_yesterday)
+		for index,bg in enumerate(bgroup):
+			if(index<=len(sale_bgroup)):
+				if(bg in sale_bgroup[index].business_group):
+					pass
+				else:
+					sale_bgroup.append({bg,1})
+			else:
+				sale_bgroup.append({bg,1})
+
+		for coa in units[single_unit].get('segment'):
+			data =get_segment_gl_data(coa,units[single_unit].get('segment').get(coa),date_yesterday,single_unit,sale_bgroup)
 		
 
 
@@ -26,7 +36,7 @@ def calculate_segment_profit():
 
 def get_unit_sales_bgroup(unit,date):
 	return frappe.db.sql("""
-	select business_group,sum(amount) from `tabSales Invoice Item` as A
+	select business_group,sum(amount) as amount from `tabSales Invoice Item` as A
 	INNER JOIN `tabItem CSD` as B ON A.`item_code` = B.item_code
 	INNER JOIN `tabSales Invoice` AS C ON C.name = A.parent
 	 where 
@@ -37,19 +47,23 @@ def get_unit_sales_bgroup(unit,date):
 	
 
 
-def get_segment_gl_data(account_title,account,date,unit):
+def get_segment_gl_data(account_title,account,date,unit,sale_bgroup):
 	data = frappe.db.sql("""
 	SELECT SUM(`credit_in_account_currency`-`debit_in_account_currency`) AS account_value 
 FROM `tabGL Entry` WHERE account IN {1}  AND company='{3}' 
 AND DATE(creation) BETWEEN '{2}' AND '{2}'
 	""".format(account_title,tuple(account),date,unit))
-	save_doc = {
-		'doctype':'Account Segment Data',
-		'segment':'CSD',
-		'account':account_title,
-		'coa': str(account),
-		'company':unit,
-		'date':date,
-		'account_value':data[0][0]
-	}
-	frappe.get_doc(save_doc).save(ignore_permissions=True)
+
+			#business group wise sale_bgroup (account_value*busines group amount/100) 
+			# put in bussiness group as segment in data base 
+
+	# save_doc = {
+	# 	'doctype':'Account Segment Data',
+	# 	'segment':business_group,
+	# 	'account':account_title,
+	# 	'coa': str(account),
+	# 	'company':unit,
+	# 	'date':date,
+	# 	#'account_value':(account_value*busines group amount/100)
+	# }
+	#frappe.get_doc(save_doc).save(ignore_permissions=True)
