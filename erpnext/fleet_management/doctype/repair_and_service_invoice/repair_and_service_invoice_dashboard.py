c4d40c947094ff92fa10fcf4054ab74c58479a10
=Biren <>
2023-03-09 13:57:36 +0600

    r&s invoice dashboard changes applied

--- erpnext/fleet_management/doctype/repair_and_service_invoice/repair_and_service_invoice_dashboard.py
+++ erpnext/fleet_management/doctype/repair_and_service_invoice/repair_and_service_invoice_dashboard.py
@@ -4,6 +4,6 @@ def get_data():
 	return {
         "fieldname": "reference_name",
 		"transactions": [
-			{"label": _("Related Transaction"), "items": ["Journal Entry"]},
+			{"label": _("Related Transaction"), "items": ["Payment Entry"]},
 		],
 	}
