8eba3ae2b32bc443630366dd2f0b0535df5fa8ef
barredterra <14891507+barredterra@users.noreply.github.com>
2023-03-28 22:17:44 +0200

    fix: switch to supported update_linked_doctypes

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -14,7 +14,7 @@ from frappe.contacts.address_and_contact import (
 from frappe.desk.reportview import build_match_conditions, get_filters_cond
 from frappe.model.mapper import get_mapped_doc
 from frappe.model.naming import set_name_by_naming_series, set_name_from_naming_options
-from frappe.model.rename_doc import update_linked_doctypes
+from frappe.model.utils.rename_doc import update_linked_doctypes
 from frappe.utils import cint, cstr, flt, get_formatted_email, today
 from frappe.utils.user import get_users_with_role
 
