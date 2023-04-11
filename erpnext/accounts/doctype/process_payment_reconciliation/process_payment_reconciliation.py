ded7b7365faba8a312bd40a47a3a8a2d778befe6
ruthra kumar <ruthra@erpnext.com>
2023-04-11 13:20:13 +0530

    chore: ability to pause the background job

--- erpnext/accounts/doctype/process_payment_reconciliation/process_payment_reconciliation.py
+++ erpnext/accounts/doctype/process_payment_reconciliation/process_payment_reconciliation.py
@@ -464,6 +464,10 @@ def reconcile(doc: None | str = None) -> None:
 						frappe.db.set_value("Process Payment Reconciliation Log", log, "reconciled", True)
 						frappe.db.set_value("Process Payment Reconciliation", doc, "status", "Completed")
 					else:
+
+						if frappe.db.get_value("Process Payment Reconciliation", doc, "pause"):
+							return
+
 						# trigger next batch in job
 						# generate reconcile job name
 						allocation = get_next_allocation(log)
