7ffeacb2f7ea59c199af32e5b96616efeb269a6c
bigerx <muhamed.abdullahu@apak.at>
2023-04-23 23:32:10 +0200

    Fixing local

---
+++ erpnext/projects/doctype/power_of_attorney/power_of_attorney.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, ERP and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Powerofattorney(Document):
+	pass
