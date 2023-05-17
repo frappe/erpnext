d9979b2ffbbf2341978099edc135e1962a161260
Rohit Waghchaure <rohitw1991@gmail.com>
2023-05-17 23:52:03 +0530

    refactor: stock balance report

---
+++ erpnext/stock/doctype/closing_stock_balance/closing_stock_balance.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Closing Stock Balance", {
+	refresh(frm) {
+		frm.trigger("generate_closing_balance");
+	},
+
+	generate_closing_balance(frm) {
+		if (in_list(["Queued", "Failed"], frm.doc.status)) {
+			frm.add_custom_button(__("Generate Closing Stock Balance"), () => {
+				frm.call({
+					method: "enqueue_job",
+					doc: frm.doc,
+					freeze: true,
+					callback: () => {
+						frm.reload_doc();
+					}
+				})
+			})
+		}
+	}
+});
