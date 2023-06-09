3a03d6853c61d5ef27c72f48c419e070d7efbf76
s-aga-r <sagarsharma.s312@gmail.com>
2023-06-09 15:24:56 +0530

    fix(ux): set warehouse for new row

--- erpnext/stock/doctype/stock_reservation_entry/stock_reservation_entry.js
+++ erpnext/stock/doctype/stock_reservation_entry/stock_reservation_entry.js
@@ -3,12 +3,10 @@
 
 frappe.ui.form.on("Stock Reservation Entry", {
 	refresh(frm) {
-		// Hide "Amend" button on cancelled document
-		if (frm.doc.docstatus == 2) {
-			frm.page.btn_primary.hide()
-		}
-
+		frm.trigger("set_queries");
 		frm.trigger("toggle_read_only_fields");
+		frm.trigger("hide_primary_action_button");
+		frm.trigger("make_sb_entries_warehouse_read_only");
 	},
 
 	has_serial_no(frm) {
@@ -19,6 +17,25 @@ frappe.ui.form.on("Stock Reservation Entry", {
 		frm.trigger("toggle_read_only_fields");
 	},
 
+	warehouse(frm) {
+		if (frm.doc.warehouse) {
+			frm.doc.sb_entries.forEach((row) => {
+				frappe.model.set_value(row.doctype, row.name, "warehouse", frm.doc.warehouse);
+			});
+		}
+	},
+
+	set_queries(frm) {
+		frm.set_query("warehouse", () => {
+			return {
+				filters: {
+					"is_group": 0,
+					"company": frm.doc.company,
+				}
+			};
+		});
+	},
+
 	toggle_read_only_fields(frm) {
 		frm.fields_dict.sb_entries.grid.update_docfield_property(
 			"serial_no", "read_only", !frm.doc.has_serial_no
@@ -27,5 +44,26 @@ frappe.ui.form.on("Stock Reservation Entry", {
 		frm.fields_dict.sb_entries.grid.update_docfield_property(
 			"batch_no", "read_only", !frm.doc.has_batch_no
 		);
+	},
+
+	hide_primary_action_button(frm) {
+		// Hide "Amend" button on cancelled document
+		if (frm.doc.docstatus == 2) {
+			frm.page.btn_primary.hide()
+		}
+	},
+
+	make_sb_entries_warehouse_read_only(frm) {
+		frm.fields_dict.sb_entries.grid.update_docfield_property(
+			"warehouse", "read_only", 1
+		);
+	},
+});
+
+frappe.ui.form.on("Serial and Batch Entry", {
+	sb_entries_add(frm, cdt, cdn) {
+		if (frm.doc.warehouse) {
+			frappe.model.set_value(cdt, cdn, 'warehouse', frm.doc.warehouse);
+		}
 	},
 });
