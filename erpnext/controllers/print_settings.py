b0f91b2eb6ce9b0a8e3761c7d536d90656eb8ba9
Anand Baburajan <anandbaburajan@gmail.com>
2023-06-13 19:42:56 +0530

    fix: make showing taxes as table in print configurable (#35672)
    (cherry picked from commit 491a50a02766d833eec0b2cad8650ef495206a8e)
    
    # Conflicts:
    #	erpnext/accounts/doctype/accounts_settings/accounts_settings.json

--- erpnext/controllers/print_settings.py
+++ erpnext/controllers/print_settings.py
@@ -30,9 +30,15 @@ def set_print_templates_for_taxes(doc, settings):
 	doc.print_templates.update(
 		{
 			"total": "templates/print_formats/includes/total.html",
-			"taxes": "templates/print_formats/includes/taxes.html",
 		}
 	)
+
+	if not doc.should_show_taxes_as_table_in_print():
+		doc.print_templates.update(
+			{
+				"taxes": "templates/print_formats/includes/taxes.html",
+			}
+		)
 
 
 def format_columns(display_columns, compact_fields):
