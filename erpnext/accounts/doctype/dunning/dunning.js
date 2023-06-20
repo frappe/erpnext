1e8db882e0a0b546e0b7081a8fcb727c594b47f7
marination <maricadsouza221197@gmail.com>
2023-06-20 11:47:04 +0530

    test: Dunning and PE against partially due invoice
    - Check if the right payment portion is picked
    - Check if the SI and Dunning are updated on submission and cancellation of PE

--- erpnext/accounts/doctype/dunning/dunning.js
+++ erpnext/accounts/doctype/dunning/dunning.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on("Dunning", {
