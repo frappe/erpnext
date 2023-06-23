cde82bc0cc978c3f4fec183825b709460062f8a8
Gursheen Anand <gursheen@frappe.io>
2023-06-23 12:12:32 +0530

    fix: modify patch

--- erpnext/patches/v14_0/set_report_in_process_SOA.py
+++ erpnext/patches/v14_0/set_report_in_process_SOA.py
@@ -5,6 +5,7 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("accounts", "doctype", "Process Statement of Accounts", force=True)
 	process_soa = frappe.qb.DocType("Process Statement of Accounts")
 	q = frappe.qb.update(process_soa).set(process_soa.report, "General Ledger")
 	q.run()
