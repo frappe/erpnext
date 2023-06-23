2bd586cb6ecc020ea301df66218f11703b0853e7
Gursheen Anand <gursheen@frappe.io>
2023-06-23 15:03:34 +0530

    fix: modify patch to fetch users correctly

--- erpnext/patches/v14_0/auto_populate_portal_users_in_supplier_customer_doctypes.py
+++ erpnext/patches/v14_0/auto_populate_portal_users_in_supplier_customer_doctypes.py
@@ -11,21 +11,20 @@ def execute():
 		.select(
 			(dynamic_link.link_doctype).as_("doctype"),
 			(dynamic_link.link_name).as_("parent"),
-			(contact.user).as_("portal_user"),
+			(contact.email_id).as_("portal_user"),
 		)
 		.where(
 			(dynamic_link.parenttype == "Contact")
 			& (dynamic_link.link_doctype.isin(["Supplier", "Customer"]))
 		)
 	)
-	contacts = q.run(as_dict=True, debug=True)
+	contacts = q.run(as_dict=True)
 	for contact in contacts:
-		portal_user_doc = frappe.new_doc("Portal User")
-		portal_user_doc.parenttype = contact.doctype
-		portal_user_doc.parentfield = "portal_users"
-		portal_user_doc.parent = contact.parent
-		portal_user_doc.user = contact.portal_user
-		portal_user_doc.save()
-		user_doc = frappe.get_doc("User", contact.portal_user)
-		user_doc.add_roles(contact.doctype)
-		user_doc.save()
+		user = frappe.db.get_value("User", {"email": contact.portal_user}, "name")
+		if user:
+			portal_user_doc = frappe.new_doc("Portal User")
+			portal_user_doc.parenttype = contact.doctype
+			portal_user_doc.parentfield = "portal_users"
+			portal_user_doc.parent = contact.parent
+			portal_user_doc.user = user
+			portal_user_doc.insert()
