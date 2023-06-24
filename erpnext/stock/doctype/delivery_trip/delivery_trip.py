85155d51f6ca102cdf6f5dc356cc866651cd9388
David Arnold <david.arnold@iohk.io>
2023-06-24 12:27:34 -0500

    fix: delivery trip driver is only required on submit
    This allows drafting trips and stops without yet deciding on the
    assignable driver which, in real life, may well be decided on after
    preparing and planning the trip.

--- erpnext/stock/doctype/delivery_trip/delivery_trip.py
+++ erpnext/stock/doctype/delivery_trip/delivery_trip.py
@@ -24,6 +24,9 @@ class DeliveryTrip(Document):
 		)
 
 	def validate(self):
+		if self._action == "submit" and not self.driver:
+			frappe.throw(_("A driver must be set to submit."))
+
 		self.validate_stop_addresses()
 
 	def on_submit(self):
