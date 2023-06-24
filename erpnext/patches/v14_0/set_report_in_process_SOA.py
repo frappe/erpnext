4de7a4c5718acbc144729f143cdfc0f61a3693f7
Deepesh Garg <deepeshgarg6@gmail.com>
2023-06-24 16:31:19 +0530

    chore: update typo in patch

--- erpnext/patches/v14_0/set_report_in_process_SOA.py
+++ erpnext/patches/v14_0/set_report_in_process_SOA.py
@@ -5,7 +5,6 @@ import frappe
 
 
 def execute():
-	frappe.reload_doc("accounts", "doctype", "Process Statement of Accounts", force=True)
-	process_soa = frappe.qb.DocType("Process Statement of Accounts")
+	process_soa = frappe.qb.DocType("Process Statement Of Accounts")
 	q = frappe.qb.update(process_soa).set(process_soa.report, "General Ledger")
 	q.run()
