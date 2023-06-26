7dcf3559544f18d417c7356ed360bba2af2013fc
Ankush Menat <ankush@frappe.io>
2023-06-26 16:03:55 +0530

    refactor: split patch and apply roles
    - if role isn't present dont add portal user
    - ignore failure as it's not critical

--- erpnext/buying/doctype/supplier/patches/migrate_supplier_portal_users.py
+++ erpnext/buying/doctype/supplier/patches/migrate_supplier_portal_users.py
@@ -1,10 +1,27 @@
+import os
+
 import frappe
+
+in_ci = os.environ.get("CI")
 
 
 def execute():
+	try:
+		contacts = get_portal_user_contacts()
+		add_portal_users(contacts)
+	except Exception:
+		frappe.db.rollback()
+		frappe.log_error("Failed to migrate portal users")
+
+		if in_ci:  # TODO: better way to handle this.
+			raise
+
+
+def get_portal_user_contacts():
 	contact = frappe.qb.DocType("Contact")
 	dynamic_link = frappe.qb.DocType("Dynamic Link")
-	contacts = (
+
+	return (
 		frappe.qb.from_(contact)
 		.inner_join(dynamic_link)
 		.on(contact.name == dynamic_link.parent)
@@ -19,12 +36,21 @@ def execute():
 		)
 	).run(as_dict=True)
 
+
+def add_portal_users(contacts):
 	for contact in contacts:
 		user = frappe.db.get_value("User", {"email": contact.portal_user}, "name")
-		if user:
-			portal_user_doc = frappe.new_doc("Portal User")
-			portal_user_doc.parenttype = contact.doctype
-			portal_user_doc.parentfield = "portal_users"
-			portal_user_doc.parent = contact.parent
-			portal_user_doc.user = user
-			portal_user_doc.insert()
+		if not user:
+			continue
+
+		roles = frappe.get_roles(user)
+		required_role = contact.doctype
+		if required_role not in roles:
+			continue
+
+		portal_user_doc = frappe.new_doc("Portal User")
+		portal_user_doc.parenttype = contact.doctype
+		portal_user_doc.parentfield = "portal_users"
+		portal_user_doc.parent = contact.parent
+		portal_user_doc.user = user
+		portal_user_doc.insert()
