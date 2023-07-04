4a200674dae65489e9a43ce00c3bbe9319231fef
ruthra kumar <ruthra@erpnext.com>
2023-07-04 14:52:30 +0530

    refactor: basic filters and validations

--- erpnext/accounts/doctype/repost_accounting_ledger/repost_accounting_ledger.js
+++ erpnext/accounts/doctype/repost_accounting_ledger/repost_accounting_ledger.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Repost Accounting Ledger", {
-// 	refresh(frm) {
+frappe.ui.form.on("Repost Accounting Ledger", {
+	setup: function(frm) {
+		frm.fields_dict['vouchers'].grid.get_field('voucher_type').get_query = function(doc) {
+			return {
+				filters: {
+					name: ['in', ['Purchase Invoice', 'Sales Invoice', 'Payment Entry', 'Journal Entry']]
+				}
+			}
+		}
 
-// 	},
-// });
+		frm.fields_dict['vouchers'].grid.get_field('voucher_no').get_query = function(doc) {
+			if (doc.company) {
+				return {
+					filters: {
+						company: doc.company,
+						docstatus: 1
+					}
+				}
+			}
+		}
+	},
+
+	refresh: function(frm) {
+
+	},
+});
--- erpnext/accounts/doctype/repost_accounting_ledger/repost_accounting_ledger.py
+++ erpnext/accounts/doctype/repost_accounting_ledger/repost_accounting_ledger.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RepostAccountingLedger(Document):
-	pass
+	def validate(self):
+		pass
+
+	def validate_period_closing_voucher(self):
+		pass
+
+	def validate_vouchers(self):
+		voucher_types = set([x.voucher_type for x in self.vouchers])
+		for x in voucher_types:
+			vouchers = set([x.voucher_no for x in self.vouchers if x.voucher_type == x])
+			filtered = set(
+				[x[0] for x in frappe.db.get_all(x, filters={"name": ["in", vouchers]}, as_list=1)]
+			)
+			# if vouchers.difference(filtered)
