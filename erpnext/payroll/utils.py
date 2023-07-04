2d1ee11ecd302f0030441494e38285d4ec4a7031
Saurabh <saurabh6790@gmail.com>
2023-07-04 17:14:58 +0530

    chore: add docstring

--- erpnext/payroll/utils.py
+++ erpnext/payroll/utils.py
@@ -4,12 +4,23 @@ import frappe
 from frappe import _
 from frappe.utils import get_link_to_form
 
-
 def sanitize_expression(string: Optional[str] = None) -> Optional[str]:
+	"""
+	Sanitizes an expression string by removing leading/trailing spaces, newlines, and line boundaries.
+
+	Args:
+	    string (Optional[str]): The input string to be sanitized (default: None).
+
+	Returns:
+	    Optional[str]: The sanitized string or None if the input string is empty or None.
+
+	Example:
+	    expression = "\r\n    gross_pay > 10000\n    "
+	    sanitized_expr = sanitize_expression(expression)
+	"""
 	if not string:
 		return None
 
-	# remove forward and trailing spaces, newlines and other line boundaries
 	parts = string.strip().splitlines()
 	string = " ".join(parts)
 
@@ -17,6 +28,31 @@ def sanitize_expression(string: Optional[str] = None) -> Optional[str]:
 
 
 def prepare_error_msg(*, row: dict, error: str, expression: str, description: str) -> str:
+	"""
+	Prepares an error message string with formatted information about the error.
+
+	Args:
+	    row (dict): A dictionary representing the row data.
+	    error (str): The error message.
+	    expression (str): The expression that caused the error.
+	    description (str): Additional description or hint for the error (optional).
+
+	Returns:
+	    str: The formatted error message string.
+
+	Example:
+	    row = {
+	        "parenttype": "Salary Structure",
+	        "parent": "Salary Structure-00001",
+	        "parentfield": "earnings",
+	        "idx": 1
+	    }
+	    error = "SyntaxError: invalid syntax"
+	    expression = " 200 if (gross_pay>10000 and month!=  'Feb')) else 0 "
+	    description = "Check the syntax of the expression."
+	    error_msg = prepare_error_msg(row=row, error=error, expression=expression, description=description)
+	"""
+	# Create a dictionary to store the error message data
 	data = frappe._dict(
 		{
 			"doctype": row.parenttype,
@@ -29,6 +65,7 @@ def prepare_error_msg(*, row: dict, error: str, expression: str, description: st
 		}
 	)
 
+	# Format and return the error message string
 	return _(
 		"Error in {parentfield}, while evaluating the {doctype} {doclink} at row {row_id}. <br><br> <b>Expression:</b> {expression}. <br><br> <b>Error:</b> {error} <br><br> <b>Hint:</b> {description}"
 	).format(**data)
