f29d43e396cd947b8321fc3d80764fb9396cb18d
David Arnold <david.arnold@iohk.io>
2023-07-03 20:39:14 -0500

    feat: add default time unit to global defaults analogue default distance

--- erpnext/setup/doctype/global_defaults/global_defaults.js
+++ erpnext/setup/doctype/global_defaults/global_defaults.js
@@ -4,6 +4,7 @@
 frappe.ui.form.on('Global Defaults', {
 	onload: function(frm) {
 		frm.trigger('get_distance_uoms');
+		frm.trigger("get_time_uoms");
 	},
 	validate: function(frm) {
 		frm.call('get_defaults', null, r => {
@@ -28,5 +29,24 @@ frappe.ui.form.on('Global Defaults', {
 		frm.set_query("default_distance_unit", function () {
 			return { filters: { "name": ["IN", units] } };
 		});
-	}
+	},
+	get_time_uoms: function (frm) {
+		let units = [];
+
+		frappe.call({
+			method: "frappe.client.get_list",
+			args: {
+				doctype: "UOM Conversion Factor",
+				filters: { category: __("Time") },
+				fields: ["to_uom"],
+				limit_page_length: 500,
+			},
+			callback: function (r) {
+				r.message.forEach((row) => units.push(row.to_uom));
+			},
+		});
+		frm.set_query("default_time_unit", function () {
+			return { filters: { name: ["IN", units] } };
+		});
+	},
 });
