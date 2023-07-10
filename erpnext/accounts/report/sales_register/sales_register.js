b1e7470011f64d916c88b0963e984c300d451e1d
Gursheen Anand <gursheen@frappe.io>
2023-07-10 18:07:55 +0530

    feat: fetch PE along with SI
    (cherry picked from commit d7ffad1dd395a6a073f18eaa7d004cbc44d5bb19)
    
    # Conflicts:
    #	erpnext/accounts/report/purchase_register/purchase_register.py
    #	erpnext/accounts/report/sales_register/sales_register.py

--- erpnext/accounts/report/sales_register/sales_register.js
+++ erpnext/accounts/report/sales_register/sales_register.js
@@ -64,6 +64,12 @@ frappe.query_reports["Sales Register"] = {
 			"label": __("Item Group"),
 			"fieldtype": "Link",
 			"options": "Item Group"
+		},
+		{
+			"fieldname": "include_payments",
+			"label": __("Include Payments"),
+			"fieldtype": "Check",
+			"default": 0
 		}
 	]
 }
