e3ac1320c88d3c34c05254fe04225591376c3990
barredterra <14891507+barredterra@users.noreply.github.com>
2023-07-13 13:29:07 +0200

    feat: add local holidays
    (cherry picked from commit aa18b25a71430d11923bbfb5abca79214c0a467f)

--- erpnext/setup/doctype/holiday_list/holiday_list.js
+++ erpnext/setup/doctype/holiday_list/holiday_list.js
@@ -6,13 +6,39 @@ frappe.ui.form.on("Holiday List", {
 		if (frm.doc.holidays) {
 			frm.set_value("total_holidays", frm.doc.holidays.length);
 		}
+
+		frm.call("get_supported_countries").then(r => {
+			frm.subdivisions_by_country = r.message;
+			frm.set_df_property("country", "options", Object.keys(r.message));
+
+			if (frm.doc.country) {
+				frm.trigger("set_subdivisions");
+			}
+		});
 	},
 	from_date: function(frm) {
 		if (frm.doc.from_date && !frm.doc.to_date) {
 			var a_year_from_start = frappe.datetime.add_months(frm.doc.from_date, 12);
 			frm.set_value("to_date", frappe.datetime.add_days(a_year_from_start, -1));
 		}
-	}
+	},
+	country: function(frm) {
+		frm.set_value("subdivision", "");
+
+		if (frm.doc.country) {
+			frm.trigger("set_subdivisions");
+		}
+	},
+	set_subdivisions: function(frm) {
+		const subdivisions = frm.subdivisions_by_country[frm.doc.country];
+		if (subdivisions.length > 0) {
+			frm.set_df_property("subdivision", "options", frm.subdivisions_by_country[frm.doc.country]);
+			frm.set_df_property("subdivision", "hidden", 0);
+		} else {
+			frm.set_df_property("subdivision", "options", "");
+			frm.set_df_property("subdivision", "hidden", 1);
+		}
+	},
 });
 
 frappe.tour["Holiday List"] = [
