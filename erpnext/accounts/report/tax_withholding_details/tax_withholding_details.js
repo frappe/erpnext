5224f13db26ddb4983430a59fc7b1053d7523c5d
Gursheen Anand <gursheen@frappe.io>
2023-07-24 14:52:29 +0530

    fix: add patch for renaming tds payable report

--- erpnext/accounts/report/tax_withholding_details/tax_withholding_details.js
+++ erpnext/accounts/report/tax_withholding_details/tax_withholding_details.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["TDS Payable Monthly"] = {
+frappe.query_reports["Tax Withholding Details"] = {
 	"filters": [
 		{
 			"fieldname":"company",
