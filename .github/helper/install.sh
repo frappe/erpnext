9750c96bf924feb1917f45eadbfc332168dfc07f
Ankush Menat <ankush@frappe.io>
2023-07-24 14:37:53 +0530

    debug

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -12,7 +12,7 @@ githubbranch=${GITHUB_BASE_REF:-${GITHUB_REF##*/}}
 frappeuser=${FRAPPE_USER:-"frappe"}
 frappebranch=${FRAPPE_BRANCH:-$githubbranch}
 
-git clone "https://github.com/${frappeuser}/frappe" --branch "${frappebranch}" --depth 1
+git clone "https://github.com/ankush/frappe" --branch "typess" --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
 
 mkdir ~/frappe-bench/sites/test_site
