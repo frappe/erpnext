db49d53aafb6e50aa70f736d6cc3f8df965e02b1
Gursheen Anand <gursheen@frappe.io>
2023-07-24 17:42:13 +0530

    fix: running balance after sorting

--- erpnext/accounts/report/purchase_register/purchase_register.py
+++ erpnext/accounts/report/purchase_register/purchase_register.py
@@ -69,7 +69,7 @@ def _execute(filters=None, additional_table_columns=None):
 				"balance": flt(opening_row.balance),
 			}
 		)
-		running_balance = flt(opening_row.balance)
+
 	data = []
 	for inv in invoice_list:
 		# invoice details
@@ -136,12 +136,16 @@ def _execute(filters=None, additional_table_columns=None):
 			row.update({"debit": inv.base_grand_total, "credit": 0.0})
 		else:
 			row.update({"debit": 0.0, "credit": inv.base_grand_total})
-		if include_payments:
-			running_balance += row["debit"] - row["credit"]
-			row.update({"balance": running_balance})
 		data.append(row)
 
 	res += sorted(data, key=lambda x: x["posting_date"])
+
+	if include_payments:
+		running_balance = flt(opening_row.balance)
+		for row in range(1, len(res)):
+			running_balance += res[row]["debit"] - res[row]["credit"]
+			res[row].update({"balance": running_balance})
+
 	return columns, res, None, None, None, include_payments
 
 
