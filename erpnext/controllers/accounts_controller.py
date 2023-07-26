479cab0336cd024477367bd8f7e535ce638f7251
Gursheen Anand <gursheen@frappe.io>
2023-07-26 09:56:48 +0530

    fix: test with None conditon in PE

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -2217,6 +2217,9 @@ def get_advance_payment_entries(
 			reference_condition = ""
 			order_list = []
 
+		if not condition:
+			condition = ""
+
 		payment_entries_against_order = frappe.db.sql(
 			"""
 			select
@@ -2231,12 +2234,12 @@ def get_advance_payment_entries(
 				and t2.reference_doctype = %s {2} {3}
 			order by t1.posting_date {4}
 		""".format(
-				currency_field, 
-				party_account_field, 
-				reference_condition, 
-				condition, 
-				limit_cond, 
-				exchange_rate_field
+				currency_field,
+				party_account_field,
+				reference_condition,
+				condition,
+				limit_cond,
+				exchange_rate_field,
 			),
 			[party_account, payment_type, party_type, party, order_doctype] + order_list,
 			as_dict=1,
