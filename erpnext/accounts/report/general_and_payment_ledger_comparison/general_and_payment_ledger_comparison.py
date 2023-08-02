a681bc8b879f7c67b4927ca90c0c8a7c4efedcb0
ruthra kumar <ruthra@erpnext.com>
2023-08-02 17:30:50 +0530

    feat: Accounting Ledger comparison report

---
+++ erpnext/accounts/report/general_and_payment_ledger_comparison/general_and_payment_ledger_comparison.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
