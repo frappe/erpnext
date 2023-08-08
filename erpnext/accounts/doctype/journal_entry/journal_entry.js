f8d6fe6be06458050105b9c57939f332e396ca80
ruthra kumar <ruthra@erpnext.com>
2023-08-08 20:27:12 +0530

    feat: utility to repost accounting ledgers without cancellation (#36469)
    * feat: introduce doctypes for repost
    
    * refactor: basic filters and validations
    
    * chore: basic validations
    
    * chore: added barebones function to generate ledger entries
    
    * chore: repost on submit
    
    * chore: repost in background
    
    * chore: include payment entry and journal entry
    
    * chore: ignore repost doc on cancel
    
    * chore: preview method
    
    * chore: rudimentary form of preview
    
    * refactor: preview template
    
    * refactor: basic background colors to differentiate old and new
    
    * chore: remove commented code
    
    * test: basic functionality
    
    * chore: fix conflict
    
    * chore: prevent repost on invoices with deferred accounting
    
    * refactor(test): rename and test basic validations and methods
    
    * refactor(test): test all validations
    
    * fix(test): use proper name account name
    
    * refactor(test): fix failing test case
    
    * refactor(test): clear old entries
    
    * refactor(test): simpler logic to clear old records
    
    * refactor(test): make use of deletion flag
    
    * refactor(test): split into multiple test cases
    
    (cherry picked from commit e64b004eca86255159c799d9b47f36a75fe70ba0)
    
    # Conflicts:
    #	erpnext/accounts/doctype/journal_entry/journal_entry.js

--- erpnext/accounts/doctype/journal_entry/journal_entry.js
+++ erpnext/accounts/doctype/journal_entry/journal_entry.js
@@ -8,7 +8,11 @@ frappe.provide("erpnext.journal_entry");
 frappe.ui.form.on("Journal Entry", {
 	setup: function(frm) {
 		frm.add_fetch("bank_account", "account", "account");
+<<<<<<< HEAD
 		frm.ignore_doctypes_on_cancel_all = ['Sales Invoice', 'Purchase Invoice', 'Journal Entry', "Repost Payment Ledger", 'Asset', 'Asset Movement'];
+=======
+		frm.ignore_doctypes_on_cancel_all = ['Sales Invoice', 'Purchase Invoice', 'Journal Entry', "Repost Payment Ledger", 'Asset', 'Asset Movement', 'Asset Depreciation Schedule', "Repost Accounting Ledger"];
+>>>>>>> e64b004eca (feat: utility to repost accounting ledgers without cancellation (#36469))
 	},
 
 	refresh: function(frm) {
