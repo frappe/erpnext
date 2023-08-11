ee04c6d5c52428f6f8e28fdf1a4e25075bee59ff
rohitwaghchaure <rohitw1991@gmail.com>
2023-08-11 13:57:27 +0530

    fix: allow negative stock condition for batch item (#36586)
    * fix: allow negative stock condition for batch item
    
    * fix: test case

--- erpnext/stock/stock_ledger.py
+++ erpnext/stock/stock_ledger.py
@@ -635,16 +635,13 @@ class update_entries_after(object):
 
 	def reset_actual_qty_for_stock_reco(self, sle):
 		doc = frappe.get_cached_doc("Stock Reconciliation", sle.voucher_no)
-		doc.recalculate_current_qty(sle.voucher_detail_no, sle.creation, sle.actual_qty > 0)
+		doc.recalculate_current_qty(sle.voucher_detail_no, sle.creation, sle.actual_qty >= 0)
 
 		if sle.actual_qty < 0:
 			sle.actual_qty = (
 				flt(frappe.db.get_value("Stock Reconciliation Item", sle.voucher_detail_no, "current_qty"))
 				* -1
 			)
-
-			if abs(sle.actual_qty) == 0.0:
-				sle.is_cancelled = 1
 
 	def validate_negative_stock(self, sle):
 		"""
