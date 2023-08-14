74bee07a270d8b9b0ff4ac8a8a3ac706f6619bb7
s-aga-r <sagarsharma.s312@gmail.com>
2023-08-14 19:28:53 +0530

    fix(ux): filters for Service Item and Finished Good

--- erpnext/subcontracting/doctype/service_item_and_finished_good_map/service_item_and_finished_good_map.js
+++ erpnext/subcontracting/doctype/service_item_and_finished_good_map/service_item_and_finished_good_map.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Service Item and Finished Good Map", {
-// 	refresh(frm) {
+frappe.ui.form.on("Service Item and Finished Good Map", {
+	setup: (frm) => {
+		frm.trigger("set_queries");
+	},
 
-// 	},
-// });
+    set_queries: (frm) => {
+        frm.set_query("service_item", () => {
+            return {
+                filters: {
+                    disabled: 0,
+                    is_stock_item: 0,
+                }
+            }
+        });
+
+        frm.set_query("finished_good_item", () => {
+            return {
+                filters: {
+                    disabled: 0,
+                    is_stock_item: 1,
+                    default_bom: ['!=', ''],
+                    is_sub_contracted_item: 1,
+                }
+            }
+        });
+    }
+});
