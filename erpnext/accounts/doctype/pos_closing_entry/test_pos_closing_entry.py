d4cc9daca1ac4ebe56828434181936f30e579c13
Ritvik Sardana <ritviksardana@Ritviks-MacBook-Air.local>
2023-08-14 11:52:49 +0530

    chore: code clean up

--- erpnext/accounts/doctype/pos_closing_entry/test_pos_closing_entry.py
+++ erpnext/accounts/doctype/pos_closing_entry/test_pos_closing_entry.py
@@ -167,7 +167,7 @@ def get_test_item_qty(pos_profile):
 		item_group="All Item Groups",
 	)
 
-	test_item_qty = [item for item in test_item_pos["items"] if item["item_code"] == "_Test Item"][0][
-		"actual_qty"
-	]
+	test_item_qty = [item for item in test_item_pos["items"] if item["item_code"] == "_Test Item"][
+		0
+	].get("actual_qty")
 	return test_item_qty
