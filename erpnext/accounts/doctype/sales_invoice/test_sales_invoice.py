a0fc68538fbfd941f2d26741770b037b89dea36a
ruthra kumar <ruthra@erpnext.com>
2023-08-14 17:38:44 +0530

    refactor: toggle for negative rates in Selling Settings

--- erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
@@ -3376,6 +3376,7 @@ class TestSalesInvoice(unittest.TestCase):
 
 		set_advance_flag(company="_Test Company", flag=0, default_account="")
 
+	@change_settings("Selling Settings", {"allow_negative_rates_for_items": 0})
 	def test_sales_return_negative_rate(self):
 		si = create_sales_invoice(is_return=1, qty=-2, rate=-10, do_not_save=True)
 		self.assertRaises(frappe.ValidationError, si.save)
