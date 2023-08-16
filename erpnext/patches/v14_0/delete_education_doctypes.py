c071523e3518c2511bcadce5b109de668ae58835
Gursheen Anand <gursheen@frappe.io>
2023-08-16 12:23:10 +0530

    fix: delete portal settings for education doctypes

--- erpnext/patches/v14_0/delete_education_doctypes.py
+++ erpnext/patches/v14_0/delete_education_doctypes.py
@@ -44,6 +44,9 @@ def execute():
 
 	doctypes = frappe.get_all("DocType", {"module": "education", "custom": 0}, pluck="name")
 	for doctype in doctypes:
+		frappe.delete_doc(
+			"Portal Menu Item", {"reference_doctype": doctype}, ignore_missing=True, force=True
+		)
 		frappe.delete_doc("DocType", doctype, ignore_missing=True)
 
 	frappe.delete_doc("Module Def", "Education", ignore_missing=True, force=True)
