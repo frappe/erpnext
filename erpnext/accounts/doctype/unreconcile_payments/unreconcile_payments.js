e48a90efe69f36dc455df3fefa8131384903e422
ruthra kumar <ruthra@erpnext.com>
2023-08-22 15:01:14 +0530

    chore: working state on barebones functions

--- erpnext/accounts/doctype/unreconcile_payments/unreconcile_payments.js
+++ erpnext/accounts/doctype/unreconcile_payments/unreconcile_payments.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Unreconcile Payments", {
-// 	refresh(frm) {
+frappe.ui.form.on("Unreconcile Payments", {
+	refresh(frm) {
+		frm.set_query("voucher_type", function() {
+			return {
+				filters: {
+					name: "Payment Entry"
+				}
+			}
+		});
 
-// 	},
-// });
+
+		frm.set_query("voucher_no", function(doc) {
+			return {
+				filters: {
+					company: doc.company,
+					docstatus: 1
+				}
+			}
+		});
+
+	},
+});
