b9647ac0a4974ed310f652e9af20acfef2ec2b28
ruthra kumar <ruthra@erpnext.com>
2023-08-24 14:52:26 +0530

    refactor: adding 'Get Allocations' button
    (cherry picked from commit 5114a9580db961a006d9b2f3c4dc08f207f374c7)

--- erpnext/accounts/doctype/unreconcile_payments/unreconcile_payments.py
+++ erpnext/accounts/doctype/unreconcile_payments/unreconcile_payments.py
@@ -2,25 +2,44 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import qb
 from frappe.model.document import Document
+from frappe.query_builder.functions import Sum
 
 from erpnext.accounts.utils import unlink_ref_doc_from_payment_entries, update_voucher_outstanding
 
 
 class UnreconcilePayments(Document):
-	def before_save(self):
-		if self.voucher_type == "Payment Entry":
-			references = frappe.db.get_all(
-				"Payment Entry Reference",
-				filters={"docstatus": 1, "parent": self.voucher_no},
-				fields=["reference_doctype", "reference_name", "allocated_amount"],
-			)
+	# def validate(self):
+	# 	parent = set([alloc.parent for alloc in self.allocations])
+	# 	if len(parent) != 1:
+	# 		pass
 
-			self.set("references", [])
-			for ref in references:
-				self.append("references", ref)
+	@frappe.whitelist()
+	def get_allocations_from_payment(self):
+		if self.voucher_type == "Payment Entry":
+			per = qb.DocType("Payment Entry Reference")
+			allocated_references = (
+				qb.from_(per)
+				.select(
+					per.reference_doctype, per.reference_name, Sum(per.allocated_amount).as_("allocated_amount")
+				)
+				.where((per.docstatus == 1) & (per.parent == self.voucher_no))
+				.groupby(per.reference_name)
+				.run(as_dict=True)
+			)
+			return allocated_references
+
+	def add_references(self):
+		allocations = self.get_allocations_from_payment()
+
+		for alloc in allocations:
+			self.append("allocations", alloc)
 
 	def on_submit(self):
+		# todo: add more granular unlinking
+		# different amounts for same invoice should be individually unlinkable
+
 		payment_type, paid_from, paid_to, party_type, party = frappe.db.get_all(
 			self.voucher_type,
 			filters={"name": self.voucher_no},
@@ -29,10 +48,10 @@ class UnreconcilePayments(Document):
 		)[0]
 		account = paid_from if payment_type == "Receive" else paid_to
 
-		for ref in self.references:
-			doc = frappe.get_doc(ref.reference_doctype, ref.reference_name)
+		for alloc in self.allocations:
+			doc = frappe.get_doc(alloc.reference_doctype, alloc.reference_name)
 			unlink_ref_doc_from_payment_entries(doc)
 			update_voucher_outstanding(
-				ref.reference_doctype, ref.reference_name, account, party_type, party
+				alloc.reference_doctype, alloc.reference_name, account, party_type, party
 			)
-			frappe.db.set_value("Unreconcile Payment Entries", ref.name, "unlinked", True)
+			frappe.db.set_value("Unreconcile Payment Entries", alloc.name, "unlinked", True)
