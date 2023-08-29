e464f5e419df0e9370d3581f8a95b7451facd001
ruthra kumar <ruthra@erpnext.com>
2023-08-29 13:46:29 +0530

    chore: move functions to a separate file in utils
    (cherry picked from commit 25fe75218578a44302f1335f8db0caa17d4d7608)
    
    # Conflicts:
    #	erpnext/accounts/doctype/payment_entry/payment_entry.js
    #	erpnext/public/js/erpnext.bundle.js

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -666,61 +666,8 @@ erpnext.utils.update_child_items = function(opts) {
 	}).show();
 }
 
-erpnext.utils.build_unreconcile_dialog = function(frm) {
-	if (['Sales Invoice', 'Purchase Invoice', 'Payment Entry', 'Journal Entry'].includes(frm.doc.doctype)) {
-		let child_table_fields = [
-			{ label: __("Voucher Type"), fieldname: "voucher_type", fieldtype: "Dynamic Link", options: "DocType", in_list_view: 1, read_only: 1},
-			{ label: __("Voucher No"), fieldname: "voucher_no", fieldtype: "Link", options: "voucher_type", in_list_view: 1, read_only: 1 },
-			{ label: __("Allocated Amount"), fieldname: "allocated_amount", fieldtype: "Float", in_list_view: 1, read_only: 1 },
-		]
-		let unreconcile_dialog_fields = [
-			{
-				label: __('Allocations'),
-				fieldname: 'allocations',
-				fieldtype: 'Table',
-				read_only: 1,
-				fields: child_table_fields,
-			},
-		];
 
-		// get linked payments
-		frappe.call({
-			"method": "erpnext.accounts.doctype.unreconcile_payments.unreconcile_payments.get_linked_payments_for_doc",
-			"args": {
-				"company": frm.doc.company,
-				"doctype": frm.doc.doctype,
-				"docname": frm.doc.name
-			},
-			callback: function(r) {
-				if (r.message) {
-					// populate child table with allocations
-					unreconcile_dialog_fields[0].data = r.message;
-					unreconcile_dialog_fields[0].get_data = function(){ return r.message};
 
-					let d = new frappe.ui.Dialog({
-						title: 'Un-Reconcile Allocations',
-						fields: unreconcile_dialog_fields,
-						size: 'large',
-						cannot_add_rows: 1,
-						primary_action_label: 'Un-Reconcile',
-						primary_action(values) {
-
-							let selected_allocations = values.allocations.filter(x=>x.__checked);
-							if (selected_allocations.length > 0) {
-								// assuming each row is an individual voucher
-								// pass this to server side method that created unreconcile doc for row
-							} else {
-								frappe.msgprint("No Selection");
-							}
-						}
-					});
-
-					d.show();
-				}
-			}
-		});
-	}
-}
 
 erpnext.utils.map_current_doc = function(opts) {
 	function _map() {
