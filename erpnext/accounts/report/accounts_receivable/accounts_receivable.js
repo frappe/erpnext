d72da3189816277ad95afc83ad5e0cce58bce167
RitvikSardana <ritvik.sardana11@gmail.com>
2023-09-01 14:49:25 +0530

    fix: filter toggle for party_type

--- erpnext/accounts/report/accounts_receivable/accounts_receivable.js
+++ erpnext/accounts/report/accounts_receivable/accounts_receivable.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Con`tributors
 // License: GNU General Public License v3. See license.txt
 
 frappe.query_reports["Accounts Receivable"] = {
