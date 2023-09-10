5c0a232e0fc4c6cb67fbdb4b76747fbd0878b109
Florian HENRY <florian.henry@open-concept.pro>
2023-09-10 17:24:01 +0200

    refactor(region): Splitting of France Regional logic from ERPNext

--- erpnext/patches/v14_0/france_depreciation_warning.py
+++ erpnext/patches/v14_0/france_depreciation_warning.py
@@ -6,7 +6,7 @@ def execute():
 	if "erpnext_france" in frappe.get_installed_apps():
 		return
 	click.secho(
-		"Feature for Region France will be remove in version-15 and moved to a separate app\n"
+		"Feature for region France will be remove in version-15 and moved to a separate app\n"
 		"Please install the app to continue using the regionnal France features: https://github.com/scopen-coop/erpnext_france.git",
 		fg="yellow",
 	)
