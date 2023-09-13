6f4f5be9904e482cbb21a3aa4b5d3555c6d4eeeb
Abdo <ruzaqi@gmail.com>
2023-09-13 18:08:45 +0300

    chore: Translate Voucher Type in  General Ledger Report (#36874)
    chore: Translate Voucher Type in  General Ledger Report

--- erpnext/accounts/report/general_ledger/general_ledger.py
+++ erpnext/accounts/report/general_ledger/general_ledger.py
@@ -434,6 +434,7 @@ def get_accountwise_gle(filters, accounting_dimensions, gl_entries, gle_map):
 
 	for gle in gl_entries:
 		group_by_value = gle.get(group_by)
+		gle.voucher_type = _(gle.voucher_type)
 
 		if gle.posting_date < from_date or (cstr(gle.is_opening) == "Yes" and not show_opening_entries):
 			if not group_by_voucher_consolidated:
