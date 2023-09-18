57ec0d6b419121ac51a762c29a4ca397771ad67a
ruthra kumar <ruthra@erpnext.com>
2023-09-18 12:35:24 +0530

    refactor: some logic
    (cherry picked from commit 4c8a8c3bcd9bf21918a78de244789c7de76cede9)

--- erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.py
+++ erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+from math import ceil, floor
+
+import frappe
+from dateutil.relativedelta import relativedelta
 from frappe.model.document import Document
+from frappe.utils import getdate
 
 
 class BisectAccountingStatements(Document):
-	pass
+	@frappe.whitelist()
+	def bisect(self):
+		cur_frm_date, cur_to_date = getdate(self.from_date), getdate(self.to_date)
+		while True:
+			delta = cur_to_date - cur_frm_date
+			if delta.days == 0:
+				return
+			cur_floor = floor(delta.days / 2)
+			cur_to_date = cur_frm_date + relativedelta(days=+cur_floor)
+			print((cur_frm_date, cur_to_date), delta, cur_floor)
