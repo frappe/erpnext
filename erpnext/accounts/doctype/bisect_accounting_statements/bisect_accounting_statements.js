4c8a8c3bcd9bf21918a78de244789c7de76cede9
ruthra kumar <ruthra@erpnext.com>
2023-09-18 12:35:24 +0530

    refactor: some logic

--- erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.js
+++ erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bisect Accounting Statements", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Bisect Accounting Statements", {
+	refresh(frm) {
+		frm.add_custom_button(__('Bisect'), () =>
+			frm.trigger("bisect")
+		);
+		frm.change_custom_button_type(__('Bisect'), null, 'primary');
+	},
+	bisect(frm) {
+		frm.call({
+			doc: frm.doc,
+			method: 'bisect',
+			callback: (r) => {
+				console.log(r);
+			}
+		});
+	}
+});
