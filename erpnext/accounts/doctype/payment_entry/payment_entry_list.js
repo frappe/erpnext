b260b8f1d1d09fa5ef1c172125bea505a359b80f
RitvikSardana <ritvik.sardana11@gmail.com>
2023-09-21 14:33:40 +0530

    chore: code cleanup

--- erpnext/accounts/doctype/payment_entry/payment_entry_list.js
+++ erpnext/accounts/doctype/payment_entry/payment_entry_list.js
@@ -1,6 +1,6 @@
 frappe.listview_settings['Payment Entry'] = {
 
-	refresh: function(listview,reportview) {
+	onload: function(listview) {
 		if (listview.page.fields_dict.party_type) {
 			listview.page.fields_dict.party_type.get_query = function() {
 				return {
