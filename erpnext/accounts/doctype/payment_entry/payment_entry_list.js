8c566001698ffcce5362ffeaefb3904daea6d3c8
RitvikSardana <ritvik.sardana11@gmail.com>
2023-09-21 14:29:10 +0530

    fix: payment entry filter not working for report view

--- erpnext/accounts/doctype/payment_entry/payment_entry_list.js
+++ erpnext/accounts/doctype/payment_entry/payment_entry_list.js
@@ -1,6 +1,6 @@
 frappe.listview_settings['Payment Entry'] = {
 
-	onload: function(listview) {
+	refresh: function(listview,reportview) {
 		if (listview.page.fields_dict.party_type) {
 			listview.page.fields_dict.party_type.get_query = function() {
 				return {
