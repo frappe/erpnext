a177137d6d37f73b8b3d8c2679ce3e47a8430b2c
ruthra kumar <ruthra@erpnext.com>
2023-09-25 12:16:27 +0530

    refactor: more buttons
    (cherry picked from commit a4270291511954b0e68fc126626967f4939c7617)

--- erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.js
+++ erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.js
@@ -3,10 +3,21 @@
 
 frappe.ui.form.on("Bisect Accounting Statements", {
 	refresh(frm) {
+		frm.add_custom_button(__('Bisect Left'), () =>
+			frm.trigger("bisect_left")
+		);
+
+		frm.add_custom_button(__('Bisect Right'), () =>
+			frm.trigger("bisect_right")
+		);
+
+		frm.add_custom_button(__('Up'), () =>
+			frm.trigger("move_up")
+		);
 		frm.add_custom_button(__('Bisect'), () =>
 			frm.trigger("bisect")
 		);
-		frm.change_custom_button_type(__('Bisect'), null, 'primary');
+		// frm.change_custom_button_type(__('Bisect'), null, 'primary');
 	},
 	bisect(frm) {
 		frm.call({
