03a38ed0257cdee5ae1ec645877691ed1292ba9c
ruthra kumar <ruthra@erpnext.com>
2023-09-25 11:05:30 +0530

    refactor: support for BFS and DFS

--- erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.py
+++ erpnext/accounts/doctype/bisect_accounting_statements/bisect_accounting_statements.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
+from collections import deque
 from math import floor
 
 import frappe
@@ -10,18 +11,49 @@ from frappe.utils import getdate
 
 
 class BisectAccountingStatements(Document):
-	@frappe.whitelist()
-	def bisect(self):
-		period_list = [(getdate(self.from_date), getdate(self.to_date))]
+	def bfs(self):
+		period_list = deque([(getdate(self.from_date), getdate(self.to_date))])
+		periods = []
 		dates = []
+		while period_list:
+			cur_frm_date, cur_to_date = period_list.popleft()
+			delta = cur_to_date - cur_frm_date
+			periods.append((cur_frm_date, cur_to_date, delta))
+			if delta.days == 0:
+				continue
+			else:
+				cur_floor = floor(delta.days / 2)
+				left = (cur_frm_date, (cur_frm_date + relativedelta(days=+cur_floor)))
+				right = ((cur_frm_date + relativedelta(days=+(cur_floor + 1))), cur_to_date)
+				period_list.append(left)
+				period_list.append(right)
+		return periods
+
+	def dfs(self):
+		period_list = [(getdate(self.from_date), getdate(self.to_date))]
+		periods = []
 		while period_list:
 			cur_frm_date, cur_to_date = period_list.pop()
 			delta = cur_to_date - cur_frm_date
-			if not delta.days > 0:
+			periods.append((cur_frm_date, cur_to_date, delta))
+			if delta.days == 0:
 				continue
+			else:
+				cur_floor = floor(delta.days / 2)
+				left = (cur_frm_date, (cur_frm_date + relativedelta(days=+cur_floor)))
+				right = ((cur_frm_date + relativedelta(days=+(cur_floor + 1))), cur_to_date)
+				period_list.append(left)
+				period_list.append(right)
+		return periods
 
-			cur_floor = floor(delta.days / 2)
-			left = (cur_frm_date, (cur_frm_date + relativedelta(days=+cur_floor)))
-			right = ((cur_frm_date + relativedelta(days=+(cur_floor + 1))), cur_to_date)
-			period_list.append(left)
-			period_list.append(right)
+	@frappe.whitelist()
+	def bisect(self):
+		if self.algorithm == "BFS":
+			periods = self.bfs()
+
+		if self.algorithm == "DFS":
+			periods = self.dfs()
+
+		print("Periods: ", len(periods))
+		for x in periods:
+			print(x)
