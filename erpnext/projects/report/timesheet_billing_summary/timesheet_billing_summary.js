f323beceb0db20c8624e5da43ed270812ff2a227
viralkansodiya15 <98073516+viralpatel15@users.noreply.github.com>
2023-10-03 09:10:46 +0530

    feat: New Report Timesheet Billing Summary , combination of three report

---
+++ erpnext/projects/report/timesheet_billing_summary/timesheet_billing_summary.js
@@ -0,0 +1,47 @@
+// Copyright (c) 2023, viral patel and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Timesheet Billing Summary"] = {
+	"filters": [
+		{
+			fieldname:"from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.month_start(), -1),
+			reqd: 1
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_days(frappe.datetime.month_start(),-1),
+			reqd: 1
+		},
+		{
+			fieldname: "project",
+			label: __("Project"),
+			fieldtype: "Link",
+			options: "Project",
+
+		},
+		{
+			fieldname: "employee",
+			label: __("Employee"),
+			fieldtype: "Link",
+			options: "Employee",
+
+		},
+		{
+			fieldname:"include_draft_timesheets",
+			label: __("Show Draft Timesheets"),
+			fieldtype: "Check",
+		},
+		{
+			fieldname:"show_timesheet_detail",
+			label: __("Show Detailed Timesheets"),
+			fieldtype: "Check",
+		},
+
+	]
+};
