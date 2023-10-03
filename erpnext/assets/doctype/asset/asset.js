6d9422f595de723b66c349432d8742c09a500434
anandbaburajan <anandbaburajan@gmail.com>
2023-10-03 13:03:54 +0530

    chore: resolving conflicts

--- erpnext/assets/doctype/asset/asset.js
+++ erpnext/assets/doctype/asset/asset.js
@@ -205,51 +205,7 @@ frappe.ui.form.on('Asset', {
 		})
 	},
 
-<<<<<<< HEAD
 	setup_chart: async function(frm) {
-=======
-	render_depreciation_schedule_view: function(frm, depr_schedule) {
-		let wrapper = $(frm.fields_dict["depreciation_schedule_view"].wrapper).empty();
-
-		let data = [];
-
-		depr_schedule.forEach((sch) => {
-			const row = [
-				sch['idx'],
-				frappe.format(sch['schedule_date'], { fieldtype: 'Date' }),
-				frappe.format(sch['depreciation_amount'], { fieldtype: 'Currency' }),
-				frappe.format(sch['accumulated_depreciation_amount'], { fieldtype: 'Currency' }),
-				sch['journal_entry'] || ''
-			];
-			data.push(row);
-		});
-
-		let datatable = new frappe.DataTable(wrapper.get(0), {
-			columns: [
-				{name: __("No."), editable: false, resizable: false, format: value => value, width: 60},
-				{name: __("Schedule Date"), editable: false, resizable: false, width: 270},
-				{name: __("Depreciation Amount"), editable: false, resizable: false, width: 164},
-				{name: __("Accumulated Depreciation Amount"), editable: false, resizable: false, width: 164},
-				{name: __("Journal Entry"), editable: false, resizable: false, format: value => `<a href="/app/journal-entry/${value}">${value}</a>`, width: 304}
-			],
-			data: data,
-			layout: "fluid",
-			serialNoColumn: false,
-			checkboxColumn: true,
-			cellHeight: 35
-		});
-
-		datatable.style.setStyle(`.dt-scrollable`, {'font-size': '0.75rem', 'margin-bottom': '1rem', 'margin-left': '0.35rem', 'margin-right': '0.35rem'});
-		datatable.style.setStyle(`.dt-header`, {'margin-left': '0.35rem', 'margin-right': '0.35rem'});
-		datatable.style.setStyle(`.dt-cell--header .dt-cell__content`, {'color': 'var(--gray-600)', 'font-size': 'var(--text-sm)'});
-		datatable.style.setStyle(`.dt-cell`, {'color': 'var(--text-color)'});
-		datatable.style.setStyle(`.dt-cell--col-1`, {'text-align': 'center'});
-		datatable.style.setStyle(`.dt-cell--col-2`, {'font-weight': 600});
-		datatable.style.setStyle(`.dt-cell--col-3`, {'font-weight': 600});
-	},
-
-	setup_chart_and_depr_schedule_view: async function(frm) {
->>>>>>> fed94845ce (feat: asset salvage_value_percentage (#37302))
 		if(frm.doc.finance_books.length > 1) {
 			return
 		}
