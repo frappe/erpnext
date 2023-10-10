b09e16a0b074b48979d268a6f2f74123d37d5b8f
viralkansodiya15 <98073516+viralpatel15@users.noreply.github.com>
2023-10-10 11:01:59 +0530

    fix: changes in identical function

--- erpnext/projects/report/timesheet_billing_summary/timesheet_billing_summary.py
+++ erpnext/projects/report/timesheet_billing_summary/timesheet_billing_summary.py
@@ -1,151 +1,213 @@
-# Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2023, viral patel and contributors
 # For license information, please see license.txt
 
+# import frappe
 
 import frappe
 from frappe import _
 from frappe.utils import flt, time_diff_in_hours
 from erpnext.projects.report.billing_summary import get_billable_and_total_duration
 
+def execute(filters=None):
+    columns, data = [], []
+    columns = get_columns(filters)
+    data = get_data(filters)
+    return columns, data
 
-def get_columns():
-	return [
-		{
-			"label": _("Project"),
-			"fieldtype": "Link",
-			"fieldname": "project",
-			"options": "Project",
-			"width": 300,
-		},
-		{
-			"label": _("Employee ID"),
-			"fieldtype": "Link",
-			"fieldname": "employee",
-			"options": "Employee",
-			"width": 300,
-		},
-		{
-			"label": _("Employee Name"),
-			"fieldtype": "data",
-			"fieldname": "employee_name",
-			"hidden": 1,
-			"width": 200,
-		},
-		{
-			"label": _("Timesheet"),
-			"fieldtype": "Link",
-			"fieldname": "timesheet",
-			"options": "Timesheet",
-			"width": 150,
-		},
-		{"label": _("Working Hours"), "fieldtype": "Float", "fieldname": "total_hours", "width": 150},
-		{
-			"label": _("Billable Hours"),
-			"fieldtype": "Float",
-			"fieldname": "total_billable_hours",
-			"width": 150,
-		},
-		{"label": _("Billing Amount"), "fieldtype": "Currency", "fieldname": "amount", "width": 150},
-	]
+
+
+def get_columns(filters):
+    columns = [
+        {
+            "label": _("Project"),
+            "fieldtype": "Link",
+            "fieldname": "project",
+            "options": "Project",
+            "width": 200,
+        },
+        {
+            "label": _("Employee ID"),
+            "fieldtype": "Link",
+            "fieldname": "employee",
+            "options": "Employee",
+            "width": 300,
+        },
+        {
+            "label": _("Employee Name"),
+            "fieldtype": "data",
+            "fieldname": "employee_name",
+            "hidden": 1,
+            "width": 200,
+        },
+        {
+            "label": _("Timesheet"),
+            "fieldtype": "Link",
+            "fieldname": "timesheet",
+            "options": "Timesheet",
+            "width": 150,
+        },
+    ]
+    if filters.get('show_timesheet_detail'):
+        columns += [
+             {
+            "label": _("Activity Type"),
+            "fieldtype": "Link",
+            "options":"Activity Type",
+            "fieldname": "activity_type",
+            "width": 200,
+            },
+        ]
+    columns += [
+        {"label": _("Working Hours"), "fieldtype": "Float", "fieldname": "total_hours", "width": 150},
+        {
+            "label": _("Billable Hours"),
+            "fieldtype": "Float",
+            "fieldname": "total_billable_hours",
+            "width": 150,
+        },
+        {"label": _("Billing Amount"), "fieldtype": "Currency", "fieldname": "amount", "width": 150},
+    ]
+    if filters.get('show_timesheet_detail'):
+        columns += [
+
+        {
+            "label": _("From Time"),
+            "fieldtype": "Datetime",
+            "fieldname": "from_time",
+            "width": 200,
+        },
+        {
+            "label": _("to Time"),
+            "fieldtype": "Datetime",
+            "fieldname": "to_time",
+            "width": 200,
+        },
+        {
+            "label": _("Task"),
+            "fieldtype": "Datetime",
+            "options":"Task",
+            "fieldname": "task",
+            "width": 200,
+        }
+
+        ]
+    return columns
 
 
 def get_data(filters):
-	data = []
-	if filters.from_date > filters.to_date:
-		frappe.msgprint(_("From Date can not be greater than To Date"))
-		return data
+    data = []
+    if filters.from_date > filters.to_date:
+        frappe.msgprint(_("From Date can not be greater than To Date"))
+        return data
 
-	timesheets = get_timesheets(filters)
+    condition =""
+    if filters.get('include_draft_timesheets'):
+        condition += "t.docstatus != 2"
+    else:
+        condition += "t.docstatus = 1"
+    if filters.get('from_date') and filters.get("to_date"):
+        condition += f" and t.start_date >= '{filters.get('from_date')}'"
+        condition += f" and t.end_date <= '{filters.get('to_date')}'"
 
-	filters.from_date = frappe.utils.get_datetime(filters.from_date)
-	filters.to_date = frappe.utils.add_to_date(
-		frappe.utils.get_datetime(filters.to_date), days=1, seconds=-1
-	)
+    record_filters = [
+        ["start_date", "<=", filters.to_date],
+        ["end_date", ">=", filters.from_date],
+    ]
+    if not filters.get("include_draft_timesheets"):
+        record_filters.append(["docstatus", "=", 1])
+    else:
+        record_filters.append(["docstatus", "!=", 2])
+    if filters.get('project'):
+        record_filters.append(["parent_project", "=", str(filters.get('project'))])
+    if filters.get('employee'):
+        record_filters.append(["employee", "=", filters.get('employee')])
+    timesheets = frappe.get_all(
+        "Timesheet", filters=record_filters, fields=["employee", "employee_name", "name" , "parent_project"]
+    )
 
-	timesheet_details = get_timesheet_details(filters, timesheets.keys())
+    timesheets_map = {}
+    timesheets_list = []
+    for row in timesheets:
+        timesheets_map[row.name] = row
+        timesheets_list.append(row.name)
 
-	for ts, ts_details in timesheet_details.items():
-		total_hours = 0
-		total_billing_hours = 0
-		total_amount = 0
+    if not timesheets_list:
+        return []
+    condition = ''
+    condition += "parent in {} ".format(
+                "(" + ", ".join([f'"{l}"' for l in timesheets_list]) + ")")
+    if filters.get('project'):
+        condition += f"and project = '{filters.get('project')}'"
 
-		for row in ts_details:
-			from_time, to_time = filters.from_date, filters.to_date
-
-			if row.to_time < from_time or row.from_time > to_time:
-				continue
-
-			if row.from_time > from_time:
-				from_time = row.from_time
-
-			if row.to_time < to_time:
-				to_time = row.to_time
-
-			activity_duration, billing_duration = get_billable_and_total_duration(row, from_time, to_time)
-
-			total_hours += activity_duration
-			total_billing_hours += billing_duration
-			total_amount += billing_duration * flt(row.billing_rate)
-
-		if total_hours:
-			data.append(
-				{
-					"employee": timesheets.get(ts).employee,
-					"employee_name": timesheets.get(ts).employee_name,
-					"timesheet": ts,
-					"total_billable_hours": total_billing_hours,
-					"total_hours": total_hours,
-					"amount": total_amount,
-				}
-			)
-
-	return data
+    timesheet_detail = frappe.db.sql(f"""Select parent , project , billing_hours  , from_time , to_time , hours , billing_rate, is_billable,
+                                     activity_type , task 
+                                  From `tabTimesheet Detail` 
+                                  Where {condition}""",as_dict = 1)
 
 
-def get_timesheets(filters):
-	record_filters = [
-		["start_date", "<=", filters.to_date],
-		["end_date", ">=", filters.from_date],
-	]
-	if not filters.get("include_draft_timesheets"):
-		record_filters.append(["docstatus", "=", 1])
-	else:
-		record_filters.append(["docstatus", "!=", 2])
-	if "employee" in filters:
-		record_filters.append(["employee", "=", filters.employee])
+    for row in timesheet_detail:
+        if timesheets_map.get(row.parent):
+            row.update(timesheets_map.get(row.parent))
 
-	timesheets = frappe.get_all(
-		"Timesheet", filters=record_filters, fields=["employee", "employee_name", "name"]
-	)
-	timesheet_map = frappe._dict()
-	for d in timesheets:
-		timesheet_map.setdefault(d.name, d)
+    from itertools import groupby
 
-	return timesheet_map
+    def key_func(k):
+        return k['name']
+
+    INFO = sorted(timesheet_detail, key=key_func)
 
 
-def get_timesheet_details(filters, timesheet_list):
-	timesheet_details_filter = {"parent": ["in", timesheet_list]}
+    for key, value in groupby(INFO, key_func):
+        total_hours = 0
+        total_billing_hours = 0
+        total_amount = 0
 
-	if "project" in filters:
-		timesheet_details_filter["project"] = filters.project
+        for row in list(value):
 
-	timesheet_details = frappe.get_all(
-		"Timesheet Detail",
-		filters=timesheet_details_filter,
-		fields=[
-			"from_time",
-			"to_time",
-			"hours",
-			"is_billable",
-			"billing_hours",
-			"billing_rate",
-			"parent",
-		],
-	)
+            from_time, to_time = filters.from_date, filters.to_date
 
-	timesheet_details_map = frappe._dict()
-	for d in timesheet_details:
-		timesheet_details_map.setdefault(d.parent, []).append(d)
+            if str(row.to_time) < from_time or str(row.from_time) > to_time:
+                continue
 
-	return timesheet_details_map
+            if str(row.from_time) > from_time:
+                from_time = row.from_time
+
+            if str(row.to_time) < to_time:
+                to_time = row.to_time
+
+            activity_duration, billing_duration = get_billable_and_total_duration(row, from_time, to_time)
+
+            if not filters.get('show_timesheet_detail'):
+                total_hours += activity_duration
+                total_billing_hours += billing_duration
+                total_amount += billing_duration * flt(row.billing_rate)
+
+            if filters.get('show_timesheet_detail'):
+                data.append({
+                    "project": row.get('project'),
+                    "employee": row.get('employee'),
+                    "employee_name": row.get('employee_name'),
+                    "timesheet": row.get('name'),
+                    "total_billable_hours": billing_duration,
+                    "total_hours": activity_duration,
+                    "amount": billing_duration * flt(row.billing_rate),
+                    "from_time":from_time,
+                    "to_time":to_time,
+                    "activity_type":row.activity_type,
+                    "task":row.task
+                })
+
+        if total_hours:
+            data.append(
+                {
+                    "project": row.get('project'),
+                    "employee": row.get('employee'),
+                    "employee_name": row.get('employee_name'),
+                    "timesheet": row.get('name'),
+                    "total_billable_hours": total_billing_hours,
+                    "total_hours": total_hours,
+                    "amount": total_amount,
+                }
+            )
+
+    return data
