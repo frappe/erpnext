e31db1891237aa22c19d71929f69d9db5596ae3c
Deepesh Garg <deepeshgarg6@gmail.com>
2023-10-17 18:19:47 +0530

    chore: Add accounting dimensions to Sales Order Item table

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -519,6 +519,7 @@ accounting_dimension_doctypes = [
 	"Sales Invoice Item",
 	"Purchase Invoice Item",
 	"Purchase Order Item",
+	"Sales Order Item",
 	"Journal Entry Account",
 	"Material Request Item",
 	"Delivery Note Item",
