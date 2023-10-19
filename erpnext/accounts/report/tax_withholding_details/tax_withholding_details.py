80dddb40ae3ed2d6c7f6c1bf91bcbc021fed3196
Gursheen Anand <gursheen@frappe.io>
2023-10-19 17:13:58 +0530

    chore: linting issues
    (cherry picked from commit 75441017c6629f81104409b892db56bb9c1bf1dd)

--- erpnext/accounts/report/tax_withholding_details/tax_withholding_details.py
+++ erpnext/accounts/report/tax_withholding_details/tax_withholding_details.py
@@ -288,7 +288,7 @@ def get_tds_docs(filters):
 def get_tds_docs_query(filters, bank_accounts, tds_accounts):
 	if not tds_accounts:
 		frappe.throw(
-			_("No {} Accounts found for this company.".format(frappe.bold("Tax Withholding"))),
+			_("No {0} Accounts found for this company.").format(frappe.bold("Tax Withholding")),
 			title="Accounts Missing Error",
 		)
 	gle = frappe.qb.DocType("GL Entry")
