e457d39b5dce2d57849d6aa2cf87929725499ef7
Smit Vora <smitvora203@gmail.com>
2023-10-20 09:33:37 +0530

    fix: update existing doc if possible
    (cherry picked from commit ff7108a3b139b2f019230be681147f1a1c90a681)
    
    # Conflicts:
    #	erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
    (cherry picked from commit 89f07dcfac3740af81c59579152646f3c806113b)

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -757,13 +757,14 @@ class PurchaseReceipt(BuyingController):
 				po_details.append(d.purchase_order_item)
 
 		if po_details:
-			updated_pr += update_billed_amount_based_on_po(po_details, update_modified)
+			updated_pr += update_billed_amount_based_on_po(po_details, update_modified, self)
 
 		for pr in set(updated_pr):
 			pr_doc = self if (pr == self.name) else frappe.get_doc("Purchase Receipt", pr)
 			update_billing_percentage(pr_doc, update_modified=update_modified)
 
 
+<<<<<<< HEAD
 def get_stock_value_difference(voucher_no, voucher_detail_no, warehouse):
 	return frappe.db.get_value(
 		"Stock Ledger Entry",
@@ -779,6 +780,9 @@ def get_stock_value_difference(voucher_no, voucher_detail_no, warehouse):
 
 
 def update_billed_amount_based_on_po(po_details, update_modified=True):
+=======
+def update_billed_amount_based_on_po(po_details, update_modified=True, pr_doc=None):
+>>>>>>> ff7108a3b1 (fix: update existing doc if possible)
 	po_billed_amt_details = get_billed_amount_against_po(po_details)
 
 	# Get all Purchase Receipt Item rows against the Purchase Order Items
@@ -807,13 +811,19 @@ def update_billed_amount_based_on_po(po_details, update_modified=True):
 		po_billed_amt_details[pr_item.purchase_order_item] = billed_against_po
 
 		if pr_item.billed_amt != billed_amt_agianst_pr:
-			frappe.db.set_value(
-				"Purchase Receipt Item",
-				pr_item.name,
-				"billed_amt",
-				billed_amt_agianst_pr,
-				update_modified=update_modified,
-			)
+			# update existing doc if possible
+			if pr_doc and pr_item.parent == pr_doc.name:
+				pr_item = next((item for item in pr_doc.items if item.name == pr_item.name), None)
+				pr_item.db_set("billed_amt", billed_amt_agianst_pr, update_modified=update_modified)
+
+			else:
+				frappe.db.set_value(
+					"Purchase Receipt Item",
+					pr_item.name,
+					"billed_amt",
+					billed_amt_agianst_pr,
+					update_modified=update_modified,
+				)
 
 			updated_pr.append(pr_item.parent)
 
