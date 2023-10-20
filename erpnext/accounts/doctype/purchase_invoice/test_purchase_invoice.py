4c3dcf1cf86eeca453a1108134d8dfa3e60a6f61
rohitwaghchaure <rohitw1991@gmail.com>
2023-10-20 16:26:58 +0530

    chore: fix conflicts

--- erpnext/accounts/doctype/purchase_invoice/test_purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/test_purchase_invoice.py
@@ -1858,34 +1858,6 @@ class TestPurchaseInvoice(FrappeTestCase, StockTestMixin):
 		pi.load_from_db()
 		self.assertFalse(pi.repost_required)
 
-<<<<<<< HEAD
-=======
-	@change_settings("Buying Settings", {"supplier_group": None})
-	def test_purchase_invoice_without_supplier_group(self):
-		# Create a Supplier
-		test_supplier_name = "_Test Supplier Without Supplier Group"
-		if not frappe.db.exists("Supplier", test_supplier_name):
-			supplier = frappe.get_doc(
-				{
-					"doctype": "Supplier",
-					"supplier_name": test_supplier_name,
-				}
-			).insert(ignore_permissions=True)
-
-			self.assertEqual(supplier.supplier_group, None)
-
-		po = create_purchase_order(
-			supplier=test_supplier_name,
-			rate=3000,
-			item="_Test Non Stock Item",
-			posting_date="2021-09-15",
-		)
-
-		pi = make_purchase_invoice(supplier=test_supplier_name)
-
-		self.assertEqual(po.docstatus, 1)
-		self.assertEqual(pi.docstatus, 1)
-
 	def test_default_cost_center_for_purchase(self):
 		from erpnext.accounts.doctype.cost_center.test_cost_center import create_cost_center
 
@@ -1910,18 +1882,6 @@ class TestPurchaseInvoice(FrappeTestCase, StockTestMixin):
 
 		self.assertEqual(pi.items[0].cost_center, "_Test Cost Center Buying - _TC")
 
-
-def set_advance_flag(company, flag, default_account):
-	frappe.db.set_value(
-		"Company",
-		company,
-		{
-			"book_advance_payments_in_separate_party_account": flag,
-			"default_advance_paid_account": default_account,
-		},
-	)
-
->>>>>>> 14b009b093 (fix: incorrect cost center in the purchase invoice (#37591))
 
 def check_gl_entries(
 	doc,
