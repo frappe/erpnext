a65bc77b0295747ffffb092f5e37fec458dc9234
Richard Case <richard@casesolved.co.uk>
2023-10-30 00:15:05 +0000

    feat: in_party_currency option for AR/AP reports

--- erpnext/accounts/report/accounts_receivable/accounts_receivable.js
+++ erpnext/accounts/report/accounts_receivable/accounts_receivable.js
@@ -185,9 +185,12 @@ frappe.query_reports["Accounts Receivable"] = {
 			"fieldname": "ignore_accounts",
 			"label": __("Group by Voucher"),
 			"fieldtype": "Check",
+		},
+		{
+			"fieldname": "in_party_currency",
+			"label": __("In Party Currency"),
+			"fieldtype": "Check",
 		}
-
-
 	],
 
 	"formatter": function(value, row, column, data, default_formatter) {
