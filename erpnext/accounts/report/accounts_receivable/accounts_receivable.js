1d8fcd66e6ca6d17a274a8b18699b40a7ac93e1a
ruthra kumar <ruthra@erpnext.com>
2023-11-10 09:49:55 +0530

    fix: new logic for handling revaluation journals

--- erpnext/accounts/report/accounts_receivable/accounts_receivable.js
+++ erpnext/accounts/report/accounts_receivable/accounts_receivable.js
@@ -180,12 +180,18 @@ frappe.query_reports["Accounts Receivable"] = {
 			"fieldname": "in_party_currency",
 			"label": __("In Party Currency"),
 			"fieldtype": "Check",
-    },
-    {
+		},
+		{
+			"fieldname": "for_revaluation_journals",
+			"label": __("Revaluation Journals"),
+			"fieldtype": "Check",
+		},
+		{
 			"fieldname": "ignore_accounts",
 			"label": __("Group by Voucher"),
 			"fieldtype": "Check",
 		}
+
 
 	],
 
