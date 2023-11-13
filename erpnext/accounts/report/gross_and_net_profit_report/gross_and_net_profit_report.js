04256ef1de23a3aec8841b8195eeba8610af6103
ruthra kumar <ruthra@erpnext.com>
2023-11-13 17:29:37 +0530

    Revert "refactor: import financial_statement in erpnext bundle"
    This reverts commit 40e7c43ce3970183b7effb53c8246af75d62b58c.

--- erpnext/accounts/report/gross_and_net_profit_report/gross_and_net_profit_report.js
+++ erpnext/accounts/report/gross_and_net_profit_report/gross_and_net_profit_report.js
@@ -2,15 +2,20 @@
 // For license information, please see license.txt
 
 
-frappe.query_reports["Gross and Net Profit Report"] = $.extend(
-	{},
-	erpnext.financial_statements
-);
+frappe.query_reports["Gross and Net Profit Report"] = {
+	"filters": [
 
-frappe.query_reports["Gross and Net Profit Report"]["filters"].push(
-	{
-		"fieldname": "accumulated_values",
-		"label": __("Accumulated Values"),
-		"fieldtype": "Check"
-	}
-);
+	]
+}
+frappe.require("assets/erpnext/js/financial_statements.js", function() {
+	frappe.query_reports["Gross and Net Profit Report"] = $.extend({},
+		erpnext.financial_statements);
+
+	frappe.query_reports["Gross and Net Profit Report"]["filters"].push(
+		{
+			"fieldname": "accumulated_values",
+			"label": __("Accumulated Values"),
+			"fieldtype": "Check"
+		}
+	);
+});
