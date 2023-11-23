f84aa85c4aa4681ba12dcefe924fd5278a1460f8
s-aga-r <sagarsharma.s312@gmail.com>
2023-11-23 16:59:42 +0530

    feat: add field `Purchase Order Item` in SCO Item
    (cherry picked from commit a2ede7d6d5241665d996447a1c9e2d1447190066)

--- erpnext/subcontracting/doctype/subcontracting_order/subcontracting_order.py
+++ erpnext/subcontracting/doctype/subcontracting_order/subcontracting_order.py
@@ -148,8 +148,6 @@ class SubcontractingOrder(SubcontractingController):
 				stock_bin.update_reserved_qty_for_sub_contracting()
 
 	def populate_items_table(self):
-		items = []
-
 		for si in self.service_items:
 			if si.fg_item:
 				item = frappe.get_doc("Item", si.fg_item)
@@ -159,7 +157,8 @@ class SubcontractingOrder(SubcontractingController):
 					)
 					or item.default_bom
 				)
-				items.append(
+				self.append(
+					"items",
 					{
 						"item_code": item.item_code,
 						"item_name": item.item_name,
@@ -168,6 +167,7 @@ class SubcontractingOrder(SubcontractingController):
 						"qty": si.fg_item_qty,
 						"stock_uom": item.stock_uom,
 						"bom": bom,
+						"purchase_order_item": si.purchase_order_item,
 					},
 				)
 			else:
@@ -176,11 +176,8 @@ class SubcontractingOrder(SubcontractingController):
 						si.item_name or si.item_code
 					)
 				)
-		else:
-			for item in items:
-				self.append("items", item)
-			else:
-				self.set_missing_values()
+
+		self.set_missing_values()
 
 	def update_status(self, status=None, update_modified=True):
 		if self.docstatus >= 1 and not status:
