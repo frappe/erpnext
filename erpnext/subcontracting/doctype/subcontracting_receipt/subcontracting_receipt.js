dd80d3b9b94c970d5070e1a5fac03cf9f1fbc455
s-aga-r <sagarsharma.s312@gmail.com>
2023-11-24 12:17:05 +0530

    fix: dont show `View` button on cancelled SCR

--- erpnext/subcontracting/doctype/subcontracting_receipt/subcontracting_receipt.js
+++ erpnext/subcontracting/doctype/subcontracting_receipt/subcontracting_receipt.js
@@ -24,7 +24,7 @@ frappe.ui.form.on('Subcontracting Receipt', {
 	},
 
 	refresh: (frm) => {
-		if (frm.doc.docstatus > 0) {
+		if (frm.doc.docstatus === 1) {
 			frm.add_custom_button(__('Stock Ledger'), () => {
 					frappe.route_options = {
 						voucher_no: frm.doc.name,
