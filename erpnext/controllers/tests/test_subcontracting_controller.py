8e370876cc14ec7ec5099b1fad003049680ed8b0
rohitwaghchaure <rohitw1991@gmail.com>
2023-11-29 12:21:11 +0530

    fix: incorrect ordered qty for Subcontracting Order (#38415)
    (cherry picked from commit fddf341f444efa89f80ec0fcb4e35b9b840061f5)
    
    # Conflicts:
    #	erpnext/controllers/tests/test_subcontracting_controller.py

--- erpnext/controllers/tests/test_subcontracting_controller.py
+++ erpnext/controllers/tests/test_subcontracting_controller.py
@@ -938,6 +938,7 @@ def make_subcontracted_items():
 		"Subcontracted Item SA5": {},
 		"Subcontracted Item SA6": {},
 		"Subcontracted Item SA7": {},
+		"Subcontracted Item SA8": {},
 	}
 
 	for item, properties in sub_contracted_items.items():
@@ -956,7 +957,12 @@ def make_raw_materials():
 			"batch_number_series": "BAT.####",
 		},
 		"Subcontracted SRM Item 4": {"has_serial_no": 1, "serial_no_series": "SRII.####"},
+<<<<<<< HEAD
 		"Subcontracted SRM Item 5": {"has_serial_no": 1, "serial_no_series": "SRII.####"},
+=======
+		"Subcontracted SRM Item 5": {"has_serial_no": 1, "serial_no_series": "SRIID.####"},
+		"Subcontracted SRM Item 8": {},
+>>>>>>> fddf341f44 (fix: incorrect ordered qty for Subcontracting Order (#38415))
 	}
 
 	for item, properties in raw_materials.items():
@@ -980,6 +986,7 @@ def make_service_items():
 		"Subcontracted Service Item 5": {},
 		"Subcontracted Service Item 6": {},
 		"Subcontracted Service Item 7": {},
+		"Subcontracted Service Item 8": {},
 	}
 
 	for item, properties in service_items.items():
@@ -1003,6 +1010,7 @@ def make_bom_for_subcontracted_items():
 		"Subcontracted Item SA5": ["Subcontracted SRM Item 5"],
 		"Subcontracted Item SA6": ["Subcontracted SRM Item 3"],
 		"Subcontracted Item SA7": ["Subcontracted SRM Item 1"],
+		"Subcontracted Item SA8": ["Subcontracted SRM Item 8"],
 	}
 
 	for item_code, raw_materials in boms.items():
