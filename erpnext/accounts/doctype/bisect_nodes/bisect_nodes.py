90c6d4dc85701902ad9d12b31716db098831dc10
ruthra kumar <ruthra@erpnext.com>
2023-12-01 16:50:17 +0530

    chore: restrict only to administrator and type info

--- erpnext/accounts/doctype/bisect_nodes/bisect_nodes.py
+++ erpnext/accounts/doctype/bisect_nodes/bisect_nodes.py
@@ -6,4 +6,23 @@ from frappe.model.document import Document
 
 
 class BisectNodes(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		balance_sheet_summary: DF.Float
+		difference: DF.Float
+		left_child: DF.Link | None
+		name: DF.Int | None
+		period_from_date: DF.Datetime | None
+		period_to_date: DF.Datetime | None
+		profit_loss_summary: DF.Float
+		right_child: DF.Link | None
+		root: DF.Link | None
+	# end: auto-generated types
+
 	pass
