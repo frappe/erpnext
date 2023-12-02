a72f25b291f2cfc25b3052eff3cb87edb0832c12
rohitwaghchaure <rohitw1991@gmail.com>
2023-12-02 23:12:12 +0530

    fix: better overlap logic for job card (#38432)
    (cherry picked from commit 74eab910427ca4af94119b0c099e7743be770006)
    
    # Conflicts:
    #	erpnext/manufacturing/doctype/job_card/job_card.py

--- erpnext/manufacturing/doctype/job_card/job_card.py
+++ erpnext/manufacturing/doctype/job_card/job_card.py
@@ -213,7 +213,12 @@ class JobCard(Document):
 			production_capacity = 1
 			query = query.where(jctl.employee == args.get("employee"))
 
+<<<<<<< HEAD
 		existing = query.run(as_dict=True)
+=======
+		if not self.has_overlap(production_capacity, time_logs):
+			return {}
+>>>>>>> 74eab91042 (fix: better overlap logic for job card (#38432))
 
 		overlap_count = self.get_overlap_count(existing)
 		if existing and production_capacity > overlap_count:
@@ -226,16 +231,15 @@ class JobCard(Document):
 
 		return existing[0] if existing else None
 
-	@staticmethod
-	def get_overlap_count(time_logs):
-		count = 1
+	def has_overlap(self, production_capacity, time_logs):
+		overlap = False
+		if production_capacity == 1 and len(time_logs) > 0:
+			return True
 
 		# Check overlap exists or not between the overlapping time logs with the current Job Card
-		for idx, row in enumerate(time_logs):
-			next_idx = idx
-			if idx + 1 < len(time_logs):
-				next_idx = idx + 1
-				next_row = time_logs[next_idx]
+		for row in time_logs:
+			count = 1
+			for next_row in time_logs:
 				if row.name == next_row.name:
 					continue
 
@@ -255,7 +259,10 @@ class JobCard(Document):
 				):
 					count += 1
 
-		return count
+			if count > production_capacity:
+				return True
+
+		return overlap
 
 	def get_workstation_based_on_available_slot(self, existing) -> Optional[str]:
 		workstations = get_workstations(self.workstation_type)
