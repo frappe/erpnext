c83c97f46621fcf2a5a0fd841c9e4a5f5ab21e70
ruthra kumar <ruthra@erpnext.com>
2023-12-05 10:14:05 +0530

    refactor: ignore unreconcile doc on PI cancel/delete
    (cherry picked from commit 4ca84eadb607263f8432698ff61c4acfd2f0363b)

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -1449,6 +1449,8 @@ class PurchaseInvoice(BuyingController):
 			"Repost Payment Ledger Items",
 			"Repost Accounting Ledger",
 			"Repost Accounting Ledger Items",
+			"Unreconcile Payment",
+			"Unreconcile Payment Entries",
 			"Payment Ledger Entry",
 			"Tax Withheld Vouchers",
 			"Serial and Batch Bundle",
