e291b5db3dbf6af8283e8370a43e2829e7893712
barredterra <14891507+barredterra@users.noreply.github.com>
2023-12-11 19:46:37 +0100

    chore: deprecate unused method
    (cherry picked from commit 956c3c50a09036dbeb9bfdf7ecd42958bdb0ec3e)

--- erpnext/startup/leaderboard.py
+++ erpnext/startup/leaderboard.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils.deprecations import deprecated
 
 
 def get_leaderboards():
@@ -217,6 +218,7 @@ def get_all_sales_person(date_range, company, field=None, limit=0):
 	)
 
 
+@deprecated
 def get_date_condition(date_range, field):
 	date_condition = ""
 	if date_range:
