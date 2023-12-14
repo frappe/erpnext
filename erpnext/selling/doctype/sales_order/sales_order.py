90f318e5a1eb1b43f1cdc29f8d17a7ed1db65468
anandbaburajan <anandbaburajan@gmail.com>
2023-12-14 23:30:03 +0530

    chore: throw error for incorrect delivery date in sales order

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -300,10 +300,14 @@ class SalesOrder(SellingController):
 		if self.order_type == "Sales" and not self.skip_delivery_note:
 			delivery_date_list = [d.delivery_date for d in self.get("items") if d.delivery_date]
 			max_delivery_date = max(delivery_date_list) if delivery_date_list else None
-			if (max_delivery_date and not self.delivery_date) or (
-				max_delivery_date and getdate(self.delivery_date) != getdate(max_delivery_date)
-			):
+			if max_delivery_date and not self.delivery_date:
 				self.delivery_date = max_delivery_date
+			elif max_delivery_date and getdate(self.delivery_date) != getdate(max_delivery_date):
+				frappe.throw(
+					_("Delivery Date should be {0}, which is the max of all items' delivery dates").format(
+						max_delivery_date
+					)
+				)
 			if self.delivery_date:
 				for d in self.get("items"):
 					if not d.delivery_date:
