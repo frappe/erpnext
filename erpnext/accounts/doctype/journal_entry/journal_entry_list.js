b89f045b9a6420bd558ee78f5b317dafe9a89899
barredterra <14891507+barredterra@users.noreply.github.com>
2023-12-28 17:16:55 +0100

    fix: journal entry list indicator

--- erpnext/accounts/doctype/journal_entry/journal_entry_list.js
+++ erpnext/accounts/doctype/journal_entry/journal_entry_list.js
@@ -1,12 +1,24 @@
-frappe.listview_settings['Journal Entry'] = {
-	add_fields: ["voucher_type", "posting_date", "total_debit", "company", "user_remark"],
-	get_indicator: function(doc) {
-		if(doc.docstatus==0) {
-			return [__("Draft", "red", "docstatus,=,0")]
-		} else if(doc.docstatus==2) {
-			return [__("Cancelled", "grey", "docstatus,=,2")]
+frappe.listview_settings["Journal Entry"] = {
+	add_fields: [
+		"voucher_type",
+		"posting_date",
+		"total_debit",
+		"company",
+		"user_remark",
+	],
+	has_indicator_for_draft: 1,
+	has_indicator_for_cancelled: 1,
+	get_indicator: function (doc) {
+		if (doc.docstatus == 0) {
+			return [__("Draft"), "red", "docstatus,=,0"];
+		} else if (doc.docstatus == 2) {
+			return [__("Cancelled"), "grey", "docstatus,=,2"];
 		} else {
-			return [__(doc.voucher_type), "blue", "voucher_type,=," + doc.voucher_type]
+			return [
+				__(doc.voucher_type),
+				"blue",
+				"voucher_type,=," + doc.voucher_type,
+			];
 		}
-	}
+	},
 };
