670d61547fb6a7ee86050c2f1020cd247f12df52
Gursheen Anand <gursheen@frappe.io>
2024-01-10 22:05:02 +0530

    fix: date in master document for dictionary condition
    (cherry picked from commit d96a777edd9cad698b9d9bb90863c1080eacb36f)
    
    # Conflicts:
    #	erpnext/accounts/utils.py

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -1222,8 +1222,18 @@ def get_autoname_with_number(number_value, doc_title, company):
 
 def parse_naming_series_variable(doc, variable):
 	if variable == "FY":
+<<<<<<< HEAD
 		date = doc.get("posting_date") or doc.get("transaction_date") or getdate()
 		return get_fiscal_year(date=date, company=doc.get("company"))[0]
+=======
+		if doc:
+			date = doc.get("posting_date") or doc.get("transaction_date") or getdate()
+			company = doc.get("company")
+		else:
+			date = getdate()
+			company = None
+		return get_fiscal_year(date=date, company=company)[0]
+>>>>>>> d96a777edd (fix: date in master document for dictionary condition)
 
 
 @frappe.whitelist()
