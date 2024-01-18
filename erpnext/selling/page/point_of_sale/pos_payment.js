128385d48308522ffb43f17a18a0a20c3c1747e5
mahsem <137205921+mahsem@users.noreply.github.com>
2024-01-18 14:05:33 +0100

    Update pos_payment.js
    Add Context to be able to translate to other languages
    Change word has different meaning in different context

--- erpnext/selling/page/point_of_sale/pos_payment.js
+++ erpnext/selling/page/point_of_sale/pos_payment.js
@@ -542,7 +542,7 @@ erpnext.PointOfSale.Payment = class {
 		const remaining = grand_total - doc.paid_amount;
 		const change = doc.change_amount || remaining <= 0 ? -1 * remaining : undefined;
 		const currency = doc.currency;
-		const label = change ? __('Change') : __('To Be Paid');
+		const label = change ? __("Change", null, "To Be Paid") : __('To Be Paid');
 
 		this.$totals.html(
 			`<div class="col">
