1fa62333773a59feb5a0a39d274ebcae362ed286
Rohit Waghchaure <rohitw1991@gmail.com>
2024-02-05 19:26:31 +0530

    perf: timeout while submitting the purchase receipt entry

--- erpnext/buying/doctype/purchase_order/purchase_order.py
+++ erpnext/buying/doctype/purchase_order/purchase_order.py
@@ -640,6 +640,7 @@ class PurchaseOrder(BuyingController):
 				update_sco_status(sco, "Closed" if self.status == "Closed" else None)
 
 
+@frappe.request_cache
 def item_last_purchase_rate(name, conversion_rate, item_code, conversion_factor=1.0):
 	"""get last purchase rate for an item"""
 
