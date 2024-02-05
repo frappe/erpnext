14d9d29913e78a585230763b3f25a2e903c263a7
Gursheen Anand <gursheen@frappe.io>
2024-02-05 15:52:46 +0530

    fix: over billing qty along with rate
    (cherry picked from commit cc96d2b50cbce3c137eebd754956344718cd1771)

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -859,8 +859,10 @@ class PurchaseInvoice(BuyingController):
 
 					if provisional_accounting_for_non_stock_items:
 						if item.purchase_receipt:
-							provisional_account, pr_base_rate = frappe.get_cached_value(
-								"Purchase Receipt Item", item.pr_detail, ["provisional_expense_account", "base_rate"]
+							provisional_account, pr_qty, pr_base_rate = frappe.get_cached_value(
+								"Purchase Receipt Item",
+								item.pr_detail,
+								["provisional_expense_account", "qty", "base_rate"],
 							)
 							provisional_account = provisional_account or self.get_company_default(
 								"default_provisional_account"
@@ -892,7 +894,7 @@ class PurchaseInvoice(BuyingController):
 									self.posting_date,
 									provisional_account,
 									reverse=1,
-									item_amount=(item.qty * pr_base_rate),
+									item_amount=(min(item.qty, pr_qty) * pr_base_rate),
 								)
 
 					if not self.is_internal_transfer():
