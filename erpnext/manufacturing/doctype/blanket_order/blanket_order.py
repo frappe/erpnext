9015376a9e607799d5137618f5eccf8eaa59ea67
s-aga-r <sagarsharma.s312@gmail.com>
2024-02-07 10:35:50 +0530

    chore: `conflicts`

--- erpnext/manufacturing/doctype/blanket_order/blanket_order.py
+++ erpnext/manufacturing/doctype/blanket_order/blanket_order.py
@@ -64,12 +64,8 @@ def make_order(source_name):
 
 	def update_item(source, target, source_parent):
 		target_qty = source.get("qty") - source.get("ordered_qty")
-<<<<<<< HEAD
 		target.qty = target_qty if not flt(target_qty) < 0 else 0
-=======
-		target.qty = target_qty if flt(target_qty) >= 0 else 0
 		target.rate = source.get("rate")
->>>>>>> 0e5b4e5f07 (fix: set rate for PO created against BO)
 		item = get_item_defaults(target.item_code, source_parent.company)
 		if item:
 			target.item_name = item.get("item_name")
