2a2784cfd29bb81ace32e79ae9efbc3c900a8a13
lmwangi <lmwangi@gmail.com>
2024-02-08 10:35:01 +0300

    Landed cost voucher enhancement: Order Purchase Receipt Items in the LC to be the same as the Purchase invoice/receipt

--- erpnext/stock/doctype/landed_cost_voucher/landed_cost_voucher.py
+++ erpnext/stock/doctype/landed_cost_voucher/landed_cost_voucher.py
@@ -300,5 +300,6 @@ def get_pr_items(purchase_receipt):
 			(pr_item.parent == purchase_receipt.receipt_document)
 			& ((item.is_stock_item == 1) | (item.is_fixed_asset == 1))
 		)
+		.orderby(pr_item.idx)
 		.run(as_dict=True)
 	)
