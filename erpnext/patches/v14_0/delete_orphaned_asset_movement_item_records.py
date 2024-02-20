ea1a0b3a28cdb4cc2f6e41c748e9577a49470ac8
Nabin Hait <nabinhait@gmail.com>
2024-02-20 15:51:29 +0530

    fix: linter issue

--- erpnext/patches/v14_0/delete_orphaned_asset_movement_item_records.py
+++ erpnext/patches/v14_0/delete_orphaned_asset_movement_item_records.py
@@ -1,7 +1,11 @@
 import frappe
 
+
 def execute():
-    frappe.db.sql("""
-        DELETE FROM `tabAsset Movement Item`
-        WHERE parent NOT IN (SELECT name FROM `tabAsset Movement`)
-    """)
+	# nosemgrep
+	frappe.db.sql(
+		"""
+		DELETE FROM `tabAsset Movement Item`
+		WHERE parent NOT IN (SELECT name FROM `tabAsset Movement`)
+		"""
+	)
