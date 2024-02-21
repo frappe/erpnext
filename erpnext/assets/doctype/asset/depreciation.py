a0c0ab7709baff88fe78373d261734f4183d8bd4
Nabin Hait <nabinhait@gmail.com>
2024-02-21 11:37:47 +0530

    fix: resolved conflict

--- erpnext/assets/doctype/asset/depreciation.py
+++ erpnext/assets/doctype/asset/depreciation.py
@@ -561,11 +561,8 @@ def modify_depreciation_schedule_for_asset_repairs(asset, notes):
 def reverse_depreciation_entry_made_after_disposal(asset, date):
 	for row in asset.get("finance_books"):
 		asset_depr_schedule_doc = get_asset_depr_schedule_doc(asset.name, "Active", row.finance_book)
-<<<<<<< HEAD
-=======
 		if not asset_depr_schedule_doc or not asset_depr_schedule_doc.get("depreciation_schedule"):
 			continue
->>>>>>> 17f85de6fb (fix: Issues regarding asset cancellation and deletion)
 
 		for schedule_idx, schedule in enumerate(asset_depr_schedule_doc.get("depreciation_schedule")):
 			if schedule.schedule_date == date and schedule.journal_entry:
