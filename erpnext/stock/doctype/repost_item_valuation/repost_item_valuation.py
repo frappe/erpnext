4f4087706161358b05caa8f6acb53780ce7481c7
rohitwaghchaure <rohitw1991@gmail.com>
2024-03-02 12:59:59 +0530

    fix: do not allow to cancel incomplete reposting (#40224)
    (cherry picked from commit 72ac56b6c4366910f7fc4f561811240ad0694965)
    
    # Conflicts:
    #	erpnext/stock/doctype/repost_item_valuation/repost_item_valuation.py

--- erpnext/stock/doctype/repost_item_valuation/repost_item_valuation.py
+++ erpnext/stock/doctype/repost_item_valuation/repost_item_valuation.py
@@ -219,6 +219,7 @@ class RepostItemValuation(Document):
 		if self.status not in ("Queued", "In Progress"):
 			return
 
+<<<<<<< HEAD
 		if not (self.voucher_no and self.voucher_no):
 			return
 
@@ -227,6 +228,11 @@ class RepostItemValuation(Document):
 			msg = _("Cannot cancel as processing of cancelled documents is  pending.")
 			msg += "<br>" + _("Please try again in an hour.")
 			frappe.throw(msg, title=_("Pending processing"))
+=======
+		msg = _("Cannot cancel as processing of cancelled documents is pending.")
+		msg += "<br>" + _("Please try again in an hour.")
+		frappe.throw(msg, title=_("Pending processing"))
+>>>>>>> 72ac56b6c4 (fix: do not allow to cancel incomplete reposting (#40224))
 
 	@frappe.whitelist()
 	def restart_reposting(self):
