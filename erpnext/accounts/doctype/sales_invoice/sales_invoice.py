d1e3b2ab6e2a15101adc03cbb706e8746da91ce7
ruthra kumar <ruthra@erpnext.com>
2024-03-11 14:49:28 +0530

    refactor: post ledger entries based on toggle
    (cherry picked from commit 77aac6f5716cf852b1f4781589e6c6c560bf4b43)

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -1247,6 +1247,10 @@ class SalesInvoice(SellingController):
 		)
 
 		if grand_total and not self.is_internal_transfer():
+			against_voucher = self.name
+			if self.is_return and self.return_against and not self.update_outstanding_for_self:
+				against_voucher = self.return_against
+
 			# Did not use base_grand_total to book rounding loss gle
 			gl_entries.append(
 				self.get_gl_dict(
@@ -1260,9 +1264,7 @@ class SalesInvoice(SellingController):
 						"debit_in_account_currency": base_grand_total
 						if self.party_account_currency == self.company_currency
 						else grand_total,
-						"against_voucher": self.name
-						if self.is_return and self.return_against and self.update_outstanding_for_self
-						else self.return_against,
+						"against_voucher": against_voucher,
 						"against_voucher_type": self.doctype,
 						"cost_center": self.cost_center,
 						"project": self.project,
