b189fc46b051edc1d1fe4f2bd6b7d54bb0624cf5
Ankush Menat <ankush@frappe.io>
2024-03-11 13:17:00 +0530

    fix: submit JVs synchronously if in background job

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -158,14 +158,14 @@ class JournalEntry(AccountsController):
 		validate_docs_for_deferred_accounting([self.name], [])
 
 	def submit(self):
-		if len(self.accounts) > 100:
+		if len(self.accounts) > 100 and not frappe.job:
 			msgprint(_("The task has been enqueued as a background job."), alert=True)
 			self.queue_action("submit", timeout=4600)
 		else:
 			return self._submit()
 
 	def cancel(self):
-		if len(self.accounts) > 100:
+		if len(self.accounts) > 100 and not frappe.job:
 			msgprint(_("The task has been enqueued as a background job."), alert=True)
 			self.queue_action("cancel", timeout=4600)
 		else:
