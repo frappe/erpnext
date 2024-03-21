69824eff80eb70b7e3139c6db84eedad84ceb8b4
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-21 11:52:10 +0100

    feat(Item Price): validate UOM

--- erpnext/stock/doctype/item_price/item_price.py
+++ erpnext/stock/doctype/item_price/item_price.py
@@ -56,6 +56,11 @@ class ItemPrice(Document):
 		if not frappe.db.exists("Item", self.item_code):
 			frappe.throw(_("Item {0} not found.").format(self.item_code))
 
+		if self.uom and not frappe.db.exists(
+			"UOM Conversion Detail", {"parenttype": "Item", "parent": self.item_code, "uom": self.uom}
+		):
+			frappe.throw(_("UOM {0} not found in Item {1}").format(self.uom, self.item_code))
+
 	def validate_dates(self):
 		if self.valid_from and self.valid_upto:
 			if getdate(self.valid_from) > getdate(self.valid_upto):
