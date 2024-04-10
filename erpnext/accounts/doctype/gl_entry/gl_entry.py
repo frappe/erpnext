b288c734376c09e25c97c53b3aaef6d55c932415
ruthra kumar <ruthra@erpnext.com>
2024-04-10 13:21:24 +0530

    fix: ignore dimension validation for cancelled entries
    (cherry picked from commit 971c867f292802f14a7635d5ebbae45a60ec868d)

--- erpnext/accounts/doctype/gl_entry/gl_entry.py
+++ erpnext/accounts/doctype/gl_entry/gl_entry.py
@@ -179,6 +179,7 @@ class GLEntry(Document):
 				and self.company == dimension.company
 				and dimension.mandatory_for_pl
 				and not dimension.disabled
+				and not self.is_cancelled
 			):
 				if not self.get(dimension.fieldname):
 					frappe.throw(
@@ -192,6 +193,7 @@ class GLEntry(Document):
 				and self.company == dimension.company
 				and dimension.mandatory_for_bs
 				and not dimension.disabled
+				and not self.is_cancelled
 			):
 				if not self.get(dimension.fieldname):
 					frappe.throw(
