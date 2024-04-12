5de9b6ac757246c6578af32f521398ca1629d51d
Rohit Waghchaure <rohitw1991@gmail.com>
2024-04-12 11:25:34 +0530

    fix: do not validate batch qty for LCV
    (cherry picked from commit baf0c83cc5c98bf74a6a437f4bec2131074a9ae0)

--- erpnext/stock/doctype/landed_cost_voucher/landed_cost_voucher.py
+++ erpnext/stock/doctype/landed_cost_voucher/landed_cost_voucher.py
@@ -253,7 +253,7 @@ class LandedCostVoucher(Document):
 			doc.make_bundle_using_old_serial_batch_fields(via_landed_cost_voucher=True)
 			doc.update_stock_ledger(allow_negative_stock=True, via_landed_cost_voucher=True)
 			doc.make_gl_entries()
-			doc.repost_future_sle_and_gle()
+			doc.repost_future_sle_and_gle(via_landed_cost_voucher=True)
 
 	def validate_asset_qty_and_status(self, receipt_document_type, receipt_document):
 		for item in self.get("items"):
