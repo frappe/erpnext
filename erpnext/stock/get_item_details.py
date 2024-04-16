af0375ee508c62f13181395d3536841392747092
rohitwaghchaure <rohitw1991@gmail.com>
2024-04-16 13:55:07 +0530

    chore: fix test case

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -524,7 +524,7 @@ def update_barcode_value(out):
 		out["barcode"] = barcode_data.get(out.item_code)[0]
 
 
-def get_barcode_data(items_list, item_code=None):
+def get_barcode_data(items_list=None, item_code=None):
 	# get itemwise batch no data
 	# exmaple: {'LED-GRE': [Batch001, Batch002]}
 	# where LED-GRE is item code, SN0001 is serial no and Pune is warehouse
