c335c2c85a26ee9c4c0b306c4cf07ae18c936cb0
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2024-04-29 12:03:06 +0530

    fix: daily prorata based depreciation bug in wdv method
    (cherry picked from commit b8a98a273b965a25a697944762fad0cd34ca1598)

--- erpnext/assets/doctype/asset_depreciation_schedule/test_asset_depreciation_schedule.py
+++ erpnext/assets/doctype/asset_depreciation_schedule/test_asset_depreciation_schedule.py
@@ -71,3 +71,68 @@ class TestAssetDepreciationSchedule(FrappeTestCase):
 			for d in get_depr_schedule(asset.name, "Draft")
 		]
 		self.assertEqual(schedules, expected_schedules)
+	# Test for Written Down Value Method
+	def test_daily_prorata_based_depreciation_for_wdv_method(self):
+		# Frequency of deprciation = 3
+		asset = create_asset(
+			item_code="Macbook Pro",
+			calculate_depreciation=1,
+			depreciation_method="Written Down Value",
+			daily_prorata_based=1,
+			available_for_use_date="2021-02-20",
+			depreciation_start_date="2021-03-31",
+			frequency_of_depreciation=3,
+			total_number_of_depreciations=18,
+			rate_of_depreciation=40,
+			submit=1,
+		)
+		asset_depr_schedule = frappe.get_last_doc("Asset Depreciation Schedule", {"asset": asset.name})
+		self.assertEqual(asset_depr_schedule.depreciation_schedule[1].depreciation_amount, 9521.45)
+
+		# Frequency of deprciation = 6
+		asset = create_asset(
+			item_code="Macbook Pro",
+			calculate_depreciation=1,
+			depreciation_method="Written Down Value",
+			daily_prorata_based=1,
+			available_for_use_date="2020-02-20",
+			depreciation_start_date="2020-03-01",
+			frequency_of_depreciation=6,
+			total_number_of_depreciations=18,
+			rate_of_depreciation=40,
+			submit=1,
+		)
+		asset_depr_schedule = frappe.get_last_doc("Asset Depreciation Schedule", {"asset": asset.name})
+		self.assertEqual(asset_depr_schedule.depreciation_schedule[1].depreciation_amount, 19889.52)
+
+		# Frequency of deprciation = 12
+		asset = create_asset(
+			item_code="Macbook Pro",
+			calculate_depreciation=1,
+			depreciation_method="Written Down Value",
+			daily_prorata_based=1,
+			available_for_use_date="2020-02-20",
+			depreciation_start_date="2020-03-01",
+			frequency_of_depreciation=12,
+			total_number_of_depreciations=4,
+			rate_of_depreciation=40,
+			submit=1,
+		)
+		asset_depr_schedule = frappe.get_last_doc("Asset Depreciation Schedule", {"asset": asset.name})
+		self.assertEqual(asset_depr_schedule.depreciation_schedule[0].depreciation_amount, 1092.90)
+
+	# Test for Straight Line Method
+	def test_daily_prorata_based_depreciation_for_straight_line_method(self):
+		asset = create_asset(
+			item_code="Macbook Pro",
+			calculate_depreciation=1,
+			depreciation_method="Straight Line",
+			daily_prorata_based=0,
+			available_for_use_date="2020-02-20",
+			depreciation_start_date="2020-03-01",
+			frequency_of_depreciation=12,
+			total_number_of_depreciations=4,
+			submit=1,
+		)
+		asset_depr_schedule = frappe.get_last_doc("Asset Depreciation Schedule", {"asset": asset.name})
+		self.assertEqual(asset_depr_schedule.depreciation_schedule[0].depreciation_amount, 751.37)
