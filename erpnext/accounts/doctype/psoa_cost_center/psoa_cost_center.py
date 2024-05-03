e82ea12cbc19f52491b4fdc3922abd829ab07f40
Deepesh Garg <deepeshgarg6@gmail.com>
2024-05-03 18:34:27 +0530

    fix: Cost center not getting saved in PSOA
    (cherry picked from commit 58f70396307846ac014398f3016cc94c0318d656)
    
    # Conflicts:
    #	erpnext/accounts/doctype/psoa_cost_center/psoa_cost_center.json
    #	erpnext/accounts/doctype/psoa_cost_center/psoa_cost_center.py

--- erpnext/accounts/doctype/psoa_cost_center/psoa_cost_center.py
+++ erpnext/accounts/doctype/psoa_cost_center/psoa_cost_center.py
@@ -7,4 +7,21 @@ from frappe.model.document import Document
 
 
 class PSOACostCenter(Document):
+<<<<<<< HEAD
+=======
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		cost_center_name: DF.Link
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+	# end: auto-generated types
+
+>>>>>>> 58f7039630 (fix: Cost center not getting saved in PSOA)
 	pass
