9a629f860c6acd0d51aaf39e9fc0cfe1c0f27d98
Poorvi <poorvi@Poorvis-MacBook-Air.local>
2024-05-10 11:47:57 +0530

    fix: fixing Item-wise sales register
    (cherry picked from commit c90185f5331e2271afb5ddc22340d3704f5bbcd3)
    (cherry picked from commit f36db7ebaf0c1e3c0de716f2733961be4f5b632e)

--- erpnext/accounts/report/purchase_register/purchase_register.py
+++ erpnext/accounts/report/purchase_register/purchase_register.py
@@ -376,8 +376,10 @@ def get_account_columns(invoice_list, include_payments):
 
 def get_invoices(filters, additional_query_columns):
 	pi = frappe.qb.DocType("Purchase Invoice")
+	pii = frappe.qb.DocType("Purchase Invoice Item") 
 	query = (
 		frappe.qb.from_(pi)
+		.left_join(pii).on(pi.name == pii.parent)
 		.select(
 			ConstantColumn("Purchase Invoice").as_("doctype"),
 			pi.name,
@@ -386,7 +388,7 @@ def get_invoices(filters, additional_query_columns):
 			pi.supplier,
 			pi.supplier_name,
 			pi.tax_id,
-			pi.bill_no,
+			pi.bill_no,	
 			pi.bill_date,
 			pi.remarks,
 			pi.base_net_total,
@@ -395,7 +397,7 @@ def get_invoices(filters, additional_query_columns):
 			pi.outstanding_amount,
 			pi.mode_of_payment,
 		)
-		.where(pi.docstatus == 1)
+		.where((pi.docstatus == 1) & pii.item_code.isnotnull())
 		.orderby(pi.posting_date, pi.name, order=Order.desc)
 	)
 
