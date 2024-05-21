163303e88818667c94338f0e9a207d91514087f2
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2024-05-21 16:55:06 +0530

    feat: introduced new field for showing total number of booked depreciations

--- erpnext/patches/v15_0/rename_number_of_depreciations_booked_to_opening_booked_depreciations.py
+++ erpnext/patches/v15_0/rename_number_of_depreciations_booked_to_opening_booked_depreciations.py
@@ -4,4 +4,4 @@ from frappe.model.utils.rename_field import rename_field
 
 def execute():
 	if frappe.db.has_column("Asset", "number_of_depreciations_booked"):
-		rename_field("Asset", "number_of_depreciations_booked", "opening_booked_depreciations")
+		rename_field("Asset", "number_of_depreciations_booked", "opening_number_of_booked_depreciations")
--- erpnext/patches/v15_0/update_gpa_and_ndb_for_assdeprsch.py
+++ erpnext/patches/v15_0/update_gpa_and_ndb_for_assdeprsch.py
@@ -9,12 +9,12 @@ def execute():
         ON `tabAsset Depreciation Schedule`.`asset`=`tabAsset`.`name`
         SET
             `tabAsset Depreciation Schedule`.`gross_purchase_amount`=`tabAsset`.`gross_purchase_amount`,
-            `tabAsset Depreciation Schedule`.`opening_booked_depreciations`=`tabAsset`.`opening_booked_depreciations`
+            `tabAsset Depreciation Schedule`.`opening_number_of_booked_depreciations`=`tabAsset`.`opening_number_of_booked_depreciations`
         WHERE
         (
             `tabAsset Depreciation Schedule`.`gross_purchase_amount`<>`tabAsset`.`gross_purchase_amount`
             OR
-            `tabAsset Depreciation Schedule`.`opening_booked_depreciations`<>`tabAsset`.`opening_booked_depreciations`
+            `tabAsset Depreciation Schedule`.`opening_number_of_booked_depreciations`<>`tabAsset`.`opening_number_of_booked_depreciations`
         )
         AND `tabAsset Depreciation Schedule`.`docstatus`<2"""
 	)
