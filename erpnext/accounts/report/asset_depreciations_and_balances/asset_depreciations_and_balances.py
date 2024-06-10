fd7486773b91fc00c6048f7ae880b58a6092db16
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2024-06-10 23:46:48 +0530

    chore: suppress linter warnings with # nosemgrep

--- erpnext/accounts/report/asset_depreciations_and_balances/asset_depreciations_and_balances.py
+++ erpnext/accounts/report/asset_depreciations_and_balances/asset_depreciations_and_balances.py
@@ -69,6 +69,7 @@ def get_asset_categories_for_grouped_by_category(filters):
 	condition = ""
 	if filters.get("asset_category"):
 		condition += " and asset_category = %(asset_category)s"
+	# nosemgrep
 	return frappe.db.sql(
 		f"""
 		SELECT a.asset_category,
