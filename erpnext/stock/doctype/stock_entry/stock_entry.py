d39739433b64e97ff245b0242cfea7e4bcdf651e
Rohit Waghchaure <rohitw1991@gmail.com>
2024-06-20 13:42:45 +0530

    fix: expense account from item group not fetched
    (cherry picked from commit 86ebe58231bf1a0271c9274a2b312bc533e1e5f3)

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -1627,7 +1627,7 @@ class StockEntry(StockController):
 				"has_serial_no": item.has_serial_no,
 				"has_batch_no": item.has_batch_no,
 				"sample_quantity": item.sample_quantity,
-				"expense_account": item.expense_account,
+				"expense_account": item.expense_account or item_group_defaults.get("expense_account"),
 			}
 		)
 
