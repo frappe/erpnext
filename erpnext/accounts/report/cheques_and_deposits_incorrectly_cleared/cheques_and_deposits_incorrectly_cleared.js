25193c5e92f7423668c696bd5996150299f58288
ruthra kumar <ruthra@erpnext.com>
2024-07-30 17:20:21 +0530

    feat: report to identify incorrectly cleared cheques
    (cherry picked from commit 28890fa833b31a40fd4f11affef4a15274335ebb)

---
+++ erpnext/accounts/report/cheques_and_deposits_incorrectly_cleared/cheques_and_deposits_incorrectly_cleared.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Cheques and Deposits Incorrectly cleared"] = {
+	filters: [],
+};
