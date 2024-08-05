1da744dc4d630ae81ea59ae4d4c1b3e7c49130a0
ruthra kumar <ruthra@erpnext.com>
2024-08-05 09:58:50 +0530

    refactor: make use of date filters on ignore filterss
    (cherry picked from commit 03f3ab522f54612f557a7d2f715367daf8bc370e)

--- erpnext/accounts/report/general_ledger/general_ledger.py
+++ erpnext/accounts/report/general_ledger/general_ledger.py
@@ -228,6 +228,7 @@ def get_conditions(filters):
 				"company": filters.get("company"),
 				"docstatus": 1,
 				"voucher_type": ("in", ["Exchange Rate Revaluation", "Exchange Gain Or Loss"]),
+				"posting_date": ["between", [filters.get("from_date"), filters.get("to_date")]],
 			},
 			as_list=True,
 		)
@@ -242,6 +243,7 @@ def get_conditions(filters):
 				"docstatus": 1,
 				"voucher_type": ("in", ["Credit Note", "Debit Note"]),
 				"is_system_generated": 1,
+				"posting_date": ["between", [filters.get("from_date"), filters.get("to_date")]],
 			},
 			as_list=True,
 		)
