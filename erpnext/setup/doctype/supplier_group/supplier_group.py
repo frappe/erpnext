2d77103c804817f07a4fe05859aeda0b36fc4ab8
ruthra kumar <ruthra@erpnext.com>
2024-08-14 17:31:19 +0530

    refactor: allow foreign currency accounts in Supplier Group
    (cherry picked from commit 164498bafb0f7763011d62cc615d4018c95ae248)
    
    # Conflicts:
    #	erpnext/setup/doctype/supplier_group/supplier_group.py

--- erpnext/setup/doctype/supplier_group/supplier_group.py
+++ erpnext/setup/doctype/supplier_group/supplier_group.py
@@ -12,6 +12,38 @@ class SupplierGroup(NestedSet):
 	def validate(self):
 		if not self.parent_supplier_group:
 			self.parent_supplier_group = get_root_of("Supplier Group")
+<<<<<<< HEAD
+=======
+		self.validate_currency_for_payable_and_advance_account()
+
+	def validate_currency_for_payable_and_advance_account(self):
+		for x in self.accounts:
+			payable_account_currency = None
+			advance_account_currency = None
+
+			if x.account:
+				payable_account_currency = frappe.get_cached_value("Account", x.account, "account_currency")
+
+			if x.advance_account:
+				advance_account_currency = frappe.get_cached_value(
+					"Account", x.advance_account, "account_currency"
+				)
+
+			if (
+				payable_account_currency
+				and advance_account_currency
+				and payable_account_currency != advance_account_currency
+			):
+				frappe.throw(
+					_(
+						"Both Payable Account: {0} and Advance Account: {1} must be of same currency for company: {2}"
+					).format(
+						frappe.bold(x.account),
+						frappe.bold(x.advance_account),
+						frappe.bold(x.company),
+					)
+				)
+>>>>>>> 164498bafb (refactor: allow foreign currency accounts in Supplier Group)
 
 	def on_update(self):
 		NestedSet.on_update(self)
