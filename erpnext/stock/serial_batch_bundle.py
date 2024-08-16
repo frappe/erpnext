48c3b0d094cbc4109fe56bed5b4a333ac804a37d
rohitwaghchaure <rohitw1991@gmail.com>
2024-08-16 15:10:22 +0530

    fix: Auto Create Serial and Batch Bundle For Outward (#42778)

--- erpnext/stock/serial_batch_bundle.py
+++ erpnext/stock/serial_batch_bundle.py
@@ -221,7 +221,15 @@ class SerialBatchBundle:
 			not self.sle.is_cancelled
 			and not self.sle.serial_and_batch_bundle
 			and self.item_details.has_batch_no == 1
-			and self.item_details.create_new_batch
+			and (
+				self.item_details.create_new_batch
+				or (
+					frappe.db.get_single_value(
+						"Stock Settings", "auto_create_serial_and_batch_bundle_for_outward"
+					)
+					and self.sle.actual_qty < 0
+				)
+			)
 		):
 			self.make_serial_batch_no_bundle()
 		elif not self.sle.is_cancelled:
