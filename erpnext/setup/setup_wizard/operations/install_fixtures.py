48513b10875eff2a01da1f22a48310e8ce755f21
barredterra <14891507+barredterra@users.noreply.github.com>
2024-08-17 02:01:35 +0200

    refactor: remove useless line

--- erpnext/setup/setup_wizard/operations/install_fixtures.py
+++ erpnext/setup/setup_wizard/operations/install_fixtures.py
@@ -477,7 +477,6 @@ def update_stock_settings():
 	stock_settings.item_naming_by = "Item Code"
 	stock_settings.valuation_method = "FIFO"
 	stock_settings.default_warehouse = frappe.db.get_value("Warehouse", {"warehouse_name": _("Stores")})
-	stock_settings.stock_uom = _("Nos")
 	stock_settings.stock_uom = "Nos"
 	stock_settings.auto_indent = 1
 	stock_settings.auto_insert_price_list_rate_if_missing = 1
