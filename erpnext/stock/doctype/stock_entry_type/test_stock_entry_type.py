1fe3308723c64bc514be274a45cf68ec034e8c34
Rohit Waghchaure <rohitw1991@gmail.com>
2024-08-20 15:57:46 +0530

    fix: custom stock entry type issue

--- erpnext/stock/doctype/stock_entry_type/test_stock_entry_type.py
+++ erpnext/stock/doctype/stock_entry_type/test_stock_entry_type.py
@@ -3,6 +3,33 @@
 
 import unittest
 
+import frappe
+
 
 class TestStockEntryType(unittest.TestCase):
-	pass
+	def test_stock_entry_type_non_standard(self):
+		stock_entry_type = "Test Manufacturing"
+
+		doc = frappe.get_doc(
+			{
+				"doctype": "Stock Entry Type",
+				"__newname": stock_entry_type,
+				"purpose": "Manufacture",
+				"is_standard": 1,
+			}
+		)
+
+		self.assertRaises(frappe.ValidationError, doc.insert)
+
+	def test_stock_entry_type_is_standard(self):
+		for stock_entry_type in [
+			"Material Issue",
+			"Material Receipt",
+			"Material Transfer",
+			"Material Transfer for Manufacture",
+			"Material Consumption for Manufacture",
+			"Manufacture",
+			"Repack",
+			"Send to Subcontractor",
+		]:
+			self.assertTrue(frappe.db.get_value("Stock Entry Type", stock_entry_type, "is_standard"))
