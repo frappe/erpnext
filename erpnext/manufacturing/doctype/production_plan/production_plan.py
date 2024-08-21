4eee52f40d659ad46544b578178d4f0bb75f6377
Türker Tunalı <turker.tunali@gmail.com>
2024-08-21 21:57:20 +0300

    Update production_plan.py for linter

--- erpnext/manufacturing/doctype/production_plan/production_plan.py
+++ erpnext/manufacturing/doctype/production_plan/production_plan.py
@@ -340,9 +340,7 @@ class ProductionPlan(Document):
 
 		for item in items:
 			# Calculate the pending qty with respect to conversion factor.
-			item.pending_qty = (
-				(flt(item.qty) - max(item.work_order_qty, item.delivered_qty, 0)) * item.conversion_factor
-			)
+			item.pending_qty = (flt(item.qty) - max(item.work_order_qty, item.delivered_qty, 0)) * item.conversion_factor
 
 		pi = frappe.qb.DocType("Packed Item")
 
