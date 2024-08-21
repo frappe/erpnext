69d2e5a8e7e097c5ea2a4a3569f28a98535d123d
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2024-08-21 17:08:31 +0530

    fix: post entries for capitalized asset through background jobs

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -697,7 +697,7 @@ class Asset(AccountsController):
 		purchase_document = self.get_purchase_document()
 		fixed_asset_account, cwip_account = self.get_fixed_asset_account(), self.get_cwip_account()
 
-		if purchase_document and self.purchase_amount and getdate(self.available_for_use_date) <= getdate():
+		if self.is_composite_asset or (purchase_document and self.purchase_amount) and getdate(self.available_for_use_date) <= getdate():
 			gl_entries.append(
 				self.get_gl_dict(
 					{
