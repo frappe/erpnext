3691a500ebde0367d51f0bb18d2b7a060df0bd50
ljain112 <ljain112@gmail.com>
2024-08-27 16:53:34 +0530

    fix: `default_advance_account` field in Process Payment Reconciliation
    (cherry picked from commit 143209f91a201c58004408cd04812aefa49e9f94)
    
    # Conflicts:
    #	erpnext/accounts/doctype/process_payment_reconciliation/process_payment_reconciliation.json
    #	erpnext/accounts/doctype/process_payment_reconciliation/process_payment_reconciliation.py

--- erpnext/accounts/doctype/process_payment_reconciliation/process_payment_reconciliation.py
+++ erpnext/accounts/doctype/process_payment_reconciliation/process_payment_reconciliation.py
@@ -11,6 +11,35 @@ from frappe.utils.scheduler import is_scheduler_inactive
 
 
 class ProcessPaymentReconciliation(Document):
+<<<<<<< HEAD
+=======
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		amended_from: DF.Link | None
+		bank_cash_account: DF.Link | None
+		company: DF.Link
+		cost_center: DF.Link | None
+		default_advance_account: DF.Link
+		error_log: DF.LongText | None
+		from_invoice_date: DF.Date | None
+		from_payment_date: DF.Date | None
+		party: DF.DynamicLink
+		party_type: DF.Link
+		receivable_payable_account: DF.Link
+		status: DF.Literal[
+			"", "Queued", "Running", "Paused", "Completed", "Partially Reconciled", "Failed", "Cancelled"
+		]
+		to_invoice_date: DF.Date | None
+		to_payment_date: DF.Date | None
+	# end: auto-generated types
+
+>>>>>>> 143209f91a (fix: `default_advance_account` field in Process Payment Reconciliation)
 	def validate(self):
 		self.validate_receivable_payable_account()
 		self.validate_bank_cash_account()
@@ -76,6 +105,7 @@ def get_pr_instance(doc: str):
 		"party_type",
 		"party",
 		"receivable_payable_account",
+		"default_advance_account",
 		"from_invoice_date",
 		"to_invoice_date",
 		"from_payment_date",
