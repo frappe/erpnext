fee2b4c7cdfb5349a9ab30cda347cd0a8a1b2c9d
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2024-09-03 14:02:07 -0400

    fix: round unallocated_amount

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -1124,6 +1124,7 @@ class PaymentEntry(AccountsController):
 					self.base_paid_amount - (total_deductions + self.base_total_allocated_amount)
 				) / self.target_exchange_rate
 				self.unallocated_amount -= included_taxes
+			self.unallocated_amount = flt(self.unallocated_amount, self.precision("unallocated_amount"))
 
 	def set_difference_amount(self):
 		base_unallocated_amount = flt(self.unallocated_amount) * (
