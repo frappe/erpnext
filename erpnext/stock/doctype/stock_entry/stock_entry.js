bcb29679db636187784fe2d4ab91f842aba10931
Ninad1306 <ninad_1063@yahoo.com>
2024-09-05 10:59:26 +0530

    fix: supplier required for stock entry return

--- erpnext/stock/doctype/stock_entry/stock_entry.js
+++ erpnext/stock/doctype/stock_entry/stock_entry.js
@@ -1296,7 +1296,7 @@ erpnext.stock.StockEntry = class StockEntry extends erpnext.stock.StockControlle
 
 		this.frm.cscript.toggle_enable_bom();
 
-		if (doc.purpose == "Send to Subcontractor") {
+		if (doc.purpose == "Send to Subcontractor" || (doc.purpose == "Material Transfer" && doc.is_return)) {
 			doc.customer =
 				doc.customer_name =
 				doc.customer_address =
