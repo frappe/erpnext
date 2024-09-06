5ce5b1b6a2a64cc5a8a19a478f95781886bfff72
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2024-09-07 01:33:12 +0530

    fix: unhide action button after form redirect

--- erpnext/assets/doctype/asset/asset.js
+++ erpnext/assets/doctype/asset/asset.js
@@ -528,6 +528,7 @@ frappe.ui.form.on("Asset", {
 			callback: function (r) {
 				var doclist = frappe.model.sync(r.message);
 				frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
+				$(".primary-action").prop("hidden", false);
 			},
 		});
 	},
