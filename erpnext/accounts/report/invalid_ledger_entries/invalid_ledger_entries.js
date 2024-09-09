37b8715096df6a7e645fedd40bc42e869857fcd7
ruthra kumar <ruthra@erpnext.com>
2024-09-09 12:31:32 +0530

    feat: utility report to identify invalid ledger entries
    (cherry picked from commit 832c4aaf82e6556a83429a3757efb3449b53de2a)

---
+++ erpnext/accounts/report/invalid_ledger_entries/invalid_ledger_entries.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Invalid Ledger Entries"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
