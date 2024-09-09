f45b1db1a402d57b7a6312d39f6ca975098a6ae5
Smit Vora <smitvora203@gmail.com>
2024-09-09 07:23:44 +0530

    fix: multiple fixes related to remarks for GL Report (#42753)
    * fix: show remarks in report only if it exists
    
    * fix: additional fixes to reduce redundancy in report print format
    
    * fix: revert changes for supplier invoice reference
    
    * fix: update remarks before submit to ensure all available details before submit are used
    
    * fix: patch to update invoice remarks where it's not set
    
    * fix: update remarks in payment ledger entry
    
    (cherry picked from commit e5a49f738bb3593a42180491114cc9f5424f8685)

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -285,7 +285,6 @@ class PurchaseInvoice(BuyingController):
 		self.set_against_expense_account()
 		self.validate_write_off_account()
 		self.validate_multiple_billing("Purchase Receipt", "pr_detail", "amount")
-		self.create_remarks()
 		self.set_status()
 		self.validate_purchase_receipt_if_update_stock()
 		validate_inter_company_party(
@@ -322,10 +321,11 @@ class PurchaseInvoice(BuyingController):
 
 	def create_remarks(self):
 		if not self.remarks:
-			if self.bill_no and self.bill_date:
-				self.remarks = _("Against Supplier Invoice {0} dated {1}").format(
-					self.bill_no, formatdate(self.bill_date)
-				)
+			if self.bill_no:
+				self.remarks = _("Against Supplier Invoice {0}").format(self.bill_no)
+				if self.bill_date:
+					self.remarks += " " + _("dated {0}").format(formatdate(self.bill_date))
+
 			else:
 				self.remarks = _("No Remarks")
 
@@ -746,6 +746,9 @@ class PurchaseInvoice(BuyingController):
 		self.validate_expense_account()
 		validate_docs_for_voucher_types(["Purchase Invoice"])
 		validate_docs_for_deferred_accounting([], [self.name])
+
+	def before_submit(self):
+		self.create_remarks()
 
 	def on_submit(self):
 		super().on_submit()
