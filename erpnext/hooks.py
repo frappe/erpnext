1509cff70c01cb9bd445ad1344a035b55dda6c9c
p-dhan <103286990+p-dhan@users.noreply.github.com>
2024-09-21 14:31:05 +0100

    use https

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -29,13 +29,13 @@ app_include_js = [
     "https://cdn.jsdelivr.net/npm/pdfkit@0.13.0/js/pdfkit.standalone.js",
     "https://cdn.jsdelivr.net/npm/blob-stream-browserify@0.1.3/index.js",
     "https://cdn.jsdelivr.net/npm/svg-to-pdfkit@0.1.8/source.js",
-	"http://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.js",
+	"https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.js",
 	"https://export.dhtmlx.com/gantt/api.js"
 ]
 
 app_include_css = [
 	"erpnext.bundle.css",
-	"http://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.css"
+	"https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.css"
 ]
 web_include_js = "erpnext-web.bundle.js"
 web_include_css = "erpnext-web.bundle.css"
