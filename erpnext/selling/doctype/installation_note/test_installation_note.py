9024621231202d5cdc128e10a5e1607ee5b8cf2f
David <dgx.arnold@gmail.com>
2024-10-08 01:52:47 +0200

    refactor: Use IntegrationTestCase in multiple files
    Signed-off-by: David <dgx.arnold@gmail.com>

--- erpnext/selling/doctype/installation_note/test_installation_note.py
+++ erpnext/selling/doctype/installation_note/test_installation_note.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
-
 import unittest
+
+from frappe.tests import IntegrationTestCase
 
 # test_records = frappe.get_test_records('Installation Note')
 
 
-class TestInstallationNote(unittest.TestCase):
+class TestInstallationNote(IntegrationTestCase):
 	pass
