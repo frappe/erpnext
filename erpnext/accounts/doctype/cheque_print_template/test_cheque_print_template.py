9340feb5e4aef5342a13e01aa1a17faff2b3d290
David <dgx.arnold@gmail.com>
2024-10-08 00:12:36 +0200

    refactor: Use IntegrationTestCase in multiple files
    Signed-off-by: David <dgx.arnold@gmail.com>

--- erpnext/accounts/doctype/cheque_print_template/test_cheque_print_template.py
+++ erpnext/accounts/doctype/cheque_print_template/test_cheque_print_template.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
-
 import unittest
+
+from frappe.tests import IntegrationTestCase
 
 # test_records = frappe.get_test_records('Cheque Print Template')
 
 
-class TestChequePrintTemplate(unittest.TestCase):
+class TestChequePrintTemplate(IntegrationTestCase):
 	pass
