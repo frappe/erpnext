c0da8f11f77c6d3bcd543e70aea6a9754427538e
ljain112 <ljain112@gmail.com>
2024-10-11 11:07:19 +0530

    fix: added parentheses for correct query formation for logical OR condition

--- erpnext/accounts/doctype/period_closing_voucher/period_closing_voucher.py
+++ erpnext/accounts/doctype/period_closing_voucher/period_closing_voucher.py
@@ -353,9 +353,10 @@ class PeriodClosingVoucher(AccountsController):
 
 		if get_opening_entries:
 			query = query.where(
-				gl_entry.posting_date.between(self.get("year_start_date"), self.posting_date)
-				| gl_entry.is_opening
-				== "Yes"
+				(  # noqa: UP034
+					(gl_entry.posting_date.between(self.get("year_start_date"), self.posting_date))
+					| (gl_entry.is_opening == "Yes")
+				)
 			)
 		else:
 			query = query.where(
