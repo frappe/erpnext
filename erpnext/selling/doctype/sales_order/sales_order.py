be78e17e8c614456cb2207c1829f00d9b567d395
ruthra kumar <ruthra@erpnext.com>
2024-10-15 13:25:14 +0530

    chore: resolve conflict

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -30,15 +30,7 @@ from erpnext.manufacturing.doctype.production_plan.production_plan import (
 from erpnext.selling.doctype.customer.customer import check_credit_limit
 from erpnext.setup.doctype.item_group.item_group import get_item_group_defaults
 from erpnext.stock.doctype.item.item import get_item_defaults
-<<<<<<< HEAD
-from erpnext.stock.get_item_details import get_default_bom, get_price_list_rate
-=======
-from erpnext.stock.doctype.stock_reservation_entry.stock_reservation_entry import (
-	get_sre_reserved_qty_details_for_voucher,
-	has_reserved_stock,
-)
 from erpnext.stock.get_item_details import get_bin_details, get_default_bom, get_price_list_rate
->>>>>>> 5f4a523340 (fix: update formatings)
 from erpnext.stock.stock_balance import get_reserved_qty, update_bin_qty
 
 form_grid_templates = {"items": "templates/form_grid/item_grid.html"}
