3f5c78f9c158be0629480600296f5fa690cc454c
p-dhan <103286990+p-dhan@users.noreply.github.com>
2024-11-25 21:59:59 +0000

    fix query

--- erpnext/manufacturing/doctype/job_card/job_card.py
+++ erpnext/manufacturing/doctype/job_card/job_card.py
@@ -1022,7 +1022,7 @@ class JobCard(Document):
 		data = frappe.get_all(
 			"Job Card",
 			fields=["operation", "status", "total_completed_qty", "sequence_id"],
-			filters={"docstatus":  ("<=", 1), "parent": self.work_order, "sequence_id": ("<", self.sequence_id)},
+			filters={"docstatus":  ("<=", 1), "work_order": self.work_order, "sequence_id": ("<", self.sequence_id)},
 			order_by="sequence_id, idx",
 		)
 
