fbfbe063be1b81bffcb71a345f2ab557f042b3cf
Mihir Kandoi <kandoimihir@gmail.com>
2025-08-08 11:44:55 +0530

    fix: ensure variant conversion factor is returned before the template's
    (cherry picked from commit d61977d002a8f7a1aa8396d31d4b19fbefea8fe0)
    
    # Conflicts:
    #	erpnext/stock/get_item_details.py

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -1277,6 +1277,7 @@ def get_pos_profile(company, pos_profile=None, user=None):
 
 @frappe.whitelist()
 def get_conversion_factor(item_code, uom):
+<<<<<<< HEAD
 	variant_of = frappe.db.get_value("Item", item_code, "variant_of", cache=True)
 	filters = {"parent": item_code, "uom": uom}
 
@@ -1286,8 +1287,35 @@ def get_conversion_factor(item_code, uom):
 	if not conversion_factor:
 		stock_uom = frappe.db.get_value("Item", item_code, "stock_uom")
 		conversion_factor = [get_uom_conv_factor(uom, stock_uom) or 1]
+=======
+	item = frappe.get_cached_value("Item", item_code, ["variant_of", "stock_uom"], as_dict=True)
+	if not item_code or not item or uom == item.stock_uom:
+		return {"conversion_factor": 1.0}
 
-	return {"conversion_factor": conversion_factor[-1]}
+	item_codes = [item_code]
+	if item.variant_of:
+		item_codes.append(item.variant_of)
+>>>>>>> d61977d002 (fix: ensure variant conversion factor is returned before the template's)
+
+	parent = frappe.qb.DocType("Item")
+	child = frappe.qb.DocType("UOM Conversion Detail")
+	query = (
+		frappe.qb.from_(parent)
+		.join(child)
+		.on(parent.name == child.parent)
+		.select(child.conversion_factor)
+		.where((parent.name.isin(item_codes)) & (child.uom == uom))
+		.orderby(parent.has_variants)
+		.limit(1)
+	)
+	conversion_factor = query.run(pluck="conversion_factor")
+
+	if not conversion_factor:
+		conversion_factor = get_uom_conv_factor(uom, item.stock_uom)
+	else:
+		conversion_factor = conversion_factor[0]
+
+	return {"conversion_factor": conversion_factor or 1.0}
 
 
 @frappe.whitelist()
