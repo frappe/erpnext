aefcf5653ab1c0bfe13cf5b9b10effee7536e296
barredterra <14891507+barredterra@users.noreply.github.com>
2025-08-29 17:17:08 +0200

    fix: stop creating transaction logs

--- erpnext/regional/__init__.py
+++ erpnext/regional/__init__.py
@@ -12,27 +12,3 @@ def check_deletion_permission(doc, method):
 	region = get_region(doc.company)
 	if region in ["Nepal"] and doc.docstatus != 0:
 		frappe.throw(_("Deletion is not permitted for country {0}").format(region))
-
-
-def create_transaction_log(doc, method):
-	"""
-	Appends the transaction to a chain of hashed logs for legal resons.
-	Called on submit of Sales Invoice and Payment Entry.
-	"""
-	if frappe.conf.get("disable_transaction_log", False):
-		return
-
-	region = get_region()
-	if region not in ["Germany"]:
-		return
-
-	data = str(doc.as_dict())
-
-	frappe.get_doc(
-		{
-			"doctype": "Transaction Log",
-			"reference_doctype": doc.doctype,
-			"document_name": doc.name,
-			"data": data,
-		}
-	).insert(ignore_permissions=True)
