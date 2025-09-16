ddceed790b7bb9b938f6b04933db6f6d5052b403
khushi8112 <khushirawat23@navgurukul.org>
2025-09-17 01:02:51 +0530

    refactor: revert debugging changes

--- erpnext/accounts/doctype/process_statement_of_accounts/test_process_statement_of_accounts.py
+++ erpnext/accounts/doctype/process_statement_of_accounts/test_process_statement_of_accounts.py
@@ -81,8 +81,8 @@ class TestProcessStatementOfAccounts(AccountsTestMixin, IntegrationTestCase):
 		process_soa = create_process_soa(
 			name="_Test Process SOA", enable_auto_email=1, report="Accounts Receivable"
 		)
-		if not frappe.flags.in_migrate and not frappe.flags.in_install:
-			send_emails(process_soa.name, from_scheduler=True)
+
+		send_emails(process_soa.name, from_scheduler=True)
 		process_soa.load_from_db()
 		self.assertEqual(process_soa.posting_date, getdate(add_days(today(), 7)))
 
