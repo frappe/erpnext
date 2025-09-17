25475d8b10cd22a0d1fc3ed209b6f1284cffa3bb
KerollesFathy <kerolles.f@outlook.com>
2025-09-17 13:14:35 +0000

    fix(accounts): update payment mode account retrieval to use namespaced function

--- erpnext/public/js/controllers/accounts.js
+++ erpnext/public/js/controllers/accounts.js
@@ -265,7 +265,7 @@ erpnext.accounts.pos = {
 		frappe.ui.form.on(doctype, {
 			mode_of_payment: function (frm, cdt, cdn) {
 				var d = locals[cdt][cdn];
-				get_payment_mode_account(frm, d.mode_of_payment, function (account) {
+				erpnext.accounts.pos.get_payment_mode_account(frm, d.mode_of_payment, function (account) {
 					frappe.model.set_value(cdt, cdn, "account", account);
 				});
 			},
