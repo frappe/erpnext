d02ca2869d587b9bee4163757cdf6e21c5e57e6d
ruthra kumar <ruthra@erpnext.com>
2025-09-25 16:35:54 +0530

    feat: process period closing voucher

---
+++ erpnext/accounts/doctype/process_period_closing_voucher/process_period_closing_voucher.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ProcessPeriodClosingVoucher(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+	# end: auto-generated types
+
+	pass
