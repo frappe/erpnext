a512ba5107c453d497e1200ab9d7a55172f8b4b7
Rohit Waghchaure <rohitw1991@gmail.com>
2025-09-29 11:41:27 +0530

    chore: removed print statement

--- erpnext/stock/stock_ledger.py
+++ erpnext/stock/stock_ledger.py
@@ -736,7 +736,6 @@ class update_entries_after:
 			elif dependant_sle.voucher_type == "Stock Entry" and is_transfer_stock_entry(
 				dependant_sle.voucher_no
 			):
-				print(dependant_sle.voucher_no)
 				self.distinct_item_warehouses[key] = val
 				self.new_items_found = True
 
