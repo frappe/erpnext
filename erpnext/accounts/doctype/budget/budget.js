08e9ee12000faa5b95cbf9ff406b721459bcf229
khushi8112 <khushirawat23@navgurukul.org>
2025-10-03 16:29:27 +0530

    fix: broken reference to removed 'use_new_budget_controller' field in Accounts Settings

--- erpnext/accounts/doctype/budget/budget.js
+++ erpnext/accounts/doctype/budget/budget.js
@@ -23,7 +23,7 @@ frappe.ui.form.on("Budget", {
 		});
 
 		erpnext.accounts.dimensions.setup_dimension_filters(frm, frm.doctype);
-		frappe.db.get_single_value("Accounts Settings", "use_new_budget_controller").then((value) => {
+		frappe.db.get_single_value("Accounts Settings", "use_legacy_budget_controller").then((value) => {
 			if (!value) {
 				frm.get_field("control_action_for_cumulative_expense_section").hide();
 			}
