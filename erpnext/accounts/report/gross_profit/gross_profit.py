7492898e7bb5fb77a6eb1bce89e63d4ff6a506b9
l0gesh29 <logeshperiyasamy24@gmail.com>
2025-10-03 14:03:28 +0530

    fix: delete column dynamically based on the naming by

--- erpnext/accounts/report/gross_profit/gross_profit.py
+++ erpnext/accounts/report/gross_profit/gross_profit.py
@@ -178,7 +178,12 @@ def get_data_when_grouped_by_invoice(columns, gross_profit_data, filters, group_
 	# to display item as Item Code: Item Name
 	columns[0] = "Sales Invoice:Link/Item:300"
 	# removing Item Code and Item Name columns
-	del columns[4:6]
+	supplier_master_name = frappe.db.get_single_value("Buying Settings", "supp_master_name")
+	customer_master_name = frappe.db.get_single_value("Selling Settings", "cust_master_name")
+	if supplier_master_name == "Supplier Name" and customer_master_name == "Customer Name":
+		del columns[4:6]
+	else:
+		del columns[5:7]
 
 	total_base_amount = 0
 	total_buying_amount = 0
@@ -275,7 +280,7 @@ def get_columns(group_wise_columns, filters):
 				"label": _("Posting Date"),
 				"fieldname": "posting_date",
 				"fieldtype": "Date",
-				"width": 100,
+				"width": 120,
 			},
 			"posting_time": {
 				"label": _("Posting Time"),
