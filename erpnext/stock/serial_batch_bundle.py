e66a7c3e055839643118dfa1b810458e1442170c
Rohit Waghchaure <rohitw1991@gmail.com>
2025-10-06 18:53:33 +0530

    fix: do not consider draft bundles

--- erpnext/stock/serial_batch_bundle.py
+++ erpnext/stock/serial_batch_bundle.py
@@ -1400,7 +1400,7 @@ def throw_negative_batch_validation(batch_no, qty):
 def get_batchwise_qty(voucher_type, voucher_no):
 	bundles = frappe.get_all(
 		"Serial and Batch Bundle",
-		filters={"voucher_no": voucher_no, "voucher_type": voucher_type},
+		filters={"voucher_no": voucher_no, "voucher_type": voucher_type, "docstatus": (">", 0)},
 		pluck="name",
 	)
 	if not bundles:
