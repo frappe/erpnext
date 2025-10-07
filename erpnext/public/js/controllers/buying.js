a94b1e3cbc22781332528a30054d663c2b6875f2
ravibharathi656 <ravibharathi656@gmail.com>
2025-10-07 12:46:09 +0530

    fix: preserve address if present

--- erpnext/public/js/controllers/buying.js
+++ erpnext/public/js/controllers/buying.js
@@ -174,13 +174,15 @@ erpnext.buying = {
 						shipping_address: this.frm.doc.shipping_address,
 					},
 					callback: (r) => {
-						this.frm.set_value("billing_address", r.message.primary_address || "");
+						if (!this.frm.doc.billing_address)
+							this.frm.set_value("billing_address", r.message.primary_address || "");
 
-						if (!frappe.meta.has_field(this.frm.doc.doctype, "shipping_address")) return;
-						this.frm.set_value(
-							"shipping_address",
-							r.message.shipping_address || this.frm.doc.shipping_address || ""
-						);
+						if (
+							!frappe.meta.has_field(this.frm.doc.doctype, "shipping_address") ||
+							this.frm.doc.shipping_address
+						)
+							return;
+						this.frm.set_value("shipping_address", r.message.shipping_address || "");
 					},
 				});
 				erpnext.utils.set_letter_head(this.frm);
--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -1170,7 +1170,8 @@ erpnext.TransactionController = class TransactionController extends erpnext.taxe
 
 		if (
 			frappe.meta.get_docfield(this.frm.doctype, "shipping_address") &&
-			["Purchase Order", "Purchase Receipt", "Purchase Invoice"].includes(this.frm.doctype)
+			["Purchase Order", "Purchase Receipt", "Purchase Invoice"].includes(this.frm.doctype) &&
+			!this.frm.doc.shipping_address
 		) {
 			let is_drop_ship = me.frm.doc.items.some((item) => item.delivered_by_supplier);
 
