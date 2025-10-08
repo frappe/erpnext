a80201adb15b5dfee8ed4d04f31d29307b553113
ruthra kumar <ruthra@erpnext.com>
2025-10-08 11:02:52 +0530

    refactor: barebones functions

--- erpnext/accounts/doctype/process_period_closing_voucher/process_period_closing_voucher.js
+++ erpnext/accounts/doctype/process_period_closing_voucher/process_period_closing_voucher.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Process Period Closing Voucher", {
-// 	refresh(frm) {
+frappe.ui.form.on("Process Period Closing Voucher", {
+	refresh(frm) {
+		if (frm.doc.docstatus == 1 && ["Queued"].find((x) => x == frm.doc.status)) {
+			let execute_btn = __("Start");
 
-// 	},
-// });
+			frm.add_custom_button(execute_btn, () => {
+				frm.call({
+					method: "erpnext.accounts.doctype.process_period_closing_voucher.process_period_closing_voucher.start_pcv_processing",
+					args: {
+						docname: frm.doc.name,
+					},
+				}).then((r) => {
+					if (!r.exc) {
+						frappe.show_alert(__("Job Started"));
+						frm.reload_doc();
+					}
+				});
+			});
+		}
+	},
+});
--- erpnext/accounts/doctype/process_period_closing_voucher/process_period_closing_voucher.py
+++ erpnext/accounts/doctype/process_period_closing_voucher/process_period_closing_voucher.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+from datetime import timedelta
+
+import frappe
 from frappe.model.document import Document
+from frappe.utils import add_days, get_datetime
 
 
 class ProcessPeriodClosingVoucher(Document):
@@ -18,9 +21,33 @@ class ProcessPeriodClosingVoucher(Document):
 			ProcessPeriodClosingVoucherDetail,
 		)
 
+		amended_from: DF.Link | None
 		dates_to_process: DF.Table[ProcessPeriodClosingVoucherDetail]
-		parent_pcv: DF.Link | None
+		parent_pcv: DF.Link
 		status: DF.Literal["Queued", "Running", "Completed"]
 	# end: auto-generated types
 
-	pass
+	def validate(self):
+		self.status = "Queued"
+		self.populate_processing_table()
+
+	def populate_processing_table(self):
+		self.dates_to_process = []
+		pcv = frappe.get_doc("Period Closing Voucher", self.parent_pcv)
+		start = get_datetime(pcv.period_start_date)
+		end = get_datetime(pcv.period_end_date)
+		dates = [start + timedelta(days=x) for x in range((end - start).days + 1)]
+		for x in dates:
+			self.append("dates_to_process", {"processing_date": x, "status": "Queued"})
+
+
+@frappe.whitelist()
+def start_pcv_processing(docname: str):
+	if frappe.db.get_value("Process Period Closing Voucher", docname, "status") == "Queued":
+		dates_to_process = frappe.db.get_all(
+			"Process Period Closing Voucher Detail",
+			filters={"parent": docname, "status": "Queued"},
+			fields=["processing_date"],
+			order_by="processing_date",
+			limit=4,
+		)
