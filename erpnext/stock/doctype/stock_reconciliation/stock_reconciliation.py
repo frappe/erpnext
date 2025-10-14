45adf07ea14e9f346ecab4fbb7feed4396f0ba39
El-Shafei H. <el.shafei.developer@gmail.com>
2025-10-14 16:26:36 +0300

    fix: save Stock Reconciliation before Send it to background job

--- erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
+++ erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
@@ -1000,6 +1000,7 @@ class StockReconciliation(StockController):
 
 	def submit(self):
 		if len(self.items) > 100:
+			self.save()
 			msgprint(
 				_(
 					"The task has been enqueued as a background job. In case there is any issue on processing in background, the system will add a comment about the error on this Stock Reconciliation and revert to the Draft stage"
