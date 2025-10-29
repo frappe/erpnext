6f48a1fb0dcd00d60538185938c91cbd8c8e043a
khushi8112 <khushirawat23@navgurukul.org>
2025-10-29 13:06:58 +0530

    fix: create GL entries via hooks only for submitted assets

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -946,7 +946,7 @@ def make_post_gl_entry():
 			assets = frappe.db.sql_list(
 				""" select name from `tabAsset`
 				where asset_category = %s and ifnull(booked_fixed_asset, 0) = 0
-				and available_for_use_date = %s""",
+				and available_for_use_date = %s and docstatus = 1""",
 				(asset_category.name, nowdate()),
 			)
 
