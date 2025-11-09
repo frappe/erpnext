349908ce3216fe22f61e2ee2f9c6a6f759e8fd1b
Thomas Antony <thomasantony12042001@gmail.com>
2025-11-09 15:15:36 +0530

    chore: changing the codes from erpnext to framework itself

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -665,4 +665,4 @@ export_python_type_annotations = True
 
 fields_for_group_similar_items = ["qty", "amount"]
 
-link_formatters = {"Item": "item_name", "Employee": "employee_name", "Project": "project_name"}
+link_formatters = ["Item", "Employee", "Project"]
