d4ed108730e1cf462712e8d730487bcd6f029644
Sagar Vora <16315650+sagarvora@users.noreply.github.com>
2025-12-11 17:26:34 +0530

    fix: ensure fresh `grand_total_diff` is used for each calculation

--- erpnext/controllers/taxes_and_totals.py
+++ erpnext/controllers/taxes_and_totals.py
@@ -398,6 +398,9 @@ class calculate_taxes_and_totals:
 			self._calculate()
 
 	def calculate_taxes(self):
+		# reset value from earlier calculations
+		self.grand_total_diff = 0
+
 		doc = self.doc
 		if not doc.get("taxes"):
 			return
@@ -684,7 +687,7 @@ class calculate_taxes_and_totals:
 				self.grand_total_diff = 0
 
 	def calculate_totals(self):
-		grand_total_diff = getattr(self, "grand_total_diff", 0)
+		grand_total_diff = self.grand_total_diff
 
 		if self.doc.get("taxes"):
 			self.doc.grand_total = flt(self.doc.get("taxes")[-1].total) + grand_total_diff
