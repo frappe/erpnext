50e3fb3049c1866a7d948d0edfa82bff8b380b03
AlekhyaNaidu-Spl <alekhya.naidu@splcg.com>
2025-12-18 11:05:47 +0000

    Added additional button to open Mixer station

--- erpnext/manufacturing/doctype/job_card/job_card_list.js
+++ erpnext/manufacturing/doctype/job_card/job_card_list.js
@@ -1,7 +1,7 @@
 frappe.listview_settings["Job Card"] = {
 	has_indicator_for_draft: true,
 	add_fields: ["expected_start_date", "expected_end_date"],
-	get_indicator: function (doc) {
+	get_indicator: function(doc) {
 		const status_colors = {
 			"Work In Progress": "orange",
 			Completed: "green",
@@ -14,4 +14,19 @@ frappe.listview_settings["Job Card"] = {
 
 		return [__(status), color, `status,=,${status}`];
 	},
+
+	button: {
+        show(doc) {
+            return true;
+        },
+        get_label() {
+            return __('Open Mixer');
+        },
+        get_description(doc) {
+            return __('Open Mixer Station for {0}', [doc.name]);
+        },
+        action(doc) {
+            frappe.set_route('mixer-station', doc.name);
+        }
+    }
 };
