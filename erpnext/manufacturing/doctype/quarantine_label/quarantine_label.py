f1f723d2a6440677938877ab7be517ab3b87e504
Suhas Bharadwaj <suhas.five@gmail.com>
2026-01-06 00:46:23 +0530

    feat: add quarantine labels to mahi settings

--- erpnext/manufacturing/doctype/quarantine_label/quarantine_label.py
+++ erpnext/manufacturing/doctype/quarantine_label/quarantine_label.py
@@ -1,21 +1,22 @@
-# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
 
-class MahiGranitesSettings(Document):
+class QuarantineLabel(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
-		from erpnext.manufacturing.doctype.quarantine_label.quarantine_label import QuarantineLabel
 		from frappe.types import DF
 
-		max_pay_line_amount: DF.Currency
-		quarantine_labels: DF.Table[QuarantineLabel]
+		parameter: DF.Data
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
 	# end: auto-generated types
 	pass
