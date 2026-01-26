17bb510b2928d1e75c0d2cdceceb7730e69a23aa
harrishragavan <harrishragavan1552005@gmail.com>
2026-01-26 22:13:13 +0530

    fix(shipment): use full name for user contact validation

--- erpnext/stock/doctype/shipment/shipment.js
+++ erpnext/stock/doctype/shipment/shipment.js
@@ -263,7 +263,7 @@ frappe.ui.form.on("Shipment", {
 			{ name: frappe.session.user },
 			["full_name", "last_name", "email", "phone", "mobile_no"],
 			(r) => {
-				if (!(r.last_name && r.email && (r.phone || r.mobile_no))) {
+				if (!(r.full_name && r.email && (r.phone || r.mobile_no))) {
 					if (delivery_type == "Delivery") {
 						frm.set_value("delivery_company", "");
 						frm.set_value("delivery_contact", "");
@@ -272,9 +272,11 @@ frappe.ui.form.on("Shipment", {
 						frm.set_value("pickup_contact", "");
 					}
 					frappe.throw(
-						__("Last Name, Email or Phone/Mobile of the user are mandatory to continue.") +
+						__(
+							"Full Name and at least one contact detail (Email or Phone/Mobile) are mandatory to continue."
+						) +
 							"</br>" +
-							__("Please first set Last Name, Email and Phone for the user") +
+							__("Please first set Full Name, Email and Phone for the user") +
 							` <a href="/app/user/${frappe.session.user}">${frappe.session.user}</a>`
 					);
 				}
