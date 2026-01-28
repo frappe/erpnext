1103b92902d2c327cbf9bc07f152fd06024280de
SowmyaArunachalam <sowmyaarunachalam57@gmail.com>
2026-01-28 22:24:59 +0530

    test: enable automatically_fetch_payment_terms_from_quotation

--- erpnext/selling/doctype/quotation/test_quotation.py
+++ erpnext/selling/doctype/quotation/test_quotation.py
@@ -181,6 +181,10 @@ class TestQuotation(IntegrationTestCase):
 
 		self.assertTrue(quotation.payment_schedule)
 
+	@IntegrationTestCase.change_settings(
+		"Selling Settings",
+		{"automatically_fetch_payment_terms_from_quotation": 1},
+	)
 	def test_make_sales_order_terms_copied(self):
 		from erpnext.selling.doctype.quotation.quotation import make_sales_order
 
@@ -324,6 +328,10 @@ class TestQuotation(IntegrationTestCase):
 	@IntegrationTestCase.change_settings(
 		"Accounts Settings",
 		{"add_taxes_from_item_tax_template": 0, "add_taxes_from_taxes_and_charges_template": 0},
+	)
+	@IntegrationTestCase.change_settings(
+		"Selling Settings",
+		{"automatically_fetch_payment_terms_from_quotation": 1},
 	)
 	def test_make_sales_order_with_terms(self):
 		from erpnext.selling.doctype.quotation.quotation import make_sales_order
