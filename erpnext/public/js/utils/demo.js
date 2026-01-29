8d1f8bb77348cd88ef90dde531ac73718e14ca02
diptanilsaha <diptanil@frappe.io>
2026-01-29 11:25:25 +0530

    fix(demo): removed toolbar eventlistener

--- erpnext/public/js/utils/demo.js
+++ erpnext/public/js/utils/demo.js
@@ -1,11 +1,5 @@
 frappe.provide("erpnext.demo");
 
-$(document).on("toolbar_setup", function () {
-	if (frappe.boot.sysdefaults.demo_company) {
-		render_clear_demo_action();
-	}
-});
-
 $(document).on("desktop_screen", function (event, data) {
 	data.desktop.add_menu_item({
 		label: __("Clear Demo Data"),
@@ -18,16 +12,6 @@ $(document).on("desktop_screen", function (event, data) {
 		},
 	});
 });
-
-function render_clear_demo_action() {
-	let demo_action = $(
-		`<a class="dropdown-item" onclick="return erpnext.demo.clear_demo()">
-			${__("Clear Demo Data")}
-		</a>`
-	);
-
-	demo_action.appendTo($("#toolbar-user"));
-}
 
 erpnext.demo.clear_demo = function () {
 	frappe.confirm(__("Are you sure you want to clear all demo data?"), () => {
