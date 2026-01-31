6c3426f873a36b5061c6179f15c9938594cab34f
Sudharsanan11 <sudharsananashok1975@gmail.com>
2026-01-31 17:20:33 +0530

    fix: remove child table condition

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -1069,15 +1069,7 @@ frappe.form.link_formatters["Project"] = function (value, doc, df) {
  * @returns {string} - The link value with the added title.
  */
 function add_link_title(value, doc, df, title_field) {
-	if (doc.doctype != df.parent) {
-		return value;
-	} else if (
-		doc &&
-		value &&
-		doc[title_field] &&
-		doc[title_field] !== value &&
-		doc[df.fieldname] === value
-	) {
+	if (doc && value && doc[title_field] && doc[title_field] !== value && doc[df.fieldname] === value) {
 		return value + ": " + doc[title_field];
 	} else if (!value && doc.doctype && doc[title_field]) {
 		return doc[title_field];
