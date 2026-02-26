c0edbf150d567346daac05c614cdd2edf9075741
Krishna Shirsath <shirsathkrishna19@gmail.com>
2026-02-26 16:41:42 +0530

    fix: allow zero debit and credit for payroll entries in journal validation

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -917,7 +917,7 @@ class JournalEntry(AccountsController):
 	def validate_debit_credit_amount(self):
 		if not (self.voucher_type == "Exchange Gain Or Loss" and self.multi_currency):
 			for d in self.get("accounts"):
-				if not flt(d.debit) and not flt(d.credit):
+				if not flt(d.debit) and not flt(d.credit) and d.reference_type != "Payroll Entry":
 					frappe.throw(_("Row {0}: Both Debit and Credit values cannot be zero").format(d.idx))
 
 	def validate_total_debit_and_credit(self):
