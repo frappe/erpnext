0d85d5d8ba1f0eb982094840965caf2683dd38d8
Suhas Bharadwaj <suhas.five@gmail.com>
2026-02-26 23:14:20 +0530

    feat(slab): implement slab number seeding

---
+++ erpnext/manufacturing/doctype/slab_seed/slab_seed.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SlabSeed(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		line: DF.Link
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		seed: DF.Int
+	# end: auto-generated types
+	pass
