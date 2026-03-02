353a2fb44f33f9134456f1fea7fafc4daf7b10b4
Shllokkk <shllokosan23@gmail.com>
2026-03-02 16:25:02 +0530

    fix: update select query field in patch and code refactor
    (cherry picked from commit 91c6475f1c0f7d6e4391800a0db73f0cb2f43fec)

--- erpnext/patches/v16_0/update_order_qty_and_requested_qty_based_on_mr_and_po.py
+++ erpnext/patches/v16_0/update_order_qty_and_requested_qty_based_on_mr_and_po.py
@@ -9,14 +9,14 @@ def execute():
 
 	poi_query = (
 		frappe.qb.from_(PurchaseOrderItem)
-		.select(PurchaseOrderItem.sales_order_item, Sum(PurchaseOrderItem.qty))
+		.select(PurchaseOrderItem.sales_order_item, Sum(PurchaseOrderItem.stock_qty))
 		.where(PurchaseOrderItem.sales_order_item.isnotnull() & PurchaseOrderItem.docstatus == 1)
 		.groupby(PurchaseOrderItem.sales_order_item)
 	)
 
 	mri_query = (
 		frappe.qb.from_(MaterialRequestItem)
-		.select(MaterialRequestItem.sales_order_item, Sum(MaterialRequestItem.qty))
+		.select(MaterialRequestItem.sales_order_item, Sum(MaterialRequestItem.stock_qty))
 		.where(MaterialRequestItem.sales_order_item.isnotnull() & MaterialRequestItem.docstatus == 1)
 		.groupby(MaterialRequestItem.sales_order_item)
 	)
@@ -24,14 +24,8 @@ def execute():
 	poi_data = poi_query.run()
 	mri_data = mri_query.run()
 
-	updates_against_poi = {}
-	updates_against_mri = {}
-
-	for data in poi_data:
-		updates_against_poi[data[0]] = {"ordered_qty": data[1]}
-
-	for data in mri_data:
-		updates_against_mri[data[0]] = {"requested_qty": data[1], "ordered_qty": 0}
+	updates_against_poi = {data[0]: {"ordered_qty": data[1]} for data in poi_data}
+	updates_against_mri = {data[0]: {"requested_qty": data[1], "ordered_qty": 0} for data in mri_data}
 
 	frappe.db.auto_commit_on_many_writes = 1
 	frappe.db.bulk_update("Sales Order Item", updates_against_mri)
