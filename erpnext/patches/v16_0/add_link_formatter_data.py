e84e7dfaf2a6af689ef911b6759736068a9a1f97
Thomas Antony <thomasantony12042001@gmail.com>
2026-03-04 19:10:08 +0530

    chore: use migrate to add rows

--- erpnext/patches/v16_0/add_link_formatter_data.py
+++ erpnext/patches/v16_0/add_link_formatter_data.py
@@ -1,7 +1,22 @@
-from frappe.utils.install import add_link_field_formatters
+import frappe
 
-from erpnext.setup.install import LINK_FIELD_DATA
+LINK_FIELD_DATA = [
+	{"doctype_name": "Item", "link_fieldname": "item_code", "display_fieldname": "item_name"},
+	{"doctype_name": "Employee", "link_fieldname": "employee", "display_fieldname": "employee_name"},
+	{"doctype_name": "Project", "link_fieldname": "project", "display_fieldname": "project_name"},
+]
 
 
 def execute():
-	add_link_field_formatters(LINK_FIELD_DATA)
+	link_formatter = frappe.get_single("Link Formatter")
+
+	for data in LINK_FIELD_DATA:
+		exists = any(
+			row.doctype_name == data["doctype_name"] and row.link_fieldname == data["link_fieldname"]
+			for row in link_formatter.link_field_display
+		)
+
+		if not exists:
+			link_formatter.append("link_field_display", data)
+
+	link_formatter.save()
