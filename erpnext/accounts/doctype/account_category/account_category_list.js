b344f5358e42328b4098d4a37b72189c21edc5cf
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-03-05 17:10:03 +0530

    refactor: Update the list view

---
+++ erpnext/accounts/doctype/account_category/account_category_list.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings["Account Category"] = {
+	hide_name_column: true,
+	hide_name_filter: true,
+};
