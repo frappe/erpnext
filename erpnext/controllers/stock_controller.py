6e89078552598f6c6cb4da8aca16cc1f81693b6a
Abdus Samad <120767334+Samad-11@users.noreply.github.com>
2026-03-06 10:34:53 +0000

    .

--- erpnext/controllers/stock_controller.py
+++ erpnext/controllers/stock_controller.py
@@ -1994,7 +1994,7 @@ def get_accounting_ledger_preview(doc, filters):
 
 	doc.docstatus = 1
 
-	if doc.get("update_stock") or doc.doctype in ("Purchase Receipt", "Delivery Note"):
+	if doc.get("update_stock") or doc.doctype in ("Purchase Receipt", "Delivery Note", "Stock Entry"):
 		doc.update_stock_ledger()
 
 	doc.make_gl_entries()
