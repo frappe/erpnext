33cfe8d6b61db8eecb46ce25e011ae490cc82827
Shubh Doshi <shubhdoshi21@gmail.com>
2026-03-10 19:06:04 +0530

    chore: revert temporary test against frappe PR 37009

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -12,7 +12,7 @@ pip install frappe-bench
 
 githubbranch=${GITHUB_BASE_REF:-${GITHUB_REF##*/}}
 frappeuser=${FRAPPE_USER:-"frappe"}
-frappecommitish="refs/pull/37009/head"
+frappecommitish=${FRAPPE_BRANCH:-$githubbranch}
 
 mkdir frappe
 pushd frappe
