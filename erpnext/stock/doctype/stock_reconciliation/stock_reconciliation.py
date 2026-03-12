37536632a3833c6bb62d3b5ae71b38fd45de26e8
Mihir Kandoi <kandoimihir@gmail.com>
2026-03-12 14:30:00 +0530

    fix: do not modify rate in the child item merely for comparison (#53301)
    (cherry picked from commit 7f644785ceecc9ad058552256653cd48e2f08e76)

--- erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
+++ erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
@@ -524,11 +524,11 @@ class StockReconciliation(StockController):
 					return True
 
 			rate_precision = item.precision("valuation_rate")
-			item_dict["rate"] = flt(item_dict.get("rate"), rate_precision)
-			item.valuation_rate = flt(item.valuation_rate, rate_precision) if item.valuation_rate else None
+			rate = flt(item_dict.get("rate"), rate_precision)
+			valuation_rate = flt(item.valuation_rate, rate_precision) if item.valuation_rate else None
 			if (
 				(item.qty is None or item.qty == item_dict.get("qty"))
-				and (item.valuation_rate is None or item.valuation_rate == item_dict.get("rate"))
+				and (valuation_rate is None or valuation_rate == rate)
 				and (not item.serial_no or (item.serial_no == item_dict.get("serial_nos")))
 			):
 				return False
@@ -1008,9 +1008,9 @@ class StockReconciliation(StockController):
 
 	def set_total_qty_and_amount(self):
 		for d in self.get("items"):
-			d.amount = flt(d.qty, d.precision("qty")) * flt(d.valuation_rate, d.precision("valuation_rate"))
-			d.current_amount = flt(d.current_qty, d.precision("current_qty")) * flt(
-				d.current_valuation_rate, d.precision("current_valuation_rate")
+			d.amount = flt(flt(d.qty) * flt(d.valuation_rate), d.precision("amount"))
+			d.current_amount = flt(
+				flt(d.current_qty) * flt(d.current_valuation_rate), d.precision("current_amount")
 			)
 
 			d.quantity_difference = flt(d.qty) - flt(d.current_qty)
