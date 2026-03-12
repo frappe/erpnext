5a6e5b2880669c7dd99907b66294f3a9d471ed42
khushi8112 <khushirawat23@navgurukul.org>
2026-03-12 13:18:00 +0530

    fix: move show_general_ledger to Asset Repair form events

--- erpnext/assets/doctype/asset_repair/asset_repair.js
+++ erpnext/assets/doctype/asset_repair/asset_repair.js
@@ -86,6 +86,26 @@ frappe.ui.form.on("Asset Repair", {
 		}
 	},
 
+	show_general_ledger: function (frm) {
+		if (frm.doc.docstatus > 0) {
+			frm.add_custom_button(
+				__("Accounting Ledger"),
+				function () {
+					frappe.route_options = {
+						voucher_no: frm.doc.name,
+						from_date: frm.doc.completion_date,
+						to_date: moment(frm.doc.modified).format("YYYY-MM-DD"),
+						company: frm.doc.company,
+						categorize_by: "",
+						show_cancelled_entries: frm.doc.docstatus === 2,
+					};
+					frappe.set_route("query-report", "General Ledger");
+				},
+				__("View")
+			);
+		}
+	},
+
 	repair_status: (frm) => {
 		if (frm.doc.completion_date && frm.doc.repair_status == "Completed") {
 			frappe.call({
@@ -163,26 +183,6 @@ frappe.ui.form.on("Asset Repair Purchase Invoice", {
 				}
 			},
 		});
-	},
-
-	show_general_ledger: (frm) => {
-		if (frm.doc.docstatus > 0) {
-			frm.add_custom_button(
-				__("Accounting Ledger"),
-				function () {
-					frappe.route_options = {
-						voucher_no: frm.doc.name,
-						from_date: frm.doc.posting_date,
-						to_date: moment(frm.doc.modified).format("YYYY-MM-DD"),
-						company: frm.doc.company,
-						categorize_by: "",
-						show_cancelled_entries: frm.doc.docstatus === 2,
-					};
-					frappe.set_route("query-report", "General Ledger");
-				},
-				__("View")
-			);
-		}
 	},
 });
 
