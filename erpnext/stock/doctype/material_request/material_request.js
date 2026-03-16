15dda554601960d4b3d13f45bcece27c79795794
Jeraldin P J <113301032+jeraldin2003@users.noreply.github.com>
2026-03-16 09:45:30 +0530

    fix: remove supplier selection dialog when creating Purchase Order from Material Request (#53391)
    Co-authored-by: jeraldin2003 <jeraldin2003>
    Co-authored-by: Nikhil Kothari <nik.kothari22@live.com>

--- erpnext/stock/doctype/material_request/material_request.js
+++ erpnext/stock/doctype/material_request/material_request.js
@@ -410,33 +410,11 @@ frappe.ui.form.on("Material Request", {
 	},
 
 	make_purchase_order: function (frm) {
-		frappe.prompt(
-			{
-				label: __("For Default Supplier (Optional)"),
-				fieldname: "default_supplier",
-				fieldtype: "Link",
-				options: "Supplier",
-				description: __(
-					"Select a Supplier from the Default Suppliers of the items below. On selection, a Purchase Order will be made against items belonging to the selected Supplier only."
-				),
-				get_query: () => {
-					return {
-						query: "erpnext.stock.doctype.material_request.material_request.get_default_supplier_query",
-						filters: { doc: frm.doc.name },
-					};
-				},
-			},
-			(values) => {
-				frappe.model.open_mapped_doc({
-					method: "erpnext.stock.doctype.material_request.material_request.make_purchase_order",
-					frm: frm,
-					args: { default_supplier: values.default_supplier },
-					run_link_triggers: true,
-				});
-			},
-			__("Enter Supplier"),
-			__("Create")
-		);
+		frappe.model.open_mapped_doc({
+			method: "erpnext.stock.doctype.material_request.material_request.make_purchase_order",
+			frm: frm,
+			run_link_triggers: true,
+		});
 	},
 
 	make_request_for_quotation: function (frm) {
