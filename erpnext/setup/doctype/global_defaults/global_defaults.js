1b85f61e79a333d4230459901aa950b67d315b12
AhmedAbukhatwa <Ahmedabukhatwa1@gmail.com>
2026-03-17 11:28:18 +0200

    fix: make demo data controls visible and accessible from Global Defaults

--- erpnext/setup/doctype/global_defaults/global_defaults.js
+++ erpnext/setup/doctype/global_defaults/global_defaults.js
@@ -2,6 +2,72 @@
 // License: GNU General Public License v3. See license.txt
 
 frappe.ui.form.on("Global Defaults", {
+	refresh: function (frm) {
+		if (!frm.doc.demo_company) {
+			frm.add_custom_button(
+				__("Setup Demo Data"),
+				function () {
+					frappe.confirm(
+						__("This will create a Demo Company and generate sample transactions. Continue?"),
+						function () {
+							frappe.call({
+								method: "erpnext.setup.demo.setup_demo_data",
+								freeze: true,
+								freeze_message: __("Create Demo Data..."),
+								callback: function (r) {
+									if (!r.exc) {
+										frm.reload_doc();
+									}
+								},
+								error: function () {
+									frappe.msgprint({
+										title: __("Error"),
+										message: __("Failed to create demo data. Check the error log."),
+										indicator: "red",
+									});
+								},
+							});
+						}
+					);
+				},
+				__("Demo")
+			);
+		}
+
+		frm.add_custom_button(
+			__("Clear Demo Data"),
+			function () {
+				frappe.confirm(
+					__("Are you sure you want to clear all demo data? This cannot be undone."),
+					function () {
+						frappe.call({
+							method: "erpnext.setup.demo.clear_demo_data",
+							freeze: true,
+							freeze_message: __("Clearing Demo Data..."),
+							callback: function (r) {
+								if (!r.exc) {
+									frappe.msgprint({
+										title: __("Success"),
+										message: __("Demo data has been cleared successfully."),
+										indicator: "green",
+									});
+									frm.reload_doc();
+								}
+							},
+							error: function () {
+								frappe.msgprint({
+									title: __("Error"),
+									message: __("Failed to clear demo data. Check the error log."),
+									indicator: "red",
+								});
+							},
+						});
+					}
+				);
+			},
+			__("Demo")
+		);
+	},
 	onload: function (frm) {
 		frm.trigger("get_distance_uoms");
 	},
