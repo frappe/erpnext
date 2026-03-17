7d09b54cb712d48967c656537619c1f0901122ac
Saeed Kola <mohammedsaeedk@gmail.com>
2026-03-17 16:55:45 +0530

    fix: skip validate_stock_accounts when perpetual inventory is disabled
    When perpetual inventory is disabled, stock accounts have no GL impact
    from stock transactions, so blocking manual Journal Entries against them
    is incorrect. Added an early return guard in validate_stock_accounts()
    to skip the check when is_perpetual_inventory_enabled() returns False.

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -275,6 +275,9 @@ class JournalEntry(AccountsController):
 			frappe.throw(_("Journal Entry type should be set as Depreciation Entry for asset depreciation"))
 
 	def validate_stock_accounts(self):
+		if not erpnext.is_perpetual_inventory_enabled(self.company):
+			return
+
 		stock_accounts = get_stock_accounts(self.company, accounts=self.accounts)
 		for account in stock_accounts:
 			account_bal, stock_bal, warehouse_list = get_stock_and_account_balance(
