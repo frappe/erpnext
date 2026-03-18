f55e9cb0358af862f49bff7a5f6c98cf92f696b2
Mihir Kandoi <kandoimihir@gmail.com>
2026-03-18 10:17:21 +0530

    chore: fix formatting

--- erpnext/manufacturing/doctype/work_order/work_order.py
+++ erpnext/manufacturing/doctype/work_order/work_order.py
@@ -1372,7 +1372,9 @@ def get_item_details(item, project=None, skip_bom_info=False, throw=True):
 
 
 @frappe.whitelist()
-def make_work_order(bom_no, item, qty=0, company=None, project=None, variant_items=None, use_multi_level_bom=None):
+def make_work_order(
+	bom_no, item, qty=0, company=None, project=None, variant_items=None, use_multi_level_bom=None
+):
 	from erpnext import get_default_company
 
 	if not frappe.has_permission("Work Order", "write"):
