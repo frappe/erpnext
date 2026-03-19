3c2f8bb04b4b126939371cbabf86c2ff21972d28
Sowmya <106989392+SowmyaArunachalam@users.noreply.github.com>
2026-03-19 09:54:17 +0530

    fix: ignore cost center (#53063)
    (cherry picked from commit 03d8a7a6af07f425108151a9b8eaabb0c250ae97)

--- erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
@@ -4805,6 +4805,33 @@ class TestSalesInvoice(ERPNextTestSuite):
 
 		self.assertEqual(stock_ledger_entry.incoming_rate, 0.0)
 
+	def test_inter_company_transaction_cost_center(self):
+		si = create_sales_invoice(
+			company="Wind Power LLC",
+			customer="_Test Internal Customer",
+			debit_to="Debtors - WP",
+			warehouse="Stores - WP",
+			income_account="Sales - WP",
+			expense_account="Cost of Goods Sold - WP",
+			parent_cost_center="Main - WP",
+			cost_center="Main - WP",
+			currency="USD",
+			do_not_save=1,
+		)
+
+		si.selling_price_list = "_Test Price List Rest of the World"
+		si.submit()
+
+		cost_center = frappe.db.get_value("Company", "_Test Company 1", "cost_center")
+		frappe.db.set_value("Company", "_Test Company 1", "cost_center", None)
+
+		target_doc = make_inter_company_transaction("Sales Invoice", si.name)
+
+		self.assertEqual(target_doc.cost_center, None)
+		self.assertEqual(target_doc.items[0].cost_center, None)
+
+		frappe.db.set_value("Company", "_Test Company 1", "cost_center", cost_center)
+
 
 def make_item_for_si(item_code, properties=None):
 	from erpnext.stock.doctype.item.test_item import make_item
