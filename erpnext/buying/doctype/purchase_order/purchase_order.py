184b117d308cc767a8dccf206aed1d9e5ec5e2cc
diptanilsaha <diptanil@frappe.io>
2026-03-19 20:19:29 +0530

    fix: validate permission before updating status (#53651)
    (cherry picked from commit 8e17c722fbb2c685d215d7daf309eb856282050c)
    
    # Conflicts:
    #	erpnext/buying/doctype/purchase_order/purchase_order.py
    #	erpnext/selling/doctype/sales_order/sales_order.py
    #	erpnext/stock/doctype/purchase_receipt/purchase_receipt.py

--- erpnext/buying/doctype/purchase_order/purchase_order.py
+++ erpnext/buying/doctype/purchase_order/purchase_order.py
@@ -911,8 +911,13 @@ def get_list_context(context=None):
 
 
 @frappe.whitelist()
+<<<<<<< HEAD
 def update_status(status, name):
 	po = frappe.get_doc("Purchase Order", name)
+=======
+def update_status(status: str, name: str):
+	po = frappe.get_lazy_doc("Purchase Order", name, check_permission="write")
+>>>>>>> 8e17c722fb (fix: validate permission before updating status (#53651))
 	po.update_status(status)
 	po.update_delivered_qty_in_sales_order()
 
