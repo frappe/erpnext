d16d144d88d9d6a589be86e8074a7aeffd51f9c3
Sudharsanan Ashok <135326972+Sudharsanan11@users.noreply.github.com>
2026-03-23 15:16:37 +0530

    fix(manufacturing): update non-stock item dict (#53689)
    (cherry picked from commit 41f986ff83d6bece00d4159cf1752a2fa908dad8)
    
    # Conflicts:
    #	erpnext/manufacturing/doctype/bom/bom.py

--- erpnext/manufacturing/doctype/bom/bom.py
+++ erpnext/manufacturing/doctype/bom/bom.py
@@ -1335,9 +1335,18 @@ def add_non_stock_items_cost(stock_entry, work_order, expense_account):
 	bom = frappe.get_doc("BOM", work_order.bom_no)
 	table = "exploded_items" if work_order.get("use_multi_level_bom") else "items"
 
-	items = {}
+	items = frappe._dict()
 	for d in bom.get(table):
+<<<<<<< HEAD
 		items.setdefault(d.item_code, d.amount)
+=======
+		# Phantom item is exploded, so its cost is considered via its components
+		if d.get("is_phantom_item"):
+			continue
+
+		items.setdefault(d.item_code, 0)
+		items[d.item_code] += flt(d.amount)
+>>>>>>> 41f986ff83 (fix(manufacturing): update non-stock item dict (#53689))
 
 	non_stock_items = frappe.get_all(
 		"Item",
