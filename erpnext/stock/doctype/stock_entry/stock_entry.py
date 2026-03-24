2091b94a60c560e117286b438bf2d649ce5f91ff
gajjug004 <gajjug004@gmail.com>
2026-03-24 23:36:57 +0530

    fix: show current stock qty in Stock Entry PDF

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -202,6 +202,13 @@ class StockEntry(StockController, SubcontractingInwardController):
 			)
 
 	def onload(self):
+		self.update_item_bin_details()
+
+	def before_print(self, settings=None):
+		super().before_print(settings)
+		self.update_item_bin_details()
+
+	def update_item_bin_details(self):
 		for item in self.get("items"):
 			item.update(get_bin_details(item.item_code, item.s_warehouse or item.t_warehouse))
 
