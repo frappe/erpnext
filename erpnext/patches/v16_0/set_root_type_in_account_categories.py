0c33a83cf37657d677ae1ef9f9869cf2bf4edbc8
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-03-26 12:01:02 +0530

    refactor: validate root types in account categories before setting

--- erpnext/patches/v16_0/set_root_type_in_account_categories.py
+++ erpnext/patches/v16_0/set_root_type_in_account_categories.py
@@ -13,9 +13,11 @@ def execute():
 
 	categories = json.loads(frappe.read_file(str(categories_file)))
 
+	valid_root_types = set(frappe.get_meta("Account Category").get_field("root_type").options.split("\n"))
+
 	root_type_categories = {}
 	for category in categories:
-		if root_type := category.get("root_type"):
+		if (root_type := category.get("root_type")) and root_type in valid_root_types:
 			root_type_categories.setdefault(root_type, []).append(category["account_category_name"])
 
 	if not root_type_categories:
