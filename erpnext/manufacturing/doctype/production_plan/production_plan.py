52b6e955e3e3d7d930560cbc851784e990aac90d
Sudharsanan Ashok <135326972+Sudharsanan11@users.noreply.github.com>
2026-03-29 21:51:36 +0530

    fix(manufacturing): update the qty precision (#53874)
    (cherry picked from commit f3a794384a05f7cb4b12d553d4024213a8b8fa91)

--- erpnext/manufacturing/doctype/production_plan/production_plan.py
+++ erpnext/manufacturing/doctype/production_plan/production_plan.py
@@ -1783,8 +1783,10 @@ def get_items_for_material_requests(doc, warehouses=None, get_parent_warehouse_d
 			)
 
 		sales_order = data.get("sales_order")
+		qty_precision = frappe.get_precision("Material Request Plan Item", "quantity")
 
 		for key, details in item_details.items():
+			details.qty = flt(details.qty, qty_precision)
 			so_item_details.setdefault(sales_order, frappe._dict())
 			if key in so_item_details.get(sales_order, {}):
 				so_item_details[sales_order][key]["qty"] = so_item_details[sales_order][key].get(
