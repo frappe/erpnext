bf7bb39263ae401b11380d7a168e33aeb7c57ac0
Nikhil Kothari <nik.kothari22@live.com>
2026-03-29 23:07:44 +0530

    feat: Espresso textarea

--- banking/src/components/ui/textarea.tsx
+++ banking/src/components/ui/textarea.tsx
@@ -1,13 +1,37 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { cva, VariantProps } from "class-variance-authority"
 
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
+
+const textareaVariants = cva("flex field-sizing-content w-full transition-all focus-visible:bg-surface-white focus-visible:border-outline-gray-4 focus-visible:shadow-focus-gray active:bg-surface-white active:shadow-textarea-active active:border-outline-gray-4 outline-none border border-transparent placeholder:text-ink-gray-4 text-ink-gray-7 disabled:bg-surface-gray-1 disabled:placeholder:text-ink-gray-3 disabled:text-ink-gray-3 disabled:cursor-not-allowed",
+  {
+    variants: {
+      size: {
+        sm: "text-p-base rounded py-1.5 px-2 min-h-15",
+        md: "text-p-lg rounded-md py-2.5 px-3 min-h-20.5",
+        lg: "text-p-xl rounded-md py-3 px-3.5 min-h-25.5",
+      },
+      variant: {
+        subtle: "bg-surface-gray-2 hover:bg-surface-gray-3",
+        outline: "bg-surface-white border-outline-gray-2 hover:border-outline-gray-3 active:border-outline-gray-4 disabled:border-outline-gray-2",
+      }
+    },
+    defaultVariants: {
+      size: "md",
+      variant: "subtle"
+    }
+  }
+)
+
+function Textarea({ size = "md", variant = "subtle", className, ...props }: React.ComponentProps<"textarea"> & VariantProps<typeof textareaVariants>) {
   return (
     <textarea
       data-slot="textarea"
+      data-size={size}
+      data-variant={variant}
       className={cn(
-        "border-input placeholder:text-ink-gray-5 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        textareaVariants({ size, variant }),
         className
       )}
       {...props}
