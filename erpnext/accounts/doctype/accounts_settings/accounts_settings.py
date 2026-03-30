b11e35c6513bceaed72093d01ae7a68259cfb17d
Jatin3128 <jatinsarna8@gmail.com>
2026-03-30 17:26:27 +0530

    feat(Payment Request): Added a toggle for using the payment schedule
    (cherry picked from commit 8ec15b537ec1da26d097378bc67b63629dbc758f)
    
    # Conflicts:
    #	erpnext/accounts/doctype/accounts_settings/accounts_settings.json

--- erpnext/accounts/doctype/accounts_settings/accounts_settings.py
+++ erpnext/accounts/doctype/accounts_settings/accounts_settings.py
@@ -73,6 +73,7 @@ class AccountsSettings(Document):
 		enable_loyalty_point_program: DF.Check
 		enable_party_matching: DF.Check
 		exchange_gain_loss_posting_date: DF.Literal["Invoice", "Payment", "Reconciliation Date"]
+		fetch_payment_schedule_in_payment_request: DF.Check
 		fetch_valuation_rate_for_internal_transaction: DF.Check
 		general_ledger_remarks_length: DF.Int
 		ignore_account_closing_balance: DF.Check
