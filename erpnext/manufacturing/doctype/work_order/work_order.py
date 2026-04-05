8864091c5d10e38d029ffca868aa9a5c0f3ba4b2
mohamedamineboukettaya <amine.boukettayaa@gmail.com>
2026-04-05 20:57:34 +0100

    fix: prevent Work Order from using disabled production items

--- erpnext/manufacturing/doctype/work_order/work_order.py
+++ erpnext/manufacturing/doctype/work_order/work_order.py
@@ -1402,9 +1402,14 @@ class WorkOrder(Document):
 	def validate_production_item(self):
 		if frappe.get_cached_value("Item", self.production_item, "has_variants"):
 			frappe.throw(_("Work Order cannot be raised against a Item Template"), ItemHasVariantError)
-
 		if self.production_item:
 			validate_end_of_life(self.production_item)
+			if frappe.db.get_value("Item", self.production_item, "disabled"):
+				frappe.throw(
+					_("Production Item {0} is disabled. Please select an active item.").format(
+						frappe.bold(self.production_item)
+					)
+				)
 
 	def validate_qty(self):
 		if self.qty <= 0:
