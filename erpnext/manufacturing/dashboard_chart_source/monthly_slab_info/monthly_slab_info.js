72c0cfffc2e35ac660604a3bde0d9619347cf3ae
AlekhyaNaidu-Spl <alekhya.naidu@splcg.com>
2026-04-06 13:11:52 +0000

    created dashboard chart source for tracking the slab monthly

---
+++ erpnext/manufacturing/dashboard_chart_source/monthly_slab_info/monthly_slab_info.js
@@ -0,0 +1,3 @@
+frappe.dashboards.chart_sources["Monthly Slab Info"] = {
+    method: "erpnext.manufacturing.dashboard_chart.monthly_slab_report.monthly_slab_report.get_monthly_slab_report_data",
+};
