87035c15ac466bf8ffac7d6bcf82e9f63b5d50f6
diptanilsaha <diptanil@frappe.io>
2026-04-07 17:16:18 +0530

    chore: resolve conflicts

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -353,12 +353,11 @@ class JournalEntry(AccountsController):
 					frappe.throw(_("Account {0} should be of type Expense").format(d.account))
 
 	def validate_stock_accounts(self):
-<<<<<<< HEAD
-		if self.voucher_type == "Periodic Accounting Entry":
-			# Skip validation for periodic accounting entry
-=======
-		if not erpnext.is_perpetual_inventory_enabled(self.company):
->>>>>>> 3bc9190795 (fix: skip validate_stock_accounts when perpetual inventory is disabled)
+		if (
+			not erpnext.is_perpetual_inventory_enabled(self.company)
+			or self.voucher_type == "Periodic Accounting Entry"
+		):
+			# Skip validation for periodic accounting entry and Perpetual Inventory Disabled Company.
 			return
 
 		stock_accounts = get_stock_accounts(self.company, accounts=self.accounts)
