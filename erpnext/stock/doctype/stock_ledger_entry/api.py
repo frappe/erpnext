1127d347c75ca5443a9c709fad693c54af2cc38d
AlekhyaNaidu-Spl <alekhya.naidu@splcg.com>
2026-04-08 10:33:55 +0000

    feat(dashboard): added slab summary related custom blocks

--- erpnext/stock/doctype/stock_ledger_entry/api.py
+++ erpnext/stock/doctype/stock_ledger_entry/api.py
@@ -36,6 +36,27 @@ def get_total_cost_slab_daily():
 	return result[0][0] or 0
 
 
+@frappe.whitelist()
+def get_daily_finished_goods_item_code_summary():
+	today = frappe_utils.today()
+
+	result = frappe.db.sql(
+		"""
+        SELECT item_code, COUNT(*) AS cnt
+        FROM `tabStock Ledger Entry`
+        WHERE DATE(creation) = %s
+		AND warehouse LIKE '%%finished goods Warehouse%%'
+        AND actual_qty > 0
+        GROUP BY item_code
+        ORDER BY cnt DESC, item_code ASC
+    """,
+		(today,),
+		as_dict=True,
+	)
+
+	return result
+
+
 # Weekly Number cards
 @frappe.whitelist()
 def get_total_no_of_slabs_weekly():
@@ -73,6 +94,28 @@ def get_total_cost_slab_weekly():
 	return result[0][0] or 0
 
 
+@frappe.whitelist()
+def get_weekly_finished_goods_item_code_summary():
+	start_date = frappe_utils.get_first_day_of_week(frappe_utils.today())
+	end_date = frappe_utils.get_last_day_of_week(frappe_utils.today())
+
+	result = frappe.db.sql(
+		"""
+        SELECT item_code, COUNT(*) AS cnt
+        FROM `tabStock Ledger Entry`
+        WHERE DATE(creation) BETWEEN %s AND %s
+		AND warehouse LIKE '%%finished goods Warehouse%%'
+        AND actual_qty > 0
+        GROUP BY item_code
+        ORDER BY cnt DESC, item_code ASC
+    """,
+		(start_date, end_date),
+		as_dict=True,
+	)
+
+	return result
+
+
 # For Monthly Number cards
 @frappe.whitelist()
 def get_total_no_of_slabs_monthly():
@@ -108,3 +151,25 @@ def get_total_cost_slab_monthly():
 		(start_date, end_date),
 	)
 	return result[0][0] or 0
+
+
+@frappe.whitelist()
+def get_finished_goods_item_code_summary():
+	start_date = frappe_utils.get_first_day(frappe_utils.today())
+	end_date = frappe_utils.get_last_day(frappe_utils.today())
+
+	result = frappe.db.sql(
+		"""
+        SELECT item_code, COUNT(*) AS cnt
+        FROM `tabStock Ledger Entry`
+        WHERE warehouse LIKE '%%finished goods Warehouse%%'
+          AND actual_qty > 0
+          AND DATE(creation) BETWEEN %s AND %s
+        GROUP BY item_code
+        ORDER BY cnt DESC, item_code ASC
+    """,
+		(start_date, end_date),
+		as_dict=True,
+	)
+
+	return result
