c241b6f354ff4ba46462baa31cb0572e8b3321e6
Mihir Kandoi <kandoimihir@gmail.com>
2026-04-08 19:09:56 +0530

    fix: inventory dimensions should not be mandatory unnecesarily (#54064)
    (cherry picked from commit 6e44b8913e95ecb9b95a5c4e71bb2ae542b24352)
    
    # Conflicts:
    #	erpnext/patches.txt
    #	erpnext/stock/doctype/inventory_dimension/inventory_dimension.py

--- erpnext/stock/doctype/inventory_dimension/inventory_dimension.py
+++ erpnext/stock/doctype/inventory_dimension/inventory_dimension.py
@@ -31,7 +31,6 @@ class InventoryDimension(Document):
 		apply_to_all_doctypes: DF.Check
 		condition: DF.Code | None
 		dimension_name: DF.Data
-		disabled: DF.Check
 		document_type: DF.Link | None
 		fetch_from_parent: DF.Literal[None]
 		istable: DF.Check
@@ -75,7 +74,6 @@ class InventoryDimension(Document):
 
 		old_doc = self._doc_before_save
 		allow_to_edit_fields = [
-			"disabled",
 			"fetch_from_parent",
 			"type_of_transaction",
 			"condition",
@@ -119,6 +117,7 @@ class InventoryDimension(Document):
 	def reset_value(self):
 		if self.apply_to_all_doctypes:
 			self.type_of_transaction = ""
+			self.mandatory_depends_on = ""
 
 			self.istable = 0
 			for field in ["document_type", "condition"]:
@@ -183,8 +182,12 @@ class InventoryDimension(Document):
 				label=_(label),
 				depends_on="eval:doc.s_warehouse" if doctype == "Stock Entry Detail" else "",
 				search_index=1,
-				reqd=self.reqd,
-				mandatory_depends_on=self.mandatory_depends_on,
+				reqd=1
+				if self.reqd and not self.mandatory_depends_on and doctype != "Stock Entry Detail"
+				else 0,
+				mandatory_depends_on="eval:doc.s_warehouse"
+				if self.reqd and doctype == "Stock Entry Detail"
+				else self.mandatory_depends_on,
 			),
 		]
 
@@ -296,12 +299,13 @@ class InventoryDimension(Document):
 					options=self.reference_document,
 					label=label,
 					depends_on=display_depends_on,
+					mandatory_depends_on=display_depends_on if self.reqd else self.mandatory_depends_on,
 				),
 			]
 		)
 
 
-def field_exists(doctype, fieldname) -> str or None:
+def field_exists(doctype, fieldname) -> str | None:
 	return frappe.db.get_value("DocField", {"parent": doctype, "fieldname": fieldname}, "name")
 
 
@@ -360,6 +364,7 @@ def get_evaluated_inventory_dimension(doc, sl_dict, parent_doc=None):
 
 
 def get_document_wise_inventory_dimensions(doctype) -> dict:
+<<<<<<< HEAD
 	if not hasattr(frappe.local, "document_wise_inventory_dimensions"):
 		frappe.local.document_wise_inventory_dimensions = {}
 
@@ -381,10 +386,25 @@ def get_document_wise_inventory_dimensions(doctype) -> dict:
 		frappe.local.document_wise_inventory_dimensions[doctype] = dimensions
 
 	return frappe.local.document_wise_inventory_dimensions[doctype]
+=======
+	return frappe.get_all(
+		"Inventory Dimension",
+		fields=[
+			"name",
+			"source_fieldname",
+			"condition",
+			"target_fieldname",
+			"type_of_transaction",
+			"fetch_from_parent",
+		],
+		or_filters={"document_type": doctype, "apply_to_all_doctypes": 1},
+	)
+>>>>>>> 6e44b8913e (fix: inventory dimensions should not be mandatory unnecesarily (#54064))
 
 
 @frappe.whitelist()
 def get_inventory_dimensions():
+<<<<<<< HEAD
 	if not hasattr(frappe.local, "inventory_dimensions"):
 		frappe.local.inventory_dimensions = {}
 
@@ -403,6 +423,20 @@ def get_inventory_dimensions():
 		frappe.local.inventory_dimensions = dimensions
 
 	return frappe.local.inventory_dimensions
+=======
+	return frappe.get_all(
+		"Inventory Dimension",
+		fields=[
+			"target_fieldname as fieldname",
+			"source_fieldname",
+			"reference_document as doctype",
+			"validate_negative_stock",
+			"name as dimension_name",
+		],
+		order_by="creation",
+		distinct=True,
+	)
+>>>>>>> 6e44b8913e (fix: inventory dimensions should not be mandatory unnecesarily (#54064))
 
 
 @frappe.whitelist()
