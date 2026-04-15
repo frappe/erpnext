6f45c3e80fdcaf1c3438ba16873df0d7b23c63b7
ljain112 <ljain112@gmail.com>
2026-04-15 17:23:20 +0530

    chore: resolve conflicts

--- erpnext/public/js/controllers/taxes_and_totals.js
+++ erpnext/public/js/controllers/taxes_and_totals.js
@@ -609,20 +609,6 @@ erpnext.taxes_and_totals = class TaxesAndTotals extends erpnext.payments {
 			disable_rounded_total = frappe.sys_defaults.disable_rounded_total;
 		}
 
-<<<<<<< HEAD
-		if (cint(disable_rounded_total)) {
-			this.frm.doc.rounded_total = 0;
-			this.frm.doc.base_rounded_total = 0;
-			this.frm.doc.rounding_adjustment = 0;
-			return;
-		}
-
-		if(frappe.meta.get_docfield(this.frm.doc.doctype, "rounded_total", this.frm.doc.name)) {
-			this.frm.doc.rounded_total = round_based_on_smallest_currency_fraction(this.frm.doc.grand_total,
-				this.frm.doc.currency, precision("rounded_total"));
-			this.frm.doc.rounding_adjustment = flt(this.frm.doc.rounded_total - this.frm.doc.grand_total,
-				precision("rounding_adjustment"));
-=======
 		if (frappe.meta.get_docfield(this.frm.doc.doctype, "rounded_total", this.frm.doc.name)) {
 			if (cint(disable_rounded_total)) {
 				this.frm.doc.rounded_total = 0;
@@ -638,7 +624,6 @@ erpnext.taxes_and_totals = class TaxesAndTotals extends erpnext.payments {
 					precision("rounding_adjustment")
 				);
 			}
->>>>>>> f8d278b733 (fix: reset base_rounded_total when rounded_total resets)
 
 			this.set_in_company_currency(this.frm.doc, ["rounding_adjustment", "rounded_total"]);
 		}
