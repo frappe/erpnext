a1b3de759cc2d85bcc70883cda61b41e782fe757
ljain112 <ljain112@gmail.com>
2026-04-24 10:43:27 +0530

    fix: replace frappe.get_all with frappe.get_list for fetching item tax withholding categories

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -1405,13 +1405,12 @@ def get_item_tax_withholding_categories(item_codes: list | str, doctype: str) ->
 	Return a mapping of {item_code: tax_withholding_category} from the Item master.
 	"""
 	item_codes = frappe.parse_json(item_codes)
-	frappe.has_permission("Item", throw=True)
 	field = (
 		"sales_tax_withholding_category" if doctype in sales_doctypes else "purchase_tax_withholding_category"
 	)
 
 	return frappe._dict(
-		frappe.get_all(
+		frappe.get_list(
 			"Item",
 			filters={"name": ["in", item_codes]},
 			fields=["name", field],
