f6e75ceb432ad59f937c9a8ad4fddb603c5c76e5
Kaajalchhattani <89331214+Kaajalchhattani@users.noreply.github.com>
2026-05-02 21:23:03 +0530

    fix: set valid_from in created Item Price (#54696)
    Co-authored-by: Kaajal-Chhattani <kaajal.chhattani@aurigait.com>
    (cherry picked from commit 6246a9aa6e713cd86aa782b530d059ba77a989d0)
    
    # Conflicts:
    #	erpnext/stock/get_item_details.py

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -1114,9 +1114,16 @@ def insert_item_price(ctx: ItemDetailsCtx):
 				"Item Price",
 				item_code=ctx.item_code,
 				price_list_rate=price_list_rate,
+<<<<<<< HEAD
 				currency=ctx.currency,
 				uom=ctx.stock_uom,
 				price_list=ctx.price_list,
+=======
+				currency=args.currency,
+				uom=args.stock_uom,
+				price_list=args.price_list,
+				valid_from=transaction_date,
+>>>>>>> 6246a9aa6e (fix: set valid_from in created Item Price (#54696))
 			)
 			item_price.insert()
 			frappe.msgprint(
@@ -1138,7 +1145,12 @@ def insert_item_price(ctx: ItemDetailsCtx):
 				"item_code": ctx.item_code,
 				"currency": ctx.currency,
 				"price_list_rate": price_list_rate,
+<<<<<<< HEAD
 				"uom": ctx.stock_uom,
+=======
+				"uom": args.stock_uom,
+				"valid_from": transaction_date,
+>>>>>>> 6246a9aa6e (fix: set valid_from in created Item Price (#54696))
 			}
 		)
 		item_price.insert()
