03aa4957b7c14921aa9f61d115a95482285d382b
Shllokkk <shllokosan23@gmail.com>
2026-05-09 18:40:34 +0530

    feat: add payment account filtering for subscription plan

--- erpnext/accounts/doctype/subscription_plan/subscription_plan.js
+++ erpnext/accounts/doctype/subscription_plan/subscription_plan.js
@@ -2,6 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Subscription Plan", {
+	setup: function (frm) {
+		if (frm.fields_dict.payment_account) {
+			frm.set_query("payment_account", function () {
+				return {
+					query: "erpnext.accounts.doctype.payment_request.payment_request.get_payment_account",
+					filters: {
+						payment_gateway: frm.doc.payment_gateway,
+					},
+				};
+			});
+		}
+	},
+
+	payment_gateway(frm) {
+		frm.set_value("payment_account", null);
+	},
+
 	price_determination: function (frm) {
 		frm.toggle_reqd("cost", frm.doc.price_determination === "Fixed rate");
 		frm.toggle_reqd("price_list", frm.doc.price_determination === "Based on price list");
