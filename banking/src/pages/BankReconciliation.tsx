08155cda517abc6d5e0c5517067cbd909c72afc1
Nikhil Kothari <nik.kothari22@live.com>
2026-05-09 16:01:08 +0530

    fix: show banner on small screens

--- banking/src/pages/BankReconciliation.tsx
+++ banking/src/pages/BankReconciliation.tsx
@@ -14,9 +14,11 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import _ from "@/lib/translate"
 import { useLayoutEffect, useRef, useState } from "react"
-import { AlertTriangleIcon, CheckCircleIcon, HomeIcon, ListIcon, ScrollTextIcon, ShuffleIcon } from "lucide-react"
+import { AlertTriangleIcon, CheckCircleIcon, HomeIcon, LandmarkIcon, ListIcon, ScrollTextIcon, ShuffleIcon } from "lucide-react"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 import { Badge } from "@/components/ui/badge"
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty"
+import { Button } from "@/components/ui/button"
 
 
 const BankReconciliation = () => {
@@ -34,67 +36,92 @@ const BankReconciliation = () => {
     const remainingHeightAfterTabs = window.innerHeight - headerHeight - 270
 
     return (
-        <div className="p-4 flex flex-col gap-4">
-            <div ref={ref} className="flex flex-col gap-4">
-                <div className="flex justify-between">
-                    <div className="flex items-center gap-6">
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem>
-                                    <a href="/desk" className="text-ink-gray-7">
-                                        <HomeIcon size={16} />
-                                    </a>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>
-                                        <div className="flex gap-1 items-center">
-                                            {_("Banking")} <Badge theme="violet" variant="subtle">{_("Beta")}</Badge>
-                                        </div>
+        <div>
+            <div className="p-4 flex-col gap-4 md:flex hidden">
+                <div ref={ref} className="flex flex-col gap-4">
+                    <div className="flex justify-between">
+                        <div className="flex items-center gap-6">
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem>
+                                        <a href="/desk" className="text-ink-gray-7">
+                                            <HomeIcon size={16} />
+                                        </a>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator />
+                                    <BreadcrumbItem>
+                                        <BreadcrumbPage>
+                                            <div className="flex gap-1 items-center">
+                                                {_("Banking")} <Badge theme="violet" variant="subtle">{_("Beta")}</Badge>
+                                            </div>
 
-                                    </BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                        <CompanySelector />
+                                        </BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                            <CompanySelector />
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <TooltipProvider>
+                                <Settings />
+                                <ActionLog />
+                            </TooltipProvider>
+                            <BankRecDateFilter />
+                        </div>
                     </div>
-                    <div className="flex items-center gap-2">
-                        <TooltipProvider>
-                            <Settings />
-                            <ActionLog />
-                        </TooltipProvider>
-                        <BankRecDateFilter />
-                    </div>
+                    <BankPicker />
+                    <BankBalance />
                 </div>
-                <BankPicker />
-                <BankBalance />
-            </div>
-            <Tabs defaultValue="Match and Reconcile">
-                <TabsList>
-                    <TabsTrigger value="Match and Reconcile"><ShuffleIcon /> {_("Match and Reconcile")}</TabsTrigger>
-                    <TabsTrigger value="Bank Reconciliation Statement"><ScrollTextIcon /> {_("Bank Reconciliation Statement")}</TabsTrigger>
-                    <TabsTrigger value="Bank Transactions"><ListIcon />{_("Bank Transactions")}</TabsTrigger>
-                    <TabsTrigger value="Bank Clearance Summary"><CheckCircleIcon />{_("Bank Clearance Summary")}</TabsTrigger>
-                    <TabsTrigger value="Incorrectly Cleared Entries"><AlertTriangleIcon /> {_("Incorrectly Cleared Entries")}</TabsTrigger>
-                </TabsList>
-                <TabsContent value="Match and Reconcile">
-                    <MatchAndReconcile contentHeight={remainingHeightAfterTabs} />
-                </TabsContent>
-                <TabsContent value="Bank Reconciliation Statement">
-                    <BankReconciliationStatement />
-                </TabsContent>
-                <TabsContent value="Bank Transactions">
-                    <BankTransactions />
-                </TabsContent>
-                <TabsContent value="Bank Clearance Summary">
-                    <BankClearanceSummary />
-                </TabsContent>
-                <TabsContent value="Incorrectly Cleared Entries">
-                    <IncorrectlyClearedEntries />
-                </TabsContent>
-            </Tabs>
+                <Tabs defaultValue="Match and Reconcile">
+                    <TabsList>
+                        <TabsTrigger value="Match and Reconcile"><ShuffleIcon /> {_("Match and Reconcile")}</TabsTrigger>
+                        <TabsTrigger value="Bank Reconciliation Statement"><ScrollTextIcon /> {_("Bank Reconciliation Statement")}</TabsTrigger>
+                        <TabsTrigger value="Bank Transactions"><ListIcon />{_("Bank Transactions")}</TabsTrigger>
+                        <TabsTrigger value="Bank Clearance Summary"><CheckCircleIcon />{_("Bank Clearance Summary")}</TabsTrigger>
+                        <TabsTrigger value="Incorrectly Cleared Entries"><AlertTriangleIcon /> {_("Incorrectly Cleared Entries")}</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="Match and Reconcile">
+                        <MatchAndReconcile contentHeight={remainingHeightAfterTabs} />
+                    </TabsContent>
+                    <TabsContent value="Bank Reconciliation Statement">
+                        <BankReconciliationStatement />
+                    </TabsContent>
+                    <TabsContent value="Bank Transactions">
+                        <BankTransactions />
+                    </TabsContent>
+                    <TabsContent value="Bank Clearance Summary">
+                        <BankClearanceSummary />
+                    </TabsContent>
+                    <TabsContent value="Incorrectly Cleared Entries">
+                        <IncorrectlyClearedEntries />
+                    </TabsContent>
+                </Tabs>
 
-            <BankTransactionUnreconcileModal />
+                <BankTransactionUnreconcileModal />
+            </div>
+            <div className="md:hidden flex h-screen items-center justify-between">
+                <Empty>
+                    <EmptyMedia>
+                        <LandmarkIcon />
+                    </EmptyMedia>
+                    <EmptyHeader>
+                        <EmptyTitle>
+                            {_("Banking")}
+                        </EmptyTitle>
+                        <EmptyDescription>
+                            {_("This screen is not supported on mobile devices.")}
+                        </EmptyDescription>
+                    </EmptyHeader>
+                    <EmptyContent>
+                        <Button asChild>
+                            <a href="/desk">
+                                {_("Go to Desktop")}
+                            </a>
+                        </Button>
+                    </EmptyContent>
+                </Empty>
+
+            </div>
         </div>
     )
 }
