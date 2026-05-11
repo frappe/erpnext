b61d9789495cbfa962cbfab0125fc22dfe41094b
ruthra kumar <ruthra@erpnext.com>
2026-05-11 11:55:56 +0530

    ci: better heuristic for splitting chunks

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -11,8 +11,10 @@ sudo apt install libcups2-dev redis-server mariadb-client libmariadb-dev
 pip install frappe-bench
 
 githubbranch=${GITHUB_BASE_REF:-${GITHUB_REF##*/}}
-frappeuser=${FRAPPE_USER:-"frappe"}
-frappecommitish=${FRAPPE_BRANCH:-$githubbranch}
+# frappeuser=${FRAPPE_USER:-"frappe"}
+frappeuser="ruthra-kumar"
+# frappecommitish=${FRAPPE_BRANCH:-$githubbranch}
+frappecommitish="use_line_count_as_weightage_heuristic"
 
 mkdir frappe
 pushd frappe
