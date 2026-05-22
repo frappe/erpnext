f9893152b666f06b32b49177ff2f3923dccde852
Loïc Oberle <loic@dokos.io>
2026-05-22 15:00:14 +0200

    refactor(sales_order): Replace SQL with ORM in check_modified_date
    Replace SQL with ORM to ensure compatibility with postgreSQL
    
    Contribution made on behalf of Orange SA

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -597,8 +597,7 @@ class SalesOrder(SellingController):
 
 	def check_modified_date(self):
 		mod_db = frappe.db.get_value("Sales Order", self.name, "modified")
-		date_diff = frappe.db.sql(f"select TIMEDIFF('{mod_db}', '{cstr(self.modified)}')")
-		if date_diff and date_diff[0][0]:
+		if mod_db and cstr(mod_db) != cstr(self.modified):
 			frappe.throw(_("{0} {1} has been modified. Please refresh.").format(self.doctype, self.name))
 
 	def update_status(self, status):
