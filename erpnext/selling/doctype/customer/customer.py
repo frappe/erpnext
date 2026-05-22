dc3a17aec2eeed5b79467a7ce84498147d59f697
Loïc Oberle <loic@dokos.io>
2026-05-22 16:34:13 +0200

    refactor(customer): replace SQL with ORM in on_trash
    Replace SQL with ORM to ensure compatibily with postgreSQL
    
    Contribution made on behalf of Orange SA

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -415,7 +415,7 @@ class Customer(TransactionBase):
 
 		delete_contact_and_address("Customer", self.name)
 		if self.lead_name:
-			frappe.db.sql("update `tabLead` set status='Interested' where name=%s", self.lead_name)
+			frappe.db.set_value("Lead", self.lead_name, "status", "Interested")
 
 	def before_rename(self, olddn, newdn, merge=False):
 		if merge:
