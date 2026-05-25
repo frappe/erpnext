4436585aa07a82ab3704d8091fd99482854aa854
Nihantra Patel <nihantra@frappe.io>
2026-05-25 21:39:54 +0530

    fix: use passed posting date in make_reverse_gl_entries
    (cherry picked from commit f040bdf1656545362d86bda44c8986274124622a)

--- erpnext/accounts/doctype/period_closing_voucher/test_period_closing_voucher.py
+++ erpnext/accounts/doctype/period_closing_voucher/test_period_closing_voucher.py
@@ -10,6 +10,7 @@ from frappe.utils import today
 from erpnext.accounts.doctype.finance_book.test_finance_book import create_finance_book
 from erpnext.accounts.doctype.journal_entry.test_journal_entry import make_journal_entry
 from erpnext.accounts.doctype.sales_invoice.test_sales_invoice import create_sales_invoice
+from erpnext.accounts.general_ledger import make_reverse_gl_entries
 from erpnext.accounts.utils import get_fiscal_year
 
 
@@ -350,6 +351,65 @@ class TestPeriodClosingVoucher(unittest.TestCase):
 			pcv.submit()
 
 		return pcv
+
+	@ERPNextTestSuite.change_settings(
+		"Accounts Settings",
+		{"enable_immutable_ledger": 1},
+	)
+	def test_immutable_ledger_reverse_entry_uses_passed_posting_date_after_pcv(self):
+		company = create_company()
+		cost_center = create_cost_center("Test Cost Center 1")
+
+		jv = make_journal_entry(
+			posting_date="2021-03-15",
+			amount=400,
+			account1="Cash - TPC",
+			account2="Sales - TPC",
+			cost_center=cost_center,
+			company=company,
+			save=False,
+		)
+		jv.company = company
+		jv.save()
+		jv.submit()
+
+		self.make_period_closing_voucher(posting_date="2021-03-31")
+
+		totals_before_cancel = frappe.db.sql(
+			"""
+				select sum(debit) as total_debit, sum(credit) as total_credit
+				from `tabGL Entry`
+				where voucher_type=%s and voucher_no=%s and is_cancelled=0
+			""",
+			("Journal Entry", jv.name),
+			as_dict=True,
+		)[0]
+
+		# Passed posting_date is after PCV end date, so cancellation should not fail.
+		make_reverse_gl_entries(
+			voucher_type="Journal Entry",
+			voucher_no=jv.name,
+			posting_date="2022-01-01",
+		)
+
+		totals_after_cancel = frappe.db.sql(
+			"""
+				select sum(debit) as total_debit, sum(credit) as total_credit
+				from `tabGL Entry`
+				where voucher_type=%s and voucher_no=%s and is_cancelled=0
+			""",
+			("Journal Entry", jv.name),
+			as_dict=True,
+		)[0]
+
+		self.assertEqual(
+			totals_after_cancel.total_debit,
+			totals_before_cancel.total_debit * 2,
+		)
+		self.assertEqual(
+			totals_after_cancel.total_credit,
+			totals_before_cancel.total_credit * 2,
+		)
 
 
 def create_company():
