bd4c24493ceee8f754b8345ec2ab88a16e6559db
Rohit Waghchaure <rohitw1991@gmail.com>
2026-05-26 13:59:25 +0530

    fix: inclusive tax amount not considered while setting LCV from purchase invoice
    (cherry picked from commit 048ddfc265b2f7a031f8eada42fafc5870d19747)

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -1374,7 +1374,7 @@ def get_billed_qty_amount_against_purchase_receipt(pr_doc):
 		.on(parent_table.name == table.parent)
 		.select(
 			table.pr_detail,
-			fn.Sum(table.amount * parent_table.conversion_rate).as_("amount"),
+			fn.Sum(table.base_net_amount).as_("amount"),
 			fn.Sum(table.qty).as_("qty"),
 		)
 		.where((table.pr_detail.isin(pr_names)) & (table.docstatus == 1))
@@ -1420,7 +1420,7 @@ def get_billed_qty_amount_against_purchase_order(pr_doc):
 			.select(
 				table.po_detail,
 				fn.Sum(table.qty).as_("qty"),
-				fn.Sum(table.amount * parent_table.conversion_rate).as_("amount"),
+				fn.Sum(table.base_net_amount).as_("amount"),
 			)
 			.where((table.po_detail.isin(po_names)) & (table.docstatus == 1) & (table.pr_detail.isnull()))
 			.groupby(table.po_detail)
