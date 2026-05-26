3129cc6ec870df4971f46c5ea71e632eb827065c
Loïc Oberle <loic@dokos.io>
2026-05-26 09:35:15 +0200

    refactor(customer): fix use of function
    fix the use on a nonexistant function

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -677,10 +677,10 @@ def send_emails(
 
 
 def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=False, cost_center=None):
-	from frappe.query_builder.functions import Sum
+	from frappe.query_builder import Criterion
+	from frappe.query_builder.functions import Coalesce, IfNull, Sum
 
 	GLEntry = frappe.qb.DocType("GL Entry")
-
 	gle_query = (
 		frappe.qb.from_(GLEntry)
 		.select(Sum(GLEntry.debit) - Sum(GLEntry.credit))
@@ -693,7 +693,6 @@ def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=F
 	if cost_center:
 		lft, rgt = frappe.get_cached_value("Cost Center", cost_center, ["lft", "rgt"])
 		CostCenter = frappe.qb.DocType("Cost Center")
-
 		cost_center_subquery = (
 			frappe.qb.from_(CostCenter)
 			.select(CostCenter.name)
@@ -703,13 +702,11 @@ def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=F
 		gle_query = gle_query.where(GLEntry.cost_center.isin(cost_center_subquery))
 
 	gle_res = gle_query.run()
-	outstanding_based_on_gle = flt(gle_res[0][0]) if gle_res else 0.0
+	outstanding_based_on_gle = flt(gle_res[0][0]) if gle_res and gle_res[0][0] is not None else 0.0
 
 	outstanding_based_on_so = 0.0
-
 	if not ignore_outstanding_sales_order:
 		SalesOrder = frappe.qb.DocType("Sales Order")
-
 		so_query = (
 			frappe.qb.from_(SalesOrder)
 			.select(Sum(SalesOrder.base_grand_total * (100 - SalesOrder.per_billed) / 100))
@@ -719,59 +716,58 @@ def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=F
 			.where(SalesOrder.per_billed < 100)
 			.where(SalesOrder.status != "Closed")
 		)
-
 		so_res = so_query.run()
-		outstanding_based_on_so = flt(so_res[0][0]) if so_res else 0.0
-
-	outstanding_based_on_dn = 0.0
+		outstanding_based_on_so = flt(so_res[0][0]) if so_res and so_res[0][0] is not None else 0.0
 
 	DeliveryNote = frappe.qb.DocType("Delivery Note")
 	DeliveryNoteItem = frappe.qb.DocType("Delivery Note Item")
+	SalesInvoiceItem = frappe.qb.DocType("Sales Invoice Item")
+
+	si_subquery = (
+		frappe.qb.from_(SalesInvoiceItem)
+		.select(SalesInvoiceItem.dn_detail, Sum(SalesInvoiceItem.amount).as_("billed_amount"))
+		.where(SalesInvoiceItem.docstatus == 1)
+		.groupby(SalesInvoiceItem.dn_detail)
+	)
 
 	dn_query = (
 		frappe.qb.from_(DeliveryNote)
 		.join(DeliveryNoteItem)
 		.on(DeliveryNote.name == DeliveryNoteItem.parent)
+		.left_join(si_subquery)
+		.on(DeliveryNoteItem.name == si_subquery.dn_detail)
 		.select(
-			DeliveryNoteItem.name,
-			DeliveryNoteItem.amount,
-			DeliveryNote.base_net_total,
-			DeliveryNote.base_grand_total,
+			Sum(
+				(
+					(DeliveryNoteItem.amount - IfNull(si_subquery.billed_amount, 0.0))
+					/ DeliveryNote.base_net_total
+				)
+				* DeliveryNote.base_grand_total
+			)
 		)
 		.where(DeliveryNote.customer == customer)
 		.where(DeliveryNote.company == company)
 		.where(DeliveryNote.docstatus == 1)
+		.where(DeliveryNote.base_net_total > 0)
 		.where(DeliveryNote.status.notin(["Closed", "Stopped"]))
-		.where(DeliveryNoteItem.against_sales_order.convert_empty_to_null().isnull())
-		.where(DeliveryNoteItem.against_sales_invoice.convert_empty_to_null().isnull())
+		.where(DeliveryNoteItem.amount > IfNull(si_subquery.billed_amount, 0.0))
+		.where(
+			Criterion.any(
+				[DeliveryNoteItem.against_sales_order.isnull(), DeliveryNoteItem.against_sales_order == ""]
+			)
+		)
+		.where(
+			Criterion.any(
+				[
+					DeliveryNoteItem.against_sales_invoice.isnull(),
+					DeliveryNoteItem.against_sales_invoice == "",
+				]
+			)
+		)
 	)
 
-	unmarked_delivery_note_items = dn_query.run(as_dict=True)
-
-	if not unmarked_delivery_note_items:
-		return outstanding_based_on_gle + outstanding_based_on_so
-
-	dn_item_names = [d.name for d in unmarked_delivery_note_items]
-
-	SalesInvoiceItem = frappe.qb.DocType("Sales Invoice Item")
-	si_query = (
-		frappe.qb.from_(SalesInvoiceItem)
-		.select(SalesInvoiceItem.dn_detail, Sum(SalesInvoiceItem.amount))
-		.where(SalesInvoiceItem.docstatus == 1)
-		.where(SalesInvoiceItem.dn_detail.isin(dn_item_names))
-		.groupby(SalesInvoiceItem.dn_detail)
-	)
-
-	si_amounts = {row[0]: row[1] for row in si_query.run()}
-
-	for dn_item in unmarked_delivery_note_items:
-		dn_amount = flt(dn_item.amount)
-		si_amount = flt(si_amounts.get(dn_item.name, 0.0))
-
-		if dn_amount > si_amount and dn_item.base_net_total:
-			outstanding_based_on_dn += (
-				(dn_amount - si_amount) / dn_item.base_net_total
-			) * dn_item.base_grand_total
+	dn_res = dn_query.run()
+	outstanding_based_on_dn = flt(dn_res[0][0]) if dn_res and dn_res[0][0] is not None else 0.0
 
 	return outstanding_based_on_gle + outstanding_based_on_so + outstanding_based_on_dn
 
