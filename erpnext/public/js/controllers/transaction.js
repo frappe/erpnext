1679680d8e2b62a65e5475cae99ee5329b1ce4d5
Sudharsanan11 <sudharsananashok1975@gmail.com>
2026-05-29 18:06:32 +0530

    fix(stock): add warning message to notify the user to configure the inspection
    (cherry picked from commit e003fe4de0b9fa59a054bfa348837d8ba1acf9ab)

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -2934,6 +2934,23 @@ erpnext.TransactionController = class TransactionController extends erpnext.taxe
 			},
 			freeze: true,
 			callback: function (r) {
+				if (r.message.length == 0) {
+					let type = inspection_type === "Incoming" ? "Purchase" : "Delivery";
+					let fieldname =
+						inspection_type === "Incoming"
+							? "Inspection Required before Purchase"
+							: "Inspection Required before Delivery";
+
+					frappe.msgprint({
+						title: __("Quality Inspection Not Configured"),
+						message: __(`Enable <b>{0}</b> on the Item master to proceed with {1} inspection.`, [
+							fieldname,
+							type,
+						]),
+					});
+					return;
+				}
+
 				r.message.forEach((item) => {
 					if (me.has_inspection_required(item)) {
 						let dialog_items = dialog.fields_dict.items;
