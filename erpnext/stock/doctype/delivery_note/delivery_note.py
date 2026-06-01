0b8dd4278671e0d57ce34932a53600c552709caf
ljain112 <ljain112@gmail.com>
2026-06-01 12:37:23 +0530

    fix: remove redundant field mapping for shipping address in inter-company transaction

--- erpnext/stock/doctype/delivery_note/delivery_note.py
+++ erpnext/stock/doctype/delivery_note/delivery_note.py
@@ -1391,7 +1391,6 @@ def make_inter_company_transaction(doctype, source_name, target_doc=None):
 				"doctype": target_doctype,
 				"postprocess": update_details,
 				"field_no_map": [*CROSS_PARTY_FIELD_NO_MAP, "set_warehouse"],
-				"field_map": {"shipping_address_name": "shipping_address"},
 			},
 			doctype + " Item": {
 				"doctype": target_doctype + " Item",
