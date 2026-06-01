54cbc9116613fa5f969d9acde607929a42cf94ea
rohitwaghchaure <rohitw1991@gmail.com>
2026-06-01 22:11:58 +0530

    chore: fixed conflicts

--- erpnext/controllers/stock_controller.py
+++ erpnext/controllers/stock_controller.py
@@ -1680,11 +1680,7 @@ def repost_required_for_queue(doc: StockController) -> bool:
 
 
 @frappe.whitelist()
-<<<<<<< HEAD
-def check_item_quality_inspection(doctype, items):
-=======
 def check_item_quality_inspection(doctype: str, docstatus: str | int, items: str | list[dict]):
->>>>>>> c6a88ab1d2 (fix(stock): allow to create quality inspection after purchase/delivery)
 	if isinstance(items, str):
 		items = json.loads(items)
 
