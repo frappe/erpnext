0984c865834d89cadafd81f9c71eca7a8eaa2be2
Pandiyan P <pandiyanpalani37@gmail.com>
2026-06-03 13:11:50 +0530

    fix(accounts): include asset items in purchase receipt validation (#55150)
    (cherry picked from commit d0d9411700a44027667f6a036c7d2f986fe3e343)

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -635,15 +635,16 @@ class PurchaseInvoice(BuyingController):
 					throw(msg, title=_("Mandatory Purchase Order"))
 
 	def pr_required(self):
-		stock_items = self.get_stock_items()
 		if frappe.db.get_single_value("Buying Settings", "pr_required") == "Yes":
+			stock_and_asset_items = self.get_stock_items()
+			stock_and_asset_items.extend(self.get_asset_items())
 			if frappe.get_value(
 				"Supplier", self.supplier, "allow_purchase_invoice_creation_without_purchase_receipt"
 			):
 				return
 
 			for d in self.get("items"):
-				if not d.purchase_receipt and d.item_code in stock_items:
+				if not d.purchase_receipt and d.item_code in stock_and_asset_items:
 					msg = _("Purchase Receipt Required for item {}").format(frappe.bold(d.item_code))
 					msg += "<br><br>"
 					msg += _(
