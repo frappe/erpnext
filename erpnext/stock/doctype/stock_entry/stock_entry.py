f510f36139deadb5fbf63572b3261db75da9c11a
Mihir Kandoi <kandoimihir@gmail.com>
2026-06-08 12:09:22 +0530

    chore: resolve conflicts

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -713,7 +713,6 @@ class StockEntry(StockController, SubcontractingInwardController):
 
 		projects = set(item.project for item in self.items if item.project)
 		for project in projects:
-<<<<<<< HEAD
 			amount = frappe.db.sql(
 				""" select ifnull(sum(amount), 0)
 				from
@@ -742,12 +741,7 @@ class StockEntry(StockController, SubcontractingInwardController):
 			amount += additional_cost_amt
 			project = frappe.get_doc("Project", project)
 			project.total_consumed_material_cost = amount
-			project.save()
-=======
-			project_doc = frappe.get_doc("Project", project)
-			project_doc.set_consumed_material_cost()
-			project_doc.save(ignore_permissions=True)
->>>>>>> 4b0b7adeee (fix: bypass project permission check when updating consumed material cost)
+			project.save(ignore_permissions=True)
 
 	def validate_item(self):
 		stock_items = self.get_stock_items()
