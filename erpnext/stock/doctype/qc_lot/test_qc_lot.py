d97126e653b1ecabcd3c69e53d60dd13b3c34352
Mihir Kandoi <kandoimihir@gmail.com>
2026-06-10 06:26:41 +0530

    test(quality): cover QC Lot status derivation

---
+++ erpnext/stock/doctype/qc_lot/test_qc_lot.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+
+from erpnext.stock.doctype.item.test_item import make_item
+from erpnext.stock.services.test_quality_warehouse import ensure_quality_warehouse_type, make_warehouse
+from erpnext.tests.utils import ERPNextTestSuite
+
+
+def qc_warehouse():
+	ensure_quality_warehouse_type()
+	return make_warehouse("_Test QC Lot WH", warehouse_type="Quality")
+
+
+def make_qc_lot(received_qty=10, accepted_qty=0, rejected_qty=0):
+	return frappe.get_doc(
+		{
+			"doctype": "QC Lot",
+			"item_code": make_item(properties={"is_stock_item": 1}).name,
+			"company": "_Test Company",
+			"quality_warehouse": qc_warehouse(),
+			"received_qty": received_qty,
+			"accepted_qty": accepted_qty,
+			"rejected_qty": rejected_qty,
+		}
+	).insert(ignore_permissions=True)
+
+
+class TestQCLot(ERPNextTestSuite):
+	def test_status_under_inspection(self):
+		lot = make_qc_lot()
+		self.assertEqual(lot.status, "Under Inspection")
+		self.assertEqual(lot.pending_qty, 10)
+
+	def test_status_partially_released(self):
+		lot = make_qc_lot(accepted_qty=4)
+		self.assertEqual(lot.status, "Partially Released")
+		self.assertEqual(lot.pending_qty, 6)
+
+	def test_status_released_when_fully_resolved(self):
+		lot = make_qc_lot(accepted_qty=8, rejected_qty=2)
+		self.assertEqual(lot.status, "Released")
+		self.assertEqual(lot.pending_qty, 0)
+
+	def test_status_rejected_when_all_rejected(self):
+		lot = make_qc_lot(rejected_qty=10)
+		self.assertEqual(lot.status, "Rejected")
+		self.assertEqual(lot.pending_qty, 0)
