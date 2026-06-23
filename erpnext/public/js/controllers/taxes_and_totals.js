e943e2a01ee134303668e6e435d3a21850cf7361
Harsh Patadia <harsh@Harshs-MacBook-Air.local>
2026-06-23 10:07:46 +0530

    fix: revert back a faulty condition logic

--- erpnext/public/js/controllers/taxes_and_totals.js
+++ erpnext/public/js/controllers/taxes_and_totals.js
@@ -17,7 +17,7 @@ erpnext.taxes_and_totals = class TaxesAndTotals extends erpnext.payments {
 		if (item.margin_type == "Percentage") {
 			item.rate_with_margin =
 				flt(effective_item_rate) + flt(effective_item_rate) * (flt(item.margin_rate_or_amount) / 100);
-		} else if (item.margin_type == "Amount") {
+		} else {
 			item.rate_with_margin = flt(effective_item_rate) + flt(item.margin_rate_or_amount);
 		}
 		item.base_rate_with_margin = flt(item.rate_with_margin) * flt(this.frm.doc.conversion_rate);
