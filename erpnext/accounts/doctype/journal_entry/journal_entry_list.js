c89f9720bca79de8079a470c7c28ce0ba9b4115b
ruthra kumar <ruthra@erpnext.com>
2026-07-10 11:53:46 +0530

    chore: resolve conflicts

--- erpnext/accounts/doctype/journal_entry/journal_entry_list.js
+++ erpnext/accounts/doctype/journal_entry/journal_entry_list.js
@@ -1,22 +1,15 @@
 frappe.listview_settings["Journal Entry"] = {
-<<<<<<< HEAD
-	add_fields: ["voucher_type", "posting_date", "total_debit", "company", "user_remark"],
+	add_fields: ["voucher_type", "posting_date", "total_debit", "company", "user_remark", "reversal_of"],
 	get_indicator: function (doc) {
 		if (doc.docstatus == 0) {
 			return [__("Draft", "red", "docstatus,=,0")];
 		} else if (doc.docstatus == 2) {
 			return [__("Cancelled", "grey", "docstatus,=,2")];
-		} else {
-			return [__(doc.voucher_type), "blue", "voucher_type,=," + doc.voucher_type];
-=======
-	add_fields: ["voucher_type", "posting_date", "total_debit", "company", "remark", "reversal_of"],
-	get_indicator: function (doc) {
-		if (doc.docstatus === 1) {
+		} else if (doc.docstatus === 1) {
 			if (doc.reversal_of && doc.voucher_type == "Exchange Rate Revaluation") {
 				return [__("Reversal Of Exchange Rate Revaluation"), "blue"];
 			}
 			return [__(doc.voucher_type), "blue", `voucher_type,=,${doc.voucher_type}`];
->>>>>>> a0b14c0607 (refactor: reversal capability on exchange rate revaluation)
 		}
 	},
 };
