61ba231fc791a5983debdf0a89c38a33a0a5aee1
diptanilsaha <diptanil@frappe.io>
2026-07-14 16:44:03 +0530

    fix: patch to add docshare for the project users

---
+++ erpnext/patches/v16_0/access_control_for_project_users.py
@@ -0,0 +1,21 @@
+import frappe
+
+
+def execute():
+	Project = frappe.qb.DocType("Project")
+	ProjectUser = frappe.qb.DocType("Project User")
+
+	query = (
+		frappe.qb.from_(Project)
+		.join(ProjectUser)
+		.on(Project.name == ProjectUser.parent)
+		.select(Project.name, ProjectUser.email)
+	)
+
+	proj_user = query.run(as_dict=1)
+
+	for d in proj_user:
+		if frappe.has_permission("Project", doc=d.name, user=d.user):
+			continue
+
+		frappe.share.add_docshare("Project", d.name, user=d.user)
