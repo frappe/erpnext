89650de896319074ede8cda8ed0f0f37a1fc5ed0
diptanilsaha <diptanil@frappe.io>
2026-07-14 16:16:17 +0530

    fix(project): enabled project access control for users without `Projects User` Role

--- erpnext/projects/doctype/project/project.py
+++ erpnext/projects/doctype/project/project.py
@@ -91,6 +91,7 @@ class Project(Document):
 		if not self.is_new():
 			self.copy_from_template()
 		self.send_welcome_email()
+		self.control_access_for_project_users()
 		self.update_costing()
 		self.update_percent_complete()
 		self.validate_from_to_dates("expected_start_date", "expected_end_date")
@@ -433,6 +434,33 @@ class Project(Document):
 						content=content,
 					)
 					user.welcome_email_sent = 1
+
+	def control_access_for_project_users(self):
+		def revoke_access_for_project_users(removed_users):
+			users = set([d.user for d in frappe.share.get_users(self.doctype, self.name)])
+			for user in removed_users:
+				if user not in users:
+					continue
+
+				frappe.share.remove(self.doctype, self.name, user)
+
+		def grant_access_for_project_users(new_users):
+			for user in new_users:
+				if self.has_permission(user=user):
+					continue
+
+				frappe.share.add_docshare(self.doctype, self.name, user=user)
+
+		old_doc = self.get_doc_before_save()
+
+		previous_users = set([d.user for d in old_doc.users])
+		current_users = set([d.user for d in self.users])
+
+		new_users = current_users - previous_users
+		removed_users = previous_users - current_users
+
+		revoke_access_for_project_users(removed_users)
+		grant_access_for_project_users(new_users)
 
 
 def get_timeline_data(doctype: str, name: str) -> dict[int, int]:
