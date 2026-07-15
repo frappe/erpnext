09d721d1be80eca48edbc6ed52676febc329cc90
diptanilsaha <diptanil@frappe.io>
2026-07-16 00:47:56 +0530

    fix(assets): add permission checks on whitelisted methods on `asset_capitalization`

--- erpnext/assets/doctype/asset_capitalization/asset_capitalization.py
+++ erpnext/assets/doctype/asset_capitalization/asset_capitalization.py
@@ -539,11 +539,13 @@ def get_target_asset_details(asset: str | None = None, company: str | None = Non
 @frappe.whitelist()
 @erpnext.normalize_ctx_input(ItemDetailsCtx)
 def get_consumed_stock_item_details(ctx: ItemDetailsCtx):
+	frappe.has_permission("Stock Ledger Entry", throw=True)
 	out = frappe._dict()
 
 	item = frappe._dict()
 	if ctx.item_code:
 		item = frappe.get_cached_doc("Item", ctx.item_code)
+		item.check_permission()
 
 	out.item_name = item.item_name
 	out.batch_no = None
@@ -553,6 +555,8 @@ def get_consumed_stock_item_details(ctx: ItemDetailsCtx):
 	out.stock_uom = item.stock_uom
 
 	out.warehouse = get_item_warehouse_(ctx, item, overwrite_warehouse=True) if item else None
+	if out.warehouse:
+		frappe.has_permission("Warehouse", doc=out.warehouse, throw=True)
 
 	# Cost Center
 	item_defaults = get_item_defaults(item.name, ctx.company)
@@ -589,6 +593,9 @@ def get_consumed_stock_item_details(ctx: ItemDetailsCtx):
 def get_warehouse_details(ctx: ItemDetailsCtx) -> frappe._dict:
 	out = frappe._dict()
 	if ctx.warehouse and ctx.item_code:
+		frappe.has_permission("Item", doc=ctx.item_code, throw=True)
+		frappe.has_permission("Warehouse", doc=ctx.warehouse, throw=True)
+		frappe.has_permission("Stock Ledger Entry", throw=True)
 		out = frappe._dict(
 			{
 				"actual_qty": get_previous_sle(ctx).get("qty_after_transaction") or 0,
