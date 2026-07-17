1fabf78dea0bc68ded4347bd783e5bb20235c649
Mihir Kandoi <kandoimihir@gmail.com>
2026-07-17 21:24:40 +0530

    chore: remove unneccessary flt

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -202,7 +202,7 @@ class MaterialRequest(BuyingController):
 				item.item_code,
 			)
 			if rate is not None:
-				item.db_set({"rate": flt(rate), "amount": flt(flt(rate) * item.qty, item.precision("amount"))})
+				item.db_set({"rate": rate, "amount": flt(rate * item.qty, item.precision("amount"))})
 
 		frappe.msgprint(
 			_("Item rates have been updated based on the selected Buying Price List {0}").format(
