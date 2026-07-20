7a8db06b9a59a6ff90f4037fb4ec2122df5a22c3
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-07-20 13:42:07 +0530

    test(territory): cover root and child parent assignment

--- erpnext/setup/doctype/territory/test_territory.py
+++ erpnext/setup/doctype/territory/test_territory.py
@@ -1,3 +1,20 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 
+import frappe
+from frappe.utils.nestedset import get_root_of
+
+from erpnext.tests.utils import ERPNextTestSuite
+
+
+class TestTerritory(ERPNextTestSuite):
+	def test_root_is_not_set_as_its_own_parent(self):
+		root = frappe.get_doc("Territory", get_root_of("Territory"))
+		root.save()
+
+		self.assertFalse(root.parent_territory)
+
+	def test_territory_without_parent_is_placed_under_root(self):
+		territory = frappe.get_doc({"doctype": "Territory", "territory_name": "Western Europe"}).insert()
+
+		self.assertEqual(territory.parent_territory, get_root_of("Territory"))
