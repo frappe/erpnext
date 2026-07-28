7f60bd8d6313308c5f101bb77477598712240f5f
diptanilsaha <diptanil@frappe.io>
2026-07-28 10:38:00 +0530

    fix: add permission check for `get_item_details`

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -86,6 +86,7 @@ def get_item_details(
 	for_validate = parse_json(for_validate)
 	overwrite_warehouse = parse_json(overwrite_warehouse)
 	item = frappe.get_cached_doc("Item", ctx.item_code)
+	item.check_permission()
 	validate_item_details(ctx, item)
 
 	doc = frappe.parse_json(doc)
