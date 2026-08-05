41b2d7fa1a03f7ea2a1237637124182929148641
ervishnucs <ervishnucs369@gmail.com>
2026-08-06 01:58:20 +0530

    test(accounts): cover missing Scheduler Event on auto reconcile trigger change

--- erpnext/accounts/doctype/accounts_settings/test_accounts_settings.py
+++ erpnext/accounts/doctype/accounts_settings/test_accounts_settings.py
@@ -13,3 +13,30 @@ class TestAccountsSettings(ERPNextTestSuite):
 
 		cur_settings.stale_days = -1
 		self.assertRaises(frappe.ValidationError, cur_settings.save)
+
+	def test_auto_reconciliation_job_trigger_creates_missing_scheduler_event(self):
+		method = (
+			"erpnext.accounts.doctype.process_payment_reconciliation."
+			"process_payment_reconciliation.trigger_reconciliation_for_queued_docs"
+		)
+		frappe.db.delete("Scheduled Job Type", {"method": method})
+		frappe.db.delete("Scheduler Event", {"method": method})
+
+		cur_settings = frappe.get_doc("Accounts Settings", "Accounts Settings")
+		self.addCleanup(self._restore_trigger, cur_settings.auto_reconciliation_job_trigger)
+
+		cur_settings.auto_reconciliation_job_trigger = 25
+		cur_settings.save()
+
+		self.assertTrue(
+			frappe.db.exists(
+				"Scheduler Event", {"scheduled_against": "Process Payment Reconciliation", "method": method}
+			)
+		)
+		self.assertEqual(
+			frappe.db.get_value("Scheduled Job Type", {"method": method}, "cron_format"), "0/25 * * * *"
+		)
+
+	@staticmethod
+	def _restore_trigger(value):
+		frappe.db.set_single_value("Accounts Settings", "auto_reconciliation_job_trigger", value)
