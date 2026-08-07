c662b3cf2e5aa5251dc749d7cd06d9eadc212316
ervishnucs <ervishnucs369@gmail.com>
2026-08-07 16:10:48 +0530

    fix(reports): respect user permissions in non-billed reports

--- erpnext/accounts/report/non_billed_report.py
+++ erpnext/accounts/report/non_billed_report.py
@@ -3,15 +3,17 @@
 
 
 import frappe
+from frappe.desk.reportview import build_match_conditions
 from frappe.model.meta import get_field_precision
 from frappe.query_builder.functions import IfNull, Round
+from pypika.terms import Bracket, LiteralValue
 
 from erpnext import get_default_currency
 
 
 def get_ordered_to_be_billed_data(args, filters=None):
-	doctype, party = args.get("doctype"), args.get("party")
-	child_tab = doctype + " Item"
+	doctype_name, party = args.get("doctype"), args.get("party")
+	child_tab = doctype_name + " Item"
 	precision = (
 		get_field_precision(
 			frappe.get_meta(child_tab).get_field("billed_amt"), currency=get_default_currency()
@@ -19,7 +21,7 @@ def get_ordered_to_be_billed_data(args, filters=None):
 		or 2
 	)
 
-	doctype = frappe.qb.DocType(doctype)
+	doctype = frappe.qb.DocType(doctype_name)
 	child_doctype = frappe.qb.DocType(child_tab)
 	item = frappe.qb.DocType("Item")
 
@@ -71,7 +73,44 @@ def get_ordered_to_be_billed_data(args, filters=None):
 	if docname:
 		query = query.where(doctype.name == docname)
 
+	if match_conditions := build_match_conditions(doctype_name):
+		query = query.where(Bracket(LiteralValue(match_conditions)))
+
+	if condition := get_child_permission_conditions(child_doctype, child_tab):
+		query = query.where(condition)
+
 	return query.run(as_dict=True)
+
+
+def get_child_permission_conditions(child_table, child_doctype_name):
+	user_permissions = frappe.permissions.get_user_permissions(frappe.session.user)
+	if not user_permissions:
+		return None
+
+	strict_user_permissions = frappe.get_system_settings("apply_strict_user_permissions")
+	condition = None
+
+	for df in frappe.get_meta(child_doctype_name).get_link_fields():
+		if df.get("ignore_user_permissions"):
+			continue
+
+		permitted_docs = [
+			d.get("doc")
+			for d in user_permissions.get(df.get("options"), [])
+			if not d.get("applicable_for") or d.get("applicable_for") == child_doctype_name
+		]
+		if not permitted_docs:
+			continue
+
+		field = child_table[df.get("fieldname")]
+		field_condition = (
+			field.isin(permitted_docs)
+			if strict_user_permissions
+			else (IfNull(field, "") == "") | field.isin(permitted_docs)
+		)
+		condition = field_condition if condition is None else condition & field_condition
+
+	return condition
 
 
 def get_project_field(doctype, child_doctype, party):
