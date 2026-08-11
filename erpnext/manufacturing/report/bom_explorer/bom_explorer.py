b4929e273703a21f0885b4e960f3101748ff1248
Mihir Kandoi <kandoimihir@gmail.com>
2026-08-11 13:00:13 +0530

    fix(manufacturing): correct nested BOM quantities

--- erpnext/manufacturing/report/bom_explorer/bom_explorer.py
+++ erpnext/manufacturing/report/bom_explorer/bom_explorer.py
@@ -24,7 +24,7 @@ def get_exploded_items(bom, data, indent=0, qty=1):
 		fields=[
 			"qty",
 			"bom_no",
-			"qty",
+			"stock_qty",
 			"item_code",
 			"item_name",
 			"description",
@@ -51,7 +51,13 @@ def get_exploded_items(bom, data, indent=0, qty=1):
 			}
 		)
 		if item.bom_no:
-			get_exploded_items(item.bom_no, data, indent=indent + 1, qty=item.qty)
+			child_bom_qty = frappe.get_cached_value("BOM", item.bom_no, "quantity")
+			get_exploded_items(
+				item.bom_no,
+				data,
+				indent=indent + 1,
+				qty=qty * item.stock_qty / child_bom_qty,
+			)
 
 
 def get_columns():
