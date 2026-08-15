c6ed31178bf44159caecbb2b406effa0a7739ec6
diptanilsaha <diptanil@frappe.io>
2026-08-15 19:29:53 +0530

    fix(email_digest): show alerts to save email digest if the form has unsaved changes on triggering actions

--- erpnext/setup/doctype/email_digest/email_digest.js
+++ erpnext/setup/doctype/email_digest/email_digest.js
@@ -5,6 +5,14 @@ frappe.ui.form.on("Email Digest", {
 	refresh: function (frm) {
 		if (!frm.is_new()) {
 			frm.add_custom_button(__("View Now"), function () {
+				if (frm.is_dirty()) {
+					frappe.show_alert({
+						message: __("Save the Email Digest first.", "yellow"),
+						indicator: "yellow",
+					});
+					return;
+				}
+
 				frm.call("get_digest_msg").then((r) => {
 					let d = new frappe.ui.Dialog({
 						title: __("Email Digest: {0}", [frm.doc.name]),
@@ -16,6 +24,14 @@ frappe.ui.form.on("Email Digest", {
 			});
 
 			frm.add_custom_button(__("Send Now"), function () {
+				if (frm.is_dirty()) {
+					frappe.show_alert({
+						message: __("Save the Email Digest first.", "yellow"),
+						indicator: "yellow",
+					});
+					return;
+				}
+
 				return frm.call("send", null, () => {
 					frappe.show_alert({ message: __("Message Sent"), indicator: "green" });
 				});
