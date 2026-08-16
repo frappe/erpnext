b982e5b655c674a5a8b58d0de140f2c7df455b34
Diptanil Saha <diptanil@frappe.io>
2026-08-16 23:22:58 +0530

    chore: resolve conflicts

--- erpnext/accounts/doctype/bank_statement_import/bank_statement_import.py
+++ erpnext/accounts/doctype/bank_statement_import/bank_statement_import.py
@@ -167,13 +167,8 @@ def get_transaction_reference(txn_data: dict) -> str:
 	).strip()
 
 
-<<<<<<< HEAD
-@frappe.whitelist()
-def convert_mt940_to_csv(data_import, mt940_file_path):
-=======
 @frappe.whitelist(methods=["POST"])
-def convert_mt940_to_csv(data_import: str, mt940_file_path: str):
->>>>>>> 2f82e0dd36 (fix(bank_statement_import): add missing permission check on multiple whitelisted methods (#58221))
+def convert_mt940_to_csv(data_import, mt940_file_path):
 	doc = frappe.get_doc("Bank Statement Import", data_import)
 	doc.check_permission("write")
 
@@ -239,32 +234,17 @@ def convert_mt940_to_csv(data_import: str, mt940_file_path: str):
 
 
 @frappe.whitelist()
-<<<<<<< HEAD
 def get_preview_from_template(data_import, import_file=None, google_sheets_url=None):
-	return frappe.get_doc("Bank Statement Import", data_import).get_preview_from_template(
-		import_file, google_sheets_url
-	)
-
-
-@frappe.whitelist()
-def form_start_import(data_import):
-	job_id = frappe.get_doc("Bank Statement Import", data_import).start_import()
-	return job_id is not None
-=======
-def get_preview_from_template(
-	data_import: str, import_file: str | None = None, google_sheets_url: str | None = None
-):
 	bsi = frappe.get_doc("Bank Statement Import", data_import)
 	bsi.check_permission()
 	return bsi.get_preview_from_template(import_file, google_sheets_url)
 
 
 @frappe.whitelist()
-def form_start_import(data_import: str):
+def form_start_import(data_import):
 	bsi = frappe.get_doc("Bank Statement Import", data_import)
 	bsi.check_permission("write")
 	return bsi.start_import()
->>>>>>> 2f82e0dd36 (fix(bank_statement_import): add missing permission check on multiple whitelisted methods (#58221))
 
 
 @frappe.whitelist()
@@ -275,15 +255,10 @@ def download_errored_template(data_import_name):
 
 
 @frappe.whitelist()
-<<<<<<< HEAD
 def download_import_log(data_import_name):
-	return frappe.get_doc("Bank Statement Import", data_import_name).download_import_log()
-=======
-def download_import_log(data_import_name: str):
 	bsi = frappe.get_doc("Bank Statement Import", data_import_name)
 	bsi.check_permission()
 	return bsi.download_import_log()
->>>>>>> 2f82e0dd36 (fix(bank_statement_import): add missing permission check on multiple whitelisted methods (#58221))
 
 
 def is_mt940_format(content: str) -> bool:
