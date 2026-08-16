5a69bb900198e707aad7f66baf57b6f4b1a9c20b
Diptanil Saha <diptanil@frappe.io>
2026-08-16 19:32:24 +0530

    fix(crm)!: remove unused `get_last_interaction` endpoint (#58214)

--- erpnext/crm/doctype/utils.py
+++ erpnext/crm/doctype/utils.py
@@ -1,48 +1,4 @@
 import frappe
-from frappe.query_builder import Criterion
-
-
-@frappe.whitelist()
-def get_last_interaction(contact: str | None = None, lead: str | None = None):
-	if not contact and not lead:
-		return
-
-	last_communication = None
-	last_issue = None
-	if contact:
-		communication = frappe.qb.DocType("Communication")
-		link_conditions = []
-		contact = frappe.get_doc("Contact", contact)
-		for link in contact.links:
-			if link.link_doctype == "Customer":
-				last_issue = get_last_issue_from_customer(link.link_name)
-			link_conditions.append(
-				(communication.reference_doctype == link.link_doctype)
-				& (communication.reference_name == link.link_name)
-			)
-
-		if link_conditions:
-			last_communication = (
-				frappe.qb.from_(communication)
-				.select(communication.name, communication.content)
-				.where((communication.sent_or_received == "Received") & Criterion.any(link_conditions))
-				.orderby(communication.creation)
-				.limit(1)
-				.run(as_dict=1)
-			)
-
-	if lead:
-		last_communication = frappe.get_all(
-			"Communication",
-			filters={"reference_doctype": "Lead", "reference_name": lead, "sent_or_received": "Received"},
-			fields=["name", "content"],
-			order_by="creation desc",
-			limit=1,
-		)
-
-	last_communication = last_communication[0] if last_communication else None
-
-	return {"last_communication": last_communication, "last_issue": last_issue}
 
 
 def get_last_issue_from_customer(customer_name):
