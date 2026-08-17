dd4d10862deb5d7ea4fbdd6938dfb402b69c7857
rohitwaghchaure <rohitw1991@gmail.com>
2026-08-17 18:28:46 +0530

    fix: don't set work order status to In Process only due to skip material transfer (#58245)

--- erpnext/manufacturing/doctype/work_order/work_order.py
+++ erpnext/manufacturing/doctype/work_order/work_order.py
@@ -696,11 +696,7 @@ class WorkOrder(Document):
 		elif self.docstatus == 1:
 			if status not in ["Closed", "Stopped"]:
 				status = "Not Started"
-				if (
-					flt(self.material_transferred_for_manufacturing) > 0
-					or self.skip_transfer
-					or self._has_transferred_material()
-				):
+				if flt(self.material_transferred_for_manufacturing) > 0 or self._has_transferred_material():
 					status = "In Process"
 
 				precision = frappe.get_precision("Work Order", "produced_qty")
