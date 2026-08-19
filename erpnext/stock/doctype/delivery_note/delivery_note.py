696f0df81167abad1f1d3c5a419e5e1f30ad922d
Mihir Kandoi <kandoimihir@gmail.com>
2026-08-19 20:52:47 +0530

    fix(stock): update pick list status for product bundles (#58306)

--- erpnext/stock/doctype/delivery_note/delivery_note.py
+++ erpnext/stock/doctype/delivery_note/delivery_note.py
@@ -463,7 +463,6 @@ class DeliveryNote(SellingController):
 
 	def on_submit(self):
 		self.validate_packed_qty()
-		self.update_pick_list_status()
 
 		# Check for Approving Authority
 		frappe.get_cached_doc("Authorization Control").validate_approving_authority(
@@ -472,6 +471,7 @@ class DeliveryNote(SellingController):
 
 		# update delivered qty in sales order
 		self.update_prevdoc_status()
+		self.update_pick_list_status()
 		self.update_billing_status()
 
 		if not self.is_return:
