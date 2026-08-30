c76ae888c050d6d1359839febbe1f69cb2410758
ervishnucs <ervishnucs369@gmail.com>
2026-08-30 22:48:21 +0530

    fix(stock): carry accounting dimensions from landed cost voucher charges into gl entries

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -452,6 +452,9 @@ class PurchaseReceipt(BuyingController):
 		from erpnext.accounts.doctype.purchase_invoice.purchase_invoice import (
 			get_purchase_document_details,
 		)
+		from erpnext.stock.doctype.landed_cost_voucher.landed_cost_voucher import (
+			get_custom_dimension_overrides,
+		)
 
 		provisional_accounting_for_non_stock_items = cint(
 			frappe.db.get_value("Company", self.company, "enable_provisional_accounting_for_non_stock_items")
@@ -567,32 +570,38 @@ class PurchaseReceipt(BuyingController):
 
 		def make_landed_cost_gl_entries(item):
 			# Amount added through landed-cost-voucher
-			if item.landed_cost_voucher_amount and landed_cost_entries:
-				if (item.item_code, item.name) in landed_cost_entries:
-					for account, amount in landed_cost_entries[(item.item_code, item.name)].items():
-						account_currency = get_account_currency(account)
-						credit_amount = (
-							flt(amount["base_amount"])
-							if (amount["base_amount"] or account_currency != self.company_currency)
-							else flt(amount["amount"])
-						)
+			if not (item.landed_cost_voucher_amount and landed_cost_entries):
+				return
 
-						if not account:
-							validate_account("Landed Cost Account")
+			for entry in landed_cost_entries.get((item.item_code, item.name), []):
+				if not (entry.amount or entry.base_amount):
+					continue
 
-						self.add_gl_entry(
-							gl_entries=gl_entries,
-							account=account,
-							cost_center=item.cost_center,
-							debit=0.0,
-							credit=credit_amount,
-							remarks=remarks,
-							against_account=stock_asset_account_name,
-							credit_in_account_currency=flt(amount["amount"]),
-							account_currency=account_currency,
-							project=item.project,
-							item=item,
-						)
+				account = entry.expense_account
+				if not account:
+					validate_account("Landed Cost Account")
+
+				account_currency = get_account_currency(account)
+				credit_amount = (
+					flt(entry.base_amount)
+					if (entry.base_amount or account_currency != self.company_currency)
+					else flt(entry.amount)
+				)
+
+				self.add_gl_entry(
+					gl_entries=gl_entries,
+					account=account,
+					cost_center=entry.dimensions.cost_center or item.cost_center,
+					debit=0.0,
+					credit=credit_amount,
+					remarks=remarks,
+					against_account=stock_asset_account_name,
+					credit_in_account_currency=flt(entry.amount),
+					account_currency=account_currency,
+					project=entry.dimensions.project or item.project,
+					item=item,
+					dimensions=get_custom_dimension_overrides(entry),
+				)
 
 		def make_amount_difference_entry(item):
 			if item.amount_difference_with_purchase_invoice and stock_asset_rbnb:
@@ -1578,6 +1587,11 @@ def make_inter_company_delivery_note(source_name, target_doc=None):
 
 
 def get_item_account_wise_additional_cost(purchase_document):
+	from erpnext.stock.doctype.landed_cost_voucher.landed_cost_voucher import (
+		get_lcv_dimension_fields,
+		get_row_dimensions,
+	)
+
 	landed_cost_vouchers = frappe.get_all(
 		"Landed Cost Purchase Receipt",
 		fields=["parent"],
@@ -1588,6 +1602,7 @@ def get_item_account_wise_additional_cost(purchase_document):
 		return
 
 	item_account_wise_cost = {}
+	dimension_fields = get_lcv_dimension_fields()
 
 	for lcv in landed_cost_vouchers:
 		landed_cost_voucher_doc = frappe.get_doc("Landed Cost Voucher", lcv.parent)
@@ -1605,28 +1620,36 @@ def get_item_account_wise_additional_cost(purchase_document):
 
 		for item in landed_cost_voucher_doc.items:
 			if item.receipt_document == purchase_document:
+				charges = item_account_wise_cost.setdefault((item.item_code, item.purchase_receipt_item), {})
+
 				for account in landed_cost_voucher_doc.taxes:
 					exchange_rate = account.exchange_rate or 1
-					item_account_wise_cost.setdefault((item.item_code, item.purchase_receipt_item), {})
-					item_account_wise_cost[(item.item_code, item.purchase_receipt_item)].setdefault(
-						account.expense_account, {"amount": 0.0, "base_amount": 0.0}
+					dimensions = get_row_dimensions(account, item, dimension_fields)
+					group_key = (
+						account.expense_account,
+						tuple(dimensions.get(field) for field in dimension_fields),
 					)
 
-					item_row = item_account_wise_cost[(item.item_code, item.purchase_receipt_item)][
-						account.expense_account
-					]
+					item_row = charges.get(group_key)
+					if item_row is None:
+						item_row = charges[group_key] = frappe._dict(
+							expense_account=account.expense_account,
+							amount=0.0,
+							base_amount=0.0,
+							dimensions=dimensions,
+						)
 
 					if total_item_cost > 0:
-						item_row["amount"] += account.amount * item.get(based_on_field) / total_item_cost
+						item_row.amount += account.amount * item.get(based_on_field) / total_item_cost
 
-						item_row["base_amount"] += (
+						item_row.base_amount += (
 							account.base_amount * item.get(based_on_field) / total_item_cost
 						)
 					else:
-						item_row["amount"] += item.applicable_charges / exchange_rate
-						item_row["base_amount"] += item.applicable_charges
+						item_row.amount += item.applicable_charges / exchange_rate
+						item_row.base_amount += item.applicable_charges
 
-	return item_account_wise_cost
+	return {key: list(charges.values()) for key, charges in item_account_wise_cost.items()}
 
 
 @erpnext.allow_regional
