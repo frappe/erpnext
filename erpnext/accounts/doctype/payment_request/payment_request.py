2788a5e5e7a9fa6dadcbe037dc99e8357a505254
diptanilsaha <diptanil@frappe.io>
2026-08-31 12:13:42 +0530

    fix(accounts): added permission checks on `get_available_payment_schedules`

--- erpnext/accounts/doctype/payment_request/payment_request.py
+++ erpnext/accounts/doctype/payment_request/payment_request.py
@@ -1359,6 +1359,7 @@ def get_irequests_of_payment_request(doc: str | None = None) -> list:
 @frappe.whitelist()
 def get_available_payment_schedules(reference_doctype: str, reference_name: str):
 	ref_doc = frappe.get_doc(reference_doctype, reference_name)
+	ref_doc.check_permission()
 
 	if not hasattr(ref_doc, "payment_schedule") or not ref_doc.payment_schedule:
 		return []
