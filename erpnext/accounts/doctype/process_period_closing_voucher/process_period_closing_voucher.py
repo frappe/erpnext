05e3666e70d0436710ffcdd6f5eca1c45f657266
Diptanil Saha <diptanil@frappe.io>
2026-09-01 19:12:21 +0530

    fix: add missing perm checks on various whitelisted methods (#58652)
    (cherry picked from commit a20fc4f9317ecc7d262625aea680c9a0cda98cef)
    
    # Conflicts:
    #	erpnext/accounts/doctype/ledger_merge/ledger_merge.py
    #	erpnext/stock/doctype/delivery_note/delivery_note.py

--- erpnext/accounts/doctype/process_period_closing_voucher/process_period_closing_voucher.py
+++ erpnext/accounts/doctype/process_period_closing_voucher/process_period_closing_voucher.py
@@ -139,6 +139,8 @@ def start_pcv_processing(docname: str):
 
 @frappe.whitelist()
 def pause_pcv_processing(docname: str):
+	frappe.has_permission("Process Period Closing Voucher", ptype="write", doc=docname, throw=True)
+
 	ppcv = qb.DocType("Process Period Closing Voucher")
 	qb.update(ppcv).set(ppcv.status, "Paused").where(ppcv.name.eq(docname)).run()
 
@@ -154,6 +156,8 @@ def pause_pcv_processing(docname: str):
 
 @frappe.whitelist()
 def cancel_pcv_processing(docname: str):
+	frappe.has_permission("Process Period Closing Voucher", ptype="cancel", doc=docname, throw=True)
+
 	ppcv = qb.DocType("Process Period Closing Voucher")
 	qb.update(ppcv).set(ppcv.status, "Cancelled").where(ppcv.name.eq(docname)).run()
 
@@ -168,6 +172,8 @@ def cancel_pcv_processing(docname: str):
 
 @frappe.whitelist()
 def resume_pcv_processing(docname: str):
+	frappe.has_permission("Process Period Closing Voucher", ptype="write", doc=docname, throw=True)
+
 	ppcv = qb.DocType("Process Period Closing Voucher")
 	qb.update(ppcv).set(ppcv.status, "Running").where(ppcv.name.eq(docname)).run()
 
