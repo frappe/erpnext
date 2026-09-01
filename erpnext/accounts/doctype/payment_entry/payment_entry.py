93926a7487f80f8aa242e4c7c9d45ee40c5b3ded
diptanilsaha <diptanil@frappe.io>
2026-09-01 14:36:32 +0530

    fix(accounts): add permission checks on payment entry whitelisted methods

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -2897,7 +2897,11 @@ def get_payment_entry(
 	ignore_permissions=False,
 	created_from_payment_request=False,
 ):
+	frappe.has_permission("Payment Entry", ptype="create", throw=True)
+
 	doc = frappe.get_doc(dt, dn)
+	doc.check_permission()
+
 	over_billing_allowance = frappe.db.get_single_value("Accounts Settings", "over_billing_allowance")
 	if dt in ("Sales Order", "Purchase Order") and flt(doc.per_billed, 2) >= (100.0 + over_billing_allowance):
 		frappe.throw(_("Can only make payment against unbilled {0}").format(_(dt)))
