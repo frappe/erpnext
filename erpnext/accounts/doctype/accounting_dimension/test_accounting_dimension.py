7a55799c5016a2b8d162f7194550add31261210f
pandiyan <pandiyanpalani37@gmail.com>
2026-09-01 23:55:42 +0530

    test(accounts): cover blank dimension filters

--- erpnext/accounts/doctype/accounting_dimension/test_accounting_dimension.py
+++ erpnext/accounts/doctype/accounting_dimension/test_accounting_dimension.py
@@ -3,12 +3,28 @@
 
 import frappe
 
+from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
+	BLANK_ACCOUNTING_DIMENSION,
+	get_dimension_filter_sql_condition,
+)
 from erpnext.accounts.doctype.journal_entry.test_journal_entry import make_journal_entry
 from erpnext.accounts.doctype.sales_invoice.test_sales_invoice import create_sales_invoice
 from erpnext.tests.utils import ERPNextTestSuite
 
 
 class TestAccountingDimension(ERPNextTestSuite):
+	def test_blank_dimension_filter_sql_condition(self):
+		condition, values = get_dimension_filter_sql_condition(
+			"department",
+			f'["_Test Department - _TC", "{BLANK_ACCOUNTING_DIMENSION}"]',
+		)
+
+		self.assertEqual(
+			condition,
+			"(department IN (?) OR (department IS NULL OR department = ''))",
+		)
+		self.assertEqual(values, ["_Test Department - _TC"])
+
 	def test_dimension_against_sales_invoice(self):
 		si = create_sales_invoice(do_not_save=1)
 
