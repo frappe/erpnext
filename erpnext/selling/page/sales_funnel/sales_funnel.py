ee6591265b071646f4ff33f19cec36e28fc586b2
diptanilsaha <diptanil@frappe.io>
2026-09-06 22:25:36 +0530

    fix(selling): add permission check to get_funnel_data

--- erpnext/selling/page/sales_funnel/sales_funnel.py
+++ erpnext/selling/page/sales_funnel/sales_funnel.py
@@ -21,6 +21,8 @@ def validate_filters(from_date, to_date, company):
 
 @frappe.whitelist()
 def get_funnel_data(from_date: str, to_date: str, company: str):
+	frappe.has_permission("Company", doc=company, throw=True)
+
 	validate_filters(from_date, to_date, company)
 
 	lead = frappe.qb.DocType("Lead")
