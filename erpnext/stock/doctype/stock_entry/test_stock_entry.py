00729a8d627ac9e1a7c2c61de946a4aae10f372e
Sudharsanan11 <sudharsananashok1975@gmail.com>
2026-09-09 10:38:11 +0530

    fix: resolve conflicts

--- erpnext/stock/doctype/stock_entry/test_stock_entry.py
+++ erpnext/stock/doctype/stock_entry/test_stock_entry.py
@@ -3,12 +3,8 @@
 
 
 from frappe.permissions import add_user_permission, remove_user_permission
-<<<<<<< HEAD
 from frappe.tests.utils import FrappeTestCase, change_settings
-from frappe.utils import add_days, cstr, flt, get_time, getdate, nowtime, today
-=======
 from frappe.utils import add_days, cstr, flt, get_time, getdate, nowdate, nowtime, today
->>>>>>> b603581 (fix(stock): distribute additional costs when incoming items have no value)
 
 from erpnext.accounts.doctype.account.test_account import get_inventory_account
 from erpnext.controllers.accounts_controller import InvalidQtyError
