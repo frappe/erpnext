fd492100b0a4ebcdd900515211f81f456ffcdf7b
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-09 14:45:44 +0530

    fix(stock): ignore disabled price lists on transactions (#58926)

--- erpnext/startup/test_boot.py
+++ erpnext/startup/test_boot.py
@@ -20,3 +20,25 @@ class TestBoot(ERPNextTestSuite):
 
 		company_docs = [d for d in bootinfo.docs if d.get("doctype") == ":Company"]
 		self.assertTrue(any(d.get("name") == "_Test Company" for d in company_docs))
+
+	def test_boot_session_drops_disabled_price_list_default(self):
+		from erpnext.startup.boot import boot_session
+
+		price_list = frappe.get_doc(
+			{
+				"doctype": "Price List",
+				"price_list_name": frappe.generate_hash(length=10),
+				"currency": "INR",
+				"selling": 1,
+				"enabled": 0,
+			}
+		).insert(ignore_permissions=True)
+
+		bootinfo = frappe._dict(
+			sysdefaults=frappe._dict(selling_price_list=price_list.name),
+			page_info=frappe._dict(),
+			docs=[],
+		)
+		boot_session(bootinfo)
+
+		self.assertIsNone(bootinfo.sysdefaults.get("selling_price_list"))
