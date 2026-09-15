f3dcc6180f60fefc4319d9fdb3a2824642254ff2
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-15 12:11:21 +0530

    fix(stock): value stock entry rows as of the posting date (#59042)

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -34,7 +34,7 @@ from erpnext.stock.get_item_details import (
 	get_default_cost_center,
 )
 from erpnext.stock.stock_ledger import get_previous_sle, get_valuation_rate
-from erpnext.stock.utils import get_incoming_rate
+from erpnext.stock.utils import get_combine_datetime, get_incoming_rate
 
 from .services.disassemble import DisassembleStockEntry
 from .services.manufacturing import (
@@ -736,6 +736,18 @@ class StockEntry(StockController, SubcontractingInwardController):
 			raise_error_if_no_rate=raise_error_if_no_rate,
 			batch_no=d.batch_no,
 			serial_and_batch_bundle=d.serial_and_batch_bundle,
+			posting_datetime=get_combine_datetime(self.posting_date, self.posting_time),
+			creation=self.first_sle_creation,
+		)
+
+	@property
+	def first_sle_creation(self):
+		"""Creation of this entry's earliest ledger entry, if it has posted any yet."""
+		return frappe.db.get_value(
+			"Stock Ledger Entry",
+			{"voucher_no": self.name, "voucher_type": self.doctype, "is_cancelled": 0},
+			"creation",
+			order_by="creation asc",
 		)
 
 	def _notify_zero_valuation_rate(self, items):
--- erpnext/stock/stock_ledger.py
+++ erpnext/stock/stock_ledger.py
@@ -2155,6 +2155,15 @@ def get_sle_by_voucher_detail_no(voucher_detail_no):
 	)
 
 
+def get_prior_ledger_condition(table, posting_datetime, creation):
+	"""Restrict a ledger lookup to the entries that precede a voucher in ledger order."""
+	if creation:
+		return (table.posting_datetime < posting_datetime) | (
+			(table.posting_datetime == posting_datetime) & (table.creation < creation)
+		)
+	return table.posting_datetime <= posting_datetime
+
+
 def get_valuation_rate(
 	item_code,
 	warehouse,
@@ -2167,6 +2176,8 @@ def get_valuation_rate(
 	raise_error_if_no_rate=True,
 	batch_no=None,
 	serial_and_batch_bundle=None,
+	posting_datetime=None,
+	creation=None,
 ):
 	from erpnext.stock.serial_batch_bundle import BatchNoValuation
 
@@ -2188,6 +2199,9 @@ def get_valuation_rate(
 		if voucher_no:
 			# Comparing against a None voucher_no yields NULL, which filters out every row
 			query = query.where((table.voucher_no != voucher_no) | (table.voucher_type != voucher_type))
+
+		if posting_datetime:
+			query = query.where(get_prior_ledger_condition(table, posting_datetime, creation))
 
 		last_valuation_rate = query.run()
 		if last_valuation_rate and last_valuation_rate[0][0] is not None:
@@ -2234,6 +2248,11 @@ def get_valuation_rate(
 		# Comparing against a None voucher_no yields NULL, which filters out every row
 		last_sle_query = last_sle_query.where(
 			~((sle_entry.voucher_no == voucher_no) & (sle_entry.voucher_type == voucher_type))
+		)
+
+	if posting_datetime:
+		last_sle_query = last_sle_query.where(
+			get_prior_ledger_condition(sle_entry, posting_datetime, creation)
 		)
 
 	if last_valuation_rate := last_sle_query.run():
@@ -2671,13 +2690,7 @@ def get_stock_value_difference(
 	elif voucher_no:
 		query = query.where(table.voucher_no != voucher_no)
 
-	if creation:
-		query = query.where(
-			(table.posting_datetime < posting_datetime)
-			| ((table.posting_datetime == posting_datetime) & (table.creation < creation))
-		)
-	else:
-		query = query.where(table.posting_datetime <= posting_datetime)
+	query = query.where(get_prior_ledger_condition(table, posting_datetime, creation))
 
 	difference_amount = query.run()
 	return flt(difference_amount[0][0]) if difference_amount else 0
