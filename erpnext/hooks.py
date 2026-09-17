bf77e990662dd30e1fb8518592567e65a4c7e5b0
Rohit Waghchaure <rohitw1991@gmail.com>
2026-09-17 21:49:24 +0530

    feat: mark update_serial_batch_entries patch as never skippable

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -87,6 +87,9 @@ after_install = "erpnext.setup.install.after_install"
 after_app_install = "erpnext.setup.install.after_app_install"
 after_app_uninstall = "erpnext.setup.install.after_app_uninstall"
 
+# patches that must stop the migration when they fail, even with `bench migrate --skip-failing`
+never_skip_patches = ["erpnext.patches.v16_0.update_serial_batch_entries"]
+
 boot_session = "erpnext.startup.boot.boot_session"
 notification_config = "erpnext.startup.notifications.get_notification_config"
 get_help_messages = "erpnext.utilities.activation.get_help_messages"
