98fa0d4cb4508d55c023203ff70eec6da9b43586
Sudharsanan11 <sudharsananashok1975@gmail.com>
2026-09-22 21:34:57 +0530

    fix: resolve conflicts

--- erpnext/controllers/tests/test_price_list_validation.py
+++ erpnext/controllers/tests/test_price_list_validation.py
@@ -55,7 +55,7 @@ class TestPriceListValidation(ERPNextTestSuite):
 	def test_internal_transfer_should_keep_the_outward_price_list(self):
 		"""The inward document of an internal transfer takes the price list of the outward one, which
 		is flagged for the opposite side."""
-		from erpnext.stock.doctype.delivery_note.mapper import make_inter_company_purchase_receipt
+		from erpnext.stock.doctype.delivery_note.delivery_note import make_inter_company_purchase_receipt
 		from erpnext.stock.doctype.delivery_note.test_delivery_note import create_delivery_note
 		from erpnext.stock.doctype.purchase_receipt.test_purchase_receipt import (
 			prepare_data_for_internal_transfer,
