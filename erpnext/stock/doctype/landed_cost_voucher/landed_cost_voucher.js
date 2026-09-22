f739e9ce8975370c046d11fcc541e59961ac7628
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-22 16:15:09 +0530

    fix(stock): guard the landed cost distribution basis
    Name the basis in use in the zero-total message instead of always telling
    the user to switch to Quantity, and round the total to the basis field's
    precision first: rows that cancel leave a float residue (100.10 + 200.20 -
    300.30 sums to -5.68e-14) which passes the zero test and then divides the
    charges by it.
    
    Mirror both on the client, and clear the allocations when the basis totals
    zero so the form does not show values the server will reject.
    
    (backport #59274)

--- erpnext/stock/doctype/landed_cost_voucher/landed_cost_voucher.js
+++ erpnext/stock/doctype/landed_cost_voucher/landed_cost_voucher.js
@@ -99,24 +99,33 @@ erpnext.stock.LandedCostVoucher = class LandedCostVoucher extends erpnext.stock.
 	}
 
 	set_applicable_charges_for_item() {
-		var me = this;
-
 		if (this.frm.doc.taxes.length) {
 			var total_item_cost = 0.0;
 			var based_on = this.frm.doc.distribute_charges_based_on.toLowerCase();
 
 			if (based_on != "distribute manually") {
-				$.each(this.frm.doc.items || [], function (i, d) {
-					total_item_cost += flt(d[based_on]);
+				var items = this.frm.doc.items || [];
+				items.forEach((item) => {
+					total_item_cost += flt(item[based_on]);
 				});
 
+				if (items.length) {
+					total_item_cost = flt(total_item_cost, precision(based_on, items[0]));
+				}
+
+				if (!total_item_cost) {
+					items.forEach((item) => {
+						item.applicable_charges = 0;
+					});
+					refresh_field("items");
+					return;
+				}
+
 				var total_charges = 0.0;
-				$.each(this.frm.doc.items || [], function (i, item) {
-					item.applicable_charges =
-						(flt(item[based_on]) * flt(me.frm.doc.total_taxes_and_charges)) /
-						flt(total_item_cost);
+				items.forEach((item) => {
 					item.applicable_charges = flt(
-						item.applicable_charges,
+						(flt(item[based_on]) * flt(this.frm.doc.total_taxes_and_charges)) /
+							flt(total_item_cost),
 						precision("applicable_charges", item)
 					);
 					total_charges += item.applicable_charges;
@@ -124,7 +133,7 @@ erpnext.stock.LandedCostVoucher = class LandedCostVoucher extends erpnext.stock.
 
 				if (total_charges != this.frm.doc.total_taxes_and_charges) {
 					var diff = this.frm.doc.total_taxes_and_charges - flt(total_charges);
-					this.frm.doc.items.slice(-1)[0].applicable_charges += diff;
+					items.slice(-1)[0].applicable_charges += diff;
 				}
 				refresh_field("items");
 			}
