9db210c57b9b7e1ff7901cfd5a8e89dfd5264398
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-23 15:39:52 +0530

    test(stock): serial and batch wise stock balance report

---
+++ erpnext/stock/report/serial_and_batch_wise_stock_balance/test_serial_and_batch_wise_stock_balance.py
@@ -0,0 +1,76 @@
+import frappe
+from frappe import _dict
+from frappe.utils import today
+
+from erpnext.stock.doctype.item.test_item import make_item
+from erpnext.stock.doctype.stock_entry.stock_entry_utils import make_stock_entry
+from erpnext.stock.report.serial_and_batch_wise_stock_balance.serial_and_batch_wise_stock_balance import (
+	execute,
+)
+from erpnext.tests.utils import ERPNextTestSuite
+
+WAREHOUSE = "Stores - _TC"
+RECEIVE = {"rate": 100, "to_warehouse": WAREHOUSE}
+ISSUE = {"from_warehouse": WAREHOUSE}
+
+
+class TestSerialAndBatchWiseStockBalance(ERPNextTestSuite):
+	def run_report(self, properties, movements):
+		item_code = make_item(properties={"is_stock_item": 1, **properties}).name
+		for movement in movements:
+			make_stock_entry(item_code=item_code, **movement)
+
+		filters = _dict(
+			company="_Test Company",
+			item_code=[item_code],
+			warehouse=WAREHOUSE,
+			from_date="2020-01-01",
+			to_date=today(),
+		)
+		return [_dict(row) for row in execute(filters)[1]]
+
+	def get_serial_nos_in_warehouse(self, item_code, batch_no=None):
+		filters = {"item_code": item_code, "warehouse": WAREHOUSE}
+		if batch_no:
+			filters["batch_no"] = batch_no
+
+		return "\n".join(sorted(frappe.get_all("Serial No", filters=filters, pluck="name")))
+
+	def test_batch_rows_under_item_row(self):
+		rows = self.run_report(
+			{"has_batch_no": 1, "create_new_batch": 1, "batch_number_series": "SBW-B-.#####"},
+			[{"qty": 10, **RECEIVE}, {"qty": 20, **RECEIVE}, {"qty": 5, **ISSUE}],
+		)
+
+		self.assertEqual(
+			[(row.indent, row.in_qty, row.out_qty, row.bal_qty, row.bal_val) for row in rows],
+			[(0, 30, 5, 25, 2500), (1, 10, 5, 5, 500), (1, 20, 0, 20, 2000)],
+		)
+		self.assertTrue(all(row.batch_no for row in rows[1:]))
+
+	def test_serial_nos_in_stock_on_item_row(self):
+		rows = self.run_report(
+			{"has_serial_no": 1, "serial_no_series": "SBW-S-.#####"},
+			[{"qty": 3, **RECEIVE}, {"qty": 1, **ISSUE}],
+		)
+
+		self.assertEqual(len(rows), 1)
+		self.assertEqual(len(rows[0].serial_no.split("\n")), 2)
+		self.assertEqual(rows[0].serial_no, self.get_serial_nos_in_warehouse(rows[0].item_code))
+
+	def test_serial_nos_in_stock_per_batch(self):
+		rows = self.run_report(
+			{
+				"has_batch_no": 1,
+				"create_new_batch": 1,
+				"batch_number_series": "SBW-SB-.#####",
+				"has_serial_no": 1,
+				"serial_no_series": "SBW-SBS-.#####",
+			},
+			[{"qty": 2, **RECEIVE}, {"qty": 2, **RECEIVE}, {"qty": 1, **ISSUE}],
+		)
+
+		self.assertEqual([row.bal_qty for row in rows], [3, 1, 2])
+		self.assertEqual(rows[0].serial_no, "")
+		for row in rows[1:]:
+			self.assertEqual(row.serial_no, self.get_serial_nos_in_warehouse(row.item_code, row.batch_no))
