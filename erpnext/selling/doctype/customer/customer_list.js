ccffaebf6282590b7f3a00ea07e34351285ccedc
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-23 11:44:13 +0530

    fix(selling): hide the On Hold indicator after the release date

--- erpnext/selling/doctype/customer/customer_list.js
+++ erpnext/selling/doctype/customer/customer_list.js
@@ -1,7 +1,16 @@
 frappe.listview_settings["Customer"] = {
-	add_fields: ["customer_name", "territory", "customer_group", "customer_type", "image", "on_hold"],
+	add_fields: [
+		"customer_name",
+		"territory",
+		"customer_group",
+		"customer_type",
+		"image",
+		"on_hold",
+		"release_date",
+	],
 	get_indicator: function (doc) {
-		if (!cint(doc.disabled) && cint(doc.on_hold)) {
+		const is_hold_active = !doc.release_date || doc.release_date >= frappe.datetime.get_today();
+		if (!cint(doc.disabled) && cint(doc.on_hold) && is_hold_active) {
 			return [__("On Hold"), "red", "on_hold,=,1"];
 		}
 	},
