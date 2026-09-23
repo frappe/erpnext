c019bb5c28a3a7df3a112c0bc7622333c7d504d4
pandiyan <pandiyanpalani37@gmail.com>
2026-09-23 18:45:02 +0530

    fix(selling): use preferred addresses in customer quotations

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -464,15 +464,11 @@ class Customer(TransactionBase):
 
 @frappe.whitelist()
 def make_quotation(source_name: str, target_doc: str | Document | None = None) -> Document:
-	def set_missing_values(source, target):
-		_set_missing_values(source, target)
-
 	target_doc = get_mapped_doc(
 		"Customer",
 		source_name,
 		{"Customer": {"doctype": "Quotation", "field_map": {"name": "party_name"}}},
 		target_doc,
-		set_missing_values,
 	)
 
 	target_doc.quotation_to = "Customer"
