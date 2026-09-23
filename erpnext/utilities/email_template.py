214e90caf0e1aec766a3e21d850f112ef48b036b
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-23 19:18:40 +0530

    chore: mark email template rendering as reviewed for semgrep
    The template comes from an Email Template record, which only System
    Managers can write, and renders with restrict_globals.

--- erpnext/utilities/email_template.py
+++ erpnext/utilities/email_template.py
@@ -9,6 +9,8 @@ def get_email_subject_and_message(
 		return default_subject, default_message
 
 	template = frappe.get_cached_doc("Email Template", template_name)
+	# nosemgrep: frappe-semgrep-rules.rules.security.frappe-ssti
 	subject = frappe.render_template(template.subject, context, restrict_globals=True)
+	# nosemgrep: frappe-semgrep-rules.rules.security.frappe-ssti
 	message = frappe.render_template(template.response_, context, restrict_globals=True)
 	return subject, message
