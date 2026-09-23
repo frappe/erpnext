a05229b9883d6086fb43782d7c9f629cab0d0263
nishkagosalia <nishka.gosalia@gmail.com>
2026-09-23 15:56:11 +0530

    fix(UX): Job card cleanup

--- erpnext/manufacturing/doctype/job_card/job_card.py
+++ erpnext/manufacturing/doctype/job_card/job_card.py
@@ -88,8 +88,8 @@ class JobCard(Document):
 		amended_from: DF.Link | None
 		backflush_from_wip_warehouse: DF.Check
 		barcode: DF.Barcode | None
-		batch_split: DF.Check
 		batch_no: DF.Link | None
+		batch_split: DF.Check
 		bom_no: DF.Link | None
 		company: DF.Link
 		employee: DF.TableMultiSelect[JobCardTimeLog]
@@ -145,9 +145,9 @@ class JobCard(Document):
 		time_required: DF.Float
 		total_completed_qty: DF.Float
 		total_time_in_mins: DF.Float
-		weight_per_piece: DF.Float
 		track_semi_finished_goods: DF.Check
 		transferred_qty: DF.Float
+		weight_per_piece: DF.Float
 		wip_warehouse: DF.Link | None
 		work_order: DF.Link
 		workstation: DF.Link
