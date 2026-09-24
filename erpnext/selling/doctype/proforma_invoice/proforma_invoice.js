b03e48f45365b97f7f83acf3ee91d596a8970f3e
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-24 21:50:30 +0530

    fix(selling): block amending a cancelled Proforma Invoice
    A proforma is only created from its Sales Order, but a cancelled one
    showed Amend to Administrator, and the amended copy could be saved
    outside that path.

--- erpnext/selling/doctype/proforma_invoice/proforma_invoice.js
+++ erpnext/selling/doctype/proforma_invoice/proforma_invoice.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Proforma Invoice", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Proforma Invoice", {
+	refresh(frm) {
+		frm.page.btn_primary.toggle(frm.doc.docstatus !== 2);
+	},
+});
