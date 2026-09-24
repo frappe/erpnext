7faf5259687f77fc3cb816502d3554491cdfb5fa
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-24 13:41:14 +0530

    test(selling): copy the margin test quotation once
    Copying an unsaved quotation a second time turns the unset Is Active into 0, so insert kept the quotation inactive and the Sales Order mapper rejected it. One copy leaves the field unset, and insert applies its default.

--- erpnext/selling/doctype/quotation/test_quotation.py
+++ erpnext/selling/doctype/quotation/test_quotation.py
@@ -656,15 +656,14 @@ class TestQuotation(ERPNextTestSuite):
 
 		rate_with_margin = flt((1500 * 18.75) / 100 + 1500)
 
-		test_record = frappe.copy_doc(self.globalTestRecords["Quotation"][0])
+		quotation = frappe.copy_doc(self.globalTestRecords["Quotation"][0])
 
-		test_record.items[0].price_list_rate = 1500
-		test_record.items[0].margin_type = "Percentage"
-		test_record.items[0].margin_rate_or_amount = 18.75
+		quotation.items[0].price_list_rate = 1500
+		quotation.items[0].margin_type = "Percentage"
+		quotation.items[0].margin_rate_or_amount = 18.75
 		# set rate to zero, so that it is recalculated on save
-		test_record.items[0].rate = 0
+		quotation.items[0].rate = 0
 
-		quotation = frappe.copy_doc(test_record)
 		quotation.transaction_date = nowdate()
 		quotation.valid_till = add_months(quotation.transaction_date, 1)
 		quotation.insert()
