03e6e96d914cf1b1ba4d13219313da7ed6dece4d
Pandiyan P <pandiyanpalani37@gmail.com>
2026-09-25 12:27:59 +0530

    fix(manufacturing): refresh planned qty after work order status update

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -2232,10 +2232,10 @@ class StockEntry(StockController):
 
 			if self.fg_completed_qty:
 				pro_doc.run_method("update_work_order_qty")
-				if self.purpose == "Manufacture":
-					pro_doc.run_method("update_planned_qty")
 
 			pro_doc.run_method("update_status")
+			if self.fg_completed_qty and self.purpose == "Manufacture":
+				pro_doc.run_method("update_planned_qty")
 			if not pro_doc.operations:
 				pro_doc.set_actual_dates()
 
