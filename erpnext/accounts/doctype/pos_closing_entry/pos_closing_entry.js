5842a0ae9c067bb497174aa0cc9a571ad1095786
Shllokkk <shllokosan23@gmail.com>
2026-09-25 12:37:11 +0530

    fix: default closing amount for payment modes not in POS opening entry
    (cherry picked from commit ec775eb78274f5d78ebe8cbe3aef281fe8932a09)

--- erpnext/accounts/doctype/pos_closing_entry/pos_closing_entry.js
+++ erpnext/accounts/doctype/pos_closing_entry/pos_closing_entry.js
@@ -203,7 +203,7 @@ function refresh_payments(d, frm, is_new) {
 				mode_of_payment: p.mode_of_payment,
 				opening_amount: 0,
 				expected_amount: p.amount,
-				closing_amount: 0,
+				closing_amount: p.amount,
 			});
 		}
 	});
