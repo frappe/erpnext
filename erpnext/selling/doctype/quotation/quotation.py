ab707bba6646cd3aef572a3342e9bf4504a4159d
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-26 18:55:45 +0530

    fix(selling): recalculate the latest quotation revision before cancel
    Adding the call to on_cancel made Semgrep report the existing unsaved lost_reasons change there as new. before_cancel runs with the quotation already marked cancelled, so the result is the same and on_cancel stays as it is on develop.

--- erpnext/selling/doctype/quotation/quotation.py
+++ erpnext/selling/doctype/quotation/quotation.py
@@ -454,6 +454,10 @@ class Quotation(SellingController):
 			ignore_permissions=ignore_permissions,
 		)
 
+	def before_cancel(self):
+		super().before_cancel()
+		self.update_latest_revision()
+
 	def on_cancel(self):
 		if self.lost_reasons:
 			self.lost_reasons = []
@@ -463,7 +467,6 @@ class Quotation(SellingController):
 		self.set_status(update=True)
 		self.update_opportunity("Open")
 		self.update_lead()
-		self.update_latest_revision()
 
 	def carry_forward_communication(self):
 		from erpnext.crm.utils import copy_comments, link_communications
